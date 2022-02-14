a3613f52a2ba30a5a326d77f2dcaac8ce55ccf54
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-14 05:20:05 +0000

    omniorb 4.3.0
    * omniorb 4.3.0
    * omniorb: override python version, update license, fix build
    
    Co-authored-by: Michael Cho <cho-m@tuta.io>

--- Formula/omniorb.rb
+++ Formula/omniorb.rb
@@ -1,10 +1,9 @@
 class Omniorb < Formula
   desc "IOR and naming service utilities for omniORB"
   homepage "https://omniorb.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/omniorb/omniORB/omniORB-4.2.4/omniORB-4.2.4.tar.bz2"
-  sha256 "28c01cd0df76c1e81524ca369dc9e6e75f57dc70f30688c99c67926e4bdc7a6f"
-  license "GPL-2.0-or-later"
-  revision 2
+  url "https://downloads.sourceforge.net/project/omniorb/omniORB/omniORB-4.3.0/omniORB-4.3.0.tar.bz2"
+  sha256 "976045a2341f4e9a85068b21f4bd928993292933eeecefea372db09e0219eadd"
+  license all_of: ["GPL-2.0-or-later", "LGPL-2.1-or-later"]
 
   livecheck do
     url :stable
@@ -26,8 +25,8 @@ class Omniorb < Formula
   depends_on "python@3.11"
 
   resource "bindings" do
-    url "https://downloads.sourceforge.net/project/omniorb/omniORBpy/omniORBpy-4.2.4/omniORBpy-4.2.4.tar.bz2"
-    sha256 "dae8d867559cc934002b756bc01ad7fabbc63f19c2d52f755369989a7a1d27b6"
+    url "https://downloads.sourceforge.net/project/omniorb/omniORBpy/omniORBpy-4.3.0/omniORBpy-4.3.0.tar.bz2"
+    sha256 "fffcfdfc34fd6e2fcc45d803d7d5db5bd4d188a747ff9f82b3684a753e001b4d"
   end
 
   def install
@@ -45,6 +44,7 @@ class Omniorb < Formula
                 /am_cv_python_version=`.*`/,
                 "am_cv_python_version='#{xy}'"
       system "./configure", "--prefix=#{prefix}"
+      ENV.deparallelize # omnipy.cc:392:44: error: use of undeclared identifier 'OMNIORBPY_DIST_DATE'
       system "make", "install"
     end
   end
