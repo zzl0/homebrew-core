9a5f510f41c609564f104a13604556dde797dce6
PaperChalice <lgamma@163.com>
2022-05-27 08:33:53 +0800

    libxcb: rebuild
    * libxcb: drop `libpthread-stubs`
    * Update libxcb.rb
    
    Closes #111928.
    
    Co-authored-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: Michael Cho <20700669+cho-m@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libxcb.rb
+++ Formula/libxcb.rb
@@ -20,9 +20,12 @@ class Libxcb < Formula
   depends_on "pkg-config" => :build
   depends_on "python@3.11" => :build # match version in `xcb-proto`
   depends_on "xcb-proto" => :build
-  depends_on "libpthread-stubs"
   depends_on "libxau"
   depends_on "libxdmcp"
+
+  # Drop libpthread-stubs on macOS
+  # remove in next release
+  patch :DATA
 
   def install
     python3 = "python3.11"
@@ -98,3 +101,18 @@ class Libxcb < Formula
     assert_equal 0, $CHILD_STATUS.exitstatus
   end
 end
+
+__END__
+diff --git a/configure b/configure
+index 2503d4b..0c36685 100755
+--- a/configure
++++ b/configure
+@@ -20662,7 +20662,7 @@ printf "%s\n" "yes" >&6; }
+ fi
+ NEEDED="xau >= 0.99.2"
+ case $host_os in
+-linux*) ;;
++linux*|darwin*) ;;
+      *) NEEDED="$NEEDED pthread-stubs" ;;
+ esac
+ 
