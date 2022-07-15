eaa68a996f130d844b9de041879e3f6430bd5cfc
McKayla Washburn <mckayla@hey.com>
2022-07-14 21:50:09 -0600

    unisonlang M4e
    * unisonlang M4
    * use M4 tag of unison-local-ui
    * fix local-ui hash
    * unisonlang M4e
    
    Closes #105842.
    
    Co-authored-by: Michael Cho <cho-m@tuta.io>
    Signed-off-by: Michael Cho <20700669+cho-m@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/unisonlang.rb
+++ Formula/unisonlang.rb
@@ -4,11 +4,16 @@ class Unisonlang < Formula
   desc "Friendly programming language from the future"
   homepage "https://unison-lang.org/"
   url "https://github.com/unisonweb/unison.git",
-      tag:      "release/M3",
-      revision: "97cce8d23147df30b0f80ae745968db09f4e1a44"
-  version "M3"
+      tag:      "release/M4e",
+      revision: "af0bc1325918c077fc62f9a7d2c3937d36d53563"
+  version "M4e"
   license "MIT"
   head "https://github.com/unisonweb/unison.git", branch: "trunk"
+
+  livecheck do
+    url :stable
+    regex(%r{^release/(M\d+[a-z]*)$}i)
+  end
 
   bottle do
     sha256 cellar: :any_skip_relocation, monterey:     "e95f66e764c2c11f2df422ae879d467ff41dae657fe5447a1aeef4fcfe9b781a"
@@ -17,26 +22,32 @@ class Unisonlang < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux: "8e01b2d90970cb66721cf5f5e5135a7867112fe6239250ea2b0cc5eeebb4f33e"
   end
 
-  depends_on "ghc" => :build
+  depends_on "ghc@8.10" => :build
   depends_on "haskell-stack" => :build
-  depends_on "node" => :build
+  depends_on "node@18" => :build
 
-  # stack/ghc currently have a number of issues building for aarch64
-  # https://github.com/unisonweb/unison/issues/3136
-  depends_on arch: :x86_64
-
+  uses_from_macos "python" => :build
   uses_from_macos "xz" => :build
   uses_from_macos "zlib"
 
-  resource "codebase-ui" do
-    url "https://github.com/unisonweb/codebase-ui/archive/refs/tags/release/M3.tar.gz"
-    sha256 "84be9135a821615f8fc73a0a894aa46a11c55393c43dc26e16a5ce75f8063012"
-    version "M3"
+  resource "local-ui" do
+    url "https://github.com/unisonweb/unison-local-ui/archive/refs/tags/release/M4e.tar.gz"
+    sha256 "9caf016902a334db1109fd51c0aceaf7f64645201d1f1c44f45e7aaf9fd2a3d3"
+    version "M4e"
   end
 
   def install
     jobs = ENV.make_jobs
     ENV.deparallelize
+
+    # Build and install the web interface
+    resource("local-ui").stage do
+      system "npm", "install", *Language::Node.local_npm_install_args
+      system "npm", "run", "ui-core:install"
+      system "npm", "run", "build"
+
+      prefix.install "dist/unisonLocal" => "ui"
+    end
 
     stack_args = [
       "-v",
@@ -51,19 +62,11 @@ class Unisonlang < Formula
 
     prefix.install "unison" => "ucm"
     bin.install_symlink prefix/"ucm"
-
-    # Build and install the web interface
-    resource("codebase-ui").stage do
-      system "npm", "install", *Language::Node.local_npm_install_args
-      system "npm", "run", "build"
-
-      prefix.install "dist/unisonLocal" => "ui"
-    end
   end
 
   test do
-    # Ensure the codebase-ui version matches the ucm version
-    assert_equal version, resource("codebase-ui").version
+    # Ensure the local-ui version matches the ucm version
+    assert_equal version, resource("local-ui").version
 
     # Initialize a codebase by starting the server/repl, but then run the "exit" command
     # once everything is set up.
