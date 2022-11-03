e715c82ef884df8a77806edcbda00e09aa2839a1
Michael Cho <cho-m@tuta.io>
2022-11-02 21:15:12 -0700

    pyside@2 5.15.7
    Closes #114575.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pyside@2.rb
+++ Formula/pyside@2.rb
@@ -1,10 +1,14 @@
 class PysideAT2 < Formula
   desc "Official Python bindings for Qt"
   homepage "https://wiki.qt.io/Qt_for_Python"
-  url "https://download.qt.io/official_releases/QtForPython/pyside2/PySide2-5.15.5-src/pyside-setup-opensource-src-5.15.5.tar.xz"
-  sha256 "3920a4fb353300260c9bc46ff70f1fb975c5e7efa22e9d51222588928ce19b33"
+  url "https://download.qt.io/official_releases/QtForPython/pyside2/PySide2-5.15.7-src/pyside-setup-opensource-src-5.15.7.tar.xz"
+  sha256 "f61210ae24e6882d5d0ca0059229e5dc4f35e2bca92dd6caf96c0f41943a8294"
   license all_of: ["GFDL-1.3-only", "GPL-2.0-only", "GPL-3.0-only", "LGPL-3.0-only"]
-  revision 3
+
+  livecheck do
+    url "https://download.qt.io/official_releases/QtForPython/pyside2/"
+    regex(%r{href=.*?PySide2[._-]v?(\d+(?:\.\d+)+)-src/}i)
+  end
 
   bottle do
     sha256 cellar: :any,                 arm64_ventura:  "160ad065834104b2a9f3077f49f7c2fe05784442e8ccb8b20dd2a1e7e6082592"
@@ -19,32 +23,46 @@ class PysideAT2 < Formula
   keg_only :versioned_formula
 
   depends_on "cmake" => :build
+  depends_on "llvm"
   depends_on "python@3.10"
   depends_on "qt@5"
 
   uses_from_macos "libxml2"
   uses_from_macos "libxslt"
 
-  on_macos do
-    depends_on "llvm"
-  end
-
   on_linux do
     depends_on "libxcb"
-    depends_on "llvm@14" # Needed until Mesa 22.2.0.
     depends_on "mesa"
   end
 
   fails_with gcc: "5"
 
-  def python3
-    "python3.10"
-  end
-
   # Don't copy qt@5 tools.
   patch do
     url "https://src.fedoraproject.org/rpms/python-pyside2/raw/009100c67a63972e4c5252576af1894fec2e8855/f/pyside2-tools-obsolete.patch"
     sha256 "ede69549176b7b083f2825f328ca68bd99ebf8f42d245908abd320093bac60c9"
+  end
+
+  # Fix build using ArchLinux patch. The corresponding code in shiboken6 was modified
+  # in a later commit. Can remove when upstream decides to backport a fix to shiboken2.
+  # Ref: https://codereview.qt-project.org/c/pyside/pyside-setup/+/365002
+  patch do
+    url "https://raw.githubusercontent.com/archlinux/svntogit-packages/7a6b2d1862ed9cbdd7ebcbcc9d6635e67cd1feab/trunk/fix-build.patch"
+    sha256 "4f8815840b5df2e1e3b9f201a3402126821b0d9702920cefdc18050639143ad1"
+  end
+
+  # Fix error: use of undeclared identifier 'NPY_ARRAY_UPDATEIFCOPY'.
+  # Remove in v5.15.10 as PYSIDE-2035 mentions fix was backported there.
+  # Ref: https://codereview.qt-project.org/c/pyside/pyside-setup/+/418321
+  # Ref: https://bugreports.qt.io/browse/PYSIDE-2035
+  patch :p3 do
+    url "https://code.qt.io/cgit/pyside/pyside-setup.git/patch/sources/shiboken6/libshiboken/sbknumpyarrayconverter.cpp?id=1422cf4a7f277fb13fd209f24a90d6c02641497d"
+    sha256 "8f80bc04be1e22cf3279f8893159d8e7d6f86a6b1021808ff8942549944df40c"
+    directory "sources/shiboken2"
+  end
+
+  def python3
+    "python3.10"
   end
 
   def install
@@ -59,24 +77,19 @@ class PysideAT2 < Formula
                 "--include-paths=${shiboken_include_dirs}",
                 "--include-paths=${shiboken_include_dirs}:#{extra_include_dirs.join(":")}"
 
-      # Avoid shim reference.
-      inreplace "sources/shiboken2/ApiExtractor/CMakeLists.txt",
-                "${CMAKE_CXX_COMPILER}",
-                "/usr/bin/c++"
-
       # Add rpath to qt@5 because it is keg-only.
       [lib, Formula["qt@5"].opt_lib]
     end
 
-    ENV.append_path "CMAKE_PREFIX_PATH", Formula["qt@5"].opt_lib
-    args = %W[
-      -DCMAKE_CXX_COMPILER=#{ENV.cxx}
-      -DPYTHON_EXECUTABLE=#{which(python3)}
-      -DCMAKE_INSTALL_RPATH=#{rpaths.join(";")}
-      -DFORCE_LIMITED_API=yes
-    ]
+    # Avoid shim reference.
+    inreplace "sources/shiboken2/ApiExtractor/CMakeLists.txt", "${CMAKE_CXX_COMPILER}", ENV.cxx
 
-    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    ENV.append_path "CMAKE_PREFIX_PATH", Formula["qt@5"].opt_lib
+    system "cmake", "-S", ".", "-B", "build",
+                    "-DPYTHON_EXECUTABLE=#{which(python3)}",
+                    "-DCMAKE_INSTALL_RPATH=#{rpaths.join(";")}",
+                    "-DFORCE_LIMITED_API=yes",
+                    *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
   end
