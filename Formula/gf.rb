ba34ac8e33653fb86f8cea91d2d0fe8456d44e26
Rainshaw <rxg@live.com>
2022-11-13 17:56:51 +0800

    gf 2.2.5 (new formula)
    * gf 2.2.2 (new formula)
    * Apply suggestions from code review
    * gf 2.2.5
    
    Closes #115650.
    
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/gf.rb
@@ -0,0 +1,25 @@
+class Gf < Formula
+  desc "App development framework of Golang"
+  homepage "https://goframe.org"
+  url "https://github.com/gogf/gf/archive/refs/tags/v2.2.5.tar.gz"
+  sha256 "908e0aa7c814da997e731610090732c8d297d178b01e5146f88610fe585e2e92"
+  license "MIT"
+  head "https://github.com/gogf/gf.git", branch: "master"
+
+  depends_on "go" => :build
+
+  def install
+    cd "cmd/gf" do
+      system "go", "build", *std_go_args(ldflags: "-s -w")
+    end
+  end
+
+  test do
+    output = shell_output("#{bin}/gf --version 2>&1")
+    assert_match "GoFrame CLI Tool v#{version}, https://goframe.org", output
+    assert_match "GoFrame Version: cannot find go.mod", output
+
+    output = shell_output("#{bin}/gf init test 2>&1")
+    assert_match "you can now run \"cd test && gf run main.go\" to start your journey, enjoy!", output
+  end
+end
