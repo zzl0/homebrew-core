9c7ae84a64d6b518b40d2e6278d07e8c4d557489
Rui Chen <rui@chenrui.dev>
2022-11-16 17:46:43 -0500

    lapack 3.11
    Closes #115963.
    
    Signed-off-by: Daniel Nachun <9205048+danielnachun@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/lapack.rb
+++ Formula/lapack.rb
@@ -1,10 +1,9 @@
 class Lapack < Formula
   desc "Linear Algebra PACKage"
   homepage "https://www.netlib.org/lapack/"
-  url "https://github.com/Reference-LAPACK/lapack/archive/v3.10.1.tar.gz"
-  sha256 "cd005cd021f144d7d5f7f33c943942db9f03a28d110d6a3b80d718a295f7f714"
+  url "https://github.com/Reference-LAPACK/lapack/archive/v3.11.tar.gz"
+  sha256 "5a5b3bac27709d8c66286b7a0d1d7bf2d7170ec189a1a756fdf812c97aa7fd10"
   license "BSD-3-Clause"
-  revision 1
   head "https://github.com/Reference-LAPACK/lapack.git", branch: "master"
 
   bottle do
