11f71ffc243a3a875438a5dacb977a09a8d90363
Dan <dan@wadoku.de>
2022-11-17 22:27:15 +0100

    mariadb 10.10.2

--- Formula/mariadb.rb
+++ Formula/mariadb.rb
@@ -1,8 +1,8 @@
 class Mariadb < Formula
   desc "Drop-in replacement for MySQL"
   homepage "https://mariadb.org/"
-  url "https://downloads.mariadb.com/MariaDB/mariadb-10.9.4/source/mariadb-10.9.4.tar.gz"
-  sha256 "1dff08a0f37ea5cf8f00cbd12d40e80759fae7d73184ccf56b5b51acfdcfc054"
+  url "https://downloads.mariadb.com/MariaDB/mariadb-10.10.2/source/mariadb-10.10.2.tar.gz"
+  sha256 "57cbd0112b22b592f657cd4eb82e2f36ad901351317bf8e17849578e803f3cb2"
   license "GPL-2.0-only"
 
   # This uses a placeholder regex to satisfy the `PageMatch` strategy
@@ -57,7 +57,7 @@ class Mariadb < Formula
 
   fails_with gcc: "5"
 
-  # fix compilation, remove in 10.9.5
+  # fix compilation, remove in 10.10.3
   patch do
     url "https://github.com/mariadb-corporation/mariadb-connector-c/commit/44383e3df4896f2d04d9141f640934d3e74e04d7.patch?full_index=1"
     sha256 "3641e17e29dc7c9bf24bc23e4d68da81f0d9f33b0568f8ff201c4ebc0487d26a"
