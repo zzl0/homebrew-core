fa78e8a13aef443d0f57e5908d976a3e3a9b876c
Rui Chen <rui@chenrui.dev>
2022-11-27 16:00:17 -0500

    brew-pip: update test
    update build
    
    Closes #116795.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/brew-pip.rb
+++ Formula/brew-pip.rb
@@ -1,4 +1,6 @@
 class BrewPip < Formula
+  include Language::Python::Shebang
+
   desc "Install pip packages as homebrew formulae"
   homepage "https://github.com/hanxue/brew-pip"
   url "https://github.com/hanxue/brew-pip/archive/0.4.1.tar.gz"
@@ -17,11 +19,14 @@ class BrewPip < Formula
   # Repository is not maintained in 9+ years
   deprecate! date: "2022-04-16", because: :unmaintained
 
+  depends_on "python@3.11"
+
   def install
     bin.install "bin/brew-pip"
+    rewrite_shebang detected_python_shebang, bin/"brew-pip"
   end
 
   test do
-    system "#{bin}/brew-pip", "help"
+    system "#{bin}/brew-pip", "--help"
   end
 end
