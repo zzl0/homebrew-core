31908822a962a3291a118ac053ce2e50522293d2
Rui Chen <rui@chenrui.dev>
2022-11-26 20:56:54 -0500

    hyperscan: rebuild
    * hyperscan: depends_on python for build
    * Update hyperscan.rb
    * Update hyperscan.rb
    * Update hyperscan.rb
    
    Closes #116775.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/hyperscan.rb
+++ Formula/hyperscan.rb
@@ -16,11 +16,19 @@ class Hyperscan < Formula
   depends_on "boost" => :build
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
+  depends_on "python@3.11" => :build
   depends_on "ragel" => :build
   # Only supports x86 instructions and will fail to build on ARM.
   # See https://github.com/intel/hyperscan/issues/197
   depends_on arch: :x86_64
   depends_on "pcre"
+
+  # fixes glibc 2.34 issue https://github.com/intel/hyperscan/issues/359
+  # remove in version > 5.4.0
+  patch do
+    url "https://github.com/intel/hyperscan/commit/564ed6f65a1058e4e0adab69bdd17ba9138c8a0c.patch?full_index=1"
+    sha256 "21a22ac92c8f61c3b06f72919d356d594fc89d090eb1f238ce11e89d55e22bfb"
+  end
 
   def install
     cmake_args = std_cmake_args + ["-DBUILD_STATIC_AND_SHARED=ON"]
