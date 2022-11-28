74bb134ab5ce455fa3aff90a225bdf942e63aa03
Rui Chen <rui@chenrui.dev>
2022-11-28 11:01:20 -0500

    convox 3.9.0
    Closes #116838.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/convox.rb
+++ Formula/convox.rb
@@ -1,8 +1,8 @@
 class Convox < Formula
   desc "Command-line interface for the Convox PaaS"
   homepage "https://convox.com/"
-  url "https://github.com/convox/convox/archive/3.8.2.tar.gz"
-  sha256 "2f76b60302abaea7b2c8cd05e7a2c991c86b4667074434e3ff02dfaec02fc291"
+  url "https://github.com/convox/convox/archive/3.9.0.tar.gz"
+  sha256 "2220509346d52a95fb984580b77cb3f5ff7171fb346a45f594c0b62e91e8cf15"
   license "Apache-2.0"
   version_scheme 1
 
