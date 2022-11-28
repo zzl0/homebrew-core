55796731303ed6d416e3a14a791c871ffc96599f
FX Coudert <fxcoudert@gmail.com>
2022-11-28 23:23:47 +0000

    kt-connect: update 0.3.7 bottle.

--- Formula/kt-connect.rb
+++ Formula/kt-connect.rb
@@ -10,6 +10,7 @@ class KtConnect < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "258847384264aa305e719b37c11785a5eee499b573dc49c6575f85d0967c098b"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "8b8a229e1d302e406d135f343c9fe5715a23ca8bcd38b9667a760a85e1f99c5e"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2f8575133862185735f1026bf8378544e307f84f6d15edadea2a15ae1242064d"
+    sha256 cellar: :any_skip_relocation, ventura:        "7817e46c79b666b37b30c2ed07ac2a9753a4c6affc0f49d1e16caf7a0b817066"
     sha256 cellar: :any_skip_relocation, monterey:       "6b7c950096ae80e9126b9868e528b98162e9ad42b93b6243f974a4b46a63025b"
     sha256 cellar: :any_skip_relocation, big_sur:        "f9a56194f87cee8113b94578482620c1f152882e49fde3e5121069a702601a1c"
     sha256 cellar: :any_skip_relocation, catalina:       "83983ce5fca4ef6597ae46664d7494ac2727d7de1c25483d5c4dd036dc2960cc"
