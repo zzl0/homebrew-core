ce3a047c112bacaff0169706fdbe0ad7a13a5f99
Patrick Linnane <patrick@linnane.io>
2022-11-28 06:27:28 +0000

    aws-cdk: update 2.52.0 bottle.

--- Formula/aws-cdk.rb
+++ Formula/aws-cdk.rb
@@ -11,6 +11,7 @@ class AwsCdk < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "40fbd3130fe88feb391a49c83aa4bfc60792a38b3a0d180b564829ea9b960737"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "40fbd3130fe88feb391a49c83aa4bfc60792a38b3a0d180b564829ea9b960737"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "40fbd3130fe88feb391a49c83aa4bfc60792a38b3a0d180b564829ea9b960737"
+    sha256 cellar: :any_skip_relocation, ventura:        "06ab2f09435fd0ea081f8dcacb1671888a050fe871dfdf36bfda4bac5ef83098"
     sha256 cellar: :any_skip_relocation, monterey:       "06ab2f09435fd0ea081f8dcacb1671888a050fe871dfdf36bfda4bac5ef83098"
     sha256 cellar: :any_skip_relocation, big_sur:        "06ab2f09435fd0ea081f8dcacb1671888a050fe871dfdf36bfda4bac5ef83098"
     sha256 cellar: :any_skip_relocation, catalina:       "06ab2f09435fd0ea081f8dcacb1671888a050fe871dfdf36bfda4bac5ef83098"
