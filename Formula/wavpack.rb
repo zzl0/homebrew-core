d29b7c2d28be566e6b511570d5635f45222b057e
Porkepix <porkepix@gmail.com>
2022-11-28 13:04:45 +0100

    wavpack 5.6.0
    Closes #116831.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/wavpack.rb
+++ Formula/wavpack.rb
@@ -1,8 +1,8 @@
 class Wavpack < Formula
   desc "Hybrid lossless audio compression"
   homepage "https://www.wavpack.com/"
-  url "https://www.wavpack.com/wavpack-5.5.0.tar.bz2"
-  sha256 "7a222f96c391138d340793a1b06d517d7a514de85b5915216051b7386f222977"
+  url "https://www.wavpack.com/wavpack-5.6.0.tar.bz2"
+  sha256 "8cbfa15927d29bcf953db35c0cfca7424344ff43ebe4083daf161577fb839cc1"
   license "BSD-3-Clause"
 
   bottle do
