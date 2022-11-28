8b4092c3950a7d40ed3236e90603e73eb67c7eed
FX Coudert <fxcoudert@gmail.com>
2022-11-28 11:38:05 +0000

    trivy: update 0.35.0 bottle.

--- Formula/trivy.rb
+++ Formula/trivy.rb
@@ -10,6 +10,7 @@ class Trivy < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "fb00399637e559724f6e25a6bb02064d16086073581375c1853d70ab3dde0fc1"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "c580dee2b424e95bb2e19062ecff94fe4ccc605e16cd307a7eceba458ab7b62a"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a0cf4d928dc3efbd2ef579fe8ddc47923a1df62862251c1a434c63daf3ad8ca2"
+    sha256 cellar: :any_skip_relocation, ventura:        "a6efd7205dc7e087d1a32b5c26f55bdc440c71732dafae957c96c22ecd2664f3"
     sha256 cellar: :any_skip_relocation, monterey:       "7413f012331291393151c7343220ac7d9278184f829fe262fde6463e3ea4ed01"
     sha256 cellar: :any_skip_relocation, big_sur:        "e1eab6c6f7e73bd98bad78bfbc4bc2938f6ba29c418dd7c498e8709af812b38d"
     sha256 cellar: :any_skip_relocation, catalina:       "9c5d5acbc141728d485cdbbf5a08d0a4eec888212ac02b493a8a556b38057cd1"
