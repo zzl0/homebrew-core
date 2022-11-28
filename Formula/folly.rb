e2d1865d3a6dd360d0a220efecee0eef5fc6d218
Zhong Ruoyu <zhongruoyu@outlook.com>
2022-11-28 16:10:18 +0800

    folly 2022.11.28.00

--- Formula/folly.rb
+++ Formula/folly.rb
@@ -1,8 +1,8 @@
 class Folly < Formula
   desc "Collection of reusable C++ library artifacts developed at Facebook"
   homepage "https://github.com/facebook/folly"
-  url "https://github.com/facebook/folly/archive/v2022.11.14.00.tar.gz"
-  sha256 "b249436cb61b6dfd5288093565438d8da642b07ae021191a4042b221bc1bdc0e"
+  url "https://github.com/facebook/folly/archive/v2022.11.28.00.tar.gz"
+  sha256 "03ab9b889fca5b31064900e817fdcaecefa6dd1b2c36a1fd8dbd4a003cb6f816"
   license "Apache-2.0"
   head "https://github.com/facebook/folly.git", branch: "main"
 
