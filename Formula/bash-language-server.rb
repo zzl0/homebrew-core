5b1eee7557e4cf95d3aa486a6969dacaebd70acb
FX Coudert <fxcoudert@gmail.com>
2022-11-28 14:52:18 +0000

    bash-language-server: update 3.3.0 bottle.

--- Formula/bash-language-server.rb
+++ Formula/bash-language-server.rb
@@ -11,6 +11,7 @@ class BashLanguageServer < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "3b46696c454753984ed3d7128e7928f9870d4769a35a98422fb8eed383683ee6"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "3b46696c454753984ed3d7128e7928f9870d4769a35a98422fb8eed383683ee6"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3b46696c454753984ed3d7128e7928f9870d4769a35a98422fb8eed383683ee6"
+    sha256 cellar: :any_skip_relocation, ventura:        "0931177d299103d3e317bcee5580a2b918a6c0b1c8aa64435f7ecb1609d730b7"
     sha256 cellar: :any_skip_relocation, monterey:       "0931177d299103d3e317bcee5580a2b918a6c0b1c8aa64435f7ecb1609d730b7"
     sha256 cellar: :any_skip_relocation, big_sur:        "0931177d299103d3e317bcee5580a2b918a6c0b1c8aa64435f7ecb1609d730b7"
     sha256 cellar: :any_skip_relocation, catalina:       "0931177d299103d3e317bcee5580a2b918a6c0b1c8aa64435f7ecb1609d730b7"
