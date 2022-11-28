9236d0d164ec48388b237b2bee1e350b364da3ae
FX Coudert <fxcoudert@gmail.com>
2022-11-28 21:55:26 +0000

    argocd: update 2.5.3 bottle.

--- Formula/argocd.rb
+++ Formula/argocd.rb
@@ -10,6 +10,7 @@ class Argocd < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "cb51a387c4b50ad89ce2f60a13b436582d31d4bdb0f09a83bc36f086341523da"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "d672b4a49303184614757fc9045d86a33524c24a81fddceef09d439128c42c2c"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "22733b36caba288bb95c8171930dd55af9368bc03dc78dec1e10be12f0e35f62"
+    sha256 cellar: :any_skip_relocation, ventura:        "ac36b5728249c860e567c2dded479bef3c54a19731fd42de4df399c98ae4091c"
     sha256 cellar: :any_skip_relocation, monterey:       "55365964748866bb98e4c3882b8b5e6ef3880dbd976c80fc8e4eb36e05faf5ae"
     sha256 cellar: :any_skip_relocation, big_sur:        "0bfb22fc8a35bc7a507c4ddfb6098a24c8e5e1b87095a4ee0787808016c8e15b"
     sha256 cellar: :any_skip_relocation, catalina:       "808427f3133c0dd979bfecc38ecb931f0411078cfb71e279553e7c1ec5f99d88"
