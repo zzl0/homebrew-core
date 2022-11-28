3011681c4ec420d066d8c44d328d14b50545a4a6
Rui Chen <rui@chenrui.dev>
2022-11-28 21:21:18 +0000

    rust-analyzer: update 2022-11-28 bottle.

--- Formula/rust-analyzer.rb
+++ Formula/rust-analyzer.rb
@@ -11,6 +11,7 @@ class RustAnalyzer < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "f9c956d2cc08fb5e445f1adbec92213bf1e2e1232eed691d514b91413f807162"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "9fa2fb59604337c94c4177cf10ec8509148861da8d820da23745e5bf3e19c888"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1c7da570161588ab4f022ee64dc16a37252b9d0f1180f75eb7deda5ca58b85c0"
+    sha256 cellar: :any_skip_relocation, ventura:        "179371caa72e657d8ec0c31347ef6ca4277548a1c082801fd69094870325a26f"
     sha256 cellar: :any_skip_relocation, monterey:       "449eeddb4761ff918f7d1a79d4f8907123ce261faddc3413ea3a82609371a98d"
     sha256 cellar: :any_skip_relocation, big_sur:        "b9aeeb5968b7ef67d7ea891810b496952dbe6996c5c56978ebf8f01c7ad6fdad"
     sha256 cellar: :any_skip_relocation, catalina:       "0ccb1034dc9d7b496af45f8e24d1b56c50163e253a2a57a9b122f2c7b316f855"
