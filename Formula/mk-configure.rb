e4114fd810387b11e5edc87ea9929de1c152a3df
Rui Chen <rui@chenrui.dev>
2022-11-28 21:08:19 +0000

    mk-configure: update 0.38.1 bottle.

--- Formula/mk-configure.rb
+++ Formula/mk-configure.rb
@@ -14,6 +14,7 @@ class MkConfigure < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "f51796e938283023d062a2c9af68dce1d7e5a2a01c91a6c5f057e732a035e11d"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "f51796e938283023d062a2c9af68dce1d7e5a2a01c91a6c5f057e732a035e11d"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "24a24408c0e83f9b58dda7659ff788114271c82df8971e17e427d2546b4d5190"
+    sha256 cellar: :any_skip_relocation, ventura:        "cc6ea28f0883c69b5315d0f8ab1f362cf871002c06ef1c432cf4fcca7323e361"
     sha256 cellar: :any_skip_relocation, monterey:       "cc6ea28f0883c69b5315d0f8ab1f362cf871002c06ef1c432cf4fcca7323e361"
     sha256 cellar: :any_skip_relocation, big_sur:        "c744781436f5d6b38bfadac871ad6b96f0063e77795fe618338d62a8f664b3de"
     sha256 cellar: :any_skip_relocation, catalina:       "0bb8077da5c7e87944b15a4594ae20b0a714c58dcc3d6ffb90b55f565c19fae2"
