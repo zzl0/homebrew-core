3f8080288d8a89ec93552da3cb7b17eb5f5fe6cb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-29 18:10:22 +0000

    velero: update 1.10.0 bottle.

--- Formula/velero.rb
+++ Formula/velero.rb
@@ -6,14 +6,13 @@ class Velero < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "9ac11ea8d9f84617c071b63c00b963a70aec238f0d665a75d99d227e54bafeb2"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "749341bd92efcffcc90f2ba8361a36b772d1ff94b0f66bf39a9fc925cc72250f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a4fb442951ee485f442c221904ca59591142e09ffea3edb687b8d630ee3b4771"
-    sha256 cellar: :any_skip_relocation, ventura:        "34cb83c792d234957508c52dd12b837ab702a0e5bb3dbd2191aac08bb4f2fb15"
-    sha256 cellar: :any_skip_relocation, monterey:       "cc2769d44cbd216728b996bcfc2fc4e27d5d18fb973d021187d6ec12c71a9882"
-    sha256 cellar: :any_skip_relocation, big_sur:        "3ea7eebdbf751e2050bd5c212178ff864410cba435222db8036370119a6c54d9"
-    sha256 cellar: :any_skip_relocation, catalina:       "ce40dfce5c1a3488accf25794bc67dbde1799f2c48aaadada003fdfa41ca6c5a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e950c979ae5e82f9e0420cb7d6f070161f9346a880f3d88f985d1654e4729519"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "c54af8d501fa971fbbef5b80980a4cf94354a4797c134c4d8e573716ee5ffd31"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c3501d73859f630fa2074584b6ff39bb1fb9c5c3efe0dbdad78b806060d8e8a4"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0a3d308fa3cbddc7a056997a62ce57685335b702e45cafc2c0f77996e07108fe"
+    sha256 cellar: :any_skip_relocation, monterey:       "3b0b0d1a2a98c320b5350e86893a5361b68552b7d9d5160b7e59b9d7d9a373e6"
+    sha256 cellar: :any_skip_relocation, big_sur:        "cf79ae529e6d7f1b260f7d3660e9f267bd01e4a72b0d860a509d4777ea8f3d10"
+    sha256 cellar: :any_skip_relocation, catalina:       "bcdbf769f38b5289ac582213f4226a4006d821f427b8692a50a602b032de9d03"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ac38f76bcb5c01c792a4e151095dfd5e12438238450b2e9c956c7145cbc47534"
   end
 
   depends_on "go" => :build
