fedc17de1ff052cf1f8cc44db82422500fe2c0f7
Rui Chen <rui@chenrui.dev>
2022-11-29 06:22:14 +0000

    devspace: update 6.2.0 bottle.

--- Formula/devspace.rb
+++ Formula/devspace.rb
@@ -16,6 +16,7 @@ class Devspace < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "17763f56ae611a710e4c5c4bf3c41205b6b330062995e6371112f42be6d9fb8a"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "8224a308766b9d13a45339b2135a82c8ca186834c46db2f9b9b4e7a267359565"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "086de8115b88776aba6a0db379512df31f297d96e542de3e61db9b1695f56dff"
+    sha256 cellar: :any_skip_relocation, ventura:        "3fd8ba205b885416969c5b79d5855bf2ad2bedef37fca86ae78eeb428e3de837"
     sha256 cellar: :any_skip_relocation, monterey:       "326894bfba2f0852e8f312f3d2a08b712c3a28efe302debe9449fb590f45f102"
     sha256 cellar: :any_skip_relocation, big_sur:        "1dabb965242c2859bcae2d4901bea143fe438ca1e3be814a4726d22f9a6db1ed"
     sha256 cellar: :any_skip_relocation, catalina:       "570b3838b2c8b3022084a88824d7d263a58618bf65ed74535a058a085422691f"
