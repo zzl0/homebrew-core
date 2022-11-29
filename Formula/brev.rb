4cdc308a17fc4c3f557bc9e2418a0085f9f47709
FX Coudert <fxcoudert@gmail.com>
2022-11-29 19:21:34 +0000

    brev: update 0.6.182 bottle.

--- Formula/brev.rb
+++ Formula/brev.rb
@@ -14,6 +14,7 @@ class Brev < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "6f20a61bfb576e667e7b1851ca7e6ea6895a0290048c2efc68aca9a315265fae"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "b28f095d3c6679daea28df03803bcef9798bb55e51178997465e5c24c4b9d459"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9de774df889b89fd9d26163244537a046d145d68dc7370da4a6843b306c1dd89"
+    sha256 cellar: :any_skip_relocation, ventura:        "2d255f2481b739a4fc81d957259ee99a7444c0d4136db2003460c0c28d031c89"
     sha256 cellar: :any_skip_relocation, monterey:       "4bab2ac5a427ffc98d9c7e03345eb04c8d8da30ae668688e0303e4d2b5425fbd"
     sha256 cellar: :any_skip_relocation, big_sur:        "d584f1b194f0ae9b9e1d5d4edf2b9707f184ba91888f17cadda0f943b9ecff06"
     sha256 cellar: :any_skip_relocation, catalina:       "24143ed678e8aa1e17536fab14295ce06f667757355c7317f7d35901abbb5742"
