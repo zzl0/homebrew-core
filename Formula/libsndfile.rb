37f443a7c13da728f24cc8c06289bb427057a957
FX Coudert <fxcoudert@gmail.com>
2022-11-29 15:47:50 +0000

    libsndfile: update 1.1.0_1 bottle.

--- Formula/libsndfile.rb
+++ Formula/libsndfile.rb
@@ -16,6 +16,7 @@ class Libsndfile < Formula
     sha256 cellar: :any,                 arm64_ventura:  "7cdb108767e5974753eaf68d0b709d309d46c4aa4dfe997ca9b7d88f7c8d4f87"
     sha256 cellar: :any,                 arm64_monterey: "6355d1153204ae329ffdd4573a5585acf3658ef37d25dc28583c6451d7b014e0"
     sha256 cellar: :any,                 arm64_big_sur:  "0f7cbb29a5fe0c69ce74965ed33ac66455b7e849954c102e58566d9101144491"
+    sha256 cellar: :any,                 ventura:        "1db3fa5ec99cc7d615bf8e3345ad05442ee469915830999a603e82cfa81560b9"
     sha256 cellar: :any,                 monterey:       "39def1916e1e36ea8ed47dbcaf57bd858f57e5ac2153a3304a2785f666a25d64"
     sha256 cellar: :any,                 big_sur:        "90d04f9535380ce27d3bd064fceaaa246c6ecc5d31d0885a4dda1c8da6335ddf"
     sha256 cellar: :any,                 catalina:       "d0e19056bec6f6cf698996f4909d8e5bb61114498c51b36cf23a1d87414a6967"
