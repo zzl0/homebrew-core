d63a1abea6ee7979c16eb48dbfcec96032a7675a
FX Coudert <fxcoudert@gmail.com>
2022-11-29 13:12:16 +0000

    wakatime-cli: update 1.60.1 bottle.

--- Formula/wakatime-cli.rb
+++ Formula/wakatime-cli.rb
@@ -11,6 +11,7 @@ class WakatimeCli < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "591bd692d57a0ff8eb2181e4d3623c5222bbf4c7124153412023fd38c9685956"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "a7cd7bfbef6f042282310b3da0620f1670615f6f9a1437fc3aabad58a06e999c"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f68816f90293cf6998d432acc01599953dcb409e2b44c6e476ff314743dd4707"
+    sha256 cellar: :any_skip_relocation, ventura:        "3b354de799bf3eec08306f933378e9c4211559ccffe7b5145ba027fe3f2d5fcd"
     sha256 cellar: :any_skip_relocation, monterey:       "8f4d30b79964a179ed2b07a8f97acb9a6cc2efac1288f1111538e1f106d271eb"
     sha256 cellar: :any_skip_relocation, big_sur:        "10006f16945b40667f4a21823c89a6a7e18a25a79149ae5c326dac3bea3fba4b"
     sha256 cellar: :any_skip_relocation, catalina:       "920b3667baabd3a8be3d82abecbea770f2bbc04f4eab251242ebced104a8327e"
