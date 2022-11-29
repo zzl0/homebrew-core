51451f2d1939700274fd7ae54929dc47eb9c438d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-29 20:05:18 +0000

    tut: update 1.0.24 bottle.

--- Formula/tut.rb
+++ Formula/tut.rb
@@ -6,14 +6,13 @@ class Tut < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "277e4858ac409150fd6ea34985844bcea8c3e59df7df080e3ff21058c486eebd"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "74b152afe9ec6510cfc781569955237ff3e72a2265bf4c8bc5e041f2473ff2f4"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "af14efd2daca9e14422ee88f3d9c18e6db7ca82a4c6da51cdbfd3efb450bf3a1"
-    sha256 cellar: :any_skip_relocation, ventura:        "14b21a4dc834da8587725bf87f1f3956059ed96053490e9c2b35b38a77539173"
-    sha256 cellar: :any_skip_relocation, monterey:       "f8409f94a116b855ec87c13aac5f757ad3f4e79a39dfef93f68e2f219071c35c"
-    sha256 cellar: :any_skip_relocation, big_sur:        "bb43fccc05f721020c6790effcf23992d44ee3422bb1d7fafb63d6cefe1b361a"
-    sha256 cellar: :any_skip_relocation, catalina:       "b4d5d18a2342f119890195406bf00e1a0cd8d269ebc6d55cfcd1f8e8508b94f6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "aaad8961ddc938a8d844d1e8bf7de8395679d525bdcc15d55272bc8d249f78b8"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "34d436edac4b3e1f54388b9accae1b1dce3ae803cbe8b8b103601a5b9200c220"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "1635fbbb83c6344a76fef76fe587674e53e56b3dfa77062d3244a0f022013bca"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9b49c6e89372d47d4eefc5537b0264e323dee0f9570da0cf28d5426502d4f7a9"
+    sha256 cellar: :any_skip_relocation, monterey:       "a2f10e73a930741029deb06bccaf572cd2406a69f095367e68906eec6e3c48e4"
+    sha256 cellar: :any_skip_relocation, big_sur:        "952091bd14a3fc3a2d9b701052088db72ffb0b9f131bde0fc6bba74a33634b12"
+    sha256 cellar: :any_skip_relocation, catalina:       "19aa23ea134ea815c99179d3ff4328c0a62bf052b5ee959fe4399baf9b464fa3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6d4de69d81372a976080c5fcf91cdf18dacd3527ba94a68fcc0c6c4f0c7555e6"
   end
 
   depends_on "go" => :build
