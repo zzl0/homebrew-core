631dbbef6ee59497774ecf9612f082211c569240
Rui Chen <rui@chenrui.dev>
2022-11-29 11:57:40 -0500

    snowflake 2.4.0
    snowflake: patch build
    
    Closes #116905.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Dawid Dziurla <9713907+dawidd6@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/snowflake.rb
+++ Formula/snowflake.rb
@@ -1,8 +1,8 @@
 class Snowflake < Formula
   desc "Pluggable Transport using WebRTC, inspired by Flashproxy"
   homepage "https://www.torproject.org"
-  url "https://gitweb.torproject.org/pluggable-transports/snowflake.git/snapshot/snowflake-2.3.1.tar.gz"
-  sha256 "47e035ff08668317e719b982a924d048fad738e6081005971e895ddcc2283fba"
+  url "https://gitweb.torproject.org/pluggable-transports/snowflake.git/snapshot/snowflake-2.4.0.tar.gz"
+  sha256 "e1f77655db895b3ec304eee029d9b7fbf205d3c6cd354a81ad20ad8c501d465e"
   license "BSD-3-Clause"
   head "https://git.torproject.org/pluggable-transports/snowflake.git", branch: "main"
 
@@ -18,6 +18,12 @@ class Snowflake < Formula
   end
 
   depends_on "go" => :build
+
+  # patch build, remove in next release
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/93d8acb/snowflake/2.4.0.patch"
+    sha256 "2c10cde8a894088791cf7395367aa27455140601038ba95ef24e6479e3cc0af3"
+  end
 
   def install
     system "go", "build", *std_go_args(output: bin/"snowflake-broker"), "./broker"
