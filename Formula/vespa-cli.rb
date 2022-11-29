fef77fc21644b1ed70b881d922477a7721f1e172
FX Coudert <fxcoudert@gmail.com>
2022-11-29 15:48:18 +0000

    vespa-cli: update 8.90.59 bottle.

--- Formula/vespa-cli.rb
+++ Formula/vespa-cli.rb
@@ -15,6 +15,7 @@ class VespaCli < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "1a0c2ca25a5b0b92dbaef4bbdd59246416d8aeb94d55c0d7b36b2e9a6246a7de"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "6b5ebabeca81e4d283f39f4a26eda53296954cec23eb513ac2d57430cb3fa4da"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "47b49116e0c2c4df4b4f16632edbe8d99d9314de3c6042b6303f1d2b593e047d"
+    sha256 cellar: :any_skip_relocation, ventura:        "728584fe405b357cb690fdc6e5d0189a43282a096e8acddc1933f6cc09d61eb9"
     sha256 cellar: :any_skip_relocation, monterey:       "860183c2860a890dd2143fcc03d0db52cf4dc81ec8e54dbab0f4a388a37c5b3c"
     sha256 cellar: :any_skip_relocation, big_sur:        "03ea56d1edafce9354ecfa6b2b1eb50f6b53bffadb2a40bc1fedc5bc16dc080c"
     sha256 cellar: :any_skip_relocation, catalina:       "fc40fd1461a29a7c19de30a25849245c9ca5ffb1f4550bc88f8cd5b52a61a84d"
