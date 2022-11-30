c4560f423745936fabd8ab4f6415566ee2b632fb
FX Coudert <fxcoudert@gmail.com>
2022-11-30 10:10:38 +0000

    tctl: update 1.17.2 bottle.

--- Formula/tctl.rb
+++ Formula/tctl.rb
@@ -9,6 +9,7 @@ class Tctl < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "23306dba780b51edd53f72c5b351826485932cbf80efa3419684d06ffedfdaf8"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "e62d9e56770170c73542750d0e0673450d329ac4cc1b82cb09137c505afec5ad"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b338afaf531528c54a443aaa53f9bd858e763fb2c731041005408844121a814c"
+    sha256 cellar: :any_skip_relocation, ventura:        "f25060e666c9f4934f9cdc40d113c73ac89ee94a684dc8fa8232f8c208164c8b"
     sha256 cellar: :any_skip_relocation, monterey:       "8b4202cc9e1cdc7cab5f877f1db859bbd4c2ad5f968e2caee6ef24bcaf22f500"
     sha256 cellar: :any_skip_relocation, big_sur:        "ee71c8e3cdd32bd0ac60f8773798be5afd3817e850c20a799cf2c5a6a35c9b02"
     sha256 cellar: :any_skip_relocation, catalina:       "a329194b90331faf0337f21e03c3b95fbc4a1603a9cbef31af8377e84261cf29"
