d794f25fe6be004c1c4f24504f3ef9cf8a609169
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-30 18:05:15 +0000

    glslviewer: update 3.0.8 bottle.

--- Formula/glslviewer.rb
+++ Formula/glslviewer.rb
@@ -8,14 +8,13 @@ class Glslviewer < Formula
   head "https://github.com/patriciogonzalezvivo/glslViewer.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "81067a7d0156aaed4c7efad225a1533d822a9261c3cfaa2df57c9be2d1967ecd"
-    sha256 cellar: :any,                 arm64_monterey: "cf33a11e088d40d9d5ae96ed374756c60984cabeb2de33d52af670c091e4a81e"
-    sha256 cellar: :any,                 arm64_big_sur:  "67f4455d6abf22ee9bd69b7651f7e4346af09e27afaa3272cd1f9a22cfefce22"
-    sha256 cellar: :any,                 ventura:        "5093442e72a18fe1ed34385e9a024f6da103229dcac44d7c916b3cba6af06f7f"
-    sha256 cellar: :any,                 monterey:       "c5123fc4e0c8d1d2fb26ab0b2b787d865f1c3301df19b8b2c8d7dd777d5b8c23"
-    sha256 cellar: :any,                 big_sur:        "6fcacd65216707ce3ec09c031626cb158cf757d6a59a74ba3b0684f4518dd2be"
-    sha256 cellar: :any,                 catalina:       "6bb4be1650a54e23c4c124b229eb01d2b945df3278ba930cd28a667c109cc09c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c8c23e67dd736ed411333a91adbb5cc69e82dd8b017dd394242bf3824f436734"
+    sha256 cellar: :any,                 arm64_ventura:  "421c84b4e4fd9b6941922c4a0ce536cd19b8ee09eaeb736976ec5b09c356c925"
+    sha256 cellar: :any,                 arm64_monterey: "fc69dc5b243e0ebaf534c8577a92f60392e017edd9733fec84d079545bf93d18"
+    sha256 cellar: :any,                 arm64_big_sur:  "689396195e735373e157a8a3c167f2a68069e090574a9d80011cf721e79eda25"
+    sha256 cellar: :any,                 monterey:       "d69b49162105747437712bd045d433a2ba05a493e456d389e73a138d9c72a8e6"
+    sha256 cellar: :any,                 big_sur:        "8c3c68e24f17d9a930be4c620739890560a1e8dbc2ed04dc718978fd3e7a4d15"
+    sha256 cellar: :any,                 catalina:       "cfd3b93ec996e2278d477647eae3a303c2bd21f6b9d7958e95d6bbbd6d713915"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0c0266debcdc9d6b9ac6af5c7ee2d211ca78ef779dbf22047e35d5284c2e0e6d"
   end
 
   depends_on "cmake" => :build
