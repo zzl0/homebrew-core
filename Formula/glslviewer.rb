b7ff1140027d1308b24ade5350d94270168c83c5
FX Coudert <fxcoudert@gmail.com>
2022-11-30 19:35:50 +0000

    glslviewer: update 3.0.8 bottle.

--- Formula/glslviewer.rb
+++ Formula/glslviewer.rb
@@ -11,6 +11,7 @@ class Glslviewer < Formula
     sha256 cellar: :any,                 arm64_ventura:  "421c84b4e4fd9b6941922c4a0ce536cd19b8ee09eaeb736976ec5b09c356c925"
     sha256 cellar: :any,                 arm64_monterey: "fc69dc5b243e0ebaf534c8577a92f60392e017edd9733fec84d079545bf93d18"
     sha256 cellar: :any,                 arm64_big_sur:  "689396195e735373e157a8a3c167f2a68069e090574a9d80011cf721e79eda25"
+    sha256 cellar: :any,                 ventura:        "bce35154a04ba6e988272b7c8faee45fcd09790b88b662d0fa81785ce309798d"
     sha256 cellar: :any,                 monterey:       "d69b49162105747437712bd045d433a2ba05a493e456d389e73a138d9c72a8e6"
     sha256 cellar: :any,                 big_sur:        "8c3c68e24f17d9a930be4c620739890560a1e8dbc2ed04dc718978fd3e7a4d15"
     sha256 cellar: :any,                 catalina:       "cfd3b93ec996e2278d477647eae3a303c2bd21f6b9d7958e95d6bbbd6d713915"
