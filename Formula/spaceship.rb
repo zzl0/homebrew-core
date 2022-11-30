d0a1a3e827059230f80abcf624d28e23b29cfe02
FX Coudert <fxcoudert@gmail.com>
2022-11-30 14:26:22 +0000

    spaceship: update 4.11.0 bottle.

--- Formula/spaceship.rb
+++ Formula/spaceship.rb
@@ -10,6 +10,7 @@ class Spaceship < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "5891a28673681ca3f1655042fb8ec24f77f295815159466d3fb153ff92ff3ebb"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "1ac943ef5eaa99591e8be98edf194bfab30239dd076c807fd94c56e8141854cb"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c5b6a355cc38b383975aeaafc349fb83503fe25a9b2f80be96d1abbf5f0a2bb7"
+    sha256 cellar: :any_skip_relocation, ventura:        "912ceec9974d1956b64845bde66ebf8a741283c4adc98109ba9de895a022a27a"
     sha256 cellar: :any_skip_relocation, monterey:       "2634ea7fd084db7686c35062f98f95be433f67e12a66be77f83722dd3579156c"
     sha256 cellar: :any_skip_relocation, big_sur:        "52925ec06e65405cbd93054cdd6b6a3f208b559977147d145fed5e5ea40185c8"
     sha256 cellar: :any_skip_relocation, catalina:       "b1666961076b140c6045164421ea8a2b3af13914eabea20f890e759cad80c854"
