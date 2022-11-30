bb19e736205859b4caa94687fb8b871af7267710
FX Coudert <fxcoudert@gmail.com>
2022-11-30 19:18:08 +0000

    cubejs-cli: update 0.31.19 bottle.

--- Formula/cubejs-cli.rb
+++ Formula/cubejs-cli.rb
@@ -11,6 +11,7 @@ class CubejsCli < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "995f6e4d134b0ac60dbe02d47e299d82ad18388bf48dc1d4a43a23f159948b5e"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "995f6e4d134b0ac60dbe02d47e299d82ad18388bf48dc1d4a43a23f159948b5e"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "995f6e4d134b0ac60dbe02d47e299d82ad18388bf48dc1d4a43a23f159948b5e"
+    sha256 cellar: :any_skip_relocation, ventura:        "4c1ba1b954c7e35b7f83c308a7ffab509bfe5e26ded8e7a1532c44be45e5ecb7"
     sha256 cellar: :any_skip_relocation, monterey:       "4c1ba1b954c7e35b7f83c308a7ffab509bfe5e26ded8e7a1532c44be45e5ecb7"
     sha256 cellar: :any_skip_relocation, big_sur:        "4c1ba1b954c7e35b7f83c308a7ffab509bfe5e26ded8e7a1532c44be45e5ecb7"
     sha256 cellar: :any_skip_relocation, catalina:       "4c1ba1b954c7e35b7f83c308a7ffab509bfe5e26ded8e7a1532c44be45e5ecb7"
