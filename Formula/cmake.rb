2cafb3915f5e4e34b8b6c043c3965d48b94a1835
FX Coudert <fxcoudert@gmail.com>
2022-11-30 22:05:09 +0000

    cmake: update 3.25.0 bottle.

--- Formula/cmake.rb
+++ Formula/cmake.rb
@@ -20,6 +20,7 @@ class Cmake < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "f2e98c55285071fff328c78d987684598b7ee0241950573d2d2c833b2da1290c"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "25bbe90c5ce335b948e098d4b0de7b680793fbe12e93162e034b7e35554e6fb7"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6cd53b5c39d1def448201935cfcdf0a0018840734fab0fb68753762089ded548"
+    sha256 cellar: :any_skip_relocation, ventura:        "45eb7b789570c7d8e2395e96227fcb859f3e154d65e253bd238cc1facbf2f113"
     sha256 cellar: :any_skip_relocation, monterey:       "986e3351822e814c8ec025fe5c8d319da6afec1e6daca74985d4f741766ec236"
     sha256 cellar: :any_skip_relocation, big_sur:        "f4a749ef3d901929aca20767730befe22cea7801d74137e2179aecbd11904720"
     sha256 cellar: :any_skip_relocation, catalina:       "0edff41ac2870ef54bc7c4710b60323324e96194f6fe9aa8a8cd4047f8310fab"
