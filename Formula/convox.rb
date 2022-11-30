78db2ba013e169800559f911b888591a2ba06f96
FX Coudert <fxcoudert@gmail.com>
2022-11-30 10:34:57 +0000

    convox: update 3.9.1 bottle.

--- Formula/convox.rb
+++ Formula/convox.rb
@@ -15,6 +15,7 @@ class Convox < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "9cb499cd5aaf29accf13b8ad45018c1ff8929058fec5fd3d10084251cb942299"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "8f29ad17f66b0bdeb8767f2d6d328ec5e0644984ad1a7eb1d058e1d54009d0bf"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c4f9253b59de77546443a1dec9c5e7ea11beab745ccd7229cb368f5be1998458"
+    sha256 cellar: :any_skip_relocation, ventura:        "7262041b8d473c80674a6fa4e3f790b8dbbb8481cf51b55f90e16bb688d41759"
     sha256 cellar: :any_skip_relocation, monterey:       "f81cf75d2d0ce1fe09d0df4371928298d32cf40a35076950248ecd5dc3e5eee5"
     sha256 cellar: :any_skip_relocation, big_sur:        "b561d8d71f2e2be7c71541bb0f69d8d2c7bb1d771840b5a940f29f5d0a8d5b42"
     sha256 cellar: :any_skip_relocation, catalina:       "9048f2cc8c95321298a38b2f8ccfe3859570c0580dc4d4afe02d2154fc6e6422"
