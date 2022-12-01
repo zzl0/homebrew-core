321f1443b53ae027e177f8117d3a42a6d0bc1103
Rui Chen <rui@chenrui.dev>
2022-12-01 16:36:00 -0500

    mavsdk: revision bump (re2 20221201)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/mavsdk.rb
+++ Formula/mavsdk.rb
@@ -7,7 +7,7 @@ class Mavsdk < Formula
       tag:      "v1.4.9",
       revision: "163601d81313d8e11a6c25559ef77bbd7338517a"
   license "BSD-3-Clause"
-  revision 1
+  revision 2
 
   livecheck do
     url :stable
