08e10e33ff7ea058c9fc9d89a0205d4ed7b81545
Rui Chen <rui@chenrui.dev>
2022-12-01 13:59:46 -0500

    uncrustify 0.76.0
    Closes #117103.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/uncrustify.rb
+++ Formula/uncrustify.rb
@@ -1,8 +1,8 @@
 class Uncrustify < Formula
   desc "Source code beautifier"
   homepage "https://uncrustify.sourceforge.io/"
-  url "https://github.com/uncrustify/uncrustify/archive/uncrustify-0.75.1.tar.gz"
-  sha256 "fd14acc0a31ed88b33137bdc26d32964327488c835f885696473ef07caf2e182"
+  url "https://github.com/uncrustify/uncrustify/archive/uncrustify-0.76.0.tar.gz"
+  sha256 "32e2f95485a933fc5667880f1a09a964ae83132c235bb606abbb0a659453acb3"
   license "GPL-2.0-or-later"
   head "https://github.com/uncrustify/uncrustify.git", branch: "master"
 
