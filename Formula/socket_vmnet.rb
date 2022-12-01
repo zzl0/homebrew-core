636ac0700e011b23c3e41cc4176b30e01a3af5ae
Patrick Linnane <patrick@linnane.io>
2022-12-01 05:39:54 +0000

    socket_vmnet: add 1.1.0 bottle.

--- Formula/socket_vmnet.rb
+++ Formula/socket_vmnet.rb
@@ -6,6 +6,15 @@ class SocketVmnet < Formula
   license "Apache-2.0"
   head "https://github.com/lima-vm/socket_vmnet.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8e309781893e7dd83edadee99ab08ec8295917f3e687f9c3949c95129be9ef03"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ceb4bc938dda5c753ac0a741f290559f4864a10c298206b573fc103806e6abce"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3cafc6fea88ace7848811345919cfe2db0857d01a46dd55a1617719faa3ff410"
+    sha256 cellar: :any_skip_relocation, monterey:       "b3a238d313d1f0ad7ed813b37cf46e68df739c7a57062e6f545290307e8ce1e2"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e129b3422301ddccddcd30c312b86bdae105f43c8cfec06163404420201e0f5f"
+    sha256 cellar: :any_skip_relocation, catalina:       "fd7fa82a159b2821a54bfe417968af6624b833f32eac6807e341536ffcab4ef7"
+  end
+
   keg_only "#{HOMEBREW_PREFIX}/bin is often writable by a non-admin user"
 
   depends_on :macos
