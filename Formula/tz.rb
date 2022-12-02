f29a198c374bc6cd435e3a42c0cc4233c7e1e618
Mitchell Blank Jr <mitch@bodyfour.com>
2022-12-02 02:01:35 +0000

    tz 0.6.2
    * tz 0.6.2
    * tz: update sha
    * tz: update test
    
    Closes #117137.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tz.rb
+++ Formula/tz.rb
@@ -1,8 +1,8 @@
 class Tz < Formula
   desc "CLI time zone visualizer"
   homepage "https://github.com/oz/tz"
-  url "https://github.com/oz/tz/archive/refs/tags/v0.6.1.tar.gz"
-  sha256 "96be9896deb2e27452d2dfde4d7121a1bd6fa1f8eec3d3b8f374b4d55626043d"
+  url "https://github.com/oz/tz/archive/refs/tags/v0.6.2.tar.gz"
+  sha256 "f4dc3ae2089701947cbdba37f9684708d5fd63e362b1355469105860b08b356b"
   license "GPL-3.0-or-later"
   head "https://github.com/oz/tz.git", branch: "main"
 
@@ -25,18 +25,8 @@ class Tz < Formula
   end
 
   test do
-    # Bubbletea-based apps are hard to test even under PTY.spawn (or via
-    # expect) because they rely on vt100-like answerback support, such as
-    # "<ESC>[6n" to report the cursor position.  For now we just run
-    # the command for a second and see that it tried to send some ANSI out of it.
-    require "pty"
-    r, _, pid = PTY.spawn "#{bin}/tz", "-q"
-    sleep 1
-    Process.kill("TERM", pid)
-    begin
-      assert_match(/\e\[/, r.read)
-    rescue Errno::EIO
-      # GNU/Linux raises EIO when read is done on closed pty
-    end
+    assert_match "US/Eastern", shell_output("#{bin}/tz --list")
+
+    assert_match version.to_s, shell_output("#{bin}/tz -v")
   end
 end
