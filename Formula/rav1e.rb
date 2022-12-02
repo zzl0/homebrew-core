4f5cfabda57e1073b7edfb2f617731488c060d45
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-12-02 10:05:52 +0000

    rav1e: update 0.6.1 bottle.

--- Formula/rav1e.rb
+++ Formula/rav1e.rb
@@ -20,15 +20,13 @@ class Rav1e < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_ventura:  "39120d6a039a8ae1c98217a7ca61662a9dd7e6ce17ba9889c0dfd690cd403ad9"
-    sha256 cellar: :any,                 arm64_monterey: "15486f6308ce05ee89ee4d1c96d5165ec1851ec66578d53f06cc8e19b34efa14"
-    sha256 cellar: :any,                 arm64_big_sur:  "b0725e463e742f65eb6c88d7115f61fec0bf60f01139c169e2a921debb62d5df"
-    sha256 cellar: :any,                 ventura:        "1030cfe47165598db33c1530da5316a815173c268de24c8ffa4a55b1c41335a3"
-    sha256 cellar: :any,                 monterey:       "6255aebcb8ec74ee0370390fca79fbbeb2c8d68058e2f7e74910ea526aa7dafb"
-    sha256 cellar: :any,                 big_sur:        "54b40a66c3d97249fbeaea13f3ec5d34ac2faffd95b5a4413c98da29dd1c593a"
-    sha256 cellar: :any,                 catalina:       "8438c37d331dc244af0680314fda8b4eea803f22f8aa108e69decc785d452be5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a078bd4ee14de7dd73b29b3fd4e91f05aff309d780a328df76486783fdf1b0d3"
+    sha256 cellar: :any,                 arm64_ventura:  "781fdba55f672f2808d5a942645de9fe9e032f9c6f4b74a626a1eed77d8d5238"
+    sha256 cellar: :any,                 arm64_monterey: "cf02db9e7de5fde855205de5c4712ea332a6165b155063bca322bfc292704b2d"
+    sha256 cellar: :any,                 arm64_big_sur:  "2a18dfad1d04729a15103f084449a1fa1378e571651ec96d145d59158abb1e45"
+    sha256 cellar: :any,                 ventura:        "a162a9c35f1adb24986a47c654250eee20a5adad72b473c44cf7f3adee821a5e"
+    sha256 cellar: :any,                 monterey:       "c26df3fe0341c72aba9ce445350e6f73bc1e4c1e730a7ada7d6e4b03d7b6a2ac"
+    sha256 cellar: :any,                 big_sur:        "6ed0c0c4c1c8dc2e3da2caec1c70e9866c372813deb1532b4ebc511fbbd5d13c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6298430329a5c5cfdb1d170b2690834a4759aa14515a3a0fac8aa91204b9835f"
   end
 
   depends_on "cargo-c" => :build
