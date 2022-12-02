395d38ff1c72f4be227a9fa5a8875382866a0c1a
Patrick Linnane <patrick@linnane.io>
2022-12-02 18:58:43 +0000

    kubevious: add 1.0.28 bottle.

--- Formula/kubevious.rb
+++ Formula/kubevious.rb
@@ -7,6 +7,16 @@ class Kubevious < Formula
   sha256 "88abc8c86755647f2d827735afd80043154291915f2831b28fa4f5a35a831b38"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "9392160ab4919e99cb46375950a97abef782c0bd1e3ee245480e75ed67217448"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "767af84d1e9631e2ecdf68dee63c1aba294e56ac0ff1e151c149a614d068442d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "79017affe7feaecb6acbd337db6c16e7e710d07b883690db7d10b9d8a5709bc1"
+    sha256 cellar: :any_skip_relocation, ventura:        "dc1c4f348133bde69ebaf684f8ce8651516bc38f520461b03bad7dac4ff80fbe"
+    sha256 cellar: :any_skip_relocation, monterey:       "0bba84e000061818396ee504068674574677ef3865616ec0162142adabcd6665"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f403d15af346f9db7c45bbb20a23d1ac9028c5a44ab9a27bd7741eb8afbe73d1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a7b52bf6a8cbc2b682d571bbb073050728f38efb383b9fd1749da54f2c1034ac"
+  end
+
   # upstream issue to track node@18 support
   # https://github.com/kubevious/cli/issues/8
   depends_on "node@14"
