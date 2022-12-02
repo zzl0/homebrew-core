83fc68976e6930ff5eccd46c8021fc5577995a3b
Rui Chen <rui@chenrui.dev>
2022-12-02 14:44:26 +0000

    temporal: add 0.2.0 bottle.

--- Formula/temporal.rb
+++ Formula/temporal.rb
@@ -6,6 +6,16 @@ class Temporal < Formula
   license "MIT"
   head "https://github.com/temporalio/cli.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "70d116a8e4550bb1fcdb5c87f5140322fe3865a9f1dfcb4005618bcec6074741"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "9a4c6dfb6ec136757572fb0894cc964895170e9987e7548a4288355a6becca05"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7c754f159a9f46385326e03f4a9515d84bfd68e653bf58f477411105dd2826b6"
+    sha256 cellar: :any_skip_relocation, ventura:        "5df738ea399bffde9c91e9a629e6e332b472802cfeefd657fa0f46a4c649d1e1"
+    sha256 cellar: :any_skip_relocation, monterey:       "a3249e13d4a241278cf681550614deaf21ea92352434665aa4fe28ba3d050078"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f7bb94266be632a5b3fafcaac74e03ef3fe9cc61316d3cbbfadc83262557c0a4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e984162867e4e4c5f658bf2535bdb9e4455fbb214709f763fece99e50b3cae05"
+  end
+
   depends_on "go" => :build
 
   def install
