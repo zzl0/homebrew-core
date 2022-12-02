0e559bed604b4e765ec0bf097a83e02469edcf24
Michael Cho <cho-m@tuta.io>
2022-12-02 13:25:54 -0800

    allureofthestars: revision bump (ghc 9.4.3)

--- Formula/allureofthestars.rb
+++ Formula/allureofthestars.rb
@@ -4,7 +4,7 @@ class Allureofthestars < Formula
   url "https://hackage.haskell.org/package/Allure-0.11.0.0/Allure-0.11.0.0.tar.gz"
   sha256 "6125cc585e2a5f28c88855c3c328385c1f21bed093d7606478f1b2af0cb2b6d6"
   license all_of: ["AGPL-3.0-or-later", "GPL-2.0-or-later", "OFL-1.1", "MIT", :cannot_represent]
-  revision 1
+  revision 2
   head "https://github.com/AllureOfTheStars/Allure.git", branch: "master"
 
   bottle do
@@ -29,11 +29,10 @@ class Allureofthestars < Formula
   end
 
   test do
-    assert_equal "",
-      shell_output("#{bin}/Allure --dbgMsgSer --dbgMsgCli --logPriority 0 --newGame 3 --maxFps 100000 " \
-                   "--stopAfterFrames 50 --automateAll --keepAutomated --gameMode battle " \
-                   "--setDungeonRng \"SMGen 7 7\" --setMainRng \"SMGen 7 7\"")
-    assert_equal "", (testpath/".Allure/stderr.txt").read
+    assert_empty shell_output("#{bin}/Allure --dbgMsgSer --dbgMsgCli --logPriority 0 --newGame 3 " \
+                              "--maxFps 100000 --stopAfterFrames 50 --automateAll --keepAutomated " \
+                              "--gameMode battle --setDungeonRng \"SMGen 7 7\" --setMainRng \"SMGen 7 7\"")
+    assert_empty (testpath/".Allure/stderr.txt").read
     assert_match "Client FactionId 1 closed frontend.", (testpath/".Allure/stdout.txt").read
   end
 end
