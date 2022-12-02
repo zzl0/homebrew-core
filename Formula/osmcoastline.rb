99d9fffcf81795f9f8a3e1db18be97dbead38fb1
Francois-Xavier Coudert <fxcoudert@gmail.com>
2022-12-02 17:56:26 +0100

    osmcoastline: revision for poppler

--- Formula/osmcoastline.rb
+++ Formula/osmcoastline.rb
@@ -4,7 +4,7 @@ class Osmcoastline < Formula
   url "https://github.com/osmcode/osmcoastline/archive/v2.3.1.tar.gz"
   sha256 "ab4a94b9bc5a5ab37b14ac4e9cbdf113d5fcf2d5a040a4eed958ffbc6cc1aa63"
   license "GPL-3.0-or-later"
-  revision 6
+  revision 7
 
   bottle do
     sha256 cellar: :any,                 arm64_ventura:  "2f0c753f2e7cd3a17ae026ecf3142a32569a59688443060ece97724351b5ff37"
