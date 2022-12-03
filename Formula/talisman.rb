e1182c0c52ad5ede4a45a0829b1a4ef7099223d2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-12-03 05:29:27 +0000

    talisman 1.29.3
    Closes #117199.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/talisman.rb
+++ Formula/talisman.rb
@@ -1,8 +1,8 @@
 class Talisman < Formula
   desc "Tool to detect and prevent secrets from getting checked in"
   homepage "https://thoughtworks.github.io/talisman/"
-  url "https://github.com/thoughtworks/talisman/archive/v1.29.2.tar.gz"
-  sha256 "0b2ff1e468d20388c382341068eea25e3e45e628220c277391aee1e21c0223a1"
+  url "https://github.com/thoughtworks/talisman/archive/v1.29.3.tar.gz"
+  sha256 "ab9722f71f5b79c4a23dc838d99fbd5b8ba55a3de5410971fb3a95aa79c27d8e"
   license "MIT"
   version_scheme 1
   head "https://github.com/thoughtworks/talisman.git", branch: "master"
