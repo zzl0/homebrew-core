7930fc5c36874eb6d72b5eb0ddf3d79f2e123de1
Porkepix <porkepix@gmail.com>
2022-12-03 02:15:31 +0100

    ruff 0.0.152
    Closes #117189.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ruff.rb
+++ Formula/ruff.rb
@@ -1,8 +1,8 @@
 class Ruff < Formula
   desc "Extremely fast Python linter, written in Rust"
   homepage "https://github.com/charliermarsh/ruff"
-  url "https://github.com/charliermarsh/ruff/archive/refs/tags/v0.0.150.tar.gz"
-  sha256 "59da061ef6b7d94843058ec9ad77f69d9af2cec463f53d84e5da9d33655f2f58"
+  url "https://github.com/charliermarsh/ruff/archive/refs/tags/v0.0.152.tar.gz"
+  sha256 "be457c95c2681da10495ea2821dddb6e3a767ef3be305d2406100b351bcd7c98"
   license "MIT"
   head "https://github.com/charliermarsh/ruff.git", branch: "main"
 
