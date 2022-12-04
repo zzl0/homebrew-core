cf45beac250e0e86593c4d816edf7eebab51c558
Rui Chen <rui@chenrui.dev>
2022-12-04 17:59:12 -0500

    osmcoastline: revision bump (gdal 3.6.0)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/osmcoastline.rb
+++ Formula/osmcoastline.rb
@@ -4,7 +4,7 @@ class Osmcoastline < Formula
   url "https://github.com/osmcode/osmcoastline/archive/v2.3.1.tar.gz"
   sha256 "ab4a94b9bc5a5ab37b14ac4e9cbdf113d5fcf2d5a040a4eed958ffbc6cc1aa63"
   license "GPL-3.0-or-later"
-  revision 7
+  revision 8
 
   bottle do
     sha256 cellar: :any,                 arm64_ventura:  "d78afe585f65333a9ff766dfb3104f118868e64eeec399632affad9a2c688262"
