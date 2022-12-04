377a904ff1029826a3357a7bd1bf44cbb4e4683f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-12-04 02:26:58 +0000

    pnpm: update 7.18.0 bottle.

--- Formula/pnpm.rb
+++ Formula/pnpm.rb
@@ -13,14 +13,13 @@ class Pnpm < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "4315d45611ecd4410d2dda95beb0148133fc4529a5282811665598ccdf1dfd09"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "4315d45611ecd4410d2dda95beb0148133fc4529a5282811665598ccdf1dfd09"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4315d45611ecd4410d2dda95beb0148133fc4529a5282811665598ccdf1dfd09"
-    sha256 cellar: :any_skip_relocation, ventura:        "fd4608972ff070352ff761bea5568f71c7701037630f3404f05a4f89db1fe2b5"
-    sha256 cellar: :any_skip_relocation, monterey:       "fd4608972ff070352ff761bea5568f71c7701037630f3404f05a4f89db1fe2b5"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b2a9919dce2d8f13339df8a84ecc44fa2aed5893a236cb24ffb3a22e416b22cc"
-    sha256 cellar: :any_skip_relocation, catalina:       "b2a9919dce2d8f13339df8a84ecc44fa2aed5893a236cb24ffb3a22e416b22cc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4315d45611ecd4410d2dda95beb0148133fc4529a5282811665598ccdf1dfd09"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "5729d439b77dd7f2fe7ba6cff95d82322d62940634b93eb6109f854d9e320bd9"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5729d439b77dd7f2fe7ba6cff95d82322d62940634b93eb6109f854d9e320bd9"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5729d439b77dd7f2fe7ba6cff95d82322d62940634b93eb6109f854d9e320bd9"
+    sha256 cellar: :any_skip_relocation, ventura:        "54001eff12f455164b771b477961d5e1690a118f62fc0d4bbf905de1bf4818b5"
+    sha256 cellar: :any_skip_relocation, monterey:       "54001eff12f455164b771b477961d5e1690a118f62fc0d4bbf905de1bf4818b5"
+    sha256 cellar: :any_skip_relocation, big_sur:        "5ae46ff7920ec50d2a0e4a5236c6a5af607338b0f24a78a3b61e0d6af962cc0e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5729d439b77dd7f2fe7ba6cff95d82322d62940634b93eb6109f854d9e320bd9"
   end
 
   depends_on "node" => :test
