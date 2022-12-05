02e11fbd15bfb2cf1da6bb9684c20c4f125fffd5
Rui Chen <rui@chenrui.dev>
2022-12-05 14:46:36 +0000

    countdown: add 1.0.0 bottle.

--- Formula/countdown.rb
+++ Formula/countdown.rb
@@ -5,6 +5,16 @@ class Countdown < Formula
   sha256 "695bb6a57d7ba5a700ac16a7676132ee61c786a5e9a1ca774607d6f2ad5f39d7"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "4a2c9d5dbdd8ea55d9d4731b4df40f27e1d1beea7ff141ccb179ce6fbd50d056"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "4a2c9d5dbdd8ea55d9d4731b4df40f27e1d1beea7ff141ccb179ce6fbd50d056"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4a2c9d5dbdd8ea55d9d4731b4df40f27e1d1beea7ff141ccb179ce6fbd50d056"
+    sha256 cellar: :any_skip_relocation, monterey:       "40c915f6f73d94d3933f77cb8b7e30365381c2f0d499cc5f3c53283d3244f3d7"
+    sha256 cellar: :any_skip_relocation, big_sur:        "40c915f6f73d94d3933f77cb8b7e30365381c2f0d499cc5f3c53283d3244f3d7"
+    sha256 cellar: :any_skip_relocation, catalina:       "40c915f6f73d94d3933f77cb8b7e30365381c2f0d499cc5f3c53283d3244f3d7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "52390eca05c053bcf280d2aed3cc293178472c9ea71c32e729e2e63949518f35"
+  end
+
   depends_on "go" => :build
 
   def install
