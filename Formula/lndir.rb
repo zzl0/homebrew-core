3a9af4257e1a23db2cc993a2cbdf99e4f98ca3f3
Porkepix <porkepix@gmail.com>
2022-12-05 02:31:08 +0100

    lndir 1.0.4
    * lndir 1.0.4
    * lndir: update artifact
    
    Closes #117328.
    
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/lndir.rb
+++ Formula/lndir.rb
@@ -1,8 +1,8 @@
 class Lndir < Formula
   desc "Create a shadow directory of symbolic links to another directory tree"
   homepage "https://gitlab.freedesktop.org/xorg/util/lndir"
-  url "https://www.x.org/releases/individual/util/lndir-1.0.3.tar.bz2"
-  sha256 "49f4fab0de8d418db4ce80dad34e9b879a4199f3e554253a8e1ab68f7c7cb65d"
+  url "https://www.x.org/releases/individual/util/lndir-1.0.4.tar.xz"
+  sha256 "3e3437a9d3bb377755dd04a2c90d4c014d9fe90987ff73450bf5b8d161795e87"
   license "MIT-open-group"
 
   bottle do
