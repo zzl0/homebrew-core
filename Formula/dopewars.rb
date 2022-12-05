cbaaa0fd3119312018c07514a9a6567f9071028e
Rui Chen <rui@chenrui.dev>
2022-12-04 19:27:20 -0500

    dopewars 1.6.2
    * dopewars 1.6.2
    * don't chgrp on install
    * tweak dopewars install
    * fix splat
    
    Closes #117312.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: Jonathan Chang <me@jonathanchang.org>
    Signed-off-by: Jonathan Chang <me@jonathanchang.org>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/dopewars.rb
+++ Formula/dopewars.rb
@@ -1,8 +1,8 @@
 class Dopewars < Formula
   desc 'Free rewrite of a game originally based on "Drug Wars"'
   homepage "https://dopewars.sourceforge.io"
-  url "https://downloads.sourceforge.net/project/dopewars/dopewars/1.6.1/dopewars-1.6.1.tar.gz"
-  sha256 "83127903a61d81cda251a022f9df150d11e27bdd040e858c09c57927cc0edea6"
+  url "https://downloads.sourceforge.net/project/dopewars/dopewars/1.6.2/dopewars-1.6.2.tar.gz"
+  sha256 "623b9d1d4d576f8b1155150975308861c4ec23a78f9cc2b24913b022764eaae1"
   license "GPL-2.0-or-later"
 
   bottle do
@@ -24,17 +24,16 @@ class Dopewars < Formula
 
   def install
     inreplace "src/Makefile.in", "$(dopewars_DEPENDENCIES)", ""
+    inreplace "src/Makefile.in", "chmod", "true"
     inreplace "auxbuild/ltmain.sh", "need_relink=yes", "need_relink=no"
     inreplace "src/plugins/Makefile.in", "LIBADD =", "LIBADD = -module -avoid-version"
-    system "./configure", "--disable-gui-client",
+    system "./configure", *std_configure_args,
+                          "--disable-gui-client",
                           "--disable-gui-server",
                           "--enable-plugins",
                           "--enable-networking",
-                          "--prefix=#{prefix}",
-                          "--mandir=#{man}",
-                          "--disable-debug",
-                          "--disable-dependency-tracking"
-    system "make", "install"
+                          "--mandir=#{man}"
+    system "make", "install", "chgrp=true"
   end
 
   test do
