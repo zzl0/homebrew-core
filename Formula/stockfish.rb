5aef224ce1947a23b86434254617538764fd2257
Porkepix <porkepix@gmail.com>
2022-12-05 02:43:23 +0100

    stockfish 15.1
    Closes #117332.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/stockfish.rb
+++ Formula/stockfish.rb
@@ -1,8 +1,8 @@
 class Stockfish < Formula
   desc "Strong open-source chess engine"
   homepage "https://stockfishchess.org/"
-  url "https://github.com/official-stockfish/Stockfish/archive/sf_15.tar.gz"
-  sha256 "0553fe53ea57ce6641048049d1a17d4807db67eecd3531a3749401362a27c983"
+  url "https://github.com/official-stockfish/Stockfish/archive/sf_15.1.tar.gz"
+  sha256 "d4272657905319328294355973faee40a8c28e3eecb0e7b266ed34ff33383b76"
   license "GPL-3.0-only"
   head "https://github.com/official-stockfish/Stockfish.git", branch: "master"
 
