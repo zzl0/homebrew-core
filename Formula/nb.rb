65417710909b122d166a0975c486ecfdba16a368
Patrick Linnane <patrick@linnane.io>
2022-12-05 04:23:16 +0000

    nb: update 7.2.0 bottle.

--- Formula/nb.rb
+++ Formula/nb.rb
@@ -7,13 +7,12 @@ class Nb < Formula
   head "https://github.com/xwmx/nb.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "8bdf43e4a720bd6538ae8dc60a43cac6f6c61fb98f229c10b9536b4c691ef355"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8bdf43e4a720bd6538ae8dc60a43cac6f6c61fb98f229c10b9536b4c691ef355"
-    sha256 cellar: :any_skip_relocation, ventura:        "a8d0036da7ab1799f79b272537f28cd00e59935d90278c158439fec200dda390"
-    sha256 cellar: :any_skip_relocation, monterey:       "a8d0036da7ab1799f79b272537f28cd00e59935d90278c158439fec200dda390"
-    sha256 cellar: :any_skip_relocation, big_sur:        "a8d0036da7ab1799f79b272537f28cd00e59935d90278c158439fec200dda390"
-    sha256 cellar: :any_skip_relocation, catalina:       "a8d0036da7ab1799f79b272537f28cd00e59935d90278c158439fec200dda390"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8bdf43e4a720bd6538ae8dc60a43cac6f6c61fb98f229c10b9536b4c691ef355"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "1bb6184b4c07b58dc7e51b76c6f6d77a249c4c356c411bd11a195d74a86bfb98"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1bb6184b4c07b58dc7e51b76c6f6d77a249c4c356c411bd11a195d74a86bfb98"
+    sha256 cellar: :any_skip_relocation, ventura:        "528e9d86c4bd78a135de4d9dbe4a2c2c51eba8bda05f9ac3e384b315d8078d90"
+    sha256 cellar: :any_skip_relocation, monterey:       "528e9d86c4bd78a135de4d9dbe4a2c2c51eba8bda05f9ac3e384b315d8078d90"
+    sha256 cellar: :any_skip_relocation, big_sur:        "528e9d86c4bd78a135de4d9dbe4a2c2c51eba8bda05f9ac3e384b315d8078d90"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1bb6184b4c07b58dc7e51b76c6f6d77a249c4c356c411bd11a195d74a86bfb98"
   end
 
   depends_on "bat"
