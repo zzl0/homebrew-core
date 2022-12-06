7a5cdae567623f0f5cd570c326778d7cbae93442
Ruoyu Zhong <zhongruoyu@outlook.com>
2022-12-06 21:58:52 +0800

    libre 2.10.0

--- Formula/libre.rb
+++ Formula/libre.rb
@@ -1,8 +1,8 @@
 class Libre < Formula
   desc "Toolkit library for asynchronous network I/O with protocol stacks"
   homepage "https://github.com/baresip/re"
-  url "https://github.com/baresip/re/archive/refs/tags/v2.9.0.tar.gz"
-  sha256 "d820e1bf595b35e5e71f33d131d105aca2c53b05876abfe5e80303aa502583f0"
+  url "https://github.com/baresip/re/archive/refs/tags/v2.10.0.tar.gz"
+  sha256 "4d2b6f8fc73efdbcb5a7b2a98d0b46ac6eb28ede5ae90f9596b49663eec623a9"
   license "BSD-3-Clause"
 
   bottle do
