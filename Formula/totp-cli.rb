2a3d4170832576e858112651bf8e1381a5589fd9
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2022-12-06 08:33:01 +0000

    totp-cli: add 1.2.4 bottle.

--- Formula/totp-cli.rb
+++ Formula/totp-cli.rb
@@ -6,6 +6,16 @@ class TotpCli < Formula
   license "MIT"
   head "https://github.com/yitsushi/totp-cli.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "e8253cf4a0d4a3454ccfbefd62636d561fc68af0d51ab4c231f6496f075a8c5e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e8253cf4a0d4a3454ccfbefd62636d561fc68af0d51ab4c231f6496f075a8c5e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e8253cf4a0d4a3454ccfbefd62636d561fc68af0d51ab4c231f6496f075a8c5e"
+    sha256 cellar: :any_skip_relocation, ventura:        "c0e5f2d9f95bebbf9019e35147c6d3fb57169b1b553eb749a36a0273f4b85fcc"
+    sha256 cellar: :any_skip_relocation, monterey:       "c0e5f2d9f95bebbf9019e35147c6d3fb57169b1b553eb749a36a0273f4b85fcc"
+    sha256 cellar: :any_skip_relocation, big_sur:        "c0e5f2d9f95bebbf9019e35147c6d3fb57169b1b553eb749a36a0273f4b85fcc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c75fd7c2487a65604209d70578610dc7c24e111bc01228799ceb5a45d7996197"
+  end
+
   depends_on "go" => :build
 
   def install
