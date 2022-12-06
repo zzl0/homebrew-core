e34050fbcd4bd0d2c3a094b1f008a8490246a1b7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-12-06 04:04:47 +0000

    virtualenv: update 20.17.1 bottle.

--- Formula/virtualenv.rb
+++ Formula/virtualenv.rb
@@ -9,14 +9,13 @@ class Virtualenv < Formula
   head "https://github.com/pypa/virtualenv.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "54d3bb49f11c7b0ede94e9f620dd29eced7026e08559123e763bd37d70bbc3b9"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3241f3fb376ba1142166c1bed03c4fd4b622c3adb7c05ea77ad39d5bfd7d182c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "208783d8b0de470141988a332ffbed5cb0f55d482a1c3db91750e184d0e6fcb5"
-    sha256 cellar: :any_skip_relocation, ventura:        "6c4fb1042b4c6919bd17984694e1bf4b520db257b21e91343ae06cff654291a8"
-    sha256 cellar: :any_skip_relocation, monterey:       "e7e14d6e58ea206e811ade9ab9f4266bb20bb14248861f894435a8c3eb8199ad"
-    sha256 cellar: :any_skip_relocation, big_sur:        "283bf18cd948ea937c14ce3af5db8aa4012ed6e7f81c8ff2e23630a044ff3e66"
-    sha256 cellar: :any_skip_relocation, catalina:       "21335f1b50bbfe14eaeb8025a7138537d4e158168293cc9bc0a7ef85c658aba2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ba49fbfff6cc8e9881403db6e7942f4de5b0ec4f7abc394837fa7c870e027eb8"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "b6433eccf52ce3b733b122932e64c2446bedfbec22f5bcf84ab6323968541dca"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a86a36013d8fb5f6878ff0fcfa37cce0db787a141ef9221f92f9d05b3075ca47"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "483243cfbff1224994175b773bb622e0acdf4acba3af10c0d377ec68aa11d20d"
+    sha256 cellar: :any_skip_relocation, ventura:        "cbd564239827182063e09cbd58dfaecb54abc213612a7ae86fab8a1cfdf9da9d"
+    sha256 cellar: :any_skip_relocation, monterey:       "1bb516dae5be32bb7953aa9109fc70d4f7e4f9a84f13f092d0e567b550611970"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d30200f296078f3ed33873306afbec5dc59def61be4b44926fff655d00dfb50c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c8fc9ae634e4488637972c71ee3d410cee13349b710d0982010d09d07244352a"
   end
 
   depends_on "python@3.11"
