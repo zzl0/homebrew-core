de366a4037ea027916b882bed730cd89ec3a6226
Ruoyu Zhong <zhongruoyu@outlook.com>
2022-12-08 01:58:52 +0800

    glooctl 1.12.36
    Closes #117583.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/glooctl.rb
+++ Formula/glooctl.rb
@@ -4,8 +4,8 @@ class Glooctl < Formula
   # NOTE: Please wait until the newest stable release is finished building and
   # no longer marked as "Pre-release" before creating a PR for a new version.
   url "https://github.com/solo-io/gloo.git",
-      tag:      "v1.12.35",
-      revision: "67640c6f0514a2379143f7337f086a31c7985fef"
+      tag:      "v1.12.36",
+      revision: "8533ccc90a1740fa1ef4f2b33fb83fd81c41bca8"
   license "Apache-2.0"
   head "https://github.com/solo-io/gloo.git", branch: "master"
 
