e33f9ec1d8044d4bf0b4429607732d745a6215a7
Porkepix <porkepix@gmail.com>
2022-12-07 02:20:48 +0100

    d2 0.1.0
    * d2 0.1.0
    * d2: update build
      relates to https://github.com/terrastruct/d2/commit/c55bf62438750294bdf45a6e7e1f6da93ad73d0c
    
    Closes #117508.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/d2.rb
+++ Formula/d2.rb
@@ -1,8 +1,8 @@
 class D2 < Formula
   desc "Modern diagram scripting language that turns text to diagrams"
   homepage "https://d2lang.com/"
-  url "https://github.com/terrastruct/d2/archive/refs/tags/v0.0.13.tar.gz"
-  sha256 "52fd8cddfb7fb34d88a0d6049c038bc31447216ede659d892ec13e288ab966e7"
+  url "https://github.com/terrastruct/d2/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "78fac0235fd583e28d961b0fd066994095a9cec4d5a834747833bbab042ab1c0"
   license "MPL-2.0"
   head "https://github.com/terrastruct/d2.git", branch: "master"
 
@@ -20,7 +20,7 @@ class D2 < Formula
 
   def install
     ldflags = "-s -w -X oss.terrastruct.com/d2/lib/version.Version=#{version}"
-    system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/d2"
+    system "go", "build", *std_go_args(ldflags: ldflags)
   end
 
   test do
