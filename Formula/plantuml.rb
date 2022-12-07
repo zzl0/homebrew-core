72c5f7642bbac71d8285230e6e5e246447194b48
Porkepix <porkepix@gmail.com>
2022-12-07 02:58:16 +0100

    plantuml 1.2022.14
    Closes #117518.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/plantuml.rb
+++ Formula/plantuml.rb
@@ -1,8 +1,8 @@
 class Plantuml < Formula
   desc "Draw UML diagrams"
   homepage "https://plantuml.com/"
-  url "https://github.com/plantuml/plantuml/releases/download/v1.2022.13/plantuml-1.2022.13.jar"
-  sha256 "dd54cde638aad75aafdbaa7a28dca0dc6bb4dcba8cacce5c810572c5e5d2a522"
+  url "https://github.com/plantuml/plantuml/releases/download/v1.2022.14/plantuml-1.2022.14.jar"
+  sha256 "159ca2021f2efa5481b56417e2abd0a301348b018fe3c34b106f41023596dd43"
   license "GPL-3.0-or-later"
   version_scheme 1
 
