e1dc38e318d2baa59976b742e4f7758e29b2158a
Porkepix <porkepix@gmail.com>
2022-12-07 02:08:36 +0100

    bwfmetaedit 22.11
    Closes #117503.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/bwfmetaedit.rb
+++ Formula/bwfmetaedit.rb
@@ -1,8 +1,8 @@
 class Bwfmetaedit < Formula
   desc "Tool for embedding, validating, and exporting BWF file metadata"
   homepage "https://mediaarea.net/BWFMetaEdit"
-  url "https://mediaarea.net/download/binary/bwfmetaedit/21.07/BWFMetaEdit_CLI_21.07_GNU_FromSource.tar.bz2"
-  sha256 "7b55d9c6df989a3cbff2bb2c9a18b0b0f3a80fa716539a160d612194906d94f2"
+  url "https://mediaarea.net/download/binary/bwfmetaedit/22.11/BWFMetaEdit_CLI_22.11_GNU_FromSource.tar.bz2"
+  sha256 "82ddb459b5c624dd1debf03e2a21758c514fbcebdea69cdd94364f777751f897"
   license "0BSD"
 
   livecheck do
