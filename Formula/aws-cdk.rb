febd37a1e64ad1c5cf9cbcca8cc63ab7c60fe8bf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-12-07 20:04:52 +0000

    aws-cdk: update 2.54.0 bottle.

--- Formula/aws-cdk.rb
+++ Formula/aws-cdk.rb
@@ -8,14 +8,13 @@ class AwsCdk < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "1876049a6492a49cf1390268b6bcb6bfb4a5f10328cd3f07f27bed93d2112508"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "1876049a6492a49cf1390268b6bcb6bfb4a5f10328cd3f07f27bed93d2112508"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1876049a6492a49cf1390268b6bcb6bfb4a5f10328cd3f07f27bed93d2112508"
-    sha256 cellar: :any_skip_relocation, ventura:        "055b01ae28e7ce0b03cd015a8bb19febc879df9ad8015167014ecb3b2f372011"
-    sha256 cellar: :any_skip_relocation, monterey:       "055b01ae28e7ce0b03cd015a8bb19febc879df9ad8015167014ecb3b2f372011"
-    sha256 cellar: :any_skip_relocation, big_sur:        "055b01ae28e7ce0b03cd015a8bb19febc879df9ad8015167014ecb3b2f372011"
-    sha256 cellar: :any_skip_relocation, catalina:       "055b01ae28e7ce0b03cd015a8bb19febc879df9ad8015167014ecb3b2f372011"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "261f7ae3769d5bd1c7a23ce2660afe2cb0e1d94cc2c71b9bf1c3c196ed2b6dcf"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "c41f3c5a95e9ad66c6c09cb2cd18c89989ca35096bbcf274f3cea6ec4e8f0365"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c41f3c5a95e9ad66c6c09cb2cd18c89989ca35096bbcf274f3cea6ec4e8f0365"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c41f3c5a95e9ad66c6c09cb2cd18c89989ca35096bbcf274f3cea6ec4e8f0365"
+    sha256 cellar: :any_skip_relocation, ventura:        "c7f461f71e35515207ede16a84fa858399271a9c50b7919ae8d1c1ef69607500"
+    sha256 cellar: :any_skip_relocation, monterey:       "c7f461f71e35515207ede16a84fa858399271a9c50b7919ae8d1c1ef69607500"
+    sha256 cellar: :any_skip_relocation, big_sur:        "c7f461f71e35515207ede16a84fa858399271a9c50b7919ae8d1c1ef69607500"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ab6aeeb6bfcd7e09b9c422f8f50eeeeb92a91c6dc1e021734d0937ab3e9dfdec"
   end
 
   depends_on "node"
