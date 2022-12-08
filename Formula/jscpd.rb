d456fcc54773b5ea1b504191f968fe5d402d7e86
Rui Chen <rui@chenrui.dev>
2022-12-08 00:08:37 -0500

    jscpd 3.5.1 (new formula)
    Closes #117632.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/jscpd.rb
@@ -0,0 +1,32 @@
+require "language/node"
+
+class Jscpd < Formula
+  desc "Copy/paste detector for programming source code"
+  homepage "https://github.com/kucherenko/jscpd"
+  url "https://registry.npmjs.org/jscpd/-/jscpd-3.5.1.tgz"
+  sha256 "90a6ce1dffaa1031c76baad87c9aff221cc2ae5caed9eb9b6e02c6f09f210e37"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    test_file = testpath/"test.js"
+    test_file2 = testpath/"test2.js"
+    test_file.write <<~EOS
+      console.log("Hello, world!");
+    EOS
+    test_file2.write <<~EOS
+      console.log("Hello, brewtest!");
+    EOS
+
+    output = shell_output("#{bin}/jscpd --min-lines 1 #{testpath}/*.js 2>&1")
+    assert_match "Found 0 clones", output
+
+    assert_match version.to_s, shell_output("#{bin}/jscpd --version")
+  end
+end
