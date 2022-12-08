fcb542646db0e819204cb90da8395eb71ad38813
Rui Chen <rui@chenrui.dev>
2022-12-08 21:33:19 +0000

    gtk+3: update 3.24.35 bottle.

--- Formula/gtk+3.rb
+++ Formula/gtk+3.rb
@@ -11,14 +11,13 @@ class Gtkx3 < Formula
   end
 
   bottle do
-    sha256 arm64_ventura:  "285e0eb1e3b0f12fa353239e601a65e6dd89b76a6ab99b8346efb1168cafcb02"
-    sha256 arm64_monterey: "433f1b8aa72afcaf5784b099c40373e60da5a5782dddbe867bd315b6dfa9466f"
-    sha256 arm64_big_sur:  "f033af2b4714244846c3eabf558626d356e0cdcff6e5b8a1302e6d7a8b21a0e7"
-    sha256 ventura:        "c141379f93930f791618e95bc877743f46f809963f1b17a9b40c18b37b87db10"
-    sha256 monterey:       "309a7ff2894c4290d18a52f83c36ba9f9b8d156492d8034f7b39fe7771127a47"
-    sha256 big_sur:        "6b502ca8e38cfc2bbf63e4d72609cb70a829381240ebd1965412ec0d80013dc9"
-    sha256 catalina:       "2b3df91ceb20e12404020549f3cf94710c99379b9090dd327ec29a00f5bb1105"
-    sha256 x86_64_linux:   "9319cbd1929a839be964268450d593b3630ab749dc906a2f6ee643beb47e538b"
+    sha256 arm64_ventura:  "9b2efc06a7642c64077d80b6e3411e8d51ab3233895b19e502d3ce4d73cba1a6"
+    sha256 arm64_monterey: "ab94913ae9bf73f5457a71b5d57c230d7a8ce9d0a99e473e70aa17ec327f610f"
+    sha256 arm64_big_sur:  "234803d64c7f59029cd6a31050a774de5a5af82629113c773568770f61da24af"
+    sha256 monterey:       "69de0c3dae59346f049d267b7ec2c2c0f47dcc659a72f28128d6eebae8b4481e"
+    sha256 big_sur:        "9d7b366b498ed385cf8b1850b3d73d86c83cb58c2065b782c915eefbbac0f25d"
+    sha256 catalina:       "3e7aebc47f8a600f25a63b9ccaec72ab9fb4fecfa9c97e48c018b32e114e0e2b"
+    sha256 x86_64_linux:   "d6208fd58f89bc1d5fa82b05d213f0ee8dc867d55563c54fa46228c76973090e"
   end
 
   depends_on "docbook" => :build
