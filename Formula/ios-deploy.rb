96ac2755294f475c117973e27f3d6a78619a62a5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-12-08 22:04:28 +0000

    ios-deploy: update 1.12.1 bottle.

--- Formula/ios-deploy.rb
+++ Formula/ios-deploy.rb
@@ -10,13 +10,12 @@ class IosDeploy < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "96bca7e7f751379de2241853b9cf2e0fe1f518493e9365c31116fb3a309c21ff"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a9afe8fd08f085f05104499bee9dd6e79110cf9f63fd7be57fc5c28628af9a09"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9d472b334895a1863262cfdceaf777ab9a6c675c265e4d83265ae3c2d9b25793"
-    sha256 cellar: :any_skip_relocation, ventura:        "ffd40c6b9216bfd90adf4c1cfe7aafd263e79b795794bded1dd05eeb986a78cb"
-    sha256 cellar: :any_skip_relocation, monterey:       "eb4951c90cae9df4a16a5c20cd24393647899eebef8fb4f313f9eb999a28230e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "4920f28ef96b15c91cbc7e0d84232f4756dd4ecb50bc5fc4b47cb6e5137ab36a"
-    sha256 cellar: :any_skip_relocation, catalina:       "6f6b852da9e2caf687adec64fbf683d2be92e1c04c00e5fb93db6faca92ed22c"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "70ee426a92f9c051982e92df8d46723cc89b8fecb7d696b99720c13d3b98007b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "da920d213de78388f4dfeb2c87e8c93f188aaa3acc506eef86ce3e6762dec43a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5d047f57995db0f9c5897455967d85c8ddd8413b853ffda376467c04a8d47960"
+    sha256 cellar: :any_skip_relocation, ventura:        "96daffa7e01337c33d71ca1afdba51b34e100327d171a4c08a0b390b404237a9"
+    sha256 cellar: :any_skip_relocation, monterey:       "9b8206addd8b1d07a8b50fa2a88c9c9ebf8697a9fefa21835336019193ca9102"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ea7341be8f08529d848ffe7fe7bfb75cbbb42e0d7d017667c704de1f0a12a4e0"
   end
 
   depends_on xcode: :build
