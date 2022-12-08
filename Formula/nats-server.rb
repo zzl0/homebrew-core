1c4965d9fd206edce5d03ffbe6fa91f5c2d3c3ce
Ruoyu Zhong <zhongruoyu@outlook.com>
2022-12-09 01:51:44 +0800

    nats-server 2.9.9
    Closes #117668.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/nats-server.rb
+++ Formula/nats-server.rb
@@ -1,8 +1,8 @@
 class NatsServer < Formula
   desc "Lightweight cloud messaging system"
   homepage "https://nats.io"
-  url "https://github.com/nats-io/nats-server/archive/v2.9.8.tar.gz"
-  sha256 "31ad53e46a3dafdd9dfe52ce751b7fd5eaf838810c42d002b11477ee8d6e3471"
+  url "https://github.com/nats-io/nats-server/archive/v2.9.9.tar.gz"
+  sha256 "1a7face94149f366a2f3e55f80271bcad926ca82cb072854eeec0ebcdea74f45"
   license "Apache-2.0"
   head "https://github.com/nats-io/nats-server.git", branch: "main"
 
