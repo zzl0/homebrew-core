1158d4977a188f57f407b2eecc2861f2c83bf262
Zhong Ruoyu <zhongruoyu@outlook.com>
2022-12-08 13:54:16 +0800

    rollup 3.7.0
    Closes #117634.
    
    Signed-off-by: Dawid Dziurla <9713907+dawidd6@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/rollup.rb
+++ Formula/rollup.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Rollup < Formula
   desc "Next-generation ES module bundler"
   homepage "https://rollupjs.org/"
-  url "https://registry.npmjs.org/rollup/-/rollup-3.6.0.tgz"
-  sha256 "e3663a443fe8bed64380ea6f41e7a1fa35f74f57a39da506917f1475e48954e8"
+  url "https://registry.npmjs.org/rollup/-/rollup-3.7.0.tgz"
+  sha256 "ae1fa1d99612ddaf21c497398e67f1b1aa920987b6095fd364531862fd668f0f"
   license all_of: ["ISC", "MIT"]
 
   bottle do
