d7f99444b95d8191f6424b5ee11f1a5465a109eb
fanquake <fanquake@gmail.com>
2022-12-09 13:52:51 +0000

    linux-headers@5.15 5.15.82
    Closes #117751.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/linux-headers@5.15.rb
+++ Formula/linux-headers@5.15.rb
@@ -1,8 +1,8 @@
 class LinuxHeadersAT515 < Formula
   desc "Header files of the Linux kernel"
   homepage "https://kernel.org/"
-  url "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.15.81.tar.gz"
-  sha256 "5633c0ab2fe76c43bc22e53f747ec111363f4c3824041a9b18943732a5a7f40f"
+  url "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.15.82.tar.gz"
+  sha256 "2a138780b0d57648a454e09c0321464bd083f9cccd694bdc4e0c5b0f399b2ff1"
   license "GPL-2.0-only"
 
   bottle do
