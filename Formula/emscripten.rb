28e1fcf0eb958d9d936e9c32d806e41362078edd
Zhong Ruoyu <zhongruoyu@outlook.com>
2022-12-10 01:58:16 +0800

    emscripten 3.1.28
    * emscripten 3.1.28
    * emscripten: update resources
    
    Closes #117758.
    
    Co-authored-by: Ruoyu Zhong <zhongruoyu@outlook.com>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/emscripten.rb
+++ Formula/emscripten.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Emscripten < Formula
   desc "LLVM bytecode to JavaScript compiler"
   homepage "https://emscripten.org/"
-  url "https://github.com/emscripten-core/emscripten/archive/3.1.27.tar.gz"
-  sha256 "fca055c262026857bb6fab1e71b734dd28a950fa0eebd23d8179fcc72016de43"
+  url "https://github.com/emscripten-core/emscripten/archive/3.1.28.tar.gz"
+  sha256 "0f8b25cac5b2a55007a45c5bfa2b918add1df90bf624bb47510d8bc887c39901"
   license all_of: [
     "Apache-2.0", # binaryen
     "Apache-2.0" => { with: "LLVM-exception" }, # llvm
@@ -51,7 +51,7 @@ class Emscripten < Formula
   # See llvm resource below for instructions on how to update this.
   resource "binaryen" do
     url "https://github.com/WebAssembly/binaryen.git",
-        revision: "eeb838155dbff2fb6731f1ae5550cbb9494532fe"
+        revision: "2cb5cefb6392619d908ce2ab683815d7e22ac9a5"
   end
 
   # emscripten does not support using the stable version of LLVM.
@@ -63,7 +63,7 @@ class Emscripten < Formula
   # Then use the listed llvm_project_revision for the resource below.
   resource "llvm" do
     url "https://github.com/llvm/llvm-project.git",
-        revision: "dee009d3b5c71a340eb4ebb3cfb49f6015a789cc"
+        revision: "ea4be70cea8509520db8638bb17bcd7b5d8d60ac"
   end
 
   def install
