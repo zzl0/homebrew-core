60cc12279fda5fb52ba95fc8814d5e715225a193
Branch Vincent <branchevincent@gmail.com>
2022-12-09 10:09:32 -0800

    fd 8.6.0
    Closes #117759.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/fd.rb
+++ Formula/fd.rb
@@ -1,8 +1,8 @@
 class Fd < Formula
   desc "Simple, fast and user-friendly alternative to find"
   homepage "https://github.com/sharkdp/fd"
-  url "https://github.com/sharkdp/fd/archive/v8.5.3.tar.gz"
-  sha256 "45a6444cf5bbfcf4ee4836d9a2ff2106d31e67da77341183392225badc87cd35"
+  url "https://github.com/sharkdp/fd/archive/v8.6.0.tar.gz"
+  sha256 "e71a139f9ca20f63bab3700d5f8810bc12ef2c4fc6e1dd680f5c96fa9d1d2834"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/sharkdp/fd.git", branch: "master"
 
