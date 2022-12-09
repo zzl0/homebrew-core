2f80ba61909b47d921491915467c9f9ce6554457
Rui Chen <rui@chenrui.dev>
2022-12-09 19:05:42 +0000

    clusterawsadm: add 2.0.2 bottle.

--- Formula/clusterawsadm.rb
+++ Formula/clusterawsadm.rb
@@ -7,6 +7,16 @@ class Clusterawsadm < Formula
   license "Apache-2.0"
   head "https://github.com/kubernetes-sigs/cluster-api-provider-aws.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "b5f784e2bce3bf83520ba1505c7d03e80c92a8db634342408d752989f0320e38"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e721e228bd9d1512f5950bb05d666dc6f2169663f3328940bdbeecd6b81161cc"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7163cfd35f4327e98e110229c15ec8b4de608e7ed10c6d5d7b9f24fe366a8e54"
+    sha256 cellar: :any_skip_relocation, ventura:        "3416aa9679b94721c5eb1eb83979e1975e2051fe9811d9dbfcfaccbbb5040173"
+    sha256 cellar: :any_skip_relocation, monterey:       "459ef54405bd2e15487389b6756c16a9832f011ffb758798dfc460e5bc5ac98c"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f76f25f3139c0904588b85683fdc1d0979d02e5e5cd941bdef16872a7ab757c0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "19d3cfe144245cf02af731b7140d9365005209b138f72a9b407be8fe6d24b541"
+  end
+
   depends_on "go" => :build
 
   def install
