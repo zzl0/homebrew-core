fe48cee9ef179cfa331174b64d0d2bb05bb3c730
Patrick Linnane <patrick@linnane.io>
2022-12-10 18:48:15 +0000

    luacheck: add 1.0.0 bottle.

--- Formula/luacheck.rb
+++ Formula/luacheck.rb
@@ -6,6 +6,16 @@ class Luacheck < Formula
   license "MIT"
   head "https://github.com/lunarmodules/luacheck.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "27ba5636ef76bbeecc3cd27ec5947f4627501dc738d344fcf41dd89bceeb226b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "efc98e9b4317978385343cf3185829d0d15f9f2cef0c0085ac01754bdd84a3bb"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f67c15c64868e620a5edc5f4e1f7d61d0d1df5be49e92ec9cb0c9881c3464cf5"
+    sha256 cellar: :any_skip_relocation, ventura:        "68a42e4a006623c4897cba39c758128b0efaf892f8530fd8f30241604bd36f16"
+    sha256 cellar: :any_skip_relocation, monterey:       "6f3b0a98a2a65e8db03c382d64e9961776eb053442500e3688a5a9d4d81664d7"
+    sha256 cellar: :any_skip_relocation, big_sur:        "7eb48083328fdd7a3556dd3b12ce0b4ea1fa90442b12517650c22a9a32e5e339"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6372ea278050cbb360766dde7c361012a8a84bede7ce973b365b27cfa8359033"
+  end
+
   depends_on "luarocks" => :build
   depends_on "lua"
 
