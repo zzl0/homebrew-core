902387e302c2fc5345d29c62d59d0f5dbd94c707
Patrick Linnane <patrick@linnane.io>
2022-12-10 18:53:29 +0000

    busted: add 2.1.1 bottle.

--- Formula/busted.rb
+++ Formula/busted.rb
@@ -6,6 +6,16 @@ class Busted < Formula
   license "MIT"
   head "https://github.com/lunarmodules/busted.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "7a1a7e69313f1ffbcea6f2a14c3ded1a28e7211ed5e1b0d368e30dade4d43d07"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ae0a45974b644832a62c5328c2ea0a15dc3ab38ff975f7adaee4db85051984dd"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b0856fdcc1fdd7997a28f1d70a70fe0a5909b9ac42470ca39bab8c038d5540f8"
+    sha256 cellar: :any_skip_relocation, ventura:        "70d5e8bd2997df38cb674f5d7466706a536007ce00e7ed85809f982d2e8fb03f"
+    sha256 cellar: :any_skip_relocation, monterey:       "9b5b41875dec98d94762d3319857ee989a567ee1dba8018caff2052c03605cb5"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ae6f94da27bc893f5e59c7e194a34ff9884d70d798640f4f55fc5d810c779a64"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "436a10b61213c94b2b3b2148dbc7c17eb9f59954b2694ceeb0eb086543b2befb"
+  end
+
   depends_on "luarocks" => :build
   depends_on "lua"
 
