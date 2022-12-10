dbb14d9ee0be88f49d532c2ca4a5ae42ce2a8558
Porkepix <porkepix@gmail.com>
2022-12-10 11:31:35 +0100

    vagrant-completion 2.3.4
    Closes #117819.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/vagrant-completion.rb
+++ Formula/vagrant-completion.rb
@@ -1,8 +1,8 @@
 class VagrantCompletion < Formula
   desc "Bash completion for Vagrant"
   homepage "https://github.com/hashicorp/vagrant"
-  url "https://github.com/hashicorp/vagrant/archive/v2.3.3.tar.gz"
-  sha256 "06142fc406e96ed4f78dc877a20e2c8509c732c280fdfb44d1a84ce4f4c6ecc3"
+  url "https://github.com/hashicorp/vagrant/archive/v2.3.4.tar.gz"
+  sha256 "43eb1461c6dcfd23a0c386570e6c2a876e06d2388bbc0f1f0c9c99e393aa2f0f"
   license "MIT"
   head "https://github.com/hashicorp/vagrant.git", branch: "main"
 
