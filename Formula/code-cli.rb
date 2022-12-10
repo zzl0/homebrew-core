63689f7be416bf9674a1e1a89ea02ee2734982be
Patrick Linnane <patrick@linnane.io>
2022-12-10 18:48:54 +0000

    code-cli: add 1.74.0 bottle.

--- Formula/code-cli.rb
+++ Formula/code-cli.rb
@@ -11,6 +11,16 @@ class CodeCli < Formula
     strategy :github_latest
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "eb3546414865927d4e0cdea74c961818d2dded38a0b7e5ecdadf0840420142b3"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "087140dcda8710aa6f06665279263bbb42b1e1aac348108c9c156e0a2202bf14"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "57787df0637a861893a6e7c31979cd8ce1aa3e952b0e846ab8264dc6c85927e9"
+    sha256 cellar: :any_skip_relocation, ventura:        "bca95deb4dee80570ab887d45f0f04affadf39c03807f0a7f0d5d55e8b2ed1c2"
+    sha256 cellar: :any_skip_relocation, monterey:       "32bbcf69bc24bf34a95404325d1d44cc19089d896d31ede853a80b4ec23b64f2"
+    sha256 cellar: :any_skip_relocation, big_sur:        "41444fb2d659f4c5ec979844e135004bdbbe423660d1d7e0f2d1eb93be7e4d58"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "92850d0d363e194cf3797d62eb49283f5f1230a6288316de08dd07cf63482d22"
+  end
+
   depends_on "rust" => :build
 
   conflicts_with cask: "visual-studio-code"
