3102d27ea9c6a17ef98dc589f747ce7996670545
Porkepix <porkepix@gmail.com>
2022-12-11 02:13:00 +0100

    exempi 2.6.3
    * exempi 2.6.3
    * exempi: remove build patch
    
    Closes #117844.
    
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/exempi.rb
+++ Formula/exempi.rb
@@ -1,8 +1,8 @@
 class Exempi < Formula
   desc "Library to parse XMP metadata"
   homepage "https://wiki.freedesktop.org/libopenraw/Exempi/"
-  url "https://libopenraw.freedesktop.org/download/exempi-2.6.2.tar.bz2"
-  sha256 "4d17d4c93df2a95da3e3172c45b7a5bf317dd31dafd1c7a340169728c7089d1d"
+  url "https://libopenraw.freedesktop.org/download/exempi-2.6.3.tar.bz2"
+  sha256 "b0749db18a9e78cf771737954a838cdcdb1d5415888bac1ba9caf8cba77c656c"
   license "BSD-3-Clause"
 
   livecheck do
@@ -24,12 +24,6 @@ class Exempi < Formula
   depends_on "boost"
 
   uses_from_macos "expat"
-
-  # Fix -flat_namespace being used on Big Sur and later.
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
-    sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
-  end
 
   def install
     system "./configure", "--disable-dependency-tracking",
