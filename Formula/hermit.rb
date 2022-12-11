0fa53b1bd3f6d5d00bb7b7e8fb13d1701b79dce3
Patrick Linnane <patrick@linnane.io>
2022-12-11 04:41:56 +0000

    hermit: add 0.32.0 bottle.

--- Formula/hermit.rb
+++ Formula/hermit.rb
@@ -5,6 +5,16 @@ class Hermit < Formula
   sha256 "8dee269c6ee4045ee31676c3619ad1884c182a92e1b64441b8f0cf3fca005749"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "52b470f1b054128ae49efc39c2d801803d90e85444574055dd2b0c4616891981"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "0591e0ac5caf1da6a2747849ee3f67e3a2b6dd2b8a12962612843408afbe317c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "dd878aee71cdba2436c410700bf9ee481fa88767124b14195d37cf41d4a865f5"
+    sha256 cellar: :any_skip_relocation, ventura:        "3f3318da0ce10f519e33b9c4194f1026719deabe8c38dad710abd1aadad8e7d8"
+    sha256 cellar: :any_skip_relocation, monterey:       "ee491ae6fac989c6f41c3a871222f5077ccfece534bb24c0f74bb5724d428995"
+    sha256 cellar: :any_skip_relocation, big_sur:        "3e8698acb61ffbd6118c09558e5a28beb3a5f907bf652cb72ae71e0d60b05c36"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "07fc17a9711b4a569f00df653067885650611ac24a92f7ee36e0da29c5c6d2b0"
+  end
+
   depends_on "go" => :build
 
   def install
