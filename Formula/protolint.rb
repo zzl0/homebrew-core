f3590b9e3546b7f729ad2ba2c50cdc4007078a8f
Rui Chen <rui@chenrui.dev>
2022-12-10 20:44:31 -0500

    protolint 0.42.2 (new formula)
    Closes #117858.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/protolint.rb
@@ -0,0 +1,45 @@
+class Protolint < Formula
+  desc "Pluggable linter and fixer to enforce Protocol Buffer style and conventions"
+  homepage "https://github.com/yoheimuta/protolint"
+  url "https://github.com/yoheimuta/protolint.git",
+      tag:      "v0.42.2",
+      revision: "1a05b7f50c01dfa0e0991ae7e66eeb8ef0f607db"
+  license "MIT"
+  head "https://github.com/yoheimuta/protolint.git", branch: "master"
+
+  depends_on "go" => :build
+
+  def install
+    protolint_ldflags = %W[
+      -s -w
+      -X github.com/yoheimuta/protolint/internal/cmd.version=#{version}
+      -X github.com/yoheimuta/protolint/internal/cmd.revision=#{Utils.git_head(length: 8)}
+    ]
+    protocgenprotolint_ldflags = %W[
+      -s -w
+      -X github.com/yoheimuta/protolint/internal/cmd/protocgenprotolint.version=#{version}
+      -X github.com/yoheimuta/protolint/internal/cmd/protocgenprotolint.revision=#{Utils.git_head(length: 8)}
+    ]
+    system "go", "build", *std_go_args(ldflags: protolint_ldflags), "./cmd/protolint"
+    system "go", "build",
+      *std_go_args(ldflags: protocgenprotolint_ldflags, output: bin/"protoc-gen-protolint"),
+      "./cmd/protoc-gen-protolint"
+
+    pkgshare.install Dir["_example/proto/*.proto"]
+  end
+
+  test do
+    cp_r Dir[pkgshare/"*.proto"], testpath
+
+    output = "[invalidFileName.proto:1:1] File name \"invalidFileName.proto\" " \
+             "should be lower_snake_case.proto like \"invalid_file_name.proto\"."
+    assert_equal output,
+      shell_output("#{bin}/protolint lint #{testpath}/invalidFileName.proto 2>&1", 1).chomp
+
+    output = "Quoted string should be \"other.proto\" but was 'other.proto'."
+    assert_match output, shell_output("#{bin}/protolint lint #{testpath}/simple.proto 2>&1", 1)
+
+    assert_match version.to_s, shell_output("#{bin}/protolint version")
+    assert_match version.to_s, shell_output("#{bin}/protoc-gen-protolint version")
+  end
+end
