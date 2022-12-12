8b5c618c1aca0325770609b5c5d653de638db318
Charles Korn <charles.korn@grafana.com>
2022-12-12 16:02:35 +1100

    mimirtool 2.4.0 (new formula)
    * mimirtool 2.4.0 (new formula)
    * Incorporate PR feedback: formatting
    * Add better test.
    * Fix linting issues.
    
    Closes #118004.
    
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>
    Co-authored-by: Charles Korn <charleskorn@users.noreply.github.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/mimirtool.rb
@@ -0,0 +1,43 @@
+class Mimirtool < Formula
+  desc "CLI for interacting with Grafana Mimir"
+  homepage "https://grafana.com/docs/mimir/latest/operators-guide/tools/mimirtool/"
+  url "https://github.com/grafana/mimir.git",
+        tag:      "mimir-2.4.0",
+        revision: "32137ee2c4c41fa649abfb9582e1f33a9e13363b"
+  license "AGPL-3.0-only"
+  head "https://github.com/grafana/mimir.git", branch: "main"
+
+  livecheck do
+    url :stable
+    regex(/^mimir-(\d+\.\d+\.\d+)$/i)
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "make", "BUILD_IN_CONTAINER=false", "cmd/mimirtool/mimirtool"
+    bin.install "cmd/mimirtool/mimirtool"
+  end
+
+  test do
+    # Check that the version number was correctly embedded in the binary
+    assert_match version.to_s, shell_output("#{bin}/mimirtool version")
+
+    # Check that the binary runs as expected by testing the 'rules check' command
+    test_rule = <<~EOF
+      namespace: my_namespace
+      groups:
+        - name: example
+          interval: 5m
+          rules:
+            - record: job_http_inprogress_requests_sum
+              expr: sum by (job) (http_inprogress_requests)
+    EOF
+
+    (testpath/"rule.yaml").write(test_rule)
+
+    output = shell_output("#{bin}/mimirtool rules check #{testpath / "rule.yaml"} 2>&1", 1)
+    expected = "recording rule name does not match level:metric:operation format, must contain at least one colon"
+    assert_match expected, output
+  end
+end
