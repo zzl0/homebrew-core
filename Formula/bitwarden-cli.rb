36918bb91803374e76fb55e2c9db1ba2f5aa8234
Adam Moss <2951486+adam-moss@users.noreply.github.com>
2022-12-12 17:15:48 +0000

    bitwarden-cli: rebuild
    * Add BitWarden shell completion
    * bitwarden-cli: remove revision
    
    Closes #117960.
    
    Signed-off-by: Adam Moss <2951486+adam-moss@users.noreply.github.com>
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/bitwarden-cli.rb
+++ Formula/bitwarden-cli.rb
@@ -22,6 +22,8 @@ class BitwardenCli < Formula
   def install
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
     bin.install_symlink Dir[libexec/"bin/*"]
+
+    generate_completions_from_executable(bin/"bw", "completion", shell_parameter_format: :arg, shells: [:zsh])
   end
 
   test do
