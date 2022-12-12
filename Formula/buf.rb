c7ff4585e48c66a28408c819dd705b86459248ed
Patrick Linnane <patrick@linnane.io>
2022-12-12 05:45:08 +0000

    buf: add 1.10.0 bottle.

--- Formula/buf.rb
+++ Formula/buf.rb
@@ -6,6 +6,16 @@ class Buf < Formula
   license "Apache-2.0"
   head "https://github.com/bufbuild/buf.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "310be695fa41a1ad649a34d1ba402cbce4a9d2fbb7d913cf65551543e2d7b81e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "bf8bab964707233c4aebfd0bfcd145493d4a2eb56d9c82e5f090f1657e782216"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fcf6d75534eb032cda8ff1aa4b5018a90878da5e061bf80502b9ef0530251d22"
+    sha256 cellar: :any_skip_relocation, ventura:        "ff2dc26c9c5ecb6d2436f5e9c3bf1f47f4959ab84847b7113fd1a8b34f5d2f71"
+    sha256 cellar: :any_skip_relocation, monterey:       "d0d5f8d27acef1322715ffd32dfd5d8bda7e531c08d4c9b92da8b981d418945a"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b1dde6c3881493bd8b66ac3d8a09b69ace1ea91e6325a9e8f93e2f3ecbf7e982"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "88cd3665f7c65ac87adb341597f3a78b80eba7bacc5578e7981f1a7bd1abfee0"
+  end
+
   depends_on "go" => :build
 
   def install
