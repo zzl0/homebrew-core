efb0065cefc4934fb23b718e573e3f8bd3078a01
Zhong Ruoyu <zhongruoyu@outlook.com>
2022-12-13 19:05:09 +0800

    freetds 1.3.16
    Closes #118039.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/freetds.rb
+++ Formula/freetds.rb
@@ -4,8 +4,8 @@ class Freetds < Formula
   license "GPL-2.0-or-later"
 
   stable do
-    url "https://www.freetds.org/files/stable/freetds-1.3.15.tar.bz2", using: :homebrew_curl
-    sha256 "ffb323a25450f45700f3fe9d3e3fea688678f0235bd213139519f33375dfcf24"
+    url "https://www.freetds.org/files/stable/freetds-1.3.16.tar.bz2", using: :homebrew_curl
+    sha256 "9e26c5994473dcc008440bd73f89980c56fccbcb28a1eec7765be6e567393481"
 
     # Fix -flat_namespace being used on Big Sur and later.
     patch do
