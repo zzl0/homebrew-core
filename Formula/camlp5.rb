cbb875641066a8aa713642b13ec07e0213fe0251
Zhong Ruoyu <zhongruoyu@outlook.com>
2022-12-13 17:41:11 +0800

    camlp5 8.00.04
    Closes #118031.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/camlp5.rb
+++ Formula/camlp5.rb
@@ -1,10 +1,9 @@
 class Camlp5 < Formula
   desc "Preprocessor and pretty-printer for OCaml"
   homepage "https://camlp5.github.io/"
-  url "https://github.com/camlp5/camlp5/archive/refs/tags/rel8.00.03.tar.gz"
-  sha256 "1a710e2a6dbb0f4440867850d605f31fe8407ee8a56c9e067866e34e584385b4"
+  url "https://github.com/camlp5/camlp5/archive/refs/tags/rel8.00.04.tar.gz"
+  sha256 "bddbcb5c3c2d410c9a61c4dfb6e46e3bbe984d25ac68221a7a65c82a29956b1d"
   license "BSD-3-Clause"
-  revision 1
   head "https://github.com/camlp5/camlp5.git", branch: "master"
 
   livecheck do
