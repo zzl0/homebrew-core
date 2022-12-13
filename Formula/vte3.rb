149ecb5824c031d26792a5cbf9b7375f731c79b2
Caleb Xu <calebcenter@live.com>
2022-12-13 17:38:04 -0500

    vte3 0.70.2
    Closes #118096.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/vte3.rb
+++ Formula/vte3.rb
@@ -1,8 +1,8 @@
 class Vte3 < Formula
   desc "Terminal emulator widget used by GNOME terminal"
   homepage "https://wiki.gnome.org/Apps/Terminal/VTE"
-  url "https://download.gnome.org/sources/vte/0.70/vte-0.70.1.tar.xz"
-  sha256 "1f4601cbfea5302b96902208c8f185e5b18b259b5358bc93cf392bf59871c5b6"
+  url "https://download.gnome.org/sources/vte/0.70/vte-0.70.2.tar.xz"
+  sha256 "4d15b4380de3f564d57eabd006389c407c705df5b0c70030fdcc24971a334d80"
   license "LGPL-2.0-or-later"
 
   bottle do
