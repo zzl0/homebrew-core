e8aa57165ab26e63352c5f47571602311d5f7947
Ruoyu Zhong <zhongruoyu@outlook.com>
2022-12-14 02:51:33 +0800

    libspnav 1.1
    * libspnav 1.1
      Upstream has moved its source code to GitHub. So we may remove the
      livecheck as well.
    * libspnav: update homepage
      Following redirection from the previous homepage.
    * libspnav: add license
      Reference: https://github.com/FreeSpacenav/libspnav/blob/master/LICENSE
    * libspnav: fix test
    
    Closes #118086.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libspnav.rb
+++ Formula/libspnav.rb
@@ -1,13 +1,9 @@
 class Libspnav < Formula
   desc "Client library for connecting to 3Dconnexion's 3D input devices"
-  homepage "https://spacenav.sourceforge.io"
-  url "https://downloads.sourceforge.net/project/spacenav/spacenav%20library%20%28SDK%29/libspnav%200.3/libspnav-0.3.tar.gz"
-  sha256 "e1f855f47da6e75bdec81fe4b67171406abaf342c6fe3208c78e13bf862a3f05"
-
-  livecheck do
-    url :stable
-    regex(%r{url=.*?/libspnav[._-]v?(\d+(?:\.\d+)+)\.t}i)
-  end
+  homepage "https://spacenav.sourceforge.net/"
+  url "https://github.com/FreeSpacenav/libspnav/releases/download/v1.1/libspnav-1.1.tar.gz"
+  sha256 "04b297f68a10db4fa40edf68d7f823ba7b9d0442f2b665181889abe2cea42759"
+  license "BSD-3-Clause"
 
   bottle do
     sha256 cellar: :any,                 arm64_ventura:  "18dd7205e07c4380822fdf507f70aa5a460b942fb7aa2c6bdf064a0040426273"
@@ -43,7 +39,7 @@ class Libspnav < Formula
         return 0;
       }
     EOS
-    system ENV.cc, "test.cpp", "-I#{include}", "-L#{lib}", "-lspnav", "-o", "test"
+    system ENV.cc, "test.cpp", "-I#{include}", "-L#{lib}", "-lspnav", "-lm", "-o", "test"
     system "./test"
   end
 end
