b5c1b1a37a7d7daf67cdb2a4ba64ec40412df323
András Maróy <andras@maroy.hu>
2022-12-13 09:29:02 -0500

    skaffold@1.39 1.39.4 (new formula)
    Closes #118055.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/skaffold@1.39.rb
@@ -0,0 +1,27 @@
+class SkaffoldAT139 < Formula
+  desc "Easy and Repeatable Kubernetes Development"
+  homepage "https://skaffold.dev/"
+  url "https://github.com/GoogleContainerTools/skaffold.git",
+    tag:      "v1.39.4",
+    revision: "a3808f67c77a9c846ffb961ee6267f116cd30478"
+  license "Apache-2.0"
+
+  keg_only :versioned_formula
+
+  # https://cloud.google.com/deploy/docs/using-skaffold/select-skaffold#skaffold_version_deprecation_and_maintenance_policy
+  disable! date: "2023-10-18", because: :deprecated_upstream
+
+  depends_on "go" => :build
+
+  def install
+    system "make"
+    bin.install "out/skaffold"
+    generate_completions_from_executable(bin/"skaffold", "completion", shells: [:bash, :zsh])
+  end
+
+  test do
+    (testpath/"Dockerfile").write "FROM scratch"
+    output = shell_output("#{bin}/skaffold init --analyze").chomp
+    assert_equal '{"builders":[{"name":"Docker","payload":{"path":"Dockerfile"}}]}', output
+  end
+end
