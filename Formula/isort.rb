86e09421965c83dc0af3686fe2bddc617c855630
Patrick Linnane <patrick@linnane.io>
2022-12-14 00:54:46 +0000

    isort: update 5.11.2 bottle.

--- Formula/isort.rb
+++ Formula/isort.rb
@@ -14,13 +14,13 @@ class Isort < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "7cf6782c45d4a993b98d32b08c838c4e0d892fe630ec8147a3b93c0daf8d9cb0"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "1f12530193537a9ba59ad1faf710ae95b413b81ea010afb7439d745915315ab0"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1b627194f06da8b15b0442b427253444a2fefccf0128b8401ca1c4a6de0527dd"
-    sha256 cellar: :any_skip_relocation, ventura:        "daa1e1eb60c8300cde95f6826247b23700a5b8bfa1f8b08ad9951bf26cd31501"
-    sha256 cellar: :any_skip_relocation, monterey:       "3948c1cca67ee7ba7f0624fd6a937b1b3d9c549e888236abd280d4187e35ff76"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f27d9bcf36b244f96c09fbf72e619f7622cf06bccb94923a7ae516a6f42bedf2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d93aa4fc778454ebd10e07b6c0d1ed47a0f6b9e748f73f33ac60ddebdb7abb0d"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "2e15ada47fe4e01ba58692ae4de2da7dd80cc3989eced93be44ed6a0ad3b18c5"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c8196480e2be5b359b7f7b0764683d9b420a4927c38e9fe8c7e36872fa66da67"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "056f649794d994a696b352f80d5998902801830cd0c1e29ca5a73f2f2c266774"
+    sha256 cellar: :any_skip_relocation, ventura:        "e0ce7da2c5c9d56de18ab6665a2cd563afc4e4f1243b82c0fab367cf64d4c4ba"
+    sha256 cellar: :any_skip_relocation, monterey:       "66c9e90b7fa42e6b36bee1c76e3594c26ddb58b733a393f47b1b880b6eb020b2"
+    sha256 cellar: :any_skip_relocation, big_sur:        "034d0d70e22cbe0e52738cfaf724f0767f63a502482b24d05de3e90e6660ef8d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bc49377733fdf41010e4e163b93e73988504b09141e80a5170fcdaf89bc74f70"
   end
 
   depends_on "python@3.11"
