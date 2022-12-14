738ab2e46f3db08d94fb840a746fef9275df246e
Rui Chen <rui@chenrui.dev>
2022-12-14 11:56:59 -0500

    kubekey 3.0.4
    Closes #118161.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/kubekey.rb
+++ Formula/kubekey.rb
@@ -2,8 +2,8 @@ class Kubekey < Formula
   desc "Installer for Kubernetes and / or KubeSphere, and related cloud-native add-ons"
   homepage "https://kubesphere.io"
   url "https://github.com/kubesphere/kubekey.git",
-      tag:      "v3.0.3",
-      revision: "fab5588dc6aa05d9b58778a8b355e049794f35d2"
+      tag:      "v3.0.4",
+      revision: "f34bf693b4c1770a9fd51c58b5c1ad85e9664f95"
   license "Apache-2.0"
   head "https://github.com/kubesphere/kubekey.git", branch: "master"
 
