491f6bb107187492a8c15d4aaad6369d747ba38d
Rui Chen <rui@chenrui.dev>
2022-12-14 04:40:03 +0000

    cog: add 0.5.1 bottle.

--- Formula/cog.rb
+++ Formula/cog.rb
@@ -6,6 +6,16 @@ class Cog < Formula
   license "Apache-2.0"
   head "https://github.com/replicate/cog.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "ff8588765fb0e57e3189a43617988660b2af8c4a4f7e0e5e998d021254f77f33"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "69aa7f3a73761f4e781c897cc44201879b97a95de5ee5f3f200bccf5c03f798e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "266813340a64964de830f11e596c0e29410a7a3b275da99c8977c633f7cb4b43"
+    sha256 cellar: :any_skip_relocation, ventura:        "8893805247faa2744b7d0a17ef822bbaba590d76cd306adaa6031933182955ba"
+    sha256 cellar: :any_skip_relocation, monterey:       "b436dbac9b7056f6dd058f8e1dd02111d2dbcc19cf10b74a708eddfe328a0e21"
+    sha256 cellar: :any_skip_relocation, big_sur:        "98191db0eb6f8523b8bb2779bc8beba4230e5b4829d167000d79f04f0f687eda"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9469910c1f95d07aa6184d8a77e7f0e0e9a513b97a3e92dedc28dae4d0a821a7"
+  end
+
   depends_on "go" => :build
   depends_on "python@3.11" => :build
   depends_on "redis"
