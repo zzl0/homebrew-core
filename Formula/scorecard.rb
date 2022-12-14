ce42a4eb1af94f7534ae3bdcf6c972efe967ed31
Porkepix <porkepix@gmail.com>
2022-12-14 01:36:30 +0100

    scorecard 4.10.0
    Closes #118110.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/scorecard.rb
+++ Formula/scorecard.rb
@@ -2,8 +2,8 @@ class Scorecard < Formula
   desc "Security health metrics for Open Source"
   homepage "https://github.com/ossf/scorecard"
   url "https://github.com/ossf/scorecard.git",
-      tag:      "v4.8.0",
-      revision: "c40859202d739b31fd060ac5b30d17326cd74275"
+      tag:      "v4.10.0",
+      revision: "93ef087828b208a8143cfd352c546c8c01d790a8"
   license "Apache-2.0"
   head "https://github.com/ossf/scorecard.git", branch: "main"
 
