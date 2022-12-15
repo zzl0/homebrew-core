2d60886eb8ea2833b48a8bc3ed05855d770156a1
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2022-12-15 08:26:50 +0000

    syslog-ng: add 3.38.1 bottle.

--- Formula/syslog-ng.rb
+++ Formula/syslog-ng.rb
@@ -5,6 +5,16 @@ class SyslogNg < Formula
   sha256 "5491f686d0b829b69b2e0fc0d66a62f51991aafaee005475bfa38fab399441f7"
   license all_of: ["LGPL-2.1-or-later", "GPL-2.0-or-later"]
 
+  bottle do
+    sha256 arm64_ventura:  "675d1a2a2b70a6ba65300a2b79aa6fb0ca593dcdcffae7f029b4b65b516a5b97"
+    sha256 arm64_monterey: "c5b183bdee146482748c0f73f4c57ae223ca3411091a09712cba45cc272a4d88"
+    sha256 arm64_big_sur:  "f44ddd51a55cff76a51fcea4b280235b4a74f5377084d43987425b6f730e63d5"
+    sha256 monterey:       "fccdba6100604e263d3be61e0a2355eda2520c27dcde94cc6b953ee457fcb842"
+    sha256 big_sur:        "ac90f726c743e787eb3727618a53afc2350651057845609bbdea72568a3a5ed7"
+    sha256 catalina:       "72fd2a1b438e832e604f8487637aa81b6f1f489f5b6aa1f7554e18902a4f3fa9"
+    sha256 x86_64_linux:   "35d12e4a4b37549a947d1754aaad2f93c2a4b54aef4fd7e3266ccc90c526a114"
+  end
+
   depends_on "autoconf" => :build
   depends_on "autoconf-archive" => :build
   depends_on "automake" => :build
