bd64201b5910f02de077dc562a757c5730d011f5
Patrick Linnane <patrick@linnane.io>
2022-12-15 03:48:31 +0000

    hebcal: update 5.5.1 bottle.

--- Formula/hebcal.rb
+++ Formula/hebcal.rb
@@ -7,13 +7,13 @@ class Hebcal < Formula
   head "https://github.com/hebcal/hebcal.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "fc730482b3a96b0fb96dc9166ce5ba3091eeb18c570426b57e5659f4f26d367b"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "fc730482b3a96b0fb96dc9166ce5ba3091eeb18c570426b57e5659f4f26d367b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fc730482b3a96b0fb96dc9166ce5ba3091eeb18c570426b57e5659f4f26d367b"
-    sha256 cellar: :any_skip_relocation, ventura:        "76e9e0830a03bbc5d5ff021e2453d03d946ef3c634aec53fb41d28f40519efbf"
-    sha256 cellar: :any_skip_relocation, monterey:       "76e9e0830a03bbc5d5ff021e2453d03d946ef3c634aec53fb41d28f40519efbf"
-    sha256 cellar: :any_skip_relocation, big_sur:        "76e9e0830a03bbc5d5ff021e2453d03d946ef3c634aec53fb41d28f40519efbf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "426e6ac777343cfde93720a150dd2d793ec63fc30fdc24ed2e7a9517b5524e3a"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "1f538f34fd2d50ad9f47c2a3358c469fbadb28216411756c81eceff7d02d104f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "1f538f34fd2d50ad9f47c2a3358c469fbadb28216411756c81eceff7d02d104f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1f538f34fd2d50ad9f47c2a3358c469fbadb28216411756c81eceff7d02d104f"
+    sha256 cellar: :any_skip_relocation, ventura:        "0023899d5f5d6ec87ecb95704ad368fa45e60fbc645864628725e0001a799468"
+    sha256 cellar: :any_skip_relocation, monterey:       "0023899d5f5d6ec87ecb95704ad368fa45e60fbc645864628725e0001a799468"
+    sha256 cellar: :any_skip_relocation, big_sur:        "0023899d5f5d6ec87ecb95704ad368fa45e60fbc645864628725e0001a799468"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "58fdcbbf7b9a630d988dbe8a36c82b90cc3e703680740cef1f8f720c3d95235e"
   end
 
   depends_on "go" => :build
