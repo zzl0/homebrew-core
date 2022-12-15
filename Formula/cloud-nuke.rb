9a22628587ada8bf2ad9af7c2adb8f34c2136c71
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-12-15 01:32:05 +0000

    cloud-nuke: update 0.22.0 bottle.

--- Formula/cloud-nuke.rb
+++ Formula/cloud-nuke.rb
@@ -7,14 +7,13 @@ class CloudNuke < Formula
   head "https://github.com/gruntwork-io/cloud-nuke.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3ec42a5825c80d8740d19fbcc8275a062c47ab0441988b4e20d2a635e49d632f"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "2c45614bf0e96572f0fbb377d38a543150ec052e27e7c855ecb180653a25c725"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "57fb0e0a6060f634a7a5b7b2efabcd27c0eef459d55bb04827893235b4361de7"
-    sha256 cellar: :any_skip_relocation, ventura:        "8bc6048c38119cb47e7f6deebd90d458116a6766201c7f87187387659d91c9f0"
-    sha256 cellar: :any_skip_relocation, monterey:       "cafd814c784e7a9d48a9c4deab10169338733ee29d434e30197cd4c31bb95d58"
-    sha256 cellar: :any_skip_relocation, big_sur:        "bb3b864efb541be20ba7a70a27e25ead45ee8019356bbb618a633addb1aec26d"
-    sha256 cellar: :any_skip_relocation, catalina:       "4bf7f9deac49bd669dc116832d3de849186990b65a70859b40a788027a25c81a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7e9949a8dfed9601d798b6c9d4ee931876fd4950cd6272e2fbdf93c488cc6f83"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0f7b46dcc0f9b93941b4303ebdf4739c2125e01a1fcb06b22969282842527e3d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e43f3a6a9ea74b4f539971afe0845f572a1e1be7ad68a6d47d8cf6ede3fe7426"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "73ae8c0a7b8b3737fdbbd48b46329375d6e83ee964f017e6190540ee6c3984e5"
+    sha256 cellar: :any_skip_relocation, ventura:        "530e536a374a615c2fcdd077d2d37137c11a666d1cbbb882d75b4aa3abcff801"
+    sha256 cellar: :any_skip_relocation, monterey:       "82377d8fc05d6f2146fd827ba282b6c3ae5e475dfa791dc1c393f4c7cd69f984"
+    sha256 cellar: :any_skip_relocation, big_sur:        "6dda81c02404d3bc36ce2d7e516b94166edee6d0af2fc30858aa078d25edf66b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2a6924504a7a55e78fe93f1f9a4ec17c32d9e65684e3732f5afda7013beab367"
   end
 
   depends_on "go" => :build
