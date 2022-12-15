c471d269555042178549aec329adb0b73cd1a26f
Patrick Linnane <patrick@linnane.io>
2022-12-15 01:55:20 +0000

    pdf-diff: add 0.0.1 bottle.

--- Formula/pdf-diff.rb
+++ Formula/pdf-diff.rb
@@ -6,6 +6,16 @@ class PdfDiff < Formula
   license "MIT"
   head "https://github.com/serhack/pdf-diff.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "fe08862077f15673639f22904af5e0e6d3953e29f5df8b8a231b38f748e6af05"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "fe08862077f15673639f22904af5e0e6d3953e29f5df8b8a231b38f748e6af05"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fe08862077f15673639f22904af5e0e6d3953e29f5df8b8a231b38f748e6af05"
+    sha256 cellar: :any_skip_relocation, ventura:        "e8b0e6e652c67281398e49a6d61c2b8624c5ea7fb538771fc4b47ce50188a877"
+    sha256 cellar: :any_skip_relocation, monterey:       "e8b0e6e652c67281398e49a6d61c2b8624c5ea7fb538771fc4b47ce50188a877"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e8b0e6e652c67281398e49a6d61c2b8624c5ea7fb538771fc4b47ce50188a877"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "66929021a8898ca16128f0ed3b1fb9ffbc4ac8b5778bdb48259207c91daa64fa"
+  end
+
   depends_on "go" => :build
   depends_on "poppler"
 
