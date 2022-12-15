34b79cb76bc6321aa2d97c710c9d9a6b2be21c43
Patrick Linnane <patrick@linnane.io>
2022-12-15 01:44:57 +0000

    cpuid: add 2.2.2 bottle.

--- Formula/cpuid.rb
+++ Formula/cpuid.rb
@@ -6,6 +6,16 @@ class Cpuid < Formula
   license "MIT"
   head "https://github.com/klauspost/cpuid.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "daa2dfeb9f30d18c5939f07d511f84c433ea066c57a9d552d5675b49005ee66b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "daa2dfeb9f30d18c5939f07d511f84c433ea066c57a9d552d5675b49005ee66b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "daa2dfeb9f30d18c5939f07d511f84c433ea066c57a9d552d5675b49005ee66b"
+    sha256 cellar: :any_skip_relocation, ventura:        "677a3cf0da89f7cbf5ca2432b879880f702a4b2588d85a48d0b0a7be36a10b8f"
+    sha256 cellar: :any_skip_relocation, monterey:       "677a3cf0da89f7cbf5ca2432b879880f702a4b2588d85a48d0b0a7be36a10b8f"
+    sha256 cellar: :any_skip_relocation, big_sur:        "677a3cf0da89f7cbf5ca2432b879880f702a4b2588d85a48d0b0a7be36a10b8f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5593f0b8e942f759210c2104dc4aa8796fe569a35fcea7f5eb7193f07a229a21"
+  end
+
   depends_on "go" => :build
 
   def install
