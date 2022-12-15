01e12a0b7d05c721432bc5cb872e252e99006629
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2022-12-15 08:29:30 +0000

    envd: add 0.2.5 bottle.

--- Formula/envd.rb
+++ Formula/envd.rb
@@ -6,6 +6,16 @@ class Envd < Formula
   license "Apache-2.0"
   head "https://github.com/tensorchord/envd.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "1627f9144f4d09c4b2b6c71d706d6564f4f2a3556fee70b392444663b0394ee1"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "1627f9144f4d09c4b2b6c71d706d6564f4f2a3556fee70b392444663b0394ee1"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1627f9144f4d09c4b2b6c71d706d6564f4f2a3556fee70b392444663b0394ee1"
+    sha256 cellar: :any_skip_relocation, ventura:        "ab3ed4b035e2b81ad9104c05cf6ac47018cb58f47dd7a8271092fa5992b873e0"
+    sha256 cellar: :any_skip_relocation, monterey:       "ab3ed4b035e2b81ad9104c05cf6ac47018cb58f47dd7a8271092fa5992b873e0"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ab3ed4b035e2b81ad9104c05cf6ac47018cb58f47dd7a8271092fa5992b873e0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "48e3ee9071a4994da99bccb02bc612961798821163f4a908aaf0ccf0c84b2c9f"
+  end
+
   depends_on "go" => :build
 
   def install
