523837b8df39f68c2109dbd86ea85ed8551466f8
Frost Ming <mianghong@gmail.com>
2022-12-16 18:10:03 +0800

    envd: 0.3.0
    Closes #118300.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/envd.rb
+++ Formula/envd.rb
@@ -1,8 +1,8 @@
 class Envd < Formula
   desc "Reproducible development environment for AI/ML"
   homepage "https://envd.tensorchord.ai"
-  url "https://github.com/tensorchord/envd/archive/v0.2.5.tar.gz"
-  sha256 "5bcd4785b1d0bbe762b4999165513392c2256a33e0a86a733eb776c1092665b7"
+  url "https://github.com/tensorchord/envd/archive/v0.3.0.tar.gz"
+  sha256 "8e3da783a1bfe7c184e40f2bf872225b368ff3364cc94e38a9de5867624c3ff6"
   license "Apache-2.0"
   head "https://github.com/tensorchord/envd.git", branch: "main"
 
@@ -29,12 +29,22 @@ class Envd < Formula
       -X github.com/tensorchord/envd/pkg/version.gitTreeState=clean
     ]
     system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/envd"
+    generate_completions_from_executable(bin/"envd", "completion", "--no-install",
+                                         shell_parameter_format: "--shell=",
+                                         shells:                 [:bash, :zsh])
   end
 
   test do
     output = shell_output("#{bin}/envd version --short")
     assert_equal "envd: v#{version}", output.strip
-    system bin/"envd", "init"
-    assert_predicate testpath/"build.envd", :exist?
+
+    expected = if OS.mac?
+      "error: Cannot connect to the Docker daemon"
+    else
+      "error: permission denied"
+    end
+
+    stderr = shell_output("#{bin}/envd env list 2>&1", 1)
+    assert_match expected, stderr
   end
 end
