a390b2da00d7b596dda4c2142e0ff000800c6f3a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-12-16 06:05:08 +0000

    kubevious: update 1.0.37 bottle.

--- Formula/kubevious.rb
+++ Formula/kubevious.rb
@@ -8,13 +8,13 @@ class Kubevious < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "46ef7f2f844e8634cb2fa44814f859a81735243b4c11dba13a500b6e2c6f98b2"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a51d3db2dda857aa3ecef670969a5b2aec6029ac45775e1bda688b7479055b2a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2be44acdc23e0502f27b09968ffde3f116086146268760662d17e61ab39d3d59"
-    sha256 cellar: :any_skip_relocation, ventura:        "6a0d86b4bf47e1216e43a2b8a6ad331d93b61bb4fef7912f3fd15d1a25d58c57"
-    sha256 cellar: :any_skip_relocation, monterey:       "f490b2bcb2644fe3eea5967543044cca1b8d189e79a09170d6ab1942b2d20602"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0c28f0c4f44fdfcd85b1157f6f0c06ffd89467368e3428590ca647414434a3f7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "908add9f0a065a6e5d39fe4d2346a04ed7210f41bd8701c8bd45e53c33cb5b01"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3ec3815952acecf3ea5d2cad4043f8cdb0f8b0045e1ce50c532e1a9e53eb7fb8"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "7595ccd10031801e6eb1bfd3b7b0aa0bc20f4589fd7ae0500e6c2d418f7d047d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "423adc241e7d00b8eb043fa2cf6ad238ae98af1db68d7e0d24176ffa20cf634a"
+    sha256 cellar: :any_skip_relocation, ventura:        "32f4946d5574458daba8d9c556de7ff1e2374715126e9c6b90e5c8f5c4a1bf8c"
+    sha256 cellar: :any_skip_relocation, monterey:       "3e6d642225cb4e4ff5f3ecad9055a10303a580472d4061744031312c27944cf9"
+    sha256 cellar: :any_skip_relocation, big_sur:        "1b39697f1a5e98ac3093031e4f795b5573a31f2090b0393dd26c5daf343fd033"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4af3b158549a6bfc0f612f3d24f974e2149708a54271548cc686b9905c74cf58"
   end
 
   # upstream issue to track node@18 support
