fdd8722159ae8e7098bf7ad9eb3786fbf89eeb88
Jianbo Sun <wonderflow.sun@gmail.com>
2022-12-16 08:19:56 +0000

    kubevela 1.6.5
    Closes #118290.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/kubevela.rb
+++ Formula/kubevela.rb
@@ -2,8 +2,8 @@ class Kubevela < Formula
   desc "Application Platform based on Kubernetes and Open Application Model"
   homepage "https://kubevela.io"
   url "https://github.com/kubevela/kubevela.git",
-      tag:      "v1.6.4",
-      revision: "1a6b7244c87db4a499e291b7138c9f4ca1afc2ac"
+      tag:      "v1.6.5",
+      revision: "18639ccbae6062929e9f2cabc19795678f22f007"
   license "Apache-2.0"
   head "https://github.com/kubevela/kubevela.git", branch: "master"
 
