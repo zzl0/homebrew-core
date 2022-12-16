1c0e7cedea21409eb78c6a42fc294417b9dbbcca
Rui Chen <rui@chenrui.dev>
2022-12-16 09:37:49 -0500

    docker-completion 20.10.22
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/docker-completion.rb
+++ Formula/docker-completion.rb
@@ -2,8 +2,8 @@ class DockerCompletion < Formula
   desc "Bash, Zsh and Fish completion for Docker"
   homepage "https://www.docker.com/"
   url "https://github.com/docker/cli.git",
-      tag:      "v20.10.21",
-      revision: "baeda1f82a10204ec5708d5fbba130ad76cfee49"
+      tag:      "v20.10.22",
+      revision: "3a2c30b63ab20acfcc3f3550ea756a0561655a77"
   license "Apache-2.0"
   head "https://github.com/docker/cli.git", branch: "master"
 
