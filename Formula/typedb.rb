d4a0da6d89b964031dd347f729e75af97d566d83
Dmitrii Ubskii <18616863+dmitrii-ubskii@users.noreply.github.com>
2022-12-16 10:04:29 +0000

    typedb: rebuild
    * typedb fix: protect user data on upgrade
    * inreplace
    * revert unneeded changes
    * remove revision
    * remove explicit Java 11 dependency
    
    Closes #118301.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/typedb.rb
+++ Formula/typedb.rb
@@ -1,5 +1,5 @@
 class Typedb < Formula
-  desc "Distributed hyper-relational database for knowledge engineering"
+  desc "Strongly-typed database with a rich and logical type system"
   homepage "https://vaticle.com/"
   url "https://github.com/vaticle/typedb/releases/download/2.14.3/typedb-all-mac-2.14.3.zip"
   sha256 "41a574d4d0fafcdfd678599b488dfb3aa7e2c4664e111dbd479bdf0a4dbd12a7"
@@ -9,12 +9,16 @@ class Typedb < Formula
     sha256 cellar: :any_skip_relocation, all: "9d2db455c9a587a9eb74879be312ccb44aa30d4a9334c3ba1cc5d647b7557d69"
   end
 
-  depends_on "openjdk@11"
+  depends_on "openjdk"
 
   def install
     libexec.install Dir["*"]
+    mkdir_p var/"typedb/data"
+    inreplace libexec/"server/conf/config.yml", "server/data", var/"typedb/data"
+    mkdir_p var/"log/typedb"
+    inreplace libexec/"server/conf/config.yml", "server/logs", var/"typedb/logs"
     bin.install libexec/"typedb"
-    bin.env_script_all_files(libexec, Language::Java.java_home_env("11"))
+    bin.env_script_all_files(libexec, Language::Java.java_home_env)
   end
 
   test do
