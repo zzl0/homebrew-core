3bc9fffbb67f7dc6230011ebc4085d41a4bc0542
Rui Chen <rui@chenrui.dev>
2022-12-17 03:52:38 +0000

    hebcal: update 5.6.0 bottle.

--- Formula/hebcal.rb
+++ Formula/hebcal.rb
@@ -7,13 +7,13 @@ class Hebcal < Formula
   head "https://github.com/hebcal/hebcal.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "1f538f34fd2d50ad9f47c2a3358c469fbadb28216411756c81eceff7d02d104f"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "1f538f34fd2d50ad9f47c2a3358c469fbadb28216411756c81eceff7d02d104f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1f538f34fd2d50ad9f47c2a3358c469fbadb28216411756c81eceff7d02d104f"
-    sha256 cellar: :any_skip_relocation, ventura:        "0023899d5f5d6ec87ecb95704ad368fa45e60fbc645864628725e0001a799468"
-    sha256 cellar: :any_skip_relocation, monterey:       "0023899d5f5d6ec87ecb95704ad368fa45e60fbc645864628725e0001a799468"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0023899d5f5d6ec87ecb95704ad368fa45e60fbc645864628725e0001a799468"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "58fdcbbf7b9a630d988dbe8a36c82b90cc3e703680740cef1f8f720c3d95235e"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8048021c5a519bce4f412b747edae20cc5c716dd0de5eeb53c8ab75a5d4f72d9"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "8048021c5a519bce4f412b747edae20cc5c716dd0de5eeb53c8ab75a5d4f72d9"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8048021c5a519bce4f412b747edae20cc5c716dd0de5eeb53c8ab75a5d4f72d9"
+    sha256 cellar: :any_skip_relocation, ventura:        "04267fa869dfa251100d1db8e57d8afb14e152565a87db85d096e17641bf8c6f"
+    sha256 cellar: :any_skip_relocation, monterey:       "04267fa869dfa251100d1db8e57d8afb14e152565a87db85d096e17641bf8c6f"
+    sha256 cellar: :any_skip_relocation, big_sur:        "04267fa869dfa251100d1db8e57d8afb14e152565a87db85d096e17641bf8c6f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "de588a074ccb392ae4428092a18b80eb35e7a0291fa799389b7ddf8da1467c0c"
   end
 
   depends_on "go" => :build
