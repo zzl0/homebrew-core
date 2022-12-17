4b53bdd8e1ba0b40cfcb778dd5eaab2498656abe
Rui Chen <rui@chenrui.dev>
2022-12-16 19:02:21 -0500

    nifi 1.19.1
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/nifi.rb
+++ Formula/nifi.rb
@@ -1,9 +1,9 @@
 class Nifi < Formula
   desc "Easy to use, powerful, and reliable system to process and distribute data"
   homepage "https://nifi.apache.org"
-  url "https://www.apache.org/dyn/closer.lua?path=/nifi/1.19.0/nifi-1.19.0-bin.zip"
-  mirror " https://archive.apache.org/dist/nifi/1.19.0/nifi-1.19.0-bin.zip"
-  sha256 "54be4c13fa81a3fe4860a728288b79fadc1e122904b19f1279a12017412ebd1a"
+  url "https://www.apache.org/dyn/closer.lua?path=/nifi/1.19.1/nifi-1.19.1-bin.zip"
+  mirror " https://archive.apache.org/dist/nifi/1.19.1/nifi-1.19.1-bin.zip"
+  sha256 "aa4c0ff9336b46a82224c8c10f6eea2d6124003ff4d73bb940ad54ae95ae866f"
   license "Apache-2.0"
 
   bottle do
