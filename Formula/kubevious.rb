b2e6b01d9cfa14d6b8f995147b86821d9748da9f
Dustin Rodrigues <dust.rod@gmail.com>
2022-12-17 12:11:50 -0500

    kubevious 1.0.39
    Closes #118394.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/kubevious.rb
+++ Formula/kubevious.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Kubevious < Formula
   desc "Detects and prevents Kubernetes misconfigurations and violations"
   homepage "https://github.com/kubevious/cli"
-  url "https://registry.npmjs.org/kubevious/-/kubevious-1.0.37.tgz"
-  sha256 "ef2c06efb4c744349fcb85028a62f0e179ccfb22399046c16f2b947743314afe"
+  url "https://registry.npmjs.org/kubevious/-/kubevious-1.0.39.tgz"
+  sha256 "0017cabd7da979f02427d997ee5f45eab072f93d8751cf978c9f129a89f30037"
   license "Apache-2.0"
 
   bottle do
