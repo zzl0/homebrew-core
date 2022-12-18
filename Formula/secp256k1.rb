45184aa607080f9ff37882984719e110c4c76202
Sam Jones <github@samjones.io>
2022-12-18 16:22:43 -0500

    secp256k1 0.2.0 (new formula)
    Closes #118463.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/secp256k1.rb
@@ -0,0 +1,33 @@
+class Secp256k1 < Formula
+  desc "Optimized C library for EC operations on curve secp256k1"
+  homepage "https://github.com/bitcoin-core/secp256k1"
+  url "https://github.com/bitcoin-core/secp256k1/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "6cb0fd596e6b1a671f96e9ed7e65a047960def73de024e7b39f45a78ab4fc8df"
+  license "MIT"
+
+  depends_on "autoconf" => [:build]
+  depends_on "automake" => [:build]
+  depends_on "libtool" => [:build]
+
+  def install
+    system "./autogen.sh"
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <secp256k1.h>
+      int main() {
+        secp256k1_context* ctx = secp256k1_context_create(SECP256K1_CONTEXT_NONE);
+        secp256k1_context_destroy(ctx);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c",
+                   "-L#{lib}", "-lsecp256k1",
+                   "-o", "test"
+    system "./test"
+  end
+end
