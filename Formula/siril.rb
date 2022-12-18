9abda68fe166da0f1241b4a8dc087ffb5b4b79ec
Patrick Linnane <patrick@linnane.io>
2022-12-18 07:04:44 -0800

    siril: revision for libraw 0.21.0
    Closes #118441.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/siril.rb
+++ Formula/siril.rb
@@ -4,7 +4,7 @@ class Siril < Formula
   url "https://free-astro.org/download/siril-1.0.6.tar.bz2"
   sha256 "f89604697ffcd43f009f8b4474daafdef220a4f786636545833be1236f38b561"
   license "GPL-3.0-or-later"
-  revision 1
+  revision 2
   head "https://gitlab.com/free-astro/siril.git", branch: "master"
 
   bottle do
