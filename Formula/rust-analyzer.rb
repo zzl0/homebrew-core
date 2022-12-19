951f52048338304f71bd855c191ec424ec7d1233
Rui Chen <rui@chenrui.dev>
2022-12-19 09:12:11 -0500

    rust-analyzer 2022-12-19
    * rust-analyzer 2022-12-19
    * Update rust-analyzer.rb
    
    Closes #118501.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/rust-analyzer.rb
+++ Formula/rust-analyzer.rb
@@ -2,9 +2,9 @@ class RustAnalyzer < Formula
   desc "Experimental Rust compiler front-end for IDEs"
   homepage "https://rust-analyzer.github.io/"
   url "https://github.com/rust-lang/rust-analyzer.git",
-       tag:      "2022-12-12",
-       revision: "21e61bee8b74e93f14205f4a6c316db08f811e38"
-  version "2022-12-12"
+       tag:      "2022-12-19",
+       revision: "9ed1829f1fb61695c21474361ec23b9976793b73"
+  version "2022-12-19"
   license any_of: ["Apache-2.0", "MIT"]
 
   bottle do
