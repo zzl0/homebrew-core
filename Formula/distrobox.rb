ebbfb222dfe76fbd2fe627b867652bd0e59c76ae
Rui Chen <rui@chenrui.dev>
2022-12-19 17:00:54 -0500

    distrobox 1.4.2.1
    Closes #118533.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/distrobox.rb
+++ Formula/distrobox.rb
@@ -1,8 +1,8 @@
 class Distrobox < Formula
   desc "Use any Linux distribution inside your terminal"
   homepage "https://distrobox.privatedns.org/"
-  url "https://github.com/89luca89/distrobox/archive/refs/tags/1.4.1.tar.gz"
-  sha256 "be3266f77011b30da5d0035e246d4e4a9360b7c90fbd8b90ac581165a00217b8"
+  url "https://github.com/89luca89/distrobox/archive/refs/tags/1.4.2.1.tar.gz"
+  sha256 "cbe9217315f848396ec1eb8e21929d23065caa6a1a55f8988500475e823b1f31"
   license "GPL-3.0-only"
   head "https://github.com/89luca89/distrobox.git", branch: "main"
 
