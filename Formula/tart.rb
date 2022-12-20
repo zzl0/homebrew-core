d37a8489a56c25d8ef023fad32fa7e0bbd4aa888
Rui Chen <rui@chenrui.dev>
2022-12-19 23:05:42 -0500

    tart 0.36.2
    * tart 0.36.2
    * tart: update resource
    
    Closes #118561.
    
    Co-authored-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tart.rb
+++ Formula/tart.rb
@@ -1,8 +1,8 @@
 class Tart < Formula
   desc "macOS and Linux VMs on Apple Silicon to use in CI and other automations"
   homepage "https://github.com/cirruslabs/tart"
-  url "https://github.com/cirruslabs/tart/archive/refs/tags/0.35.2.tar.gz"
-  sha256 "0b35754ecd1267290f53099a64e38642d7f743bff0501a19d53945d8630dc010"
+  url "https://github.com/cirruslabs/tart/archive/refs/tags/0.36.2.tar.gz"
+  sha256 "5f8e3b1d92907a1c6949ac8430ad1c48f24d28e8af73dd28b7cc87df69401eef"
   license "AGPL-3.0-or-later"
 
   bottle do
@@ -19,8 +19,8 @@ class Tart < Formula
   uses_from_macos "swift"
 
   resource "softnet" do
-    url "https://github.com/cirruslabs/softnet/archive/refs/tags/0.3.2.tar.gz"
-    sha256 "7bdb4c4c1996257769f11fb1b17d50954bc083c0155cb1509ad30bd8f304ee76"
+    url "https://github.com/cirruslabs/softnet/archive/refs/tags/0.6.0.tar.gz"
+    sha256 "fd01589b9cb1497394f1fd0fbed385dca51558352b5a7d1337cb92a1a2d2f95d"
   end
 
   def install
