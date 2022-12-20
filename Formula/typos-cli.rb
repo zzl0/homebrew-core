df26886ee0d014df783ffb76fcb9c3847347d8cf
Rui Chen <rui@chenrui.dev>
2022-12-19 23:07:00 -0500

    typos-cli 1.13.5
    * typos-cli 1.13.5
    * typos-cli: add livecheck
    
    Closes #118562.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Sam Ford <1584702+samford@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/typos-cli.rb
+++ Formula/typos-cli.rb
@@ -1,9 +1,14 @@
 class TyposCli < Formula
   desc "Source code spell checker"
   homepage "https://github.com/crate-ci/typos"
-  url "https://github.com/crate-ci/typos/archive/refs/tags/v1.12.14.tar.gz"
-  sha256 "ace6adea86c9f0430b6957710d51fa9b48b46224deb10ef215efc5f62fdfc831"
+  url "https://github.com/crate-ci/typos/archive/refs/tags/v1.13.5.tar.gz"
+  sha256 "48cb286f5bcc7f0d82418711582b8236ab0cecf3ce2b320f2fa443794dfba26f"
   license any_of: ["Apache-2.0", "MIT"]
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "4e9dae03b16fe77bda08952bbe0ce905383d142bfac2294bf1fbdb4eb6d526ce"
