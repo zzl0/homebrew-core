57a6ba97b1bb880704b58bf9e40eafbb5264d9ba
Patrick Linnane <patrick@linnane.io>
2022-12-20 19:44:23 +0000

    nave: update 3.4.0 bottle.

--- Formula/nave.rb
+++ Formula/nave.rb
@@ -7,14 +7,13 @@ class Nave < Formula
   head "https://github.com/isaacs/nave.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "ba1b26cb794a07bace9800d71264fceb8b4b9efbddf6d9291399654d98bc88f9"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ba1b26cb794a07bace9800d71264fceb8b4b9efbddf6d9291399654d98bc88f9"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ba1b26cb794a07bace9800d71264fceb8b4b9efbddf6d9291399654d98bc88f9"
-    sha256 cellar: :any_skip_relocation, ventura:        "1f245fcfdd7109a0939d1cb82a854407ac78bedb2bf140966b0d736c69e9a9a4"
-    sha256 cellar: :any_skip_relocation, monterey:       "1f245fcfdd7109a0939d1cb82a854407ac78bedb2bf140966b0d736c69e9a9a4"
-    sha256 cellar: :any_skip_relocation, big_sur:        "1f245fcfdd7109a0939d1cb82a854407ac78bedb2bf140966b0d736c69e9a9a4"
-    sha256 cellar: :any_skip_relocation, catalina:       "1f245fcfdd7109a0939d1cb82a854407ac78bedb2bf140966b0d736c69e9a9a4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ba1b26cb794a07bace9800d71264fceb8b4b9efbddf6d9291399654d98bc88f9"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "890868f3c01fec6535abe4daeb7f01bf1406752f29034dd7b86c22ce3a1ad446"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "890868f3c01fec6535abe4daeb7f01bf1406752f29034dd7b86c22ce3a1ad446"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "890868f3c01fec6535abe4daeb7f01bf1406752f29034dd7b86c22ce3a1ad446"
+    sha256 cellar: :any_skip_relocation, ventura:        "0b1e29dd90ffd9d6b7985a5a6384f95ed8bfb0d7bbf29d462796133758611c84"
+    sha256 cellar: :any_skip_relocation, monterey:       "0b1e29dd90ffd9d6b7985a5a6384f95ed8bfb0d7bbf29d462796133758611c84"
+    sha256 cellar: :any_skip_relocation, big_sur:        "0b1e29dd90ffd9d6b7985a5a6384f95ed8bfb0d7bbf29d462796133758611c84"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "890868f3c01fec6535abe4daeb7f01bf1406752f29034dd7b86c22ce3a1ad446"
   end
 
   def install
