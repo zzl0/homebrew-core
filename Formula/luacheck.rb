4dd5033ab5cb436bf8696449a63907ed4f64bbda
Porkepix <porkepix@gmail.com>
2022-12-20 18:10:33 +0100

    luacheck 1.1.0
    Closes #118623.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/luacheck.rb
+++ Formula/luacheck.rb
@@ -1,8 +1,8 @@
 class Luacheck < Formula
   desc "Package manager for the Lua programming language"
   homepage "https://luacheck.readthedocs.io/"
-  url "https://github.com/lunarmodules/luacheck/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "360586c7b51aa1f368e6f14c9697a576cf902d55d44ef0bd0cd4b082b10700a8"
+  url "https://github.com/lunarmodules/luacheck/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "cd2fb0f54d7b597a64526c052108c7f3fd9ee894bd71afec9851c3253a247864"
   license "MIT"
   head "https://github.com/lunarmodules/luacheck.git", branch: "master"
 
