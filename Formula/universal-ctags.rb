b4e58b7d26d0b77f0c26820282f100393ac854a6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-12-22 05:24:42 +0000

    universal-ctags p6.0.20221218.0
    Closes #118740.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/universal-ctags.rb
+++ Formula/universal-ctags.rb
@@ -1,9 +1,9 @@
 class UniversalCtags < Formula
   desc "Maintained ctags implementation"
   homepage "https://github.com/universal-ctags/ctags"
-  url "https://github.com/universal-ctags/ctags/archive/refs/tags/p5.9.20221120.0.tar.gz"
-  version "p5.9.20221120.0"
-  sha256 "e84f1decf1337f3cf50bba46d1d18022d40b32958f73ebfc617b8f80c4a66886"
+  url "https://github.com/universal-ctags/ctags/archive/refs/tags/p6.0.20221218.0.tar.gz"
+  version "p6.0.20221218.0"
+  sha256 "790bfabae5d5694a902a39dbc9be63002f7827ac479a6417d8b049da6c432f0f"
   license "GPL-2.0-only"
   head "https://github.com/universal-ctags/ctags.git", branch: "master"
 
