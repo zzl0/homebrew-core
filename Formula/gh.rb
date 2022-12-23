d968d3b58ca27bb548c8bed8207becfa89fc4a21
Mislav Marohnić <hi@mislav.net>
2022-12-23 17:37:50 +0100

    gh 2.21.1
    Created by https://github.com/mislav/bump-homebrew-formula-action
    
    Closes #118824.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gh.rb
+++ Formula/gh.rb
@@ -1,8 +1,8 @@
 class Gh < Formula
   desc "GitHub command-line tool"
   homepage "https://github.com/cli/cli"
-  url "https://github.com/cli/cli/archive/v2.21.0.tar.gz"
-  sha256 "081d3c203b37068e0df47f05832c8154b969ffbea30c72ea7073514130d23d71"
+  url "https://github.com/cli/cli/archive/v2.21.1.tar.gz"
+  sha256 "7c57f77ce5c8f1f418582b891224cedb364672f397251a1c593b8875f7c34f87"
   license "MIT"
 
   head "https://github.com/cli/cli.git", branch: "trunk"
