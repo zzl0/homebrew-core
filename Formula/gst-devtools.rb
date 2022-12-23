ffe2aee6d6d20812322b553dedf30871c202434f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-12-23 21:04:50 +0000

    gst-devtools: update 1.20.5 bottle.

--- Formula/gst-devtools.rb
+++ Formula/gst-devtools.rb
@@ -14,14 +14,13 @@ class GstDevtools < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 arm64_ventura:  "e859091dd0314ec5ac9615e93b3f989b925e385112c80756de54880f55addb2e"
-    sha256 arm64_monterey: "34bc999c3ec0b285638aefcbfcb09c91dac1970522b2360264a182d7696cf530"
-    sha256 arm64_big_sur:  "87c2d002078bb392b1e8e84744490fa9e1e23d338b76d394dc1be3073d771ef4"
-    sha256 ventura:        "18865d7967560dc7a01177e72adcf09824549dea709e9489cd6eee6e4dc3754b"
-    sha256 monterey:       "7d241f7159010d90dd0ecf27595f2f34e90f0ae2e8c31afc1422967e6413ce30"
-    sha256 big_sur:        "57671b39b02280e4f57fd784d8868569f19e6f5d392ad94a6d6b2a105d898b80"
-    sha256 x86_64_linux:   "00a51ad101b5b16c07310b628787704fac546e48218d1e12161076c5be622a03"
+    sha256 arm64_ventura:  "fd54ccc7cff8566b374875d65899ef1a36913e1e4048bab2b74efe941f263994"
+    sha256 arm64_monterey: "8e70edceb0d37d00142e838b23985f713f5a2893937ca77c3fa10907453c8d4e"
+    sha256 arm64_big_sur:  "fbcc046496fa19f38210bebfef79d53e5073c775866316126f5bdea515ebaf73"
+    sha256 ventura:        "79be9fb59c15220c5d6dc9ad978537883251f35fa2a17792f1c03fd18addacea"
+    sha256 monterey:       "348827f557cab2431747a6387a3c5b647ab4cfb13b37886b45830bd0c094e9ab"
+    sha256 big_sur:        "8ace31da36bac519db16469a83a180354d853968c9153c980aedec981dd2e187"
+    sha256 x86_64_linux:   "4d5e361535caf26074c61d1615f57323c7a30cfb250f60b606afbed1dcb3dadb"
   end
 
   depends_on "gobject-introspection" => :build
