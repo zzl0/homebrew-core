e48531b02044b9ebcc926467d230960eca8f3163
Porkepix <porkepix@gmail.com>
2022-12-23 22:40:48 +0100

    cppad 20220000.5
    Closes #118833.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cppad.rb
+++ Formula/cppad.rb
@@ -1,8 +1,8 @@
 class Cppad < Formula
   desc "Differentiation of C++ Algorithms"
   homepage "https://www.coin-or.org/CppAD"
-  url "https://github.com/coin-or/CppAD/archive/20220000.4.tar.gz"
-  sha256 "0f4e11f20f8436b2d04522b1279f0ed335b28f454e71425ecf39106497363cb4"
+  url "https://github.com/coin-or/CppAD/archive/20220000.5.tar.gz"
+  sha256 "9fb4562f6169855eadcd86ac4671593d1c0edf97bb6ce7cbb28e19af2bfc165e"
   license "EPL-2.0"
   version_scheme 1
   head "https://github.com/coin-or/CppAD.git", branch: "master"
