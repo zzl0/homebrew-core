816084c168317fa012f53ad523160ebedc6203da
Patrick Linnane <patrick@linnane.io>
2022-12-23 03:24:36 +0000

    nasm: update 2.16 bottle.

--- Formula/nasm.rb
+++ Formula/nasm.rb
@@ -11,16 +11,13 @@ class Nasm < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "ea233a725bc6ecde3e61967f64d1ffad54e17a3cf3947b700b8d4a46ef9bc310"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b890027bb7fef2cf20c69f3fd38ed0c19c373bce79c949ab6d123601617ae949"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8e3fb019ffdfcd4ce6b418adbe3d1d1d0e1e232a7de3b62fc4872c2401543cb3"
-    sha256 cellar: :any_skip_relocation, ventura:        "39cfdb815cf5285b57f62f149abab2da481ca72c4b28fd8aac7cc65d6d1a1280"
-    sha256 cellar: :any_skip_relocation, monterey:       "d22292dce7f323c908b6774fbc76c4716142e2b73a292556e22d3b42589fd5d4"
-    sha256 cellar: :any_skip_relocation, big_sur:        "cc45793ac9f3fedd01dd08bbbf766137a40bf22e18c43742498b0a542aa319e8"
-    sha256 cellar: :any_skip_relocation, catalina:       "1875e67160bac1675dd505d66a6b78469767d2dfe8baab2652409f91ac0549ef"
-    sha256 cellar: :any_skip_relocation, mojave:         "4b3614f857264edfa9aeab961c523b3910cdef0ceccaf9957888b477c1c512f4"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "8f8c181994b1f05bf425a4034f76d6973c9e1a85ecb64af7f67d47556f23a0d6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "21fd7041a27b500983d56bf9b24739790f5de243da14a4a549a2afb3ed23deb4"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "86c9627036e1737daf6833c3d0ae95ecd9e6808fd11c6b4235df0bf6f15c1c7d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "204a67941110b735ed0eb101fb4f8913b9706827446987c339f2a6431a888078"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "df2356c1b5bc3bfee26ca38cb5576b74363641392b0a20bf84f641fdd1366fa2"
+    sha256 cellar: :any_skip_relocation, ventura:        "8c1285c9dfacc1575971e4ed24ce1c7a239a3406e425bfff60daf28d40115b07"
+    sha256 cellar: :any_skip_relocation, monterey:       "f0e25e83241434f6a23a6211079d634da7b9f337b5927d98678caf5125a3ce32"
+    sha256 cellar: :any_skip_relocation, big_sur:        "094c65f9e94b7857a46b4c20585557f75e9933d00189674dda7c26e8e8299266"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "23cdf43daa59587b7766525bdb0d722054981bc4f1d57168b4393fbc17037f19"
   end
 
   head do
