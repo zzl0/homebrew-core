619d8035082185d7f938d9d829e8ca79b0c8df2f
Rui Chen <rui@chenrui.dev>
2022-12-24 16:55:19 -0500

    gupnp-tools 0.12.0
    Closes #120031.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Michael Cho <20700669+cho-m@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gupnp-tools.rb
+++ Formula/gupnp-tools.rb
@@ -1,8 +1,8 @@
 class GupnpTools < Formula
   desc "Free replacements of Intel's UPnP tools"
   homepage "https://wiki.gnome.org/GUPnP/"
-  url "https://download.gnome.org/sources/gupnp-tools/0.10/gupnp-tools-0.10.3.tar.xz"
-  sha256 "457f4d923935b078415cd2ba88d78db60079b725926b7ee106e4565efe3204de"
+  url "https://download.gnome.org/sources/gupnp-tools/0.12/gupnp-tools-0.12.0.tar.xz"
+  sha256 "5ea7607ee365642c555927fedc5b5e1fe08e74f061d0c3eb08bd901b5eb20082"
   license all_of: ["GPL-2.0-or-later", "LGPL-2.0-or-later"]
 
   bottle do
@@ -20,18 +20,17 @@ class GupnpTools < Formula
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
   depends_on "gettext"
+  depends_on "gssdp"
   depends_on "gtk+3"
   depends_on "gtksourceview4"
   depends_on "gupnp"
   depends_on "gupnp-av"
-  depends_on "libsoup@2"
+  depends_on "libsoup"
 
   def install
-    mkdir "build" do
-      system "meson", *std_meson_args, ".."
-      system "ninja", "-v"
-      system "ninja", "install", "-v"
-    end
+    system "meson", *std_meson_args, "build"
+    system "meson", "compile", "-C", "build", "--verbose"
+    system "meson", "install", "-C", "build"
   end
 
   test do
