172d4f74a8282b6c519d78fe5d29d93e8126c88d
Rui Chen <rui@chenrui.dev>
2022-12-24 16:36:48 -0500

    gssdp 1.6.2
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/gssdp.rb
+++ Formula/gssdp.rb
@@ -1,9 +1,8 @@
 class Gssdp < Formula
   desc "GUPnP library for resource discovery and announcement over SSDP"
   homepage "https://wiki.gnome.org/GUPnP/"
-  url "https://download.gnome.org/sources/gssdp/1.4/gssdp-1.4.0.1.tar.xz"
-  sha256 "8676849d57fb822b8728856dbadebf3867f89ee47a0ec47a20045d011f431582"
-  revision 1
+  url "https://download.gnome.org/sources/gssdp/1.6/gssdp-1.6.2.tar.xz"
+  sha256 "410b376deeced9836b57f488f80052fe4a14f89e075b1ceccf28e51f490f9fb9"
 
   bottle do
     rebuild 2
@@ -20,22 +19,19 @@ class Gssdp < Formula
   depends_on "gobject-introspection" => :build
   depends_on "meson" => :build
   depends_on "ninja" => :build
+  depends_on "pandoc" => :build
   depends_on "pkg-config" => :build
   depends_on "vala" => :build
   depends_on "gettext"
   depends_on "glib"
-  depends_on "libsoup@2"
+  depends_on "libsoup"
 
   def install
-    ENV.prepend_path "PKG_CONFIG_PATH", Formula["libsoup@2"].opt_lib/"pkgconfig"
-    ENV.prepend_path "XDG_DATA_DIRS", Formula["libsoup@2"].opt_share
     ENV.prepend_path "XDG_DATA_DIRS", HOMEBREW_PREFIX/"share"
 
-    mkdir "build" do
-      system "meson", *std_meson_args, "-Dsniffer=false", ".."
-      system "ninja"
-      system "ninja", "install"
-    end
+    system "meson", *std_meson_args, "build", "-Dsniffer=false"
+    system "meson", "compile", "-C", "build", "-v"
+    system "meson", "install", "-C", "build"
   end
 
   test do
@@ -53,10 +49,10 @@ class Gssdp < Formula
       -I#{gettext.opt_include}
       -I#{glib.opt_include}/glib-2.0
       -I#{glib.opt_lib}/glib-2.0/include
-      -I#{include}/gssdp-1.2
+      -I#{include}/gssdp-#{version.major_minor}
       -D_REENTRANT
       -L#{lib}
-      -lgssdp-1.2
+      -lgssdp-#{version.major_minor}
     ]
     system ENV.cc, "test.c", "-o", "test", *flags
     system "./test"
