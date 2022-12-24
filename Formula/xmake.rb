fd13a03ec4d45a5a2821c143852401efed6d1303
ruki <waruqi@gmail.com>
2022-12-24 16:43:51 +0800

    xmake 2.7.4
    * xmake: 2.7.4
    * fix configure
    * remove trailing spaces
    
    Closes #118864.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/xmake.rb
+++ Formula/xmake.rb
@@ -1,8 +1,8 @@
 class Xmake < Formula
   desc "Cross-platform build utility based on Lua"
   homepage "https://xmake.io/"
-  url "https://github.com/xmake-io/xmake/releases/download/v2.7.3/xmake-v2.7.3.tar.gz"
-  sha256 "3e71437ad2a59d1fbbc9fba75ab4ca8d428c49beefcce86c11f6c4710dd4b6f2"
+  url "https://github.com/xmake-io/xmake/releases/download/v2.7.4/xmake-v2.7.4.tar.gz"
+  sha256 "d490ff8825fa53fe5abfb549310cb54a2dfef1ebd3f82e24548483772994e06a"
   license "Apache-2.0"
   head "https://github.com/xmake-io/xmake.git", branch: "master"
 
@@ -21,11 +21,17 @@ class Xmake < Formula
     depends_on "readline"
   end
 
+  patch do
+    url "https://github.com/xmake-io/xmake/releases/download/v2.7.4/configure.diff"
+    sha256 "fa46107403b2ed062631c83009852130b5641eaf703589230c6daea428a13bf5"
+  end
+
   def install
     ENV["XMAKE_ROOT"] = "y" if OS.linux? && ENV["HOMEBREW_GITHUB_ACTIONS"]
 
+    system "./configure"
     system "make"
-    system "make", "install", "prefix=#{prefix}"
+    system "make", "install", "PREFIX=#{prefix}"
   end
 
   test do
