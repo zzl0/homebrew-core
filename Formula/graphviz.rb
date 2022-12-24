748e47dae480648fc80dc03ff3e17f444d116db8
Rui Chen <rui@chenrui.dev>
2022-12-24 16:36:08 -0500

    graphviz 7.0.5
    Closes #118890.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/graphviz.rb
+++ Formula/graphviz.rb
@@ -2,8 +2,8 @@ class Graphviz < Formula
   desc "Graph visualization software from AT&T and Bell Labs"
   homepage "https://graphviz.org/"
   url "https://gitlab.com/graphviz/graphviz.git",
-      tag:      "7.0.4",
-      revision: "274d174ce715e31af5bfe6495a2f32fd6cd658b2"
+      tag:      "7.0.5",
+      revision: "b85fef0b0360ed5422c247d028055d90bed6c242"
   license "EPL-1.0"
   version_scheme 1
   head "https://gitlab.com/graphviz/graphviz.git", branch: "main"
