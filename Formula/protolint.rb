d8f5e676b0e8f8e200e03eeda1a9e5564bd48914
Rui Chen <rui@chenrui.dev>
2022-12-25 02:32:06 +0000

    protolint: add 0.42.2 bottle.

--- Formula/protolint.rb
+++ Formula/protolint.rb
@@ -7,6 +7,16 @@ class Protolint < Formula
   license "MIT"
   head "https://github.com/yoheimuta/protolint.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f2ef4c851f1dc91fac09f9710bcce7f1579938a32d633dc29defc49a3dafe047"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3f30160d5d36c07be1f201f257daf5feceb94d2fd3202ea4cf4c88dcd7ee3182"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f258eb00cc5aba88e0d32600eae18aed9be1553339bbbd2b266108666506fb16"
+    sha256 cellar: :any_skip_relocation, ventura:        "f3022e3e5a0bd0f16ccf3644442d447decb598ede2d23fe08f64674a945d5329"
+    sha256 cellar: :any_skip_relocation, monterey:       "11f647aed64d69b8dda61d2f3ba82d67a2705b61e33219761e01c40daf0b5d73"
+    sha256 cellar: :any_skip_relocation, big_sur:        "211c303c740817ac6a9eeb494c386920b6a03fa6b00b27c452d38bbff5e8e809"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d93aff754379c171066cc05567a22211d13e5f8d6883a83418a4bd1ab57a1365"
+  end
+
   depends_on "go" => :build
 
   def install
