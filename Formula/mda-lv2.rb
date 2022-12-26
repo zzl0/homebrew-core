a0581a8681a2fae5af3170b1a6cde95c848b0744
ryenus <ryenus@gmail.com>
2022-12-26 14:52:45 +0800

    mda-lv2: migrate to python@3.11
    Closes #119048.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/mda-lv2.rb
+++ Formula/mda-lv2.rb
@@ -24,7 +24,7 @@ class MdaLv2 < Formula
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
-  depends_on "python@3.10" => :build
+  depends_on "python@3.11" => :build
   depends_on "sord" => :test
   depends_on "lv2"
 
