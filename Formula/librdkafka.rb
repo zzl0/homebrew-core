a0d7efc3bca149a24efc4a89b9916e5e9a98185b
ryenus <ryenus@gmail.com>
2022-12-26 14:52:34 +0800

    librdkafka: migrate to python@3.11
    Closes #119029.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/librdkafka.rb
+++ Formula/librdkafka.rb
@@ -23,7 +23,7 @@ class Librdkafka < Formula
   end
 
   depends_on "pkg-config" => :build
-  depends_on "python@3.10" => :build
+  depends_on "python@3.11" => :build
   depends_on "lz4"
   depends_on "lzlib"
   depends_on "openssl@1.1"
