22e7f70f15125a478484ba15dba0ed918383db1f
ryenus <ryenus@gmail.com>
2022-12-26 14:52:53 +0800

    ns-3 3.37
    * ns-3: migrate to python@3.11
    * ns-3 3.37
    * ns-3: add more resources
    * ns-3: remove Python bindings
    
    Closes #119063.
    
    Co-authored-by: Michka Popoff <michkapopoff@gmail.com>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ns-3.rb
+++ Formula/ns-3.rb
@@ -1,8 +1,10 @@
 class Ns3 < Formula
+  include Language::Python::Virtualenv
+
   desc "Discrete-event network simulator"
   homepage "https://www.nsnam.org/"
-  url "https://gitlab.com/nsnam/ns-3-dev/-/archive/ns-3.36.1/ns-3-dev-ns-3.36.1.tar.bz2"
-  sha256 "8826dbb35290412df9885d8a936ab0c3fe380dec4dd48c57889148c0a2c1a856"
+  url "https://gitlab.com/nsnam/ns-3-dev/-/archive/ns-3.37/ns-3-dev-ns-3.37.tar.gz"
+  sha256 "70f4dca7ff59eabedcdf97c75d1d8d593c726f0d75a6b9470f29871629a341f3"
   license "GPL-2.0-only"
 
   bottle do
@@ -19,55 +21,26 @@ class Ns3 < Formula
 
   depends_on "boost" => :build
   depends_on "cmake" => :build
-  depends_on xcode: [:build, "11"]
-
   depends_on "gsl"
   depends_on "open-mpi"
-  depends_on "python@3.10"
 
   uses_from_macos "libxml2"
   uses_from_macos "sqlite"
 
-  # Clears the Python3_LIBRARIES variable. Removing `PRIVATE ${Python3_LIBRARIES}`
-  # in ns3-module-macros is not sufficient as it doesn't apply to visualizer.so.
-  # Should be no longer needed when 3.37 rolls out.
-  on_macos do
-    patch :DATA
-  end
-
-  # Needs GCC 8 or above
-  fails_with gcc: "5"
-  fails_with gcc: "6"
-  fails_with gcc: "7"
-
-  resource "pybindgen" do
-    url "https://files.pythonhosted.org/packages/e0/8e/de441f26282eb869ac987c9a291af7e3773d93ffdb8e4add664b392ea439/PyBindGen-0.22.1.tar.gz"
-    sha256 "8c7f22391a49a84518f5a2ad06e3a5b1e839d10e34da7631519c8a28fcba3764"
-  end
-
   def install
-    resource("pybindgen").stage buildpath.parent/"pybindgen"
-    ENV.append "PYTHONPATH", buildpath.parent/"pybindgen"
-
     # Fix binding's rpath
     linker_flags = ["-Wl,-rpath,#{loader_path}"]
-    linker_flags << "-Wl,-undefined,dynamic_lookup" if OS.mac?
 
     system "cmake", "-S", ".", "-B", "build",
                     "-DNS3_GTK3=OFF",
-                    "-DNS3_PYTHON_BINDINGS=ON",
+                    "-DNS3_PYTHON_BINDINGS=OFF",
                     "-DNS3_MPI=ON",
                     "-DCMAKE_SHARED_LINKER_FLAGS=#{linker_flags.join(" ")}",
                     *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
 
-    # Starting 3.36, bindings are no longer installed
-    # https://gitlab.com/nsnam/ns-3-dev/-/merge_requests/1060
-    site_packages = Language::Python.site_packages("python3.10")
-    (prefix/site_packages).install (buildpath/"build/bindings/python").children
-
-    pkgshare.install "examples/tutorial/first.cc", "examples/tutorial/first.py"
+    pkgshare.install "examples/tutorial/first.cc"
   end
 
   test do
@@ -76,23 +49,5 @@ class Ns3 < Formula
            "-lns#{version}-point-to-point", "-lns#{version}-applications",
            "-std=c++17", "-o", "test"
     system "./test"
-
-    system Formula["python@3.10"].opt_bin/"python3.10", pkgshare/"first.py"
   end
 end
-
-__END__
-diff --git a/build-support/macros-and-definitions.cmake b/build-support/macros-and-definitions.cmake
-index 304ccdde7..64ae322c5 100644
---- a/build-support/macros-and-definitions.cmake
-+++ b/build-support/macros-and-definitions.cmake
-@@ -723,7 +723,8 @@ macro(process_options)
-   if(${Python3_Interpreter_FOUND})
-     if(${Python3_Development_FOUND})
-       set(Python3_FOUND TRUE)
--      if(APPLE)
-+      set(Python3_LIBRARIES "")
-+      if(FALSE)
-         # Apple is very weird and there could be a lot of conflicting python
-         # versions which can generate conflicting rpaths preventing the python
-         # bindings from working
