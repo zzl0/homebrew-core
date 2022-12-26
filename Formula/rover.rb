1f8bc5b8f61e9887211213c637483d7c4c6961bf
Rui Chen <rui@chenrui.dev>
2022-12-26 01:42:27 -0500

    rover: rebuild
    * rover: add build patch to build with rust 1.66.0
    * rover: fix code style
    
    Closes #118952.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/rover.rb
+++ Formula/rover.rb
@@ -1,10 +1,18 @@
 class Rover < Formula
   desc "CLI for managing and maintaining data graphs with Apollo Studio"
   homepage "https://www.apollographql.com/docs/rover/"
-  url "https://github.com/apollographql/rover/archive/refs/tags/v0.10.0.tar.gz"
-  sha256 "5132d1e1f4f5eb3c6e8b2254e7c75c638d35023992869185704e3ead2c99e2ff"
   license "MIT"
   head "https://github.com/apollographql/rover.git", branch: "main"
+
+  stable do
+    url "https://github.com/apollographql/rover/archive/refs/tags/v0.10.0.tar.gz"
+    sha256 "5132d1e1f4f5eb3c6e8b2254e7c75c638d35023992869185704e3ead2c99e2ff"
+
+    # Fix build for Rust 1.66.0
+    # Upstream commit ref, https://github.com/apollographql/rover/commit/f41e0894824663f62caf8fab6d10a6904449ec39
+    # remove in next release
+    patch :DATA
+  end
 
   bottle do
     rebuild 1
@@ -34,3 +42,31 @@ class Rover < Formula
     assert_match version.to_s, shell_output("#{bin}/rover --version")
   end
 end
+
+__END__
+diff --git a/crates/rover-client/src/operations/graph/check/types.rs b/crates/rover-client/src/operations/graph/check/types.rs
+index 842db8e..9074c32 100644
+--- a/crates/rover-client/src/operations/graph/check/types.rs
++++ b/crates/rover-client/src/operations/graph/check/types.rs
+@@ -22,7 +22,7 @@ impl From<CheckSchemaAsyncInput> for MutationVariables {
+             graph_id: input.graph_ref.name,
+             name: input.graph_ref.variant,
+             input: MutationInput {
+-                graph_ref: graph_ref.to_string(),
++                graph_ref: Some(graph_ref.to_string()),
+                 proposed_schema_document: Some(input.proposed_schema),
+                 git_context: input.git_context.into(),
+                 config: input.config.into(),
+diff --git a/crates/rover-client/src/operations/subgraph/check/types.rs b/crates/rover-client/src/operations/subgraph/check/types.rs
+index 6a76692..cb06d72 100644
+--- a/crates/rover-client/src/operations/subgraph/check/types.rs
++++ b/crates/rover-client/src/operations/subgraph/check/types.rs
+@@ -23,7 +23,7 @@ impl From<SubgraphCheckAsyncInput> for MutationVariables {
+             graph_id: input.graph_ref.name,
+             name: input.graph_ref.variant,
+             input: MutationInput {
+-                graph_ref: graph_ref.to_string(),
++                graph_ref: Some(graph_ref.to_string()),
+                 proposed_schema: input.proposed_schema,
+                 git_context: input.git_context.into(),
+                 config: input.config.into(),
