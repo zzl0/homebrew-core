fc148151b48bad1a54dee6971c9b44c8ef2c49de
ryenus <ryenus@gmail.com>
2022-12-26 17:28:18 +0800

    pympress: migrate to python@3.11
    Closes #119083.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pympress.rb
+++ Formula/pympress.rb
@@ -30,7 +30,7 @@ class Pympress < Formula
   depends_on "libyaml"
   depends_on "poppler"
   depends_on "pygobject3"
-  depends_on "python@3.10"
+  depends_on "python@3.11"
 
   resource "watchdog" do
     url "https://files.pythonhosted.org/packages/b3/d2/a04951838e0b0cea20aff5214109144e6869101818e7f90bf3b68ea2facf/watchdog-2.1.7.tar.gz"
