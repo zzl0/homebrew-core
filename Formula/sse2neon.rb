c52434610160b960f189b6842c3ba0fbbb494b74
Porkepix <porkepix@gmail.com>
2022-12-26 11:45:19 +0100

    sse2neon 1.6.0
    Closes #119117.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/sse2neon.rb
+++ Formula/sse2neon.rb
@@ -1,8 +1,8 @@
 class Sse2neon < Formula
   desc "Translator from Intel SSE intrinsics to Arm/Aarch64 NEON implementation"
   homepage "https://github.com/DLTcollab/sse2neon"
-  url "https://github.com/DLTcollab/sse2neon/archive/v1.5.1.tar.gz"
-  sha256 "4001e2dfb14fcf3831211581ed83bcc83cf6a3a69f638dcbaa899044a351bb2a"
+  url "https://github.com/DLTcollab/sse2neon/archive/v1.6.0.tar.gz"
+  sha256 "06f4693219deccb91b457135d836fc514a1c0a57e9fa66b143982901d2d19677"
   license "MIT"
   head "https://github.com/DLTcollab/sse2neon.git", branch: "master"
 
