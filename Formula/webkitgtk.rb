263002cf14f0c0e562ba01f132057e1d3caec54b
ryenus <ryenus@gmail.com>
2022-12-26 19:35:23 +0800

    webkitgtk: migrate to python@3.11
    Update formula **webkitgtk** to use python@3.11 instead of python@3.10
    
    see the related pr https://github.com/Homebrew/homebrew-core/pull/114154

--- Formula/webkitgtk.rb
+++ Formula/webkitgtk.rb
@@ -17,7 +17,7 @@ class Webkitgtk < Formula
   depends_on "cmake" => :build
   depends_on "gobject-introspection" => :build
   depends_on "pkg-config" => :build
-  depends_on "python@3.10" => :build
+  depends_on "python@3.11" => :build
   depends_on "cairo"
   depends_on "enchant"
   depends_on "fontconfig"
