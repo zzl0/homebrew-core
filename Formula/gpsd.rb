c7e6c585468bd3e768107bfe2adc6fad4b6e4845
ryenus <ryenus@gmail.com>
2022-12-26 14:52:21 +0800

    gpsd: migrate to python@3.11
    Closes #119004.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gpsd.rb
+++ Formula/gpsd.rb
@@ -23,7 +23,7 @@ class Gpsd < Formula
   end
 
   depends_on "asciidoctor" => :build
-  depends_on "python@3.10" => :build
+  depends_on "python@3.11" => :build
   depends_on "scons" => :build
 
   uses_from_macos "ncurses"
