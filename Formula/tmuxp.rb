5a7e9330ec7b822d74bd4fff9f3196a56c3a67aa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-12-27 16:04:53 +0000

    tmuxp: update 1.20.1 bottle.

--- Formula/tmuxp.rb
+++ Formula/tmuxp.rb
@@ -8,13 +8,13 @@ class Tmuxp < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "cb0fec68592b20a21c762f582f017ddaa0a7a46ac25cf500f448dd67a57b9e76"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "d2495188df16c96912aa6cf059a9ef665e56e53837c8318a4795103eba30be33"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b27ef187d293ae13cdbdce75cda60ef8222d549b68338a0762a2080659229732"
-    sha256 cellar: :any_skip_relocation, ventura:        "668b415c0356afe763a754078ca9ad693a34b5dc87d998bcabfe4e6e0d3a86c0"
-    sha256 cellar: :any_skip_relocation, monterey:       "177d84ab3249defe282557def26f6fbbede057c3666fbd0121eccf0398039afa"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b7c43e0b30df5655f5f29853bcbeb23593ab1246618678934bc71448a6465b55"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "843cfae7eaf01126df32c6063f2fe7afc0d2bcf43f6468228fca3a51993d498c"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "ec07d2022f105ed1045dff30e8db36839ebf8b1b48641e579038ce2cacfc0ea8"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c6328efad8337d35cc58781147204ca6e0248733ee87b556c3f66e71ecc002b1"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9825c3e75afba30e715af3d23af63c690d63505a507f8b467638e56cf91f008b"
+    sha256 cellar: :any_skip_relocation, ventura:        "c39c15fd58989474a5f8d31dc9dcce98f7ed0daf32c5a26ca8db7c076136e7c4"
+    sha256 cellar: :any_skip_relocation, monterey:       "7d5a50d071485fb4cc713deddaf0113c373b211a8a019a12707cd05c3ac6d31a"
+    sha256 cellar: :any_skip_relocation, big_sur:        "58c28e0dd8ca644393a8eb5f1c9c962cf9463fabae4d1e779a1f9c7b426f70ea"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "574705a720ff06fd44a5453969914ccec8fcd1bb2c7160851b126933b2c1fdb6"
   end
 
   depends_on "python@3.11"
