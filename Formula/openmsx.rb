b1e29f1c55233736929f5de907a3c11b43b623d4
Rui Chen <rui@chenrui.dev>
2022-12-26 22:32:32 -0500

    openmsx 18.0
    * openmsx 18.0
    * openmsx: update license
    
    Closes #119190.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/openmsx.rb
+++ Formula/openmsx.rb
@@ -1,9 +1,9 @@
 class Openmsx < Formula
   desc "MSX emulator"
   homepage "https://openmsx.org/"
-  url "https://github.com/openMSX/openMSX/releases/download/RELEASE_17_0/openmsx-17.0.tar.gz"
-  sha256 "70ec6859522d8e3bbc97227abb98c87256ecda555b016d1da85cdd99072ce564"
-  license "GPL-2.0"
+  url "https://github.com/openMSX/openMSX/releases/download/RELEASE_18_0/openmsx-18.0.tar.gz"
+  sha256 "23db7756e6c6b5cfd157bb4720a0d96aa2bb75e88d1fdf5a0f76210eef4aff60"
+  license "GPL-2.0-or-later"
   head "https://github.com/openMSX/openMSX.git", branch: "master"
 
   livecheck do
