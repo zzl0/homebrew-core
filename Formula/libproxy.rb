54ac87cb07fb370036814632f125c4d988c8fdaf
Rui Chen <rui@chenrui.dev>
2022-12-28 23:49:46 +0000

    libproxy: update 0.4.18 bottle.

--- Formula/libproxy.rb
+++ Formula/libproxy.rb
@@ -7,15 +7,13 @@ class Libproxy < Formula
   head "https://github.com/libproxy/libproxy.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 arm64_ventura:  "d4df7a0e2c40acb852625f0a5a70b1c737fd5c13203787a929d1bb20f49f3190"
-    sha256 arm64_monterey: "1de17b75f1c12ddacec2ace3a6152586bd49bdeeac07982b000c83486d3adeb3"
-    sha256 arm64_big_sur:  "54df3618c53e7f3a55441b74319aa2c32ea68ee1d30a8287b8ad3f9cf58b8f8d"
-    sha256 ventura:        "2c27a8613e1b335043ef94a4233553a04cefc47caf5fe0a44de7758ee8e2aeea"
-    sha256 monterey:       "a74b532012e5169309a1ef4226792f3d7533204ee33bfccd5863340041f9a71c"
-    sha256 big_sur:        "f4fe74825ca139acd6cef52317eade9511f2ff90b5b584b0ca1706729ad6cc8d"
-    sha256 catalina:       "f9946d8dee8b0915e160343b112259e5bba99bfb28f51364bf17a9bf6aabb6eb"
-    sha256 x86_64_linux:   "b8f12e9287c1ee704325efea0468632d56bc3852a5a40a2752ccee51a97d046b"
+    sha256 arm64_ventura:  "6246d732f961d77005bd78e3e26dcb17ca6e30db717ff15153e318567e2d50d2"
+    sha256 arm64_monterey: "443454cdeda3546c1d04c36f51d1c71312806abd99ac968dfa22ee6dd3ac6119"
+    sha256 arm64_big_sur:  "00438a3c641cdb2326ad06e45f446ec78bd247740415d5f969cd14875c6f6902"
+    sha256 ventura:        "1dfa2bf3dec13e70f0a4af42f131cddea3016de6e0a3c12bcf9e595f2e13c911"
+    sha256 monterey:       "b1de5bf78ffc1fc870d383cd713c438e181d037506d11c95c9dafffe302e05e1"
+    sha256 big_sur:        "b22d402e7747a6a4f725c0cef38256d29292544b6117be5f761627182be3b585"
+    sha256 x86_64_linux:   "9e610ba5049b018c45b4c2a8eeae8f01391227dc30189d54ecd476496d6fdbba"
   end
 
   depends_on "cmake" => :build
