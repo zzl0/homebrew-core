24b992b0f6595f1d9851333c5732d96a4f09e753
Porkepix <porkepix@gmail.com>
2022-12-29 13:06:43 +0100

    libcpuid 0.6.2
    * libcpuid 0.6.2
    * libcpuid: add patch
    
    Closes #119322.
    
    Co-authored-by: Alexander Bayandin <alexander@bayandin.dev>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libcpuid.rb
+++ Formula/libcpuid.rb
@@ -1,8 +1,8 @@
 class Libcpuid < Formula
   desc "Small C library for x86 CPU detection and feature extraction"
   homepage "https://github.com/anrieff/libcpuid"
-  url "https://github.com/anrieff/libcpuid/archive/v0.5.1.tar.gz"
-  sha256 "36d62842ef43c749c0ba82237b10ede05b298d79a0e39ef5fd1115ba1ff8e126"
+  url "https://github.com/anrieff/libcpuid/archive/v0.6.2.tar.gz"
+  sha256 "3e7f2fc243a6a68d6c909b701cfa0db6422ec33fccf91ea5ab7beda3eb798672"
   license "BSD-2-Clause"
   head "https://github.com/anrieff/libcpuid.git", branch: "master"
 
@@ -19,6 +19,13 @@ class Libcpuid < Formula
   depends_on "automake" => :build
   depends_on "libtool" => :build
   depends_on arch: :x86_64
+
+  # Fix build for macOS
+  # Remove in the next release
+  patch do
+    url "https://github.com/anrieff/libcpuid/commit/3b0a1f7e5b10efb978cea4c5cb5b727ba1ef3655.patch?full_index=1"
+    sha256 "d4dcc843e78fe5872aba483b0fb5adb0b8e702f6343a383db566dae81eec0d9d"
+  end
 
   def install
     system "autoreconf", "-ivf"
