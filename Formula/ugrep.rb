76af7560a467f286d0d07605b19d32e5b82efdb1
Porkepix <porkepix@gmail.com>
2022-12-30 02:11:42 +0100

    ugrep 3.9.3
    Closes #119376.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ugrep.rb
+++ Formula/ugrep.rb
@@ -1,8 +1,8 @@
 class Ugrep < Formula
   desc "Ultra fast grep with query UI, fuzzy search, archive search, and more"
   homepage "https://github.com/Genivia/ugrep"
-  url "https://github.com/Genivia/ugrep/archive/v3.9.2.tar.gz"
-  sha256 "3416267ac5a4dd2938ca91e7bd91db958d65510c9fd33b221f067bd3c6b3fc6a"
+  url "https://github.com/Genivia/ugrep/archive/v3.9.3.tar.gz"
+  sha256 "d0b6d772cfe75ef28e15932ddd4d177c897625be45da20fd3b27658bb0cf1194"
   license "BSD-3-Clause"
 
   bottle do
