d6c9cb759deba20fc5081c11bcbb129cad25daf0
Rui Chen <rui@chenrui.dev>
2022-12-30 17:12:32 -0500

    jbang 0.101.0 (new formula)
    Closes #119431.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/jbang.rb
@@ -0,0 +1,21 @@
+class Jbang < Formula
+  desc "Tool to create, edit and run self-contained source-only Java programs"
+  homepage "https://jbang.dev/"
+  url "https://github.com/jbangdev/jbang/releases/download/v0.101.0/jbang-0.101.0.zip"
+  sha256 "32ae1b46d26a1e5f7e3084e7c1adc53058d5f75356e4464ab8327ff5e133cb29"
+  license "MIT"
+
+  depends_on "openjdk"
+
+  def install
+    libexec.install "bin/jbang.jar"
+    bin.write_jar_script libexec/"jbang.jar", "jbang"
+  end
+
+  test do
+    system "#{bin}/jbang", "init", "--template=cli", testpath/"hello.java"
+    assert_match "hello made with jbang", (testpath/"hello.java").read
+
+    assert_match version.to_s, shell_output("#{bin}/jbang --version 2>&1")
+  end
+end
