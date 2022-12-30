b16db09c4b09709f316fbf9311bc0ca4ed4eacbe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-12-30 05:30:27 +0000

    wakatime-cli 1.60.4
    Closes #119386.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/wakatime-cli.rb
+++ Formula/wakatime-cli.rb
@@ -2,8 +2,8 @@ class WakatimeCli < Formula
   desc "Command-line interface to the WakaTime api"
   homepage "https://wakatime.com/"
   url "https://github.com/wakatime/wakatime-cli.git",
-    tag:      "v1.60.1",
-    revision: "e9e36317129875443314338a5250a4c9ede605c0"
+    tag:      "v1.60.4",
+    revision: "9baaf4d2052da0d18504df1dce1d305ca5ac8aa4"
   license "BSD-3-Clause"
   version_scheme 1
 
