b9d7f7745ccd0a1630e0f740e698e442e2a03ea7
Rui Chen <rui@chenrui.dev>
2022-12-30 14:05:03 -0500

    fgbio 2.0.2 (new formula)
    Closes #119426.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/fgbio.rb
@@ -0,0 +1,24 @@
+class Fgbio < Formula
+  desc "Tools for working with genomic and high throughput sequencing data"
+  homepage "https://fulcrumgenomics.github.io/fgbio/"
+  url "https://github.com/fulcrumgenomics/fgbio/releases/download/2.0.2/fgbio-2.0.2.jar"
+  sha256 "85a01664fc8c3aa853d636ce8975c25c11d87352d7878e877af5f6a5bb1b3dce"
+  license "MIT"
+
+  depends_on "openjdk"
+
+  def install
+    libexec.install "fgbio-#{version}.jar"
+    bin.write_jar_script libexec/"fgbio-#{version}.jar", "fgbio"
+  end
+
+  test do
+    (testpath/"test.fasta").write <<~EOS
+      >U00096.2:1-70
+      AGCTTTTCATTCTGACTGCAACGGGCAATATGTCT
+      ctgtgtggattaaaaaaagagtgtctgatagcagc
+    EOS
+    cmd = "#{bin}/fgbio HardMaskFasta -i test.fasta -o /dev/stdout"
+    assert_match "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN", shell_output(cmd)
+  end
+end
