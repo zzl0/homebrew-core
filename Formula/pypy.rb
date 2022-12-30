60c1b34e26d566586ef22892173421bd6a0691d3
Patrick Linnane <patrick@linnane.io>
2022-12-30 10:37:55 -0800

    pypy 7.3.11
    * pypy 7.3.11
    * pypy: update bootstrap
    * pypy: update patch
    
    Closes #119424.
    
    Co-authored-by: Michael Cho <cho-m@tuta.io>
    Signed-off-by: Michael Cho <20700669+cho-m@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pypy.rb
+++ Formula/pypy.rb
@@ -1,8 +1,8 @@
 class Pypy < Formula
   desc "Highly performant implementation of Python 2 in Python"
   homepage "https://pypy.org/"
-  url "https://downloads.python.org/pypy/pypy2.7-v7.3.10-src.tar.bz2"
-  sha256 "35e2cf4519cb51c4d5ffb4493ee24f0c7f42b4b04944903ca4b33981a04a3bc5"
+  url "https://downloads.python.org/pypy/pypy2.7-v7.3.11-src.tar.bz2"
+  sha256 "1117afb66831da4ea6f39d8d2084787a74689fd0229de0be301f9ed9b255093c"
   license "MIT"
   head "https://foss.heptapod.net/pypy/pypy", using: :hg
 
@@ -37,17 +37,17 @@ class Pypy < Formula
   resource "bootstrap" do
     on_macos do
       on_arm do
-        url "https://downloads.python.org/pypy/pypy2.7-v7.3.10-macos_arm64.tar.bz2"
-        sha256 "14b178f005603e3df6db7574b77b9c65ae79feda1a629214cafcb4eee7da679d"
+        url "https://downloads.python.org/pypy/pypy2.7-v7.3.11-macos_arm64.tar.bz2"
+        sha256 "cc5696ab4f93cd3481c1e4990b5dedd7ba60ac0602fa1890d368889a6c5bf771"
       end
       on_intel do
-        url "https://downloads.python.org/pypy/pypy2.7-v7.3.4-osx64.tar.bz2"
-        sha256 "ee7bf42ce843596521e02c763408a5164d18f23c9617f1b8e032ce0675686582"
+        url "https://downloads.python.org/pypy/pypy2.7-v7.3.11-macos_x86_64.tar.bz2"
+        sha256 "56deee9c22640f5686c35b9d64fdb1ce3abd044583e4078f0b171ca2fd2a198e"
       end
     end
     on_linux do
-      url "https://downloads.python.org/pypy/pypy2.7-v7.3.4-linux64.tar.bz2"
-      sha256 "d3f7b0625e770d9be62201765d7d2316febc463372fba9c93a12969d26ae03dd"
+      url "https://downloads.python.org/pypy/pypy2.7-v7.3.11-linux64.tar.bz2"
+      sha256 "ba8ed958a905c0735a4cfff2875c25089954dc020e087d982b0ffa5b9da316cd"
     end
   end
 
@@ -189,16 +189,19 @@ end
 __END__
 --- a/lib_pypy/_tkinter/tklib_build.py
 +++ b/lib_pypy/_tkinter/tklib_build.py
-@@ -17,12 +17,12 @@ elif sys.platform == 'win32':
+@@ -17,7 +17,7 @@ elif sys.platform == 'win32':
      incdirs = []
      linklibs = ['tcl86t', 'tk86t']
      libdirs = []
 -elif sys.platform == 'darwin':
 +else:
      # homebrew
+     homebrew = os.environ.get('HOMEBREW_PREFIX', '')
      incdirs = ['/usr/local/opt/tcl-tk/include']
-     linklibs = ['tcl8.6', 'tk8.6']
-     libdirs = ['/usr/local/opt/tcl-tk/lib']
+@@ -26,7 +26,7 @@ elif sys.platform == 'darwin':
+     if homebrew:
+         incdirs.append(homebrew + '/include')
+         libdirs.append(homebrew + '/opt/tcl-tk/lib')
 -else:
 +if False: # disable Linux system tcl-tk detection
      # On some Linux distributions, the tcl and tk libraries are
