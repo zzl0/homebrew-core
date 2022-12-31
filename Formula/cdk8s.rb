7e594b790d2286976a1f11e8463a251a0f8f2ef1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-12-31 05:05:11 +0000

    cdk8s 2.1.91
    Closes #119447.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cdk8s.rb
+++ Formula/cdk8s.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Cdk8s < Formula
   desc "Define k8s native apps and abstractions using object-oriented programming"
   homepage "https://cdk8s.io/"
-  url "https://registry.npmjs.org/cdk8s-cli/-/cdk8s-cli-2.1.90.tgz"
-  sha256 "e7b5e2782621f8900ce3ffd28b2b8ec0aa55b7be708aefa238131690672d6a60"
+  url "https://registry.npmjs.org/cdk8s-cli/-/cdk8s-cli-2.1.91.tgz"
+  sha256 "76f1535e1fcd1a19155bd22aad12123b84f91e67b9a1b60833c403c5b34de9e1"
   license "Apache-2.0"
 
   bottle do
