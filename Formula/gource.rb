08da478be0fedc58e4435b093dec27add91e4083
fanquake <fanquake@gmail.com>
2022-12-31 11:02:35 +0000

    gource: revision bump (boost 1.81.0)

--- Formula/gource.rb
+++ Formula/gource.rb
@@ -4,7 +4,7 @@ class Gource < Formula
   url "https://github.com/acaudwell/Gource/releases/download/gource-0.53/gource-0.53.tar.gz"
   sha256 "3d5f64c1c6812f644c320cbc9a9858df97bc6036fc1e5f603ca46b15b8dd7237"
   license "GPL-3.0-or-later"
-  revision 2
+  revision 3
 
   bottle do
     sha256 arm64_ventura:  "557adabdc455f36a53771b2dfdc4d105011de331d4592fc73225a5f0c34890bb"
