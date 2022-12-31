21d749611ae8fa6af130fec593511b7d9b729d89
Nathan Whetsell <nathan.whetsell@gmail.com>
2022-12-31 16:18:26 -0500

    lilypond: revision
    * lilypond: add font
    * lilypond: add license
    * lilypond: add TeX Gyre font
    * lilypond: remove unnecessary string stripping
    * lilypond: install subset of TeX Gyre fonts
    * lilypond: test fonts more comprehensively
    
    Closes #119481.
    
    Co-authored-by: Nate Whetsell <nathan.whetsell@gmail.com>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/lilypond.rb
+++ Formula/lilypond.rb
@@ -10,7 +10,10 @@ class Lilypond < Formula
     "GFDL-1.3-no-invariants-or-later",
     :public_domain,
     "MIT",
+    "AGPL-3.0-only",
+    "LPPL-1.3c",
   ]
+  revision 1
 
   livecheck do
     url "https://lilypond.org/source.html"
@@ -36,6 +39,7 @@ class Lilypond < Formula
   depends_on "bison" => :build # bison >= 2.4.1 is required
   depends_on "fontforge" => :build
   depends_on "gettext" => :build
+  depends_on "libpthread-stubs" => :build
   depends_on "pkg-config" => :build
   depends_on "t1utils" => :build
   depends_on "texinfo" => :build # makeinfo >= 6.1 is required
@@ -50,14 +54,19 @@ class Lilypond < Formula
   uses_from_macos "flex" => :build
   uses_from_macos "perl" => :build
 
+  resource "font-urw-base35" do
+    url "https://github.com/ArtifexSoftware/urw-base35-fonts/archive/refs/tags/20200910.tar.gz"
+    sha256 "e0d9b7f11885fdfdc4987f06b2aa0565ad2a4af52b22e5ebf79e1a98abd0ae2f"
+  end
+
   def install
     system "./autogen.sh", "--noconfigure" if build.head?
 
     system "./configure", "--datadir=#{share}",
                           "--disable-documentation",
-                          *std_configure_args,
                           "--with-flexlexer-dir=#{Formula["flex"].include}",
-                          "GUILE_FLAVOR=guile-3.0"
+                          "GUILE_FLAVOR=guile-3.0",
+                          *std_configure_args
 
     system "make"
     system "make", "install"
@@ -66,11 +75,41 @@ class Lilypond < Formula
     system "make", "install-bytecode"
 
     elisp.install share.glob("emacs/site-lisp/*.el")
+
+    fonts = pkgshare/version/"fonts/otf"
+
+    resource("font-urw-base35").stage do
+      ["C059", "NimbusMonoPS", "NimbusSans"].each do |name|
+        Dir["fonts/#{name}-*.otf"].each do |font|
+          fonts.install font
+        end
+      end
+    end
+
+    ["cursor", "heros", "schola"].each do |name|
+      cp Dir[Formula["texlive"].share/"texmf-dist/fonts/opentype/public/tex-gyre/texgyre#{name}-*.otf"], fonts
+    end
   end
 
   test do
     (testpath/"test.ly").write "\\relative { c' d e f g a b c }"
     system bin/"lilypond", "--loglevel=ERROR", "test.ly"
     assert_predicate testpath/"test.pdf", :exist?
+
+    output = shell_output("#{bin}/lilypond --define-default=show-available-fonts 2>&1")
+    output = output.encode("UTF-8", invalid: :replace, replace: "")
+    fonts = {
+      "C059"            => ["Roman", "Bold", "Italic", "Bold Italic"],
+      "Nimbus Mono PS"  => ["Regular", "Bold", "Italic", "Bold Italic"],
+      "Nimbus Sans"     => ["Regular", "Bold", "Italic", "Bold Italic"],
+      "TeX Gyre Cursor" => ["Regular", "Bold", "Italic", "Bold Italic"],
+      "TeX Gyre Heros"  => ["Regular", "Bold", "Italic", "Bold Italic"],
+      "TeX Gyre Schola" => ["Regular", "Bold", "Italic", "Bold Italic"],
+    }
+    fonts.each do |family, styles|
+      styles.each do |style|
+        assert_match(/^\s*#{family}:style=#{style}$/, output)
+      end
+    end
   end
 end
