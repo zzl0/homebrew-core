ce3a4c2fd9cef8cb7cd0b9469464114d49b722ca
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-01 05:03:33 +0000

    bottom 0.7.0
    * bottom 0.7.0
    * bottom: update build and test
    * bottom: fix code style
    * bottom: simplify test
    
    Closes #119505.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/bottom.rb
+++ Formula/bottom.rb
@@ -1,8 +1,8 @@
 class Bottom < Formula
   desc "Yet another cross-platform graphical process/system monitor"
   homepage "https://clementtsang.github.io/bottom/"
-  url "https://github.com/ClementTsang/bottom/archive/0.6.8.tar.gz"
-  sha256 "4e4eb251972a7af8c46dd36bcf1335fea334fb670569434fbfd594208905b2d9"
+  url "https://github.com/ClementTsang/bottom/archive/0.7.0.tar.gz"
+  sha256 "54b5118499afe5935cd95ce42174288f0f50a67d6051fb160ac25ff60124800f"
   license "MIT"
   head "https://github.com/ClementTsang/bottom.git", branch: "master"
 
@@ -21,24 +21,22 @@ class Bottom < Formula
   depends_on "rust" => :build
 
   def install
+    # enable build-time generation of completion scripts and manpage
+    ENV["BTM_GENERATE"] = "true"
+
     system "cargo", "install", *std_cargo_args
 
     # Completion scripts are generated in the crate's build
     # directory, which includes a fingerprint hash. Try to locate it first
-    out_dir = Dir["target/release/build/bottom-*/out"].first
-    bash_completion.install "#{out_dir}/btm.bash"
-    fish_completion.install "#{out_dir}/btm.fish"
-    zsh_completion.install "#{out_dir}/_btm"
+    out_dir = "target/tmp/bottom"
+    bash_completion.install "#{out_dir}/completion/btm.bash"
+    fish_completion.install "#{out_dir}/completion/btm.fish"
+    zsh_completion.install "#{out_dir}/completion/_btm"
+    man1.install "#{out_dir}/manpage/btm.1"
   end
 
   test do
     assert_equal "bottom #{version}", shell_output(bin/"btm --version").chomp
-    assert_match "error: Found argument '--invalid'", shell_output(bin/"btm --invalid 2>&1", 1)
-
-    fork do
-      exec bin/"btm", "--config", "nonexistent-file"
-    end
-    sleep 1
-    assert_predicate testpath/"nonexistent-file", :exist?
+    assert_match "error: Found argument '--invalid'", shell_output(bin/"btm --invalid 2>&1", 2)
   end
 end
