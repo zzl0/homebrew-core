11435c9f3b5cfb9ed775bf117f5d3158f2dec175
Patrick Linnane <patrick@linnane.io>
2023-01-01 17:23:10 +0000

    file-formula: update 5.44 bottle.

--- Formula/file-formula.rb
+++ Formula/file-formula.rb
@@ -14,14 +14,13 @@ class FileFormula < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "5af7991cbec38b44fbde8e3f698b586b9621f2da7680f5ff74d57cb6aa654d64"
-    sha256 cellar: :any,                 arm64_monterey: "4670bf33cb4a2a2ec2167ba316f91d61ef524e67c0ec232ca2f513e4c6cc4a62"
-    sha256 cellar: :any,                 arm64_big_sur:  "983ef3239b886fa044a56adc2797f63b4d2930d7e68f589e8cd386d4663f2fe7"
-    sha256 cellar: :any,                 ventura:        "a52371b9d176c1d2504772344c3862e8ed31fe474b7ae1a553c9822532a58591"
-    sha256 cellar: :any,                 monterey:       "70ff46e2a33856064bfd7ab5752a26e2479fbef2c031cbca2a3c7c0eee867c33"
-    sha256 cellar: :any,                 big_sur:        "91a17666871d695c9c0df10afdb2b41835da1a8114dcba529e65f0feeffe2c8f"
-    sha256 cellar: :any,                 catalina:       "95ce9f08f5efa8f6c43390ab8ddc1f1807bba036419985f529ad1678c60cd595"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "156e61105587eaf3a1b7ee41b5a427cae1aa3eb873b07cf320989458957365e1"
+    sha256 cellar: :any,                 arm64_ventura:  "b0bacf7fc3496f0522a26087a42d0bfd3f190b248b7cd42f490723b65df15a4b"
+    sha256 cellar: :any,                 arm64_monterey: "fa40f52045db7b1c89b8a1853c03edfd35af995d880d167301e93203c473c0a1"
+    sha256 cellar: :any,                 arm64_big_sur:  "c821076e5ea013a8356a594de19743600dc14ecbc701be392af8f404885f39e0"
+    sha256 cellar: :any,                 ventura:        "9994529f7a658129dec00bfa95f133b29690881a423871c6584b7d4716992dc8"
+    sha256 cellar: :any,                 monterey:       "a5b0105a244262d002c7bb25219cf4d76058c643de7eda6722cbb0bc5b7c2420"
+    sha256 cellar: :any,                 big_sur:        "1457e4b2031ef8bf6aa3f26df91934bf7f7985da6708765749c724b0a189c84c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "557b2b68cc936ee2f9cbe2fca7bcd80535b837c7c7538b464ee8672a05f08b18"
   end
 
   keg_only :provided_by_macos
