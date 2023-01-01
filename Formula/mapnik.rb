c1941b98b61ca945bcbddcda0477d89e53f7b65b
Michael Cho <cho-m@tuta.io>
2022-12-31 20:03:38 -0800

    mapnik: use `scons` to build instead of bundled copy

--- Formula/mapnik.rb
+++ Formula/mapnik.rb
@@ -5,14 +5,17 @@ class Mapnik < Formula
   revision 20
   head "https://github.com/mapnik/mapnik.git", branch: "master"
 
+  # TODO: Try switching to CMake build on next release as it works better with
+  # Homebrew's build environment and avoids `--env=std` when using `scons`.
   stable do
     url "https://github.com/mapnik/mapnik/releases/download/v3.1.0/mapnik-v3.1.0.tar.bz2"
     sha256 "43d76182d2a975212b4ad11524c74e577576c11039fdab5286b828397d8e6261"
 
-    # Allow Makefile to use PYTHON set in the environment. Remove in the next release.
+    # Fix build with Scons 4 using Arch Linux patch. Remove in the next release.
+    # Ref: https://github.com/mapnik/mapnik/commit/84a05a6597a941acfad220dae3fbfe5d20bfeb26
     patch do
-      url "https://github.com/mapnik/mapnik/commit/a53c90172c664d29cd877302de9790a6ee9b5330.patch?full_index=1"
-      sha256 "9e0e06fd64d16b9fbe59d72402e805c94335397385ab57c49a6b468b9cc5a39c"
+      url "https://raw.githubusercontent.com/archlinux/svntogit-community/239768d7cd1217d5910d3f7d8ace86a7f85ad23c/trunk/scons4.patch"
+      sha256 "79a85ddba3ec17b86cb216e21442611498a9f2612f03e98708057b3c3a6e8b06"
     end
   end
 
@@ -32,7 +35,7 @@ class Mapnik < Formula
   end
 
   depends_on "pkg-config" => :build
-  depends_on "python@3.9" => :build
+  depends_on "scons" => :build
   depends_on "boost"
   depends_on "cairo"
   depends_on "freetype"
@@ -44,61 +47,69 @@ class Mapnik < Formula
   depends_on "libpq"
   depends_on "libtiff"
   depends_on "proj"
+  depends_on "sqlite"
   depends_on "webp"
 
+  uses_from_macos "zlib"
+
   def install
-    ENV.cxx11
-    ENV["PYTHON"] = "python3.9"
+    boost = Formula["boost"]
+    harfbuzz = Formula["harfbuzz"]
+    icu = Formula["icu4c"]
+    jpeg = Formula["jpeg-turbo"]
+    libpng = Formula["libpng"]
+    libpq = Formula["libpq"]
+    libtiff = Formula["libtiff"]
+    proj = Formula["proj"]
+    sqlite = Formula["sqlite"]
+    webp = Formula["webp"]
 
-    # Work around "error: no member named 'signbit' in the global namespace"
-    # encountered when trying to detect boost regex in configure
-    ENV.delete("SDKROOT") if DevelopmentTools.clang_build_version >= 900
+    # upstream issue, see https://github.com/boostorg/phoenix/issues/115
+    # workaround to avoid the inclusion of `boost/phoenix/stl/tuple.hpp`
+    ENV.append "CXXFLAGS", "-DBOOST_PHOENIX_STL_TUPLE_H_"
 
-    boost = Formula["boost"].opt_prefix
-    freetype = Formula["freetype"].opt_prefix
-    harfbuzz = Formula["harfbuzz"].opt_prefix
-    icu = Formula["icu4c"].opt_prefix
-    jpeg = Formula["jpeg-turbo"].opt_prefix
-    libpng = Formula["libpng"].opt_prefix
-    libtiff = Formula["libtiff"].opt_prefix
-    proj = Formula["proj"].opt_prefix
-    webp = Formula["webp"].opt_prefix
+    if OS.linux?
+      deps.map(&:to_formula).select(&:keg_only?).map(&:opt_lib).each do |libdir|
+        ENV.append "LDFLAGS", "-Wl,-rpath,#{libdir}"
+      end
+      ENV.append "LDFLAGS", "-Wl,-rpath,#{HOMEBREW_PREFIX}/lib"
+    end
 
     args = %W[
       CC=#{ENV.cc}
       CXX=#{ENV.cxx}
+      CUSTOM_CFLAGS=#{ENV.cflags}
+      CUSTOM_CXXFLAGS=#{ENV.cxxflags}
+      CUSTOM_LDFLAGS=#{ENV.ldflags}
       PREFIX=#{prefix}
-      BOOST_INCLUDES=#{boost}/include
-      BOOST_LIBS=#{boost}/lib
-      CAIRO=True
+      FAST=True
       CPP_TESTS=False
-      FREETYPE_CONFIG=#{freetype}/bin/freetype-config
-      GDAL_CONFIG=#{Formula["gdal"].opt_bin}/gdal-config
-      HB_INCLUDES=#{harfbuzz}/include
-      HB_LIBS=#{harfbuzz}/lib
-      ICU_INCLUDES=#{icu}/include
-      ICU_LIBS=#{icu}/lib
       INPUT_PLUGINS=all
-      JPEG_INCLUDES=#{jpeg}/include
-      JPEG_LIBS=#{jpeg}/lib
-      NIK2IMG=False
-      PG_CONFIG=#{Formula["libpq"].opt_bin}/pg_config
-      PNG_INCLUDES=#{libpng}/include
-      PNG_LIBS=#{libpng}/lib
-      PROJ_INCLUDES=#{proj}/include
-      PROJ_LIBS=#{proj}/lib
-      TIFF_INCLUDES=#{libtiff}/include
-      TIFF_LIBS=#{libtiff}/lib
-      WEBP_INCLUDES=#{webp}/include
-      WEBP_LIBS=#{webp}/lib
+      BOOST_INCLUDES=#{boost.opt_include}
+      BOOST_LIBS=#{boost.opt_lib}
+      FREETYPE_CONFIG=#{Formula["freetype"].opt_bin}/freetype-config
+      GDAL_CONFIG=#{Formula["gdal"].opt_bin}/gdal-config
+      HB_INCLUDES=#{harfbuzz.opt_include}
+      HB_LIBS=#{harfbuzz.opt_lib}
+      ICU_INCLUDES=#{icu.opt_include}
+      ICU_LIBS=#{icu.opt_lib}
+      JPEG_INCLUDES=#{jpeg.opt_include}
+      JPEG_LIBS=#{jpeg.opt_lib}
+      PG_CONFIG=#{libpq.opt_bin}/pg_config
+      PNG_INCLUDES=#{libpng.opt_include}
+      PNG_LIBS=#{libpng.opt_lib}
+      PROJ_INCLUDES=#{proj.opt_include}
+      PROJ_LIBS=#{proj.opt_lib}
+      SQLITE_INCLUDES=#{sqlite.opt_include}
+      SQLITE_LIBS=#{sqlite.opt_lib}
+      TIFF_INCLUDES=#{libtiff.opt_include}
+      TIFF_LIBS=#{libtiff.opt_lib}
+      WEBP_INCLUDES=#{webp.opt_include}
+      WEBP_LIBS=#{webp.opt_lib}
     ]
-    # upstream issue, see https://github.com/boostorg/phoenix/issues/115
-    # workaround to avoid the inclusion of `boost/phoenix/stl/tuple.hpp`
-    args << "CUSTOM_CXXFLAGS=-DBOOST_PHOENIX_STL_TUPLE_H_"
 
-    system "./configure", *args
-    system "make"
-    system "make", "install"
+    system "scons", "configure", *args
+    system "scons", "install", "--jobs=#{ENV.make_jobs}"
   end
 
   test do
