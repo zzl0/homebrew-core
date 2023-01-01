1daa1248dc6c1cc81631f1a6dadcf8552e3885fc
Rui Chen <rui@chenrui.dev>
2023-01-01 11:51:21 -0500

    upscaledb: revision bump (boost 1.81.0)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/upscaledb.rb
+++ Formula/upscaledb.rb
@@ -2,7 +2,7 @@ class Upscaledb < Formula
   desc "Database for embedded devices"
   homepage "https://upscaledb.com/"
   license "Apache-2.0"
-  revision 2
+  revision 3
   head "https://github.com/cruppstahl/upscaledb.git", branch: "master"
 
   stable do
