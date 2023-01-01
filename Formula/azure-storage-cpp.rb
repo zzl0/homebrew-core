04345575f4276879a5ff1f5b622db1ccf678db50
Rui Chen <rui@chenrui.dev>
2023-01-01 11:51:06 -0500

    azure-storage-cpp: revision bump (boost 1.81.0)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/azure-storage-cpp.rb
+++ Formula/azure-storage-cpp.rb
@@ -4,7 +4,7 @@ class AzureStorageCpp < Formula
   url "https://github.com/Azure/azure-storage-cpp/archive/v7.5.0.tar.gz"
   sha256 "446a821d115949f6511b7eb01e6a0e4f014b17bfeba0f3dc33a51750a9d5eca5"
   license "Apache-2.0"
-  revision 3
+  revision 4
 
   bottle do
     sha256 cellar: :any,                 arm64_ventura:  "b47dacae19d53efa3e73b64ed99ac6c44f5971ae762c5ee76daf25a49eae8619"
