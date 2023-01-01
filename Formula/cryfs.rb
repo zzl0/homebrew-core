621c8c3a49648fc9e0a16007fbf44a68ebe405aa
Rui Chen <rui@chenrui.dev>
2023-01-01 11:51:07 -0500

    cryfs: revision bump (boost 1.81.0)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/cryfs.rb
+++ Formula/cryfs.rb
@@ -6,7 +6,7 @@ class Cryfs < Formula
   url "https://github.com/cryfs/cryfs/releases/download/0.11.3/cryfs-0.11.3.tar.gz"
   sha256 "cffef7669b8cbec3e7420088faac492390b9e1f3d3d0dc2a245b87f8df05f190"
   license "LGPL-3.0"
-  revision 1
+  revision 2
 
   bottle do
     rebuild 1
