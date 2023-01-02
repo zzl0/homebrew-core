d1e81012116d1d28aa22b2872fc928e697ef67d2
Tao He <sighingnow@gmail.com>
2023-01-02 21:49:34 +0800

    vineyard 0.11.4
    Closes #119581.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/vineyard.rb
+++ Formula/vineyard.rb
@@ -3,10 +3,9 @@ class Vineyard < Formula
 
   desc "In-memory immutable data manager. (Project under CNCF)"
   homepage "https://v6d.io"
-  url "https://github.com/v6d-io/v6d/releases/download/v0.11.3/v6d-0.11.3.tar.gz"
-  sha256 "18befb052270d5cfd1280db571b9dbdeb876d988d308d7cee1deb5ddde0121a0"
+  url "https://github.com/v6d-io/v6d/releases/download/v0.11.4/v6d-0.11.4.tar.gz"
+  sha256 "cc807b991e5e35847783ee679ab14e75e23bbf04b309f2dd56ca37e3b6bc7ca8"
   license "Apache-2.0"
-  revision 1
 
   bottle do
     sha256 arm64_ventura:  "418b82028a5d3369b8bfea1118b1203e1a698282a62dbb4c70a4ddfaba330f7e"
