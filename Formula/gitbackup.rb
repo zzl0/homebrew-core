c98793240c1a1714f0ce11d1b1a2346259faffb7
Patrick Linnane <patrick@linnane.io>
2023-01-02 01:05:03 +0000

    gitbackup: update 0.9.0 bottle.

--- Formula/gitbackup.rb
+++ Formula/gitbackup.rb
@@ -6,14 +6,13 @@ class Gitbackup < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "ee940778c4de907a7c62a212d36bdcf18f603d4ef8a13107eb4ee8ed544fa2d8"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "1d252dfcf227ed7bbef92ce38e7583c30eb6076b4aea1fbda4a3787e534c08d2"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1d252dfcf227ed7bbef92ce38e7583c30eb6076b4aea1fbda4a3787e534c08d2"
-    sha256 cellar: :any_skip_relocation, ventura:        "00bd5db22cf631c65a3e94117956cf634d8a3884af6f157147ff76ad351b00a5"
-    sha256 cellar: :any_skip_relocation, monterey:       "fd373f206b493ec79fbcb94cca3f36e2de7881e4aef9fcb4422cf94334ba6057"
-    sha256 cellar: :any_skip_relocation, big_sur:        "fd373f206b493ec79fbcb94cca3f36e2de7881e4aef9fcb4422cf94334ba6057"
-    sha256 cellar: :any_skip_relocation, catalina:       "fd373f206b493ec79fbcb94cca3f36e2de7881e4aef9fcb4422cf94334ba6057"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1597e86a57a79f25d0acea80e94255a10df45c6494767853025cf87755ba76fe"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "e2662dc58792c8acff07d745e88026a58ad2916aa943f03e214861ffff83ced8"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e2662dc58792c8acff07d745e88026a58ad2916aa943f03e214861ffff83ced8"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e2662dc58792c8acff07d745e88026a58ad2916aa943f03e214861ffff83ced8"
+    sha256 cellar: :any_skip_relocation, ventura:        "9c458fa964243f8611a16564f9afc526a101cc2c1167a0f1dd85def0cd35ce20"
+    sha256 cellar: :any_skip_relocation, monterey:       "9c458fa964243f8611a16564f9afc526a101cc2c1167a0f1dd85def0cd35ce20"
+    sha256 cellar: :any_skip_relocation, big_sur:        "9c458fa964243f8611a16564f9afc526a101cc2c1167a0f1dd85def0cd35ce20"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2a062c0da560c0779e9ee952408cc4131980d0658f594933525ee795dd2e9635"
   end
 
   depends_on "go" => :build
