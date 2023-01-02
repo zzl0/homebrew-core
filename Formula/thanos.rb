c41a76614fece3abe7cbe8d8cde26e8da816e598
Rui Chen <rui@chenrui.dev>
2023-01-02 13:39:32 -0500

    thanos 0.30.0
    Closes #119594.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/thanos.rb
+++ Formula/thanos.rb
@@ -1,8 +1,8 @@
 class Thanos < Formula
   desc "Highly available Prometheus setup with long term storage capabilities"
   homepage "https://thanos.io"
-  url "https://github.com/thanos-io/thanos/archive/v0.29.0.tar.gz"
-  sha256 "45fa97935a8d4f426c677c6077e404cd863c16f758187ad16262451df3262a14"
+  url "https://github.com/thanos-io/thanos/archive/v0.30.0.tar.gz"
+  sha256 "6e18d9f032e9f0a7735259e367aaa528b2339eae5eafb18af46049dceaa7c13d"
   license "Apache-2.0"
 
   bottle do
