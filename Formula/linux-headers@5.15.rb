f46c7f66093625a9b94d4838460bd60288efe43a
fanquake <fanquake@gmail.com>
2023-01-03 14:53:01 +0000

    linux-headers@5.15 5.15.86
    Closes #119661.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/linux-headers@5.15.rb
+++ Formula/linux-headers@5.15.rb
@@ -1,8 +1,8 @@
 class LinuxHeadersAT515 < Formula
   desc "Header files of the Linux kernel"
   homepage "https://kernel.org/"
-  url "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.15.85.tar.gz"
-  sha256 "d046e26c5851943d612528beecc2a187c52ca5cd1ee0af092048c47e447c4e35"
+  url "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.15.86.tar.gz"
+  sha256 "acd450838f720c39659aada8f06a3b4d8c237ab6147cad597e016205dab966e6"
   license "GPL-2.0-only"
 
   bottle do
