ede901be792e106fad9a43ac5068983e5cd7e9d8
Zhong Ruoyu <zhongruoyu@outlook.com>
2023-01-03 11:36:12 +0800

    cdk8s 2.1.94
    Closes #119631.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cdk8s.rb
+++ Formula/cdk8s.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Cdk8s < Formula
   desc "Define k8s native apps and abstractions using object-oriented programming"
   homepage "https://cdk8s.io/"
-  url "https://registry.npmjs.org/cdk8s-cli/-/cdk8s-cli-2.1.93.tgz"
-  sha256 "3bbb554c109c01b51f31d93dc43e70dc36acf67fffd786cc0f32c4875088f10c"
+  url "https://registry.npmjs.org/cdk8s-cli/-/cdk8s-cli-2.1.94.tgz"
+  sha256 "112c0b60dec330783366d34da03764c75191aa4430724f1ca72e0571437f99b7"
   license "Apache-2.0"
 
   bottle do
