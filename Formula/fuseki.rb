ed924e24c7caf7a34b2099f03be0612fe025d741
Porkepix <porkepix@gmail.com>
2023-01-03 13:52:47 +0100

    fuseki 4.7.0
    Closes #119642.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/fuseki.rb
+++ Formula/fuseki.rb
@@ -1,9 +1,9 @@
 class Fuseki < Formula
   desc "SPARQL server"
   homepage "https://jena.apache.org/documentation/fuseki2/"
-  url "https://www.apache.org/dyn/closer.lua?path=jena/binaries/apache-jena-fuseki-4.6.1.tar.gz"
-  mirror "https://archive.apache.org/dist/jena/binaries/apache-jena-fuseki-4.6.1.tar.gz"
-  sha256 "2d468da5871e80cc5ded65f70c2be06f61f1b2b7b8107a7b1a4012cc97f068cd"
+  url "https://www.apache.org/dyn/closer.lua?path=jena/binaries/apache-jena-fuseki-4.7.0.tar.gz"
+  mirror "https://archive.apache.org/dist/jena/binaries/apache-jena-fuseki-4.7.0.tar.gz"
+  sha256 "ac8a76a14cd1f1e9f6e3c73b50571fd5b37a47dd41ce050ea09b6906469afe28"
   license "Apache-2.0"
 
   bottle do
