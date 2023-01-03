dffb775132395e24df3242d63bd9f39689222ebf
Patrick Linnane <patrick@linnane.io>
2023-01-03 13:32:39 -0800

    i2pd 2.45.0
    * i2pd 2.45.0
    * i2pd: add upstream patch
    
    Closes #119694.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/i2pd.rb
+++ Formula/i2pd.rb
@@ -1,8 +1,8 @@
 class I2pd < Formula
   desc "Full-featured C++ implementation of I2P client"
   homepage "https://i2pd.website/"
-  url "https://github.com/PurpleI2P/i2pd/archive/2.44.0.tar.gz"
-  sha256 "b653c845ac7a16fefab2ace78e3ae496c12b05304bb66e41e776071635d4e070"
+  url "https://github.com/PurpleI2P/i2pd/archive/2.45.0.tar.gz"
+  sha256 "4050c0a38fda06a76defbd2721f468f5b09889ed7b6b5a7e207e4659d300738f"
   license "BSD-3-Clause"
 
   bottle do
@@ -19,6 +19,13 @@ class I2pd < Formula
   depends_on "boost"
   depends_on "miniupnpc"
   depends_on "openssl@3"
+
+  # Upstream issue ref: https://github.com/PurpleI2P/i2pd/issues/1836
+  # Remove in next release
+  patch do
+    url "https://github.com/PurpleI2P/i2pd/commit/acd6af709ee6371b3d0340d2c92cb86f6f4bcb97.patch?full_index=1"
+    sha256 "54343ce6a8cf970cd65c9b39c55b3fe15f28c3a1dc6dbc07df870ad8118cebc7"
+  end
 
   def install
     args = %W[
