c491cdbfdcb263d6ad6a0683e8683d9f5950cb63
Patrick Linnane <patrick@linnane.io>
2023-01-03 13:21:02 -0800

    bde 3.112.0.0
    * bde 3.112.0.0
    * bde: update resource
    * bde: stage bde-tools in subdir instead of buildpath
    
    Closes #119687.
    
    Co-authored-by: Caleb Xu <calebcenter@live.com>
    Signed-off-by: Dawid Dziurla <9713907+dawidd6@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/bde.rb
+++ Formula/bde.rb
@@ -1,10 +1,9 @@
 class Bde < Formula
   desc "Basic Development Environment: foundational C++ libraries used at Bloomberg"
   homepage "https://github.com/bloomberg/bde"
-  url "https://github.com/bloomberg/bde/archive/3.61.0.0.tar.gz"
-  sha256 "46dcdcf06f3cf582170848721dd6d8ca9c993f9cfa34445103d3cee34a5d6dda"
+  url "https://github.com/bloomberg/bde/archive/3.112.0.0.tar.gz"
+  sha256 "e6dfade0a1d9a1b9554b8a94e359169dab492162ffa956cb889817033daf5405"
   license "Apache-2.0"
-  revision 1
 
   livecheck do
     url :stable
@@ -28,31 +27,23 @@ class Bde < Formula
   depends_on "pcre2"
 
   resource "bde-tools" do
-    url "https://github.com/bloomberg/bde-tools/archive/3.61.0.0.tar.gz"
-    sha256 "7d66eb149c7e59021467c386ba5c6149a8923e836c6a61b815651b3cac019a7d"
-  end
-
-  # Backport support for Apple Silicon. Remove in the next release
-  patch do
-    on_arm do
-      url "https://github.com/bloomberg/bde/commit/39a52e09c83eec761874be2260b692b715117fae.patch?full_index=1"
-      sha256 "d3721d8a297687ddd2003386845aaa6ec3f8664ab81551b1ef581fe65ad1cb96"
-    end
+    url "https://github.com/bloomberg/bde-tools/archive/3.112.0.0.tar.gz"
+    sha256 "4588c478f995f65fbd805cbe102f4440b602504d2cbd4937b79b3e49b06da0f4"
   end
 
   def install
-    buildpath.install resource("bde-tools")
+    (buildpath/"bde-tools").install resource("bde-tools")
 
     # Use brewed pcre2 instead of bundled sources
     inreplace "project.cmake", "${listDir}/thirdparty/pcre2\n", ""
     inreplace "groups/bdl/group/bdl.dep", "pcre2", "libpcre2-posix"
     inreplace "groups/bdl/bdlpcre/bdlpcre_regex.h", "#include <pcre2/pcre2.h>", "#include <pcre2.h>"
 
-    toolchain_file = "cmake/toolchains/#{OS.kernel_name.downcase}/default.cmake"
+    toolchain_file = "bde-tools/cmake/toolchains/#{OS.kernel_name.downcase}/default.cmake"
     args = std_cmake_args + %W[
       -DBUILD_BITNESS=64
       -DUFID=opt_exc_mt_64_shr
-      -DCMAKE_MODULE_PATH=cmake
+      -DCMAKE_MODULE_PATH=./bde-tools/cmake
       -DCMAKE_INSTALL_RPATH=#{rpath}
       -DCMAKE_TOOLCHAIN_FILE=#{toolchain_file}
       -DPYTHON_EXECUTABLE=#{which("python3.11")}
