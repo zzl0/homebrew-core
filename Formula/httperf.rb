4d6e130307f412a34281fbf03987d520e3d26be6
Michael Cho <cho-m@tuta.io>
2023-01-05 13:52:03 -0800

    httperf: migrate to `openssl@3` and fix HEAD build
    Closes #119873.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/httperf.rb
+++ Formula/httperf.rb
@@ -1,10 +1,20 @@
 class Httperf < Formula
   desc "Tool for measuring webserver performance"
   homepage "https://github.com/httperf/httperf"
-  url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/httperf/httperf-0.9.0.tar.gz"
-  sha256 "e1a0bf56bcb746c04674c47b6cfa531fad24e45e9c6de02aea0d1c5f85a2bf1c"
   license "GPL-2.0"
   revision 2
+
+  stable do
+    url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/httperf/httperf-0.9.0.tar.gz"
+    sha256 "e1a0bf56bcb746c04674c47b6cfa531fad24e45e9c6de02aea0d1c5f85a2bf1c"
+
+    # Upstream patch for OpenSSL 1.1 compatibility
+    # https://github.com/httperf/httperf/pull/48
+    patch do
+      url "https://raw.githubusercontent.com/Homebrew/formula-patches/85fa66a9/httperf/openssl-1.1.diff"
+      sha256 "69d5003f60f5e46d25813775bbf861366fb751da4e0e4d2fe7530d7bb3f3660a"
+    end
+  end
 
   # Until the upstream GitHub repository creates a new release (something after
   # 0.9.0), we're unable to create a check that can identify new versions.
@@ -27,25 +37,18 @@ class Httperf < Formula
   end
 
   head do
-    url "https://github.com/httperf/httperf.git"
+    url "https://github.com/httperf/httperf.git", branch: "master"
 
     depends_on "autoconf" => :build
     depends_on "automake" => :build
     depends_on "libtool" => :build
   end
 
-  depends_on "openssl@1.1"
-
-  # Upstream patch for OpenSSL 1.1 compatibility
-  # https://github.com/httperf/httperf/pull/48
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/85fa66a9/httperf/openssl-1.1.diff"
-    sha256 "69d5003f60f5e46d25813775bbf861366fb751da4e0e4d2fe7530d7bb3f3660a"
-  end
+  depends_on "openssl@3"
 
   def install
-    system "autoreconf", "-fvi" if build.head?
-    system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
+    system "autoreconf", "--force", "--install", "--verbose" if build.head?
+    system "./configure", *std_configure_args
     system "make", "install"
   end
 
