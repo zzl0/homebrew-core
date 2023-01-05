7b9c7bc539f145050f408cf1befce5cb2ef2b40b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-05 17:04:54 +0000

    urdfdom_headers: update 1.1.0 bottle.

--- Formula/urdfdom_headers.rb
+++ Formula/urdfdom_headers.rb
@@ -6,13 +6,7 @@ class UrdfdomHeaders < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "9d1960994a18f178a7ff7b5f6a0b479bbd5abcba370fb72b816fd99ff044283c"
-    sha256 cellar: :any_skip_relocation, big_sur:       "45e38439bb14220663e856c3db0017987adfe1765c815344c38f404cf49849a6"
-    sha256 cellar: :any_skip_relocation, catalina:      "c6840c3177042a718ae29bc8238e5ec20dbcde4f9269b3042f8eed1a2aa6292c"
-    sha256 cellar: :any_skip_relocation, mojave:        "c6840c3177042a718ae29bc8238e5ec20dbcde4f9269b3042f8eed1a2aa6292c"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "c6840c3177042a718ae29bc8238e5ec20dbcde4f9269b3042f8eed1a2aa6292c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f9b714415e5939d4ee8c7cbee3494f3f84eb06def8635f8d579dff740af05d6f"
-    sha256 cellar: :any_skip_relocation, all:           "c851880d554db90688c10eb68a1ea22051f0bb7453667590aa61f9821983bedb"
+    sha256 cellar: :any_skip_relocation, all: "7af8770bcbebfab71cb7921651eae92d0d57a1e4176842c0a63dc7fc64715e8c"
   end
 
   depends_on "cmake" => :build
