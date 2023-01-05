630bd40cf69caa972fe7a91695ff77d6de5c9a14
Patrick Linnane <patrick@linnane.io>
2023-01-05 11:00:26 -0800

    cpufetch 1.03
    * cpufetch 1.03
    * cpufetch: update license
    * cpufetch: add upstream patches
    
    Closes #119853.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cpufetch.rb
+++ Formula/cpufetch.rb
@@ -1,9 +1,9 @@
 class Cpufetch < Formula
   desc "CPU architecture fetching tool"
   homepage "https://github.com/Dr-Noob/cpufetch"
-  url "https://github.com/Dr-Noob/cpufetch/archive/v1.02.tar.gz"
-  sha256 "3d1c80aba3daa5fe300b6de6e06d9030f97b7be5210f8ea4110de733ea4373f8"
-  license "GPL-2.0"
+  url "https://github.com/Dr-Noob/cpufetch/archive/v1.03.tar.gz"
+  sha256 "550168e0523240a1fb837e85073e0aa69de1894f1b89ec3a5721a5d935679afb"
+  license "GPL-2.0-only"
   head "https://github.com/Dr-Noob/cpufetch.git", branch: "master"
 
   bottle do
@@ -14,6 +14,20 @@ class Cpufetch < Formula
     sha256 cellar: :any_skip_relocation, big_sur:        "0364a04659759812076a6b70117e26679bbac74cee4943b7575844cb24169d16"
     sha256 cellar: :any_skip_relocation, catalina:       "fecfd3b7d1c1a4b9811ec886f5733b1ebe944a9ceab4a0526ff18702674c5591"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "22c8f22e55957d44ba4a34167bd8d64d3dd01c54ba61a36c4fba9191dc343064"
+  end
+
+  # Upstream issue ref: https://github.com/Dr-Noob/cpufetch/issues/168
+  # Remove in next release
+  patch do
+    url "https://github.com/Dr-Noob/cpufetch/commit/22a80d817d57814fc552365ad553c0a22f065fcd.patch?full_index=1"
+    sha256 "063b602cd5013ba7c2c5ea4e134c911164ec49b2ed14209c313c2ef005bd3d42"
+  end
+
+  # Upstream issue ref: https://github.com/Dr-Noob/cpufetch/issues/168
+  # Remove in next release
+  patch do
+    url "https://github.com/Dr-Noob/cpufetch/commit/095bbfb784f0b367558741e9b02f6278126e1c93.patch?full_index=1"
+    sha256 "494756db04ab00a0a57d519704f5032d2b77e7539d4c0233b789c5a6178fbab8"
   end
 
   def install
