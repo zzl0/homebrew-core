2b53e5f3993195a162a37e06c928d45432eeadcd
Porkepix <porkepix@gmail.com>
2023-01-05 02:39:59 +0100

    quicktype 16.0.10
    Closes #119809.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/quicktype.rb
+++ Formula/quicktype.rb
@@ -4,8 +4,8 @@ class Quicktype < Formula
   desc "Generate types and converters from JSON, Schema, and GraphQL"
   homepage "https://github.com/quicktype/quicktype"
   # quicktype should only be updated every 10 releases on multiples of 10
-  url "https://registry.npmjs.org/quicktype/-/quicktype-16.0.0.tgz"
-  sha256 "78f98d4f90adda22ececad14994fd897890d6fa175ad86b62d77ba1774b91c03"
+  url "https://registry.npmjs.org/quicktype/-/quicktype-16.0.10.tgz"
+  sha256 "5df536669ea21698bf72ccfc4c9360c8dbd5d961500920389352b1bb57313aae"
   license "Apache-2.0"
   head "https://github.com/quicktype/quicktype.git", branch: "master"
 
