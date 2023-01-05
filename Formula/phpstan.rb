2fa2d7feb5f1749ccad7593c40afd11c51451aae
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-05 04:04:48 +0000

    phpstan: update 1.9.7 bottle.

--- Formula/phpstan.rb
+++ Formula/phpstan.rb
@@ -6,13 +6,13 @@ class Phpstan < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "7df40e93ec60fe5f0aff2b4eb170247fac37aab03c1b54ebb2fb388a3c09830c"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "7df40e93ec60fe5f0aff2b4eb170247fac37aab03c1b54ebb2fb388a3c09830c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7df40e93ec60fe5f0aff2b4eb170247fac37aab03c1b54ebb2fb388a3c09830c"
-    sha256 cellar: :any_skip_relocation, ventura:        "dbb54b3d50c67c11250449232768b912ae55cf6b496a8e413f56cc1ce2920e05"
-    sha256 cellar: :any_skip_relocation, monterey:       "dbb54b3d50c67c11250449232768b912ae55cf6b496a8e413f56cc1ce2920e05"
-    sha256 cellar: :any_skip_relocation, big_sur:        "dbb54b3d50c67c11250449232768b912ae55cf6b496a8e413f56cc1ce2920e05"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7df40e93ec60fe5f0aff2b4eb170247fac37aab03c1b54ebb2fb388a3c09830c"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "aa5f853498cee5de5e28e7d2c02f7ea4aceb7db8f8c16a0ff8a0ee1b88d320c0"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "aa5f853498cee5de5e28e7d2c02f7ea4aceb7db8f8c16a0ff8a0ee1b88d320c0"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "aa5f853498cee5de5e28e7d2c02f7ea4aceb7db8f8c16a0ff8a0ee1b88d320c0"
+    sha256 cellar: :any_skip_relocation, ventura:        "5921c450bb0c721944160d9ab2b313f0f6aedd54b9460700e86896ebe18319ea"
+    sha256 cellar: :any_skip_relocation, monterey:       "5921c450bb0c721944160d9ab2b313f0f6aedd54b9460700e86896ebe18319ea"
+    sha256 cellar: :any_skip_relocation, big_sur:        "5921c450bb0c721944160d9ab2b313f0f6aedd54b9460700e86896ebe18319ea"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "aa5f853498cee5de5e28e7d2c02f7ea4aceb7db8f8c16a0ff8a0ee1b88d320c0"
   end
 
   depends_on "php" => :test
