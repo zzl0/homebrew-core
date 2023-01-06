f394534247df6fd4959db48ce95cab36fb486396
Tilt Dev <it@tilt.dev>
2023-01-06 20:18:46 +0000

    tilt 0.31.0
    Closes #119956.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tilt.rb
+++ Formula/tilt.rb
@@ -2,8 +2,8 @@ class Tilt < Formula
   desc "Define your dev environment as code. For microservice apps on Kubernetes"
   homepage "https://tilt.dev/"
   url "https://github.com/tilt-dev/tilt.git",
-    tag:      "v0.30.13",
-    revision: "c4513e8b69a8b64970ee954235f4e3d1ddaee605"
+    tag:      "v0.31.0",
+    revision: "f53e57313538a39ffb0c6e9dbd0b33cfc6cef590"
   license "Apache-2.0"
   head "https://github.com/tilt-dev/tilt.git", branch: "master"
 
