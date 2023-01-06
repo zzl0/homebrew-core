c5d436612dd6632720f5d3808f9338baaec0083b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-06 01:32:15 +0000

    nrpe: update 4.1.0 bottle.

--- Formula/nrpe.rb
+++ Formula/nrpe.rb
@@ -6,15 +6,12 @@ class Nrpe < Formula
   license "GPL-2.0"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any, arm64_ventura:  "91ee555fcf3a779c40ce93dccc6be2cc7b02dd1a6b0e18fae7096b43eb238ec1"
-    sha256 cellar: :any, arm64_monterey: "81d5307bca972fc7945af1226e9e80e3bd798a2296ef7da9fa16240c27064b28"
-    sha256 cellar: :any, arm64_big_sur:  "cec620b7f488a0e443b032921653c3bbf8e43438e8ca8b02b23cf6ea9284da5c"
-    sha256 cellar: :any, ventura:        "1213aefe88e396f74bc72002d81daaf592c3da0e76baaab5043a80827ab6e1d7"
-    sha256 cellar: :any, monterey:       "9e8adb2e3df46f2a950a95d285d70e8142d9ce3a00eb7171fb34b27daf8624d6"
-    sha256 cellar: :any, big_sur:        "0d5e76f99e6ea435cda18d6476158ced7debd4a4633176bcf1f3d6e721cd9724"
-    sha256 cellar: :any, catalina:       "d142c84ff35a78b7a2705ac915552c617420ff747eb47fd8d3ba27347ec384bc"
-    sha256 cellar: :any, mojave:         "ea0e988a1aef0f1905f0fe807d687096f10389c6b09f415015f713e529af4740"
+    sha256 cellar: :any, arm64_ventura:  "1fc4928fdac6257f935fac1840c39f68a70e9634cc1bf9a6087011ea1804698c"
+    sha256 cellar: :any, arm64_monterey: "c8ce52dc60241ee1e361db1085c8b341c2b72dbf946932efcada9e221add88dd"
+    sha256 cellar: :any, arm64_big_sur:  "09b54c81df11d937d138916c8eaa5cd22795cd003f9ffa59f47927668af0b93d"
+    sha256 cellar: :any, ventura:        "7a52a2a6506171a6ed4c859899cfde0614fdff521002d97098f3844e99d1f7b9"
+    sha256 cellar: :any, monterey:       "83bca8ddf7e379b010c390cfd7a0bb42ff9b0d08ae09f9c81af4a44c769737dc"
+    sha256 cellar: :any, big_sur:        "af5068970374e0d732400f3de3f6013215fbe95aea879109a399c5724df059f5"
   end
 
   depends_on "nagios-plugins"
