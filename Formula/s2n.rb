6cc1044c69b65126cf5c4f37e01017064a34cb77
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-06 05:24:50 +0000

    s2n 1.3.32
    Closes #119909.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/s2n.rb
+++ Formula/s2n.rb
@@ -1,8 +1,8 @@
 class S2n < Formula
   desc "Implementation of the TLS/SSL protocols"
   homepage "https://github.com/aws/s2n-tls"
-  url "https://github.com/aws/s2n-tls/archive/v1.3.31.tar.gz"
-  sha256 "23cfb42f82cbe1ce94b59f3b1c1c8eb9d24af2a1ae4c8f854209ff88fddd3900"
+  url "https://github.com/aws/s2n-tls/archive/v1.3.32.tar.gz"
+  sha256 "01f3db28a091004afa19ecf1324151857146b80cbe421b875f27bc4284454212"
   license "Apache-2.0"
   head "https://github.com/aws/s2n-tls.git", branch: "main"
 
