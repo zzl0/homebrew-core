4f2831879ed602961d54a7ba30b2f0df8ad0cc81
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-06 03:11:21 +0000

    hapi-fhir-cli: update 6.2.4 bottle.

--- Formula/hapi-fhir-cli.rb
+++ Formula/hapi-fhir-cli.rb
@@ -16,7 +16,7 @@ class HapiFhirCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "e421b6e241b47f11bb15fd3548fc451aef3524266f3bc3446b5a2c573cb4ff47"
+    sha256 cellar: :any_skip_relocation, all: "70f9768ccebd14d98ab10b357a130ecc4c1c8d3df88b91b8f2c417a8ad9d5388"
   end
 
   depends_on "openjdk"
