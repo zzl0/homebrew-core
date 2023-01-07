00ad0865d169ee86fe396128d6fee7d55287ddf5
Patrick Linnane <patrick@linnane.io>
2023-01-06 20:28:39 -0800

    libnice 0.1.21
    * libnice 0.1.20
    * libnice 0.1.21
    
    Closes #119986.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libnice.rb
+++ Formula/libnice.rb
@@ -1,8 +1,8 @@
 class Libnice < Formula
   desc "GLib ICE implementation"
   homepage "https://wiki.freedesktop.org/nice/"
-  url "https://nice.freedesktop.org/releases/libnice-0.1.19.tar.gz"
-  sha256 "6747af710998cf708a2e8ceef51cccd181373d94201dd4b8d40797a070ed47cc"
+  url "https://libnice.freedesktop.org/releases/libnice-0.1.21.tar.gz"
+  sha256 "72e73a2acf20f59093e21d5601606e405873503eb35f346fa621de23e99b3b39"
   license any_of: ["LGPL-2.1-only", "MPL-1.1"]
 
   livecheck do
