b9b47ccb3dcef3cc44c1e9c450d7fd3cd611f5a9
Porkepix <porkepix@gmail.com>
2023-01-07 12:41:48 +0100

    markdownlint-cli 0.33.0
    Closes #120004.
    
    Signed-off-by: Alexander Bayandin <alexander@bayandin.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/markdownlint-cli.rb
+++ Formula/markdownlint-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class MarkdownlintCli < Formula
   desc "CLI for Node.js style checker and lint tool for Markdown files"
   homepage "https://github.com/igorshubovych/markdownlint-cli"
-  url "https://registry.npmjs.org/markdownlint-cli/-/markdownlint-cli-0.32.2.tgz"
-  sha256 "0a0dc00260b09102f8111ce33833b7c797cea419b14f955e5a24d82bf179553a"
+  url "https://registry.npmjs.org/markdownlint-cli/-/markdownlint-cli-0.33.0.tgz"
+  sha256 "db3da4195fe53de3a696e728ffbb927c89b5997093aa1eb3ff711255ea86a42b"
   license "MIT"
 
   bottle do
