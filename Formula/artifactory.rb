ae832acea613d259aa302eba3204a09782042625
Michka Popoff <michkapopoff@gmail.com>
2023-01-08 17:48:04 +0100

    artifactory: deprecate (#120005)
    * artifactory: deprecate
    
    Artifactory 6 is depracted since April 2022
    
    Artifcatory 7 does not look it can easily built from source.
    The https://releases.jfrog.io/artifactory/bintray-artifactory/org/artifactory/oss/jfrog-artifactory-oss/7.10.2/jfrog-artifactory-oss-7.10.2-sources.tar.gz
    tar contains some build scripts that are specific to jfrogs CI/CD. It looks like they prefer to ship precompiled binaries,
    as already stated as a comment in the formula itself.
    
    I was not able to build artifcatory 7 from source.
    A previous attempt also failed: https://github.com/Homebrew/homebrew-core/pull/105222
    
    The download count is quite low  (72 in the last 30 days),
    so I think we can just redirect users to upstreams pre-compiled packages.
    
    Last but not least, no-one is shipping version 7:
    https://repology.org/projects/?search=artifactory
    
    except AUR that just ships the pre-compiled linux package.
    
    * artifactory: fix code style
    
    * artifactory: remove from autobump
    
    Co-authored-by: Rui Chen <rui@chenrui.dev>

--- Formula/artifactory.rb
+++ Formula/artifactory.rb
@@ -16,6 +16,8 @@ class Artifactory < Formula
     sha256 cellar: :any_skip_relocation, all: "39f95a12d5e609016171cb09106bdfd211f9ac910a9c1b1ac4241fb64e0fbc8c"
   end
 
+  deprecate! date: "2022-04-20", because: :deprecated_upstream
+
   depends_on "openjdk"
 
   def install
