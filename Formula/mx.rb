ef74defa71fe51b98798ea00fc99d9b82443404a
Rui Chen <rui@chenrui.dev>
2023-01-09 10:37:28 -0500

    mx 6.14.13
    Closes #120136.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/mx.rb
+++ Formula/mx.rb
@@ -1,8 +1,8 @@
 class Mx < Formula
   desc "Command-line tool used for the development of Graal projects"
   homepage "https://github.com/graalvm/mx"
-  url "https://github.com/graalvm/mx/archive/refs/tags/6.14.12.tar.gz"
-  sha256 "7039b50daa7d5de985a23c5b08eec2e2d372af71657a467940e0f5542a8b55f6"
+  url "https://github.com/graalvm/mx/archive/refs/tags/6.14.13.tar.gz"
+  sha256 "bcf3db74b0289c1adbfbabe1d28cddb991cde712ea80b25a01060ab0b36f383e"
   license "GPL-2.0-only"
 
   livecheck do
