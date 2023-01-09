65016e607b434fb7bfd2276cdeea503cfc1acaaf
Sam Ford <1584702+samford@users.noreply.github.com>
2023-01-09 16:24:13 -0500

    cloudflare-wrangler2 2.7.0
    Closes #120161.
    
    Signed-off-by: Michael Cho <20700669+cho-m@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cloudflare-wrangler2.rb
+++ Formula/cloudflare-wrangler2.rb
@@ -3,8 +3,8 @@ require "language/node"
 class CloudflareWrangler2 < Formula
   desc "CLI tool for Cloudflare Workers"
   homepage "https://github.com/cloudflare/wrangler2"
-  url "https://registry.npmjs.org/wrangler/-/wrangler-2.6.2.tgz"
-  sha256 "f2a5a803db8c5e8eaa2d75affc12330deb8cef33ec01c29bbc0ebdb3bb3b4985"
+  url "https://registry.npmjs.org/wrangler/-/wrangler-2.7.0.tgz"
+  sha256 "0f0c991e58d74405480e2b8b0ec1279f86476243107d262334b07b7d0861263a"
   license any_of: ["Apache-2.0", "MIT"]
 
   bottle do
@@ -21,17 +21,17 @@ class CloudflareWrangler2 < Formula
 
   def install
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
-    bin.install_symlink Dir["#{libexec}/bin/wrangler2"]
+    bin.install_symlink Dir["#{libexec}/bin/wrangler*"]
 
     # Replace universal binaries with their native slices
     deuniversalize_machos libexec/"lib/node_modules/wrangler/node_modules/fsevents/fsevents.node"
   end
 
   test do
-    system "#{bin}/wrangler2", "init", "--yes"
+    system "#{bin}/wrangler", "init", "--yes"
     assert_predicate testpath/"wrangler.toml", :exist?
     assert_match "wrangler", (testpath/"package.json").read
 
-    assert_match "dry-run: exiting now.", shell_output("#{bin}/wrangler2 publish --dry-run")
+    assert_match "dry-run: exiting now.", shell_output("#{bin}/wrangler publish --dry-run")
   end
 end
