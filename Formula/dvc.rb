ec638d7f8f94f2a26333f93a95f8090fcfe4171a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-09 08:05:55 +0000

    dvc: update 2.40.0 bottle.

--- Formula/dvc.rb
+++ Formula/dvc.rb
@@ -8,13 +8,13 @@ class Dvc < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "527a018f2986a34f7a2e56042c7f09a51cb6d832ae158656b45ed4cbeca9b39d"
-    sha256 cellar: :any,                 arm64_monterey: "8a67e9583d57bf9885455f92ed6f62c272cd541650744c12ca60425e7db9f452"
-    sha256 cellar: :any,                 arm64_big_sur:  "343bdfdff4f0d054286ac3accde5a0ce06173b5f36cf80c80e5a6ff3f07ef27a"
-    sha256 cellar: :any,                 ventura:        "90824a0bd95417434e60c8ceef27a72c852ef4b50523fce6531ea1930f378385"
-    sha256 cellar: :any,                 monterey:       "dfe2389daf8eb788502e617e249c688fd9d43198f13e822189a20081834ff265"
-    sha256 cellar: :any,                 big_sur:        "9ad3d73ba7a5d46e9e6451138a08e4dad903f00d619a0f23db5bb5999ef1af69"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bebabcd26ff95f05a3ed5823772e50ac607ef02f921945ba822e8e95fc413b90"
+    sha256 cellar: :any,                 arm64_ventura:  "5d28719f058e40e588bf489aab5689da04019ba458bb165a6ac2a28bfec7e27d"
+    sha256 cellar: :any,                 arm64_monterey: "4f34c8b223ffa7e482c67338ecce43cbf4738f821d2fb738a4102c5ffc9101ab"
+    sha256 cellar: :any,                 arm64_big_sur:  "ad2c93c6779a4c7561548a8f0e9a77fd385d01d3bdbff129f99868889a682839"
+    sha256 cellar: :any,                 ventura:        "fe628ae88671b25010364ba5fc0d3303545eb7dcb31478c384098138d39fbf70"
+    sha256 cellar: :any,                 monterey:       "c1d832475e3f974c69ffbe631ba168c0d2e781bd94abc242de400f7c918bc415"
+    sha256 cellar: :any,                 big_sur:        "9a4f89b2f05338f4da519803b7ad23a1fd1d85cd480176792f61cf83d4618e16"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "951547651f6b45fd31852b8f424a66bfc7fb16102d38738ef29e4efced94d006"
   end
 
   depends_on "openjdk" => :build # for hydra-core
