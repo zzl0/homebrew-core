619dd9e3725c6682716c57d4c85b50a3f2369ed5
Rui Chen <rui@chenrui.dev>
2023-01-09 10:56:42 -0500

    rure 1.7.1
    Closes #120138.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/rure.rb
+++ Formula/rure.rb
@@ -1,8 +1,8 @@
 class Rure < Formula
   desc "C API for RUst's REgex engine"
   homepage "https://github.com/rust-lang/regex/tree/HEAD/regex-capi"
-  url "https://github.com/rust-lang/regex/archive/1.7.0.tar.gz"
-  sha256 "9eb315d2874029e55397f5c4ff610215bc65d589679d72979378a94bcbc47586"
+  url "https://github.com/rust-lang/regex/archive/1.7.1.tar.gz"
+  sha256 "23b0ebce7aa113cb82115d19bea27ea120836e162523d1a9200341b436bf2843"
   license all_of: [
     "Unicode-TOU",
     any_of: ["Apache-2.0", "MIT"],
