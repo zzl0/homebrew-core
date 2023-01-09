2457385251b9fd2948f423c29c76c8f7266ffc8a
Patrick Linnane <patrick@linnane.io>
2023-01-08 23:20:15 -0800

    deployer 7.1.0
    Closes #120110.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/deployer.rb
+++ Formula/deployer.rb
@@ -2,10 +2,9 @@ class Deployer < Formula
   desc "Deployment tool written in PHP with support for popular frameworks"
   homepage "https://deployer.org/"
   # Bump to php 8.2 on the next release, if possible.
-  url "https://github.com/deployphp/deployer/releases/download/v7.0.2/deployer.phar"
-  sha256 "0dd3d3a4aac4b27338359843fc9f4f974b06276c2854b41b6fd54b0786473936"
+  url "https://github.com/deployphp/deployer/releases/download/v7.1.0/deployer.phar"
+  sha256 "22210f41f784798e56a49182d2be4ffc8f122d166b06f51d9a5e82c453e70b27"
   license "MIT"
-  revision 1
 
   bottle do
     rebuild 1
