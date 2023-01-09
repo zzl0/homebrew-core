6a75b9d7db8e4c55862b4fac7f89cb8f78335f33
Patrick Linnane <patrick@linnane.io>
2023-01-09 14:10:36 -0800

    plantuml 1.2023.0
    Closes #120172.
    
    Signed-off-by: Michael Cho <20700669+cho-m@users.noreply.github.com>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/plantuml.rb
+++ Formula/plantuml.rb
@@ -1,8 +1,8 @@
 class Plantuml < Formula
   desc "Draw UML diagrams"
   homepage "https://plantuml.com/"
-  url "https://github.com/plantuml/plantuml/releases/download/v1.2022.14/plantuml-1.2022.14.jar"
-  sha256 "159ca2021f2efa5481b56417e2abd0a301348b018fe3c34b106f41023596dd43"
+  url "https://github.com/plantuml/plantuml/releases/download/v1.2023.0/plantuml-1.2023.0.jar"
+  sha256 "0404edcf0af28e5b409bc17aa59ad8b05051f47347377749c46c8018135d0dec"
   license "GPL-3.0-or-later"
   version_scheme 1
 
