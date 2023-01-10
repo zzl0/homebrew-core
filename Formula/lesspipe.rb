7afd5e055234c33ae7f22cb787672f26bac51166
Porkepix <porkepix@gmail.com>
2023-01-10 17:02:16 +0100

    lesspipe 2.07
    * lesspipe 2.07
    * Update lesspipe.rb
    
    Closes #120221.
    
    Co-authored-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/lesspipe.rb
+++ Formula/lesspipe.rb
@@ -1,22 +1,16 @@
 class Lesspipe < Formula
   desc "Input filter for the pager less"
   homepage "https://www-zeuthen.desy.de/~friebel/unix/lesspipe.html"
-  url "https://github.com/wofr06/lesspipe/archive/v2.06.tar.gz"
-  sha256 "f46c7b0b06f951613535a480d22ba7e8563f4f08d3d3f8e370047df122cb1637"
+  url "https://github.com/wofr06/lesspipe/archive/v2.07.tar.gz"
+  sha256 "b6a591c053057c3968d0d1fbd32e4a0a8026cd5c27e861023e3542772eda1cba"
   license "GPL-2.0-only"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "1fea365d12621bd65e1df169e2355b4d91ab157081ca83bb8c9346ae799f5669"
   end
 
-  # build patch, remove in next release
-  patch do
-    url "https://github.com/wofr06/lesspipe/commit/8ebede2ee629fa7154dcb854d50b325ef9c54163.patch?full_index=1"
-    sha256 "a39ecba93b829d055973773746dd1447e38487169d89bb7161322066799c1534"
-  end
-
   def install
-    system "./configure", "--prefix=#{prefix}", "--yes"
+    system "./configure", "--prefix=#{prefix}"
     man1.mkpath
     system "make", "install"
   end
