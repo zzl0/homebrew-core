7e03f90723467a1120b3947968e12d7f78036a87
Patrick Linnane <patrick@linnane.io>
2023-01-10 09:44:50 -0800

    bzip3 1.2.2
    * bzip3 1.2.2
    * bzip3: remove build patch
    
    Closes #120229.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/bzip3.rb
+++ Formula/bzip3.rb
@@ -1,8 +1,8 @@
 class Bzip3 < Formula
   desc "Better and stronger spiritual successor to BZip2"
   homepage "https://github.com/kspalaiologos/bzip3"
-  url "https://github.com/kspalaiologos/bzip3/releases/download/1.2.1/bzip3-1.2.1.tar.gz"
-  sha256 "bba90d867f53efa4291de1df9c22da1578e1a93ca819e0fc68881da6fcc4149d"
+  url "https://github.com/kspalaiologos/bzip3/releases/download/1.2.2/bzip3-1.2.2.tar.gz"
+  sha256 "19e8d379f48610f945a04a988fd0c330ff6613b3df96405d56bed35a7d216dee"
   license "LGPL-3.0-only"
 
   bottle do
@@ -13,14 +13,6 @@ class Bzip3 < Formula
     sha256 cellar: :any,                 monterey:       "d6610408df172f22e746dab08602c07772c3bf76db11c1d98d85307a0565f0e5"
     sha256 cellar: :any,                 big_sur:        "9acf778c85c51dc05573eb120546e1e6dafd07d68a7b747a86096f18b964f889"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "652ff77bbe2107000ebfea60b7f37cdcf621e6693f4705b5d457d072ca500408"
-  end
-
-  # Fix -flat_namespace being used on Big Sur and later.
-  # upstream patch commit, https://github.com/kspalaiologos/bzip3/commit/e667cacfaa0b4b56e45d48e0496041c376c82d53
-  # remove in next release
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
-    sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
   end
 
   def install
