db996f5a50d908e5bc536fa469262da3cca3b4ca
Michael Cho <20700669+cho-m@users.noreply.github.com>
2023-01-10 03:01:09 +0000

    wownero: update 0.10.1.0_5 bottle.

--- Formula/wownero.rb
+++ Formula/wownero.rb
@@ -24,12 +24,13 @@ class Wownero < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_ventura:  "22a6f390fd78441fa6db3358c033412b4653edf0c8ade88e6b305024a0baff59"
-    sha256 cellar: :any, arm64_monterey: "534a027fbcb3a5c58aad7ec30e1a2a96ca54e8af692c3f1329e1ce8c7d66c8ad"
-    sha256 cellar: :any, arm64_big_sur:  "545e44ab1d59defcf8b8044f1a5584df7cb1f4483f19926d7bfa0a4f95000199"
-    sha256 cellar: :any, ventura:        "82f4bc6fba08587bbccd071734ddf9c30ce4b16467efe33ca87a8aec3f17c2a2"
-    sha256 cellar: :any, monterey:       "97ae2ff9de853bed0086a0a5864ae16abf5c349ad428f69faf43bb157395feff"
-    sha256 cellar: :any, big_sur:        "cd83f5871eca818eeeda63f6a253daa68cc04acd7df77665e849452c636a1c51"
+    sha256 cellar: :any,                 arm64_ventura:  "22a6f390fd78441fa6db3358c033412b4653edf0c8ade88e6b305024a0baff59"
+    sha256 cellar: :any,                 arm64_monterey: "534a027fbcb3a5c58aad7ec30e1a2a96ca54e8af692c3f1329e1ce8c7d66c8ad"
+    sha256 cellar: :any,                 arm64_big_sur:  "545e44ab1d59defcf8b8044f1a5584df7cb1f4483f19926d7bfa0a4f95000199"
+    sha256 cellar: :any,                 ventura:        "82f4bc6fba08587bbccd071734ddf9c30ce4b16467efe33ca87a8aec3f17c2a2"
+    sha256 cellar: :any,                 monterey:       "97ae2ff9de853bed0086a0a5864ae16abf5c349ad428f69faf43bb157395feff"
+    sha256 cellar: :any,                 big_sur:        "cd83f5871eca818eeeda63f6a253daa68cc04acd7df77665e849452c636a1c51"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "02c83be263f8e9e34be121d9bcab6715affd4cd4946414873219534b453ab7f9"
   end
 
   depends_on "cmake" => :build
