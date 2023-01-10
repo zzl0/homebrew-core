198797180e5bdb81ac05829e53cbb9204ce1dfaf
Jose Colella <jose.colella@gusto.com>
2023-01-09 17:25:20 -0800

    flagd 0.3.0 (new formula)
    * flag 0.3.0 (new formula)
    * change test
    * flagd: update code style
    
    Closes #120187.
    
    Signed-off-by: Jose Colella <jose.colella@gusto.com>
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/flagd.rb
@@ -0,0 +1,48 @@
+class Flagd < Formula
+  desc "Feature flag daemon with a Unix philosophy"
+  homepage "https://github.com/open-feature/flagd"
+  url "https://github.com/open-feature/flagd.git",
+      tag:      "v0.3.0",
+      revision: "2ad206e635cc5b68ae42287b740ddfc86b0194ff"
+  license "Apache-2.0"
+  head "https://github.com/open-feature/flagd.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPRIVATE"] = "buf.build/gen/go"
+    ldflags = %W[
+      -s -w
+      -X main.version=#{version}
+      -X main.commit=#{Utils.git_head}
+      -X main.date=#{time.iso8601}
+    ]
+
+    system "go", "build", *std_go_args(ldflags: ldflags)
+    generate_completions_from_executable(bin/"flagd", "completion")
+  end
+
+  test do
+    port = free_port
+
+    begin
+      pid = fork do
+        exec bin/"flagd", "start", "-f",
+            "https://raw.githubusercontent.com/open-feature/flagd/main/config/samples/example_flags.json",
+            "-p", port.to_s
+      end
+      sleep 3
+
+      resolve_boolean_command = <<-BASH
+        curl -X POST "localhost:#{port}/schema.v1.Service/ResolveBoolean" -d '{"flagKey":"myBoolFlag","context":{}}' -H "Content-Type: application/json"
+      BASH
+
+      expected_output = /true/
+
+      assert_match expected_output, shell_output(resolve_boolean_command)
+    ensure
+      Process.kill("TERM", pid)
+      Process.wait(pid)
+    end
+  end
+end
