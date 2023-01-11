671e4517940596e88cb6721e6824515821b1bc4f
Michael Cho <cho-m@tuta.io>
2023-01-11 15:41:45 -0800

    ruby@3.1: migrate to `openssl@3`

--- Formula/ruby@3.1.rb
+++ Formula/ruby@3.1.rb
@@ -4,6 +4,7 @@ class RubyAT31 < Formula
   url "https://cache.ruby-lang.org/pub/ruby/3.1/ruby-3.1.3.tar.gz"
   sha256 "5ea498a35f4cd15875200a52dde42b6eb179e1264e17d78732c3a57cd1c6ab9e"
   license "Ruby"
+  revision 1
 
   livecheck do
     url "https://www.ruby-lang.org/en/downloads/"
@@ -24,7 +25,7 @@ class RubyAT31 < Formula
 
   depends_on "pkg-config" => :build
   depends_on "libyaml"
-  depends_on "openssl@1.1"
+  depends_on "openssl@3"
   depends_on "readline"
 
   uses_from_macos "libffi"
@@ -56,7 +57,7 @@ class RubyAT31 < Formula
     #       https://github.com/Homebrew/brew/pull/12508
     inreplace "tool/mkconfig.rb", /^(\s+val = )'"\$\(SDKROOT\)"'\+/, "\\1"
 
-    paths = %w[libyaml openssl@1.1 readline].map { |f| Formula[f].opt_prefix }
+    paths = %w[libyaml openssl@3 readline].map { |f| Formula[f].opt_prefix }
     args = %W[
       --prefix=#{prefix}
       --enable-shared
