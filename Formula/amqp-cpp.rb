6c75f093c7a62db10a8d3d0f59271cc5097c4d13
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-11 20:04:33 +0000

    amqp-cpp: update 4.3.19 bottle.

--- Formula/amqp-cpp.rb
+++ Formula/amqp-cpp.rb
@@ -12,15 +12,13 @@ class AmqpCpp < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_ventura:  "827e422fe2c209e48d781dd11881ddb11ecabda55f06ba1095019f7e667b5d35"
-    sha256 cellar: :any,                 arm64_monterey: "d6823017b0bbbddc0490d68c7a73beaa580b22bab360784baf60a4ff0d9046fb"
-    sha256 cellar: :any,                 arm64_big_sur:  "ae9cf4ec980c8cc699ebb1c70a785139d26759e703324ad1df8151c90aa18461"
-    sha256 cellar: :any,                 ventura:        "24ed76c164ad89e3a24e60909a85814c1ee446fec47b800bec2b41c400deb440"
-    sha256 cellar: :any,                 monterey:       "32e68fbfaeee7409a6b7e8fe4dd742d4589762cb601e0800e235acb2c326e3a4"
-    sha256 cellar: :any,                 big_sur:        "f0222f1ff29774ba9e39d11c92797f465cb066529cc7255e5c65f508e3ca3d46"
-    sha256 cellar: :any,                 catalina:       "7b8abc464785804e5ee792e480baef60e565d4567bb6b330230ccd99e5e63473"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e4b54f0272c3d9e660443bec29e24b785a9987835bd8c7fff4e6b6c7efaa17dc"
+    sha256 cellar: :any,                 arm64_ventura:  "43d468a1385290e82f248bad31d0655e3ea395ff47ef47d2fc389a43b34d61d7"
+    sha256 cellar: :any,                 arm64_monterey: "72a92e24bb075265592b021c8c39ee6fdc1cf0d8b9fbfaec068666cae59a1c1b"
+    sha256 cellar: :any,                 arm64_big_sur:  "82c4faf577c73febc31d29a72e6ba0078379ad3c8974eba34e90959083fa77af"
+    sha256 cellar: :any,                 ventura:        "2eafeb6699ff9a3d5aaec844fd0016b4e4f560b30c2e93fd9c20dd522da7e5ab"
+    sha256 cellar: :any,                 monterey:       "3e45a055f43706e3bcc7d3f31b16bee4cecfe4b0a8c8e1952ae15db6d8748cbf"
+    sha256 cellar: :any,                 big_sur:        "6742b624c72ddb1bd2353bb3ab9d15f533085f7170926ffd68d5ccbba64084b4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0518aa063464db3125d602010074798d0fecacb75344a329bcb8f858a8b4198e"
   end
 
   depends_on "cmake" => :build
