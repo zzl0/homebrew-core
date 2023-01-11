b6fe482eb80c9b6edef3ee8e9aee26e15b2cf363
Patrick Linnane <patrick@linnane.io>
2023-01-11 12:39:19 -0800

    epubcheck 5.0.0
    Closes #120371.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/epubcheck.rb
+++ Formula/epubcheck.rb
@@ -1,8 +1,8 @@
 class Epubcheck < Formula
   desc "Validate EPUB files, version 2.0 and later"
   homepage "https://github.com/w3c/epubcheck"
-  url "https://github.com/w3c/epubcheck/releases/download/v4.2.6/epubcheck-4.2.6.zip"
-  sha256 "3f73c1265cc92e3b53ffda6cd5bbeb58505b2b0c43411c26e74d8df1b193b2c0"
+  url "https://github.com/w3c/epubcheck/releases/download/v5.0.0/epubcheck-5.0.0.zip"
+  sha256 "98c5ecce0a6a6bf37034f73465613c4088916b3ef3489f50a7f6897a37a9725a"
   license "BSD-3-Clause"
 
   bottle do
