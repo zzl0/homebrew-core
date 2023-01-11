e2f406bb7110923b34c4fe75f2d682321b8532ab
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-11 04:05:02 +0000

    gtksourceview5: update 5.6.2 bottle.

--- Formula/gtksourceview5.rb
+++ Formula/gtksourceview5.rb
@@ -11,14 +11,13 @@ class Gtksourceview5 < Formula
   end
 
   bottle do
-    sha256 arm64_ventura:  "8500a38bde8f2362eba345a641ef494181ca8f9ca9d371d8c3eb5390cb3e447c"
-    sha256 arm64_monterey: "95ecf13f25c7b6dba6ce922afea5933b2932d0bc099a69a7c11fff678afa3a53"
-    sha256 arm64_big_sur:  "e89bff5608ef32a1e086e088a952f4cad107ea93889b8d50300c4585743bd021"
-    sha256 ventura:        "f8d9d594914e7ad2d94fb44d99a530dcb252e1a02d93dd19a09207dc90a75b89"
-    sha256 monterey:       "9ce97c46b9e259a2650e6afad120346ce35b979d781a57352134a877fc8d386e"
-    sha256 big_sur:        "326a12dd4ed21e70aa71a5e56c85fd10d0bb88f18f5a550037a958b0ad6af293"
-    sha256 catalina:       "b9e031a18b4e9ee7cf66a18f241cb0f589da07639ccc0ee4874b3fb12c32440f"
-    sha256 x86_64_linux:   "c8ea21321b5ea9dc065b15eae4d2fd955194146383b14a4640795d6743ce541c"
+    sha256 arm64_ventura:  "b4a05aca3094980009eb6b8baa31cf78ed70bc04d028a28bdf19bf02cb1c1c22"
+    sha256 arm64_monterey: "415fa593676498627c5d079bd4071169c4b0efe8f4aeaf4f19aec9bba765a433"
+    sha256 arm64_big_sur:  "9855c6bd1607fb74ad27750ec0703e2c341906486eed08692b38915ae54c560c"
+    sha256 ventura:        "d467af830c910703728a374a5bc8a81681c66b8f14c7202a1fd10bbd11860e16"
+    sha256 monterey:       "4535fb5d6950fa716efd0d2b8cd86fb207f14c75bb302697988f0ccfad2783e2"
+    sha256 big_sur:        "cca04854d2481bee15975cc84547057dd18088d605943c84bbfc2e38afee0f6e"
+    sha256 x86_64_linux:   "52ab5d38daec7b0e7d08518dc0bf6718f4d9d963fdc5d7bbafdc8e808c61238e"
   end
 
   depends_on "gobject-introspection" => :build
