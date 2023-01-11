fe4e422ea3676e51eeefeca1a9a23f66bb91e37b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-11 02:26:49 +0000

    i2p: update 2.1.0 bottle.

--- Formula/i2p.rb
+++ Formula/i2p.rb
@@ -11,14 +11,13 @@ class I2p < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "45a65eb4b71fb6ed5df5b186d8f0a5e48ba4056df946e3b03b19ab21914f6c26"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "76d4d5f640e34d9aa5dae8fd4fce1510306b4211c86c46a7c9dd540e4bf5f3bb"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "74650f2d87ef6817bf8e615f820c73f3fcaacdcb23bba6c462bfb98778993c92"
-    sha256 cellar: :any_skip_relocation, ventura:        "daf8069447b4c5970b5cfd1f566083636fb1154d7d582d1ac7ca6f4ec9f3dd22"
-    sha256 cellar: :any_skip_relocation, monterey:       "9869a854557471178ceaddb9525ea4f0d26471475b267a8aa2e1385a3e2757ee"
-    sha256 cellar: :any_skip_relocation, big_sur:        "9a31977f6a664aeeea23ec2de5058a9df1547894657b73b08ae99d39804a99a6"
-    sha256 cellar: :any_skip_relocation, catalina:       "5018081f9e3b21e86cfb41d18c2cf0b9d9a93b71126fa369f35f9af35c8b6d8c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4480a759a0f64a3ff7d7433696b065e7d599633c3bb3f15c99692bf29a519b7b"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "03a77dcbb2e35347e8a9a6d3157ac37ed7dfeae7e0f62068b9435f6aab28d128"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f1b367590785d89af4f0fa4a69bdee2938e86e686709e8376ac58b410180ada2"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7edb7d6b0efd6e445862d6d8342eee66528ecec22a4c3009d3517e1b1d33885a"
+    sha256 cellar: :any_skip_relocation, ventura:        "e9b98da624ed7e397d0af02433f90065c877ed88fddba6024ee3241a47fa7275"
+    sha256 cellar: :any_skip_relocation, monterey:       "5fdff3db94d31afbb9de04844f275fd8f8492833029db8ad2f3751be9ba9e64a"
+    sha256 cellar: :any_skip_relocation, big_sur:        "cc076979cd3d39d63eec64a165032fbebb37cbb703c818305e75a8e2f047a599"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "717b8e132b375d4849353d8228169c099508de3df9811835d8ddc509942bda7f"
   end
 
   depends_on "ant" => :build
