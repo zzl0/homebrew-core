fd20b0538c0d7662722cea2600111f97044547b0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-12 00:14:01 +0000

    gloox: update 1.0.24 bottle.

--- Formula/gloox.rb
+++ Formula/gloox.rb
@@ -11,16 +11,14 @@ class Gloox < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "4d48f51b26619a85c9e5a937f58b076eb3e14caec7882956457a94ecfe7b0a35"
-    sha256 cellar: :any,                 arm64_monterey: "7ec91986b31b93a0271334d280e1838d14bab2af7d62f813f20e7c0df5f4e564"
-    sha256 cellar: :any,                 arm64_big_sur:  "d3a7c4aa4b061fe4dce520662e632bc9983b294cb94e479ea46221f43ea46c9f"
-    sha256 cellar: :any,                 ventura:        "1802ffa3991a20e4c6f4adccc8750577afc47e9a4d3aaa35462eacb4ce082fb6"
-    sha256 cellar: :any,                 monterey:       "fbd2eedb0aa00300684582f8cf54f071e84c05e917ea2360ee29e17d8c767523"
-    sha256 cellar: :any,                 big_sur:        "143b0a77842e9df885799b07e6cfb166c7951841b8b23027db51c9b93a5ba8a7"
-    sha256 cellar: :any,                 catalina:       "decf46b20a794855cf3bd3c06e05111d15fd11de4dec1c5fdf6a1253eb865e7a"
-    sha256 cellar: :any,                 mojave:         "607baeeadc43775af6799d5bc4715239cbe6455ec72d2e14d82523d425fa7799"
-    sha256 cellar: :any,                 high_sierra:    "011c8a88d0f8970c9ad4ed339972b55b56c26120e64ef9d1576b68c03b10f706"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "20accd40e451cec9b2bf58e94be2ff78af6db0d719ec905fa5e2123049e63267"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_ventura:  "46eab221247ef01010c6b159feb6e2f0fc1355c53d1aaa3cbf144c3e5b825f61"
+    sha256 cellar: :any,                 arm64_monterey: "5c9ba1c56bf72838684beaf32e452e20bb7e003151e69d7e2beb9ed06d57af3b"
+    sha256 cellar: :any,                 arm64_big_sur:  "3a074b66d0c8b73f75842fcb350da30d5c1dfa4babc0f63134d01156c577c3eb"
+    sha256 cellar: :any,                 ventura:        "704e1f7fcbfeea920fd04cc51b4e0a986ef51874c352cd66e060ed1fe5b57243"
+    sha256 cellar: :any,                 monterey:       "6507e5a77b70a01cfb5d86a7f4ec8bd71755c33bd533b3f726ba293c492155a2"
+    sha256 cellar: :any,                 big_sur:        "ff6c2d2cc04577792e75b187f62c6243eeefe6dc715b0d3ebc5d1397dd557861"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6a520a0278c099ab8d34b6a01354846be465efba2a04e0463c9b7b9baaadb027"
   end
 
   depends_on "pkg-config" => :build
