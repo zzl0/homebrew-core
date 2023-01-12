2a2d50a4a4b7b4429926f8226bafc9f37aaa40c9
Muneeb Ahmed <muneebmahmed@gmail.com>
2023-01-12 00:28:08 -0800

    lesspipe: revision
    * lesspipe: fix macOS build
    * lesspipe: Use upstream patch
    
    Closes #120432.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/lesspipe.rb
+++ Formula/lesspipe.rb
@@ -4,6 +4,7 @@ class Lesspipe < Formula
   url "https://github.com/wofr06/lesspipe/archive/v2.07.tar.gz"
   sha256 "b6a591c053057c3968d0d1fbd32e4a0a8026cd5c27e861023e3542772eda1cba"
   license "GPL-2.0-only"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "eab7ed95d12d5b9ccf4f2600e41ec76f771cf6efbce668e1be80497366029a1f"
@@ -13,6 +14,12 @@ class Lesspipe < Formula
     sha256 cellar: :any_skip_relocation, monterey:       "eab7ed95d12d5b9ccf4f2600e41ec76f771cf6efbce668e1be80497366029a1f"
     sha256 cellar: :any_skip_relocation, big_sur:        "eab7ed95d12d5b9ccf4f2600e41ec76f771cf6efbce668e1be80497366029a1f"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "5569b10632037ead5f2f19b5c8b6328bbbac744dfe523da1cadfbd760ebe22ab"
+  end
+
+  # patch for runtime error, remove in next release
+  patch do
+    url "https://github.com/wofr06/lesspipe/commit/ff6ecf9671a417ee85218a99c47a93ce2c0388be.patch?full_index=1"
+    sha256 "4204136f2e1ad0fa8a9b1f42b192ce422799860d073663130e77eefa107260ca"
   end
 
   def install
