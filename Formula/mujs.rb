1a0548d5393f0271e52d8dcc4936cc76072eeca2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-12 02:25:00 +0000

    mujs: update 1.3.2 bottle.

--- Formula/mujs.rb
+++ Formula/mujs.rb
@@ -9,14 +9,13 @@ class Mujs < Formula
   head "https://github.com/ccxvii/mujs.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "8717abdb989f9eaa115bbe5afac344fc517712fc825b7756c5362d9a6ec057d7"
-    sha256 cellar: :any,                 arm64_monterey: "2cc98920066db183fbc6c024a676254dfb46628d3ba2e41a36cd94e35016c23a"
-    sha256 cellar: :any,                 arm64_big_sur:  "49a0fa9258cb985ffc33ec3317e21ca7ce250a8e6ece38e1b65f21821410baa6"
-    sha256 cellar: :any,                 ventura:        "5d06e568377be944bdf54127af5e35ebd36d9368cb38921ec867512060465d57"
-    sha256 cellar: :any,                 monterey:       "70e32a32fc2f81d22e499516e97dc771c93e57c88058f59b5a7f9b7e0ce65daa"
-    sha256 cellar: :any,                 big_sur:        "95c9549b0f7969d7ebec612e2d8c35bb5c2fe5a76ab353264b034abe32907eed"
-    sha256 cellar: :any,                 catalina:       "4854410e56af3f104f3cb99efd5657b4a23d40fcda702c7f59701197775eb5bd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a414797c314cba62749d4a862dba487d6033e07fba8f04f205962a8e4e46508c"
+    sha256 cellar: :any,                 arm64_ventura:  "67b0f0b5da7f7068bc5ceaae7cb2057d7523aadecf4b92da8eb967ae7bd049d1"
+    sha256 cellar: :any,                 arm64_monterey: "2c27809e1c3b82eac6cad849b735d9da958ca6b235fa8f6bb777a74759d106a0"
+    sha256 cellar: :any,                 arm64_big_sur:  "d5877bcfd36acbd4a40ae2e225fb13a5af73c78147c2f8b260a8f3b4432f9427"
+    sha256 cellar: :any,                 ventura:        "35b4f3d06f7e7efa10ebb992e5c88c343f225eb86a4b7abf5b848ef707d81f36"
+    sha256 cellar: :any,                 monterey:       "008c3cef8cf1aca12fbfe055a82be756e3bf79b7bdaebb9b999b2d92c83f1979"
+    sha256 cellar: :any,                 big_sur:        "e9f7444b3d9590a01d1e214c4b2e7df388b4c7cbfe52cbbaff42b0b693a834b3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3accf35f6e3756676992e88f1e4e5600829cf99f59dd149379a3baf6e6b140f1"
   end
 
   on_linux do
