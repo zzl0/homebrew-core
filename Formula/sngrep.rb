07df0574650a7240eecba87832780a657d291d74
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-12 17:12:56 +0000

    sngrep: update 1.6.0 bottle.

--- Formula/sngrep.rb
+++ Formula/sngrep.rb
@@ -6,14 +6,14 @@ class Sngrep < Formula
   license "GPL-3.0-or-later" => { with: "openvpn-openssl-exception" }
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "0b7aa728a1094a767d83ce9e21be2cbe9815485d86d9aa0ceca8f5eeabe8ba77"
-    sha256 cellar: :any,                 arm64_monterey: "2bbb0e2441c8192002e427ba0b5a3e8673cee8689fd5befebd0f3ba0532e3cb2"
-    sha256 cellar: :any,                 arm64_big_sur:  "69a7368edfbff2b60ed13cf1dbd9891ca8346bc4764108441c48e28a27ff08cc"
-    sha256                               ventura:        "8be2f7a825986f7d7819b0d3aaa0881a5c636e7bb50ad1dff87bd6ea2cf39941"
-    sha256                               monterey:       "c0d3b02aa03ce6c44e7d0a14e06d34bea256934504fce358c0ad94308b4e410f"
-    sha256                               big_sur:        "d02e2bb89ebe085ba45d1bf9ccd3da89b4bb2b79dd69942bf56a1b3e70acfad4"
-    sha256                               catalina:       "f66d29b77e3a75b7824eccba63ee8e7c4d97e684afbb7cb1dccf79ce5ca8a273"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f787893debb7a1c5e9bfc7dd4d951f7806cfd6d0c9a04f53579f421c5f5bebc5"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_ventura:  "791eed53070a6547cb7edae0bfc4d874538812183b9b11759ba050b5ee2eb4e3"
+    sha256 cellar: :any,                 arm64_monterey: "090b38f6af7d6768d3b8816186ce8db5f1678f8576513f63bfece2664ed521ea"
+    sha256 cellar: :any,                 arm64_big_sur:  "84c553de370ed38fc5775b108fb4aef65da63234c3a2f983bf32f32904926d48"
+    sha256                               ventura:        "7b01851336139959d3523cffd9aca8981529fc8cfaa0eff27d9a56264ef8a526"
+    sha256                               monterey:       "4d695f06ed5a14c400a0fc2415e84827de36d62f3d4889af7c71447ca7d085b8"
+    sha256                               big_sur:        "f8f72fe53910b2ee8557f3e5e954fa759fad39aa3b8ce171b4673e0b6f8eb6a4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8f1379a3381f77310dbdba9239c8e05678b69239dcaf495fb8f96940366fdfc3"
   end
 
   depends_on "autoconf" => :build
