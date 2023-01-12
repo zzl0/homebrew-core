fcc3860026417f14ec2f545d539a41fb857b2bd7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-12 19:04:36 +0000

    micronaut: update 3.8.1 bottle.

--- Formula/micronaut.rb
+++ Formula/micronaut.rb
@@ -11,13 +11,13 @@ class Micronaut < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "6e3b8e51f21b5b26b0dc63525c2cf7d78e60d24b0eed180a2e368f63d4d9715e"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "59e829f8a58c14106ddfc4827e28e6985f0a8ff2706db7cf2a0d383ae5b72453"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "eb851970f452a7c1f0a2daccb71775a3d5e552e29551b783476eb49699412c96"
-    sha256 cellar: :any_skip_relocation, ventura:        "4da4872710ca3eaa67dd8f14a8d72278e5714195c0ccb0d585309935d704e3f1"
-    sha256 cellar: :any_skip_relocation, monterey:       "510c1b08e8244ba3dad463f4cbf356e7bf13421d3455b3034b37fbce987150ff"
-    sha256 cellar: :any_skip_relocation, big_sur:        "469728afb35343c3daa47a7a6865e323164367f44d4d83961b963c8051fb4ca9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "00edbcf80005dff1b6a2754bee35ab61a2b7a58b17e92aafdbcfea19b7909cc0"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "1e34dd3a228d2d2b506b4575be0972e6c37e7255d92bae8289f524cc27e03009"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "28831d18db13946ba8f0f481f5e464dd74639ee913635e68ec20b9bb2bf691d4"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5270719895c85da460c8716713e8940c03cb607971d8396bc30c2400f8d02150"
+    sha256 cellar: :any_skip_relocation, ventura:        "3b7b3107b0268373d230187f2e4250c7a8cb42a7a4ca90201c3c9b137aa7313c"
+    sha256 cellar: :any_skip_relocation, monterey:       "001a2a14b3cbe303676f4b387775a13a59a221a693dcc82f30df260afa27dbeb"
+    sha256 cellar: :any_skip_relocation, big_sur:        "21d5db0a531646dbaea96de43ed32c05510ec30a953d5e801fb10985711f28b5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f62eb2e86b5fd9a6b4562b2063cbc0a868ad5db64107ed97e07dea40224e90e5"
   end
 
   # Uses a hardcoded list of supported JDKs. Try switching to `openjdk` on update.
