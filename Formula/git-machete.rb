1a99a0dc3df46c26b9dc773e6d0a0f8a6e0c166c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-13 16:04:48 +0000

    git-machete: update 3.14.1 bottle.

--- Formula/git-machete.rb
+++ Formula/git-machete.rb
@@ -8,13 +8,13 @@ class GitMachete < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "4049f84021b6414a2f2c97accefd3432d413d3fa169c6712eb5690f3177a111b"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "4049f84021b6414a2f2c97accefd3432d413d3fa169c6712eb5690f3177a111b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4049f84021b6414a2f2c97accefd3432d413d3fa169c6712eb5690f3177a111b"
-    sha256 cellar: :any_skip_relocation, ventura:        "72d3e9ba47c19d487b7899e111902476f993ee1d498d52fa32f51e6509104d12"
-    sha256 cellar: :any_skip_relocation, monterey:       "72d3e9ba47c19d487b7899e111902476f993ee1d498d52fa32f51e6509104d12"
-    sha256 cellar: :any_skip_relocation, big_sur:        "72d3e9ba47c19d487b7899e111902476f993ee1d498d52fa32f51e6509104d12"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "908538deb8bde139c94ac1b873b779bbad15bede9a2bdf62938eef012e0d626c"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "468c6cad158a021c312630b8f3e9fdd281f90a9c73f6ea4b62971c9aab4d5eb6"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "468c6cad158a021c312630b8f3e9fdd281f90a9c73f6ea4b62971c9aab4d5eb6"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "468c6cad158a021c312630b8f3e9fdd281f90a9c73f6ea4b62971c9aab4d5eb6"
+    sha256 cellar: :any_skip_relocation, ventura:        "bd45a2ed150e3d6de27b6e29e087e6f78e65f5bd5eea5ac7291f2b7932b44ed7"
+    sha256 cellar: :any_skip_relocation, monterey:       "bd45a2ed150e3d6de27b6e29e087e6f78e65f5bd5eea5ac7291f2b7932b44ed7"
+    sha256 cellar: :any_skip_relocation, big_sur:        "bd45a2ed150e3d6de27b6e29e087e6f78e65f5bd5eea5ac7291f2b7932b44ed7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3a964853776c4534078a080c6eca9618abf0d4020766e92ef9d1cb0549aebba4"
   end
 
   depends_on "python@3.11"
