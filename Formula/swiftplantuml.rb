f6901f2391255c41836649cad68e064a89fd1f05
Marco Eidinger <eidingermarco@gmail.com>
2023-01-13 14:51:32 -0800

    swiftplantuml 0.7.1
    Closes #120561.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/swiftplantuml.rb
+++ Formula/swiftplantuml.rb
@@ -1,8 +1,8 @@
 class Swiftplantuml < Formula
   desc "Generate UML class diagrams from Swift sources"
   homepage "https://github.com/MarcoEidinger/SwiftPlantUML"
-  url "https://github.com/MarcoEidinger/SwiftPlantUML/archive/0.7.0.tar.gz"
-  sha256 "4c6745364c69efcfeaca9dda0a37d0671aae26ff6298d24b237fc0ebefc5dd0c"
+  url "https://github.com/MarcoEidinger/SwiftPlantUML/archive/0.7.1.tar.gz"
+  sha256 "4b4725d0bdd26bd12c1bb4fe1e101b1d75921208bf0a97aff6d5936fed5967bc"
   license "MIT"
 
   bottle do
