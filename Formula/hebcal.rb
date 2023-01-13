3fad09bfa257f19bebc1908227e7f0b9da2c96f1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-13 20:04:42 +0000

    hebcal: update 5.7.2 bottle.

--- Formula/hebcal.rb
+++ Formula/hebcal.rb
@@ -7,13 +7,13 @@ class Hebcal < Formula
   head "https://github.com/hebcal/hebcal.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "2de63fe840bda94bec04c3b7c03ce39754a01db4cca2e8bdc42b20c46082708f"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "2de63fe840bda94bec04c3b7c03ce39754a01db4cca2e8bdc42b20c46082708f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2de63fe840bda94bec04c3b7c03ce39754a01db4cca2e8bdc42b20c46082708f"
-    sha256 cellar: :any_skip_relocation, ventura:        "2f1b1c71f887e6b57985daff0711aba66317cdb219785ffd90defc27857bff20"
-    sha256 cellar: :any_skip_relocation, monterey:       "2f1b1c71f887e6b57985daff0711aba66317cdb219785ffd90defc27857bff20"
-    sha256 cellar: :any_skip_relocation, big_sur:        "2f1b1c71f887e6b57985daff0711aba66317cdb219785ffd90defc27857bff20"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "02537284c0ae770ca57d6c6316da736156c1c121ab2a79f52df8c4959d252325"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "d0be2b41479110058201b536199ac41e4e122484f2647c344caeed295e6fc287"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "d0be2b41479110058201b536199ac41e4e122484f2647c344caeed295e6fc287"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d0be2b41479110058201b536199ac41e4e122484f2647c344caeed295e6fc287"
+    sha256 cellar: :any_skip_relocation, ventura:        "20dc3b7c5cdade5a3a7685463c5d40b841f55e3aa0b08bf142a5ba9a3deb2372"
+    sha256 cellar: :any_skip_relocation, monterey:       "20dc3b7c5cdade5a3a7685463c5d40b841f55e3aa0b08bf142a5ba9a3deb2372"
+    sha256 cellar: :any_skip_relocation, big_sur:        "20dc3b7c5cdade5a3a7685463c5d40b841f55e3aa0b08bf142a5ba9a3deb2372"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8161256ee94a9d41bf315b2394b44710039f7cc3524079c803c7591e1836492a"
   end
 
   depends_on "go" => :build
