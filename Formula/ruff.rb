09a8e12639d5b148c0663d9b9f341feb92cf8c42
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-13 04:04:35 +0000

    ruff: update 0.0.220 bottle.

--- Formula/ruff.rb
+++ Formula/ruff.rb
@@ -8,13 +8,13 @@ class Ruff < Formula
   head "https://github.com/charliermarsh/ruff.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "cac51b2ee4c736d4c49fa981e809ec4e18e4c421a913ca7b59f12ed723e0fbaf"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "949174270b697f9da3fe62e0feee39e19711d4a16b6ef1ab3e20e1ce99364fd7"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1777148ab3c1ebcc7ccf6d843c7c7654aa4dc6a952ecc39909c4a3ec0d524354"
-    sha256 cellar: :any_skip_relocation, ventura:        "f97977b0120a6f58a6d466577ee03618c8b28e7c80f830332632588e1773d5c9"
-    sha256 cellar: :any_skip_relocation, monterey:       "b5fd5f0a51f63ca7383296e89c7f6fdc7e43f7b55f3b2721a10d60f39a9bb937"
-    sha256 cellar: :any_skip_relocation, big_sur:        "4b223400fbac798c7b37950a620e0b2e46718f8c53c7e5b28ddf59dff2e8294e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "648531ce746bfa21ff74674edff1b08d4db240743dfe09e835667e6e56d20921"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "04848f426282927b1435355089d687718a6a6271f6c3e15b231678cac3429c65"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f9b92f0a6b1ae52ddf6ab3474201e4feb46da816980fb7d140eab7fba2165407"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "94a1548b925ee7380ba51035782b40d9c875025f373aa46dd519ab3fdebca3d9"
+    sha256 cellar: :any_skip_relocation, ventura:        "3f1c55c1ce1b02611764fab291e7608aa8971e54a00ef6da53ec9d8d03202dc5"
+    sha256 cellar: :any_skip_relocation, monterey:       "1e1b5e8523ca10c34c51fccc9a4f8c92f613ac8bc8c8e161625181cac0ca3347"
+    sha256 cellar: :any_skip_relocation, big_sur:        "789d5943c38760ade6f4006b3246e7ec353a5ad339564ed2b379b2825d0ebb18"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e058f384022b09225771c2ef3c670929f3d80fe48e419737d8cc04068e6d651e"
   end
 
   depends_on "rust" => :build
