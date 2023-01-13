e48efaef98a95b17709fdbad6c25ae1ea76cb54c
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-01-14 01:10:38 +0800

    tomcat@9 9.0.71
    Closes #120539.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tomcat@9.rb
+++ Formula/tomcat@9.rb
@@ -1,9 +1,9 @@
 class TomcatAT9 < Formula
   desc "Implementation of Java Servlet and JavaServer Pages"
   homepage "https://tomcat.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=tomcat/tomcat-9/v9.0.70/bin/apache-tomcat-9.0.70.tar.gz"
-  mirror "https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.70/bin/apache-tomcat-9.0.70.tar.gz"
-  sha256 "38bc58342dccc3b0b2c238f55ff4f579a0a550758be940492e47e3fa622fb3a4"
+  url "https://www.apache.org/dyn/closer.lua?path=tomcat/tomcat-9/v9.0.71/bin/apache-tomcat-9.0.71.tar.gz"
+  mirror "https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.71/bin/apache-tomcat-9.0.71.tar.gz"
+  sha256 "d78a3a09a67695d4f391d2e40713eeeb437bb201ad4571d200f9d23cc7868a78"
   license "Apache-2.0"
 
   livecheck do
