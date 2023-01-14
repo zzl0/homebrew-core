cf53d4a09d8691e6afc353426d777802b6695fcd
Patrick Linnane <patrick@linnane.io>
2023-01-13 22:29:48 -0800

    bmake 20230101
    Closes #120584.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/bmake.rb
+++ Formula/bmake.rb
@@ -1,8 +1,8 @@
 class Bmake < Formula
   desc "Portable version of NetBSD make(1)"
   homepage "https://www.crufty.net/help/sjg/bmake.html"
-  url "https://www.crufty.net/ftp/pub/sjg/bmake-20220928.tar.gz"
-  sha256 "c804b77de3feb8e31dee2a4c9fb1e9ec24c5a34764e7a2815c88b4ed01650e90"
+  url "https://www.crufty.net/ftp/pub/sjg/bmake-20230101.tar.gz"
+  sha256 "0150300a8c644b876eee57822a872cd84f060983c9006643ead0e75cedc3e904"
   license "BSD-3-Clause"
 
   livecheck do
