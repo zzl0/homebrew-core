d56344ee943b69b71190512b5e9f2435aefb3bef
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-14 01:31:07 +0000

    editorconfig-checker: update 2.7.0 bottle.

--- Formula/editorconfig-checker.rb
+++ Formula/editorconfig-checker.rb
@@ -7,14 +7,13 @@ class EditorconfigChecker < Formula
   head "https://github.com/editorconfig-checker/editorconfig-checker.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "385c4709db6ab2a7d38f9f022ac51bb60787478ddb2409deba9e03430f6d4587"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "07aaae3448d74c1135ff5b3e35f8cd8bedd8f0b3727c6b6c7a3b37c5cc669a5f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "07aaae3448d74c1135ff5b3e35f8cd8bedd8f0b3727c6b6c7a3b37c5cc669a5f"
-    sha256 cellar: :any_skip_relocation, ventura:        "42584005a18f4a1af03ae436b17ce742c43ccf1e5114c21b6cca0c19027e63b3"
-    sha256 cellar: :any_skip_relocation, monterey:       "a1e76c97a829f2b4bb869c90b7cb52ba89163c97b06910e9b060c9478d5f0a65"
-    sha256 cellar: :any_skip_relocation, big_sur:        "a1e76c97a829f2b4bb869c90b7cb52ba89163c97b06910e9b060c9478d5f0a65"
-    sha256 cellar: :any_skip_relocation, catalina:       "a1e76c97a829f2b4bb869c90b7cb52ba89163c97b06910e9b060c9478d5f0a65"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "af56963c31d38a4203d8c2bc581e23b0691b4ec81e5792a69b07de80369c1c12"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8c3ff8bba72c42bc77d381d3273b2219f4bb060f14c826bad8e2a6c054e1ceae"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "8c3ff8bba72c42bc77d381d3273b2219f4bb060f14c826bad8e2a6c054e1ceae"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8c3ff8bba72c42bc77d381d3273b2219f4bb060f14c826bad8e2a6c054e1ceae"
+    sha256 cellar: :any_skip_relocation, ventura:        "34872cd61b879386460265318386355db50e30002cd2c3ab81c632485cf25bd9"
+    sha256 cellar: :any_skip_relocation, monterey:       "34872cd61b879386460265318386355db50e30002cd2c3ab81c632485cf25bd9"
+    sha256 cellar: :any_skip_relocation, big_sur:        "34872cd61b879386460265318386355db50e30002cd2c3ab81c632485cf25bd9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bcdae0ba7faf0330f8f46663b72e15aedcacaa4bfaac97252e574c1909c9109a"
   end
 
   depends_on "go" => :build
