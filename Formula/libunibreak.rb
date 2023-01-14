7a5f283d7d0d222dd43d3c3c4e6ba53d7659de7c
Rui Chen <rui@chenrui.dev>
2023-01-14 12:15:11 -0500

    libunibreak 5.1
    libunibreak: add livecheck
    
    Closes #120611.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libunibreak.rb
+++ Formula/libunibreak.rb
@@ -1,9 +1,17 @@
 class Libunibreak < Formula
   desc "Implementation of the Unicode line- and word-breaking algorithms"
   homepage "https://github.com/adah1972/libunibreak"
-  url "https://github.com/adah1972/libunibreak/releases/download/libunibreak_5_0/libunibreak-5.0.tar.gz"
-  sha256 "58f2fe4f9d9fc8277eb324075ba603479fa847a99a4b134ccb305ca42adf7158"
+  url "https://github.com/adah1972/libunibreak/releases/download/libunibreak_5_1/libunibreak-5.1.tar.gz"
+  sha256 "dd1a92d4c5646aa0e457ff41d89812ec5243863be6c20bbcb5ee380f3dd78377"
   license "Zlib"
+
+  livecheck do
+    url :stable
+    regex(/v?(\d+(?:[_-]\d+)+)$/i)
+    strategy :git do |tags|
+      tags.map { |tag| tag[regex, 1]&.tr("_", ".") }
+    end
+  end
 
   bottle do
     sha256 cellar: :any,                 arm64_ventura:  "775a04ffdd0c01bac19771fe2c5303754f6115cab75ac1250f9a102b857fe2ff"
