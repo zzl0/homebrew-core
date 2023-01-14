630b1e7b0f294d11397ac20f6bbbce4197fff7b3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-14 17:04:12 +0000

    bmake: update 20230101 bottle.

--- Formula/bmake.rb
+++ Formula/bmake.rb
@@ -11,14 +11,13 @@ class Bmake < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "e536bcfc52946f631c00183164126e6fd26006acd27b21e7da7b06ab310300d4"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "6a4b5a42dadc037e158e4cfe5571b3adf9882024be07147431adfe2617a96a31"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "95a14d565b7eec3c2ca6d3ecb793d69cc2a29eddbfa4d8a891afe66e2e2ae945"
-    sha256                               ventura:        "1549aa5b240a604482f5e0b1838abcd88e40670847469d8515f6d66167be603c"
-    sha256                               monterey:       "801cbdcbe3ce5ff1e436b511923e75074a825e7d8e34cc80f7ce151a64cfbcd0"
-    sha256                               big_sur:        "d1e86cb4561374d4fa6d18c5b3e00afd4055640b10061c0c62b564ec8b522975"
-    sha256                               catalina:       "ef5c79a571b508e81a122960f1b3fa11a0640ce315c0595ed3030ea6dce100ee"
-    sha256                               x86_64_linux:   "0997b8c0c53881ead6f629f2a7d06dc88bcd6583c0ce6861d1e2351afe533e82"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "d02d846461c664d378b6fc5626b8cab592b46aa09303e1c4fe34227313e96548"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ac9d711d32c9227088e4a620a75494fbdf0cab2d7f76c44965792a370619e5e5"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "596c1ae20ea8695caac3ccb289063fd17ef3e7ffb7e25082a473dd25090abe55"
+    sha256                               ventura:        "1dd7f910b82553562e7fdc90cbf40b13a34e24cc0f49a01bd5d7a916c42b75e7"
+    sha256                               monterey:       "51aa10f29bc2bd6c21c251a34aab7b065a9d8c960f6794ce551aabc4f275baf2"
+    sha256                               big_sur:        "2bcf2aef6c89543cfb7561cd7048e0da2812c1c8e59bcb8dd1b20e1935cf23f6"
+    sha256                               x86_64_linux:   "b4f37fc544c7b340c571178bf371f104e981282e8a3e2a499b2743acf9c6c9a4"
   end
 
   def install
