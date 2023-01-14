bb42633c5d23748c606eee174bd3841ff4fe86c1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-14 18:04:31 +0000

    elixir: update 1.14.3 bottle.

--- Formula/elixir.rb
+++ Formula/elixir.rb
@@ -7,14 +7,13 @@ class Elixir < Formula
   head "https://github.com/elixir-lang/elixir.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "1d5846e972c092c16e91b58b8b0ad58b34519021d946451543dea9121440fa71"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "bba230b8f383dc8bc896ffc1d9c61d8c574a54289f1b68f7ec7160c20a75063a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8f4e2e610011dc4343595fbb5c905664470ca2aa2e4a8b4dc7df2eecde49ad20"
-    sha256 cellar: :any_skip_relocation, ventura:        "a562173ee5f2b201f5b12181716609c4b8f85435c4d1f0497a273e28ac690e97"
-    sha256 cellar: :any_skip_relocation, monterey:       "073d89fba1c8d633cfedd6f482ad1098e1acbc6d9d1f6eaa4adfcf3221dc3824"
-    sha256 cellar: :any_skip_relocation, big_sur:        "792186e3168e3dc51e336401290916ae45b48c78c2e7ac76bfda55fbd035aeb1"
-    sha256 cellar: :any_skip_relocation, catalina:       "0496616c1d69c9e3a0434f9208bbbad24bc2cb8aaa17dd8b8909fd35e487165f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "53e217f98eb4d875d5977dd794db8e8941e5257e7b495c0a8571623faa4bfc33"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "610af16d19faccbe2ceb01aa6003a0d5267967602ec831a88f2ef650474750f6"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "91559a7a5e28a5416c0b13d0c13638257b807dcfa67361fb9118df3de6866ab2"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "09fd7722ac5f72a3a006d0d4d0676ec13e90b136e4c606e2da1cf8bcf9b27842"
+    sha256 cellar: :any_skip_relocation, ventura:        "a42564f0a3a0b750b44058e8ad9dcde29fbf67e690ea919e704767afa9d7fc41"
+    sha256 cellar: :any_skip_relocation, monterey:       "16915c5b3cc8ed81fb59a383c1892229448748e3451e94230cfc7301c8bf20c0"
+    sha256 cellar: :any_skip_relocation, big_sur:        "76249a7142ec9ca2d171ad82faac3f468bfc95cb9bbcf2a27458a9cb3f8f19ab"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6181457efa0de2642794679e8a090b1fae9da0df6064668dc6b3166ec3963888"
   end
 
   depends_on "erlang"
