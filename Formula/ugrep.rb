0cac84e08b8c1e756a5241482b20c20fa3952b39
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-14 19:04:31 +0000

    ugrep: update 3.9.5 bottle.

--- Formula/ugrep.rb
+++ Formula/ugrep.rb
@@ -6,13 +6,13 @@ class Ugrep < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256                               arm64_ventura:  "14e1823d7c4d49cea77158d3ae3de7c4a2cdfdc1859f71e2cfd8dc951acc067c"
-    sha256                               arm64_monterey: "217e3393e52733efe6c30a09e484ca4692a277c751a6fbd1a4e552f7868f51ec"
-    sha256                               arm64_big_sur:  "37914d2a5e1de9366f1eba4dd118b2ace5ddf665b7e43f05b72078aba2d92e40"
-    sha256                               ventura:        "c1f2098384fe65c30bedf1cf112879b22273f887b875edf0f1455032e028f6f4"
-    sha256                               monterey:       "7aa317f9d3f90e8c26bd6b6a283b90fb87b20415541831c52697555f7c8a40e5"
-    sha256                               big_sur:        "1a0c529255433f61a0e238f85a43fca2472ba8f9a0ab0f406e751142b2c254ab"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "029c9c2a3201c97ef9ae32534a18016cfd58ba6c9cb9d654a2fce817fcf2621b"
+    sha256                               arm64_ventura:  "1994845dfe42f3a4004cda55f5e7b9c67ec8fb743916148d9b7704de0d1c32c7"
+    sha256                               arm64_monterey: "f13c91a3fcecea7c329ce79c5fd9fc2cea8c7c37970dd1a10d6eb37122af71cc"
+    sha256                               arm64_big_sur:  "4353fbf0b6ef6d3457db79f7a2b8c278115bf5dd2003b5fbe73bd1ed0a76a621"
+    sha256                               ventura:        "92fa002fa2d2479ed88c76fc228ca6744089c8ab827397cd771c91039f4ae474"
+    sha256                               monterey:       "ff8b3167c753b37d267f19ebbc858f0b1ad151c6da3a8137111d3f5b3a21d445"
+    sha256                               big_sur:        "7b1f61aaba755708350cfbf79050febcf35411cffdd0216fe601def297739922"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f43c98f255ad89116927762ced26afd5226d03541b9d73e4fd420bdcabd6ee35"
   end
 
   depends_on "pcre2"
