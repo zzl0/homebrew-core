aba73d1598e2db20ab5433b5f6ce5e89f7a476b4
Rui Chen <rui@chenrui.dev>
2023-01-14 17:36:09 -0500

    gitql 2.3.1
    * gitql 2.3.1
    * gitql: remove build patch
    
    Closes #120624.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gitql.rb
+++ Formula/gitql.rb
@@ -1,8 +1,8 @@
 class Gitql < Formula
   desc "Git query language"
   homepage "https://github.com/filhodanuvem/gitql"
-  url "https://github.com/filhodanuvem/gitql/archive/v2.3.0.tar.gz"
-  sha256 "e1866471dd3fc5d52fd18af5df489a25dca1168bf2517db2ee8fb976eee1e78a"
+  url "https://github.com/filhodanuvem/gitql/archive/v2.3.1.tar.gz"
+  sha256 "e3d34649f3dc714cb3189638103918314cf63b1ddbfd99a067d802730d1119b2"
   license "MIT"
   head "https://github.com/filhodanuvem/gitql.git", branch: "main"
 
@@ -19,13 +19,6 @@ class Gitql < Formula
   end
 
   depends_on "go" => :build
-
-  # Support 1.18 by updating dependencies.
-  # Remove with the next release.
-  patch do
-    url "https://github.com/filhodanuvem/gitql/commit/1bad3899592b0a8265e4a9c66e1c26e0bcbcd111.patch?full_index=1"
-    sha256 "b002683a2eac09f7342869cbbcb94a971f51db03fc7895bf7fa5a8069b030378"
-  end
 
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w")
