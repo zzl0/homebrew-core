825aa6ca16148a2545d1faf7d5e803b56bb19829
Patrick Linnane <patrick@linnane.io>
2023-01-14 09:16:54 -0800

    gssh 2.11.0
    * gssh 2.11.0
    * gssh: fix build and update test
    
    Closes #120610.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gssh.rb
+++ Formula/gssh.rb
@@ -1,10 +1,9 @@
 class Gssh < Formula
   desc "SSH automation tool based on Groovy DSL"
   homepage "https://github.com/int128/groovy-ssh"
-  url "https://github.com/int128/groovy-ssh/archive/2.10.1.tar.gz"
-  sha256 "d1a6e2293e4f23f3245ede7d473a08d4fb6019bf18efbef1a74c894d5c50d6a1"
+  url "https://github.com/int128/groovy-ssh/archive/2.11.0.tar.gz"
+  sha256 "86dba70bc6370af9db2e1364c1c97dc57f884703bc6814c7a845ec2bdf1ed279"
   license "Apache-2.0"
-  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "fb94322029d9beb3032089198fb9b755b38396151200ac14bd1be0e333f6e6bf"
@@ -21,14 +20,21 @@ class Gssh < Formula
 
   depends_on "openjdk@11"
 
+  # Fix shadowJar build failure, remove in next release
+  patch do
+    url "https://github.com/int128/groovy-ssh/commit/87913563ebb96c95a9fd17737c56aeff29916f0a.patch?full_index=1"
+    sha256 "c4c41e991ebb2eed737fb4bc90667a4b1f00958cabe8c4a5695c855072c7aeaf"
+  end
+
   def install
     ENV["CIRCLE_TAG"] = version
+    ENV["VERSION"] = version
     system "./gradlew", "shadowJar"
     libexec.install "cli/build/libs/gssh.jar"
     bin.write_jar_script libexec/"gssh.jar", "gssh"
   end
 
   test do
-    system bin/"gssh"
+    assert_match "groovy-ssh-#{version}", shell_output("#{bin}/gssh --version")
   end
 end
