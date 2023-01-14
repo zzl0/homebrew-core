5a0b2f4275ef2e06c3140922b781d6399ed1e973
Patrick Linnane <patrick@linnane.io>
2023-01-14 09:33:49 -0800

    pyside 6.4.2
    * pyside 6.4.2
    * pyside: use `python@3.11`
    * pyside: fix shim references
    
    Closes #120617.
    
    Co-authored-by: danielnachun <daniel.nachun@gmail.com>
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pyside.rb
+++ Formula/pyside.rb
@@ -3,9 +3,8 @@ class Pyside < Formula
 
   desc "Official Python bindings for Qt"
   homepage "https://wiki.qt.io/Qt_for_Python"
-  url "https://download.qt.io/official_releases/QtForPython/pyside6/PySide6-6.4.0.1-src/pyside-setup-opensource-src-6.4.0.tar.xz"
-  version "6.4.0.1"
-  sha256 "6e5be5defccacd21ec7e2579d6d6493366dd8e00f8899746abd174f1eb1eff14"
+  url "https://download.qt.io/official_releases/QtForPython/pyside6/PySide6-6.4.2-src/pyside-setup-opensource-src-6.4.2.tar.xz"
+  sha256 "1ec9d0936332efd229650cf10fed36cadddff7a613a2ea6e897de4d504c1b505"
   license all_of: ["GFDL-1.3-only", "GPL-2.0-only", "GPL-3.0-only", "LGPL-3.0-only"]
 
   livecheck do
@@ -27,7 +26,7 @@ class Pyside < Formula
   depends_on "ninja" => :build
   depends_on xcode: :build
   depends_on "llvm"
-  depends_on "python@3.10"
+  depends_on "python@3.11"
   depends_on "qt"
 
   uses_from_macos "libxml2"
@@ -40,7 +39,7 @@ class Pyside < Formula
   fails_with gcc: "5"
 
   def python3
-    "python3.10"
+    "python3.11"
   end
 
   def install
@@ -64,17 +63,18 @@ class Pyside < Formula
     # Install python scripts into pkgshare rather than bin
     inreplace "sources/pyside-tools/CMakeLists.txt", "DESTINATION bin", "DESTINATION #{pkgshare}"
 
-    args = std_cmake_args + [
-      "-DCMAKE_CXX_COMPILER=#{ENV.cxx}",
-      "-DCMAKE_PREFIX_PATH=#{Formula["qt"].opt_lib}",
-      "-DPYTHON_EXECUTABLE=#{which(python3)}",
-      "-DBUILD_TESTS=OFF",
-      "-DNO_QT_TOOLS=yes",
-      "-DCMAKE_INSTALL_RPATH=#{lib}",
-      "-DFORCE_LIMITED_API=yes",
-    ]
+    # Avoid shim reference
+    inreplace "sources/shiboken6/ApiExtractor/CMakeLists.txt", "${CMAKE_CXX_COMPILER}", ENV.cxx
 
-    system "cmake", "-S", ".", "-B", "build", *args
+    system "cmake", "-S", ".", "-B", "build",
+                     "-DCMAKE_INSTALL_RPATH=#{lib}",
+                     "-DCMAKE_PREFIX_PATH=#{Formula["qt"].opt_lib}",
+                     "-DPYTHON_EXECUTABLE=#{which(python3)}",
+                     "-DBUILD_TESTS=OFF",
+                     "-DNO_QT_TOOLS=yes",
+                     "-DFORCE_LIMITED_API=yes",
+                     *std_cmake_args
+
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
   end
@@ -93,17 +93,16 @@ class Pyside < Formula
       Widgets
       Xml
     ]
-
     modules << "WebEngineCore" if OS.linux? || (DevelopmentTools.clang_build_version > 1200)
-
     modules.each { |mod| system python3, "-c", "import PySide6.Qt#{mod}" }
 
-    python3_config = Formula["python@3.10"].opt_bin/"#{python3}-config"
-    pyincludes = shell_output("#{python3_config} --includes").chomp.split
-    pylib = shell_output("#{python3_config} --ldflags --embed").chomp.split
+    pyincludes = shell_output("#{python3}-config --includes").chomp.split
+    pylib = shell_output("#{python3}-config --ldflags --embed").chomp.split
+
     if OS.linux?
+      pyver = Language::Python.major_minor_version python3
       pylib += %W[
-        -Wl,-rpath,#{Formula["python@3.10"].opt_lib}
+        -Wl,-rpath,#{Formula["python@#{pyver}"].opt_lib}
         -Wl,-rpath,#{lib}
       ]
     end
@@ -119,9 +118,9 @@ class Pyside < Formula
       }
     EOS
     system ENV.cxx, "-std=c++17", "test.cpp",
-           "-I#{include}/shiboken6",
-           "-L#{lib}", "-lshiboken6.abi3",
-           *pyincludes, *pylib, "-o", "test"
+                    "-I#{include}/shiboken6",
+                    "-L#{lib}", "-lshiboken6.abi3",
+                    *pyincludes, *pylib, "-o", "test"
     system "./test"
   end
 end
