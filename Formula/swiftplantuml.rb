8cddeda3b206f4822189d20a62383e62ac542b3f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-14 00:13:24 +0000

    swiftplantuml: update 0.7.1 bottle.

--- Formula/swiftplantuml.rb
+++ Formula/swiftplantuml.rb
@@ -6,12 +6,12 @@ class Swiftplantuml < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "b60acd2cc0de576dff6c533234fd479ce0d490d66ecc2bd6b6f3a6116a8f8097"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "db2ce18037cffd29a3fac1670f3cbd97718b10275b7bd818fafaa66a7d604005"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a3ee0031c3e55d0862084e137315baff5378ddb7d69fde605aae4e940d1d8004"
-    sha256 cellar: :any_skip_relocation, ventura:        "b38fa85b22889cf37186242f6bd2ddbea1a247e536d4176267d7889740b548c9"
-    sha256 cellar: :any_skip_relocation, monterey:       "a67b36ee7df9136c6cd5d6b9fe89720d18688738566d82c788d11fe769ab39b4"
-    sha256 cellar: :any_skip_relocation, big_sur:        "133b8220e0ca1a04b56507026d20fc00c43be05cb4be929d0f7dd56d40f725db"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "1e85ee1d7c67cf689483a6c081dd20de591ce08d73a2ea554c24e0ca63ed5198"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "dc134e57cc3611c9ba258863bad1160b7119fcf945550b99162b83b93b260fac"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8d1c60e82a49065fb29d80205ee2da1fa32637d1d48695f343d3b59e6e310e46"
+    sha256 cellar: :any_skip_relocation, ventura:        "ff16cfd9bd1cf27ab248595bb389d9545b12c7915c943fc00c93f0bc0e2ba959"
+    sha256 cellar: :any_skip_relocation, monterey:       "69cfa38c6a8e7967db655d331034e8c4be5b93d328bb7840fee7f7f70729a971"
+    sha256 cellar: :any_skip_relocation, big_sur:        "98626c7c3c2358109007a1bde7730024ad4611e7315f0e522746d0fcd283fb8d"
   end
 
   depends_on xcode: ["12.2", :build]
