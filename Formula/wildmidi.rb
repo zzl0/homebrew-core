3e8c7687a4c964faeee09d6d34962f2ee931a9d5
Patrick Linnane <patrick@linnane.io>
2023-01-14 09:32:56 -0800

    wildmidi 0.4.5
    * wildmidi 0.4.5
    * wildmidi: update build
    * wildmidi: specify rpath
    
    Closes #120616.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/wildmidi.rb
+++ Formula/wildmidi.rb
@@ -1,8 +1,8 @@
 class Wildmidi < Formula
   desc "Simple software midi player"
   homepage "https://www.mindwerks.net/projects/wildmidi/"
-  url "https://github.com/Mindwerks/wildmidi/archive/refs/tags/wildmidi-0.4.4.tar.gz"
-  sha256 "6f267c8d331e9859906837e2c197093fddec31829d2ebf7b958cf6b7ae935430"
+  url "https://github.com/Mindwerks/wildmidi/archive/refs/tags/wildmidi-0.4.5.tar.gz"
+  sha256 "116c0f31d349eaa74a630ed5a9a17b6a351204877a4ed9fb9aacd9dbd7f6c874"
   license all_of: ["GPL-3.0-only", "LGPL-3.0-only"]
 
   bottle do
@@ -20,12 +20,7 @@ class Wildmidi < Formula
   depends_on "cmake" => :build
 
   def install
-    cmake_args = std_cmake_args + %w[
-      -S .
-      -B build
-    ]
-
-    system "cmake", *cmake_args
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args, "-DCMAKE_INSTALL_RPATH=#{rpath}"
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
   end
