7568d3e90d2da042989a5cecff9c5bbaa411798f
Michael Cho <20700669+cho-m@users.noreply.github.com>
2023-01-15 22:05:49 +0000

    tracker: update 3.4.2_1 bottle.

--- Formula/tracker.rb
+++ Formula/tracker.rb
@@ -13,13 +13,13 @@ class Tracker < Formula
   end
 
   bottle do
-    sha256 arm64_ventura:  "25220e2fe811db4a7887b3f071ad49d5136066bb132f859df6f53975a2e2d5a2"
-    sha256 arm64_monterey: "0eb8634c38d2b09071c4283442a5df7dcc108c4bcb1fe89af17dbaaebb79cb6c"
-    sha256 arm64_big_sur:  "5200c2039328369a8f497f65e657ab29e31e0e8b64484f671361655af14188cd"
-    sha256 ventura:        "1dd30eb22b7f302bb6926ff8cb3e360c440a319ebfb77eaab0898b3f75df07b8"
-    sha256 monterey:       "e899d719c821a34cc30282a6203be5c8cd4141d9ba917e6804b8f6d8ea855c3d"
-    sha256 big_sur:        "28f909c8b463cb4ee9b056473850b7adbfc470b36a06ef90d2710be1d6bf6dba"
-    sha256 x86_64_linux:   "284fc92a1363d399eed150db8c9b323778a48e5d025cf200f705ea9b386187ce"
+    sha256 arm64_ventura:  "e342d63c43edfa31a3b8a50cb911b6997ab397f13284d1000df7ed9907fc195a"
+    sha256 arm64_monterey: "37836c628dbe2454bf45f25cff7ca2d04b0bbc898fb2b2816fce0b8d10e10b9e"
+    sha256 arm64_big_sur:  "70da64159075343b79ac5b79d2b08813cd4433aff0a9fd4e6160b9ab027009ad"
+    sha256 ventura:        "83418c8ceec2f3f6e44401b968f1f56666c1c29d460674a9a13e05eab0660a4b"
+    sha256 monterey:       "96b41d7fbcbf5666283e6253264b2400f97528fd4d712deb3eb1d5225e05aa52"
+    sha256 big_sur:        "9dff729210dd10853993448f7fb5373af65b5fa5aae3b8b3a4d974fc445965da"
+    sha256 x86_64_linux:   "0fc8435e75ae39d0d617349f6ab4aaa24fe98fc7b65bd4090e4d9ee7f87a8433"
   end
 
   depends_on "gobject-introspection" => :build
