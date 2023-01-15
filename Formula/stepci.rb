f027c2e3b89fe760c0c9e675ebb44e9cebdfb0da
Patrick Linnane <patrick@linnane.io>
2023-01-14 16:11:44 -0800

    stepci 2.6.2
    Closes #120636.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/stepci.rb
+++ Formula/stepci.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Stepci < Formula
   desc "API Testing and Monitoring made simple"
   homepage "https://stepci.com"
-  url "https://registry.npmjs.org/stepci/-/stepci-2.6.0.tgz"
-  sha256 "e30ffbac5205ace4aa4bd6dd07a7e659a068c4d7c38255ca0faf3d7eb4b2bdec"
+  url "https://registry.npmjs.org/stepci/-/stepci-2.6.2.tgz"
+  sha256 "ff1914245604552353f5332ef7abf106c25b2c0b7a1086be75bb5ee3d0d3eea7"
   license "MPL-2.0"
 
   bottle do
