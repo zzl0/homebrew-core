3728fe2b62090d77cb5e41092591d44747a60db4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-15 00:14:38 +0000

    pomsky: update 0.9 bottle.

--- Formula/pomsky.rb
+++ Formula/pomsky.rb
@@ -7,13 +7,13 @@ class Pomsky < Formula
   head "https://github.com/rulex-rs/pomsky.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a2ad385fcc566b4350f9997bdead5f424bd69b20197790b9eb7173fbf276a3e5"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "d4714400d4aa1dcbc25f615307a14261f2c9ba9b8ab784ee57c3fbeb41b48ab0"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "68875a5e6f05fbbbb9266c37bcec86c57bf6114f05d592922acfc86faf0aa6bf"
-    sha256 cellar: :any_skip_relocation, ventura:        "c279770f8cd5acc209b1c377f44c84372e4e08053d15271743b31bd790369fd6"
-    sha256 cellar: :any_skip_relocation, monterey:       "63fd4ffdf988addb5f86b3264834224860466817beebac49519df73bc8beb447"
-    sha256 cellar: :any_skip_relocation, big_sur:        "54ba3679a2457afe15dd419bdf257d5e7b37bf02e14b0fa85ebca9b9c2c0ea5a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fa903b95850e748107dda93da237315f1ea8a09c3c2c92779cf7dc8b18ddce9a"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "cde41360135f32e1dbff5262783389ca8d9cea35a0f37f4477354fc679603281"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f028bec0428e542dabf8421a1437cd8862ca02136f1ccb5b1ea11a340f59e426"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3bcffeb4ebc8cdfe8aae2996081a941092fa8c6a7785389aa50ca5fe1a0af968"
+    sha256 cellar: :any_skip_relocation, ventura:        "95d5c57f43e7755d9088a84f923ad68e3a362314113f5a4d1cdba046b352c539"
+    sha256 cellar: :any_skip_relocation, monterey:       "75edb8c7e06b54fb78349392f60ab49e03844bf2c739a1a44f704347e75b9583"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f15b6afdad27659c8597194932104ca4adb3e49e6b619695547406c3c37c8e5c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d1c4516f29ca77af397d190e5a949a6a26d031477340df9cc6e512a5f10bacba"
   end
 
   depends_on "rust" => :build
