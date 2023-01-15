30d273b6a4e8a625e8eee44d21b7449cc6b258e7
Michael Cho <20700669+cho-m@users.noreply.github.com>
2023-01-15 22:05:56 +0000

    liblcf: update 0.7.0_3 bottle.

--- Formula/liblcf.rb
+++ Formula/liblcf.rb
@@ -8,14 +8,13 @@ class Liblcf < Formula
   head "https://github.com/EasyRPG/liblcf.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "a2b5fc7ec2b96d5c2605404d7843664873ce443f9a8570982ee2abe7acc3c7fd"
-    sha256 cellar: :any,                 arm64_monterey: "68e94e4f58730ea5e6c9feeeea014c1bb698473749280327cc2aafa402e46cd1"
-    sha256 cellar: :any,                 arm64_big_sur:  "c53896d6203b499b21cfe49e23b8f9c8b8e9821f6fa15507e612ef4cc1d9be39"
-    sha256 cellar: :any,                 ventura:        "0ad367fac0a53f72379fe4df08907f13ab619fdd671a2e78bc7fecb6204545f8"
-    sha256 cellar: :any,                 monterey:       "78dc57cba649af4c239fdb76883bb01e810511072cfaec1724d9ad5548392749"
-    sha256 cellar: :any,                 big_sur:        "f4e5fcf1451d07aa37cf64b068a76b2b7eb6c0c7eb4cfc67f76cd906a82494ac"
-    sha256 cellar: :any,                 catalina:       "dbaa6069c2a9b1e2991e718e02c54fcf47ac369db007030b1f5123028beb4e52"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "40f199d20953a9bb89a29829807573fe4ebcdd3ff6b3bc22b90ed8fce2cf26cb"
+    sha256 cellar: :any,                 arm64_ventura:  "420ae4d8fd864017912ebd7863f6bead1aded6604f72f56a5a674a7cffb071c8"
+    sha256 cellar: :any,                 arm64_monterey: "09dd2d9ba9b6d1a7b004051586a6a1abe6929a036a5556479840477c4e8b0b5a"
+    sha256 cellar: :any,                 arm64_big_sur:  "6130ad6bf07fda73da7ba0f6b2ff98cc37122f0e3b6cd4a32c011f65db9a2499"
+    sha256 cellar: :any,                 ventura:        "72a859a84917356fad5415a44dcf89274ead353c25866696c1156597edc90256"
+    sha256 cellar: :any,                 monterey:       "c97668249d45a99d5da6355d8ec127db44dad2a133ca3dddf1e046035ab8d1fd"
+    sha256 cellar: :any,                 big_sur:        "3e01df65a875158b6d06780db5f60286a7291201dca42c99960179b66eb25014"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "66e456b887b1a3e490fedd8ead94a0f6cd95fbdcfad4df086042ec6078d7eb90"
   end
 
   depends_on "cmake" => :build
