00e23fffa753bd8fc496ebeaf4bdb36517551251
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-15 18:04:58 +0000

    carla: update 2.5.3 bottle.

--- Formula/carla.rb
+++ Formula/carla.rb
@@ -12,14 +12,13 @@ class Carla < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_ventura:  "c7697e9831c2368b1231fdfd7d3488782644a5d20b0b2bef050828e62b009ee7"
-    sha256 cellar: :any,                 arm64_monterey: "a3054ec4823bd72dcbee11bb683d3d65662136b31900050d655ab314bf6a0aab"
-    sha256 cellar: :any,                 arm64_big_sur:  "f14c740272442ce6f573995c253a38cace30a88918a6e4ce4eb5c065c5f60281"
-    sha256 cellar: :any,                 ventura:        "9f7c9e2370458aa307de52175d51eadd6eedd8a03da2b22750320592acffa2dd"
-    sha256 cellar: :any,                 monterey:       "1d3fc9b4f2fc1d46800cc522ac0a826eede707e86cd522e9d244ef9665e8e84b"
-    sha256 cellar: :any,                 big_sur:        "81d2ec0a2a986aaf286abf4c169df0ceec42435ca0376fab0d268155a2424209"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "637095b6218896ea500a6318420faea52aa087d552a273dfbf2b5454428fd55a"
+    sha256 cellar: :any,                 arm64_ventura:  "af4603464e92f63b973f418c6756e7b5ef0597fc68bde190537023533f4e71a3"
+    sha256 cellar: :any,                 arm64_monterey: "5cae6077d549dfa5736c5a0a21bb0689cf09507664ee1c51e52913f2bbb04004"
+    sha256 cellar: :any,                 arm64_big_sur:  "27e94c1b2b9c15483c29fc3a55e26ef05f6dc645eee27d2b11586a681ffb474f"
+    sha256 cellar: :any,                 ventura:        "8e21634e6fb47bbe96c0ff3f64609b0d42ef5c43c3c02552e08fbaa2dc4eb80d"
+    sha256 cellar: :any,                 monterey:       "55d77147433c3cb484c235e0ac5b306669e5a6d256e1fa6285189f236763a3e0"
+    sha256 cellar: :any,                 big_sur:        "90e5abcd75da09eed552d1cb272353450af73af44118c04c8eb2b86567450728"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "284aeaa8d6d5fceb4e2c6c5ab6c07143e238adf309cddaa8ecd79c4a8eb76adb"
   end
 
   depends_on "pkg-config" => :build
