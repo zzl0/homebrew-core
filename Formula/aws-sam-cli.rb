30cc28777302d506feb799b629637af71f005072
Patrick Linnane <patrick@linnane.io>
2023-01-16 02:06:32 +0000

    aws-sam-cli: add 1.70.0 bottle.

--- Formula/aws-sam-cli.rb
+++ Formula/aws-sam-cli.rb
@@ -7,6 +7,16 @@ class AwsSamCli < Formula
   sha256 "609c6111b803c5fbca8ddfb28e5b953777c3527754c1b3f319feb8c3db19dbe2"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "272904241063eb4678e81ab5beffc9b494fdee90e7434cfe15f1b85db4f5352a"
+    sha256 cellar: :any,                 arm64_monterey: "43939b4037709e21e909a5b9f706b03197594ba39cfdb9faaca9eb85a461e5e2"
+    sha256 cellar: :any,                 arm64_big_sur:  "7e580b2a9d5363160f651e6874f5e57e13b7b0f28d498f8688099b65391ebdd5"
+    sha256 cellar: :any,                 ventura:        "ccda9e6995b38af2b83231e7d96f368af2299e680a9ed33cea8f72297dcf6239"
+    sha256 cellar: :any,                 monterey:       "1b4fd42129adfc8e7b53cc7c7aaec0a85bd620bbca1dc4d91d91b5471d9203e4"
+    sha256 cellar: :any,                 big_sur:        "6eff40ed3293a8ce6b8c6baae48f60def39b707eda1979bdc73c6b34cdb1d06d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "131ff7af5116e5cd39213eac1f394495da4a0d8aef83291e1c2c11f8de2cc32c"
+  end
+
   depends_on "rust" => :build # for cryptography
   depends_on "python@3.11"
   depends_on "six"
