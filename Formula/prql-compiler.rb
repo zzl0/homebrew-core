c7d400409ef78451a8b2a637d0fc7192e832b4be
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-16 21:04:45 +0000

    prql-compiler: update 0.4.0 bottle.

--- Formula/prql-compiler.rb
+++ Formula/prql-compiler.rb
@@ -6,13 +6,13 @@ class PrqlCompiler < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "d07fd60670f3fd4b0645820acead90edee4dc5024080d3988b504d62b794bfb5"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ef81cc46298cfa39fe4801052c728c97e447b6a692d107f76993d3121cb45888"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a7b44aa761f35a07f75c14eec94bc1ccb3e4531aba3794a4fa376526344769a5"
-    sha256 cellar: :any_skip_relocation, ventura:        "4e08789caab64d6fc1e714b48460509578f1886fa43d6a242c0e1217522646cd"
-    sha256 cellar: :any_skip_relocation, monterey:       "cf4fd08cb067d65de1a76f265d65155f2c0ec5ccb85802a31ab4f80d58acb301"
-    sha256 cellar: :any_skip_relocation, big_sur:        "6b0935a128a3fd9042c121224da933059d5f69acb69e066546d53f831e0de645"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a415fd572a75fe63edfff03cc2ce761409fca23ba9e88d466c775755ef77eb71"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "b7424fe736ef2fd15f5e67a76b6576bac3e7930adba598c9dadba939ad915b9c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "56cc9833b457c187014dce5adc9ff212987a983b88089b608ad58f06d43bd5d5"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7190ec9bf9c2c060d3864278171fd7ca3406d58505d88057edc9c1c11f02b529"
+    sha256 cellar: :any_skip_relocation, ventura:        "cac3ad853cd4575e9e4711a5a9e9541e5802b48dd717ae0fdbdb6737a8872a54"
+    sha256 cellar: :any_skip_relocation, monterey:       "d93a32516573867316e32d973f77eca392c7e194cbb5b17a0674195e8adebb2c"
+    sha256 cellar: :any_skip_relocation, big_sur:        "eeb82dd2a0ba3dd1082dbcc90500e174cadcfe71e84d498c05fbd45bd242611c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a9da8a68182ce95b628532794dc3167fb5e2e5c7b773f9456229b6bd56f7da2d"
   end
 
   depends_on "rust" => :build
