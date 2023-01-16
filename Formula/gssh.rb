977c2af7baebe2c36c6d9ac9e9004cfd4776e5c8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-16 04:24:38 +0000

    gssh: update 2.11.1 bottle.

--- Formula/gssh.rb
+++ Formula/gssh.rb
@@ -6,13 +6,13 @@ class Gssh < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "6f6a927a89bd21da514b23a6e0926195b3975801b1acf804f338e7e2050ee85f"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "88fb042ee8885cc77b8ccd0a05ee677b6c7c8380022ec537c6315d69d7207f91"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "eeb64ee7ee58144ec406ccf1ec6e3ebe18bbed47cfe61fdd4ae125c1b279f3e9"
-    sha256 cellar: :any_skip_relocation, ventura:        "7d7197d83722d205ed7a7f46851aae1fa3a7767198456e30e3fa0cf1f2fb0c87"
-    sha256 cellar: :any_skip_relocation, monterey:       "c2167a462ec989275bfda2794363baf9c3e9827d5f8171ea197d437346a67e74"
-    sha256 cellar: :any_skip_relocation, big_sur:        "ecc793e157526f843e6576f6e4798f01e80f08483721a5521b82778f494bfd1f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2af492fe81b59ffc2012783fb24bf8dfb2d4c34b3cc001c257e8007595823f44"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "b108fdcbbbaa38298f3a7301e05c8ffcef457526d8852ae8a4d263a0deb6ac39"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "06426726661799e46b4c8929ec508d9208b01203f9e22307a6824a3ace5271cf"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d284f171bc0059cbed05743d3e3d66bbb20102a0ba10c0a328fae523866ad5a9"
+    sha256 cellar: :any_skip_relocation, ventura:        "2c7bf01481c17630137f8f9be1cd8f6e2413634c0e13060d56ff593541ff622b"
+    sha256 cellar: :any_skip_relocation, monterey:       "c618b02253d978153c2beb9be03db15b37d23939efdbb9452af3a8d3fa63a454"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f7fe60cb94a979432b20323000bfa21c0d6f90101f53e98c45f0201a1ea3ccf5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "48300591ae3b0f38ca254b2c95261d002464fddcdbcbb5fddd9a1b0e615839eb"
   end
 
   depends_on "openjdk@11"
