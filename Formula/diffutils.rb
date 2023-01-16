38c5e3db6e05e4d584e44a3f6c956a16d51c754c
Patrick Linnane <patrick@linnane.io>
2023-01-15 17:01:35 -0800

    diffutils 3.9
    * diffutils 3.9
    * diffutils: update license
    
    Closes #120692.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/diffutils.rb
+++ Formula/diffutils.rb
@@ -1,10 +1,10 @@
 class Diffutils < Formula
   desc "File comparison utilities"
   homepage "https://www.gnu.org/s/diffutils/"
-  url "https://ftp.gnu.org/gnu/diffutils/diffutils-3.8.tar.xz"
-  mirror "https://ftpmirror.gnu.org/diffutils/diffutils-3.8.tar.xz"
-  sha256 "a6bdd7d1b31266d11c4f4de6c1b748d4607ab0231af5188fc2533d0ae2438fec"
-  license "GPL-3.0"
+  url "https://ftp.gnu.org/gnu/diffutils/diffutils-3.9.tar.xz"
+  mirror "https://ftpmirror.gnu.org/diffutils/diffutils-3.9.tar.xz"
+  sha256 "d80d3be90a201868de83d78dad3413ad88160cc53bcc36eb9eaf7c20dbf023f1"
+  license "GPL-3.0-or-later"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "db92359b7a7cc8eacdc1b5998df69cfa24bdaa54e02ae06f4a10bdb339c190f8"
