a5f46986da99381316759790902ffbb73e016fc9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-16 04:24:34 +0000

    libvterm: update 0.3.1 bottle.

--- Formula/libvterm.rb
+++ Formula/libvterm.rb
@@ -12,14 +12,13 @@ class Libvterm < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "2948569d2a9ef42012e53f5596e6c57d0579ba2fd67289276aee87f838aa18d0"
-    sha256 cellar: :any,                 arm64_monterey: "a1f3253ab0132353337dda08fd4cb4813a7feb0985f109439295f779b502637b"
-    sha256 cellar: :any,                 arm64_big_sur:  "d3edd5f0a00464f01ef2994845724f30e920c0278d2f3c664d83ced7edf441ef"
-    sha256 cellar: :any,                 ventura:        "961518ea7372798bb713451dd3a6a1d2a2b344a749028dbc2c44951dc7a4a635"
-    sha256 cellar: :any,                 monterey:       "512bcb9ef343d4179be42ce254d150c2501915d1c32d41b003df82864cc3a245"
-    sha256 cellar: :any,                 big_sur:        "999760fd3801afdc3d79e0cf95c3de23233649c191797385c535738b979ae303"
-    sha256 cellar: :any,                 catalina:       "8c73c0852230a0302efef48f61a3257322c32b104df3b924b0585b2f402578a7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "313e6d0a86af3f5788ebb3d27e2ee7b3a0bed3855e60acdbbc0c6356cf90ba8a"
+    sha256 cellar: :any,                 arm64_ventura:  "8223c9b38657d9029e9ca4fbed618de2819a26d2c55662a2ac249ddb2e19ee20"
+    sha256 cellar: :any,                 arm64_monterey: "fb4d33a68c1d2f74bf3a365b999d00699cb458470ef14473f681c28db5d862ae"
+    sha256 cellar: :any,                 arm64_big_sur:  "93895eb12f869135a997949df26a6dac909061a16b5161236acc79c2a8c48de8"
+    sha256 cellar: :any,                 ventura:        "047bb97a27597a58514e7cdf012064d13cf42676e1546fbe78020064ba2f88dc"
+    sha256 cellar: :any,                 monterey:       "e5cb1676eea4d4a2e6aaa76d39edec5387245342811861531e747697f264aa94"
+    sha256 cellar: :any,                 big_sur:        "52d39b4b5dbe5c70ff96f44ec9772b89a2218a5cc53e942ade2bf0922942d9d6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "910c60368d29ac5b99138523a0e4a03ab4c64abbe1089333ded4b481c024a1d2"
   end
 
   depends_on "libtool" => :build
