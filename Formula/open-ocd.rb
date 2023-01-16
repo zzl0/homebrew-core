31dab5dafd618f8ef587d20c8852a7406c0456d2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-16 18:04:36 +0000

    open-ocd: update 0.12.0 bottle.

--- Formula/open-ocd.rb
+++ Formula/open-ocd.rb
@@ -11,13 +11,14 @@ class OpenOcd < Formula
   end
 
   bottle do
-    sha256 arm64_ventura:  "f45c81b908590c4a6f14e41864079b9d5d15b30e5c3c0dc8fff116419629354c"
-    sha256 arm64_monterey: "0f2c67a9ac1fbeedaecab8c7b28a13ad02b23b3f22f0291824536e6dfc0400f3"
-    sha256 arm64_big_sur:  "10bc538f2fa15a6b5394d62a23a9791d11caf5220aee5977ad132c8e49d2383a"
-    sha256 ventura:        "fc651defb7f42ba32d3f7879ecaea300045f19a686b1fe614e4d194dfde0b86a"
-    sha256 monterey:       "b08e2fcad452c7b511c152c347f46879ad4818c74e84fc415a4ad6cdb57c8278"
-    sha256 big_sur:        "f9c34229e17528c93e4297292cd51ed477096559d5b9661a52c7f040df0e0635"
-    sha256 x86_64_linux:   "7360c435cdf6e48fd6310e203f0911ce90668ec918fbe60814b866048be60f66"
+    rebuild 1
+    sha256 arm64_ventura:  "c611d6b6c9e57cb090035cb957b452df65f7cbc631efad899fb7ab41e1ea32e1"
+    sha256 arm64_monterey: "7b08b217ff97444f3b1543efe5d94f5b48dcdd74ea501426d894490e76f06405"
+    sha256 arm64_big_sur:  "7cdba4d5b4ac53b160be03c26f96f8f8d1d4963ff2abaac7c219bc9fee03b50a"
+    sha256 ventura:        "f55c5f34b7842ba9529b93f83997e1e6abac12ca77cd67bb76df8029894473a5"
+    sha256 monterey:       "5fe0d9ced9ebdac6350e56e4ef5594f4080ce2c81054fb0b5fb55eca99c75f24"
+    sha256 big_sur:        "fb7fef372618e8b59b07016c380b727d61103ee67ad5b68811bcbcc1516b3a6d"
+    sha256 x86_64_linux:   "0b26fad3eda406a9cb72f04943b43015371b8f224fc624347fd9616f2604ad9e"
   end
 
   head do
