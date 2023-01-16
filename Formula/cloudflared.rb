cca8583dddd1bf1338a3939fd2f6ce7807b3e1ec
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-16 17:04:38 +0000

    cloudflared: update 2023.1.0 bottle.

--- Formula/cloudflared.rb
+++ Formula/cloudflared.rb
@@ -7,13 +7,13 @@ class Cloudflared < Formula
   head "https://github.com/cloudflare/cloudflared.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0b7f6d5c5b221e098195045fa401ffebe895866d5bec36fa4b0313d8d657576b"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "8a3926c2ddd04706a9981740c3a74a314beb576cf9b43396c6bf83e8cb5f88e8"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "22ae7d17cf539c0fffd763d38e6882c35ef8637afd20b00871f43c4c7313e7f5"
-    sha256 cellar: :any_skip_relocation, ventura:        "fd2c308fcd428a6884d5bc62387ebb0533a1305b2e33b63317c729de3ce70ade"
-    sha256 cellar: :any_skip_relocation, monterey:       "e8e3f44110565607ad7b8f198254e7e98555980bbb32727c1b60ea1dc7223597"
-    sha256 cellar: :any_skip_relocation, big_sur:        "c35f672e288a79ae4b11abc1dad8b35329d2d3ea4ab6c754c37feb166c694bb8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4e0edac6d1e031862e54308f73afa53e12f8159e48571ac3ceeceb42f26604e0"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "5bab313459270795e88f66ba170b8a7b43bdfd2b76117c1023b9b544cdaf046e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "300be568aa3a35f5107d33cd1993069d2baf39a8a3b6fd6dae78e51a1efb180c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e8926cd831109524a384c8d84e1286b663e1d8a47955a300d632ba8745539982"
+    sha256 cellar: :any_skip_relocation, ventura:        "125ef471626a74e3e12c162359f3530eb307f43a8210f3c065f4bab640674010"
+    sha256 cellar: :any_skip_relocation, monterey:       "b005671fce5bc22ea8db0613c7942a1244b5915018fe6b07be5ed926d97d6eb9"
+    sha256 cellar: :any_skip_relocation, big_sur:        "3f68da18bd5b68adf0f214bf757bb4ea7427e7d635c209b2d2374d3ef652d337"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "af03d95d243379cbcdae21e798e5ba71d0efb9b52adcb08cb664fc18f3152cb0"
   end
 
   depends_on "go" => :build
