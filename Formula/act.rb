2cb96c0ce80248b7db112453639baeffbaf0dc03
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-16 20:04:40 +0000

    act: update 0.2.38 bottle.

--- Formula/act.rb
+++ Formula/act.rb
@@ -7,13 +7,13 @@ class Act < Formula
   head "https://github.com/nektos/act.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "49d867052b564ace082efd48c20c817dd1322e66d38daa3d24dbebf6f00638ec"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f10ca85d16b0c68e878125a6564343725c2c592a26f8c4dc7d9b5ed13ff92984"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2e742a1f06e221f27b5c368bb39580a794b20206f13f87821de4c485511dd451"
-    sha256 cellar: :any_skip_relocation, ventura:        "c05d3086fb22e183c8e0ed503ee55318eadf126b010907956406b7152cf131b8"
-    sha256 cellar: :any_skip_relocation, monterey:       "793d30b1ae739a46c80da72d64166b9d3f2842bd84854b5c30327e77a650ebc3"
-    sha256 cellar: :any_skip_relocation, big_sur:        "3481fc5a1c4eff1577f25f58cbce443de332c275492f6361135dec450c5c0f47"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bc0f6a3e51334f0880cf4f908d0cfbe2972a2e2a1649c4bff29f079278c00180"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "101fb443cca685c9179ca6dd8c1c6721bb315a0c1c3ff67863834daa668b6e85"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "92851d16c63202818fe5b4bee3f11f39cb0e1f65abc4d3aa63f36576af266529"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ac0c66353ec92588d6f806a4a4cc72e74406182b78eca918694f2392b15f67db"
+    sha256 cellar: :any_skip_relocation, ventura:        "84688001ccac6b66769d6e79570b38ab4fa4a41c77f44bb0dac987d5a065aa36"
+    sha256 cellar: :any_skip_relocation, monterey:       "2b0ac698570d42ba1b8cca82c7b67516ca0a7d71679fb23ff83602624fcacb0e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "3404c9de797ad5a737c7289a38e0b41ced9bd69de3ca271c8fef5de365f9eab5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "57b20a303bcb800fc54020c2963f90f1c4bb4805b4a3294cf249db99ba361ac1"
   end
 
   depends_on "go" => :build
