69c1717bfce96fc6b5f6dcff3036d4ec75cfc5ff
Rui Chen <rui@chenrui.dev>
2023-01-15 19:18:28 -0500

    helmsman 3.16.1
    Closes #120687.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/helmsman.rb
+++ Formula/helmsman.rb
@@ -2,8 +2,8 @@ class Helmsman < Formula
   desc "Helm Charts as Code tool"
   homepage "https://github.com/Praqma/helmsman"
   url "https://github.com/Praqma/helmsman.git",
-      tag:      "v3.16.0",
-      revision: "c1dec137141cc4b1cc76bd46744941ba6370b1e0"
+      tag:      "v3.16.1",
+      revision: "cce09a4fb79a33340160e6e9b7b7d07481f1a8f5"
   license "MIT"
   head "https://github.com/Praqma/helmsman.git", branch: "master"
 
