7717ee3d842e50a075c616ccafc115834d246df6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-16 01:32:10 +0000

    nuclei: update 2.8.7 bottle.

--- Formula/nuclei.rb
+++ Formula/nuclei.rb
@@ -7,13 +7,13 @@ class Nuclei < Formula
   head "https://github.com/projectdiscovery/nuclei.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "637ecb34b181ca0068f83af01e220e170b473d09c0bd9d936a1a7f2d5afff9c3"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "7508899b91347df325007571dc3a2ea8cb6c6ab5dfe8481d48a9cfa22b6b1154"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "68f5f1eaa9e8ae59019404a651a7e67fdd9f4f912837f13524eac9d31ff0a05a"
-    sha256 cellar: :any_skip_relocation, ventura:        "189385933c4b3be73f64473f35dbdcb2cb6ccabcbe43c496dfa102598e8cd851"
-    sha256 cellar: :any_skip_relocation, monterey:       "2a19644f8b35fe32d664c0f4b73450722c944ec22d1f7301ef238f77a1189617"
-    sha256 cellar: :any_skip_relocation, big_sur:        "d9e52cb601631326558cc3cf1df2246e61d0f310d1df9512d02b1c1004b26599"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d92d9c8eb9514eb4c75d2f8a15e8c8dcf6fa93f373e2ff238c03dbc766c26dbb"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "9d64958f8af6ddf20ae83867a2bc07ec5b8a97a411f39193986136ab30a96ce4"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "9cc22fc9e7c274d7b93c13c11456dffe8e2cec4d39dae3938fba2e884ada0746"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "285e129d6a6cc946c72442ed2fd796f880494a5c01245086b6d267f007e76a26"
+    sha256 cellar: :any_skip_relocation, ventura:        "08b7f7adb80de26bad97896cc39912d7211464c7e3d6d3f7c1d1f01db0669fb3"
+    sha256 cellar: :any_skip_relocation, monterey:       "7f4d3736bd3a03c6339e08249b308cea72571eda8f5b6f35132747dfb72e43cf"
+    sha256 cellar: :any_skip_relocation, big_sur:        "6879cc031f2362f957473bac067ee238619b3b23f3411a323dba7c1c507f0727"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e37068a0095bb192537389e9ccc7dd20b81d4ae7f9e81e01bff74cc621657fe0"
   end
 
   depends_on "go" => :build
