14b11ad84cd9f38e9404167a82c7ae70dac79890
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-01-17 16:43:27 +0800

    crowdin 3.9.3
    Closes #120783.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/crowdin.rb
+++ Formula/crowdin.rb
@@ -1,8 +1,8 @@
 class Crowdin < Formula
   desc "Command-line tool that allows to manage your resources with crowdin.com"
   homepage "https://support.crowdin.com/cli-tool/"
-  url "https://github.com/crowdin/crowdin-cli/releases/download/3.9.2/crowdin-cli.zip"
-  sha256 "24f353745aa6a612e244472d6edb695d14155e5acd081844e190fda06f91ae90"
+  url "https://github.com/crowdin/crowdin-cli/releases/download/3.9.3/crowdin-cli.zip"
+  sha256 "a7c69b2200805e8fcb5bf0193767ac498564568820436be3e50900b1e1f05c9a"
   license "MIT"
 
   livecheck do
