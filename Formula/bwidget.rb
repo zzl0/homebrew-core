d342c67258460f850eacf5e57506eb23da5276ec
Will Cohen <willcohen@users.noreply.github.com>
2023-01-17 16:08:02 -0500

    bwidget 1.9.16 (new formula)
    * bwidget 1.9.16 (new formula)
      This commit adds an additional Tcl package called bwidget. It is
      required by the Perl package Tcl::pTk.
    * bwidget: update test
    
    Closes #120817.
    
    Co-authored-by: Adrian Ho <the.gromgit@gmail.com>
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/bwidget.rb
@@ -0,0 +1,24 @@
+class Bwidget < Formula
+  desc "Tcl/Tk script-only set of megawidgets to provide the developer additional tools"
+  homepage "https://core.tcl-lang.org/bwidget/home"
+  url "https://downloads.sourceforge.net/project/tcllib/BWidget/1.9.16/bwidget-1.9.16.tar.gz"
+  sha256 "bfe0036374b84293d23620a7f6dda86571813d0c7adfed983c1f337e5ce81ae0"
+  license "TCL"
+
+  depends_on "tcl-tk"
+
+  def install
+    (lib/"bwidget").install Dir["*"]
+  end
+
+  test do
+    # Fails with: no display name and no $DISPLAY environment variable
+    return if OS.linux? && ENV["HOMEBREW_GITHUB_ACTIONS"]
+
+    test_bwidget = <<~EOS
+      puts [package require BWidget]
+      exit
+    EOS
+    assert_equal version.to_s, pipe_output("#{Formula["tcl-tk"].bin}/tclsh", test_bwidget).chomp
+  end
+end
