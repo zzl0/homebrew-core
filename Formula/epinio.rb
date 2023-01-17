f2774ea7e33be946d57962f2e3e5a64ef937499b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-17 17:04:56 +0000

    epinio: update 1.6.0 bottle.

--- Formula/epinio.rb
+++ Formula/epinio.rb
@@ -6,13 +6,13 @@ class Epinio < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "7e49d9d681b1ff94e6a8f04da22d91487b03a02f9e3ed5f8c29274002a1b701a"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0cac8abcd74d7bfb753aa10b6da2a9947bb1ca835a48862342b9dbe52321bf22"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e4932790489a43dce89c54f31191e117d2bf06c0a2f2b69d35daeb4331745709"
-    sha256 cellar: :any_skip_relocation, ventura:        "79881657034c31dab4131927283fde4eb09261514975d8b685f05b2909da079f"
-    sha256 cellar: :any_skip_relocation, monterey:       "f5ef9a94209fe5205aa0f52261be6376d18f1d2d812fa2cc3b48c5355b40768b"
-    sha256 cellar: :any_skip_relocation, big_sur:        "e70eccce938d9ddbb9c40d9dcb0c322f502084a4289f857831626115b626970a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4c8d3f941f0a73283e0d6efae25e79c2ab73dc5cc1bb543fb29016b0348a79a9"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "be14862f70a90035a5733ef712fa1f105df9f82e7cccb87dfcbc283df0a85829"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c37e3b1306d1a3758b5d65f7c04bce221a9ce993d731363feb0982a9f887fc5b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c25d8858626c6417c99811a055ef75c8d8b9f7d1d2e61d540922125b27923117"
+    sha256 cellar: :any_skip_relocation, ventura:        "82e4276b530a5e48ea8f78d149ba003ea4b217d43d854b6e8e730ac13497ef9e"
+    sha256 cellar: :any_skip_relocation, monterey:       "bb1bab94d0e2bf182badd4c98c7d356ed4c62b0772e3beec95c619cee99992af"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ab87b4bf31b53dcaf4e7ee2bfedc2e7a48d24c37f21b41b8084f231a4f6bf61b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "43415f4c965fc553f85e0e1bb6a25bad0c33cc48aea386b104de01057b57701f"
   end
 
   depends_on "go" => :build
