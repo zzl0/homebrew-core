e6629e37655b95922924613b45e58dfec0208e5e
Patrick Linnane <patrick@linnane.io>
2023-01-17 20:16:10 -0800

    copilot 1.25.0
    Closes #120870.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/copilot.rb
+++ Formula/copilot.rb
@@ -4,8 +4,8 @@ class Copilot < Formula
   desc "CLI tool for Amazon ECS and AWS Fargate"
   homepage "https://aws.github.io/copilot-cli/"
   url "https://github.com/aws/copilot-cli.git",
-      tag:      "v1.24.0",
-      revision: "3313b1d4a73bc7871720b9936805361b0bc717f3"
+      tag:      "v1.25.0",
+      revision: "ba44a7ebf4c879a97c3f1ce2ff028bd6357f7893"
   license "Apache-2.0"
   head "https://github.com/aws/copilot-cli.git", branch: "mainline"
 
