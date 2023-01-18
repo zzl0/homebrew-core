d7615d6c41e44ff1fd4d74df6f503fc73d74c4e7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-18 22:06:31 +0000

    karn: update 0.1.0 bottle.

--- Formula/karn.rb
+++ Formula/karn.rb
@@ -6,15 +6,13 @@ class Karn < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "91b3061405fb2c0de965f367a209f05d29c53f01d5ec9ed386980d29ada4b53d"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b074d05548362367465ecd94b64f067b845e7f8aba75e93eefe73eab08293d11"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9fff2a71abf62d3eaedba7d095e3fcfd9fbaa17895dab15fabefc4cd2d5a8725"
-    sha256 cellar: :any_skip_relocation, ventura:        "ec617c79910918e30a4ec52d3a9f254b85b11554a88174b525605ba49dc00b78"
-    sha256 cellar: :any_skip_relocation, monterey:       "a46d49dec3cbcb3e0753ff3b95e38721446670b1cc9cd501d1bedfceb99adb48"
-    sha256 cellar: :any_skip_relocation, big_sur:        "bdcf0389352b56c208549750ec502a1bfc01c02c0466c981abfcace027cc479b"
-    sha256 cellar: :any_skip_relocation, catalina:       "6f9d3e100d55f950b54ee3ada80008209a1f61aefe62ae7f171e9615554b2f93"
-    sha256 cellar: :any_skip_relocation, mojave:         "4d676e8bc136599f4ec3ef0d6cb604b003ced4a0537c190ea430d5b0ca8609cf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "022a73d7ffbf3aff64711ff7f853575c6f4f214db576d54609ec8831d550bbbb"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "2ddacad3313683762032b2d8eb15463f175ed266c8774618c85950eb05492740"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2ddacad3313683762032b2d8eb15463f175ed266c8774618c85950eb05492740"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2ddacad3313683762032b2d8eb15463f175ed266c8774618c85950eb05492740"
+    sha256 cellar: :any_skip_relocation, ventura:        "39702da4deba9720c8e7559835351012e44c5906c7f06d9bff276b845d58c573"
+    sha256 cellar: :any_skip_relocation, monterey:       "39702da4deba9720c8e7559835351012e44c5906c7f06d9bff276b845d58c573"
+    sha256 cellar: :any_skip_relocation, big_sur:        "39702da4deba9720c8e7559835351012e44c5906c7f06d9bff276b845d58c573"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8d05d6010fc0025f1200c6cb81deac286e22b1a5633833fb3eb99619cd0c2fc8"
   end
 
   depends_on "go" => :build
