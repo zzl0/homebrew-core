5c339492f453827ff261baaf283b18d9f2bfd823
Rui Chen <rui@chenrui.dev>
2023-01-17 19:58:31 -0500

    fortio 1.39.1
    Closes #120861.
    
    Signed-off-by: Branch Vincent <19800529+branchvincent@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/fortio.rb
+++ Formula/fortio.rb
@@ -2,8 +2,8 @@ class Fortio < Formula
   desc "HTTP and gRPC load testing and visualization tool and server"
   homepage "https://fortio.org/"
   url "https://github.com/fortio/fortio.git",
-      tag:      "v1.39.0",
-      revision: "cc145c47d7829751bd0f7c9986d2ff432f57b8b2"
+      tag:      "v1.39.1",
+      revision: "d40d4b4a04025e12a96d9e75534a9e40c988c568"
   license "Apache-2.0"
 
   livecheck do
