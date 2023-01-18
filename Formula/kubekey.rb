75f3b7b7b2ed02f4319cb72b7c6f9b44be775dca
Patrick Linnane <patrick@linnane.io>
2023-01-17 20:20:00 -0800

    kubekey 3.0.7
    Closes #120874.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/kubekey.rb
+++ Formula/kubekey.rb
@@ -2,8 +2,8 @@ class Kubekey < Formula
   desc "Installer for Kubernetes and / or KubeSphere, and related cloud-native add-ons"
   homepage "https://kubesphere.io"
   url "https://github.com/kubesphere/kubekey.git",
-      tag:      "v3.0.6",
-      revision: "ec903fe13dfed73ffd3f72f4beec3123675ce4d0"
+      tag:      "v3.0.7",
+      revision: "e755baf67198d565689d7207378174f429b508ba"
   license "Apache-2.0"
   head "https://github.com/kubesphere/kubekey.git", branch: "master"
 
