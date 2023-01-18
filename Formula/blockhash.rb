3156e566f965ed7e018eec32be07f60900541554
Patrick Linnane <patrick@linnane.io>
2023-01-18 11:12:00 -0800

    blockhash 0.3.3
    * blockhash 0.3.3
    * blockhash: migrate to `python@3.11`
    
    Closes #120933.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/blockhash.rb
+++ Formula/blockhash.rb
@@ -1,8 +1,8 @@
 class Blockhash < Formula
   desc "Perceptual image hash calculation tool"
   homepage "https://github.com/commonsmachinery/blockhash"
-  url "https://github.com/commonsmachinery/blockhash/archive/v0.3.2.tar.gz"
-  sha256 "add1e27e43b35dde56e44bc6d1f0556facf4a18a0f9072df04d4134d8f517365"
+  url "https://github.com/commonsmachinery/blockhash/archive/v0.3.3.tar.gz"
+  sha256 "3c48af7bdb1f673b2f3c9f8c0bfa9107a7019b54ac3b4e30964bc0707debdd3a"
   license "MIT"
   head "https://github.com/commonsmachinery/blockhash.git", branch: "master"
 
@@ -19,7 +19,7 @@ class Blockhash < Formula
   end
 
   depends_on "pkg-config" => :build
-  depends_on "python@3.10" => :build
+  depends_on "python@3.11" => :build
   depends_on "imagemagick"
 
   resource "homebrew-testdata" do
@@ -28,7 +28,7 @@ class Blockhash < Formula
   end
 
   def install
-    python3 = "python3.10"
+    python3 = "python3.11"
     system python3, "./waf", "configure", "--prefix=#{prefix}"
     # pkg-config adds -fopenmp flag during configuring
     # This fails the build on system clang, and OpenMP is not used in blockhash
