3e718f0b0a3575f8e7c31840014d63b71639bcf7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-18 06:05:32 +0000

    argocd: update 2.5.7 bottle.

--- Formula/argocd.rb
+++ Formula/argocd.rb
@@ -7,13 +7,13 @@ class Argocd < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "1d562d71f15e3dd7da44b5d9c70277d44fd531c29e37f4989f2104e9c55a51ca"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "945371241f06cf6d952fc661cf2deebeebcf426e8b43c53cfe3991825155b595"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8d2b439b2a6025487eab88bbc97ab7aa19517590083f6a30f13dc8c1fd83e88f"
-    sha256 cellar: :any_skip_relocation, ventura:        "2a7c1a2d0e5a1064609ae814560c1673887c9575275527b38755e921c83e7f26"
-    sha256 cellar: :any_skip_relocation, monterey:       "93d468b00a119494d1ba9064d3d99b3103e114acc8c7743578a3050dc0e6a323"
-    sha256 cellar: :any_skip_relocation, big_sur:        "6305603cd211c6d260651ab8772af3398604ff35a01c64f90f4b30ee0c3bbd26"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "defd4eb39322a2c6d819d533a48d764aa30994b1c728e28a4e628d0bd669fe13"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "86e09f89f6122caeac8d6304bc69f195bd68fa792c82f495474839802e59058b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2c12f3251beb2c13773b6d01f00001dda54c5264cd9dd1e8b6f0e2bb31c93a7a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5725c13f9426a8e7edccc0b922d8d3d038fbf1d368bee369ecbbae1635ee2c61"
+    sha256 cellar: :any_skip_relocation, ventura:        "2ec0771fcd49aeab4689c2f6582d7006da0d10abd613ee2e06f4593e047ec4d6"
+    sha256 cellar: :any_skip_relocation, monterey:       "7a0dc5e1316d5ccb88e1c81c28cd2a6779d098eac470438f95af161e3becb80e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "c334c288c47a764d170ac2f3c34e98919ef3db313dc98f7638487159e82fac5f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e91fa7b5a4ff0de676bc93b5b963ac84f3be9a7d4ca2f7024c8dc3d5da2816ac"
   end
 
   depends_on "go" => :build
