d9d7e55e020eaa63b0ec95ca46a92d09e527ee09
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-18 06:04:58 +0000

    bowtie2: update 2.5.1 bottle.

--- Formula/bowtie2.rb
+++ Formula/bowtie2.rb
@@ -6,14 +6,13 @@ class Bowtie2 < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "2105ca0b03e8362c3f700da25f436e7aaa3a296dc6f09a511e18bd004c0bef01"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f4682917b22c7bc0f3ce2ef3baad4f1d95c510cf5c7639177ee07612c5783e75"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "91bce9d10f339a8c83595112691b2b6205fc6d4e61e63d46f2f8f7c32d8380a0"
-    sha256 cellar: :any_skip_relocation, ventura:        "4558f49d23397aac8d27c0a2c14529b57af08f894e89474ff914dc067addb2ee"
-    sha256 cellar: :any_skip_relocation, monterey:       "a2067f6944a3e80808476582ba2dcd3da72d791bb8c9371d4c8dadf47749d34c"
-    sha256 cellar: :any_skip_relocation, big_sur:        "8086d31b98a4fee884fc459cb01f33c98e57fedbb9651273068e3709e54736b0"
-    sha256 cellar: :any_skip_relocation, catalina:       "5bd8be0504e7806243d33210f54de2c791521af72aa35897b33e14ec1aa734b4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d83bdfb506a8e464f6adec0342f12a3da423a01942314a853e962230595610c7"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "6a5f45f3bf45cffefc8598581d5547169d8e07f6e3c1efd31898e36582eebff0"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "60d96c890b137f91f46544d867f98186d03592848e2e77939c8af952c5b39047"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8421e1f412403865337c4a9af25a7144ee89d691367864ff91440d7faa73a420"
+    sha256 cellar: :any_skip_relocation, ventura:        "594c8dcbfbb4acdc5628a75eca10e691dbf7fa52602a26295618d6291705c73d"
+    sha256 cellar: :any_skip_relocation, monterey:       "e59ed1f423747d14edd5cc80c1f5985f622e00b17babfd7343e7e2fd1e730468"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f9e5eeb5344bc134741c981997984622105ee104e7bcdc301e86a289b98ba9b2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "825b7afefdf1fe78b37362eda6e6706e3c850c955fdd01fba714232ff187051d"
   end
 
   uses_from_macos "perl"
