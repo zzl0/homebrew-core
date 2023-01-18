0377bb153c63643f1e254915ea73e6e27debab4d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-18 15:22:12 +0000

    nano: update 7.2 bottle.

--- Formula/nano.rb
+++ Formula/nano.rb
@@ -11,13 +11,13 @@ class Nano < Formula
   end
 
   bottle do
-    sha256 arm64_ventura:  "c202cf2b6b1251f0caed55ffa083f71e6b63d17d0c9dd3d3673b133b85cc5e79"
-    sha256 arm64_monterey: "b96c11037d0cb45f726a3ac0f02f9312d6327217781dbeabaf6f28fd25b1cae2"
-    sha256 arm64_big_sur:  "cf1620dade11f6ecac23c16ea62ce40db48bc4583b6d0312c35585488e29d273"
-    sha256 ventura:        "6e81fda89fd4f2acc937a596b5a52eac88ba497f2a15ed73c66f898edd39f411"
-    sha256 monterey:       "7efca387c11b43f3342593b170c7a5b843780fe41b9831b09b600603d84ce0d2"
-    sha256 big_sur:        "050af59bf8fc46e40ad25566756c0aee526603e86f03da49e2d48239707ac452"
-    sha256 x86_64_linux:   "eb541e3efe8242f2b5d758e2c0c3467f0b2bf4cc8d992b31ce72f2c0e99f0022"
+    sha256 arm64_ventura:  "b12fe58c8d442f8f338d319d7934fe439d441cb15da40a8db25b709226cf3a3e"
+    sha256 arm64_monterey: "f0554d184323c34a57cef42df8fd3b56afd723af5e97275a3a5628220d4e8e9a"
+    sha256 arm64_big_sur:  "50fbf0b54f56afe0a05c98b393e61257c965eca162a32367583d6bf8bf34865c"
+    sha256 ventura:        "2a27a1f2d44f1c82388d8952f05bfcf6ffadc0d08c87a6f62bbda2eda4d50826"
+    sha256 monterey:       "db212d2c6de758fc9c0c213ae5285dbb3bbf6978363548cafa4ac3af356a7b75"
+    sha256 big_sur:        "65b76ac9bce041b20a5a91d1ff21e511c28f4995f7e8a604395eed57d35c5b10"
+    sha256 x86_64_linux:   "e29112cb799708f597542f8bd8bb13fa0a7ba2807ec2634892e78536c08939a9"
   end
 
   depends_on "pkg-config" => :build
