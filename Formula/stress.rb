0a3638869b801dc89295a50cda6e1c4ae07c13e8
Patrick Linnane <patrick@linnane.io>
2023-01-17 20:41:41 -0800

    stress 1.0.6
    * stress 1.0.6
    * stress: add upstream build patch
    
    Closes #120878.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/stress.rb
+++ Formula/stress.rb
@@ -1,14 +1,9 @@
 class Stress < Formula
   desc "Tool to impose load on and stress test a computer system"
-  homepage "https://web.archive.org/web/20190702093856/https://people.seas.harvard.edu/~apw/stress/"
-  url "https://deb.debian.org/debian/pool/main/s/stress/stress_1.0.5.orig.tar.gz"
-  sha256 "1798e49ca365d928fb194ba1b8e8d1e09963b49e9edb0a78bcbba15750bb5027"
+  homepage "https://github.com/resurrecting-open-source-projects/stress"
+  url "https://github.com/resurrecting-open-source-projects/stress/archive/refs/tags/1.0.6.tar.gz"
+  sha256 "480e879cd7d9a835eca7c145949f3e5c5dd644e503ab2bf6309cae7672b2fa68"
   license "GPL-2.0-or-later"
-
-  livecheck do
-    url "https://deb.debian.org/debian/pool/main/s/stress/"
-    regex(/href=.*?stress[._-]v?(\d+(?:\.\d+)+)\.orig\.t/i)
-  end
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "682417d0cce887de4bfad5dbf1382917cda827e21b760f890a74d1856e45915d"
@@ -25,6 +20,10 @@ class Stress < Formula
   depends_on "autoconf" => :build
   depends_on "automake" => :build
 
+  # Fix build on macOS, remove in next release
+  # upstream patch, https://github.com/vbendel/stress/commit/7ae7ebe50f389054f5a78873867b5eef92b8cf97
+  patch :DATA
+
   def install
     system "./autogen.sh"
     system "./configure", "--disable-dependency-tracking",
@@ -36,3 +35,108 @@ class Stress < Formula
     system bin/"stress", "--cpu", "2", "--io", "1", "--vm", "1", "--vm-bytes", "128M", "--timeout", "1s", "--verbose"
   end
 end
+
+__END__
+diff --git a/configure.ac b/configure.ac
+index d9de2ee..7679930 100644
+--- a/configure.ac
++++ b/configure.ac
+@@ -10,7 +10,7 @@ AC_PROG_INSTALL
+ AC_PROG_MAKE_SET
+
+ # Checks for header files.
+-AC_CHECK_HEADERS([stdlib.h string.h unistd.h])
++AC_CHECK_HEADERS([stdlib.h string.h unistd.h sys/prctl.h])
+
+ # Checks for library functions.
+ AC_FUNC_FORK
+diff --git a/src/stress.c b/src/stress.c
+index 7cf3583..74888e5 100644
+--- a/src/stress.c
++++ b/src/stress.c
+@@ -29,10 +29,13 @@
+ #include <signal.h>
+ #include <time.h>
+ #include <unistd.h>
+-#include <sys/prctl.h>
+ #include <sys/wait.h>
+ #include "config.h"
+
++#ifdef HAVE_SYS_PRCTL_H
++  #include <sys/prctl.h>
++#endif
++
+ /* By default, print all messages of severity info and above.  */
+ static int global_debug = 2;
+
+@@ -71,6 +74,7 @@ int usage (int status);
+ int version (int status);
+ long long atoll_s (const char *nptr);
+ long long atoll_b (const char *nptr);
++void worker_init(void);
+
+ /* Prototypes for worker functions.  */
+ int hogcpu (void);
+@@ -298,8 +302,7 @@ main (int argc, char **argv)
+             switch (pid = fork ())
+             {
+             case 0:            /* child */
+-                if (prctl(PR_SET_PDEATHSIG, SIGTERM) == -1)
+-                    exit(0);
++                worker_init();
+                 alarm (timeout);
+                 usleep (backoff);
+                 if (do_dryrun)
+@@ -321,8 +324,7 @@ main (int argc, char **argv)
+             switch (pid = fork ())
+             {
+             case 0:            /* child */
+-                if (prctl(PR_SET_PDEATHSIG, SIGTERM) == -1)
+-                    exit(0);
++                worker_init();
+                 alarm (timeout);
+                 usleep (backoff);
+                 if (do_dryrun)
+@@ -343,14 +345,12 @@ main (int argc, char **argv)
+             switch (pid = fork ())
+             {
+             case 0:            /* child */
+-                if (prctl(PR_SET_PDEATHSIG, SIGTERM) == -1)
+-                    exit(0);
++                worker_init();
+                 alarm (timeout);
+                 usleep (backoff);
+                 if (do_dryrun)
+                     exit (0);
+-                exit (hogvm
+-                      (do_vm_bytes, do_vm_stride, do_vm_hang, do_vm_keep));
++                exit (hogvm (do_vm_bytes, do_vm_stride, do_vm_hang, do_vm_keep));
+             case -1:           /* error */
+                 err (stderr, "fork failed: %s\n", strerror (errno));
+                 break;
+@@ -366,8 +366,7 @@ main (int argc, char **argv)
+             switch (pid = fork ())
+             {
+             case 0:            /* child */
+-                if (prctl(PR_SET_PDEATHSIG, SIGTERM) == -1)
+-                    exit(0);
++                worker_init();
+                 alarm (timeout);
+                 usleep (backoff);
+                 if (do_dryrun)
+@@ -469,6 +468,15 @@ main (int argc, char **argv)
+     exit (retval);
+ }
+
++
++void worker_init(void)
++{
++#ifdef HAVE_SYS_PRCTL_H
++  if (prctl(PR_SET_PDEATHSIG, SIGTERM) == -1)
++    exit(0);
++#endif
++}
++
+ int
+ hogcpu (void)
+ {
