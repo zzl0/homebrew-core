c962e702870e69ae44baecf74c7ac6c30f43567d
Pavel Zwerschke <pavelzw@gmail.com>
2023-01-18 18:32:31 +0100

    micromamba 1.3.0 (new formula)
    Closes #122551.
    
    Co-authored-by: Yasin Tatar <ytatar9@googlemail.com>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/micromamba.rb
@@ -0,0 +1,75 @@
+class Micromamba < Formula
+  desc "Fast Cross-Platform Package Manager"
+  homepage "https://github.com/mamba-org/mamba"
+  url "https://github.com/mamba-org/mamba/archive/refs/tags/2023.02.03.tar.gz"
+  version "1.3.0"
+  sha256 "4c794246dd6008caebebef8c6266e6ba3f9ff5ce48908c830e448811587e9921"
+  license "BSD-3-Clause"
+
+  depends_on "cli11" => :build
+  depends_on "cmake" => :build
+  depends_on "nlohmann-json" => :build
+  depends_on "spdlog" => :build
+  depends_on "tl-expected" => :build
+  depends_on "fmt"
+  depends_on "libsolv"
+  depends_on "lz4"
+  depends_on "openssl@1.1"
+  depends_on "reproc"
+  depends_on "xz"
+  depends_on "yaml-cpp"
+  depends_on "zstd"
+
+  uses_from_macos "python" => :build
+  uses_from_macos "curl", since: :monterey # uses CURLINFO_RETRY_AFTER, available since curl 7.66.0
+  uses_from_macos "krb5"
+  uses_from_macos "libarchive", since: :monterey
+  uses_from_macos "zlib"
+
+  resource "libarchive-headers" do
+    on_monterey :or_newer do
+      url "https://github.com/apple-oss-distributions/libarchive/archive/refs/tags/libarchive-113.tar.gz"
+      sha256 "b422c37cc5f9ec876d927768745423ac3aae2d2a85686bc627b97e22d686930f"
+    end
+  end
+
+  def install
+    args = %W[
+      -DBUILD_LIBMAMBA=ON
+      -DBUILD_SHARED=ON
+      -DBUILD_MICROMAMBA=ON
+      -DMICROMAMBA_LINKAGE=DYNAMIC
+      -DCMAKE_INSTALL_RPATH=#{rpath}
+    ]
+
+    if OS.mac? && MacOS.version >= :monterey
+      resource("libarchive-headers").stage do
+        cd "libarchive/libarchive" do
+          (buildpath/"homebrew/include").install "archive.h", "archive_entry.h"
+        end
+      end
+      args << "-DLibArchive_INCLUDE_DIR=#{buildpath}/homebrew/include"
+      ENV.append_to_cflags "-I#{buildpath}/homebrew/include"
+    end
+
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  def caveats
+    <<~EOS
+      Please run the following to setup your shell:
+        #{opt_bin}/micromamba shell init -s <your-shell> -p ~/micromamba
+      and restart your terminal.
+    EOS
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/micromamba --version").strip
+
+    python_version = "3.9.13"
+    system "#{bin}/micromamba", "create", "-n", "test", "python=#{python_version}", "-y", "-c", "conda-forge"
+    assert_match "Python #{python_version}", shell_output("#{bin}/micromamba run -n test python --version").strip
+  end
+end
