b6d35b9f624cb69c2816b91041357dfa50af4506
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-18 17:04:15 +0000

    nagios-plugins: update 2.4.3 bottle.

--- Formula/nagios-plugins.rb
+++ Formula/nagios-plugins.rb
@@ -7,13 +7,13 @@ class NagiosPlugins < Formula
   head "https://github.com/nagios-plugins/nagios-plugins.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any, arm64_ventura:  "63ee1204cec7047eed4b5918b050f7c4b967c3da5e16870ed9bea02a05555d97"
-    sha256 cellar: :any, arm64_monterey: "52a9da57c6482d6db104d60111b65232e4c634328ed22d2ee37ccc02c850e6c5"
-    sha256 cellar: :any, arm64_big_sur:  "966e9d28573f5e197ce9dae6f86ad5459a3af4aef54380b0639649925bac39a4"
-    sha256 cellar: :any, ventura:        "5160ba69a59c03858756ef81c665cb5df1e9357e8cd6c5c1f21e1414f4248d60"
-    sha256 cellar: :any, monterey:       "59d48c969aafa22d69f8aff8761c41784f97cb09bc93ca99c897239ba2f77344"
-    sha256 cellar: :any, big_sur:        "3086d491e7bb155e149fcf7cf8266f249ff34776e0e42d53a02fc7f9fc0bd865"
-    sha256               x86_64_linux:   "06553b8d5727460be7a4b20a52ac714d4a5cc2997c7c779b660eee4ed117653a"
+    sha256 cellar: :any, arm64_ventura:  "43a9d1d5eb86b6acabf2f160a686e5498fbff4cdc0de62568af2bdd98897939e"
+    sha256 cellar: :any, arm64_monterey: "9ef5dd8a0cd8af4971d13489786b37e34e2c19d80d477c81a0b7daf97bf87387"
+    sha256 cellar: :any, arm64_big_sur:  "29e6ccd18dfe1e41952ca10a0f62fb21fe13a2c524be8bfbecce372a068f2bff"
+    sha256 cellar: :any, ventura:        "cdd09933e8e53624dc05d83afb7033ba4ec20722faa595166b97ed740866946e"
+    sha256 cellar: :any, monterey:       "d1ec2d9ab16dacc476bffd52296bb9703c6457f187dc3720b4b334cf8319eb72"
+    sha256 cellar: :any, big_sur:        "a1e7a1f63e645dba0c33231ab053fd2ef67366ca4836f5a76178585a045f3f2f"
+    sha256               x86_64_linux:   "bae16216eb65550deac5f0c373870f541e745832e49dae4e99bcc2a722a26fe5"
   end
 
   depends_on "autoconf" => :build
