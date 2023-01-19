0d5e8bbc111ff0285acec5c2ada0330dc0006048
Rui Chen <rui@chenrui.dev>
2023-01-19 07:28:27 -0500

    sdl2_image: revision bump (jpeg-xl 0.8.0)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/sdl2_image.rb
+++ Formula/sdl2_image.rb
@@ -4,7 +4,7 @@ class Sdl2Image < Formula
   url "https://github.com/libsdl-org/SDL_image/releases/download/release-2.6.2/SDL2_image-2.6.2.tar.gz"
   sha256 "48355fb4d8d00bac639cd1c4f4a7661c4afef2c212af60b340e06b7059814777"
   license "Zlib"
-  revision 1
+  revision 2
 
   # This formula uses a file from a GitHub release, so we check the latest
   # release version instead of Git tags.
