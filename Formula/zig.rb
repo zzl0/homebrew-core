2f35b2eb5a73832e1acd001ae993884e2298db69
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-19 19:05:40 +0000

    zig: update 0.10.1 bottle.

--- Formula/zig.rb
+++ Formula/zig.rb
@@ -7,13 +7,13 @@ class Zig < Formula
   head "https://github.com/ziglang/zig.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "a3908bf383d7754fbc4875440c9b8392e9c4bc5bb9a0f4e62f80d2ea0044414b"
-    sha256 cellar: :any,                 arm64_monterey: "c9c484c3a6161663eda02c97a2898629e2c82576105e47f32cd9e4e741545f66"
-    sha256 cellar: :any,                 arm64_big_sur:  "5ec09008b3393fc4fb4793a39c9938776f69695ea51b509ad3c68a6d6be00e5e"
-    sha256 cellar: :any,                 ventura:        "d52517dc0a1e61ab460bc4453e616c3e18f3b088b6d66e16eda12a70815aead5"
-    sha256 cellar: :any,                 monterey:       "fc156c0ddf4bb31e6258c905acc3121fda3ecb7930385d7baa245099cb7d90ef"
-    sha256 cellar: :any,                 big_sur:        "42647236a8dee891946d8e399cf06d07c3f16e46eaf83db460f1443e8013c52b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "900d27fc1b3a98e1be76db86621b76d195336712a409e5088223e230dd12d76d"
+    sha256 cellar: :any,                 arm64_ventura:  "df936652f951eae31e4faf5c878d613e4ced6028a1fa555047ffec9fc7f02cd7"
+    sha256 cellar: :any,                 arm64_monterey: "0db450570200f124aed4d3750f6472491424a47dbcff91418ca84a649201819a"
+    sha256 cellar: :any,                 arm64_big_sur:  "1c3190b58e7c36c3ebe5f00c5423d65a502eedd422992b8e28b223cae17c67d3"
+    sha256 cellar: :any,                 ventura:        "2deaff1dfc54e41f92adf130167a644d57d38cca7c074f5c1054a9c628b71a30"
+    sha256 cellar: :any,                 monterey:       "e7c66e2a7d7d94af2844132e20913810d821fdb0f901f2336aa8ee88b39c93f0"
+    sha256 cellar: :any,                 big_sur:        "171497e2e4e8cd729c3bdd332bb26c3ae820cbfabff1d0876e7c6101c47a0703"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "99a414e113877e3348717d54b4fe4b6e4e78db2b179a1ddce5bdf3be48af04f7"
   end
 
   depends_on "cmake" => :build
