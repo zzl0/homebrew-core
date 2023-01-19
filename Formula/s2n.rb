bc5feedc538c4ab7c2ca5c0ecf1051c4cd7605f3
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-01-19 11:00:15 +0800

    s2n 1.3.33
    Closes #120981.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/s2n.rb
+++ Formula/s2n.rb
@@ -1,8 +1,8 @@
 class S2n < Formula
   desc "Implementation of the TLS/SSL protocols"
   homepage "https://github.com/aws/s2n-tls"
-  url "https://github.com/aws/s2n-tls/archive/v1.3.32.tar.gz"
-  sha256 "01f3db28a091004afa19ecf1324151857146b80cbe421b875f27bc4284454212"
+  url "https://github.com/aws/s2n-tls/archive/v1.3.33.tar.gz"
+  sha256 "142959030f7fb5e08774e96682003fc8445b99b32d7b1a56cce7760cebc761ec"
   license "Apache-2.0"
   head "https://github.com/aws/s2n-tls.git", branch: "main"
 
