345242f25b6b84d95c971bc3478c6b37b5300b11
Porkepix <porkepix@gmail.com>
2023-01-19 11:51:11 +0100

    luajit-openresty 2.1-20230119
    Closes #121016.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/luajit-openresty.rb
+++ Formula/luajit-openresty.rb
@@ -1,8 +1,8 @@
 class LuajitOpenresty < Formula
   desc "OpenResty's Branch of LuaJIT 2"
   homepage "https://github.com/openresty/luajit2"
-  url "https://github.com/openresty/luajit2/archive/refs/tags/v2.1-20220915.tar.gz"
-  sha256 "a6104d4fa342d864ae00cb3313b07091af02dc61294b1d8fce1e17779998b497"
+  url "https://github.com/openresty/luajit2/archive/refs/tags/v2.1-20230119.tar.gz"
+  sha256 "4133bb04e239bd3282bbad84edb536b3947165aba01da6886e888322bf740e1c"
   license "MIT"
   version_scheme 1
   head "https://github.com/openresty/luajit2.git", branch: "v2.1-agentzh"
