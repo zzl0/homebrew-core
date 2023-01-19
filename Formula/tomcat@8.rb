3abc9cc6ab0293e0ed5fb212736a45b3d850b193
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-01-20 00:59:53 +0800

    tomcat@8 8.5.85
    Closes #121038.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tomcat@8.rb
+++ Formula/tomcat@8.rb
@@ -1,9 +1,9 @@
 class TomcatAT8 < Formula
   desc "Implementation of Java Servlet and JavaServer Pages"
   homepage "https://tomcat.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=tomcat/tomcat-8/v8.5.84/bin/apache-tomcat-8.5.84.tar.gz"
-  mirror "https://archive.apache.org/dist/tomcat/tomcat-8/v8.5.84/bin/apache-tomcat-8.5.84.tar.gz"
-  sha256 "d7123ddee6f66d34e6fb33b669bdd6ebc77832bf123516474c299f3cdee3efdf"
+  url "https://www.apache.org/dyn/closer.lua?path=tomcat/tomcat-8/v8.5.85/bin/apache-tomcat-8.5.85.tar.gz"
+  mirror "https://archive.apache.org/dist/tomcat/tomcat-8/v8.5.85/bin/apache-tomcat-8.5.85.tar.gz"
+  sha256 "0e86bb75e7166ebe6d48527727744245ec03e1be81797c6f5855af2d86812d97"
   license "Apache-2.0"
 
   livecheck do
