241f9232e9a86e5666b7bd00d32f033375bd655b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-19 13:09:28 +0000

    pnpm: update 7.25.1 bottle.

--- Formula/pnpm.rb
+++ Formula/pnpm.rb
@@ -13,13 +13,13 @@ class Pnpm < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "70f8dca9e4b5e8f1e4b705ff2e8763efd9e6bf9ef32e85cf0454b34690c10161"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "70f8dca9e4b5e8f1e4b705ff2e8763efd9e6bf9ef32e85cf0454b34690c10161"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "70f8dca9e4b5e8f1e4b705ff2e8763efd9e6bf9ef32e85cf0454b34690c10161"
-    sha256 cellar: :any_skip_relocation, ventura:        "55bba8cc31c65b98ec096a67b6de8598c004cc245aea9c15e8385fcd11bcf009"
-    sha256 cellar: :any_skip_relocation, monterey:       "55bba8cc31c65b98ec096a67b6de8598c004cc245aea9c15e8385fcd11bcf009"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b9ba2c4365d20c40aee62b70445ed3d8df3ed09955cc2130e5f56c249dc61898"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "70f8dca9e4b5e8f1e4b705ff2e8763efd9e6bf9ef32e85cf0454b34690c10161"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3ccf60e5b24098eb2d5e51810b6c668f51881ee70ec5aa8b59eb5fbd3622abb5"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3ccf60e5b24098eb2d5e51810b6c668f51881ee70ec5aa8b59eb5fbd3622abb5"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3ccf60e5b24098eb2d5e51810b6c668f51881ee70ec5aa8b59eb5fbd3622abb5"
+    sha256 cellar: :any_skip_relocation, ventura:        "fe0bc1c5e354fa3b048e9b515bc37c36a130d120dc8c94ad7a2d3a9bc6851556"
+    sha256 cellar: :any_skip_relocation, monterey:       "fe0bc1c5e354fa3b048e9b515bc37c36a130d120dc8c94ad7a2d3a9bc6851556"
+    sha256 cellar: :any_skip_relocation, big_sur:        "8adc2c3aa667aae9b01a3fa49e6d07c4bb4b79aea0c96d043eaa8a12bb88b73d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3ccf60e5b24098eb2d5e51810b6c668f51881ee70ec5aa8b59eb5fbd3622abb5"
   end
 
   depends_on "node" => :test
