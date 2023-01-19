b82d5657642470d155c6d8cbc9fb90adf0088813
Dustin Rodrigues <dust.rod@gmail.com>
2023-01-18 21:28:20 -0500

    pywhat: remove unused six resource
    Closes #120977.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pywhat.rb
+++ Formula/pywhat.rb
@@ -22,7 +22,6 @@ class Pywhat < Formula
   end
 
   depends_on "python@3.11"
-  depends_on "six"
 
   resource "click" do
     url "https://files.pythonhosted.org/packages/27/6f/be940c8b1f1d69daceeb0032fee6c34d7bd70e3e649ccac0951500b4720e/click-7.1.2.tar.gz"
