e935ac29b9bd9c396304a687bd469afc6177286f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-20 04:04:37 +0000

    plz-cli: update 0.1.7 bottle.

--- Formula/plz-cli.rb
+++ Formula/plz-cli.rb
@@ -7,13 +7,13 @@ class PlzCli < Formula
   head "https://github.com/m1guelpf/plz-cli.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "52f9b33a7c71716c449f149917edded90c289e4b4b3a686915e7a2d215209608"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "65a068de71817e7420350a8ad4cc6e8b80e9a4082b9356f4acbf6372037055c9"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "864faf853dc0a8296d7713a49ecd346aa9b5c2b8609ae83bc1b8eef935239c8d"
-    sha256 cellar: :any_skip_relocation, ventura:        "ec81d160a3e163bd8b438719061a3540743c79aca1580947c84efd224a234cea"
-    sha256 cellar: :any_skip_relocation, monterey:       "5162ed279aa33bb15a122d632b87b73c4ee4f3b383f6b2cb5bad69db4da2d787"
-    sha256 cellar: :any_skip_relocation, big_sur:        "4ccfd187e4f9ef17b489e4d3ad7d93239d76dd19e4268a858d63c88e8b88b0fb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "843e67cdcad7210a37f73ea3c4e0f42ac536b1d26eb232b159eeae7e55e39ba0"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "7b759c8bd3faecb16a8255d4cb579ab31889d6a65dd9cddff8870362e3befb83"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5e445e675441a360cdd0cac091975920d7c933a00952d25ffacf975ecd6032cb"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "524a9879589ba979fcbeb074419a9924d969287acfff3cf5295638db690a1375"
+    sha256 cellar: :any_skip_relocation, ventura:        "66b64a8fe94c38f8faa25f0ef8057a737a86979efe91f9d53a6bde6403d50eb5"
+    sha256 cellar: :any_skip_relocation, monterey:       "de1b6da32b7ba0f9798ee454eea129b26cb2c86240f24d53cff25766eaa65f92"
+    sha256 cellar: :any_skip_relocation, big_sur:        "94599fd498263edc0cf61c72ab4f88980e2961a6d5c9fac7a54910e077e868a1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cda5b1b67611359fdca995c6a2fb68a3dde49ae6aa02bb0cc91b69f48eb7d48c"
   end
 
   depends_on "rust" => :build
