1af391ce2246c9f68e3f95d26de564064f4871a0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-20 05:04:38 +0000

    firebase-cli: update 11.21.0 bottle.

--- Formula/firebase-cli.rb
+++ Formula/firebase-cli.rb
@@ -9,13 +9,13 @@ class FirebaseCli < Formula
   head "https://github.com/firebase/firebase-tools.git", branch: "master"
 
   bottle do
-    sha256                               arm64_ventura:  "06e5d17ced341c87837bf2f62571d025fdffe6a66059e013c6434c634366973b"
-    sha256                               arm64_monterey: "29ec8747e934ee232a9c01845affb85a68749a80770c990516fe5358ffd3af72"
-    sha256                               arm64_big_sur:  "18dd41e334c94d734aea17ebd3a02e8102bdf9adab3aa46844d331d1b7060cc2"
-    sha256 cellar: :any_skip_relocation, ventura:        "bc225060dcc4b7a2c01d543106ead0b98ef03ca1aad769000fcae3150787cf76"
-    sha256 cellar: :any_skip_relocation, monterey:       "2fe93d13ab604ee8b39ffb7e314219ecb9db7e336fa29d595baee37e70a833f2"
-    sha256 cellar: :any_skip_relocation, big_sur:        "2fe93d13ab604ee8b39ffb7e314219ecb9db7e336fa29d595baee37e70a833f2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "feb2668a2a1118913daae6779c86ad54196282c1eafa5e7e2d8e8230abb0a21a"
+    sha256                               arm64_ventura:  "c993a87d8590e0cde888b44c0214e455d7937fdeca2f182704b567def4f50f8e"
+    sha256                               arm64_monterey: "5b9aab3edb119a35130d03a887c57209eb8771522400f504920f7494c73b5915"
+    sha256                               arm64_big_sur:  "f1d1df1a9247fcd80f0ccaf2255e0ddf2b9120e1c3c574c41be2da3d261b90fd"
+    sha256 cellar: :any_skip_relocation, ventura:        "4b2ece6218dc65e6461d6f7b883a07843b69220a2b7f4a2c73321a0f0ad57789"
+    sha256 cellar: :any_skip_relocation, monterey:       "4b2ece6218dc65e6461d6f7b883a07843b69220a2b7f4a2c73321a0f0ad57789"
+    sha256 cellar: :any_skip_relocation, big_sur:        "4b2ece6218dc65e6461d6f7b883a07843b69220a2b7f4a2c73321a0f0ad57789"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c119fcc5d1be2eccc4900fd46e84ec5ee5ed1ddf170d6d57eaba20a2ace60d67"
   end
 
   depends_on "node"
