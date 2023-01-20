2c8f5768dc34efad0af64bfd6345f61d1d179706
Artur <arturcic@gmail.com>
2023-01-20 14:14:49 +0000

    gitversion 5.12.0
    Closes #121098.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gitversion.rb
+++ Formula/gitversion.rb
@@ -2,10 +2,9 @@ class Gitversion < Formula
   desc "Easy semantic versioning for projects using Git"
   homepage "https://gitversion.net"
   # TODO: Switch `dotnet@6` to `dotnet` with v6 release
-  url "https://github.com/GitTools/GitVersion/archive/5.11.1.tar.gz"
-  sha256 "98ed28bfb22fadde72da412634f309d81030a76997ca998e1b34edc39beff489"
+  url "https://github.com/GitTools/GitVersion/archive/5.12.0.tar.gz"
+  sha256 "fe2ecbd2d63a4458f19eb9f0ee6853b5041e8b2f6d7c75b0fa606be2d1a81476"
   license "MIT"
-  revision 1
 
   bottle do
     sha256 cellar: :any,                 arm64_ventura:  "dab9486fded11ba18ab033fdca31afca9ff5aad0456ee48c40a65d87fc2dbadf"
