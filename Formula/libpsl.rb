92ab573105e53864a6c9c73f3d91fece5b4cdbcc
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2023-01-20 15:10:48 +0800

    libpsl: include built-in PSL data
    Upstream includes this in default builds, so I'm not sure why we would
    disable this.
    
    We can also update the test to avoid regressions in the future.
    
    Fixes #121081.
    
    Closes #121083.
    
    Co-authored-by: Charles Kerr <charles@charleskerr.com>
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libpsl.rb
+++ Formula/libpsl.rb
@@ -23,26 +23,30 @@ class Libpsl < Formula
   depends_on "icu4c"
 
   def install
-    system "meson", "setup", *std_meson_args, "build", "-Druntime=libicu", "-Dbuiltin=false"
+    system "meson", "setup", "build", "-Druntime=libicu", "-Dbuiltin=true", *std_meson_args
     system "meson", "compile", "-C", "build"
     system "meson", "install", "-C", "build"
   end
 
   test do
     (testpath/"test.c").write <<~EOS
-      #include <stdio.h>
-      #include <libpsl.h>
       #include <assert.h>
+      #include <stdio.h>
+      #include <string.h>
+
+      #include <libpsl.h>
 
       int main(void)
       {
-          const char *domain = ".eu";
-          const char *cookie_domain = ".eu";
           const psl_ctx_t *psl = psl_builtin();
 
+          const char *domain = ".eu";
           assert(psl_is_public_suffix(psl, domain));
 
-          psl_free(psl);
+          const char *host = "www.example.com";
+          const char *expected_domain = "example.com";
+          const char *actual_domain = psl_registrable_domain(psl, host);
+          assert(strcmp(actual_domain, expected_domain) == 0);
 
           return 0;
       }
