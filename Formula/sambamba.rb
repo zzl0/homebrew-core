0dd86fd42b1863c66d07e69c2179d55e562b3ad7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-21 05:04:26 +0000

    sambamba: update 1.0.0 bottle.

--- Formula/sambamba.rb
+++ Formula/sambamba.rb
@@ -6,14 +6,13 @@ class Sambamba < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "95169e54575022eecd424edf0555e69a192b5eb0956f8b609ae6886d4afcfb93"
-    sha256 cellar: :any,                 arm64_monterey: "8fa5db4c381cdb2dd7f62dec9e2eba2acdeceaea3d62809fde83d30cca6624e4"
-    sha256 cellar: :any,                 arm64_big_sur:  "8a2c3933eee3a6b8a8bc79828edc8f3b102a0172496081b1de124b763b698c5a"
-    sha256 cellar: :any,                 ventura:        "497d638b63b6a95f8a07f57ccedb2d3631a793c5df4c4e89803e5748786a6c13"
-    sha256 cellar: :any,                 monterey:       "9b9676ff4648e7fccfb38b9759b6f860ebde95ab82a5db5817b1c71323ce0c48"
-    sha256 cellar: :any,                 big_sur:        "2bb46c59c474b37504d4c20bd0cb5d218c0ac8260bcd31717a7b8c1cf87de8eb"
-    sha256 cellar: :any,                 catalina:       "57897d985402185cfe2a311893b4e98ebe05a450346a39e7d11716a5924cb4a8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9ada234fb653592954123072c1ae9f4a956e195a4e57212d08bcfe710eb96015"
+    sha256 cellar: :any,                 arm64_ventura:  "65fec6529bf575d9b2b9862176e7473bede61ae4b9f62d1a774c92b178ce191c"
+    sha256 cellar: :any,                 arm64_monterey: "95c29568fd32c390b9f7fb3b987571de33bec7b7c69f10ff361b2e2db3c16ae9"
+    sha256 cellar: :any,                 arm64_big_sur:  "1184fa77484b25973406fcb32f502c2e9ee59aacb696264e4f5cc6288bcb0047"
+    sha256 cellar: :any,                 ventura:        "c3a31963af0c4fb3ecbac0ba82b713f9846015f3102cbec71ef36d656467746f"
+    sha256 cellar: :any,                 monterey:       "5a87b0561b1c39d09c5fcdb194c72761da5caed1f7179e4951b09ec36446200f"
+    sha256 cellar: :any,                 big_sur:        "75c2ecf2e3d05a749cbf6b9cbab7ecb393eeea5b4d89cb678b4502beab45c1b4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "babf0d1ef2aede94f2e48c3def6d1fa50f8450f2d2002f23394858169a4d3014"
   end
 
   depends_on "ldc" => :build
