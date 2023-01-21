a07f347ea549db1b9a4672f8a881f5a9ad2e9426
Rui Chen <rui@chenrui.dev>
2023-01-21 15:24:00 +0000

    scriptisto: add 2.0.1 bottle.

--- Formula/scriptisto.rb
+++ Formula/scriptisto.rb
@@ -5,6 +5,16 @@ class Scriptisto < Formula
   sha256 "893a06d5349d2462682021f1e053488b07a608eee138dfcc9e68853223d48b81"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a5ebbf7af4d2b00e8ab3096dca55dbe031c74f7e59dcf06421584dcd851a5862"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "1ad843e920b8bb4abdb8ab53120952c9e9c4dd63a1dafcc599daa102e55cf12e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4e945e6fd4037fdd52d72c34ca383be631b5a3bb87dad4e9165b63590b272d91"
+    sha256 cellar: :any_skip_relocation, ventura:        "0a36cd8392e6db538f39381d08757d2b1618cab7471029508b8dd40aed35579b"
+    sha256 cellar: :any_skip_relocation, monterey:       "43e6db419727d10fe03742a11f9cdf998f0ce09cec9906b1989f9fd8a7f74b0b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d1a452e563ab6946bca6be8666bb793ba34255c690243cc4cdcd4aa9a6646d34"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "19f409e45a80582afc370744deed2477ce3f222b35edd485d10105fcfaffafde"
+  end
+
   depends_on "pkg-config" => :build
   depends_on "rust" => :build
 
