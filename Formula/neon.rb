127cf4b5ab4ce31574d367e4971c4dbe8a21eb00
Patrick Linnane <patrick@linnane.io>
2023-01-21 10:24:44 -0800

    neon 0.32.5
    * neon 0.32.5
    * neon: remove build patch
    
    Closes #121205.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/neon.rb
+++ Formula/neon.rb
@@ -1,9 +1,9 @@
 class Neon < Formula
   desc "HTTP and WebDAV client library with a C interface"
   homepage "https://notroj.github.io/neon/"
-  url "https://notroj.github.io/neon/neon-0.32.4.tar.gz"
-  mirror "https://fossies.org/linux/www/neon-0.32.4.tar.gz"
-  sha256 "b1e2120e4ae07df952c4a858731619733115c5f438965de4fab41d6bf7e7a508"
+  url "https://notroj.github.io/neon/neon-0.32.5.tar.gz"
+  mirror "https://fossies.org/linux/www/neon-0.32.5.tar.gz"
+  sha256 "4872e12f802572dedd4b02f870065814b2d5141f7dbdaf708eedab826b51a58a"
   license "LGPL-2.0-or-later"
 
   livecheck do
@@ -28,12 +28,6 @@ class Neon < Formula
 
   uses_from_macos "libxml2"
 
-  # Configure switch unconditionally adds the -no-cpp-precomp switch
-  # to CPPFLAGS, which is an obsolete Apple-only switch that breaks
-  # builds under non-Apple compilers and which may or may not do anything
-  # anymore.
-  patch :DATA
-
   def install
     # Work around configure issues with Xcode 12
     ENV.append "CFLAGS", "-Wno-implicit-function-declaration"
@@ -49,17 +43,3 @@ class Neon < Formula
     system "make", "install"
   end
 end
-
-__END__
-diff --git a/configure b/configure
-index d7702d2..5c3b5a3 100755
---- a/configure
-+++ b/configure
-@@ -4224,7 +4224,6 @@ fi
- $as_echo "$ne_cv_os_uname" >&6; }
-
- if test "$ne_cv_os_uname" = "Darwin"; then
--  CPPFLAGS="$CPPFLAGS -no-cpp-precomp"
-   LDFLAGS="$LDFLAGS -flat_namespace"
-   # poll has various issues in various Darwin releases
-   if test x${ac_cv_func_poll+set} != xset; then
