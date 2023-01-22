524b4af138e5b42e12a25a297cc87ac12ab94d37
Patrick Linnane <patrick@linnane.io>
2023-01-22 11:09:37 -0800

    advancecomp 2.5
    * advancecomp 2.5
    * advancecomp: update license
    
    Closes #121261.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/advancecomp.rb
+++ Formula/advancecomp.rb
@@ -1,9 +1,9 @@
 class Advancecomp < Formula
   desc "Recompression utilities for .PNG, .MNG, .ZIP, and .GZ files"
   homepage "https://www.advancemame.it/comp-readme.html"
-  url "https://github.com/amadvance/advancecomp/releases/download/v2.4/advancecomp-2.4.tar.gz"
-  sha256 "911133b8bdebd43aa86379e19584112b092459304401a56066e964207da423a5"
-  license "GPL-3.0"
+  url "https://github.com/amadvance/advancecomp/releases/download/v2.5/advancecomp-2.5.tar.gz"
+  sha256 "90b8ecad387b4770658e85be850b22318ee9e375cbad85ad25c8519d93317c07"
+  license "GPL-3.0-only"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "d6cdc0557059ffac0375d3871cd6b7b2ef42011e4673e56d8dfd65cd5906bcb1"
