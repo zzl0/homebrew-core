78b1cbd96337fb9e9741ad94fc38fc26c5acd42d
Rui Chen <rui@chenrui.dev>
2023-01-22 13:38:00 -0500

    lsof 4.98.0
    Closes #121250.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/lsof.rb
+++ Formula/lsof.rb
@@ -1,8 +1,8 @@
 class Lsof < Formula
   desc "Utility to list open files"
   homepage "https://github.com/lsof-org/lsof"
-  url "https://github.com/lsof-org/lsof/archive/refs/tags/4.97.0.tar.gz"
-  sha256 "633fb6bdceb672bc09f8943ad54d4d105fff27caa728c775d9c3d08b147890cc"
+  url "https://github.com/lsof-org/lsof/archive/refs/tags/4.98.0.tar.gz"
+  sha256 "80308a614508814ac70eb2ae1ed2c4344dcf6076fa60afc7734d6b1a79e62b16"
   license "Zlib"
 
   bottle do
