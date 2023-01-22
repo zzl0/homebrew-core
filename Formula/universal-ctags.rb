a5a43ce3a25c6510c3f319a898d47d91c1592a05
Patrick Linnane <patrick@linnane.io>
2023-01-21 18:29:15 -0800

    universal-ctags p6.0.20230122.0
    Closes #121218.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/universal-ctags.rb
+++ Formula/universal-ctags.rb
@@ -1,9 +1,9 @@
 class UniversalCtags < Formula
   desc "Maintained ctags implementation"
   homepage "https://github.com/universal-ctags/ctags"
-  url "https://github.com/universal-ctags/ctags/archive/refs/tags/p6.0.20230115.0.tar.gz"
-  version "p6.0.20230115.0"
-  sha256 "3164d8fbb0194663720962f65669d5cf2a3d9bbea554b68c803d35374f6c9744"
+  url "https://github.com/universal-ctags/ctags/archive/refs/tags/p6.0.20230122.0.tar.gz"
+  version "p6.0.20230122.0"
+  sha256 "649582b13abbea06d779b6031f2f9206d79290005fd5ccded50cbff503831dc1"
   license "GPL-2.0-only"
   head "https://github.com/universal-ctags/ctags.git", branch: "master"
 
