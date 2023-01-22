3865bba6aff1ba174be41a1e7ff1456866652731
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-22 19:04:22 +0000

    suite-sparse: update 7.0.1 bottle.

--- Formula/suite-sparse.rb
+++ Formula/suite-sparse.rb
@@ -18,13 +18,13 @@ class SuiteSparse < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "800547f35ec665c5285aa4b07e84e2397c062e09dccd46e65481e433f442b0de"
-    sha256 cellar: :any,                 arm64_monterey: "5686b2ea580a81059913df0d9dd1f8d117b89f583967ea25a2a4f741a6714921"
-    sha256 cellar: :any,                 arm64_big_sur:  "88337a211c6c36a234b1e007d696dd97cee549f06b316d3523e1a186bbac2f83"
-    sha256 cellar: :any,                 ventura:        "4fd414d62bc14a2737736dfab169c9e5f7a1684766ff1338278e75929aa0b882"
-    sha256 cellar: :any,                 monterey:       "44cde4609e04dd354f160735bbaf07e4142dc1ff33295c817bdfef64c2e482a1"
-    sha256 cellar: :any,                 big_sur:        "45f684ff114624b410ac9e24a5660c4f9d943697647dd56c97cd2e8ef1ba3160"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "93309af5befc94e7641a0159f95bef6b087a6399e35516d159356b901af2acae"
+    sha256 cellar: :any,                 arm64_ventura:  "b6f9bfc437eb8c888ad2b49127dd8b6d6d2ace0fe39ce5c375b6f59b4edcfbb2"
+    sha256 cellar: :any,                 arm64_monterey: "6e8b79482446c5d3651af243673c38bddb77c4bf12b04cf241bf3d1d3803abcb"
+    sha256 cellar: :any,                 arm64_big_sur:  "717d20eabb9375578fbb3eb2dc1983528b594bd79a19d169630d59c2293dace0"
+    sha256 cellar: :any,                 ventura:        "690df0fd91c2e9fc691f0d22c9d3f32ba233eb29d4d249a8abdf3c3d22ee1a08"
+    sha256 cellar: :any,                 monterey:       "e33accf36e6cbe4680f30435ce7678137df51adecc0dd567b871524097659097"
+    sha256 cellar: :any,                 big_sur:        "0830a6f917ff6bb25488761cc7c932bc168db3bd28d0d361941c7d7042c060ca"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "45e6d11898083878b3fef560f985e74a3f2f9a659c63e7575acba297d374a7d3"
   end
 
   depends_on "cmake" => :build
