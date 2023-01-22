512121d33b4c6dfbc34e79815cdb74eb3b0cf887
Rui Chen <rui@chenrui.dev>
2023-01-22 00:12:59 -0500

    cargo-outdated 0.11.2
    Closes #121222.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cargo-outdated.rb
+++ Formula/cargo-outdated.rb
@@ -1,10 +1,9 @@
 class CargoOutdated < Formula
   desc "Cargo subcommand for displaying when Rust dependencies are out of date"
   homepage "https://github.com/kbknapp/cargo-outdated"
-  url "https://github.com/kbknapp/cargo-outdated/archive/v0.11.1.tar.gz"
-  sha256 "2d80f0243d70a3563c48644dd3567519c32a733fb5d20f1161fd5d9f8e6e9146"
+  url "https://github.com/kbknapp/cargo-outdated/archive/v0.11.2.tar.gz"
+  sha256 "7e82d1507594d86cb1c2007d58e329a9780a22bdb0f38d5e71d2692a7f1727d9"
   license "MIT"
-  revision 1
   head "https://github.com/kbknapp/cargo-outdated.git", branch: "master"
 
   bottle do
