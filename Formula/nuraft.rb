e2a69a7c488554dfa08dc166b31f7cb892cbfc4e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-22 19:04:17 +0000

    nuraft: update 2.1.0 bottle.

--- Formula/nuraft.rb
+++ Formula/nuraft.rb
@@ -6,13 +6,13 @@ class Nuraft < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "28d93fd60657c2a7eeb21be7fa86c7e129d9e036b2cdac6391b71e156b76b28e"
-    sha256 cellar: :any,                 arm64_monterey: "d07ece97bc536bc22f77e009c4c09d467cf84c848f98f449d382a217222cf8df"
-    sha256 cellar: :any,                 arm64_big_sur:  "ef7e06e935ab63bb8d7001639846922d2a12c4c902783a198e65ed4f8d8d6c3c"
-    sha256 cellar: :any,                 ventura:        "486c038044bb14e9afdd47e4470fc92c5bad6ba5e21334ecec94cc6829ab6c3a"
-    sha256 cellar: :any,                 monterey:       "e1988cf74f43cfdfc746d37ff5ffa461421aa0602e28227a5715e4e7565f3598"
-    sha256 cellar: :any,                 big_sur:        "c048da37867c7c846bbd0ef91c81e62c37e73b2e55ed5080f479940f8ad91ed5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "272f2298f6b55efe1f93e63c2d44af4d317499ea1e5b2eaf87acf8107ea2eb30"
+    sha256 cellar: :any,                 arm64_ventura:  "17cdd2860bbcd32bfa028c1706c02da066a796b0f1abfca96c76bdbcb05ca012"
+    sha256 cellar: :any,                 arm64_monterey: "5d024f15a5a6644bb74b7293ab5939376e9115102c3dcf466d9ef209496a27c4"
+    sha256 cellar: :any,                 arm64_big_sur:  "46efae0c6123d49ce9cf3f9f4798b4a556bf55e1cf7fbb1aaa12ce6b458613b8"
+    sha256 cellar: :any,                 ventura:        "e38d6cbd1be543fc3ee42ce4573309c0f058c83d8151519f9ce9272c4edd82f3"
+    sha256 cellar: :any,                 monterey:       "81250cae0a2c2ef68e88b1ab3e0f394d6ad803f257e23cac33ce07c7f4bfbe93"
+    sha256 cellar: :any,                 big_sur:        "668d54563b382c1160246452e6cf54fd6832c238e33731613d6537418f474b0a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "72a6fa91392aacbbc97f42a17330a2bc0c4b38dc97c3477f87b6c9c90474bd28"
   end
 
   depends_on "cmake" => :build
