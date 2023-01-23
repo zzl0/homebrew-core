3c6f6a7cf4b14ea6dd797fe0c13660592321f1de
Michael Cho <cho-m@tuta.io>
2023-01-22 18:26:43 -0800

    gtranslator 42.0
    Closes #121293.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gtranslator.rb
+++ Formula/gtranslator.rb
@@ -1,10 +1,9 @@
 class Gtranslator < Formula
   desc "GNOME gettext PO file editor"
   homepage "https://wiki.gnome.org/Design/Apps/Translator"
-  url "https://download.gnome.org/sources/gtranslator/40/gtranslator-40.0.tar.xz"
-  sha256 "ec3eba36dee1c549377d1475aef71748dbaebd295005e1990ea9821f02b38834"
+  url "https://download.gnome.org/sources/gtranslator/42/gtranslator-42.0.tar.xz"
+  sha256 "2a67bcbfe643061b0696d89a10847d50bf35905ff3361b9871357d3e3422f13b"
   license "GPL-3.0-or-later"
-  revision 1
 
   bottle do
     rebuild 1
@@ -18,36 +17,30 @@ class Gtranslator < Formula
     sha256 x86_64_linux:   "8e7e8ca6e284f89497170bd47e232d47c9db1566b2e6c5835cc615086bdeeb58"
   end
 
+  depends_on "itstool" => :build
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
   depends_on "adwaita-icon-theme"
+  depends_on "gettext"
   depends_on "glib"
   depends_on "gspell"
   depends_on "gtk+3"
   depends_on "gtksourceview4"
-  depends_on "iso-codes"
-  depends_on "itstool"
   depends_on "json-glib"
-  depends_on "libdazzle"
   depends_on "libgda"
   depends_on "libhandy"
-  depends_on "libsoup@2"
+  depends_on "libsoup"
 
-  # Apply upstream commit to fix build with meson. Remove with next release.
-  patch do
-    url "https://gitlab.gnome.org/GNOME/gtranslator/-/commit/7ac572cc8c8c37ca3826ecf0d395edd3c38e8e22.diff"
-    sha256 "cc93ba73ab5e010171fa21d5e345a2b4f69773bc786d07952181f86d1b66f368"
-  end
+  uses_from_macos "libxml2"
 
   def install
     # stop meson_post_install.py from doing what needs to be done in the post_install step
     ENV["DESTDIR"] = "/"
-    mkdir "build" do
-      system "meson", *std_meson_args, ".."
-      system "ninja", "-v"
-      system "ninja", "install", "-v"
-    end
+
+    system "meson", "setup", "build", *std_meson_args
+    system "meson", "compile", "-C", "build", "--verbose"
+    system "meson", "install", "-C", "build"
   end
 
   def post_install
