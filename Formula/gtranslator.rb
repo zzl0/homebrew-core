1dc5aaa9676576e54c23efae7329bcc941cd98f7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-23 09:04:42 +0000

    gtranslator: update 42.0 bottle.

--- Formula/gtranslator.rb
+++ Formula/gtranslator.rb
@@ -6,15 +6,13 @@ class Gtranslator < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    rebuild 1
-    sha256 arm64_ventura:  "6271144e4bfac02229a6039cbde55ea5dc0022b4d6327cfb642307f384cb6a5c"
-    sha256 arm64_monterey: "15e9c85730e88ddb9544d01722e972bd6b68b3baab42effdf25a585381d90772"
-    sha256 arm64_big_sur:  "e787ad4e441d4ecd4d9c328fb18d2384a21862b0a389170ee54cbbd01879a556"
-    sha256 ventura:        "60e7cdef5ca027b32bd7405c6e6efac3cb029c44f4259933a9d71cdec645321c"
-    sha256 monterey:       "25908591830110dc32edc52b04ef4c3dbf296b782d1ace73191826205a889fd3"
-    sha256 big_sur:        "8a5b528e062820cdc3ebd9b024b4e96f3ec2d65d521b03026828d32d748774e1"
-    sha256 catalina:       "91dd2d4c85608fb04f7a4f92423dce39ab2ab04a3f11152c30efad1bd96f14c6"
-    sha256 x86_64_linux:   "8e7e8ca6e284f89497170bd47e232d47c9db1566b2e6c5835cc615086bdeeb58"
+    sha256 arm64_ventura:  "eff3164e795130ffbfb819d020f10d12c7c26433645d41258f9168b179a5e0fa"
+    sha256 arm64_monterey: "c9dad313a4b72b1a06828305fd3c7be3cdc9ed990faac40dc8325245d19d1a23"
+    sha256 arm64_big_sur:  "7dc1c16652e097e44e7e02f3ad95c361b208426517898de58160a98b8a1361a4"
+    sha256 ventura:        "d1b51162cd3762bf620685a287603026937831e492896ccbfb99485a9516e031"
+    sha256 monterey:       "6f82d622765a3269c0b0bca386ee425375b369f20d1eaa2bc5b9fdbe19951998"
+    sha256 big_sur:        "c4b68d96cb52329c1ae13d146ce5d76ed47a8d92be5ae33022af2194e14f353a"
+    sha256 x86_64_linux:   "bbd7b7cc4d3466f46bac926ae3ca16ab81cbeadd13baafdd7dfe550d530e67cc"
   end
 
   depends_on "itstool" => :build
