e64354de466bce571d424496857a32def8d45569
Rui Chen <rui@chenrui.dev>
2023-01-23 13:11:43 +0000

    skaffold: update 2.1.0 bottle.

--- Formula/skaffold.rb
+++ Formula/skaffold.rb
@@ -8,13 +8,13 @@ class Skaffold < Formula
   head "https://github.com/GoogleContainerTools/skaffold.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3c655bbd1223469fd659086b7eb7b9e0a6a8135cc645847b52d5e1c366f557c7"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a8a70416ce3d04de837faaf3c41e652e9e0a62adb4fb7d0d508a7dfe87fc4780"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "13477b93f800042a2c6b12f60d300afc5cb715c6eac2f5786d9290949041ada5"
-    sha256 cellar: :any_skip_relocation, ventura:        "8f82e2773f3990e6926e4189b83a7f12a9351235961ebd53ce6d306aa06a02c9"
-    sha256 cellar: :any_skip_relocation, monterey:       "922b76a8e75ccefd64c07049157901be46bfc404842e1ef01fa5c56242eaa8f9"
-    sha256 cellar: :any_skip_relocation, big_sur:        "6c0111fbae798d51dfe5363ec71b5828ade9ee1a8e525d8d6ae1b56f98be4244"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e57c02b989e358c09021f1b5cfce412f9ecdaeb9a743d3e19c8d22b327c61fa7"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "7219ed397048efad7c51ddfbeed43b94a2f0cb34b6c4280cfba5fd87740aa92b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a2be3a149316bdb205cf52cacd1184b21bc094d47de2ba09466f285369e26fba"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9c07939ed16e40ce7aaecaf35cc40de5d5f1a5f965c07aaf83cf8703e187d1e2"
+    sha256 cellar: :any_skip_relocation, ventura:        "d913ca23899d452875f7a2d70bc67188911898cf18a2acdce25d2165a21bb423"
+    sha256 cellar: :any_skip_relocation, monterey:       "b6fc296c4de9006c261d5720f53c9c9ca040c0e509d19d5afcf37b1710290788"
+    sha256 cellar: :any_skip_relocation, big_sur:        "c8f824f399aeefba17bd9d5a60645b20dfa9507b4f0153262557da1ad98da54c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ec9d01a4758ef95cdf54ec96ee018bc3c343c8fcb102a4e7ea584cb3f55e651f"
   end
 
   depends_on "go" => :build
