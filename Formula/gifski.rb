dd9a8e8c21fef9fd0112c00519e1af19e81479d9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-23 01:31:54 +0000

    gifski: update 1.10.0 bottle.

--- Formula/gifski.rb
+++ Formula/gifski.rb
@@ -6,13 +6,13 @@ class Gifski < Formula
   license "AGPL-3.0-only"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "eb4c31be3fa7f493b16d840d0ed7e62f356616e9c5af3826d809c4b6e2fa96a1"
-    sha256 cellar: :any,                 arm64_monterey: "17050fe339d24ebbbd40e4f979b76687a66cd173692d63cd9e454c43781eca76"
-    sha256 cellar: :any,                 arm64_big_sur:  "0a9e815d460fa201ae4bbfce979b930600d2ed8b37700ebc4e31cd35d2bf175c"
-    sha256 cellar: :any,                 ventura:        "e96d17b2c2789165f7b002110a9accad4e573332d0fbbf5b81aedcffddfc72a9"
-    sha256 cellar: :any,                 monterey:       "b8f692bbff90310bd2c9a53d498d6e018f11e4deda1fc268eff70574df23f6b2"
-    sha256 cellar: :any,                 big_sur:        "4b77e27210658817253ce7d467802cf0e9191b2d70c38f97455399863dd99957"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "106102f3e9ba2c93d3b9bdedca2bc9cb5e85e117211461840ca617d1abbd1bd4"
+    sha256 cellar: :any,                 arm64_ventura:  "2bff5cc701dcab0998ac5faa2d188098d260e065883a642f70b86acc8faed0c6"
+    sha256 cellar: :any,                 arm64_monterey: "6365d6386b896f732e65c0bade8e05c9e8fe9b6b2498595e15cbd8b92598067b"
+    sha256 cellar: :any,                 arm64_big_sur:  "ad1fff271c9c7ffe684243c4fdf9572a51291faa53283d4916225af2fc62f251"
+    sha256 cellar: :any,                 ventura:        "c7dc1fa5f1ce84fdf50662558d5363cc7bf2c46f359df39ee55c1e490733ab58"
+    sha256 cellar: :any,                 monterey:       "2f866f1ba36dcb6ba87b40e14d8f864283f4e5fea5dedea7ad42e09708c14c90"
+    sha256 cellar: :any,                 big_sur:        "421ae4ed99a0c7bfc07a673f83027380a50d61c586efc30d271b93e65afba3a2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9902c36327e0cc159fd8334dbc432088a95f11ec2f44d5cb49fa59186803196d"
   end
 
   depends_on "pkg-config" => :build
