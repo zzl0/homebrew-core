3a3b8afadee420c08df90c02c3c993d4fc1a60c2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-23 09:04:42 +0000

    libgda: update 6.0.0 bottle.

--- Formula/libgda.rb
+++ Formula/libgda.rb
@@ -8,16 +8,13 @@ class Libgda < Formula
   license "LGPL-2.0-or-later"
 
   bottle do
-    sha256 arm64_ventura:  "549596f0f624bf5479e52239da0e558792426ba5077360822a773f2a40b81c3a"
-    sha256 arm64_monterey: "31de7c27443d4f477cfefa7b7598311b98b1998368385ea7a0787644c30a3c6f"
-    sha256 arm64_big_sur:  "993a414772b41e1f0b2cffe21f9af240dbcd7e2b6de5d62a0e51b89a8144e40a"
-    sha256 ventura:        "7882245a6f2192ffa9d8c483076f439dcbf5d4f7351639e3d48ef0667ff378fd"
-    sha256 monterey:       "13a192f90ad01f376f5cbc977308d91bb096f3132ec41bea14a4b961866bfc1d"
-    sha256 big_sur:        "1fd18afa48f013fcee08cadebf89c4bbb3e37444b591b16cd61e5848b93d6395"
-    sha256 catalina:       "83d65ccf6e92620dd833dd23d1a02880f020ab24a0a6ed2ab5cb1a5149a32c5b"
-    sha256 mojave:         "e48a5aea9d860765e58bcd756c8e81956974d4284189604755a63232fc13a806"
-    sha256 high_sierra:    "8c9a8133c1fd1c554f995c089b12cbe049d2a8a01ac31cb5e68c089857a200a1"
-    sha256 x86_64_linux:   "65f1e3cae4f56ec7264a6d59421564c75f10f65bae8ea3e3914dbb5e08fb7eee"
+    sha256 arm64_ventura:  "b18587293e001ff6935192e6fcd7abe9e4331567676b93e05c82ec1f26852c78"
+    sha256 arm64_monterey: "9507bbd5bbe926aaa34cfc60f45c0d1356344b5f0d58d2439dae2c9369070287"
+    sha256 arm64_big_sur:  "5a362970b76959e90e8ded9f9963a54b9944c05fd478848d894bab5f6f231f4e"
+    sha256 ventura:        "047a234bcf4fb07a8076e6aa3661531db094ded0b6ffe31fc2ddfb4f3bc45feb"
+    sha256 monterey:       "cdcf75e0a86528db4a8b07e26d11742aea3c677105f9450f8f4a14b9f47d371a"
+    sha256 big_sur:        "5ce190057fd44a4d5ae1be137fe44297a17059be7e22e08fd424c15b34be0669"
+    sha256 x86_64_linux:   "712d4518f1133a0e15869f011539833bc0ccce38fc57fddab40616f12c242b38"
   end
 
   depends_on "gettext" => :build
