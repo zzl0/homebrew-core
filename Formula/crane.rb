507b08b21e2e1fb356ae585279e3168c540db671
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-24 20:04:38 +0000

    crane: update 0.13.0 bottle.

--- Formula/crane.rb
+++ Formula/crane.rb
@@ -6,14 +6,13 @@ class Crane < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "ee15286bbbd8f37159c82a1c88e0c65a53c9573b4ee07e2b459e057b235c046d"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3a7ac13f91c7af9d7c9f95b1833ed7b9ac4bd71ff855bd93e4c95dfbfe050077"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9d1ed2a72a9d686e7db19a70a8705f7310f84debed78444c7837eb78b0f5f059"
-    sha256 cellar: :any_skip_relocation, ventura:        "2cbbfa1272340ef473df76655ee9d1f167b02865afb1443ca6b4ae79c9b7d8f5"
-    sha256 cellar: :any_skip_relocation, monterey:       "d5b0ef8390b39eed471081a4c4ee105122e2f585535290f19dda36c212a96ce0"
-    sha256 cellar: :any_skip_relocation, big_sur:        "76de07d96ee16e0890090ca16825c43592257c47d0b062859befec6ece942e75"
-    sha256 cellar: :any_skip_relocation, catalina:       "505afd464da89511d84dafe77f02fdea2f5bff33555eafb3527cd1fc9ff8bd23"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5449a04a47d6628c80f376eba8c429f840c57d04f2689661b2e200f9da755526"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "abee3f66e8952e4cf2af806a0f2d703e002e23610acab9539df1b7e776858e86"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "40e50a5aa5ead8e8d9626bab79b00c2b93c167f7cd6255b352de8b9edbe39e39"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b4cf362e59bcc1257fbe62694f2a19940348c0fb8dbf16b84bf74953ea1d8db2"
+    sha256 cellar: :any_skip_relocation, ventura:        "d17c792b19b9eb0b726df5687b79b1f1482b821008011323c7327805b21da4b2"
+    sha256 cellar: :any_skip_relocation, monterey:       "7a646e540fc1921a7d8535c2a11101df5c6e05ecb4480ae23547d28260112571"
+    sha256 cellar: :any_skip_relocation, big_sur:        "6f2701308a1b810a19a1a3e147adf0072f18a77e0911ed18fa6f31cd7ac4d5dd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "599106f2740faec14d37d4e82b080aabd140ad108e5430856a54a9140df6356d"
   end
 
   depends_on "go" => :build
