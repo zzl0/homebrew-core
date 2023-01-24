2775ce1879d6ab2028d5ffeda118d3cb42304718
Patrick Linnane <patrick@linnane.io>
2023-01-24 10:40:28 -0800

    aws-sdk-cpp 1.11.0

--- Formula/aws-sdk-cpp.rb
+++ Formula/aws-sdk-cpp.rb
@@ -3,8 +3,8 @@ class AwsSdkCpp < Formula
   homepage "https://github.com/aws/aws-sdk-cpp"
   # aws-sdk-cpp should only be updated every 10 releases on multiples of 10
   url "https://github.com/aws/aws-sdk-cpp.git",
-      tag:      "1.10.50",
-      revision: "341fa41cf8a0a3ed052f7dc872223ebb4a6c98da"
+      tag:      "1.11.0",
+      revision: "276ee83080fcc521d41d456dbbe61d49392ddf77"
   license "Apache-2.0"
   head "https://github.com/aws/aws-sdk-cpp.git", branch: "main"
 
