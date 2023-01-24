f3af459e012057acd03b2e30cc313bb57153701e
Rui Chen <rui@chenrui.dev>
2023-01-24 00:54:22 +0000

    cmark: update 0.30.3 bottle.

--- Formula/cmark.rb
+++ Formula/cmark.rb
@@ -6,14 +6,13 @@ class Cmark < Formula
   license "BSD-2-Clause"
 
   bottle do
-    rebuild 2
-    sha256 cellar: :any,                 arm64_ventura:  "bfa1d758eb00bf9693f42cb010065ff0154d99ecc9f54933db7d633d1a413860"
-    sha256 cellar: :any,                 arm64_monterey: "422041e7f5634ac51994edb5f7313782ad1de31b707b55eb6cf042a2a6020b02"
-    sha256 cellar: :any,                 arm64_big_sur:  "9b5b1fe680848c5c2daa8f6f636c4d926fffa87487003b30b1654f21459c12f1"
-    sha256 cellar: :any,                 ventura:        "aeba72c89d42c0f92b57054b0cb729830b9f04c544b6fc7ceef6a8074fc1adab"
-    sha256 cellar: :any,                 monterey:       "7844d321c93675aea242f5e1dc7c0e7c35c1970a3e9e0a8c3dbe0d36ae005a1c"
-    sha256 cellar: :any,                 big_sur:        "a62222cbcb8e767902d0be715b8dd43fd0b5588d1a8967a08276eb0ad116f0e0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ef97bf27186db785e00914f19fff87648efac9dc573a53e6a68a8e900b5c6a1c"
+    sha256 cellar: :any,                 arm64_ventura:  "0b22613ee9aa75990bdb4bbbfc6166ef8d176b17c8caf7bbad25ed0738841a7b"
+    sha256 cellar: :any,                 arm64_monterey: "739ea11aa0a356b621c49661721ceb371e3c5ea56c244328bd10aae74a0f95a4"
+    sha256 cellar: :any,                 arm64_big_sur:  "162ade26201f90662fc6305a83c72ae2a550ddc4326ccf453d5ab1fd85879c25"
+    sha256 cellar: :any,                 ventura:        "ea945f37fb8de82dffc9ba85f6592b564036228e7ee2ba49951bc639b51266c6"
+    sha256 cellar: :any,                 monterey:       "43e230aa0745cc9362d1f2f7c1d85424005690242c725117d3a88be8b88d31c2"
+    sha256 cellar: :any,                 big_sur:        "cef92df088c591e3b123ad841a0773fd75c4961d59f701b5e6d27902ecde14af"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f35b1d16c83135d09f2f022a0e4f5a25479feea6411c5115bd8dddfc866f4d1a"
   end
 
   depends_on "cmake" => :build
