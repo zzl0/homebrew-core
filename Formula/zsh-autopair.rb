c59e1890f6805dbdf310e147626f24158f645197
Patrick Linnane <patrick@linnane.io>
2023-01-24 16:23:53 +0000

    zsh-autopair: add 1.0 bottle.

--- Formula/zsh-autopair.rb
+++ Formula/zsh-autopair.rb
@@ -5,6 +5,10 @@ class ZshAutopair < Formula
   sha256 "4b6f4d20d89ea08fd239089ad4133cff5ebdb71f07f589d5e41d0814d4cf4165"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "43069acaa2147e5783bd7af94b7a469e80a065f9e41f05f20e6a7d62eb9eda53"
+  end
+
   uses_from_macos "zsh" => :test
 
   def install
