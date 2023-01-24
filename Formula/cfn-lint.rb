c29e46c141df5e69501a818be1ebc1602670de15
Rui Chen <rui@chenrui.dev>
2023-01-24 15:11:46 -0500

    cfn-lint: update to use pypi source for junit-xml
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/cfn-lint.rb
+++ Formula/cfn-lint.rb
@@ -71,10 +71,9 @@ class CfnLint < Formula
     sha256 "0f864437ab8b6076ba6707453ef8f98a6a0d512a80e93f8abdb676f737ecb60d"
   end
 
-  # only doing this because junit-xml source is not available in PyPI for v1.9
   resource "junit-xml" do
-    url "https://github.com/kyrus/python-junit-xml.git",
-        revision: "4bd08a272f059998cedf9b7779f944d49eba13a6"
+    url "https://files.pythonhosted.org/packages/98/af/bc988c914dd1ea2bc7540ecc6a0265c2b6faccc6d9cdb82f20e2094a8229/junit-xml-1.9.tar.gz"
+    sha256 "de16a051990d4e25a3982b2dd9e89d671067548718866416faec14d9de56db9f"
   end
 
   resource "networkx" do
