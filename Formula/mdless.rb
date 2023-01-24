1cd986a046af1d55c9b23752cfc887459a59a1fa
Bug Fest <52962234+bugfest@users.noreply.github.com>
2023-01-24 18:10:09 +0100

    mdless 1.0.32 (new formula)
    * mdless 1.0.32 (new formula)
    * mdless improved testing
    * mdless tests using match_assert on multiline output
    * mdless fix audit errors
    
    Closes #121410.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/mdless.rb
@@ -0,0 +1,28 @@
+class Mdless < Formula
+  desc "Provides a formatted and highlighted view of Markdown files in Terminal"
+  homepage "https://github.com/ttscoff/mdless"
+  url "https://github.com/ttscoff/mdless/archive/refs/tags/1.0.32.tar.gz"
+  sha256 "ce67a184aaeba66955c96f9f2d3353040b359c166329a149c49b470bf8edeb39"
+  license "MIT"
+
+  uses_from_macos "ruby"
+
+  def install
+    ENV["GEM_HOME"] = libexec
+    system "gem", "build", "#{name}.gemspec"
+    system "gem", "install", "#{name}-#{version}.gem"
+    bin.install libexec/"bin/#{name}"
+    bin.env_script_all_files(libexec/"bin", GEM_HOME: ENV["GEM_HOME"])
+  end
+
+  test do
+    assert_match "mdless #{version}", shell_output("#{bin}/mdless --version")
+    (testpath/"test.md").write <<~EOS
+      # title first level
+      ## title second level
+    EOS
+    out = shell_output("#{bin}/mdless --no-color -P test.md")
+    assert_match(/^title first level =+$/, out)
+    assert_match(/^title second level -+$/, out)
+  end
+end
