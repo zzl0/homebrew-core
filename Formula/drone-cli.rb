e47d741b5462447d09db234625d52401a22b9cc7
Patrick Linnane <patrick@linnane.io>
2023-01-24 09:41:02 -0800

    drone-cli 1.7.0
    Closes #121419.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/drone-cli.rb
+++ Formula/drone-cli.rb
@@ -2,8 +2,8 @@ class DroneCli < Formula
   desc "Command-line client for the Drone continuous integration server"
   homepage "https://drone.io"
   url "https://github.com/harness/drone-cli.git",
-      tag:      "v1.6.2",
-      revision: "24bea58512640de52ef16c45f48645e255d8b46c"
+      tag:      "v1.7.0",
+      revision: "c75a1a82d46e687d2d961115c849bf3a137b8e9d"
   license "Apache-2.0"
   head "https://github.com/harness/drone-cli.git", branch: "master"
 
