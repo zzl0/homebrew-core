9a69b2d66d0056ce470327163d73be7996821665
Rui Chen <rui@chenrui.dev>
2023-01-24 15:31:09 -0500

    moto: update to use pypi source for junit-xml
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/moto.rb
+++ Formula/moto.rb
@@ -157,9 +157,9 @@ class Moto < Formula
     sha256 "780a22d517cdc857d9714a80d8349c546945063f20853ea32ba7f85bc643ec7d"
   end
 
-  resource "junit-xml-2" do
-    url "https://files.pythonhosted.org/packages/4d/f2/a99adf9deb57949b81ff8e113edf971da1840251794a6f4184d61faa5a65/junit-xml-2-1.9.tar.gz"
-    sha256 "3b8d9635c5215f754c7807104f6493e3ea3bc9481e2d33db294560da3a1b00f7"
+  resource "junit-xml" do
+    url "https://files.pythonhosted.org/packages/98/af/bc988c914dd1ea2bc7540ecc6a0265c2b6faccc6d9cdb82f20e2094a8229/junit-xml-1.9.tar.gz"
+    sha256 "de16a051990d4e25a3982b2dd9e89d671067548718866416faec14d9de56db9f"
   end
 
   resource "lazy-object-proxy" do
