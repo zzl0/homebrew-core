74aa78dc087e27ec0eeed22e8db61d1c064fbbc7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-26 20:04:40 +0000

    metricbeat: update 8.6.1 bottle.

--- Formula/metricbeat.rb
+++ Formula/metricbeat.rb
@@ -8,13 +8,13 @@ class Metricbeat < Formula
   head "https://github.com/elastic/beats.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "d4f6d2e116fa2760b488fb2107696b29b7b056809c3ee4569d8295194c06ec23"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b2f9a31f1f40853d29bba08468529dc4d91f1930e63924d9eb7d399b8e25fb88"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9860a29d5d77a55fc39fc6dac86c6ab5e70e52a4695c3d3080ff2e12741b96ac"
-    sha256 cellar: :any_skip_relocation, ventura:        "5f778d9ca231ce35a27780a8938c5c7e39d99b0b87c28149aa88ed65d8796bee"
-    sha256 cellar: :any_skip_relocation, monterey:       "2a21de175f4da1d57fcb045ecf3c86c6adcf6e0e1ec8eb4b7f087f37a2e4c1f4"
-    sha256 cellar: :any_skip_relocation, big_sur:        "17669af656913855f337c498faa9e4c925fed4e54c1073ba326bdc6fba92164c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5c36ef206de57b4b1b02626e5e15ee2bee8777ac43d6ba37563fab2b5e51175b"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "9d744af7c60a7fe84cbc506e7ea638417febce1d15336802ab0f736f09737653"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "1af83ecdfc6cd1eb43366d70a81c6add80a5a5dd68b6bb78f6b0caac1468c8d7"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "68e8f3d97c6299ce9fa57dab4ea51abd3f7aa7b912171cce8b8b1c3126b3a184"
+    sha256 cellar: :any_skip_relocation, ventura:        "450d4c9403b1020e1820965ba70bdfae2983358fbf946d19f2bf8d584c1a2192"
+    sha256 cellar: :any_skip_relocation, monterey:       "8d51bb6ab5375d535c586c8f268e021936844eaf6771a7d2b143cd1dad705346"
+    sha256 cellar: :any_skip_relocation, big_sur:        "c9e9907d71232aace6c4a61db93821313b63f1b2d106627b2946131d869e4e62"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8b1ab7dd95144ef5917feb6ea1951a59deeb37dab28e9141c9928eb818377a58"
   end
 
   depends_on "go" => :build
