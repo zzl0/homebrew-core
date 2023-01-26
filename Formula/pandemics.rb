140e0f971352a9b0a98e2fa090e5219ebffde0a6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-26 06:05:42 +0000

    pandemics: update 0.11.11_1 bottle.

--- Formula/pandemics.rb
+++ Formula/pandemics.rb
@@ -9,12 +9,13 @@ class Pandemics < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "9d067a0bb598178d1d774015aaa93d8ad21d021501541a78c005afe8f19da109"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "9363a6ce01d8d91fa2eec4d3956589b599b6c72405f1b130736977c037e1f8bd"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9363a6ce01d8d91fa2eec4d3956589b599b6c72405f1b130736977c037e1f8bd"
-    sha256 cellar: :any_skip_relocation, monterey:       "1fa20d0ac8a7faa555023482cc4387c9caa07a39b99149517ea24ae2683b2a2e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "1fa20d0ac8a7faa555023482cc4387c9caa07a39b99149517ea24ae2683b2a2e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9363a6ce01d8d91fa2eec4d3956589b599b6c72405f1b130736977c037e1f8bd"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3ba3d95dc31bec61a6be96c0a0ee08befebf159f2d92c3f900bf5c6d29f7959e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3ba3d95dc31bec61a6be96c0a0ee08befebf159f2d92c3f900bf5c6d29f7959e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3ba3d95dc31bec61a6be96c0a0ee08befebf159f2d92c3f900bf5c6d29f7959e"
+    sha256 cellar: :any_skip_relocation, ventura:        "fcb7becebfe11194903b3c2f602a3e20840c6fccc9e2482cba6672acf630cbe8"
+    sha256 cellar: :any_skip_relocation, monterey:       "fcb7becebfe11194903b3c2f602a3e20840c6fccc9e2482cba6672acf630cbe8"
+    sha256 cellar: :any_skip_relocation, big_sur:        "fcb7becebfe11194903b3c2f602a3e20840c6fccc9e2482cba6672acf630cbe8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3ba3d95dc31bec61a6be96c0a0ee08befebf159f2d92c3f900bf5c6d29f7959e"
   end
 
   depends_on "librsvg"
