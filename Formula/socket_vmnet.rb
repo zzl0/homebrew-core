1a92d0c7f5f498a3f5aa9c9136594cd55e52e202
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-26 15:04:45 +0000

    socket_vmnet: update 1.1.1 bottle.

--- Formula/socket_vmnet.rb
+++ Formula/socket_vmnet.rb
@@ -7,13 +7,12 @@ class SocketVmnet < Formula
   head "https://github.com/lima-vm/socket_vmnet.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8e309781893e7dd83edadee99ab08ec8295917f3e687f9c3949c95129be9ef03"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ceb4bc938dda5c753ac0a741f290559f4864a10c298206b573fc103806e6abce"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3cafc6fea88ace7848811345919cfe2db0857d01a46dd55a1617719faa3ff410"
-    sha256 cellar: :any_skip_relocation, ventura:        "9e144430bd06b3522662a8c97a52971df0de69f3161303de8c076882b478dd47"
-    sha256 cellar: :any_skip_relocation, monterey:       "b3a238d313d1f0ad7ed813b37cf46e68df739c7a57062e6f545290307e8ce1e2"
-    sha256 cellar: :any_skip_relocation, big_sur:        "e129b3422301ddccddcd30c312b86bdae105f43c8cfec06163404420201e0f5f"
-    sha256 cellar: :any_skip_relocation, catalina:       "fd7fa82a159b2821a54bfe417968af6624b833f32eac6807e341536ffcab4ef7"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "01ab44a93ed874d4500ac8a7f8ef44252fe5ff96a2e78df35ed4c4da837b848f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "da933499a674b796edc643629ecc50382fe13057e86d86173fbcb9878b649cfd"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6f1a4c54f294dfc2851218b0cf1432120e606c84e577d065a9e3caac5660ab36"
+    sha256 cellar: :any_skip_relocation, ventura:        "113fb0d997b92520557a7430bd10695c17099d55308cd5ee731c0b386d749b94"
+    sha256 cellar: :any_skip_relocation, monterey:       "fcd1d497e67e89debad36400637317534338de174ed7c7876bd869e9e9efc09c"
+    sha256 cellar: :any_skip_relocation, big_sur:        "fb87bb0eaadd73398fcde0924b3ffc49c67354267474d8e89788d58191b980d8"
   end
 
   keg_only "#{HOMEBREW_PREFIX}/bin is often writable by a non-admin user"
