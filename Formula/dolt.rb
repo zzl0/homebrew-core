a941584ed5443f38ec373079341a0df6e841b9c9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-26 20:12:02 +0000

    dolt: update 0.52.13 bottle.

--- Formula/dolt.rb
+++ Formula/dolt.rb
@@ -11,13 +11,13 @@ class Dolt < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f0cef114c08f9cebc9f5326ecac596fd851fc38c0d56d9b636c055c2b757929f"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a0e75b5c2e63d56cb72cb5d273638c52d85c01f814a5c7002aba599ad5a56a38"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "47635e4307f61b46e53932d3773aa5cc204d153e567b3895767dc5606f2f548d"
-    sha256 cellar: :any_skip_relocation, ventura:        "dd0c585b1674fd579960e4c453b2868aa05c0633a8165ad74c62f90f9a3bda96"
-    sha256 cellar: :any_skip_relocation, monterey:       "cc5a185b2ecc999607eda1dcf1920bbecfbf26b1e6a20559a32b9fe52df7c42d"
-    sha256 cellar: :any_skip_relocation, big_sur:        "56a0a59c57c59e8684866aa26f38f99026fc614655169dc6cd497812026a7db4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "77e404d21df2b55e03b8e3c756b35d58e8c7a021829131ed13f179f9cce4d19d"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "32efafd99344ce0999ee4add9a2b5f8b3cf2eaed28288e269634f9b0a5e1c046"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "31699675cb0cc060a192ddfecafddff3a8e5ca47e95a93ba2e484db8550bbfe3"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6f5fed5e77976f90de876e66fc31c99e306f66fde939c1d3e45a14f037adc7fe"
+    sha256 cellar: :any_skip_relocation, ventura:        "7a5ac1a382b81fa2e8f0753eece3cf61f1a7ca597f619eb443eb182bdf62439a"
+    sha256 cellar: :any_skip_relocation, monterey:       "2dd597cbe5ca00aa6e7815873fe0fcbc53c3ffe4641c38e14251e55c4377385f"
+    sha256 cellar: :any_skip_relocation, big_sur:        "25041c2ae53b23ebf4adc59735ced18708b0300133c6964f9ea63d0170921e69"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f55c56862855cf03e862732680367619f9072e9b178ab5ce22ef71d7734dfca9"
   end
 
   depends_on "go" => :build
