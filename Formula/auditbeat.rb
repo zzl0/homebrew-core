606cd71624d188e9593a031ae5d6064dc6d37ada
Rui Chen <rui@chenrui.dev>
2023-01-26 16:23:10 -0500

    auditbeat 8.6.1
    Closes #121585.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/auditbeat.rb
+++ Formula/auditbeat.rb
@@ -2,8 +2,8 @@ class Auditbeat < Formula
   desc "Lightweight Shipper for Audit Data"
   homepage "https://www.elastic.co/products/beats/auditbeat"
   url "https://github.com/elastic/beats.git",
-      tag:      "v8.6.0",
-      revision: "561a3e1839f1a50ce832e8e114de399b2bee2542"
+      tag:      "v8.6.1",
+      revision: "14f2f8d585f8c380945feee789771bd782cd6b2d"
   license "Apache-2.0"
   head "https://github.com/elastic/beats.git", branch: "main"
 
