347601c164278a9fb870ac2c08b99d53e6b1ad69
Rui Chen <rui@chenrui.dev>
2023-01-27 10:53:03 -0500

    kubefirst 1.11.1
    Closes #121676.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/kubefirst.rb
+++ Formula/kubefirst.rb
@@ -1,8 +1,8 @@
 class Kubefirst < Formula
   desc "GitOps Infrastructure & Application Delivery Platform for kubernetes"
   homepage "https://kubefirst.io/"
-  url "https://github.com/kubefirst/kubefirst/archive/refs/tags/1.11.0.tar.gz"
-  sha256 "c9337a457befd64c8c5fd127aa43506b40568464e47de39cb0f96be797d5f78c"
+  url "https://github.com/kubefirst/kubefirst/archive/refs/tags/1.11.1.tar.gz"
+  sha256 "8f90a1b83d88f0a7c383da7b8dfc7c606400f6e0af8e98daa687b8e67c265d20"
   license "MIT"
   head "https://github.com/kubefirst/kubefirst.git", branch: "main"
 
