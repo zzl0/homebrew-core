70d133fdc25baa4aa4d6779bb74e4b4da5804171
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2023-01-27 09:25:49 +0000

    xcdiff: add 0.10.0 bottle.

--- Formula/xcdiff.rb
+++ Formula/xcdiff.rb
@@ -6,6 +6,13 @@ class Xcdiff < Formula
     revision: "289872e572224ae429d0f4d25ff9be906c9df1a0"
   license "Apache-2.0"
   head "https://github.com/bloomberg/xcdiff.git", branch: "main"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "5ce60f558eccc79ec85fbbc6487ee6fe864d9b8f839ad64796654ed4105babe5"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "567bb880d0e5552caca4190e0eb0d3a0ca1b3f5943f95a5ef233372ae2bead7d"
+    sha256 cellar: :any_skip_relocation, ventura:        "206fb28c862e5e9e0a152f7da8d7a1d44c37e17483d7befdbc8dc8e2a4734657"
+    sha256 cellar: :any_skip_relocation, monterey:       "7145bb9bd7d4dbe4b910d453660307328c33f06764664a1288044b002efc3b92"
+  end
   depends_on :macos
   depends_on xcode: "14.1"
 
