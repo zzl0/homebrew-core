4282186642a4d0594cfbb4099698eaa7966cd191
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-27 01:22:34 +0000

    cnats: update 3.6.0 bottle.

--- Formula/cnats.rb
+++ Formula/cnats.rb
@@ -6,14 +6,13 @@ class Cnats < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "2957ec06cf57e9403a4f0e66dd38fc00270a1f523ccc6a489f6b9c74cf883c12"
-    sha256 cellar: :any,                 arm64_monterey: "a482821e19b8f4361c6a9793a4803463d12e647e96bea94f0a0080e50a3001e1"
-    sha256 cellar: :any,                 arm64_big_sur:  "d926ef2351aa53fb393ac2a2cb48cd4c27915f93ddc3d1ae0a8accd823993b1f"
-    sha256 cellar: :any,                 ventura:        "2a610b3216a6800086c4f80a380eed31ddc0a2bbf7b8138ee526cb2945e5b8f5"
-    sha256 cellar: :any,                 monterey:       "fc1e4265ac55a0811a84cd98ce225960146a7c580d5f9d859a955a9736c40f2d"
-    sha256 cellar: :any,                 big_sur:        "f82735102ad5bee2b66caaff59d0a80fb8f9e2f0148686c31ab20cbf1a4aa098"
-    sha256 cellar: :any,                 catalina:       "3a780b02e05c05632ce2d7fe1542150ccaecd844d4ff2eb9300def11ab0cbd8d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2f8bb6744c1b36364c7fbed5081159b5445c539679c5dac8b63fefbb831cccc3"
+    sha256 cellar: :any,                 arm64_ventura:  "be5da6be7ba9b82a4ababc2200099084367c35c53f4636245b79cbd1f8d9253d"
+    sha256 cellar: :any,                 arm64_monterey: "89a3b2b6421ac6af1652eb01aa6c9d6e0ee0cc171fb19f8d7f65933d1e38eac7"
+    sha256 cellar: :any,                 arm64_big_sur:  "c4fee4b22461d6ec27e29c4a396a590d9b8ac02d2aac1000ed964c2a33898b37"
+    sha256 cellar: :any,                 ventura:        "d2f97aa422ac2d7dfa9ba2c5a3677b9bc4c5dd495ae756afd19a93b296229133"
+    sha256 cellar: :any,                 monterey:       "3aad61de788fc4219c444f7f3adfa33c8c7ab5680dc2e2f8fdfb744fe9625a9d"
+    sha256 cellar: :any,                 big_sur:        "baacfbe43787c4cff1ea3dd2efbc1be2c94d36155742017b0cd3df0ef3b9f1e7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6d069e1ff2518b96e9f3ea7249fd0fa484055569cad1e3070acab4466658a653"
   end
 
   depends_on "cmake" => :build
