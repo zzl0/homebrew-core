a87c3eb9ca52a5ea2c00ef738afd55adab62e8a6
Rui Chen <rui@chenrui.dev>
2023-01-29 18:55:48 -0500

    pyenv-virtualenv 1.2.1
    Closes #121813.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pyenv-virtualenv.rb
+++ Formula/pyenv-virtualenv.rb
@@ -1,8 +1,8 @@
 class PyenvVirtualenv < Formula
   desc "Pyenv plugin to manage virtualenv"
   homepage "https://github.com/pyenv/pyenv-virtualenv"
-  url "https://github.com/pyenv/pyenv-virtualenv/archive/v1.2.0.tar.gz"
-  sha256 "1824a1a6d273f6efb38a278b60070bd412cfe6592440b73fcf0e0f2d22730aeb"
+  url "https://github.com/pyenv/pyenv-virtualenv/archive/v1.2.1.tar.gz"
+  sha256 "c60fe08d8d0d5c3ae0eba224081214ce831135d62d75e1d607206411621427d7"
   license "MIT"
   version_scheme 1
   head "https://github.com/pyenv/pyenv-virtualenv.git", branch: "master"
