7f45a281dabd66334bdfb99c203728106952a351
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-29 15:12:32 +0000

    imagemagick: update 7.1.0-59 bottle.

--- Formula/imagemagick.rb
+++ Formula/imagemagick.rb
@@ -12,13 +12,13 @@ class Imagemagick < Formula
   end
 
   bottle do
-    sha256 arm64_ventura:  "a23510ced10fc0545427484fbd45f18b5554330a479493cb57b554f18919881b"
-    sha256 arm64_monterey: "42c5b026d8cc6e51846a1531ad56d94a1efa7172974b71a1e020ae794254a50b"
-    sha256 arm64_big_sur:  "65a569245eda1ca74714790a623e8a56a0928b3ded8a2fe07b8ce2d66b04c77d"
-    sha256 ventura:        "0c318705907c2177065bdd8e55e4cd4c4e56bfce25013d58d745a87d90d52b97"
-    sha256 monterey:       "cc480475364d9667d9422fef0a334fe47c7c99b957ac65e9b96e6c4a46526f19"
-    sha256 big_sur:        "1667ae52deee976227bd68c23bdf6dc3790a86043c4f1ff4d43d9077782ca72e"
-    sha256 x86_64_linux:   "bf3e664306a554b76b6e6277109b2c5f0e85dafde8601140cd4c5777fd7acf2a"
+    sha256 arm64_ventura:  "8210307e11a10df7cfbd54fb99ad5dd0aee57767eb2379db6c6b538167109e10"
+    sha256 arm64_monterey: "bac5b94b44a8748f772170eb3d94fd692d875cd9b8d95491941c673f15a24456"
+    sha256 arm64_big_sur:  "299cb012608536c30fddaeff2009beb42841d807456a2b96276e2b22788aeb95"
+    sha256 ventura:        "31791906b4a07e20bdfc6c8f08b46e4dd41f44d2d660530bfd8961706f1a7bc2"
+    sha256 monterey:       "d7eab9eceffd9b5f2ccbc947c4ab39078410df71a102f2f21515c22c26a51e78"
+    sha256 big_sur:        "4eec4c6fe3a8b4cd8d08f960a5834f5b2dd5cc05b242c9c11a1ec7afdb91b611"
+    sha256 x86_64_linux:   "dcf3e7e117fdb4577e3f703ae0ab2ad3eb0251a4e29faeeef836d80c9425f357"
   end
 
   depends_on "pkg-config" => :build
