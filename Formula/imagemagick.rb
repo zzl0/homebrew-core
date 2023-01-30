4cf1e2e280641b32ce18e1507c5d7c1c4c970378
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-01-30 11:00:53 +0800

    imagemagick 7.1.0-60
    Closes #121831.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/imagemagick.rb
+++ Formula/imagemagick.rb
@@ -1,8 +1,8 @@
 class Imagemagick < Formula
   desc "Tools and libraries to manipulate images in many formats"
   homepage "https://imagemagick.org/index.php"
-  url "https://imagemagick.org/archive/releases/ImageMagick-7.1.0-59.tar.xz"
-  sha256 "ee2247f2ad8798e674de7fba37e7f3986f5b6be9abec042ab00b0befa8ae5b70"
+  url "https://imagemagick.org/archive/releases/ImageMagick-7.1.0-60.tar.xz"
+  sha256 "ea1a6de16328b0801d684df02498334bff5ad70521f0c509ea9f1209eb4ac5f6"
   license "ImageMagick"
   head "https://github.com/ImageMagick/ImageMagick.git", branch: "main"
 
