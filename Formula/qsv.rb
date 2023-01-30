84d62cc991d6c1460145e4cd2088ece1433bc971
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-30 09:04:47 +0000

    qsv: update 0.87.0 bottle.

--- Formula/qsv.rb
+++ Formula/qsv.rb
@@ -7,13 +7,13 @@ class Qsv < Formula
   head "https://github.com/jqnatividad/qsv.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "27fd785b46fac65e1a4e7f0d05b713ed6f26cf7df27842d0b3cd1838a29594e7"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ce526a15b1d6da7e1ed969a64386e8ac536d1a01adeeef863c819ba41f087b2b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3a82c890e6eca44b2990ece25a7444b17055bf6f74b7ab2f22cba83a3e1b054e"
-    sha256 cellar: :any_skip_relocation, ventura:        "e8e70e16508a3a224c68bab947ec440aac46f600c3c5f8557c941f40d9fb643c"
-    sha256 cellar: :any_skip_relocation, monterey:       "bc6692d9719cdcfa44b14c8c60ade92c5b8ba5970cd9dd633a9365747318ae3a"
-    sha256 cellar: :any_skip_relocation, big_sur:        "59c87fb39a532fd0a87c500e3e743a5d29f8e6abb3298ff5143daedccfe8e259"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "913e44f95da33e7664198dbd6f46d94318695476ef8ed291f661e7f1c179677b"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "dfce25a821827f059cad4617f5d9bfb91f331b2ca18cca8bd0e066e6cf0302e0"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "755432c8e8ff73babad92107f2ac067fa8f4549a330cf2229239ecb7cdc6a89b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3e2523c13403c177c7beccbd14daaf85968a0ce18a245a883ba15fcabacb78ba"
+    sha256 cellar: :any_skip_relocation, ventura:        "d883f79d180b356a5da9a9d932527cd74174d7a8b1f3c1f23cc8503ac554e27c"
+    sha256 cellar: :any_skip_relocation, monterey:       "4bff3ca9a0c43fcbf5c931ca2ac82152ee84e3b2ef20ad71f6eb82a5059dc8a6"
+    sha256 cellar: :any_skip_relocation, big_sur:        "5511a8d725bf32430497d4f966d6e7808d4ffb7818ef12b910d8c5f64a04ef20"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d9306aa4df7b0741fba28e63b128210cf6d59f9e3417dbae2b5fad24c1837188"
   end
 
   depends_on "rust" => :build
