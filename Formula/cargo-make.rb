70de90945a9963bc9227f41b97a922415f3d3b8c
Rui Chen <rui@chenrui.dev>
2023-01-30 14:35:28 +0000

    cargo-make: add 0.36.4 bottle.

--- Formula/cargo-make.rb
+++ Formula/cargo-make.rb
@@ -5,6 +5,16 @@ class CargoMake < Formula
   sha256 "69c24bd9d0405d07bdf681e4fdf96bf32a95ca4b439f203e7f3997fdf9800781"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "51981f929d924672b96fc1be7ed9ba2243cfca5229d334fc2daad22eb10d2960"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f3d226f76d15c051c72fe247146740cbd2bc7fe6c8e6d62c65aec24c94709b7a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bb0a42822849e445d504fa06f3162a5664975f041d1c6e8e0a0dc12eb196551b"
+    sha256 cellar: :any_skip_relocation, ventura:        "6640ce0b0d36997660bf7fe4bbe579cad3655edc9ec3ffa8f3c4d58f0562164b"
+    sha256 cellar: :any_skip_relocation, monterey:       "ab1050f052b1b356fa2cefc873b1bc36a9f6e47b1e06b305e8d02ec1ef3fd483"
+    sha256 cellar: :any_skip_relocation, big_sur:        "084f666b270ab356c9a88f75b004ac5f4eff51318891a830faef58b9da3b2e12"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "146e38a7fa55d1f6b5869b0949fcf6f183991a2ed088ce2f65969be244860278"
+  end
+
   depends_on "rust" # uses `cargo` at runtime
 
   def install
