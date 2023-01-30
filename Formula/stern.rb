b6c44a9f1c967f8d4754d407822cf7bc7a730804
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-30 02:23:06 +0000

    stern: update 1.23.0 bottle.

--- Formula/stern.rb
+++ Formula/stern.rb
@@ -7,14 +7,13 @@ class Stern < Formula
   head "https://github.com/stern/stern.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "c5c169b70031f867d167d50b6cc48e1ac2cd1cf43cffe1fb2c2b4a9c41470d78"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "65a3d21674e3bcfd264e85d1c97a5aa15343c2fa4610d540706ea91df0ba5af4"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ea3cca066c91689c2b9dc2abd907a990914e8128a8ec4a5028cc2b939f254ecd"
-    sha256 cellar: :any_skip_relocation, ventura:        "a73a7987fd803b11154e28a1da6a5d6dd9152ffc77a6531769d31872d1b56493"
-    sha256 cellar: :any_skip_relocation, monterey:       "94b19b9e469907adb6a87c582f60e408cf7768e0ca9559943c6b54a0134533a7"
-    sha256 cellar: :any_skip_relocation, big_sur:        "a34de49b0e3970f5e7ef95b118cc96ef9db29cece500cf91f48b53341a7fdbc8"
-    sha256 cellar: :any_skip_relocation, catalina:       "29fd319cbaccaa116eec241aa8855d632e7c87f613135e254a1a0dbb594b5b23"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6a0feae6285e6386b4727b8f141c2b99a8a451093e669dc719e9b83e89425c7d"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "779bfcc0582b42cc58e40e0eb2df3c1051e4b362bcb2be09c4d4a33e9647c70c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a872d0f4339f4d49aec7c7466339c477fce387e9b65a9488d36444105fad494b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7ba22acfc51250bb56dec297c93edf5259ff0ef7bd6fd9a0c3a2254818f8c85e"
+    sha256 cellar: :any_skip_relocation, ventura:        "ee7e364fa474787dac605b33b162787e1766678e07f5c13c93340844aae5cac3"
+    sha256 cellar: :any_skip_relocation, monterey:       "fd864cf5ad9a921d71edaa2e34478845f5a81514939e5ae02b9bd0962e01d556"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b9c296769c4e62895fe661dc1e6f59b8226f282fc20de0f6602e22bb589d6033"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4ce12cade569bbd3bd5d21a513693dcb8cd3b393b29183c89c1c74c0f1b93e92"
   end
 
   depends_on "go" => :build
