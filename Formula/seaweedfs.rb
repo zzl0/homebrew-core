b2439adf3b60c0e598a8a2f82b8574b8849ccecb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-30 10:05:19 +0000

    seaweedfs: update 3.41 bottle.

--- Formula/seaweedfs.rb
+++ Formula/seaweedfs.rb
@@ -8,13 +8,13 @@ class Seaweedfs < Formula
   head "https://github.com/seaweedfs/seaweedfs.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "1325d33eea3d4a42ad3685ecd3296ce925a9347fd82d6b8eff325ee1a2c8e63e"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ef0cccc997c0bc2e12070afb2582354dd78cd21e7cc9a6d4482959693e5a7dd2"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4dc8d83723418609d7d8b2c876556b22c7bf49b9471c110447378c6015845c23"
-    sha256 cellar: :any_skip_relocation, ventura:        "61e28ac0578cd5ca6ce26511d4945c775b4eb4f7cf2feed1c1b233cea3874e53"
-    sha256 cellar: :any_skip_relocation, monterey:       "138d2932e5be1c71566c589e6bd35b24485c05a5566b7bf1cd2ffa8938a20ae9"
-    sha256 cellar: :any_skip_relocation, big_sur:        "715658c012d5dca55c5c5af0af1940b455bfbe6b2cb52852b0d26f8557a6463f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4d7d5c03446128e26dffad73b92574040b1d95259e7bd6458bbf313f2d0d9f27"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "b1d44643a8b9170d1302521651447a39aacae01fbff34d15385af2c8c14a306c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "fd5d747d4e8f05f212d32a7886f0bf9be52feca8df47d88846a24da57c08830b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0f5c347f6b1df299c6905662497edb1d93d5314f854da70776cadea025370fb3"
+    sha256 cellar: :any_skip_relocation, ventura:        "49d0f96c2a13c1ca47003eb9ffcd88962499cd0878fdfe863e7f5207990d5111"
+    sha256 cellar: :any_skip_relocation, monterey:       "ac916cdcd044bd87bc73c2b10b50be01dac491a9ca74c45acca5cca59ec73d9f"
+    sha256 cellar: :any_skip_relocation, big_sur:        "66d37d1587bbbf19e57872a151429398eee1fb3d9175955ad656687f2971542a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "61169caf655c7caacbf7fa3edb5dd2df3047de1b4c14b3aeebfd4a3e3da7b7e4"
   end
 
   depends_on "go" => :build
