087be307d7ab390f7114d30f5576ec2784fc7260
Porkepix <porkepix@gmail.com>
2023-01-31 01:06:13 +0100

    apache-flink 1.16.1
    Closes #121865.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/apache-flink.rb
+++ Formula/apache-flink.rb
@@ -1,10 +1,10 @@
 class ApacheFlink < Formula
   desc "Scalable batch and stream data processing"
   homepage "https://flink.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=flink/flink-1.16.0/flink-1.16.0-bin-scala_2.12.tgz"
-  mirror "https://archive.apache.org/dist/flink/flink-1.16.0/flink-1.16.0-bin-scala_2.12.tgz"
-  version "1.16.0"
-  sha256 "63ba08dd23d2979dab2cd3c192a69f548040b5411479a7ef430517c8e4a8009b"
+  url "https://www.apache.org/dyn/closer.lua?path=flink/flink-1.16.1/flink-1.16.1-bin-scala_2.12.tgz"
+  mirror "https://archive.apache.org/dist/flink/flink-1.16.1/flink-1.16.1-bin-scala_2.12.tgz"
+  version "1.16.1"
+  sha256 "30c0b96cbf73fbe834fa5bfb8b187d463ebfa955f5d43d7aa3198d87eb2c7a65"
   license "Apache-2.0"
   head "https://github.com/apache/flink.git", branch: "master"
 
