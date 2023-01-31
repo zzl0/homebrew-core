43469083d500cb2a4986b44d3c07c8acf55d7cfa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-31 17:04:47 +0000

    checkstyle: update 10.7.0 bottle.

--- Formula/checkstyle.rb
+++ Formula/checkstyle.rb
@@ -6,7 +6,13 @@ class Checkstyle < Formula
   license "LGPL-2.1-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "ecac86db8bcdc7817092a817fe6a105cb3e5f74793b96ccf785552f227f9eff6"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "5164c76783d978eb06cbfe1f016104277a2c81560e74be3559b179f544960308"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5164c76783d978eb06cbfe1f016104277a2c81560e74be3559b179f544960308"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5164c76783d978eb06cbfe1f016104277a2c81560e74be3559b179f544960308"
+    sha256 cellar: :any_skip_relocation, ventura:        "5164c76783d978eb06cbfe1f016104277a2c81560e74be3559b179f544960308"
+    sha256 cellar: :any_skip_relocation, monterey:       "5164c76783d978eb06cbfe1f016104277a2c81560e74be3559b179f544960308"
+    sha256 cellar: :any_skip_relocation, big_sur:        "5164c76783d978eb06cbfe1f016104277a2c81560e74be3559b179f544960308"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6da8240bca42fec747eaecd3a56417aa08a5417f92c90e7ed31540782838d3bd"
   end
 
   depends_on "openjdk"
