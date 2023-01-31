8f37fa8d7c71d9fe375a0dad6044abeb03fda43f
Paulo Moura <pmoura@logtalk.org>
2023-01-31 16:09:02 +0000

    logtalk 3.62.0
    Closes #121942.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/logtalk.rb
+++ Formula/logtalk.rb
@@ -1,9 +1,9 @@
 class Logtalk < Formula
   desc "Declarative object-oriented logic programming language"
   homepage "https://logtalk.org/"
-  url "https://github.com/LogtalkDotOrg/logtalk3/archive/lgt3610stable.tar.gz"
-  version "3.61.0"
-  sha256 "63f59e72061a4abbd62dd237627566768fe1ec148146358f0a63204640170216"
+  url "https://github.com/LogtalkDotOrg/logtalk3/archive/lgt3620stable.tar.gz"
+  version "3.62.0"
+  sha256 "f60d5a5dced8e0b0256854870ddbb0a3d3b08f001c5e62d8af56ada2668ce563"
   license "Apache-2.0"
 
   livecheck do
