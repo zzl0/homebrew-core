b774041e364d8b70e0f7789392a7e81a0d5d6b30
Porkepix <porkepix@gmail.com>
2023-01-31 01:25:21 +0100

    msmtp 1.8.23
    Closes #121876.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/msmtp.rb
+++ Formula/msmtp.rb
@@ -1,8 +1,8 @@
 class Msmtp < Formula
   desc "SMTP client that can be used as an SMTP plugin for Mutt"
   homepage "https://marlam.de/msmtp/"
-  url "https://marlam.de/msmtp/releases/msmtp-1.8.22.tar.xz"
-  sha256 "1b04206286a5b82622335e4eb09e17074368b7288e53d134543cbbc6b79ea3e7"
+  url "https://marlam.de/msmtp/releases/msmtp-1.8.23.tar.xz"
+  sha256 "cf04c16b099b3d414db4b5b93fc5ed9d46aad564c81a352aa107a33964c356b8"
   license "GPL-3.0-or-later"
 
   livecheck do
