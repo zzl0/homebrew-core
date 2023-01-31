2313d9ed7e67f5107877ce36d1595e2559d71894
Patrick Linnane <patrick@linnane.io>
2023-01-31 09:27:35 -0800

    crun 1.8
    Closes #121964.
    
    Signed-off-by: Alexander Bayandin <alexander@bayandin.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/crun.rb
+++ Formula/crun.rb
@@ -1,8 +1,8 @@
 class Crun < Formula
   desc "Fast and lightweight fully featured OCI runtime and C library"
   homepage "https://github.com/containers/crun"
-  url "https://github.com/containers/crun/releases/download/1.7.2/crun-1.7.2.tar.xz"
-  sha256 "dfce0fdf042c7de84e8672369f54f723c2f788d2bde076a4c6edf530e6306b5a"
+  url "https://github.com/containers/crun/releases/download/1.8/crun-1.8.tar.xz"
+  sha256 "6dc621b6825eaf0d6a8f70d2a7fa9adde24cee3ad9221a5efbadb0b8b25e0843"
   license any_of: ["GPL-2.0-or-later", "LGPL-2.1-or-later"]
 
   bottle do
