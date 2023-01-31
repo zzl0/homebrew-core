99230782ddeba4b5968910b776f67989d54177e9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-31 20:11:38 +0000

    grafana-agent: update 0.31.0 bottle.

--- Formula/grafana-agent.rb
+++ Formula/grafana-agent.rb
@@ -6,13 +6,13 @@ class GrafanaAgent < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "2e939578782b1f131d02745817d0ce292a98e85852364135a532ecd50afea22a"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ebf49db6472bc2e5325f764d82b79ed92cf426da8b80d7803e34f0058864dd02"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fd5691486d9ad604af38b54ecccf14ae204c5ce0b903bf6d81b9bfd2bfd21f22"
-    sha256 cellar: :any_skip_relocation, ventura:        "a6de2e77ccb56495c96e06083a2ac0bb15f4d7543cd0a810292e70fbef63aa56"
-    sha256 cellar: :any_skip_relocation, monterey:       "5a41ed36b795304ae278978bf293646c01830cf4a3ec2d47a250f1f562b7c956"
-    sha256 cellar: :any_skip_relocation, big_sur:        "55ca13561a26e89d09de9b607ee857e87302a4e281f480f862aca201c09d9f04"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "aee0e18946028edfec3bb00312226f17236126659d62bfcc943075a27ec0f1c5"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "bbf086f1e39bc501d9f7c2dc51d2fa20e0b1e35d02f2e698821787d042fabb62"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "573b0270125d6e42a956c5f0266cd9a392ce8aa6917b95317dcb5f782b9e864f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "acff1c648b3c8eb11a71b18327c17a11fb140c2cf077d282c355f6487a8c68b4"
+    sha256 cellar: :any_skip_relocation, ventura:        "72e99cf6195d6d868094a4aa5b2cfbd77c6623684288cca0c48c6f9bfdcd9819"
+    sha256 cellar: :any_skip_relocation, monterey:       "348b394bac5c22d7b4c29b3665b54a49cee707b8d8d3ea1178dc5a9f0cb516c3"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f045b21e7b72f50f1fbfd4a2516bbc91af8f50dddac583ade3ed0b6095c8830c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "539c82c833a329045e7fdd9e3bbc523ddaf26fca11c64a20198a48a014f2cf5d"
   end
 
   depends_on "go" => :build
