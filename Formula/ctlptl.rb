3d360255e1ad585de3f0f5cc2441e5c2db42a4da
Porkepix <porkepix@gmail.com>
2023-01-31 01:09:06 +0100

    ctlptl 0.8.16
    * ctlptl 0.8.16
    * ctlptl: update sha
    
    Closes #121868.
    
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ctlptl.rb
+++ Formula/ctlptl.rb
@@ -1,8 +1,8 @@
 class Ctlptl < Formula
   desc "Making local Kubernetes clusters fun and easy to set up"
   homepage "https://github.com/tilt-dev/ctlptl"
-  url "https://github.com/tilt-dev/ctlptl/archive/v0.8.15.tar.gz"
-  sha256 "64aa07c2553f7189f345223cbffeb02589bce1c7700364d8cae8727f5d97a55a"
+  url "https://github.com/tilt-dev/ctlptl/archive/v0.8.16.tar.gz"
+  sha256 "c82d088e31e56458d9ddb6412e81d173c1eaf996b9b3029834b6026cd3c2124e"
   license "Apache-2.0"
 
   bottle do
