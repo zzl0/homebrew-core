cb9a5d7beb70f588d93a8d89b1871f230eac0ace
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-31 23:04:50 +0000

    atmos: update 1.25.0 bottle.

--- Formula/atmos.rb
+++ Formula/atmos.rb
@@ -6,13 +6,13 @@ class Atmos < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "bf204cc4db47f54733a7b8e8798be44c4a03e3b06e15a5b3e84fd70154633817"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "7c0945bdb8d498181c17019930fecdfcff472bafb6ab7827450276489c9b4a75"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "53d93d8071f6a3a057ac340f0e3cd2c226fe845215142d0bdf4c74cd3cd6d840"
-    sha256 cellar: :any_skip_relocation, ventura:        "7206acf3d644f95e5dbd7ffcd1c22727066d74b2c625102d6d5cd38a4765543a"
-    sha256 cellar: :any_skip_relocation, monterey:       "f3218b125f713ac3758290df6f39f383e717004d3acb1201d1d776f527966389"
-    sha256 cellar: :any_skip_relocation, big_sur:        "bc3874c5f38f383619368ff3aa31cc65cf67a1328ad49b96a6dd42a756cd2d5d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "878511f16f76174e8333c7be95245a401c9c68a303b6982676fc7e6603f4c16c"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "9fae087e094659a15eb1d2cbc0957b847fec000297c302740cd9496a0975f546"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5cd375879cc1241609f25aa9892cdcb136bcdb402dfe6e56668b756f74b397b2"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "35aaa218ed3ec5f6c70ca2fc031401fca39c0ee6f8410ba3f50ca41cdbd15511"
+    sha256 cellar: :any_skip_relocation, ventura:        "6f0e608a84fe531e7d61dbcd44fab8041187f86b2e064226430adae78a0de5e2"
+    sha256 cellar: :any_skip_relocation, monterey:       "ae1d40f651a19e3de96857ecd88375a1b1a1f87757d75777c5f25c62aebaee91"
+    sha256 cellar: :any_skip_relocation, big_sur:        "9ea20d4a6be2c6f56c62cfd12cd3ba1d8f285afeb13e538a44c2be7aaff905a1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9ac7d542f79e608e6a2a96a6bce38fcb4052a3740b6cab273fa3581234830012"
   end
 
   depends_on "go" => :build
