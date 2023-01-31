fab2e8946772e447ab81eaa79471a313b6bb663a
Rui Chen <rui@chenrui.dev>
2023-01-31 14:14:23 +0000

    opencl-clhpp-headers: add 2022.09.30 bottle.

--- Formula/opencl-clhpp-headers.rb
+++ Formula/opencl-clhpp-headers.rb
@@ -6,6 +6,10 @@ class OpenclClhppHeaders < Formula
   license "Apache-2.0"
   head "https://github.com/KhronosGroup/OpenCL-CLHPP.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "77525acc6124b241ec72b91b5d6fa5fdb681e823c58451a2d17ee98d43c9456d"
+  end
+
   keg_only :shadowed_by_macos, "macOS provides OpenCL.framework"
 
   depends_on "cmake" => :build
