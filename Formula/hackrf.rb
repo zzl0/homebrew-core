87368537eea6cec4ec19afd56d891dd1e43e1482
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-01 14:26:59 +0000

    hackrf: update 2023.01.1 bottle.

--- Formula/hackrf.rb
+++ Formula/hackrf.rb
@@ -12,14 +12,13 @@ class Hackrf < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "4c6c13e649df4b2457a93a5269f2376cfc7fab7cafc88afd4914cdce52d238e1"
-    sha256 cellar: :any,                 arm64_monterey: "8c6eed0a7f15d8fe524f3db7256624caababc440e6a3db0d2c7ce39244592d87"
-    sha256 cellar: :any,                 arm64_big_sur:  "5ce7b90e03e6cfe0e3a4cae61133539d7a930a5cbfb3d841918a4adde7bc95d3"
-    sha256 cellar: :any,                 ventura:        "fae966e776918d07134f6cb9db2ff637e5693a32fa5bb29116828f0a219174b1"
-    sha256 cellar: :any,                 monterey:       "afbe5e6273018258bb621487af4d00e6dc5167ad8d153eedddc85679b530f227"
-    sha256 cellar: :any,                 big_sur:        "5d33b942ba20327b818a4f3bb77a62bddcc15956708ba1cfee27daabb748c14c"
-    sha256 cellar: :any,                 catalina:       "e4e88439a674526e27764c61813571ded730ea4e7ab8fbf1e573332e428d4ef2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2009cd97f0d99ffe8a075e65fd329c0687997336bf260374a1cf08cba4aedbb0"
+    sha256 cellar: :any,                 arm64_ventura:  "5e3a08883caa11b3efc07014bb53cac7f5de322b2eb2a6e3e27bade295b1e6f7"
+    sha256 cellar: :any,                 arm64_monterey: "ec83b7be635e119651ee1715f6efa57c72d4d98f560e27fbfd4456598ab805e7"
+    sha256 cellar: :any,                 arm64_big_sur:  "60831932de8d4499a1f1c54f1810d001ab3b7e3af0732735dd5e3df5a93ab935"
+    sha256 cellar: :any,                 ventura:        "99bd5f158dbb6d6ceeafa5eb0c53b7bf91cdad7ace7bdd2c3102022b130c6cd5"
+    sha256 cellar: :any,                 monterey:       "b21f730ddf1a1a8b117585bbf9beb271c73767fe3c69d0a7bd84812cb0256bfd"
+    sha256 cellar: :any,                 big_sur:        "0c8b55d2e955787afeccdbee0ce02e168d35c3d72be0b7c627f9e9dc693c0f4e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fb0ddaecf5f1a3b3422c029113568f8fc5f04a5a3b72a0dae20b47da8e210a2b"
   end
 
   depends_on "cmake" => :build
