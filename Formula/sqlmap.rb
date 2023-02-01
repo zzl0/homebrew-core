a13cd7c63783a9eebd5e9b83614e4be06fd0916e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-01 14:27:25 +0000

    sqlmap: update 1.7.2 bottle.

--- Formula/sqlmap.rb
+++ Formula/sqlmap.rb
@@ -9,13 +9,13 @@ class Sqlmap < Formula
   head "https://github.com/sqlmapproject/sqlmap.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a4700564a062b13cdb54ff7dcb3073de2e97181fe3370f5ad48ca9a4ca8a91a9"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a4700564a062b13cdb54ff7dcb3073de2e97181fe3370f5ad48ca9a4ca8a91a9"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a4700564a062b13cdb54ff7dcb3073de2e97181fe3370f5ad48ca9a4ca8a91a9"
-    sha256 cellar: :any_skip_relocation, ventura:        "7d52433d0a3ff9ea5a0fde23799aac67d54bff616a7b981155b0a3d7022b5550"
-    sha256 cellar: :any_skip_relocation, monterey:       "7d52433d0a3ff9ea5a0fde23799aac67d54bff616a7b981155b0a3d7022b5550"
-    sha256 cellar: :any_skip_relocation, big_sur:        "7d52433d0a3ff9ea5a0fde23799aac67d54bff616a7b981155b0a3d7022b5550"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "52352a4ce1819d0b244f121ee6ae4097d10982493bf433d5ea2e5026b6727b31"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "2f78c876e31c3d6d1da72ab159e9cd173bc5de9b85bb91d92d4af4399c0f637c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2f78c876e31c3d6d1da72ab159e9cd173bc5de9b85bb91d92d4af4399c0f637c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2f78c876e31c3d6d1da72ab159e9cd173bc5de9b85bb91d92d4af4399c0f637c"
+    sha256 cellar: :any_skip_relocation, ventura:        "59ff1490cb1bcd310f57c564eaa766d7c5bf81c7e30022584152ec6247b3e990"
+    sha256 cellar: :any_skip_relocation, monterey:       "59ff1490cb1bcd310f57c564eaa766d7c5bf81c7e30022584152ec6247b3e990"
+    sha256 cellar: :any_skip_relocation, big_sur:        "59ff1490cb1bcd310f57c564eaa766d7c5bf81c7e30022584152ec6247b3e990"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5254bdecaa42d5f48ce7b3f10ba962d10d55e57749fa1aa22c9c05f04f70bed6"
   end
 
   depends_on "python@3.11"
