df144df6e8bb231a2f1332b3617cf49b311f4aa4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-01 12:05:33 +0000

    lazygit: update 0.37.0 bottle.

--- Formula/lazygit.rb
+++ Formula/lazygit.rb
@@ -7,14 +7,13 @@ class Lazygit < Formula
   head "https://github.com/jesseduffield/lazygit.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "9a75d4db80160423bdedce1aff7cad42e92b1e977caf50e2dffd2da798ad2505"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "dcb5b4c336b73a2d5afd5999ec46b2f47644b5af9945cae2cf12e942ba4240db"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2206a8386cff28f6cb2e0f37c8656eff4a75b2adaa86fb7eb68f1dc5b6000b1f"
-    sha256 cellar: :any_skip_relocation, ventura:        "ca4758a1f2b71d77ff363acea9c6674fbbdbb2cc0e4b64ca5ff3eb31ba0ba11f"
-    sha256 cellar: :any_skip_relocation, monterey:       "134491db74e4fecb5f83e9a7fd39003f183c8e54311d208c704ba039f128e6a9"
-    sha256 cellar: :any_skip_relocation, big_sur:        "e2bedc1f205405960d6f5a31a5430216527f6312ba8125f89b0e1920fc281ccc"
-    sha256 cellar: :any_skip_relocation, catalina:       "72ca9a23d0b77b20b57a00b4fe06abdc2e7bc1def7c355a411838a9d5b3d560f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "737ff10826b6c948a4bf5cc8efdae0388c5fa7bd6f86e0f1121495847fc7c18d"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "401a0c31032572f855a5f2207d78f318edfa082cfced8c586911d9aff711dfab"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "56dcb01075d98bac3834eca8f5023822b255c955e4087d58c8a420516eea8d3c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "564d259b773bf98fd0f5cc5e5c0ccffed6b948d2f0c64cb72bc23d064cdd0979"
+    sha256 cellar: :any_skip_relocation, ventura:        "7a7f25fdb5cb29c582e1b2fabaaec87064f295bb6d545b52bba7f19c34605b01"
+    sha256 cellar: :any_skip_relocation, monterey:       "908b7ccde3658256507427bb9586d594797d65bda3e846e029a10076658bab59"
+    sha256 cellar: :any_skip_relocation, big_sur:        "01e9bc26a8b7cc95dc8f667a10fffb680436c2b19b2f8c58bee05f04396005f3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "35c4642f0ce88752b94add6464bbf519120ba4fd3d40407b654923c05ca1e8e9"
   end
 
   depends_on "go" => :build
