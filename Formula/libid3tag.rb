d4fb6e84da8aa97c7c4b8dc4c29e90de3b1ce96e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-01 23:47:14 +0000

    libid3tag: update 0.16.2 bottle.

--- Formula/libid3tag.rb
+++ Formula/libid3tag.rb
@@ -6,14 +6,14 @@ class Libid3tag < Formula
   license "GPL-2.0-only"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "ea3be63c9af50a5727ffea6b348d2dd1f71e3e1d002923d82d78baafd15833b3"
-    sha256 cellar: :any,                 arm64_monterey: "5f3d9e17a9e11f9e7ce446b248031ff52f3ad7a3fe6ca45dfeec015faa3e7720"
-    sha256 cellar: :any,                 arm64_big_sur:  "fb715e7759925e8f295d11ec506681abb2f72657ba2394b05c01fb0604c293f2"
-    sha256 cellar: :any,                 ventura:        "80ced912c1c8af4628cc7c5c377aca60122d24fe627d59b0aa60c01edd71335b"
-    sha256 cellar: :any,                 monterey:       "9b079278969ce8cc27f7740b3a071f1060970164a2e047aeae0167e185eee3ff"
-    sha256 cellar: :any,                 big_sur:        "ffeb1df56aa2f9a1cc0e9dda5116cb1017fa3055a47e63eaefc159f5ae5db8a3"
-    sha256 cellar: :any,                 catalina:       "110a77d41509a1fe27d1638005f6b903488e7591052e1d0f327555f6e72fcf2b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "62d2e52d95f5d011219fe29d4571ec9b27d2acbb8ec9ee1e394ce196fb4abea9"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_ventura:  "f881ed95a669df1c88dca0c60fcf2eee77a752884a3e2b6332b70cba9d51f2f9"
+    sha256 cellar: :any,                 arm64_monterey: "bafa60c5f60c3f181eaf53ff64acae1cf04cb3600a49755f0eb7a589ad2f6b0f"
+    sha256 cellar: :any,                 arm64_big_sur:  "05d1ea6709e912c43d07043476aab2ddc131198215bcf2caffe3a1c5176a6d7b"
+    sha256 cellar: :any,                 ventura:        "2dfe1c49fc68e3ea8e18a456775282288158ae32f0d61d659ce1a8b3d70b3ff6"
+    sha256 cellar: :any,                 monterey:       "86d3dbe6f29ca5cc9ccd255c2c488c2267d2fa3bdcada964b25d3cf6014d0701"
+    sha256 cellar: :any,                 big_sur:        "d37b4456d86459e44c6784bb0c722f15bf59f558905c3c17edfd8537d80d141b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c78951ee4f80eaec7f2d958c790612e6f7edcadddc7a38f2c89c24438d0eeb48"
   end
 
   depends_on "cmake" => :build
