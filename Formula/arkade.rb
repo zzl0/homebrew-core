a1aa672681a0e0ca209cf6858c1e2e71ca5c4b62
Patrick Linnane <patrick@linnane.io>
2023-02-01 09:14:07 -0800

    arkade 0.8.62
    Closes #122065.
    
    Signed-off-by: Alexander Bayandin <alexander@bayandin.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/arkade.rb
+++ Formula/arkade.rb
@@ -2,8 +2,8 @@ class Arkade < Formula
   desc "Open Source Kubernetes Marketplace"
   homepage "https://blog.alexellis.io/kubernetes-marketplace-two-year-update/"
   url "https://github.com/alexellis/arkade.git",
-      tag:      "0.8.61",
-      revision: "995b69f9c7aef7f1d851d57be60262d128ee8e65"
+      tag:      "0.8.62",
+      revision: "003e8716bbc987c412643af3a90cf87ef778bd2e"
   license "MIT"
 
   livecheck do
