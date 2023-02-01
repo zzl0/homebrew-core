5f684bb348c86932f3e63784c01a13a5b7a2ebff
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-01 14:27:32 +0000

    highs: update 1.4.2 bottle.

--- Formula/highs.rb
+++ Formula/highs.rb
@@ -6,13 +6,13 @@ class Highs < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "99d84f9503723aa94d494bfd5822fb47682c0d2a5f367ad6f62620eee2929696"
-    sha256 cellar: :any,                 arm64_monterey: "4f40fcd5160c5eff1467f73c7472ef045973530e31a0adb3b486fb1cdfc8db9d"
-    sha256 cellar: :any,                 arm64_big_sur:  "cdc09f5a1b582099b30d76980d0526579546ea8d626c264dee8dfc15049f1f2d"
-    sha256 cellar: :any,                 ventura:        "93f06ef7febdb7d40eca38d4986c1820b7899e56ebb1825fca79f7540244ad30"
-    sha256 cellar: :any,                 monterey:       "84cb0d8c138cecdf025bb6d6ed38322e9232dc3a1aefd26c9e967ce2b3208a9c"
-    sha256 cellar: :any,                 big_sur:        "a50c1abc5c0b796bc28545a3781764a064c3ed79e1a8fcebda4443ec17b2c5bf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6c6b47c9dd833f6adcb9e0db6a4b29b9a35cf42b177821f947ce1dd57ab8ac3e"
+    sha256 cellar: :any,                 arm64_ventura:  "a4a684b011f96af569fb32340d18a58b3359b61f89f1910fdeda21efe299b147"
+    sha256 cellar: :any,                 arm64_monterey: "c9dd1425677efb108ded014763fac5d40d54bd2e33c0bbd33887f90acf08769d"
+    sha256 cellar: :any,                 arm64_big_sur:  "e3df26ac9753742df94b0952646b059d46f5092c8a226544cc2cf0308abf71cf"
+    sha256 cellar: :any,                 ventura:        "34e452ace5161f50329756ee7d397c7910dfdcc6cdadc8b34d4c994a39d0953c"
+    sha256 cellar: :any,                 monterey:       "de80d9d305f440d3152f58662776eee4acbb7171b6f4f97bb38c48aa8210437d"
+    sha256 cellar: :any,                 big_sur:        "ba6b57734736f84d0d6eb8656e50fcd4d419493e323239fbb6e9f27e194f065a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3a59b79dc5843a6acf27c25d0a935cfa0ffb2b7e3840d7bf5ea0c6cb642420f4"
   end
 
   depends_on "cmake" => :build
