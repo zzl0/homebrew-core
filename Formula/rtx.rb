fca5a55894e24f66ea9e4ac81371c27415eaabeb
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2023-02-02 15:53:22 +0000

    rtx 1.8.0 (new formula)
    * rtx 1.5.1 (new formula)
      https://github.com/jdxcode/rtx
    * rtx 1.8.0
    
    Closes #122157.
    
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/rtx.rb
@@ -0,0 +1,18 @@
+class Rtx < Formula
+  desc "Polyglot runtime manager (asdf rust clone)"
+  homepage "https://github.com/jdxcode/rtx"
+  url "https://github.com/jdxcode/rtx/archive/refs/tags/v1.8.0.tar.gz"
+  sha256 "3983c2239a31c0376e8c8da4ec614e96ed19e80bd41610407572e2ad4303e87a"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    system "#{bin}/rtx", "install", "nodejs@18.13.0"
+    assert_match "v18.13.0", shell_output("#{bin}/rtx exec nodejs@18.13.0 -- node -v")
+  end
+end
