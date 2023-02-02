a1f1fb6481b2234e36d82a1014463f13f1c44768
Rui Chen <rui@chenrui.dev>
2023-02-02 07:44:53 +0000

    crfsuite: add 0.12 bottle.

--- Formula/crfsuite.rb
+++ Formula/crfsuite.rb
@@ -5,6 +5,16 @@ class Crfsuite < Formula
   sha256 "e7fc2d88353b1f4de799245f777d90f3c89a9d9744ba9fbde8c7553fa78a1ea1"
   license "BSD-3-Clause"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "7c00ca61a741c84e661cb89f208a79105453f051fb766390ca4afb6d5cbc815c"
+    sha256 cellar: :any,                 arm64_monterey: "de3937f3b5caa7a27f48b183cca1a3dbe64fe0ba642f624f35a5d3e1827fa13f"
+    sha256 cellar: :any,                 arm64_big_sur:  "8f4faf686ec2dd149e8c55505fb18f4f2fd246cf0966ed315c19eb811855aeb1"
+    sha256 cellar: :any,                 ventura:        "9044e7b8b91b781be38409cc180e7889fdf5430699025628726dc21919324704"
+    sha256 cellar: :any,                 monterey:       "72d451e62bf3ab7b5b2d73d9cb4757946e1c0aa75c3c5f28c1c2d899d052bdd1"
+    sha256 cellar: :any,                 big_sur:        "72b8c9d618a16bd4287990ae6c7b46bfdfd964cbe20582d4fa10f5b4b12f09ba"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "45a6dd13114c20e2a4bd3d82033e463316b635d8c7a61c582e299bca8832ec58"
+  end
+
   depends_on "liblbfgs"
 
   uses_from_macos "python" => :test
