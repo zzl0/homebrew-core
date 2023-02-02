440e496049fe06e1d832a981b7f2136bd49a0157
Rui Chen <rui@chenrui.dev>
2023-02-02 16:31:24 +0100

    docker-completion 23.0.0
    Closes #122146.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Alexander Bayandin <alexander@bayandin.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/docker-completion.rb
+++ Formula/docker-completion.rb
@@ -2,8 +2,8 @@ class DockerCompletion < Formula
   desc "Bash, Zsh and Fish completion for Docker"
   homepage "https://www.docker.com/"
   url "https://github.com/docker/cli.git",
-      tag:      "v20.10.23",
-      revision: "715524332ff91d0f9ec5ab2ec95f051456ed1dba"
+      tag:      "v23.0.0",
+      revision: "e92dd87c3209361f29b692ab4b8f0f9248779297"
   license "Apache-2.0"
   head "https://github.com/docker/cli.git", branch: "master"
 
