1806f1931564c4afda99eef3e48374b8ae7e9667
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-03 06:05:53 +0000

    cubejs-cli: update 0.31.58 bottle.

--- Formula/cubejs-cli.rb
+++ Formula/cubejs-cli.rb
@@ -8,13 +8,13 @@ class CubejsCli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "6e173fe9836c660542ac5c27f8d7da789f30ae1f805f9bdcc6e924bc1ea63af2"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "6e173fe9836c660542ac5c27f8d7da789f30ae1f805f9bdcc6e924bc1ea63af2"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6e173fe9836c660542ac5c27f8d7da789f30ae1f805f9bdcc6e924bc1ea63af2"
-    sha256 cellar: :any_skip_relocation, ventura:        "ec92ed91227bd3f5aadf612689c7c60d15c8a051bcb3b7818f3f70a5928da588"
-    sha256 cellar: :any_skip_relocation, monterey:       "ec92ed91227bd3f5aadf612689c7c60d15c8a051bcb3b7818f3f70a5928da588"
-    sha256 cellar: :any_skip_relocation, big_sur:        "ec92ed91227bd3f5aadf612689c7c60d15c8a051bcb3b7818f3f70a5928da588"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6e173fe9836c660542ac5c27f8d7da789f30ae1f805f9bdcc6e924bc1ea63af2"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8e7242fbe8b468e86d10c0257395af0f4abf91fffb154471890ed3807e68cd96"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "8e7242fbe8b468e86d10c0257395af0f4abf91fffb154471890ed3807e68cd96"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8e7242fbe8b468e86d10c0257395af0f4abf91fffb154471890ed3807e68cd96"
+    sha256 cellar: :any_skip_relocation, ventura:        "dccd65545e44cbc2f5fbe67a64fad8d273f2b92f22488d9873c171f443474977"
+    sha256 cellar: :any_skip_relocation, monterey:       "dccd65545e44cbc2f5fbe67a64fad8d273f2b92f22488d9873c171f443474977"
+    sha256 cellar: :any_skip_relocation, big_sur:        "dccd65545e44cbc2f5fbe67a64fad8d273f2b92f22488d9873c171f443474977"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8e7242fbe8b468e86d10c0257395af0f4abf91fffb154471890ed3807e68cd96"
   end
 
   depends_on "node"
