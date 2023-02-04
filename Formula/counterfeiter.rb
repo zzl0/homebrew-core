2872571c950799e83d91e6310de7821bba68d6ce
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-04 23:04:22 +0000

    counterfeiter: update 6.6.1 bottle.

--- Formula/counterfeiter.rb
+++ Formula/counterfeiter.rb
@@ -7,14 +7,13 @@ class Counterfeiter < Formula
   head "https://github.com/maxbrunsfeld/counterfeiter.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "6dcf6bd51b7bad5b7fe86cb9f58bdc699e3e10ead33a7d2e6d1e1c8c6ad8e84d"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "8e43cf62248f6e7ff02b67cfffa38f5f1ae81b3f79d6e289bebc16a814238ad2"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8e43cf62248f6e7ff02b67cfffa38f5f1ae81b3f79d6e289bebc16a814238ad2"
-    sha256 cellar: :any_skip_relocation, ventura:        "e0f1a7dcf1b3c2f0c4a1de04c6f8c03363e28753ec6d794c40ee9085b084225b"
-    sha256 cellar: :any_skip_relocation, monterey:       "d77a7211900355c0e440921a8b06fb8e1d91a650d98d1eea280bded849dc2739"
-    sha256 cellar: :any_skip_relocation, big_sur:        "d77a7211900355c0e440921a8b06fb8e1d91a650d98d1eea280bded849dc2739"
-    sha256 cellar: :any_skip_relocation, catalina:       "d77a7211900355c0e440921a8b06fb8e1d91a650d98d1eea280bded849dc2739"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9ae35345e995a8d7ab54d0c08d10dbfefb2fc28b47da0e93b6bf16c1f6f45955"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "c2848e9d73b109da41de138bccee3ad15c923a2a692b0a3d76a7d36cffc958ae"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "aeb93e13a4af27fff9048d2e30d8369476ecb82aea961333ccf10442ce693c0e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0165588244c3e6da51aeae2a004713add997a6c57116595e4076f4f6e70ffda6"
+    sha256 cellar: :any_skip_relocation, ventura:        "0feb02c66fba1c465552f88e84481a5768e3ae6af77c7fa5d78fd44a2fe761d1"
+    sha256 cellar: :any_skip_relocation, monterey:       "5811e4ef4446b5e2575da1ff06dad56094b2a43b2308a605cc1174e342c7872b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "8565f9078aa1bb68cf18674d70ea4074ad88ef743cb79034f8a9a1db2a8c2bbf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "956477417ee2bc8303141fdb8a51570f03abc98f8ae31a536636d5d960b19a99"
   end
 
   depends_on "go"
