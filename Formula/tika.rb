e1040bd4c73dced5c70dc53d2fed3d19dcf491de
Porkepix <porkepix@gmail.com>
2023-02-04 02:14:36 +0100

    tika 2.7.0
    * tika 2.7.0
    * tika-server-standard 2.7.0
    
    Closes #122270.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tika.rb
+++ Formula/tika.rb
@@ -1,9 +1,9 @@
 class Tika < Formula
   desc "Content analysis toolkit"
   homepage "https://tika.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=tika/2.6.0/tika-app-2.6.0.jar"
-  mirror "https://archive.apache.org/dist/tika/2.6.0/tika-app-2.6.0.jar"
-  sha256 "fa289b58a5c1bb531ace78324625512a9448aa8472b5eb88b65988964048815a"
+  url "https://www.apache.org/dyn/closer.lua?path=tika/2.7.0/tika-app-2.7.0.jar"
+  mirror "https://archive.apache.org/dist/tika/2.7.0/tika-app-2.7.0.jar"
+  sha256 "d901ed1dfbfbd151e0d208b3906434394922fc134747c88d462022c9c94257a5"
   license "Apache-2.0"
 
   bottle do
@@ -13,9 +13,9 @@ class Tika < Formula
   depends_on "openjdk"
 
   resource "server" do
-    url "https://www.apache.org/dyn/closer.lua?path=tika/2.6.0/tika-server-standard-2.6.0.jar"
-    mirror "https://archive.apache.org/dist/tika/2.6.0/tika-server-standard-2.6.0.jar"
-    sha256 "f06541fb0518d090db919e5ca4f367ac79f0701a9c7a7346c62e3130aa8414ba"
+    url "https://www.apache.org/dyn/closer.lua?path=tika/2.7.0/tika-server-standard-2.7.0.jar"
+    mirror "https://archive.apache.org/dist/tika/2.7.0/tika-server-standard-2.7.0.jar"
+    sha256 "ce60c414184084ed0b0defe6673645453a2078c889e05195d56d9468a8e12011"
   end
 
   def install
