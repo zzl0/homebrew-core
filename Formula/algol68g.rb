05209a0cb6bf51562b147f60f9af760d26acf653
Sam Ford <1584702+samford@users.noreply.github.com>
2023-02-06 15:26:13 +0100

    algol68g: update livecheck

--- Formula/algol68g.rb
+++ Formula/algol68g.rb
@@ -9,12 +9,8 @@ class Algol68g < Formula
   license "GPL-3.0-or-later"
   revision 1
 
-  # The homepage hasn't been updated for the latest release (2.8.5), even though
-  # the related archive is available on the site. Until the website is updated
-  # (and seems like it will continue to be updated for new releases), we're
-  # checking a third-party source for new releases as an interim solution.
   livecheck do
-    url "https://openports.se/lang/algol68g"
+    url "https://jmvdveer.home.xs4all.nl/en.download.algol-68-genie-current.html"
     regex(/href=.*?algol68g[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
