664aabdd795ac9ae1ce0dbff63e2dc90206dc77f
Issy Long <me@issyl0.co.uk>
2023-02-07 00:53:09 +0100

    mage: Use Ruby's `Time.iso8601` not ActiveSupport's `Time.rfc3339`

--- Formula/mage.rb
+++ Formula/mage.rb
@@ -23,7 +23,7 @@ class Mage < Formula
   def install
     ldflags = %W[
       -s -w
-      -X github.com/magefile/mage/mage.timestamp=#{time.rfc3339}
+      -X github.com/magefile/mage/mage.timestamp=#{time.iso8601}
       -X github.com/magefile/mage/mage.commitHash=#{Utils.git_short_head}
       -X github.com/magefile/mage/mage.gitTag=#{version}
     ]
