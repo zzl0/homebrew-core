ee90d3d70ae23a605a5ecd5eb4b28472f72bb250
Rui Chen <rui@chenrui.dev>
2023-02-06 21:56:14 +0100

    mx 6.15.0
    Closes #122469.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/mx.rb
+++ Formula/mx.rb
@@ -1,8 +1,8 @@
 class Mx < Formula
   desc "Command-line tool used for the development of Graal projects"
   homepage "https://github.com/graalvm/mx"
-  url "https://github.com/graalvm/mx/archive/refs/tags/6.14.20.tar.gz"
-  sha256 "28a0507d33586aa2200377e1eece173af127d476802bc3094e94a9b2528f02d8"
+  url "https://github.com/graalvm/mx/archive/refs/tags/6.15.0.tar.gz"
+  sha256 "370aa7930adbc586d293c83d67f59fe266306d9177576a9925e86359c5fd0029"
   license "GPL-2.0-only"
 
   livecheck do
