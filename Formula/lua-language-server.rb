b2e0d919e1d19e2b2bb4f8a8e74e75e1f5fed581
Porkepix <porkepix@gmail.com>
2023-02-07 01:54:15 +0100

    lua-language-server 3.6.10
    Closes #122493.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/lua-language-server.rb
+++ Formula/lua-language-server.rb
@@ -3,8 +3,8 @@ class LuaLanguageServer < Formula
   homepage "https://github.com/sumneko/lua-language-server"
   # pull from git tag to get submodules
   url "https://github.com/sumneko/lua-language-server.git",
-      tag:      "3.6.8",
-      revision: "ba8d6c611e3ce320d563d927dbb67deec7205c4c"
+      tag:      "3.6.10",
+      revision: "42f48c7777abe9243372688701f8609ce923168c"
   license "MIT"
   head "https://github.com/sumneko/lua-language-server.git", branch: "master"
 
