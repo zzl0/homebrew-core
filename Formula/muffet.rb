be5e56c30a86253197dc96610da9a5dac4b5c9c4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-07 10:04:52 +0000

    muffet: update 2.7.0 bottle.

--- Formula/muffet.rb
+++ Formula/muffet.rb
@@ -7,13 +7,13 @@ class Muffet < Formula
   head "https://github.com/raviqqe/muffet.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "fe82c7afbc2708cf158dcec44267bec8340a7de511c5d0528df0655b4af89558"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e5cda579881cb6262f600214d6f5f8be21c96c4dc8044328818e5b4806b6983d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7cfeed665f5c6ec73140aa78e5f9368ce375d08dce7885a25c744b376a2703c1"
-    sha256 cellar: :any_skip_relocation, ventura:        "bc94885bf1bd396086be6d51ff43dfbc00ea813245cbf3d0155a53c2081887bd"
-    sha256 cellar: :any_skip_relocation, monterey:       "37c0887dbad078f794a5813fb3b340435910c575d1a154ca7825e1e0d3a3c9ec"
-    sha256 cellar: :any_skip_relocation, big_sur:        "29eaba21a1d19d2edf563c39b364d76eb10c80a8e10e4916e876232a7bb0fa69"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "28b51ce5f7bd5f77ac50494999cadf91f10731b5c74d6d3a0bdf2e727ed8756d"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f3399c577fa1d0a2cd9eacf50f44f9e36f5df3ed8d776046f819d8859d28efd6"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "dd104eeeb8cd1dd1404380d9956f92d7ce1d6eb8f858b65c7adfc1ae8632d876"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ff448fdf9c8d60ebb5dbdd3eb4db3dc0812f30b6b3b12b910fc5a183468af6d2"
+    sha256 cellar: :any_skip_relocation, ventura:        "5b2e9e4403078581ee0ab01d2f99197d4e1c720f76ba394fa34a867d8fa7aa1e"
+    sha256 cellar: :any_skip_relocation, monterey:       "aaab4287f8dd3a5c41edee89ba6eccea756c71dd78beb59d27f41834ba090a66"
+    sha256 cellar: :any_skip_relocation, big_sur:        "4809499f184b22cf5281af80c0e01e37280fbd5c657c4f61b53fdcd1b90a10ee"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b09363a25177f4da7566b2472e6d6e2cd21ef4853fbb4753a7c180f6980950da"
   end
 
   depends_on "go" => :build
