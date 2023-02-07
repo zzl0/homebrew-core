bb12294096ecacf8e52cdbc466f4c7d24d04b10a
Patrick Linnane <patrick@linnane.io>
2023-02-07 14:39:10 +0100

    aws-sdk-cpp 1.11.10
    Closes #122559.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/aws-sdk-cpp.rb
+++ Formula/aws-sdk-cpp.rb
@@ -3,8 +3,8 @@ class AwsSdkCpp < Formula
   homepage "https://github.com/aws/aws-sdk-cpp"
   # aws-sdk-cpp should only be updated every 10 releases on multiples of 10
   url "https://github.com/aws/aws-sdk-cpp.git",
-      tag:      "1.11.0",
-      revision: "276ee83080fcc521d41d456dbbe61d49392ddf77"
+      tag:      "1.11.10",
+      revision: "f290bf9013e375f90a4826c0f5ea5de4daba10c6"
   license "Apache-2.0"
   head "https://github.com/aws/aws-sdk-cpp.git", branch: "main"
 
