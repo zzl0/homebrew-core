5809247570ceac4c5d4c9280285ef19b06ef9c00
Patrick Linnane <patrick@linnane.io>
2023-02-07 14:59:34 +0100

    jack 1.9.22
    * jack 1.9.22
    * jack: use `python@3.11`
    * jack: update build
    * jack: update test
    * jack: update build and test
    * jack: build jack-example-tools after jack build
    * jack: revert path changes for jack tools
    * jack: update build
    * jack: fix style
    * jack-example-tools: move build into post_install step as jack is runtime dep
    * jack: remove jack-example-tools build
    * jack: fix audit failure
    
    Closes #122566.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/jack.rb
+++ Formula/jack.rb
@@ -1,8 +1,8 @@
 class Jack < Formula
   desc "Audio Connection Kit"
   homepage "https://jackaudio.org/"
-  url "https://github.com/jackaudio/jack2/archive/v1.9.21.tar.gz"
-  sha256 "8b044a40ba5393b47605a920ba30744fdf8bf77d210eca90d39c8637fe6bc65d"
+  url "https://github.com/jackaudio/jack2/archive/v1.9.22.tar.gz"
+  sha256 "1e42b9fc4ad7db7befd414d45ab2f8a159c0b30fcd6eee452be662298766a849"
   license "GPL-2.0-or-later"
 
   livecheck do
@@ -25,11 +25,9 @@ class Jack < Formula
   depends_on "automake" => :build
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
-  depends_on "python@3.10" => :build
+  depends_on "python@3.11" => :build
   depends_on "berkeley-db"
   depends_on "libsamplerate"
-  depends_on "libsndfile"
-  depends_on "readline"
 
   on_macos do
     depends_on "aften"
@@ -42,12 +40,13 @@ class Jack < Formula
 
   def install
     if OS.mac? && MacOS.version <= :high_sierra
-      # See https://github.com/jackaudio/jack2/issues/640#issuecomment-723022578
+      # See jackaudio/jack2#issues/640#issuecomment-723022578
       ENV.append "LDFLAGS", "-Wl,-compatibility_version,1"
       ENV.append "LDFLAGS", "-Wl,-current_version,#{version}"
     end
-    python3 = "python3.10"
-    system python3, "./waf", "configure", "--prefix=#{prefix}", "--example-tools"
+
+    python3 = "python3.11"
+    system python3, "./waf", "configure", "--prefix=#{prefix}"
     system python3, "./waf", "build"
     system python3, "./waf", "install"
   end
@@ -60,8 +59,6 @@ class Jack < Formula
   end
 
   test do
-    source_name = "test_source"
-    sink_name = "test_sink"
     fork do
       if OS.mac?
         exec "#{bin}/jackd", "-X", "coremidi", "-d", "dummy"
@@ -69,18 +66,7 @@ class Jack < Formula
         exec "#{bin}/jackd", "-d", "dummy"
       end
     end
-    system "#{bin}/jack_wait", "--wait", "--timeout", "10"
-    fork do
-      exec "#{bin}/jack_midiseq", source_name, "16000", "0", "60", "8000"
-    end
-    midi_sink = IO.popen "#{bin}/jack_midi_dump #{sink_name}"
-    sleep 1
-    system "#{bin}/jack_connect", "#{source_name}:out", "#{sink_name}:input"
-    sleep 1
-    Process.kill "TERM", midi_sink.pid
 
-    midi_dump = midi_sink.read
-    assert_match "90 3c 40", midi_dump
-    assert_match "80 3c 40", midi_dump
+    assert_match "jackdmp version #{version}", shell_output("#{bin}/jackd --version")
   end
 end
