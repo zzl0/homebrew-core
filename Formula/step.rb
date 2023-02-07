2ce9607a25c0c76d4b1d82a222a0bfe5fc22beb0
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-02-07 10:47:20 +0800

    step 0.23.2
    * step 0.23.2
    * step: update resource
    
    Closes #122513.
    
    Signed-off-by: Caleb Xu <3536482+alebcay@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/step.rb
+++ Formula/step.rb
@@ -1,8 +1,8 @@
 class Step < Formula
   desc "Crypto and x509 Swiss-Army-Knife"
   homepage "https://smallstep.com"
-  url "https://github.com/smallstep/cli/releases/download/v0.23.1/step_0.23.1.tar.gz"
-  sha256 "81fe0f2a1e3a897757b3dc51e7edb6cb1fca3cece80d952388b4c41ca57be334"
+  url "https://github.com/smallstep/cli/releases/download/v0.23.2/step_0.23.2.tar.gz"
+  sha256 "98b561b185e43fa48567bffa65011dc55d4c762f477cb975511828d6d0934059"
   license "Apache-2.0"
 
   bottle do
@@ -18,8 +18,8 @@ class Step < Formula
   depends_on "go" => :build
 
   resource "certificates" do
-    url "https://github.com/smallstep/certificates/releases/download/v0.23.1/step-ca_0.23.1.tar.gz"
-    sha256 "9d31db46f2da00ff72bbc5584cecd971e9600a8791d0385fd3e1106d957b4741"
+    url "https://github.com/smallstep/certificates/releases/download/v0.23.2/step-ca_0.23.2.tar.gz"
+    sha256 "a12b7b580846efcdef60e2dd80187c713c808a841daf17f6b42da37e1a5df45c"
   end
 
   def install
