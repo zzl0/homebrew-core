643ea11643da06a5a59714822be62713be21851e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-07 02:24:01 +0000

    open-adventure: update 1.12 bottle.

--- Formula/open-adventure.rb
+++ Formula/open-adventure.rb
@@ -12,15 +12,13 @@ class OpenAdventure < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0e12500870339890aa4e38adb46743e8cd2459645749f98b985c126a039c26a9"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e626a51cb92c97db9628a4dd4f3220b4aaa5fca24b044e9d84b1a27986597e47"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b775593afe027000123bb388f84553f5c2213a566cce24eff46be74e96f000c9"
-    sha256 cellar: :any_skip_relocation, ventura:        "45c2c8b7f32981e7c6be6b4be03c6f076c8fedb0c754f985cac236ceeefa5a04"
-    sha256 cellar: :any_skip_relocation, monterey:       "207d746678eb8bc2d241af54fcd33785cc25066037d4211669321304a6d9dfab"
-    sha256 cellar: :any_skip_relocation, big_sur:        "bf645264db48637a2a60113fd80945feb3048e1b17d6e77bf1c6970bb6f621ba"
-    sha256 cellar: :any_skip_relocation, catalina:       "c49210129ed491e094473e663171f5f6a554b41c3ba319cafc4fdc7d25289c8f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b12d65ac8f76be7b2393bff7eb8672582b840732e3228351e5d8e2fb5b13dafb"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8938593859998a112c053d0e323545cf7827919ff2f0fead50f137994ece929c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f8d3583727927282f48385c9f9546024db0bdc680e7e30704214023533fe1fd5"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "123ea99c32d8aa671ba7dfbf923181b4e43561179f4d9d9a0d6a39c907c06065"
+    sha256 cellar: :any_skip_relocation, ventura:        "5624298331d6dbd57f857f1c47284ea7516af65dd09194088095b95ad9b84c20"
+    sha256 cellar: :any_skip_relocation, monterey:       "d9e0078dbf3740ab22f3217e3ccc7955a87fa9c770ff8ead1fba8e2246855af8"
+    sha256 cellar: :any_skip_relocation, big_sur:        "65f339d5f226a7d797ef08a1f0dc87128fe140d22a6486d260acc320e93671ab"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "671bacafec5d29e464486e7ae038c7bda0e6a7af15574f4a37d04cb4122d91e0"
   end
 
   depends_on "asciidoc" => :build
