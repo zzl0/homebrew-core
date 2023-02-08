9c232007fb6d4afbdc5375675b995c454c11491b
Rui Chen <rui@chenrui.dev>
2023-02-08 10:32:16 +0100

    hysteria 1.3.3
    Closes #122652.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/hysteria.rb
+++ Formula/hysteria.rb
@@ -2,8 +2,8 @@ class Hysteria < Formula
   desc "Feature-packed proxy & relay tool optimized for lossy, unstable connections"
   homepage "https://hysteria.network/"
   url "https://github.com/apernet/hysteria.git",
-    tag:      "v1.3.2",
-    revision: "dd4c17972fdfef7517c22d017ec922463fb94350"
+    tag:      "v1.3.3",
+    revision: "a48d6ddb7c83d605ef0d7f8ebe51515438a85a3a"
   license "MIT"
   head "https://github.com/apernet/hysteria.git", branch: "master"
 
