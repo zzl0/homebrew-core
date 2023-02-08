66e600d9e6a20a2a00f8b2bf21058f052974a9c2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-08 01:33:04 +0000

    mx: update 6.15.1 bottle.

--- Formula/mx.rb
+++ Formula/mx.rb
@@ -11,7 +11,13 @@ class Mx < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "eae980ead238f37090b2a877b025fb60fd1b88ebd813cfd0ea051bdefd2a2926"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "897a7f20c2008e2ac4f8300076f355a6a4e0a7579ed38d4de4d8c50647874cf6"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c483562fdb65df7000eb510ff6705668201da5b3398842888225c18bfcbf070c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4758dae427ce361991c21f3d4e1d74082f0b51fb26d6d7c4b2df12145af17829"
+    sha256 cellar: :any_skip_relocation, ventura:        "4a98b3bc5be1717cd84079bac7ce58bf93415cbe3fa0d83b86612d0d8d357914"
+    sha256 cellar: :any_skip_relocation, monterey:       "6f1dee8549b624c63ebe5cd7da15df8c51eef8696b432293d6c67ce7be75d64d"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ce9a50feff4fb04f5efe467979a545fe0b9e9ae277594c357d2a0413f540c73f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "75d8fa691cdb47e40ddf82507ab8adf70458faa05928d99c14ad2ca0335fba02"
   end
 
   depends_on "openjdk" => :test
