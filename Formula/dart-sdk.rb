a459c0e6d8c94197f75ce07899bc35e52acaf560
Patrick Linnane <patrick@linnane.io>
2023-02-09 14:32:40 +0100

    dart-sdk 2.19.2
    * dart-sdk 2.19.2
    * dart-sdk: update deps
    
    Closes #122777.
    
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/dart-sdk.rb
+++ Formula/dart-sdk.rb
@@ -1,8 +1,8 @@
 class DartSdk < Formula
   desc "Dart Language SDK, including the VM, dart2js, core libraries, and more"
   homepage "https://dart.dev"
-  url "https://github.com/dart-lang/sdk/archive/refs/tags/2.19.0.tar.gz"
-  sha256 "4e4d25257b47e1a6e44c8d08f459f94fd2726535f2227ddb3e947ed3639d1674"
+  url "https://github.com/dart-lang/sdk/archive/refs/tags/2.19.2.tar.gz"
+  sha256 "30f49ed47a878834621d6703076b9fefab6390814fe6ff7cab2e19f5fccedb60"
   license "BSD-3-Clause"
 
   bottle do
@@ -24,7 +24,7 @@ class DartSdk < Formula
 
   resource "depot-tools" do
     url "https://chromium.googlesource.com/chromium/tools/depot_tools.git",
-        revision: "44e9bee34fcc7e8dedc2a988750c9d7cc7f73eba"
+        revision: "d8fb7c966777733491e11037dcdb2bce1c16662d"
   end
 
   def install
