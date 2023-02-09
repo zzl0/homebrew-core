b66eb8d5ec1d98b2b6f761d5b1d59b6eb5fa52cf
Rui Chen <rui@chenrui.dev>
2023-02-09 17:37:49 +0000

    libsolv: add 0.7.23 bottle.

--- Formula/libsolv.rb
+++ Formula/libsolv.rb
@@ -5,6 +5,16 @@ class Libsolv < Formula
   sha256 "0286155964373c6cc3802d025750786c3ee79608d5cb884598e110e3918bb2fe"
   license "BSD-3-Clause"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "f48d9544a317cb66ffce9d2b31b16f8e5b467859fdbc29254557efd87abe2c13"
+    sha256 cellar: :any,                 arm64_monterey: "a52ed60a8dc9907672a1d3dbd2ecce9a2ced03237e664e85e97c44371c1e8c30"
+    sha256 cellar: :any,                 arm64_big_sur:  "84e605e61a32b4dee8d3ceb10066415c00f36abc6f050025919ba8c17ba7771c"
+    sha256 cellar: :any,                 ventura:        "e836a6d1bee9148cf85916cf8a0b392334a459915ea3617fae242f49dd21aec0"
+    sha256 cellar: :any,                 monterey:       "d622fd7478b2ebe090a614224167c18269a520997499da4f786d1c90d485f2ca"
+    sha256 cellar: :any,                 big_sur:        "b94ec4999f1b7b6292de66414111706d9e658f48bcf5ab44aa68657660668b44"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "42d5bced4e97cb13c26bbb3d614beb72173088d5345e1509551cf5cb9103cbc6"
+  end
+
   depends_on "cmake" => :build
   depends_on "xz"
   depends_on "zstd"
