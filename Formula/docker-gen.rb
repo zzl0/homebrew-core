a0084b590142e81089a58780bbb680102a51695e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-09 15:05:21 +0000

    docker-gen: update 0.10.0 bottle.

--- Formula/docker-gen.rb
+++ Formula/docker-gen.rb
@@ -7,13 +7,13 @@ class DockerGen < Formula
   head "https://github.com/nginx-proxy/docker-gen.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "b1cddad4b19cfa649e91b0095cadf00c6c05477153f3c69c80022bd741e1f8b6"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "d62926377aa2e6debe319dd30b771f9b52af29ed60fe26025f047bb56e624f20"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "97620ddb7b21d103c786e48e3672e1d860af31fcf8348eeaf98a232a44ec767b"
-    sha256 cellar: :any_skip_relocation, ventura:        "4c99a8b9612cc538d4b608bb4dbf6d899c1153ee1131a0b3dfdd52cbcb5b18df"
-    sha256 cellar: :any_skip_relocation, monterey:       "1b35b899981444829c1f5428e44eff955a8dc65e1177cc7a5332ce3e3550a675"
-    sha256 cellar: :any_skip_relocation, big_sur:        "24fa854a9bf89e14ae58946117840044b664f89d52d492f9e766957c7c3e4518"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e6239ac629bc1c3e0b24fdc5a22a87173ea4586ad673730ff39c45707e7ea3bd"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3d47a47ae032488f916238eeede322dc8bd72df4ac54e65b5222aa358774d583"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5c853155cb8be3f81485c6cc7a943daadcf8ffdeb50ff830893a4279970ab3f2"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2f2dbbd6263a8c0dc48fa87b1b3ce6233251c036a1e6598e49f8b0bc85f9055b"
+    sha256 cellar: :any_skip_relocation, ventura:        "e3ccc7feee447cde08f69a285c6b2584f7b0b013de2283c42ea980cbd08aafe8"
+    sha256 cellar: :any_skip_relocation, monterey:       "c61f30a45558913c4d59099084bb3a60e0a67f069d81a0a7963904a76fbaa2a3"
+    sha256 cellar: :any_skip_relocation, big_sur:        "0459fe3dedec4e9f4676d324698d6c683d2235c473a4cb3eb5f32be4f2a70420"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8d33c9a46dd56ef825fe7b8a0ae59da1925e069ef41cbf0729fafe2ffb76d221"
   end
 
   depends_on "go" => :build
