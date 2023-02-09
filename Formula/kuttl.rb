1ac08d3d7f3e77a50cd369d47bfe8a3f1684776d
Alexander Bayandin <alexander@bayandin.dev>
2023-02-09 13:29:05 +0000

    kuttl: add 0.15.0 bottle.

--- Formula/kuttl.rb
+++ Formula/kuttl.rb
@@ -7,6 +7,16 @@ class Kuttl < Formula
   license "Apache-2.0"
   head "https://github.com/kudobuilder/kuttl.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "7c4fc3fe0666e60cf54ad6519f0a009c1566abd9c4bd564bed43f03449134a8a"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "88a99dffaf3b1700ea052cc4d05ade69217155ade4017f7051206eab9802442a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d243c5230366bb5df0bff7ac5727e1ca304f4afa5adab9663e0570dc32376cbd"
+    sha256 cellar: :any_skip_relocation, ventura:        "8b7dd1d7937b53bc383162f1f3c41b1e7d52e1b0293c0be410dcb06d24eb6b4a"
+    sha256 cellar: :any_skip_relocation, monterey:       "b6c9a6a13f298b8c1c7be62d77db1c50d3d025bdfd6560c6d30611fa241f3735"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d7e57ff14aea3c2f7e76302fe30c70c27970389173227b19be78dd2ddee03887"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5df65c29150f83e33447e0966532292be84bfe15a6d23ffd4ac2dde42236cc3f"
+  end
+
   depends_on "go" => :build
   depends_on "kubernetes-cli" => :test
 
