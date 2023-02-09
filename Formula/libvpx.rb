73abe636c2092918036099563d59f39b34c151d2
miccal <miccal.matthews@gmail.com>
2023-02-10 07:24:13 +0800

    libvpx 1.13.0
    libvpx: remove build patch
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/libvpx.rb
+++ Formula/libvpx.rb
@@ -1,8 +1,8 @@
 class Libvpx < Formula
   desc "VP8/VP9 video codec"
   homepage "https://www.webmproject.org/code/"
-  url "https://github.com/webmproject/libvpx/archive/v1.12.0.tar.gz"
-  sha256 "f1acc15d0fd0cb431f4bf6eac32d5e932e40ea1186fe78e074254d6d003957bb"
+  url "https://github.com/webmproject/libvpx/archive/v1.13.0.tar.gz"
+  sha256 "cb2a393c9c1fae7aba76b950bb0ad393ba105409fe1a147ccd61b0aaa1501066"
   license "BSD-3-Clause"
   head "https://chromium.googlesource.com/webm/libvpx.git", branch: "main"
 
@@ -21,10 +21,6 @@ class Libvpx < Formula
   on_intel do
     depends_on "yasm" => :build
   end
-
-  # Patch for macOS Ventura
-  # Reported upstream: https://groups.google.com/a/webmproject.org/g/codec-devel/c/ofpypqweL5U
-  patch :DATA
 
   def install
     args = %W[
@@ -52,47 +48,3 @@ class Libvpx < Formula
     system "ar", "-x", "#{lib}/libvpx.a"
   end
 end
-
-__END__
-diff --git a/build/make/configure.sh b/build/make/configure.sh
-index 581042e38..fac9ea57b 100644
---- a/build/make/configure.sh
-+++ b/build/make/configure.sh
-@@ -791,7 +791,7 @@ process_common_toolchain() {
-         tgt_isa=x86_64
-         tgt_os=`echo $gcctarget | sed 's/.*\(darwin1[0-9]\).*/\1/'`
-         ;;
--      *darwin2[0-1]*)
-+      *darwin2[0-9]*)
-         tgt_isa=`uname -m`
-         tgt_os=`echo $gcctarget | sed 's/.*\(darwin2[0-9]\).*/\1/'`
-         ;;
-@@ -940,7 +940,7 @@ process_common_toolchain() {
-       add_cflags  "-mmacosx-version-min=10.15"
-       add_ldflags "-mmacosx-version-min=10.15"
-       ;;
--    *-darwin2[0-1]-*)
-+    *-darwin2[0-9]-*)
-       add_cflags  "-arch ${toolchain%%-*}"
-       add_ldflags "-arch ${toolchain%%-*}"
-       ;;
-diff --git a/configure b/configure
-index 1b850b5e0..bf92e1ad1 100755
---- a/configure
-+++ b/configure
-@@ -101,6 +101,7 @@ all_platforms="${all_platforms} arm64-android-gcc"
- all_platforms="${all_platforms} arm64-darwin-gcc"
- all_platforms="${all_platforms} arm64-darwin20-gcc"
- all_platforms="${all_platforms} arm64-darwin21-gcc"
-+all_platforms="${all_platforms} arm64-darwin22-gcc"
- all_platforms="${all_platforms} arm64-linux-gcc"
- all_platforms="${all_platforms} arm64-win64-gcc"
- all_platforms="${all_platforms} arm64-win64-vs15"
-@@ -157,6 +158,7 @@ all_platforms="${all_platforms} x86_64-darwin18-gcc"
- all_platforms="${all_platforms} x86_64-darwin19-gcc"
- all_platforms="${all_platforms} x86_64-darwin20-gcc"
- all_platforms="${all_platforms} x86_64-darwin21-gcc"
-+all_platforms="${all_platforms} x86_64-darwin22-gcc"
- all_platforms="${all_platforms} x86_64-iphonesimulator-gcc"
- all_platforms="${all_platforms} x86_64-linux-gcc"
- all_platforms="${all_platforms} x86_64-linux-icc"
