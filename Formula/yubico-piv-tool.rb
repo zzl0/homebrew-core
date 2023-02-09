285ce12d3230ce2ee752bee4ba21392feb03b894
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-09 13:09:07 +0000

    yubico-piv-tool: update 2.3.1 bottle.

--- Formula/yubico-piv-tool.rb
+++ Formula/yubico-piv-tool.rb
@@ -11,14 +11,13 @@ class YubicoPivTool < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_ventura:  "4121657e010a147567cfb9c119719caca1997131d8bc3a1c3d64cc51749bfc21"
-    sha256 cellar: :any,                 arm64_monterey: "048f6b502ba5cf242a18b858fa92f7a80be05f691131bc536741dadf4e1f9602"
-    sha256 cellar: :any,                 arm64_big_sur:  "4334a3092fcac6f5c894029fd94ea42f6d5955fb23c274a6db683ba391a75057"
-    sha256 cellar: :any,                 ventura:        "9342208cb9d59cefbe8997d3dfae6347ef8735d60bdec4e4dfe02afb1ec3b6e2"
-    sha256 cellar: :any,                 monterey:       "805bd4bbea1cf4befe41f725073609f96e815ea143a6fd964aa9060aad2b10f6"
-    sha256 cellar: :any,                 big_sur:        "aaab33462adc61ae55d3b0163dc1360ff8a084822bfb5dfbc63ab8d17de810a6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f111e091d70132fe778b05e7bcca3d929cfbfcbd893eeebcb7cb5db059231a28"
+    sha256 cellar: :any,                 arm64_ventura:  "f4bcaee48a934e59a22a29021a5dcaa857417651fcd9cd274c2476b5c6cbcc1f"
+    sha256 cellar: :any,                 arm64_monterey: "883cf669ce25204e04a81c1df515ea02f848a7d907bd82fdbc394543d1375097"
+    sha256 cellar: :any,                 arm64_big_sur:  "4b6849717fab3c4f870d3a619f202bf9fc1a90ad86c87a63c119aa88e79d8d8f"
+    sha256 cellar: :any,                 ventura:        "7e27d4943fa7280709765beb41ff01272cf208e53a0c5405074df14dafd6170d"
+    sha256 cellar: :any,                 monterey:       "55fd2c1dfdbc687e30ffe5d527030a4c8100e85d447b5094b2621e125668285b"
+    sha256 cellar: :any,                 big_sur:        "97a1f11168ba2107003b5f9346289cf639cb5922c11d6995272e1ec45610d434"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "16aad2f1a7e9e78c05fc1b3c87435033cb854f3683e6be062096d2e27a9532bf"
   end
 
   depends_on "check" => :build
