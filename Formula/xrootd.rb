a7c610cb398b4b58780e7dc562856266edd9a5b9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-09 16:05:00 +0000

    xrootd: update 5.5.2 bottle.

--- Formula/xrootd.rb
+++ Formula/xrootd.rb
@@ -12,14 +12,13 @@ class Xrootd < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "2977803b0ad82bab2bcd695a2367af781a3e9e97d76507edc45a7eb862a8ff57"
-    sha256 cellar: :any,                 arm64_monterey: "1c167ab2da3c4f78eaa80b5306ee8cbf015a8d29db420bfcd79eecfd9d53a514"
-    sha256 cellar: :any,                 arm64_big_sur:  "8b87fe62c1ce8698d7cb34fec96eaf2b5333719027638895d6264413ed21bae8"
-    sha256 cellar: :any,                 ventura:        "ddfa75293506519ded76e9d0c2611736d98ab9750a2cec180ec2d98e5ca39f4e"
-    sha256 cellar: :any,                 monterey:       "19af39f982ea8059f26bb6fe352cd0b0bba7bef3bda1648fb5a34849dbeb3487"
-    sha256 cellar: :any,                 big_sur:        "c4d34d6f0aadb5a94f1b9338ed839009f0d7236761b51e7bb88a1f2f625dba40"
-    sha256 cellar: :any,                 catalina:       "56b5e1f2161ac941a50a656134024aeb0d924fc8477a6f158fe2db2b226340a7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e665252ff42f1e3283c1ca42ea94d28ec43641fd8665d67db4dd833cb070ddbd"
+    sha256 cellar: :any,                 arm64_ventura:  "56dd037997378e68148a366deedca6c310163e62baf2e474e1e516a1fc9dcb77"
+    sha256 cellar: :any,                 arm64_monterey: "f7f7ede271ab38f3357bc3525e09f9f5358e862ce71d23ecf827a402e65563a4"
+    sha256 cellar: :any,                 arm64_big_sur:  "583f54092976ad9e0bfd6645af0fad6a19bef2fabc3075bed5053b1082658081"
+    sha256 cellar: :any,                 ventura:        "5c741b06e23762186202183376c4e9ab5ad316cbf9a73808579c7f84df36d1a0"
+    sha256 cellar: :any,                 monterey:       "21adcc31e4ff9abf5317cc1b96124d3c16c925ee30e22afdde5b9b4c75c1451e"
+    sha256 cellar: :any,                 big_sur:        "3dec53c1a4355f269b9072d6a57f7602b386d25661227bd0d10907039f87b0f7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "700bc4b4c8763159d33e0576b778c4c3dff2e5f5d8d23c805d1947a8577efcb2"
   end
 
   depends_on "cmake" => :build
