7ca935c220534490b04f8419d34bd0b3ee9fe37e
Michael Cho <cho-m@tuta.io>
2023-02-10 12:56:40 -0800

    uutils-coreutils: only `conflicts_with` on macOS

--- Formula/uutils-coreutils.rb
+++ Formula/uutils-coreutils.rb
@@ -21,9 +21,11 @@ class UutilsCoreutils < Formula
   depends_on "rust" => :build
   depends_on "sphinx-doc" => :build
 
-  conflicts_with "coreutils", because: "uutils-coreutils and coreutils install the same binaries"
-  conflicts_with "aardvark_shell_utils", because: "both install `realpath` binaries"
-  conflicts_with "truncate", because: "both install `truncate` binaries"
+  on_macos do
+    conflicts_with "coreutils", because: "uutils-coreutils and coreutils install the same binaries"
+    conflicts_with "aardvark_shell_utils", because: "both install `realpath` binaries"
+    conflicts_with "truncate", because: "both install `truncate` binaries"
+  end
 
   def install
     man1.mkpath
