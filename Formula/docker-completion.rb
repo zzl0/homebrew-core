cb95846cb81cefa16b15a31937886fa40fbba312
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-02-10 08:53:31 +0800

    docker-completion 23.0.1
    Closes #122883.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/docker-completion.rb
+++ Formula/docker-completion.rb
@@ -2,8 +2,8 @@ class DockerCompletion < Formula
   desc "Bash, Zsh and Fish completion for Docker"
   homepage "https://www.docker.com/"
   url "https://github.com/docker/cli.git",
-      tag:      "v23.0.0",
-      revision: "e92dd87c3209361f29b692ab4b8f0f9248779297"
+      tag:      "v23.0.1",
+      revision: "a5ee5b1dfc9b8f08ed9e020bb54fc18550173ef6"
   license "Apache-2.0"
   head "https://github.com/docker/cli.git", branch: "master"
 
