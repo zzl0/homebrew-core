e2199cf5ba38d810a9fdc3ccbf3647318da99f65
Rui Chen <rui@chenrui.dev>
2023-02-10 22:03:29 +0100

    qt: revision bump (libvpx 1.13.0)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/qt.rb
+++ Formula/qt.rb
@@ -12,7 +12,7 @@ class Qt < Formula
     { "GPL-3.0-only" => { with: "Qt-GPL-exception-1.0" } },
     "LGPL-3.0-only",
   ]
-  revision 1
+  revision 2
   head "https://code.qt.io/qt/qt5.git", branch: "dev"
 
   # The first-party website doesn't make version information readily available,
