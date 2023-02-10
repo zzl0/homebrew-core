469ccc95efffd45e929c8c0ccbb2054fed61f33a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-10 07:04:35 +0000

    jenkins-job-builder: update 4.1.0 bottle.

--- Formula/jenkins-job-builder.rb
+++ Formula/jenkins-job-builder.rb
@@ -8,15 +8,14 @@ class JenkinsJobBuilder < Formula
   license "Apache-2.0"
 
   bottle do
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "1db587e3f2322b6b3cf75ab30993008b12e2c4f2c3eab6a9941211b85dc84c2c"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e68aeb1306e7d5b3b07d1e6c6eb4121f11a3306a082250b6da684702fd0f2afb"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "cae82ede6ac57d56ae9f8e507bd890365036fb45bb989f7a829a5241e8a68d35"
-    sha256 cellar: :any_skip_relocation, ventura:        "2a6ccbc82b33735df0e64039044d486ffb5579363eae888146c9a40435513192"
-    sha256 cellar: :any_skip_relocation, monterey:       "325fc32604dd452eee660da4be8e10d0635cb0c86542b64b0805a54fc09fdf9b"
-    sha256 cellar: :any_skip_relocation, big_sur:        "63042b584656c8f987289298d8723778bab8af7461d118c1cd42f49cf8958f01"
-    sha256 cellar: :any_skip_relocation, catalina:       "dbb9684dc8dce274e52388e7d89b0617e376eb63a2e7cf0aea8d63272cf9679c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d9ebeb0bb6b29520e93048a242bfbce9a241dafa7b8d7d762e243f5cd9ac1dab"
+    rebuild 3
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "b40f87867d119e87c2d5f9ef99b36d4a0f69f03fe6a51a8a1df045852f503d86"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "19a74f39c3a6d907a9beec188d04e443f8387ed6b139ff44497e22c27e523c19"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "50218be4da055e335e9ad50bc7d5075251ea52ddf86ecbac4ba8713e6c82a399"
+    sha256 cellar: :any_skip_relocation, ventura:        "fc37b940474a459dd89faad6a83b71a645d1fbccce6bf46b9d0c6f8ddc9c57a5"
+    sha256 cellar: :any_skip_relocation, monterey:       "596cf393567832612ad75c1a220f771ba79c284a618386d81a03aae8f765bdde"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d0a5a54d8e50b3d5e1bcf89521b0af287230e04da48aa5c37be81071a3ea67d5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f3f913f9cffd5e70cd668c73c582786002e7e17428b5e6a18b3b2627fa503d57"
   end
 
   depends_on "python@3.11"
