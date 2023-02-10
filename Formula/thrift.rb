8f0b9dde69b560dbffad719e3a341fa3018cca7d
Mike McQuaid <mike@mikemcquaid.com>
2023-02-10 16:32:30 +0000

    thrift: brew style --fix
    For Homebrew/brew#14574.

--- Formula/thrift.rb
+++ Formula/thrift.rb
@@ -84,7 +84,7 @@ class Thrift < Formula
   end
 
   test do
-    (testpath/"test.thrift").write <<~'EOS'
+    (testpath/"test.thrift").write <<~EOS
       service MultiplicationService {
         i32 multiply(1:i32 x, 2:i32 y),
       }
