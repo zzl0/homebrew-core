7e6810ae3983a8e0e236b9cfe90b3eacdd91eb4a
Patrick Linnane <patrick@linnane.io>
2023-02-11 08:16:35 -0800

    sdlpop 1.23
    * sdlpop 1.23
    * sdlpop: remove build patch
    * sdlpop: update test
    
    Closes #123022.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/sdlpop.rb
+++ Formula/sdlpop.rb
@@ -1,8 +1,8 @@
 class Sdlpop < Formula
   desc "Open-source port of Prince of Persia"
   homepage "https://github.com/NagyD/SDLPoP"
-  url "https://github.com/NagyD/SDLPoP/archive/v1.22.tar.gz"
-  sha256 "1af170f7f6def61b2ab9c3a9227feca335461d224faa99f3578fc09115ac505c"
+  url "https://github.com/NagyD/SDLPoP/archive/v1.23.tar.gz"
+  sha256 "41a9aa64b4e8d0a9d7a84ffced48f74f9528d81adbffc08593ecf84776c5d77a"
   license "GPL-3.0-or-later"
 
   bottle do
@@ -18,12 +18,6 @@ class Sdlpop < Formula
   depends_on "pkg-config" => :build
   depends_on "sdl2"
   depends_on "sdl2_image"
-
-  # Fix SDL2 header search location during build. Patch accepted upstream, remove on next release.
-  patch do
-    url "https://github.com/NagyD/SDLPoP/commit/26d3fb9ffee2831ab98b1f0359ba25b41f6fffc8.patch?full_index=1"
-    sha256 "4c62ddef19d5550f3dc0db6d5a2fff7ba2c2454d376ca624a147b4c650512097"
-  end
 
   def install
     system "make", "-C", "src"
@@ -49,6 +43,6 @@ class Sdlpop < Formula
   end
 
   test do
-    assert_equal "See doc/Readme.txt", shell_output("#{bin}/prince --help").chomp
+    assert_equal "See README.md", shell_output("#{bin}/prince --help").chomp
   end
 end
