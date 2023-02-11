f235198b71b850d85f5ac3c070b116882cffa4ef
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-11 20:04:38 +0000

    simdjson: update 3.1.1 bottle.

--- Formula/simdjson.rb
+++ Formula/simdjson.rb
@@ -7,13 +7,13 @@ class Simdjson < Formula
   head "https://github.com/simdjson/simdjson.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "d091f0f1e98e1a61c13f63845f3ccc541e82e417e6ce63b1252c2cabb68959b9"
-    sha256 cellar: :any,                 arm64_monterey: "4bf3b5b4c29d4fac04289267eadcc143c13374ddaa6d699d8b05b5c56a5ff911"
-    sha256 cellar: :any,                 arm64_big_sur:  "cdca0ef5150412785094c13de9f6b543bc1c431f04847c7ae882f6d11bd01acf"
-    sha256 cellar: :any,                 ventura:        "c3f74d15effef67527d7b6df05bbae171d68e398d18ffad7a1d2bfbcc1fd2a40"
-    sha256 cellar: :any,                 monterey:       "5bdf34abc2c556198e5d0c8bf063cb71f59cdeae0778512340bbe59fb795fbe1"
-    sha256 cellar: :any,                 big_sur:        "a23d83f29c7ed45f4164465815ad01172b188ff342f6d10dc246e8f2c24dc9dc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6c34e589b5290025b4792d9e51a604134e6065dd7665503ba1f71dc49e34e689"
+    sha256 cellar: :any,                 arm64_ventura:  "b991ad995e684b5d21929d3d4966288f51dc7a33d7298936d6d9d9f2e14b214f"
+    sha256 cellar: :any,                 arm64_monterey: "32a30a662c4892011ddf862c129ff9245225ff9e4b013e6cb7e144b4ff185953"
+    sha256 cellar: :any,                 arm64_big_sur:  "430e6074cbb508e57aba6559fac4570fa83eec65e6a56c70df753edac4b17fb0"
+    sha256 cellar: :any,                 ventura:        "1f80c77a29178311e128ba024b091c85d489fb868184cb67c1b9248348d672e7"
+    sha256 cellar: :any,                 monterey:       "48a85e860fbe4217862ccdb5f24a1ea176530c322f09ff7f2a0273f55b09c6ae"
+    sha256 cellar: :any,                 big_sur:        "3dcf56b6959b38b42b09eb3f5cc8f0915506cb1508a878a99e1a50e002eb3b66"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fc1f7a8bef7b50691a75be1438b3ac98f9a0b4dcaac09ab3ba5346718c9fa23b"
   end
 
   depends_on "cmake" => :build
