d91a807284d2326a10e25fe9c796e48fefcde164
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-11 11:04:14 +0000

    leaf-proxy: update 0.7.0 bottle.

--- Formula/leaf-proxy.rb
+++ Formula/leaf-proxy.rb
@@ -7,15 +7,13 @@ class LeafProxy < Formula
   head "https://github.com/eycorsican/leaf.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "e99be2d32e40df7b3f32c039af20e9b750662c9afd4bca18e5d286133d100702"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0e90f6d4c260eb3bab88182c0cf7360e86f30b865a813bff0825c7d885e24c43"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d355aebf0aad23fc9bb94a03bfefba8a1c8dec92c2f46b24735ce0ed281dbfdc"
-    sha256 cellar: :any_skip_relocation, ventura:        "70d4593110e5c7bf6355b36dd5574efa0cba6dc10074c0e68b9af521bb488747"
-    sha256 cellar: :any_skip_relocation, monterey:       "f1a13b5e3dbfcaaabc3d624282b872d29b74efaafaa3a8bd64be1d50b1d8b487"
-    sha256 cellar: :any_skip_relocation, big_sur:        "9f70f05afb9f60dae8a45a393927a26507a7aa14839073d1af0bb81685d22dc5"
-    sha256 cellar: :any_skip_relocation, catalina:       "3044f6769b0ed5c003ad77c9ecd1ac30e7bc6943bb423481ee129d552eedbf72"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5d961c8bda1fc66585b47492e3efc427602dbf91b7be5775f3b2a32e464883e4"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "de37957630021ce2ebee575813a57fdb95c85582141745f9f5b9a2173a26b02b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "089b35b83585be79d9b12e828631105d9d2f6621d9cb39cf6345e7ee347e2ce1"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c8e81f59b2beafe5bca7f0ea95cb425a10a35f5f0d5d6694c840ed30160753a5"
+    sha256 cellar: :any_skip_relocation, ventura:        "1caa577ff36a7188a7e759bf88ebec9e0693e159e084ffd8a9ca07d3cb06f674"
+    sha256 cellar: :any_skip_relocation, monterey:       "fe3833ca3889e965fbd925e53a176844d0d3cf17b2ba30f80cdf1715538a9911"
+    sha256 cellar: :any_skip_relocation, big_sur:        "3bb2fdd86ef8955a96f5f034839e8febc6a485485b181ce753aee66a165d66a0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "38687d9fc2eb1bd29fdd5399678e70a0d92a5549d64f96e30ad55e00451741e5"
   end
 
   depends_on "rust" => :build
