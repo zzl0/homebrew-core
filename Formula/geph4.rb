f1489a07e38a2b9e17e036b868854e617871a4f3
Patrick Linnane <patrick@linnane.io>
2023-02-10 21:06:17 -0800

    geph4 4.7.6
    Closes #122967.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/geph4.rb
+++ Formula/geph4.rb
@@ -1,8 +1,8 @@
 class Geph4 < Formula
   desc "Modular Internet censorship circumvention system to deal with national filtering"
   homepage "https://geph.io/"
-  url "https://github.com/geph-official/geph4-client/archive/refs/tags/v4.7.5.tar.gz"
-  sha256 "399bd61afde4c4bdb5dc5910479e3779df5cd1888c554b0bbd7ae3b97620780c"
+  url "https://github.com/geph-official/geph4-client/archive/refs/tags/v4.7.6.tar.gz"
+  sha256 "fa27df0a26ec196a3f65f64aa7b33b2f2e2a086044528109b5a7d82fa66be062"
   license "GPL-3.0-only"
   head "https://github.com/geph-official/geph4-client.git", branch: "master"
 
