ddb63d30bb939c28d2a09e3fd8cc58bea14030bf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-11 05:04:56 +0000

    wxmaxima: update 23.02.0 bottle.

--- Formula/wxmaxima.rb
+++ Formula/wxmaxima.rb
@@ -7,12 +7,12 @@ class Wxmaxima < Formula
   head "https://github.com/wxMaxima-developers/wxmaxima.git", branch: "main"
 
   bottle do
-    sha256 arm64_ventura:  "3fd8f0a32926ea41a6ff908e479e205d5cba6788cbd062a2b211b965a550bceb"
-    sha256 arm64_monterey: "00bc43efc9e354fdd196814ddaa9a11b7c969a8e603d7295117c64c4fc5f6f1d"
-    sha256 arm64_big_sur:  "12be2dc241d92964d199cb82b47bc76e5ab8ae151ccb96fdc35b127dcfff6f5e"
-    sha256 ventura:        "0d6215787b99ea593f3bee36da37bb40f532fa6f7841396dbc7e64c40e77b9e7"
-    sha256 monterey:       "7aac965dbcacd466bafaed8fa4b1f28f484e55e3b3c9c9169a0931e5671272d8"
-    sha256 big_sur:        "8f96c8e5f788d48ddcc3b55c45b5d3715311e82ec404d0dae4f2ce196bc4bd04"
+    sha256 arm64_ventura:  "6f74e4c363edb4dc0b8ab62109ab7862c053be507f1906eae6184a3f06ade621"
+    sha256 arm64_monterey: "94515265d74da6227d9073cb3d96150053f6ab62f61c7251d82b16a5ea2c3bca"
+    sha256 arm64_big_sur:  "84d39b3fec61ef36853febce70cc1ca7c813d89d166656828a356ae8c3b266d9"
+    sha256 ventura:        "0e29da4d1464fafee4e1eeb9a07531f8cdf7e3c86e5f1c2e9ae804edfb8f0af3"
+    sha256 monterey:       "5728f0d663b611c5796c79fd15f03fa18d6d5de6f824a483ce7ebe36b62f7c9c"
+    sha256 big_sur:        "5660e9817816b506afd3e437570318907307c5b0f884aeec3ddfd21e089e8261"
   end
 
   depends_on "cmake" => :build
