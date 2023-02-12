4be5efc33c3a2903c149fd64467f956370078c38
Rui Chen <rui@chenrui.dev>
2023-02-12 11:22:04 +0100

    kdoctools 5.103.0
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/kdoctools.rb
+++ Formula/kdoctools.rb
@@ -1,8 +1,8 @@
 class Kdoctools < Formula
   desc "Create documentation from DocBook"
   homepage "https://api.kde.org/frameworks/kdoctools/html/index.html"
-  url "https://download.kde.org/stable/frameworks/5.102/kdoctools-5.102.0.tar.xz"
-  sha256 "23bc59ad1de668b03f81470d08e707be49a7c751a57ae7cf7400a84d6b38e685"
+  url "https://download.kde.org/stable/frameworks/5.103/kdoctools-5.103.0.tar.xz"
+  sha256 "ba5782de152606269a3589ae560bab2ce6709b85f0e07181fe3ef6ebd6ace4a8"
   license all_of: [
     "BSD-3-Clause",
     "GPL-2.0-or-later",
