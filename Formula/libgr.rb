dd57adde11a3f466656f833ae1ab4cd50eeb37cb
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-02-13 21:48:15 +0800

    libgr 0.71.7
    Closes #123113.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libgr.rb
+++ Formula/libgr.rb
@@ -1,8 +1,8 @@
 class Libgr < Formula
   desc "GR framework: a graphics library for visualisation applications"
   homepage "https://gr-framework.org/"
-  url "https://github.com/sciapp/gr/archive/v0.71.6.tar.gz"
-  sha256 "cfb9cddb8377dc672b6793f942fd57ce99f3c248ab69ed55533de9e776fdc26d"
+  url "https://github.com/sciapp/gr/archive/v0.71.7.tar.gz"
+  sha256 "781bd832e55939daa7362f86476b67eaa4158beacfdeadb8c4d41be0dab10c54"
   license "MIT"
 
   bottle do
