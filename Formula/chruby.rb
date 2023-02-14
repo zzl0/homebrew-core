e8bddccd6eb12bdd11313c9942a56d60c4c1f963
Michael Cho <cho-m@tuta.io>
2023-02-14 15:46:11 -0800

    chruby: remove all bottle

--- Formula/chruby.rb
+++ Formula/chruby.rb
@@ -15,7 +15,6 @@ class Chruby < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "17dc507695fed71749b5a58152d652bb7b92a4574f200b631a39f5f004e86cca"
     sha256 cellar: :any_skip_relocation, el_capitan:    "ff70dff83817f093d39384a40d3dfb2aaccc1cbe475d58383d4ef157085f2c64"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "bea6c750e5ce28f5a8ade003baef8a42bcbdf2b376e2d4ae8e12c7b3b112fef6"
-    sha256 cellar: :any_skip_relocation, all:           "977cf9319a21ddbbd26d3f0a43ed75825eb2a514bdce56b4045e5214732ec13b"
   end
 
   def install
