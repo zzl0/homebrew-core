3ee62f07c37b1b5dd83508eeec3104726854ae61
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-14 03:07:32 +0000

    mdbook: update 0.4.27 bottle.

--- Formula/mdbook.rb
+++ Formula/mdbook.rb
@@ -7,13 +7,13 @@ class Mdbook < Formula
   head "https://github.com/rust-lang/mdBook.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3011a0c850a5ac4d41f4297f65c6a69882027ef7602b3bd162f36b097f038aaf"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "de1e131ce2322c8c5c17baf8b9c16a5a4d5ad2c57f778204598af2ffc731f0f5"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "12ae068a045aa5e64a93b89e26e081f862acf86f353391bee79fada041cf1885"
-    sha256 cellar: :any_skip_relocation, ventura:        "2924e65436fa33bdb0cc03b2071cb19fcce96e10c29fbf429e05fbd1d44dc194"
-    sha256 cellar: :any_skip_relocation, monterey:       "3acccdb9774a9f12ae0d6223d7c161b493878ba9f8fae303a9949f8a3bf2b035"
-    sha256 cellar: :any_skip_relocation, big_sur:        "edae0197f4b5d53d404fe2719a33f169f83cf9e85c9eef44b1b865fde8614676"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "598523a335bb06a10e50e749a00472aeccf5ffcc800079ff164e85e4f46c01fa"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "6ef633a7236d8ff9f1fd4dc8c1cf8ea108c7bc2a3ef5669f380bfb554e02141d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "af2704ce2a383e115eabca8a732ebff736803c6001ac714c9a3a9245044a0ecb"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "426f06ce9a893197655bc0ba926e4c7cada35913fdd5b043db237e827961e4f7"
+    sha256 cellar: :any_skip_relocation, ventura:        "b8f0d0043258c6902bce035ed5276b45556b9ee0c62e6873c16663f8aa04d1cb"
+    sha256 cellar: :any_skip_relocation, monterey:       "e8b0b52ddf7e52cad5cd114efa40a1d2256558f52712a3323f05ee4d2d2d013d"
+    sha256 cellar: :any_skip_relocation, big_sur:        "72c380c2c1ed5271ea8b06c5d5dc7f5bda60b9a2a18761a90c55444132377da8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "eb5406683edaf280a681cb9a24477ff7c06c2843fcccd5a7fe465d6f16d98c14"
   end
 
   depends_on "rust" => :build
