8e2f5958fff9cabce8064e53a918eaa5549018eb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-14 14:05:55 +0000

    aliyun-cli: update 3.0.149 bottle.

--- Formula/aliyun-cli.rb
+++ Formula/aliyun-cli.rb
@@ -7,13 +7,13 @@ class AliyunCli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "ac47b12c30dd5bb2c1c86ff8f66f14e6aebfcd9d3679533f0dab7f833cd72505"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "fb02771de40b2fae130699e43dc7a69e719acb85686ca89d0a354aeacf79fb89"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3429b0d377a0eea71718b09d0192d5ce5fc92697f2eb0a857c12560c57b078f5"
-    sha256 cellar: :any_skip_relocation, ventura:        "74403d9494ee401961e066fbe28151709470d303e25455cd8d40f075b96eba28"
-    sha256 cellar: :any_skip_relocation, monterey:       "5f4e81aa71d31e60c5b498f9495438d81406e76bfcfd2b2db7cc9189ba3ece67"
-    sha256 cellar: :any_skip_relocation, big_sur:        "77135cc7056b47e4e5c03c69323401555034d65b1a25c3d8f2d0d8d0dbadaa34"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "96a726df02e36b8a4d1bc42f0b7c50fb696333022acfba768b5f2512787f6279"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "dd942f58c27273e26f5ad4b368595ab8636e4eb9d6d129629294a60ee004ce24"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "6f378803d21d7552b224d8f198f862c976916387d2a42692c09501c4bf15faa3"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3638c567fa247d061e074e2b373a5ed79efb12c849fe5efd73f55b0891af3674"
+    sha256 cellar: :any_skip_relocation, ventura:        "decaf96fa76ca115b9fc245e0b84a3e895438013a3e2b6848df7220871b9f988"
+    sha256 cellar: :any_skip_relocation, monterey:       "767b0aa08ef2e4466acc48d2a13f2286c022cb5108bd54ef0dd751ee7e66608f"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d9754a4aa6c4e89a5b461121256ca65dd6c5cdc590a0171dd1a27cc15ce207e5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "22aa70d408993ae250cd9e39e0d2298be124d017463b71c69d3db706f7119c0f"
   end
 
   depends_on "go" => :build
