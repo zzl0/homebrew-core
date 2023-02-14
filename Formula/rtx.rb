12e0ac6250cef10db08a9ebd7326ab9e43a9fab8
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2023-02-14 08:22:23 +0000

    rtx: add 1.8.0 bottle.

--- Formula/rtx.rb
+++ Formula/rtx.rb
@@ -5,6 +5,16 @@ class Rtx < Formula
   sha256 "3983c2239a31c0376e8c8da4ec614e96ed19e80bd41610407572e2ad4303e87a"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "c1798d7cee58b5a0c7078f8d5cd8b28b65eb69dad54b129bc84d60bd7e8df6d7"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "66901af3a7897148788911546f837decd4b1cd5c17d1b1395bcb11e407876377"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c3895cc1d1f21267de512c922aa9ed67931790409055ccc812a4db66bb938101"
+    sha256 cellar: :any_skip_relocation, ventura:        "a35da4f245a32f883c1b5ca6cdbd55e8747676471fb7242978deb590162c751f"
+    sha256 cellar: :any_skip_relocation, monterey:       "3d24ec88a6a2dde24075fde18cdcea98a87fb9d723e3d5d34ffee660e9902c68"
+    sha256 cellar: :any_skip_relocation, big_sur:        "835a419189ed8ca90d0375cd646510ec73e4395bcddb3d51887b1eb55fcc9654"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "df2c1b36d16a2682378016aa97278b853ce58a988b53a6a77f1fb15428342610"
+  end
+
   depends_on "rust" => :build
 
   def install
