9e1df38b0a7c71567eeafc4307c17a124d83b347
Patrick Linnane <patrick@linnane.io>
2023-02-14 11:59:43 -0800

    dotnet@6 6.0.114
    Closes #123226.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/dotnet@6.rb
+++ Formula/dotnet@6.rb
@@ -3,8 +3,8 @@ class DotnetAT6 < Formula
   homepage "https://dotnet.microsoft.com/"
   # Source-build tag announced at https://github.com/dotnet/source-build/discussions
   url "https://github.com/dotnet/installer.git",
-      tag:      "v6.0.113",
-      revision: "4a23b50f9783e31c496e8c21b8f117351feea870"
+      tag:      "v6.0.114",
+      revision: "346c0065dd9540261ec07e938b808833446d2a9e"
   license "MIT"
 
   # https://github.com/dotnet/source-build/#support
