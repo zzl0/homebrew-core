149f230ef05b067b982a3ba919bb5039408aac4a
Michael Cho <cho-m@tuta.io>
2023-02-13 23:31:22 -0800

    docker-machine-driver-vmware: migrate to `go@1.18`

--- Formula/docker-machine-driver-vmware.rb
+++ Formula/docker-machine-driver-vmware.rb
@@ -18,8 +18,8 @@ class DockerMachineDriverVmware < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "c2699119a539f3b9b7c86f900d5510d5b4cdb952ebdc60b7f5c12bf80f5d4932"
   end
 
-  # Bump to 1.18 on the next release, if possible.
-  depends_on "go@1.17" => :build
+  # Bump to 1.19 on the next release, if possible.
+  depends_on "go@1.18" => :build
   depends_on "docker-machine"
 
   def install
