fbe17bb91e01f531887581965e0cbb9866d5d502
Michael Cho <20700669+cho-m@users.noreply.github.com>
2023-02-15 04:14:58 +0000

    sjk: update 0.20 bottle.

--- Formula/sjk.rb
+++ Formula/sjk.rb
@@ -6,11 +6,15 @@ class Sjk < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "94b4054e1a1ad665cfc6ed8701d2b02746dfb1f099a2a688a2b945ec664c1575"
-    sha256 cellar: :any_skip_relocation, big_sur:       "94b4054e1a1ad665cfc6ed8701d2b02746dfb1f099a2a688a2b945ec664c1575"
-    sha256 cellar: :any_skip_relocation, catalina:      "94b4054e1a1ad665cfc6ed8701d2b02746dfb1f099a2a688a2b945ec664c1575"
-    sha256 cellar: :any_skip_relocation, mojave:        "94b4054e1a1ad665cfc6ed8701d2b02746dfb1f099a2a688a2b945ec664c1575"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "51d29b3fd8b210d7dbd7d881ab0dda3aa40b99c99362f5ed3aa8c2a1de5b7d54"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "ce4c75268949b89a47d762e67ccbefdf55a8685188edb27f1827f58ebddc4a6e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ce4c75268949b89a47d762e67ccbefdf55a8685188edb27f1827f58ebddc4a6e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "94b4054e1a1ad665cfc6ed8701d2b02746dfb1f099a2a688a2b945ec664c1575"
+    sha256 cellar: :any_skip_relocation, ventura:        "ce4c75268949b89a47d762e67ccbefdf55a8685188edb27f1827f58ebddc4a6e"
+    sha256 cellar: :any_skip_relocation, monterey:       "ce4c75268949b89a47d762e67ccbefdf55a8685188edb27f1827f58ebddc4a6e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "94b4054e1a1ad665cfc6ed8701d2b02746dfb1f099a2a688a2b945ec664c1575"
+    sha256 cellar: :any_skip_relocation, catalina:       "94b4054e1a1ad665cfc6ed8701d2b02746dfb1f099a2a688a2b945ec664c1575"
+    sha256 cellar: :any_skip_relocation, mojave:         "94b4054e1a1ad665cfc6ed8701d2b02746dfb1f099a2a688a2b945ec664c1575"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "51d29b3fd8b210d7dbd7d881ab0dda3aa40b99c99362f5ed3aa8c2a1de5b7d54"
   end
 
   depends_on "openjdk"
