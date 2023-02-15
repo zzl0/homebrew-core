6cbc6017401be4ec439837d93cc4a74042b15803
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-15 05:06:42 +0000

    python-typing-extensions: update 4.5.0 bottle.

--- Formula/python-typing-extensions.rb
+++ Formula/python-typing-extensions.rb
@@ -6,14 +6,13 @@ class PythonTypingExtensions < Formula
   license "Python-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "52f12013179c269cd8735b5238d85fcb814b24397e54c943951b42e998c3293e"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e1f31efd887ea1b281e6a1ac7e63a22f2e6e9ede8c423d5e6cccf16178af430c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b8aa9684ffa353c2f80d5a6ab7b2190f46edf7950d57b6d77238225c501a3393"
-    sha256 cellar: :any_skip_relocation, ventura:        "2628d2292eeeb952f40403c9b81edc4fb67702652d4a6577dd64d95c4a230d0e"
-    sha256 cellar: :any_skip_relocation, monterey:       "3b3e03bca911d61c1842f96c24aee6270138bfff8212656f373b0948f2cc1a29"
-    sha256 cellar: :any_skip_relocation, big_sur:        "c36fa17f6f27f24de979efdad54bfac73849866ddc54872977b04db78d950774"
-    sha256 cellar: :any_skip_relocation, catalina:       "bf3f04ac84a6858acfcb506a39a20132974778301216cc39089807a0ef14f476"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "86225068aa47ac1ffffd5af308df0b29ff470b4924840fc7a57212c39cfa529b"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "74d44da39b2998a115f06b2d345e4495eb018c031610a0102636b182c09e0949"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e3638521661bfe4a51eb9f650e2b4f9582fa1f777ede9347a679ec7b61a4ce61"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b6ac83ffd476cdd6331c870a728313b87270196b0124e688a500726b975e1aa1"
+    sha256 cellar: :any_skip_relocation, ventura:        "c8510db67c9f6a3456f1760b4bf3d1e45a444562e09efcf85f06027cf7a136c4"
+    sha256 cellar: :any_skip_relocation, monterey:       "11ba874e162d3aec1a860cb54de4a1905ef5f6d71730b14b578fe2b9f8e0d290"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b4a351ca344fcee7bb3fa385062600089c468692d2551f3cab4632e36a49b815"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "dffe6514c5c31c6d969741bf76174d2170d72e3441a6a57bcbf795b1d422f825"
   end
 
   depends_on "flit" => :build
