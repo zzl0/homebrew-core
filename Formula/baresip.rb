add4e1d6a816ea6bb93213e2e25b31f1dabcea4d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-15 17:44:34 +0000

    baresip: update 2.12.0 bottle.

--- Formula/baresip.rb
+++ Formula/baresip.rb
@@ -6,13 +6,13 @@ class Baresip < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 arm64_ventura:  "7e62d8094d091a3a66be7a0e19df3c54c80a27dfb029b2c25c12cd8a14522eed"
-    sha256 arm64_monterey: "cd7ac2867683c110fe7248e5b7bc4231cd8f0bcb5842554cd61e22c21e2a13f9"
-    sha256 arm64_big_sur:  "82d29c691a6a032af6201dbe3aebaa9376a0be2b9f2322279dd8ec4b22342237"
-    sha256 ventura:        "0f03d80938e43f78efc62611a2cf336e68ff7ddb8fada3dd1c488e52c14606be"
-    sha256 monterey:       "5c752d177687a9aafa852749a635bb0d620b4a64d7820007f0dd2bd12eacb39d"
-    sha256 big_sur:        "7064d966690cd20890df14aa98985ab460b03418c473bf4242e245f5a79f617c"
-    sha256 x86_64_linux:   "978e3ed2d26adcb80c096cddf4242f60eb53afcbaeba36c2a5cbb8b7facc9c95"
+    sha256 arm64_ventura:  "143a4140cefd1ae972cd4613b67842c304b1d0824bdb1eb514bb55b293369c6c"
+    sha256 arm64_monterey: "0e5ba17ba6284d947b3889c832141c9d3e1012320def7f8baa88ff87b401a0ab"
+    sha256 arm64_big_sur:  "e4f2f639e7200954de9f3363d861d4c4795b2042175e4d240311fa69eb454233"
+    sha256 ventura:        "42f62b96b58e5bc0c4124abeab97b8ab5e3f9582f3ebb60cc31fffb6706b3a25"
+    sha256 monterey:       "14e80778bd6089f718d6ff248782153a3241b04e2c8dcf371bc6b3f678b9758f"
+    sha256 big_sur:        "4b3801176016f45dc1c46fd571fc81ef6ef531f2365db2c85394b0150eda7bb8"
+    sha256 x86_64_linux:   "1d1ae88fca25da68ccb3437cb285c5950ecef050a5402fb0d47c6b6da2be7402"
   end
 
   depends_on "cmake" => :build
