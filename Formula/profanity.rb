5a240c5e373c212ff7a4ecd955afee76d798bbd7
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2023-02-15 13:31:27 +0000

    profanity: update 0.13.1 bottle.

--- Formula/profanity.rb
+++ Formula/profanity.rb
@@ -6,15 +6,14 @@ class Profanity < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    rebuild 1
-    sha256 arm64_ventura:  "ed139220e7ccbc28492b21a36b46938ca0a0eb9726d12a5b5a5ec2732a70b2e9"
-    sha256 arm64_monterey: "0445e9095f46d2feb9d4885addcbe3776d0582eebbf69a1ed6159323c31a46b7"
-    sha256 arm64_big_sur:  "fbc2566e1def33062a2d48435900ac0ee347ae5e3b9083a5840afdedbfff6c3f"
-    sha256 ventura:        "53a9218eb2090e6b70d7b14ad9b8aab6f503ac2e4e18e92a655e02eea5f5608c"
-    sha256 monterey:       "dba2f230257a1bac9b5a3667f8a339bcc4bd1eaa6ebd22afe798bf078dbe0e77"
-    sha256 big_sur:        "8b7edbbab26d273e2acb2ce927c796e3cd25b90c0d9c49a4f9788eed6049688d"
-    sha256 catalina:       "234b9a17bc5bcb40fa28093cb430f613c3e4b6dea64ab58abfe18fc2840afa7e"
-    sha256 x86_64_linux:   "534112eda8a5e3a78ef8b1fa301fef91c001ab73e5e57eb128ce9f2418f2bab4"
+    rebuild 2
+    sha256 arm64_ventura:  "fcfcc39a0046f3dea14792a29b17c1f9975ae8ddf6e9e0aa6c290447c77f585b"
+    sha256 arm64_monterey: "056130e0175fe061171aa23c0aba2626987020a62162bd49c28d3b2ded79e6ad"
+    sha256 arm64_big_sur:  "2d6420972cfb0ab56b968245d67d557e5f4b08a1378c2277979ff40ba9fde266"
+    sha256 ventura:        "b3ca86b0f0e3d8e457f9b1acc436da3ce6fdd036c25a64efd7c85d8c6c2013c5"
+    sha256 monterey:       "e942a19b775d15b1c160c5f3b0924f1e93249309fa1a54f8e96c2a6b0c750d86"
+    sha256 big_sur:        "2a12f1e8209b0078713bb71e54469a6a8a3f8bbffe37a59f06541f34cf08a800"
+    sha256 x86_64_linux:   "22412eee0a04d94e03ea8871df887ab99cbc1a93d7d6b10843fd96588bc6d741"
   end
 
   head do
