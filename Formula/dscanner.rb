34a8fe66f36017703dea2c9c96c5af662434bfe8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-15 14:04:36 +0000

    dscanner: update 0.14.0 bottle.

--- Formula/dscanner.rb
+++ Formula/dscanner.rb
@@ -8,13 +8,13 @@ class Dscanner < Formula
   head "https://github.com/dlang-community/D-Scanner.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "cf97055e28d37f7ec405b2b74d8df2dbd81592bf949b4b314e9d1dc349670c38"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "84350066676dcd3809188dfc10b55478f65bacc819f64b202d14bee6d402e85a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6c183c51804c63baccb3a326d63a950f897a896e4286aa8ee23da4d30f2da737"
-    sha256 cellar: :any_skip_relocation, ventura:        "0b49192fb45fd1b0340d39ef82d1590683d76199f73ea218c315fe84539a268f"
-    sha256 cellar: :any_skip_relocation, monterey:       "69e8d5ceced1e610d29888ea9d68e57d212f87eb259852fb53e7c840df0f81e3"
-    sha256 cellar: :any_skip_relocation, big_sur:        "add5c81f3f83f509909a4ad2f3e04cbf659fec27340e0b32f41893867e5c1859"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "20d650b1923faf383be1733c311b61f9dbc7c0b7817655c00ff42a49439815da"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "b103273d63236f1b0248e4ae49cc8222d20aaa0a1caea7868b9fc534c6ebb88d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "6aa9de194ec0c6fc7eeaebc1b248246d2b6a277a90a90a750d15286b304696dc"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e29961bc77ba4286bf0639b6d5820c1e6f2eaceed7acb8d33c1d745281ac20b7"
+    sha256 cellar: :any_skip_relocation, ventura:        "42ef416a861cc5c3417280a2e9a174443f1e6d7f8b3643b10239d86417f33289"
+    sha256 cellar: :any_skip_relocation, monterey:       "f536efe9c19e9487d576cd25a2c6327c5aacf2fb88e003941e5487b130b48448"
+    sha256 cellar: :any_skip_relocation, big_sur:        "1cdfbe7edce737e08e428c5cb8617c44b5a8ec433a51406654f0c4865a1f9a56"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f621b9b86043c107ff01833bc027bfa6e1a5f160fd987696dc85de4ca80441ce"
   end
 
   on_arm do
