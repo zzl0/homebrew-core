dce822e341da57923ce5f5d2c8ef6bd126a8b8f4
Patrick Linnane <patrick@linnane.io>
2023-02-15 20:48:48 -0800

    i2pd 2.46.0
    Closes #123371.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/i2pd.rb
+++ Formula/i2pd.rb
@@ -1,8 +1,8 @@
 class I2pd < Formula
   desc "Full-featured C++ implementation of I2P client"
   homepage "https://i2pd.website/"
-  url "https://github.com/PurpleI2P/i2pd/archive/2.45.1.tar.gz"
-  sha256 "a84b1e3cb5ac45f39af98e63291d34725ef67337e11acbe0e245acde7a5b2b72"
+  url "https://github.com/PurpleI2P/i2pd/archive/2.46.0.tar.gz"
+  sha256 "daa5e47bb2b80fbdaa3836b209e869017893421f5225dfe019e5d43d3f8a86d4"
   license "BSD-3-Clause"
 
   bottle do
