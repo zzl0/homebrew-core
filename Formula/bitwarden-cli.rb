cf3cf0b2374ac5cceaf5a5be189bae1fe663a83f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-16 10:00:09 +0000

    bitwarden-cli: update 2023.2.0 bottle.

--- Formula/bitwarden-cli.rb
+++ Formula/bitwarden-cli.rb
@@ -8,13 +8,13 @@ class BitwardenCli < Formula
   license "GPL-3.0-only"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "b5003c94ebc6f68b58c635a53d7faae01b3c8f3662139a8ffef10d7fd0fa60c4"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b5003c94ebc6f68b58c635a53d7faae01b3c8f3662139a8ffef10d7fd0fa60c4"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b5003c94ebc6f68b58c635a53d7faae01b3c8f3662139a8ffef10d7fd0fa60c4"
-    sha256 cellar: :any_skip_relocation, ventura:        "e7dd69a2be5f17dad5cd31849fcc7351f4bce8ae1f413be89446f56bbab16372"
-    sha256 cellar: :any_skip_relocation, monterey:       "e7dd69a2be5f17dad5cd31849fcc7351f4bce8ae1f413be89446f56bbab16372"
-    sha256 cellar: :any_skip_relocation, big_sur:        "e7dd69a2be5f17dad5cd31849fcc7351f4bce8ae1f413be89446f56bbab16372"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b5003c94ebc6f68b58c635a53d7faae01b3c8f3662139a8ffef10d7fd0fa60c4"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "6fb370700737923d21f8cfc9e64efcd7959648834eafe0fdac00131f490fce5f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "68f3e1e1a739d837c6701a2658f4a29f15b9593a8a788e89cf8734ff150ad474"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "230d1faae76e7d8f409e30184ff8697c464fac4b8f73a2277d9044042550a56c"
+    sha256 cellar: :any_skip_relocation, ventura:        "eccfec7c0b0a9f4fb94c36189d9c0cb2d2383fbc77d6f97af2f07a38a8048e7f"
+    sha256 cellar: :any_skip_relocation, monterey:       "063ebded1451260c2e38652b98f5a49316da1201b28049b8802f2f449f7c9567"
+    sha256 cellar: :any_skip_relocation, big_sur:        "6f16d56d5d845d43eca130dcb876162ad1cba9224b4c4a69b523dfc03ed69400"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "25db80452b26cd7a7f0651de1898faa24a0d96346de6c1cb9012811741a4048a"
   end
 
   depends_on "node"
