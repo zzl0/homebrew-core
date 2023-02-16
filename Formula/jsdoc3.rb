77e2d5781f96fef2d1620e0ce6705fc66a2096cf
Patrick Linnane <patrick@linnane.io>
2023-02-16 10:39:10 -0800

    jsdoc3 4.0.1
    Closes #123430.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/jsdoc3.rb
+++ Formula/jsdoc3.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Jsdoc3 < Formula
   desc "API documentation generator for JavaScript"
   homepage "https://jsdoc.app/"
-  url "https://registry.npmjs.org/jsdoc/-/jsdoc-4.0.0.tgz"
-  sha256 "aee74ea546974b095d0c26b0b712955b03ece78e8f752d04a153dbe6ae72aa83"
+  url "https://registry.npmjs.org/jsdoc/-/jsdoc-4.0.1.tgz"
+  sha256 "32afba1c8544ad4709f0a492c52e5910cf84fe8c82c32539e3b4f8e682fe9525"
   license "Apache-2.0"
   head "https://github.com/jsdoc3/jsdoc.git", branch: "main"
 
