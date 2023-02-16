f6d733ec3855e33d1858da2eb0986b5925faaeea
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-16 20:05:19 +0000

    vercel-cli: update 28.16.1 bottle.

--- Formula/vercel-cli.rb
+++ Formula/vercel-cli.rb
@@ -8,13 +8,13 @@ class VercelCli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "50e269387c1db3f3318daf257b67bb94ef30ec339e1355e47b938c1175a04b04"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "343e45d73311730d57f9ddf2d5d5e95f08a276d6bbbfdf938cee532eee04bc63"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1d22df9717a44b2a4b531c30ba5b39c9b04611e98d8ce86d941395414f9f60d8"
-    sha256 cellar: :any_skip_relocation, ventura:        "7d098343335c2c26462fcf0008f08453c558a243492aa7aeed0e784690dd589f"
-    sha256 cellar: :any_skip_relocation, monterey:       "19fadaddce4e4fa96504a4be2dd961c2496410ddc29e972a829bc41850d9bbae"
-    sha256 cellar: :any_skip_relocation, big_sur:        "ae0021fd9e4dd28d1bc0f4187c438db039e8ec362c84794340e02e137753f9b0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "83e8da8c91eb6fca5338cbc1db27a5f102d2eb5c73d7f35fbfee1f66711aaf4e"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "b565d1f6bf2133944dafab05429de7012807b1f916989b66be60be8afa7482db"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3179002698dcb8908dfc27893837cda60b418d9ba55c73317ca6e02f8de58a2c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "567392784948a9842dcc0e413586bc9d3b09ac7276cb9ec8da57bb517b76a741"
+    sha256 cellar: :any_skip_relocation, ventura:        "9fa0414f81d0fb9fb67ae67f459dede7c9bdb268669159f96f0ed8db37c76a27"
+    sha256 cellar: :any_skip_relocation, monterey:       "c596c648dc015fc067ef3b7170adf9126b7a7d47d6b54f91131a0d84d7ce7286"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e47bd9d0adaf0024f09b007efe212696ff910f4367fc63584e79b63d6926ac82"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4cb69bacd97a33091eaffad72fbbbb9c058f9600281cc838963948c070d22ddc"
   end
 
   depends_on "node"
