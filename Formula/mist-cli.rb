3bd8da21e064cbabe2b6cdfb30b17200695a62a9
Moulick Aggarwal <moulickaggarwal@gmail.com>
2023-02-16 18:39:13 +0530

    mist-cli: renamed from mist
    Closes #123608.
    
    Signed-off-by: Moulick Aggarwal <moulickaggarwal@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/mist-cli.rb
+++ Formula/mist-cli.rb
@@ -1,4 +1,4 @@
-class Mist < Formula
+class MistCli < Formula
   desc "Mac command-line tool that automatically downloads macOS Firmwares / Installers"
   homepage "https://github.com/ninxsoft/mist-cli"
   url "https://github.com/ninxsoft/mist-cli/archive/refs/tags/v1.10.tar.gz"
@@ -6,14 +6,7 @@ class Mist < Formula
   license "MIT"
   head "https://github.com/ninxsoft/mist-cli.git", branch: "main"
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "bc508927c1bdda99abe23778eac259286e54a7f36ccd0c76ed02821440261b9b"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b81d11c1a272ed78fc7dfd4c476d34d3f4e46184232e33013e83cfa917b937fc"
-    sha256 cellar: :any_skip_relocation, ventura:        "6772980dd498a6b6ff21ad5541257c51f9eba540a3b252908ebc3b63a8a0f4db"
-    sha256 cellar: :any_skip_relocation, monterey:       "1b55c65fcc47bc62c4ca6700610a4722a350deb69a1126cea19dce2b6dfd0765"
-  end
-
-  # Mist requires Swift 5.7
+  # mist-cli requires Swift 5.7
   depends_on xcode: ["14.0", :build]
   depends_on :macos
   uses_from_macos "swift"
