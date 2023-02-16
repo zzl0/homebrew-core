cf14c0c2b2331d7378876703490aa569d450d16d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-16 20:05:05 +0000

    jfrog-cli: update 2.34.2 bottle.

--- Formula/jfrog-cli.rb
+++ Formula/jfrog-cli.rb
@@ -6,13 +6,13 @@ class JfrogCli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "5cf4669e2d9466ef4ab3a0a60a858c384d7d16e9781d080f236b5d4647043b30"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "7992f14b7469865067263a2d9e729231187db0ea356258cb3768f8a3190c282a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fe774d169250c1efbcb4c118e170a3977c32dc51753fb5a0467525bf06152a10"
-    sha256 cellar: :any_skip_relocation, ventura:        "4830b194a5dcbf4f93f3dc02c762bc4810faf5457a0d05eca9fbb1c0a7a34f44"
-    sha256 cellar: :any_skip_relocation, monterey:       "a0a19ee6e9dee53b58ff344b3aa33414c8ebbd399f3125bd3da902dfe53b5a2a"
-    sha256 cellar: :any_skip_relocation, big_sur:        "18481aa72ac131f87108b7a862b84e56c492d2389037de7057afd68266a083a4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "45c3238d1e599c032584cddc8202656a135f9bb43edbcf5759244653e9ecf73c"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0f169c04f6328248619b5775957712a1a6c970c3c69a81988de67bb83d212f9e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "817f6d95962a099627b96302f1c95c5c5053ae8ce093c5217ef6439700d25265"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "92cfa89a68f08e8ef447bd0be763d667017032211373d1b89c0d27fc587d0f8c"
+    sha256 cellar: :any_skip_relocation, ventura:        "75a5bab054dc754dc50f687583a32af4d4767e28d5538fbd16bf857359f174d8"
+    sha256 cellar: :any_skip_relocation, monterey:       "94ced211c976b558f4489993da8e08ccacd3e9491876d9f1f800ca5bafb8e28e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "970c93a0831632c84b5c40bbde2bcb4f5790420567ef995736913e29ecc3fe44"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0b7c87bc55a0a8aa6ea4ceacb38a8cc015a528ecb62aa3079e6523100127fa9a"
   end
 
   depends_on "go" => :build
