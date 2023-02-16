2591fa79dc913e5d829d86e8ee332c9c9a6a5d09
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-16 10:00:02 +0000

    xterm: update 379 bottle.

--- Formula/xterm.rb
+++ Formula/xterm.rb
@@ -12,13 +12,13 @@ class Xterm < Formula
   end
 
   bottle do
-    sha256 arm64_ventura:  "0e7826a1d4e31d29a4cf8c2fad253c24933507a876824af7a5d01a071b68d823"
-    sha256 arm64_monterey: "bd3cbc6a31c95fc15562a6da7533b4a045d6f7a83428c06004d8494a0680ebad"
-    sha256 arm64_big_sur:  "2eec52a969c954b15bfeeeaafd1bbd2fecfaab152f5835ec2ce386fd7f6c0030"
-    sha256 ventura:        "fde2c3e73ef5255eaa0c9ea7b0f52b78ba31bf9712e7b0dd5ba9f8cbc6d78fcd"
-    sha256 monterey:       "25980a4da2592e6b22280e0260fb33322a095b3e144d2c771d3484a643b35909"
-    sha256 big_sur:        "5ffa040624f8694d77d2b90ee44e292bb9f28b3f965ef216b7d3eae4214888f9"
-    sha256 x86_64_linux:   "5455a225231ee544afb391511d1e4c853122b4ebd1e7384a7d2095d8cfd01276"
+    sha256 arm64_ventura:  "34c8c4422635d55289d117ced43e6fdf88f6ff485331f580d2700a710e5f1869"
+    sha256 arm64_monterey: "7bc7f8c3b8b6afca991c153d274fb7adfe286ae8948118a3774ecb9782cbc6ca"
+    sha256 arm64_big_sur:  "39ca9f3e5e97570339e83ead95b2a64e060e6eb33e857c75394573dfdbc5c6c7"
+    sha256 ventura:        "29288a517ffec0ff1a0d4939ccb0294603e8cabc0cb0f4c2aae1fbc6fc4f45bb"
+    sha256 monterey:       "121dc96617a3281bb299e5a6db57bccb54cc7db839fd9da0bfad2c5dab5b93c8"
+    sha256 big_sur:        "8527b1baf75d4abf42635a5361ea0704888c7360e1a6f2aa76e84aa82013c1d8"
+    sha256 x86_64_linux:   "362ebd15081ad6c51aa8118b02ce8087923ca4dd152adcba86e0d7f11c6bc994"
   end
 
   depends_on "fontconfig"
