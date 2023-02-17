63b9674513e8a4734d5c67bef3e4d707b03f6ebc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-17 01:33:21 +0000

    cmu-pocketsphinx: update 5.0.0 bottle.

--- Formula/cmu-pocketsphinx.rb
+++ Formula/cmu-pocketsphinx.rb
@@ -12,18 +12,13 @@ class CmuPocketsphinx < Formula
   end
 
   bottle do
-    sha256 arm64_ventura:  "cb3db8f8860e169f7bcdb427935fccdf2b9cddb1c3c1891d296a098664f4ba42"
-    sha256 arm64_monterey: "314f80412a27862cdb5b857b3ae8273e28e29f81dcb4279d60228b149df9b97f"
-    sha256 arm64_big_sur:  "4c3fc6b03df2530653a4ff38b270e9a37dd968748b38a011cae6500ab38d8084"
-    sha256 ventura:        "b3ce7baee259f699334d2a00e412c99ee1ee582f104875bc747e372933b19625"
-    sha256 monterey:       "263434383a2856a30d3119a4683a7c61a52521f08b8ee7bd08f02d8b1efa2de7"
-    sha256 big_sur:        "25ce90366420de438c34e8f037f1da3c7ded680375fcc69127cdec7695f84fed"
-    sha256 catalina:       "f2fc23a67634f26befdd128d21e886d7b3789484a14d498a40949e0e100d8afa"
-    sha256 mojave:         "cc655d82bfce35b2976e4dd0867fceb02b233363e7e101c62b09e60e2be9f8bb"
-    sha256 high_sierra:    "628d162751962337c769090867c3f9921d10b09f704f8f208b63abbefef205eb"
-    sha256 sierra:         "12abc8b527906e7ed0d2f6f0a6b6cb5c00f548fe94fcce995bdc80f43b4cf17b"
-    sha256 el_capitan:     "2f1f4738dbcf7641a530b82c4dc6447ecadb5f9b60cd2484c33c379efb5c46e5"
-    sha256 x86_64_linux:   "3807f33dd6becb2a82ff3ba4062f6604a4001a2650d98526815b17b799627a17"
+    sha256 arm64_ventura:  "0061941ed90615f083e805696e01ae0533e841cb0898efe8ed0170fe2f55b5a6"
+    sha256 arm64_monterey: "14914963bdeccfbb391f4a9c6e5fde36e7aceb9042592b70a3e11406a553d55b"
+    sha256 arm64_big_sur:  "0c8d323fbae3061aedc96f195ac9eb22b8cabc9b14b3f3ff2afbf327442a2050"
+    sha256 ventura:        "7e44c0fc71cf5cb8794dfd900492af1ae1e0ab2b3db8b1d261293b3b22c27964"
+    sha256 monterey:       "6db158c91df59ec2d1545c5e31d0b0e297c4955e651fd963c1c4d70d8a3f88d2"
+    sha256 big_sur:        "f5784b0c53d002bde02fe0e2c09a4c0c305fb6216a6eeaa57b1914ec94e7bdf8"
+    sha256 x86_64_linux:   "d85571c34ac30744302170922e466c101cd7a6c8d8bf7cbccc5941cd416a2a83"
   end
 
   depends_on "cmake" => :build
