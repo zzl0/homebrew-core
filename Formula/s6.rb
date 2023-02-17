86f4f7fc0ba265ac62e1f298abc51f7528e8739f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-17 18:05:23 +0000

    s6: update 2.11.3.0 bottle.

--- Formula/s6.rb
+++ Formula/s6.rb
@@ -11,13 +11,13 @@ class S6 < Formula
   end
 
   bottle do
-    sha256 arm64_ventura:  "9036deb01a38925a5a2224617aba7215b7e2bd090c604e7fffb19664a1496a01"
-    sha256 arm64_monterey: "f30edaca6c99ca5375595ccb9aed3ad15a453ddea2e26d6240af305df521ad60"
-    sha256 arm64_big_sur:  "267b0c2cd212a38eb8e166c4d60759a3701d8276f7624d5adaa9c4b086f11d7a"
-    sha256 ventura:        "db1053bd3a19a232999a4d51d34c60594cf7932bb4dfe918b28d8631d3f1ddc7"
-    sha256 monterey:       "c169933c1a6903f900295b4b7993762df24a63f2ab73129200a7de415b18faef"
-    sha256 big_sur:        "9eef22c17e62fcad2a02830dd15d395ef89966ebbbd68e6be41489170f6449dd"
-    sha256 x86_64_linux:   "1ddcba8807707b9221dd242f72b3d72ef6fa8f5a5e66d938dc7a1166f65ba995"
+    sha256 arm64_ventura:  "c82ea3b1c591f6aaa077f84962ee7c1c224dad5e382ac8d5162c1befd936d8fb"
+    sha256 arm64_monterey: "e94af0b2a1119143b2d76f094f6e444ab7f19f12963d554573a9397fa508eee0"
+    sha256 arm64_big_sur:  "5b42da2e8f1bc145f5cddcc5f5daf7a8078e3d2f77991da30a70ee60ec48265a"
+    sha256 ventura:        "c1085920c308c32680e12d0fafd5d0f79d9117276d5fb7dd722ac8124de57953"
+    sha256 monterey:       "5de223f19bd661602e0a77bf51688b1800716d32b6baf1e388da4a1d55ce0d58"
+    sha256 big_sur:        "e4e412e0fb5001c580794a58bf8c0ad5d0236c73d7555a0f5b7aae61942cc231"
+    sha256 x86_64_linux:   "4891d27cf93044135193523ef758834f1b2caac14de80cb6f72593db59d0626f"
   end
 
   resource "skalibs" do
