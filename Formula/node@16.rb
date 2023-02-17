1655c17518c96f9f5c4819b95cbe51539d30d3e9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-17 05:05:17 +0000

    node@16: update 16.19.1 bottle.

--- Formula/node@16.rb
+++ Formula/node@16.rb
@@ -11,13 +11,13 @@ class NodeAT16 < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "409301e232b4522fc92d9236b34feda83e566487ff3482685f086184cdd95728"
-    sha256 cellar: :any,                 arm64_monterey: "86681e0881084419c49698dea823a595d3c436cc2a2e09dda8435a342b81533f"
-    sha256 cellar: :any,                 arm64_big_sur:  "cac7580ac336f12b2c4f52d7a08a989e27d59509e4156ef13147bed6bce66e12"
-    sha256 cellar: :any,                 ventura:        "35f5c627199a794ba1f79de4da0c35aaedc0e8871cfe5255a8449475638ba441"
-    sha256 cellar: :any,                 monterey:       "6b32504d6420c6b52d08d1aba3a97c7eba3af0a1a56ba02ca7db9637a808ffec"
-    sha256 cellar: :any,                 big_sur:        "36e741a2cfd9b934a12468bcb89f3cbc4c9f82da8b8d2db9c8734993e9e7172d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1b8e667c9dfa32f37fc2b05d89eca46625215baa773309290e1ec0e045dc6ec5"
+    sha256 cellar: :any,                 arm64_ventura:  "f58aff3deda91093e2f5c240ba9c888a37c759e85156fdc4dda73abbd5e8fc2b"
+    sha256 cellar: :any,                 arm64_monterey: "4cc5889fb689a3b73e001aa4b08747e6ea3db52619886e60f76cfd16c171ab1f"
+    sha256 cellar: :any,                 arm64_big_sur:  "45c2c4055a56808c95ee4e7cc2f8674b512fd646f2c72fae94d06367989535af"
+    sha256 cellar: :any,                 ventura:        "e85fd70a25422f067e9dec4425b0a16a9d6afdd661bcedba110f315d42dacaaf"
+    sha256 cellar: :any,                 monterey:       "f74dd23e22b3ac428f0d3f8abb73dd84afe8767675aa646dfe3acc3a37015c9b"
+    sha256 cellar: :any,                 big_sur:        "b0e8bffee8f402f4bc5fe56906607fec414f610f2e72ba229d9f8e2d6611ed1d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "34b37dcacb982c2cf907b61c20a914ae2465f7b50870dceb8f4620ddad30a94b"
   end
 
   keg_only :versioned_formula
