a825a2ba0a2453dfba77730c9809fbc05dfe511b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-17 18:05:29 +0000

    netlify-cli: update 12.13.0 bottle.

--- Formula/netlify-cli.rb
+++ Formula/netlify-cli.rb
@@ -9,13 +9,13 @@ class NetlifyCli < Formula
   head "https://github.com/netlify/cli.git", branch: "main"
 
   bottle do
-    sha256                               arm64_ventura:  "85e5bbdc92d1fd4507523e11a02cf4d900d91d948bf78fd45c5ee7a0c9eec7e6"
-    sha256                               arm64_monterey: "b6bf5f0d4bcb0ac5823a248df4d5f03693ee5c5ab8beff81cc79f9ad65ecaa28"
-    sha256                               arm64_big_sur:  "34640b1f295ea3e97617544293fdb88d2f0434b7b2a175a7f2f4c52104625317"
-    sha256                               ventura:        "3471e12e24105ba5ed75cefe9517ce29282dcad036f298245016bc1f3591b4ce"
-    sha256                               monterey:       "d42c132ca5fda425fda0af1065543bb979a856b99b1b76b4547ca9a90d853a71"
-    sha256                               big_sur:        "66d1932f1b1117fd353e5aef6c780f02fd58b3f35d0af42bcfec417432d1b18f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "68930e80125831e172ea0c7c4ba3f2c4e941b3e184b9ef55dc34ee38bbfa0719"
+    sha256                               arm64_ventura:  "f9d3d71c56e1a5fb1ed4b6da4c9b5313f176fce1a7da417fb1537f5a09227186"
+    sha256                               arm64_monterey: "ad9b83c1b47822fa52560443bf91cdc2870c3b6577a58ce25e82c36e2a9e61d6"
+    sha256                               arm64_big_sur:  "bc6e429173afbe2220298fe4956944a0fee3d0cedbc3a1cfe975b732b1a756ae"
+    sha256                               ventura:        "84e374a811084d23cdcc0ffb0ce99f554aff9ff9de96e15412745cb10988f017"
+    sha256                               monterey:       "b2bda6f2de632f0ffa64648b33a629136db1e6f23d34f342d7e3516a762a790c"
+    sha256                               big_sur:        "29fa9a4c4eca0df8ad714747932bf1a9caf6ea91cc10cae9d0a45ecf44546975"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "04e412defc606c301a1d495bb461bfd61ca8f2598804330e385ad54a7202e4ce"
   end
 
   depends_on "node"
