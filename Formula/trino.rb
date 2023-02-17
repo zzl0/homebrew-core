c7bcfbd05075ee110767305fd6086e036bad2063
Rui Chen <rui@chenrui.dev>
2023-02-17 10:31:34 +0100

    trino 407
    Closes #123506.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/trino.rb
+++ Formula/trino.rb
@@ -3,8 +3,8 @@ class Trino < Formula
 
   desc "Distributed SQL query engine for big data"
   homepage "https://trino.io"
-  url "https://search.maven.org/remotecontent?filepath=io/trino/trino-server/406/trino-server-406.tar.gz", using: :nounzip
-  sha256 "3de29173cc860abf30522d8a53761e5c4f38f1ff182765dbbc58638421364d79"
+  url "https://search.maven.org/remotecontent?filepath=io/trino/trino-server/407/trino-server-407.tar.gz", using: :nounzip
+  sha256 "748679eeac0b4f1976d79c5364aa66e45bb43b9bb868f56b1023738fc48a4fb5"
   license "Apache-2.0"
 
   livecheck do
@@ -21,25 +21,23 @@ class Trino < Formula
   depends_on "python@3.11"
 
   resource "trino-src" do
-    url "https://github.com/trinodb/trino/archive/refs/tags/406.tar.gz", using: :nounzip
-    sha256 "647d08c00693113f41fe600718b3436ee6c6738046876ea14796ed84230a0fd2"
+    url "https://github.com/trinodb/trino/archive/refs/tags/407.tar.gz", using: :nounzip
+    sha256 "e8a777efcab913ed80bd2a08ad18a5afaf9338e0ab15fc996edaa74f5863e11b"
   end
 
   resource "trino-cli" do
-    url "https://search.maven.org/remotecontent?filepath=io/trino/trino-cli/406/trino-cli-406-executable.jar"
-    sha256 "70f7ab57cf74f655942d14f4baafebac32ca64ca833790411691a69b574c16bb"
+    url "https://search.maven.org/remotecontent?filepath=io/trino/trino-cli/407/trino-cli-407-executable.jar"
+    sha256 "1b1fd386501758b7a392a4791e49da061614b2890d3bf76ecfa130aa56380912"
   end
 
   def install
     # Manually extract tarball to avoid losing hardlinks which increases bottle
     # size from MBs to GBs. Remove once Homebrew is able to preserve hardlinks.
-    # Ref: https://github.com/Homebrew/brew/pull/13154
     libexec.mkpath
     system "tar", "-C", libexec.to_s, "--strip-components", "1", "-xzf", "trino-server-#{version}.tar.gz"
 
     # Manually untar, since macOS-bundled tar produces the error:
     #   trino-363/plugin/trino-hive/src/test/resources/<truncated>.snappy.orc.crc: Failed to restore metadata
-    # Remove when https://github.com/trinodb/trino/issues/8877 is fixed
     resource("trino-src").stage do |r|
       ENV.prepend_path "PATH", Formula["gnu-tar"].opt_libexec/"gnubin"
       system "tar", "-xzf", "trino-#{r.version}.tar.gz"
