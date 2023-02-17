067632ee5d27c883aba81cc6afad8ae14d072e4a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-17 17:05:03 +0000

    aravis: update 0.8.26 bottle.

--- Formula/aravis.rb
+++ Formula/aravis.rb
@@ -6,13 +6,13 @@ class Aravis < Formula
   license "LGPL-2.1-or-later"
 
   bottle do
-    sha256 arm64_ventura:  "82eb96c6252e2b1e70fe9b674fc39039c29e88099619aa7e5ebf0bbaaf45e67d"
-    sha256 arm64_monterey: "e78951e5a3b1796850c87b7f063caaeea7752ae41d14174b7d36932cc8b7325a"
-    sha256 arm64_big_sur:  "e82e9a4a1a50e98d26f4d9b8995fb1b875aab4c579cfbe5b1e12436dfd88a0ee"
-    sha256 ventura:        "b04b602a5369417202f6e1c58ae07d026076e204a9586b23abf961498081fe56"
-    sha256 monterey:       "98b52d161ea00386e3430bd147df980262a44adc74ffacf105ca524b34b3d5c2"
-    sha256 big_sur:        "4f9d5e6be023e7356b475c35a3a939fe1e86bf377d0aec3337422fc415bbf90c"
-    sha256 x86_64_linux:   "96e081f2e5608a943e48eebc34f0f3137b73f8f99b3062330b70223fadcd3681"
+    sha256 arm64_ventura:  "7b8539b5c6715fbe5e1c1190729a0cafcd8c7ad2b68f9b2de3a367d2888d96a4"
+    sha256 arm64_monterey: "f1b8c71965cc30d5a0e67b74cd4e413c08f43524ee3c085947ab6f4e23dbc914"
+    sha256 arm64_big_sur:  "92c03d273363efbeddd1718d304fda597f7a90bec40ddf9841bd5b8e98e9b37b"
+    sha256 ventura:        "7c0ee991c488b3e8b47cbf0931f6da3498af1c4c9b1bc2da7aaf2e05ee2d5a29"
+    sha256 monterey:       "c1cbe8f7c81f5e6129b13939c61318769db6a5bcaa96062daee10ac2abd2e91b"
+    sha256 big_sur:        "058cf7af98f1f5b12398e7783e5fd8e0b5790e6e564af83c7b77b2e1a5a2c3cd"
+    sha256 x86_64_linux:   "191211b85ea19f09472092668492e9ba9b83979c79b2f2bb22e12b197ef63489"
   end
 
   depends_on "gobject-introspection" => :build
