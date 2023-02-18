8211c0622d5793fd2ec1c34f83650580522b98e1
Michael Cho <20700669+cho-m@users.noreply.github.com>
2023-02-18 10:09:56 +0000

    odo-dev: update 3.6.0 bottle.

--- Formula/odo-dev.rb
+++ Formula/odo-dev.rb
@@ -8,8 +8,12 @@ class OdoDev < Formula
   head "https://github.com/redhat-developer/odo.git", branch: "main"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "79f6e74e8e605335aa6b841b328fcb014b4680dc2d41de4d49293f0976a6a7fb"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "acc7899b7060ba2ed4c4dc1883ce0c91ec104b39307b5369dc34d9017839293f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "592bad9e4cb0265e45a1a4d72c997e5862b37736f4067c053de3d46f17fe4065"
     sha256 cellar: :any_skip_relocation, ventura:        "66a3e2b392787b1db2688f8b4d7cedc8ec1b4150eb016b4c6e47a9ee29c7f236"
+    sha256 cellar: :any_skip_relocation, monterey:       "da8c0f9401ebd8077757bc5ea2641054675da4152c08a3aa2b1d19e61cb25d2e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d0d2dce02165cdf883b358c7129f138a1d46cc38c86c6dcf8c1e4d63e5a47f1c"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "73dd235856ef918800d0fef88f6609adfeb26c8ea4c9dd447bcb804cdcc42032"
   end
 
