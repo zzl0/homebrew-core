9ee83dfb7a799b60f2532af459ab1538f8bf6048
Patrick Linnane <patrick@linnane.io>
2023-02-17 17:56:47 -0800

    fortio 1.50.1
    Closes #123570.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/fortio.rb
+++ Formula/fortio.rb
@@ -2,8 +2,8 @@ class Fortio < Formula
   desc "HTTP and gRPC load testing and visualization tool and server"
   homepage "https://fortio.org/"
   url "https://github.com/fortio/fortio.git",
-      tag:      "v1.50.0",
-      revision: "d1d19b6d862c57ef2b5394e21d954d1df6d4fe70"
+      tag:      "v1.50.1",
+      revision: "f7ca38b31431e8ae81e2ab343e6fef71dcdfae6c"
   license "Apache-2.0"
 
   livecheck do
