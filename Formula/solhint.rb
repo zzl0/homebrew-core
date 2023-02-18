1b23c9364f1a421b429b1d9a44b8aeb84c340b44
Rui Chen <rui@chenrui.dev>
2023-02-19 00:26:59 +0100

    solhint 3.4.0 (new formula)
    Closes #123642.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/solhint.rb
@@ -0,0 +1,37 @@
+require "language/node"
+
+class Solhint < Formula
+  desc "Linter for Solidity code"
+  homepage "https://protofire.github.io/solhint/"
+  url "https://registry.npmjs.org/solhint/-/solhint-3.4.0.tgz"
+  sha256 "d7a214896a9b9d0c79110cdf1774057c0d78bfa9e8fbafb6628aeb8953aa00f8"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    test_config = testpath/".solhint.json"
+    test_config.write <<~EOS
+      {
+        "rules": {
+          "no-empty-blocks": "error"
+        }
+      }
+    EOS
+
+    (testpath/"test.sol").write <<~EOS
+      pragma solidity ^0.4.0;
+      contract Test {
+        function test() {
+        }
+      }
+    EOS
+    assert_match "error  Code contains empty blocks  no-empty-blocks",
+      shell_output("#{bin}/solhint --config #{test_config} test.sol 2>&1", 1)
+  end
+end
