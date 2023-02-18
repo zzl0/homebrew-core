f2bcfbba87e7c202ae04ed508e1aa8a500f26d67
Rui Chen <rui@chenrui.dev>
2023-02-18 09:06:53 +0000

    clipboard: add 0.3.2 bottle.

--- Formula/clipboard.rb
+++ Formula/clipboard.rb
@@ -6,6 +6,14 @@ class Clipboard < Formula
   license "GPL-3.0-or-later"
   head "https://github.com/Slackadays/Clipboard.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "2e5e31417b447abae3fc5f894f44d8f91cbca6437200ee268d7c0a9fdcfe0ef8"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "80a55860d2a5cae77caf4ee411bb475b4c4964d115258c9e24f652fc04fc6628"
+    sha256 cellar: :any_skip_relocation, ventura:        "bd37f12d2857e2796e95a7a826e407976da9482c4b2457a64baa2bc1c13d3424"
+    sha256 cellar: :any_skip_relocation, monterey:       "30c672dffc025e258f3fe09873668e5069c36fdccd71fb611481dd22914d4a4f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "306606b0588bee5288dc88a1df28e210fccc541d504bcfc34b5bce57628908df"
+  end
+
   depends_on "cmake" => :build
   depends_on macos: :monterey
 
