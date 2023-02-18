11be789f7755cb6521ad7c6521d8808f9fdbe5fd
Bo Anderson <mail@boanderson.me>
2023-02-18 00:06:23 +0000

    curl: patch HTTP/2 corruption issues
    Closes #123566.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/curl.rb
+++ Formula/curl.rb
@@ -2,11 +2,12 @@ class Curl < Formula
   desc "Get a file from an HTTP, HTTPS or FTP server"
   homepage "https://curl.se"
   url "https://curl.se/download/curl-7.88.0.tar.bz2"
-  mirror "https://github.com/curl/curl/releases/download/curl-7_84_0/curl-7.88.0.tar.bz2"
+  mirror "https://github.com/curl/curl/releases/download/curl-7_88_0/curl-7.88.0.tar.bz2"
   mirror "http://fresh-center.net/linux/www/curl-7.88.0.tar.bz2"
   mirror "http://fresh-center.net/linux/www/legacy/curl-7.88.0.tar.bz2"
   sha256 "c81f439ed02442f6a9b95836dfb3a98e0c477610ca7b2f4d5aa1fc329543d33f"
   license "curl"
+  revision 1
 
   livecheck do
     url "https://curl.se/download/"
@@ -45,6 +46,17 @@ class Curl < Formula
 
   uses_from_macos "krb5"
   uses_from_macos "zlib"
+
+  # Fix HTTP/2 corruption issues.
+  # Remove with 7.88.1.
+  patch do
+    url "https://github.com/curl/curl/commit/3103de2053ca8cacf9cdbe78764ba6814481709f.patch?full_index=1"
+    sha256 "f4abbeb8174ab51b393da02c2761ba56bc40c577b5802aa41e74a3adc7d5a0be"
+  end
+  patch do
+    url "https://github.com/curl/curl/commit/87ed650d04dc1a6f7944a5d952f7d5b0934a19ac.patch?full_index=1"
+    sha256 "39f74a9c88dced544a8ea0a1c1e8c9f30eae19c41223350991ebf03e5dec764d"
+  end
 
   def install
     system "./buildconf" if build.head?
