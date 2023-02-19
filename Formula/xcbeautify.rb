5f488a6f22e63bbce2af71a03c87d8387867b1c5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-19 05:33:08 +0000

    xcbeautify 0.17.0
    Closes #123657.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/xcbeautify.rb
+++ Formula/xcbeautify.rb
@@ -2,8 +2,8 @@ class Xcbeautify < Formula
   desc "Little beautifier tool for xcodebuild"
   homepage "https://github.com/tuist/xcbeautify"
   url "https://github.com/tuist/xcbeautify.git",
-      tag:      "0.16.0",
-      revision: "b578117619ffed44fb879b29e4e9a481143b0b1b"
+      tag:      "0.17.0",
+      revision: "9f3d55d3b6e048f91e03af843b488f99c483f1aa"
   license "MIT"
   head "https://github.com/tuist/xcbeautify.git", branch: "master"
 
