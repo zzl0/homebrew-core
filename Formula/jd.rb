01f9f97b7d36380de14c81ba8fe5b59433dbf4d6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-19 18:12:50 +0000

    jd: update 1.7.1 bottle.

--- Formula/jd.rb
+++ Formula/jd.rb
@@ -6,13 +6,13 @@ class Jd < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8255ec6e06bc4d044f6181924ddf842e217c798d5f52a0c4690bacb6f64ad960"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "bf3ee6b9cb4222a1623e03ff3a036ae4047e1f8f976c9060d06ce683c13262c1"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "eecc45d5b53ae750cf709d9e53d5cf95f36f8196cbfc420cee06cb285a7e4b7d"
-    sha256 cellar: :any_skip_relocation, ventura:        "c1317eebb4d15892ecd020bd411f4544fa7ef65627fbc42aa40c1a68e865f645"
-    sha256 cellar: :any_skip_relocation, monterey:       "59793f1d9eee68ad5597b45aad611a80626d95f37e8a5e3eb0c1b5ee0187a8f6"
-    sha256 cellar: :any_skip_relocation, big_sur:        "a07f8e0c35e55b8c199ab9650d59723797efffbefc587e7bf3a0e372f4b9baa7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "51979891e39cd617e79d33642fc905b13ece762fe2cd5e898bff66cd06d86e91"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0a2b07ba96ff95b927175fcdf22386d458b43fd0fd318263d7dfcd851deaa920"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a5459ba7e6a8b80bc8874dde1d866fda9b99f291ac41e462ec0de9c1eee45d2a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e23b8fd52d1d4d56c8e402127d7fd5ec9f7d3939ebf0be57c0360265bdab09ab"
+    sha256 cellar: :any_skip_relocation, ventura:        "17ae4f04c0016a933f923aacc09696a7ec54ac8ea76e5f2e7ba28903d84b2738"
+    sha256 cellar: :any_skip_relocation, monterey:       "9080e532d8e9c322d6322148d2c7a21bc8606fd9257683724d02f43f21cd4742"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f40bd8456140f8332ec1e13efef5a260f12e33908306a6b6429cea3446140408"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "112215528c11ec54910cdbd320d9c0ced6714d87f27a573fb86e40c26829b70b"
   end
 
   depends_on "go" => :build
