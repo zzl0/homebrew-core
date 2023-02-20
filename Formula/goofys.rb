d433070465868e93d6a77d1115d3c9b1c4252b60
Alexej Kubarev <alexej.kubarev@accedo.tv>
2023-02-20 10:49:41 +0100

    goofys: deprecating due to broken upstream
    Closes #123745.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/goofys.rb
+++ Formula/goofys.rb
@@ -12,6 +12,9 @@ class Goofys < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux: "53acc931a935c3e7c6230a59d492bf0cea7238167415083232d6ef37741b1cdc"
   end
 
+  # Discussion ref: https://github.com/Homebrew/homebrew-core/pull/122082#issuecomment-1436535501
+  deprecate! date: "2023-02-20", because: :does_not_build
+
   depends_on "go" => :build
   depends_on "libfuse"
   depends_on :linux # on macOS, requires closed-source macFUSE
