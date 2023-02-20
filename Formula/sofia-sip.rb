75ecec85227a9d078cc458938a1e62feaf4b37fa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-20 20:13:27 +0000

    sofia-sip: update 1.13.14 bottle.

--- Formula/sofia-sip.rb
+++ Formula/sofia-sip.rb
@@ -6,13 +6,13 @@ class SofiaSip < Formula
   license "LGPL-2.1-or-later"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "1514b21e9dec127cbb1f3b49fda1e6cc0b0d1259e4762e392ef9f25933875151"
-    sha256 cellar: :any,                 arm64_monterey: "9f00bc43eabee791ffb5e2639cac4f792e49a5485cd20e97bc40de59eedf32e0"
-    sha256 cellar: :any,                 arm64_big_sur:  "49692dca83d35179e7754fc825d060cb29ee53eeb7c2e3ed5f2df2193efa8644"
-    sha256 cellar: :any,                 ventura:        "120d5f17a56f48a6de094135ba6b56ab66c62c06b2c85f2c4a68066552b7a1a7"
-    sha256 cellar: :any,                 monterey:       "abf98051d6e0bd8fedf7a5cab730dc7ed578ff44d36156a53ad9a43b4d51c114"
-    sha256 cellar: :any,                 big_sur:        "06276e4a41103eca3d4bbe2d195c0ec87f71d16e7ebc24b1ee547d708a2a1bc0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "02b921d146163c3ee6e4c491b6f75ba89ed72809d0f028465b6641fbe38b586c"
+    sha256 cellar: :any,                 arm64_ventura:  "bfb64a936a3aeaa45e62ebbec38ef5c80367a90b35c132937cfffe7915e88634"
+    sha256 cellar: :any,                 arm64_monterey: "a813e90a4350f194fadb9b391166fc4f228365164aeb023f82e49351b2af8db7"
+    sha256 cellar: :any,                 arm64_big_sur:  "d1e1035d5208ec5ed8524fc0fa833f24d5779ded1ad8bf460c98e189d3293e76"
+    sha256 cellar: :any,                 ventura:        "e3eba59e3f47f5a1eec8fc2742b2ce283a2342c931dd2021cf72371a026c4a0c"
+    sha256 cellar: :any,                 monterey:       "65c5af6c9db07ea047a68655649384cecf400e312048738364698d3b94a46102"
+    sha256 cellar: :any,                 big_sur:        "394da7b648bd351e1e6c88d222f44b9912268a0fe20dbfaf277b486bf058b51d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0b2b254d850f0c82836fff43ab69e8dd01dc3306753e200b9d08faec38f65532"
   end
 
   depends_on "autoconf" => :build
