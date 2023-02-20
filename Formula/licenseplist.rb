15a5ba3502344f8e9466af8c27b1e5b0440ab9f6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-20 13:16:59 +0000

    licenseplist: update 3.24.1 bottle.

--- Formula/licenseplist.rb
+++ Formula/licenseplist.rb
@@ -6,12 +6,12 @@ class Licenseplist < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "1f28689d869f14bbb805f185534c07cbd9f9077bc8138a91223faa7d374b6a89"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ec02edd588d04760511dbe68588d342995896dc269c91920bb871e77ffebc196"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ee9275ac68ddc69538a9fb8e6ce2261220bc56e2654bf444ef33fcf00575006a"
-    sha256 cellar: :any_skip_relocation, ventura:        "358eb1ce5cce2fe3c412b4d65ebdb1929ce6855f4ee2dd20d6be9fa427c57bdc"
-    sha256 cellar: :any_skip_relocation, monterey:       "c7ec27a0485dc26db6f82e0530e61f2c7247e27e7ced6d90e99d74720483ec02"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f5472f9f6f7b2d8e3fc9ccd8dfe8b488799f3666b29c7895a5e57976f0cb5e64"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "eeb366c8ae834fd6b0b4d2d349a7ef01a33bcc95aea62950605d8939394cfaf1"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "62b519bf10bb411fcd4f21322eaab87329403a9fbd2d4db9f2d2c98376ee48a7"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "805ee737dbe224c7835bf300369d1b9acd78677374a1b2da6ae45a075056c6b7"
+    sha256 cellar: :any_skip_relocation, ventura:        "914def0736ac988057e2595a7d8911516e36024a1e5c483c029e92fb036ccb4e"
+    sha256 cellar: :any_skip_relocation, monterey:       "b3db1929297293290d127a96af4fd4c502235ed73727c893e6853b971e7f392f"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b82155f27fa0e8efdd8b676b4c49b9daa83aa8593379456d4d0e3e6e1d4de9e2"
   end
 
   depends_on xcode: ["13.0", :build]
