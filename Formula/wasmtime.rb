c7b2c6043f808a32a93519eed65a58a879b61d44
Patrick Linnane <patrick@linnane.io>
2023-02-20 08:20:25 -0800

    wasmtime 6.0.0
    Closes #123789.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/wasmtime.rb
+++ Formula/wasmtime.rb
@@ -2,8 +2,8 @@ class Wasmtime < Formula
   desc "Standalone JIT-style runtime for WebAssembly, using Cranelift"
   homepage "https://wasmtime.dev/"
   url "https://github.com/bytecodealliance/wasmtime.git",
-      tag:      "v5.0.0",
-      revision: "dbc6db0cfb114f1cfa30866f602eff7c9025a597"
+      tag:      "v6.0.0",
+      revision: "c00d3f0542855a13adffffd5f4ff0177dfbdcb34"
   license "Apache-2.0" => { with: "LLVM-exception" }
   head "https://github.com/bytecodealliance/wasmtime.git", branch: "main"
 
