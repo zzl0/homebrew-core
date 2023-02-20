b74806851c28efebd515521c86af6af09ab63cd4
FX Coudert <fxcoudert@gmail.com>
2023-02-20 16:05:46 +0000

    mist-cli: add 1.10 bottle.

--- Formula/mist-cli.rb
+++ Formula/mist-cli.rb
@@ -6,6 +6,13 @@ class MistCli < Formula
   license "MIT"
   head "https://github.com/ninxsoft/mist-cli.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "c2bb107b9296ff961c6397d425783c20f4203cf9eab2925dfb9de4291ee304ff"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "36341b7ec3eb7e8e7e450c3050932255060e2558aaa098d72196de04f874c270"
+    sha256 cellar: :any_skip_relocation, ventura:        "53e4fd9e88e5b171e3a50f417164d2343a852e34437508fbb36875e14ae3a60d"
+    sha256 cellar: :any_skip_relocation, monterey:       "59dbaea07bd73bd259009939e94f362bcce79c21286576aac533b179e89a027f"
+  end
+
   # mist-cli requires Swift 5.7
   depends_on xcode: ["14.0", :build]
   depends_on :macos
