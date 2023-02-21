367909eec1383cbb167cf8ea5dba9394add6cf66
Rui Chen <rui@chenrui.dev>
2023-02-21 21:57:27 +0100

    xsd: add license
    Closes #123888.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/xsd.rb
+++ Formula/xsd.rb
@@ -4,6 +4,7 @@ class Xsd < Formula
   url "https://www.codesynthesis.com/download/xsd/4.0/xsd-4.0.0+dep.tar.bz2"
   version "4.0.0"
   sha256 "eca52a9c8f52cdbe2ae4e364e4a909503493a0d51ea388fc6c9734565a859817"
+  license "GPL-2.0-only" => { with: "Classpath-exception-2.0" }
   revision 1
 
   bottle do
