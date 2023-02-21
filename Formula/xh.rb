bc318379d1299e395b4f739800ffc46d7c723fbc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-21 04:13:16 +0000

    xh: update 0.18.0 bottle.

--- Formula/xh.rb
+++ Formula/xh.rb
@@ -7,14 +7,13 @@ class Xh < Formula
   head "https://github.com/ducaale/xh.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "c132e2860dc243963d76084a6239efaa4e9b6223fe66fb585829fb8a91025d5a"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3d61902a6895bbeae9b9c2a3dcb8d1c8d4b06dfaebe4944489560c0b849b742d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "479a24e7e0e1da17ea1748bbbbd06ac80f6c676df39ed4267bfe50ab370bd4ca"
-    sha256 cellar: :any_skip_relocation, ventura:        "fa9120582a5726dd651a67ba937c8be974a60f06223c253d1d387ccda1b8c10d"
-    sha256 cellar: :any_skip_relocation, monterey:       "bb41f5acdc9aa1803405415cd9899db8f463c854e4b4cca2a7a8b3a4fe0d25aa"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b5c1ad60fca17aa9b6629cef945f00da342dd4d837c9e866ddaea5ac51e1b3b8"
-    sha256 cellar: :any_skip_relocation, catalina:       "bfddbb660e997acea4a4185b75b00474e0cf30bbb1b3f7f6c287263f9ced8986"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4a7459afd19edcd704db82dae3c3acef47df8236fa7d49865cb857abec4e37aa"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "d61598252fc5fbb54df6eafebff6ca858d460357a45f6d1f6f6bae0551c11630"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "4be4b4127f5e092eb010635197b27e9a7578447f2f4cbca3a27262b5207fc059"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d761e673f263c3faecd46adced465de42066d766625e7f75d401189f9a17fba6"
+    sha256 cellar: :any_skip_relocation, ventura:        "1f40065f6ff5b6df2c9738588f260ff1f831d772537102380b1963b71f5d66e5"
+    sha256 cellar: :any_skip_relocation, monterey:       "2637f17856ec93fea48593d9b3b10edf5958c02bc4a52b0e25f443fe672e3c03"
+    sha256 cellar: :any_skip_relocation, big_sur:        "bf162b38e669261cc35ad5f160f8053b66d74f4ce92ff5c550d5b40aa2dd26e7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ed590304329cbd86904ff0ced04a11d7116e81287cebf975ea4b483dedc42b3b"
   end
 
   depends_on "rust" => :build
