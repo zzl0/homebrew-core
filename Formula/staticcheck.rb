d2198ce78832a1acf8dcdab752ca8b02bcc80a31
Patrick Linnane <patrick@linnane.io>
2023-02-21 22:41:19 +0000

    staticcheck: update 2023.1.2_1 bottle.

--- Formula/staticcheck.rb
+++ Formula/staticcheck.rb
@@ -8,13 +8,13 @@ class Staticcheck < Formula
   head "https://github.com/dominikh/go-tools.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "daf5f4d93ead3bfbf18dae5bc1ba65a5e15012ceb8dc6e2ba07dcdfa282593fd"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3acfd1aa0aee2318fcb8c90b768c704019ce232cfa0ad089992e4ee379efef14"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e412f3f69e4d8131a09b257a9c52950e09f78235f2b414530d900ec973c31a34"
-    sha256 cellar: :any_skip_relocation, ventura:        "d3a1e6f3503075326cabb7a583a67fe2d34a3c8c7f780fc9fa6423a3413711f6"
-    sha256 cellar: :any_skip_relocation, monterey:       "d71b8a1b2e80b6bf34e709ba08efb7c625457afc87db477c05ea796be4add5f6"
-    sha256 cellar: :any_skip_relocation, big_sur:        "4c5f867d35fb0a1f24d0c64d3d8f72c410dbe616bedc8fd7c4d734ba2df44c37"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a15e9c3d88500747c0a684296a921c2bd0e2ace74af17e81cf78253f72152255"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "fd651494f08137d48328fa67f233a0ed2240c789b291a9a07e7c16142861a92c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "fd651494f08137d48328fa67f233a0ed2240c789b291a9a07e7c16142861a92c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fd651494f08137d48328fa67f233a0ed2240c789b291a9a07e7c16142861a92c"
+    sha256 cellar: :any_skip_relocation, ventura:        "7b109b5a726cc27b68ae5dc85760a1686ccce982d685b6ddcf3db2001f9aa778"
+    sha256 cellar: :any_skip_relocation, monterey:       "7b109b5a726cc27b68ae5dc85760a1686ccce982d685b6ddcf3db2001f9aa778"
+    sha256 cellar: :any_skip_relocation, big_sur:        "7b109b5a726cc27b68ae5dc85760a1686ccce982d685b6ddcf3db2001f9aa778"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a260bfbc44bbff085430a44aaa4ac9acd4ff94695e9aa0cfa21d6f5050d667c9"
   end
 
   depends_on "go"
