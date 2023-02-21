c57fc6bc19c3fffff611a306b630fe2f13a026f1
Patrick Linnane <patrick@linnane.io>
2023-02-21 22:41:32 +0000

    whalebrew: update 0.4.0 bottle.

--- Formula/whalebrew.rb
+++ Formula/whalebrew.rb
@@ -8,12 +8,14 @@ class Whalebrew < Formula
   head "https://github.com/whalebrew/whalebrew.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "adc2032cb648b90f43903e425a4f117b104a65bbe31259cd3e6099f5ca895adf"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8124f6c257d211ad2ea6f26fd1803efe6408ea7c91e12b8e2f75f7c993feba31"
-    sha256 cellar: :any_skip_relocation, monterey:       "21e0746c7700f2e3755d99ee64f85c89767dc611a5280dee81f8e58b60dfcc80"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0ba0c1e96cb7b482e7053e897c2ed50a22fffded6e939d552237b6c54f7f612f"
-    sha256 cellar: :any_skip_relocation, catalina:       "f18d17d362d27dd2fe1df05591c679653395e0a56e39cec148770b5f89011b87"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a8c4059b725199ee59d80e73f44502223d32dcee3d457c8fd8b86764ca6139ef"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "2b261a5634eb50b002b2611c01c633d1f633cb9407f58f9659d565057bba82a9"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2b261a5634eb50b002b2611c01c633d1f633cb9407f58f9659d565057bba82a9"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2b261a5634eb50b002b2611c01c633d1f633cb9407f58f9659d565057bba82a9"
+    sha256 cellar: :any_skip_relocation, ventura:        "aff7415c0ca2bbf63f06bbd53474f0870a5ce6259c6cbdf16778848d9c74c1ca"
+    sha256 cellar: :any_skip_relocation, monterey:       "aff7415c0ca2bbf63f06bbd53474f0870a5ce6259c6cbdf16778848d9c74c1ca"
+    sha256 cellar: :any_skip_relocation, big_sur:        "aff7415c0ca2bbf63f06bbd53474f0870a5ce6259c6cbdf16778848d9c74c1ca"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b1b48ff5791412a01db7203c15164644bddecb122fb037e0b84dc0cc468523ed"
   end
 
   depends_on "go" => :build
