39602bba31ff0c2244fc0434e6804c68aba81e52
Patrick Linnane <patrick@linnane.io>
2023-02-21 22:41:21 +0000

    counterfeiter: update 6.6.1_1 bottle.

--- Formula/counterfeiter.rb
+++ Formula/counterfeiter.rb
@@ -8,13 +8,13 @@ class Counterfeiter < Formula
   head "https://github.com/maxbrunsfeld/counterfeiter.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "c2848e9d73b109da41de138bccee3ad15c923a2a692b0a3d76a7d36cffc958ae"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "aeb93e13a4af27fff9048d2e30d8369476ecb82aea961333ccf10442ce693c0e"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0165588244c3e6da51aeae2a004713add997a6c57116595e4076f4f6e70ffda6"
-    sha256 cellar: :any_skip_relocation, ventura:        "0feb02c66fba1c465552f88e84481a5768e3ae6af77c7fa5d78fd44a2fe761d1"
-    sha256 cellar: :any_skip_relocation, monterey:       "5811e4ef4446b5e2575da1ff06dad56094b2a43b2308a605cc1174e342c7872b"
-    sha256 cellar: :any_skip_relocation, big_sur:        "8565f9078aa1bb68cf18674d70ea4074ad88ef743cb79034f8a9a1db2a8c2bbf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "956477417ee2bc8303141fdb8a51570f03abc98f8ae31a536636d5d960b19a99"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "4805d187ac82131e2b3e40d2c4039ae4c350d2b13f550c97d2528ed2e5541079"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "8909cfcee1ffa290ece1ee980282d8b2d4adf26afbb8b564d6f92db1e71cb3fd"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b1c60d6667c481be2435bdce9dd16dbd68d3de2dfae2b52d19cb478e76046b22"
+    sha256 cellar: :any_skip_relocation, ventura:        "d9733648376ee08012007691095c563440debca5049da4b7443202bb5070ac05"
+    sha256 cellar: :any_skip_relocation, monterey:       "6dfd611772dfce91fda225ab3d3beb32dd9bf4b4254cde116a9ac667f4e7787d"
+    sha256 cellar: :any_skip_relocation, big_sur:        "1e199e420938cd08d270bd3f457ada00683e7184c56058331c06cd773fc8f153"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2835f65e0865ccca3483ae6b0d7a7edae45eac549603f3885573fb3a841e8839"
   end
 
   depends_on "go"
