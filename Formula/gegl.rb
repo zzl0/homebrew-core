5197276dd7ee27f03e9ad5b2d0c1c4a58d881225
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-21 05:14:09 +0000

    gegl: update 0.4.42 bottle.

--- Formula/gegl.rb
+++ Formula/gegl.rb
@@ -12,14 +12,13 @@ class Gegl < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 arm64_ventura:  "ddee658a85cb1891dd4c6ae10b40a7e4c68922eac49777dd165362fc93b3bffa"
-    sha256 arm64_monterey: "91bb8f33c1700d9c45f906cee3854e7dfbc24fc2b42b2b0c0c161884f7d3c902"
-    sha256 arm64_big_sur:  "ef0f3b155ba34c48daeac9b302af952004aefe40a088ffd86490bf1366a6e7bf"
-    sha256 ventura:        "49e264a36a9b996b9710b7cd005d51512df61797676ffa302cbd3bae65142bba"
-    sha256 monterey:       "31d3ae63442125320cf3c99a8541f3e4be97baba6b694e64efb4c257bdb6750f"
-    sha256 big_sur:        "1761f7e677647fab54583b4bd272c999e5432703f575ad3dbf608d3088500808"
-    sha256 x86_64_linux:   "e0fea30f49b22c7aac70841a860c9b1e66bab14e4355147c2ab98e7019e05638"
+    sha256 arm64_ventura:  "bcdfeedb7a207307f6aa5a020a88ad074c23c4d7aa730fa3721b5caa46265dbc"
+    sha256 arm64_monterey: "0a743cf58b1f6be5b11854c8813a1354c50d284f2ea77883d35ed43c07cd1e55"
+    sha256 arm64_big_sur:  "593117443e80ef49af158abf3984b9ef4516b796d98e733a335145b54c2ddc25"
+    sha256 ventura:        "55012dd581675488ad651c9d8d6fab11911d1512ad927ff00a179243a2ecd7cc"
+    sha256 monterey:       "dde0419be53714acbf79b3f34b0a509244e959367664f01c2d42e5de92411268"
+    sha256 big_sur:        "2fd9f8aeeee3333420b8ddb4118600c8e67b1ea6158d0c325f5c285cb68a68b1"
+    sha256 x86_64_linux:   "0b170ac8cb5cb501062f1a39eb2cf6842a750d03e4a0fa4153799d7203e11060"
   end
 
   depends_on "gobject-introspection" => :build
