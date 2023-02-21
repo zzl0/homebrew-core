5cd67d515adf03e94b73cbd7e6662fb757724168
Patrick Linnane <patrick@linnane.io>
2023-02-21 22:41:21 +0000

    promtail: update 2.7.3 bottle.

--- Formula/promtail.rb
+++ Formula/promtail.rb
@@ -11,13 +11,14 @@ class Promtail < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "85c9f483122df447662d1c14c2b24f0bd4a1c5f50b72f993adc9944011b85664"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "08093f5b8441cccc813d85d2c9d9103275cda01d37f3e5427f431ddfd32dba1d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7325a77661d080ceb49b8a78bbc75d01d01dc616aa6624b44f5eaa620bb2d51d"
-    sha256 cellar: :any_skip_relocation, ventura:        "ce0effa9e159583ae60f47c89391cd24460da8113aaf8161f240943603bae3c7"
-    sha256 cellar: :any_skip_relocation, monterey:       "31e039aa3c0f13bb75c57bd1b2733cd225256dba73825ab54e82a46f8db1efd4"
-    sha256 cellar: :any_skip_relocation, big_sur:        "454aca2431d3185e1179630b7b71700c22cfd4dfb838cf1b029ddcf3bed90838"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2c874f61a5c459aeeb973459ebd3551cb5092b105e54d8050118a1869d98b0f3"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "23e9af057f5e372caeaa3cf80e6debe7252a7ebbf309551a747d40cd3cb7970f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b69733a407c7e16462eef2ef65f1a9353404f19c556cbbe94917077dd5778460"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "46f23b21ca702192566d6eeed009afdb225da561593b8331af766918a29e4ea9"
+    sha256 cellar: :any_skip_relocation, ventura:        "c699d6c2234b090722376da1e7de7fd8afcffa1d08a42c47274e38fc898fbeb9"
+    sha256 cellar: :any_skip_relocation, monterey:       "68fc3ea98ca7a26bcb67942b0a46a3564bdd79d94f86f5bc3258b5bdcbcf2996"
+    sha256 cellar: :any_skip_relocation, big_sur:        "76e0855d57e840463d7bb8e0b9d003763cc7c05611772d0bfc74c9ea410af402"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b7d12eb4cfefaa68a6792b121c70241185e88e7d041a0e263c669057daa281f5"
   end
 
   # https://github.com/grafana/loki/issues/8399
