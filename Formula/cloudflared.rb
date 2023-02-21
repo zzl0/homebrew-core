aa3bd6b31566c9c47ea717c7d2585434eea331fc
Patrick Linnane <patrick@linnane.io>
2023-02-21 22:41:29 +0000

    cloudflared: update 2023.2.1 bottle.

--- Formula/cloudflared.rb
+++ Formula/cloudflared.rb
@@ -7,13 +7,14 @@ class Cloudflared < Formula
   head "https://github.com/cloudflare/cloudflared.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "36297c8f88d365677b7e7079455660fc64f7c5d8f4b21a38f23f2cb2c550f0fd"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "2a43a59f05769042bf5ad2c75a917ab02bdc1b8b2cd3de55ca3bd90331e58a8d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "575e0c2f2f7648df54d78b0d31824e32b16152222b870113068a5be5c7c61aa6"
-    sha256 cellar: :any_skip_relocation, ventura:        "ef825ce850859727a56f2a82c5da799b7eb3fbaa33db99b07739c4624978fbec"
-    sha256 cellar: :any_skip_relocation, monterey:       "5eca8c8482831338d8a12cb1ee6cfb752fa29a29e41ccd7e0a3b515bc7707c28"
-    sha256 cellar: :any_skip_relocation, big_sur:        "e0d11ee88256cc3c6a9557f0196c65f0ef4b5be720d11632ed1ec17da8cef47d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cf2e729603eb85d8274d5504b58d3aa13fec98244e5a3e0bdcf5b231a0cf713f"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "cb97844bdc3edc7ed96cbdf1d1fc1355e045c3bebb275266bbabf77a873f3caf"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "dcd537c9734e752786c676f238f4aa3f0a9941c7ac3f9ffbd950a62df0bd6b17"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "40d5b2d2dd8db389408c161ee601254e24e0786af0552812a4e1980725224808"
+    sha256 cellar: :any_skip_relocation, ventura:        "f4dce8ca761d4b05af33638e4e4728295b95b8d4b993e5b86a2e0a1cee9c1087"
+    sha256 cellar: :any_skip_relocation, monterey:       "cf28d662c52274d8e499465fe12de831bd7d4cc590b55a0e175d00cc55f61b6f"
+    sha256 cellar: :any_skip_relocation, big_sur:        "09e451f83cd0c270b55e47edc4ce404a0681da060699a7a78013056094db1754"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "24c6a1d7a07c3a5efa7be8ddb87cf3fb44638f2bb855197893c874d4f02620d1"
   end
 
   # https://github.com/cloudflare/cloudflared/issues/888
