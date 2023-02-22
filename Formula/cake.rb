82089f591e918106598c7568ce21686d2f9872e8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-22 04:13:26 +0000

    cake: update 3.0.0 bottle.

--- Formula/cake.rb
+++ Formula/cake.rb
@@ -6,7 +6,13 @@ class Cake < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "07296553a62cd35f5c25ab24cd8a36179db132c965ed5c2afd30853aa2313e51"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "cbb05c3469ebbc09371e1d718dfb778200f703fc1c53bc396f89548ffc7cd548"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "461c9577cc5c6259300acb11754c3a7ac96d93dbe138046d2ae278fe7ce23d4b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "23c07c321306938f0f2646eda74581b0760eea2bcc442049f25edbcfcc984830"
+    sha256 cellar: :any_skip_relocation, ventura:        "09047ccfdc00726c3fa3dd218907ee21d835b6d4b23f625b5da8e5bf2fd11556"
+    sha256 cellar: :any_skip_relocation, monterey:       "ff1d7318bcc39206aea75063be39cbf2b924da3e2be8ea21834312ded7ecae1c"
+    sha256 cellar: :any_skip_relocation, big_sur:        "fa889b18b3ce98e02e27a2ab5a2a7ced8a420a6ef7d7490dab02449929732812"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e3fda2e3c37cbad6ae0e407291e5447857dbc36359500be831b34b19a673679c"
   end
 
   depends_on "dotnet"
