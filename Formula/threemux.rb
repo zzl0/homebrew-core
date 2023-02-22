d4386f3f23cc952b1e2c515008efe4e0cb8f5778
Michael Cho <cho-m@tuta.io>
2023-02-21 19:11:42 -0800

    threemux: migrate to `go`

--- Formula/threemux.rb
+++ Formula/threemux.rb
@@ -18,8 +18,7 @@ class Threemux < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "33200f3fd9175837129386fdfa67575eebacc806d9da099d98b0888aaff56124"
   end
 
-  # Bump to 1.19 on the next release, if possible.
-  depends_on "go@1.18" => :build
+  depends_on "go" => :build
 
   def install
     system "go", "build", *std_go_args, "-o", bin/"3mux"
