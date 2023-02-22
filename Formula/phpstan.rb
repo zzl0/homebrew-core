8bb31033ccb4c8262a531a83b686c77c79ca4fc2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-22 02:36:50 +0000

    phpstan: update 1.10.1 bottle.

--- Formula/phpstan.rb
+++ Formula/phpstan.rb
@@ -6,13 +6,13 @@ class Phpstan < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "fe38d5eda7b7a8b979abff4bfd3e970a043ef6ca6671f2154a579a9e942acb27"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "fe38d5eda7b7a8b979abff4bfd3e970a043ef6ca6671f2154a579a9e942acb27"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fe38d5eda7b7a8b979abff4bfd3e970a043ef6ca6671f2154a579a9e942acb27"
-    sha256 cellar: :any_skip_relocation, ventura:        "1d6886841e332d9c847ccffbb6bdadeb2ae5d75d47c90a81aea3f1634208aef8"
-    sha256 cellar: :any_skip_relocation, monterey:       "1d6886841e332d9c847ccffbb6bdadeb2ae5d75d47c90a81aea3f1634208aef8"
-    sha256 cellar: :any_skip_relocation, big_sur:        "1d6886841e332d9c847ccffbb6bdadeb2ae5d75d47c90a81aea3f1634208aef8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fe38d5eda7b7a8b979abff4bfd3e970a043ef6ca6671f2154a579a9e942acb27"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "6317f19cb6a9b36a4f4e759c8c182b595e7bb8534b47840270e0e095009cb44b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "6317f19cb6a9b36a4f4e759c8c182b595e7bb8534b47840270e0e095009cb44b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6317f19cb6a9b36a4f4e759c8c182b595e7bb8534b47840270e0e095009cb44b"
+    sha256 cellar: :any_skip_relocation, ventura:        "8283ee3d79d01bd399f2c6033930738b364b984af081696e0c652d19ddb171e6"
+    sha256 cellar: :any_skip_relocation, monterey:       "8283ee3d79d01bd399f2c6033930738b364b984af081696e0c652d19ddb171e6"
+    sha256 cellar: :any_skip_relocation, big_sur:        "8283ee3d79d01bd399f2c6033930738b364b984af081696e0c652d19ddb171e6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6317f19cb6a9b36a4f4e759c8c182b595e7bb8534b47840270e0e095009cb44b"
   end
 
   depends_on "php" => :test
