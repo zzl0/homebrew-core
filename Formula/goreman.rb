6089f17d0b3648cfaf6d127be7606b6e5c21b815
Caleb Xu <calebcenter@live.com>
2023-02-22 08:28:54 -0500

    goreman: use std_go_args

--- Formula/goreman.rb
+++ Formula/goreman.rb
@@ -25,7 +25,7 @@ class Goreman < Formula
   depends_on "go" => :build
 
   def install
-    system "go", "build", "-ldflags", "-s -w", "-trimpath", "-o", bin/"goreman"
+    system "go", "build", *std_go_args(ldflags: "-s -w")
   end
 
   test do
