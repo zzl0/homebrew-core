a05d49ee7fc0cc434b3c67c71f4387c58acf0cdf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-22 09:12:40 +0000

    fastfetch: update 1.10.2 bottle.

--- Formula/fastfetch.rb
+++ Formula/fastfetch.rb
@@ -7,13 +7,13 @@ class Fastfetch < Formula
   head "https://github.com/LinusDierheimer/fastfetch.git", branch: "dev"
 
   bottle do
-    sha256 arm64_ventura:  "79cffd18a5efb891cf52448d30744b5c1da9d62ec1b266de50026db66d157786"
-    sha256 arm64_monterey: "287fc2200dadf5543fe3aa85b64f5e21e3e98730befbad76c05da442f8c1f143"
-    sha256 arm64_big_sur:  "45906d10c7942c164bcae699eed615368a88b083ac01b6cb26a1fcd4931b141b"
-    sha256 ventura:        "adc1892a507ee706ad51fc55b869f34cc51419a27a6e8c02d67a1848d3af6172"
-    sha256 monterey:       "380019f80a806b1d4b32ede394099c59d4c43d618693e478a5ac2a779a297fcf"
-    sha256 big_sur:        "aa72c9d126369f65ab162663764d810c4841008ab0a017e6c408b637d142380e"
-    sha256 x86_64_linux:   "8bb712471c70ad058a4dea142aeda071e0340b29975381cccf1aac5e8a7a934c"
+    sha256 arm64_ventura:  "724b46d0f89170d9a4e7a65df623d1e62588d1863095dc8187825ce00c1e505d"
+    sha256 arm64_monterey: "dd71cf06148cc21eef50503e3b249ab29f61629b55bfae2933e8b8934ac21f98"
+    sha256 arm64_big_sur:  "1351221109d8c03ac28b02fb8615d74001269bf85697354996f26b054a4b56a3"
+    sha256 ventura:        "ca846985063531ef862220a0afde1dea975d40600f044b9977a7b14dd2fc1ffc"
+    sha256 monterey:       "82cdc0561fc2437bfba1a976137b570a05e622cdaffedfe6134595bf9f46a18e"
+    sha256 big_sur:        "5a7da362cef12def52dda1b1baed1022e34e8f01e9eaf8aee5fcf292a50cb2d9"
+    sha256 x86_64_linux:   "248429ecf7ab438951620455dfac06891ecc1595d6ed1e702b3dd04c53c8df07"
   end
 
   depends_on "chafa" => :build
