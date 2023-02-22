953112225c8c0c1ebae3cccdd02596f79b73f6e3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-22 12:14:03 +0000

    lefthook: update 1.3.0 bottle.

--- Formula/lefthook.rb
+++ Formula/lefthook.rb
@@ -7,13 +7,13 @@ class Lefthook < Formula
   head "https://github.com/evilmartians/lefthook.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "e0565208b037380ed5f38ad1a7893c7e3dd65c455b56a17012d51be63c280d6b"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "df943d0bddec1fa9017eb2c8a10e016afb34995f1eae9933ff8d7196fefcf992"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1eacb82e0246504f106a6f31eb1c9dfa72eb44c6072f9cfda74c72a1eac1d0cc"
-    sha256 cellar: :any_skip_relocation, ventura:        "a8e1e6d07eeac04aaf3a8049ccf1d16aaa5db93e5f1a9f568e1381177956ff5d"
-    sha256 cellar: :any_skip_relocation, monterey:       "a9f47e7aed62c269287708becb2dc5ba2381afff3a47ef425afe43e3dee9f026"
-    sha256 cellar: :any_skip_relocation, big_sur:        "01d319d5fb0076f0333a51c854a568bcc032ff689206be9527af489379fb8986"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d8672c9b7775598be2f88aee8381109ebf8d23c0dcf2b9d75a46597891fcddcf"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "dece5967844b1df89607144cff651b738efc132618a00cb684fac092cd14e6d3"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "dece5967844b1df89607144cff651b738efc132618a00cb684fac092cd14e6d3"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "dece5967844b1df89607144cff651b738efc132618a00cb684fac092cd14e6d3"
+    sha256 cellar: :any_skip_relocation, ventura:        "628ab1d9a0256995668e6607a9e3646d691d652d5403f5c6596d459711ea6916"
+    sha256 cellar: :any_skip_relocation, monterey:       "628ab1d9a0256995668e6607a9e3646d691d652d5403f5c6596d459711ea6916"
+    sha256 cellar: :any_skip_relocation, big_sur:        "628ab1d9a0256995668e6607a9e3646d691d652d5403f5c6596d459711ea6916"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "73602909c5607391409bfbd9df405716f5b2453e48f130b50d9272c9cfe1f93a"
   end
 
   depends_on "go" => :build
