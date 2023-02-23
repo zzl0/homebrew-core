d6fdf3c674e654b3b27bde7e4c38c777b6c9c978
Rui Chen <rui@chenrui.dev>
2023-02-23 16:20:14 +0000

    zls: add 0.10.0 bottle.

--- Formula/zls.rb
+++ Formula/zls.rb
@@ -7,6 +7,16 @@ class Zls < Formula
   license "MIT"
   head "https://github.com/zigtools/zls.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "57e41a00eacea3a15f363470b5a64f6da2a7002cd69692df83910cbeee3bbe0b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "7f7686c0ea862ad5ab3ae4431dafadf9a28951bb7854e7e7c02c359d63caf0dd"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c75a6b48d33d6b129b020c0e227f6df7107dcbbf9a296d74f52397d8b2234836"
+    sha256 cellar: :any_skip_relocation, ventura:        "ba4279657dc4293a8261202ba7553a3f72254556e7b32b0c49586090f0ccb9af"
+    sha256 cellar: :any_skip_relocation, monterey:       "035e8403b2a33986d35251e49a1db2e18f1892bd1a3f029e069e44179537d5ca"
+    sha256 cellar: :any_skip_relocation, big_sur:        "8883d3da3be2111f3b1c569027d6454ac627891de95d3268303275f995473ff3"
+    sha256                               x86_64_linux:   "96c162fc09934028b9d3e2f10703e1f2f23c4ce8c9737b5e62fc4b54805f075e"
+  end
+
   depends_on "zig"
 
   def install
