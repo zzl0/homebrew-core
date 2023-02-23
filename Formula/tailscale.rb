76570acc6aa1d0befe7a74fbbdeeea280dacf666
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-23 02:15:28 +0000

    tailscale: update 1.36.2 bottle.

--- Formula/tailscale.rb
+++ Formula/tailscale.rb
@@ -7,13 +7,13 @@ class Tailscale < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "9a2476ad744b07f38718c06f734f84709ffe663fe3c34d0f0afb8c003b9dc980"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "4b6f7418c54225579fde65f26ce4302480291c2444c9407a43168b889b2433a4"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "54e1c76b924df28fa2d423e5ec6a9c7e810420005c4a35fab6f3c440653f4948"
-    sha256 cellar: :any_skip_relocation, ventura:        "5fc43586839dc91a7ab2cf8555e218e22a6e454b1f72650038a86a558e7ca818"
-    sha256 cellar: :any_skip_relocation, monterey:       "367b9dbacf0d7e00ec7bc532f0477816997e7c8a97e82f064be9fb2b50d37cd1"
-    sha256 cellar: :any_skip_relocation, big_sur:        "a0ba4eca00f07d270cd80e13ba8a4d20e3fc08e2356b4d76ea690decbad9adb9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "008fa2f3e98267c417b3ac75ab523c0d46d5813a6f39acedde76e32664987bbd"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "69bb17b4f1f4ab70c270a21698cb458929092ff269e88d76a3c95e3563931340"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "69bb17b4f1f4ab70c270a21698cb458929092ff269e88d76a3c95e3563931340"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "69bb17b4f1f4ab70c270a21698cb458929092ff269e88d76a3c95e3563931340"
+    sha256 cellar: :any_skip_relocation, ventura:        "beffdde494f7e901e1627ee72ee761b4dd2ccaabe07f8f1b3163522a43e615d1"
+    sha256 cellar: :any_skip_relocation, monterey:       "beffdde494f7e901e1627ee72ee761b4dd2ccaabe07f8f1b3163522a43e615d1"
+    sha256 cellar: :any_skip_relocation, big_sur:        "beffdde494f7e901e1627ee72ee761b4dd2ccaabe07f8f1b3163522a43e615d1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0e485a0c7912cb2cae140cffe4e9ea80ad1be218d630851b1f86096810c2a26b"
   end
 
   depends_on "go" => :build
