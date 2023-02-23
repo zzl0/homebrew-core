ea26916b48bf06859cbb869305847792ae71df11
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-23 23:12:26 +0000

    faas-cli: update 0.16.0 bottle.

--- Formula/faas-cli.rb
+++ Formula/faas-cli.rb
@@ -13,13 +13,13 @@ class FaasCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "7123d6ad4efdd2f57b9d122f8b21d8c892ae5b0e80fa15b39f9a4203688291bc"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e33551845aa2660d88dd0079162c0033e41238eb22e829e7a1f7fdb5cde09a40"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "93fae6db6da734a250d187b2fb240be2198b01b7a8aca49691ea61e2a1620465"
-    sha256 cellar: :any_skip_relocation, ventura:        "e68c5da1c695e0236f4ea4011695502e0accbd1e4b6c070a6886eab34c63456f"
-    sha256 cellar: :any_skip_relocation, monterey:       "8b6483b946dbd965706ac91de363dbeb5d3dbe5b6baf5d15b2815f9d592d6027"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b414cb8cd590cb04d94e8a7b2092fd49001b2ace35951a62c55e96b7c8719f67"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b9e5caeda50b936f1fb4b71e115d789bd05817d79f25c4738d615b1b64690f56"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "1bf9b057d22c92dcc52e8436323074f38c65a8fe8d9e812dae8b290c629e9eee"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "1bf9b057d22c92dcc52e8436323074f38c65a8fe8d9e812dae8b290c629e9eee"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1bf9b057d22c92dcc52e8436323074f38c65a8fe8d9e812dae8b290c629e9eee"
+    sha256 cellar: :any_skip_relocation, ventura:        "8d831772a74733c5d5cbb193795cb4a581eb1602a45938cca4edb4ba3448d6f3"
+    sha256 cellar: :any_skip_relocation, monterey:       "8d831772a74733c5d5cbb193795cb4a581eb1602a45938cca4edb4ba3448d6f3"
+    sha256 cellar: :any_skip_relocation, big_sur:        "8d831772a74733c5d5cbb193795cb4a581eb1602a45938cca4edb4ba3448d6f3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b9ee2d601546a5218dc2a5c3f1564b9014ef94d4cbc38bec613f27fb848049ff"
   end
 
   depends_on "go" => :build
