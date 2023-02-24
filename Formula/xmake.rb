220ede528ffe8ff6e2ee4c18034a1c698c171b03
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-24 19:13:50 +0000

    xmake: update 2.7.7 bottle.

--- Formula/xmake.rb
+++ Formula/xmake.rb
@@ -7,13 +7,13 @@ class Xmake < Formula
   head "https://github.com/xmake-io/xmake.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8185d7ecaca3829491c6b707d23ac37ff763de2e21c92681600229c01a2d6338"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "506a82530a7cff0217d6fab0a8e378502e9c56a040402152787906886c9d92da"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a8866311fc2b085199cd46946801ff942ebc9c712c1e37f49b1d2ac779e94cc4"
-    sha256 cellar: :any_skip_relocation, ventura:        "9795ba1a21b591fc47e9cf6764a342d47c0106ed4f052342598747b4bd9a8eee"
-    sha256 cellar: :any_skip_relocation, monterey:       "0bc50c40cc1c17f4049c69ae8b463ba5ef230cb8347a34d60bd8bcf973d1f784"
-    sha256 cellar: :any_skip_relocation, big_sur:        "882fc7e3f8bcb217156b2f54dcfc548bad96e608cbcee14efdc20c134abd3aa4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b333e94c8ef4da5c3c170973c83b0c79fca68b8240d77d233f9d6d312d9327e6"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "6f6e043545b1139b92e822c461c3ed9711bdec3e00371fdd7e6944936e659f88"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "4b4a084dfa37d6cc2c5c61a9b00e6bac41adfc4d8491a01f6ce393830f08de1a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4a1cdd678a8e80f89c4136875289c22be582fc24a0aeef198ff18245320ea5d1"
+    sha256 cellar: :any_skip_relocation, ventura:        "01014f446bb29957997effd9b22161195e9d8aa5e60ea9fc0695600d6533c975"
+    sha256 cellar: :any_skip_relocation, monterey:       "a3d9445d1c8bf1257c39a1d3d69a066ce8c6d23aca364f8c72145df036d602ae"
+    sha256 cellar: :any_skip_relocation, big_sur:        "af9f347abd4f5657dd4d85738985bb94ea028811bef8894c62c2f9f43bc5fba6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1e8aa1a524da6d94bafd387c68922ef9b5b7b621a282f757a55005cbf1c33c18"
   end
 
   on_linux do
