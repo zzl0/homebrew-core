dd1961a509d54d6fb95cbb6e170d065eb902d97b
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-02-25 01:14:52 +0800

    arkade 0.9.2
    Closes #124146.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/arkade.rb
+++ Formula/arkade.rb
@@ -2,8 +2,8 @@ class Arkade < Formula
   desc "Open Source Kubernetes Marketplace"
   homepage "https://blog.alexellis.io/kubernetes-marketplace-two-year-update/"
   url "https://github.com/alexellis/arkade.git",
-      tag:      "0.9.0",
-      revision: "57c87307e2423a85c4515c059c9c0c99ebc3a0ca"
+      tag:      "0.9.2",
+      revision: "302ea08874bf21196930a9938e9f3b2d6f1d3fa7"
   license "MIT"
 
   livecheck do
