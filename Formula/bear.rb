b6ea9824bd53a130fb192e2e3f0c5c94ff3c2672
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2023-02-24 07:51:26 +0000

    bear: update 3.1.1_1 bottle.

--- Formula/bear.rb
+++ Formula/bear.rb
@@ -8,13 +8,13 @@ class Bear < Formula
   head "https://github.com/rizsotto/Bear.git", branch: "master"
 
   bottle do
-    sha256 arm64_ventura:  "d0d3c23f3823f1c40f7fadd5d0ca596007a392b59a52fbf35c4af734e01934d2"
-    sha256 arm64_monterey: "8b01bc8d9e431ad71e4a667f9c51d2711257a02442a8070db308c5fc925e3e51"
-    sha256 arm64_big_sur:  "4bea459179bf5e61b668d3aeac58b30940fc07bfa2f8ea857382cfc66eaaf183"
-    sha256 ventura:        "e04ffc67e6b9cd1a2f0bceea1d80a89de68be618b3645c31c74f3d8304d5afdf"
-    sha256 monterey:       "8715121ee27e85d59a71dee9c9b08f10474051fe399a9380315b2a2d3028483d"
-    sha256 big_sur:        "e5a0667606f8c81aada3efe8b071c9d745e87b4b0a89841e0940cffc9c7bc39e"
-    sha256 x86_64_linux:   "d99ddfff4ef147053da6b1cc8fb4d04d62df451a9511c18ecbe276021e4f856a"
+    sha256 arm64_ventura:  "0ce5a926327fb07e48533deac8d46bf715631090b0ff2019eaf3ecc26b617d36"
+    sha256 arm64_monterey: "1f93e7b42fc1de65b2783b97d610bb1f746537da322f53bc34ea28a85297e4ff"
+    sha256 arm64_big_sur:  "10db9c8ddd3d958e442c03b582b6b8c9538da0c7584254dda8ef5febb0c446ee"
+    sha256 ventura:        "0e247e8e1e83a43ae251e107d474ed9ff22d103bd4f1692bcaf88d68f001b909"
+    sha256 monterey:       "b2b439eeeabb2ae7efb6449de0c8ec6e488706e8fd233bc01e94333993bed859"
+    sha256 big_sur:        "1af8d201342e2ff72d6980287ca07d6224b22902e215a60b505c737a9e879df7"
+    sha256 x86_64_linux:   "45376b1a30f8bcba3f632715ab381cd18ac4be9ec9878453581ae4415b6dca72"
   end
 
   depends_on "cmake" => :build
