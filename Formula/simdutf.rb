fb400c6a6f74e99f02d1a725c1d6f45a8c4fad47
Patrick Linnane <patrick@linnane.io>
2023-02-24 14:06:00 -0800

    simdutf 3.2.2
    Closes #124086.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/simdutf.rb
+++ Formula/simdutf.rb
@@ -1,8 +1,8 @@
 class Simdutf < Formula
   desc "Unicode conversion routines, fast"
   homepage "https://github.com/simdutf/simdutf"
-  url "https://github.com/simdutf/simdutf/archive/refs/tags/v3.2.0.tar.gz"
-  sha256 "0d9f63e2f308b6b54f399ebbe3a02776b902a2670c88c28de2d75ea2197dc4e9"
+  url "https://github.com/simdutf/simdutf/archive/refs/tags/v3.2.2.tar.gz"
+  sha256 "5a5c84c05bf30d681126d1dcbde903615f2c927e201e0c6d489f74a91b7f506f"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/simdutf/simdutf.git", branch: "master"
 
