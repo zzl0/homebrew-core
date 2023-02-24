dd7cb5ae3745d19547e7def1b6676162ff28f5a4
Sean Molenaar <smillerdev@me.com>
2023-02-24 09:26:42 +0100

    raylib: update checksum
    Closes #124121.
    
    Signed-off-by: Ruoyu Zhong <zhongruoyu@outlook.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/raylib.rb
+++ Formula/raylib.rb
@@ -2,7 +2,7 @@ class Raylib < Formula
   desc "Simple and easy-to-use library to learn videogames programming"
   homepage "https://www.raylib.com/"
   url "https://github.com/raysan5/raylib/archive/4.2.0.tar.gz"
-  sha256 "5fd2e7d825f345924800c38291f44423bcbcc036bd72fa2e3abedfc54e5b68b5"
+  sha256 "676217604a5830cb4aa31e0ede0e4233c942e2fc5c206691bded58ebcd82a590"
   license "Zlib"
   head "https://github.com/raysan5/raylib.git", branch: "master"
 
@@ -28,20 +28,22 @@ class Raylib < Formula
   end
 
   def install
-    system "cmake", ".", "-DBUILD_SHARED_LIBS=ON",
-                         "-DMACOS_FATLIB=OFF",
-                         "-DBUILD_EXAMPLES=OFF",
-                         "-DBUILD_GAMES=OFF",
-                         *std_cmake_args
-    system "make", "install"
-    system "make", "clean"
-    system "cmake", ".", "-DBUILD_SHARED_LIBS=OFF",
-                         "-DMACOS_FATLIB=OFF",
-                         "-DBUILD_EXAMPLES=OFF",
-                         "-DBUILD_GAMES=OFF",
-                         *std_cmake_args
-    system "make"
-    lib.install "raylib/libraylib.a"
+    system "cmake", "-S", ".", "-B", "build",
+                               "-DBUILD_SHARED_LIBS=ON",
+                               "-DMACOS_FATLIB=OFF",
+                               "-DBUILD_EXAMPLES=OFF",
+                               "-DBUILD_GAMES=OFF",
+                               *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+    system "cmake", "-S", ".", "-B", "build-static",
+                               "-DBUILD_SHARED_LIBS=OFF",
+                               "-DMACOS_FATLIB=OFF",
+                               "-DBUILD_EXAMPLES=OFF",
+                               "-DBUILD_GAMES=OFF",
+                               *std_cmake_args
+    system "cmake", "--build", "build-static"
+    lib.install "build-static/raylib/libraylib.a"
   end
 
   test do
