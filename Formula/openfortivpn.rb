fc969eac22a6c4a01826b3308e6d676d444efd3e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-25 19:13:35 +0000

    openfortivpn: update 1.20.1 bottle.

--- Formula/openfortivpn.rb
+++ Formula/openfortivpn.rb
@@ -6,14 +6,13 @@ class Openfortivpn < Formula
   license "GPL-3.0-or-later" => { with: "openvpn-openssl-exception" }
 
   bottle do
-    sha256 arm64_ventura:  "cf3085228d1c1d791e69d60fc848b4758ff224359f3dedd46d7aa8d149fa1936"
-    sha256 arm64_monterey: "2f65a3ce47bfa55079ece31bf270b4111ddd9ac81ac742a8243afe1c6600e932"
-    sha256 arm64_big_sur:  "7e130cbe9a544dbd3d447d455399ef8cef88d31b0e81e8a8e0713b2a469f67a2"
-    sha256 ventura:        "c06e9f31eda964a6622822a8a1b0178c73c9eb38ceae9dbefdf6994558a1c454"
-    sha256 monterey:       "ff5c82ec39545a45ca3676cca127c699e438e0eb2d0972cf98bddab8994a9c6e"
-    sha256 big_sur:        "36df758b6d887467cd0e90f704d0b603f5486ac6f5375d845bb391cdbb9afdfb"
-    sha256 catalina:       "4a09b2f6e762b7eac8e5e8ce0ca2997e1764439c3af6e5b54aa019c532c45e2b"
-    sha256 x86_64_linux:   "66bd0b3ebcaea501bda4aca5664ec71503b6930c24fdc2300f0ab96094438f7b"
+    sha256 arm64_ventura:  "1e20140f29ff6464bab208319a6e354b5f901e8f606e2eaa15a038e14858e35d"
+    sha256 arm64_monterey: "998fdd38c2a49f71032db7080f903145bb935d2d208bfce33c9169a2209554c3"
+    sha256 arm64_big_sur:  "6fadba8b3269fdc7858878aeb6964a5de898cc2b37481a5a0ccb7e90ca2a7fff"
+    sha256 ventura:        "8c665659c92748f15ccb99ac0996d3d2606dd28db7743cb93529b72793b9dc0b"
+    sha256 monterey:       "7726a275dc6cdf004296fdf9e7154b3fc6bbd26682ce6774f2bbaeee93bfc850"
+    sha256 big_sur:        "7f40301f138068b60a05bc991cb69707ba9183ea16ce04b0daa3e92c9cd1354d"
+    sha256 x86_64_linux:   "1d0a65ed65ba9ce156414f7ee08998fa69ae34d2488f15694fd4edbda5d898fc"
   end
 
   depends_on "autoconf" => :build
