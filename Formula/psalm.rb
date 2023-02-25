b20ee3bf76efba71371edc4b0f5ba31768cfa9a8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-25 07:13:14 +0000

    psalm: update 5.7.7 bottle.

--- Formula/psalm.rb
+++ Formula/psalm.rb
@@ -6,13 +6,13 @@ class Psalm < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "40b94ca76c8d7f90998861a1c2cce22cb9862296d7427d4509406a907141f569"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "40b94ca76c8d7f90998861a1c2cce22cb9862296d7427d4509406a907141f569"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "40b94ca76c8d7f90998861a1c2cce22cb9862296d7427d4509406a907141f569"
-    sha256 cellar: :any_skip_relocation, ventura:        "d43129d6fcf14f4058870501e78df89ed65f4b440bfc7b4b13d6bfa9552d5617"
-    sha256 cellar: :any_skip_relocation, monterey:       "d43129d6fcf14f4058870501e78df89ed65f4b440bfc7b4b13d6bfa9552d5617"
-    sha256 cellar: :any_skip_relocation, big_sur:        "d43129d6fcf14f4058870501e78df89ed65f4b440bfc7b4b13d6bfa9552d5617"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "40b94ca76c8d7f90998861a1c2cce22cb9862296d7427d4509406a907141f569"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0dd4415b852350f882d3908580dabf59bb872284a10eee7ab03389c7b19960aa"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "0dd4415b852350f882d3908580dabf59bb872284a10eee7ab03389c7b19960aa"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0dd4415b852350f882d3908580dabf59bb872284a10eee7ab03389c7b19960aa"
+    sha256 cellar: :any_skip_relocation, ventura:        "3fd4e1b2843e3f18ea75d288abc32d1876727bc767231c43c65dd1758bcd2ff5"
+    sha256 cellar: :any_skip_relocation, monterey:       "3fd4e1b2843e3f18ea75d288abc32d1876727bc767231c43c65dd1758bcd2ff5"
+    sha256 cellar: :any_skip_relocation, big_sur:        "3fd4e1b2843e3f18ea75d288abc32d1876727bc767231c43c65dd1758bcd2ff5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0dd4415b852350f882d3908580dabf59bb872284a10eee7ab03389c7b19960aa"
   end
 
   depends_on "composer" => :test
