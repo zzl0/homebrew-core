393d63227947b4c18ffe32d44320973fc6a33fb7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-25 00:25:07 +0000

    qxmpp: update 1.5.2 bottle.

--- Formula/qxmpp.rb
+++ Formula/qxmpp.rb
@@ -6,13 +6,13 @@ class Qxmpp < Formula
   license "LGPL-2.1-or-later"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "41ee2cc8d1ce336ab3ae51f6341b65615bf69fdff1fbc437079740180fb56388"
-    sha256 cellar: :any,                 arm64_monterey: "7608654ff68ea5e85f247fe6b7a371da0253dabd4a5b52e77a5e39c349ac78cd"
-    sha256 cellar: :any,                 arm64_big_sur:  "aa70f35f7bd948d48ad2f87979b59c09f0794ee980c3e5c712da9a8c84ddbf40"
-    sha256 cellar: :any,                 ventura:        "a35b8b8aba3007ec5bef729f7acc3da1cd88aad702b61210b01fdb15ab43e1a1"
-    sha256 cellar: :any,                 monterey:       "cabf6dfd56b1523f69d60cd6011408cde00a174558c94783649d05b2cf702ddd"
-    sha256 cellar: :any,                 big_sur:        "26e7c8faf89cc2302f7b67fbc632c2e93111089fb072acfe2cd872ffbf80706d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "02387de3793afccd0635bd8d77c19600a8d1ab8ce0e7d4cdaa6473f78e218917"
+    sha256 cellar: :any,                 arm64_ventura:  "5fb385b017377722f83eac85b8ed7b0e0d0f43385579b8094114cf54996151b7"
+    sha256 cellar: :any,                 arm64_monterey: "5f353ce355b053c61216d26bdbc3e5f4d7c9bd39ac5b210db96f536d205f61fc"
+    sha256 cellar: :any,                 arm64_big_sur:  "6dec127ef1d7bc9515126273ea265c903294a6bd3e798a38493f4cb3ffbb5871"
+    sha256 cellar: :any,                 ventura:        "81bc5a12af5bf7eebd5eaa19a5d403ac44ba11e36d7ea1fb63b6d996866f72e8"
+    sha256 cellar: :any,                 monterey:       "9a96061a0e5a481f89a77910ffb1e3266fcc6decb192db0418812c45769ad202"
+    sha256 cellar: :any,                 big_sur:        "ed7474fc00f6cf9a5bf874e6c4801e130af71308a5319ab2e12c593e15a0e51f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b352fb7533de5367c1b99f6191d7338f0e1759dd2335ef3dad755c1c8eac1c28"
   end
 
   depends_on "cmake" => :build
