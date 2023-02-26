eeec43a9b84f8ea4efc09dbf6b8c7ea9fbf80925
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-26 06:13:37 +0000

    octosql: update 0.12.1 bottle.

--- Formula/octosql.rb
+++ Formula/octosql.rb
@@ -7,14 +7,13 @@ class Octosql < Formula
   head "https://github.com/cube2222/octosql.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "52b4c13b613637d66e7d0443efe0040ac90442d1d684f8e8884fe6ceb4f56c33"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "6156175e4da8c8aadc75a8d0e0d58eae0c168f9980cdf885a0b2bfbd34c907c8"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fa962c981b327d7bebf33b92131ef1862dac2276a3611e74ec16f36af0b4d358"
-    sha256 cellar: :any_skip_relocation, ventura:        "877a3cd39081445e58f956c706e46f9cfb0b273f19647b080587b6f4ba32bac5"
-    sha256 cellar: :any_skip_relocation, monterey:       "19811afa112521f3bb76d0af5906399e8305349b134cc6af2dacb89aa7f2a7e3"
-    sha256 cellar: :any_skip_relocation, big_sur:        "184450555f85574e9978f781ef488013a77a4a30d12360fa1d40e4859492e78e"
-    sha256 cellar: :any_skip_relocation, catalina:       "d8da82a4f2204f4a3ab90ab1b35b7e14bdf1d72c230de1cc0e27b2c7cf8b131b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c666326c70572fbc67ff37e07483baec324ef7c2afdbf6b274f477c2a4874bb5"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "e5e82c0efe3f4a270645266a48e8e16931ecb28ec529e04329a08ba3b375f35a"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e5e82c0efe3f4a270645266a48e8e16931ecb28ec529e04329a08ba3b375f35a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e5e82c0efe3f4a270645266a48e8e16931ecb28ec529e04329a08ba3b375f35a"
+    sha256 cellar: :any_skip_relocation, ventura:        "c5e2e135184eaa7f0d9100b81481c101c7a47a6f8fa65ef28e406948ea2a9333"
+    sha256 cellar: :any_skip_relocation, monterey:       "c5e2e135184eaa7f0d9100b81481c101c7a47a6f8fa65ef28e406948ea2a9333"
+    sha256 cellar: :any_skip_relocation, big_sur:        "c5e2e135184eaa7f0d9100b81481c101c7a47a6f8fa65ef28e406948ea2a9333"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "86288065c44f12feabacda45e376cd2dbc3926b1e9cce8bdd0e145c7f8e23d93"
   end
 
   depends_on "go" => :build
