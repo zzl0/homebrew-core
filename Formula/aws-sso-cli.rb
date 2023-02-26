ad9f875d104d3a271f4ce0c42c8e3e0643715dd0
Aaron Turner <synfinatic@gmail.com>
2023-02-26 12:39:06 -0800

    aws-sso-cli 1.9.10 (new formula)
    * aws-sso-cli 1.9.9 (new formula)
    * Update Formula/aws-sso-cli.rb
      include build head support
    * aws-sso-cli: update build
    
    Closes #124280.
    
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: Aaron Turner <synfinatic@users.noreply.github.com>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/aws-sso-cli.rb
@@ -0,0 +1,22 @@
+class AwsSsoCli < Formula
+  desc "Securely manage AWS API credentials using AWS SSO"
+  homepage "https://github.com/synfinatic/aws-sso-cli"
+  url "https://github.com/synfinatic/aws-sso-cli.git",
+      tag:      "v1.9.10",
+      revision: "11ec65288f78702a5e80078c04e0ef8b4ac7cb67"
+  license "GPL-3.0-only"
+  head "https://github.com/synfinatic/aws-sso-cli.git", branch: "main"
+
+  depends_on "go" => :build
+  depends_on xcode: :build
+
+  def install
+    system "make", "install", "INSTALL_PREFIX=#{prefix}"
+  end
+
+  test do
+    assert_match "AWS SSO CLI Version #{version}", shell_output("#{bin}/aws-sso version")
+    assert_match "No AWS SSO providers have been configured.",
+        shell_output("#{bin}/aws-sso --config /dev/null 2>&1", 1)
+  end
+end
