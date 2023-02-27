a4af9aba5e4105a68959dfe7412e66c7749acdd8
Sean Molenaar <smillerdev@me.com>
2023-02-27 09:56:35 +0100

    vips: revision bump for libtiff 4.5.0

--- Formula/vips.rb
+++ Formula/vips.rb
@@ -4,6 +4,7 @@ class Vips < Formula
   url "https://github.com/libvips/libvips/releases/download/v8.14.2/vips-8.14.2.tar.xz"
   sha256 "27dad021f0835a5ab14e541d02abd41e4c3bd012d2196438df5a9e754984f7ce"
   license "LGPL-2.1-or-later"
+  revision 1
 
   livecheck do
     url :stable
