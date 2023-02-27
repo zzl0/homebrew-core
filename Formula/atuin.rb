528ca1fa97ee37b6d5496010742264e0c51297b3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-27 04:16:13 +0000

    atuin: update 13.0.0 bottle.

--- Formula/atuin.rb
+++ Formula/atuin.rb
@@ -6,15 +6,13 @@ class Atuin < Formula
   license "MIT"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "e7fff6ab64237cfd0296d657146c0ccac42f62ae25d135bebb5f0fbd2b841763"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "08ce3c57d76020baaaee7672c31aeab51ae753dfb2c42242ef1ca9bffc33df14"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "76b5bbebe4c2758f9c44cc86030279399d501c0a0ddee820900738cf45db3b65"
-    sha256 cellar: :any_skip_relocation, ventura:        "f633e62d06144cf42194fa59c960fc05de0401652f042c5892d787fc9a8be967"
-    sha256 cellar: :any_skip_relocation, monterey:       "56a9895ff2a37d67490851811422b180312975732e2f34e778249c2e4bfcbd9a"
-    sha256 cellar: :any_skip_relocation, big_sur:        "741867bcf7ed64988d3084a1583b5186a62f542d13bedde156feb31af338579c"
-    sha256 cellar: :any_skip_relocation, catalina:       "44805dd61ee6163c3ae4ee537a00168f28b0b616181e202a2808867b8d0e4d55"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bba7de6d1c3dd8a1e7ca6041f135056bccf8a268bd23ba6c3e215351ff87206b"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "344d9c78907e78da772d71301d88a74ad1ba044f7aea83bf6e0d346c7a3efd3b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "6947bf3d2fb7716c19a67d17402845b74e201bc4d9a06d08c8d44a44fb642ee1"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "241fab685d35619042a623ed27a531a3130cb6af85b711b6d9477540d6e76744"
+    sha256 cellar: :any_skip_relocation, ventura:        "fee00d3104646864a666ddcafced63cad827dd926a74f308c1dd0c14b3f83c3d"
+    sha256 cellar: :any_skip_relocation, monterey:       "a226a276a9131cff2631156042f7d90f727358bf2c0a18225f6694f0041adec1"
+    sha256 cellar: :any_skip_relocation, big_sur:        "20bd948fbbf52e400ac27e48294bd5256f644eb3865ab0be04ddbc3c958d0f38"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a8f3c131fc8ee08ec6465930734917f563263ff9626da017ef4146e79c93670b"
   end
 
   depends_on "rust" => :build
