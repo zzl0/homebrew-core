3b0c1bd191c728149a66399be11f896384ab37cd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-27 18:14:30 +0000

    dbmate: update 2.0.0 bottle.

--- Formula/dbmate.rb
+++ Formula/dbmate.rb
@@ -7,13 +7,13 @@ class Dbmate < Formula
   head "https://github.com/amacneil/dbmate.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "33d74783e02c3cfd5f622a860c82b9f1978b0e8c62b8e50a40f94734e29fed96"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "06193eff6dbb1b1ba16dd4702e73733e705669dce19ab91c97ba578804734b2a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5a508ba543c252c6ca5e32dc50642a12a6d17dc586235c4ba2d792a67208f441"
-    sha256 cellar: :any_skip_relocation, ventura:        "9faa92d12184c7379b09dc45214510f8df32f6bdc61b61f587c80d6c744f02bb"
-    sha256 cellar: :any_skip_relocation, monterey:       "4aa4c47ffdf7837c6cded893880a9c4cdf1c743e3f6a6bcc4cfcc6cb5260e5cf"
-    sha256 cellar: :any_skip_relocation, big_sur:        "173154e4f74ccb8e3101242edc0600958cce4ab787f5bd19d82711ad024c74ff"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "099604b3eb89224ccc2ec8092835016e15b5862ed9b2c3cc78eee9a440c5e298"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3169981a2fd0e46620022f2f354e37eefa8dae29774298d3a66ba1d4d49e4c0f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "711517cf6be5331e3e121277420fc10171829c3f230a60b6a1128c90cc3425d2"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "38c4f9b9a948738ba92c0d6c776c38af2f1bd0fc360eb8f407272d541873549d"
+    sha256 cellar: :any_skip_relocation, ventura:        "bfd3fbd5055533a8650a1ca9dc7cef0854637ec49120e626961fb29dae519d4c"
+    sha256 cellar: :any_skip_relocation, monterey:       "42e833a0b840bf724712952efd3b705d4f277b5442dd683aa015661ffcf6b9cb"
+    sha256 cellar: :any_skip_relocation, big_sur:        "861b7b2f4de10462cef163f6dd94ec708af1371f0d8e7674def28f52cebe1bad"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "56cb9995292dccbcec8d8d1087408372c68214658e352433bfcccb5d3e4f2aac"
   end
 
   depends_on "go" => :build
