30001db32937bd313968fa417c1055e980eb3965
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-27 20:13:18 +0000

    datafusion: update 19.0.0 bottle.

--- Formula/datafusion.rb
+++ Formula/datafusion.rb
@@ -7,13 +7,13 @@ class Datafusion < Formula
   head "https://github.com/apache/arrow-datafusion.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "ad3e1c806b727cba54bf29537171e29c7b30ed5fc7a13a4e9c03a6e820477c37"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "1690229454bc351d25077bf203a27de252a8299afd9125941327b9a29e81ce82"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "67e33c791c9a6f3acd51b3cd0274f80563c9f9acabc3b9e986f4efba652ab2fa"
-    sha256 cellar: :any_skip_relocation, ventura:        "5251f1a17071058099d4caead0946afb70e03716944dedf119058b779cc14825"
-    sha256 cellar: :any_skip_relocation, monterey:       "230b943cb7df92ca03dcaabcca73120bff105a383a6bfc9ea484a38e733a86b4"
-    sha256 cellar: :any_skip_relocation, big_sur:        "cd381c070f244481b0a2a0e6e95d95cbba846742cc09dfd8e48a8f2c099898fc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ea60ed0f29fd26a1d7e0eb8c1e47e2afe1c1ee6879fdec1cce3b0cd67055287f"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "d80ca56f89f2dc6b279705cf84f99ef94c9f6f587fff0b2c21b08f0d9cee483f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "41b965c7cd8142598614ae1d14443b24e01e9192449bd53db04a132e734d361d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "aef2ce1d1c00a73f2047923ed6ccb78da23916007b78af5b6714aeb46bc0fd37"
+    sha256 cellar: :any_skip_relocation, ventura:        "e577c4b3db71bc5aad5573a73d2c40e2e4ef81bdbaaf00af9da4dbe257a3c2c6"
+    sha256 cellar: :any_skip_relocation, monterey:       "10155987c2c4cac6e07a50954529d6f1df829f84a6a54a386374ddf20c3c82c4"
+    sha256 cellar: :any_skip_relocation, big_sur:        "309cbec331f1d570cb35e0027df479d4d4c7b51c09254476923ca948e5dc0a06"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b0fbe997195922dd82c9bf799f9253d3a2f54b7b6e5c023559e08bc39414eb60"
   end
 
   depends_on "rust" => :build
