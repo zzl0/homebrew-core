4bcfd292c98fb71be9e6372e95e68e58a7c3e514
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-27 20:13:39 +0000

    ponyc: update 0.54.0 bottle.

--- Formula/ponyc.rb
+++ Formula/ponyc.rb
@@ -7,13 +7,13 @@ class Ponyc < Formula
   license "BSD-2-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "4f99db7771287ed73885b3bce1c92676b3cb91fe255de9ff70279d293dd18577"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c580b8fa85f41b5ba3a037d2042a7541c05d00b8b0f64f41ada7c63ef31aec1d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b5ba3b91679d59f714b9e6bdb2879870a7274f501655a0560ae84486e13ab072"
-    sha256 cellar: :any_skip_relocation, ventura:        "a423addcc6f8801d40ac44352dac0413d4db435fb0b55a770066390a224233e4"
-    sha256 cellar: :any_skip_relocation, monterey:       "72c16360876671eba47702d8099992e1579b3db03910552d8d49e2dfec651ef3"
-    sha256 cellar: :any_skip_relocation, big_sur:        "5ac7a090a381490925785b4dfe6eda4aab78a58e0d0f6d6df006365bca74d4d6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3077f5dd95968426f35fe6f58411bfc34a0f54816e1c4424a40b6bb7ca93bb16"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "7d8ce728f68182f4816c677a938bf0c83cd9494106a6fd2ab0d1eb3d8dfb544a"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "9c9abac35735cb2ad32c7cf1508a40fb5c3cd6da73e0f577e04b9972217bf0cb"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "dd048d64293e528c89ee01fed4d5bc9f92df6d29bff8464eb44d4c9a94eff5d1"
+    sha256 cellar: :any_skip_relocation, ventura:        "b37a538c0bfd5aa5196627b00bbdf6bde1776c18f882d398f0379b829f867cf9"
+    sha256 cellar: :any_skip_relocation, monterey:       "74d080e5415303f411b3d0f7dc59163ab08f930915c60d34a9f353dc659295b6"
+    sha256 cellar: :any_skip_relocation, big_sur:        "fce57c062870c9e1a380d9685c986b9c77f5464a7a9eefd966874cbd44cb5d0b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "da4dc54cc9026e80e83cb0e020bcab7d69c011feb79c58e49ffc44437fe64559"
   end
 
   depends_on "cmake" => :build
