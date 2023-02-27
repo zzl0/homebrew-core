7a62553b3bff27a5774b59ed64f6be0fa93c3c7f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-27 19:12:25 +0000

    datalad: update 0.18.2 bottle.

--- Formula/datalad.rb
+++ Formula/datalad.rb
@@ -8,13 +8,13 @@ class Datalad < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "47711a8570a6e2313a7c8c5aa074eba1aef6c347e2732192fcd4c5cd1be50eaf"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e658a2a2d522e184cb6be6ab8e610ddc97ff72da4c292c4af5e14210dade2959"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a2fe9b33efd8fbf8df1a7f6fc7b6e4a4e5cd2832042cbbab681c963129437754"
-    sha256 cellar: :any_skip_relocation, ventura:        "074541b54326a2f8e0888a4895914b62ac5d003578ab323c82c6c518141cf192"
-    sha256 cellar: :any_skip_relocation, monterey:       "f9edffcf6cd020f531e5153ad2f5d276ffb903c989355eab187d5964773c2ad3"
-    sha256 cellar: :any_skip_relocation, big_sur:        "8719323067d49f3cf56dd87b09808102e4ad313b57d5cdba9c339dc2ad6a3534"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "403f0d9d934d89950cc6a59bfb531670242f8491b9c8815f75a30b010fce6a26"
+    sha256 cellar: :any,                 arm64_ventura:  "9f2582370eccff4585c6bc786e7ec3afbca50ff649b746ec95e332dc73199d32"
+    sha256 cellar: :any,                 arm64_monterey: "438dcd2e50d055dca35e9a90a7498be914edfeb2a45605b2c50c2003aec1324b"
+    sha256 cellar: :any,                 arm64_big_sur:  "d5c07c04f36acc980ff39032f90ac958949d62ba1262ffc4147799d5f16bb281"
+    sha256 cellar: :any,                 ventura:        "fa3deb42cddd74379c23659fa65c103e0549637c02492466bccf5079d7e6fc17"
+    sha256 cellar: :any,                 monterey:       "b33388041c43d2807215d5f52d1ed02da70b86670a183138cd20660b6e6eff8f"
+    sha256 cellar: :any,                 big_sur:        "27526dfc0f97ccea7c66bdf5726bb0ae5ca9e70f2e91d3d0f3f37d06541dbe8d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e221faecfbfea3afd8b10199f836a391a830946dac910ed22ce78a20658baa22"
   end
 
   depends_on "rust" => :build # for cryptography
