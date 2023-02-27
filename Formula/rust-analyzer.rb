c5fc04b2019879218984a6d094272e48ab879c66
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-02-27 16:00:22 +0800

    rust-analyzer 2023-02-27
    Closes #124298.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/rust-analyzer.rb
+++ Formula/rust-analyzer.rb
@@ -2,9 +2,9 @@ class RustAnalyzer < Formula
   desc "Experimental Rust compiler front-end for IDEs"
   homepage "https://rust-analyzer.github.io/"
   url "https://github.com/rust-lang/rust-analyzer.git",
-       tag:      "2023-02-20",
-       revision: "a6603fc21d50b3386a488c96225b2d1fd492e533"
-  version "2023-02-20"
+       tag:      "2023-02-27",
+       revision: "4e29820f6d9880606a403e7bec6e91312e7f0575"
+  version "2023-02-27"
   license any_of: ["Apache-2.0", "MIT"]
 
   bottle do
