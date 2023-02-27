ed98bcc0078700eb2508c24a80c3e3179e0a5728
Sam Ford <1584702+samford@users.noreply.github.com>
2023-02-26 23:13:29 -0500

    pax-construct: remove livecheck (#124290)

--- Formula/pax-construct.rb
+++ Formula/pax-construct.rb
@@ -5,11 +5,6 @@ class PaxConstruct < Formula
   sha256 "fc832b94a7d095d5ee26b1ce4b3db449261f0154e55b34a7bc430cb685d51064"
   license "Apache-2.0"
 
-  livecheck do
-    url "https://search.maven.org/remotecontent?filepath=org/ops4j/pax/construct/scripts/maven-metadata.xml"
-    regex(%r{<version>\s*v?(\d+(?:\.\d+)+)\s*</version>}i)
-  end
-
   bottle do
     sha256 cellar: :any_skip_relocation, all: "7b033ca0a10e6011280fce831ec89b49717cf985137b4134caac26da3669c5a5"
   end
