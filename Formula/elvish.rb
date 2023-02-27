f34a91eeb584fb66d4fd2721f8faef79830e8b02
Porkepix <porkepix@gmail.com>
2023-02-27 10:04:32 +0100

    elvish 0.19.0
    Closes #124301.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/elvish.rb
+++ Formula/elvish.rb
@@ -1,8 +1,8 @@
 class Elvish < Formula
   desc "Friendly and expressive shell"
   homepage "https://github.com/elves/elvish"
-  url "https://github.com/elves/elvish/archive/v0.18.0.tar.gz"
-  sha256 "f4635db90af2241bfd37e17ac1a72567b92d18a396598da2099a908b3d88c590"
+  url "https://github.com/elves/elvish/archive/v0.19.0.tar.gz"
+  sha256 "f7a5bd9bcbc42fb894e94e90bd3a3a964a9dc488d39c4fe668d06688651f60e4"
   license "BSD-2-Clause"
   head "https://github.com/elves/elvish.git", branch: "master"
 
