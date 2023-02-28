8accbaf537e1e22bddf09a5ec1d131c2f4463f59
Bevan Kay <email@bevankay.me>
2023-02-28 22:40:46 +1100

    audacious: revision bump (ffmpeg 6.0)

--- Formula/audacious.rb
+++ Formula/audacious.rb
@@ -2,6 +2,7 @@ class Audacious < Formula
   desc "Lightweight and versatile audio player"
   homepage "https://audacious-media-player.org/"
   license "BSD-2-Clause"
+  revision 1
 
   stable do
     url "https://distfiles.audacious-media-player.org/audacious-4.3.tar.bz2"
