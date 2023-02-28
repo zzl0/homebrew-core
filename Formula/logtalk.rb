5955e6f39d197a69d5971c0ff88116172e48d6a7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-28 15:13:45 +0000

    logtalk: update 3.63.0 bottle.

--- Formula/logtalk.rb
+++ Formula/logtalk.rb
@@ -12,11 +12,11 @@ class Logtalk < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "fafb0f4ce065ee767944e0c861e0ee2a068d08758b0836fc59989375d01dfbfb"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b834585f3fa36cb4c7a31cf345b098b116e71fbbab8335ee90729f70ef22180c"
-    sha256 cellar: :any_skip_relocation, monterey:       "3cd3a58cd71399f571885dedd321771aad8678dac796214dc0c85e386f383f28"
-    sha256 cellar: :any_skip_relocation, big_sur:        "975ecb49f737ff3305ef2c8d4087efe9826ddc6cd0454e6360e3bb8a5be3e745"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fa3a521308076db67f50084b50f56214b90b437df3b34ab4004f7ab9b179199a"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "210a61f8d1dbd4274734a2d45c5b3e745436056fc1bd1934cdf37492901971d9"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "72a49e827d0e7affa66dfa0a0391da6afc87437751d0d352d0ed4f8b39f5fcd2"
+    sha256 cellar: :any_skip_relocation, monterey:       "9c4f8c03513108aeb5648cf64f972228cd8c4c3fc1235c4b68e68e2fa1202168"
+    sha256 cellar: :any_skip_relocation, big_sur:        "64bd7d4feb53c98991a7c024ae4436b9fc14fdd8863b6ecdff3cab0e80b4140e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a3f8945de221d0ae189d767f85f35b5b81076b0f18a5f5229ccd1ac2bca9aec0"
   end
 
   depends_on "gnu-prolog"
