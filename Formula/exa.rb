a1215489aae173e50e71556bccc1f5f9d93c83d2
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2023-02-28 13:54:34 +0800

    exa: test for linkage with `libgit2`
    This should simplify reviews of #124227 and future version bumps of
    `libgit2` to make sure that `cargo` isn't switching out our `libgit2`
    without our noticing it.
    
    Closes #124395.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/exa.rb
+++ Formula/exa.rb
@@ -23,9 +23,9 @@ class Exa < Formula
   end
 
   depends_on "pandoc" => :build
+  depends_on "pkg-config" => :build
   depends_on "rust" => :build
   depends_on "libgit2"
-  uses_from_macos "zlib"
 
   def install
     system "cargo", "install", *std_cargo_args
@@ -54,5 +54,13 @@ class Exa < Formula
   test do
     (testpath/"test.txt").write("")
     assert_match "test.txt", shell_output("#{bin}/exa")
+
+    linkage_with_libgit2 = (bin/"exa").dynamically_linked_libraries.any? do |dll|
+      next false unless dll.start_with?(HOMEBREW_PREFIX.to_s)
+
+      File.realpath(dll) == (Formula["libgit2"].opt_lib/shared_library("libgit2")).realpath.to_s
+    end
+
+    assert linkage_with_libgit2, "No linkage with libgit2! Cargo is likely using a vendored version."
   end
 end
