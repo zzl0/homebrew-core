62f96cc40cf90c51e494ad2be6fca98898b38e43
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-28 05:14:33 +0000

    plantuml: update 1.2023.2 bottle.

--- Formula/plantuml.rb
+++ Formula/plantuml.rb
@@ -12,13 +12,13 @@ class Plantuml < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "369ef04c9d1683fd5c9859aaee715b68d0f2cc16ee3b38894491e0739065fd81"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "369ef04c9d1683fd5c9859aaee715b68d0f2cc16ee3b38894491e0739065fd81"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "369ef04c9d1683fd5c9859aaee715b68d0f2cc16ee3b38894491e0739065fd81"
-    sha256 cellar: :any_skip_relocation, ventura:        "369ef04c9d1683fd5c9859aaee715b68d0f2cc16ee3b38894491e0739065fd81"
-    sha256 cellar: :any_skip_relocation, monterey:       "369ef04c9d1683fd5c9859aaee715b68d0f2cc16ee3b38894491e0739065fd81"
-    sha256 cellar: :any_skip_relocation, big_sur:        "369ef04c9d1683fd5c9859aaee715b68d0f2cc16ee3b38894491e0739065fd81"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "529f07e44a2e40797b7b75cb96c724a732df6cb1bff16bfc452c3c20ff4f11ca"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "d50d8baa4997519fc1f5362be3ba152dca793bde71686df12aff101cfd0f3bdd"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "d50d8baa4997519fc1f5362be3ba152dca793bde71686df12aff101cfd0f3bdd"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d50d8baa4997519fc1f5362be3ba152dca793bde71686df12aff101cfd0f3bdd"
+    sha256 cellar: :any_skip_relocation, ventura:        "d50d8baa4997519fc1f5362be3ba152dca793bde71686df12aff101cfd0f3bdd"
+    sha256 cellar: :any_skip_relocation, monterey:       "d50d8baa4997519fc1f5362be3ba152dca793bde71686df12aff101cfd0f3bdd"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d50d8baa4997519fc1f5362be3ba152dca793bde71686df12aff101cfd0f3bdd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "dfbd60dc5243e9e924941073aa076728a4231922c1855fdbc8cbde2cdbc65773"
   end
 
   depends_on "graphviz"
