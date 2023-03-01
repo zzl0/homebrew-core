a4e4a054514670d3ebbe02f793ae524588de8678
Alexander M. Turek <me@derrabus.de>
2023-03-01 09:06:20 +0100

    phpunit 10.0.14
    Closes #124510.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/phpunit.rb
+++ Formula/phpunit.rb
@@ -1,8 +1,8 @@
 class Phpunit < Formula
   desc "Programmer-oriented testing framework for PHP"
   homepage "https://phpunit.de"
-  url "https://phar.phpunit.de/phpunit-10.0.13.phar"
-  sha256 "3e5261fb500e449244429a4784ad2eb69abc3aefc3817d083473ff21cb140ed3"
+  url "https://phar.phpunit.de/phpunit-10.0.14.phar"
+  sha256 "b40364e00f6d674829fa95fca8ac6732c47b44fe79fb913fcbd117afb6642d53"
   license "BSD-3-Clause"
 
   livecheck do
