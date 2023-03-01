b2893972fbfbde8555814f4fb1100c2180e7d93c
Denys Holius <d.holius@victoriametrics.com>
2023-03-01 17:00:53 +0200

    victoriametrics 1.88.1 (new formula)
    * adds VictoriaMetrics
    * added using free_port and extend test
    * fixed CI error
    * set args in service instead of args file
    * fixed CI issues
    * added sugestions from comments, fixed CI test
    * removed not default flags for service
    * adds suggestions
    * fixed typo in service
    * fixed long line
    * flags are sorted by literal
    * fixed failed CI test
    * fixed missed coma
    * Update victoriametrics.rb
    * removed make from dependencies
    
    Closes #124542.
    
    Co-authored-by: Dawid Dziurla <dawidd0811@gmail.com>
    Co-authored-by: Denys Holius <denaspireone@gmail.com>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/victoriametrics.rb
@@ -0,0 +1,62 @@
+class Victoriametrics < Formula
+  desc "Cost-effective and scalable monitoring solution and time series database"
+  homepage "https://victoriametrics.com/"
+  url "https://github.com/VictoriaMetrics/VictoriaMetrics/archive/v1.88.1.tar.gz"
+  sha256 "92803ae61f927b7173e25d4537c429bf3054edfa2f719bafc8fb9fcd411cab87"
+  license "Apache-2.0"
+
+  depends_on "go" => :build
+
+  def install
+    system "make", "victoria-metrics"
+    bin.install "bin/victoria-metrics"
+
+    (etc/"victoriametrics/scrape.yml").write <<~EOS
+      global:
+        scrape_interval: 10s
+
+      scrape_configs:
+        - job_name: "victoriametrics"
+          static_configs:
+          - targets: ["127.0.0.1:8428"]
+    EOS
+  end
+
+  service do
+    run [
+      opt_bin/"victoria-metrics",
+      "-httpListenAddr=127.0.0.1:8428",
+      "-promscrape.config=#{etc}/victoriametrics/scrape.yml",
+      "-storageDataPath=#{var}/victoriametrics-data",
+    ]
+    keep_alive false
+    log_path var/"log/victoria-metrics.log"
+    error_log_path var/"log/victoria-metrics.err.log"
+  end
+
+  test do
+    http_port = free_port
+
+    (testpath/"scrape.yml").write <<~EOS
+      global:
+        scrape_interval: 10s
+
+      scrape_configs:
+        - job_name: "victoriametrics"
+          static_configs:
+          - targets: ["127.0.0.1:#{http_port}"]
+    EOS
+
+    pid = fork do
+      exec bin/"victoria-metrics",
+        "-httpListenAddr=127.0.0.1:#{http_port}",
+        "-promscrape.config=#{testpath}/scrape.yml",
+        "-storageDataPath=#{testpath}/victoriametrics-data"
+    end
+    sleep 3
+    assert_match "Single-node VictoriaMetrics", shell_output("curl -s 127.0.0.1:#{http_port}")
+  ensure
+    Process.kill(9, pid)
+    Process.wait(pid)
+  end
+end
