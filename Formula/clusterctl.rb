ea6be6602cd10dae6fddc6a6bff71dc914609d17
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-03-01 06:14:03 +0000

    clusterctl: update 1.3.4 bottle.

--- Formula/clusterctl.rb
+++ Formula/clusterctl.rb
@@ -18,13 +18,13 @@ class Clusterctl < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "144dd5d6f737c7bc8c9df8e212577c6440ced9fff82e2360715579971dc1d916"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "d16e1ca3e300ab44e43ccaaae8f6e4825d551c0fe4e45c8f654a9e04d14c1ec1"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1a6fa7219ca24f770d91df0f8088a1cdb5a60aac078702cfe1b98d8158b1bc27"
-    sha256 cellar: :any_skip_relocation, ventura:        "4d4660959c1f265a6c7018033983e3f639bd2d7487996af830457b0aeceef13b"
-    sha256 cellar: :any_skip_relocation, monterey:       "04e0976b880fdab1979f4be5c3a297394ac683ae68e5a492d3b1a2028a0faebc"
-    sha256 cellar: :any_skip_relocation, big_sur:        "d6951ea8f500cc03612e0e2d6e335ed9f356ce3b101aad5902a741f9f887afd1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8afb81cde92e599e011629cddb83a2ef7015c0711f7fcb4ff48fd897f40bb4fd"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a4edd6397b3042e0898755198fde0be514df61573f165498cbf9f22dd4598721"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a4edd6397b3042e0898755198fde0be514df61573f165498cbf9f22dd4598721"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a4edd6397b3042e0898755198fde0be514df61573f165498cbf9f22dd4598721"
+    sha256 cellar: :any_skip_relocation, ventura:        "df170d11db0052a66be362a73d0b2770bb2fa0bd5cd2c6f208930934330b12fd"
+    sha256 cellar: :any_skip_relocation, monterey:       "df170d11db0052a66be362a73d0b2770bb2fa0bd5cd2c6f208930934330b12fd"
+    sha256 cellar: :any_skip_relocation, big_sur:        "df170d11db0052a66be362a73d0b2770bb2fa0bd5cd2c6f208930934330b12fd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9eac2ceb7c05d71ced4ab5d655ef545fff9d404538ca6b0100e18e2127274dbd"
   end
 
   depends_on "go" => :build
