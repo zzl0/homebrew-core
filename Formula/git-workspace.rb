4827d3045f7280fc97064e7c87411c40099bf3fa
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2023-03-02 22:12:03 +0800

    git-workspace: avoid vendored `libgit2`
    Closes #124647.
    
    Signed-off-by: Ruoyu Zhong <zhongruoyu@outlook.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/git-workspace.rb
+++ Formula/git-workspace.rb
@@ -15,9 +15,9 @@ class GitWorkspace < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "e151fbcb03e3e4eeb7bd02611c087e732778a11c25990c0575620f4cc554fb75"
   end
 
+  depends_on "pkg-config" => :build
   depends_on "rust" => :build
-
-  uses_from_macos "zlib"
+  depends_on "libgit2"
 
   def install
     system "cargo", "install", *std_cargo_args
@@ -30,5 +30,13 @@ class GitWorkspace < Formula
     assert_match "provider = \"github\"", File.read("workspace.toml")
     output = shell_output("#{bin}/git-workspace update 2>&1", 1)
     assert_match "Error fetching repositories from Github user/org foo", output
+
+    linkage_with_libgit2 = (bin/"git-workspace").dynamically_linked_libraries.any? do |dll|
+      next false unless dll.start_with?(HOMEBREW_PREFIX.to_s)
+
+      File.realpath(dll) == (Formula["libgit2"].opt_lib/shared_library("libgit2")).realpath.to_s
+    end
+
+    assert linkage_with_libgit2, "No linkage with libgit2! Cargo is likely using a vendored version."
   end
 end
