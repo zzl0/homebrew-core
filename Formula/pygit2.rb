664e22c16043ba447983b8bf7c1fa737793dbaae
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2023-03-02 03:49:50 +0000

    pygit2: add 1.11.1 bottle.

--- Formula/pygit2.rb
+++ Formula/pygit2.rb
@@ -6,6 +6,16 @@ class Pygit2 < Formula
   license "GPL-2.0-only" => { with: "GCC-exception-2.0" }
   head "https://github.com/libgit2/pygit2.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "9453ad61e5e525c57a6a667ee2f6dafe787e24491af80817c95f0bc5a656cc10"
+    sha256 cellar: :any,                 arm64_monterey: "1608908e2e6d47a93a4f9dbc535a860daeba98ae324156ee8de2758a12d7ad96"
+    sha256 cellar: :any,                 arm64_big_sur:  "510f9e7195127118e6820478dbc77ebcce480bad25caf1279783fc955777a6f9"
+    sha256 cellar: :any,                 ventura:        "b56a942dc7c7fafe53c17b8f64e19b51a24cafb766ea999bfec30016730ddd8b"
+    sha256 cellar: :any,                 monterey:       "c6848c85a2b795f7f9abaf4ff169df4181cc6bddf2cf9e2d603a7c93b3a95747"
+    sha256 cellar: :any,                 big_sur:        "49cba7208c3ddd74368dd071e6a0197e8809ab9e4df3b9717eb3eabf40d58fff"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "15c06e62ed0d37a97b9ddc54e0395a2820048ea65cd10ba00b296c24148aecb6"
+  end
+
   depends_on "cffi"
   depends_on "libgit2"
   depends_on "python@3.11"
