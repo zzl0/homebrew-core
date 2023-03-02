a968388e0e722fab6eec054ef1a1f36e58dd37d8
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-03-02 05:31:08 +0000

    alluxio: update 2.9.2 bottle.

--- Formula/alluxio.rb
+++ Formula/alluxio.rb
@@ -11,13 +11,13 @@ class Alluxio < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a8e749ffce5b7ec29fc80b9cc955600b095959e748395bc306e316acd1af849f"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a8e749ffce5b7ec29fc80b9cc955600b095959e748395bc306e316acd1af849f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a8e749ffce5b7ec29fc80b9cc955600b095959e748395bc306e316acd1af849f"
-    sha256 cellar: :any_skip_relocation, ventura:        "3019c4436d34bdfa4cf6d3797ad92619ca5c2c68c3b093074d4c195773ae94d1"
-    sha256 cellar: :any_skip_relocation, monterey:       "3019c4436d34bdfa4cf6d3797ad92619ca5c2c68c3b093074d4c195773ae94d1"
-    sha256 cellar: :any_skip_relocation, big_sur:        "3019c4436d34bdfa4cf6d3797ad92619ca5c2c68c3b093074d4c195773ae94d1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a8e749ffce5b7ec29fc80b9cc955600b095959e748395bc306e316acd1af849f"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "36e2fcb11b4650673f03a41bcfdf3b23199c3b7a9411bcb35e40cdee8b8a9bd3"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "36e2fcb11b4650673f03a41bcfdf3b23199c3b7a9411bcb35e40cdee8b8a9bd3"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "36e2fcb11b4650673f03a41bcfdf3b23199c3b7a9411bcb35e40cdee8b8a9bd3"
+    sha256 cellar: :any_skip_relocation, ventura:        "5f10bc6b5a20da394c5b0458a663d88e7acf09b0ae1bdb86c32211253dfa9be5"
+    sha256 cellar: :any_skip_relocation, monterey:       "5f10bc6b5a20da394c5b0458a663d88e7acf09b0ae1bdb86c32211253dfa9be5"
+    sha256 cellar: :any_skip_relocation, big_sur:        "5f10bc6b5a20da394c5b0458a663d88e7acf09b0ae1bdb86c32211253dfa9be5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "36e2fcb11b4650673f03a41bcfdf3b23199c3b7a9411bcb35e40cdee8b8a9bd3"
   end
 
   # Alluxio requires Java 8 or Java 11
