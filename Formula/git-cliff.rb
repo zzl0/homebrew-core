6211dc962173e7fe0c828aa5ae0588351152885d
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2023-03-02 18:48:28 +0800

    git-cliff: avoid vendored `libgit2`
    Closes #124624.
    
    Signed-off-by: Ruoyu Zhong <zhongruoyu@outlook.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/git-cliff.rb
+++ Formula/git-cliff.rb
@@ -15,9 +15,9 @@ class GitCliff < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "b5f6579d52cdb2eb44b30928ce0d892d4e5b8d774f3f2722f0b9100772d79f23"
   end
 
+  depends_on "pkg-config" => :build
   depends_on "rust" => :build
-
-  uses_from_macos "zlib"
+  depends_on "libgit2"
 
   def install
     system "cargo", "install", *std_cargo_args(path: "git-cliff")
@@ -38,5 +38,13 @@ class GitCliff < Formula
     system "git", "commit", "-m", "chore: initial commit"
     changelog = "### Miscellaneous Tasks\n\n- Initial commit"
     assert_match changelog, shell_output("git cliff")
+
+    linkage_with_libgit2 = (bin/"git-cliff").dynamically_linked_libraries.any? do |dll|
+      next false unless dll.start_with?(HOMEBREW_PREFIX.to_s)
+
+      File.realpath(dll) == (Formula["libgit2"].opt_lib/shared_library("libgit2")).realpath.to_s
+    end
+
+    assert linkage_with_libgit2, "No linkage with libgit2! Cargo is likely using a vendored version."
   end
 end
