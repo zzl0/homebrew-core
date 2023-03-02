8992272fbd7d31f0868bf95fa44a06d5fda9ff36
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-03-02 10:14:10 +0000

    terminator: update 2.1.3 bottle.

--- Formula/terminator.rb
+++ Formula/terminator.rb
@@ -8,15 +8,13 @@ class Terminator < Formula
   license "GPL-2.0-only"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "553547ec5d0f5f0704c279cdd9f1df9ba027f9eb704ed52877865f2e6f064d22"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ada81984b24711c6cfdd4e31dd4015582bf81eec755423ae29d7ae9bc8365f0f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f8d4d53151ad1204d1af781c284a4353a30910ccdaab57f8dff08fb70a5c9dc4"
-    sha256 cellar: :any_skip_relocation, ventura:        "10d1ddcb032ee692a31e415b988a37c79b9bba7a897768ef17311ec83073720a"
-    sha256 cellar: :any_skip_relocation, monterey:       "9983a9baec039953cf6c2c91cbe363a63e276e77bb84723e7634fb040417779a"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b8e88d9fba374decf7b2629a4369c9a23e358e2b48752f6ff7f099d9770be673"
-    sha256 cellar: :any_skip_relocation, catalina:       "c1fce0d5da33ab49ff3594ecbbeaff1f5f3f14b91d85f1c773df4f6f19fafaf5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "68406abd3adba8d745c09e2ab5869e21785c3e16237078f0e3ae98da2f8580e8"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "2307bdb153aa6a5fc0e05cdac0b3b71e7c6e4277c293bf65c722b60e5319ad5c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "77b5e90f5d1fc261e196e25196d73be1c936d1f91ab2c4a19b8db4f62925d2c4"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9a9e3d8693c6bd2d93d3aa7b56e958e53d4fb2d0c0bbe457f91e6394df2438d8"
+    sha256 cellar: :any_skip_relocation, ventura:        "9827d8d873ac411d20b41aaf7610c1f2e9d7b2526620f7df2bc9be2b5da8af05"
+    sha256 cellar: :any_skip_relocation, monterey:       "311ad497c491d2e0667c16671c6a95ab683b47b1946422d51aee6d131f69b67e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "85457c6098ae589d2b16d6aa4cbed08df9ce4c364503ff4a5c8385d9e8da616d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8dc1c9a39ed76ef6d67b3831a687776f880afd77609982e37cc7459b334a2190"
   end
 
   depends_on "pygobject3"
