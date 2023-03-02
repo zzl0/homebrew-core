25813d6be7a5f36a991ac358d74c4bdfb0b18b8c
Porkepix <porkepix@gmail.com>
2023-03-02 04:51:35 +0100

    elm-format 0.8.6
    elm-format: update build
    
    elm-format: update build
    
    Closes #124605.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/elm-format.rb
+++ Formula/elm-format.rb
@@ -2,8 +2,8 @@ class ElmFormat < Formula
   desc "Elm source code formatter, inspired by gofmt"
   homepage "https://github.com/avh4/elm-format"
   url "https://github.com/avh4/elm-format.git",
-      tag:      "0.8.5",
-      revision: "80f15d85ee71e1663c9b53903f2b5b2aa444a3be"
+      tag:      "0.8.6",
+      revision: "7e80dd48dd9b30994e43f4804b2ea7118664e8e0"
   license "BSD-3-Clause"
   head "https://github.com/avh4/elm-format.git", branch: "main"
 
@@ -18,7 +18,6 @@ class ElmFormat < Formula
 
   depends_on "cabal-install" => :build
   depends_on "haskell-stack" => :build
-  depends_on arch: :x86_64 # no ghc (OSX,AArch64) binary via `haskell-stack`
 
   uses_from_macos "xz" => :build # for `haskell-stack` to unpack ghc
 
@@ -31,13 +30,13 @@ class ElmFormat < Formula
     # in Homebrew. Try using Homebrew `ghc` on update. Optionally, consider adding `ghcup`
     # as a lighter-weight alternative to `haskell-stack` for installing particular ghc version.
     jobs = ENV.make_jobs
-    ENV.deparallelize { system "stack", "-j#{jobs}", "setup", "8.10.4", "--stack-root", buildpath/".stack" }
+    ENV.deparallelize { system "stack", "-j#{jobs}", "setup", "9.2.5", "--stack-root", buildpath/".stack" }
     ENV.prepend_path "PATH", Dir[buildpath/".stack/programs/*/ghc-*/bin"].first
     system "cabal", "v2-update"
 
     # Directly running `cabal v2-install` fails: Invalid file name in tar archive: "avh4-lib-0.0.0.1/../"
     # Instead, we can use the upstream's build.sh script, which utilizes the Shake build system.
-    system "./build.sh", "--", "build"
+    system "./dev/build.sh", "--", "build"
     bin.install "_build/elm-format"
   end
 
