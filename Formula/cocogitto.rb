40ff26db7589bf8f6bf90fdcb0fdc543959f3172
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2023-03-02 18:34:58 +0800

    cocogitto: avoid vendored `libgit2`
    Closes #124620.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cocogitto.rb
+++ Formula/cocogitto.rb
@@ -15,8 +15,9 @@ class Cocogitto < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "11a9c51c4a4a154710e738290684698cf0dab3952321d9ec6d86e26bb565859f"
   end
 
+  depends_on "pkg-config" => :build
   depends_on "rust" => :build
-  uses_from_macos "zlib"
+  depends_on "libgit2"
 
   def install
     system "cargo", "install", *std_cargo_args
@@ -32,5 +33,13 @@ class Cocogitto < Formula
     system "git", "config", "user.email", "author@example.com"
     system "git", "commit", "-m", "chore: initial commit"
     assert_equal "No errored commits", shell_output("#{bin}/cog check 2>&1").strip
+
+    linkage_with_libgit2 = (bin/"cog").dynamically_linked_libraries.any? do |dll|
+      next false unless dll.start_with?(HOMEBREW_PREFIX.to_s)
+
+      File.realpath(dll) == (Formula["libgit2"].opt_lib/shared_library("libgit2")).realpath.to_s
+    end
+
+    assert linkage_with_libgit2, "No linkage with libgit2! Cargo is likely using a vendored version."
   end
 end
