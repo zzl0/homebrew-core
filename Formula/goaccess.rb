f23e2fe35e410c6ffa828f4a51e80f956cc71b1e
Porkepix <porkepix@gmail.com>
2023-03-02 04:53:59 +0100

    goaccess 1.7.1
    Closes #124606.
    
    Signed-off-by: Ruoyu Zhong <zhongruoyu@outlook.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/goaccess.rb
+++ Formula/goaccess.rb
@@ -1,8 +1,8 @@
 class Goaccess < Formula
   desc "Log analyzer and interactive viewer for the Apache Webserver"
   homepage "https://goaccess.io/"
-  url "https://tar.goaccess.io/goaccess-1.7.tar.gz"
-  sha256 "a7549cb5bc78e99fb6c4f75aa53bed98403156123c08eeedef7831dafe87e676"
+  url "https://tar.goaccess.io/goaccess-1.7.1.tar.gz"
+  sha256 "3d6876d91d02816a93ad86e179369942d914a82bf09d6a6c02964ed7de48610d"
   license "MIT"
   head "https://github.com/allinurl/goaccess.git", branch: "master"
 
