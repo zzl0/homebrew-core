691b5a9772372da1f577b261645c9af38bb40471
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-03-03 08:56:28 +0000

    logcheck: update 1.4.2 bottle.

--- Formula/logcheck.rb
+++ Formula/logcheck.rb
@@ -11,7 +11,7 @@ class Logcheck < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "0dd5a372a2cf9bee6870ea922f2cdb988c6f73316d7323c2b60147735467575f"
+    sha256 cellar: :any_skip_relocation, all: "80c5420361fea2b961a749a213e5a4c769d6784f8034528eecae7365b9fd252e"
   end
 
   on_macos do
