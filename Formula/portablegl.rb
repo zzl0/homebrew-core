48a721a4de16005d58de1ecdcbb66dc8a96b1cd2
Porkepix <porkepix@gmail.com>
2023-03-03 12:06:26 +0100

    portablegl 0.97.1
    * portablegl 0.97.1
    * portablegl: fix test
    
    Closes #124721.
    
    Co-authored-by: Ruoyu Zhong <zhongruoyu@outlook.com>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/portablegl.rb
+++ Formula/portablegl.rb
@@ -2,8 +2,8 @@ class Portablegl < Formula
   desc "Implementation of OpenGL 3.x-ish in clean C"
   homepage "https://github.com/rswinkle/PortableGL"
   url "https://github.com/rswinkle/PortableGL.git",
-    tag:      "0.94",
-    revision: "ff02769271294639a3a91bef06c5a8b71fc55cfd"
+    tag:      "0.97.1",
+    revision: "24f8840b800f247c328860c578c19b0535be6d58"
   license "MIT"
   head "https://github.com/rswinkle/PortableGL.git", branch: "master"
 
@@ -17,22 +17,23 @@ class Portablegl < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "5072b5c557a56c16b3db6a916074c0c4933e925681c80a88bb78b9fbe6e7c307"
   end
 
-  depends_on "python@3.11" => :test
   depends_on "sdl2" => :test
 
   def install
     include.install "portablegl.h"
     include.install "portablegl_unsafe.h"
-    (pkgshare/"tests").install "glcommon"
-    (pkgshare/"tests").install "testing"
+    (pkgshare/"tests").install %w[glcommon media testing]
   end
 
   test do
-    python = Formula["python@3.11"].opt_bin/"python3.11"
+    # Tests require PNG image outputs to be pixel-identical.
+    # Such exactness may be broken by -march=native.
+    ENV.remove_from_cflags "-march=native"
+
     cp_r Dir["#{pkgshare}/tests/*"], testpath
     cd "testing" do
       system "make", "run_tests"
-      assert_match "All tests passed", shell_output("#{python} check_tests.py")
+      assert_match "All tests passed", shell_output("./run_tests")
     end
   end
 end
