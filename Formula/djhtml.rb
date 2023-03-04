47b2529cebfc90c5515732a99ec24370709a256b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-03-04 16:22:24 +0000

    djhtml: update 3.0.3 bottle.

--- Formula/djhtml.rb
+++ Formula/djhtml.rb
@@ -8,13 +8,13 @@ class Djhtml < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "04faeb1da5b17a16bcc5a707236b93285c225dcf2779428c4aee84dcef6eca7d"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "8478b30ef745876f32be53ebf93320ffd79c555bdb97a11c6d6b23c3acd183da"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b3eb977a27efb89abf0d1940ff37b98d456d2a0ec2a56df09b02c6ab419fc9a4"
-    sha256 cellar: :any_skip_relocation, ventura:        "2267f0deabd8580c8f4f3e297b865d93451d47b9a74fbe8f469bba7b70a2e2b2"
-    sha256 cellar: :any_skip_relocation, monterey:       "ff48f6bbd7352cef4112d6e70617e2306964277541b87c751338577e26a2b868"
-    sha256 cellar: :any_skip_relocation, big_sur:        "5efd376bcb18d6973cf020cd35b0c986ea84889935bd9a1d3c9b39b3f209b1cb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a4c197c330de5ae1629f5b817b761308db290df4284afb590f16264a666812b7"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f56220e387d3afd11dcd214d359e05e3d8d5d26adb87c01653ae2cb8c8ad0a38"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "989274f8e02ab952802e700e1aa1611b2c995548241a70bcd955db2c01bf52df"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bfaadde817f3b04f669f40ead2c9a6e3fe259e6938eecade025ffbc1fcaea2e2"
+    sha256 cellar: :any_skip_relocation, ventura:        "c60b7b054f804eb477565f573db30f22431e354db9c49c2c3a4d3f47004b5fab"
+    sha256 cellar: :any_skip_relocation, monterey:       "544669d32751a634c4d7c2d3caeda3b7f3b70f3b277009885dadbd1322e17d9d"
+    sha256 cellar: :any_skip_relocation, big_sur:        "9d8adc9d415a2e79730b76ab07693499ffc5869962e01051f89c6355a448793a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a6cca52e25a5d6fe80af2fee8a2bcb6ab421a0a43841014f12e1fe9bef2c8ec2"
   end
 
   depends_on "python@3.11"
