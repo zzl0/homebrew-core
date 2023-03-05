ff76933d87a8047b9396e0389394b8b0fdfc1ed1
Patrick Linnane <patrick@linnane.io>
2023-03-05 10:09:37 -0800

    jql 5.1.7
    Closes #124864.
    
    Signed-off-by: Ruoyu Zhong <zhongruoyu@outlook.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/jql.rb
+++ Formula/jql.rb
@@ -1,8 +1,8 @@
 class Jql < Formula
   desc "JSON query language CLI tool"
   homepage "https://github.com/yamafaktory/jql"
-  url "https://github.com/yamafaktory/jql/archive/v5.1.6.tar.gz"
-  sha256 "2dfc00dbf4731db932618acbe31366effecf76eb08c37f11dbfdb16d42463f89"
+  url "https://github.com/yamafaktory/jql/archive/v5.1.7.tar.gz"
+  sha256 "e0bb7e8ac8e8776adacca694603acd9efc35e76315c4aa6f35ba9641d3967a89"
   license "MIT"
   head "https://github.com/yamafaktory/jql.git", branch: "main"
 
