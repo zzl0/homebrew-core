dba999d212e4dcd1bebbc6b6cca13668d32ec9e8
iliana etaoin <iliana@buttslol.net>
2023-03-05 12:36:39 -0800

    vscode-langservers-extracted 4.6.0 (new formula)
    Closes #124874.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/vscode-langservers-extracted.rb
@@ -0,0 +1,40 @@
+require "language/node"
+
+class VscodeLangserversExtracted < Formula
+  desc "Language servers for HTML, CSS, JavaScript, and JSON extracted from vscode"
+  homepage "https://github.com/hrsh7th/vscode-langservers-extracted"
+  url "https://registry.npmjs.org/vscode-langservers-extracted/-/vscode-langservers-extracted-4.6.0.tgz"
+  sha256 "d94b4dc5c4095ccfbb140119aa2dafdb4c7fdf4204c69ef08e56633b068346e6"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    require "open3"
+
+    json = <<~JSON
+      {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "initialize",
+        "params": {
+          "rootUri": null,
+          "capabilities": {}
+        }
+      }
+    JSON
+
+    %w[css eslint html json markdown].each do |lang|
+      Open3.popen3("#{bin}/vscode-#{lang}-language-server", "--stdio") do |stdin, stdout|
+        stdin.write "Content-Length: #{json.size}\r\n\r\n#{json}"
+        sleep 3
+        assert_match(/^Content-Length: \d+/i, stdout.readline)
+      end
+    end
+  end
+end
