3cd76170bb91a7c45298bfce2f303dac9324e325
Rui Chen <rui@chenrui.dev>
2023-03-05 11:53:58 -0500

    whistle 2.9.42
    * whistle 2.9.42
    * whistle: remove x86 specific optional feature
      relates to https://github.com/avwo/whistle/issues/734
    
    Closes #124856.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/whistle.rb
+++ Formula/whistle.rb
@@ -3,22 +3,23 @@ require "language/node"
 class Whistle < Formula
   desc "HTTP, HTTP2, HTTPS, Websocket debugging proxy"
   homepage "https://github.com/avwo/whistle"
-  url "https://registry.npmjs.org/whistle/-/whistle-2.9.41.tgz"
-  sha256 "f6cb9cc4a5b87719830b049cda54bee5a0b3cd04cd9a52f941e55c65618a0e61"
+  url "https://registry.npmjs.org/whistle/-/whistle-2.9.42.tgz"
+  sha256 "4ef367522b357d3788fa90f0cdce75c5db0b4f39d89cc738880675a176ce340e"
   license "MIT"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "73cff8825dc749e1da449a04af00964614890101605e8b20e012d383b39bf066"
   end
 
-  # `bin/proxy/mac/Whistle` was only built for `x86_64`
-  # upstream issue tracker, https://github.com/avwo/whistle/issues/734
-  depends_on arch: :x86_64
   depends_on "node"
 
   def install
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
     bin.install_symlink Dir["#{libexec}/bin/*"]
+
+    # Remove x86 specific optional feature
+    node_modules = libexec/"lib/node_modules/whistle/node_modules"
+    rm_f node_modules/"set-global-proxy/lib/mac/whistle" if Hardware::CPU.arm?
   end
 
   test do
