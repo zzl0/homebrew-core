de325021ece88ff25b44aa6c6c3e0b2ed5fd33cb
Rui Chen <rui@chenrui.dev>
2023-03-05 19:21:24 -0500

    service-weaver 0.1.1 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/service-weaver.rb
@@ -0,0 +1,47 @@
+class ServiceWeaver < Formula
+  desc "Programming framework for writing and deploying cloud applications"
+  homepage "https://serviceweaver.dev/"
+  license "Apache-2.0"
+
+  stable do
+    url "https://github.com/ServiceWeaver/weaver/archive/refs/tags/v0.1.1.tar.gz"
+    sha256 "85513dd68adb1ae62d98db93686abd5dfe4bb482f984cac940b2434fd6409d12"
+
+    resource "weaver-gke" do
+      url "https://github.com/ServiceWeaver/weaver-gke/archive/refs/tags/v0.1.0.tar.gz"
+      sha256 "63ebcd3b72d3859f54d215ba7a2f33aadb275dee94f1028471b15527b728090e"
+    end
+  end
+
+  head do
+    url "https://github.com/ServiceWeaver/weaver.git", branch: "main"
+
+    resource "weaver-gke" do
+      url "https://github.com/ServiceWeaver/weaver-gke.git", branch: "main"
+    end
+  end
+
+  depends_on "go" => :build
+
+  conflicts_with "weaver", because: "both install a `weaver` binary"
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w", output: bin/"weaver"), "./cmd/weaver"
+    resource("weaver-gke").stage do
+      ["weaver-gke", "weaver-gke-local"].each do |f|
+        system "go", "build", *std_go_args(ldflags: "-s -w", output: bin/f), "./cmd/#{f}"
+      end
+    end
+  end
+
+  test do
+    output = shell_output("#{bin}/weaver single status")
+    assert_match "DEPLOYMENTS", output
+
+    gke_output = shell_output("#{bin}/weaver gke status 2>&1", 1)
+    assert_match "gcloud not installed", gke_output
+
+    gke_local_output = shell_output("#{bin}/weaver gke-local status 2>&1", 1)
+    assert_match "connect: connection refused", gke_local_output
+  end
+end
