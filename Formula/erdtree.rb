150690d09836540aaaf4ab5f7cffa9175e115051
Rui Chen <rui@chenrui.dev>
2023-03-06 14:19:06 +0000

    erdtree: add 1.2.0 bottle.

--- Formula/erdtree.rb
+++ Formula/erdtree.rb
@@ -5,6 +5,16 @@ class Erdtree < Formula
   sha256 "5114f694d2430374ce4d251613e16acfa1e5d34a911306062cdb43f7f6544ea9"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "d170b23d58a67abfec80c9c5f29ba01fe1b2659656a955f7b579ee4b4f7c0f59"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5a1c1270bdeda3430db564a76339fdcb277c88c6efa9668c05585beaf5a86f6d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b7d370f7cc6a65c6cb9878cf8cf285526fdbddf868acff1ea7e8732f56c84d94"
+    sha256 cellar: :any_skip_relocation, ventura:        "2bcc3b14a12af3415a883c7a04e367a6fa6922d2e3c25f103f021d3276aa1a12"
+    sha256 cellar: :any_skip_relocation, monterey:       "16540e70ba5d4b1fb2f09a5e407062053f7450599e3b2cd824eb2d1df240c1fa"
+    sha256 cellar: :any_skip_relocation, big_sur:        "1b78a56f5462a60933b4054b5d2cc083121419158f08e6656eb50d13e4719d25"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "07c30f096a56ddab09af2ccbb39ed1db52de3a99483b3595e2def8856cfaf086"
+  end
+
   depends_on "rust" => :build
 
   def install
