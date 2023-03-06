a32c61f55ebb8b70add119789795ce59482eb1a2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-03-06 04:05:24 +0000

    scons: update 4.5.0 bottle.

--- Formula/scons.rb
+++ Formula/scons.rb
@@ -8,15 +8,13 @@ class Scons < Formula
   license "MIT"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "aa18ea62fe2e200bc398f7cfa52c480877ac55336893aa749ef4a86328e8bd14"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "aa18ea62fe2e200bc398f7cfa52c480877ac55336893aa749ef4a86328e8bd14"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "aa18ea62fe2e200bc398f7cfa52c480877ac55336893aa749ef4a86328e8bd14"
-    sha256 cellar: :any_skip_relocation, ventura:        "f6d6e507ea5bbeabae6fa2798e660bd809a240aad0fb33f89bbecf4795157448"
-    sha256 cellar: :any_skip_relocation, monterey:       "f6d6e507ea5bbeabae6fa2798e660bd809a240aad0fb33f89bbecf4795157448"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f6d6e507ea5bbeabae6fa2798e660bd809a240aad0fb33f89bbecf4795157448"
-    sha256 cellar: :any_skip_relocation, catalina:       "f6d6e507ea5bbeabae6fa2798e660bd809a240aad0fb33f89bbecf4795157448"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b2ffa8002a575f2c7e36d6bb2fa1c2f561cd391681ff6bf8afa0156645066d77"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "1e420b52485379506b897d341b8833daa97caeda873f4609f3dff491a5e46095"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "632a8be58de3d7c2e3c218ef7ede5a8dd1ea63f0e1213e4a92afbdcbd87cf0e9"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "01a3913680e2d5442f55f8198d0b84a11911674407814530143f23906ede47bb"
+    sha256 cellar: :any_skip_relocation, ventura:        "b786a0f61d3597564d1ca185299898116bc8918f97f0c699eecf5bdf8d381b3a"
+    sha256 cellar: :any_skip_relocation, monterey:       "5bb1b863b61337d8393acb3776f8432cb1303e6286784611ced049f2e68bbc14"
+    sha256 cellar: :any_skip_relocation, big_sur:        "5f20ae0b87247b76de637c9fce703a9092f4f492c4568ad6e5545c272d9f3eaa"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "31cf379929e0bd95c8b0e72c22c120e786e110b3166eb8831d0f2fb7cb0a0699"
   end
 
   depends_on "python@3.11"
