bf9d7a93bb51bb12aae5a9046ddf41b789cb9826
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-03-07 23:11:45 +0800

    jackett 0.20.3521
    Closes #125012.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/jackett.rb
+++ Formula/jackett.rb
@@ -1,8 +1,8 @@
 class Jackett < Formula
   desc "API Support for your favorite torrent trackers"
   homepage "https://github.com/Jackett/Jackett"
-  url "https://github.com/Jackett/Jackett/archive/refs/tags/v0.20.3486.tar.gz"
-  sha256 "c02db37fe353c279facd0d5877e84f5dd6a516271de631a5f51fe719b85e72c4"
+  url "https://github.com/Jackett/Jackett/archive/refs/tags/v0.20.3521.tar.gz"
+  sha256 "26731250d5ee8abb52762dc1438a98a2d3c3e6bef12cfe07431b12bc69104c3c"
   license "GPL-2.0-only"
   head "https://github.com/Jackett/Jackett.git", branch: "master"
 
