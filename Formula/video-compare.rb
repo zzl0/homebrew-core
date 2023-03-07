637785b3269e1701877312c8eede4c7be564d49a
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-03-07 23:25:57 +0800

    video-compare 20230306
    Closes #125015.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/video-compare.rb
+++ Formula/video-compare.rb
@@ -1,8 +1,8 @@
 class VideoCompare < Formula
   desc "Split screen video comparison tool using FFmpeg and SDL2"
   homepage "https://github.com/pixop/video-compare"
-  url "https://github.com/pixop/video-compare/archive/refs/tags/20230223.tar.gz"
-  sha256 "f591e584045e738149221ffd0176362f17ec05ddd178a990b02d4d617eba9f05"
+  url "https://github.com/pixop/video-compare/archive/refs/tags/20230306.tar.gz"
+  sha256 "8997538442baa5ac16d7d7d85bdc5e2b2be8668413c90fab69286407c683dce6"
   license "GPL-2.0-only"
 
   bottle do
