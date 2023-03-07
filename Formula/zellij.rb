77813180c8d49d559aabae5d17442808656ce863
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-03-07 10:05:30 +0000

    zellij: update 0.35.0 bottle.

--- Formula/zellij.rb
+++ Formula/zellij.rb
@@ -6,13 +6,13 @@ class Zellij < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f5441641d0d340f0bc3ad2a6fb1c64c13bade8e6a7586d4b9331cd53d0f26c74"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b45bf371093febf1da924fe3a9d241733e3c128b5ebfc330787981ffe78d1d92"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "59267ce4d52a9dd39021048d39334aaf5919f6e94880b88a8227ccd9e698fea5"
-    sha256 cellar: :any_skip_relocation, ventura:        "65fd10a1538a817dc2a9133264026c7dbde53e7d98340b3ed5833df05f1952a5"
-    sha256 cellar: :any_skip_relocation, monterey:       "6dbd74c04d6c5161261f42105bfb13dd7512bcb815497b8029f35b8ed72c0eea"
-    sha256 cellar: :any_skip_relocation, big_sur:        "45e195e4b9db695160738bdaa6fc2ef565fd5c1bcf5fb87d852f1429a29cf005"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c66afab475ce488b82bfcd06116d6424ff98d97dd59d012832ee2b56ce83cea1"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "ef5dd57fcbdae693e10d4f79dd7bce56154b368133c88ffbfc236eac8ce3cd0f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ac7c95957376939cf7c761c4443d1ee310d3caec319f0340e4e881baeac5a9f3"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "28a781ee8b7a8a953a7a5eb91f9ee8464f17768d7f6869869955101b8aef1618"
+    sha256 cellar: :any_skip_relocation, ventura:        "c22280765c0bbcd152b4bc58d9b3eb1ded85d2c253a55a851b6416dc11501c5d"
+    sha256 cellar: :any_skip_relocation, monterey:       "8a7ae1f884576642cdc76d1e56abfb706e7102cef117a982105c1dcec8c7946f"
+    sha256 cellar: :any_skip_relocation, big_sur:        "0c8adf3cf8047c5fd819c151aa119409bdc7e6b10ab14d29dcf2a20ae77ddd21"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e110a3857b5e8de087b94b1a04dcbbe4d1f75150cf7c10a5f0df3d56ecd09f63"
   end
 
   depends_on "rust" => :build
