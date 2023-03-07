0d71b71afb85bd8e83422dbd4bcad4fdf63b0081
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-03-07 06:14:00 +0000

    pgsync: update 0.7.4 bottle.

--- Formula/pgsync.rb
+++ Formula/pgsync.rb
@@ -6,13 +6,13 @@ class Pgsync < Formula
   license "MIT"
 
   bottle do
-    sha256                               arm64_ventura:  "d1e1f2222f90e61de8734634c95ba9a827e69d19f42a5fc3b35cea757ed33192"
-    sha256                               arm64_monterey: "65a64594047d853f3521a9df8531daa6a5a267997183138241285af11e75f567"
-    sha256                               arm64_big_sur:  "e2c3459dcabb4fa25ec7180a5b0e28d5c926204eda322f19e09e35b3eed383d1"
-    sha256                               ventura:        "df290f2de349e3c2e409220be2f4c8b49248454c589d59f80dc942b152e5b5f1"
-    sha256                               monterey:       "984d3269acf9e81069269efd228a6c6005527ba25719dd7cebaa37784b1d1289"
-    sha256                               big_sur:        "98498d42a544c430b9fa30c219c1b783c66fa68d79b08ad30e23d20173136a8e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5783edd9237dd34e39a2f67be2976e2929f5febd16981cda3ab26677ca2bb169"
+    sha256                               arm64_ventura:  "2dfb0ba78422a71258c0046a1c59e433a7c70fd98e8564494b8878624e056756"
+    sha256                               arm64_monterey: "169345dc69a037d36dac2f0d54ffd229236bd62c86c97f85f07cced38f8c7f65"
+    sha256                               arm64_big_sur:  "c3dd3f2937b118494d0002e08fd95ec0959b982396d9f3008d24f7ba05d84fec"
+    sha256                               ventura:        "c621ec145ac0bb5ba6297e2b24ee351c418e8144bc6d18c178c5a27c3e431a26"
+    sha256                               monterey:       "8f1be82ef9f91002531609773443d58aea9bfb0bd11e6979f7944680960e860e"
+    sha256                               big_sur:        "a38c199ddfda5b26938988cb5f575669e3bb0748c045ca5d49da6774988a09b2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6a6884e6e6c1ed462f3d06a42a2f1c3babfe12e25b9ae784f0072420b7c00344"
   end
 
   depends_on "libpq"
