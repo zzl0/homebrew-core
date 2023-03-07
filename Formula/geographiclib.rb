17e0b0a549d6f5654444dbe3e3f1e8b90f3c3dbe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-03-07 19:05:03 +0000

    geographiclib: update 2.2 bottle.

--- Formula/geographiclib.rb
+++ Formula/geographiclib.rb
@@ -7,14 +7,13 @@ class Geographiclib < Formula
   head "https://github.com/geographiclib/geographiclib.git", branch: "main"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_ventura:  "8fe27a4ff40ba356a6051c12de9be885e0975f650470112cd2fd866c73ccac70"
-    sha256 cellar: :any,                 arm64_monterey: "0656460a081194749f8145ec92a301341a98bf744f49c10f80c585943817aa98"
-    sha256 cellar: :any,                 arm64_big_sur:  "e15443f94e1ef88f0ce67a9c8e3e63d6e5e26e56376c8e26edd8e0552cfdc111"
-    sha256 cellar: :any,                 ventura:        "39603727354eb99973d9e6c5e9c15befb255448fde85f764a837f4dccf391f63"
-    sha256 cellar: :any,                 monterey:       "96bfea5ee3d69286edd6b92db4f8f211aae8fc02ddf5bfa333379125f085ab28"
-    sha256 cellar: :any,                 big_sur:        "8ad28b466412f738bccf88be96aa4f167d771d3f06379f59b7aeb427f6a10fc7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2f18b550bc3afdc667b4a2282e84f603d278d9c957b6ccdd45b032c05162427a"
+    sha256 cellar: :any,                 arm64_ventura:  "d3af59a982fb0fa2f335a31dae74bc7b6edf4b8a54dd678018fcf4e1e8b60180"
+    sha256 cellar: :any,                 arm64_monterey: "00ffe52a19790b22caa22a1b8ddb4fe0f98005994893d0b83cd9f9d614695e00"
+    sha256 cellar: :any,                 arm64_big_sur:  "f45f2076d3f4abfd6d824ed97ee4fc1dc8caba81890b8029e41c1f9c9a075882"
+    sha256 cellar: :any,                 ventura:        "994095d46559b93a47d531b9db13de51a88e99e873a6b867cde833091d508a94"
+    sha256 cellar: :any,                 monterey:       "c66f9146360b2e7f16bdbed3a9307e2281745a21e06fc38cc1f2c8f856d0082d"
+    sha256 cellar: :any,                 big_sur:        "cf5e40e907f037c90a065d3c487caf0409707211f268ac314e02883f287642d6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "69d905b53ed94ccb162274756388b4f73fc6bdd7431560cc967590d93c9bacd2"
   end
 
   depends_on "cmake" => :build
