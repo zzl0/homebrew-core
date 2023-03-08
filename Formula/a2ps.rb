7837b865f3a5b5b1c1324b8740f8c4f630dc527f
Rui Chen <rui@chenrui.dev>
2023-03-07 20:00:13 -0500

    a2ps 4.15
    * a2ps 4.15
    * Update a2ps.rb
    * Update a2ps.rb
    * a2ps: update build
    
    Closes #125062.
    
    Co-authored-by: Dawid Dziurla <dawidd0811@gmail.com>
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/a2ps.rb
+++ Formula/a2ps.rb
@@ -1,9 +1,9 @@
 class A2ps < Formula
   desc "Any-to-PostScript filter"
   homepage "https://www.gnu.org/software/a2ps/"
-  url "https://ftp.gnu.org/gnu/a2ps/a2ps-4.14.tar.gz"
-  mirror "https://ftpmirror.gnu.org/a2ps/a2ps-4.14.tar.gz"
-  sha256 "f3ae8d3d4564a41b6e2a21f237d2f2b104f48108591e8b83497500182a3ab3a4"
+  url "https://ftp.gnu.org/gnu/a2ps/a2ps-4.15.tar.gz"
+  mirror "https://ftpmirror.gnu.org/a2ps/a2ps-4.15.tar.gz"
+  sha256 "a5adc5a9222f98448a57c6b5eb6948b72743eaf9a30c67a134df082e99c76652"
   license "GPL-3.0-or-later"
 
   bottle do
@@ -19,30 +19,18 @@ class A2ps < Formula
     sha256 x86_64_linux:   "063b4b31a62c4d5bd905bc4faab09ac2a50c77291de52ab216fc6a7a56f8e406"
   end
 
+  depends_on "pkg-config" => :build
+  depends_on "bdw-gc"
+  depends_on "libpaper"
   uses_from_macos "gperf"
 
-  on_macos do
-    # Software was last updated in 2007.
-    # https://trac.macports.org/ticket/18255
-    patch :p0 do
-      url "https://raw.githubusercontent.com/Homebrew/formula-patches/0ae366e6/a2ps/patch-contrib_sample_Makefile.in"
-      sha256 "5a34c101feb00cf52199a28b1ea1bca83608cf0a1cb123e6af2d3d8992c6011f"
-    end
-
-    # https://trac.macports.org/ticket/20867
-    patch :p0 do
-      url "https://raw.githubusercontent.com/Homebrew/formula-patches/0ae366e6/a2ps/patch-lib__xstrrpl.c"
-      sha256 "89fa3c95c329ec326e2e76493471a7a974c673792725059ef121e6f9efb05bf4"
-    end
-  end
-
   def install
-    # Work around configure issues with Xcode 12
-    ENV.append "CFLAGS", "-Wno-implicit-function-declaration"
-
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}", "--sysconfdir=#{etc}",
-                          "--with-lispdir=#{elisp}"
+    system "./configure", *std_configure_args,
+                          "--sysconfdir=#{etc}",
+                          "--with-lispdir=#{elisp}",
+                          "--with-packager=#{tap.user}",
+                          "--with-packager-version=#{pkg_version}",
+                          "--with-packager-bug-reports=#{tap.issues_url}"
     system "make", "install"
   end
 
