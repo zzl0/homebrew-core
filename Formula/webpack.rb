f4f685c81fc2912126ada79b95955ba7ddd1917d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-03-08 22:05:09 +0000

    webpack: update 5.76.0 bottle.

--- Formula/webpack.rb
+++ Formula/webpack.rb
@@ -10,14 +10,13 @@ class Webpack < Formula
   head "https://github.com/webpack/webpack.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a64dae4d651e7d6e4ccdf856ac5d619ca3c484fe637ff677dd4e61d6a5c84945"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a64dae4d651e7d6e4ccdf856ac5d619ca3c484fe637ff677dd4e61d6a5c84945"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a64dae4d651e7d6e4ccdf856ac5d619ca3c484fe637ff677dd4e61d6a5c84945"
-    sha256 cellar: :any_skip_relocation, ventura:        "808a689da5e87b61c7931561bf2f256b109f528048a8b74a5cf2ccae313ea800"
-    sha256 cellar: :any_skip_relocation, monterey:       "0db9d8a423809eb9f5bfe88ec7db1ab9c04c12aed70657d8623acda44d3bf999"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0db9d8a423809eb9f5bfe88ec7db1ab9c04c12aed70657d8623acda44d3bf999"
-    sha256 cellar: :any_skip_relocation, catalina:       "0db9d8a423809eb9f5bfe88ec7db1ab9c04c12aed70657d8623acda44d3bf999"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3b4662df24c313a4c2eea8f298cde61f536353203539eac93ee9b204a3b6b792"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "2cb22275949817e42327f19603cec5a2bae246ffcc8584be30ff0e8dc780b53b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2cb22275949817e42327f19603cec5a2bae246ffcc8584be30ff0e8dc780b53b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2cb22275949817e42327f19603cec5a2bae246ffcc8584be30ff0e8dc780b53b"
+    sha256 cellar: :any_skip_relocation, ventura:        "575cd2bc1039abbf559d061fb60ee0e0a89ad5befa532491b60a88aca0462c66"
+    sha256 cellar: :any_skip_relocation, monterey:       "575cd2bc1039abbf559d061fb60ee0e0a89ad5befa532491b60a88aca0462c66"
+    sha256 cellar: :any_skip_relocation, big_sur:        "575cd2bc1039abbf559d061fb60ee0e0a89ad5befa532491b60a88aca0462c66"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2cb22275949817e42327f19603cec5a2bae246ffcc8584be30ff0e8dc780b53b"
   end
 
   depends_on "node"
