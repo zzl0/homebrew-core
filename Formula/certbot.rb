5322fb338674020cf7d6acd303d01840c440d87c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-03-08 02:31:48 +0000

    certbot: update 2.4.0 bottle.

--- Formula/certbot.rb
+++ Formula/certbot.rb
@@ -9,13 +9,13 @@ class Certbot < Formula
   head "https://github.com/certbot/certbot.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "cef89be345ce793e277330e1eb6bc45acab6a90c11b11055e0ac4514e71327ad"
-    sha256 cellar: :any,                 arm64_monterey: "9f8bd89f6aa4f1e69756db65a9ec973a6b7b041f4e8c22f3f3238a0471b3e329"
-    sha256 cellar: :any,                 arm64_big_sur:  "dd2ce8ca372869d0c60e15e169a6905cc70ac4e41c0397515de3a00515999236"
-    sha256 cellar: :any,                 ventura:        "5aa964444683b70b813a7d9ac2717f83e61a5204b0310c39797e5f466b271b59"
-    sha256 cellar: :any,                 monterey:       "19dc0c7eabdc4192d40e5f802155ddc5465abff3e879a1c3475777ee1c0ae1af"
-    sha256 cellar: :any,                 big_sur:        "b761f2dbb30d227aaaaa5a22d8bdbf3dd7295f6f52c373aa8079ff3b6cbf92df"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9bd1fbe5a75127111d13f3811551711ec7985aad83885cfa93c2a4b179839fd5"
+    sha256 cellar: :any,                 arm64_ventura:  "0d8cf9b2fc280ce121451d88fe387e175816617be33a0c184a0adae728fc9b9c"
+    sha256 cellar: :any,                 arm64_monterey: "e44499389118247ead84a86551c9fb51012cc8a6e5f9d9b44d17e23e5dc415af"
+    sha256 cellar: :any,                 arm64_big_sur:  "8147419ed0f4b3aa328cda397f67ce3d5e29e3c12ac2728c03a881e0f688d975"
+    sha256 cellar: :any,                 ventura:        "b6cb1afd2125aa4f2fc369b01e60b1766d92f554ddf6dc7f6a4532fb07e5f298"
+    sha256 cellar: :any,                 monterey:       "cc55eae69c02930af8ce5a6a7166d83bfd8597bd2c2e00cc70d4922563a5c9f9"
+    sha256 cellar: :any,                 big_sur:        "e90a558270fc6a3d415b4b2a4756f85af36f35b68fb78f14a063842be656fbff"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0af68b47de27b0bdd72ab25876308eff36ecc575657180afdb4c6a3d544f9b3a"
   end
 
   depends_on "rust" => :build # for cryptography
