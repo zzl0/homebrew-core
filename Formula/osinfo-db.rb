7708073447bc13f5764f5dfbbe3eee7a0bd08ee9
Patrick Linnane <patrick@linnane.io>
2023-03-08 09:15:46 -0800

    osinfo-db 20230308
    Closes #125135.
    
    Signed-off-by: Ruoyu Zhong <zhongruoyu@outlook.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/osinfo-db.rb
+++ Formula/osinfo-db.rb
@@ -1,8 +1,8 @@
 class OsinfoDb < Formula
   desc "Osinfo database of operating systems for virtualization provisioning tools"
   homepage "https://libosinfo.org/"
-  url "https://releases.pagure.org/libosinfo/osinfo-db-20221130.tar.xz"
-  sha256 "a2954cad7a1bef2679949987c63888f1e081b09e11fb725054372f037ec7717f"
+  url "https://releases.pagure.org/libosinfo/osinfo-db-20230308.tar.xz"
+  sha256 "546ba04ecc5e933ba2d7f3f3b4333a2980d4ae4dfc5284989b9c54758f2b9088"
   license "GPL-2.0-or-later"
 
   livecheck do
