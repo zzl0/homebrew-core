cb984c86901f20ca48ea3c632f0426792d51308b
Rui Chen <rui@chenrui.dev>
2023-03-08 08:58:52 -0500

    frpc 0.48.0
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/frpc.rb
+++ Formula/frpc.rb
@@ -2,8 +2,8 @@ class Frpc < Formula
   desc "Client app of fast reverse proxy to expose a local server to the internet"
   homepage "https://github.com/fatedier/frp"
   url "https://github.com/fatedier/frp.git",
-      tag:      "v0.47.0",
-      revision: "88e74ff24d7f2cbbae536904fe00324b195a278d"
+      tag:      "v0.48.0",
+      revision: "8fb99ef7a99c7a87065247d60be4a08218afa60b"
   license "Apache-2.0"
 
   bottle do
