77564e16bb5df6ef0163d853028a95a6d6727e1d
Porkepix <porkepix@gmail.com>
2023-03-08 01:33:45 +0100

    openliberty-webprofile8 23.0.0.2
    Closes #125059.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Ruoyu Zhong <zhongruoyu@outlook.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/openliberty-webprofile8.rb
+++ Formula/openliberty-webprofile8.rb
@@ -1,8 +1,8 @@
 class OpenlibertyWebprofile8 < Formula
   desc "Lightweight open framework for Java (Jakarta EE Web Profile 8)"
   homepage "https://openliberty.io"
-  url "https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/release/23.0.0.1/openliberty-webProfile8-23.0.0.1.zip"
-  sha256 "1910072fa412a4b0d080a2cf082be32b5b1c60c6088ecde85e51e007bc0156e4"
+  url "https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/release/23.0.0.2/openliberty-webProfile8-23.0.0.2.zip"
+  sha256 "59c6766ae1b4ac68c45fe5aaeed014aa19869e8509d037361424c2c681bbd103"
   license "EPL-1.0"
 
   livecheck do
