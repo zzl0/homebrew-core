4d0b7f8ef76b18b282459b3ea0c9ebbce03dd9e9
Dawid Dziurla <9713907+dawidd6@users.noreply.github.com>
2023-03-09 10:04:15 +0000

    okta-aws-cli: add 0.2.1 bottle.

--- Formula/okta-aws-cli.rb
+++ Formula/okta-aws-cli.rb
@@ -5,6 +5,16 @@ class OktaAwsCli < Formula
   sha256 "06374f0eb3e371a8ef1a5e8fb2bcd0e5bfcac607aa2083f6c1101b54713a47bf"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "375c1e6b9dfbee2a4e0bd105cc713a0fe4f72f13b71834b02d69098a54ba838f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "375c1e6b9dfbee2a4e0bd105cc713a0fe4f72f13b71834b02d69098a54ba838f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "375c1e6b9dfbee2a4e0bd105cc713a0fe4f72f13b71834b02d69098a54ba838f"
+    sha256 cellar: :any_skip_relocation, ventura:        "91a2bfe1595bbf1300b72bd7c81e25072c6ceffe357457df815c87e4a805f046"
+    sha256 cellar: :any_skip_relocation, monterey:       "91a2bfe1595bbf1300b72bd7c81e25072c6ceffe357457df815c87e4a805f046"
+    sha256 cellar: :any_skip_relocation, big_sur:        "91a2bfe1595bbf1300b72bd7c81e25072c6ceffe357457df815c87e4a805f046"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "72b02559723af7c5701ec7002a8650d9e97ebe68132d8c7a92136a2cdc5dc201"
+  end
+
   depends_on "go" => :build
 
   def install
