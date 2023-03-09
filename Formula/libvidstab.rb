233bb353357f8c09f2244a2ca1487b0af88341c8
Patrick Linnane <patrick@linnane.io>
2023-03-09 14:48:21 -0800

    libvidstab 1.1.1
    * libvidstab 1.1.1
    * libvidstab: remove build patch

--- Formula/libvidstab.rb
+++ Formula/libvidstab.rb
@@ -1,8 +1,8 @@
 class Libvidstab < Formula
   desc "Transcode video stabilization plugin"
   homepage "http://public.hronopik.de/vid.stab/"
-  url "https://github.com/georgmartius/vid.stab/archive/v1.1.0.tar.gz"
-  sha256 "14d2a053e56edad4f397be0cb3ef8eb1ec3150404ce99a426c4eb641861dc0bb"
+  url "https://github.com/georgmartius/vid.stab/archive/v1.1.1.tar.gz"
+  sha256 "9001b6df73933555e56deac19a0f225aae152abbc0e97dc70034814a1943f3d4"
   license "GPL-2.0-or-later"
 
   bottle do
@@ -20,16 +20,6 @@ class Libvidstab < Formula
   end
 
   depends_on "cmake" => :build
-
-  # A bug in the FindSSE CMake script means that, if a variable is defined
-  # as an empty string without quoting, it doesn't get passed to a function
-  # and CMake throws an error. This only occurs on ARM, because the
-  # sysctl value being checked is always a non-empty string on Intel.
-  # Upstream PR: https://github.com/georgmartius/vid.stab/pull/93
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/5bf1a0e0cfe666ee410305cece9c9c755641bfdf/libvidstab/fix_cmake_quoting.patch"
-    sha256 "45c16a2b64ba67f7ca5335c2f602d8d5186c29b38188b3cc7aff5df60aecaf60"
-  end
 
   def install
     system "cmake", ".", "-DUSE_OMP=OFF", *std_cmake_args
