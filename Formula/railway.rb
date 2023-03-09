111f98f0815f3bedba1e69ddac282d6197a42e0f
Rui Chen <rui@chenrui.dev>
2023-03-09 08:15:00 -0500

    railway 3.0.8
    Closes #125200.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/railway.rb
+++ Formula/railway.rb
@@ -1,8 +1,8 @@
 class Railway < Formula
   desc "Develop and deploy code with zero configuration"
   homepage "https://railway.app/"
-  url "https://github.com/railwayapp/cli/archive/refs/tags/v3.0.6.tar.gz"
-  sha256 "42dd87deb73bf07de5fce0ecd250980d47275807fe7cf7a453d532d468d9e9ed"
+  url "https://github.com/railwayapp/cli/archive/refs/tags/v3.0.8.tar.gz"
+  sha256 "d8a679e5bae8276c3c7500dbfd51ed0c789413f42b72d3d1ca8915869f93c9e7"
   license "MIT"
   head "https://github.com/railwayapp/cli.git", branch: "master"
 
