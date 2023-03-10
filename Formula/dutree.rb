51db25d09e3146484864095caf9b723c14437676
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-03-10 14:10:24 +0000

    dutree: add 0.2.15 bottle.

--- Formula/dutree.rb
+++ Formula/dutree.rb
@@ -6,6 +6,16 @@ class Dutree < Formula
   license "GPL-3.0-only"
   head "https://github.com/nachoparker/dutree.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "2cf132b20727d426b4db9cf9002881fd1a6deead6c7946d1fb587fee88b3f297"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ce3b7ffe71986b87939fa203fb329153181a09e3959dc80074e14bdabfce2bf9"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ff5818c530c77c1213688ddc314fb26c0f9c2e117cf6b014ed012cd40525c760"
+    sha256 cellar: :any_skip_relocation, ventura:        "2ccacb1b47b7c491dfe861c28b63ee1c134cf798494e4626cff8affaabccd1b9"
+    sha256 cellar: :any_skip_relocation, monterey:       "53b9d1818729192217fe07029f62de3fbdd468933487e881135455f15f05b254"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d023124a064c2be039be8d2fb1629c9688a94a71359392a5c825e3304b958246"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e8ff2f1f3ea275dfef72124d45733be37ebf069eb4bb20bed09a33f3e2d68420"
+  end
+
   depends_on "rust" => :build
 
   def install
