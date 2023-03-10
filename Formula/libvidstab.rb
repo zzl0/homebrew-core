d64ea1fbec56da65a3fc18ad784739f913430ff0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-03-10 06:42:35 +0000

    libvidstab: update 1.1.1 bottle.

--- Formula/libvidstab.rb
+++ Formula/libvidstab.rb
@@ -6,17 +6,13 @@ class Libvidstab < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_ventura:  "e039ff1cf26ac9c1394519f31ff22894a803abe3393ecdbfd422fe8e092b6986"
-    sha256 cellar: :any,                 arm64_monterey: "ece699a3cc725790bad5d1153f0203e06bfd64427f2b4915e4f4778a75d59635"
-    sha256 cellar: :any,                 arm64_big_sur:  "b98be46d2375a1e6b30947b31c981009785a7c0e97c31ca0c64a52228b0d1576"
-    sha256 cellar: :any,                 ventura:        "1572202724878ea4ebe12390dfe1b4919b14572a6ac4c15ba533413c07c3823b"
-    sha256 cellar: :any,                 monterey:       "8ca80c30e8cbd76cf6aa593e39da3f0579ce60edbbb5bd4039b34b3cc00f493c"
-    sha256 cellar: :any,                 big_sur:        "b4c67e80b92e95aa19520b0b130a60cc3949db7899d9d02520d32d9fc62ec837"
-    sha256 cellar: :any,                 catalina:       "df23e5e7933b6535f34c429ee8286e4d9dec6d0a2349cf3256f44ec687e7968f"
-    sha256 cellar: :any,                 mojave:         "783224577a1cc7a57de76eac74b00aac69e7fe15c920d26454e58a369854974f"
-    sha256 cellar: :any,                 high_sierra:    "d3a80889cbeaa5a8af0abc5037c35afefb181e902b79f4f986a6b4c4e29d88a5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1d3241dc43becd382cf761a8b79a253696f52d3ffeb4f762d82d9f9458152d57"
+    sha256 cellar: :any,                 arm64_ventura:  "25efabe3bf9a85b25065758c1ea62ef096bf2e334ce073450ef4478f7e469b38"
+    sha256 cellar: :any,                 arm64_monterey: "0bff0aa6aba039cc22d6a65fbfd46f67c35ecc6db3baca439b9b1e45c3710002"
+    sha256 cellar: :any,                 arm64_big_sur:  "bca6a787bd17369819451093009d298fc8a4104be1d9bd25bd9606263d097784"
+    sha256 cellar: :any,                 ventura:        "40cc585d8dfe08ddb7938667997fac65cd96261f9699c4d7a1705e30267cdb61"
+    sha256 cellar: :any,                 monterey:       "8caa0dce4af772a443f814f51960227e769428025f734a9a82085fc8ee499ef9"
+    sha256 cellar: :any,                 big_sur:        "2b7d9891009c53a925e971b600bad2a43e5bcca062119b9080e1c5b59e2e25b0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d87451266aa81a807919bb25dec5cb85e2e79eca8fa768dba8c7b74f06e11be9"
   end
 
   depends_on "cmake" => :build
