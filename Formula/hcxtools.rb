406dd340a5830531457c73428e8e9a0d51e89a5c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-03-10 09:04:59 +0000

    hcxtools: update 6.2.8 bottle.

--- Formula/hcxtools.rb
+++ Formula/hcxtools.rb
@@ -7,15 +7,13 @@ class Hcxtools < Formula
   head "https://github.com/ZerBea/hcxtools.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_ventura:  "8185ea5bf24763e28884679562966c84985056eebb4dbb69ff1e022c91ffabf6"
-    sha256 cellar: :any,                 arm64_monterey: "6156b1ef29b02fdadfbdc54f71b3a078b96d3d8b9796dbd04718867dd8b00456"
-    sha256 cellar: :any,                 arm64_big_sur:  "186091dc51a382aa1333327948c39332ee2f27d486c2f1fc53078ffbd4ea3310"
-    sha256 cellar: :any,                 ventura:        "eb789fb97c96d8317a35b641c9aa81ac1b894667f32a6ffc8c25f76c19c49e9e"
-    sha256 cellar: :any,                 monterey:       "5904fbf23939d2088b281db6b7b57644489b56abc853359e5d7816c5d58de976"
-    sha256 cellar: :any,                 big_sur:        "5958acc14ba6fdf7f0caf618ebf8df79adfea1f25fd639010691e9540aea4304"
-    sha256 cellar: :any,                 catalina:       "52b1f37012a6a76b4cdecc0bfb8675fdb1ce04119d8910602121f0727fdf7dd9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7b34c7c93cbe7fe057686b5c46a68b91d7a618d413d1052ff61acd422d869686"
+    sha256 cellar: :any,                 arm64_ventura:  "cd520809604df090a7c797e3bd71563aee5c0e28bb16b1996df13e0c8800c79b"
+    sha256 cellar: :any,                 arm64_monterey: "c7323b1a2a5bf0c7f716e4c82dd19cae28047387e7c3940794987b714992c91d"
+    sha256 cellar: :any,                 arm64_big_sur:  "9953aa1c8fb8f595b9fc93f5c0a1a4cf0444e566e0a03eb4a6474c2d4a15f283"
+    sha256 cellar: :any,                 ventura:        "0b9e4c8d20b70c24ead9ace5ee14d615d9956a199a5c3fc0f2728f8490fdf30a"
+    sha256 cellar: :any,                 monterey:       "5c4009d927e4f73728dbbec849eefe083848b9bfb537525e92b53f8caf8c8781"
+    sha256 cellar: :any,                 big_sur:        "05c7aecb385d87e318310cee951bab88430d97499e508b362193c56463aefa55"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b93a81fdbc7388ce7e1f168e6cb5ba2eb9f512f7857da23ec65d11458e8160ba"
   end
 
   depends_on "pkg-config" => :build
