c8eca3e74a23914b0360cb9bc3d082f42e8f5b06
Rui Chen <rui@chenrui.dev>
2023-03-10 01:08:12 -0500

    libimagequant 4.1.1
    Closes #125291.
    
    Signed-off-by: Ruoyu Zhong <zhongruoyu@outlook.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libimagequant.rb
+++ Formula/libimagequant.rb
@@ -1,8 +1,8 @@
 class Libimagequant < Formula
   desc "Palette quantization library extracted from pnquant2"
   homepage "https://pngquant.org/lib/"
-  url "https://github.com/ImageOptim/libimagequant/archive/4.1.0.tar.gz"
-  sha256 "7673521655cdf0ec16b99fd0c7c8b1bd542b6f3a8469fa2effe34b0f53e9fa92"
+  url "https://github.com/ImageOptim/libimagequant/archive/4.1.1.tar.gz"
+  sha256 "726ce0bf2a8175a32d1ef303c3e8971a1fda354e760f1327c08656268bb019c2"
   license :cannot_represent
 
   bottle do
