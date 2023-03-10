6151a2d8c0bbe2b3bc116212e5ce9c7bb9e2f66d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-03-10 06:42:04 +0000

    goose: update 3.10.0 bottle.

--- Formula/goose.rb
+++ Formula/goose.rb
@@ -6,13 +6,13 @@ class Goose < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8fd235460429b5899cdf8ea5341eee4aad7e8bd6341b7939619ba13839e96ecb"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "19d5ff4709f1cf956a1a67167daae1343cdafe4b5b17c57d52810df42e5d3745"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "69f227d890a9137360c96293a9d54c8c98599cc7f8084cdceba5af802e27a44a"
-    sha256 cellar: :any_skip_relocation, ventura:        "a24ca911c02580cdcb88172ec29e23840be7415265c2134e671580a508252680"
-    sha256 cellar: :any_skip_relocation, monterey:       "a0d4bd96f4000db1f27f98a9f2aadfbe55c5ce0f70554b34e6e8ea14b3c10f28"
-    sha256 cellar: :any_skip_relocation, big_sur:        "9140c30159af04615db62d7c45e6974f9a9163f186bfdd9f5df35839e7e40d56"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "56f1724a08d39e13e3f9f1419874129c593d041ccc791b1997283d9843aba1bc"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "9135daff0a43b932861d5bb3e1789eeb2438803ecf3f83b5c5dc725733a9e024"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "92ee69e093cc61158687de8894276ce899516c8599ee60415df7e05df9c878fc"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d747c7e2f64ca820be051abf2a50a7f2769b82849272dbc704baeaa00f9d55d8"
+    sha256 cellar: :any_skip_relocation, ventura:        "355fae4658f8113c280d9616d550af7600529159fa20e41bef8396b45d22796b"
+    sha256 cellar: :any_skip_relocation, monterey:       "6e11002dadcb91a731d7e512ca39fdcd0aa4f4a5e70c31fe6211b379676ea56b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "a074a76ade74ded15fa0ea580751a895a68dea57ddd1bfcb4babecefc4a84bc3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "da824fadc7285f0d3523c94e93eeaf15308d9edd1a43ba20d74a4a8f5a99aa13"
   end
 
   depends_on "go" => :build
