2015002e3bd860d7850cc7ef551157d0de3cef3e
Patrick Linnane <patrick@linnane.io>
2023-03-10 15:46:43 -0800

    glooctl 1.13.10
    Closes #125350.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/glooctl.rb
+++ Formula/glooctl.rb
@@ -4,8 +4,8 @@ class Glooctl < Formula
   # NOTE: Please wait until the newest stable release is finished building and
   # no longer marked as "Pre-release" before creating a PR for a new version.
   url "https://github.com/solo-io/gloo.git",
-      tag:      "v1.13.9",
-      revision: "43cefbf5d76037c11d6e7ced20b541bd31369d97"
+      tag:      "v1.13.10",
+      revision: "7fd876f4e4c92a2aaa99ae8e9774f7a35277cb91"
   license "Apache-2.0"
   head "https://github.com/solo-io/gloo.git", branch: "master"
 
