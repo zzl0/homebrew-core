ac8d0c97254dc7a1e78afec7590b56bcace212e2
Patrick Linnane <patrick@linnane.io>
2023-03-10 13:17:36 -0800

    lacework-cli 1.13.2 (new formula)
    * lacework-cli 1.13.2 (new formula)
    * Apply suggestions from code review
    
    Closes #125339.
    
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/lacework-cli.rb
@@ -0,0 +1,28 @@
+class LaceworkCli < Formula
+  desc "CLI for managing Lacework"
+  homepage "https://docs.lacework.com/cli"
+  url "https://github.com/lacework/go-sdk.git",
+      tag:      "v1.13.2",
+      revision: "829f36abd73a6e85757869e998516368800e63a5"
+  license "Apache-2.0"
+  head "https://github.com/lacework/go-sdk.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/lacework/go-sdk/cli/cmd.Version=#{version}
+      -X github.com/lacework/go-sdk/cli/cmd.GitSHA=#{Utils.git_head}
+      -X github.com/lacework/go-sdk/cli/cmd.BuildTime=#{time.iso8601}
+    ]
+    system "go", "build", *std_go_args(output: bin/"lacework", ldflags: ldflags), "./cli"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/lacework version")
+
+    output = shell_output("#{bin}/lacework configure list 2>&1", 1)
+    assert_match "ERROR unable to load profiles. No configuration file found.", output
+  end
+end
