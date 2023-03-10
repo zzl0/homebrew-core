d0d47eeb4731570a0bda0190b16d32fb1cc7fc2a
Patrick Linnane <patrick@linnane.io>
2023-03-10 15:46:03 -0800

    flank 23.03.2
    Closes #125349.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/flank.rb
+++ Formula/flank.rb
@@ -1,8 +1,8 @@
 class Flank < Formula
   desc "Massively parallel Android and iOS test runner for Firebase Test Lab"
   homepage "https://github.com/Flank/flank"
-  url "https://github.com/Flank/flank/releases/download/v23.03.1/flank.jar"
-  sha256 "d5c97accc9bb79ffe165fedc877b38f0ebb8284a61f1e84f1948fdd2cdcf10d1"
+  url "https://github.com/Flank/flank/releases/download/v23.03.2/flank.jar"
+  sha256 "e95d92a3f8044fb990308b50d9613ccde43376af3e745417e6e7c63b11d5968f"
   license "Apache-2.0"
 
   livecheck do
