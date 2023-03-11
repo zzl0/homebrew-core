1ed0021fac4880b1f0ce51e95a93b2b42f5947a1
Bennett Panesar <bennett@tfllab.com>
2023-03-10 20:13:26 -0500

    zlint 3.4.1 (new formula)
    Closes #125367.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/zlint.rb
@@ -0,0 +1,45 @@
+class Zlint < Formula
+  desc "X.509 Certificate Linter focused on Web PKI standards and requirements"
+  homepage "https://github.com/zmap/zlint"
+  url "https://github.com/zmap/zlint.git",
+      tag:      "v3.4.1",
+      revision: "84da589869bc2d096b7ae42606bf75c602d88806"
+  license "Apache-2.0"
+  head "https://github.com/zmap/zlint.git", branch: "master"
+
+  depends_on "go" => :build
+
+  def install
+    system "make", "--directory=v3", "GIT_VERSION=v#{version}", "zlint"
+    bin.install "v3/zlint"
+  end
+
+  test do
+    assert_match "ZLint version v#{version}",
+      shell_output("#{bin}/zlint -version")
+
+    (testpath/"cert.pem").write <<~EOS
+      -----BEGIN CERTIFICATE-----
+      MIIB3jCCAWSgAwIBAgIUU3hxzxSDV5V1DeRyZjgzdPKatBEwCgYIKoZIzj0EAwMw
+      HjEcMBoGA1UEAwwTaG9tZWJyZXctemxpbnQtdGVzdDAeFw0yMjAxMDEwMDAwMDBa
+      Fw0yMzAxMDEwMDAwMDBaMB4xHDAaBgNVBAMME2hvbWVicmV3LXpsaW50LXRlc3Qw
+      djAQBgcqhkjOPQIBBgUrgQQAIgNiAASn3DDorzrDQiWnnXv0uS722si9zx1HK6Va
+      CXQpHm/8t1SmMEYdVIU4j5UzbVKpoMIkk9twC3AiDUVZwdBNL2rqO8smZiKOh0Tz
+      BnRf8OBu55C7fsCHRayljjW0IpyZCjCjYzBhMB0GA1UdDgQWBBRIDxULqVXg4e4Z
+      +3QzKRG4UpfiFjAfBgNVHSMEGDAWgBRIDxULqVXg4e4Z+3QzKRG4UpfiFjAPBgNV
+      HRMBAf8EBTADAQH/MA4GA1UdDwEB/wQEAwIBhjAKBggqhkjOPQQDAwNoADBlAjBO
+      c1zwDbnya5VkmlROFco5TpcZM7w1L4eRFdJ/q7rZF5udqVuy4vtu0dJaazwiMusC
+      MQDEMciPyBdrKwnJilT2kVwIMdMmxAjcmV048Ai0CImT5iRERKdBa7QeydMcJo3Z
+      7zs=
+      -----END CERTIFICATE-----
+    EOS
+
+    output = shell_output("#{bin}/zlint -longSummary cert.pem")
+    %w[
+      e_ca_organization_name_missing
+      e_ca_country_name_missing
+    ].each do |err|
+      assert_match err, output
+    end
+  end
+end
