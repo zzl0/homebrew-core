93f5ca20e20cbbab24a576b2c1bc62dff6c5d550
Rui Chen <rui@chenrui.dev>
2023-03-11 18:24:23 -0500

    i2pd 2.47.0
    Closes #125416.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/i2pd.rb
+++ Formula/i2pd.rb
@@ -1,8 +1,8 @@
 class I2pd < Formula
   desc "Full-featured C++ implementation of I2P client"
   homepage "https://i2pd.website/"
-  url "https://github.com/PurpleI2P/i2pd/archive/2.46.1.tar.gz"
-  sha256 "76b41d02a41a03d627fcd7fe695cad7f521b66e99a04ec9678f132a1eb052bb8"
+  url "https://github.com/PurpleI2P/i2pd/archive/2.47.0.tar.gz"
+  sha256 "c988baf23215c37d5f566b7b2059a3c168c78d157eac6dc04a30ac266c6335f0"
   license "BSD-3-Clause"
 
   bottle do
