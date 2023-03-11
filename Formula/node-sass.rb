af1629a53fd274730b879e975eae3f6d48ab189e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-03-11 05:05:01 +0000

    node-sass: update 1.59.2 bottle.

--- Formula/node-sass.rb
+++ Formula/node-sass.rb
@@ -8,13 +8,13 @@ class NodeSass < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "591486113c61e5158fc36449da590bbd0f5b6b0db9cecd2bb86b49fb579dc6e5"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "591486113c61e5158fc36449da590bbd0f5b6b0db9cecd2bb86b49fb579dc6e5"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "591486113c61e5158fc36449da590bbd0f5b6b0db9cecd2bb86b49fb579dc6e5"
-    sha256 cellar: :any_skip_relocation, ventura:        "591486113c61e5158fc36449da590bbd0f5b6b0db9cecd2bb86b49fb579dc6e5"
-    sha256 cellar: :any_skip_relocation, monterey:       "591486113c61e5158fc36449da590bbd0f5b6b0db9cecd2bb86b49fb579dc6e5"
-    sha256 cellar: :any_skip_relocation, big_sur:        "591486113c61e5158fc36449da590bbd0f5b6b0db9cecd2bb86b49fb579dc6e5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fc09134b51664c453bda40541aefe9d14badd90e53ef46e920bfebd9e1aed54d"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "22ef9baacfe259d1ae6373c242b2705bcc3f51d4d41c193ae5622c47ce7162fa"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "22ef9baacfe259d1ae6373c242b2705bcc3f51d4d41c193ae5622c47ce7162fa"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "22ef9baacfe259d1ae6373c242b2705bcc3f51d4d41c193ae5622c47ce7162fa"
+    sha256 cellar: :any_skip_relocation, ventura:        "22ef9baacfe259d1ae6373c242b2705bcc3f51d4d41c193ae5622c47ce7162fa"
+    sha256 cellar: :any_skip_relocation, monterey:       "22ef9baacfe259d1ae6373c242b2705bcc3f51d4d41c193ae5622c47ce7162fa"
+    sha256 cellar: :any_skip_relocation, big_sur:        "22ef9baacfe259d1ae6373c242b2705bcc3f51d4d41c193ae5622c47ce7162fa"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5da30cc64ba2c8fcf312cafbd9d9a758951880ebaa3584b2ffc1d97201856824"
   end
 
   depends_on "node"
