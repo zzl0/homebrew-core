b34a87b0043845d09c95bd8178cb6d097c586678
Rui Chen <rui@chenrui.dev>
2023-03-12 06:21:03 +0000

    picotool: add 1.1.1 bottle.

--- Formula/picotool.rb
+++ Formula/picotool.rb
@@ -13,6 +13,16 @@ class Picotool < Formula
     end
   end
 
+  bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "d56b7760c3d5a77669d1e69ebf3dbdac4b0d6680d01d438bb447bf0c09aa549e"
+    sha256 cellar: :any,                 arm64_monterey: "6cf9bc19040bbcdfb70a6ea547c22a6480f53168f68fcd8474f45f8701cf03c3"
+    sha256 cellar: :any,                 arm64_big_sur:  "573308465e29e22d7ea592aa0a835f861695921194432feb9a69fc99b93aab62"
+    sha256 cellar: :any,                 ventura:        "4d5d2046ad99ada6f69ca50ad8bc8f08c2b33ec6082c9395d3ec8de4735c4da9"
+    sha256 cellar: :any,                 monterey:       "62f2fc8a6f196cbf98f6fbc1d5807c933cf695511e8a9258cc46cb727426a58f"
+    sha256 cellar: :any,                 big_sur:        "18791cd0bf1b69a5215d232f56425a02b4d44a890f3af087931b8e287f32c0f6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a50cabfece6ec443857dc7290ea69bdf5ca889cccc610c8043f954cf061c8e0b"
+  end
+
   head do
     url "https://github.com/raspberrypi/picotool.git", branch: "master"
 
