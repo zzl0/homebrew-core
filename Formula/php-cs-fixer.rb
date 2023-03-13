dc9717b2a755b579482beca6cfcc2ae05f51ed26
Porkepix <porkepix@gmail.com>
2023-03-13 04:04:01 +0100

    php-cs-fixer 3.15.0
    * php-cs-fixer 3.15.0
    * php-cs-fixer: update php reference
    
    Closes #125488.
    
    Co-authored-by: Branch Vincent <branchevincent@gmail.com>
    Signed-off-by: Ruoyu Zhong <zhongruoyu@outlook.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/php-cs-fixer.rb
+++ Formula/php-cs-fixer.rb
@@ -1,9 +1,8 @@
 class PhpCsFixer < Formula
   desc "Tool to automatically fix PHP coding standards issues"
   homepage "https://cs.symfony.com/"
-  # Bump to php 8.2 on the next release, if possible.
-  url "https://github.com/FriendsOfPHP/PHP-CS-Fixer/releases/download/v3.14.4/php-cs-fixer.phar"
-  sha256 "c0a023616e6cac6286bb478247ee63e070e5f31a0a0c85bf23e8720a9668f142"
+  url "https://github.com/FriendsOfPHP/PHP-CS-Fixer/releases/download/v3.15.0/php-cs-fixer.phar"
+  sha256 "cd7b675680c8365b67742c77e62b92996e8f3439d314a306ffed86c7a2d6bf1d"
   license "MIT"
 
   bottle do
@@ -16,13 +15,13 @@ class PhpCsFixer < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "c78eff6f4d8ae00266717f8d7b909e6f02e3e898a0892c151cfbaf6c7f573ba9"
   end
 
-  depends_on "php@8.1"
+  depends_on "php"
 
   def install
     libexec.install "php-cs-fixer.phar"
 
     (bin/"php-cs-fixer").write <<~EOS
-      #!#{Formula["php@8.1"].opt_bin}/php
+      #!#{Formula["php"].opt_bin}/php
       <?php require '#{libexec}/php-cs-fixer.phar';
     EOS
   end
@@ -37,7 +36,7 @@ class PhpCsFixer < Formula
       $this->foo('homebrew rox');
     EOS
 
-    system "#{bin}/php-cs-fixer", "fix", "test.php"
+    system bin/"php-cs-fixer", "fix", "test.php"
     assert compare_file("test.php", "correct_test.php")
   end
 end
