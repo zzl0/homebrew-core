ce7a7f88a26aeea4883348a19227abda20e18aa6
Porkepix <porkepix@gmail.com>
2023-03-13 13:22:44 +0100

    makeself 2.5.0
    Closes #125510.
    
    Signed-off-by: Ruoyu Zhong <zhongruoyu@outlook.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/makeself.rb
+++ Formula/makeself.rb
@@ -1,8 +1,8 @@
 class Makeself < Formula
   desc "Generates a self-extracting compressed tar archive"
   homepage "https://makeself.io/"
-  url "https://github.com/megastep/makeself/archive/release-2.4.5.tar.gz"
-  sha256 "91deafdbfddf130abe67d7546f0c50be6af6711bb1c351b768043bd527bd6e45"
+  url "https://github.com/megastep/makeself/archive/release-2.5.0.tar.gz"
+  sha256 "705d0376db9109a8ef1d4f3876c9997ee6bed454a23619e1dbc03d25033e46ea"
   license "GPL-2.0-or-later"
   head "https://github.com/megastep/makeself.git", branch: "master"
 
