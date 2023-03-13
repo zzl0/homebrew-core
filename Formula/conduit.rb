aa4823b376aae9db2619be3b5e3ff2878c0a068f
maha-hajja <maha.d.hajja@gmail.com>
2023-03-13 12:01:44 +0000

    conduit 0.5.4
    Closes #125505.
    
    Signed-off-by: Dawid Dziurla <9713907+dawidd6@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/conduit.rb
+++ Formula/conduit.rb
@@ -1,8 +1,8 @@
 class Conduit < Formula
   desc "Streams data between data stores. Kafka Connect replacement. No JVM required"
   homepage "https://conduit.io/"
-  url "https://github.com/ConduitIO/conduit/archive/refs/tags/v0.5.0.tar.gz"
-  sha256 "ba57506c17a99356c443d3c4459977c825dcce1b039965ba7699140e11d95afc"
+  url "https://github.com/ConduitIO/conduit/archive/refs/tags/v0.5.4.tar.gz"
+  sha256 "812e11636912cdbd5d0434b25fd305c3854c946aa73d6e83537584e54185d7fb"
   license "Apache-2.0"
   head "https://github.com/ConduitIO/conduit.git", branch: "main"
 
