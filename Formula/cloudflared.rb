edf8ce404999189b871e9f354f73422717d2124f
Patrick Linnane <patrick@linnane.io>
2023-03-14 10:53:24 -0700

    cloudflared 2023.3.1
    Closes #125643.
    
    Signed-off-by: Ruoyu Zhong <zhongruoyu@outlook.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cloudflared.rb
+++ Formula/cloudflared.rb
@@ -1,8 +1,8 @@
 class Cloudflared < Formula
   desc "Cloudflare Tunnel client (formerly Argo Tunnel)"
   homepage "https://developers.cloudflare.com/cloudflare-one/connections/connect-apps/install-and-setup/tunnel-guide"
-  url "https://github.com/cloudflare/cloudflared/archive/refs/tags/2023.3.0.tar.gz"
-  sha256 "90fad1f6eb59689e06d09837c5ec9a3e7e3d177fea15190027f60c5b9e7950be"
+  url "https://github.com/cloudflare/cloudflared/archive/refs/tags/2023.3.1.tar.gz"
+  sha256 "cdd0f02fc4170842c8210db2b700bad88d8a7b5d00fb8f7336073737f11fc718"
   license "Apache-2.0"
   head "https://github.com/cloudflare/cloudflared.git", branch: "master"
 
