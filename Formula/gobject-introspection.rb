d030d9e1f7f7a4f6cbd1d5a46c51d947f93c6a21
Rui Chen <rui@chenrui.dev>
2023-03-13 21:32:39 -0400

    gobject-introspection 1.76.0
    gobject-introspection: specify test resource
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/gobject-introspection.rb
+++ Formula/gobject-introspection.rb
@@ -3,8 +3,8 @@ class GobjectIntrospection < Formula
 
   desc "Generate introspection data for GObject libraries"
   homepage "https://gi.readthedocs.io/en/latest/"
-  url "https://download.gnome.org/sources/gobject-introspection/1.74/gobject-introspection-1.74.0.tar.xz"
-  sha256 "347b3a719e68ba4c69ff2d57ee2689233ea8c07fc492205e573386779e42d653"
+  url "https://download.gnome.org/sources/gobject-introspection/1.76/gobject-introspection-1.76.0.tar.xz"
+  sha256 "8552ff3a56758b8dba21d421795a52b9a9fbf984565907b83f1c64f4deb8380c"
   license all_of: ["GPL-2.0-or-later", "LGPL-2.0-or-later", "MIT"]
 
   bottle do
@@ -31,7 +31,7 @@ class GobjectIntrospection < Formula
   uses_from_macos "flex" => :build
   uses_from_macos "libffi", since: :catalina
 
-  resource "tutorial" do
+  resource "homebrew-tutorial" do
     url "https://gist.github.com/7a0023656ccfe309337a.git",
         revision: "499ac89f8a9ad17d250e907f74912159ea216416"
   end
@@ -67,7 +67,7 @@ class GobjectIntrospection < Formula
   end
 
   test do
-    resource("tutorial").stage testpath
+    resource("homebrew-tutorial").stage testpath
     system "make"
     assert_predicate testpath/"Tut-0.1.typelib", :exist?
   end
