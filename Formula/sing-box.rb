9a4422e73a86bdebc5a4c09d0bafb24112de8c8e
从现在开始 <imhansai@foxmail.com>
2023-03-14 10:36:26 +0800

    sing-box 1.1.6 (new formula)
    * sing-box 1.1.6 (new formula)
      * sing-box 1.1.6 (new formula)
      * sing-box: add proxy test
      * sing-box: check config
    * sing-box: update desc
    * sing-box: update ldflags variable
    
    Closes #125581.
    
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/sing-box.rb
@@ -0,0 +1,67 @@
+class SingBox < Formula
+  desc "Universal proxy platform"
+  homepage "https://sing-box.sagernet.org"
+  url "https://github.com/SagerNet/sing-box/archive/refs/tags/v1.1.6.tar.gz"
+  sha256 "2fdf93fd49c9375cd14b2fe2e2163cbad4b65d0cfa422c592855e7810036ef56"
+  license "GPL-3.0-or-later"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X github.com/sagernet/sing-box/constant.Version=#{version} -buildid="
+    tags = "with_gvisor,with_quic,with_wireguard,with_utls,with_reality_server,with_clash_api"
+    system "go", "build", "-tags", tags, *std_go_args(ldflags: ldflags), "./cmd/sing-box"
+  end
+
+  test do
+    ss_port = free_port
+    (testpath/"shadowsocks.json").write <<~EOS
+      {
+        "inbounds": [
+          {
+            "type": "shadowsocks",
+            "listen": "::",
+            "listen_port": #{ss_port},
+            "method": "2022-blake3-aes-128-gcm",
+            "password": "8JCsPssfgS8tiRwiMlhARg=="
+          }
+        ]
+      }
+    EOS
+    server = fork { exec "#{bin}/sing-box", "run", "-D", testpath, "-c", testpath/"shadowsocks.json" }
+
+    sing_box_port = free_port
+    (testpath/"config.json").write <<~EOS
+      {
+        "inbounds": [
+          {
+            "type": "mixed",
+            "listen": "::",
+            "listen_port": #{sing_box_port}
+          }
+        ],
+        "outbounds": [
+          {
+            "type": "shadowsocks",
+            "server": "127.0.0.1",
+            "server_port": #{ss_port},
+            "method": "2022-blake3-aes-128-gcm",
+            "password": "8JCsPssfgS8tiRwiMlhARg=="
+          }
+        ]
+      }
+    EOS
+    system "#{bin}/sing-box", "check", "-D", testpath, "-c", "config.json"
+    client = fork { exec "#{bin}/sing-box", "run", "-D", testpath, "-c", "config.json" }
+
+    sleep 3
+    begin
+      system "curl", "--socks5", "127.0.0.1:#{sing_box_port}", "github.com"
+    ensure
+      Process.kill 9, server
+      Process.wait server
+      Process.kill 9, client
+      Process.wait client
+    end
+  end
+end
