2df854a52e4be9e1058cd447ad575bde11aad40f
Rui Chen <rui@chenrui.dev>
2023-03-15 17:11:05 -0400

    source-to-image 1.3.5
    Closes #125775.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/source-to-image.rb
+++ Formula/source-to-image.rb
@@ -2,8 +2,8 @@ class SourceToImage < Formula
   desc "Tool for building source and injecting into docker images"
   homepage "https://github.com/openshift/source-to-image"
   url "https://github.com/openshift/source-to-image.git",
-      tag:      "v1.3.4",
-      revision: "c6c9fedfe04013db9f7ef064d1b6a3583566be04"
+      tag:      "v1.3.5",
+      revision: "c5a053620a176349993a11dc4fad723e243c93bd"
   license "Apache-2.0"
   head "https://github.com/openshift/source-to-image.git", branch: "master"
 
