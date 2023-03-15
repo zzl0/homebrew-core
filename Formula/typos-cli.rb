f8872012e077ac4a6304491fa1a752abb1f8b4aa
Porkepix <porkepix@gmail.com>
2023-03-15 17:02:01 +0100

    typos-cli 1.13.24
    Closes #125747.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/typos-cli.rb
+++ Formula/typos-cli.rb
@@ -1,8 +1,8 @@
 class TyposCli < Formula
   desc "Source code spell checker"
   homepage "https://github.com/crate-ci/typos"
-  url "https://github.com/crate-ci/typos/archive/refs/tags/v1.13.23.tar.gz"
-  sha256 "b8839a6f9552952887565106332da9e9d1b4852e49e5ad4821d263102a63a721"
+  url "https://github.com/crate-ci/typos/archive/refs/tags/v1.13.24.tar.gz"
+  sha256 "44ce214ee9ec4f6921fd7e713ed9e351e7b793e73a675e6a2026ec769cb94efb"
   license any_of: ["Apache-2.0", "MIT"]
 
   livecheck do
