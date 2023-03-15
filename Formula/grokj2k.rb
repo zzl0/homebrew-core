41f66105a28da08d014030dc7c0bb7c35ef8a550
Patrick Linnane <patrick@linnane.io>
2023-03-15 12:28:22 -0700

    grokj2k 10.0.6
    * grokj2k 10.0.6
    * grokj2k: update test
    
    Closes #125765.
    
    Signed-off-by: Ruoyu Zhong <zhongruoyu@outlook.com>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/grokj2k.rb
+++ Formula/grokj2k.rb
@@ -1,8 +1,8 @@
 class Grokj2k < Formula
   desc "JPEG 2000 Library"
   homepage "https://github.com/GrokImageCompression/grok"
-  url "https://github.com/GrokImageCompression/grok/archive/refs/tags/v10.0.5.tar.gz"
-  sha256 "3ce11361d41c4c6ff17c197f47cf224511969a4f2df8d87d1afe883d8e469baf"
+  url "https://github.com/GrokImageCompression/grok/archive/refs/tags/v10.0.6.tar.gz"
+  sha256 "968bd7aad7315c9a389fcec56ab611f1c8724083d4e0838d095992f555087906"
   license "AGPL-3.0-or-later"
   head "https://github.com/GrokImageCompression/grok.git", branch: "master"
 
@@ -106,7 +106,7 @@ class Grokj2k < Formula
         const GRK_COLOR_SPACE color_space = GRK_CLRSPC_GRAY;
 
         grk_image *image;
-        image = grk_image_new(1, &cmptparm, color_space);
+        image = grk_image_new(1, &cmptparm, color_space, true);
 
         grk_object_unref(&image->obj);
         return 0;
