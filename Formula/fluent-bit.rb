d45e0dd74a473b50c3c5b42e799dede1cf0d23a5
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-03-17 05:09:00 +0800

    fluent-bit 2.0.10
    * fluent-bit 2.0.10
    * fluent-bit: work around audit failure
      This issue was not resolved.
    
    Closes #125887.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/fluent-bit.rb
+++ Formula/fluent-bit.rb
@@ -1,8 +1,8 @@
 class FluentBit < Formula
   desc "Fast and Lightweight Logs and Metrics processor"
   homepage "https://github.com/fluent/fluent-bit"
-  url "https://github.com/fluent/fluent-bit/archive/v2.0.9.tar.gz"
-  sha256 "393ad4a6ced48c327607653d610ef273843085a17b6e5c8013877abdf31e6945"
+  url "https://github.com/fluent/fluent-bit/archive/v2.0.10.tar.gz"
+  sha256 "aad5176cb4dcadacacd379ca43160074c6690012d37c4749536ac3b977d50495"
   license "Apache-2.0"
   head "https://github.com/fluent/fluent-bit.git", branch: "master"
 
@@ -31,8 +31,7 @@ class FluentBit < Formula
 
   def install
     # Prevent fluent-bit to install files into global init system
-    #
-    # For more information see https://github.com/fluent/fluent-bit/issues/3393
+    # For more information see fluent/fluent-bit#3393
     inreplace "src/CMakeLists.txt", "if(IS_DIRECTORY /lib/systemd/system)", "if(False)"
     inreplace "src/CMakeLists.txt", "elseif(IS_DIRECTORY /usr/share/upstart)", "elif(False)"
 
