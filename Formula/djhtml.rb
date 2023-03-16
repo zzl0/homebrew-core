7473f29c2e4da0e2e65a127e9b6298e4b478d50f
Porkepix <porkepix@gmail.com>
2023-03-17 00:57:48 +0100

    djhtml 3.0.6
    Closes #125900.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/djhtml.rb
+++ Formula/djhtml.rb
@@ -3,8 +3,8 @@ class Djhtml < Formula
 
   desc "Django/Jinja template indenter"
   homepage "https://github.com/rtts/djhtml"
-  url "https://files.pythonhosted.org/packages/bc/24/a06ecc98fa29a24983ad30975d98385cb0539d0b000e06b06687311f86f8/djhtml-3.0.5.tar.gz"
-  sha256 "f38fd4e7d575538b2791c5caa2b54c5397adc08f8f8d265853e1dafa8a239ae9"
+  url "https://files.pythonhosted.org/packages/a0/03/aac9bfb7c9b03604a2c4b0d474af22731ef41cb662fad07f956ae7bf0f6b/djhtml-3.0.6.tar.gz"
+  sha256 "abfc4d7b4730432ca6a98322fbdf8ae9d6ba254ea57ba3759a10ecb293bc57de"
   license "GPL-3.0-or-later"
 
   bottle do
