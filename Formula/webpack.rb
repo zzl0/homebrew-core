ed46dd45cc2e43ec74439f0305c0e928dec85eba
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-03-16 08:05:45 +0000

    webpack: update 5.76.2 bottle.

--- Formula/webpack.rb
+++ Formula/webpack.rb
@@ -10,13 +10,13 @@ class Webpack < Formula
   head "https://github.com/webpack/webpack.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "dc0f59edc953fec15d07346613ab7a72f63336b6ecb9c0f16d1be0af334c48e8"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "dc0f59edc953fec15d07346613ab7a72f63336b6ecb9c0f16d1be0af334c48e8"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "dc0f59edc953fec15d07346613ab7a72f63336b6ecb9c0f16d1be0af334c48e8"
-    sha256 cellar: :any_skip_relocation, ventura:        "fa8893f7e611f01372a07e73b78a45bd76c86a3398398b69607ca40342f16cd6"
-    sha256 cellar: :any_skip_relocation, monterey:       "fa8893f7e611f01372a07e73b78a45bd76c86a3398398b69607ca40342f16cd6"
-    sha256 cellar: :any_skip_relocation, big_sur:        "fa8893f7e611f01372a07e73b78a45bd76c86a3398398b69607ca40342f16cd6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "dc0f59edc953fec15d07346613ab7a72f63336b6ecb9c0f16d1be0af334c48e8"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "449107f69e070faadbb933e922f6e22612adae8458158f8a2cfd03f8f0a0d49f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "449107f69e070faadbb933e922f6e22612adae8458158f8a2cfd03f8f0a0d49f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "449107f69e070faadbb933e922f6e22612adae8458158f8a2cfd03f8f0a0d49f"
+    sha256 cellar: :any_skip_relocation, ventura:        "9f242d531fc015a940644cf7717013932f22471512136daa0b6a4a3ab10d67ba"
+    sha256 cellar: :any_skip_relocation, monterey:       "9f242d531fc015a940644cf7717013932f22471512136daa0b6a4a3ab10d67ba"
+    sha256 cellar: :any_skip_relocation, big_sur:        "9f242d531fc015a940644cf7717013932f22471512136daa0b6a4a3ab10d67ba"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "449107f69e070faadbb933e922f6e22612adae8458158f8a2cfd03f8f0a0d49f"
   end
 
   depends_on "node"
