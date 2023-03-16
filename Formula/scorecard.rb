d5d52ba37816bdf44b1aa61166205da2fc2ea288
Rui Chen <rui@chenrui.dev>
2023-03-16 10:04:09 -0400

    scorecard 4.10.4
    * scorecard 4.10.3
    * scorecard: update test
    * scorecard 4.10.4
    
    Closes #125830.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/scorecard.rb
+++ Formula/scorecard.rb
@@ -2,8 +2,8 @@ class Scorecard < Formula
   desc "Security health metrics for Open Source"
   homepage "https://github.com/ossf/scorecard"
   url "https://github.com/ossf/scorecard.git",
-      tag:      "v4.10.2",
-      revision: "376f465c111c39c6a5ad7408e8896cd790cb5219"
+      tag:      "v4.10.4",
+      revision: "98316298749fdd62d3cc99423baec45ae11af662"
   license "Apache-2.0"
   head "https://github.com/ossf/scorecard.git", branch: "main"
 
@@ -38,7 +38,7 @@ class Scorecard < Formula
   test do
     ENV["GITHUB_AUTH_TOKEN"] = "test"
     output = shell_output("#{bin}/scorecard --repo=github.com/kubernetes/kubernetes --checks=Maintained 2>&1", 1)
-    expected_output = "InitRepo: repo unreachable: GET https://api.github.com/repos/google/oss-fuzz: 401"
+    expected_output = "Error: RunScorecard: repo unreachable: GET https://api.github.com/repos/kubernetes/kubernetes"
     assert_match expected_output, output
 
     assert_match version.to_s, shell_output("#{bin}/scorecard version 2>&1")
