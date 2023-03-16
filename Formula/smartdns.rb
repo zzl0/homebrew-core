a590a524e07a07a5a217f26a4315132b419b3f57
YiiSh <mokeyish@hotmail.com>
2023-03-16 22:17:49 +0800

    smartdns 0.3.1 (new formula)
    Closes #125820.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/smartdns.rb
@@ -0,0 +1,43 @@
+class Smartdns < Formula
+  desc "Rule-based DNS server for fast IP resolution, DoT/DoH/DoQ supported"
+  homepage "https://github.com/mokeyish/smartdns-rs"
+  url "https://github.com/mokeyish/smartdns-rs/archive/refs/tags/0.3.1.tar.gz"
+  sha256 "3a11e1b1c6ffc33d1120b6cec516d19a90375be283c114771cf950e2b364215d"
+  license "GPL-3.0-only"
+  head "https://github.com/mokeyish/smartdns-rs.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  on_linux do
+    depends_on "bind" => :test # for `dig`
+  end
+
+  def install
+    system "cargo", "install", *std_cargo_args
+    sbin.install bin/"smartdns"
+    pkgetc.install "etc/smartdns/smartdns.conf"
+  end
+
+  service do
+    run [opt_sbin/"smartdns", "run", "-c", etc/"smartdns/smartdns.conf"]
+    keep_alive true
+    require_root true
+  end
+
+  test do
+    port = free_port
+
+    (testpath/"smartdns.conf").write <<~EOS
+      bind 127.0.0.1:#{port}
+      server 8.8.8.8
+      local-ttl 3
+      address /example.com/1.2.3.4
+    EOS
+    fork do
+      exec sbin/"smartdns", "run", "-c", testpath/"smartdns.conf"
+    end
+    sleep(2)
+    output = shell_output("dig @127.0.0.1 -p #{port} example.com.")
+    assert_match("example.com.\t\t3\tIN\tA\t1.2.3.4", output)
+  end
+end
