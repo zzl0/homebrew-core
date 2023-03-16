e2e3d7db19795d6a39a86ca47b5b82abe246e62a
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-03-17 00:26:34 +0800

    f3d: revision bump (opencascade 7.7.0)
    Closes #115638.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/f3d.rb
+++ Formula/f3d.rb
@@ -4,7 +4,7 @@ class F3d < Formula
   url "https://github.com/f3d-app/f3d/archive/refs/tags/v1.3.1.tar.gz"
   sha256 "653dc4044e14d0618c1d947a8ee85d2513e100b3fc24bd6e51830131a13e795d"
   license "BSD-3-Clause"
-  revision 2
+  revision 3
 
   bottle do
     sha256 cellar: :any,                 arm64_ventura:  "d19122637681362a34a7db04dc5c917443abd922da557c331e08bffde44eb53b"
