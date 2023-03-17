beea8990f2f29be9442aa8d5c2cef7a7548c8726
Rui Chen <rui@chenrui.dev>
2023-03-17 19:24:26 -0400

    sing-box 1.1.7
    * sing-box 1.1.7
    * sing-box: update to use `:homebrew_curl`
    * sing-box: fix syntax
    
    Closes #126002.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/sing-box.rb
+++ Formula/sing-box.rb
@@ -1,8 +1,9 @@
 class SingBox < Formula
   desc "Universal proxy platform"
   homepage "https://sing-box.sagernet.org"
-  url "https://github.com/SagerNet/sing-box/archive/refs/tags/v1.1.6.tar.gz"
-  sha256 "2fdf93fd49c9375cd14b2fe2e2163cbad4b65d0cfa422c592855e7810036ef56"
+  # using `:homebrew_curl` to work around audit failure from TLS 1.3-only homepage
+  url "https://github.com/SagerNet/sing-box/archive/refs/tags/v1.1.7.tar.gz", using: :homebrew_curl
+  sha256 "d0c75af7a5b649b5f10af99e845134741dd4ea4147d804c9d0d2a3a552843356"
   license "GPL-3.0-or-later"
 
   bottle do
