a78354834f05cd36407c560c4d96d0cd854df578
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-03-17 19:05:59 +0000

    structurizr-cli: update 1.29.0 bottle.

--- Formula/structurizr-cli.rb
+++ Formula/structurizr-cli.rb
@@ -6,7 +6,7 @@ class StructurizrCli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "68bc16162ac126b17e2c6e83437613dee092dd38a76f26d7e8e4cee09743ec7c"
+    sha256 cellar: :any_skip_relocation, all: "8e3011b1dce2872ea5d091a070c6d7e0e267f6037434d7f5ea86223a6697e655"
   end
 
   depends_on "openjdk"
