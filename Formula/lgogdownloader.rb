d495c5df5a2ec87f3509109fc1a88fd1880743dd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-03-17 04:05:13 +0000

    lgogdownloader: update 3.10 bottle.

--- Formula/lgogdownloader.rb
+++ Formula/lgogdownloader.rb
@@ -12,13 +12,13 @@ class Lgogdownloader < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "5b7309454be5bb9ebcf3d0e8406b7f443e7abb71f2b49bdc7e08bc5aa32e3d49"
-    sha256 cellar: :any,                 arm64_monterey: "cf59cafe7b2b7c27613daef308d55ccae6727cd71d2627bfdbb309bbfb244166"
-    sha256 cellar: :any,                 arm64_big_sur:  "e2b28d2bde8295b152315d22445fafc85eaebad79288d32340212909f2f8c598"
-    sha256 cellar: :any,                 ventura:        "0b31fd256677a4ebb84474367c3a8faaf6fee81e0d49148855daddb9707543af"
-    sha256 cellar: :any,                 monterey:       "21d77cf72d41e155c7afa10bd1907c611aee00c640c6bf37c2b11dec882d179e"
-    sha256 cellar: :any,                 big_sur:        "0bfd430cabfaeaa893e3a6733f7193832d61e34a991e346c0db8097347d9c181"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c9b780d368e3deeac346ed9e580fd9758bb3c897a5e93ff7b11f93111dedb659"
+    sha256 cellar: :any,                 arm64_ventura:  "d279f6f6081ee152d98c913c68d3fb170bad072f9e86421a29bfe12abbf07faf"
+    sha256 cellar: :any,                 arm64_monterey: "7e95d0d0f2374b6644757adaa1e15ce3fd07d8318ab92e85716e7f30c20e972a"
+    sha256 cellar: :any,                 arm64_big_sur:  "6b7eac0ab1909e6aff662fc795496fc900299494c2a5c2fbcf0b47cf9367ef33"
+    sha256 cellar: :any,                 ventura:        "21c4c8c065b9cc591b9c64aaf7e89dc73f97b46b45f96c36c5cd0ca7da46571f"
+    sha256 cellar: :any,                 monterey:       "885033cd23ecad2758386d55887a5db8244c71b0173e5a30fa93a4cfc54e0dd2"
+    sha256 cellar: :any,                 big_sur:        "37d7cce5462b3bb759e551440cbf26d00391f48112c8202612b2de957f0910a0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f8bdf0e04dd32e770f41ce2cf8b00c4f38c467658865d4ba51ea59d420fab54c"
   end
 
   depends_on "cmake" => :build
