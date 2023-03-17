102076da10b6bb68d1fd09912bd738059a215213
Patrick Linnane <patrick@linnane.io>
2023-03-17 10:22:01 -0700

    hypre 2.28.0
    Closes #125959.
    
    Signed-off-by: Ruoyu Zhong <zhongruoyu@outlook.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/hypre.rb
+++ Formula/hypre.rb
@@ -1,8 +1,8 @@
 class Hypre < Formula
   desc "Library featuring parallel multigrid methods for grid problems"
   homepage "https://computing.llnl.gov/projects/hypre-scalable-linear-solvers-multigrid-methods"
-  url "https://github.com/hypre-space/hypre/archive/v2.27.0.tar.gz"
-  sha256 "507a3d036bb1ac21a55685ae417d769dd02009bde7e09785d0ae7446b4ae1f98"
+  url "https://github.com/hypre-space/hypre/archive/v2.28.0.tar.gz"
+  sha256 "2eea68740cdbc0b49a5e428f06ad7af861d1e169ce6a12d2cf0aa2fc28c4a2ae"
   license any_of: ["MIT", "Apache-2.0"]
   head "https://github.com/hypre-space/hypre.git", branch: "master"
 
