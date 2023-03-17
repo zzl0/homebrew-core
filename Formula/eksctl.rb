76588ac3a449ae0b2b4aef404b38131a5bd0178c
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-03-17 17:55:23 +0800

    eksctl 0.134.0
    Closes #125931.
    
    Signed-off-by: Dawid Dziurla <9713907+dawidd6@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/eksctl.rb
+++ Formula/eksctl.rb
@@ -2,8 +2,8 @@ class Eksctl < Formula
   desc "Simple command-line tool for creating clusters on Amazon EKS"
   homepage "https://eksctl.io"
   url "https://github.com/weaveworks/eksctl.git",
-      tag:      "0.133.0",
-      revision: "9b3b150a0d4cdbddca7abdf6c37854f4fbc95f36"
+      tag:      "0.134.0",
+      revision: "97d10e0c6541b71a743cfbe2c2eec5543ecbb611"
   license "Apache-2.0"
   head "https://github.com/weaveworks/eksctl.git", branch: "main"
 
