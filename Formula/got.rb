4d64618a5d0301550b2964095dff3ae7bfa11032
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2023-03-17 19:32:26 +0000

    got: update 0.86 bottle.

--- Formula/got.rb
+++ Formula/got.rb
@@ -17,6 +17,7 @@ class Got < Formula
     sha256 ventura:        "fedc5283c67903b13c3179b2e4f028a0ee2607de5ea348cead64adccc68abe29"
     sha256 monterey:       "7b78ebc651efb8178eaf3baf49a8549b874187f4eda717737685513457995bc4"
     sha256 big_sur:        "286c8e5373691559b492c873152d857a37bf7d0f70c552ece315069aa8cc6d9f"
+    sha256 x86_64_linux:   "bdfaf34a35a338c02cea202dc5294089a0b326e515a623e6b92d34d9a968909f"
   end
 
   depends_on "bison" => :build
