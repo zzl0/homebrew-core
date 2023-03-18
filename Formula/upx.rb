317cd988861ea4753fdb11f5934c96ba43703353
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2023-03-18 19:14:13 +0000

    upx: update 4.0.2 bottle.

--- Formula/upx.rb
+++ Formula/upx.rb
@@ -7,11 +7,8 @@ class Upx < Formula
   head "https://github.com/upx/upx.git", branch: "devel"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, monterey:     "ad32270c324e07cb3ff55008834932c59394f315e8d82ccd4ebb08c0917a633c"
-    sha256 cellar: :any_skip_relocation, big_sur:      "5fc54db6b0fb2e8ebfa630d48c893e569e49b5c6795646d8912c447f3b0a1747"
-    sha256 cellar: :any_skip_relocation, catalina:     "c04d7040eeaa8d2842449b86789ece0f0a73ee0ac1c013c6a00596288251abbc"
-    sha256 cellar: :any_skip_relocation, mojave:       "a2253a74b3531dc9173eac2ae2ea816ff7b8af3657aee2180ca1253f49cd9fec"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "4a00316a5883460cc1c16c75c93cb5a36d4e6ea1bbca394df82047715171d795"
+    sha256 cellar: :any_skip_relocation, monterey: "2e7f9c35cac2e339b6de8a3608f436314e7d8b2c4ee60689195a6bb9b3aeb7c9"
+    sha256 cellar: :any_skip_relocation, big_sur:  "2ed424d5f94c10e279b0a03c08de69e07ee96f2eefe77d7ffaea6eddddaefb18"
   end
 
   depends_on "cmake" => :build
