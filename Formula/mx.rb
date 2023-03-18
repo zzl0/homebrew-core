c5fab6492b8e6f4e15753a9303b9017a0de938c9
Rui Chen <rui@chenrui.dev>
2023-03-18 11:44:24 -0400

    mx 6.17.0
    Closes #126043.
    
    Signed-off-by: Ruoyu Zhong <zhongruoyu@outlook.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/mx.rb
+++ Formula/mx.rb
@@ -1,8 +1,8 @@
 class Mx < Formula
   desc "Command-line tool used for the development of Graal projects"
   homepage "https://github.com/graalvm/mx"
-  url "https://github.com/graalvm/mx/archive/refs/tags/6.16.6.tar.gz"
-  sha256 "d9247ccbaaadb25d8202a7565676e2a804322e149549933e71101e523b7b0bde"
+  url "https://github.com/graalvm/mx/archive/refs/tags/6.17.0.tar.gz"
+  sha256 "dcce09aa9dea257ae80752be7a88d4b96923891d15d536cd8381257f5e116797"
   license "GPL-2.0-only"
 
   livecheck do
