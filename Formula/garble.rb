29d9c7008d90c30e269712dbea48029f660e1475
Rui Chen <rui@chenrui.dev>
2023-03-18 22:07:05 +0000

    garble: add 0.9.3 bottle.

--- Formula/garble.rb
+++ Formula/garble.rb
@@ -6,6 +6,16 @@ class Garble < Formula
   license "BSD-3-Clause"
   head "https://github.com/burrowers/garble.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a2749498749a5db85febca9b6ec5fed5a05f3abfe4d09f13972addae9092dab0"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a2749498749a5db85febca9b6ec5fed5a05f3abfe4d09f13972addae9092dab0"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a2749498749a5db85febca9b6ec5fed5a05f3abfe4d09f13972addae9092dab0"
+    sha256 cellar: :any_skip_relocation, ventura:        "26e6c5a71f0ef051a6b2d0c032baf1d6f8de627311e33208833f60bfe5b10b4e"
+    sha256 cellar: :any_skip_relocation, monterey:       "26e6c5a71f0ef051a6b2d0c032baf1d6f8de627311e33208833f60bfe5b10b4e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "26e6c5a71f0ef051a6b2d0c032baf1d6f8de627311e33208833f60bfe5b10b4e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b23bedfb5d02de1158cdc773edbb9409ccda7c6307758835e8db0e190f7bd82d"
+  end
+
   depends_on "go" => [:build, :test]
 
   def install
