6bac932478306a9ac5a27963284906e45a97ad44
Rui Chen <rui@chenrui.dev>
2023-03-18 22:07:12 +0000

    tkey-ssh-agent: add 0.0.4 bottle.

--- Formula/tkey-ssh-agent.rb
+++ Formula/tkey-ssh-agent.rb
@@ -5,6 +5,16 @@ class TkeySshAgent < Formula
   sha256 "b3f3547401159a8a4277a4b9689632699cb6cf5bebde6ba6a093aab80b1e7ea9"
   license "GPL-2.0-only"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "c99cd35b8b9151430bf011fdcf20f0ec6debd3971d2cf0f7166195d3843688bc"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a820a1814e836a720f87236442edf6e8683dc76fa2ad1c0e06e91a9309663094"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "35abf52ced85ea3539f362daa8c23b98debfb028d4b291db05ca382d03cdd94d"
+    sha256 cellar: :any_skip_relocation, ventura:        "d0505ece81479ebf6832f8987fb72f6dd62e8de872189db0b9f3540a63563f7e"
+    sha256 cellar: :any_skip_relocation, monterey:       "9b291196d56d9a84a847f4337fbad530a4b88cce303ece39716e10855d7ea7bc"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e10dcf27d94c7ec2392812743d69648e810363bdf464c21a6ccac77ac25e8b97"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "aea8b0212bc5cd1260d5419cbe8dde5c9425b841344079885d99abd05b4de23a"
+  end
+
   depends_on "go" => :build
 
   on_macos do
