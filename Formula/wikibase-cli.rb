e9cd6784855c64e85a8b5c503506ee6d3b1e58ad
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-03-19 21:04:51 +0000

    wikibase-cli: update 16.3.1 bottle.

--- Formula/wikibase-cli.rb
+++ Formula/wikibase-cli.rb
@@ -8,13 +8,13 @@ class WikibaseCli < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "c188b14b9da086097207f7325b99d1948aa87ed920d6e6b96cf1972fe711d88d"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c188b14b9da086097207f7325b99d1948aa87ed920d6e6b96cf1972fe711d88d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c188b14b9da086097207f7325b99d1948aa87ed920d6e6b96cf1972fe711d88d"
-    sha256 cellar: :any_skip_relocation, ventura:        "d9c256d851bd335acea7a843ef9614e3cec0f08b2357bdc68fcdc5a45e0cd3ac"
-    sha256 cellar: :any_skip_relocation, monterey:       "d9c256d851bd335acea7a843ef9614e3cec0f08b2357bdc68fcdc5a45e0cd3ac"
-    sha256 cellar: :any_skip_relocation, big_sur:        "d9c256d851bd335acea7a843ef9614e3cec0f08b2357bdc68fcdc5a45e0cd3ac"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c188b14b9da086097207f7325b99d1948aa87ed920d6e6b96cf1972fe711d88d"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "7d17ee440a1c72ad942cd30e3e55fa124cdf529364ec48020381cee3afe00cb3"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "7d17ee440a1c72ad942cd30e3e55fa124cdf529364ec48020381cee3afe00cb3"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7d17ee440a1c72ad942cd30e3e55fa124cdf529364ec48020381cee3afe00cb3"
+    sha256 cellar: :any_skip_relocation, ventura:        "b4a8ed6699627ec3267ebb92021e7a29ab19e0d4debc22861e27bf28d0ebb3ab"
+    sha256 cellar: :any_skip_relocation, monterey:       "b4a8ed6699627ec3267ebb92021e7a29ab19e0d4debc22861e27bf28d0ebb3ab"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b4a8ed6699627ec3267ebb92021e7a29ab19e0d4debc22861e27bf28d0ebb3ab"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7d17ee440a1c72ad942cd30e3e55fa124cdf529364ec48020381cee3afe00cb3"
   end
 
   depends_on "node"
