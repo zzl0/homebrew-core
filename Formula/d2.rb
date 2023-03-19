8781d32fa7f04df9bc138b9d5bbf25c716f2d76c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-03-19 01:32:38 +0000

    d2: update 0.2.6 bottle.

--- Formula/d2.rb
+++ Formula/d2.rb
@@ -7,13 +7,13 @@ class D2 < Formula
   head "https://github.com/terrastruct/d2.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "62eb0e5b01275ca8a2ecd2a11f1dc855a807ea75bc10622a35f607e7436b919b"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "62eb0e5b01275ca8a2ecd2a11f1dc855a807ea75bc10622a35f607e7436b919b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "62eb0e5b01275ca8a2ecd2a11f1dc855a807ea75bc10622a35f607e7436b919b"
-    sha256 cellar: :any_skip_relocation, ventura:        "973ddcf2d29d509016d9b8c602dd5a76d9f77c9c0050eedf65d0e9a200e823d6"
-    sha256 cellar: :any_skip_relocation, monterey:       "973ddcf2d29d509016d9b8c602dd5a76d9f77c9c0050eedf65d0e9a200e823d6"
-    sha256 cellar: :any_skip_relocation, big_sur:        "973ddcf2d29d509016d9b8c602dd5a76d9f77c9c0050eedf65d0e9a200e823d6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ac4b1a41aff3c95773f663d46217d89e54eabb7eb8a18e892dc4561f5066fd95"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0328d1404b316f616383c6d9f5e44a65ee89b13431feb30e4345d77e72be05f4"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "0328d1404b316f616383c6d9f5e44a65ee89b13431feb30e4345d77e72be05f4"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0328d1404b316f616383c6d9f5e44a65ee89b13431feb30e4345d77e72be05f4"
+    sha256 cellar: :any_skip_relocation, ventura:        "98b209365fc65974a69372ba039b8327593efff429ea2b20d6d78a8ef08271ee"
+    sha256 cellar: :any_skip_relocation, monterey:       "98b209365fc65974a69372ba039b8327593efff429ea2b20d6d78a8ef08271ee"
+    sha256 cellar: :any_skip_relocation, big_sur:        "98b209365fc65974a69372ba039b8327593efff429ea2b20d6d78a8ef08271ee"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a78b435f1181af911f524f49c4c5453718a31c7002035f46409c1e6a7abef999"
   end
 
   depends_on "go" => :build
