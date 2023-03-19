a83cd9e5e7f30609855fc7e226fc2f265c453722
Caleb Xu <calebcenter@live.com>
2023-03-18 22:45:38 -0400

    ugrep 3.11.0
    Closes #126074.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ugrep.rb
+++ Formula/ugrep.rb
@@ -1,8 +1,8 @@
 class Ugrep < Formula
   desc "Ultra fast grep with query UI, fuzzy search, archive search, and more"
   homepage "https://github.com/Genivia/ugrep"
-  url "https://github.com/Genivia/ugrep/archive/v3.10.1.tar.gz"
-  sha256 "720b80c5aa733d906dba2caa07c50ac6242d64fde8c7402a5d69d471a94d7e76"
+  url "https://github.com/Genivia/ugrep/archive/v3.11.0.tar.gz"
+  sha256 "35ed29b251f71a7165b059b99b17cffe6947abebb8db025383aa9b897408e0be"
   license "BSD-3-Clause"
 
   bottle do
