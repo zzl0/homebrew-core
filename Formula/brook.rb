e527a22e9040caf29f0a59059d05a3f49d0b39b7
Porkepix <porkepix@gmail.com>
2023-03-19 11:43:53 +0100

    brook 20230401
    * brook 20230401
    * brook: update test
    
    Closes #126083.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/brook.rb
+++ Formula/brook.rb
@@ -1,8 +1,8 @@
 class Brook < Formula
   desc "Cross-platform strong encryption and not detectable proxy. Zero-Configuration"
   homepage "https://txthinking.github.io/brook/"
-  url "https://github.com/txthinking/brook/archive/refs/tags/v20230122.tar.gz"
-  sha256 "52643df51144b4b1afbacb51156f92ba61adbcff77dd8f76e3278ce70644f237"
+  url "https://github.com/txthinking/brook/archive/refs/tags/v20230401.tar.gz"
+  sha256 "332069845b9c205070c0edbddc9e0204187499b97d4d1bda593535d775f556a3"
   license "GPL-3.0-only"
 
   bottle do
@@ -23,7 +23,7 @@ class Brook < Formula
 
   test do
     output = shell_output "#{bin}/brook link --server 1.2.3.4:56789 --password hello"
-    # We expect something like "brook://server?password=hello&server=1.2.3.4%3A56789&username="
+    # We expect something like "brook://server?password=hello&server=1.2.3.4%3A56789"
     uri = URI(output)
     assert_equal "brook", uri.scheme
     assert_equal "server", uri.host
@@ -31,6 +31,5 @@ class Brook < Formula
     query = URI.decode_www_form(uri.query).to_h
     assert_equal "1.2.3.4:56789", query["server"]
     assert_equal "hello", query["password"]
-    assert_equal "", query["username"]
   end
 end
