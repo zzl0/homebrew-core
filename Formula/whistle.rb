1e691dfe76b3690d1d2f890957d6bd0527d27525
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-03-20 04:36:40 +0800

    whistle 2.9.43
    Closes #126109.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/whistle.rb
+++ Formula/whistle.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Whistle < Formula
   desc "HTTP, HTTP2, HTTPS, Websocket debugging proxy"
   homepage "https://github.com/avwo/whistle"
-  url "https://registry.npmjs.org/whistle/-/whistle-2.9.42.tgz"
-  sha256 "4ef367522b357d3788fa90f0cdce75c5db0b4f39d89cc738880675a176ce340e"
+  url "https://registry.npmjs.org/whistle/-/whistle-2.9.43.tgz"
+  sha256 "8d8bfcd7155006d2ab3ea8034ecd10cf6bc053f827c31b164cac84b8767dfd33"
   license "MIT"
 
   bottle do
