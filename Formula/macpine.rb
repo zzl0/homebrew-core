c6eca9d97389e31812235bf8240a61fd289b49ab
Max Zinkus <5474655+maxzinkus@users.noreply.github.com>
2023-03-20 17:43:57 -0500

    macpine: add base_name to match binary (alpine)
    Forgot to account for base name difference (`alpine` rather than `macpine`)
    
    Closes #126193.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/macpine.rb
+++ Formula/macpine.rb
@@ -38,7 +38,7 @@ class Macpine < Formula
 
   def install
     system "make", "install", "PREFIX=#{prefix}"
-    generate_completions_from_executable(bin/"alpine", "completion")
+    generate_completions_from_executable(bin/"alpine", "completion", base_name: "alpine")
   end
 
   test do
