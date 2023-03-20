ace119775c0ad1c5abd601cb9fea3e1ff2b9f0cc
Rui Chen <rui@chenrui.dev>
2023-03-20 18:26:49 -0400

    libtensorflow 2.11.1

--- Formula/libtensorflow.rb
+++ Formula/libtensorflow.rb
@@ -1,8 +1,8 @@
 class Libtensorflow < Formula
   desc "C interface for Google's OS library for Machine Intelligence"
   homepage "https://www.tensorflow.org/"
-  url "https://github.com/tensorflow/tensorflow/archive/refs/tags/v2.11.0.tar.gz"
-  sha256 "99c732b92b1b37fc243a559e02f9aef5671771e272758aa4aec7f34dc92dac48"
+  url "https://github.com/tensorflow/tensorflow/archive/refs/tags/v2.11.1.tar.gz"
+  sha256 "624ed1cc170cdcc19e8a15d8cdde989a9a1c6b0534c90b38a6b2f06fb2963e5f"
   license "Apache-2.0"
 
   bottle do
