e0e94b895690393dcf113fd0867b685b31afa994
Porkepix <porkepix@gmail.com>
2023-03-21 10:53:25 +0100

    peco 0.5.11
    Closes #126226.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/peco.rb
+++ Formula/peco.rb
@@ -1,8 +1,8 @@
 class Peco < Formula
   desc "Simplistic interactive filtering tool"
   homepage "https://github.com/peco/peco"
-  url "https://github.com/peco/peco/archive/refs/tags/v0.5.10.tar.gz"
-  sha256 "781c2effc4f6a58d9ff96fb0fc8b0fba3aab56a91a34933d68c5de3aea5fe3f6"
+  url "https://github.com/peco/peco/archive/refs/tags/v0.5.11.tar.gz"
+  sha256 "8e32c8af533e03795f27feb4ee134960611d2fc0266528b1c512a6f1f065b164"
   license "MIT"
   head "https://github.com/peco/peco.git", branch: "master"
 
