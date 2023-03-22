a650b7b8a39fb62c4dd626012a31ef4cfbdf85d5
dhanu <andreasdhanu@gmail.com>
2023-03-23 01:51:10 +0700

    go-feature-flag-relay-proxy 1.5.1 (new formula)

---
+++ Formula/go-feature-flag-relay-proxy.rb
@@ -0,0 +1,56 @@
+class GoFeatureFlagRelayProxy < Formula
+  desc "Stand alone server to run GO Feature Flag"
+  homepage "https://gofeatureflag.org"
+  url "https://github.com/thomaspoignant/go-feature-flag.git",
+      tag:      "v1.5.1",
+      revision: "b4120c73dad7ed27a069768646672b0a8b8325d9"
+  license "MIT"
+  head "https://github.com/thomaspoignant/go-feature-flag.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["CGO_ENABLED"] = "0"
+    ldflags = %W[
+      -s -w
+      -X main.version=#{version}
+    ]
+
+    system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/relayproxy"
+  end
+
+  test do
+    port = free_port
+
+    (testpath/"flags.yml").write <<~EOS
+      test-flag:
+        variations:
+          true-var: true
+          false-var: false
+        defaultRule:
+          variation: true-var
+    EOS
+
+    (testpath/"test.yml").write <<~EOS
+      listen: #{port}
+      pollingInterval: 1000
+      retriever:
+        kind: file
+        path: #{testpath}/flags.yml
+    EOS
+
+    begin
+      pid = fork do
+        exec bin/"go-feature-flag-relay-proxy", "--config", "#{testpath}/test.yml"
+      end
+      sleep 3
+
+      expected_output = /true/
+
+      assert_match expected_output, shell_output("curl -s http://localhost:#{port}/health")
+    ensure
+      Process.kill("TERM", pid)
+      Process.wait(pid)
+    end
+  end
+end
