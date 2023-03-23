73dc4ee6bf5cd4ba5cff1081d9120f2b5d5a2d42
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2023-03-23 06:54:45 +0000

    musikcube: add 0.99.7 bottle.

--- Formula/musikcube.rb
+++ Formula/musikcube.rb
@@ -6,6 +6,15 @@ class Musikcube < Formula
   license "BSD-3-Clause"
   head "https://github.com/clangen/musikcube.git", branch: "master"
 
+  bottle do
+    sha256 arm64_ventura:  "a132d430e8e2925c248b2b5c213a7c3091675f08b1967079632c4977072c2a1b"
+    sha256 arm64_monterey: "78d9275f04cd807636e4be2acc0dcb6535d17d86007ad62689ae66a4852ab06e"
+    sha256 arm64_big_sur:  "db45728f40e566719aee003e3b4860f8b5a3aba83cd0a580b93a4843e6e5f627"
+    sha256 ventura:        "9ddea21473697c3daa6c439b0bfd5c3fcb9524c6d6ecb94e65c2f7725456df8f"
+    sha256 monterey:       "c625c36435cbcaa3c3b642ca80a8d6731a68417bde9fac1f7c5c52a1186b146f"
+    sha256 big_sur:        "e086be62cb22cc12f91fcb48605022fe90dc64a9712f57f51a881ab374a0ac6b"
+  end
+
   depends_on "asio" => :build
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
