343cca47fe697e4738a41417e60e27f8cf18edf3
geeohgeegeeoh <ggm@pobox.com>
2023-03-24 08:27:13 +1000

    rpki-client 8.3
    We apply a patch from upstream to fix the build on macOS.

--- Formula/rpki-client.rb
+++ Formula/rpki-client.rb
@@ -1,8 +1,9 @@
 class RpkiClient < Formula
   desc "OpenBSD portable rpki-client"
   homepage "https://www.rpki-client.org/"
-  url "https://ftp.openbsd.org/pub/OpenBSD/rpki-client/rpki-client-8.2.tar.gz"
-  sha256 "dc0d19679b57ae657b92d21730b1678823974d43300faa8c24ee155c1e2b1d64"
+  # TODO: Remove `autoconf`, `automake` and `libtool` dependencies when the patch is removed.
+  url "https://ftp.openbsd.org/pub/OpenBSD/rpki-client/rpki-client-8.3.tar.gz"
+  sha256 "8c78f82ae959a900f47c7319cbf7688182cde39dcc4c4b9aa399a142be4dc143"
   license "ISC"
 
   bottle do
@@ -15,6 +16,11 @@ class RpkiClient < Formula
     sha256 x86_64_linux:   "90f625188abd2191d12b7b0a4f4304910071b15aca3aef0b8a2003db62d7a142"
   end
 
+  # We need `autoconf`, `automake` and `libtool` to apply the patch below.
+  # Remove when the patch is no longer needed.
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
   depends_on "pkg-config" => :build
   depends_on "libretls"
   depends_on "openssl@3"
@@ -22,7 +28,16 @@ class RpkiClient < Formula
 
   uses_from_macos "expat"
 
+  # Fix absence of `HOST_NAME_MAX` on macOS.
+  # Remove in next release.
+  patch do
+    url "https://github.com/rpki-client/rpki-client-portable/commit/65e5d4b99131b7cc8091ea222b70d24ec04fac60.patch?full_index=1"
+    sha256 "014b522efa2f656853b42673ac7bf592ed662468d911b251c918b0154bac0a3d"
+  end
+
   def install
+    # We call `autoreconf` because we apply a patch. Remove when the patch is no longer needed.
+    system "autoreconf", "--force", "--install", "--verbose"
     system "./configure", *std_configure_args,
                           "--with-rsync=#{Formula["rsync"].opt_bin}/rsync",
                           "--disable-silent-rules",
