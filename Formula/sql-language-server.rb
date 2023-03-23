a922e9c0f2bf88dd4033782c2e1b524eef2753e1
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-03-23 03:27:35 +0000

    sql-language-server: update 1.3.3 bottle.

--- Formula/sql-language-server.rb
+++ Formula/sql-language-server.rb
@@ -8,13 +8,13 @@ class SqlLanguageServer < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "476969435c23bf8cdc0239ffcea49746b721743b38a3f806fa9118fcbf4518f2"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "40fd78130749ef35ecd5e10acdc1f17ff10e5a5fe795ca96d17d54123dd8cf3e"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4ec93b144a23e45f0a7124b28b657a51312cae28c58e4b53a1d2410e30eea432"
-    sha256 cellar: :any_skip_relocation, ventura:        "e2bd7c3bd3a999e637ac9b433fbaf1d97230b0772a6ea33331c0821c6bbbfa56"
-    sha256 cellar: :any_skip_relocation, monterey:       "e1887d0649453f493d2b0d223a20badd8bb141d30e600bec944f1fae4613f660"
-    sha256 cellar: :any_skip_relocation, big_sur:        "49db52f3ef1a043cafbce9b0370ff63c789362b306ecb0ad9e0d437f79aac870"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6b3ceb4de69ee6f04c8e2536613941c6feec1e6dc173febbdc392502b084b558"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "178d45fa97410f3a031787885552ba57db26812be7b7609e937c7b90c412239a"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f200e7971b71abf7eaca742eb250ceb59e041c91b31b28a38b2033375af6732d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d8648ca214dd4e88fff14566b3c23a3726747400bb33bdb1f96fdd4131c04c32"
+    sha256 cellar: :any_skip_relocation, ventura:        "01aae01ed0c626b87b93d90dd7bd5d0cbe3cd7d319689536cf7d095b8c44166a"
+    sha256 cellar: :any_skip_relocation, monterey:       "81dbb2cbf7c268b08fd71ad5ee0c4da3c0dbcc12fd06df849527e37dd261ecbe"
+    sha256 cellar: :any_skip_relocation, big_sur:        "deb8fcfe3839fd8eb610de5e770da74ac77f54450ba22475b643d052015a5ea4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "756714188d04bf0d5ce602111866aa0d2f8afb0afd08d368b9611234df986562"
   end
 
   depends_on "node"
