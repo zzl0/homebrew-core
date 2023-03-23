97d1532d6c2017c9ed3f7442cc4e04640056247c
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2023-03-23 17:21:48 +0000

    spirv-llvm-translator: update 15.0.0_1 bottle.

--- Formula/spirv-llvm-translator.rb
+++ Formula/spirv-llvm-translator.rb
@@ -8,12 +8,13 @@ class SpirvLlvmTranslator < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any, arm64_ventura:  "8375284c22ec1390ef4b531a793b0c98983408c4b848c816be5bca5e796bf7e7"
-    sha256 cellar: :any, arm64_monterey: "09d09dd2456ed69c811ab5295442fb53f63f228c4aaefaddd64684b6547537a8"
-    sha256 cellar: :any, arm64_big_sur:  "7d130ffac31962ad5368bf834c3a04e1a774d1f764c540ef6beb7ae2b1cefb2b"
-    sha256 cellar: :any, ventura:        "404c0423ad8d2e50a2eeabc7fb01105c38df052097a12ef53d74297388de26ef"
-    sha256 cellar: :any, monterey:       "6d04c2058054c55979cf7d8807e93ddebab495f5f68e55a14ba524d172143c93"
-    sha256 cellar: :any, big_sur:        "4b06e446e11b82361617d10ef8275a47272fa65ab1337dfbf2422e28dec7cf84"
+    sha256 cellar: :any,                 arm64_ventura:  "8375284c22ec1390ef4b531a793b0c98983408c4b848c816be5bca5e796bf7e7"
+    sha256 cellar: :any,                 arm64_monterey: "09d09dd2456ed69c811ab5295442fb53f63f228c4aaefaddd64684b6547537a8"
+    sha256 cellar: :any,                 arm64_big_sur:  "7d130ffac31962ad5368bf834c3a04e1a774d1f764c540ef6beb7ae2b1cefb2b"
+    sha256 cellar: :any,                 ventura:        "404c0423ad8d2e50a2eeabc7fb01105c38df052097a12ef53d74297388de26ef"
+    sha256 cellar: :any,                 monterey:       "6d04c2058054c55979cf7d8807e93ddebab495f5f68e55a14ba524d172143c93"
+    sha256 cellar: :any,                 big_sur:        "4b06e446e11b82361617d10ef8275a47272fa65ab1337dfbf2422e28dec7cf84"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4e032267e9b20f01db748cd2b6a3addcf56eb23792657230168899d106540a42"
   end
 
   depends_on "cmake" => :build
