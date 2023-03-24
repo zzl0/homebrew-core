fe4d3496e55d88de53ada421180876c29b63755f
Porkepix <porkepix@gmail.com>
2023-03-24 05:06:20 +0100

    s2n 1.3.41

--- Formula/s2n.rb
+++ Formula/s2n.rb
@@ -1,8 +1,8 @@
 class S2n < Formula
   desc "Implementation of the TLS/SSL protocols"
   homepage "https://github.com/aws/s2n-tls"
-  url "https://github.com/aws/s2n-tls/archive/v1.3.40.tar.gz"
-  sha256 "d365027221f105ff2c547ff3bffc0c2764242494b62c186e13e3bca272cab2c0"
+  url "https://github.com/aws/s2n-tls/archive/v1.3.41.tar.gz"
+  sha256 "46d68a57094f2671809d43263996d4ba36457a79e856cbcd721748a7b13c60ab"
   license "Apache-2.0"
   head "https://github.com/aws/s2n-tls.git", branch: "main"
 
