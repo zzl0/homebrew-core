909107803c6b649415ba883fa3c516c7c5d60646
Sorin Sbarnea <sorin.sbarnea@gmail.com>
2023-03-24 15:54:00 +0000

    ansible-lint 6.14.3
    * ansible-lint 6.14.3
      Created by https://github.com/mislav/bump-homebrew-formula-action
    * ansible-lint: update to use pypi resources
    
    Closes #126562.
    
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ansible-lint.rb
+++ Formula/ansible-lint.rb
@@ -3,8 +3,8 @@ class AnsibleLint < Formula
 
   desc "Checks ansible playbooks for practices and behaviour"
   homepage "https://ansible-lint.readthedocs.io/"
-  url "https://files.pythonhosted.org/packages/23/14/2a8876727995a0fb06925f032f18e95e0390400cead6801688129d0b647a/ansible-lint-6.14.2.tar.gz"
-  sha256 "de87ef104302c2c6fcb43798a75c42d4f0a5c07e887c2a1d07a0b5a9fb774de0"
+  url "https://files.pythonhosted.org/packages/69/3c/c800f00e516ed76ef1b102f60edd299207ab51fedbeb2e13c7170a8a7947/ansible-lint-6.14.3.tar.gz"
+  sha256 "73ec59929b6214f6d2ce5858c22c64e3a1daba7b8cdc1271833bb6d3c7155449"
   license all_of: ["MIT", "GPL-3.0-or-later"]
 
   bottle do
@@ -36,8 +36,8 @@ class AnsibleLint < Formula
   end
 
   resource "filelock" do
-    url "https://files.pythonhosted.org/packages/0b/dc/eac02350f06c6ed78a655ceb04047df01b02c6b7ea3fc02d4df24ca87d24/filelock-3.9.0.tar.gz"
-    sha256 "7b319f24340b51f55a2bf7a12ac0755a9b03e718311dac567a0f4f7fabd2f5de"
+    url "https://files.pythonhosted.org/packages/b2/ea/e1985ab55c048a93245b9be021a896760311ed6609e5818cc7630b83d66b/filelock-3.10.4.tar.gz"
+    sha256 "9fc1734dbddcdcd4aaa02c160dd94db5272b92dfa859b44ec8df28e160b751f0"
   end
 
   resource "jsonschema" do
