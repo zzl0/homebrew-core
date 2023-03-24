a903dc43bb26397cf78f6fea8e17d1ce98797bc4
Rui Chen <rui@chenrui.dev>
2023-03-24 11:26:42 -0400

    envd 0.3.15

--- Formula/envd.rb
+++ Formula/envd.rb
@@ -1,8 +1,8 @@
 class Envd < Formula
   desc "Reproducible development environment for AI/ML"
   homepage "https://envd.tensorchord.ai"
-  url "https://github.com/tensorchord/envd/archive/v0.3.14.tar.gz"
-  sha256 "c8ca3948060b8c1240c121bf2daa30448c2e60f2ac06568e7ce99c23cd37c2f8"
+  url "https://github.com/tensorchord/envd/archive/v0.3.15.tar.gz"
+  sha256 "e01aa5bdc705c25c0ce5b0acd625de314e27c1fec4c63c871ba05d598a640dc0"
   license "Apache-2.0"
   head "https://github.com/tensorchord/envd.git", branch: "main"
 
