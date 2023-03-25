ef5e8df7d448aa867fed457ca333661d657eed30
Patrick Linnane <patrick@linnane.io>
2023-03-25 16:59:34 -0700

    vgmstream: add `pkg-config` build dep

--- Formula/vgmstream.rb
+++ Formula/vgmstream.rb
@@ -28,6 +28,7 @@ class Vgmstream < Formula
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
   depends_on "ffmpeg"
   depends_on "jansson"
   depends_on "libao"
