626f6ca0bbb0d09502c44255598ac2d4413d90c7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-03-25 17:04:37 +0000

    jpegoptim: update 1.5.3 bottle.

--- Formula/jpegoptim.rb
+++ Formula/jpegoptim.rb
@@ -7,13 +7,13 @@ class Jpegoptim < Formula
   head "https://github.com/tjko/jpegoptim.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "a4e84a197e57cad5030cb1a0236729e42b8fb8812bff3af4750eea5a421f951b"
-    sha256 cellar: :any,                 arm64_monterey: "0b84fa4d614118e5ad31c904b21b14d0e838ddb2b868dda33e23aa728f0dd3f5"
-    sha256 cellar: :any,                 arm64_big_sur:  "2d053fd692342f8e0ffc3bf73cde05b414212b679e7f0c550dc16e72cc8259fa"
-    sha256 cellar: :any,                 ventura:        "a7118ac5b89a31a18251fe3bb669449e18d143961068167efe26d39e52894157"
-    sha256 cellar: :any,                 monterey:       "1bfcb4ad450508a3072766b65ad975b22fd37b20eb77888a044960d012900ed4"
-    sha256 cellar: :any,                 big_sur:        "a548d4a4621dacb702f3af88bcaab382d899dd570c8e1428ece5f846b58f4a7e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cc2fa149032fa3622ad418e4f32510a6ebfa047758b20cf1ba49328b1a118418"
+    sha256 cellar: :any,                 arm64_ventura:  "ad500f2fb30d2217f72af4b131571d6fa7cd98c61be8c585a6477cf21538f031"
+    sha256 cellar: :any,                 arm64_monterey: "76b5a0ea886b0a021420c55d22a615a1113e6d9d106d4175aa858e9467dae902"
+    sha256 cellar: :any,                 arm64_big_sur:  "5f20eaae27de467ce9c8977ea8900bbff361bedeeb75b9ffb7cd970b253d06c4"
+    sha256 cellar: :any,                 ventura:        "5e7a7c005f27280f6b994d2ee124cbb3a14fc1eda0e782a9e080a8dcc2bca0ce"
+    sha256 cellar: :any,                 monterey:       "e5648c7b4ff0fae543ff422cb4323eb9cfac344d2bc5a1320b54f792eb43a24d"
+    sha256 cellar: :any,                 big_sur:        "2b3bb6f6e56f46b5fb999839e9b1dae731c52c33657be0ce7b623efb9bf8fbb4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e41a044bc8811107483210b120b743d659cc29847f60da83fff8dd78d5306673"
   end
 
   depends_on "jpeg-turbo"
