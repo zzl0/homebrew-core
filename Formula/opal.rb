d4a0546c63b93c6d24b1d71cd766925eea403e38
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-03-25 21:00:21 +0000

    opal: add 1.7.3 bottle.

--- Formula/opal.rb
+++ Formula/opal.rb
@@ -7,6 +7,16 @@ class Opal < Formula
   license "MIT"
   head "https://github.com/opal/opal.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0770525b9220274fa50cf9ff0a16aa0c234d17c421461a3108b4dffe7059556d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "0770525b9220274fa50cf9ff0a16aa0c234d17c421461a3108b4dffe7059556d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0770525b9220274fa50cf9ff0a16aa0c234d17c421461a3108b4dffe7059556d"
+    sha256 cellar: :any_skip_relocation, ventura:        "0770525b9220274fa50cf9ff0a16aa0c234d17c421461a3108b4dffe7059556d"
+    sha256 cellar: :any_skip_relocation, monterey:       "0770525b9220274fa50cf9ff0a16aa0c234d17c421461a3108b4dffe7059556d"
+    sha256 cellar: :any_skip_relocation, big_sur:        "0770525b9220274fa50cf9ff0a16aa0c234d17c421461a3108b4dffe7059556d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2dea8efa3acb510fa6bbde473500e225f4a72875ce3f303271aede952b1ca2e4"
+  end
+
   depends_on "quickjs" => :test
 
   uses_from_macos "ruby"
