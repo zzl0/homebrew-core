dfe33201dcb12c516529bdfeb5d979dd3acc1177
rtx-vm <123107610+rtx-vm@users.noreply.github.com>
2023-03-25 17:40:49 -0500

    rtx 1.27.7 (#126660)
    * rtx 1.27.7
    
    * rtx: update 1.27.7 bottle.
    
    ---------
    
    Co-authored-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/rtx.rb
+++ Formula/rtx.rb
@@ -1,19 +1,19 @@
 class Rtx < Formula
   desc "Polyglot runtime manager (asdf rust clone)"
   homepage "https://github.com/jdxcode/rtx"
-  url "https://github.com/jdxcode/rtx/archive/refs/tags/v1.27.6.tar.gz"
-  sha256 "3ba8a14a9dd9494682f83bfbad9280cd9f43569e5890669a1bceab4287798055"
+  url "https://github.com/jdxcode/rtx/archive/refs/tags/v1.27.7.tar.gz"
+  sha256 "708040a948a278fbcd1a987babbe005c93fb77514099fc3df75d296391095c70"
   license "MIT"
   head "https://github.com/jdxcode/rtx.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3b6e602d25ea8f83427a1168b22dfe4eb9fc383bc17a1ded78aa585905603f11"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "014e78257b80f591ad46ff9ab3c8cec0a7424de948cb276f2f69b1cce1942817"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7bdd7662e2086f443ce62030eff48643da9c274d48614a8229246088b8c3ea23"
-    sha256 cellar: :any_skip_relocation, ventura:        "9cdbb7ee117e7d07ce085f877a041982cb9f33a7d68a40cf6697fc4aa99b382a"
-    sha256 cellar: :any_skip_relocation, monterey:       "18134970bc877c692afd362dd920df8ff4fbea34bb1334cf92b379c20436aed4"
-    sha256 cellar: :any_skip_relocation, big_sur:        "d7c963d8c343a06030c37b4f15e606957903d7dee303f99e4518cf6970f11b4c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8f0b397b0032f85f398226679eabd948b982160339f652d1129d26c983c30d5e"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "857f6a3a209adef90fc7b2f41745e02bfc7ed1ff24096e0adfc2b795dcf230ad"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "30c4b1854d3c6ba5d1797377e2ddcddee5856b9057f555a9fac31d495a3664a5"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4a409d04826a8e8dbdabeb0883cbe007ad7e30db86c26bf81cca968f5e594d35"
+    sha256 cellar: :any_skip_relocation, ventura:        "86e44f3f28365d399653e16443d7ed8ef69b497389a211c26cb97b2fba6b83fc"
+    sha256 cellar: :any_skip_relocation, monterey:       "cecf8b7ef955cf33b53422bb6f07476bce870e27db209bcd42b3d2c8681a553a"
+    sha256 cellar: :any_skip_relocation, big_sur:        "7e495e4b0f2617613eca9617b5a98b6397b50b0c7acc3e47f4319433d6ae4213"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f0b177e7760e1669ea6aaca5aaf68733aec723024b168bbdbfe202d4012a16b5"
   end
 
   depends_on "rust" => :build
