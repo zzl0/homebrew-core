f0f1d4b0c493c269fae0f70047b4250d755fa87f
Porkepix <porkepix@gmail.com>
2023-03-25 01:22:42 +0100

    libdeflate 1.18
    Closes #126604.
    
    Signed-off-by: Ruoyu Zhong <zhongruoyu@outlook.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libdeflate.rb
+++ Formula/libdeflate.rb
@@ -1,8 +1,8 @@
 class Libdeflate < Formula
   desc "Heavily optimized DEFLATE/zlib/gzip compression and decompression"
   homepage "https://github.com/ebiggers/libdeflate"
-  url "https://github.com/ebiggers/libdeflate/archive/v1.17.tar.gz"
-  sha256 "fa4615af671513fa2a53dc2e7a89ff502792e2bdfc046869ef35160fcc373763"
+  url "https://github.com/ebiggers/libdeflate/archive/v1.18.tar.gz"
+  sha256 "225d982bcaf553221c76726358d2ea139bb34913180b20823c782cede060affd"
   license "MIT"
 
   bottle do
