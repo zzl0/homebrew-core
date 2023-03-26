85b03fe9977a76bf6e847e990c9422f1ef8057a1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-03-26 09:27:34 +0000

    streamvbyte: add 0.5.2 bottle.

--- Formula/streamvbyte.rb
+++ Formula/streamvbyte.rb
@@ -6,6 +6,16 @@ class Streamvbyte < Formula
   license "Apache-2.0"
   head "https://github.com/lemire/streamvbyte.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "c91d461a760c7f3f1e24f6e89471e25ffdf1d7060ba7c3b1d3a3c8ab5657b29b"
+    sha256 cellar: :any,                 arm64_monterey: "63125859b4d53740c682cca73941722d335f4c6acf98eb9c96a9ef10f4d82d9a"
+    sha256 cellar: :any,                 arm64_big_sur:  "abb3a56c9e6af35c50b713e05d145e19c0a36fca92d4e68fc19afd4a1166b26e"
+    sha256 cellar: :any,                 ventura:        "2812d68567baac71fd240c2c4ef18b3d4e6cad40baadb337c9f779c46c877a38"
+    sha256 cellar: :any,                 monterey:       "8539b7a1a74b4cf2d91625eef4490a3451fba6838bc240ebf74c1517b232fe66"
+    sha256 cellar: :any,                 big_sur:        "2f3c52073a943d8277dfcdb19315eaca97d713120892c5e5488b08f849188ae3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ed60a7eee68d7ef8b2009f46b66df9af72ef9a65639d9313d076909904a0d93d"
+  end
+
   depends_on "cmake" => :build
 
   def install
