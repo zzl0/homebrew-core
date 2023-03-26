3d60d96dcae9e4659b67e205769b041dcb4c4f6e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-03-26 00:26:08 +0000

    btrfs-progs: update 6.2.2 bottle.

--- Formula/btrfs-progs.rb
+++ Formula/btrfs-progs.rb
@@ -14,7 +14,7 @@ class BtrfsProgs < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "a454a9071d0a4bc66bf91a978d83d8c532122bbe240350ce8d02587c6ac3424c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "94a8e153ee9e6117507606e1bac866cb68ef00a52d9e936861d1916d603de7c5"
   end
 
   depends_on "pkg-config" => :build
