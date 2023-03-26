369bb6f0a6a9a31e1740721283f55037f7cfdf0e
Caleb Xu <calebcenter@live.com>
2023-03-25 20:11:30 -0400

    zpaqfranz 57.4 (new formula)

---
+++ Formula/zpaqfranz.rb
@@ -0,0 +1,26 @@
+class Zpaqfranz < Formula
+  desc "Deduplicating command-line archiver and backup tool"
+  homepage "https://github.com/fcorbelli/zpaqfranz"
+  url "https://github.com/fcorbelli/zpaqfranz/archive/refs/tags/57.4.tar.gz"
+  sha256 "eaf8a37af9eb9184b6c1e58ec237a8582b17096931b2c33eb13d02e1682952d9"
+  license all_of: [:public_domain, "MIT", "Zlib", "Unlicense", "BSD-2-Clause", "Apache-2.0"]
+  head "https://github.com/fcorbelli/zpaqfranz.git", branch: "main"
+
+  def install
+    bin.mkdir
+
+    # JIT only works on Intel (might work on Linux aarch64, but not Apple Silicon)
+    ENV.append_to_cflags "-DNOJIT" unless Hardware::CPU.intel?
+
+    system "make", "install", "-f", "NONWINDOWS/Makefile", "BINDIR=#{bin}/#{name}"
+    man1.install Utils::Gzip.compress("man/zpaqfranz.1")
+  end
+
+  test do
+    system bin/"zpaqfranz", "autotest", "-to", testpath/"archive"
+    system bin/"zpaqfranz", "extract", testpath/"archive/sha256.zpaq", "-to", testpath/"out/"
+    testpath.glob("out/*").each do |path|
+      assert_equal path.basename.to_s.downcase, Digest::SHA256.hexdigest(path.read)
+    end
+  end
+end
