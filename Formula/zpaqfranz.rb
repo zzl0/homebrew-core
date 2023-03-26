61455e7f7a74ebc1ff6330bc4f9b476c2c4c4247
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-03-26 02:16:12 +0000

    zpaqfranz: add 57.4 bottle.

--- Formula/zpaqfranz.rb
+++ Formula/zpaqfranz.rb
@@ -6,6 +6,16 @@ class Zpaqfranz < Formula
   license all_of: [:public_domain, "MIT", "Zlib", "Unlicense", "BSD-2-Clause", "Apache-2.0"]
   head "https://github.com/fcorbelli/zpaqfranz.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "35547fcb098e2c9ba6a9b3a97c33c7b63251eca64c2f2255434bf4714bf1b99a"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "6ef8fce40757ad582d98dee8448d856fff55b9ea5c62845ac67a8522f5918b55"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "56a90011e9fe15f5802987bb4d8850d2d791b1b0f42fe5b3fe777fc78aa6b14f"
+    sha256 cellar: :any_skip_relocation, ventura:        "daf3742b6f070348f604739eec61e4ad94a532c912b3678c660350a7fbcc9398"
+    sha256 cellar: :any_skip_relocation, monterey:       "95cfef3ae6d6d5430d473f4b8ac6755d12ecb9559b4c3df4564df499591f1d24"
+    sha256 cellar: :any_skip_relocation, big_sur:        "4bf77b13859510afc92ce1b7a9fdfa6a4b51492840486ca9f22a76ebea2e6f69"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "56b3da3986185c1486f366b06f47e8fb9b20ac2a32ad20812119e006e49b7a34"
+  end
+
   def install
     bin.mkdir
 
