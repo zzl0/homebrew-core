8c7ae2f3fac440fcbe0a83ca4fca53f4356ca6da
Benjamin Reich <dev@benjaminreich.de>
2023-03-27 17:44:36 +0200

    Renovate 35.18.1 (new formula)

---
+++ Formula/renovate.rb
@@ -0,0 +1,22 @@
+require "language/node"
+
+class Renovate < Formula
+  desc "Automated dependency updates. Flexible so you don't need to be"
+  homepage "https://github.com/renovatebot/renovate"
+  url "https://registry.npmjs.org/renovate/-/renovate-35.18.1.tgz"
+  sha256 "e048c9c8762288eddb328d81b8dfdb87f73a13109d8d7cb162bf97c68fd1024c"
+  license "AGPL-3.0-only"
+
+  depends_on "node"
+
+  uses_from_macos "git", since: :monterey
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match "FATAL: You must configure a GitHub token", shell_output("#{bin}/renovate 2>&1", 1)
+  end
+end
