4b24c387599744018eaaf76c191bb0963a942e24
Patrick Linnane <patrick@linnane.io>
2023-03-27 14:26:49 -0700

    blahtexml 1.0

--- Formula/blahtexml.rb
+++ Formula/blahtexml.rb
@@ -1,10 +1,9 @@
 class Blahtexml < Formula
   desc "Converts equations into Math ML"
-  homepage "http://gva.noekeon.org/blahtexml/"
-  url "http://gva.noekeon.org/blahtexml/blahtexml-0.9-src.tar.gz"
-  sha256 "c5145b02bdf03cd95b7b136de63286819e696639824961d7408bec4591bc3737"
+  homepage "https://github.com/gvanas/blahtexml"
+  url "https://github.com/gvanas/blahtexml/archive/refs/tags/v1.0.tar.gz"
+  sha256 "ef746642b1371f591b222ce3461c08656734c32ad3637fd0574d91e83995849e"
   license "BSD-3-Clause"
-  revision 1
 
   bottle do
     sha256 cellar: :any,                 arm64_ventura:  "8649e81f949cbc530d00a446b56fee8ea6013a9bac9f8abc732c0ceda928b9d9"
@@ -22,17 +21,6 @@ class Blahtexml < Formula
   end
 
   depends_on "xerces-c"
-
-  # Add missing unistd.h includes, taken from MacPorts
-  patch :p0 do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/0632225f/blahtexml/patch-mainPng.cpp.diff"
-    sha256 "7d4bce5630881099b71beedbbc09b64c61849513b4ac00197b349aab2eba1687"
-  end
-
-  patch :p0 do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/0632225f/blahtexml/patch-main.cpp.diff"
-    sha256 "d696d10931f2c2ded1cef50842b78887dba36679fbb2e0abc373e7b6405b8468"
-  end
 
   def install
     ENV.cxx11
