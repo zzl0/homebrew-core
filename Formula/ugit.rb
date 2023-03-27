f88276b91c9826c809e28e7e995c35f76ff8551d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-03-27 14:46:07 +0000

    ugit: update 5.5 bottle.

--- Formula/ugit.rb
+++ Formula/ugit.rb
@@ -6,7 +6,7 @@ class Ugit < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "d39f93399c5b790f6af0d142eb21344469915e29f7348ad541fb7ba09013e9d5"
+    sha256 cellar: :any_skip_relocation, all: "bba82208e7ab1f5966bb67c5f13867616e3547ba5517c62d69f9539b143efab1"
   end
 
   depends_on "bash"
