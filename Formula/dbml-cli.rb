f59ba507691288ed0a3d093811d6b558239c0ce9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-03-27 14:46:29 +0000

    dbml-cli: update 2.5.2 bottle.

--- Formula/dbml-cli.rb
+++ Formula/dbml-cli.rb
@@ -8,13 +8,13 @@ class DbmlCli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a2dcc8c46db4e6603f3c2b90bec64d51fb42c2cf603aa471a6d672fcd1f1b0fb"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a2dcc8c46db4e6603f3c2b90bec64d51fb42c2cf603aa471a6d672fcd1f1b0fb"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a2dcc8c46db4e6603f3c2b90bec64d51fb42c2cf603aa471a6d672fcd1f1b0fb"
-    sha256 cellar: :any_skip_relocation, ventura:        "bb484f814d7f09259612125bd5ff387cf60746779f0bfa0324bb1e5f91165a36"
-    sha256 cellar: :any_skip_relocation, monterey:       "bb484f814d7f09259612125bd5ff387cf60746779f0bfa0324bb1e5f91165a36"
-    sha256 cellar: :any_skip_relocation, big_sur:        "bb484f814d7f09259612125bd5ff387cf60746779f0bfa0324bb1e5f91165a36"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9e75a022791c7b438ffd4a2a5ccf2013087647fea8ba488852585b49f79b8c64"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "d7ab3e40a879f58af3c86bb9df8c95b36292ffaf23de5fd2515c3c9b0d379a2d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "d7ab3e40a879f58af3c86bb9df8c95b36292ffaf23de5fd2515c3c9b0d379a2d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d7ab3e40a879f58af3c86bb9df8c95b36292ffaf23de5fd2515c3c9b0d379a2d"
+    sha256 cellar: :any_skip_relocation, ventura:        "f95d20cae4045620b6c0126a3bb5de6e7cfc7a9655241ed0afc4f8d69117b157"
+    sha256 cellar: :any_skip_relocation, monterey:       "f95d20cae4045620b6c0126a3bb5de6e7cfc7a9655241ed0afc4f8d69117b157"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f95d20cae4045620b6c0126a3bb5de6e7cfc7a9655241ed0afc4f8d69117b157"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4161b5e7da0288272f4638273f91a9f5aefb31fa2683587dfdf1508465bf1aac"
   end
 
   depends_on "node"
