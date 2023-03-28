5480cb37e364c52aa848bf1934de39cea5ad9bdc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-03-28 22:59:37 +0000

    fonttools: update 4.39.3 bottle.

--- Formula/fonttools.rb
+++ Formula/fonttools.rb
@@ -9,13 +9,13 @@ class Fonttools < Formula
   head "https://github.com/fonttools/fonttools.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "28adc41013dc88c1b016d5d6e46a5be98f39f1c2e2e7dae72460c287c212a565"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "fb28656d2bb03dcc67d7b5a6efa9a47c2a44a9600c987209a2c9496a2fbd56d5"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bbc8ace4d1c8e0d4defd68a6fa6e1a738b06e1e2e927d699136ef73f4cccc28d"
-    sha256 cellar: :any_skip_relocation, ventura:        "c8b450c8e6dbd9feff01de12a3f54816012ff7255c174e9c92cdb5b19314db84"
-    sha256 cellar: :any_skip_relocation, monterey:       "5c02a4e2b3193128e017a87740f363bfaea7e9db6387ce07aa320e8dd597ec4f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "436253baaf064ea682faa3613384f2f16cc7ae49c977dbecdd6e1c40655c71a9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fe39d937dd2feee5fde9c7399013d991e2512665e910bdd5e577eafee31a7f48"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "126a1f7b209d33b376eeecdf3652eac7bd4cd025b31d1ef5214d20a5905173f4"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3090e113d33e59d24def55d5b6e866e3f7c29743e4238684882d0d11c84e13d1"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4903adb26d76e0e79c5d0274628be83b355e4768ab37d2da8d3f68c5bba47d4c"
+    sha256 cellar: :any_skip_relocation, ventura:        "54b9e5dc40720de0aa4f931cb5e6d2ffaf043d644b09fca59330f891e2d0602a"
+    sha256 cellar: :any_skip_relocation, monterey:       "3aa7f9a3ef22343a96fae40d13a63aac5e215dd1851c190259bfc0e62bc9ada8"
+    sha256 cellar: :any_skip_relocation, big_sur:        "2acd69cfe6b2e03da5944c562bdc7089cc3e3f41bdeb3154f96adbfed6691f06"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "57e6763fdc511ad7f1144c23c42a03fe1a44b143657460baceade9a1b93d56d2"
   end
 
   depends_on "python@3.11"
