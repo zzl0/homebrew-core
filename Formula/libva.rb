89730a055ec2696cdb447f0c36cd57b264bee71a
Rui Chen <rui@chenrui.dev>
2023-03-28 11:36:07 -0400

    libva 2.18.0

--- Formula/libva.rb
+++ Formula/libva.rb
@@ -1,8 +1,8 @@
 class Libva < Formula
   desc "Hardware accelerated video processing library"
   homepage "https://github.com/intel/libva"
-  url "https://github.com/intel/libva/releases/download/2.17.0/libva-2.17.0.tar.bz2"
-  sha256 "f3e5eb27c305e05a9bb62703d1915a127301cc0c4f9f209025767432230c4eac"
+  url "https://github.com/intel/libva/releases/download/2.18.0/libva-2.18.0.tar.bz2"
+  sha256 "a3577eeba0c23924686c7e2f2030073736c8282a80f27b5473e33ea94ccd4982"
   license "MIT"
 
   livecheck do
