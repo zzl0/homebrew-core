bd5009da516ae99064a3dc7d33597e342b8f458d
Rui Chen <rui@chenrui.dev>
2023-03-30 10:07:48 -0400

    gitleaks 8.16.2

--- Formula/gitleaks.rb
+++ Formula/gitleaks.rb
@@ -1,8 +1,8 @@
 class Gitleaks < Formula
   desc "Audit git repos for secrets"
   homepage "https://github.com/zricethezav/gitleaks"
-  url "https://github.com/zricethezav/gitleaks/archive/v8.16.1.tar.gz"
-  sha256 "203269c7eeda5129ae07536242e3dba8c850868582fb58827d81a09a0969233b"
+  url "https://github.com/zricethezav/gitleaks/archive/v8.16.2.tar.gz"
+  sha256 "1636fcc82cc2a37c4e4965990080f779ddfeb69d7f49ef805d0d6e9a124a6dae"
   license "MIT"
 
   bottle do
