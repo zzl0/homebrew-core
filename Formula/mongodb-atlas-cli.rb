a5c8d173ba6ffb8f2684446edef79b1e8005a139
Rui Chen <rui@chenrui.dev>
2023-03-30 10:46:08 -0400

    mongodb-atlas-cli: update sha

--- Formula/mongodb-atlas-cli.rb
+++ Formula/mongodb-atlas-cli.rb
@@ -2,7 +2,7 @@ class MongodbAtlasCli < Formula
   desc "Atlas CLI enables you to manage your MongoDB Atlas"
   homepage "https://www.mongodb.com/docs/atlas/cli/stable/"
   url "https://github.com/mongodb/mongodb-atlas-cli/archive/refs/tags/atlascli/v1.6.0.tar.gz"
-  sha256 "c435fc295f6d6653ce0bb6a6bec8dab77ede4e3c7bd845b8f048f6ac6fbe5f46"
+  sha256 "736591909f205c52e2f8dceb8918fb6a0a9645ef387ea8ad2dac3a81734897fa"
   license "Apache-2.0"
   head "https://github.com/mongodb/mongodb-atlas-cli.git", branch: "master"
 
