7a631733fc13eee218fa05415384825b53ecaa4f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-03-30 18:10:04 +0000

    libansilove: add 1.3.1 bottle.

--- Formula/libansilove.rb
+++ Formula/libansilove.rb
@@ -5,6 +5,16 @@ class Libansilove < Formula
   sha256 "4919d9a1243df7b23de677ea595f56aa7f6be7187fb0835f1915a06865c11f85"
   license "BSD-2-Clause"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "a119370b770618edb36cba9dd12c3ca461c5203ede0a3e9b35e260aeea2a1c04"
+    sha256 cellar: :any,                 arm64_monterey: "72a6824d93c50b1745a9a9b0540f7e6037136322017bfdae7a691fa82a432508"
+    sha256 cellar: :any,                 arm64_big_sur:  "7e16775fc888b219e6a1aa91cbc364f95bd4b29be6969301b72b8cd4d9ecddba"
+    sha256 cellar: :any,                 ventura:        "58302db2bcfef0eb28c35a05b565a954936b8fb864fcfd5556118d4001992680"
+    sha256 cellar: :any,                 monterey:       "8a51214b8cab8d5f07831ece9877f436b3f6982fb23a8cf2158e406dbc4525e0"
+    sha256 cellar: :any,                 big_sur:        "85b2bbf28557b4105ba352129e50bec2013bfff45e7e69c44490b52d4ec5ddd3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ba47e2c915af469866f1d726e03cad019a33872d147c344c06523d1a223b1c1d"
+  end
+
   depends_on "cmake" => :build
   depends_on "gd"
 
