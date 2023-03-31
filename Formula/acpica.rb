34b1704d0e13bbd7472a4dcaa8db9ae47bca3739
Patrick Linnane <patrick@linnane.io>
2023-03-31 16:06:26 -0700

    acpica 20230331

--- Formula/acpica.rb
+++ Formula/acpica.rb
@@ -1,11 +1,8 @@
 class Acpica < Formula
   desc "OS-independent implementation of the ACPI specification"
   homepage "https://www.acpica.org/"
-  # https://acpica.org/sites/acpica/files/acpica-unix-20221022.tar.gz is not available
-  # upstream issue report, https://github.com/acpica/acpica/issues/823
-  url "https://github.com/acpica/acpica/archive/refs/tags/R10_20_22.tar.gz"
-  version "20221022"
-  sha256 "1aa17eb1779cd171110074ce271a65c06046eacbba7be7ce5ee71df1b31c3b86"
+  url "https://acpica.org/sites/acpica/files/acpica-unix-20230331.tar.gz"
+  sha256 "0c5d695d605aaa61709f3c63f57a1a99b8902291723998446b0813b57ac310e2"
   license any_of: ["Intel-ACPI", "GPL-2.0-only", "BSD-3-Clause"]
   head "https://github.com/acpica/acpica.git", branch: "master"
 
