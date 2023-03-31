af21a0d3812eaf328cb16788cfb5a2edaca89f23
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-03-31 12:06:09 +0000

    helix: update 23.03 bottle.

--- Formula/helix.rb
+++ Formula/helix.rb
@@ -7,13 +7,13 @@ class Helix < Formula
   head "https://github.com/helix-editor/helix.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "9682e86e2bf301c945c77fb92e31606af64261483046f8efdcfd867d7fa0883d"
-    sha256 cellar: :any,                 arm64_monterey: "286b20ac1d02cea45262328602fb66476c6842c5042fdf16a05702893b28e5cd"
-    sha256 cellar: :any,                 arm64_big_sur:  "ea0bd7cd2c49c6f21f561972962d8d6f13c2431929024c8a31d191240cbd3a12"
-    sha256 cellar: :any,                 ventura:        "6752baa1f900ee7b74ced053bcb7b5e5ac00dd8d823cf5c8dd8afab7dea46023"
-    sha256 cellar: :any,                 monterey:       "dcafc9eac338190380361fe820515fb2745af1e6e044708853847fa912242405"
-    sha256 cellar: :any,                 big_sur:        "859393abe2375d95c1414438f3550daa9041c430b91173fb37acb6e35312b6e9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "06a540e98ac6c2153badcd1ebbddb65bf5767f8434a01810b7a9030992d1de17"
+    sha256 cellar: :any,                 arm64_ventura:  "1f1a431df3efe823df3b3905f0bf3c8ac8bf28a374df5b66363c2c4e6115b7fc"
+    sha256 cellar: :any,                 arm64_monterey: "f9456c6acbdc9706ecbe936bee549624cec6149ba8cacd940b93426ab2987b6b"
+    sha256 cellar: :any,                 arm64_big_sur:  "c1a6a23839017a74767a188b8d937e4ef6d41442c2997c23098209f2769cdeac"
+    sha256 cellar: :any,                 ventura:        "d1f3d99486edcc3b95ec5aeb205815020e0ce25f20340427c429f1f2eb4c8891"
+    sha256 cellar: :any,                 monterey:       "4a29b97dcccd03067764b8478d4c41c6a7a8e52a21440f9c08e15c29ab7b6052"
+    sha256 cellar: :any,                 big_sur:        "15d34c5105e8c2672594446b3a372e7ab458ce985c6dda4ce7b5bcfe72969881"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a996a20a3c9e045579f9c0bab62c64d0989f77bdc34608d87d07e163a6b6dcac"
   end
 
   depends_on "rust" => :build
