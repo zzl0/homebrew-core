d037fa22b556b532f4e9fc2ee01baacbcbf7f631
Patrick Linnane <patrick@linnane.io>
2023-03-30 18:28:08 -0700

    odpi 4.6.1

--- Formula/odpi.rb
+++ Formula/odpi.rb
@@ -1,8 +1,8 @@
 class Odpi < Formula
   desc "Oracle Database Programming Interface for Drivers and Applications"
   homepage "https://oracle.github.io/odpi/"
-  url "https://github.com/oracle/odpi/archive/v4.6.0.tar.gz"
-  sha256 "fdb07c734c59b807787b5677ef23edd52766bd2d3cf075d285994793edeb40c5"
+  url "https://github.com/oracle/odpi/archive/v4.6.1.tar.gz"
+  sha256 "30a50f831436528dcd5c8409e0c5e493838e0626c7718c8ec2c8f3e4f2e4b4ab"
   license any_of: ["Apache-2.0", "UPL-1.0"]
 
   bottle do
