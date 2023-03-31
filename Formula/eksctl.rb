641124b9f2f6ea6b8d9fe10ab9a7d77ee089709d
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-03-31 17:53:11 +0800

    eksctl 0.136.0

--- Formula/eksctl.rb
+++ Formula/eksctl.rb
@@ -2,8 +2,8 @@ class Eksctl < Formula
   desc "Simple command-line tool for creating clusters on Amazon EKS"
   homepage "https://eksctl.io"
   url "https://github.com/weaveworks/eksctl.git",
-      tag:      "0.135.0",
-      revision: "8d9c9c3237d3d02f3550c2a2f0dcb27a88f97eb4"
+      tag:      "0.136.0",
+      revision: "3f5a7c5e0f7a13c534103ad297a04823bb6c3998"
   license "Apache-2.0"
   head "https://github.com/weaveworks/eksctl.git", branch: "main"
 
