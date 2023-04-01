1891c6a1df52586284892b7d691078d6ee6c94fb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-01 22:09:24 +0000

    hz: add 0.6.1 bottle.

--- Formula/hz.rb
+++ Formula/hz.rb
@@ -6,6 +6,16 @@ class Hz < Formula
   license "Apache-2.0"
   head "https://github.com/cloudwego/hertz.git", branch: "develop"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "ed7731688eb9686f6f1ebd3d993b51027b75a02826f16510da623fdc68faffce"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c570eba9712d2f5a3012d418f6400a463f9d2549ef6fb39b646ef8599d5c381d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f1e00d677eb366b91f8ceb3c085346eeb4d66c0ce788dce810dff7389e2d7570"
+    sha256 cellar: :any_skip_relocation, ventura:        "e6ebcac5fb9a3d70005e134d7c7b2b7e6575f2d2a3aba92de4e2202da2713085"
+    sha256 cellar: :any_skip_relocation, monterey:       "b43dd296ede1399e0e815036e1067814ca8add5096122cadb867d8ba1dd3881b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "5f9b481c58964d03b3b3d65ffe3dadf0fce15b1ac4b199e96dfb38aaf77f8850"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a9f43a9bfed9d61729109c8d702cec19f0bb99efb72d410ef477c00c507e7aad"
+  end
+
   depends_on "go" => :build
 
   def install
