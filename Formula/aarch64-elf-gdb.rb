902006be0892cf457ffc8d70f8e757d00b61634b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-02 16:13:20 +0000

    aarch64-elf-gdb: add 13.1 bottle.

--- Formula/aarch64-elf-gdb.rb
+++ Formula/aarch64-elf-gdb.rb
@@ -11,6 +11,16 @@ class Aarch64ElfGdb < Formula
     formula "gdb"
   end
 
+  bottle do
+    sha256 arm64_ventura:  "4c87c1677c2c04d7423f9caf06c9ce3edb259540bfb978df9a18a77354c2cba6"
+    sha256 arm64_monterey: "a4e588e7a35203d448bf14b0369c4337640b699ae7363d81d73ae469b38cbbc5"
+    sha256 arm64_big_sur:  "6c44025b0dd5016633b0848d91b2486957eca035b9f33252c6ffcdb0829f3a05"
+    sha256 ventura:        "37739d6f6aae10d4af684060c13765afc327228e50fed8eeae832bb86db3f0c3"
+    sha256 monterey:       "9a4c6368131cb5d8d6affd8205b9d79a7db4a2873aaf9ca6b4e7641debbe8f67"
+    sha256 big_sur:        "27a8be044f81f96a7c42f24c106b2df4d0e455406545a569c2e4691a8de5d092"
+    sha256 x86_64_linux:   "24b721fd59b732201a5d8cfd9b166af67f8220f5a3a564f1f7721e9663e185da"
+  end
+
   depends_on "aarch64-elf-gcc" => :test
   depends_on "gmp"
   depends_on "python@3.11"
