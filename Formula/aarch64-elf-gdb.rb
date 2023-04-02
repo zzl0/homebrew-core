8e9d471c7689257abd8cb2db431aa129143c8b05
Daniel Bertalan <dani@danielbertalan.dev>
2023-04-02 09:05:19 +0200

    aarch64-elf-gdb 13.1 (new formula)
    The `i386-elf` and `x86_64-elf` toolchains already have a formula for
    GDB. This commit adds it for AArch64 as well.
    
    This can be used for cross-debugging AArch64 ELF binaries on Apple
    Silicon Macs where the regular `gdb` formula is not supported.

---
+++ Formula/aarch64-elf-gdb.rb
@@ -0,0 +1,58 @@
+class Aarch64ElfGdb < Formula
+  desc "GNU debugger for aarch64-elf cross development"
+  homepage "https://www.gnu.org/software/gdb/"
+  url "https://ftp.gnu.org/gnu/gdb/gdb-13.1.tar.xz"
+  mirror "https://ftpmirror.gnu.org/gdb/gdb-13.1.tar.xz"
+  sha256 "115ad5c18d69a6be2ab15882d365dda2a2211c14f480b3502c6eba576e2e95a0"
+  license "GPL-3.0-or-later"
+  head "https://sourceware.org/git/binutils-gdb.git", branch: "master"
+
+  livecheck do
+    formula "gdb"
+  end
+
+  depends_on "aarch64-elf-gcc" => :test
+  depends_on "gmp"
+  depends_on "python@3.11"
+  depends_on "xz" # required for lzma support
+
+  uses_from_macos "zlib"
+
+  on_system :linux, macos: :ventura_or_newer do
+    depends_on "texinfo" => :build
+  end
+
+  def install
+    target = "aarch64-elf"
+    args = %W[
+      --target=#{target}
+      --prefix=#{prefix}
+      --datarootdir=#{share}/#{target}
+      --includedir=#{include}/#{target}
+      --infodir=#{info}/#{target}
+      --mandir=#{man}
+      --disable-debug
+      --disable-dependency-tracking
+      --with-lzma
+      --with-python=#{Formula["python@3.11"].opt_bin}/python3.11
+      --with-system-zlib
+      --disable-binutils
+    ]
+
+    mkdir "build" do
+      system "../configure", *args
+      ENV.deparallelize # Error: common/version.c-stamp.tmp: No such file or directory
+      system "make"
+
+      # Don't install bfd or opcodes, as they are provided by binutils
+      system "make", "install-gdb"
+    end
+  end
+
+  test do
+    (testpath/"test.c").write "void _start(void) {}"
+    system "#{Formula["aarch64-elf-gcc"].bin}/aarch64-elf-gcc", "-g", "-nostdlib", "test.c"
+    assert_match "Symbol \"_start\" is a function at address 0x",
+          shell_output("#{bin}/aarch64-elf-gdb -batch -ex 'info address _start' a.out")
+  end
+end
