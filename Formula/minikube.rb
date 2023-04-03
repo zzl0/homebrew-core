ff1848e4c195689c8233707f50d796a8ae76c842
Patrick Linnane <patrick@linnane.io>
2023-04-03 10:06:20 -0700

    minikube 1.30.0

--- Formula/minikube.rb
+++ Formula/minikube.rb
@@ -2,8 +2,8 @@ class Minikube < Formula
   desc "Run a Kubernetes cluster locally"
   homepage "https://minikube.sigs.k8s.io/"
   url "https://github.com/kubernetes/minikube.git",
-      tag:      "v1.29.0",
-      revision: "ddac20b4b34a9c8c857fc602203b6ba2679794d3"
+      tag:      "v1.30.0",
+      revision: "ba4594e7b78814fd52a9376decb9c3d59c133712"
   license "Apache-2.0"
   head "https://github.com/kubernetes/minikube.git", branch: "master"
 
