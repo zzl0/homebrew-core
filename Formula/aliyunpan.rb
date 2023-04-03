f13ff430d4974944c4adc280e41137fa4e0f797b
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2023-04-03 10:44:09 +0800

    aliyunpan: add 0.2.7-1 bottle.

--- Formula/aliyunpan.rb
+++ Formula/aliyunpan.rb
@@ -6,6 +6,16 @@ class Aliyunpan < Formula
   license "Apache-2.0"
   head "https://github.com/tickstep/aliyunpan.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "384505bd3adff369a42ea6cc7354c2b825a0066cda5a87b6905ccd409bf45c9c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "384505bd3adff369a42ea6cc7354c2b825a0066cda5a87b6905ccd409bf45c9c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "384505bd3adff369a42ea6cc7354c2b825a0066cda5a87b6905ccd409bf45c9c"
+    sha256 cellar: :any_skip_relocation, ventura:        "52b80436a036263b15a9248486220d9216ac471d4cf581c1813200b2170040bd"
+    sha256 cellar: :any_skip_relocation, monterey:       "52b80436a036263b15a9248486220d9216ac471d4cf581c1813200b2170040bd"
+    sha256 cellar: :any_skip_relocation, big_sur:        "52b80436a036263b15a9248486220d9216ac471d4cf581c1813200b2170040bd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b2e1d6b4d32d819117b1d0ddf73b973264790c522ccc657f543d9cb94dfad8d8"
+  end
+
   depends_on "go" => :build
 
   def install
