ad571ad521c020bc8a47bc7810a6e46d68a58e48
Rui Chen <rui@chenrui.dev>
2023-04-04 15:10:15 -0400

    s2n 1.3.42

--- Formula/s2n.rb
+++ Formula/s2n.rb
@@ -1,8 +1,8 @@
 class S2n < Formula
   desc "Implementation of the TLS/SSL protocols"
   homepage "https://github.com/aws/s2n-tls"
-  url "https://github.com/aws/s2n-tls/archive/v1.3.41.tar.gz"
-  sha256 "46d68a57094f2671809d43263996d4ba36457a79e856cbcd721748a7b13c60ab"
+  url "https://github.com/aws/s2n-tls/archive/v1.3.42.tar.gz"
+  sha256 "b1287f71753cd3812fd3d6cfa91c458840f98c891fae6d5585a0ee09477cb8ff"
   license "Apache-2.0"
   head "https://github.com/aws/s2n-tls.git", branch: "main"
 
