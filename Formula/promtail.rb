9b52065d01342efa4602699e3b0eaa10df00b760
Patrick Linnane <patrick@linnane.io>
2023-04-03 18:15:21 -0700

    promtail 2.8.0

--- Formula/promtail.rb
+++ Formula/promtail.rb
@@ -1,8 +1,8 @@
 class Promtail < Formula
   desc "Log agent for Loki"
   homepage "https://grafana.com/loki"
-  url "https://github.com/grafana/loki/archive/v2.7.5.tar.gz"
-  sha256 "8bfc01da348e875ff7a999af3842a14e2c698e06facdf486754127991d6b8f19"
+  url "https://github.com/grafana/loki/archive/v2.8.0.tar.gz"
+  sha256 "b54c4b11c935f267a80693a97a6037ae7fb5cd2a25f30fed17d994d134a1ea3b"
   license "AGPL-3.0-only"
   head "https://github.com/grafana/loki.git", branch: "main"
 
@@ -20,8 +20,7 @@ class Promtail < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "762bacdab267d83242d904e6d0eb8d1b327006d9a87310ea28e41a8fc6ac7478"
   end
 
-  # TODO: Try `go@1.20` or newer on the next release
-  depends_on "go@1.19" => :build
+  depends_on "go" => :build
 
   on_linux do
     depends_on "systemd"
