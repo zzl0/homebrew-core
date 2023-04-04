e98bda9038d1c8d61afe85ccf8a5e1c99e3e6e08
Joe Kratzat <joe.kratzat@oracle.com>
2023-04-04 14:45:34 -0400

    clusterctl 1.4.1
    * clusterctl 1.4.1
    * clusterctl: update sha
    
    Closes #127547.
    
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/clusterctl.rb
+++ Formula/clusterctl.rb
@@ -2,8 +2,8 @@ class Clusterctl < Formula
   desc "Home for the Cluster Management API work, a subproject of sig-cluster-lifecycle"
   homepage "https://cluster-api.sigs.k8s.io"
   url "https://github.com/kubernetes-sigs/cluster-api.git",
-      tag:      "v1.4.0",
-      revision: "2c0771782941d624e6281c953ffb33413ce9106a"
+      tag:      "v1.4.1",
+      revision: "39d87e91080088327c738c43f39e46a7f557d03b"
   license "Apache-2.0"
   head "https://github.com/kubernetes-sigs/cluster-api.git", branch: "main"
 
