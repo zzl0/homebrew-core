b86da9242bd3fc2019004783c82bcf811b85d25d
Pierre Wendling <pierre.wendling.4@gmail.com>
2023-04-05 15:45:48 -0400

    sdl2 2.26.5

--- Formula/sdl2.rb
+++ Formula/sdl2.rb
@@ -1,8 +1,8 @@
 class Sdl2 < Formula
   desc "Low-level access to audio, keyboard, mouse, joystick, and graphics"
   homepage "https://www.libsdl.org/"
-  url "https://github.com/libsdl-org/SDL/releases/download/release-2.26.4/SDL2-2.26.4.tar.gz"
-  sha256 "1a0f686498fb768ad9f3f80b39037a7d006eac093aad39cb4ebcc832a8887231"
+  url "https://github.com/libsdl-org/SDL/releases/download/release-2.26.5/SDL2-2.26.5.tar.gz"
+  sha256 "ad8fea3da1be64c83c45b1d363a6b4ba8fd60f5bde3b23ec73855709ec5eabf7"
   license "Zlib"
 
   livecheck do
