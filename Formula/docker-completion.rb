6515dceaabb47a0e862b21b00efe5cd3a57abf36
Patrick Linnane <patrick@linnane.io>
2023-04-04 18:57:15 -0700

    docker-completion 23.0.3

--- Formula/docker-completion.rb
+++ Formula/docker-completion.rb
@@ -2,8 +2,8 @@ class DockerCompletion < Formula
   desc "Bash, Zsh and Fish completion for Docker"
   homepage "https://www.docker.com/"
   url "https://github.com/docker/cli.git",
-      tag:      "v23.0.2",
-      revision: "569dd73db13099a7c3104d73aa15117b359045bc"
+      tag:      "v23.0.3",
+      revision: "3e7cbfdee1eb5be2ac23ed3668c654362dcd29b5"
   license "Apache-2.0"
   head "https://github.com/docker/cli.git", branch: "master"
 
