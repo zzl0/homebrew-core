0e05e21d974c8d4e5d7bb33d7a1f2771c51eca27
fanquake <fanquake@gmail.com>
2023-04-05 12:22:30 +0100

    linux-headers@5.15 5.15.106

--- Formula/linux-headers@5.15.rb
+++ Formula/linux-headers@5.15.rb
@@ -1,8 +1,8 @@
 class LinuxHeadersAT515 < Formula
   desc "Header files of the Linux kernel"
   homepage "https://kernel.org/"
-  url "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.15.105.tar.gz"
-  sha256 "8ea249e656879023173cb4c31bea6cdd55b02674dce75c3f6800d2931dbee135"
+  url "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.15.106.tar.gz"
+  sha256 "0529ca59457f8bee343e1d7e5fc94abf487b69fe7105eaa94dc29c92399f5680"
   license "GPL-2.0-only"
 
   livecheck do
