ef9482e95f6917c40b37131cc68c1a9c32112096
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-06 00:13:38 +0000

    cubejs-cli: update 0.32.20 bottle.

--- Formula/cubejs-cli.rb
+++ Formula/cubejs-cli.rb
@@ -8,13 +8,13 @@ class CubejsCli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "e297ac1e859a66e41fa0f6248107915ec725b52dff4ba16e2d6334106b40a3c6"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e297ac1e859a66e41fa0f6248107915ec725b52dff4ba16e2d6334106b40a3c6"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e297ac1e859a66e41fa0f6248107915ec725b52dff4ba16e2d6334106b40a3c6"
-    sha256 cellar: :any_skip_relocation, ventura:        "c8a041683217ca8f7bfc8c61756417cc052ca2765a426d612635c61be79eec50"
-    sha256 cellar: :any_skip_relocation, monterey:       "c8a041683217ca8f7bfc8c61756417cc052ca2765a426d612635c61be79eec50"
-    sha256 cellar: :any_skip_relocation, big_sur:        "c8a041683217ca8f7bfc8c61756417cc052ca2765a426d612635c61be79eec50"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e297ac1e859a66e41fa0f6248107915ec725b52dff4ba16e2d6334106b40a3c6"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "24602816409d37c9eaffb83c577d5fd64ea2ef136ca3129455ffbe5c9aa2add2"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "24602816409d37c9eaffb83c577d5fd64ea2ef136ca3129455ffbe5c9aa2add2"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "24602816409d37c9eaffb83c577d5fd64ea2ef136ca3129455ffbe5c9aa2add2"
+    sha256 cellar: :any_skip_relocation, ventura:        "b1b7069378deebfd2e29ef146622a64a8d7a0287f17161a63525654e3ed5c577"
+    sha256 cellar: :any_skip_relocation, monterey:       "b1b7069378deebfd2e29ef146622a64a8d7a0287f17161a63525654e3ed5c577"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b1b7069378deebfd2e29ef146622a64a8d7a0287f17161a63525654e3ed5c577"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "24602816409d37c9eaffb83c577d5fd64ea2ef136ca3129455ffbe5c9aa2add2"
   end
 
   depends_on "node"
