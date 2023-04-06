d2a90cdc19dfb5d53cf69870dbedb07c8fd019d5
Porkepix <porkepix@gmail.com>
2023-04-06 12:54:53 +0200

    ncmpc 0.48

--- Formula/ncmpc.rb
+++ Formula/ncmpc.rb
@@ -1,8 +1,8 @@
 class Ncmpc < Formula
   desc "Curses Music Player Daemon (MPD) client"
   homepage "https://www.musicpd.org/clients/ncmpc/"
-  url "https://www.musicpd.org/download/ncmpc/0/ncmpc-0.47.tar.xz"
-  sha256 "61da23b1bc6c7a593fdc28611932cd7a30fcf6803830e01764c29b8abed2249c"
+  url "https://www.musicpd.org/download/ncmpc/0/ncmpc-0.48.tar.xz"
+  sha256 "b4b2d27e518096de2a145ef5ddf86cf46f8ba1f849bf45c6d81183a38869b90c"
   license "GPL-2.0-or-later"
 
   livecheck do
