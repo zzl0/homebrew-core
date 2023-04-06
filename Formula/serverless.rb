c71934ad298b8089984c88d3bc70a8e66f5fad01
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-06 03:05:18 +0000

    serverless: update 3.30.0 bottle.

--- Formula/serverless.rb
+++ Formula/serverless.rb
@@ -9,13 +9,13 @@ class Serverless < Formula
   head "https://github.com/serverless/serverless.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "fefb0db960110f9120c5b57449ae34cf13f1671ab3f58b1e95409f8436b3f536"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "fefb0db960110f9120c5b57449ae34cf13f1671ab3f58b1e95409f8436b3f536"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fefb0db960110f9120c5b57449ae34cf13f1671ab3f58b1e95409f8436b3f536"
-    sha256 cellar: :any_skip_relocation, ventura:        "a88a44bada85609bf63d0a2462e05370ee0281cf6194da88781c6c8a0a9a7464"
-    sha256 cellar: :any_skip_relocation, monterey:       "a88a44bada85609bf63d0a2462e05370ee0281cf6194da88781c6c8a0a9a7464"
-    sha256 cellar: :any_skip_relocation, big_sur:        "a88a44bada85609bf63d0a2462e05370ee0281cf6194da88781c6c8a0a9a7464"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "73f0f0f43646a6ba31cb18453d59b9f3bfc837bc84bbc585cecf68d1189b7a53"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "2d31cfaf7bf47ae14969ae64ed5d507779c62258979cace7f6484389df32c4d8"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2d31cfaf7bf47ae14969ae64ed5d507779c62258979cace7f6484389df32c4d8"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2d31cfaf7bf47ae14969ae64ed5d507779c62258979cace7f6484389df32c4d8"
+    sha256 cellar: :any_skip_relocation, ventura:        "a1ea12dd84244cadf289938dc2dd36510fa6125bafecd29a3a64b33e7e4836fc"
+    sha256 cellar: :any_skip_relocation, monterey:       "a1ea12dd84244cadf289938dc2dd36510fa6125bafecd29a3a64b33e7e4836fc"
+    sha256 cellar: :any_skip_relocation, big_sur:        "a1ea12dd84244cadf289938dc2dd36510fa6125bafecd29a3a64b33e7e4836fc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d1ac4f4a3f2c14f749b63dc07eb3f9acf13ca8f3421a90041cb975d6dc0b44af"
   end
 
   depends_on "node"
