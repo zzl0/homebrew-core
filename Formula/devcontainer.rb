db1777559cda08683dcb43efd19ee302b3677879
bradschwartz <baschwartz95@gmail.com>
2023-04-06 17:59:55 -0700

    devcontainer 0.37.0 (new formula)

---
+++ Formula/devcontainer.rb
@@ -0,0 +1,30 @@
+require "language/node"
+
+class Devcontainer < Formula
+  desc "Reference implementation for the Development Containers specification"
+  homepage "https://containers.dev"
+  url "https://registry.npmjs.org/@devcontainers/cli/-/cli-0.37.0.tgz"
+  sha256 "91faa34ab83dfd61a72361247816d434a026778806c795910f35343dae4ac591"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    ENV["DOCKER_HOST"] = "/dev/null"
+    # Modified .devcontainer/devcontainer.json from CLI example:
+    # https://github.com/devcontainers/cli#try-out-the-cli
+    (testpath/".devcontainer.json").write <<~EOS
+      {
+        "name": "devcontainer-homebrew-test",
+        "image": "mcr.microsoft.com/devcontainers/rust:0-1-bullseye"
+      }
+    EOS
+    output = shell_output("#{bin}/devcontainer up --workspace-folder .", 1)
+    assert_match '{"outcome":"error","message":"', output
+  end
+end
