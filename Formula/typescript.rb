4a5eba01d10c58fffcf907b66c52e5b3a062f04e
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-04-08 07:17:34 +0800

    typescript 5.0.4

--- Formula/typescript.rb
+++ Formula/typescript.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Typescript < Formula
   desc "Language for application scale JavaScript development"
   homepage "https://www.typescriptlang.org/"
-  url "https://registry.npmjs.org/typescript/-/typescript-5.0.3.tgz"
-  sha256 "da91b8fb10d74d49214053d86e93d6f74697b7972baf53a5011cdb07df6d0527"
+  url "https://registry.npmjs.org/typescript/-/typescript-5.0.4.tgz"
+  sha256 "1e83cd17f6d48dc60d539b64684d225c019db032685f28903aa45c42dac9fa5e"
   license "Apache-2.0"
   head "https://github.com/Microsoft/TypeScript.git", branch: "main"
 
