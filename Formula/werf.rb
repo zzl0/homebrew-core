01a0349a36cce6660fc8f5b84f2735ac2c20de69
Rui Chen <rui@chenrui.dev>
2023-04-07 15:00:09 -0400

    werf 1.2.221

--- Formula/werf.rb
+++ Formula/werf.rb
@@ -1,8 +1,8 @@
 class Werf < Formula
   desc "Consistent delivery tool for Kubernetes"
   homepage "https://werf.io/"
-  url "https://github.com/werf/werf/archive/refs/tags/v1.2.220.tar.gz"
-  sha256 "2304301c241239a65a33123f2ce6df7d2e2236aea187d8be684bea52581709b0"
+  url "https://github.com/werf/werf/archive/refs/tags/v1.2.221.tar.gz"
+  sha256 "cea01e148921058ddff9d4f74ac1f9df60e12422a2519369455e05229ad1b615"
   license "Apache-2.0"
   head "https://github.com/werf/werf.git", branch: "main"
 
