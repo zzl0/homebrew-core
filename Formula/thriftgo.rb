05f97268fcb93389e901b7a607a0e495a3c66b27
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-08 18:35:49 +0000

    thriftgo: add 0.2.0 bottle.

--- Formula/thriftgo.rb
+++ Formula/thriftgo.rb
@@ -6,6 +6,16 @@ class Thriftgo < Formula
   license "Apache-2.0"
   head "https://github.com/cloudwego/thriftgo.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "e8c3d4aecbeacdf5e70458f50cb2423ecfa60a69eeb5443b2d4d236a0a7f6b49"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e8c3d4aecbeacdf5e70458f50cb2423ecfa60a69eeb5443b2d4d236a0a7f6b49"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e8c3d4aecbeacdf5e70458f50cb2423ecfa60a69eeb5443b2d4d236a0a7f6b49"
+    sha256 cellar: :any_skip_relocation, ventura:        "13cc304c6f4ebcb2978f433583b6234b4da6e3b2444d658b5608d3e9a6f0775b"
+    sha256 cellar: :any_skip_relocation, monterey:       "13cc304c6f4ebcb2978f433583b6234b4da6e3b2444d658b5608d3e9a6f0775b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "13cc304c6f4ebcb2978f433583b6234b4da6e3b2444d658b5608d3e9a6f0775b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "68cbef468ebff03271b020468a5fe696bb2dab10e94665bad0f4254fa512181c"
+  end
+
   depends_on "go" => :build
 
   def install
