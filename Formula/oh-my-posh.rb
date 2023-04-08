213169926b4e22edd53a9d568960b5fd7b083ff4
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-04-08 19:48:45 +0800

    oh-my-posh 14.29.0

--- Formula/oh-my-posh.rb
+++ Formula/oh-my-posh.rb
@@ -1,8 +1,8 @@
 class OhMyPosh < Formula
   desc "Prompt theme engine for any shell"
   homepage "https://ohmyposh.dev"
-  url "https://github.com/JanDeDobbeleer/oh-my-posh/archive/v14.28.0.tar.gz"
-  sha256 "f7140752cfa89269988e06e48aa370d46b5413104504a556e4c285cd3badaa0c"
+  url "https://github.com/JanDeDobbeleer/oh-my-posh/archive/v14.29.0.tar.gz"
+  sha256 "75eb30c8811c4625352fed09951fca9f8040eb3e0cc61a77e2e6cfe0cb0635ff"
   license "MIT"
   head "https://github.com/JanDeDobbeleer/oh-my-posh.git", branch: "main"
 
