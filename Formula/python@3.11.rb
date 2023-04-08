9f7b1a68760fbb5bb3142c3b5909a0f7f932220e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-08 17:45:57 +0000

    python@3.11: update 3.11.3 bottle.

--- Formula/python@3.11.rb
+++ Formula/python@3.11.rb
@@ -11,14 +11,13 @@ class PythonAT311 < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 arm64_ventura:  "ac5541e8c7d1c6fa43f5ab87c47906c80bebbbe31ab4bb75ec33aa112cce02af"
-    sha256 arm64_monterey: "f6847de5ca24821e72c27f6515dd51ca39ce7410569c919a06672e84bbdabb97"
-    sha256 arm64_big_sur:  "70141e420bf14f8fa020d2cd924c414f2feb9bc6523bdb827250bdc60c84c8ed"
-    sha256 ventura:        "228565950db8e0b73697fd246dba83e560aa7ca444bec0a79f53323c35471253"
-    sha256 monterey:       "24c3f95885f3eb81ae9668fe2ba61a5d6c797fe71dde11b84fefbd3ac4dab7c4"
-    sha256 big_sur:        "f0696ec651ef0f1dde1035f14deef4c255b16bcd2b8bd55a62ae37da75e36ffd"
-    sha256 x86_64_linux:   "fbb82e5e45a52f66a9c8110f8d1326b13d32a47fc2f9124e4094355a2911ee2a"
+    sha256 arm64_ventura:  "a6f7dd653b6c5904629effbfff359da0aa9903786b38abc0840c595e281a278e"
+    sha256 arm64_monterey: "270d1f7c56978757922b246dfd8bccead979c3d30a8e95d77a7b7b644050e6cd"
+    sha256 arm64_big_sur:  "13a7123b4e99f4bd574ec38e0a48d80ae6bbc36527cf9e158679f045d21160f4"
+    sha256 ventura:        "59844694b2bb56614623c8ede1679689fa32e8becf296108716de31ff3db6807"
+    sha256 monterey:       "e86210ffc0380bf4ccff3e3081e4dbd9c9ee3c2f72574d41498a817050f1ef86"
+    sha256 big_sur:        "af625209f6474e67aa4cc911c972425631d4e7ef343235b25bdf8ef11b761303"
+    sha256 x86_64_linux:   "e2df7e7b96ec43663841c3dfc8bc28d15087d39be147fde3d0dd1ba564be7a95"
   end
 
   # setuptools remembers the build flags python is built with and uses them to
