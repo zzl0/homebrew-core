7a3adef12ebe9e911a02d22c1c123befbe8e959e
koki-develop <kou.pg.0131@gmail.com>
2023-04-10 06:48:04 +0900

    gat 0.8.1 (new formula)

---
+++ Formula/gat.rb
@@ -0,0 +1,22 @@
+class Gat < Formula
+  desc "🐱 cat alternative written in Go"
+  homepage "https://github.com/koki-develop/gat"
+  url "https://github.com/koki-develop/gat/archive/refs/tags/v0.8.1.tar.gz"
+  sha256 "9e3b24f739581af9cf37323d28cd0ec1790c97b40c6a4212b9c4358465aee385"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w -X github.com/koki-develop/gat/cmd.version=v#{version}")
+  end
+
+  test do
+    (testpath/"test.sh").write 'echo "hello gat"'
+
+    assert_equal \
+      "\e[38;5;231mecho\e[0m\e[38;5;231m \e[0m\e[38;5;186m\"hello gat\"\e[0m",
+      shell_output("#{bin}/gat --force-color test.sh")
+    assert_match version.to_s, shell_output("#{bin}/gat --version")
+  end
+end
