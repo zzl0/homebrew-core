b1fe0b44a24788827289fcd11327ae9e1e5ab76e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-09 01:24:17 +0000

    trust-dns: add 0.22.0 bottle.

--- Formula/trust-dns.rb
+++ Formula/trust-dns.rb
@@ -6,6 +6,16 @@ class TrustDns < Formula
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/bluejekyll/trust-dns.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "e9a08d010802fff7fa6a94847f816669a92265596c8506fbd810e0b2248d7c75"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "aa0fdb12c49171ce77337e73fb2ec8b06a48e6a0cf78d7b2f77cef6395adfe59"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "713bfc37472d6d9fcfab685f164fd3a313f3c75f0431d75276d5a05996660250"
+    sha256 cellar: :any_skip_relocation, ventura:        "f2861da740e151a590f6e6042973302b9eb33830c0e0221496cfbe9582a5202d"
+    sha256 cellar: :any_skip_relocation, monterey:       "e2028776dc3f0710aa7f37e40b5ae9d77f61dd63adfb4e076d96870d8d563453"
+    sha256 cellar: :any_skip_relocation, big_sur:        "27cadb40add7948af20d07e7f89ffd34d72fd2fbd2584469e9558f3a94565fcf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2b521af6e9575530584db481b3cf0a225b1d5844b710eaed2652f3202519e73f"
+  end
+
   depends_on "rust" => :build
 
   on_linux do
