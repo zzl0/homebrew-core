6f8e9ef2158c8114bdd4f8e4dda569c7e60b3c1d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-10 01:44:48 +0000

    gat: add 0.8.1 bottle.

--- Formula/gat.rb
+++ Formula/gat.rb
@@ -5,6 +5,16 @@ class Gat < Formula
   sha256 "9e3b24f739581af9cf37323d28cd0ec1790c97b40c6a4212b9c4358465aee385"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "b69a1e8b2f4519a7d59dc72db3e9adbfb4a601cce72c8782bf02370e146534d3"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b69a1e8b2f4519a7d59dc72db3e9adbfb4a601cce72c8782bf02370e146534d3"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b69a1e8b2f4519a7d59dc72db3e9adbfb4a601cce72c8782bf02370e146534d3"
+    sha256 cellar: :any_skip_relocation, ventura:        "1232c9c7a48665af66e5900a7b66968fc77344fe76d4b2f5432c46820494d0a6"
+    sha256 cellar: :any_skip_relocation, monterey:       "1232c9c7a48665af66e5900a7b66968fc77344fe76d4b2f5432c46820494d0a6"
+    sha256 cellar: :any_skip_relocation, big_sur:        "1232c9c7a48665af66e5900a7b66968fc77344fe76d4b2f5432c46820494d0a6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2bd8837f4b4365c00281c603507b52989c6dee582526c72ddd5e4e0791a7a0de"
+  end
+
   depends_on "go" => :build
 
   def install
