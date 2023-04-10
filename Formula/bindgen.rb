80c5c87c3d85fa9c58134e9b30f6b57990c2f9ee
Rui Chen <rui@chenrui.dev>
2023-04-10 15:57:00 -0400

    bindgen 0.65.0

--- Formula/bindgen.rb
+++ Formula/bindgen.rb
@@ -1,8 +1,8 @@
 class Bindgen < Formula
   desc "Automatically generates Rust FFI bindings to C (and some C++) libraries"
   homepage "https://rust-lang.github.io/rust-bindgen/"
-  url "https://github.com/rust-lang/rust-bindgen/archive/refs/tags/v0.64.0.tar.gz"
-  sha256 "9d987e7e2cefebed2c856ba36438e75af00aa08d4274fc15b8c20886065cb1f2"
+  url "https://github.com/rust-lang/rust-bindgen/archive/refs/tags/v0.65.0.tar.gz"
+  sha256 "0d4009103e5ef3a200e50dddda6731b3e8204cc2c4fe4ff7ace7def85f944817"
   license "BSD-3-Clause"
 
   bottle do
