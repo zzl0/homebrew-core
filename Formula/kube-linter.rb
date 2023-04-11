ca3609e8df25c1e8efebdc9ff07c105b396259d1
Porkepix <porkepix@gmail.com>
2023-04-11 15:16:12 +0200

    kube-linter 0.6.3
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/kube-linter.rb
+++ Formula/kube-linter.rb
@@ -1,8 +1,8 @@
 class KubeLinter < Formula
   desc "Static analysis tool for Kubernetes YAML files and Helm charts"
   homepage "https://github.com/stackrox/kube-linter"
-  url "https://github.com/stackrox/kube-linter/archive/refs/tags/v0.6.1.tar.gz"
-  sha256 "07666f3bb620864b3ad5634d9acfff3c06964ec03c69542e21258c72a4503f40"
+  url "https://github.com/stackrox/kube-linter/archive/refs/tags/v0.6.3.tar.gz"
+  sha256 "a7a7bb9a94f0677b388375d96857b4a8b6db7be1ab7d6493c44274c0e7397637"
   license "Apache-2.0"
   head "https://github.com/stackrox/kube-linter.git", branch: "master"
 
