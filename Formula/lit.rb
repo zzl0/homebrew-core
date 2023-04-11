a7ff6ac4673044c9caf04ea66488b4f05a3fe388
Porkepix <porkepix@gmail.com>
2023-04-11 10:01:41 +0200

    lit 16.0.1

--- Formula/lit.rb
+++ Formula/lit.rb
@@ -3,8 +3,8 @@ class Lit < Formula
 
   desc "Portable tool for LLVM- and Clang-style test suites"
   homepage "https://llvm.org"
-  url "https://files.pythonhosted.org/packages/90/d8/acc8162b58aa44e899f6d4a4607650290624db71564e9b168716900510af/lit-16.0.0.tar.gz"
-  sha256 "3c4ac372122a1de4a88deb277b956f91b7209420a0bef683b1ab2d2b16dabe11"
+  url "https://files.pythonhosted.org/packages/ba/7f/981185aaea6ac3a4ae5de9eca143a6382e3c62c9ee9ad47cffe94f8181aa/lit-16.0.1.tar.gz"
+  sha256 "630a47291b714cb115015df23ab04267c24fe59aec7ecd7e637d5c75cdb45c91"
   license "Apache-2.0" => { with: "LLVM-exception" }
 
   bottle do
