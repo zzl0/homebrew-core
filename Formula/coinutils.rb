db7b52550b059161896a200145c1d0f8020d732c
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-04-11 12:05:34 +0800

    coinutils 2.11.8

--- Formula/coinutils.rb
+++ Formula/coinutils.rb
@@ -1,8 +1,8 @@
 class Coinutils < Formula
   desc "COIN-OR utilities"
   homepage "https://github.com/coin-or/CoinUtils"
-  url "https://github.com/coin-or/CoinUtils/archive/releases/2.11.6.tar.gz"
-  sha256 "6ea31d5214f7eb27fa3ffb2bdad7ec96499dd2aaaeb4a7d0abd90ef852fc79ca"
+  url "https://github.com/coin-or/CoinUtils/archive/releases/2.11.8.tar.gz"
+  sha256 "202e347d1c1d2ccf5355e3c2874a4dc16500226c180b00d6677f464d80be337e"
   license "EPL-2.0"
   head "https://github.com/coin-or/CoinUtils.git", branch: "master"
 
@@ -44,11 +44,8 @@ class Coinutils < Formula
     ]
     system "./configure", *args
     system "make"
-
     # Deparallelize due to error 1: "mkdir: #{include}/coinutils/coin: File exists."
-    # https://github.com/coin-or/Clp/issues/109
-    ENV.deparallelize
-    system "make", "install"
+    ENV.deparallelize { system "make", "install" }
   end
 
   test do
