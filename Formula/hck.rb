2a6bd99a142fdc9618b5b07bc08a657f063e856e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-12 20:43:16 +0000

    hck: add 0.9.2 bottle.

--- Formula/hck.rb
+++ Formula/hck.rb
@@ -6,6 +6,16 @@ class Hck < Formula
   license any_of: ["MIT", "Unlicense"]
   head "https://github.com/sstadick/hck.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "30f7c688301cd4aec0be0c3933fa31fbee2eb0294d6e9b4074e31ecc2e0808d3"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "61af5723cbd318a8039590921123d00c8b15f340cd2051ddd41da032845afd76"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1f29515c298fd34c7a5a14eea5ca3628ac6d0547887d745c1f4291acdd1e8763"
+    sha256 cellar: :any_skip_relocation, ventura:        "5ba9b32234652704f527debeed8ae7e2c4068c234080fdffad7d734480ecf893"
+    sha256 cellar: :any_skip_relocation, monterey:       "51a6f46c9751bdf496126402e6eff137a257e583164046ed82e9225c83099cd5"
+    sha256 cellar: :any_skip_relocation, big_sur:        "306221d742060825f2bd6d714637abac4a339334a51ea449fe0fde161bc73866"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d87dcb0ce645b7804532a41770208580748153d1607eb242dbce0597ea5ddfc5"
+  end
+
   depends_on "cmake" => :build
   depends_on "rust" => :build
 
