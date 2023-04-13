d38b0655638a55693871d73c339d001dcea780b1
Porkepix <porkepix@gmail.com>
2023-04-13 13:07:42 +0200

    bkt 0.6.1

--- Formula/bkt.rb
+++ Formula/bkt.rb
@@ -1,8 +1,8 @@
 class Bkt < Formula
   desc "Utility for caching the results of shell commands"
   homepage "https://www.bkt.rs"
-  url "https://github.com/dimo414/bkt/archive/refs/tags/0.6.0.tar.gz"
-  sha256 "59c8fe8b29101a47d928896468dce2f8fb3cee7598201d46011804a7bee7f6e2"
+  url "https://github.com/dimo414/bkt/archive/refs/tags/0.6.1.tar.gz"
+  sha256 "01ae0983eb199d2abd027e2b7b160c870d89477afdee2c5281faac4740814607"
   license "MIT"
 
   bottle do
