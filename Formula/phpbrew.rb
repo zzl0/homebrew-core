3bffe023176f3fb7aadffc51b60ea2227a10e50c
Rui Chen <rui@chenrui.dev>
2023-04-14 13:17:11 -0400

    phpbrew 2.1.0
    * phpbrew 2.1.0
    * phpbrew: switch to `php@8.1`
    
    Closes #128391.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: Ruoyu Zhong <zhongruoyu@outlook.com>
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/phpbrew.rb
+++ Formula/phpbrew.rb
@@ -1,8 +1,8 @@
 class Phpbrew < Formula
   desc "Brew & manage PHP versions in pure PHP at HOME"
   homepage "https://phpbrew.github.io/phpbrew"
-  url "https://github.com/phpbrew/phpbrew/releases/download/2.0.0/phpbrew"
-  sha256 "b85d2c7a2bf6cf7ec9173e0db7eb6cf9e6de56589b2d845fdbecc492dd24111a"
+  url "https://github.com/phpbrew/phpbrew/releases/download/2.1.0/phpbrew.phar"
+  sha256 "0f8f55bb31f6680ad3b9703cddb46d9c5186ea67778fc1896b35f44569d9d006"
   license "MIT"
 
   bottle do
@@ -15,7 +15,9 @@ class Phpbrew < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "03a4ce471cc9c831a38897771691b9e1089210951a6ad6e2722dfb8aa4ffa716"
   end
 
-  depends_on "php"
+  # TODO: When `php` 8.2+ support is landed, switch back to `php`.
+  # https://github.com/phpbrew/phpbrew/blob/#{version}/composer.json#L27
+  depends_on "php@8.1"
 
   # Keg-relocation breaks the formula when it replaces `/usr/local` with a non-default prefix
   on_macos do
@@ -25,7 +27,14 @@ class Phpbrew < Formula
   end
 
   def install
-    bin.install "phpbrew"
+    # When switched back to `php`, use `bin.install` instead.
+    # bin.install "phpbrew.phar" => "phpbrew"
+
+    libexec.install "phpbrew.phar"
+    (bin/"phpbrew").write <<~EOS
+      #!#{Formula["php@8.1"].opt_bin}/php
+      <?php require '#{libexec}/phpbrew.phar';
+    EOS
   end
 
   test do
