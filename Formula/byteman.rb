e0b1a8fb3801776738767e2f586dbb1789246c4f
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-04-14 19:29:28 +0800

    byteman 4.0.21

--- Formula/byteman.rb
+++ Formula/byteman.rb
@@ -1,8 +1,8 @@
 class Byteman < Formula
   desc "Java bytecode manipulation tool for testing, monitoring and tracing"
   homepage "https://byteman.jboss.org/"
-  url "https://downloads.jboss.org/byteman/4.0.20/byteman-download-4.0.20-bin.zip"
-  sha256 "e37c15b854c5002716a5a9d384c443100a203f47516657533a9808f6e429bcfd"
+  url "https://downloads.jboss.org/byteman/4.0.21/byteman-download-4.0.21-bin.zip"
+  sha256 "016e2f069e78611e28035d6692dfc674110dc36c0e403c5ef57d464661639359"
   license "LGPL-2.1-or-later"
   head "https://github.com/bytemanproject/byteman.git", branch: "main"
 
