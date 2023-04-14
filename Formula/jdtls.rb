1ab2785893d51ed52f261bbf64660df18b090e9f
Rui Chen <rui@chenrui.dev>
2023-04-13 21:39:32 -0400

    jdtls 1.22.0

--- Formula/jdtls.rb
+++ Formula/jdtls.rb
@@ -3,9 +3,9 @@ class Jdtls < Formula
 
   desc "Java language specific implementation of the Language Server Protocol"
   homepage "https://github.com/eclipse/eclipse.jdt.ls"
-  url "https://download.eclipse.org/jdtls/milestones/1.21.0/jdt-language-server-1.21.0-202303161431.tar.gz"
-  version "1.21.0"
-  sha256 "73c4434af3a02db974e4b0cd7a52a0417b9c6c99e327b19572eb7a9954fa1a30"
+  url "https://download.eclipse.org/jdtls/milestones/1.22.0/jdt-language-server-1.22.0-202304131553.tar.gz"
+  version "1.22.0"
+  sha256 "5fe2b9595379bb40354c91b9224eb172f2b4d7ce855c3294ab92e84d2c0bbf41"
   license "EPL-2.0"
   version_scheme 1
 
