abbe707f04ecdd343d972034de475eba6c420f22
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-14 14:57:51 +0000

    aiven-client: update 2.19.0 bottle.

--- Formula/aiven-client.rb
+++ Formula/aiven-client.rb
@@ -9,13 +9,13 @@ class AivenClient < Formula
   head "https://github.com/aiven/aiven-client.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "36505cd0ec09fad6c224ac9423477e12bc5ac5968afe716f8c522709a5c75bec"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a5b33d55acfa430417c5f60fb071ac2ba67e4229b2c4458781ba9de51d08364a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a56a8300fc654ebba93d4fa8b52cd597b8acbf369753008117adf3052b6b4b80"
-    sha256 cellar: :any_skip_relocation, ventura:        "0d4887f17b7ebaa172d61aaaf34c4d467985705e3485c99a66e77c67ea7ce8fe"
-    sha256 cellar: :any_skip_relocation, monterey:       "7b6089b99528c0a55169b4e2d941d6610c308b663acf408c3bd19a1b6ac3db1b"
-    sha256 cellar: :any_skip_relocation, big_sur:        "01caefe620f3ec23be6a20bd8a901a2167c56ea83d6039cf180aef9bcc32b2eb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "304d7f6618163e5b44456a54583c982bdceca9c3baffed22cb597a952dd894c4"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "ce354e85c6f7f6d1de5370ff5c2f4002a33745a61c0698c347693c4d50087b9e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b37d5dfa7d3763ffcf1ef8b11c0ce69ca4269737ca0a8121bae19292b7bcbc94"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "dd9e293b9dd3d2c482642b7dacd41fa4509ebe674fb06242f5a51767dd76ba29"
+    sha256 cellar: :any_skip_relocation, ventura:        "be7b7aed8f3db4a8503b7ec21ca6f96e5e19b23788d5f26700ef44db4f126f5f"
+    sha256 cellar: :any_skip_relocation, monterey:       "6d72e266b15812c48f951673224c4f2ef90370f0a98cc6dc00476e362366363f"
+    sha256 cellar: :any_skip_relocation, big_sur:        "91d7c373fb675d2d8c74a9416a5eb3deb69299f6fd3eb786a2bfebcaf1eaadee"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "13d9062d25342099f1f87ab92c85e1b0c899c6f3b7be09997ae506c68f5d6e03"
   end
 
   depends_on "python@3.11"
