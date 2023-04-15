6097d64278b3777d9a0c8a8c37b9616d1d1846f2
Rui Chen <rui@chenrui.dev>
2023-04-15 08:57:09 -0400

    asciidoctorj 2.5.8

--- Formula/asciidoctorj.rb
+++ Formula/asciidoctorj.rb
@@ -1,8 +1,8 @@
 class Asciidoctorj < Formula
   desc "Java wrapper and bindings for Asciidoctor"
   homepage "https://github.com/asciidoctor/asciidoctorj"
-  url "https://search.maven.org/remotecontent?filepath=org/asciidoctor/asciidoctorj/2.5.7/asciidoctorj-2.5.7-bin.zip"
-  sha256 "e25292735620cbb31af11e2fc730b52d9dac35a68f2f2f7ca4ee5aef0dfb7c37"
+  url "https://search.maven.org/remotecontent?filepath=org/asciidoctor/asciidoctorj/2.5.8/asciidoctorj-2.5.8-bin.zip"
+  sha256 "1a99c39fbff715fa4d6eff7f1954e164e5d60169ad64005a87fcd9827adc8c90"
   license "Apache-2.0"
 
   livecheck do
