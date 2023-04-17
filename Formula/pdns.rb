5957a98d5352af694247f9dece337096f5ff8229
Patrick Linnane <patrick@linnane.io>
2023-04-17 09:43:55 -0700

    pdns 4.7.4

--- Formula/pdns.rb
+++ Formula/pdns.rb
@@ -1,10 +1,9 @@
 class Pdns < Formula
   desc "Authoritative nameserver"
   homepage "https://www.powerdns.com"
-  url "https://downloads.powerdns.com/releases/pdns-4.7.3.tar.bz2"
-  sha256 "8bad351b2e09426f6d4fb0346881a5155fe555497c3d85071e531e7c7afe3e76"
+  url "https://downloads.powerdns.com/releases/pdns-4.7.4.tar.bz2"
+  sha256 "7469dd81fb7df11197f49638fa49ceff9f973225fc8f9c7160b0bfc00a2e7471"
   license "GPL-2.0-or-later"
-  revision 1
 
   livecheck do
     url "https://downloads.powerdns.com/releases/"
