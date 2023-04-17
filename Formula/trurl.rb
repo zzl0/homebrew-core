63c57a3a88f8528c81efa111f822b648ee775f7f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-17 20:09:33 +0000

    trurl: update 0.5 bottle.

--- Formula/trurl.rb
+++ Formula/trurl.rb
@@ -7,13 +7,13 @@ class Trurl < Formula
   head "https://github.com/curl/trurl.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "5f01af7d6379b6e93b6fa5602b289a357152475128c844d0f9904b508cb86b7f"
-    sha256 cellar: :any,                 arm64_monterey: "7114fb84d28396ae1c19f4c5346328c74876e8afdccea69677482bcd177c9201"
-    sha256 cellar: :any,                 arm64_big_sur:  "55b6e491b7312b657c3d95e08630dd3ac4c5570e4c8480c80c643dc5c7b84c2b"
-    sha256 cellar: :any_skip_relocation, ventura:        "234518183f073ad2ab68d9d3774630a1cc9051e96c6a7ac42f4af3e192eea53d"
-    sha256 cellar: :any,                 monterey:       "e40d5f777bb17ef4a35ef4fa8226d075b51555386e38a704b30066e278801495"
-    sha256 cellar: :any,                 big_sur:        "d6a5e9d40f9190676884095553c7407af0d398d7ac652740558bcc7e785c0fae"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "50dbd17162dc9f0d0aeb8e04c7c4c1280e3eb3cd1a797c68ee1b12b53376f6fe"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "66aff69a2b7a173f02e50946918441fe9f37146b880319bef235e1fe41e1a8c3"
+    sha256 cellar: :any,                 arm64_monterey: "85053c1512b84feaab1c8a1356d7a0b5ca4c5250884131b99bb325a7791127ea"
+    sha256 cellar: :any,                 arm64_big_sur:  "50af1bb799bf25da845e027c74fbbad1695f3de672099da512418b34e09b01d7"
+    sha256 cellar: :any_skip_relocation, ventura:        "13639d4eb0c341705953748ac7ba01771a3a52144d129ed80dcce2e23f8181a9"
+    sha256 cellar: :any,                 monterey:       "bd5399456f622cc57ce403dcd08fa946e84ebe952b333bc9099aef1fca97a0a4"
+    sha256 cellar: :any,                 big_sur:        "f64d616f19edc5e7252cdf68c3ad2f77b595db289d04a11c7e09306674000f5b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fa32008f4c49b68d44d165a9d6e2a52229db9a0ea58ff867f4e2addbe81d1ff4"
   end
 
   uses_from_macos "curl", since: :ventura # uses CURLUE_NO_ZONEID, available since curl 7.81.0
