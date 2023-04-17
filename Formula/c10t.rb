19c397d806578008e2a9773bd454a44d37c0ed78
Sean Molenaar <smillerdev@me.com>
2023-04-17 11:09:46 +0200

    c10t: revision bump for boost 1.82

--- Formula/c10t.rb
+++ Formula/c10t.rb
@@ -4,7 +4,7 @@ class C10t < Formula
   url "https://github.com/udoprog/c10t/archive/1.7.tar.gz"
   sha256 "0e5779d517105bfdd14944c849a395e1a8670bedba5bdab281a0165c3eb077dc"
   license "BSD-3-Clause"
-  revision 4
+  revision 5
 
   bottle do
     sha256 cellar: :any,                 arm64_ventura:  "87bfb448e0461b3f6854ca2d35216fcc949cd652935bf4f9ebdf60ea717a3351"
