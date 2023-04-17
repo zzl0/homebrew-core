550d1f48a29697d8fa8ba86e0ad859a480982d36
Patrick Linnane <patrick@linnane.io>
2023-04-17 09:32:33 -0700

    clickhouse-cpp 2.4.0
    * clickhouse-cpp 2.4.0
    * clickhouse-cpp: fix build
      Also:
      1. Add deps that would otherwise be vendored.
      2. Eliminate CMake usage in test.
    
    Closes #128575.
    
    Co-authored-by: Ruoyu Zhong <zhongruoyu@outlook.com>
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/clickhouse-cpp.rb
+++ Formula/clickhouse-cpp.rb
@@ -1,8 +1,8 @@
 class ClickhouseCpp < Formula
   desc "C++ client library for ClickHouse"
   homepage "https://github.com/ClickHouse/clickhouse-cpp#readme"
-  url "https://github.com/ClickHouse/clickhouse-cpp/archive/refs/tags/v2.3.0.tar.gz"
-  sha256 "8eb8b49044247ccc57db73fdf41807a187d8a376d3469f255bab5c0eb0a64359"
+  url "https://github.com/ClickHouse/clickhouse-cpp/archive/refs/tags/v2.4.0.tar.gz"
+  sha256 "336a1d0b4c4d6bd67bd272afab3bdac51695f8b0e93dd6c85d4d774d6c7df8ad"
   license "Apache-2.0"
   head "https://github.com/ClickHouse/clickhouse-cpp.git", branch: "master"
 
@@ -17,18 +17,30 @@ class ClickhouseCpp < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "59b4a9754fd3934831f20c2efe2398c4a56b4e7fb0de43aff7a50642e80b4bf6"
   end
 
-  depends_on "cmake" => [:build, :test]
+  depends_on "cmake" => :build
   depends_on "abseil"
+  depends_on "cityhash"
+  depends_on "lz4"
   depends_on "openssl@3"
 
   fails_with gcc: "5"
   fails_with gcc: "6"
 
   def install
-    system "cmake", "-S", ".", "-B", "build",
-                    "-DWITH_OPENSSL=ON",
-                    "-DOPENSSL_ROOT_DIR=#{Formula["openssl@3"].opt_prefix}",
-                    *std_cmake_args
+    # `cityhash` does not provide a pkg-config or CMake config file.
+    # Help CMake find it.
+    inreplace "CMakeLists.txt", "FIND_PACKAGE(cityhash REQUIRED)",
+                                "FIND_LIBRARY(CITYHASH NAMES cityhash REQUIRED)"
+    inreplace "clickhouse/CMakeLists.txt", "cityhash::cityhash", "cityhash"
+
+    args = %W[
+      -DWITH_OPENSSL=ON
+      -DOPENSSL_ROOT_DIR=#{Formula["openssl@3"].opt_prefix}
+      -DWITH_SYSTEM_ABSEIL=ON
+      -DWITH_SYSTEM_CITYHASH=ON
+      -DWITH_SYSTEM_LZ4=ON
+    ]
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
   end
@@ -73,24 +85,19 @@ class ClickhouseCpp < Formula
       }
     EOS
 
-    (testpath/"CMakeLists.txt").write <<~EOS
-      project (clickhouse-cpp-test-client LANGUAGES CXX)
-
-      set (CMAKE_CXX_STANDARD 17)
-      set (CMAKE_CXX_STANDARD_REQUIRED ON)
-
-      set (CLICKHOUSE_CPP_INCLUDE "#{include}")
-      find_library (CLICKHOUSE_CPP_LIB NAMES clickhouse-cpp-lib PATHS "#{lib}" REQUIRED NO_DEFAULT_PATH)
-
-      add_executable (test-client main.cpp)
-      target_include_directories (test-client PRIVATE ${CLICKHOUSE_CPP_INCLUDE})
-      target_link_libraries (test-client PRIVATE ${CLICKHOUSE_CPP_LIB})
-      target_compile_definitions (test-client PUBLIC WITH_OPENSSL)
-    EOS
-
-    system "cmake", "-S", testpath, "-B", (testpath/"build"), *std_cmake_args
-    system "cmake", "--build", (testpath/"build")
-
-    assert_match "Exception: fail to connect: ", shell_output(testpath/"build"/"test-client", 1)
+    args = %W[
+      -std=c++17
+      -I#{include}
+      -L#{lib}
+      -lclickhouse-cpp-lib
+      -L#{Formula["openssl@3"].opt_lib}
+      -lcrypto -lssl
+      -L#{Formula["cityhash"].opt_lib}
+      -lcityhash
+      -L#{Formula["lz4"].opt_lib}
+      -llz4
+    ]
+    system ENV.cxx, "main.cpp", *args, "-o", "test-client"
+    assert_match "Exception: fail to connect: ", shell_output("./test-client", 1)
   end
 end
