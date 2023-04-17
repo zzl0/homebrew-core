b2f9651382875197da808fc6e27c4eb733568433
Patrick Linnane <patrick@linnane.io>
2023-04-16 22:07:33 -0700

    mockery 2.25.0

--- Formula/mockery.rb
+++ Formula/mockery.rb
@@ -1,8 +1,8 @@
 class Mockery < Formula
   desc "Mock code autogenerator for Golang"
   homepage "https://github.com/vektra/mockery"
-  url "https://github.com/vektra/mockery/archive/refs/tags/v2.24.0.tar.gz"
-  sha256 "b8fcb80796c826db67dcd7a41b322ad736222e21db820272899d9c7693096f2a"
+  url "https://github.com/vektra/mockery/archive/refs/tags/v2.25.0.tar.gz"
+  sha256 "4b62a1df117ceb92b8b67603dc234895107fe8212fc955ad56990d63e0ceae3b"
   license "BSD-3-Clause"
   head "https://github.com/vektra/mockery.git", branch: "master"
 
