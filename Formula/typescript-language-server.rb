fe8abc59f380f681f2cb21aa60f07e7ac14f6161
Patrick Linnane <patrick@linnane.io>
2023-04-17 09:48:47 -0700

    typescript-language-server 3.3.2

--- Formula/typescript-language-server.rb
+++ Formula/typescript-language-server.rb
@@ -3,8 +3,8 @@ require "language/node"
 class TypescriptLanguageServer < Formula
   desc "Language Server Protocol implementation for TypeScript wrapping tsserver"
   homepage "https://github.com/typescript-language-server/typescript-language-server"
-  url "https://registry.npmjs.org/typescript-language-server/-/typescript-language-server-3.3.1.tgz"
-  sha256 "d1d03dae690388ba497dedb10039b7023b3f14a87ec386664e8fece0ebebd245"
+  url "https://registry.npmjs.org/typescript-language-server/-/typescript-language-server-3.3.2.tgz"
+  sha256 "206c4a18f201d19180f960b23acd4ccb20f5dcb8345cba25e5f681873a084bef"
   license all_of: ["MIT", "Apache-2.0"]
 
   bottle do
