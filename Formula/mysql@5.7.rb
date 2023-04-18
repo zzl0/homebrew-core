434740de6dac8f92beeadc9ad801aec47bc8fece
Patrick Linnane <patrick@linnane.io>
2023-04-18 16:22:43 -0700

    mysql@5.7 5.7.42

--- Formula/mysql@5.7.rb
+++ Formula/mysql@5.7.rb
@@ -1,8 +1,8 @@
 class MysqlAT57 < Formula
   desc "Open source relational database management system"
   homepage "https://dev.mysql.com/doc/refman/5.7/en/"
-  url "https://cdn.mysql.com/Downloads/MySQL-5.7/mysql-boost-5.7.41.tar.gz"
-  sha256 "d5735e172fbd235d22d2c7eec084c51e7a1648d9e28c78b54e0c8b8d46751cb9"
+  url "https://cdn.mysql.com/Downloads/MySQL-5.7/mysql-boost-5.7.42.tar.gz"
+  sha256 "7e1a7d45e7ca382eb3a992f63631c380904dd49c89f3382ec950aef01997524f"
   license "GPL-2.0-only"
 
   livecheck do
