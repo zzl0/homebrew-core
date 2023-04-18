cbd11c00e91204013bf55ccb7ea171f7dfcde9d6
Patrick Linnane <patrick@linnane.io>
2023-04-18 16:21:30 -0700

    openjdk@11 11.0.19

--- Formula/openjdk@11.rb
+++ Formula/openjdk@11.rb
@@ -1,8 +1,8 @@
 class OpenjdkAT11 < Formula
   desc "Development kit for the Java programming language"
   homepage "https://openjdk.java.net/"
-  url "https://github.com/openjdk/jdk11u/archive/refs/tags/jdk-11.0.18-ga.tar.gz"
-  sha256 "c0560c3480e7ded2a59d783ddf2cb624a44ece9d3036f4a7a7575d597b18fb2e"
+  url "https://github.com/openjdk/jdk11u/archive/refs/tags/jdk-11.0.19-ga.tar.gz"
+  sha256 "5077e9713e59ccf92fbe58b9bb63e18294fb2110df644f476638e1528e5dfd46"
   license "GPL-2.0-only"
 
   livecheck do
