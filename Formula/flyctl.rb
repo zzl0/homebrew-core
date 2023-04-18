5fbb880ef2ec21df7738f8665b647b929ee2b42e
Patrick Linnane <patrick@linnane.io>
2023-04-18 16:13:00 -0700

    flyctl 0.0.526

--- Formula/flyctl.rb
+++ Formula/flyctl.rb
@@ -2,8 +2,8 @@ class Flyctl < Formula
   desc "Command-line tools for fly.io services"
   homepage "https://fly.io"
   url "https://github.com/superfly/flyctl.git",
-      tag:      "v0.0.525",
-      revision: "01be82d654103586d6363b276d268cfca5946e60"
+      tag:      "v0.0.526",
+      revision: "7444c8822a7c3c2db02f2bd615a2f7cadcec8c99"
   license "Apache-2.0"
   head "https://github.com/superfly/flyctl.git", branch: "master"
 
