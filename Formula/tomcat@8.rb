8e5424bb5ac648ae60ca87b901b2673cd5c4b7fb
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-04-20 05:10:03 +0800

    tomcat@8 8.5.88

--- Formula/tomcat@8.rb
+++ Formula/tomcat@8.rb
@@ -1,9 +1,9 @@
 class TomcatAT8 < Formula
   desc "Implementation of Java Servlet and JavaServer Pages"
   homepage "https://tomcat.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=tomcat/tomcat-8/v8.5.87/bin/apache-tomcat-8.5.87.tar.gz"
-  mirror "https://archive.apache.org/dist/tomcat/tomcat-8/v8.5.87/bin/apache-tomcat-8.5.87.tar.gz"
-  sha256 "86acc508b29ca1928a6597379f1c3dc79c767922e863c9145e2477768b4f2a75"
+  url "https://www.apache.org/dyn/closer.lua?path=tomcat/tomcat-8/v8.5.88/bin/apache-tomcat-8.5.88.tar.gz"
+  mirror "https://archive.apache.org/dist/tomcat/tomcat-8/v8.5.88/bin/apache-tomcat-8.5.88.tar.gz"
+  sha256 "fd030eb0120aad3493f7354a57b2ee11a4d77d07ec48de2b8ab090046f0ed2a2"
   license "Apache-2.0"
 
   livecheck do
