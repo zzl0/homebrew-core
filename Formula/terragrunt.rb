c151ff5ed5db1956a65b1db69eec521cbefcc49b
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-04-20 05:09:15 +0800

    terragrunt 0.45.4

--- Formula/terragrunt.rb
+++ Formula/terragrunt.rb
@@ -1,8 +1,8 @@
 class Terragrunt < Formula
   desc "Thin wrapper for Terraform e.g. for locking state"
   homepage "https://terragrunt.gruntwork.io/"
-  url "https://github.com/gruntwork-io/terragrunt/archive/v0.45.3.tar.gz"
-  sha256 "872202cd9a50f974d620fb18e89349dde9b0fdc74c6443e56af8df347f0c1a35"
+  url "https://github.com/gruntwork-io/terragrunt/archive/v0.45.4.tar.gz"
+  sha256 "0a29dc0a60701fecf18b067e466787f93ee4e0160d3421377446d996cd235d68"
   license "MIT"
 
   bottle do
