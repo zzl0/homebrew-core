3e1a8f6c9e269c231ec47ef9cc72edb43211aef2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-19 02:19:22 +0000

    rsyslog: update 8.2304.0 bottle.

--- Formula/rsyslog.rb
+++ Formula/rsyslog.rb
@@ -11,13 +11,13 @@ class Rsyslog < Formula
   end
 
   bottle do
-    sha256 arm64_ventura:  "b1a7ed6814f95a6fbe4858b973af2f6719a7bfc1983d263c47a63fb0c1bbd417"
-    sha256 arm64_monterey: "e52eff29c253fbb61862cb1b85e45f180d0342fb3c91cc94a3e49d76b1167107"
-    sha256 arm64_big_sur:  "4d1bb386a8757ba93bc75f1d75871151553348d42d891333b5644209f93f3673"
-    sha256 ventura:        "89f747cf19212c53d8db3361903930e70f842d1dbf5738477f45c052912b815e"
-    sha256 monterey:       "a1a4cb997fb8003c0450d61699e8aeb671ea1a66ee525b52fa2d14e90953f66a"
-    sha256 big_sur:        "e55ffb3260240b859cf35089fec89cea135e917f072aba3ea724395da3ec4c24"
-    sha256 x86_64_linux:   "c80403c933caa3f9790c7dc47d449f5cb4e11c8f6f9668ae1664ebef4f40cb50"
+    sha256 arm64_ventura:  "0a4fcad36d21b12b9cbfa594a847a0e06edf54472edaffd23f6ac3f433953ccd"
+    sha256 arm64_monterey: "80c02825e6a0af2cbd25c43c69b56fad0376e0a847429878e0044ecef8c2a51c"
+    sha256 arm64_big_sur:  "96f9efab15f6f918fae94b6bbb9d606054aec4daf05c4de1f021fec011a58cc5"
+    sha256 ventura:        "950cbc5384ed9cb58ff35ca03212b91b59383138549af632c0e20f11b101049e"
+    sha256 monterey:       "37570aee68bdc7b2cb7686550ee2d57eccbb79bdb2bd17de30168b0aaaa673ff"
+    sha256 big_sur:        "42ea1e3c316466a6520bfcc246be05b5f4daa92a53e5c68d76b390bbd26b42f3"
+    sha256 x86_64_linux:   "3aff4c470389cdf5b5669780422a6c00d28eefeb7cd929a694c3b80c671c0ce5"
   end
 
   depends_on "pkg-config" => :build
