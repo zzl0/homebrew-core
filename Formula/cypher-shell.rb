806b2216a39b9a75f7893306bb65a3a2a0a98387
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-04-20 23:48:12 +0800

    cypher-shell 5.7.0

--- Formula/cypher-shell.rb
+++ Formula/cypher-shell.rb
@@ -1,8 +1,8 @@
 class CypherShell < Formula
   desc "Command-line shell where you can execute Cypher against Neo4j"
   homepage "https://neo4j.com"
-  url "https://dist.neo4j.org/cypher-shell/cypher-shell-5.6.0.zip"
-  sha256 "b7ec3334b098d88dafa7a91ae8d731d377515809b169323a675406e5c89d5735"
+  url "https://dist.neo4j.org/cypher-shell/cypher-shell-5.7.0.zip"
+  sha256 "0771508483f27b65d311237158e4d97458df2fe94e1e55e2036f004cdfc9dcc5"
   license "GPL-3.0-only"
   version_scheme 1
 
