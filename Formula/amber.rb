d664a4b429d33e713e92c8250de9cf3f93008ba8
crimson-knight <crimsonknightstudios@gmail.com>
2023-04-20 10:18:59 -0400

    amber 1.3.2 (new formula)

---
+++ Formula/amber.rb
@@ -0,0 +1,37 @@
+class Amber < Formula
+  desc "Crystal web framework. Bare metal performance, productivity and happiness"
+  homepage "https://amberframework.org/"
+  url "https://github.com/amberframework/amber/archive/refs/tags/v1.3.2.tar.gz"
+  sha256 "37511d6b4afe308e1943cedeab9114b01d5787d868c23d2c0cc555917a21c830"
+  license "MIT"
+
+  depends_on "crystal"
+  uses_from_macos "sqlite"
+
+  def install
+    system "shards", "install"
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    output = shell_output("#{bin}/amber new test_app")
+    %w[
+      config/environments
+      amber.yml
+      shard.yml
+      public
+      src/controllers
+      src/views
+      src/assets
+      src/test_app.cr
+    ].each do |path|
+      assert_match path, output
+    end
+
+    cd "test_app" do
+      build_app = shell_output("shards build test_app")
+      assert_match "Building", build_app
+      assert_predicate testpath/"test_app/bin/test_app", :exist?
+    end
+  end
+end
