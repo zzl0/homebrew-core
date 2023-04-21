a9f59ce76fb2c2c7b56d662d289082ccae243927
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-21 04:50:30 +0000

    amber: add 1.3.2 bottle.

--- Formula/amber.rb
+++ Formula/amber.rb
@@ -5,6 +5,16 @@ class Amber < Formula
   sha256 "37511d6b4afe308e1943cedeab9114b01d5787d868c23d2c0cc555917a21c830"
   license "MIT"
 
+  bottle do
+    sha256 arm64_ventura:  "09874b1c0219b906acb44c2e5e7cc289f7d302fae6c1b6095683535267a07dd0"
+    sha256 arm64_monterey: "b3861f5e4f7e94f731cb0de75d03d0e0751db0bff733b6db9e19dcb062f78053"
+    sha256 arm64_big_sur:  "3aae57960a61b66b2c6e3383af499e72b24423174ce6b33278785423dbe10f36"
+    sha256 ventura:        "78e8fbf9f87122ccb0bfa93abdf3a1d6c54bc1486a0774a5eb2ca68088479b36"
+    sha256 monterey:       "f162565aaf09c4cec2a72da13d37237d0e2a854eb8f463e4a96de0251f192dbd"
+    sha256 big_sur:        "bcefbabba33b53d726ac043cf74798c1dddbc0d5df8bbdf05acec1632853abd7"
+    sha256 x86_64_linux:   "0c15fd19b2005974609aeef0cfd6fffbb3ed1337bc4af58b3f4e59cca7bb4710"
+  end
+
   depends_on "crystal"
   uses_from_macos "sqlite"
 
