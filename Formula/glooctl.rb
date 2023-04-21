2894b9a1e57c792ab3c4cea44fe599205bb8ccfb
Patrick Linnane <patrick@linnane.io>
2023-04-21 14:09:34 -0700

    glooctl 1.13.14

--- Formula/glooctl.rb
+++ Formula/glooctl.rb
@@ -4,8 +4,8 @@ class Glooctl < Formula
   # NOTE: Please wait until the newest stable release is finished building and
   # no longer marked as "Pre-release" before creating a PR for a new version.
   url "https://github.com/solo-io/gloo.git",
-      tag:      "v1.13.12",
-      revision: "d6e0c1c73236ac33656824accbf22bece8d78c0c"
+      tag:      "v1.13.14",
+      revision: "a40c7d7db03bba3d4fec73eddf2de106195d675c"
   license "Apache-2.0"
   head "https://github.com/solo-io/gloo.git", branch: "master"
 
