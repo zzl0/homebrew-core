08a5c62d12c1d2659ce881ae6f07d35b7b9a976c
David Baumgold <david@davidbaumgold.com>
2023-04-22 08:34:10 +0200

    chatblade: depend on pygments and pyyaml

--- Formula/chatblade.rb
+++ Formula/chatblade.rb
@@ -18,7 +18,9 @@ class Chatblade < Formula
   end
 
   depends_on "rust" => :build
+  depends_on "pygments"
   depends_on "python@3.11"
+  depends_on "pyyaml"
 
   resource "aiohttp" do
     url "https://files.pythonhosted.org/packages/c2/fd/1ff4da09ca29d8933fda3f3514980357e25419ce5e0f689041edb8f17dab/aiohttp-3.8.4.tar.gz"
@@ -83,16 +85,6 @@ class Chatblade < Formula
   resource "platformdirs" do
     url "https://files.pythonhosted.org/packages/79/c4/f98a05535344f79699bbd494e56ac9efc986b7a253fe9f4dba7414a7f505/platformdirs-3.1.1.tar.gz"
     sha256 "024996549ee88ec1a9aa99ff7f8fc819bb59e2c3477b410d90a16d32d6e707aa"
-  end
-
-  resource "Pygments" do
-    url "https://files.pythonhosted.org/packages/da/6a/c427c06913204e24de28de5300d3f0e809933f376e0b7df95194b2bb3f71/Pygments-2.14.0.tar.gz"
-    sha256 "b3ed06a9e8ac9a9aae5a6f5dbe78a8a58655d17b43b93c078f094ddc476ae297"
-  end
-
-  resource "PyYAML" do
-    url "https://files.pythonhosted.org/packages/36/2b/61d51a2c4f25ef062ae3f74576b01638bebad5e045f747ff12643df63844/PyYAML-6.0.tar.gz"
-    sha256 "68fb519c14306fec9720a2a5b45bc9f0c8d1b9c72adf45c37baedfcd949c35a2"
   end
 
   resource "regex" do
