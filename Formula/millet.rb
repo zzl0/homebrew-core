8c5bc55d720b29055d88a8afaaba50cbb963da6b
Arturo Vergara <hello@dead.computer>
2023-04-22 17:38:54 -0600

    millet 0.9.4 (new formula)
    millet is an LSP server for Standard ML, written in Rust
    
    Add dual license
    
    Make newlines consistent
    
    Small refactor
    Link to tarball instead of to git tag
    
    Add checksum
    
    Remove json import

---
+++ Formula/millet.rb
@@ -0,0 +1,89 @@
+class Millet < Formula
+  desc "Language server for Standard ML (SML)"
+  homepage "https://github.com/azdavis/millet"
+  url "https://github.com/azdavis/millet/archive/refs/tags/v0.9.4.tar.gz"
+  sha256 "b957b7dfc12d261376feca73221f75c12af2bfbd3ac82c1b3e9d0f49fe750728"
+  license any_of: ["Apache-2.0", "MIT"]
+  head "https://github.com/azdavis/millet.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args(path: "crates/millet-ls")
+  end
+
+  test do
+    initialize_request = { jsonrpc: "2.0",
+                           id:      1,
+                           method:  "initialize",
+                           params:  { processId:    nil,
+                                      rootUri:      nil,
+                                      capabilities: {} } }
+
+    initialized_notification = { jsonrpc: "2.0",
+                                 method:  "initialized",
+                                 params:  {} }
+
+    shutdown_request = { jsonrpc: "2.0",
+                         id:      2,
+                         method:  "shutdown",
+                         params:  {} }
+
+    exit_notification = { jsonrpc: "2.0",
+                          method:  "exit",
+                          params:  {} }
+
+    parse_content_length = lambda { |header_part|
+      content_length_header = header_part.split("\r\n")[0]
+      content_length = content_length_header.split(":")[1].to_i
+      return content_length
+    }
+
+    read_header_part = lambda { |pipe|
+      return pipe.readline("\r\n\r\n")
+    }
+
+    read_response = lambda { |pipe|
+      header_part = read_header_part.call(pipe)
+      content_length = parse_content_length.call(header_part)
+      return JSON.parse(pipe.readpartial(content_length))
+    }
+
+    json_rpc_message = lambda { |msg|
+      msg_string = msg.to_json
+      return "Content-Length: #{msg_string.length}\r\n\r\n" + msg_string
+    }
+
+    send_message = lambda { |pipe, msg|
+      pipe.write(json_rpc_message.call(msg))
+    }
+
+    IO.popen("#{bin}/millet-ls", "r+") do |pipe|
+      pipe.sync = true
+
+      # send initialization request
+      send_message.call(pipe, initialize_request)
+
+      # read initialization response
+      response = read_response.call(pipe)
+      assert_equal 1, response["id"]
+
+      # send initialized notification
+      send_message.call(pipe, initialized_notification)
+
+      # send shutdown request
+      send_message.call(pipe, shutdown_request)
+
+      # read shutdown response
+      response = read_response.call(pipe)
+      assert_equal 2, response["id"]
+
+      # send exit notification, which kills the child process
+      send_message.call(pipe, exit_notification)
+
+      pipe.close
+    end
+
+    assert_equal 0, $CHILD_STATUS.exitstatus
+  end
+end
