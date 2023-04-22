f7c093d8d2144425fd0a9a5cf833d73ae5746b2a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-22 18:46:55 +0000

    nbdime: update 3.2.0 bottle.

--- Formula/nbdime.rb
+++ Formula/nbdime.rb
@@ -8,13 +8,13 @@ class Nbdime < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a7c5b33b28d93fad13dac3b0928b65e223161cde5f3626d69a6a72047630c160"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "95e20bfc6f088d254c3d358ca2d2095e3942d9b8b23fc1c68e51b85803fc7230"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "862654b5b3d60d3bc3be2be36bad82afa94bc87cd07d568835f5a7719810c5db"
-    sha256 cellar: :any_skip_relocation, ventura:        "065fa10531e6e935291da89abf0f21aeac4c59e423e6ee83c7632f91a235abea"
-    sha256 cellar: :any_skip_relocation, monterey:       "5b02f776a442b4a38eaaaa263639028d2f3917639e7722174bec7de64aa884b2"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f8363ae41ac9a8be9677536cc527a80e17f251d441be7a205f2b983d06173e19"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fc10e7bdd99bf91e6f09588f7a3045f43e5c55be667ac217a923893ec247675d"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "cca12c01508b85397210edf46ae1d2e650738981352f866197722e6bc485b9d3"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ef4f3c5270f894e30268dac8561067c1504c915993f6c39ec432b66f1e3ca453"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "99462e3922cb7fea876de0e9c06f913c24bdd9574c61fa509f7bcb3ac26ce1b8"
+    sha256 cellar: :any_skip_relocation, ventura:        "2b8cd1aed0e6a4d9bc5c2d8ebf04e2a85bfebd36cc6f6877c187e2d48697e61e"
+    sha256 cellar: :any_skip_relocation, monterey:       "3f29e570a8b06bd471002a2cd6dce38e06d103001c77094f5aa939df684aa3a9"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e55477537b2e1b10cc3509951c6d764e55c7ae84d93decc364ef291edcf4d5bc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7cddf7f566d8a5fc110e1c43ce993ef5603275c5fb1a1da9600dd016633258a3"
   end
 
   depends_on "ipython"
