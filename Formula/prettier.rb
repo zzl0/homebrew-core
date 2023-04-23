051f27b521d9f6fa126bd32f64b2ac969aa44b8c
MoonFruit <dkmoonfruit@gmail.com>
2023-04-23 15:52:46 +0800

    prettier 2.8.8

--- Formula/prettier.rb
+++ Formula/prettier.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Prettier < Formula
   desc "Code formatter for JavaScript, CSS, JSON, GraphQL, Markdown, YAML"
   homepage "https://prettier.io/"
-  url "https://registry.npmjs.org/prettier/-/prettier-2.8.7.tgz"
-  sha256 "e0a76a4dc74608eb28f7992be02ccb22422fc895900c7bf90e6b59c53d41adba"
+  url "https://registry.npmjs.org/prettier/-/prettier-2.8.8.tgz"
+  sha256 "61ff77c8b71fc0b9eb0ab2523d761735c66609f8de40cf60d67b98edda40a28d"
   license "MIT"
   head "https://github.com/prettier/prettier.git", branch: "main"
 
