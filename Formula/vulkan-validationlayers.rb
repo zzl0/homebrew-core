e2af36fde3039ce7e54733be4617c7114dde586e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-24 14:23:12 +0000

    vulkan-validationlayers: add 1.3.247 bottle.

--- Formula/vulkan-validationlayers.rb
+++ Formula/vulkan-validationlayers.rb
@@ -6,6 +6,16 @@ class VulkanValidationlayers < Formula
   license "Apache-2.0"
   head "https://github.com/KhronosGroup/Vulkan-ValidationLayers.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "485d831b0e8e5023469d700f3814f3616d818e2421698f0ae4d2d700ad39a7f6"
+    sha256 cellar: :any,                 arm64_monterey: "a9be726a917a5d858f500d39e375f94e10c0a099cf2b07241607efa9bf85a0f3"
+    sha256 cellar: :any,                 arm64_big_sur:  "5b0b441797173f64321b1b411e54a2be959cbd0850111a021c81bd9a870f2602"
+    sha256 cellar: :any,                 ventura:        "e7ec1b0ab19458b1f9c93d7e3dc1a14396f0a69ed64601a3ab2737120b7e7c71"
+    sha256 cellar: :any,                 monterey:       "c5a3251f32e8e117325c4cbf473fe4b4ca4a9d83595a51d14ea4286d0f0005f2"
+    sha256 cellar: :any,                 big_sur:        "39a94e7ad88c9019a272b4e726dc825d1f270d5168946c47b9698d7b8291a700"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "99940a98424fd2bbb6ac2b50cca23054264b023d094be90d0a04a2bb042f5422"
+  end
+
   depends_on "cmake" => :build
   depends_on "googletest" => :build
   depends_on "python@3.11" => :build
