38c991c80eb27ebbbee30319c6b10aa28bb6cb1b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-24 17:11:16 +0000

    git-big-picture: add 1.2.2 bottle.

--- Formula/git-big-picture.rb
+++ Formula/git-big-picture.rb
@@ -7,6 +7,16 @@ class GitBigPicture < Formula
   sha256 "7b2826d72e146c7a53e7a1cc9533c360cd8e0feb870c7d1eadcc189b8bc2c5f6"
   license "GPL-3.0-or-later"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "7646579430d2ab35d1aabe5661be2eacde3da778db1259e962c4c0df78720bf1"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "7646579430d2ab35d1aabe5661be2eacde3da778db1259e962c4c0df78720bf1"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7646579430d2ab35d1aabe5661be2eacde3da778db1259e962c4c0df78720bf1"
+    sha256 cellar: :any_skip_relocation, ventura:        "705f6bfdb8623f2a158fc034a7b51097f4cf541da9db461f9000c33ccdf3d193"
+    sha256 cellar: :any_skip_relocation, monterey:       "705f6bfdb8623f2a158fc034a7b51097f4cf541da9db461f9000c33ccdf3d193"
+    sha256 cellar: :any_skip_relocation, big_sur:        "705f6bfdb8623f2a158fc034a7b51097f4cf541da9db461f9000c33ccdf3d193"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9725b03021184088a0eacfc3beb7d62db8e7a6429a00eb8815d6af2939f96967"
+  end
+
   depends_on "graphviz"
   depends_on "python@3.11"
 
