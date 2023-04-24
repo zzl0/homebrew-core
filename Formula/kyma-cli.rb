b8a2ed5c0d1ce69b72188fecc3149b391ec2cbe2
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-04-24 18:58:07 +0800

    kyma-cli 2.13.1

--- Formula/kyma-cli.rb
+++ Formula/kyma-cli.rb
@@ -1,8 +1,8 @@
 class KymaCli < Formula
   desc "Kyma command-line interface"
   homepage "https://kyma-project.io"
-  url "https://github.com/kyma-project/cli/archive/2.13.0.tar.gz"
-  sha256 "686cb0d0594d85b290e111f44ccd78bfc74576e03f249527240f609fc12edb19"
+  url "https://github.com/kyma-project/cli/archive/2.13.1.tar.gz"
+  sha256 "70654f250e57996339f8b3627b4cd27529a1554131c19db8a90fead5dc2e870b"
   license "Apache-2.0"
   head "https://github.com/kyma-project/cli.git", branch: "main"
 
