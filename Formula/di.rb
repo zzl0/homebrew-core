7b2a991cb8526525ed5f7c970faefc94ba43c9b0
MoonFruit <dkmoonfruit@gmail.com>
2023-04-25 10:00:51 +0800

    di 4.52

--- Formula/di.rb
+++ Formula/di.rb
@@ -1,8 +1,8 @@
 class Di < Formula
   desc "Advanced df-like disk information utility"
   homepage "https://gentoo.com/di/"
-  url "https://downloads.sourceforge.net/project/diskinfo-di/di-4.51.tar.gz"
-  sha256 "79b2129c4aff27428695441175940a1717fa0fe2ec2f46d1b886ebb4921461bb"
+  url "https://downloads.sourceforge.net/project/diskinfo-di/di-4.52.tar.gz"
+  sha256 "b775ea8acb089c3440a8621da684fa8f98f6551c06e705593b79d4fe82b57a1f"
   license "Zlib"
 
   livecheck do
