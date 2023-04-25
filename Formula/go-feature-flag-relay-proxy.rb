242d54118addcfc60dc5638f10a39af14704da42
Thomas Poignant <thomas.poignant@gmail.com>
2023-04-25 11:09:42 +0200

    go-feature-flag-relay-proxy 1.9.0
    Version `1.9.0` of the go-feature-flag-relay-proxy is available.
    This PR bump the version in homebrew.
    
    Check https://gofeatureflag.org if you want to have more information about GO Feature Flag.
    
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/go-feature-flag-relay-proxy.rb
+++ Formula/go-feature-flag-relay-proxy.rb
@@ -2,8 +2,8 @@ class GoFeatureFlagRelayProxy < Formula
   desc "Stand alone server to run GO Feature Flag"
   homepage "https://gofeatureflag.org"
   url "https://github.com/thomaspoignant/go-feature-flag.git",
-      tag:      "v1.8.2",
-      revision: "d0e1789908aa1c5be9cb122dcd73642a69773223"
+      tag:      "v1.9.0",
+      revision: "fe37218eb1e928f7ae9ff8b3e6ff8deb2e4520b5"
   license "MIT"
   head "https://github.com/thomaspoignant/go-feature-flag.git", branch: "main"
 
