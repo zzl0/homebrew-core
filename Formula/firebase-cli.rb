112bd15212701d92ea77752382bb2a3f73e7315f
Patrick Linnane <patrick@linnane.io>
2023-04-26 16:08:24 -0700

    firebase-cli 11.29.1

--- Formula/firebase-cli.rb
+++ Formula/firebase-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class FirebaseCli < Formula
   desc "Firebase command-line tools"
   homepage "https://firebase.google.com/docs/cli/"
-  url "https://registry.npmjs.org/firebase-tools/-/firebase-tools-11.29.0.tgz"
-  sha256 "616d7bda0ac9a2daad6e101e8ab00466168f2fcbe2dac381ed2f1c8aed15eda5"
+  url "https://registry.npmjs.org/firebase-tools/-/firebase-tools-11.29.1.tgz"
+  sha256 "cf7530bc994a6a44a8cac78c6ec3f8ee55032492c3d6edd01385e0139b1a08cd"
   license "MIT"
   head "https://github.com/firebase/firebase-tools.git", branch: "master"
 
