8cfd2732064db9ef715788f60784cfde7c412332
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-26 17:53:08 +0000

    checkstyle: update 10.10.0 bottle.

--- Formula/checkstyle.rb
+++ Formula/checkstyle.rb
@@ -6,13 +6,13 @@ class Checkstyle < Formula
   license "LGPL-2.1-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "94e39432c38b84ea93b40fafc07c809e1e6ba5ea2d8c1086115f2cd9d9fde517"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "94e39432c38b84ea93b40fafc07c809e1e6ba5ea2d8c1086115f2cd9d9fde517"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "94e39432c38b84ea93b40fafc07c809e1e6ba5ea2d8c1086115f2cd9d9fde517"
-    sha256 cellar: :any_skip_relocation, ventura:        "94e39432c38b84ea93b40fafc07c809e1e6ba5ea2d8c1086115f2cd9d9fde517"
-    sha256 cellar: :any_skip_relocation, monterey:       "94e39432c38b84ea93b40fafc07c809e1e6ba5ea2d8c1086115f2cd9d9fde517"
-    sha256 cellar: :any_skip_relocation, big_sur:        "94e39432c38b84ea93b40fafc07c809e1e6ba5ea2d8c1086115f2cd9d9fde517"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d559df221f54156c9463f5c4c4b2e0917fd360c68764d53b3d2819e96d555921"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "fcd540c8493a5720b5e0cd74a87808a08e46e0a2d3576878f5b0af67d1ba1cc1"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "fcd540c8493a5720b5e0cd74a87808a08e46e0a2d3576878f5b0af67d1ba1cc1"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fcd540c8493a5720b5e0cd74a87808a08e46e0a2d3576878f5b0af67d1ba1cc1"
+    sha256 cellar: :any_skip_relocation, ventura:        "fcd540c8493a5720b5e0cd74a87808a08e46e0a2d3576878f5b0af67d1ba1cc1"
+    sha256 cellar: :any_skip_relocation, monterey:       "fcd540c8493a5720b5e0cd74a87808a08e46e0a2d3576878f5b0af67d1ba1cc1"
+    sha256 cellar: :any_skip_relocation, big_sur:        "fcd540c8493a5720b5e0cd74a87808a08e46e0a2d3576878f5b0af67d1ba1cc1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2c4d65e22f51efd05e0ed88e27fc32fae7f74fd42476872b137fb959e67b8051"
   end
 
   depends_on "openjdk"
