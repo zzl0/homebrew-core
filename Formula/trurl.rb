4233b45d03d54b1e0302b7bce81e03058c3cd008
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2023-04-26 08:43:13 +0000

    trurl 0.6

--- Formula/trurl.rb
+++ Formula/trurl.rb
@@ -1,8 +1,8 @@
 class Trurl < Formula
   desc "Command-line tool for URL parsing and manipulation"
   homepage "https://curl.se/trurl/"
-  url "https://github.com/curl/trurl/archive/refs/tags/trurl-0.5.tar.gz"
-  sha256 "b5c5600cd3533e208b720f13aa06de724270d1750406b41a22f48ce95c51844d"
+  url "https://github.com/curl/trurl/archive/refs/tags/trurl-0.6.tar.gz"
+  sha256 "4564dff7441d33a29aa02fe64bea7ef0809d9fabc1609ac5b50ca5503e81caa6"
   license "curl"
   head "https://github.com/curl/trurl.git", branch: "master"
 
