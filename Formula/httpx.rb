c98e36c4b4c86d482fe612a5e8425971a384c90c
Porkepix <porkepix@gmail.com>
2023-04-28 01:07:55 +0200

    httpx 1.3.0
    httpx: use shoenig/go-m1cpu@0.1.5
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/httpx.rb
+++ Formula/httpx.rb
@@ -1,8 +1,8 @@
 class Httpx < Formula
   desc "Fast and multi-purpose HTTP toolkit"
   homepage "https://github.com/projectdiscovery/httpx"
-  url "https://github.com/projectdiscovery/httpx/archive/v1.2.9.tar.gz"
-  sha256 "f61217ad67a6b8ffb4c841ee64501df4e0311df50cd89cb716539fe8a47d41c0"
+  url "https://github.com/projectdiscovery/httpx/archive/v1.3.0.tar.gz"
+  sha256 "7f8f80a0b9fc03f8481c56365d043b927a85c87543fcf83aa212443c2fc3ca4a"
   license "MIT"
   head "https://github.com/projectdiscovery/httpx.git", branch: "master"
 
@@ -18,6 +18,11 @@ class Httpx < Formula
 
   depends_on "go" => :build
 
+  # Fix kIOMasterPortDefault symbol rename.
+  # Remove when github.com/shoenig/go-m1cpu is bumped to v0.1.5 or newer.
+  # Check: https://github.com/projectdiscovery/httpx/blob/main/go.mod#L110
+  patch :DATA
+
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w"), "./cmd/httpx"
   end
@@ -28,3 +33,33 @@ class Httpx < Formula
     assert_equal "Example Domain", output["title"]
   end
 end
+
+__END__
+diff --git a/go.mod b/go.mod
+index 7f6ccae..4b8efe6 100644
+--- a/go.mod
++++ b/go.mod
+@@ -107,7 +107,7 @@ require (
+ 	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
+ 	github.com/sashabaranov/go-openai v1.8.0 // indirect
+ 	github.com/shirou/gopsutil/v3 v3.23.3 // indirect
+-	github.com/shoenig/go-m1cpu v0.1.4 // indirect
++	github.com/shoenig/go-m1cpu v0.1.5 // indirect
+ 	github.com/syndtr/goleveldb v1.0.0 // indirect
+ 	github.com/tidwall/btree v1.6.0 // indirect
+ 	github.com/tidwall/buntdb v1.2.10 // indirect
+diff --git a/go.sum b/go.sum
+index 097dd4e..797f347 100644
+--- a/go.sum
++++ b/go.sum
+@@ -241,8 +241,9 @@ github.com/sashabaranov/go-openai v1.8.0 h1:IZrNK/gGqxtp0j19F4NLGbmfoOkyDpM3oC9i
+ github.com/sashabaranov/go-openai v1.8.0/go.mod h1:lj5b/K+zjTSFxVLijLSTDZuP7adOgerWeFyZLUhAKRg=
+ github.com/shirou/gopsutil/v3 v3.23.3 h1:Syt5vVZXUDXPEXpIBt5ziWsJ4LdSAAxF4l/xZeQgSEE=
+ github.com/shirou/gopsutil/v3 v3.23.3/go.mod h1:lSBNN6t3+D6W5e5nXTxc8KIMMVxAcS+6IJlffjRRlMU=
+-github.com/shoenig/go-m1cpu v0.1.4 h1:SZPIgRM2sEF9NJy50mRHu9PKGwxyyTTJIWvCtgVbozs=
+ github.com/shoenig/go-m1cpu v0.1.4/go.mod h1:Wwvst4LR89UxjeFtLRMrpgRiyY4xPsejnVZym39dbAQ=
++github.com/shoenig/go-m1cpu v0.1.5 h1:LF57Z/Fpb/WdGLjt2HZilNnmZOxg/q2bSKTQhgbrLrQ=
++github.com/shoenig/go-m1cpu v0.1.5/go.mod h1:Wwvst4LR89UxjeFtLRMrpgRiyY4xPsejnVZym39dbAQ=
+ github.com/shoenig/test v0.6.3 h1:GVXWJFk9PiOjN0KoJ7VrJGH6uLPnqxR7/fe3HUPfE0c=
+ github.com/shoenig/test v0.6.3/go.mod h1:byHiCGXqrVaflBLAMq/srcZIHynQPQgeyvkvXnjqq0k=
+ github.com/sirupsen/logrus v1.3.0/go.mod h1:LxeOpSwHxABJmUn/MG1IvRgCAasNZTLOkJPxbbu5VWo=
