4439abcee38f30125b122800a9555a854156bc82
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-27 10:05:11 +0000

    xcodegen: update 2.35.0 bottle.

--- Formula/xcodegen.rb
+++ Formula/xcodegen.rb
@@ -7,12 +7,12 @@ class Xcodegen < Formula
   head "https://github.com/yonaskolb/XcodeGen.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "30a9c593d0d156e7a0b0733a36bd9ed9d8b122d92f535f5de57e955d72b063f1"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5cfb54c7c4ca5e7e4b754d147f3b4f9b508a896b54de08c06a1ea9100926456d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "aec3bd05fa69ce9b301f2758c2fa42a37e670474d691cf7fe537aaff71c35100"
-    sha256 cellar: :any_skip_relocation, ventura:        "1106db445e164d03888b496b2f3ebacd442df2a1bb7a3ad575958d5a3064eb9d"
-    sha256 cellar: :any_skip_relocation, monterey:       "6dd5d8674e0af610acaf1fab726bb26091adb49969a30ef8fbc5da91fbe46dba"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b4834f7117abca2bcbae00702af72ebd553129ea0c993ce97d44a742420c8ff3"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "ca9aa5cf7c7a4573bd3ff2c1afbe871566e57478c09967abca7585fc1c237470"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "dadb3716295017a392da394eb0bb52360d95e9142e21ac5532e1d29def593ad6"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6026a0e84873586f7e65584bd3e9a9456f4b590c2498ebf986e563d66bccb3a7"
+    sha256 cellar: :any_skip_relocation, ventura:        "bb982470aad36dececfab94449696b1a327ea931b61475832fd41c5fa59ebb60"
+    sha256 cellar: :any_skip_relocation, monterey:       "366403595292790a4a84e2db6f0f32c14fa907c77ac424a5e24d431d372e7a17"
+    sha256 cellar: :any_skip_relocation, big_sur:        "6db46a3673d4cd395cabd22bbe28e701eef7eaa4f198829fd8a4df072eb9a6ff"
   end
 
   depends_on xcode: ["10.2", :build]
