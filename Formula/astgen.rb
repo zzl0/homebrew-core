958c9e2b6cf081a48bd4892e452f2307f19c195e
Porkepix <porkepix@gmail.com>
2023-04-28 00:45:56 +0200

    astgen 2.23.0
    astgen: update test

--- Formula/astgen.rb
+++ Formula/astgen.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Astgen < Formula
   desc "Generate AST in json format for JS/TS"
   homepage "https://github.com/joernio/astgen"
-  url "https://github.com/joernio/astgen/archive/refs/tags/v2.21.0.tar.gz"
-  sha256 "854dc620d807813f884ba4469bee97eb3137dffc682315672b04c8f7ee2219a4"
+  url "https://github.com/joernio/astgen/archive/refs/tags/v2.23.0.tar.gz"
+  sha256 "e8bb0ced3dc1fc3f7790780d03bce238eac9b3ac63d3c84dfa6afe66523309a9"
   license "Apache-2.0"
 
   bottle do
@@ -26,6 +26,6 @@ class Astgen < Formula
 
     assert_match "Converted AST", shell_output("#{bin}/astgen -t js -i . -o #{testpath}/out")
     assert_match '"fullName": "main.js"', (testpath/"out/main.js.json").read
-    assert_match '"0":"Console"', (testpath/"out/main.js.typemap").read
+    assert_match '"0": "Console"', (testpath/"out/main.js.typemap").read
   end
 end
