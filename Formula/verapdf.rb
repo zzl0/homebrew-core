185645fbabcd18eb485dac39f123bea03444ecc0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-27 15:23:33 +0000

    verapdf: update 1.23.187 bottle.

--- Formula/verapdf.rb
+++ Formula/verapdf.rb
@@ -12,13 +12,13 @@ class Verapdf < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "222169fae4f6e09cace71e14cb1e40233750131d5915423028af33f02b7ec9eb"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "caac000f6d1df3f26499daf6255965128737b7ee9a73e93ac2709a1747eb2635"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ed4508e3fcdf01d0180d7e3e78dca025c08b1ff55a74084074b3df80a1b783a4"
-    sha256 cellar: :any_skip_relocation, ventura:        "9192fc8086a94a72bb44b914a20df62d3b91da6e2703faee1e73a0f18495461c"
-    sha256 cellar: :any_skip_relocation, monterey:       "08545e3fa590ebb3e869efb94f0c792d651228139d56d3a9457946a67fb453d5"
-    sha256 cellar: :any_skip_relocation, big_sur:        "ec5aadec81bb938c8b16f09148c35badc1208c0d873bd6d6b54e686c36362483"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "640747a96361be3b704c5c2bf31357cf8466e49ce01cab3afe7cd846341010a3"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "38383c0c62cde8c4b8ee675c1eeba3587ee25f343c85a97c4cd21233f7ee0546"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "19155a76d207184c2194bb3e567675228327c5d9631c1baf4ce1ff7897fb6749"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "21a486d51b33e5cfd8b14c58978c2a758ab258e4ef8b90423b2814a0be5c4742"
+    sha256 cellar: :any_skip_relocation, ventura:        "17d4e279e748e465b318d4e3af94fb3afc764907d0dbf8486beff107cce4fdcd"
+    sha256 cellar: :any_skip_relocation, monterey:       "cdc6abea6c966b1c623759e3f3326c37f8586820e189df937f6fc1851ff9ae29"
+    sha256 cellar: :any_skip_relocation, big_sur:        "87966590dcb92e0af9cf5419f71c327634bfeeae18cf2c9e9bebcce0bc89879f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d5a069222284223ebbfc25c29794f2b39d0f628f8d97f48005e1c1e1401e40f1"
   end
 
   depends_on "maven" => :build
