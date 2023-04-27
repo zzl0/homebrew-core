4dfac71337ee61abcc4f2937a2af333084efd5e3
Christoph Junghans <junghans@lanl.gov>
2023-04-27 09:18:52 -0700

    votca 2022.1 (new formula)

---
+++ Formula/votca.rb
@@ -0,0 +1,46 @@
+class Votca < Formula
+  desc "Versatile Object-oriented Toolkit for Coarse-graining Applications"
+  homepage "https://www.votca.org/"
+  url "https://github.com/votca/votca/archive/refs/tags/v2022.1.tar.gz"
+  sha256 "4710a7552f94789936324d76d2e0830b576de8d3f1c605748e2d20947d018100"
+  license "Apache-2.0"
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "boost"
+  depends_on "eigen"
+  depends_on "fftw"
+  depends_on "gcc" # for OpenMP
+  # add gromacs back once it was built with clang
+  depends_on "hdf5"
+  depends_on "python@3.11"
+
+  uses_from_macos "expat"
+
+  on_macos do
+    depends_on "libomp"
+  end
+
+  def install
+    args = [
+      "-DINSTALL_RC_FILES=OFF",
+      "-DINSTALL_CSGAPPS=ON",
+      "-DBUILD_XTP=OFF",
+      "-DCMAKE_DISABLE_FIND_PACKAGE_GROMACS=ON",
+      "-DENABLE_RPATH_INJECT=ON",
+    ]
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args, *args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    system "#{bin}/csg_property", "--help"
+    (testpath/"table.in").write <<~EOS
+      0 0 i
+      1 1 i
+    EOS
+    system "#{bin}/csg_resample", "--in", "table.in", "--out", "table.out", "--grid", "0:0.1:1", "--type", "linear"
+    assert_path_exists "#{testpath}/table.out"
+  end
+end
