6fe1e08d37fa65cb210d8218abb3ae01fd7fc1e1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-27 13:25:18 +0000

    contentful-cli: update 2.5.0 bottle.

--- Formula/contentful-cli.rb
+++ Formula/contentful-cli.rb
@@ -10,13 +10,13 @@ class ContentfulCli < Formula
   head "https://github.com/contentful/contentful-cli.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "e3ef639ad62200047a98a16515d84e6b3607158753582cd2a4e2246324931644"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e3ef639ad62200047a98a16515d84e6b3607158753582cd2a4e2246324931644"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e3ef639ad62200047a98a16515d84e6b3607158753582cd2a4e2246324931644"
-    sha256 cellar: :any_skip_relocation, ventura:        "4d0bf231565875daadd3383d1ecf73d2a1207f7af08a82413b66c7eb27c49e59"
-    sha256 cellar: :any_skip_relocation, monterey:       "4d0bf231565875daadd3383d1ecf73d2a1207f7af08a82413b66c7eb27c49e59"
-    sha256 cellar: :any_skip_relocation, big_sur:        "4d0bf231565875daadd3383d1ecf73d2a1207f7af08a82413b66c7eb27c49e59"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e3ef639ad62200047a98a16515d84e6b3607158753582cd2a4e2246324931644"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "5e13ace5673aa6f14da0f88ea0b50f492e07b60c61fbaea9d2eb93923bb9a879"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5e13ace5673aa6f14da0f88ea0b50f492e07b60c61fbaea9d2eb93923bb9a879"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5e13ace5673aa6f14da0f88ea0b50f492e07b60c61fbaea9d2eb93923bb9a879"
+    sha256 cellar: :any_skip_relocation, ventura:        "b8c746751d01e20e7007e958b32b6c91130e227a77a4e9b250a361a5e0a372e6"
+    sha256 cellar: :any_skip_relocation, monterey:       "b8c746751d01e20e7007e958b32b6c91130e227a77a4e9b250a361a5e0a372e6"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b8c746751d01e20e7007e958b32b6c91130e227a77a4e9b250a361a5e0a372e6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5e13ace5673aa6f14da0f88ea0b50f492e07b60c61fbaea9d2eb93923bb9a879"
   end
 
   depends_on "node"
