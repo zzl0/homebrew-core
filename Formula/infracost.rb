621bc147826d1bd4008da404fddb9688698389e9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-27 11:22:08 +0000

    infracost: update 0.10.21 bottle.

--- Formula/infracost.rb
+++ Formula/infracost.rb
@@ -7,13 +7,13 @@ class Infracost < Formula
   head "https://github.com/infracost/infracost.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "e5d8fe255b9228d03f8487881853d621e3ca7b4f702ac71b53bef2274c40336b"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e5d8fe255b9228d03f8487881853d621e3ca7b4f702ac71b53bef2274c40336b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e5d8fe255b9228d03f8487881853d621e3ca7b4f702ac71b53bef2274c40336b"
-    sha256 cellar: :any_skip_relocation, ventura:        "f9f6a5c4bc3586c2361b91f5e857ec677ea98de847d9311bc2e32656d53b294a"
-    sha256 cellar: :any_skip_relocation, monterey:       "f9f6a5c4bc3586c2361b91f5e857ec677ea98de847d9311bc2e32656d53b294a"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f9f6a5c4bc3586c2361b91f5e857ec677ea98de847d9311bc2e32656d53b294a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "96a877f49a02a4371e70b94d25bc8c43c93bd16d5ed2daf422c302f232570468"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "63c34084a9066f8ed261d984cd845205a76ce09464fa674fbf7b00e9bb163ba3"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "63c34084a9066f8ed261d984cd845205a76ce09464fa674fbf7b00e9bb163ba3"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "63c34084a9066f8ed261d984cd845205a76ce09464fa674fbf7b00e9bb163ba3"
+    sha256 cellar: :any_skip_relocation, ventura:        "fe72e93f6afa000f23571acff5030497118a6e5ffcc41850c5768e4e54f2b5be"
+    sha256 cellar: :any_skip_relocation, monterey:       "fe72e93f6afa000f23571acff5030497118a6e5ffcc41850c5768e4e54f2b5be"
+    sha256 cellar: :any_skip_relocation, big_sur:        "fe72e93f6afa000f23571acff5030497118a6e5ffcc41850c5768e4e54f2b5be"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a57c97d43764480256500571738645d60d7784814e2c33f50f5497aba7ad8878"
   end
 
   depends_on "go" => :build
