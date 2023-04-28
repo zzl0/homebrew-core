0b31046db0dcbec915dd33cdce1e0710e0873476
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-28 16:05:16 +0000

    prestd 1.2.6

--- Formula/prestd.rb
+++ Formula/prestd.rb
@@ -1,8 +1,8 @@
 class Prestd < Formula
   desc "Simplify and accelerate development on any Postgres application, existing or new"
   homepage "https://github.com/prest/prest"
-  url "https://github.com/prest/prest/archive/v1.2.5.tar.gz"
-  sha256 "9899e7cc3188a42cdd8702a2547dc37dc6a37659a17d169d2b567b4c5eaa68ef"
+  url "https://github.com/prest/prest/archive/v1.2.6.tar.gz"
+  sha256 "0babff0b7017c07df7d88eae15679c9349796de03fd2c980aa0627fcbc0c7ccd"
   license "MIT"
   head "https://github.com/prest/prest.git", branch: "main"
 
