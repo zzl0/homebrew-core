2c6dfd333a0d404bcce29be6f8f51fd26b698906
Francois-Xavier Coudert <fxcoudert@gmail.com>
2023-04-29 20:30:37 +0200

    rsync: update patch

--- Formula/rsync.rb
+++ Formula/rsync.rb
@@ -6,6 +6,7 @@ class Rsync < Formula
   mirror "https://www.mirrorservice.org/sites/rsync.samba.org/rsync-3.2.7.tar.gz"
   sha256 "4e7d9d3f6ed10878c58c5fb724a67dacf4b6aac7340b13e488fb2dc41346f2bb"
   license "GPL-3.0-or-later"
+  revision 1
 
   livecheck do
     url "https://rsync.samba.org/ftp/rsync/?C=M&O=D"
@@ -23,6 +24,8 @@ class Rsync < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "a91692c5fc5b38bc0d8f86fde53870a1fce5bf7ce4353a4d8ae09c556b65baa8"
   end
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
   depends_on "lz4"
   depends_on "openssl@3"
   depends_on "popt"
@@ -32,29 +35,23 @@ class Rsync < Formula
   uses_from_macos "zlib"
 
   # hfs-compression.diff has been marked by upstream as broken since 3.1.3
-  # and has not been reported fixed as of 3.2.6
+  # and has not been reported fixed as of 3.2.7
   patch do
-    url "https://download.samba.org/pub/rsync/src/rsync-patches-3.2.6.tar.gz"
-    mirror "https://www.mirrorservice.org/sites/rsync.samba.org/rsync-patches-3.2.6.tar.gz"
-    sha256 "c3d13132b560f456fd8fc9fdf9f59377e91adf0dfc8117e33800d14b483d1a85"
+    url "https://download.samba.org/pub/rsync/src/rsync-patches-3.2.7.tar.gz"
+    mirror "https://www.mirrorservice.org/sites/rsync.samba.org/rsync-patches-3.2.7.tar.gz"
+    sha256 "e7e5e9ea0b6dd7639c7a5c6f49a1d06be20d449d59f60ba59b238e1aa79b13f0"
     apply "patches/fileflags.diff"
   end
 
   def install
     args = %W[
-      --disable-debug
-      --prefix=#{prefix}
       --with-rsyncd-conf=#{etc}/rsyncd.conf
       --with-included-popt=no
       --with-included-zlib=no
       --enable-ipv6
     ]
 
-    # SIMD code throws ICE or is outright unsupported due to lack of support for
-    # function multiversioning on older versions of macOS
-    args << "--disable-simd" if MacOS.version < :catalina
-
-    system "./configure", *args
+    system "./configure", *std_configure_args, *args
     system "make"
     system "make", "install"
   end
