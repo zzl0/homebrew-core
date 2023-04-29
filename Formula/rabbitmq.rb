b7b4985bba7d1da6e906f9f30689d8b4baf80ccf
Mikl Wolfe <wolfemikl@gmail.com>
2023-04-29 07:37:50 -0600

    rabbitmq 3.11.15

--- Formula/rabbitmq.rb
+++ Formula/rabbitmq.rb
@@ -1,8 +1,8 @@
 class Rabbitmq < Formula
   desc "Messaging broker"
   homepage "https://www.rabbitmq.com"
-  url "https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.11.14/rabbitmq-server-generic-unix-3.11.14.tar.xz"
-  sha256 "1e3e8f8526f2c936e6aca5ec59e9c11e7f4caea54f9e63e845a587be919d368f"
+  url "https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.11.15/rabbitmq-server-generic-unix-3.11.15.tar.xz"
+  sha256 "538be3c85e8cad10de62705714f71eaacb04339f73a81477c91478600bb8253c"
   license "MPL-2.0"
 
   livecheck do
