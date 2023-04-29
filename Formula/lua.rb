454a17cbbaf8ac5c0f81d9d79ce34ae8e595f2a0
Rui Chen <rui@chenrui.dev>
2023-04-29 10:27:46 -0400

    lua 5.4.5

--- Formula/lua.rb
+++ Formula/lua.rb
@@ -1,10 +1,9 @@
 class Lua < Formula
   desc "Powerful, lightweight programming language"
   homepage "https://www.lua.org/"
-  url "https://www.lua.org/ftp/lua-5.4.4.tar.gz"
-  sha256 "164c7849653b80ae67bec4b7473b884bf5cc8d2dca05653475ec2ed27b9ebf61"
+  url "https://www.lua.org/ftp/lua-5.4.5.tar.gz"
+  sha256 "59df426a3d50ea535a460a452315c4c0d4e1121ba72ff0bdde58c2ef31d6f444"
   license "MIT"
-  revision 1
 
   livecheck do
     url "https://www.lua.org/ftp/"
