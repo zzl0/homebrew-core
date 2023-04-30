2a9ab87ebe1400a0dba56c3a8e5946a7692cf041
Patrick Linnane <patrick@linnane.io>
2023-04-30 15:02:12 -0700

    libopenmpt 0.7.0

--- Formula/libopenmpt.rb
+++ Formula/libopenmpt.rb
@@ -1,9 +1,9 @@
 class Libopenmpt < Formula
   desc "Software library to decode tracked music files"
   homepage "https://lib.openmpt.org/libopenmpt/"
-  url "https://lib.openmpt.org/files/libopenmpt/src/libopenmpt-0.6.10+release.autotools.tar.gz"
-  version "0.6.10"
-  sha256 "c25be8dc0dac23cec025487e58f195e4d14f7d94aeddd5fe46b21c04f1ce2774"
+  url "https://lib.openmpt.org/files/libopenmpt/src/libopenmpt-0.7.0+release.autotools.tar.gz"
+  version "0.7.0"
+  sha256 "411796b55aef73cab09c7a6e65d33f1d7bf4ee7fc2dade6bc8de5138dedcc6d1"
   license "BSD-3-Clause"
 
   livecheck do
