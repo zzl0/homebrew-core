16de0e5d359cdbcb43cb54e225f99b980a7ea9ad
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-30 16:38:51 +0000

    felinks: update 0.16.1 bottle.

--- Formula/felinks.rb
+++ Formula/felinks.rb
@@ -6,13 +6,13 @@ class Felinks < Formula
   license "GPL-2.0-only"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "161812829640eb38d7b3cc8745a357dde5c5504d5c275b024a39c7d765cbf6ac"
-    sha256 cellar: :any,                 arm64_monterey: "a0f23116d119af283254276777bc0e15a212ff734ea62162638f5f90a9c693e9"
-    sha256 cellar: :any,                 arm64_big_sur:  "a019ea4cddf3176b28231e2fed2618a0dcd0ebbaebe11a51cdb764871e311011"
-    sha256 cellar: :any,                 ventura:        "6b26c82e2c6caa670821b65f559e717cc07a509baf7097d7cb8805cdb3c7dc31"
-    sha256 cellar: :any,                 monterey:       "6e9bf6181366d6326f086453fae724f0f22c0ea72d6efac56c00e928a7e61760"
-    sha256 cellar: :any,                 big_sur:        "bdd387e06cfefa3f3990fd59b08c8d94ca9a5b48ef9cd19876d3db1ff976fe0f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "065972f98316dabbeb4f753183a4b3a0e7915f5f9168389282e19bc453b4626e"
+    sha256 cellar: :any,                 arm64_ventura:  "b2e1dd238b70becd3976f3a79f2397e9c9fc8f7e9c4ceede41c7e0cd76640d53"
+    sha256 cellar: :any,                 arm64_monterey: "30aa20c58c1745c000b45d14a6405292398fbd216684eb99bb173c4fd7fa6368"
+    sha256 cellar: :any,                 arm64_big_sur:  "9762cc8cd5b6bc4b2a4f186c8ce37cfa6e70f909d7ff33e820815597965c5e8c"
+    sha256 cellar: :any,                 ventura:        "83e88e06d52a3b1a38fbceaf80c48d9d0e81ab386966368141f2ef31461aefdc"
+    sha256 cellar: :any,                 monterey:       "1f3457b85aa91f3d07708b3f15a3824392dd1556a0153b332abd691c47e3c288"
+    sha256 cellar: :any,                 big_sur:        "dccc132f4c45426fb9913d6c6103cc0ff56321ca4593d33cdc5620148efb956f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7b970e83b510aa123da7200fbb9d5860fc02513fdf5c9d33b03fe3cea6b9fda1"
   end
 
   head do
