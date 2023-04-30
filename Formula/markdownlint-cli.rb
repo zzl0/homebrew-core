2d3ab08d41264bf3b3abf0c6c2771564f28904b3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-30 18:29:44 +0000

    markdownlint-cli: update 0.34.0 bottle.

--- Formula/markdownlint-cli.rb
+++ Formula/markdownlint-cli.rb
@@ -8,7 +8,13 @@ class MarkdownlintCli < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "075904ea0fceec4f953df170e952f7cbd48a67ea6661a621ecaf645ed3b38a11"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f6fe846f96176e8b0d225d35567239b92846daa569132583d2ae98e4af5699a0"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f6fe846f96176e8b0d225d35567239b92846daa569132583d2ae98e4af5699a0"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f6fe846f96176e8b0d225d35567239b92846daa569132583d2ae98e4af5699a0"
+    sha256 cellar: :any_skip_relocation, ventura:        "1269f50059c47f83b7ad61197fd0cc970f5612a492467a63daf6e32c131949bb"
+    sha256 cellar: :any_skip_relocation, monterey:       "1269f50059c47f83b7ad61197fd0cc970f5612a492467a63daf6e32c131949bb"
+    sha256 cellar: :any_skip_relocation, big_sur:        "1269f50059c47f83b7ad61197fd0cc970f5612a492467a63daf6e32c131949bb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f6fe846f96176e8b0d225d35567239b92846daa569132583d2ae98e4af5699a0"
   end
 
   depends_on "node"
