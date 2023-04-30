395fcb58723126f25439a609ef985c12a35a537c
Porkepix <porkepix@gmail.com>
2023-04-30 23:25:31 +0200

    graphviz 8.0.5

--- Formula/graphviz.rb
+++ Formula/graphviz.rb
@@ -2,8 +2,8 @@ class Graphviz < Formula
   desc "Graph visualization software from AT&T and Bell Labs"
   homepage "https://graphviz.org/"
   url "https://gitlab.com/graphviz/graphviz.git",
-      tag:      "8.0.4",
-      revision: "a2a825846905f05e9b6be4dfaa87c9f96cb43b15"
+      tag:      "8.0.5",
+      revision: "7bdda21f9e7877c2af653f82246bc4f62151b6a2"
   license "EPL-1.0"
   version_scheme 1
   head "https://gitlab.com/graphviz/graphviz.git", branch: "main"
