d9f41505b74cd730577eb7e8fee194d9e930e504
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-30 22:12:09 +0000

    apidoc: update 1.0.2 bottle.

--- Formula/apidoc.rb
+++ Formula/apidoc.rb
@@ -8,13 +8,13 @@ class Apidoc < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3dceaf411ae051794c9e727f5a554cc8d3a5632b5beb846722cd0101a5e6d0e5"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3dceaf411ae051794c9e727f5a554cc8d3a5632b5beb846722cd0101a5e6d0e5"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3dceaf411ae051794c9e727f5a554cc8d3a5632b5beb846722cd0101a5e6d0e5"
-    sha256 cellar: :any_skip_relocation, ventura:        "954c08c8c81e294270ea164aa947092633da88c712f230e739bf61dfdbc3e81b"
-    sha256 cellar: :any_skip_relocation, monterey:       "954c08c8c81e294270ea164aa947092633da88c712f230e739bf61dfdbc3e81b"
-    sha256 cellar: :any_skip_relocation, big_sur:        "954c08c8c81e294270ea164aa947092633da88c712f230e739bf61dfdbc3e81b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c8656188e99c7f8868acbb63abcdc58fb8f9a4a854c4dd10e6752bf7271a40d9"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "bca0aae352d4e22f865a9b7c09f11d42af01311b85e41569eebed4d0908e616d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "bca0aae352d4e22f865a9b7c09f11d42af01311b85e41569eebed4d0908e616d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bca0aae352d4e22f865a9b7c09f11d42af01311b85e41569eebed4d0908e616d"
+    sha256 cellar: :any_skip_relocation, ventura:        "5c181515ea9dcfbab3286ff2721d6068fb895ecfadcd87e64a1dc7fd025374d8"
+    sha256 cellar: :any_skip_relocation, monterey:       "5c181515ea9dcfbab3286ff2721d6068fb895ecfadcd87e64a1dc7fd025374d8"
+    sha256 cellar: :any_skip_relocation, big_sur:        "5c181515ea9dcfbab3286ff2721d6068fb895ecfadcd87e64a1dc7fd025374d8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "43212918028806c4271f35452853814d2d56a05af4fda8f9ee550575da70036c"
   end
 
   depends_on "node"
