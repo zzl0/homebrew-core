fc98ce67de8819ba8031162574674dd8ccacf4b5
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-05-02 06:14:05 +0800

    google-java-format 1.17.0

--- Formula/google-java-format.rb
+++ Formula/google-java-format.rb
@@ -3,8 +3,8 @@ class GoogleJavaFormat < Formula
 
   desc "Reformats Java source code to comply with Google Java Style"
   homepage "https://github.com/google/google-java-format"
-  url "https://github.com/google/google-java-format/releases/download/v1.16.0/google-java-format-1.16.0-all-deps.jar"
-  sha256 "82819a2c5f7067712e0233661b864c1c034f6657d63b8e718b4a50e39ab028f6"
+  url "https://github.com/google/google-java-format/releases/download/v1.17.0/google-java-format-1.17.0-all-deps.jar"
+  sha256 "33068bbbdce1099982ec1171f5e202898eb35f2919cf486141e439fc6e3a4203"
   license "Apache-2.0"
 
   bottle do
@@ -21,7 +21,7 @@ class GoogleJavaFormat < Formula
   depends_on "python@3.11"
 
   resource "google-java-format-diff" do
-    url "https://raw.githubusercontent.com/google/google-java-format/v1.16.0/scripts/google-java-format-diff.py"
+    url "https://raw.githubusercontent.com/google/google-java-format/v1.17.0/scripts/google-java-format-diff.py"
     sha256 "4c46a4ed6c39c2f7cbf2bc7755eefd7eaeb0a3db740ed1386053df822f15782b"
   end
 
