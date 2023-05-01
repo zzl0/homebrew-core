78525cfafca7707c0e8736accdaac07a1c99dd4d
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-05-01 17:22:12 +0800

    sqlmap 1.7.5

--- Formula/sqlmap.rb
+++ Formula/sqlmap.rb
@@ -3,8 +3,8 @@ class Sqlmap < Formula
 
   desc "Penetration testing for SQL injection and database servers"
   homepage "https://sqlmap.org"
-  url "https://github.com/sqlmapproject/sqlmap/archive/1.7.4.tar.gz"
-  sha256 "ad09a2e58d489762055f289a2e39c5f94e1ec80973cf7be488c4582bc43ae8e6"
+  url "https://github.com/sqlmapproject/sqlmap/archive/1.7.5.tar.gz"
+  sha256 "0bed6c991d02efa39deaa93293ef1183e6d54b63c1c12b585f5ffc0e9dee20e9"
   license "GPL-2.0-or-later"
   head "https://github.com/sqlmapproject/sqlmap.git", branch: "master"
 
