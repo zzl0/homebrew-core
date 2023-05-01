db78b37b70e8358bba9fe59174ba62db0f6232be
Porkepix <porkepix@gmail.com>
2023-05-01 13:47:03 +0200

    enzyme 0.0.59

--- Formula/enzyme.rb
+++ Formula/enzyme.rb
@@ -2,8 +2,8 @@ class Enzyme < Formula
   desc "High-performance automatic differentiation of LLVM"
   homepage "https://enzyme.mit.edu"
   # TODO: Check if we can use unversioned `llvm` at version bump.
-  url "https://github.com/EnzymeAD/Enzyme/archive/refs/tags/v0.0.58.tar.gz", using: :homebrew_curl
-  sha256 "651071e397f8b9b20e5846e44864a002c461fd1ad91e99136d804942d4157c10"
+  url "https://github.com/EnzymeAD/Enzyme/archive/refs/tags/v0.0.59.tar.gz", using: :homebrew_curl
+  sha256 "7bf5f46645bb448e5f160ac74235aa8ccc583e708f1a4a0e3ad48fb0b909e537"
   license "Apache-2.0" => { with: "LLVM-exception" }
   head "https://github.com/EnzymeAD/Enzyme.git", branch: "main"
 
