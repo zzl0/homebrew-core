15ac3c456d5ac20f37af5c6aabbe1bb3f6cd6bb1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-05-01 16:14:19 +0000

    access: add 0.10.9 bottle.

--- Formula/access.rb
+++ Formula/access.rb
@@ -7,6 +7,16 @@ class Access < Formula
   license "Apache-2.0"
   head "https://github.com/indentapis/access.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "4a51449899ec8fd96c9ff3091a9adfaf6c9d59534d5e53f1e35ee8faf0c5caa2"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "4a51449899ec8fd96c9ff3091a9adfaf6c9d59534d5e53f1e35ee8faf0c5caa2"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4a51449899ec8fd96c9ff3091a9adfaf6c9d59534d5e53f1e35ee8faf0c5caa2"
+    sha256 cellar: :any_skip_relocation, ventura:        "6ac545fdb6d6298f0fdbabd89f84b74b449526287ff866545c8f9565af842f74"
+    sha256 cellar: :any_skip_relocation, monterey:       "6ac545fdb6d6298f0fdbabd89f84b74b449526287ff866545c8f9565af842f74"
+    sha256 cellar: :any_skip_relocation, big_sur:        "6ac545fdb6d6298f0fdbabd89f84b74b449526287ff866545c8f9565af842f74"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e4617c7c101c3172512c99356b253bb3aa5c46fa4dcaa5f96b404c3e18be8d2f"
+  end
+
   depends_on "go" => :build
 
   def install
