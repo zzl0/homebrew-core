80da91ec282f55d6c9143c8d3200470873c391f9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-05-02 18:04:27 +0000

    zpaqfranz: update 58.2 bottle.

--- Formula/zpaqfranz.rb
+++ Formula/zpaqfranz.rb
@@ -14,13 +14,13 @@ class Zpaqfranz < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a0bef53ae17206bed5dce3ec443a1f611157133dd841c984fbcb23ffc3a8545d"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "7e308f03281301a4cb56a6c432c253521f776f61bd40194eafcf50ab06061816"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "73b7e69a62b70769cde9407431cf529cab09894c5a7cad7d0fb4fa7213c176a2"
-    sha256 cellar: :any_skip_relocation, ventura:        "296d7c6c32febf879490a5b854cfd266746100f38603ce91aada3e13dfa231b8"
-    sha256 cellar: :any_skip_relocation, monterey:       "5a7078257b14dae4d22d4d563bcb983c580a28718ba068cc485cfb2bd21f137b"
-    sha256 cellar: :any_skip_relocation, big_sur:        "13ed30b4cf87c0f341c2c88d7b105c9610c9e60e9c6524d8413f5650eff4339b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ada8e2725825db710b34e3bd86bf4b9231071bfb61b774e5ea4bbcf504bfcf4c"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "1428ecc0db3b3a71f656211d2c1910e1fae658481a3662abbd272a7c83ec10fd"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c49c248cbb6aa9be78fec92898aa93c5d8b282397890330fcfa7fc28795ad208"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ae4a99f06ce303403355b0b7edd12021894350317db7999ecd4663a572b50059"
+    sha256 cellar: :any_skip_relocation, ventura:        "9a76dd6b1ebc7d84ce13cd6682ae16f6c97f7d5462eea276e7f69f14299ef862"
+    sha256 cellar: :any_skip_relocation, monterey:       "8c4e349d56fe1c48a992b6ff32d5385071cb226c2e37b3c9060b0ebd7b73fe9e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "be658e93e4a00fb0be8628a28db99ed8c0e8119a6e7210408773da4750be3606"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f4636bd3edb7264beb167bf70364a5a9d05207038b0436eaa786c50eb6376bbb"
   end
 
   def install
