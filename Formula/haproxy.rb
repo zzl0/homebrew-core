c7f14b8673f90b3d261a08d09985124b90d8ea3a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-05-02 13:32:25 +0000

    haproxy: update 2.7.8 bottle.

--- Formula/haproxy.rb
+++ Formula/haproxy.rb
@@ -11,13 +11,13 @@ class Haproxy < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "237b76447e8d85c7f353c9edae0e0e46d27d859d375908ca04d9400d0e45dc4a"
-    sha256 cellar: :any,                 arm64_monterey: "86c07f6a715403a1bbd97b7b513f310e2cd6d6eef93fb98b70763a4f25030010"
-    sha256 cellar: :any,                 arm64_big_sur:  "48cc55206810b662fbfcba1e80b55f993102caf602acc6f325702328be31ec1a"
-    sha256 cellar: :any,                 ventura:        "c2dcc670306dc6e555cac9b74f412c33000aa68a419cea55e70ec23b4e9ff2aa"
-    sha256 cellar: :any,                 monterey:       "e9ee0177de320c6b4f66c3af97e9c9060fd72bb26acf4c50b3f442d7e89afc5f"
-    sha256 cellar: :any,                 big_sur:        "0d2e6323ffcb252ac854e79823e860bd738028123c5264d5d465be8c05933b23"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "969783af1e8f0d2cfc8a2f03a5d0fa65f032353ea247536d40ea7ce57677bf09"
+    sha256 cellar: :any,                 arm64_ventura:  "7dfde8d00ec028c92e39164da68aeadd28457a45e54dd84d18a017d3018c5b14"
+    sha256 cellar: :any,                 arm64_monterey: "5d1c5179bf6a309cc66d565d5a7c0c91fb670d1e0993571eba909ef0749d24e6"
+    sha256 cellar: :any,                 arm64_big_sur:  "b3edc98f5041968c18e30242d26bb039e8f8e3776a92f0233add9c2a06e7ee8a"
+    sha256 cellar: :any,                 ventura:        "bacff0b4c59357be353685d94c76c8a7bdbd5bf773adf1028dda04bd79faab58"
+    sha256 cellar: :any,                 monterey:       "f082a41a0ff440f950d1a1f94b22365ff4493007e1f5125a390020f76b5b9630"
+    sha256 cellar: :any,                 big_sur:        "13bdb8c59e5745663bb75201688437130895ed65ea945e6d5c9bdf6ca913f3cd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ea00a572f5385ee6df351862612105a36f1ec5c1ba1fef6c34194940177bbd55"
   end
 
   depends_on "openssl@3"
