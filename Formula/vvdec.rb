c59e38e6bdcfb74ac47769c52af7c6ca9b09b5eb
Porkepix <porkepix@gmail.com>
2023-05-02 15:47:45 +0200

    vvdec 2.0.0
    vvdec: update resource name

--- Formula/vvdec.rb
+++ Formula/vvdec.rb
@@ -1,8 +1,8 @@
 class Vvdec < Formula
   desc "Fraunhofer Versatile Video Decoder"
   homepage "https://github.com/fraunhoferhhi/vvdec"
-  url "https://github.com/fraunhoferhhi/vvdec/archive/refs/tags/v1.6.1.tar.gz"
-  sha256 "72de0cbcd24285e6f66209be9270f8f0c897d24e586b3876c6a7bb5691375c48"
+  url "https://github.com/fraunhoferhhi/vvdec/archive/refs/tags/v2.0.0.tar.gz"
+  sha256 "55e9c2b7510ae7ecf686107e9ebf969079c05d763f822c571827e38a5e90dfeb"
   license "BSD-3-Clause-Clear"
   head "https://github.com/fraunhoferhhi/vvdec.git", branch: "master"
 
@@ -18,7 +18,7 @@ class Vvdec < Formula
 
   depends_on "cmake" => :build
 
-  resource("test_video") do
+  resource("homebrew-test-video") do
     url "https://archive.org/download/testvideo_20230410_202304/test.vvc"
     sha256 "753261009b6472758cde0dee2c004ff712823b43e62ec3734f0f46380bec8e46"
   end
@@ -33,7 +33,7 @@ class Vvdec < Formula
   end
 
   test do
-    resource("test_video").stage testpath
+    resource("homebrew-test-video").stage testpath
     system bin/"vvdecapp", "-b", testpath/"test.vvc", "-o", testpath/"test.yuv"
     assert_predicate testpath/"test.yuv", :exist?
   end
