3e51dd7fa76d050579c723e9b534383c9fc1879f
Matias Yocca <5626046+mayocca@users.noreply.github.com>
2023-05-03 10:45:04 -0300

    pocketbase 0.15.2 (new formula)
    * pocketbase 0.15.2 (new formula)
    * Update pocketbase.rb
    * Remove trailing whitespaces
    * Apply suggestions from code review
    
    Closes #130059.
    
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>
    Co-authored-by: Matías Yocca <matuyocca@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/pocketbase.rb
@@ -0,0 +1,30 @@
+class Pocketbase < Formula
+  desc "Open source backend for your next project in 1 file"
+  homepage "https://pocketbase.io/"
+  url "https://github.com/pocketbase/pocketbase/archive/refs/tags/v0.15.2.tar.gz"
+  sha256 "abc524d9a4e9235e6b2d9d274df1a9fd987f17d42427f3b085f777c064a29fd9"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["CGO_ENABLED"] = "0"
+
+    system "go", "build", *std_go_args(ldflags: "-s -w -X github.com/pocketbase/pocketbase.Version=#{version}"), "./examples/base"
+  end
+
+  test do
+    assert_match "pocketbase version #{version}", shell_output("#{bin}/pocketbase --version")
+
+    system "#{bin}/pocketbase", "--dir", testpath/"pb_data"
+
+    assert_predicate testpath/"pb_data", :exist?, "pb_data directory should exist"
+    assert_predicate testpath/"pb_data", :directory?, "pb_data should be a directory"
+
+    assert_predicate testpath/"pb_data/data.db", :exist?, "pb_data/data.db should exist"
+    assert_predicate testpath/"pb_data/data.db", :file?, "pb_data/data.db should be a file"
+
+    assert_predicate testpath/"pb_data/logs.db", :exist?, "pb_data/logs.db should exist"
+    assert_predicate testpath/"pb_data/logs.db", :file?, "pb_data/logs.db should be a file"
+  end
+end
