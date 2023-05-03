9aea988465c528043a5b66faf2845e53ff59dffc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-05-03 23:09:26 +0000

    forgit: add 23.04.0 bottle.

--- Formula/forgit.rb
+++ Formula/forgit.rb
@@ -5,6 +5,10 @@ class Forgit < Formula
   sha256 "89826d12b551829c4bce396551b1505c0604ba29130f4b8e2f842c2ca0b4c9e3"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "dd4754e370b3a0aad8afdf03901f14e5263b893dc87fe6e1b31d008df2b03694"
+  end
+
   depends_on "fzf"
 
   def install
