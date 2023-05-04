728a2cfec93eb46cea3b638dde7abd60a3047a6d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-05-04 17:30:47 +0000

    puzzles: update 20230504 bottle.

--- Formula/puzzles.rb
+++ Formula/puzzles.rb
@@ -18,13 +18,13 @@ class Puzzles < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "5ca10ae618a5566b60cbc898d6143944bee7d562431fbb76b2d2d31ef8c9f6a7"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "84a804fcaecb6f172a51c26e2f138ae481afada83c095989c8ba7a2b47b5c53d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8f23663288e6ecc4ead947b6fdc8a73b260270de6e633dcea1dcd2e1a16b4b65"
-    sha256 cellar: :any_skip_relocation, ventura:        "662a237264a68bcb40728b4824ac29300977cfbfc6f7cd6b967af80ec3cb63f5"
-    sha256 cellar: :any_skip_relocation, monterey:       "c42df1d4fac6c5369eb3332ebc72d039807763605f1554c673075057f0bfcb8d"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0c7772fad08efd6ab179103b05a10e2b3cebe843319210f9d0adb2ffede2068c"
-    sha256                               x86_64_linux:   "2bb7383b696edc540a05c1d6537547d6ca07e37aefef766c1c6930ccf3a3bcb3"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "533fd878392c71e637aa514e87b3fa1e91fe0d229e54369d9b64d0ecc8ad1648"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "7d537b86d6cfe8246b296ed5580b754dc5b0f9be6814cd100cae299390a38c09"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7d798b696c5ae176a204a238b66aee4c041cf324eabd75f56316c1e4d11ef141"
+    sha256 cellar: :any_skip_relocation, ventura:        "de86b94df24be45f80588d029ade84995a344998336464d0110470ab0245456d"
+    sha256 cellar: :any_skip_relocation, monterey:       "6ed55307cf90b2f018e5a762a0e216a367b2106847edf6e306da682de79d3363"
+    sha256 cellar: :any_skip_relocation, big_sur:        "32d34a0fab2a7f74821f631e7425ce8482068edb6651145b42ee6f1d02448e9b"
+    sha256                               x86_64_linux:   "19c943ecb876d126bc54087fffaa33f02d731b57e1329e2883f6760bc288531b"
   end
 
   depends_on "cmake" => :build
