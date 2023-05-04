be7048179d66d51b5e89af8763cc43e439f091fa
Patrick Linnane <patrick@linnane.io>
2023-05-04 14:54:04 -0700

    abyss 2.3.7
    abyss: update build and deps

--- Formula/abyss.rb
+++ Formula/abyss.rb
@@ -1,10 +1,9 @@
 class Abyss < Formula
   desc "Genome sequence assembler for short reads"
   homepage "https://www.bcgsc.ca/resources/software/abyss"
-  url "https://github.com/bcgsc/abyss/releases/download/2.3.5/abyss-2.3.5.tar.gz"
-  sha256 "5455f7708531681ee15ec4fd5620526a53c86d28f959e630dc495f526b7d40f7"
+  url "https://github.com/bcgsc/abyss/releases/download/2.3.7/abyss-2.3.7.tar.gz"
+  sha256 "ba37780e79ec3aa359b6003e383caef13479a87f4d0022af01b86398f9ffca1f"
   license all_of: ["GPL-3.0-only", "LGPL-2.1-or-later", "MIT", "BSD-3-Clause"]
-  revision 1
 
   livecheck do
     url :stable
@@ -29,12 +28,30 @@ class Abyss < Formula
   end
 
   depends_on "boost" => :build
+  depends_on "cmake" => :build # For btllib
   depends_on "google-sparsehash" => :build
+  depends_on "meson" => :build # For btllib
+  depends_on "ninja" => :build # For btllib
+  depends_on "python@3.11" => :build # For btllib
   depends_on "gcc"
   depends_on "open-mpi"
 
+  uses_from_macos "sqlite"
+
   fails_with gcc: "5"
   fails_with :clang # no OpenMP support
+
+  resource "btllib" do
+    url "https://github.com/bcgsc/btllib/releases/download/v1.6.0/btllib-1.6.0.tar.gz"
+    sha256 "4a122c1047785dc865b8c94063714667f8ca43b8a881754eebd96dbb44fd1c3f"
+
+    # Fixes 'uint' was not declared in this scope
+    # Remove in next release
+    patch do
+      url "https://github.com/bcgsc/btllib/commit/43adf3d2671cc1ab780d23666e038055edb9d669.patch?full_index=1"
+      sha256 "47e0f70501c8f5d543eb2a956a226f0a1a51816123a9b2061a081fb92c7b3f0c"
+    end
+  end
 
   resource "homebrew-testdata" do
     url "https://www.bcgsc.ca/sites/default/files/bioinformatics/software/abyss/releases/1.3.4/test-data.tar.gz"
@@ -43,13 +60,19 @@ class Abyss < Formula
 
   def install
     ENV.delete("HOMEBREW_SDKROOT") if MacOS.version >= :mojave && MacOS::CLT.installed?
+
+    (buildpath/"btllib").install resource("btllib")
+    cd "btllib" do
+      system "./compile"
+    end
+
     system "./autogen.sh" if build.head?
-    system "./configure", "--enable-maxk=128",
-                          "--prefix=#{prefix}",
+    system "./configure", *std_configure_args,
+                          "--enable-maxk=128",
                           "--with-boost=#{Formula["boost"].include}",
+                          "--with-btllib=#{buildpath}/btllib/install",
                           "--with-mpi=#{Formula["open-mpi"].prefix}",
                           "--with-sparsehash=#{Formula["google-sparsehash"].prefix}",
-                          "--disable-dependency-tracking",
                           "--disable-silent-rules"
     system "make", "install"
   end
