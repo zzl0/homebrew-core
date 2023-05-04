570ce3aa8bf8d991da6716d107876b53edaddcbb
Tien Nguyen <duytien.nguyen@okta.com>
2023-05-04 14:18:09 -0400

    okta-aws-cli 1.0.1
    update test

--- Formula/okta-aws-cli.rb
+++ Formula/okta-aws-cli.rb
@@ -1,8 +1,8 @@
 class OktaAwsCli < Formula
   desc "Okta federated identity for AWS CLI"
   homepage "https://github.com/okta/okta-aws-cli"
-  url "https://github.com/okta/okta-aws-cli/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "f8ddb7933cbee81ea5b78557708cd297c548ec633b8663bd5f67b981f8425dbc"
+  url "https://github.com/okta/okta-aws-cli/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "c901b9221556e6ecf64a6ea20f7e6c43a4d629add306d9e4a03cd58c46f71912"
   license "Apache-2.0"
 
   bottle do
@@ -26,6 +26,6 @@ class OktaAwsCli < Formula
     assert_match "Usage:", str_help
     assert_match "Flags:", str_help
     str_error = shell_output("#{bin}/okta-aws-cli -o example.org -c homebrew-test 2>&1", 1)
-    assert_match 'Error: ValidationError: Field="Org Domain" Message="is not from Okta"', str_error
+    assert_match 'Error: authorize received API response "404 Not Found"', str_error
   end
 end
