1ae6c2b09f6d415d3d3cd31636d88d87da4ba2e5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-05-04 18:30:18 +0000

    zinit: update 3.11.0 bottle.

--- Formula/zinit.rb
+++ Formula/zinit.rb
@@ -7,13 +7,13 @@ class Zinit < Formula
   head "https://github.com/zdharma-continuum/zinit.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "9f3680c8222c88b4dc8497acbfb373e546c07ebaee77dad10ca5ed8a45f149ce"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "9f3680c8222c88b4dc8497acbfb373e546c07ebaee77dad10ca5ed8a45f149ce"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9f3680c8222c88b4dc8497acbfb373e546c07ebaee77dad10ca5ed8a45f149ce"
-    sha256 cellar: :any_skip_relocation, ventura:        "b13c70a2e5d04306b425bc9f4d72380c5a048a3429174eba579403f2f5260267"
-    sha256 cellar: :any_skip_relocation, monterey:       "b13c70a2e5d04306b425bc9f4d72380c5a048a3429174eba579403f2f5260267"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b13c70a2e5d04306b425bc9f4d72380c5a048a3429174eba579403f2f5260267"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9f3680c8222c88b4dc8497acbfb373e546c07ebaee77dad10ca5ed8a45f149ce"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "089b7e3f89a494031391647bfed6fb44b3b34c5a3e86d7f3d1c7a1545cc0aac9"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "089b7e3f89a494031391647bfed6fb44b3b34c5a3e86d7f3d1c7a1545cc0aac9"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "089b7e3f89a494031391647bfed6fb44b3b34c5a3e86d7f3d1c7a1545cc0aac9"
+    sha256 cellar: :any_skip_relocation, ventura:        "d3bea09b4e34937f7a725ffaf70a85e895f6cc3a09114a8155c9a8afb6e97065"
+    sha256 cellar: :any_skip_relocation, monterey:       "d3bea09b4e34937f7a725ffaf70a85e895f6cc3a09114a8155c9a8afb6e97065"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d3bea09b4e34937f7a725ffaf70a85e895f6cc3a09114a8155c9a8afb6e97065"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "089b7e3f89a494031391647bfed6fb44b3b34c5a3e86d7f3d1c7a1545cc0aac9"
   end
 
   uses_from_macos "zsh"
