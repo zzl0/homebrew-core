90605f75e9f3d9e1755661f4ef9e7fa7e38c61f5
Patrick Linnane <patrick@linnane.io>
2023-05-05 11:50:02 -0700

    radare2 5.8.6

--- Formula/radare2.rb
+++ Formula/radare2.rb
@@ -1,8 +1,8 @@
 class Radare2 < Formula
   desc "Reverse engineering framework"
   homepage "https://radare.org"
-  url "https://github.com/radareorg/radare2/archive/5.8.4.tar.gz"
-  sha256 "8ea45bd82e5ea37e270ca14ac2a6f947c647a24f9de9e18bf8cebc71c0816dcd"
+  url "https://github.com/radareorg/radare2/archive/5.8.6.tar.gz"
+  sha256 "f6db0bb1a97b1fc51a968ac3131b4b36ad5c1cc15802da03a63c244e24fe7530"
   license "LGPL-3.0-only"
   head "https://github.com/radareorg/radare2.git", branch: "master"
 
