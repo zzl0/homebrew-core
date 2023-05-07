db45be864815b6ef95e611602b5cb543fdfd344f
Patrick Linnane <patrick@linnane.io>
2023-05-07 08:08:29 -0700

    calabash 1.5.5-120

--- Formula/calabash.rb
+++ Formula/calabash.rb
@@ -1,8 +1,8 @@
 class Calabash < Formula
   desc "XProc (XML Pipeline Language) implementation"
   homepage "https://xmlcalabash.com/"
-  url "https://github.com/ndw/xmlcalabash1/releases/download/1.5.4-120/xmlcalabash-1.5.4-120.zip"
-  sha256 "f50d132d0c55a363fdcb4e7541d77558d700e8235853908f2925d45ad55c4334"
+  url "https://github.com/ndw/xmlcalabash1/releases/download/1.5.5-120/xmlcalabash-1.5.5-120.zip"
+  sha256 "74fb7aed82944fdb0c814c5ebb534fd35083993de9d14afd6052d72563bba2e3"
   license any_of: ["GPL-2.0-only", "CDDL-1.0"]
 
   # According to ndw/xmlcalabash1#342, each release comes in "flavours" that
