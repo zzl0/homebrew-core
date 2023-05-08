169f87cb9695cb237a75dccb067ab5369d720d17
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-05-08 15:41:07 +0000

    cilium-cli: update 0.14.2 bottle.

--- Formula/cilium-cli.rb
+++ Formula/cilium-cli.rb
@@ -6,13 +6,13 @@ class CiliumCli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "6f4c99b52107f8513cc31522601866a5544a9afa7da06dff861612778bdab7ea"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "86f8b1bf22472dd4afef19e3abf60d3544af6225550f6bba2372e2cf73315d9f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "314ee4dd7f5e6e3daffbef8d41aa084296f2cddc3a2348ec7b4c5f8b55988cea"
-    sha256 cellar: :any_skip_relocation, ventura:        "a178d1fd3a745e975b3d101148d803d641739b8a5fa0df496e6364f906aaade3"
-    sha256 cellar: :any_skip_relocation, monterey:       "954905fb529fc249a38eb34942a7d75e5c565a53ce5aa1e61653e34ae4f14999"
-    sha256 cellar: :any_skip_relocation, big_sur:        "5c438094e1f247470aeee3f252c9d099e30ed6ebbe07b1747c072fac798312ca"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7b0e03ff8981f079cc00ca5be5fabf6a16eee33a3e31f484f8578c8916d658bf"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "4e90e343e8b58f9b936b77b07f823f71edac314b8756a3f19d03eb7b2eab80ee"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "8b74d763f8810e58b3c8b68479ed4b594151bbe4507c8537a2f21523388b2593"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "23980a402047ceaec326b42970a04217b8bd8fc92b9ba0d94a06f8e24668e22f"
+    sha256 cellar: :any_skip_relocation, ventura:        "02b73e88ea70bc1a848bf4d03f8382fdfea0e1892d9a27c1690daf26a7817580"
+    sha256 cellar: :any_skip_relocation, monterey:       "06a51fe64cb6f6c9ea99911b700b02f9ca3e9707045df5ddc9af1b1d5a5eb0fa"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b67ade96f41a445de9c3548fc93b2fdafd820a37e6a0ff0618e84a4d7f10bb0a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "351233464b7a6e391446f2bc1697783d82324d10018e43a9242be1057779ee0b"
   end
 
   depends_on "go" => :build
