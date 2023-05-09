c069b7390210515c3051c17b8d9c0888fe5e309a
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-05-10 05:44:21 +0800

    s2n 1.3.44

--- Formula/s2n.rb
+++ Formula/s2n.rb
@@ -1,8 +1,8 @@
 class S2n < Formula
   desc "Implementation of the TLS/SSL protocols"
   homepage "https://github.com/aws/s2n-tls"
-  url "https://github.com/aws/s2n-tls/archive/v1.3.43.tar.gz"
-  sha256 "6fd844f290b37ef3b08854eb2dbd7951f9cb7452f1c4280267833e9eaa4b564d"
+  url "https://github.com/aws/s2n-tls/archive/v1.3.44.tar.gz"
+  sha256 "16b25209bfe08c9d14c74277f2e98bba90bc9c37cae3885ad4122c545c3110c8"
   license "Apache-2.0"
   head "https://github.com/aws/s2n-tls.git", branch: "main"
 
