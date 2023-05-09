ce1b513d31b1e779d5a94ab6b555d96cefe908c4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-05-09 16:16:02 +0000

    phpstan: update 1.10.15 bottle.

--- Formula/phpstan.rb
+++ Formula/phpstan.rb
@@ -6,13 +6,13 @@ class Phpstan < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a8f9699393f6d703add456b1236a9233731e34532775da8f52a39e89e7ba35b4"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a8f9699393f6d703add456b1236a9233731e34532775da8f52a39e89e7ba35b4"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a8f9699393f6d703add456b1236a9233731e34532775da8f52a39e89e7ba35b4"
-    sha256 cellar: :any_skip_relocation, ventura:        "089a94b18ab6a43463834105de800de79eb5dc8f1672ce6f882c2a7dd5378cd2"
-    sha256 cellar: :any_skip_relocation, monterey:       "089a94b18ab6a43463834105de800de79eb5dc8f1672ce6f882c2a7dd5378cd2"
-    sha256 cellar: :any_skip_relocation, big_sur:        "089a94b18ab6a43463834105de800de79eb5dc8f1672ce6f882c2a7dd5378cd2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a8f9699393f6d703add456b1236a9233731e34532775da8f52a39e89e7ba35b4"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "601cb30d38a328008985e6bce537fac422f29ac8264ac0c2483061ffb5b9ce83"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "601cb30d38a328008985e6bce537fac422f29ac8264ac0c2483061ffb5b9ce83"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "601cb30d38a328008985e6bce537fac422f29ac8264ac0c2483061ffb5b9ce83"
+    sha256 cellar: :any_skip_relocation, ventura:        "e44ce28e146b9290e50fd92eb74c6387373d6b4bac255b7bfca2fa44315972a4"
+    sha256 cellar: :any_skip_relocation, monterey:       "e44ce28e146b9290e50fd92eb74c6387373d6b4bac255b7bfca2fa44315972a4"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e44ce28e146b9290e50fd92eb74c6387373d6b4bac255b7bfca2fa44315972a4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "601cb30d38a328008985e6bce537fac422f29ac8264ac0c2483061ffb5b9ce83"
   end
 
   depends_on "php" => :test
