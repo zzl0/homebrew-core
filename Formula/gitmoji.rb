0f66a576e28332ad40b4a5d40187b5367652446c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-05-09 23:29:52 +0000

    gitmoji: update 8.2.2 bottle.

--- Formula/gitmoji.rb
+++ Formula/gitmoji.rb
@@ -8,13 +8,13 @@ class Gitmoji < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "79c3ec4420fc581b720e9cb108bb4ede4b17703ce8d1757b73de5587598623bf"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "79c3ec4420fc581b720e9cb108bb4ede4b17703ce8d1757b73de5587598623bf"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "79c3ec4420fc581b720e9cb108bb4ede4b17703ce8d1757b73de5587598623bf"
-    sha256 cellar: :any_skip_relocation, ventura:        "9fcc6ec154672fb0c25f8c1eade4f0d65105e864cf6edab6baf1e31b45961178"
-    sha256 cellar: :any_skip_relocation, monterey:       "9fcc6ec154672fb0c25f8c1eade4f0d65105e864cf6edab6baf1e31b45961178"
-    sha256 cellar: :any_skip_relocation, big_sur:        "9fcc6ec154672fb0c25f8c1eade4f0d65105e864cf6edab6baf1e31b45961178"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "79c3ec4420fc581b720e9cb108bb4ede4b17703ce8d1757b73de5587598623bf"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "566582130eff5c5f4392a4b30781c7ae271defe465a4f325e3f8d17d5badcb95"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "566582130eff5c5f4392a4b30781c7ae271defe465a4f325e3f8d17d5badcb95"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "566582130eff5c5f4392a4b30781c7ae271defe465a4f325e3f8d17d5badcb95"
+    sha256 cellar: :any_skip_relocation, ventura:        "e1c59b3faf421f387348ebbbc662b95df5f8a83728ac3c360dc341aa762a0e86"
+    sha256 cellar: :any_skip_relocation, monterey:       "e1c59b3faf421f387348ebbbc662b95df5f8a83728ac3c360dc341aa762a0e86"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e1c59b3faf421f387348ebbbc662b95df5f8a83728ac3c360dc341aa762a0e86"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "566582130eff5c5f4392a4b30781c7ae271defe465a4f325e3f8d17d5badcb95"
   end
 
   depends_on "node"
