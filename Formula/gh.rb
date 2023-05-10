3dacda4b03a33c8797aa4e1cdb955fd84cf83469
Mislav Marohnić <hi@mislav.net>
2023-05-10 12:57:18 +0200

    gh 2.29.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/gh.rb
+++ Formula/gh.rb
@@ -1,8 +1,8 @@
 class Gh < Formula
   desc "GitHub command-line tool"
   homepage "https://github.com/cli/cli"
-  url "https://github.com/cli/cli/archive/v2.28.0.tar.gz"
-  sha256 "cf3c0fb7f601d717d8b5177707a197c49fd426f5dc3c9aa52a932e96ba7166af"
+  url "https://github.com/cli/cli/archive/v2.29.0.tar.gz"
+  sha256 "f77718f109ff5817cde33eb004137f58bdcd5934b921aed1440c6b3e93e1df27"
   license "MIT"
 
   head "https://github.com/cli/cli.git", branch: "trunk"
