1a25b9c49e1bd0b3805e51948c4f75a61a739ceb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-05-10 08:03:05 +0000

    lpeg: add 1.0.2 bottle.

--- Formula/lpeg.rb
+++ Formula/lpeg.rb
@@ -11,6 +11,16 @@ class Lpeg < Formula
     regex(/href=.*?lpeg[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
+  bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "22325d9fc7125511176621bf0bc1ab1490875255258e613b7646d688fb65895b"
+    sha256 cellar: :any,                 arm64_monterey: "3f7b628261f3db631abd0af83e6f579504838b4b00f52dd4ad83ec8bb87a3a7c"
+    sha256 cellar: :any,                 arm64_big_sur:  "2cf8b5089a23d86f10ae205d93bfd335af81a449acc4d40c25e2675a06e6d33b"
+    sha256 cellar: :any,                 ventura:        "da6e45a73eb1e54264b5d04fc04c13605bc799606e15afc37e592e420ef8f813"
+    sha256 cellar: :any,                 monterey:       "46819d3db41b35eacab885a8314f69de5417dd33f1dd8a7931292bb214687479"
+    sha256 cellar: :any,                 big_sur:        "4c18893291c3fcbcfa98991fce241f95357af66df9160a3bbee5520a8573d5bb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4e3da90255288f5b4ebdb5d9fea1b4a40704dd20b393643cf42cea8259a5706a"
+  end
+
   depends_on "cmake" => :build
   depends_on "luajit" => [:build, :test]
 
