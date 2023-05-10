a67ce03bdaf18ec0dea09e85cd8fb60c5eeff93c
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-05-10 17:50:13 +0800

    newrelic-infra-agent 1.41.0

--- Formula/newrelic-infra-agent.rb
+++ Formula/newrelic-infra-agent.rb
@@ -2,8 +2,8 @@ class NewrelicInfraAgent < Formula
   desc "New Relic infrastructure agent"
   homepage "https://github.com/newrelic/infrastructure-agent"
   url "https://github.com/newrelic/infrastructure-agent.git",
-      tag:      "1.40.1",
-      revision: "a19bf2faf96efaa6fb93ec06a20eab91e9d7bce5"
+      tag:      "1.41.0",
+      revision: "c7affda7b8b70cdf119e3f726f2add5680b1b160"
   license "Apache-2.0"
   head "https://github.com/newrelic/infrastructure-agent.git", branch: "master"
 
