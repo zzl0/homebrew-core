2a92fd181823decc3b3b5a4f12c5279c57a1b054
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-05-11 13:07:05 +0000

    access: update 0.10.11 bottle.

--- Formula/access.rb
+++ Formula/access.rb
@@ -8,13 +8,13 @@ class Access < Formula
   head "https://github.com/indentapis/access.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "d8f0ccea9cc161ffc7d503af8d2f1c6d014a8a16a4456a88e3c4fbe3f56e3a74"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "d8f0ccea9cc161ffc7d503af8d2f1c6d014a8a16a4456a88e3c4fbe3f56e3a74"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d8f0ccea9cc161ffc7d503af8d2f1c6d014a8a16a4456a88e3c4fbe3f56e3a74"
-    sha256 cellar: :any_skip_relocation, ventura:        "6912ee022be7fdb94c61b729de12789606d59a8950d363ae22bf93d054624dcd"
-    sha256 cellar: :any_skip_relocation, monterey:       "6912ee022be7fdb94c61b729de12789606d59a8950d363ae22bf93d054624dcd"
-    sha256 cellar: :any_skip_relocation, big_sur:        "6912ee022be7fdb94c61b729de12789606d59a8950d363ae22bf93d054624dcd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1ad0e7891cc1da6cb77bf68779b80033da65d19ae6c6a993f46c1f60d9e4fec3"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "ae9a11f6e119aa508d2b526b6b539b871f45c852075bfe6c1e92d24374b5da70"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ae9a11f6e119aa508d2b526b6b539b871f45c852075bfe6c1e92d24374b5da70"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ae9a11f6e119aa508d2b526b6b539b871f45c852075bfe6c1e92d24374b5da70"
+    sha256 cellar: :any_skip_relocation, ventura:        "3b051721265cc9b779171af4620b1e74208cd95c62453407def25e3c79fc6c41"
+    sha256 cellar: :any_skip_relocation, monterey:       "3b051721265cc9b779171af4620b1e74208cd95c62453407def25e3c79fc6c41"
+    sha256 cellar: :any_skip_relocation, big_sur:        "3b051721265cc9b779171af4620b1e74208cd95c62453407def25e3c79fc6c41"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3cef331bbd2b8e9b6d2551926c041997ed12b0324c9495223c624190c4431aa2"
   end
 
   depends_on "go" => :build
