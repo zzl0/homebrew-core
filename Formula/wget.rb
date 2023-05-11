ea5f9cfa02c84910a0ccc5ea3ffa261d7ef000d7
MoonFruit <dkmoonfruit@gmail.com>
2023-05-11 09:38:07 +0800

    wget 1.21.4

--- Formula/wget.rb
+++ Formula/wget.rb
@@ -1,10 +1,9 @@
 class Wget < Formula
   desc "Internet file retriever"
   homepage "https://www.gnu.org/software/wget/"
-  url "https://ftp.gnu.org/gnu/wget/wget-1.21.3.tar.gz"
-  sha256 "5726bb8bc5ca0f6dc7110f6416e4bb7019e2d2ff5bf93d1ca2ffcc6656f220e5"
+  url "https://ftp.gnu.org/gnu/wget/wget-1.21.4.tar.gz"
+  sha256 "81542f5cefb8faacc39bbbc6c82ded80e3e4a88505ae72ea51df27525bcde04c"
   license "GPL-3.0-or-later"
-  revision 1
 
   bottle do
     rebuild 1
