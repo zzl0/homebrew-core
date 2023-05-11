eb2db610cdd2ef611e5af8452f3f00fbefda03d0
Porkepix <porkepix@gmail.com>
2023-05-11 21:18:18 +0200

    marked 5.0.2

--- Formula/marked.rb
+++ Formula/marked.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Marked < Formula
   desc "Markdown parser and compiler built for speed"
   homepage "https://marked.js.org/"
-  url "https://registry.npmjs.org/marked/-/marked-5.0.1.tgz"
-  sha256 "37c502f594101c1634942efe47f522c1d4081b69003a7ec304d4e288e7b916b4"
+  url "https://registry.npmjs.org/marked/-/marked-5.0.2.tgz"
+  sha256 "49eba0c16a479d2cf33bd25634b5f5633d685a787ea5afdda4c0c1306944331a"
   license "MIT"
 
   bottle do
