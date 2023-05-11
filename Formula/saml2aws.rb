ce26618de8d500694e6dfecf1a8d48889abed3b1
Philip Mallegol-Hansen <philip@mallegolhansen.com>
2023-05-10 17:25:23 -0700

    saml2aws: add completion
    Adds zsh autocomplete for the saml2aws formula

--- Formula/saml2aws.rb
+++ Formula/saml2aws.rb
@@ -21,6 +21,19 @@ class Saml2aws < Formula
   def install
     ldflags = "-s -w -X main.Version=#{version}"
     system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/saml2aws"
+    (zsh_completion/"_saml2aws").write <<~EOS
+      #compdef saml2aws
+
+      _saml2aws_bash_autocomplete() {
+          local cur prev opts base
+          COMPREPLY=()
+          cur="${COMP_WORDS[COMP_CWORD]}"
+          opts=$( ${COMP_WORDS[0]} --completion-bash ${COMP_WORDS[@]:1:$COMP_CWORD} )
+          COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+          return 0
+      }
+      complete -F _saml2aws_bash_autocomplete saml2aws
+    EOS
   end
 
   test do
