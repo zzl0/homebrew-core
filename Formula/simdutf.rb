f09a04be59e89ceea9b21dde0e6ebb59bdeb2e0c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-05-12 23:26:02 +0000

    simdutf: update 3.2.9 bottle.

--- Formula/simdutf.rb
+++ Formula/simdutf.rb
@@ -7,12 +7,12 @@ class Simdutf < Formula
   head "https://github.com/simdutf/simdutf.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any, arm64_ventura:  "6f4f2e5e053c70beb7843daa09e1a1716aecec8a12700827ad4fc9f62f5f850f"
-    sha256 cellar: :any, arm64_monterey: "d3fbae321c28a688ed4d73a2b7ccf7344ef4cc6f7aaa7bd59c4dc9c6db46d160"
-    sha256 cellar: :any, arm64_big_sur:  "f949e234f145fabb1f020f369baca0663209da959dfa015e9232c506fea93315"
-    sha256 cellar: :any, ventura:        "b7bc51ce7d79ec083cb4652958f6c1462639c72111cb25e3e27e928581934c95"
-    sha256 cellar: :any, monterey:       "bf4a7dff56c3e8cb186398da13dc974b011110e6ad8428a1871ba8188fd9d895"
-    sha256 cellar: :any, big_sur:        "897ed2f62779c26bf189fe3db01235b84d36912709be0e99e044db48581011c2"
+    sha256 cellar: :any, arm64_ventura:  "60fdd6b1d7531a08e6563c1fadb06b44afd0282cd9d2933e8dd0fca6d9e9e94d"
+    sha256 cellar: :any, arm64_monterey: "3ef0ebf6ad59569bc42daa950ab17f440538b225301ab832de84b3371a9b0fda"
+    sha256 cellar: :any, arm64_big_sur:  "1de3c9c5cd071dc8e39cb43f140d8c17451837be0a7053da6d22e2e4302d1999"
+    sha256 cellar: :any, ventura:        "931b66433cfc6a0136b3c91e02d50dcf4e1dbdae8aa0d788b7a691bfe794053d"
+    sha256 cellar: :any, monterey:       "2a96c6b3d59eae11d5cbd058a383554b037e7322832ec3b990799860b2f81078"
+    sha256 cellar: :any, big_sur:        "9fff31dca61857ec784b70ba5fb4f580a0888be714e97c5bb10748a54095d96e"
   end
 
   depends_on "cmake" => :build
