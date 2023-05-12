a61da1e246935dc3f2dfcf5709ece1cfe543c2e4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-05-12 18:03:04 +0000

    neomutt: update 20230512 bottle.

--- Formula/neomutt.rb
+++ Formula/neomutt.rb
@@ -7,13 +7,13 @@ class Neomutt < Formula
   head "https://github.com/neomutt/neomutt.git", branch: "main"
 
   bottle do
-    sha256 arm64_ventura:  "5d1d75187c259fe4736cae143fa000716260e5a371f8cf768631157883d977aa"
-    sha256 arm64_monterey: "86c587c0dec446c9d808087c90d017f19ab0b0880e040707969aefeae55d0637"
-    sha256 arm64_big_sur:  "28c466ffe01de9f7f56a8b5308282e8a29fbe68f2fa767c8fabcf3eb99f464e4"
-    sha256 ventura:        "ae7c3cb919267cdeec06fc0343a239b6ba315e9ff8db949ddbd4c1a076518dbe"
-    sha256 monterey:       "f190f82b685d2b957825c0de0a11e8fe9aec81437664fa5008763e28949d358e"
-    sha256 big_sur:        "e65b85fb0d87bd7ccd2c2225d8f0aad786b92b62943e3f3c9c83067e8041edfd"
-    sha256 x86_64_linux:   "fdc45ba2883a200404e01e73150b7441a2de7333ef90e7bf2e97d2b54b26e9a0"
+    sha256 arm64_ventura:  "b9a96c2c9f1217ad7bf5fbaac79dcb28fd67002b2876d92135c5fadb0da19f4c"
+    sha256 arm64_monterey: "ccc9371c53a3e0949f52e8f3299209b6ad394d840bef74a81b6534d58e9d809d"
+    sha256 arm64_big_sur:  "2d8a0d0d8f5a25360489459b5fc82aeadd448889b18eabf51014f4ec04600a54"
+    sha256 ventura:        "40edbf93f35700d3e169782331e63c52c6f4d4b393cc855ad51d591a42eb92bd"
+    sha256 monterey:       "65789156fabd18cca7e9cb1c066ec65421d64a03df113a6eab823e796562dcae"
+    sha256 big_sur:        "9d8ffe8d05b87cefbe64b0b9d62d43448bae88bcbf4f2e7c35838c156937f6c7"
+    sha256 x86_64_linux:   "6e94a6cdec212be091712b251e5a30752b5922252b4d67ddec88cb5d0b38eca6"
   end
 
   depends_on "docbook-xsl" => :build
