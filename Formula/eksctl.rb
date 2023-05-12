3e27ffb861f40ac73ba5bf3a18b5b867af936bbd
Rui Chen <rui@chenrui.dev>
2023-05-12 07:21:19 -0400

    eksctl 0.141.0
    eksctl: update to use ifacemaker
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/eksctl.rb
+++ Formula/eksctl.rb
@@ -2,8 +2,8 @@ class Eksctl < Formula
   desc "Simple command-line tool for creating clusters on Amazon EKS"
   homepage "https://eksctl.io"
   url "https://github.com/weaveworks/eksctl.git",
-      tag:      "0.140.0",
-      revision: "94e5eaaba20ca38fc4dbea7f736a892eef48c4cd"
+      tag:      "0.141.0",
+      revision: "5c8318ed582031aaf4aab19caa388472245ab30f"
   license "Apache-2.0"
   head "https://github.com/weaveworks/eksctl.git", branch: "main"
 
@@ -20,25 +20,11 @@ class Eksctl < Formula
   depends_on "counterfeiter" => :build
   depends_on "go" => :build
   depends_on "go-bindata" => :build
+  depends_on "ifacemaker" => :build
   depends_on "mockery" => :build
   depends_on "aws-iam-authenticator"
 
-  # Eksctl requires newer version of ifacemaker
-  #
-  # Replace with `depends_on "ifacemaker" => :build` when ifacemaker > 1.2.0
-  # Until then get the resource version from go.mod
-  resource "ifacemaker" do
-    url "https://github.com/vburenin/ifacemaker/archive/b2018d8549dc4d51ce7e2254d6b0a743643613be.tar.gz"
-    sha256 "41888bf97133b4e7e190f2040378661b5bcab290d009e1098efbcb9db0f1d82f"
-  end
-
   def install
-    resource("ifacemaker").stage do
-      system "go", "build", *std_go_args(ldflags: "-s -w", output: buildpath/"ifacemaker")
-    end
-    inreplace "build/scripts/generate-aws-interfaces.sh", "${GOBIN}/ifacemaker",
-                                                          buildpath/"ifacemaker"
-
     ENV["GOBIN"] = HOMEBREW_PREFIX/"bin"
     ENV.deparallelize # Makefile prerequisites need to be run in order
     system "make", "build"
