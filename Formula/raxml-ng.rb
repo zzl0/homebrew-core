d24cdd607008daea36c41fadc852be9c6ab23172
Rui Chen <rui@chenrui.dev>
2023-05-12 11:15:43 -0400

    raxml-ng 1.2.0
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/raxml-ng.rb
+++ Formula/raxml-ng.rb
@@ -2,8 +2,8 @@ class RaxmlNg < Formula
   desc "RAxML Next Generation: faster, easier-to-use and more flexible"
   homepage "https://sco.h-its.org/exelixis/web/software/raxml/"
   url "https://github.com/amkozlov/raxml-ng.git",
-      tag:      "1.1.0",
-      revision: "9b8150852c21fd0caa764752797e17382fc03aa0"
+      tag:      "1.2.0",
+      revision: "fd32e7f73c3ee44c526c7555a8d04e84b03bd51c"
   license "AGPL-3.0-or-later"
 
   bottle do
@@ -18,25 +18,14 @@ class RaxmlNg < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "e1b99fec129377549ae701ce7eae61eb0289fe34f9e33d11c452c5bf0b1b584d"
   end
 
+  depends_on "bison" => :build # fix syntax error with `parse_utree.y`
   depends_on "cmake" => :build
   depends_on "gmp"
 
-  uses_from_macos "bison" => :build
   uses_from_macos "flex" => :build
 
   on_linux do
     depends_on "open-mpi"
-  end
-
-  # Backport ARM support. Remove in the next release.
-  # Ref: https://github.com/amkozlov/raxml-ng/commit/6a8f3d98ba0243b9f1452e0f7aab928e45d59b6f
-  on_arm do
-    patch :DATA
-    patch do
-      url "https://github.com/xflouris/libpll-2/commit/201983d128aa34e658d145e99ad775f441b42197.patch?full_index=1"
-      sha256 "6f14c55450567672aa7c2b82dcce19c6e00395f7f9b8ed7529a18b3030e70e16"
-      directory "libs/pll-modules/libs/libpll"
-    end
   end
 
   resource "homebrew-example" do
@@ -65,26 +54,3 @@ class RaxmlNg < Formula
     system bin/"raxml-ng", "--msa", "dna.phy", "--start", "--model", "GTR"
   end
 end
-
-__END__
-diff --git a/src/util/sysutil.cpp b/src/util/sysutil.cpp
-index 8d6501f..cdc4c72 100644
---- a/src/util/sysutil.cpp
-+++ b/src/util/sysutil.cpp
-@@ -1,4 +1,4 @@
--#ifndef _WIN32
-+#if (!defined(_WIN32) && !defined(__aarch64__))
- #include <cpuid.h>
- #endif
- #include <sys/time.h>
-@@ -170,7 +170,9 @@ unsigned long sysutil_get_memtotal(bool ignore_errors)
-
- static void get_cpuid(int32_t out[4], int32_t x)
- {
--#ifdef _WIN32
-+#ifdef __aarch64__
-+// not supported
-+#elif defined(_WIN32)
-   __cpuid(out, x);
- #else
-   __cpuid_count(x, 0, out[0], out[1], out[2], out[3]);
