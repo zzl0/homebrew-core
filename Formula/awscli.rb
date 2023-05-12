b9593b776b6bc5d03688dbf13c1056a36960802b
Porkepix <porkepix@gmail.com>
2023-05-12 21:35:32 +0200

    awscli 2.11.20

--- Formula/awscli.rb
+++ Formula/awscli.rb
@@ -3,8 +3,8 @@ class Awscli < Formula
 
   desc "Official Amazon AWS command-line interface"
   homepage "https://aws.amazon.com/cli/"
-  url "https://github.com/aws/aws-cli/archive/2.11.19.tar.gz"
-  sha256 "68fdb9ed9473efd679ba8589eeaee33a2afb219736d49f7d229c4a4bc381ae79"
+  url "https://github.com/aws/aws-cli/archive/2.11.20.tar.gz"
+  sha256 "504dc94b9dc8bafb11c10bcf4dd25ce4e31d493de2e136caec4d18f4c87b5efa"
   license "Apache-2.0"
   head "https://github.com/aws/aws-cli.git", branch: "v2"
 
