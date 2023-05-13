024fd9473b44c23d38326ff13ca2c417918b4ab0
Rui Chen <rui@chenrui.dev>
2023-05-13 09:51:18 -0400

    jackett 0.20.4143

--- Formula/jackett.rb
+++ Formula/jackett.rb
@@ -1,8 +1,8 @@
 class Jackett < Formula
   desc "API Support for your favorite torrent trackers"
   homepage "https://github.com/Jackett/Jackett"
-  url "https://github.com/Jackett/Jackett/archive/refs/tags/v0.20.4141.tar.gz"
-  sha256 "6a081253261d2fbb4021ab0a524943f640aa803d905e2d108b79d15ddeaa7b1f"
+  url "https://github.com/Jackett/Jackett/archive/refs/tags/v0.20.4143.tar.gz"
+  sha256 "f7d97009c974a8f467ed811045ac73a3bd91d9a1f2d5180a9d576980862b8ae6"
   license "GPL-2.0-only"
   head "https://github.com/Jackett/Jackett.git", branch: "master"
 
