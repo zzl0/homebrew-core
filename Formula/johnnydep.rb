d226cc4757a71370b741bdbd1d92c4eac107df17
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-05-13 15:53:11 +0000

    johnnydep: update 1.20.0 bottle.

--- Formula/johnnydep.rb
+++ Formula/johnnydep.rb
@@ -8,13 +8,13 @@ class Johnnydep < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f444ac6b3b3e6234c048c8b68d37af3e44bb0c4c1f6282c016f1fc41de13715c"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c1a78cad6ca3de0b3032b862ac6f0c055e44dc2c7396ca8188b48e4f93503ed5"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b0455585d1d7134bcf14dee621d9c8750343c1c850003c971434527afa5b677b"
-    sha256 cellar: :any_skip_relocation, ventura:        "f66f6b947b62247ebf837ac8b246fcb5b23a541cc7c9fe8e027cee581a06e65c"
-    sha256 cellar: :any_skip_relocation, monterey:       "f62adef5e1b86d677eb83f2b59a7f0d84471b68886ddced2986058996c03f2bd"
-    sha256 cellar: :any_skip_relocation, big_sur:        "05a7a70e0678fbbdcdfd627a4efff1513d262d3cd6a6592c0ea19f2cfacf240b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c659887453b4d90fc117a106e91b254f0e69c116c98f3db2fbddea6b3fb8cd06"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "c39d49cfd39559b89bde176002de951d5e4f0d231504cbd26fd205da09e25881"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "6c014eb23a2c321e04f8cfa57b7cf9d419a026c6c8f466d6c46a4f5f431b1394"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9e25fd4d5a96d0093a28e5acf1c9addf37c437053b4b376101d709740c2d152c"
+    sha256 cellar: :any_skip_relocation, ventura:        "c05135eb4c57e530f37b5245a9c3a64c3e6a7b795561b6f4900a6888d4d88b3e"
+    sha256 cellar: :any_skip_relocation, monterey:       "66cd9f7532d36606d2ad00c05e6e23e622442def6ee40ebce0b7615b534382f1"
+    sha256 cellar: :any_skip_relocation, big_sur:        "461ebbd75d6853f6e0cd586e9e33785381991850e1c51dddeb3003fcc7219ad4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "28e0d5898cdc16fbdac1b435b2427fbb132b2acaa3bb2fb7f432989ab6330744"
   end
 
   depends_on "python-tabulate"
