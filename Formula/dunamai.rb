1248e7fa75069e2a93a62ef964799cf872dc9c24
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-05-13 17:05:41 +0000

    dunamai: update 1.16.1 bottle.

--- Formula/dunamai.rb
+++ Formula/dunamai.rb
@@ -8,13 +8,13 @@ class Dunamai < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "6c4204d358885f27d6de6ed77fd0336e704a2631e44967f976274d6e6576d08a"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "9273ab108881fe453076d86ca877cf65f5bad38a03c15705dc926ee871ac7564"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "384c8d969a95427b143d314634187c60ac28ea8bbd0d33722b68d4a6843d481f"
-    sha256 cellar: :any_skip_relocation, ventura:        "45fa238049a432348aad737ce70ae745f0ed2687623118cada00806a09128ad5"
-    sha256 cellar: :any_skip_relocation, monterey:       "d2055773827aec50e583881261f9c95309454e4de573abf693687b2690fa1358"
-    sha256 cellar: :any_skip_relocation, big_sur:        "e616d2f808ffdd3b64ad0f6dfb8e6cf0910fe108f4f86f3f12868e5b6480a9ca"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d2eb36512553e61968e063640abea85d294713fb74b67a45e8b7dfa88d321c73"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "7dcadbaa28d94a2d540668261dfbc850417acc8c26e118508e9dc06ea79a1f67"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "8e5b6d8bb7802d5bd9d7c5e114928d64b08bf2dba57cb0fedfbe7b34043449bd"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "dc3fc88e9e207f4d342ef74d6c31bee24981049963487a1b3c1dc0eb9ac7776d"
+    sha256 cellar: :any_skip_relocation, ventura:        "681029834e2879902cc6d4d2af9d374aa111b92a1cf796124fcd4896fa9551c9"
+    sha256 cellar: :any_skip_relocation, monterey:       "30f2e57bfeac493a87f0ccd6071caa832eb5b7ccc97c1ea4ab071af84c2c9adb"
+    sha256 cellar: :any_skip_relocation, big_sur:        "8a4573631255954d09fca3c9bcf3117d61ec5a7eba4034a3e4ed162f06f7ee9b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6546a822d7adecd6f1b7a0ac48f39cf87f6dc373fa26d76a1eee57569560bc23"
   end
 
   depends_on "python@3.11"
