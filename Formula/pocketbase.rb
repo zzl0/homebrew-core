67de5f4aab1b92a9b25dcad2e8a6b372611b07a2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-05-14 03:36:04 +0000

    pocketbase: add 0.15.2 bottle.

--- Formula/pocketbase.rb
+++ Formula/pocketbase.rb
@@ -5,6 +5,16 @@ class Pocketbase < Formula
   sha256 "abc524d9a4e9235e6b2d9d274df1a9fd987f17d42427f3b085f777c064a29fd9"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "6ba7fda122847e24fe667092030cfb835141b79d1e00cd3ce03f0043702f50c6"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "6ba7fda122847e24fe667092030cfb835141b79d1e00cd3ce03f0043702f50c6"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6ba7fda122847e24fe667092030cfb835141b79d1e00cd3ce03f0043702f50c6"
+    sha256 cellar: :any_skip_relocation, ventura:        "cb537aaa2a40a00586e03f4822c94d76c56d182317c4e22c2806951a1a4cea25"
+    sha256 cellar: :any_skip_relocation, monterey:       "cb537aaa2a40a00586e03f4822c94d76c56d182317c4e22c2806951a1a4cea25"
+    sha256 cellar: :any_skip_relocation, big_sur:        "cb537aaa2a40a00586e03f4822c94d76c56d182317c4e22c2806951a1a4cea25"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "19a5dea9256ee7d8a1a94f045d7ab3dabbc3f26f698ce5d8c7de069bc09c3df0"
+  end
+
   depends_on "go" => :build
 
   def install
