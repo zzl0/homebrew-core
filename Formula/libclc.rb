7fafec192b485ea526777ae70f3f4382a7dfe6cb
PaperChalice <example@example.com>
2023-05-14 20:29:15 +0800

    libclc 16.0.3 (new formula)

---
+++ Formula/libclc.rb
@@ -0,0 +1,39 @@
+class Libclc < Formula
+  desc "Implementation of the library requirements of the OpenCL C programming language"
+  homepage "https://libclc.llvm.org/"
+  url "https://github.com/llvm/llvm-project/releases/download/llvmorg-16.0.3/libclc-16.0.3.src.tar.xz"
+  sha256 "b8f11158cd298062602285391496bb2acaf7219923fdd13b93927753c491e7c1"
+  license "Apache-2.0" => { with: "LLVM-exception" }
+
+  depends_on "cmake" => :build
+  depends_on "llvm" => [:build, :test]
+  depends_on "spirv-llvm-translator" => :build
+
+  def install
+    llvm_spirv = Formula["spirv-llvm-translator"].opt_bin/"llvm-spirv"
+    system "cmake", "-S", ".", "-B", "build",
+                    "-DLLVM_SPIRV=#{llvm_spirv}",
+                    *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    clang_args = %W[
+      -target nvptx--nvidiacl
+      -c -emit-llvm
+      -Xclang -mlink-bitcode-file
+      -Xclang #{share}/clc/nvptx--nvidiacl.bc
+    ]
+    llvm_bin = Formula["llvm"].opt_bin
+
+    (testpath/"add_sat.cl").write <<~EOS
+      __kernel void foo(__global char *a, __global char *b, __global char *c) {
+        *a = add_sat(*b, *c);
+      }
+    EOS
+
+    system llvm_bin/"clang", *clang_args, "./add_sat.cl"
+    assert_match "@llvm.sadd.sat.i8", shell_output("#{llvm_bin}/llvm-dis ./add_sat.bc -o -")
+  end
+end
