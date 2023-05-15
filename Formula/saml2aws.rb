22b38ed26ca38e02d5b45e1d93e1691c5bdbd0a7
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-05-15 08:39:27 +0800

    saml2aws 2.36.8

--- Formula/saml2aws.rb
+++ Formula/saml2aws.rb
@@ -2,8 +2,8 @@ class Saml2aws < Formula
   desc "Login and retrieve AWS temporary credentials using a SAML IDP"
   homepage "https://github.com/Versent/saml2aws"
   url "https://github.com/Versent/saml2aws.git",
-      tag:      "v2.36.7",
-      revision: "68d09f20dfde6bc617f1820850599b005a01c034"
+      tag:      "v2.36.8",
+      revision: "be18355f2fecf98ccd985e3d543f9ca30a948ab2"
   license "MIT"
 
   bottle do
