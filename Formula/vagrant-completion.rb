41b584977acf8fa70664b1e84f1bae388bb54d64
Patrick Linnane <patrick@linnane.io>
2023-05-15 22:34:42 -0700

    vagrant-completion 2.3.5

--- Formula/vagrant-completion.rb
+++ Formula/vagrant-completion.rb
@@ -1,8 +1,8 @@
 class VagrantCompletion < Formula
   desc "Bash completion for Vagrant"
   homepage "https://github.com/hashicorp/vagrant"
-  url "https://github.com/hashicorp/vagrant/archive/v2.3.4.tar.gz"
-  sha256 "43eb1461c6dcfd23a0c386570e6c2a876e06d2388bbc0f1f0c9c99e393aa2f0f"
+  url "https://github.com/hashicorp/vagrant/archive/v2.3.5.tar.gz"
+  sha256 "34fce02219c67174457a6f8750063047f6c29b7148e425e4f3647cb80b8a5b2e"
   license "MIT"
   head "https://github.com/hashicorp/vagrant.git", branch: "main"
 
