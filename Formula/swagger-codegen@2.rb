b841728227e144687936abc80b2ea841c02d3362
Rui Chen <rui@chenrui.dev>
2023-05-17 17:06:32 -0400

    swagger-codegen@2 2.4.32

--- Formula/swagger-codegen@2.rb
+++ Formula/swagger-codegen@2.rb
@@ -1,8 +1,8 @@
 class SwaggerCodegenAT2 < Formula
   desc "Generate clients, server stubs, and docs from an OpenAPI spec"
   homepage "https://swagger.io/swagger-codegen/"
-  url "https://github.com/swagger-api/swagger-codegen/archive/v2.4.31.tar.gz"
-  sha256 "6baceedcfcb8b073f117bd44276f5ea378c0cda468efd7f0a49d740ff4b4a402"
+  url "https://github.com/swagger-api/swagger-codegen/archive/v2.4.32.tar.gz"
+  sha256 "6c09797b16e2d17be2592b8dac4d7ef65765eec540c41929a403df59bd9c9fae"
   license "Apache-2.0"
 
   livecheck do
