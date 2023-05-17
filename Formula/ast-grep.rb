9b178993bf8d9046004a40f484cb4278a6e6b225
Henry Chu <henrytech@outlook.com>
2023-05-17 13:15:33 +0800

    ast-grep 0.5.2 (new formula)

---
+++ Formula/ast-grep.rb
@@ -0,0 +1,18 @@
+class AstGrep < Formula
+  desc "Code searching, linting, rewriting"
+  homepage "https://github.com/ast-grep/ast-grep"
+  url "https://github.com/ast-grep/ast-grep/archive/refs/tags/v0.5.2.tar.gz"
+  sha256 "8226770c72c069132943cf136e23f7c703c8672fe6d84fb87cd5efe353b1ad2b"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args(path: "crates/cli")
+  end
+
+  test do
+    (testpath/"Hello.js").write("console.log('123')")
+    system "#{bin}/sg", "run", "-p console.log", (testpath/"Hello.js")
+  end
+end
