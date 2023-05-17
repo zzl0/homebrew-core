9cedf5ee94186645b375f74ce2295e1b1f1bab09
Patrick Linnane <patrick@linnane.io>
2023-05-16 21:20:19 -0700

    dpkg 1.21.22

--- Formula/dpkg.rb
+++ Formula/dpkg.rb
@@ -4,8 +4,8 @@ class Dpkg < Formula
   # Please use a mirror as the primary URL as the
   # dpkg site removes tarballs regularly which means we get issues
   # unnecessarily and older versions of the formula are broken.
-  url "https://deb.debian.org/debian/pool/main/d/dpkg/dpkg_1.21.21.tar.xz"
-  sha256 "985073817aa0512122f1a7e77598a6b0be168d6c71ac56a3383927eb0813e089"
+  url "https://deb.debian.org/debian/pool/main/d/dpkg/dpkg_1.21.22.tar.xz"
+  sha256 "5a1d15481bba79d7a4899fd55b4b6b18a987ca8d56ee8c43e9cab63b8a0a3545"
   license "GPL-2.0-only"
 
   livecheck do
