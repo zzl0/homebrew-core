3ea1235a2890fed6c817ba6b7f89cda65bbd2b6a
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2023-05-17 07:45:13 +0000

    curl 8.1.0

--- Formula/curl.rb
+++ Formula/curl.rb
@@ -1,11 +1,11 @@
 class Curl < Formula
   desc "Get a file from an HTTP, HTTPS or FTP server"
   homepage "https://curl.se"
-  url "https://curl.se/download/curl-8.0.1.tar.bz2"
-  mirror "https://github.com/curl/curl/releases/download/curl-8_0_1/curl-8.0.1.tar.bz2"
-  mirror "http://fresh-center.net/linux/www/curl-8.0.1.tar.bz2"
-  mirror "http://fresh-center.net/linux/www/legacy/curl-8.0.1.tar.bz2"
-  sha256 "9b6b1e96b748d04b968786b6bdf407aa5c75ab53a3d37c1c8c81cdb736555ccf"
+  url "https://curl.se/download/curl-8.1.0.tar.bz2"
+  mirror "https://github.com/curl/curl/releases/download/curl-8_0_1/curl-8.1.0.tar.bz2"
+  mirror "http://fresh-center.net/linux/www/curl-8.1.0.tar.bz2"
+  mirror "http://fresh-center.net/linux/www/legacy/curl-8.1.0.tar.bz2"
+  sha256 "8439f39f0f5dd41f399cf60f3f6f5c3e47a4a41c96f99d991b77cecb921c553b"
   license "curl"
 
   livecheck do
