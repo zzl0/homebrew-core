5de45802224ca1919c766ab1a0fb484a9e9304f5
iivusly <52052910+iivusly@users.noreply.github.com>
2023-05-17 11:27:06 -0700

    roblox-ts 2.1.0 (new formula)
    * roblox-ts 2.1.0 (new formula)
      remove comment
    
      Replace universal binaries with native slices
    
      Update Formula/roblox-ts.rb
    * roblox-ts: improve test
    
    Closes #131242.
    
    Signed-off-by: iivusly <52052910+iivusly@users.noreply.github.com>
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/roblox-ts.rb
@@ -0,0 +1,23 @@
+require "language/node"
+
+class RobloxTs < Formula
+  desc "TypeScript-to-Luau Compiler for Roblox"
+  homepage "https://roblox-ts.com/"
+  url "https://registry.npmjs.org/roblox-ts/-/roblox-ts-2.1.0.tgz"
+  sha256 "cfe33e88a9973fe39543b01f945533bc392df9771915422265f9164b3d42f860"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+    deuniversalize_machos
+  end
+
+  test do
+    system bin/"rbxtsc", "init", "game", "-y"
+    assert_predicate testpath/"package.json", :exist?
+    assert_predicate testpath/"package-lock.json", :exist?
+  end
+end
