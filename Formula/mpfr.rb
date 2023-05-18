c265d423b5283f826ea3844714023922c180c197
Rui Chen <rui@chenrui.dev>
2023-05-18 13:10:21 -0400

    mpfr 4.2.0-p9
    Co-authored-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/mpfr.rb
+++ Formula/mpfr.rb
@@ -7,7 +7,7 @@ class Mpfr < Formula
     url "https://ftp.gnu.org/gnu/mpfr/mpfr-4.2.0.tar.xz"
     mirror "https://ftpmirror.gnu.org/mpfr/mpfr-4.2.0.tar.xz"
     sha256 "06a378df13501248c1b2db5aa977a2c8126ae849a9d9b7be2546fb4a9c26d993"
-    version "4.2.0-p7"
+    version "4.2.0-p9"
 
     # Upstream patches, list at https://www.mpfr.org/mpfr-current/#fixed
     %w[
@@ -18,6 +18,8 @@ class Mpfr < Formula
       05 c4144564097a1be89c9cc2e7ee255c9fe59eb1b94a17c9d4a08169223e705ac1
       06 70456748a8072265ba103d93ba94e9f93ae64565e6a5742194c56030086540fa
       07 472386aa5f8c51fbdf60154c19268ce2212be03e1c2f9004c1673b6c270508f6
+      08 6ecd3bd2edf178f4ede4be612964d1b2d0a0bb10ad6f8c51d1a8011fff87d5ea
+      09 3e9aed5bcea95d34d0bd179a61cd7acb712c89c9a745535f18f0ef619833ba3b
     ].each_slice(2) do |p, checksum|
       patch do
         url "https://www.mpfr.org/mpfr-4.2.0/patch#{p}"
