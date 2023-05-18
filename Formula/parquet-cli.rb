25e5759f2f30d8a47ed63227544b3939ddb3c34f
Porkepix <porkepix@gmail.com>
2023-05-18 15:36:52 +0200

    parquet-cli 1.13.1
    parquet-cli: specify test resource
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/parquet-cli.rb
+++ Formula/parquet-cli.rb
@@ -2,8 +2,8 @@ class ParquetCli < Formula
   desc "Apache Parquet command-line tools and utilities"
   homepage "https://parquet.apache.org/"
   url "https://github.com/apache/parquet-mr.git",
-      tag:      "apache-parquet-1.13.0",
-      revision: "2e369ed173f66f057c296e63c1bc31d77f294f41"
+      tag:      "apache-parquet-1.13.1",
+      revision: "db4183109d5b734ec5930d870cdae161e408ddba"
   license "Apache-2.0"
   head "https://github.com/apache/parquet-mr.git", branch: "master"
 
@@ -26,7 +26,7 @@ class ParquetCli < Formula
 
   # This file generated with `red-parquet` gem:
   #   Arrow::Table.new("values" => ["foo", "Homebrew", "bar"]).save("homebrew.parquet")
-  resource("test-parquet") do
+  resource("homebrew-test-parquet") do
     url "https://gist.github.com/bayandin/2144b5fc6052153c1a33fd2679d50d95/raw/7d793910a1afd75ee4677f8c327491f7bdd2256b/homebrew.parquet"
     sha256 "5caf572cb0df5ce9d6893609de82d2369b42c3c81c611847b6f921d912040118"
   end
@@ -46,7 +46,7 @@ class ParquetCli < Formula
   end
 
   test do
-    resource("test-parquet").stage testpath
+    resource("homebrew-test-parquet").stage testpath
 
     output = shell_output("#{bin}/parquet cat #{testpath}/homebrew.parquet")
     assert_match "{\"values\": \"Homebrew\"}", output
