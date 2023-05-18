dd6c2917caed039e5906e4980c97e88b6dfeab35
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-05-18 18:10:42 +0000

    service-weaver: update 0.11.0 bottle.

--- Formula/service-weaver.rb
+++ Formula/service-weaver.rb
@@ -14,13 +14,13 @@ class ServiceWeaver < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "b7988e70914da1992dd9459302abcd48e92809d923014ca6288e4a95cf5b5e87"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f83797783462be32b51d2dbbaa0659f943030375e012543adba0fbb9edf13e9c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "244533458ba847a5f3f8961c55dda4a5b9a70e8a57e63ee89336e3a775a0d95a"
-    sha256 cellar: :any_skip_relocation, ventura:        "6a69f043627c8995da9b2792cc63f3edefb4b25c03b448fa8459c1a80fb742da"
-    sha256 cellar: :any_skip_relocation, monterey:       "79d2368211290fdc90dbe5237ea4962937996e4894f5cea5370e6252ab59c912"
-    sha256 cellar: :any_skip_relocation, big_sur:        "9eb64dbdbcada9d5f6555906a2ad752075f976a44136f46d239ad004003392ce"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "42c4b26fa6c7efe563b9790f22e5bf0a08c75d2b2cfb9327bd69e22be77c469e"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0b713fe3100a1b5403484586b789c1e7df9146592a25969b87f69038e36deec0"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f3c2e2bbb0879569ebe37fe35491b699c865178f5c790603f7284b3278f887e6"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "33534e3bb688fa72239ea660f163b75264d7ca4ef9ad09dbb737f9abc70dab62"
+    sha256 cellar: :any_skip_relocation, ventura:        "82d5188852159adb4339ec67b0152dde164222a15b20bd35ab1e23f601fba39c"
+    sha256 cellar: :any_skip_relocation, monterey:       "77cc4da78d7d578dbbd47cee06a18a6d6e51c0debce5723d07d282fc9862ff6b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "821fe08cd58cc3cddb67ffcf1828ed5ebf4b87daaf881d4eac9cf4fee8efb910"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f7fa9b60653242185d345e66c7026c0ceabbbb095a1e135e92281e56386cc64b"
   end
 
   head do
