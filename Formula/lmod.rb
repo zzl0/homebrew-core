f83e15b6d92d80503d827d95404501f25eb1656d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-05-19 00:20:24 +0000

    lmod: update 8.7.25 bottle.

--- Formula/lmod.rb
+++ Formula/lmod.rb
@@ -6,13 +6,13 @@ class Lmod < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8dae24c319faceacb8a65f35b1c7473db2e7cad21e3080e0bbc70bfbcc588b14"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0e403c0c64b28fac36aeed6b208474e2c3f853dff0bc9dfdc42b3a3746241e57"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "24ffa57144ff3bb9ed206084d0ba63fceca953afb8278be426433a7faf251af1"
-    sha256 cellar: :any_skip_relocation, ventura:        "4c96f3087fa6be3a2364b2d0e31fd00cc53af76f88a26bf33e8b233e445c1627"
-    sha256 cellar: :any_skip_relocation, monterey:       "d965c95cb392dea0b9a0424fb7db1a725a5866fbbcd4ade9c854e2aca9b9f89c"
-    sha256 cellar: :any_skip_relocation, big_sur:        "5ffa637a61be8ef548c1ec752a4e34038c26264749e9c4f634836fcd28fb076c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0514bd24c23fa05fb0570869553eef1314c6d2312d5b6bff62db7b6fcebacf77"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "b510ec691edd461c404c05cad7222f5c934ec65d33d1dc01937e80081e04bf3d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "7e59740135f3c7d8377d7bbaff83ad788a5cbdf64eb80742735231219aef4dc1"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "37563a8fae54fbedb56e5a53d4befe9d74196aa7688f1aeb3656253adfbbd450"
+    sha256 cellar: :any_skip_relocation, ventura:        "b97b9004a9040066af5859feddbe5e5636046ab1395a8fdd890006ce8b93306a"
+    sha256 cellar: :any_skip_relocation, monterey:       "98f1c2e930b0d77e294749cd5b76c09f73de66492fbd0e1b94e47dab6aa557f4"
+    sha256 cellar: :any_skip_relocation, big_sur:        "6e38bf13e33a75756364d9d85d1dcc9847b1786eeb0e50d6593ede1ce38aa03c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "47288a97c125a0a649bae20017c0e24717c8930a4d66a2c2c965790c7e78123a"
   end
 
   depends_on "luarocks" => :build
