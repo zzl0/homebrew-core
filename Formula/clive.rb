59351c49bd72923346086a04c999684ab7058f3a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-05-19 12:52:13 +0000

    clive: add 0.12.5 bottle.

--- Formula/clive.rb
+++ Formula/clive.rb
@@ -5,6 +5,16 @@ class Clive < Formula
   sha256 "eb4c32ae67cfb8c8b76733fec34047718cdf2d2682125480d478b5a313b04a32"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "905bab7ac96d636a6d46c458a42bf4348c2c8c2d2334c6c50c6aa560deeec9e0"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "905bab7ac96d636a6d46c458a42bf4348c2c8c2d2334c6c50c6aa560deeec9e0"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "905bab7ac96d636a6d46c458a42bf4348c2c8c2d2334c6c50c6aa560deeec9e0"
+    sha256 cellar: :any_skip_relocation, ventura:        "5f79bdc37333481124bdaf626c37a8d91907f3ea8249f7130292ad8fcbb695b4"
+    sha256 cellar: :any_skip_relocation, monterey:       "5f79bdc37333481124bdaf626c37a8d91907f3ea8249f7130292ad8fcbb695b4"
+    sha256 cellar: :any_skip_relocation, big_sur:        "5f79bdc37333481124bdaf626c37a8d91907f3ea8249f7130292ad8fcbb695b4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7502d6977cb4822020ba8d40de5a9b3959817f4c8ce16be436f21a5205f7bb71"
+  end
+
   depends_on "go" => :build
   depends_on "ttyd"
 
