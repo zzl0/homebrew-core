d3178ee20a35a80882594e712af6474bb39d69fa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-05-20 18:41:23 +0000

    roblox-ts: add 2.1.0 bottle.

--- Formula/roblox-ts.rb
+++ Formula/roblox-ts.rb
@@ -7,6 +7,16 @@ class RobloxTs < Formula
   sha256 "cfe33e88a9973fe39543b01f945533bc392df9771915422265f9164b3d42f860"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "603722d00d6cdd36513f59dade2a1ddf66036b1409e70950e4afe1b22c0c12af"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "603722d00d6cdd36513f59dade2a1ddf66036b1409e70950e4afe1b22c0c12af"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "603722d00d6cdd36513f59dade2a1ddf66036b1409e70950e4afe1b22c0c12af"
+    sha256 cellar: :any_skip_relocation, ventura:        "6368f79fa4e8203b148f68eae071b19de99c657349257eacae8a2d82ce22cdb6"
+    sha256 cellar: :any_skip_relocation, monterey:       "6368f79fa4e8203b148f68eae071b19de99c657349257eacae8a2d82ce22cdb6"
+    sha256 cellar: :any_skip_relocation, big_sur:        "6368f79fa4e8203b148f68eae071b19de99c657349257eacae8a2d82ce22cdb6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6407b06d0f066e4649e0fe06d0f753650d105e147434125308078a07429048f8"
+  end
+
   depends_on "node"
 
   def install
