c9d65cb4731b91cb778b360d75fc3fe79edee18b
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2023-05-20 22:00:06 +0800

    bloaty: switch to `protobuf@21`

--- Formula/bloaty.rb
+++ Formula/bloaty.rb
@@ -4,7 +4,7 @@ class Bloaty < Formula
   url "https://github.com/google/bloaty/releases/download/v1.1/bloaty-1.1.tar.bz2"
   sha256 "a308d8369d5812aba45982e55e7c3db2ea4780b7496a5455792fb3dcba9abd6f"
   license "Apache-2.0"
-  revision 10
+  revision 11
 
   bottle do
     sha256 cellar: :any,                 arm64_ventura:  "485449be09072bf5044e7a8252cf4385d3f1b4365a7907fb5cea4646dbfae409"
@@ -19,7 +19,7 @@ class Bloaty < Formula
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "capstone"
-  depends_on "protobuf"
+  depends_on "protobuf@21"
   depends_on "re2"
 
   def install
