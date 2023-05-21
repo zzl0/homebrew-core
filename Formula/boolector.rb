e9c65467ec90f3e26fafd22cfea780d270a1b632
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-05-21 11:56:47 +0000

    boolector: add 3.2.2 bottle.

--- Formula/boolector.rb
+++ Formula/boolector.rb
@@ -5,6 +5,16 @@ class Boolector < Formula
   sha256 "9a5bdbacf83f2dd81dbed1e1a9f923766807470afa29b73729c947ae769d42b9"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "cb8385667d296041aabcc2aaef2d7568db50906aba76e0f7dc6f99edf81e0232"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "6301f56a8dde1141da48935c21c3854dcf9011eccc26caae953f46b6c63bd66d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e691ec93a807fe0498ef1eb9ab85e13814512fadafc364894411e24a50d3da0c"
+    sha256 cellar: :any_skip_relocation, ventura:        "c4ca654c705345f47ba8bff6f66fcf5ab4731b91fd9cd25d3b9721e4d8477d84"
+    sha256 cellar: :any_skip_relocation, monterey:       "c074d7ff389aa3f973e132e3e7bb10cfc0f7748a74d3092c3ce817674b4331e7"
+    sha256 cellar: :any_skip_relocation, big_sur:        "713adc03b74133ec2c7111903965a9d185be406f00766041b5c740271083f3bf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "64395e0ecc2ca0893266743ffee7deeed87016d434674787c8e67bce5db35f4e"
+  end
+
   depends_on "cmake" => :build
 
   # Use commit hash from `contrib/setup-lingeling.sh`
