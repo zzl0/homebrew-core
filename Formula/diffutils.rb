b6921486c6582128d1c92fe23260e5e0257b8c00
Porkepix <porkepix@gmail.com>
2023-05-21 18:19:14 +0200

    diffutils 3.10

--- Formula/diffutils.rb
+++ Formula/diffutils.rb
@@ -1,9 +1,9 @@
 class Diffutils < Formula
   desc "File comparison utilities"
   homepage "https://www.gnu.org/s/diffutils/"
-  url "https://ftp.gnu.org/gnu/diffutils/diffutils-3.9.tar.xz"
-  mirror "https://ftpmirror.gnu.org/diffutils/diffutils-3.9.tar.xz"
-  sha256 "d80d3be90a201868de83d78dad3413ad88160cc53bcc36eb9eaf7c20dbf023f1"
+  url "https://ftp.gnu.org/gnu/diffutils/diffutils-3.10.tar.xz"
+  mirror "https://ftpmirror.gnu.org/diffutils/diffutils-3.10.tar.xz"
+  sha256 "90e5e93cc724e4ebe12ede80df1634063c7a855692685919bfe60b556c9bd09e"
   license "GPL-3.0-or-later"
 
   bottle do
