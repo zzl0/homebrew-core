8aca88c451fc6ead5333ba16402cea58299b3eec
Bevan Kay <email@bevankay.me>
2023-05-22 22:14:36 +1000

    jackett 0.21.14

--- Formula/jackett.rb
+++ Formula/jackett.rb
@@ -1,8 +1,8 @@
 class Jackett < Formula
   desc "API Support for your favorite torrent trackers"
   homepage "https://github.com/Jackett/Jackett"
-  url "https://github.com/Jackett/Jackett/archive/refs/tags/v0.20.4207.tar.gz"
-  sha256 "b714a4c1372df5206e42df39fcf10f921820106aa982424533809194679720a9"
+  url "https://github.com/Jackett/Jackett/archive/refs/tags/v0.21.14.tar.gz"
+  sha256 "22533a1805ff19e20d15a7774c8fc0e8306db16709d047e4cb9dbd9b7e4e3e0f"
   license "GPL-2.0-only"
   head "https://github.com/Jackett/Jackett.git", branch: "master"
 
