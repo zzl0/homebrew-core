f95a002b27cbef3c1a89fc87b2a91564bda7e245
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-05-22 11:20:46 +0800

    byacc 20230521

--- Formula/byacc.rb
+++ Formula/byacc.rb
@@ -1,8 +1,8 @@
 class Byacc < Formula
   desc "(Arguably) the best yacc variant"
   homepage "https://invisible-island.net/byacc/"
-  url "https://invisible-mirror.net/archives/byacc/byacc-20230219.tgz"
-  sha256 "36b972a6d4ae97584dd186925fbbc397d26cb20632a76c2f52ac7653cd081b58"
+  url "https://invisible-mirror.net/archives/byacc/byacc-20230521.tgz"
+  sha256 "5ad915a7d5833aa38a5e31bd077505666029c35e365dff8569fe4598eaa9fef2"
   license :public_domain
 
   livecheck do
