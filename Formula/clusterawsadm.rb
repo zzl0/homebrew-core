87984bb7ede8d65b6d58ed497be0fd274ca503fc
Rui Chen <rui@chenrui.dev>
2023-05-22 09:28:10 -0400

    clusterawsadm 2.1.2

--- Formula/clusterawsadm.rb
+++ Formula/clusterawsadm.rb
@@ -2,8 +2,8 @@ class Clusterawsadm < Formula
   desc "Home for bootstrapping, AMI, EKS, and other helpers in Cluster API Provider AWS"
   homepage "https://cluster-api-aws.sigs.k8s.io/clusterawsadm/clusterawsadm.html"
   url "https://github.com/kubernetes-sigs/cluster-api-provider-aws.git",
-      tag:      "v2.1.1",
-      revision: "b22f9d9bc0b5f904102e562ba033566393a11bd9"
+      tag:      "v2.1.2",
+      revision: "4c6b62ff6f8094d5ab0b0b842d788e26fd84bc8a"
   license "Apache-2.0"
   head "https://github.com/kubernetes-sigs/cluster-api-provider-aws.git", branch: "main"
 
