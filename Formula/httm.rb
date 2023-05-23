e2f5c072036be17050d66fc3b38e5470c15733dd
Rui Chen <rui@chenrui.dev>
2023-05-23 08:17:43 -0400

    httm 0.26.6

--- Formula/httm.rb
+++ Formula/httm.rb
@@ -1,8 +1,8 @@
 class Httm < Formula
   desc "Interactive, file-level Time Machine-like tool for ZFS/btrfs"
   homepage "https://github.com/kimono-koans/httm"
-  url "https://github.com/kimono-koans/httm/archive/refs/tags/0.26.5.tar.gz"
-  sha256 "5f841dbd8c10763ddc9651a90070a7d26ad2f3ffd520fa63b9e975c81801d8dc"
+  url "https://github.com/kimono-koans/httm/archive/refs/tags/0.26.6.tar.gz"
+  sha256 "13eb514339e888c0cfd5e56435a2c5662ef93c089a134de671b9b6581b4e78f1"
   license "MPL-2.0"
   head "https://github.com/kimono-koans/httm.git", branch: "master"
 
