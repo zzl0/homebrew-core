0310a42aa9e332cede01757e235313cf034fb777
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-05-23 22:59:03 +0000

    wildfly-as: update 28.0.1 bottle.

--- Formula/wildfly-as.rb
+++ Formula/wildfly-as.rb
@@ -11,12 +11,12 @@ class WildflyAs < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_ventura:  "e2e3788c11c9cd8b00ccac090920e6bd3e0898cf9a3d882ce5b3e9794ae71532"
-    sha256 cellar: :any, arm64_monterey: "e2e3788c11c9cd8b00ccac090920e6bd3e0898cf9a3d882ce5b3e9794ae71532"
-    sha256 cellar: :any, arm64_big_sur:  "e2e3788c11c9cd8b00ccac090920e6bd3e0898cf9a3d882ce5b3e9794ae71532"
-    sha256 cellar: :any, ventura:        "b75417c171458f69c15981e31470785cbc7c348f2f202fa2ee08dc841fae3024"
-    sha256 cellar: :any, monterey:       "b75417c171458f69c15981e31470785cbc7c348f2f202fa2ee08dc841fae3024"
-    sha256 cellar: :any, big_sur:        "b75417c171458f69c15981e31470785cbc7c348f2f202fa2ee08dc841fae3024"
+    sha256 cellar: :any, arm64_ventura:  "89984e80061d0973be062542db8ab821f2985bd2eb895ae3fe5cdbd2a8220283"
+    sha256 cellar: :any, arm64_monterey: "89984e80061d0973be062542db8ab821f2985bd2eb895ae3fe5cdbd2a8220283"
+    sha256 cellar: :any, arm64_big_sur:  "89984e80061d0973be062542db8ab821f2985bd2eb895ae3fe5cdbd2a8220283"
+    sha256 cellar: :any, ventura:        "dd476c610cb14a5457f0e2152b86b7530cb8a3037f9fecf553d802b749bf2790"
+    sha256 cellar: :any, monterey:       "dd476c610cb14a5457f0e2152b86b7530cb8a3037f9fecf553d802b749bf2790"
+    sha256 cellar: :any, big_sur:        "dd476c610cb14a5457f0e2152b86b7530cb8a3037f9fecf553d802b749bf2790"
   end
 
   # Installs a pre-built `libartemis-native-64.so` file with linkage to libaio.so.1
