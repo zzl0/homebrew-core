b539b869e27b680a3efcfcd25788110c4225b3e0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-05-24 05:28:33 +0000

    urlwatch: update 2.28 bottle.

--- Formula/urlwatch.rb
+++ Formula/urlwatch.rb
@@ -8,13 +8,14 @@ class Urlwatch < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "be44c7ecce9775e9b6952ae0de3ebb8738da84249e039bc37fbed3fa4a9154c8"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "564d864de9c5cb290e1c01ac668ecdafe5b8abfda3f2cb5c669dcf36051441eb"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c6287ae0c724be77661e48719f373cd388c9c5beddf3f7f5342edcbadd5bd3a0"
-    sha256 cellar: :any_skip_relocation, ventura:        "8c11f7b949f82f998a5be622c114d0957d84a74496267f52077903839002e7a2"
-    sha256 cellar: :any_skip_relocation, monterey:       "45d8d0353089252a6f7d1c1f2c92dd410be89aef8747bd0ce60e5fb19db94b78"
-    sha256 cellar: :any_skip_relocation, big_sur:        "8a9b0056c4e62ee5e7c1458e97fd9ba8819a7f82c9060cc747d8d6824cad5b41"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c7ea3927179e5af2b77bd1957f4e58215f58411743d01d3fd71bc9c08cb6bdb5"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "2f9fab61b7d67e4a706478c96e7ca1d5ec5f15cc9d1736afb50b0c428fa27a16"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "4021af7d4b5f22f15cd8a02a27af4413cd5b3b309981577907730499b6d71bea"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fa3b4f3c4dc3a26c1f85581acb6361a55e00f7687ca0a7c0ff31c196ae0a1c2d"
+    sha256 cellar: :any_skip_relocation, ventura:        "97c0e63626e0f4fdd99587587c50ef5047ba2be29920d228c3d13d299a7c5e35"
+    sha256 cellar: :any_skip_relocation, monterey:       "25342b1bf481da0a44bfad38209b6cd400ffe3596dea88c64987efdf50256888"
+    sha256 cellar: :any_skip_relocation, big_sur:        "92b2bac4d1a1835022af88819927d4e8f318e3d73ff9a0a523106bce8d5a1cf7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1690daba3be0770bdc70d97ef22b999e755639355afa51737c55673d2a90d8eb"
   end
 
   depends_on "python@3.11"
