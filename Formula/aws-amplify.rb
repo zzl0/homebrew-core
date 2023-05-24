0aa3c1c68deab943df3d03b11aad0e40d3f04605
helbing <helbingxxx@gmail.com>
2023-05-25 03:44:27 +0800

    aws-amplify 12.0.0 (new formula)
    aws-amplify: update build and test
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    aws-amplify: fix build
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    aws-amplify: fix style issue
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/aws-amplify.rb
@@ -0,0 +1,50 @@
+require "language/node"
+
+class AwsAmplify < Formula
+  desc "Build full-stack web and mobile apps in hours. Easy to start, easy to scale"
+  homepage "https://aws.amazon.com/amplify"
+  url "https://registry.npmjs.org/@aws-amplify/cli-internal/-/cli-internal-12.0.0.tgz"
+  sha256 "3bad1aad93f0e226cdeed8d742d912a0d149abc0618a4258acb8549205092310"
+  license "Apache-2.0"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+
+    unless Hardware::CPU.intel?
+      rm_rf "#{libexec}/lib/node_modules/@aws-amplify/cli-internal/node_modules" \
+            "/@aws-amplify/amplify-frontend-ios/resources/amplify-xcode"
+    end
+
+    # Remove non-native libsqlite4java files
+    os = OS.kernel_name.downcase
+    if Hardware::CPU.intel?
+      arch = if OS.mac?
+        "x86_64"
+      else
+        "amd64"
+      end
+    elsif OS.mac? # apple silicon
+      arch = "aarch64"
+    end
+    node_modules = libexec/"lib/node_modules/@aws-amplify/cli-internal/node_modules"
+    (node_modules/"amplify-dynamodb-simulator/emulator/DynamoDBLocal_lib").glob("libsqlite4java-*").each do |f|
+      rm f if f.basename.to_s != "libsqlite4java-#{os}-#{arch}"
+    end
+
+    # Replace universal binaries with native slices
+    deuniversalize_machos node_modules/"fsevents/fsevents.node"
+  end
+
+  test do
+    require "open3"
+
+    Open3.popen3(bin/"amplify", "status", "2>&1") do |_, stdout, _|
+      assert_match "No Amplify backend project files detected within this folder.", stdout.read
+    end
+
+    assert_match version.to_s, shell_output(bin/"amplify version")
+  end
+end
