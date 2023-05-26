159edfeb5180f70c55ebed1d73bec0ce3d594fe2
Rui Chen <rui@chenrui.dev>
2023-05-26 17:14:47 -0400

    melange 0.3.2 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/melange.rb
@@ -0,0 +1,72 @@
+class Melange < Formula
+  desc "Build APKs from source code"
+  homepage "https://github.com/chainguard-dev/melange"
+  url "https://github.com/chainguard-dev/melange/archive/refs/tags/v0.3.2.tar.gz"
+  sha256 "e095a6572a7b5449e1105566d874ed82af321fd35e684d6fd055445ce3f17ef0"
+  license "Apache-2.0"
+  head "https://github.com/chainguard-dev/melange.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X sigs.k8s.io/release-utils/version.gitVersion=#{version}
+      -X sigs.k8s.io/release-utils/version.gitCommit=brew
+      -X sigs.k8s.io/release-utils/version.gitTreeState=clean
+      -X sigs.k8s.io/release-utils/version.buildDate=#{time.iso8601}
+    ]
+    system "go", "build", *std_go_args(ldflags: ldflags)
+
+    generate_completions_from_executable(bin/"melange", "completion")
+  end
+
+  test do
+    (testpath/"test.yml").write <<~EOS
+      package:
+        name: hello
+        version: 2.12
+        epoch: 0
+        description: "the GNU hello world program"
+        copyright:
+          - paths:
+            - "*"
+            attestation: |
+              Copyright 1992, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2005,
+              2006, 2007, 2008, 2010, 2011, 2013, 2014, 2022 Free Software Foundation,
+              Inc.
+            license: GPL-3.0-or-later
+        dependencies:
+          runtime:
+
+      environment:
+        contents:
+          repositories:
+            - https://dl-cdn.alpinelinux.org/alpine/edge/main
+          packages:
+            - alpine-baselayout-data
+            - busybox
+            - build-base
+            - scanelf
+            - ssl_client
+            - ca-certificates-bundle
+
+      pipeline:
+        - uses: fetch
+          with:
+            uri: https://ftp.gnu.org/gnu/hello/hello-${{package.version}}.tar.gz
+            expected-sha256: cf04af86dc085268c5f4470fbae49b18afbc221b78096aab842d934a76bad0ab
+        - uses: autoconf/configure
+        - uses: autoconf/make
+        - uses: autoconf/make-install
+        - uses: strip
+    EOS
+
+    assert_equal "hello-2.12-r0", shell_output("#{bin}/melange package-version #{testpath}/test.yml")
+
+    system bin/"melange", "keygen"
+    assert_predicate testpath/"melange.rsa", :exist?
+
+    assert_match version.to_s, shell_output(bin/"melange version 2>&1")
+  end
+end
