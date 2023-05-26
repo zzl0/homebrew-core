2ddfede7d14f634ff02a043539001fedd623126e
Rui Chen <rui@chenrui.dev>
2023-05-25 20:27:04 -0400

    flyctl 0.1.17

--- Formula/flyctl.rb
+++ Formula/flyctl.rb
@@ -2,8 +2,8 @@ class Flyctl < Formula
   desc "Command-line tools for fly.io services"
   homepage "https://fly.io"
   url "https://github.com/superfly/flyctl.git",
-      tag:      "v0.1.16",
-      revision: "2b23fd76a0fff98d462e31b2e1f787dcb1244ce8"
+      tag:      "v0.1.17",
+      revision: "78070840ec8911b34bb9bb7b66df9a92642a2304"
   license "Apache-2.0"
   head "https://github.com/superfly/flyctl.git", branch: "master"
 
