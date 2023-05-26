d1f4d14682fef96beb0e2d1c0fcb9aabf84e95eb
Rui Chen <rui@chenrui.dev>
2023-05-26 17:03:45 -0400

    apko 0.8.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/apko.rb
@@ -0,0 +1,44 @@
+class Apko < Formula
+  desc "Build OCI images from APK packages directly without Dockerfile"
+  homepage "https://github.com/chainguard-dev/apko"
+  url "https://github.com/chainguard-dev/apko/archive/refs/tags/v0.8.0.tar.gz"
+  sha256 "e990b5bbaf5099de226cb8ea77717982f74d0554a286595db0344d4c31173623"
+  license "Apache-2.0"
+  head "https://github.com/chainguard-dev/apko.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X sigs.k8s.io/release-utils/version.gitVersion=#{version}
+      -X sigs.k8s.io/release-utils/version.gitCommit=brew
+      -X sigs.k8s.io/release-utils/version.gitTreeState=clean
+      -X sigs.k8s.io/release-utils/version.buildDate=#{time.iso8601}
+    ]
+    system "go", "build", *std_go_args(ldflags: ldflags)
+
+    generate_completions_from_executable(bin/"apko", "completion")
+  end
+
+  test do
+    (testpath/"test.yml").write <<~EOS
+      contents:
+        repositories:
+          - https://dl-cdn.alpinelinux.org/alpine/edge/main
+        packages:
+          - alpine-base
+
+      entrypoint:
+        command: /bin/sh -l
+
+      # optional environment configuration
+      environment:
+        PATH: /usr/sbin:/sbin:/usr/bin:/bin
+    EOS
+    system bin/"apko", "build", testpath/"test.yml", "apko-alpine:test", "apko-alpine.tar"
+    assert_predicate testpath/"apko-alpine.tar", :exist?
+
+    assert_match version.to_s, shell_output(bin/"apko version 2>&1")
+  end
+end
