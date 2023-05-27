6ae76e607f6ad05cd129c838f5dddf52ed167228
Porkepix <porkepix@gmail.com>
2023-05-27 11:59:51 +0200

    wtf 20230526

--- Formula/wtf.rb
+++ Formula/wtf.rb
@@ -1,8 +1,8 @@
 class Wtf < Formula
   desc "Translate common Internet acronyms"
   homepage "https://sourceforge.net/projects/bsdwtf/"
-  url "https://downloads.sourceforge.net/project/bsdwtf/wtf-20230515.tar.gz"
-  sha256 "5147187b35d86f86b82f75eefec03261e85b960cc65146d4625ad57b8981c6c6"
+  url "https://downloads.sourceforge.net/project/bsdwtf/wtf-20230526.tar.gz"
+  sha256 "5edd2c987877a949fb57838b3185d3fdc5eaab2b8f619f363982dc11a6a1b67b"
   license :public_domain
 
   livecheck do
