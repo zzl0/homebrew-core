95918c4fd3024619cfcf4ab468b69f24a80b6279
Rui Chen <rui@chenrui.dev>
2023-05-27 00:43:32 -0400

    cfn-format 1.4.0
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/cfn-format.rb
+++ Formula/cfn-format.rb
@@ -1,8 +1,8 @@
 class CfnFormat < Formula
   desc "Command-line tool for formatting AWS CloudFormation templates"
   homepage "https://github.com/aws-cloudformation/rain"
-  url "https://github.com/aws-cloudformation/rain/archive/v1.3.3.tar.gz"
-  sha256 "230db11449b34043dc9e10a009134bd5dca240dc20a5d12710b98606f62559a7"
+  url "https://github.com/aws-cloudformation/rain/archive/v1.4.0.tar.gz"
+  sha256 "3dd05a6bb4c08193fe9ffad2d99c4d06cf205e7a0e31d64655a76ebdbbbc29e6"
   license "Apache-2.0"
 
   livecheck do
