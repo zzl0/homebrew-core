54659db02caf749307a08d6801278a2d826eba16
Rui Chen <rui@chenrui.dev>
2023-05-29 08:48:45 -0400

    webkitgtk 2.40.2
    webkitgtk: add upstream build patch
    
    relates to webkit/webkit#14177
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/webkitgtk.rb
+++ Formula/webkitgtk.rb
@@ -1,8 +1,8 @@
 class Webkitgtk < Formula
   desc "GTK interface to WebKit"
   homepage "https://webkitgtk.org"
-  url "https://webkitgtk.org/releases/webkitgtk-2.40.1.tar.xz"
-  sha256 "64e526984f8cd2161ef03ae949af99c002ff333d615e6386b460164a3c1b7ef6"
+  url "https://webkitgtk.org/releases/webkitgtk-2.40.2.tar.xz"
+  sha256 "96898870d994da406ee7a632816dcde9a3bb395ee5f344fcb3f3b8cc8a77e000"
   license "GPL-3.0-or-later"
 
   livecheck do
@@ -56,6 +56,12 @@ class Webkitgtk < Formula
   uses_from_macos "zlib"
 
   fails_with gcc: "5"
+
+  # patch to fix `BWRAP_EXECUTABLE` undefailed failure
+  patch do
+    url "https://github.com/WebKit/WebKit/commit/4977290ab4ab35258a6da9b13795c9b0f7894bf4.patch?full_index=1"
+    sha256 "5d5fa4af5ae355ee1ea93aebb15d232cdb508c9804e31fe30c768ff4d330120d"
+  end
 
   def install
     args = %w[
