42fddf74989c66f79368906ab7f42201cf922856
Tom Wright <contact@tomwright.me>
2023-05-29 19:10:59 +0000

    dasel 2.3.1

--- Formula/dasel.rb
+++ Formula/dasel.rb
@@ -1,8 +1,8 @@
 class Dasel < Formula
   desc "JSON, YAML, TOML, XML, and CSV query and modification tool"
   homepage "https://github.com/TomWright/dasel"
-  url "https://github.com/TomWright/dasel/archive/v2.3.0.tar.gz"
-  sha256 "634f36891f38a6a7166f0ab7da34f50c1c92a9ccf2de2e27546570c83dd019a2"
+  url "https://github.com/TomWright/dasel/archive/v2.3.1.tar.gz"
+  sha256 "e242359621b30e56c1af6feb0a99ef4ef7f5aade324f923360095d5bf811e848"
   license "MIT"
   head "https://github.com/TomWright/dasel.git", branch: "master"
 
