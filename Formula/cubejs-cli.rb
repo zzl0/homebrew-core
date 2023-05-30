c365d7ccd26f56d74f7a90dacdd581b20d77581b
Mikl Wolfe <wolfemikl@gmail.com>
2023-05-30 13:59:46 -0600

    cubejs-cli 0.33.19

--- Formula/cubejs-cli.rb
+++ Formula/cubejs-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class CubejsCli < Formula
   desc "Cube.js command-line interface"
   homepage "https://cube.dev/"
-  url "https://registry.npmjs.org/cubejs-cli/-/cubejs-cli-0.33.18.tgz"
-  sha256 "a5cd3cdacabe39f561b57b4e7a30563f07642e975483d4fd6fa02a1faede2484"
+  url "https://registry.npmjs.org/cubejs-cli/-/cubejs-cli-0.33.19.tgz"
+  sha256 "05d0798f2bf3338a4b1f7542891ff911d5c073fa93c069743de7abb5d3147eb8"
   license "Apache-2.0"
 
   bottle do
