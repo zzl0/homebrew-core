b5a7bc2cdc963963750372fd9620cce900ab4f54
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-05-30 13:32:17 +0000

    nexttrace: add 1.1.6 bottle.

--- Formula/nexttrace.rb
+++ Formula/nexttrace.rb
@@ -6,6 +6,16 @@ class Nexttrace < Formula
   license "GPL-3.0-only"
   head "https://github.com/sjlleo/nexttrace.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "b3e42f9e333063173e506fe8187f603bf4e21839b2300fe47efb5d6e3524c9e5"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b3e42f9e333063173e506fe8187f603bf4e21839b2300fe47efb5d6e3524c9e5"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b3e42f9e333063173e506fe8187f603bf4e21839b2300fe47efb5d6e3524c9e5"
+    sha256 cellar: :any_skip_relocation, ventura:        "077d2910cbfef29607e4385d7d2fe8f321a965fb7953003b36491b376f99af9a"
+    sha256 cellar: :any_skip_relocation, monterey:       "077d2910cbfef29607e4385d7d2fe8f321a965fb7953003b36491b376f99af9a"
+    sha256 cellar: :any_skip_relocation, big_sur:        "077d2910cbfef29607e4385d7d2fe8f321a965fb7953003b36491b376f99af9a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "24dbfb0a31dd4bbff50d261d88d77b54a3350fbf176e6adf50ff4b540dc82ded"
+  end
+
   depends_on "go" => :build
 
   def install
