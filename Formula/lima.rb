20290d6b28895a26a4e87983dcd930c936faf4c1
Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>
2023-05-30 16:05:11 +0900

    lima 0.16.0
    Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

--- Formula/lima.rb
+++ Formula/lima.rb
@@ -1,8 +1,8 @@
 class Lima < Formula
   desc "Linux virtual machines"
   homepage "https://github.com/lima-vm/lima"
-  url "https://github.com/lima-vm/lima/archive/v0.15.1.tar.gz"
-  sha256 "1e8790257810b704462a03736ef4bb8a2a3a873b21cceba86afcd6b2b1a8482b"
+  url "https://github.com/lima-vm/lima/archive/v0.16.0.tar.gz"
+  sha256 "deb23bd5d913790943dfa4cee8a6d50629e031750da2a84006736e13240e3977"
   license "Apache-2.0"
   head "https://github.com/lima-vm/lima.git", branch: "master"
 
@@ -20,7 +20,7 @@ class Lima < Formula
   depends_on "qemu"
 
   def install
-    system "make", "VERSION=#{version}", "clean", "binaries"
+    system "make", "VERSION=#{version}", "clean", "all"
 
     bin.install Dir["_output/bin/*"]
     share.install Dir["_output/share/*"]
