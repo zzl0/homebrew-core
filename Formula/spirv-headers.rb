f8deb50ea47fd5a95bbb373dbfa1eb5d280b60c0
Rui Chen <rui@chenrui.dev>
2023-05-30 23:34:55 -0400

    spirv-headers 1.3.250.0
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/spirv-headers.rb
+++ Formula/spirv-headers.rb
@@ -1,25 +1,10 @@
 class SpirvHeaders < Formula
   desc "Headers for SPIR-V"
   homepage "https://github.com/KhronosGroup/SPIRV-Headers"
+  url "https://github.com/KhronosGroup/SPIRV-Headers/archive/refs/tags/sdk-1.3.250.0.tar.gz"
+  sha256 "9d632a4dddd11f89a74a8c6f19cd29e8b0741d2fbb41ecc4dec26b922d28a2f3"
   license "MIT"
   head "https://github.com/KhronosGroup/SPIRV-Headers.git", branch: "main"
-
-  stable do
-    url "https://github.com/KhronosGroup/SPIRV-Headers/archive/refs/tags/sdk-1.3.246.1.tar.gz"
-    sha256 "71668e18ef7b318b06f8c466f46abad965b2646eaa322594cd015c2ac87133e6"
-
-    # remove upon next release; adds SPV_EXT_shader_tile_image
-    patch do
-      url "https://github.com/KhronosGroup/SPIRV-Headers/commit/d218c4ab72500ed8be5809ab2a03ff348a85e349.patch?full_index=1"
-      sha256 "a8fc93bcaae4eea3b03c18e9c031361b228ecff6aa8c0a55d5ccb0839d51259f"
-    end
-
-    # remove upon next release; adds SPV_KHR_ray_tracing_position_fetch
-    patch do
-      url "https://github.com/KhronosGroup/SPIRV-Headers/commit/131fddea36482695f04bfa47df5fdcb9fa15a246.patch?full_index=1"
-      sha256 "531bab56a97ce5cb2202d6cfa4fc4b9ab78bde899bd94cb4c6ae2ec2735c581f"
-    end
-  end
 
   livecheck do
     url :stable
@@ -37,11 +22,20 @@ class SpirvHeaders < Formula
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
 
-    pkgshare.install "example"
+    pkgshare.install "tests"
   end
 
   test do
-    system "cmake", "-S", pkgshare/"example", "-B", "build", *std_cmake_args
+    cp pkgshare/"tests/example.cpp", testpath
+
+    (testpath/"CMakeLists.txt").write <<~EOS
+      add_library(SPIRV-Headers-example
+                  ${CMAKE_CURRENT_SOURCE_DIR}/example.cpp)
+      target_include_directories(SPIRV-Headers-example
+                  PRIVATE ${SPIRV-Headers_SOURCE_DIR}/include)
+    EOS
+
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
     system "cmake", "--build", "build"
   end
 end
