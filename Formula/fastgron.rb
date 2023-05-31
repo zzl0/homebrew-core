d3154eada025903cc02dc0463f2880b525d3e3ac
adamritter <58403584+adamritter@users.noreply.github.com>
2023-05-31 20:59:12 +0200

    fastgron 0.4.8 (new formula)
    Update fastgron.rb
    
    fastgron: update build and test
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/fastgron.rb
@@ -0,0 +1,29 @@
+class Fastgron < Formula
+  desc "High-performance JSON to GRON converter"
+  homepage "https://github.com/adamritter/fastgron"
+  url "https://github.com/adamritter/fastgron/archive/refs/tags/v0.4.8.tar.gz"
+  sha256 "ee028cbf47813f65f08c37a4746c88bd0e326928893815d98c08720e2e3980b1"
+  license "MIT"
+
+  depends_on "cmake" => :build
+
+  uses_from_macos "curl"
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    expected = <<~EOS
+      json = [];
+      json[0] = 3;
+      json[1] = 4;
+      json[2] = 5;
+    EOS
+    assert_equal expected, pipe_output(bin/"fastgron", "[3,4,5]")
+
+    assert_match version.to_s, shell_output(bin/"fastgron --version 2>&1")
+  end
+end
