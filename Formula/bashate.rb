857473db002816180fe4669deb2df35fc16881f4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-05-31 20:47:16 +0000

    bashate: add 2.1.1 bottle.

--- Formula/bashate.rb
+++ Formula/bashate.rb
@@ -7,6 +7,16 @@ class Bashate < Formula
   sha256 "4bab6e977f8305a720535f8f93f1fb42c521fcbc4a6c2b3d3d7671f42f221f4c"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0404cc095c3112a2393efa839c8ede16e4016a80f3e72767ca568d7956a685c4"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "0404cc095c3112a2393efa839c8ede16e4016a80f3e72767ca568d7956a685c4"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0404cc095c3112a2393efa839c8ede16e4016a80f3e72767ca568d7956a685c4"
+    sha256 cellar: :any_skip_relocation, ventura:        "92be21fb90d03ca6e332233b0128d97c512ec5299988fdaa2f1da61e2bf7fe98"
+    sha256 cellar: :any_skip_relocation, monterey:       "92be21fb90d03ca6e332233b0128d97c512ec5299988fdaa2f1da61e2bf7fe98"
+    sha256 cellar: :any_skip_relocation, big_sur:        "92be21fb90d03ca6e332233b0128d97c512ec5299988fdaa2f1da61e2bf7fe98"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5969e7badb3d13d8392e1672235b959e0eed7f36545fd5ecf3a861cf4c8cfb80"
+  end
+
   depends_on "python@3.11"
 
   resource "pbr" do
