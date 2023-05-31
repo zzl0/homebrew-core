a943928f6c26fd42a6280ed16c16ad25139d5ebd
Alain Schlesser <alain.schlesser@gmail.com>
2023-05-31 21:20:08 +0200

    wp-cli 2.8.0

--- Formula/wp-cli.rb
+++ Formula/wp-cli.rb
@@ -1,8 +1,8 @@
 class WpCli < Formula
   desc "Command-line interface for WordPress"
   homepage "https://wp-cli.org/"
-  url "https://github.com/wp-cli/wp-cli/releases/download/v2.7.1/wp-cli-2.7.1.phar"
-  sha256 "bbf096bccc6b1f3f1437e75e3254f0dcda879e924bbea403dff3cfb251d4e468"
+  url "https://github.com/wp-cli/wp-cli/releases/download/v2.8.0/wp-cli-2.8.0.phar"
+  sha256 "ed64e680c92b8a878a80f3ae21721533dfcbf88cfd2f5f83f4fe5a884a214cdc"
   license "MIT"
 
   livecheck do
