0e6a96de749398a53ce3f40b70f2cd475914b791
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-06-01 01:52:59 +0000

    votca: add 2022.1 bottle.

--- Formula/votca.rb
+++ Formula/votca.rb
@@ -5,6 +5,16 @@ class Votca < Formula
   sha256 "4710a7552f94789936324d76d2e0830b576de8d3f1c605748e2d20947d018100"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "da7266fac5df3301435b6d8d3be146ac5cc13806283eb1ef97b63ca51440d016"
+    sha256 cellar: :any,                 arm64_monterey: "81b8dcb3765fc378847f6c71a0c0df5c85c3bc92daae12ada03520e0b0a4c7e5"
+    sha256 cellar: :any,                 arm64_big_sur:  "522f72f060f854a3623cb3f882e56c9b29b5e69a65bfc1df7faea26ba3e49fef"
+    sha256 cellar: :any,                 ventura:        "ee0d6a62064f0509481f4b6e6e24303e91d65d3589631af0dad3c049b606b3e6"
+    sha256 cellar: :any,                 monterey:       "9a98e188bfb8a78436893c7d6143ab927deb8253b25edeb0b88d381994f54aad"
+    sha256 cellar: :any,                 big_sur:        "0a3e8d97911b6800408237a03b67c98a94898fbc208c3eb2c9e674bb38a95d2e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2204dd5e27aea832f7d2f5c658c42ed190750447a89157cf45985824f11a07cb"
+  end
+
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "boost"
