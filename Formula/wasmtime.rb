e90e9094d0ac22132582f2e4ac412155a35ae597
Patrick Linnane <patrick@linnane.io>
2023-05-31 19:12:45 -0700

    wasmtime 9.0.3

--- Formula/wasmtime.rb
+++ Formula/wasmtime.rb
@@ -2,8 +2,8 @@ class Wasmtime < Formula
   desc "Standalone JIT-style runtime for WebAssembly, using Cranelift"
   homepage "https://wasmtime.dev/"
   url "https://github.com/bytecodealliance/wasmtime.git",
-      tag:      "v9.0.2",
-      revision: "0aa00479c9fbb39ef19a9f35d2ed0137454c93f5"
+      tag:      "v9.0.3",
+      revision: "271b605e8d3d44c5d0a39bb4e65c3efb3869ff74"
   license "Apache-2.0" => { with: "LLVM-exception" }
   head "https://github.com/bytecodealliance/wasmtime.git", branch: "main"
 
