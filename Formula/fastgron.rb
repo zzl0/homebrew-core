98a085c98c3b06387030ceb476a2f9b0a271f34f
adamritter <58403584+adamritter@users.noreply.github.com>
2023-06-02 00:36:45 +0100

    fastgron 0.4.11
    fastgron: update test
    
    fastgron: add build patch
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/fastgron.rb
+++ Formula/fastgron.rb
@@ -1,8 +1,8 @@
 class Fastgron < Formula
   desc "High-performance JSON to GRON converter"
   homepage "https://github.com/adamritter/fastgron"
-  url "https://github.com/adamritter/fastgron/archive/refs/tags/v0.4.8.tar.gz"
-  sha256 "ee028cbf47813f65f08c37a4746c88bd0e326928893815d98c08720e2e3980b1"
+  url "https://github.com/adamritter/fastgron/archive/refs/tags/v0.4.11.tar.gz"
+  sha256 "0c43c22132d5169cd074df84d445daa4a28b2dd4fb79d7291e01fdcbb1dfbacd"
   license "MIT"
 
   bottle do
@@ -19,6 +19,12 @@ class Fastgron < Formula
 
   uses_from_macos "curl"
 
+  # fix version setup, remove in next release
+  patch do
+    url "https://github.com/adamritter/fastgron/commit/ebf75de517bd903ea76bbd3d900bedd1830d980c.patch?full_index=1"
+    sha256 "249cbc9c7bb8a60eaa8f1cca98abf71eaf4f092a8c2f2e7318486c2cb44012db"
+  end
+
   def install
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args
     system "cmake", "--build", "build"
@@ -27,10 +33,10 @@ class Fastgron < Formula
 
   test do
     expected = <<~EOS
-      json = [];
-      json[0] = 3;
-      json[1] = 4;
-      json[2] = 5;
+      json = []
+      json[0] = 3
+      json[1] = 4
+      json[2] = 5
     EOS
     assert_equal expected, pipe_output(bin/"fastgron", "[3,4,5]")
 
