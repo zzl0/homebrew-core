2e3d827c394f17b07ca0f1719dee4550fc0ec8bf
Patrick Linnane <patrick@linnane.io>
2023-06-01 11:12:19 -0700

    verapdf 1.23.228

--- Formula/verapdf.rb
+++ Formula/verapdf.rb
@@ -1,8 +1,8 @@
 class Verapdf < Formula
   desc "Open-source industry-supported PDF/A validation"
   homepage "https://verapdf.org/home/"
-  url "https://github.com/veraPDF/veraPDF-apps/archive/refs/tags/v1.23.227.tar.gz"
-  sha256 "9120424e6bd187ec1527589540840b43913f1f0416f0dc75820556e11787d3cc"
+  url "https://github.com/veraPDF/veraPDF-apps/archive/refs/tags/v1.23.228.tar.gz"
+  sha256 "298f286fd864dd63e8d00a48bd511f4eefbc0d921c892907059a9d7547d6252a"
   license any_of: ["GPL-3.0-or-later", "MPL-2.0"]
   head "https://github.com/veraPDF/veraPDF-apps.git", branch: "integration"
 
