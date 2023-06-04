76021a1cd3d38f9f5f138ff18b8e7398c186ee23
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-06-04 06:35:53 +0000

    ddns-go: add 5.3.1 bottle.

--- Formula/ddns-go.rb
+++ Formula/ddns-go.rb
@@ -6,6 +6,16 @@ class DdnsGo < Formula
   license "MIT"
   head "https://github.com/jeessy2/ddns-go.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "47cfd7718d63616111ad8ad5f20705381d0582f9f3bfce52140dce85662ad2c1"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "47cfd7718d63616111ad8ad5f20705381d0582f9f3bfce52140dce85662ad2c1"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "47cfd7718d63616111ad8ad5f20705381d0582f9f3bfce52140dce85662ad2c1"
+    sha256 cellar: :any_skip_relocation, ventura:        "3e250a8f17ca4604897026ee3e8ffdfb0911648c6e4fed1f9c1fb2a50ad27d15"
+    sha256 cellar: :any_skip_relocation, monterey:       "3e250a8f17ca4604897026ee3e8ffdfb0911648c6e4fed1f9c1fb2a50ad27d15"
+    sha256 cellar: :any_skip_relocation, big_sur:        "3e250a8f17ca4604897026ee3e8ffdfb0911648c6e4fed1f9c1fb2a50ad27d15"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2ddf963d321c14120e18ac110bb9c18f64909ca7f75b37693ac9454f554c7de8"
+  end
+
   depends_on "go" => :build
 
   def install
