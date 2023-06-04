552a1b2fc3dd5f9b835da4042cbfa0fe2e3b8742
Pierre Wendling <pierre.wendling.4@gmail.com>
2023-06-04 13:40:13 -0400

    dsda-doom 0.26.2

--- Formula/dsda-doom.rb
+++ Formula/dsda-doom.rb
@@ -1,8 +1,8 @@
 class DsdaDoom < Formula
   desc "Fork of prboom+ with a focus on speedrunning"
   homepage "https://github.com/kraflab/dsda-doom"
-  url "https://github.com/kraflab/dsda-doom/archive/refs/tags/v0.26.1.tar.gz"
-  sha256 "c81882328a75bf26489bdf35beff831070badc3df7d9799378418dcbdd9d2769"
+  url "https://github.com/kraflab/dsda-doom/archive/refs/tags/v0.26.2.tar.gz"
+  sha256 "f9f76125b9ffb5be98de175f1e9839b4f563d0113edbe21c4ebb048da714b1d2"
   license "GPL-2.0-only"
   head "https://github.com/kraflab/dsda-doom.git", branch: "master"
 
