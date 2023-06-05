b67082ac041bc82916a327c172e8b7c5046ad89b
Ilya Lavrenov <ilya.lavrenov@intel.com>
2023-06-06 01:36:42 +0400

    ittapi 3.24.2 (new formula)

---
+++ Formula/ittapi.rb
@@ -0,0 +1,36 @@
+class Ittapi < Formula
+  desc "Intel Instrumentation and Tracing Technology (ITT) and Just-In-Time (JIT) API"
+  homepage "https://github.com/intel/ittapi"
+  url "https://github.com/intel/ittapi/archive/refs/tags/v3.24.2.tar.gz"
+  sha256 "006c0379ea072965b735922c5e01ab0f2a32e288675c200523a59279a03254fb"
+  license "GPL-2.0-only"
+  head "https://github.com/intel/ittapi.git", branch: "master"
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <ittnotify.h>
+
+      __itt_domain* domain = __itt_domain_create("Example.Domain.Global");
+      __itt_string_handle* handle_main = __itt_string_handle_create("main");
+
+      int main()
+      {
+        __itt_task_begin(domain, __itt_null, __itt_null, handle_main);
+        __itt_task_end(domain);
+        return 0;
+      }
+    EOS
+    system ENV.cxx, "test.cpp", "-o", "test",
+                    "-I#{include}",
+                    "-L#{lib}", "-littnotify"
+    system "./test"
+  end
+end
