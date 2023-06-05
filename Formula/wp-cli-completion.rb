134bbe4bba559d1b4146d9d3f498ffa301e06e93
Alain Schlesser <alain.schlesser@gmail.com>
2023-06-05 09:55:48 +0200

    wp-cli-completion 2.8.1
    Bump wp-cli-completion to v2.8.1
    
    Closes #132855.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/wp-cli-completion.rb
+++ Formula/wp-cli-completion.rb
@@ -1,8 +1,8 @@
 class WpCliCompletion < Formula
   desc "Bash completion for Wpcli"
   homepage "https://github.com/wp-cli/wp-cli"
-  url "https://github.com/wp-cli/wp-cli/archive/refs/tags/v2.8.0.tar.gz"
-  sha256 "d7ec18a684ac6e3f45324abb36610fd60469e268c4b29a391abb38c8f920784e"
+  url "https://github.com/wp-cli/wp-cli/archive/refs/tags/v2.8.1.tar.gz"
+  sha256 "483bb6692bdfb4a5f995500e35dccde9117c033f6c85ec4eb169fc6952f458fc"
   license "MIT"
   head "https://github.com/wp-cli/wp-cli.git", branch: "master"
 
