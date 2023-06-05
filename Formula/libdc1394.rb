3c8e3dff5fe4462994e758365dbe25d957088d49
Patrick Linnane <patrick@linnane.io>
2023-06-05 09:20:00 -0700

    libdc1394 2.2.7
    libdc1394: update license
    
    libdc1394: use std_configure_args
    
    libdc1394: remove HEAD
    
    libdc1394: remove outdated patch comment
    
    libdc1394: update license
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/libdc1394.rb
+++ Formula/libdc1394.rb
@@ -1,27 +1,9 @@
 class Libdc1394 < Formula
   desc "Provides API for IEEE 1394 cameras"
   homepage "https://damien.douxchamps.net/ieee1394/libdc1394/"
-  license "LGPL-2.1"
-  revision 1
-
-  stable do
-    url "https://downloads.sourceforge.net/project/libdc1394/libdc1394-2/2.2.6/libdc1394-2.2.6.tar.gz"
-    sha256 "2b905fc9aa4eec6bdcf6a2ae5f5ba021232739f5be047dec8fe8dd6049c10fed"
-
-    # fix issue due to bug in OSX Firewire stack
-    # libdc1394 author comments here:
-    # https://permalink.gmane.org/gmane.comp.multimedia.libdc1394.devel/517
-    patch do
-      url "https://raw.githubusercontent.com/Homebrew/formula-patches/b8275aa07f/libdc1394/capture.patch"
-      sha256 "6e3675b7fb1711c5d7634a76d723ff25e2f7ae73cd1fbf3c4e49ba8e5dcf6c39"
-    end
-
-    # Fix -flat_namespace being used on Big Sur and later.
-    patch do
-      url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-pre-0.4.2.418-big_sur.diff"
-      sha256 "83af02f2aa2b746bb7225872cab29a253264be49db0ecebb12f841562d9a2923"
-    end
-  end
+  url "https://downloads.sourceforge.net/project/libdc1394/libdc1394-2/2.2.7/libdc1394-2.2.7.tar.gz"
+  sha256 "537ceb78dd3cef271a183f4a176191d1cecf85f025520e6bd3758b0e19e6609f"
+  license "LGPL-2.1-or-later"
 
   bottle do
     sha256 cellar: :any,                 arm64_ventura:  "9a16dd0bb058c51d700fc93c1cbe13d2b185cfeccd9ff90eb300088fd76f595b"
@@ -34,23 +16,22 @@ class Libdc1394 < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "f5d8ff3fd2c11d8c8387ec2c1d30d6b93bee3f1d5516c2a9df4d2454ad1842cb"
   end
 
-  head do
-    url "https://git.code.sf.net/p/libdc1394/code.git", branch: "master"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
-    depends_on "pkg-config" => :build
-    depends_on "libusb"
-  end
-
   depends_on "sdl12-compat"
 
+  # fix issue due to bug in OSX Firewire stack
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/b8275aa07f/libdc1394/capture.patch"
+    sha256 "6e3675b7fb1711c5d7634a76d723ff25e2f7ae73cd1fbf3c4e49ba8e5dcf6c39"
+  end
+
+  # Fix -flat_namespace being used on Big Sur and later.
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-pre-0.4.2.418-big_sur.diff"
+    sha256 "83af02f2aa2b746bb7225872cab29a253264be49db0ecebb12f841562d9a2923"
+  end
+
   def install
-    Dir.chdir("libdc1394") if build.head?
-    system "autoreconf", "-i", "-s" if build.head?
-    system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}",
+    system "./configure", *std_configure_args,
                           "--disable-examples",
                           "--disable-sdltest"
     system "make", "install"
