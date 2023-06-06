8cda586c78dd432b2a20aa088c6f456f1da6ad50
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-06-06 03:09:21 +0000

    hblock: update 3.4.2 bottle.

--- Formula/hblock.rb
+++ Formula/hblock.rb
@@ -6,7 +6,7 @@ class Hblock < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "81738da59ef03c9d08da825a5893f84c2ec946e954e4a1872a4d0c9df442f247"
+    sha256 cellar: :any_skip_relocation, all: "49a7f650761fea0f964bc4542c745ebf4ee80325f18e1c666c352fd36a9b03ee"
   end
 
   uses_from_macos "curl"
