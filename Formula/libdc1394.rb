4c6eaf26489380ab06c92813e64d41b1e977e018
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-06-06 03:14:41 +0000

    libdc1394: update 2.2.7 bottle.

--- Formula/libdc1394.rb
+++ Formula/libdc1394.rb
@@ -6,14 +6,13 @@ class Libdc1394 < Formula
   license "LGPL-2.1-or-later"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "9a16dd0bb058c51d700fc93c1cbe13d2b185cfeccd9ff90eb300088fd76f595b"
-    sha256 cellar: :any,                 arm64_monterey: "df7e187b4be0fd2c0b1d44ae4affaf06c880d10405b49676c46654f7578c578e"
-    sha256 cellar: :any,                 arm64_big_sur:  "1130efff7b225327ca8743b863d808f509c1391afb19692be2b14b01f65fc3bf"
-    sha256 cellar: :any,                 ventura:        "0f2463909e7b1e94ace46e3a13125073f1ed0b3126e2787b3840ce0900777497"
-    sha256 cellar: :any,                 monterey:       "9a293bb439cf246d321f3a2f3b913a0cabcec397dec51134d6bb5c315949d2d4"
-    sha256 cellar: :any,                 big_sur:        "d2783838520007d2620ce7a640dc68e7aaf3c47e9ff857d03404594b0343ddc1"
-    sha256 cellar: :any,                 catalina:       "392b2fc67276fabb361f3e9d88878c79baff7b50f8666c63e6b3c58f04af81cc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f5d8ff3fd2c11d8c8387ec2c1d30d6b93bee3f1d5516c2a9df4d2454ad1842cb"
+    sha256 cellar: :any,                 arm64_ventura:  "38a5b54a9c968f7cf4ad345b6c679cf752c3634492158dfbe120c6f5e086cefd"
+    sha256 cellar: :any,                 arm64_monterey: "48b4198b7d90c2a14d4a66075e2342c1cf4d6c03fb88670b0424a51b3dd9a4b3"
+    sha256 cellar: :any,                 arm64_big_sur:  "6dc506be57b363bf54f40304cb87e0a70827786087d0bb355401a45b49121d46"
+    sha256 cellar: :any,                 ventura:        "1b71b1a62895a6223862b1e0a0a052032c8ce358245926befec4a7a1210091ed"
+    sha256 cellar: :any,                 monterey:       "5eb178bc37614499766470dda514437cbcfd00516619b7dd3d63308d0c297ec8"
+    sha256 cellar: :any,                 big_sur:        "b292acc61a9a2acd0fb61b52e3d0ff624adb9c482871982cb1ebd696d581ae58"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "53fba90024fafc156e594ce967d376cb60f86ca1f0fed7f3416abbc7bb023bd1"
   end
 
   depends_on "sdl12-compat"
