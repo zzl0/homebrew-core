6d312fdae013b57d7fcd32839db5983b899ce820
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2023-06-06 23:46:01 +0800

    opencv@3: revision bump (imath 3.1.9)

--- Formula/opencv@3.rb
+++ Formula/opencv@3.rb
@@ -5,7 +5,7 @@ class OpencvAT3 < Formula
   url "https://github.com/opencv/opencv/archive/3.4.16.tar.gz"
   sha256 "5e37b791b2fe42ed39b52d9955920b951ee42d5da95f79fbc9765a08ef733399"
   license "BSD-3-Clause"
-  revision 8
+  revision 9
 
   bottle do
     sha256 arm64_ventura:  "6f02e702023aba86299a0855320b048d8b2cb38d2f2db8b9863fab711ce37443"
