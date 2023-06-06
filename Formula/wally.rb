a61a897a02f70f3a951beaae88058b25145cc33d
Rui Chen <rui@chenrui.dev>
2023-06-05 23:04:11 -0400

    wally 0.3.2
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/wally.rb
+++ Formula/wally.rb
@@ -1,8 +1,8 @@
 class Wally < Formula
   desc "Modern package manager for Roblox projects inspired by Cargo"
   homepage "https://github.com/UpliftGames/wally"
-  url "https://github.com/UpliftGames/wally/archive/refs/tags/v0.3.1.tar.gz"
-  sha256 "c9b8bcdbfc906848fac5a49d69f54d3b2b4830b23dda7cd47190c5624c2bf04c"
+  url "https://github.com/UpliftGames/wally/archive/refs/tags/v0.3.2.tar.gz"
+  sha256 "f7da2a10865b05efe97273aa1d08e0efbb9d26fecb770d819bdc01fbb98a0ad9"
   license "MPL-2.0"
   head "https://github.com/UpliftGames/wally.git", branch: "main"
 
@@ -18,15 +18,13 @@ class Wally < Formula
 
   depends_on "pkg-config" => :build
   depends_on "rust" => :build
-  # TODO: move to openssl@3 on `version` bump
-  depends_on "openssl@1.1"
+  depends_on "openssl@3"
 
   uses_from_macos "zlib"
 
   def install
-    odie "Check for new OpenSSL version" if version > "0.3.1"
     ENV["OPENSSL_NO_VENDOR"] = "1"
-    ENV["OPENSSL_DIR"] = Formula["openssl@1.1"].opt_prefix
+    ENV["OPENSSL_DIR"] = Formula["openssl@3"].opt_prefix
     system "cargo", "install", *std_cargo_args
   end
 
