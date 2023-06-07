81d050d5af59d1ee80f48d9e3c93812ac39073d7
Rui Chen <rui@chenrui.dev>
2023-06-07 13:06:06 -0400

    nghttp2 1.54.0
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/nghttp2.rb
+++ Formula/nghttp2.rb
@@ -1,9 +1,9 @@
 class Nghttp2 < Formula
   desc "HTTP/2 C Library"
   homepage "https://nghttp2.org/"
-  url "https://github.com/nghttp2/nghttp2/releases/download/v1.53.0/nghttp2-1.53.0.tar.gz"
-  mirror "http://fresh-center.net/linux/www/nghttp2-1.53.0.tar.gz"
-  sha256 "f5f3f18b377d1e7658e4655a32d9a7ce6fef39fa13f600fe920f5f77c60fc34b"
+  url "https://github.com/nghttp2/nghttp2/releases/download/v1.54.0/nghttp2-1.54.0.tar.gz"
+  mirror "http://fresh-center.net/linux/www/nghttp2-1.54.0.tar.gz"
+  sha256 "890cb0761d2fac570f0aa7bce085cd7d3c77bcfd56510456b6ea0278cde812f0"
   license "MIT"
 
   bottle do
