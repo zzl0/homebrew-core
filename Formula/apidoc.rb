d846740e5211adc8efbda52c39ac763803b854c4
Rui Chen <rui@chenrui.dev>
2023-06-07 10:15:51 -0400

    apidoc: remove macos-term-size dep
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/apidoc.rb
+++ Formula/apidoc.rb
@@ -19,10 +19,6 @@ class Apidoc < Formula
 
   depends_on "node"
 
-  on_macos do
-    depends_on "macos-term-size"
-  end
-
   def install
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
     bin.install_symlink Dir["#{libexec}/bin/*"]
