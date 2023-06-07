3347365ae91ac22b0c2c92bc5acc1d6e337b2f7f
Porkepix <porkepix@gmail.com>
2023-06-07 19:50:45 +0200

    pyright 1.1.313

--- Formula/pyright.rb
+++ Formula/pyright.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Pyright < Formula
   desc "Static type checker for Python"
   homepage "https://github.com/microsoft/pyright"
-  url "https://registry.npmjs.org/pyright/-/pyright-1.1.312.tgz"
-  sha256 "37561df721723a157f4364b4f87ee2a90961d681fa8e863671fab928f6f5c42c"
+  url "https://registry.npmjs.org/pyright/-/pyright-1.1.313.tgz"
+  sha256 "258813417419ea2990682419e44b03a285b894609eb3d28ae89ae436afc789cb"
   license "MIT"
   head "https://github.com/microsoft/pyright.git", branch: "main"
 
