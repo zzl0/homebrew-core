cc37d002d1915f687b5073add961607302668a6e
Rui Chen <rui@chenrui.dev>
2023-06-07 13:14:18 -0400

    lttng-ust 2.13.6

--- Formula/lttng-ust.rb
+++ Formula/lttng-ust.rb
@@ -1,8 +1,8 @@
 class LttngUst < Formula
   desc "Linux Trace Toolkit Next Generation Userspace Tracer"
   homepage "https://lttng.org/"
-  url "https://lttng.org/files/lttng-ust/lttng-ust-2.13.5.tar.bz2"
-  sha256 "f1d7bb4984a3dc5dacd3b7bcb4c10c04b041b0eecd7cba1fef3d8f86aff02bd6"
+  url "https://lttng.org/files/lttng-ust/lttng-ust-2.13.6.tar.bz2"
+  sha256 "e7e04596dd73ac7aa99e27cd000f949dbb0fed51bd29099f9b08a25c1df0ced5"
   license all_of: ["LGPL-2.1-only", "MIT", "GPL-2.0-only", "BSD-3-Clause", "BSD-2-Clause", "GPL-3.0-or-later"]
 
   livecheck do
