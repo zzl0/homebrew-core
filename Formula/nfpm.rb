b5a2671ae44a6581050c46fb56972b216e99193e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-06-07 05:22:10 +0000

    nfpm 2.30.1

--- Formula/nfpm.rb
+++ Formula/nfpm.rb
@@ -1,8 +1,8 @@
 class Nfpm < Formula
   desc "Simple deb and rpm packager"
   homepage "https://nfpm.goreleaser.com/"
-  url "https://github.com/goreleaser/nfpm/archive/v2.30.0.tar.gz"
-  sha256 "8ddda2490aea4b49a9b2e0658b59eb9cf523931466236ee689525ca8a4c8325e"
+  url "https://github.com/goreleaser/nfpm/archive/v2.30.1.tar.gz"
+  sha256 "0a1c9f5439e4fc155dc8e073d05ec7815e0dbeacffdbfeec107cd60ac6e6aa6e"
   license "MIT"
   head "https://github.com/goreleaser/nfpm.git", branch: "master"
 
