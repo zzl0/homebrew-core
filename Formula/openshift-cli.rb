cfa6484265a42bd6eda027b2980007782b189b91
Christoph Blecker <admin@toph.ca>
2023-06-08 10:46:10 -0700

    openshift-cli: update source as git tags are not updated

--- Formula/openshift-cli.rb
+++ Formula/openshift-cli.rb
@@ -1,15 +1,14 @@
 class OpenshiftCli < Formula
   desc "OpenShift command-line interface tools"
   homepage "https://www.openshift.com/"
-  url "https://github.com/openshift/oc.git",
-      tag:      "openshift-clients-4.13.0-202304190216",
-      revision: "92b1a3d0e5d092430b523f6541aa0c504b2222b3"
+  url "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.13.1/openshift-client-src.tar.gz"
+  sha256 "c362a296c6e9e92f75cf4f2818a7277cc031426f38da605e5eaf39b3207c8c47"
   license "Apache-2.0"
-  head "https://github.com/openshift/oc.git", branch: "master"
+  head "https://github.com/openshift/oc.git", shallow: false, branch: "master"
 
   livecheck do
-    url :stable
-    regex(/^openshift-clients[._-](\d+(?:\.\d+)+(?:[._-]p?\d+)?)$/i)
+    url "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/stable/"
+    regex(/href=.*?openshift-client-mac-(\d+(?:\.\d+)+)\.t/i)
   end
 
   bottle do
@@ -22,31 +21,49 @@ class OpenshiftCli < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "9cdefea95d7c9c3529c625e5a00b50214abbed58985419877a6d53a4b4f7623d"
   end
 
-  depends_on "coreutils" => :build
   depends_on "go" => :build
-  depends_on "socat"
-
   uses_from_macos "krb5"
 
   def install
     arch = Hardware::CPU.intel? ? "amd64" : Hardware::CPU.arch.to_s
     os = OS.kernel_name.downcase
+    revision = build.head? ? Utils.git_head : Pathname.pwd.basename.to_s.delete_prefix("oc-")
 
-    # See https://github.com/golang/go/issues/26487
+    # See https://github.com/Homebrew/brew/issues/14763
     ENV.O0 if OS.linux?
 
-    system "make", "cross-build-#{os}-#{arch}", "OS_GIT_VERSION=#{version}", "SHELL=/bin/bash"
+    system "make", "cross-build-#{os}-#{arch}", "OS_GIT_VERSION=#{version}", "SOURCE_GIT_COMMIT=#{revision}", "SHELL=/bin/bash"
     bin.install "_output/bin/#{os}_#{arch}/oc"
-
-    bash_completion.install "contrib/completions/bash/oc"
-    zsh_completion.install "contrib/completions/zsh/oc" => "_oc"
+    generate_completions_from_executable(bin/"oc", "completion", base_name: "oc")
   end
 
   test do
+    # Grab version details from built client
+    version_raw = shell_output("#{bin}/oc version --client --output=json")
+    version_json = JSON.parse(version_raw)
+
+    # Ensure that we had a clean build tree
+    assert_equal "clean", version_json["clientVersion"]["gitTreeState"]
+
+    if stable?
+      # Verify the built artifact matches the formula
+      assert_match version_json["clientVersion"]["gitVersion"], "v#{version}"
+
+      # Get remote release details
+      release_raw = shell_output("#{bin}/oc adm release info #{version} --output=json")
+      release_json = JSON.parse(release_raw)
+
+      # Verify the formula matches the release data for the version
+      assert_match version_json["clientVersion"]["gitCommit"],
+        release_json["references"]["spec"]["tags"].find { |tag|
+          tag["name"]=="cli"
+        } ["annotations"]["io.openshift.build.commit.id"]
+
+    end
+
+    # Test that we can generate and write a kubeconfig
     (testpath/"kubeconfig").write ""
     system "KUBECONFIG=#{testpath}/kubeconfig #{bin}/oc config set-context foo 2>&1"
-    context_output = shell_output("KUBECONFIG=#{testpath}/kubeconfig #{bin}/oc config get-contexts -o name")
-
-    assert_match "foo", context_output
+    assert_match "foo", shell_output("KUBECONFIG=#{testpath}/kubeconfig #{bin}/oc config get-contexts -o name")
   end
 end
