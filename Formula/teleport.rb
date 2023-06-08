443fb7204714cd25814380e7c77417c4bf1d9f14
Patrick Linnane <patrick@linnane.io>
2023-06-08 13:41:24 -0700

    teleport 13.1.0
    teleport: update livecheck
    
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Formula/teleport.rb
+++ Formula/teleport.rb
@@ -1,16 +1,16 @@
 class Teleport < Formula
   desc "Modern SSH server for teams managing distributed infrastructure"
   homepage "https://gravitational.com/teleport"
-  url "https://github.com/gravitational/teleport/archive/v13.0.4.tar.gz"
-  sha256 "6b30ed176b1b5b2e26d5b5cfbe451b88bf030a827190bdead3698da3a1816d83"
+  url "https://github.com/gravitational/teleport/archive/v13.1.0.tar.gz"
+  sha256 "2cbc15d2e4032bb40a32a6b586c6fabe2d06fe5f7d4024d1a9135b2037183b5e"
   license "Apache-2.0"
   head "https://github.com/gravitational/teleport.git", branch: "master"
 
   # As of writing, two major versions of `teleport` are being maintained
-  # side by side and the "latest" tag can point to a release from the older major version.
+  # side by side and the "latest" release can point to an older major version.
   livecheck do
     url :stable
-    strategy :github_release
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
   bottle do
