a1907cd0ed84448be7ed0c6b90828316182803e1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-06-08 19:12:34 +0000

    core-lightning: add 23.05 bottle.

--- Formula/core-lightning.rb
+++ Formula/core-lightning.rb
@@ -6,6 +6,16 @@ class CoreLightning < Formula
       revision: "d1cf88c62e8ff10485f3b40cddb93fc0063ba92a"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "6a0a9f3d12062fe4335ee9236882735e0cc65da9cd9e185dbba9d25788095ad1"
+    sha256 cellar: :any,                 arm64_monterey: "68380caa6742aea13e5cd0b358bd4065649fbc04c3ad0703ff925bd51fef08b7"
+    sha256 cellar: :any,                 arm64_big_sur:  "5c66f2b5b1d2094fab9c38a64691a6d9bd4babbc009e128feb810e9fa149f959"
+    sha256 cellar: :any,                 ventura:        "a77fd27dd88e07740eb5a553e5881bf8f10ec1a9d2597e82cdf6ead99a41476e"
+    sha256 cellar: :any,                 monterey:       "35f13f39af45a773b0297891ff7ead7122752ab332e679c272224368d2ae7aed"
+    sha256 cellar: :any,                 big_sur:        "10dccc640fde1ef2a6c657dffcede0f7e0e447aa52e586bceed9d109722c19fc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "136008b34be22709bca55c5953751f944543c7e39bddc3fb2504529caa51a020"
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "gettext" => :build
