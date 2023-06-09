7a9fc9fa4629c1360648a0a1704f9aa6e9f855a9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-06-09 03:22:26 +0000

    lowdown: add 1.0.2 bottle.

--- Formula/lowdown.rb
+++ Formula/lowdown.rb
@@ -5,6 +5,16 @@ class Lowdown < Formula
   sha256 "049b7883874f8a8e528dc7c4ed7b27cf7ceeb9ecf8fe71c3a8d51d574fddf84b"
   license "ISC"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8fb0c7d57f545fd6ed5902049ec23eaa7f54a3bde10c0e6808cc54adb76e6abb"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2bc8fd2653eab9542d2afe9d6aad18481ed647dec62228d1b2e2f5a2cb03d141"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "da6b18b0d4734244f9e5d677298f4a0874d87326f269656e76120d4fb867d8de"
+    sha256 cellar: :any_skip_relocation, ventura:        "05886e27d8b52b8755130af5e4741f0920908d9742781975aa3cdf398ca613c3"
+    sha256 cellar: :any_skip_relocation, monterey:       "a4a41ff54df94cafab6b52b1ce062cf5dbf3a99c6ad22b494c59e99bf95088b3"
+    sha256 cellar: :any_skip_relocation, big_sur:        "fb19bded8bdfe9220682b03c8c9aeaf7c24c86bd20d8cc294ad5cc35fba5e87b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7b3aae261818590324f0ed233f8f104541d5adddc0da9abe36f035919c480657"
+  end
+
   def install
     configure_args = %W[MANDIR=#{man} PREFIX=#{prefix}]
     if OS.mac?
