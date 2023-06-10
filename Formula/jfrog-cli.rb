8b448894065995ddd527affa2867abfbd1b252e1
Rui Chen <rui@chenrui.dev>
2023-06-10 10:17:18 -0400

    jfrog-cli 2.40.0

--- Formula/jfrog-cli.rb
+++ Formula/jfrog-cli.rb
@@ -1,8 +1,8 @@
 class JfrogCli < Formula
   desc "Command-line interface for JFrog products"
   homepage "https://www.jfrog.com/confluence/display/CLI/JFrog+CLI"
-  url "https://github.com/jfrog/jfrog-cli/archive/refs/tags/v2.39.1.tar.gz"
-  sha256 "3adfe5034556843f7fff70485c61710bfb2638529730256b12ae7bbf807458a6"
+  url "https://github.com/jfrog/jfrog-cli/archive/refs/tags/v2.40.0.tar.gz"
+  sha256 "c1c39c88280c861b1a41aa7bda895c3f00afe51b426fa1b78a99f058221b8b76"
   license "Apache-2.0"
 
   bottle do
