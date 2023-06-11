19ccc2b2907b7caf74de68f6aa1b022d67431dc6
Rui Chen <rui@chenrui.dev>
2023-06-11 10:19:28 -0400

    goawk 1.23.3

--- Formula/goawk.rb
+++ Formula/goawk.rb
@@ -1,8 +1,8 @@
 class Goawk < Formula
   desc "POSIX-compliant AWK interpreter written in Go"
   homepage "https://benhoyt.com/writings/goawk/"
-  url "https://github.com/benhoyt/goawk/archive/refs/tags/v1.23.2.tar.gz"
-  sha256 "f4613343fa6d43866da5e34d5bd8fc72275907a4c45f64ed63fcc800b4f358c3"
+  url "https://github.com/benhoyt/goawk/archive/refs/tags/v1.23.3.tar.gz"
+  sha256 "ad9c8431d0a0976445f47015e263b3b2ec05cc4c17703fa8d2b02e3242daef08"
   license "MIT"
 
   bottle do
