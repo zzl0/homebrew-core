4f77f090d1642a6d20aaca3012f26ace91fe3545
Patrick Linnane <patrick@linnane.io>
2023-06-10 17:04:10 -0700

    bash-language-server 4.10.0

--- Formula/bash-language-server.rb
+++ Formula/bash-language-server.rb
@@ -3,8 +3,8 @@ require "language/node"
 class BashLanguageServer < Formula
   desc "Language Server for Bash"
   homepage "https://github.com/bash-lsp/bash-language-server"
-  url "https://registry.npmjs.org/bash-language-server/-/bash-language-server-4.9.2.tgz"
-  sha256 "431b0ee81582c5f8e3d5eeb130f1bd63c509abf76131965d2543add8f742f5f4"
+  url "https://registry.npmjs.org/bash-language-server/-/bash-language-server-4.10.0.tgz"
+  sha256 "acfe9c75fa26074fd56ce55147ac8b0b3dfcf1ce71d42e16f493062d1bbb7488"
   license "MIT"
 
   bottle do
