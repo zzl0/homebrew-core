3fa1ce1635c597a4f3e6ee1fccbd4f05e3fc3ab0
Porkepix <porkepix@gmail.com>
2023-06-12 14:15:56 +0200

    strongswan 5.9.11

--- Formula/strongswan.rb
+++ Formula/strongswan.rb
@@ -1,18 +1,9 @@
 class Strongswan < Formula
   desc "VPN based on IPsec"
   homepage "https://www.strongswan.org"
+  url "https://download.strongswan.org/strongswan-5.9.11.tar.bz2"
+  sha256 "ddf53f1f26ad26979d5f55e8da95bd389552f5de3682e35593f9a70b2584ed2d"
   license "GPL-2.0-or-later"
-
-  stable do
-    url "https://download.strongswan.org/strongswan-5.9.10.tar.bz2"
-    sha256 "3b72789e243c9fa6f0a01ccaf4f83766eba96a5e5b1e071d36e997572cf34654"
-
-    # Fix -flat_namespace being used on Big Sur and later.
-    patch do
-      url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
-      sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
-    end
-  end
 
   livecheck do
     url "https://download.strongswan.org/"
