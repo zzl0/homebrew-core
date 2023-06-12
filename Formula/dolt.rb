e251de56fcebd5c471addd01679458baaa9b6a59
Porkepix <porkepix@gmail.com>
2023-06-13 01:16:19 +0200

    dolt 1.4.0

--- Formula/dolt.rb
+++ Formula/dolt.rb
@@ -1,8 +1,8 @@
 class Dolt < Formula
   desc "Git for Data"
   homepage "https://github.com/dolthub/dolt"
-  url "https://github.com/dolthub/dolt/archive/v1.3.0.tar.gz"
-  sha256 "db26812fe1e93df62dc02eefe1083dc94ce8ace368cba4412c8c51079e707e7f"
+  url "https://github.com/dolthub/dolt/archive/v1.4.0.tar.gz"
+  sha256 "ff537e1660cfd70fedd268c1055ffd8fa2e0ab45abaf04c5737c7a00b56aadde"
   license "Apache-2.0"
 
   livecheck do
