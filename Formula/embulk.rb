6eca17ab3c3c0593baad879e9390212a53566027
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-06-13 05:09:29 +0000

    embulk 0.11.0

--- Formula/embulk.rb
+++ Formula/embulk.rb
@@ -4,8 +4,8 @@ class Embulk < Formula
   # https://www.embulk.org/articles/2020/03/13/embulk-v0.10.html
   # v0.10.* is a "development" series, not for your production use.
   # In your production, keep using v0.9.* stable series.
-  url "https://github.com/embulk/embulk/releases/download/v0.9.24/embulk-0.9.24.jar"
-  sha256 "acf204ffa0112ae4fd6757b4557fef6f72e2d5dd163af439f8b13ae98fb9dafd"
+  url "https://github.com/embulk/embulk/releases/download/v0.11.0/embulk-0.11.0.jar"
+  sha256 "16664089e2676d5f7fe71c1ccf48b2a24f982b89e4f483e650a15f52a72309c1"
   license "Apache-2.0"
   version_scheme 1
 
