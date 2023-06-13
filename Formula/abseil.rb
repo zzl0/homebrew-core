736d82a7f768c81e11c90a2d907014874b061969
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2023-06-13 13:06:17 +0800

    abseil: install test helpers
    These are useful when writing tests for software that uses Abseil. Other
    package managers also install these libraries.
    
    Also, use `ENV.runtime_cpu_detection`.

--- Formula/abseil.rb
+++ Formula/abseil.rb
@@ -18,24 +18,38 @@ class Abseil < Formula
 
   depends_on "cmake" => :build
 
+  on_macos do
+    depends_on "googletest" => :build # For test helpers
+  end
+
   fails_with gcc: "5" # C++17
 
   def install
+    ENV.runtime_cpu_detection
+
+    # Install test helpers. Doing this on Linux requires rebuilding `googltest` with `-fPIC`.
+    extra_cmake_args = if OS.mac?
+      %w[ABSL_BUILD_TEST_HELPERS ABSL_USE_EXTERNAL_GOOGLETEST ABSL_FIND_GOOGLETEST].map do |arg|
+        "-D#{arg}=ON"
+      end
+    end.to_a
+
     system "cmake", "-S", ".", "-B", "build",
                     "-DCMAKE_INSTALL_RPATH=#{rpath}",
                     "-DCMAKE_CXX_STANDARD=17",
                     "-DBUILD_SHARED_LIBS=ON",
                     "-DABSL_PROPAGATE_CXX_STD=ON",
-                    *std_cmake_args
+                    "-DABSL_ENABLE_INSTALL=ON",
+                    *extra_cmake_args, *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
 
+    return unless OS.mac?
+
     # Remove bad flags in .pc files.
     # https://github.com/abseil/abseil-cpp/issues/1408
-    if OS.mac?
-      inreplace lib.glob("pkgconfig/absl_random_internal_randen_hwaes{,_impl}.pc"),
-                "-Xarch_x86_64 -Xarch_x86_64 -Xarch_arm64 ", ""
-    end
+    inreplace lib.glob("pkgconfig/absl_random_internal_randen_hwaes{,_impl}.pc"),
+              "-Xarch_x86_64 -Xarch_x86_64 -Xarch_arm64 ", ""
   end
 
   test do
