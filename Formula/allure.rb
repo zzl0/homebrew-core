1ef172da36c6ded02204954e68f505ac60fecfdc
Porkepix <porkepix@gmail.com>
2023-06-14 00:34:03 +0200

    allure 2.22.2

--- Formula/allure.rb
+++ Formula/allure.rb
@@ -1,8 +1,8 @@
 class Allure < Formula
   desc "Flexible lightweight test report tool"
   homepage "https://github.com/allure-framework/allure2"
-  url "https://repo.maven.apache.org/maven2/io/qameta/allure/allure-commandline/2.22.1/allure-commandline-2.22.1.zip"
-  sha256 "fdc7a62d94b14c5e0bf25198ae1feded6b005fdbed864b4d3cb4e5e901720b0b"
+  url "https://repo.maven.apache.org/maven2/io/qameta/allure/allure-commandline/2.22.2/allure-commandline-2.22.2.zip"
+  sha256 "eb5e3be830fd5bbbffe457aa7170293ec4fdebd24ab13918bcba5b69f0d7f44a"
   license "Apache-2.0"
 
   livecheck do
