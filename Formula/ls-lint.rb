d90d947737920a8d57ebc0c744f9f5d27a53b68c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-06-13 04:17:32 +0000

    ls-lint: add 2.0.0 bottle.

--- Formula/ls-lint.rb
+++ Formula/ls-lint.rb
@@ -6,6 +6,16 @@ class LsLint < Formula
   license "MIT"
   head "https://github.com/loeffel-io/ls-lint.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "01e7397d278be5b9d956b5e93a58a279ddc1c41c496afd759b3b8f0c6e336613"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "01e7397d278be5b9d956b5e93a58a279ddc1c41c496afd759b3b8f0c6e336613"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "01e7397d278be5b9d956b5e93a58a279ddc1c41c496afd759b3b8f0c6e336613"
+    sha256 cellar: :any_skip_relocation, ventura:        "b827045da76c16c11dc99fad2b7ad5f3c3b6caf33a88708dd80f52edc16371c3"
+    sha256 cellar: :any_skip_relocation, monterey:       "b827045da76c16c11dc99fad2b7ad5f3c3b6caf33a88708dd80f52edc16371c3"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b827045da76c16c11dc99fad2b7ad5f3c3b6caf33a88708dd80f52edc16371c3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "65fe7d5d01f198e714f91e8b743c40b39587c0d9850333380ac888b3afcabc54"
+  end
+
   depends_on "go" => :build
 
   def install
