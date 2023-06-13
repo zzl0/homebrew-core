49468decf7c47e946a3938a3ef3023fe89af5680
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-06-13 14:50:33 +0000

    benthos: update 4.17.0 bottle.

--- Formula/benthos.rb
+++ Formula/benthos.rb
@@ -7,13 +7,13 @@ class Benthos < Formula
   head "https://github.com/benthosdev/benthos.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "1c656fa29e1cb71f8b12eed47d0b6bb10b3eff2f78ba30c0e7e7f3195469cd86"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "44510e956bfaa10c0efdafe6b37ffbb051b6b37257bcfbbd433e03e16d314119"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ad1a17669a35963eab5f662a1550d1f4d46ff8aae16ca8d2f3d3fb9c002d7363"
-    sha256 cellar: :any_skip_relocation, ventura:        "c700a4c686f3637f4bff31ce2d029fbde7db3875e98595033925662c62dff159"
-    sha256 cellar: :any_skip_relocation, monterey:       "e9795e1e0fb4dd012d807b4dfadbf6b7c68b1a5e72d027e9ece4c064378ddb84"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0804e961b86247fe65995005877e0e961e388f6242755a07390ea9609ed466bf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "78a7f0d912af7e4213eea0f97d719757b07e691c6f29e34d45b26cd875d38de0"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "073ea6f81ecab3a2148afaf4e825bd4a0654ac22a7d3f50be7883129cb9b366a"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "eae5170b0ce6426f348ceab337163a1ff300c0679d2e056c797660fa9ae118b6"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d2b0b7aab1105086de82760927c233b1e978c8235e266539ec8fcd7c64ce1e3b"
+    sha256 cellar: :any_skip_relocation, ventura:        "6334601dff65749b3659e4820c5b369ef74304bb53a2792eef8385ded5b7482a"
+    sha256 cellar: :any_skip_relocation, monterey:       "b65e85d199a6b511965b15c04e84258d25916b107bccd0b9bf81bf0825e9ac0b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "3902d5b0351af20f316d9f7dfb576f5b5e819f7ebfef8c28ebaae31f72778f52"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fe1e77ac416cd26ab38d0c2e02b79f05d8999d9ec67b404a586e34624e3e6fa0"
   end
 
   depends_on "go" => :build
