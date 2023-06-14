4f1a786c00208f9dd823b90b059d091bdedb8d4a
Rui Chen <rui@chenrui.dev>
2023-06-14 14:23:58 -0400

    git-codereview 1.3.0

--- Formula/git-codereview.rb
+++ Formula/git-codereview.rb
@@ -1,8 +1,8 @@
 class GitCodereview < Formula
   desc "Tool for working with Gerrit code reviews"
   homepage "https://pkg.go.dev/golang.org/x/review/git-codereview"
-  url "https://github.com/golang/review/archive/refs/tags/v1.2.0.tar.gz"
-  sha256 "2151eb3ea0a288b7f65489b8bbc835d2ee52e38d202171aa9a183ff53664e7f0"
+  url "https://github.com/golang/review/archive/refs/tags/v1.3.0.tar.gz"
+  sha256 "7b522916c31d012c4eb0ee2c1138f92f2692ec76f7caadd981c7fa15f237bbf4"
   license "BSD-3-Clause"
 
   bottle do
