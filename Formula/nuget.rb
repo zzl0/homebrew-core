ff122b422d055b7103fea15411078f0e84b7ef93
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-06-14 02:04:48 +0000

    nuget: update 6.6.1 bottle.

--- Formula/nuget.rb
+++ Formula/nuget.rb
@@ -11,12 +11,12 @@ class Nuget < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "1e2d8ab4f8fc85dd381657e02af4aa7dfea4a1e527fcd79a164f5e5297be3338"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1e2d8ab4f8fc85dd381657e02af4aa7dfea4a1e527fcd79a164f5e5297be3338"
-    sha256 cellar: :any_skip_relocation, ventura:        "1e2d8ab4f8fc85dd381657e02af4aa7dfea4a1e527fcd79a164f5e5297be3338"
-    sha256 cellar: :any_skip_relocation, monterey:       "1e2d8ab4f8fc85dd381657e02af4aa7dfea4a1e527fcd79a164f5e5297be3338"
-    sha256 cellar: :any_skip_relocation, big_sur:        "1e2d8ab4f8fc85dd381657e02af4aa7dfea4a1e527fcd79a164f5e5297be3338"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "11616721cea5ce9d7707e7c2965ead699197edc7eb55c54ee7e659d5192379dc"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "dc7238fddb23eb45fe55703630fb0605c1b75f7ec100636e70e9f271b710cb82"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "dc7238fddb23eb45fe55703630fb0605c1b75f7ec100636e70e9f271b710cb82"
+    sha256 cellar: :any_skip_relocation, ventura:        "dc7238fddb23eb45fe55703630fb0605c1b75f7ec100636e70e9f271b710cb82"
+    sha256 cellar: :any_skip_relocation, monterey:       "dc7238fddb23eb45fe55703630fb0605c1b75f7ec100636e70e9f271b710cb82"
+    sha256 cellar: :any_skip_relocation, big_sur:        "dc7238fddb23eb45fe55703630fb0605c1b75f7ec100636e70e9f271b710cb82"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5e12d1767a00632aacf3f7accee284e875a50282a0558e04a0e5b4ab09d73910"
   end
 
   depends_on "mono"
