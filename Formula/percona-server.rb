4bf6ba9f547b60f91dc119e1dbc43906641205f6
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2023-06-15 12:42:10 +0800

    percona-server: revision bump (icu4c 73.2)

--- Formula/percona-server.rb
+++ Formula/percona-server.rb
@@ -5,7 +5,7 @@ class PerconaServer < Formula
   url "https://downloads.percona.com/downloads/Percona-Server-8.0/Percona-Server-8.0.32-24/source/tarball/percona-server-8.0.32-24.tar.gz"
   sha256 "2867706e914597cb3a5161751573c5463caf8343684ed7eeafcad1eb8f2d081e"
   license "BSD-3-Clause"
-  revision 1
+  revision 2
 
   livecheck do
     url "https://www.percona.com/downloads/Percona-Server-LATEST/"
