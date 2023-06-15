8cb594c239ce1326d1808b300012b8a62add53e2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-06-15 17:32:04 +0000

    stress-ng: update 0.15.10 bottle.

--- Formula/stress-ng.rb
+++ Formula/stress-ng.rb
@@ -6,13 +6,13 @@ class StressNg < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3b4035286661f27afd82b24c45a1c82bd999cfb44afc91ec84f45f29add10edd"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f7d08ab8a9a83039311a29316ebce35fb7074e396d875aa9b40ce6f3272813a2"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "264cf56147d3f898ca16d4c584e279cbd80213fb622f9e96a911fe092d04f386"
-    sha256 cellar: :any_skip_relocation, ventura:        "89e9603b2df313f49f5e616941f6461a89ca93ad3b1bd5b45822bb8abc16d9e2"
-    sha256 cellar: :any_skip_relocation, monterey:       "e8c9ef28cace8ec8395904ff76803ca001bc12712e88c694aac08d703fd98e5c"
-    sha256 cellar: :any_skip_relocation, big_sur:        "719fdd53910b2dc084ea32b43ef0d5c8e0bb1acef4c28ea20636d86cee157a2e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "033b7150ad3fc1ab6413eb0791cfdf6590f62e6f8469d72d832bf229e9c1b38b"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0d351f22d2b5da9c363754fe164f5fb31bb9f8223e48157430bc2922a624a9ab"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "96c6a3cb467d4bb81abbc599c850765e5bd8af9867d0e0816e958f632225e469"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7ec9ccbdfbe22d2dfb6628a206dcc75daf02738cee920725ea8f9a3ec02a2bee"
+    sha256 cellar: :any_skip_relocation, ventura:        "b998ea9521db8491e72f9126477eb86e66e06db5817c7a1b6a7cd4b8cf19d1ad"
+    sha256 cellar: :any_skip_relocation, monterey:       "b0b75bfcbd6e817866977167c37ae8f0e49d98c620fcda57114f31b23cacefec"
+    sha256 cellar: :any_skip_relocation, big_sur:        "696960621259ecd4fc21bd0636893d01185f7c8871082d6193ba5ab41492f0a7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c9df5fa69efc3466853344071853df425e3a5b9436455f51100efc5942fb09f6"
   end
 
   depends_on macos: :sierra
