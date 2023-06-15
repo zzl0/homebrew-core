c7cd4e7f44f68e1b23212a37243355535f55e58f
circleci <ops+deploy-bot@circleci.com>
2023-06-15 14:28:13 +0000

    circleci 0.1.27054

--- Formula/circleci.rb
+++ Formula/circleci.rb
@@ -3,8 +3,8 @@ class Circleci < Formula
   homepage "https://circleci.com/docs/2.0/local-cli/"
   # Updates should be pushed no more frequently than once per week.
   url "https://github.com/CircleCI-Public/circleci-cli.git",
-      tag:      "v0.1.26896",
-      revision: "2b4f80b58ab8f2107377134473e87affcf98ef3c"
+      tag:      "v0.1.27054",
+      revision: "68a3d97d39be29235867f926a683ab8601c48d09"
   license "MIT"
   head "https://github.com/CircleCI-Public/circleci-cli.git", branch: "master"
 
