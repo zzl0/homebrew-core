3abc3a60950e5c83121bbaf65331a9e6e2dfbbde
Rui Chen <rui@chenrui.dev>
2023-06-14 21:05:48 -0400

    fheroes2 1.0.5

--- Formula/fheroes2.rb
+++ Formula/fheroes2.rb
@@ -1,8 +1,8 @@
 class Fheroes2 < Formula
   desc "Free Heroes of Might and Magic II is a recreation of HoMM2 game engine"
   homepage "https://ihhub.github.io/fheroes2/"
-  url "https://github.com/ihhub/fheroes2/archive/1.0.4.tar.gz"
-  sha256 "2fe965fcd069618c0bf4e31a560d90345f4854feec0b36a785c3417dcd7e05d1"
+  url "https://github.com/ihhub/fheroes2/archive/1.0.5.tar.gz"
+  sha256 "53d889bb712cede9ad85d9fbaa09338b06c6948b247221464dd5b767a95f12c6"
   license "GPL-2.0-or-later"
   head "https://github.com/ihhub/fheroes2.git", branch: "master"
 
