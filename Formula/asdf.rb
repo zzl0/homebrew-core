d9c2420a5c51d5a8d01ed629724cbf29761c13b9
Rui Chen <rui@chenrui.dev>
2023-06-14 20:31:21 -0400

    asdf 0.12.0

--- Formula/asdf.rb
+++ Formula/asdf.rb
@@ -1,8 +1,8 @@
 class Asdf < Formula
   desc "Extendable version manager with support for Ruby, Node.js, Erlang & more"
   homepage "https://asdf-vm.com/"
-  url "https://github.com/asdf-vm/asdf/archive/refs/tags/v0.11.3.tar.gz"
-  sha256 "344cf4489180fc6409feba60b0c02a12df54eef470422b3f75bb757456b60bee"
+  url "https://github.com/asdf-vm/asdf/archive/refs/tags/v0.12.0.tar.gz"
+  sha256 "8416d3556f093b3f2f9931b30165efd8800c520c129bed22a537f889aa6dff90"
   license "MIT"
   head "https://github.com/asdf-vm/asdf.git", branch: "master"
 
