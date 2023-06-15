b5b4239497b662dad7e2f6c3a3136c31387e25e1
Patrick Linnane <patrick@linnane.io>
2023-06-15 08:41:21 -0700

    aliddns 0.0.14

--- Formula/aliddns.rb
+++ Formula/aliddns.rb
@@ -2,8 +2,8 @@ class Aliddns < Formula
   desc "Aliyun(Alibaba Cloud) ddns for golang"
   homepage "https://github.com/OpenIoTHub/aliddns"
   url "https://github.com/OpenIoTHub/aliddns.git",
-      tag:      "v0.0.13",
-      revision: "2c2214baf6b016ded184373252cff16bb377d3c0"
+      tag:      "v0.0.14",
+      revision: "b257133019c0dc01dd3c51170029acefd42d3561"
   license "MIT"
   head "https://github.com/OpenIoTHub/aliddns.git", branch: "master"
 
