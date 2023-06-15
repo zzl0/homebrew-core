822b99a1c41731e4db24480983dc01f668c4e686
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-06-15 12:18:14 +0000

    gobackup: update 2.4.0 bottle.

--- Formula/gobackup.rb
+++ Formula/gobackup.rb
@@ -7,13 +7,13 @@ class Gobackup < Formula
   head "https://github.com/gobackup/gobackup.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a699ac41d4feedf4f0f2c94db61f159dcfd8c48129d4132a3b3bff06c3d98e26"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a699ac41d4feedf4f0f2c94db61f159dcfd8c48129d4132a3b3bff06c3d98e26"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a699ac41d4feedf4f0f2c94db61f159dcfd8c48129d4132a3b3bff06c3d98e26"
-    sha256 cellar: :any_skip_relocation, ventura:        "8b23fffb65a9101b31e577bc63edcc494f76266ac43660e74b403b51e71b26f8"
-    sha256 cellar: :any_skip_relocation, monterey:       "8b23fffb65a9101b31e577bc63edcc494f76266ac43660e74b403b51e71b26f8"
-    sha256 cellar: :any_skip_relocation, big_sur:        "8b23fffb65a9101b31e577bc63edcc494f76266ac43660e74b403b51e71b26f8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d8392376de4eabf3f8c666f8c3d9b2f67660427a53f2f25acde1885d639aefb5"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3a857622c7c640a66cee5c7d68d8c24bddf8bc7639e6539cd9882591461bb536"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3a857622c7c640a66cee5c7d68d8c24bddf8bc7639e6539cd9882591461bb536"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3a857622c7c640a66cee5c7d68d8c24bddf8bc7639e6539cd9882591461bb536"
+    sha256 cellar: :any_skip_relocation, ventura:        "6673836cefcc6f1adb32cabaa1ed7024580b9af4d94b944dd884a94571191641"
+    sha256 cellar: :any_skip_relocation, monterey:       "6673836cefcc6f1adb32cabaa1ed7024580b9af4d94b944dd884a94571191641"
+    sha256 cellar: :any_skip_relocation, big_sur:        "6673836cefcc6f1adb32cabaa1ed7024580b9af4d94b944dd884a94571191641"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fd118a2ff8092d2adce4428bf33930917deac5ef9e8bf9ae0cdc473942cde037"
   end
 
   depends_on "go" => :build
