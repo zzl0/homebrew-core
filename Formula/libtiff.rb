7b02a4a585a0e828590c508cf058c0b26a86cd41
Porkepix <porkepix@gmail.com>
2023-06-16 09:46:59 +0200

    libtiff 4.5.1

--- Formula/libtiff.rb
+++ Formula/libtiff.rb
@@ -1,9 +1,9 @@
 class Libtiff < Formula
   desc "TIFF library and utilities"
   homepage "https://libtiff.gitlab.io/libtiff/"
-  url "https://download.osgeo.org/libtiff/tiff-4.5.0.tar.gz"
-  mirror "https://fossies.org/linux/misc/tiff-4.5.0.tar.gz"
-  sha256 "c7a1d9296649233979fa3eacffef3fa024d73d05d589cb622727b5b08c423464"
+  url "https://download.osgeo.org/libtiff/tiff-4.5.1.tar.gz"
+  mirror "https://fossies.org/linux/misc/tiff-4.5.1.tar.gz"
+  sha256 "d7f38b6788e4a8f5da7940c5ac9424f494d8a79eba53d555f4a507167dca5e2b"
   license "libtiff"
 
   livecheck do
