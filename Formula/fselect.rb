af9577771c91b86fad10777e71b33eeeccec1b56
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-06-16 17:35:52 +0000

    fselect: update 0.8.4 bottle.

--- Formula/fselect.rb
+++ Formula/fselect.rb
@@ -6,13 +6,13 @@ class Fselect < Formula
   license any_of: ["Apache-2.0", "MIT"]
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "972128acffa16c8a965086e80ad4b26e287bc9cd9a98fd493f87d203eac0e148"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "bcfe84da0b9ee7d01af51de754542c1e5bfacb4339caf3426b26e6addd5b9322"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "349e706fd7754a51ccf3496cbfc9cf96a8910811d4aff1a22f2edf03059b8864"
-    sha256 cellar: :any_skip_relocation, ventura:        "4ebdff903e2ab3f51c4888d095b95041a43689c39ac8cafde77a77b7046eab03"
-    sha256 cellar: :any_skip_relocation, monterey:       "5495bdd64c48dffb26d4b0cb26642ffc39fe852180696a55eab8264e4a37fa49"
-    sha256 cellar: :any_skip_relocation, big_sur:        "32f7a9ead32e427bb95ae256c7725b4b85ae429c19304ccd29cd169fd810079e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "59b62c16737ea8f6d2159ce0cfd274c2a4d0c70474779a44c2ee47851b6d08a2"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "de733f375d9bc4102ae9cc5df84b687a71f0bb1906ab7509ac0f0c94fcabe719"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ce1fccb27a86677f00d118ca3f3a571495c57613aa35992576336f2dbbbc6a50"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bd1061f80d85c8e083b77b290fcde9b7b5cfc94a8b38ffd8a657cdb3f3c35c38"
+    sha256 cellar: :any_skip_relocation, ventura:        "e1585c19d310ec08fa643b2e18d674044cb64968affd22b0fc1fd1f0467408a3"
+    sha256 cellar: :any_skip_relocation, monterey:       "061426ee1e45f67667c47d3ae29ecbb5c132babec1e7588f8e8c0eaa8bcaf879"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d45dc50de51c3da6a21eeba921290cfdba1771dc6e4d54e8b618cb952a1494e4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "55c906233ccf466b84826bb6d0f83dd08d5d721e305ddb9980c3648a0487d381"
   end
 
   depends_on "rust" => :build
