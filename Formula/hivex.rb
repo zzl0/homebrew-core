f66f94224379c4cfbe48bd18b4bcfda5ed13cde6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-06-17 05:32:47 +0000

    hivex: add 1.3.23 bottle.

--- Formula/hivex.rb
+++ Formula/hivex.rb
@@ -10,6 +10,16 @@ class Hivex < Formula
     regex(/href=.*?hivex[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
+  bottle do
+    sha256 cellar: :any, arm64_ventura:  "5b762d9899d0ae209079abae74a4f18d7e9f29824cf7c6c5144f6626d87debba"
+    sha256 cellar: :any, arm64_monterey: "ad381758881bcfa6a41864c472eacd4476d7a21ddef776b78d285d7139cb93c9"
+    sha256 cellar: :any, arm64_big_sur:  "837c408efabee26bb2edb3f354bfcff31987b57d1275acd0988bd2cd1ed0a5f8"
+    sha256 cellar: :any, ventura:        "65a4a0fb292c7d4989e08984b39470077a24950d75b54e18145b258d97afa28f"
+    sha256 cellar: :any, monterey:       "cc5e7f4536be0fe9560b4b1808c9015754d3e81d530866f7c349463b25d0479e"
+    sha256 cellar: :any, big_sur:        "758e1d6c28c1dfbe725b99fe7f3fe339ff8b1150f5c85e3f9368a5e1386aad45"
+    sha256               x86_64_linux:   "b080edd6e833144a7e66f2346171dd28a392cfec2013979bc4b6dee9c4c93156"
+  end
+
   depends_on "pkg-config" => :build
   depends_on "gettext"
   depends_on "readline"
