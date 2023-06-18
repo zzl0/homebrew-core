8ead8dc94c3ccf58408ee7db5b0b832a04b42505
rami3l <rami3l@outlook.com>
2023-06-18 12:47:15 +0800

    cargo-release: remove `rust` dependency except for building

--- Formula/cargo-release.rb
+++ Formula/cargo-release.rb
@@ -16,16 +16,47 @@ class CargoRelease < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "d63c11ca314240c51f31f66765f8daa1dcfd8ba9a7feb01de8eff9ec21096da1"
   end
 
-  depends_on "rust" # uses `cargo` at runtime
+  depends_on "pkg-config" => :build
+  depends_on "rust" => :build
+  depends_on "rustup-init" => :test
+  depends_on "libgit2@1.5"
+  depends_on "openssl@1.1"
 
   def install
-    system "cargo", "install", *std_cargo_args
+    ENV["LIBGIT2_SYS_USE_PKG_CONFIG"] = "1"
+    ENV["LIBSSH2_SYS_USE_PKG_CONFIG"] = "1"
+    ENV["OPENSSL_DIR"] = Formula["openssl@1.1"].opt_prefix
+    ENV["OPENSSL_NO_VENDOR"] = "1"
+    system "cargo", "install", "--no-default-features", *std_cargo_args
+  end
+
+  def check_binary_linkage(binary, library)
+    binary.dynamically_linked_libraries.any? do |dll|
+      next false unless dll.start_with?(HOMEBREW_PREFIX.to_s)
+
+      File.realpath(dll) == File.realpath(library)
+    end
   end
 
   test do
+    # Show that we can use a different toolchain than the one provided by the `rust` formula.
+    # https://github.com/Homebrew/homebrew-core/pull/134074#pullrequestreview-1484979359
+    ENV["RUSTUP_INIT_SKIP_PATH_CHECK"] = "yes"
+    system "#{Formula["rustup-init"].bin}/rustup-init", "-y", "--no-modify-path"
+    ENV.prepend_path "PATH", HOMEBREW_CACHE/"cargo_cache/bin"
+    system "rustup", "default", "beta"
+
     system "cargo", "new", "hello_world", "--bin"
     cd "hello_world" do
       assert_match "tag = true", shell_output("cargo release config 2>&1").chomp
+    end
+
+    [
+      Formula["libgit2@1.5"].opt_lib/shared_library("libgit2"),
+      Formula["openssl@1.1"].opt_lib/shared_library("libssl"),
+    ].each do |library|
+      assert check_binary_linkage(bin/"cargo-release", library),
+             "No linkage with #{library.basename}! Cargo is likely using a vendored version."
     end
   end
 end
