dbc4f898e76f2df05ad01cdd9d4a6fe8a3be82b4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-06-18 15:56:30 +0000

    renovate: update 35.128.0 bottle.

--- Formula/renovate.rb
+++ Formula/renovate.rb
@@ -19,13 +19,13 @@ class Renovate < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "e9604e8ef4fff6a80f70487e48f98b682054eea83af404ebe03c355f611ac18f"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "6f1ca97c830cedbba011a3162ea0243dced30433e60d0577b021d20afbe7fb45"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c28cb8533f135cc23c9cb8b2e62c2f0f2a930360708f922383d92b93ec786c03"
-    sha256 cellar: :any_skip_relocation, ventura:        "54bc7948e1e5a21685bb385f2f022c9a0b5644278b7bc71398fbac93e9594f00"
-    sha256 cellar: :any_skip_relocation, monterey:       "643dd26535e953e811a3593e776210c4e40b779a283757ae0f2f19dfff8deb1a"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f1920c34108571c64b6e53d361653b59f9a5a2efa37502b102154a8da2647591"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "827d35c7af679d12c86d4e7273a3b93c342b0a1184290abdcaf7cf6f82ebcdf1"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "99f7a538e0d9494ea2957e7e9338e03fb151b396bb1b5719e871a1dfa05ef7ce"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "1cbb1a2365d0fd42a93025c3010fc3c2d752fd46e38ac55b223cc6a6eb30ce3f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a94c488a6191f2de016bb5050722e6220feafd44b2afb7a5df09fd4cd4fe1c65"
+    sha256 cellar: :any_skip_relocation, ventura:        "a9adc59dc37cb1b34ddb43b91d901d4c7df5c9b7b249c8eedeaa0ddbe6999442"
+    sha256 cellar: :any_skip_relocation, monterey:       "cf6e4af31e9f54c4ecc79dfc1a174f17ed8bead7756340f3a492a9e98bd7dcb7"
+    sha256 cellar: :any_skip_relocation, big_sur:        "cc8ca7b030c7df4b766dce6ab930e98d41254a5a7ba2db84eba2d520e13f6626"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6e2441bfcd94a38b50d63f6b96a604e99664d9645637433b606d2c1402aad7d7"
   end
 
   depends_on "node"
