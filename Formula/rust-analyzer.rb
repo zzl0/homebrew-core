ef1dc90d908171bacf6295f29dfc3ce533d6a6a5
Porkepix <porkepix@gmail.com>
2023-06-19 12:29:36 +0200

    rust-analyzer 2023-06-19

--- Formula/rust-analyzer.rb
+++ Formula/rust-analyzer.rb
@@ -2,9 +2,9 @@ class RustAnalyzer < Formula
   desc "Experimental Rust compiler front-end for IDEs"
   homepage "https://rust-analyzer.github.io/"
   url "https://github.com/rust-lang/rust-analyzer.git",
-       tag:      "2023-06-12",
-       revision: "d567091f472a6c2b55262e25d092f1d43a7a4fae"
-  version "2023-06-12"
+       tag:      "2023-06-19",
+       revision: "cd3bf9fe51676b520c546460e6d8919b8c8ff99f"
+  version "2023-06-19"
   license any_of: ["Apache-2.0", "MIT"]
 
   bottle do
