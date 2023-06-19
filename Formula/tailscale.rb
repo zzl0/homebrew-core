242337aa3580ec978b3a1740b0bbfad9f7fe7975
Patrick Linnane <patrick@linnane.io>
2023-06-19 15:51:13 -0700

    tailscale 1.42.1

--- Formula/tailscale.rb
+++ Formula/tailscale.rb
@@ -2,8 +2,8 @@ class Tailscale < Formula
   desc "Easiest, most secure way to use WireGuard and 2FA"
   homepage "https://tailscale.com"
   url "https://github.com/tailscale/tailscale.git",
-      tag:      "v1.42.0",
-      revision: "ab797f0abd067750d474668ed615d7dc9d258cec"
+      tag:      "v1.42.1",
+      revision: "f33400c5d4812b1141bae1a69d665dc91eec4095"
   license "BSD-3-Clause"
 
   bottle do
