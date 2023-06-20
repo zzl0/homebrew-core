fcadf56e6d3749d64494e16a1ae4fc67c8ac6498
Patrick Linnane <patrick@linnane.io>
2023-06-20 08:26:33 -0700

    bgpq4 1.11

--- Formula/bgpq4.rb
+++ Formula/bgpq4.rb
@@ -1,8 +1,8 @@
 class Bgpq4 < Formula
   desc "BGP filtering automation for Cisco, Juniper, BIRD and OpenBGPD routers"
   homepage "https://github.com/bgp/bgpq4"
-  url "https://github.com/bgp/bgpq4/archive/refs/tags/1.10.tar.gz"
-  sha256 "189e36108651c2009168282011d2e5cc8fa9179f808cf1a621c7d911fcc0e571"
+  url "https://github.com/bgp/bgpq4/archive/refs/tags/1.11.tar.gz"
+  sha256 "0e5325633f607e00262a6b96d1d246a9d27b4b869bbcf582a46e8a43c5fa4e18"
   license "BSD-2-Clause"
   head "https://github.com/bgp/bgpq4.git", branch: "main"
 
