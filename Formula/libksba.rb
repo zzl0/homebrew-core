df7435eb3fa023f3fa1684b802a4627ff56d26f2
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2023-06-21 02:44:08 +0800

    libksba: improve test
    The existing test block doesn't test anything important, so let's fix
    that.
    
    Also, since this is a cryptography library, let's run `make check`.

--- Formula/libksba.rb
+++ Formula/libksba.rb
@@ -26,13 +26,27 @@ class Libksba < Formula
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}"
+    system "make", "check"
     system "make", "install"
 
     # avoid triggering mandatory rebuilds of software that hard-codes this path
-    inreplace bin/"ksba-config", prefix, opt_prefix
+    inreplace [bin/"ksba-config", lib/"pkgconfig/ksba.pc"], prefix, opt_prefix
   end
 
   test do
-    system "#{bin}/ksba-config", "--libs"
+    (testpath/"ksba-test.c").write <<~C
+      #include "ksba.h"
+      #include <stdio.h>
+      int main() {
+        printf("%s", ksba_check_version(NULL));
+        return 0;
+      }
+    C
+
+    ENV.append_to_cflags shell_output("#{bin}/ksba-config --cflags").strip
+    ENV.append "LDLIBS", shell_output("#{bin}/ksba-config --libs").strip
+
+    system "make", "ksba-test"
+    assert_equal version.to_s, shell_output("./ksba-test")
   end
 end
