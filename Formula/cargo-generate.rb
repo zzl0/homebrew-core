33168d59c6e25e838ad157db749cb014e4f93c17
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-06-20 23:18:10 +0000

    cargo-generate: add 0.18.3 bottle.

--- Formula/cargo-generate.rb
+++ Formula/cargo-generate.rb
@@ -6,6 +6,16 @@ class CargoGenerate < Formula
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/cargo-generate/cargo-generate.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "96267876fc18cd2aeeb8abed43006fd6b173ed6fa75edaee72722f2a9f1ebd2f"
+    sha256 cellar: :any,                 arm64_monterey: "50d13728fe22dfccda32e2f283891ea4caaf5f66de0e45af8966f446628e076b"
+    sha256 cellar: :any,                 arm64_big_sur:  "10ac1a36f53c717d616eecdd20a685af8b62ec0c337f86a5e17e5404752678a1"
+    sha256 cellar: :any,                 ventura:        "1d0b43580ea388d45b78e9ec6ac6f6deb058c3492557322877011a1ebe2c04d3"
+    sha256 cellar: :any,                 monterey:       "f8e6ff39f3c70b44de632745c9602f17bc8460a4f92d8363c7d45e1838a90385"
+    sha256 cellar: :any,                 big_sur:        "198efa2b66cbbf71e4fa834a0448b2efaab6e66079a4ab9de8af229c0c38f5f5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c3b748fd77898ed192c4443821d5eea3d3dfc6566e23eb60e9b7356a63362fb1"
+  end
+
   depends_on "pkg-config" => :build
   depends_on "rust" => :build
   depends_on "libgit2"
