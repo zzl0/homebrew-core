f44902dbe969dbaeede1904afeb3b51e2e0d80a7
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2023-06-21 05:40:21 +0800

    cyrus-sasl: switch to `openssl@3`

--- Formula/cyrus-sasl.rb
+++ Formula/cyrus-sasl.rb
@@ -4,7 +4,7 @@ class CyrusSasl < Formula
   url "https://github.com/cyrusimap/cyrus-sasl/releases/download/cyrus-sasl-2.1.28/cyrus-sasl-2.1.28.tar.gz"
   sha256 "7ccfc6abd01ed67c1a0924b353e526f1b766b21f42d4562ee635a8ebfc5bb38c"
   license "BSD-3-Clause-Attribution"
-  revision 1
+  revision 2
 
   bottle do
     sha256 arm64_ventura:  "07b4c2870d17ea1e775451a698f0f15a72c845fb9eee0a1db44704369cc5d5ab"
@@ -20,7 +20,7 @@ class CyrusSasl < Formula
   keg_only :provided_by_macos
 
   depends_on "krb5"
-  depends_on "openssl@1.1"
+  depends_on "openssl@3"
 
   uses_from_macos "libxcrypt"
 
