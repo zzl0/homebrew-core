171be29051bc90de2e2922c5ce2467e769294a20
Minsoo Choo <minsoochoo0122@proton.me>
2023-06-20 21:24:30 -0400

    mariadb@10.10: switch to `openssl@3`

--- Formula/mariadb@10.10.rb
+++ Formula/mariadb@10.10.rb
@@ -4,6 +4,7 @@ class MariadbAT1010 < Formula
   url "https://downloads.mariadb.com/MariaDB/mariadb-10.10.5/source/mariadb-10.10.5.tar.gz"
   sha256 "91cd4d426d38af09852ebdaf00e74211775ec6cc907f74c1aee0d45cdf5d996b"
   license "GPL-2.0-only"
+  revision 1
 
   # This uses a placeholder regex to satisfy the `PageMatch` strategy
   # requirement. In the future, this will be updated to use a `Json` strategy
@@ -43,7 +44,7 @@ class MariadbAT1010 < Formula
   depends_on "fmt" => :build
   depends_on "pkg-config" => :build
   depends_on "groonga"
-  depends_on "openssl@1.1"
+  depends_on "openssl@3"
   depends_on "pcre2"
   depends_on "zstd"
 
