3a5414b78e6790253c558d68715e116d37e3ca7d
Porkepix <porkepix@gmail.com>
2023-06-21 12:32:24 +0200

    distrobox 1.5.0.1

--- Formula/distrobox.rb
+++ Formula/distrobox.rb
@@ -1,8 +1,8 @@
 class Distrobox < Formula
   desc "Use any Linux distribution inside your terminal"
   homepage "https://distrobox.privatedns.org/"
-  url "https://github.com/89luca89/distrobox/archive/refs/tags/1.5.0.tar.gz"
-  sha256 "a33c54270898118fc8f31bd954637814160c399f656d836d36d6e4e8c1b620a0"
+  url "https://github.com/89luca89/distrobox/archive/refs/tags/1.5.0.1.tar.gz"
+  sha256 "72d7896ac9b669622a74d683c9f1d4c6343dfe7f6f4dc6cb4eefee6f375fadd6"
   license "GPL-3.0-only"
   head "https://github.com/89luca89/distrobox.git", branch: "main"
 
