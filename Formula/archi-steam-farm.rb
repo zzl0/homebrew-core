debe48d9df7ec8ab540efface5dfb73f5cbe9fdc
Rui Chen <rui@chenrui.dev>
2023-06-21 12:55:25 -0400

    archi-steam-farm 5.4.7.2

--- Formula/archi-steam-farm.rb
+++ Formula/archi-steam-farm.rb
@@ -2,8 +2,8 @@ class ArchiSteamFarm < Formula
   desc "Application for idling Steam cards from multiple accounts simultaneously"
   homepage "https://github.com/JustArchiNET/ArchiSteamFarm"
   url "https://github.com/JustArchiNET/ArchiSteamFarm.git",
-      tag:      "5.4.6.3",
-      revision: "0c85c1413d9367be23e10ecc95c5dace9542308b"
+      tag:      "5.4.7.2",
+      revision: "7132340ff7e8b12fa810e1154eefe00bfbb41f4b"
   license "Apache-2.0"
   head "https://github.com/JustArchiNET/ArchiSteamFarm.git", branch: "main"
 
