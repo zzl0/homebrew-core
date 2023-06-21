534ab443379bfb2118301b671c072d7877c6e665
Rui Chen <rui@chenrui.dev>
2023-06-21 01:11:29 -0400

    sevenzip 23.01

--- Formula/sevenzip.rb
+++ Formula/sevenzip.rb
@@ -1,9 +1,9 @@
 class Sevenzip < Formula
   desc "7-Zip is a file archiver with a high compression ratio"
   homepage "https://7-zip.org"
-  url "https://7-zip.org/a/7z2201-src.tar.xz"
-  version "22.01"
-  sha256 "393098730c70042392af808917e765945dc2437dee7aae3cfcc4966eb920fbc5"
+  url "https://7-zip.org/a/7z2301-src.tar.xz"
+  version "23.01"
+  sha256 "356071007360e5a1824d9904993e8b2480b51b570e8c9faf7c0f58ebe4bf9f74"
   license all_of: ["LGPL-2.1-or-later", "BSD-3-Clause"]
 
   livecheck do
