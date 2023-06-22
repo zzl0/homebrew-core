1bbf2fdca2d1cfd6b100fb68f43045468a348cb1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-06-22 22:47:31 +0000

    zrok: add 0.3.6 bottle.

--- Formula/zrok.rb
+++ Formula/zrok.rb
@@ -8,6 +8,16 @@ class Zrok < Formula
   license "Apache-2.0"
   head "https://github.com/openziti/zrok.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "cd3a231757b5273262a11022f6de1ce152e3525a30389516df7cede0680fa04b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e58251c7477f402db5dc4c3dc6bec4e16f38017f19c3cb936432e76df0899898"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e756df2133c1a3b5245b3203fd66f4107ff1b6760bcb4f0688db7ef455396670"
+    sha256 cellar: :any_skip_relocation, ventura:        "f41d5c00b3a8c8e288b2765ac18ed88c31531fb824c3bed33a017e464bad552f"
+    sha256 cellar: :any_skip_relocation, monterey:       "ddcd1ebbd8666e84e6ca8f7bfbf0ddf7045e41001d3048b04912d4248cfda5cf"
+    sha256 cellar: :any_skip_relocation, big_sur:        "b962ef81e15b8327dfdf2299b15da31c3ff336bd98e41987072eb33d436dcfbe"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ed6026a235ed0d6da509cb1ab040d2a4283f20c76a4ac10f7a957549fcded45c"
+  end
+
   depends_on "go" => :build
   depends_on "node" => :build
 
