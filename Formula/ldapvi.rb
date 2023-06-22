c875c92c48192f2031859648a3eebe1ccae16919
Rui Chen <rui@chenrui.dev>
2023-06-21 23:56:16 -0400

    ldapvi: switch to `openssl@3`
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/ldapvi.rb
+++ Formula/ldapvi.rb
@@ -5,7 +5,7 @@ class Ldapvi < Formula
   mirror "https://deb.debian.org/debian/pool/main/l/ldapvi/ldapvi_1.7.orig.tar.gz"
   sha256 "6f62e92d20ff2ac0d06125024a914b8622e5b8a0a0c2d390bf3e7990cbd2e153"
   license "GPL-2.0-or-later"
-  revision 8
+  revision 9
 
   livecheck do
     url :homepage
@@ -27,7 +27,7 @@ class Ldapvi < Formula
   depends_on "xz" => :build # Homebrew bug. Shouldn't need declaring explicitly.
   depends_on "gettext"
   depends_on "glib"
-  depends_on "openssl@1.1"
+  depends_on "openssl@3"
   depends_on "popt"
   depends_on "readline"
 
@@ -45,6 +45,9 @@ class Ldapvi < Formula
   end
 
   def install
+    # Workaround for Xcode 14.3.
+    ENV.append_to_cflags "-Wno-implicit-function-declaration"
+
     # Fix compilation with clang by changing `return` to `return 0`.
     inreplace "ldapvi.c", "if (lstat(sasl, &st) == -1) return;",
                           "if (lstat(sasl, &st) == -1) return 0;"
