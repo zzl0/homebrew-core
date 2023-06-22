be15859eec4fe66eb7777d27d71e2e4f7bae273f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-06-22 08:43:24 +0000

    hydra: update 9.5_1 bottle.

--- Formula/hydra.rb
+++ Formula/hydra.rb
@@ -8,13 +8,13 @@ class Hydra < Formula
   head "https://github.com/vanhauser-thc/thc-hydra.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "d3599aadf0811fad47f7f7b1d4971186c71ad4cd4e66ada47186281719c6d6c5"
-    sha256 cellar: :any,                 arm64_monterey: "5bafa12886bbaf89e78c5d0a85597d12eeddcbf721439fac1c5399c941a80f8f"
-    sha256 cellar: :any,                 arm64_big_sur:  "3d55edf004a94b138a0a3c453a774d0f9876ad6e6ac286210008399f50f9e045"
-    sha256 cellar: :any,                 ventura:        "e706864437e9b1e995c991616fcaba4d35b0dabb91bbd615d28b8ea3b3c9628a"
-    sha256 cellar: :any,                 monterey:       "48bee24f5a7687f354c6f5a385b49dede529c1ab5d0f29b815de4e7bc27f2e7e"
-    sha256 cellar: :any,                 big_sur:        "830702b6eaa0e22e83ff92b4a1b0dca79633969b2c535ae9b73d8bf162322206"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a17ee21be0401deebaf16c0bc18cbacdd0f3e441fd60f6bdc3713a27d931e392"
+    sha256 cellar: :any,                 arm64_ventura:  "4233b96be61ab5a51cc06ce000551cdda2930b9908520c4431e7f8a6778fc08c"
+    sha256 cellar: :any,                 arm64_monterey: "d6027630cf3aaa8c88dcb70e0d4f8cb202a1eab7b8b2ad348ff4c60753fcf457"
+    sha256 cellar: :any,                 arm64_big_sur:  "fc87e1cf552aafa0b3948c475b2a2ea5a89feb39572393f80ff17db9623c5e8c"
+    sha256 cellar: :any,                 ventura:        "7166d64116322a253217a1363c32838ff3003d3d2ab70299a9b89e6503278a35"
+    sha256 cellar: :any,                 monterey:       "6a3c2fce671fa1b2ae2ce7291f9300d180ed1595176d6fc7d7c8cbc67530081e"
+    sha256 cellar: :any,                 big_sur:        "d0dce2ce89065f3026e5e07bfd3935e17b4e4e21357a03d77806766bef2766e1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "aa4a0a0ba72ad73ea3d23e84bef1ff6ecb0a88ea3c307c8997a6c9f95af9aa2b"
   end
 
   depends_on "pkg-config" => :build
