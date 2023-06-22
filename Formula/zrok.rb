3fe5577a0f16d0c028fe02d0396755c0351167a5
Cam Otts <otts.cameron@gmail.com>
2023-06-22 09:25:05 -0400

    zrok 0.3.5 (new formula)

---
+++ Formula/zrok.rb
@@ -0,0 +1,39 @@
+require "language/node"
+
+class Zrok < Formula
+  desc "Geo-scale, next-generation sharing platform built on top of OpenZiti"
+  homepage "https://zrok.io"
+  url "https://github.com/openziti/zrok/archive/refs/tags/v0.3.6.tar.gz"
+  sha256 "2543bffb999428abf20ce0f2dec3d63e32ffb624a51255bcc3e76356905b4135"
+  license "Apache-2.0"
+  head "https://github.com/openziti/zrok.git", branch: "main"
+
+  depends_on "go" => :build
+  depends_on "node" => :build
+
+  def install
+    cd buildpath/"ui" do
+      system "npm", "install", *Language::Node.local_npm_install_args
+      system "npm", "run", "build"
+    end
+    ldflags = ["-X github.com/openziti/zrok/build.Version=#{version}",
+               "-X github.com/openziti/zrok/build.Hash=brew"]
+    system "go", "build", *std_go_args(ldflags: ldflags), "github.com/openziti/zrok/cmd/zrok"
+  end
+
+  test do
+    (testpath/"ctrl.yml").write <<~EOS
+      v: 2
+      maintenance:
+        registration:
+          expiration_timeout: 24h
+    EOS
+
+    version_output = shell_output("#{bin}/zrok version")
+    assert_match(version.to_s, version_output)
+    assert_match(/[[a-f0-9]{40}]/, version_output)
+
+    status_output = shell_output("#{bin}/zrok controller validate #{testpath}/ctrl.yml 2>&1")
+    assert_match("expiration_timeout = 24h0m0s", status_output)
+  end
+end
