5054ff5f7c81f153983b90a8e4cb9a1f1b04d389
Patrick Linnane <patrick@linnane.io>
2023-06-23 17:07:31 -0700

    cadaver 0.24

--- Formula/cadaver.rb
+++ Formula/cadaver.rb
@@ -1,12 +1,9 @@
 class Cadaver < Formula
   desc "Command-line client for DAV"
-  homepage "https://directory.fsf.org/wiki/Cadaver"
-  url "https://mirrorservice.org/sites/download.salixos.org/i486/extra-14.2/source/network/cadaver/cadaver-0.23.3.tar.gz"
-  mirror "https://src.fedoraproject.org/repo/pkgs/cadaver/cadaver-0.23.3.tar.gz/502ecd601e467f8b16056d2acca39a6f/cadaver-0.23.3.tar.gz"
-  mirror "https://web.archive.org/web/20170629224036/www.webdav.org/cadaver/cadaver-0.23.3.tar.gz"
-  sha256 "fd4ce68a3230ba459a92bcb747fc6afa91e46d803c1d5ffe964b661793c13fca"
-  license "GPL-2.0-or-later"
-  revision 5
+  homepage "https://notroj.github.io/cadaver/"
+  url "https://notroj.github.io/cadaver/cadaver-0.24.tar.gz"
+  sha256 "46cff2f3ebd32cd32836812ca47bcc75353fc2be757f093da88c0dd8f10fd5f6"
+  license "GPL-2.0-only"
   head "https://github.com/notroj/cadaver.git", branch: "master"
 
   livecheck do
@@ -30,18 +27,15 @@ class Cadaver < Formula
   depends_on "pkg-config" => :build
   depends_on "gettext"
   depends_on "neon"
-  depends_on "openssl@1.1"
+  depends_on "openssl@3"
   depends_on "readline"
 
-  # enable build with the latest neon
-  patch :DATA
-
   def install
-    system "./configure", "--prefix=#{prefix}",
+    system "./configure", *std_configure_args,
                           "--with-ssl=openssl",
-                          "--with-libs=#{Formula["openssl@1.1"].opt_prefix}",
+                          "--with-libs=#{Formula["openssl@3"].opt_prefix}",
                           "--with-neon=#{Formula["neon"].opt_prefix}"
-    system "make", "-C", "lib/intl"
+    system "make"
     system "make", "install"
   end
 
@@ -49,27 +43,3 @@ class Cadaver < Formula
     assert_match "cadaver #{version}", shell_output("#{bin}/cadaver -V", 255)
   end
 end
-
-__END__
---- cadaver-0.23.3-orig/configure	2009-12-16 01:36:26.000000000 +0300
-+++ cadaver-0.23.3/configure	2013-11-04 22:44:00.000000000 +0400
-@@ -10328,7 +10328,7 @@
- $as_echo "$ne_cv_lib_neon" >&6; }
-     if test "$ne_cv_lib_neon" = "yes"; then
-        ne_cv_lib_neonver=no
--       for v in 27 28 29; do
-+       for v in 27 28 29 30 31 32; do
-           case $ne_libver in
-           0.$v.*) ne_cv_lib_neonver=yes ;;
-           esac
-@@ -10975,8 +10975,8 @@
-     fi
- 
- else
--    { $as_echo "$as_me:$LINENO: incompatible neon library version $ne_libver: wanted 0.27 28 29" >&5
--$as_echo "$as_me: incompatible neon library version $ne_libver: wanted 0.27 28 29" >&6;}
-+    { $as_echo "$as_me:$LINENO: incompatible neon library version $ne_libver: wanted 0.27 28 29 30 31 32" >&5
-+$as_echo "$as_me: incompatible neon library version $ne_libver: wanted 0.27 28 29 30 31 32" >&6;}
-     neon_got_library=no
- fi
- 
