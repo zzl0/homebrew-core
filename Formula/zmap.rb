a55c02064d017645f3d4e77d048f367cbae82d01
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-06-24 16:10:04 +0000

    zmap: update 3.0.0 bottle.

--- Formula/zmap.rb
+++ Formula/zmap.rb
@@ -12,15 +12,13 @@ class Zmap < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 arm64_ventura:  "379a8920b68618ff9daeaedb12c0653e55d4f231d9d00c77e37251cb4d9a2706"
-    sha256 arm64_monterey: "4f6a24cc441b2c1591370d13cd9f9b58b391780e4aee353976dd9698b1a85c2c"
-    sha256 arm64_big_sur:  "c1ee5777fb69a3df8b0b0107404effdbaa3a6d73138fda9a93f279efd9c0bb8d"
-    sha256 ventura:        "4864581bb657fecbcaf07f318d343455706766543a45e605f63fd23b2a9746d1"
-    sha256 monterey:       "f68c0305b46be402d4b41bb709960228e76f4c17b7b39f42cdd5b11460dea905"
-    sha256 big_sur:        "edcf5c3a608c25386b8b5719ffba9ee6f18a140593c4dc941b43a768d1d14084"
-    sha256 catalina:       "7e94d9738a92dff6de1e76d69ab0e38ca7c6488d06c34d5bb40c3aa8cb08c8f4"
-    sha256 x86_64_linux:   "bf4676cca925307267ddc7fae14da7c3d4857a4636088a2c3361ec193ebc2137"
+    sha256 arm64_ventura:  "684a70d7b7de73b6e8f8784d371bc09f378853a8c29f87bf30ec3e3e1846e966"
+    sha256 arm64_monterey: "96bd279a71f9d5e798047080f563868ca79b18a98db11edc138e20cf6eacf837"
+    sha256 arm64_big_sur:  "058c06f623a87893e4df2df875d34542935002167a7f17247a3dd6bb5d69fc24"
+    sha256 ventura:        "5a4d3cf68235de582a225e82e78b5672af74ae1fd1ef843093232015e5c1f751"
+    sha256 monterey:       "ed089861c3c552d6531cef3f75067dedb46b74ddff4d43bfb37b3dc20c4eb4b0"
+    sha256 big_sur:        "259f7d1b308ae26692995457b9933fd9bb4affcf17dd4429effb503a0f1b1d73"
+    sha256 x86_64_linux:   "ef0ab2fc170be2e64fc1b32b55b45b7e498d827f97c258483b399e698ecd6d61"
   end
 
   depends_on "byacc" => :build
