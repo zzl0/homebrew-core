12c7cf5d2d0d9264f5a1031a526e1058ab5ead05
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-06-24 14:42:52 +0000

    commitlint: update 17.6.6 bottle.

--- Formula/commitlint.rb
+++ Formula/commitlint.rb
@@ -9,13 +9,13 @@ class Commitlint < Formula
   head "https://github.com/conventional-changelog/commitlint.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "c0a66fe787abfba0308a3866ea58128b6f4209932866301e38be802b7f7a1b19"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c0a66fe787abfba0308a3866ea58128b6f4209932866301e38be802b7f7a1b19"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c0a66fe787abfba0308a3866ea58128b6f4209932866301e38be802b7f7a1b19"
-    sha256 cellar: :any_skip_relocation, ventura:        "31d751b9f9d9bc8fef761754ac8df289514a3b47e6b33315244835613bd96015"
-    sha256 cellar: :any_skip_relocation, monterey:       "31d751b9f9d9bc8fef761754ac8df289514a3b47e6b33315244835613bd96015"
-    sha256 cellar: :any_skip_relocation, big_sur:        "31d751b9f9d9bc8fef761754ac8df289514a3b47e6b33315244835613bd96015"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c0a66fe787abfba0308a3866ea58128b6f4209932866301e38be802b7f7a1b19"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "6d009b426051afce47e98a6e9f82791f8d3d94cdfb86efaff1fe9ddb0147a759"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "6d009b426051afce47e98a6e9f82791f8d3d94cdfb86efaff1fe9ddb0147a759"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6d009b426051afce47e98a6e9f82791f8d3d94cdfb86efaff1fe9ddb0147a759"
+    sha256 cellar: :any_skip_relocation, ventura:        "564994503d3494e0d1196880ca6fec123a9df83be96eb7ff7c47cf467ac2010d"
+    sha256 cellar: :any_skip_relocation, monterey:       "564994503d3494e0d1196880ca6fec123a9df83be96eb7ff7c47cf467ac2010d"
+    sha256 cellar: :any_skip_relocation, big_sur:        "564994503d3494e0d1196880ca6fec123a9df83be96eb7ff7c47cf467ac2010d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6d009b426051afce47e98a6e9f82791f8d3d94cdfb86efaff1fe9ddb0147a759"
   end
 
   depends_on "node"
