d26b8aab87009a04fd9dc142a89790b3f53f51e3
Porkepix <porkepix@gmail.com>
2023-06-24 11:55:38 +0200

    rocksdb 8.3.2

--- Formula/rocksdb.rb
+++ Formula/rocksdb.rb
@@ -1,8 +1,8 @@
 class Rocksdb < Formula
   desc "Embeddable, persistent key-value store for fast storage"
   homepage "https://rocksdb.org/"
-  url "https://github.com/facebook/rocksdb/archive/refs/tags/v8.1.1.tar.gz"
-  sha256 "9102704e169cfb53e7724a30750eeeb3e71307663852f01fa08d5a320e6155a8"
+  url "https://github.com/facebook/rocksdb/archive/refs/tags/v8.3.2.tar.gz"
+  sha256 "b55d6da6f562eb79bad7fdc9c368efc9ea4bfa239e69cead37923fc845a43fba"
   license any_of: ["GPL-2.0-only", "Apache-2.0"]
   head "https://github.com/facebook/rocksdb.git", branch: "main"
 
