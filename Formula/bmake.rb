446a71f33cc537baf4b88e3639f2f59906308a8d
Patrick Linnane <patrick@linnane.io>
2023-06-24 18:01:06 -0700

    bmake 20230622

--- Formula/bmake.rb
+++ Formula/bmake.rb
@@ -1,8 +1,8 @@
 class Bmake < Formula
   desc "Portable version of NetBSD make(1)"
   homepage "https://www.crufty.net/help/sjg/bmake.html"
-  url "https://www.crufty.net/ftp/pub/sjg/bmake-20230601.tar.gz"
-  sha256 "8d0b2e593946539065530a241040b0b0525ebb2b924ed67fc497ad17845a5a09"
+  url "https://www.crufty.net/ftp/pub/sjg/bmake-20230622.tar.gz"
+  sha256 "b404c99d60289d78362d0ba0468f541d8a9b4215befee2fbe5750534849cec00"
   license "BSD-3-Clause"
 
   livecheck do
