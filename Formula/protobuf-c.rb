6df09e1c5b3d7895a2fb0caf4e90a5c1c273cf53
Steve Peters <scpeters@openrobotics.org>
2023-06-24 22:33:54 -0700

    protobuf-c: switch to unversioned `protobuf`
    - add patch and HEAD

--- Formula/protobuf-c.rb
+++ Formula/protobuf-c.rb
@@ -1,11 +1,10 @@
 class ProtobufC < Formula
   desc "Protocol buffers library"
   homepage "https://github.com/protobuf-c/protobuf-c"
-  # TODO: Check if we can use unversioned `protobuf` at version bump
   url "https://github.com/protobuf-c/protobuf-c/releases/download/v1.4.1/protobuf-c-1.4.1.tar.gz"
   sha256 "4cc4facd508172f3e0a4d3a8736225d472418aee35b4ad053384b137b220339f"
   license "BSD-2-Clause"
-  revision 2
+  revision 3
 
   bottle do
     sha256 cellar: :any,                 arm64_ventura:  "7faeb770aed57e27102f2376d93ae7e82834d3dc672bdea3eeebe50354d800e9"
@@ -17,15 +16,46 @@ class ProtobufC < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "68cbaee2214be0208918ae530369e455627e6f7ffe8cb3f9f662ff78131be7ed"
   end
 
+  head do
+    url "https://github.com/protobuf-c/protobuf-c.git", branch: "master"
+    depends_on "asciidoc" => :build
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  # TODO: `autoconf`, `automake`, and `libtool` are needed for the patches.
+  #       Remove when they are no longer needed.
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
   depends_on "pkg-config" => :build
-  depends_on "protobuf@21" # https://github.com/protobuf-c/protobuf-c/issues/544
+  depends_on "protobuf"
+
+  # The next three patches are for compatibility with the newest protobuf.
+  # https://github.com/protobuf-c/protobuf-c/pull/556
+  # TODO: Uncomment `if build.head?` in `#install` when these are no longer needed.
+  patch do
+    url "https://github.com/protobuf-c/protobuf-c/commit/66a0b0d205224f63f19dd8f96abf9dcdc2112331.patch?full_index=1"
+    sha256 "a3561ad37f33048c59a1ceece246a515b62cef91126e4041056d10ea26a19230"
+  end
+
+  patch do
+    url "https://github.com/protobuf-c/protobuf-c/commit/7706c95d4835e75f182ab56d9dad5c8cd8517e0a.patch?full_index=1"
+    sha256 "86364b4da6e077bd9f89a82d6e2ac965776ee1a544e43d1964c2e800424cdb6e"
+  end
+
+  patch do
+    url "https://github.com/protobuf-c/protobuf-c/commit/66574f3fd85a205eb7c90b790477d5415364209e.patch?full_index=1"
+    sha256 "2d1d6edbd615dff4f0a9c4a974d325effefc44466e1855f8c0b88e5977962a9d"
+  end
 
   def install
-    ENV.cxx11
-
     # https://github.com/protocolbuffers/protobuf/issues/9947
     ENV.append_to_cflags "-DNDEBUG"
 
+    # TODO: Uncomment `if build.head?` when the patches are no longer needed.
+    system "autoreconf", "--force", "--install", "--verbose" # if build.head?
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make", "install"
   end
@@ -42,6 +72,6 @@ class ProtobufC < Formula
       }
     EOS
     (testpath/"test.proto").write testdata
-    system Formula["protobuf@21"].opt_bin/"protoc", "test.proto", "--c_out=."
+    system Formula["protobuf"].opt_bin/"protoc", "test.proto", "--c_out=."
   end
 end
