53f92e8e078524029214d070981bde0f018c45aa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-06-26 02:33:22 +0000

    xtrans: update 1.5.0 bottle.

--- Formula/xtrans.rb
+++ Formula/xtrans.rb
@@ -6,8 +6,8 @@ class Xtrans < Formula
   license "MIT"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, all: "2be2401621614877b9ee88a8e26eafe987217194e03eb5a34901ee905cd39760"
+    rebuild 2
+    sha256 cellar: :any_skip_relocation, all: "9b83139171ea1c0b61f5d053e824f1be8715c28e7fe190a8784a1a6ea9234047"
   end
 
   depends_on "pkg-config" => :build
