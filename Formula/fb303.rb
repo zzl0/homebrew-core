bce9027de329aaf3dcd567181af950209c01176a
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-06-26 07:45:35 -0400

    fb303 2023.06.26.00

--- Formula/fb303.rb
+++ Formula/fb303.rb
@@ -1,10 +1,9 @@
 class Fb303 < Formula
   desc "Thrift functions for querying information from a service"
   homepage "https://github.com/facebook/fb303"
-  url "https://github.com/facebook/fb303/archive/refs/tags/v2023.06.12.00.tar.gz"
-  sha256 "f9cdc2f6631e09707ed5f9afe8b3b9acb94b9a3c04dc896d7570a07290024292"
+  url "https://github.com/facebook/fb303/archive/refs/tags/v2023.06.26.00.tar.gz"
+  sha256 "416103e9e4188c84e1d47fe65d79e2ab5fdc56df908c6b0e12803389871518a9"
   license "Apache-2.0"
-  revision 1
   head "https://github.com/facebook/fb303.git", branch: "main"
 
   bottle do
