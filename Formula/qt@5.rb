8264919fc5b75b989d8371f15508022f15921aaf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-06-27 14:22:14 +0000

    qt@5: update 5.15.10 bottle.

--- Formula/qt@5.rb
+++ Formula/qt@5.rb
@@ -11,13 +11,14 @@ class QtAT5 < Formula
   license all_of: ["GFDL-1.3-only", "GPL-2.0-only", "GPL-3.0-only", "LGPL-2.1-only", "LGPL-3.0-only"]
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "49861de4ac7615f45f4bbad0156ae1ee1d27c8adac53a24410a29457b04e9c47"
-    sha256 cellar: :any,                 arm64_monterey: "6b51be782af005e149136152da27360857bea4e73bd19be98eafe85a6be5e10e"
-    sha256 cellar: :any,                 arm64_big_sur:  "9513287059bb862dfff9e0baa4a27b4c5dcde58ff7fb3a3185fe69eb001c634e"
-    sha256 cellar: :any,                 ventura:        "d253e6509205cabb5c13ee228ec6fa0a405b46a4bdeca54001c2744dc4b3c710"
-    sha256 cellar: :any,                 monterey:       "164585376c85906780f5cfa11cabc91b6d23733c5c08790d97c57fec8995f0ba"
-    sha256 cellar: :any,                 big_sur:        "b231c7e83af13162da5b9e7eb8fd9196d3e2dd5ab8ab1a98f41861752075f7f3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5331eeaedc37894e18057e8524a948bc92c7ea524d20784caaa510b7736b6538"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_ventura:  "8caeb580012da1742aa9d2ce0b06ebd69a14d1e2cb1c5f8452de5bf351eb38a0"
+    sha256 cellar: :any,                 arm64_monterey: "82d9c11e66478d53c106a9b6cf827e15ed3d9f985191a39b9d50f2273a31f093"
+    sha256 cellar: :any,                 arm64_big_sur:  "9bfa378a951e54e0b557d72fd9c73c6ca6d35c914ddab249573987ed289fe8c9"
+    sha256 cellar: :any,                 ventura:        "74d8d57fc5e31ef60565e74d421f1b80fa68f67adf41663d30922b68bc4ccfbd"
+    sha256 cellar: :any,                 monterey:       "f0beebfcdc5eafa89b84cc6ffa15f0359be50969efb706688168f94647c8077c"
+    sha256 cellar: :any,                 big_sur:        "ada5a2bd5fb44eb222e8b99a06cd075bc7d78e146291acbf905922482acf3914"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3cefabf81e57004029003a2fc31e836487c51695f261071d318f9c2f74d776c3"
   end
 
   keg_only :versioned_formula
