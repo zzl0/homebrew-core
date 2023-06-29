2dd22080a839fef504fcc2d6a65dd6a3c5b9e2fc
Rui Chen <rui@chenrui.dev>
2023-06-29 18:59:40 -0400

    acpica 20230628

--- Formula/acpica.rb
+++ Formula/acpica.rb
@@ -1,8 +1,8 @@
 class Acpica < Formula
   desc "OS-independent implementation of the ACPI specification"
   homepage "https://www.acpica.org/"
-  url "https://acpica.org/sites/acpica/files/acpica-unix-20230331.tar.gz"
-  sha256 "0c5d695d605aaa61709f3c63f57a1a99b8902291723998446b0813b57ac310e2"
+  url "https://acpica.org/sites/acpica/files/acpica-unix-20230628.tar.gz"
+  sha256 "86876a745e3d224dcfd222ed3de465b47559e85811df2db9820ef09a9dff5cce"
   license any_of: ["Intel-ACPI", "GPL-2.0-only", "BSD-3-Clause"]
   head "https://github.com/acpica/acpica.git", branch: "master"
 
