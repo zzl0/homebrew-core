eeac69b2dc8abb7defd6093b65968db817fa26f3
Alexander Frisch <crunchtime-ali@users.noreply.github.com>
2023-06-29 18:25:29 +0000

    aws-cdk 2.86.0

--- Formula/aws-cdk.rb
+++ Formula/aws-cdk.rb
@@ -3,8 +3,8 @@ require "language/node"
 class AwsCdk < Formula
   desc "AWS Cloud Development Kit - framework for defining AWS infra as code"
   homepage "https://github.com/aws/aws-cdk"
-  url "https://registry.npmjs.org/aws-cdk/-/aws-cdk-2.85.0.tgz"
-  sha256 "b68c06d833b6008fed88cfcd414143f5861f7cc537a7122ae4efe63e92fbb502"
+  url "https://registry.npmjs.org/aws-cdk/-/aws-cdk-2.86.0.tgz"
+  sha256 "11a9d7baaf088a975df3451dacfaf7083b5ef64d965490d1c2fc15033958615e"
   license "Apache-2.0"
 
   bottle do
