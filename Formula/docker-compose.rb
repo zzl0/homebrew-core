55f720de5fbf3d2830280f8fabb533af13dbd596
Rui Chen <rui@chenrui.dev>
2023-06-29 19:01:52 -0400

    docker-compose 2.19.1

--- Formula/docker-compose.rb
+++ Formula/docker-compose.rb
@@ -1,8 +1,8 @@
 class DockerCompose < Formula
   desc "Isolated development environments using Docker"
   homepage "https://docs.docker.com/compose/"
-  url "https://github.com/docker/compose/archive/v2.19.0.tar.gz"
-  sha256 "0be3fd713ef84985f63d76de4c58451401aed1dfbeb235127dd77a57d1890996"
+  url "https://github.com/docker/compose/archive/v2.19.1.tar.gz"
+  sha256 "869d4ffd3f6b6835dcbb5cd627e98a9d2f1d30a9ae2c8e712275db9aa3e6e97d"
   license "Apache-2.0"
   head "https://github.com/docker/compose.git", branch: "v2"
 
