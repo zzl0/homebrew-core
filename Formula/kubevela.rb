993b3c50ce94f20b5386e210e0a7df46620a0cce
Rui Chen <rui@chenrui.dev>
2023-06-29 09:55:57 -0400

    kubevela 1.9.2

--- Formula/kubevela.rb
+++ Formula/kubevela.rb
@@ -2,8 +2,8 @@ class Kubevela < Formula
   desc "Application Platform based on Kubernetes and Open Application Model"
   homepage "https://kubevela.io"
   url "https://github.com/kubevela/kubevela.git",
-      tag:      "v1.9.1",
-      revision: "6e9063d40c4c2ba79fbb6a0bfeb7c238ee68a748"
+      tag:      "v1.9.2",
+      revision: "48cf6fb10e11aed56c4e2914b34cfcb891a718e0"
   license "Apache-2.0"
   head "https://github.com/kubevela/kubevela.git", branch: "master"
 
