4a6e3cb3a9c3afa646fda6767c1521abd7ba8266
Rui Chen <rui@chenrui.dev>
2023-06-29 19:38:49 -0400

    glassfish 7.0.6

--- Formula/glassfish.rb
+++ Formula/glassfish.rb
@@ -1,9 +1,9 @@
 class Glassfish < Formula
   desc "Java EE application server"
   homepage "https://glassfish.org/"
-  url "https://download.eclipse.org/ee4j/glassfish/glassfish-7.0.5.zip"
-  mirror "https://github.com/eclipse-ee4j/glassfish/releases/download/7.0.5/glassfish-7.0.5.zip"
-  sha256 "33105e802d9eb6f611b126e4ddfae69874301a6f02378ca45bbb25b231ac0bb6"
+  url "https://download.eclipse.org/ee4j/glassfish/glassfish-7.0.6.zip"
+  mirror "https://github.com/eclipse-ee4j/glassfish/releases/download/7.0.6/glassfish-7.0.6.zip"
+  sha256 "801510c4522cc260964a05a519270db97013bbfabac3bb6009449f4b4532a9b8"
   license "EPL-2.0"
 
   livecheck do
