06c8f85d5544ba35c77514e99fae9ed2d76905b3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-06-30 04:35:57 +0000

    contentful-cli: update 2.6.24 bottle.

--- Formula/contentful-cli.rb
+++ Formula/contentful-cli.rb
@@ -9,13 +9,13 @@ class ContentfulCli < Formula
   head "https://github.com/contentful/contentful-cli.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f78c5f14d8afb65abb72937a3b58e5a9ad359fe80bee70f5fba39489bc5e79db"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f78c5f14d8afb65abb72937a3b58e5a9ad359fe80bee70f5fba39489bc5e79db"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f78c5f14d8afb65abb72937a3b58e5a9ad359fe80bee70f5fba39489bc5e79db"
-    sha256 cellar: :any_skip_relocation, ventura:        "f859329478a4f40719fc56cb6f4117110ad9858c1f91f83d81e5d77e7b78a743"
-    sha256 cellar: :any_skip_relocation, monterey:       "f859329478a4f40719fc56cb6f4117110ad9858c1f91f83d81e5d77e7b78a743"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f859329478a4f40719fc56cb6f4117110ad9858c1f91f83d81e5d77e7b78a743"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f78c5f14d8afb65abb72937a3b58e5a9ad359fe80bee70f5fba39489bc5e79db"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a35f6368f76194e7f63f8750750b727de99fac61ffa30ef425eb53693ff3316b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a35f6368f76194e7f63f8750750b727de99fac61ffa30ef425eb53693ff3316b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a35f6368f76194e7f63f8750750b727de99fac61ffa30ef425eb53693ff3316b"
+    sha256 cellar: :any_skip_relocation, ventura:        "d112845bb01bb0b511553430621620b0d4e83f7879dc8b62a507add03a5a63bb"
+    sha256 cellar: :any_skip_relocation, monterey:       "d112845bb01bb0b511553430621620b0d4e83f7879dc8b62a507add03a5a63bb"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d112845bb01bb0b511553430621620b0d4e83f7879dc8b62a507add03a5a63bb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a35f6368f76194e7f63f8750750b727de99fac61ffa30ef425eb53693ff3316b"
   end
 
   depends_on "node"
