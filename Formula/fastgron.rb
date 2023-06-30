a045e93428ac9e2e81b0df91a2b90cb72cfa90bf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-06-30 00:46:46 +0000

    fastgron: update 0.6.1 bottle.

--- Formula/fastgron.rb
+++ Formula/fastgron.rb
@@ -6,13 +6,13 @@ class Fastgron < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "2dbeb22bc8040fa493808296f63df10814d2254b08d778fa05e42de67aa7de62"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ac0ff8bcdc6d7a639032bbc9550aaf41c10ca5aa789790ef053e01009d7407e9"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "761ae7dcd4c2accbe531130e81ce0107312ba67d6dfa7cdd112e3624b5449e25"
-    sha256 cellar: :any_skip_relocation, ventura:        "a3b6ad749a6ae4eb9a8a19708332285ee7d5994ad0f19bf1b16ec5d5c109f382"
-    sha256 cellar: :any_skip_relocation, monterey:       "067c945c716cc5fbb716cd670490492fd9392289619ab918ee2263784b413a2f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "60b7dce4f0a31b0f225929a21e9e998222ea481a6baeeafe182d800421266bcb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8c2992581aa247cd37011859d91e98f80fb8cab0d876b907c05b96bd1ccbcd41"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "2288f12e1d0d6729e4e94e9dfa89f9e985117a86efb694fad0e7e48faaf875de"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e1870323ca659724b61932dc16825ac693723ad02dce47a2fb33c4a7b0131db7"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8965cf52126bef61e8edfcbfa43a4e7f545182753586e46b73b13504f8682717"
+    sha256 cellar: :any_skip_relocation, ventura:        "bad5ca7fe6bb1f3e8d327043b6d5fceafcbef788aae58c6e99509311b3d71769"
+    sha256 cellar: :any_skip_relocation, monterey:       "6c5b6173609ef8a1ca32bc9c76f248eac90f3375524cf0b37beef97e2ec72f60"
+    sha256 cellar: :any_skip_relocation, big_sur:        "c2d77a7480c23064a8e4fcb3a9ceff0ce7ea458561bdc1df613842df9d65c0ff"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5325ab342dc252195a5914125200395e42a42342dc906673ccae75a6910eb86e"
   end
 
   depends_on "cmake" => :build
