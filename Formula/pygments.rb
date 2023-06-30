623226b77a148708197a10c0d1eab3f8d6ca005b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-06-30 17:46:51 +0000

    pygments: update 2.15.1_1 bottle.

--- Formula/pygments.rb
+++ Formula/pygments.rb
@@ -8,8 +8,13 @@ class Pygments < Formula
   head "https://github.com/pygments/pygments.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, all: "4f9ad4f7947192121e59a218ef21d9416eb2162a936945c60335778fbce3939a"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "22dd62a13b5fa28d0b13af36ac5c065d54671946c8a04e25c9da8c3f5dab0c1e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "8cf29db56c6db4b94612f46c8c6901faeda2d6638e620eda8a54c42f83088701"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "35f8b8852d8ddde37570525f201edad241dbb0f35a323ef4761de0722a47942f"
+    sha256 cellar: :any_skip_relocation, ventura:        "f5f47a58e80f27048a3ad496eeace812d50b73fbe570059efab7e8b68feb81e9"
+    sha256 cellar: :any_skip_relocation, monterey:       "c60a1549d0f8ac1af0130f98982c987d13ba3ca499d90243f03cbe5011da8f6f"
+    sha256 cellar: :any_skip_relocation, big_sur:        "1eadfd8db2188556b8c8afcc580a8dd767bba6e44e6908c940e42f72798c514c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1a0407703d117068c5fde3fb7bc7633e3cbb3b023fd662f5742c2939db97eaa7"
   end
 
   depends_on "python@3.10" => [:build, :test]
