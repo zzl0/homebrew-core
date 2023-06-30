3cdb9d45e5b1cbc740bd7db581d8f596f5517edb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-06-30 19:52:19 +0000

    erlang@25: add 25.3.2.2 bottle.

--- Formula/erlang@25.rb
+++ Formula/erlang@25.rb
@@ -11,6 +11,16 @@ class ErlangAT25 < Formula
     regex(/^OTP[._-]v?(25(?:\.\d+)+)$/i)
   end
 
+  bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "73ed0663012d8f60f290e24b57217aca3f69494f1748f51b9b1408cd4b1533ff"
+    sha256 cellar: :any,                 arm64_monterey: "85a1bb4d254e383cba643c383120e7b4b323e10df3c6491424190517da40fef8"
+    sha256 cellar: :any,                 arm64_big_sur:  "b1ba80c82f2502245a485a1ade0b337a54be726ab9233ab99cf55c6d0a0eb32a"
+    sha256 cellar: :any,                 ventura:        "a4ac143b8f6f8033fda5a41a94fa793caee7ac5e53ebad2a09cfa20e7b0318d3"
+    sha256 cellar: :any,                 monterey:       "68e409b7f8b9337cee6fb993f53e30d6dc6f58766ac431ef2c9c50c021d4e9d7"
+    sha256 cellar: :any,                 big_sur:        "1221902d898182748e3d15383f0d3510a6af6a69b1f51f570d353e27afdbd587"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f0ce5150277e375d501c9bf2af1c28006f990bd803beb1a64593788c0f8f2ffb"
+  end
+
   keg_only :versioned_formula
 
   depends_on "openssl@3"
