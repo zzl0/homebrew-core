21eeeaab4087cf694cc4a4fe0253275300bc7b62
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-07-01 14:10:56 +0000

    erdtree: update 3.1.1 bottle.

--- Formula/erdtree.rb
+++ Formula/erdtree.rb
@@ -6,13 +6,13 @@ class Erdtree < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "16feadb04e4caafa8cebb926309e6c9ace2c48b82014c67a00feed58410d95a3"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "19a8dc75e2e4ccbeaded787b6d511dc17c2cbcdb71fdc32a4f69401afa27dfb5"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3cd45623439450d6852218a230e9af81850a9422ea156b09dab8de7afbc77d21"
-    sha256 cellar: :any_skip_relocation, ventura:        "49ee681f02872339a5409d92826a76090ebe623cdba3bfdf2cba509f27204e50"
-    sha256 cellar: :any_skip_relocation, monterey:       "aa396ac24f954bc3458c3c67f0d82eafbe5761d3a85459443cbc2c99d52ca475"
-    sha256 cellar: :any_skip_relocation, big_sur:        "5365d68f1405200f27c976050fec52615b7cf1f13aa472b1dc2c37fc15f1680a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "807a8f67954f89adc41ba3877395d6ade1ab5e6643fa64885073cf8ddece1a07"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "1bd7cc2a5d23f010b9b7ee369ea108eee80a56716eae2b38c06df5c449a28e19"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "4098b47d69fac2631c0f39ceadcfb287bd738469698f60996b93d1427e91988f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b82b1d5e6768a9fd1c20b24e7ae88fbe72a9ce6db87e61609dfd04440477145e"
+    sha256 cellar: :any_skip_relocation, ventura:        "645d88e437cab97da99b44002349d88cb60d0eb258db74d8048ebbaff3da636d"
+    sha256 cellar: :any_skip_relocation, monterey:       "e772631a3cc19e52ae3e75f50debf779fc6cee9402e47f90a0532d2fa570d061"
+    sha256 cellar: :any_skip_relocation, big_sur:        "351d179b2e5a9c334dcc3bf1406e0ca4d5e9950fb448281e535a904546ccf641"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a637ac09328d36648a729f7cb109c2ae4bd519e4cddf4557f852127fd737aae8"
   end
 
   depends_on "rust" => :build
