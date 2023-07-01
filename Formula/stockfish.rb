ab327068781858a9b80b2315b89405b7ba42c58d
Rui Chen <rui@chenrui.dev>
2023-07-01 00:27:41 -0400

    stockfish 16

--- Formula/stockfish.rb
+++ Formula/stockfish.rb
@@ -1,8 +1,8 @@
 class Stockfish < Formula
   desc "Strong open-source chess engine"
   homepage "https://stockfishchess.org/"
-  url "https://github.com/official-stockfish/Stockfish/archive/sf_15.1.tar.gz"
-  sha256 "d4272657905319328294355973faee40a8c28e3eecb0e7b266ed34ff33383b76"
+  url "https://github.com/official-stockfish/Stockfish/archive/sf_16.tar.gz"
+  sha256 "a1600ebdaf4e324ba3e10cec2e0c9a810dc64c6f0db5cc955b2fd5e1eefa1cc6"
   license "GPL-3.0-only"
   head "https://github.com/official-stockfish/Stockfish.git", branch: "master"
 
