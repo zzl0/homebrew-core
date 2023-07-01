0a158d75c89f30cbe7b9fdd6f33593e64687d559
Rui Chen <rui@chenrui.dev>
2023-06-30 22:32:51 -0400

    dyld-headers 1066.8

--- Formula/dyld-headers.rb
+++ Formula/dyld-headers.rb
@@ -1,8 +1,8 @@
 class DyldHeaders < Formula
   desc "Header files for the dynamic linker"
   homepage "https://opensource.apple.com/"
-  url "https://github.com/apple-oss-distributions/dyld/archive/refs/tags/dyld-1042.1.tar.gz"
-  sha256 "5ff902c50c78e809e90fb92038b86e531aafbc6dff36d254d6548ce6d41a0aa8"
+  url "https://github.com/apple-oss-distributions/dyld/archive/refs/tags/dyld-1066.8.tar.gz"
+  sha256 "f5bd73aa970b3882520c308017688bd7e5fa5a0d3ad0727af4d053154add158a"
   license "APSL-2.0"
 
   bottle do
