be131c6c8ea3744a39d7fe059d2fb53cae464fc6
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2023-07-02 21:12:49 +0800

    bloaty: use unversioned `protobuf`
    Needed for #132437.
    
    We need to apply a patch to avoid using the vendored `abseil`, which
    will conflict with the one pulled in by our Protobuf.

--- Formula/bloaty.rb
+++ Formula/bloaty.rb
@@ -1,7 +1,6 @@
 class Bloaty < Formula
   desc "Size profiler for binaries"
   homepage "https://github.com/google/bloaty"
-  # TODO: Check if we can use unversioned `protobuf` at version bump
   url "https://github.com/google/bloaty/releases/download/v1.1/bloaty-1.1.tar.bz2"
   sha256 "a308d8369d5812aba45982e55e7c3db2ea4780b7496a5455792fb3dcba9abd6f"
   license "Apache-2.0"
@@ -19,14 +18,24 @@ class Bloaty < Formula
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
+  depends_on "abseil"
   depends_on "capstone"
-  depends_on "protobuf@21"
+  depends_on "protobuf"
   depends_on "re2"
+
+  # Support system Abseil. Needed for Protobuf 22+.
+  # Backport of: https://github.com/google/bloaty/pull/347
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/86c6fb2837e5b96e073e1ee5a51172131d2612d9/bloaty/system-abseil.patch"
+    sha256 "d200e08c96985539795e13d69673ba48deadfb61a262bdf49a226863c65525a7"
+  end
 
   def install
     # https://github.com/protocolbuffers/protobuf/issues/9947
     ENV.append_to_cflags "-DNDEBUG"
-    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    # Remove vendored dependencies
+    %w[abseil-cpp capstone protobuf re2].each { |dir| (buildpath/"third_party"/dir).rmtree }
+    system "cmake", "-S", ".", "-B", "build", "-DCMAKE_CXX_STANDARD=17", *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
   end
