b2323ea453acb9617e03084d4858966a645b60f1
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2023-07-04 00:17:58 +0800

    proxygen 2023.07.03.00 (new formula)

---
+++ Formula/proxygen.rb
@@ -0,0 +1,38 @@
+class Proxygen < Formula
+  desc "Collection of C++ HTTP libraries"
+  homepage "https://github.com/facebook/proxygen"
+  url "https://github.com/facebook/proxygen/releases/download/v2023.07.03.00/proxygen-v2023.07.03.00.tar.gz"
+  sha256 "cc136282c73c88fc20e5abe37c1988b86613737689b549d37524e52a82f2e689"
+  license "BSD-3-Clause"
+  head "https://github.com/facebook/proxygen.git", branch: "main"
+
+  depends_on "cmake" => :build
+  depends_on "boost"
+  depends_on "fizz"
+  depends_on "fmt"
+  depends_on "folly"
+  depends_on "gflags"
+  depends_on "openssl@3"
+  depends_on "wangle"
+  depends_on "zstd"
+  uses_from_macos "gperf" => :build
+  uses_from_macos "python" => :build
+  uses_from_macos "zlib"
+
+  def install
+    system "cmake", "-S", ".", "-B", "build",
+                    "-DBUILD_SHARED_LIBS=ON",
+                    "-DCMAKE_INSTALL_RPATH=#{rpath}",
+                    *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    pid = spawn bin/"proxygen_echo"
+    sleep 5
+    system "curl", "-v", "http://localhost:11000"
+  ensure
+    Process.kill "TERM", pid
+  end
+end
