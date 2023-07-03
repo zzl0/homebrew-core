27f5ef361f3c059d4874abde0e73073b831bc22a
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2023-07-03 17:03:06 +0800

    wangle 2023.07.03.00

--- Formula/wangle.rb
+++ Formula/wangle.rb
@@ -1,8 +1,8 @@
 class Wangle < Formula
   desc "Modular, composable client/server abstractions framework"
   homepage "https://github.com/facebook/wangle"
-  url "https://github.com/facebook/wangle/releases/download/v2023.06.26.00/wangle-v2023.06.26.00.tar.gz"
-  sha256 "9a5d83874c36d4937fe711d210fbcf8a92ad45d21dda0e21e7613183f1082257"
+  url "https://github.com/facebook/wangle/releases/download/v2023.07.03.00/wangle-v2023.07.03.00.tar.gz"
+  sha256 "cdc2783f6d17f5e757c19373ee4839de5c4c9e9ad820e17a2cf9deff3543f028"
   license "Apache-2.0"
   head "https://github.com/facebook/wangle.git", branch: "main"
 
