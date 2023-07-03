5460f224e8309c86ca749f3a56b6685ff18c965b
Rui Chen <rui@chenrui.dev>
2023-07-03 18:38:58 -0400

    trufflehog 3.42.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/trufflehog.rb
@@ -0,0 +1,24 @@
+class Trufflehog < Formula
+  desc "Find and verify credentials"
+  homepage "https://trufflesecurity.com/"
+  url "https://github.com/trufflesecurity/trufflehog/archive/refs/tags/v3.42.0.tar.gz"
+  sha256 "d8a309d2f8618ca8bc8ee1647dfb76ea3356bb770e57440d6929f77deb63ff63"
+  # upstream license ask, https://github.com/trufflesecurity/trufflehog/issues/1446
+  license "AGPL-3.0-only"
+  head "https://github.com/trufflesecurity/driftwood.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X github.com/trufflesecurity/trufflehog/v3/pkg/version.BuildVersion=#{version}"
+    system "go", "build", *std_go_args(ldflags: ldflags)
+  end
+
+  test do
+    repo = "https://github.com/trufflesecurity/test_keys"
+    output = shell_output("#{bin}/trufflehog git #{repo} --no-update --only-verified 2>&1")
+    assert_match "loaded decoders	{\"count\": 3}", output
+
+    assert_match version.to_s, shell_output("#{bin}/trufflehog --version 2>&1")
+  end
+end
