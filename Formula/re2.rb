34a76320dc08173cf756242523fa4b9bb10f1a28
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2023-07-03 12:23:54 +0800

    re2 20230701

--- Formula/re2.rb
+++ Formula/re2.rb
@@ -1,9 +1,9 @@
 class Re2 < Formula
   desc "Alternative to backtracking PCRE-style regular expression engines"
   homepage "https://github.com/google/re2"
-  url "https://github.com/google/re2/archive/refs/tags/2023-06-01.tar.gz"
-  version "20230601"
-  sha256 "8b4a8175da7205df2ad02e405a950a02eaa3e3e0840947cd598e92dca453199b"
+  url "https://github.com/google/re2/releases/download/2023-07-01/re2-2023-07-01.tar.gz"
+  version "20230701"
+  sha256 "18cf85922e27fad3ed9c96a27733037da445f35eb1a2744c306a37c6d11e95c4"
   license "BSD-3-Clause"
   head "https://github.com/google/re2.git", branch: "main"
 
@@ -33,14 +33,19 @@ class Re2 < Formula
 
   def install
     # Build and install static library
-    system "cmake", "-B", "build-static", "-DRE2_BUILD_TESTING=OFF", *std_cmake_args
-    system "make", "-C", "build-static"
-    system "make", "-C", "build-static",  "install"
+    system "cmake", "-S", ".", "-B", "build-static",
+                    "-DRE2_BUILD_TESTING=OFF",
+                    *std_cmake_args
+    system "cmake", "--build", "build-static"
+    system "cmake", "--install", "build-static"
 
     # Build and install shared library
-    system "cmake", "-B", "build-shared", "-DBUILD_SHARED_LIBS=ON", "-DRE2_BUILD_TESTING=OFF", *std_cmake_args
-    system "make", "-C", "build-shared"
-    system "make", "-C", "build-shared", "install"
+    system "cmake", "-S", ".", "-B", "build-shared",
+                    "-DBUILD_SHARED_LIBS=ON",
+                    "-DRE2_BUILD_TESTING=OFF",
+                    *std_cmake_args
+    system "cmake", "--build", "build-shared"
+    system "cmake", "--install", "build-shared"
   end
 
   test do
