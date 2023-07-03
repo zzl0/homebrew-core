f73d297e763407c10993e3780e1ea5187fccec3e
Rui Chen <rui@chenrui.dev>
2023-07-03 09:36:06 -0400

    steampipe 0.20.8

--- Formula/steampipe.rb
+++ Formula/steampipe.rb
@@ -1,8 +1,8 @@
 class Steampipe < Formula
   desc "Use SQL to instantly query your cloud services"
   homepage "https://steampipe.io/"
-  url "https://github.com/turbot/steampipe/archive/refs/tags/v0.20.7.tar.gz"
-  sha256 "c7946e08ecf04b0b993f905c3c4a6899d0c02697d46a6a8534386d48f9445049"
+  url "https://github.com/turbot/steampipe/archive/refs/tags/v0.20.8.tar.gz"
+  sha256 "95206a01fe365a074160189c1718919658834bb5cdd14ab93982201c0d60740b"
   license "AGPL-3.0-only"
   head "https://github.com/turbot/steampipe.git", branch: "main"
 
