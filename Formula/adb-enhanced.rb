2b8666b70cbbfdac33e28812e595a6f6f6df9290
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-07-04 16:29:58 +0000

    adb-enhanced: add 2.5.21 bottle.

--- Formula/adb-enhanced.rb
+++ Formula/adb-enhanced.rb
@@ -7,6 +7,16 @@ class AdbEnhanced < Formula
   sha256 "976db27d1334782965592d7cd2202d4de803eecb1360f2c710ad07dcc2af3539"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "7f38d15411c5ad59b5de031c4560f6f1e84236e304a68285f23561b4f20800af"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "7742cf36049fee56e182fcb4fb49d53511f793fcc9cc6ff612eb86009256bf37"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b54b0e7487a425bc19a52e0647288e7ead6b4ae19afd5b531ca89ee585507865"
+    sha256 cellar: :any_skip_relocation, ventura:        "0327734f4e7f3366723a9438ef8c5c567ecb2d312ecb17f8d83f99ab574ed045"
+    sha256 cellar: :any_skip_relocation, monterey:       "ff96759ae1e33b4228ded4a95ea8f4e8cbfbefe94bce5b42a001a9f9bc6024bf"
+    sha256 cellar: :any_skip_relocation, big_sur:        "1212b6a4d6c7e2d2caf7e372f0d5d3701d1f376f984cb9990db407e0ac93cf5e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "977aac2bed3a019b3a6ce71fec3479531870b3a965dbc3460eb4cacee6e67191"
+  end
+
   depends_on "python@3.11"
 
   resource "docopt" do
