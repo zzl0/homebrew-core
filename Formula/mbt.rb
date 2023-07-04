24be4acd0655d6aa1a1b853607539d9607f02e4b
Porkepix <porkepix@gmail.com>
2023-07-04 13:00:02 +0200

    mbt 1.2.24

--- Formula/mbt.rb
+++ Formula/mbt.rb
@@ -1,8 +1,8 @@
 class Mbt < Formula
   desc "Multi-Target Application (MTA) build tool for Cloud Applications"
   homepage "https://sap.github.io/cloud-mta-build-tool"
-  url "https://github.com/SAP/cloud-mta-build-tool/archive/v1.2.23.tar.gz"
-  sha256 "a98f06248336d18b24b8da80a2f289f52bdd58adc41c34f048350194ad7e7831"
+  url "https://github.com/SAP/cloud-mta-build-tool/archive/v1.2.24.tar.gz"
+  sha256 "1e5b612aa912bf756ee828f91d8d368cf886f56b61e9aa0cb3f5b9ca8975fe6e"
   license "Apache-2.0"
   head "https://github.com/SAP/cloud-mta-build-tool.git", branch: "master"
 
