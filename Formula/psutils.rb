9196218be8f6a935d20eb23ea3fe8cc16b0bc753
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-07-04 16:10:28 +0000

    psutils: update 3.0.6 bottle.

--- Formula/psutils.rb
+++ Formula/psutils.rb
@@ -8,13 +8,13 @@ class Psutils < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "64515b1055b7d26815955832107f47ee1506ce213661ab13941ade76aebc655a"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "d4fbc2712d3f9b8e1efd50adb7f546d505f7471181ba9c761693266ffad6c025"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "70c1f55be883bcba43d362d23fc1b6e22d0cc64b6af0a8f18a1edf4cb0a8f216"
-    sha256 cellar: :any_skip_relocation, ventura:        "5e6d262306656a25dfb63ac6e09d50f647e82fc09a911b1a87263aab66ce6a2c"
-    sha256 cellar: :any_skip_relocation, monterey:       "1fbdf1c2b1a5be3092d68fc79ee83fe47525a47b4ffb209f243185a78f35cf0b"
-    sha256 cellar: :any_skip_relocation, big_sur:        "c4a64fc5e2adee4af646035d07b43831138484f6111338ff36c54ffd68f16be7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "24ef040b916f7e8c9a8a65ef2c8790945cd4098ae0143f2c4c4fe0109224035b"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "1bafba6dd4d2db7a9327970fcb985845ee15aed51510bca9a3c31d1c3662cb61"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5a0bddd52024d74213492e90b1e87d2350ecc72d4db3c48790f4722434245f73"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d6f7059dd6bf8d26b6f88b3858848019bceb1485cb5210c386ee3171f4335d9c"
+    sha256 cellar: :any_skip_relocation, ventura:        "5b491e3a4ed7d2bf6692f8d0f095e43933ee6a3f2ab596ca9c974d3a9a817361"
+    sha256 cellar: :any_skip_relocation, monterey:       "cce9cc324c46f4e94b89116c9e1a3d1e5eab38094585ee55a0bc62bca6f2ff64"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f3b8f7b188540b43e3c2495d5d2e92ef6de1137544857212d446ba7d3f2d042e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "36d9f847c3f3e90c30affd7cd510df256f0ef25eaee10a950b5f45b58dc5eab7"
   end
 
   depends_on "libpaper"
