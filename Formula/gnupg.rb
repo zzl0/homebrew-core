930f5dc6c8fffed1a80f3647659569918b272458
Patrick Linnane <patrick@linnane.io>
2023-07-04 08:22:22 -0700

    gnupg 2.4.3

--- Formula/gnupg.rb
+++ Formula/gnupg.rb
@@ -1,8 +1,8 @@
 class Gnupg < Formula
   desc "GNU Pretty Good Privacy (PGP) package"
   homepage "https://gnupg.org/"
-  url "https://gnupg.org/ftp/gcrypt/gnupg/gnupg-2.4.2.tar.bz2"
-  sha256 "97eb47df8ae5a3ff744f868005a090da5ab45cb48ee9836dbf5ee739a4e5cf49"
+  url "https://gnupg.org/ftp/gcrypt/gnupg/gnupg-2.4.3.tar.bz2"
+  sha256 "a271ae6d732f6f4d80c258ad9ee88dd9c94c8fdc33c3e45328c4d7c126bd219d"
   license "GPL-3.0-or-later"
 
   livecheck do
@@ -32,6 +32,7 @@ class Gnupg < Formula
   depends_on "readline"
 
   uses_from_macos "bzip2"
+  uses_from_macos "openldap"
   uses_from_macos "sqlite", since: :catalina
   uses_from_macos "zlib"
 
@@ -46,7 +47,6 @@ class Gnupg < Formula
     mkdir "build" do
       system "../configure", *std_configure_args,
                              "--disable-silent-rules",
-                             "--sbindir=#{bin}",
                              "--sysconfdir=#{etc}",
                              "--enable-all-tests",
                              "--with-pinentry-pgm=#{Formula["pinentry"].opt_bin}/pinentry"
