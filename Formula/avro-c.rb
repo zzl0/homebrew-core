4625508ac88536262cea31db276d9009ab9aac6a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-07-04 20:27:38 +0000

    avro-c: update 1.11.2 bottle.

--- Formula/avro-c.rb
+++ Formula/avro-c.rb
@@ -7,14 +7,13 @@ class AvroC < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "e866c69e2e7cf6ed942aef633b01b38bbfd1fce8c9f2e6ab563f386c82652e5e"
-    sha256 cellar: :any,                 arm64_monterey: "003f19bcd51baea53f86d97488f83552dc8477eedef78a371bdc166ac0d183ab"
-    sha256 cellar: :any,                 arm64_big_sur:  "f30243ab877db4e81ff07c5dea0e266228ac04bf178144d3f9caaab45a876f86"
-    sha256 cellar: :any,                 ventura:        "8b128950bcbd63c5f82e88773a0fcf5eac8c044f033ee5322ac4e35150fbd501"
-    sha256 cellar: :any,                 monterey:       "35a23d1c97d7e5b50adf7ff2f8ff3ddcb6f9271c85fc033306e8447484e23b5c"
-    sha256 cellar: :any,                 big_sur:        "cbb5817beab9e6dfb2dd1a5fafed449c6aa5dabf8525b71142854771ba46ab35"
-    sha256 cellar: :any,                 catalina:       "d91479cfd09b368e971ed2c926c480edfe88f62df39476e780ec0d863cc19dd5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "80da35f4303fb1c13aa3a8728d2e4dd35537ba984a075531c2f7d17f6d03e823"
+    sha256 cellar: :any,                 arm64_ventura:  "b04715d07a7b86605570d4c1cefb4a784eec94a439afce5c53f04e4993b3e5c3"
+    sha256 cellar: :any,                 arm64_monterey: "ecd0e6cae754d2dee7b07be2c0c05d32488ecf4f60a1d6d282d342844fbc50be"
+    sha256 cellar: :any,                 arm64_big_sur:  "3e8cfc7191ad79c820f528bfff349667db31f4c8e11137982b26072924e46f7c"
+    sha256 cellar: :any,                 ventura:        "51030bee05f8e25587334eb4c08f687860c46e6c3285547ca10575909e6692db"
+    sha256 cellar: :any,                 monterey:       "0a490e95841fd1afc7e101963aad7485c3a791ba23429a993e0e55dab8c3de89"
+    sha256 cellar: :any,                 big_sur:        "1e06df4db7975bc94497ad4dfab39b9879400b421495abef6981354a2bc5fd00"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1a15848747806cba35a46abbaafa68b8f67f8d8f3ed9eadf2fcf4c5b36db0d18"
   end
 
   depends_on "cmake" => :build
