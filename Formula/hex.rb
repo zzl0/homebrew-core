a3f634a08e9259b65955f94a9960c1b1b60d44a6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-07-05 12:19:09 +0000

    hex: add 0.4.2 bottle.

--- Formula/hex.rb
+++ Formula/hex.rb
@@ -6,6 +6,16 @@ class Hex < Formula
   license "MIT"
   head "https://github.com/sitkevij/hex.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "245116da00ee18fb6c48ddbd0083b62e86a41957f2b599b47d51ffec1a5adcd4"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "d926a35caa10284418a160ec7a6cb5cb44121d2fd03b52d84fe7734196b4524f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "de567e3c63410e409c9d2940c0b95db95a1a7b86ef3b6583a701d0b6dfc4f77e"
+    sha256 cellar: :any_skip_relocation, ventura:        "f91fdf2c20304970c38cd1aec11cbe0e5e416917f9c22a4a19cd7d2d395f0401"
+    sha256 cellar: :any_skip_relocation, monterey:       "3630ae16eab4b22999fd1f4b34bdd69a8de15a63973f82268fb45dddbb5f080e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d4e9bcfc557b0325b96003c1626506f4c1f66ad0a6dda54b9a8868c251a61ba1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "965b7895b352c914b2bf1a8b26bff934adf9eb5d6223a3da309b5b57154e31c0"
+  end
+
   depends_on "rust" => :build
 
   def install
