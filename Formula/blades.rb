7d99d8ce887467f25c8f708321c3f22726196e4a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-07-05 12:19:17 +0000

    blades: add 0.5.0 bottle.

--- Formula/blades.rb
+++ Formula/blades.rb
@@ -6,6 +6,16 @@ class Blades < Formula
   license "GPL-3.0-or-later"
   head "https://github.com/grego/blades.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "65e48a93926a6c28a4f056a17f433ce32afcb1bc937459ca671f629294f44400"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "25a4ef2be400ac8c799eb18c9b531a4d30137f8e1d7dca05d4734ebe5bbfd213"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b686021040a3c826a056cb55757f18ac678889ead9cd11cc6220315da4fa8f73"
+    sha256 cellar: :any_skip_relocation, ventura:        "27bb16f34cfd0e165212d4e85c91f6b3f4548adfd8a6e67bde4643ff2889bd52"
+    sha256 cellar: :any_skip_relocation, monterey:       "ca4e983b24c5375b4b8bf65bcbd828a04035c1a54ac1dd69d5304ca243d0e6f8"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e6c2064c05c14d57321c87cf96fc764a0b5374d2459c2e0964a675a8a58ffacd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6c1bacb2b79fc1edf95554aafc1523ae05962bb68c651207bfeb3d8964afef62"
+  end
+
   depends_on "rust" => :build
   uses_from_macos "expect" => :test
 
