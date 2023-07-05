512185dbcb637d76fee3153d39587cd728fe309e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-07-05 16:36:27 +0000

    freeling: update 4.2.1 bottle.

--- Formula/freeling.rb
+++ Formula/freeling.rb
@@ -6,13 +6,13 @@ class Freeling < Formula
   license "AGPL-3.0-only"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "d1375d1d4cc6303dc9b156988355553dc8d6ec59348e337a52f55843ec300482"
-    sha256 cellar: :any,                 arm64_monterey: "b4e0db7bbfb6ba92f46db0d2158b214b2c5be227d1b397996ffaf2575e42f723"
-    sha256 cellar: :any,                 arm64_big_sur:  "d06b85bef4251aa6563e5ab30dbf11ae5e9474811cac288006f9ea30c18e419e"
-    sha256 cellar: :any,                 ventura:        "b8f1a95c50bd350a81f094fde02de5efc6d2689b8308c0d9db975c8ceb321228"
-    sha256 cellar: :any,                 monterey:       "3c9f5f25a3593caa160ee0c2e2843ddd400a2c6769df5c94654a7ae98c0ce6db"
-    sha256 cellar: :any,                 big_sur:        "35f850be27e9b49d8f404c630632e782229db76072b08a3fd41e5fb85089a0d0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f106e8ed2dfc18b30bc46e9bf416f5b3cc15c4e16045bf47aaeaabf36c7a7fd1"
+    sha256 cellar: :any,                 arm64_ventura:  "ac2822ac4f5c777cc7bef01ac5b57e1c77ba193817f8e1d1d57b19fbb25a77ce"
+    sha256 cellar: :any,                 arm64_monterey: "c2788cb4af12d5fa65c214cb1d0af80b747fb83cc51cb5d788fb6f1462bfc80f"
+    sha256 cellar: :any,                 arm64_big_sur:  "ca7bc8d5219f6bac9051a19e9404fd5452876260c9dd16b96e0f10c80f6b90a4"
+    sha256 cellar: :any,                 ventura:        "321c4a350ec1b843723b2ff0cea9b4633a4c33b73f3aa7080b5831abecfc245a"
+    sha256 cellar: :any,                 monterey:       "42ce7757ef606fae613b6db9a1c66af4e7270b23002785dac583abcccad87aa7"
+    sha256 cellar: :any,                 big_sur:        "47ebbfe29be84a737912f9aba05df90520b2fa76730f90f295991603c20d79ce"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "35f72701267b2a0fd8bbc46e6e24b79cebb2b9326da5280d4504cc15cf3c3e56"
   end
 
   depends_on "cmake" => :build
