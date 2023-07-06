37ce11238f05879aab874e71baa9cbf3c7cec1ea
Rui Chen <rui@chenrui.dev>
2023-07-06 19:57:54 -0400

    mosh: revision bump (protobuf 23.4)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/mosh.rb
+++ Formula/mosh.rb
@@ -4,7 +4,7 @@ class Mosh < Formula
   url "https://github.com/mobile-shell/mosh/releases/download/mosh-1.4.0/mosh-1.4.0.tar.gz"
   sha256 "872e4b134e5df29c8933dff12350785054d2fd2839b5ae6b5587b14db1465ddd"
   license "GPL-3.0-or-later"
-  revision 3
+  revision 4
 
   bottle do
     sha256 cellar: :any,                 arm64_ventura:  "e2f247157c264d0f83534894b1266c7acaf6736a7f3f2132726dc9c4384ea8ed"
