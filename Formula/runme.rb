a1eb091af26d3ea0046ef9e8bbc0ad2eaa57989c
Christian Bromann <git@bromann.dev>
2023-07-06 15:41:28 -0700

    runme 1.3.0 (new formula)
    Adding a new formula that helps users to install Runme (runme.dev)
    through the Brew core repository.

---
+++ Formula/runme.rb
@@ -0,0 +1,39 @@
+class Runme < Formula
+  desc "Execute commands inside your runbooks, docs, and READMEs"
+  homepage "https://runme.dev/"
+  url "https://github.com/stateful/runme.git",
+      tag:      "v1.3.0",
+      revision: "da4fb8ff2264ad7765eda78d9ad5b125b0e6284d"
+  license "Apache-2.0"
+  head "https://github.com/stateful/runme.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/stateful/runme/internal/version.BuildDate=#{time.iso8601}
+      -X github.com/stateful/runme/internal/version.BuildVersion=#{version}
+      -X github.com/stateful/runme/internal/version.Commit=#{Utils.git_head}
+    ]
+
+    system "go", "build", "-o", bin, *std_go_args(ldflags: ldflags), "./main.go"
+    generate_completions_from_executable(bin/"runme", "completion", shells: [:bash, :zsh, :fish])
+  end
+
+  test do
+    system "#{bin}/runme", "--version"
+    markdown = (testpath/"README.md")
+    markdown.write <<~EOS
+      # Some Markdown
+
+      Has some text.
+
+      ```sh { name=foobar }
+      echo "Hello World"
+      ```
+    EOS
+    assert_match "Hello World", shell_output("#{bin}/runme run foobar")
+    assert_match "foobar", shell_output("#{bin}/runme list")
+  end
+end
