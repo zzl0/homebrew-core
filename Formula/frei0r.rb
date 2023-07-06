3b3fc596a2ce27e0ecdb2521d2de3adad18be71b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-07-06 00:30:26 +0000

    frei0r: update 2.3.0 bottle.

--- Formula/frei0r.rb
+++ Formula/frei0r.rb
@@ -6,14 +6,13 @@ class Frei0r < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "40373a9832b28d15c8ffaa880b775882c25b0ab93ab31ae19ec2f2a27799731b"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "dc29537f1ea00bd0f303886fa5819ef44890f0435b12276919c952104843247b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c45ba867a2cc39664a405db9cb2ee9f97289f101fff6be8ca9ba88dcfcb19f8e"
-    sha256 cellar: :any_skip_relocation, ventura:        "49619d94f0580ee04d812a1e598d266f31a571cdda1bff9a297fa11b0c96aaf4"
-    sha256 cellar: :any_skip_relocation, monterey:       "27768088554351600fcc15987a3671cf7cd133533c41fad06776383c4f96cb2c"
-    sha256 cellar: :any_skip_relocation, big_sur:        "e97f700c8a5a2df70aeb941217314d313df4f343992c9dfa8a4f1b80300add27"
-    sha256 cellar: :any_skip_relocation, catalina:       "0287b2caef7477439e9e07420e68ba794524b9348807c36903d5c22caf84134e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1d33e8b0bd9932b996d6d2f612b4d1484ca9c41466b2c5df1e3db6fcbebf5bf6"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "9377921bab7290f44ee4851e67ee2edef807be0fb221156695c27682ed5c04d2"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "6c31413ef3e76554d9f625db882f844945e70dfeecd0fc0a9f806448418f7d10"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "89d7fa7a43cb0197d5f02308584a3c1ef85094904dde22d25c16285991258789"
+    sha256 cellar: :any_skip_relocation, ventura:        "d72f91bc351a162d041b4fbf46fb5244d09a84ecdee25f94a4fe931e05eabef4"
+    sha256 cellar: :any_skip_relocation, monterey:       "22f0cb824fbcda47a91645cd5e3782050a7adb1faa804ba115f0ccb8c6990447"
+    sha256 cellar: :any_skip_relocation, big_sur:        "85733d5d2a2f38e1d9dc9043a600b4567913f115a2920fd3939dacdbec6afd3a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "823095d9928500f908ed3c1798c54aa3096daad87883d5a468449686cc3ce244"
   end
 
   depends_on "cmake" => :build
