804d4ac737cb5673b52b0f476429758176a33c29
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-07-07 04:58:43 +0000

    runme: add 1.3.0 bottle.

--- Formula/runme.rb
+++ Formula/runme.rb
@@ -7,6 +7,16 @@ class Runme < Formula
   license "Apache-2.0"
   head "https://github.com/stateful/runme.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "6deab5d3a6e0f6455a7de1344b2ae293054143742730ddd2eb1317b2ad3072c4"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "050a99f3c51dcaa3fd42c807b83d8a2bdefadeba90248d7eeb1cec450885f3d8"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7650642dd7e7628af46a1b27a380b2b5ed606996f12161d339fa0aeb599ff2da"
+    sha256 cellar: :any_skip_relocation, ventura:        "92e66ed0c275b2bb91b33bf7e8313a50f3f6b38d64e73463f6c81c1dc64e1362"
+    sha256 cellar: :any_skip_relocation, monterey:       "421de20037f267d8d7168676ab58a91ffe78058b1f7c7c8aa8cc2f750fe25495"
+    sha256 cellar: :any_skip_relocation, big_sur:        "35592e25705f1b5227980c2ec7e5dec8aa0bd1602cff1318a2c72f6cfb4bc435"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "eaf1c8ad85b2c7c5402c1df32a7efca9ebe7530cc5b824fb1e0fa27717b5b5a3"
+  end
+
   depends_on "go" => :build
 
   def install
