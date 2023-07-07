c54ebed8f344fb25286e54b2d953e1fbc483c5df
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-07-07 14:09:34 +0000

    inih: update 57 bottle.

--- Formula/inih.rb
+++ Formula/inih.rb
@@ -6,14 +6,13 @@ class Inih < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "62d63767678a16ba47ca7dcbe47ded6e081fe9f4de657868e9db4abf9cfbdfc8"
-    sha256 cellar: :any,                 arm64_monterey: "c58eb0d442cec960657feff64d84a98f936909ee157c8bfc296a020368a0b29c"
-    sha256 cellar: :any,                 arm64_big_sur:  "d321facd01bf101f9ab0dfe77e16bb6ffb673f70940af3f7866dee722fcd0f63"
-    sha256 cellar: :any,                 ventura:        "55e9f70f34da60eb227bf1b8771891332b3e0d3ecf6320ee44f9f194b5b16dbb"
-    sha256 cellar: :any,                 monterey:       "9479af24ebfb40bf0af7b7c932e2a28ee576a6cac5049813046bd77e43057fcc"
-    sha256 cellar: :any,                 big_sur:        "8758bc663cf469d9c56332283c2bc1011b193b0358491ff0a0f53388c62adb5e"
-    sha256 cellar: :any,                 catalina:       "f046d88bcab9301f78c4de41c70bd543920021f572e4fb1622e81f98b0b7e07a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bf5fcb21fdf047cf73e88c099f4813dd11c2fd63ea1099f07663a842063a8496"
+    sha256 cellar: :any,                 arm64_ventura:  "83e40eadfcc9e82827f9362cb096bbe1a3981ced363176e8c9302d652a00ca8c"
+    sha256 cellar: :any,                 arm64_monterey: "001ee6ba12164e738596ef82997f20fbff5f294ec87f19938d366b1b9c9a39a6"
+    sha256 cellar: :any,                 arm64_big_sur:  "ecda1c6e35982227486a058b6ca24a25f2a0940db5dc888d76f51014a7890140"
+    sha256 cellar: :any,                 ventura:        "58d4d12cd171b910f3ce583fe89cd99299628af9d72466e50d04bc1da1be01ad"
+    sha256 cellar: :any,                 monterey:       "fbc1662368467136569d18f316ef473943c165a4a6ba5fdc783788533f71a2a4"
+    sha256 cellar: :any,                 big_sur:        "3fef066c8824e259abf50cf8a1f1bb647ec09edae68e5d5bdd70d639027bf921"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "27431f7f23ab770703972c03dc4324d82d1805c27f384ccbd3b40cedda4ec6a6"
   end
 
   depends_on "meson" => :build
