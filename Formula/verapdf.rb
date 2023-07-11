6d632ab45f6ac1d15c267ca911b6bcfe9bc5c6ab
Rui Chen <rui@chenrui.dev>
2023-07-11 11:10:55 -0400

    verapdf 1.25.16

--- Formula/verapdf.rb
+++ Formula/verapdf.rb
@@ -1,8 +1,8 @@
 class Verapdf < Formula
   desc "Open-source industry-supported PDF/A validation"
   homepage "https://verapdf.org/home/"
-  url "https://github.com/veraPDF/veraPDF-apps/archive/refs/tags/v1.25.14.tar.gz"
-  sha256 "08aadbd7859c404a974a695aaf924b259b04215b7faebd7b6e318dad08a8cec0"
+  url "https://github.com/veraPDF/veraPDF-apps/archive/refs/tags/v1.25.16.tar.gz"
+  sha256 "5c4561ddaeaca44f30cbd9cf373f1d459d808fd581a43c6f45fd4943b6626b95"
   license any_of: ["GPL-3.0-or-later", "MPL-2.0"]
   head "https://github.com/veraPDF/veraPDF-apps.git", branch: "integration"
 
