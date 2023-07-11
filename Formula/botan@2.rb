2ea953d68cb52c9c60413d52ac2320d18a225f79
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2023-07-11 18:54:56 +0800

    botan@2 2.19.3 (new formula)
    This will be supported until 2024.[^1]
    
    Fixes #130139.
    
    [^1]: https://botan.randombit.net/handbook/support.html#branch-support-status

---
+++ Formula/botan@2.rb
@@ -0,0 +1,45 @@
+class BotanAT2 < Formula
+  desc "Cryptographic algorithms and formats library in C++"
+  homepage "https://botan.randombit.net/"
+  url "https://botan.randombit.net/releases/Botan-2.19.3.tar.xz"
+  sha256 "dae047f399c5a47f087db5d3d9d9e8f11ae4985d14c928d71da1aff801802d55"
+  license "BSD-2-Clause"
+  head "https://github.com/randombit/botan.git", branch: "release-2"
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?Botan[._-]v?(2(?:\.\d+)+)\.t/i)
+  end
+
+  keg_only :versioned_formula
+
+  depends_on "pkg-config" => :build
+  depends_on "python@3.11"
+  depends_on "sqlite"
+
+  uses_from_macos "bzip2"
+  uses_from_macos "zlib"
+
+  fails_with gcc: "5"
+
+  def install
+    ENV.cxx11
+
+    args = %W[
+      --prefix=#{prefix}
+      --docdir=share/doc
+      --with-zlib
+      --with-bzip2
+      --with-sqlite3
+    ]
+
+    system "python3.11", "configure.py", *args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.txt").write "Homebrew"
+    (testpath/"testout.txt").write shell_output("#{bin}/botan base64_enc test.txt")
+    assert_match "Homebrew", shell_output("#{bin}/botan base64_dec testout.txt")
+  end
+end
