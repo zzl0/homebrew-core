722784a2e069ea8647460dd35aab71329d37eaf3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-07-11 00:14:16 +0000

    spack: update 0.20.1 bottle.

--- Formula/spack.rb
+++ Formula/spack.rb
@@ -12,13 +12,13 @@ class Spack < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "055156567773c2be5a98e28b5d161f199480fa60a0fedf6b04b60c2afd3dba92"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "055156567773c2be5a98e28b5d161f199480fa60a0fedf6b04b60c2afd3dba92"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "055156567773c2be5a98e28b5d161f199480fa60a0fedf6b04b60c2afd3dba92"
-    sha256 cellar: :any_skip_relocation, ventura:        "53c1d31ad25dae2a66c6fa9a9842d66659f19ffe4190679839e32cb0cbc653bc"
-    sha256 cellar: :any_skip_relocation, monterey:       "53c1d31ad25dae2a66c6fa9a9842d66659f19ffe4190679839e32cb0cbc653bc"
-    sha256 cellar: :any_skip_relocation, big_sur:        "53c1d31ad25dae2a66c6fa9a9842d66659f19ffe4190679839e32cb0cbc653bc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b0314777e3c63212048d0e9354476fb7f8c576c83a07d77cb7f6af59a2862fb3"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "5111d4ffb1be8bbeba5f918eb67d85386f789af65da08b4b675d1908e13aa47b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5111d4ffb1be8bbeba5f918eb67d85386f789af65da08b4b675d1908e13aa47b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5111d4ffb1be8bbeba5f918eb67d85386f789af65da08b4b675d1908e13aa47b"
+    sha256 cellar: :any_skip_relocation, ventura:        "dac4de5eca776add618fc66d3cc601aecaf1a9c0def08ce074632d2a68ce2ac4"
+    sha256 cellar: :any_skip_relocation, monterey:       "dac4de5eca776add618fc66d3cc601aecaf1a9c0def08ce074632d2a68ce2ac4"
+    sha256 cellar: :any_skip_relocation, big_sur:        "dac4de5eca776add618fc66d3cc601aecaf1a9c0def08ce074632d2a68ce2ac4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1239c89be5e006ccf00fca3d40f6d66ebeba426b62ccc659394e3edd9891f73f"
   end
 
   uses_from_macos "python"
