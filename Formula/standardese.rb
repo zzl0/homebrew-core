c91c9181f219652e6320c3d5affeed3628ccfdde
Rui Chen <rui@chenrui.dev>
2023-07-13 18:34:34 -0400

    standardese: revision bump (cmark-gfm 0.29.0.gfm.12)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/standardese.rb
+++ Formula/standardese.rb
@@ -3,7 +3,7 @@ class Standardese < Formula
   homepage "https://standardese.github.io"
   # TODO: use resource blocks for vendored deps
   license "MIT"
-  revision 13
+  revision 14
   head "https://github.com/standardese/standardese.git", branch: "master"
 
   # Remove stable block when patch is no longer needed.
