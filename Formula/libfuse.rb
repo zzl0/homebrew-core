7ce60869c3503411784a9cde2d3b5d58d6641d0d
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-07-13 12:44:00 +0800

    libfuse: update hash
    Resolves #136487.

--- Formula/libfuse.rb
+++ Formula/libfuse.rb
@@ -2,7 +2,7 @@ class Libfuse < Formula
   desc "Reference implementation of the Linux FUSE interface"
   homepage "https://github.com/libfuse/libfuse"
   url "https://github.com/libfuse/libfuse/releases/download/fuse-3.15.1/fuse-3.15.1.tar.gz"
-  sha256 "d236874dab83f2618901627cff4aff0fda69346e17f856da1ddcfdd8db2f3a41"
+  sha256 "13ef77cda531a21c2131f9576042970e98035c0a5f019abf661506efd2d38a4e"
   license any_of: ["LGPL-2.1-only", "GPL-2.0-only"]
   head "https://github.com/libfuse/libfuse.git", branch: "master"
 
