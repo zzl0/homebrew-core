0b74103e780930ae0fb7c2b048b38a50efc3baa7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-07-13 14:57:25 +0000

    imgproxy: update 3.18.2 bottle.

--- Formula/imgproxy.rb
+++ Formula/imgproxy.rb
@@ -7,13 +7,13 @@ class Imgproxy < Formula
   head "https://github.com/imgproxy/imgproxy.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "da603e59b353a694ef491c5806086f70980f033edaacd978aca32ea4fd946f4d"
-    sha256 cellar: :any,                 arm64_monterey: "19613bcf28e16c470704cf988a13122ad0a8e4fa78eaada986dd852b941cdc1a"
-    sha256 cellar: :any,                 arm64_big_sur:  "ece99570436a0ec652ab862da76fc528423cd48abfdd2514a3861c02faf7889f"
-    sha256 cellar: :any,                 ventura:        "7a5716cbfdd68fcb7adaeb0f5e4978e383777cbc17ef0ab7c44f9b6f36e8f036"
-    sha256 cellar: :any,                 monterey:       "b02f0c1ec50b784c2cbfab82c1aa2e93e047a65cd5908a5aaf1f3bb567b1ed4d"
-    sha256 cellar: :any,                 big_sur:        "f1c0403c7a5e9a24504e44d01508ce56b0b5686de11cdc8c433d768a21d0128c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e861caf7968050616e1d82928a40b4a51a245182b9f700f4bc28e1743cd09802"
+    sha256 cellar: :any,                 arm64_ventura:  "ee64eeb07dde664b337fd81b265b56c868f8f7c0256f55117a87a2392fc0e6c8"
+    sha256 cellar: :any,                 arm64_monterey: "ed60da6cac8f055ac7560b4bed51f17deaa33a7c3092c3a780733762b921d03f"
+    sha256 cellar: :any,                 arm64_big_sur:  "d6f96cbc9cb67cedf2f3c2d23dc9d2e7bc097548cb36cc1cb905ed73d97b0216"
+    sha256 cellar: :any,                 ventura:        "dcf6067df1db88aa61fc960f0107e467e14f6b57b21bf656906a6ee832212db6"
+    sha256 cellar: :any,                 monterey:       "4be9c0a79c89705f882e3a1cea5d8a41ce1b92da5d27e52e8a8db4e476fc55f8"
+    sha256 cellar: :any,                 big_sur:        "f10f366e64ba074cb39c4b47a471883d78210a793b944fa7bc70b0257d60f3ed"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d28d46d0e4d5d538ee915eb462ccb7082b2a197bc0f9318d501c5124c3c0fe62"
   end
 
   depends_on "go" => :build
