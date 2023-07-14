28bf98a9840908847629c401cd31a22d876252c5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-07-14 07:27:32 +0000

    clipboard: update 0.8.1_1 bottle.

--- Formula/clipboard.rb
+++ Formula/clipboard.rb
@@ -13,11 +13,13 @@ class Clipboard < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "905b56d8cb61547a48b81c6d20c4c6bf86c87a4b9cbaecbb9b921cad64824045"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "59c1528a5bca57fd3193687e2afd434ea72200f2548e49714456893f9db29637"
-    sha256 cellar: :any_skip_relocation, ventura:        "4cedf3b788c9152db3090b085358358de4ca83694a992e74df9831ae698b5ab1"
-    sha256 cellar: :any_skip_relocation, monterey:       "accf49f04c232386d8c07287e0cc6c91c2676ea3ac50d04c57d25fa558f6ea6f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "089ef1b75f66379f325a6177cb5228752f449c85d5b22cc297a08c5e843ccc26"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "eb7cd95c14967d778fb8c6bf8fc41df9872df5d168302d4a9ba4009705ce81d5"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "9aa8b15aa9d96a00ebc14a7d9ad158dcfa8f20f9ee1b6ebf361e13438b29da86"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c49325785554c9ebb365c1a8ec2213c905a74831238f341e3494096339cd6b6e"
+    sha256 cellar: :any_skip_relocation, ventura:        "6e6d598fc672e75eea970319c52aa14827f617a09ec75ae71c11fbd76fd4c127"
+    sha256 cellar: :any_skip_relocation, monterey:       "6a184431252b3ed79a6b013494d31812b87fa1b0ddfac98e71f6374472dcfee9"
+    sha256 cellar: :any_skip_relocation, big_sur:        "fb880f65f93efd42a453bee516c21ff2125a11eef024cebf76bc6dc654ccf5a7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "214673f6c32a86563805184515292afa2e5c0dee3ae570591ce1885d904dbba0"
   end
 
   depends_on "cmake" => :build
