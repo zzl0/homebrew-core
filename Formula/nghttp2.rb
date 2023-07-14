54637e59b03a2e0c4326df6c4dd638a492736c36
Patrick Linnane <patrick@linnane.io>
2023-07-14 14:15:44 -0700

    nghttp2 1.55.1

--- Formula/nghttp2.rb
+++ Formula/nghttp2.rb
@@ -1,9 +1,9 @@
 class Nghttp2 < Formula
   desc "HTTP/2 C Library"
   homepage "https://nghttp2.org/"
-  url "https://github.com/nghttp2/nghttp2/releases/download/v1.55.0/nghttp2-1.55.0.tar.gz"
-  mirror "http://fresh-center.net/linux/www/nghttp2-1.55.0.tar.gz"
-  sha256 "1e2d802c19041bc16c1bcc48d13858beb39f4ea64c0dfe3f04bfac6de970329d"
+  url "https://github.com/nghttp2/nghttp2/releases/download/v1.55.1/nghttp2-1.55.1.tar.gz"
+  mirror "http://fresh-center.net/linux/www/nghttp2-1.55.1.tar.gz"
+  sha256 "e12fddb65ae3218b4edc083501519379928eba153e71a1673b185570f08beb96"
   license "MIT"
 
   bottle do
