39dac2ceff2963f940b6b9679d60348d93dd642a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-07-14 18:12:01 +0000

    eslint: update 8.45.0 bottle.

--- Formula/eslint.rb
+++ Formula/eslint.rb
@@ -8,13 +8,13 @@ class Eslint < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "be9a7b6133fcd754b94d73d37c97e36571c75f29fac945f4b3d7c8a1f22e333a"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "be9a7b6133fcd754b94d73d37c97e36571c75f29fac945f4b3d7c8a1f22e333a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "be9a7b6133fcd754b94d73d37c97e36571c75f29fac945f4b3d7c8a1f22e333a"
-    sha256 cellar: :any_skip_relocation, ventura:        "5fb483bbc84db5743da0035eac2409c5e0e27b64c4d1051c61318be6e1080e35"
-    sha256 cellar: :any_skip_relocation, monterey:       "5fb483bbc84db5743da0035eac2409c5e0e27b64c4d1051c61318be6e1080e35"
-    sha256 cellar: :any_skip_relocation, big_sur:        "5fb483bbc84db5743da0035eac2409c5e0e27b64c4d1051c61318be6e1080e35"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "be9a7b6133fcd754b94d73d37c97e36571c75f29fac945f4b3d7c8a1f22e333a"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "104f6edd2902d918b171afbcd4be3b911dc828b2881b5d8353494de9c410957d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "104f6edd2902d918b171afbcd4be3b911dc828b2881b5d8353494de9c410957d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "104f6edd2902d918b171afbcd4be3b911dc828b2881b5d8353494de9c410957d"
+    sha256 cellar: :any_skip_relocation, ventura:        "9aa216d09f008826f9d3ff76b8a8ec65bda04836e466fe7a03aaab95bf8c9b7c"
+    sha256 cellar: :any_skip_relocation, monterey:       "9aa216d09f008826f9d3ff76b8a8ec65bda04836e466fe7a03aaab95bf8c9b7c"
+    sha256 cellar: :any_skip_relocation, big_sur:        "9aa216d09f008826f9d3ff76b8a8ec65bda04836e466fe7a03aaab95bf8c9b7c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "104f6edd2902d918b171afbcd4be3b911dc828b2881b5d8353494de9c410957d"
   end
 
   depends_on "node"
