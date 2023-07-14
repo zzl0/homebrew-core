4c7c869baacad2e2e39e80f44543476a46a9bf06
Rui Chen <rui@chenrui.dev>
2023-07-14 09:49:28 -0400

    ansible-language-server 1.2.0

--- Formula/ansible-language-server.rb
+++ Formula/ansible-language-server.rb
@@ -3,8 +3,8 @@ require "language/node"
 class AnsibleLanguageServer < Formula
   desc "Language Server for Ansible Files"
   homepage "https://github.com/ansible/ansible-language-server"
-  url "https://registry.npmjs.org/@ansible/ansible-language-server/-/ansible-language-server-1.1.0.tgz"
-  sha256 "79649ce78b8c1636321f6dfee79a8b1e8d69dd4f7dfb8ad4ced50d31bf8352f4"
+  url "https://registry.npmjs.org/@ansible/ansible-language-server/-/ansible-language-server-1.2.0.tgz"
+  sha256 "29154d4c16fdf2859901eebe8e6715c8c47f1023dc4e1c94a4d8d5ec028668f6"
   license "MIT"
 
   bottle do
