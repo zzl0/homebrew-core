6cc49cd9c1810ab82367a86cacefecb00e52634d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-07-15 06:55:42 +0000

    avrdude: update 7.1 bottle.

--- Formula/avrdude.rb
+++ Formula/avrdude.rb
@@ -7,14 +7,13 @@ class Avrdude < Formula
   head "https://github.com/avrdudes/avrdude.git", branch: "main"
 
   bottle do
-    sha256 arm64_ventura:  "50c20fe810d534c1c4cea3209ca812d35167c4b51e140f7154d96563d9e397e0"
-    sha256 arm64_monterey: "14bda58af73b8b6a92edfccab045e6118a0c18feb436e6afff74d51dfc7610ef"
-    sha256 arm64_big_sur:  "a4fb2845fc9fcad852484d388e6a904cfab655c66b1a61a0b5fff8b56062be97"
-    sha256 ventura:        "52f5fbc144157c77c41a534cddb19b0b81b9734ea2527ad254e103816af81035"
-    sha256 monterey:       "37913838cd9ceadfafe1759b293ebb24ddc9f2c8b8168e297f8fbbfed612aab6"
-    sha256 big_sur:        "c7ddfb9110e029c2b10fe2ddf5e60d1dfa7aa52143aedfb7538a343dc388d156"
-    sha256 catalina:       "b6eba14a4c1e300a69c76d221c4709d1988af07a9bbb2586f10c4515db1dcae3"
-    sha256 x86_64_linux:   "1c317e35642426bce04e756295230de6cfd0d69689aed14c0520d8d4d190097e"
+    sha256 arm64_ventura:  "153e82b4dcaf4819b8eb08485392fb66346c89340ddc682ad567ac0e20b1073a"
+    sha256 arm64_monterey: "8bcf7ba0c42c763fb26e035d528ad1c9aee7996bb13936b20d232977e239f2af"
+    sha256 arm64_big_sur:  "29841e83dcfffe270236c1d0ae38b3e12b38a57b7f538e773b1d4c0765afc6ef"
+    sha256 ventura:        "70682c2e5ea06dde187953d8c39ffbbde8106f337fc2953626d8e32fb44e903b"
+    sha256 monterey:       "afcb908d0862f38423b88a215551fe899cb5e9589a55ea4a1a96fdf9c9e390e7"
+    sha256 big_sur:        "cf690b8c39eae957712aed6f3e12d2fc87674b2664441a4a1b31036bcf9377f0"
+    sha256 x86_64_linux:   "3a2bf4b7c4f5c47866db54defc83d6145c80c406c2ee4100652fab04f079559f"
   end
 
   depends_on "cmake" => :build
