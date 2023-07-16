1082d318fe39e56ae73b0fca6d4e625998354815
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-07-16 07:05:33 +0000

    seqkit: update 2.5.0 bottle.

--- Formula/seqkit.rb
+++ Formula/seqkit.rb
@@ -7,13 +7,13 @@ class Seqkit < Formula
   head "https://github.com/shenwei356/seqkit.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "38404f14993d1fdfecdb0396975c7e2543878ed3a023a3ef3d1b9ba454b1ab5f"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "38404f14993d1fdfecdb0396975c7e2543878ed3a023a3ef3d1b9ba454b1ab5f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "38404f14993d1fdfecdb0396975c7e2543878ed3a023a3ef3d1b9ba454b1ab5f"
-    sha256 cellar: :any_skip_relocation, ventura:        "ab59d265baf201dfe399d255658ca131f79ba58e19c6cc16beb78f7196bdccd8"
-    sha256 cellar: :any_skip_relocation, monterey:       "ab59d265baf201dfe399d255658ca131f79ba58e19c6cc16beb78f7196bdccd8"
-    sha256 cellar: :any_skip_relocation, big_sur:        "ab59d265baf201dfe399d255658ca131f79ba58e19c6cc16beb78f7196bdccd8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "35c3383ed1d88a4b4f48b874833c09f67622d00449554a650e31fc99874116ed"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "eacda5a3ceee7133b8166c49034b9988be5a69c29e10e707c4768ab59469a865"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "eacda5a3ceee7133b8166c49034b9988be5a69c29e10e707c4768ab59469a865"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "eacda5a3ceee7133b8166c49034b9988be5a69c29e10e707c4768ab59469a865"
+    sha256 cellar: :any_skip_relocation, ventura:        "436d7d075821b1c2ff19491cf514173fb2ccc19d4f1d1b3f6b754b22830dea16"
+    sha256 cellar: :any_skip_relocation, monterey:       "436d7d075821b1c2ff19491cf514173fb2ccc19d4f1d1b3f6b754b22830dea16"
+    sha256 cellar: :any_skip_relocation, big_sur:        "436d7d075821b1c2ff19491cf514173fb2ccc19d4f1d1b3f6b754b22830dea16"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0b76e4872cc908ccdf1a90e93fbf39a2bab03f34052bd40e5638d63e6c7904d6"
   end
 
   depends_on "go" => :build
