9d45f6cb00da02e0c08f9e31cc07e605e9b10433
Rui Chen <rui@chenrui.dev>
2023-07-16 12:01:06 -0400

    irssi 1.4.4
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    irssi: update build and test
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    irssi: remove head build
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/irssi.rb
+++ Formula/irssi.rb
@@ -1,10 +1,9 @@
 class Irssi < Formula
   desc "Modular IRC client"
   homepage "https://irssi.org/"
-  url "https://github.com/irssi/irssi/releases/download/1.2.3/irssi-1.2.3.tar.xz"
-  sha256 "a647bfefed14d2221fa77b6edac594934dc672c4a560417b1abcbbc6b88d769f"
+  url "https://github.com/irssi/irssi/releases/download/1.4.4/irssi-1.4.4.tar.xz"
+  sha256 "fefe9ec8c7b1475449945c934a2360ab12693454892be47a6d288c63eb107ead"
   license "GPL-2.0-or-later" => { with: "openvpn-openssl-exception" }
-  revision 4
 
   # This formula uses a file from a GitHub release, so we check the latest
   # release version instead of Git tags.
@@ -23,24 +22,13 @@ class Irssi < Formula
     sha256 x86_64_linux:   "7f06f06ed9c12a97f710a690865d0b549fd88ab346c49876bdfe7f56fea1c27c"
   end
 
-  head do
-    url "https://github.com/irssi/irssi.git", branch: "master"
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
-    depends_on "lynx" => :build
-  end
-
   depends_on "pkg-config" => :build
   depends_on "glib"
   depends_on "openssl@3"
 
   uses_from_macos "ncurses"
-  uses_from_macos "perl"
 
   def install
-    ENV.delete "HOMEBREW_SDKROOT" if MacOS.version == :high_sierra
-
     args = %W[
       --disable-dependency-tracking
       --prefix=#{prefix}
@@ -49,20 +37,8 @@ class Irssi < Formula
       --with-proxy
       --enable-true-color
       --with-socks=no
-      --with-perl=yes
-      --with-perl-lib=#{lib}/perl5/site_perl
+      --with-perl=no
     ]
-
-    args << if OS.mac?
-      "--with-ncurses=#{MacOS.sdk_path/"usr"}"
-    else
-      "--with-ncurses=#{Formula["ncurses"].prefix}"
-    end
-
-    if build.head?
-      ENV["NOCONFIGURE"] = "yes"
-      system "./autogen.sh", *args
-    end
 
     system "./configure", *args
     # "make" and "make install" must be done separately on some systems
@@ -71,17 +47,11 @@ class Irssi < Formula
   end
 
   test do
-    IO.popen("#{bin}/irssi --connect=irc.freenode.net", "w") do |pipe|
-      pipe.puts "/quit\n"
-      pipe.close_write
-    end
+    require "pty"
 
-    # This is not how you'd use Perl with Irssi but it is enough to be
-    # sure the Perl element didn't fail to compile, which is needed
-    # because upstream treats Perl build failures as non-fatal.
-    # To debug a Perl problem copy the following test at the end of the install
-    # block to surface the relevant information from the build warnings.
-    ENV["PERL5LIB"] = lib/"perl5/site_perl"
-    system "perl", "-e", "use Irssi"
+    stdout, = PTY.spawn("#{bin}/irssi -c irc.freenode.net -n testbrew")
+    assert_match "Terminal doesn't support cursor movement", stdout.readline
+
+    assert_match version.to_s, shell_output("#{bin}/irssi --version")
   end
 end
