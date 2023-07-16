48e7505287dd21390c624d8edff3c5dc1e8e5e73
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-07-16 01:17:37 +0000

    hoverfly: add 1.5.3 bottle.

--- Formula/hoverfly.rb
+++ Formula/hoverfly.rb
@@ -6,6 +6,16 @@ class Hoverfly < Formula
   license "Apache-2.0"
   head "https://github.com/SpectoLabs/hoverfly.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "bdd72317748a9528ead8a9f3de5da842e52cf1f7827ff1d4e46e061c0e4d2f09"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "bdd72317748a9528ead8a9f3de5da842e52cf1f7827ff1d4e46e061c0e4d2f09"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bdd72317748a9528ead8a9f3de5da842e52cf1f7827ff1d4e46e061c0e4d2f09"
+    sha256 cellar: :any_skip_relocation, ventura:        "0b9fc18f545f92fec77c4285739eb2b2b8e23518d8e2fd991c31a75c6151a3c2"
+    sha256 cellar: :any_skip_relocation, monterey:       "0b9fc18f545f92fec77c4285739eb2b2b8e23518d8e2fd991c31a75c6151a3c2"
+    sha256 cellar: :any_skip_relocation, big_sur:        "0b9fc18f545f92fec77c4285739eb2b2b8e23518d8e2fd991c31a75c6151a3c2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "873fb82a7cab81e7cb4c6fa64649e4f95408a3dc640443614ada842a4cf332cc"
+  end
+
   depends_on "go" => :build
 
   def install
