0f2e5da0c47dcad15f0155906a61a70c2ba11f76
Rui Chen <rui@chenrui.dev>
2023-07-16 14:39:10 -0400

    bacon 2.11.1 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/bacon.rb
@@ -0,0 +1,49 @@
+class Bacon < Formula
+  desc "Background rust code check"
+  homepage "https://dystroy.org/bacon/"
+  url "https://github.com/Canop/bacon/archive/refs/tags/v2.11.1.tar.gz"
+  sha256 "c0081bfe33064707e464bdeb7d569d818d90614adcb8f662e0acc8c5e1e0ccf3"
+  license "AGPL-3.0-or-later"
+  head "https://github.com/Canop/bacon.git", branch: "main"
+
+  depends_on "rust" => :build
+  depends_on "rustup-init" => :test
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    # Show that we can use a different toolchain than the one provided by the `rust` formula.
+    # https://github.com/Homebrew/homebrew-core/pull/134074#pullrequestreview-1484979359
+    ENV["RUSTUP_INIT_SKIP_PATH_CHECK"] = "yes"
+    system "#{Formula["rustup-init"].bin}/rustup-init", "-y", "--no-modify-path"
+    ENV.prepend_path "PATH", HOMEBREW_CACHE/"cargo_cache/bin"
+    system "rustup", "default", "beta"
+
+    crate = testpath/"demo-crate"
+    mkdir crate do
+      (crate/"src/main.rs").write <<~EOS
+        #[cfg(test)]
+        mod tests {
+          #[test]
+          fn test_it() {
+            assert_eq!(1 + 1, 2);
+          }
+        }
+      EOS
+      (crate/"Cargo.toml").write <<~EOS
+        [package]
+        name = "demo-crate"
+        version = "0.1.0"
+        license = "MIT"
+      EOS
+
+      system bin/"bacon", "--init"
+      assert_match "[jobs.check]", (crate/"bacon.toml").read
+    end
+
+    output = shell_output("#{bin}/bacon --version")
+    assert_match version.to_s, output
+  end
+end
