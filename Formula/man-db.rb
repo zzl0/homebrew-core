4235a0cbff971220d902c7a1d5e660a296b9834a
Rui Chen <rui@chenrui.dev>
2023-07-16 12:22:19 -0400

    man-db: update test (groff 1.23.0)
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    man-db: remove link test
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/man-db.rb
+++ Formula/man-db.rb
@@ -90,12 +90,11 @@ class ManDb < Formula
     ENV["PAGER"] = "cat"
     if OS.mac?
       output = shell_output("#{bin}/gman true")
-      assert_match "BSD General Commands Manual", output
+      assert_match "General Commands Manual", output
       assert_match(/The true utility always returns with (an )?exit code (of )?zero/, output)
     else
       output = shell_output("#{bin}/gman gman")
       assert_match "gman - an interface to the system reference manuals", output
-      assert_match "https://savannah.nongnu.org/bugs/?group=man-db", output
     end
   end
 end
