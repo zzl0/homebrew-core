d3913e4cfacec88fbd334e802cf6b771057cf70d
MoonFruit <dkmoonfruit@gmail.com>
2023-07-17 18:04:45 +0800

    libgit2 1.7.1
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/lib/libgit2.rb
+++ Formula/lib/libgit2.rb
@@ -1,10 +1,9 @@
 class Libgit2 < Formula
   desc "C library of Git core methods that is re-entrant and linkable"
   homepage "https://libgit2.github.com/"
-  url "https://github.com/libgit2/libgit2/archive/v1.6.4.tar.gz"
-  sha256 "d25866a4ee275a64f65be2d9a663680a5cf1ed87b7ee4c534997562c828e500d"
+  url "https://github.com/libgit2/libgit2/archive/refs/tags/v1.7.1.tar.gz"
+  sha256 "17d2b292f21be3892b704dddff29327b3564f96099a1c53b00edc23160c71327"
   license "GPL-2.0-only" => { with: "GCC-exception-2.0" }
-  revision 1
   head "https://github.com/libgit2/libgit2.git", branch: "main"
 
   livecheck do
