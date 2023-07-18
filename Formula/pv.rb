c023d6d0e9a1d3e59b8ce9c0ce8355337d18733c
Porkepix <porkepix@gmail.com>
2023-07-18 14:25:27 +0200

    pv 1.7.24
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/pv.rb
+++ Formula/pv.rb
@@ -1,8 +1,8 @@
 class Pv < Formula
   desc "Monitor data's progress through a pipe"
   homepage "https://www.ivarch.com/programs/pv.shtml"
-  url "https://www.ivarch.com/programs/sources/pv-1.6.20.tar.bz2"
-  sha256 "e831951eff0718fba9b1ef286128773b9d0e723e1fbfae88d5a3188814fdc603"
+  url "https://www.ivarch.com/programs/sources/pv-1.7.24.tar.gz"
+  sha256 "3bf43c5809c8d50066eaeaea5a115f6503c57a38c151975b710aa2bee857b65e"
   license "Artistic-2.0"
 
   livecheck do
@@ -22,11 +22,10 @@ class Pv < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "8f2992e31bcce7194a85d1a2677b8bcf3f5f3e1c6649c2c0b38197b243fc4465"
   end
 
-  # Patch for macOS 11 on Apple Silicon support. Emailed to the maintainer in January 2021.
-  # There is no upstream issue tracker or public mailing list.
-  patch :DATA
-
   def install
+    # Workaround for Xcode 14.3
+    ENV.append_to_cflags "-Wno-implicit-function-declaration" if DevelopmentTools.clang_build_version >= 1403
+
     system "./configure", "--prefix=#{prefix}", "--mandir=#{man}", "--disable-nls"
     system "make", "install"
   end
@@ -34,25 +33,7 @@ class Pv < Formula
   test do
     progress = pipe_output("#{bin}/pv -ns 4 2>&1 >/dev/null", "beer")
     assert_equal "100", progress.strip
+
+    assert_match version.to_s, shell_output("#{bin}/pv --version")
   end
 end
-__END__
-diff --git a/src/include/pv-internal.h b/src/include/pv-internal.h
-index db65eaa..176fc86 100644
---- a/src/include/pv-internal.h
-+++ b/src/include/pv-internal.h
-@@ -18,6 +18,14 @@
- #include <sys/time.h>
- #include <sys/stat.h>
-
-+// Since macOS 10.6, stat64 variants are equivalent to plain stat, and the
-+// suffixed versions have been removed in macOS 11 on Apple Silicon. See stat(2).
-+#ifdef __APPLE__
-+#define stat64 stat
-+#define fstat64 fstat
-+#define lstat64 lstat
-+#endif
-+
- #ifdef __cplusplus
- extern "C" {
- #endif
