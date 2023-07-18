06c5ca0c6e46d88a5a0bd5b04d60d6001aae708e
Patrick Linnane <patrick@linnane.io>
2023-07-18 13:05:44 -0700

    waypoint 0.11.3

--- Formula/waypoint.rb
+++ Formula/waypoint.rb
@@ -1,8 +1,8 @@
 class Waypoint < Formula
   desc "Tool to build, deploy, and release any application on any platform"
   homepage "https://www.waypointproject.io/"
-  url "https://github.com/hashicorp/waypoint/archive/v0.11.2.tar.gz"
-  sha256 "45d38bf222ed7d3b1e5eba4499e2513b6d04e4b26cb77acd1272f4258e1a9822"
+  url "https://github.com/hashicorp/waypoint/archive/v0.11.3.tar.gz"
+  sha256 "cd150448b0720e6b0009d38226c9736fba27c102d76820203f876f9715e69a2d"
   license "MPL-2.0"
   head "https://github.com/hashicorp/waypoint.git", branch: "main"
 
