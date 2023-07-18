7a1d6c47fe7b9117447c33c7ccd6566954f12191
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-07-18 15:51:05 +0000

    kompose: update 1.30.0 bottle.

--- Formula/kompose.rb
+++ Formula/kompose.rb
@@ -6,13 +6,13 @@ class Kompose < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "347d28c38145d2f7b0d8f91195b605e3197b5d43abb22fff7295f2adec15d7b9"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "347d28c38145d2f7b0d8f91195b605e3197b5d43abb22fff7295f2adec15d7b9"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "347d28c38145d2f7b0d8f91195b605e3197b5d43abb22fff7295f2adec15d7b9"
-    sha256 cellar: :any_skip_relocation, ventura:        "e8de4a5ed0f0860ea1e6b9de4a1a2cbb2c20f3d144874c4c792bbc9276468ec2"
-    sha256 cellar: :any_skip_relocation, monterey:       "e8de4a5ed0f0860ea1e6b9de4a1a2cbb2c20f3d144874c4c792bbc9276468ec2"
-    sha256 cellar: :any_skip_relocation, big_sur:        "e8de4a5ed0f0860ea1e6b9de4a1a2cbb2c20f3d144874c4c792bbc9276468ec2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8826dd078f4e1287371b7b2cd6ad5d8509de1dc353e7175998cf3b8bf96596e8"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "6f471417595398f4064e6d93777eed6c307e7e84adacce916016f21eaa04e179"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "6f471417595398f4064e6d93777eed6c307e7e84adacce916016f21eaa04e179"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6f471417595398f4064e6d93777eed6c307e7e84adacce916016f21eaa04e179"
+    sha256 cellar: :any_skip_relocation, ventura:        "2e7f236b87e494cefae14fe2bfe91ca812b47e45396f4248cb9c8f8d5740b664"
+    sha256 cellar: :any_skip_relocation, monterey:       "2e7f236b87e494cefae14fe2bfe91ca812b47e45396f4248cb9c8f8d5740b664"
+    sha256 cellar: :any_skip_relocation, big_sur:        "2e7f236b87e494cefae14fe2bfe91ca812b47e45396f4248cb9c8f8d5740b664"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "41a8d654edf6864f1fc7cbffbc0b876316637e2ff58874dbc57bae378e9aa5c5"
   end
 
   depends_on "go" => :build
