7842652258a0bf1c6d644f4ab5eb76a7d2c721f7
Porkepix <porkepix@gmail.com>
2023-07-18 13:32:58 +0200

    elixir 1.15.4

--- Formula/elixir.rb
+++ Formula/elixir.rb
@@ -1,8 +1,8 @@
 class Elixir < Formula
   desc "Functional metaprogramming aware language built on Erlang VM"
   homepage "https://elixir-lang.org/"
-  url "https://github.com/elixir-lang/elixir/archive/v1.15.3.tar.gz"
-  sha256 "7dc895b09132d344948e05976bfbcdadf0884cda2a26ad4bda095b622d98aeef"
+  url "https://github.com/elixir-lang/elixir/archive/v1.15.4.tar.gz"
+  sha256 "302bf8065ab792a88f6c3a0c01a6bb58737be3e4fc2564c8afd418bf9792501c"
   license "Apache-2.0"
   head "https://github.com/elixir-lang/elixir.git", branch: "main"
 
