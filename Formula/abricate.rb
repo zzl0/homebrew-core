e7a667236c87eea7fd33cf2e24217aad3cc30053
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-07-19 21:11:44 +0000

    abricate: update 1.0.1_1 bottle.

--- Formula/abricate.rb
+++ Formula/abricate.rb
@@ -9,6 +9,7 @@ class Abricate < Formula
 
   bottle do
     rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "fda2b58bf152b001521cacd1dd46f3aa4ea0aab81ef8287e374d2a9fdca74979"
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "20bfb72d3f3e5888db151b87faa3b19a570496c164e4a88d1b1a826ef460fe23"
     sha256 cellar: :any_skip_relocation, ventura:       "17c58b1ad438f67604a57d942865f23c0ffdf78dba3e4147c1db163cf9db249b"
     sha256 cellar: :any_skip_relocation, monterey:      "9af68aea3abc83447b1399866d25687869d443750e737a87ec17cec44af506c3"
