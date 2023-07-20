30185c7601b632accad2a45864add0a8aa06c880
Porkepix <porkepix@gmail.com>
2023-07-20 20:34:56 +0200

    minikube 1.31.1

--- Formula/minikube.rb
+++ Formula/minikube.rb
@@ -2,8 +2,8 @@ class Minikube < Formula
   desc "Run a Kubernetes cluster locally"
   homepage "https://minikube.sigs.k8s.io/"
   url "https://github.com/kubernetes/minikube.git",
-      tag:      "v1.31.0",
-      revision: "47aea544e09f11f475f33e8d126f9eb15e34a4c0"
+      tag:      "v1.31.1",
+      revision: "fd3f3801765d093a485d255043149f92ec0a695f"
   license "Apache-2.0"
   head "https://github.com/kubernetes/minikube.git", branch: "master"
 
