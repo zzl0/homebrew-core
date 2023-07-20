a520739f49f67597c9d68b2ec10ccdb2946bfd79
Rui Chen <rui@chenrui.dev>
2023-07-19 20:17:11 -0400

    copilot 1.29.0

--- Formula/copilot.rb
+++ Formula/copilot.rb
@@ -4,8 +4,8 @@ class Copilot < Formula
   desc "CLI tool for Amazon ECS and AWS Fargate"
   homepage "https://aws.github.io/copilot-cli/"
   url "https://github.com/aws/copilot-cli.git",
-      tag:      "v1.28.0",
-      revision: "88a24783b5c7d1d4f056b3098e37cb7efd7b4381"
+      tag:      "v1.29.0",
+      revision: "55dba4d34ba58be60af2adf7350c51de538b08e8"
   license "Apache-2.0"
   head "https://github.com/aws/copilot-cli.git", branch: "mainline"
 
