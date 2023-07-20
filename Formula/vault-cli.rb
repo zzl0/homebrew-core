8c00dbc417e21ad74da397e6297e57afb023cf2a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-07-20 04:35:21 +0000

    vault-cli: update 3.7.0 bottle.

--- Formula/vault-cli.rb
+++ Formula/vault-cli.rb
@@ -12,7 +12,13 @@ class VaultCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "ec1572a127f1ede8ca9dc5b35c32599e555d450de8f9935c252001070d9b40b3"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "cde05d75b070ebd23dc721b1e772a743b33383c6889676868eae51dbc5314b32"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "cde05d75b070ebd23dc721b1e772a743b33383c6889676868eae51dbc5314b32"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "cde05d75b070ebd23dc721b1e772a743b33383c6889676868eae51dbc5314b32"
+    sha256 cellar: :any_skip_relocation, ventura:        "cde05d75b070ebd23dc721b1e772a743b33383c6889676868eae51dbc5314b32"
+    sha256 cellar: :any_skip_relocation, monterey:       "cde05d75b070ebd23dc721b1e772a743b33383c6889676868eae51dbc5314b32"
+    sha256 cellar: :any_skip_relocation, big_sur:        "cde05d75b070ebd23dc721b1e772a743b33383c6889676868eae51dbc5314b32"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "af7d39d5b57316e6a97a7440b27df46143b0559d83f4d55f088c4980d7476e7f"
   end
 
   depends_on "openjdk"
