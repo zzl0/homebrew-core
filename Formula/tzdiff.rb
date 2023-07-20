6b2f3330a58814f107a2e069640f7e9a78f7f2d8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-07-20 12:55:46 +0000

    tzdiff: add 1.2 bottle.

--- Formula/tzdiff.rb
+++ Formula/tzdiff.rb
@@ -5,6 +5,16 @@ class Tzdiff < Formula
   sha256 "6c3b6afc2bb36b001ee11c091144b8d2c451c699b69be605f2b8a4baf1f55d0a"
   license "BSD-2-Clause"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "653e627cd07a9e45f405a5d49c852eb00b19e1ed66253bf37329b028388c56cc"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "653e627cd07a9e45f405a5d49c852eb00b19e1ed66253bf37329b028388c56cc"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "653e627cd07a9e45f405a5d49c852eb00b19e1ed66253bf37329b028388c56cc"
+    sha256 cellar: :any_skip_relocation, ventura:        "653e627cd07a9e45f405a5d49c852eb00b19e1ed66253bf37329b028388c56cc"
+    sha256 cellar: :any_skip_relocation, monterey:       "653e627cd07a9e45f405a5d49c852eb00b19e1ed66253bf37329b028388c56cc"
+    sha256 cellar: :any_skip_relocation, big_sur:        "653e627cd07a9e45f405a5d49c852eb00b19e1ed66253bf37329b028388c56cc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0325053015b97d2c15992d83cdda4ca756fede5fc9d852ed925e42de045f847b"
+  end
+
   def install
     bin.install "tzdiff"
     man1.install "tzdiff.1"
