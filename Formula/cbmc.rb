e5addd4987fca6f7488f3cea3bb5b24c39526b7b
Diffblue CI CProver <69641018+db-ci-cprover@users.noreply.github.com>
2023-07-20 14:02:12 +0000

    cbmc 5.88.0

--- Formula/cbmc.rb
+++ Formula/cbmc.rb
@@ -2,8 +2,8 @@ class Cbmc < Formula
   desc "C Bounded Model Checker"
   homepage "https://www.cprover.org/cbmc/"
   url "https://github.com/diffblue/cbmc.git",
-      tag:      "cbmc-5.87.0",
-      revision: "5650fb9e116291b44da32341db4288a3bcff0fac"
+      tag:      "cbmc-5.88.0",
+      revision: "63b8b71ae8ed1ca86f8db29c5a738923cae1dbef"
   license "BSD-4-Clause"
 
   bottle do
