d7aff0bc5e6c94b8d24c5d32745ecda4d06c087d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-07-20 12:08:44 +0000

    istioctl: update 1.18.1 bottle.

--- Formula/istioctl.rb
+++ Formula/istioctl.rb
@@ -7,13 +7,13 @@ class Istioctl < Formula
   head "https://github.com/istio/istio.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "47efcb9d2bf4d65f11ad38f7bf8c9d5264825d39f17d3276f8db48df30662af4"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "47efcb9d2bf4d65f11ad38f7bf8c9d5264825d39f17d3276f8db48df30662af4"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "47efcb9d2bf4d65f11ad38f7bf8c9d5264825d39f17d3276f8db48df30662af4"
-    sha256 cellar: :any_skip_relocation, ventura:        "0a12321eb2f5442b53cc9727745ca4a53141ae136dd836359bcea8c6f7d8548f"
-    sha256 cellar: :any_skip_relocation, monterey:       "0a12321eb2f5442b53cc9727745ca4a53141ae136dd836359bcea8c6f7d8548f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0a12321eb2f5442b53cc9727745ca4a53141ae136dd836359bcea8c6f7d8548f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4dbd7e96569b73df3b74baf39a87ad0b10981424ff97fbdc9868411d9345e566"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "4e34b182a1a90d6ad7888269f48f8502c252f8d27677b0a86c9b0b6d27788789"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "846bad65b3b29765e103a77bb2aaae08895636ce95b341777fff1138143854e2"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "53a12129dbca1f4fa1d4d356bd9a2d3bfe03c2b41d85d7b2e240c8d14b6c8782"
+    sha256 cellar: :any_skip_relocation, ventura:        "832a070e14fb16abf87a8de156cb1b9c0a67487dd0ba09a7ada15a9da3f3976d"
+    sha256 cellar: :any_skip_relocation, monterey:       "b142617426fb1445309c4f3a80f59580dd8367f88c8182f42adad99178c99ff0"
+    sha256 cellar: :any_skip_relocation, big_sur:        "850a47325eec07a9eac338e3fe8c5c7aba7235d2e6ea7e9c223448fb10a3461e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e371c2d80705456a16e80f9d0400e03cbdac9f98cd83a4c982805dcd4304f04b"
   end
 
   depends_on "go" => :build
