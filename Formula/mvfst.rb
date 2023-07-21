ee6bdd8dd41c270f3379bfee4754df2a19d48496
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2023-07-21 14:32:40 +0800

    mvfst 2023.07.17.00 (new formula)
    Co-authored-by: Ruoyu Zhong <zhongruoyu@outlook.com>

---
+++ Formula/mvfst.rb
@@ -0,0 +1,78 @@
+class Mvfst < Formula
+  desc "QUIC transport protocol implementation"
+  homepage "https://github.com/facebookincubator/mvfst"
+  # TODO: Upstream has no tags yet. They will next week. Use the tag instead of a commit hash then.
+  #       https://github.com/facebookincubator/mvfst/issues/301
+  url "https://github.com/facebookincubator/mvfst/archive/403b412e57a00f79f3c36f3dbbf2aaf9fc9baa4b.tar.gz"
+  version "2023.07.17.00"
+  sha256 "de3e68e3081f5d56fc574f8b0e270ffc6dea932fa4839c4120c544da64f5c4c9"
+  license "MIT"
+  head "https://github.com/facebookincubator/mvfst.git", branch: "main"
+
+  depends_on "cmake" => [:build, :test]
+  depends_on "googletest" => :test
+  depends_on "boost"
+  depends_on "fizz"
+  depends_on "fmt"
+  depends_on "folly"
+  depends_on "gflags"
+  depends_on "glog"
+  depends_on "openssl@3"
+
+  def install
+    system "cmake", "-S", ".", "-B", "_build",
+                    "-DBUILD_TESTS=OFF",
+                    "-DCMAKE_POSITION_INDEPENDENT_CODE=ON",
+                    *std_cmake_args
+    system "cmake", "--build", "_build"
+    system "cmake", "--install", "_build"
+  end
+
+  test do
+    ENV.delete "CPATH"
+    stable.stage testpath
+
+    (testpath/"CMakeLists.txt").atomic_write <<~CMAKE
+      cmake_minimum_required(VERSION 3.20)
+      project(echo CXX)
+      set(CMAKE_CXX_STANDARD 17)
+
+      list(PREPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/cmake")
+      find_package(fizz REQUIRED)
+      find_package(gflags REQUIRED)
+      find_package(GTest REQUIRED)
+      find_package(mvfst REQUIRED)
+
+      add_executable(echo
+        quic/samples/echo/main.cpp
+        quic/common/test/TestUtils.cpp
+        quic/common/test/TestPacketBuilders.cpp
+      )
+      target_link_libraries(echo ${mvfst_LIBRARIES} fizz::fizz_test_support GTest::gmock)
+      target_include_directories(echo PRIVATE ${CMAKE_CURRENT_SOURCE_DIR})
+    CMAKE
+
+    system "cmake", ".", *std_cmake_args
+    system "cmake", "--build", "."
+
+    server_port = free_port
+    server_pid = spawn "./echo", "--mode", "server",
+                                 "--host", "127.0.0.1", "--port", server_port.to_s
+    sleep 5
+
+    Open3.popen3(
+      "./echo", "--mode", "client",
+                "--host", "127.0.0.1", "--port", server_port.to_s
+    ) do |stdin, _, stderr|
+      stdin.write "Hello world!\n"
+      Timeout.timeout(15) do
+        stderr.each do |line|
+          break if line.include? "Client received data=echo Hello world!"
+        end
+      end
+      stdin.close
+    end
+  ensure
+    Process.kill "TERM", server_pid
+  end
+end
