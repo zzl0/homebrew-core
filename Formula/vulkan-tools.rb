63cb98d4f14d16a2d67d13c4cbf72f1f42972e2c
Rui Chen <rui@chenrui.dev>
2023-07-21 19:36:14 -0400

    vulkan-tools 1.3.258
    Co-authored-by: Ruoyu Zhong <zhongruoyu@outlook.com>

--- Formula/vulkan-tools.rb
+++ Formula/vulkan-tools.rb
@@ -1,8 +1,8 @@
 class VulkanTools < Formula
   desc "Vulkan utilities and tools"
   homepage "https://github.com/KhronosGroup/Vulkan-Tools"
-  url "https://github.com/KhronosGroup/Vulkan-Tools/archive/refs/tags/v1.3.257.tar.gz"
-  sha256 "ed73525c2dd1041a6b680caa5ce1b8936b7f6a200b5715b88fc4b030b110a056"
+  url "https://github.com/KhronosGroup/Vulkan-Tools/archive/refs/tags/v1.3.258.tar.gz"
+  sha256 "6be802a3d933ba60b145c6c0d95062fec206db592206ec092c059951125251b1"
   license "Apache-2.0"
   head "https://github.com/KhronosGroup/Vulkan-Tools.git", branch: "main"
 
@@ -81,7 +81,7 @@ class VulkanTools < Formula
 
     targets = [
       Formula["molten-vk"].opt_lib/shared_library("libMoltenVK"),
-      Formula["vulkan-loader"].opt_lib/shared_library("libvulkan", Formula["vulkan-loader"].version),
+      Formula["vulkan-loader"].opt_lib/shared_library("libvulkan", Formula["vulkan-loader"].version.to_s),
     ]
     prefix.glob("cube/*.app/Contents/Frameworks").each do |framework_dir|
       ln_sf targets, framework_dir, verbose: true
