2f20ebfb2918182601273782020d7735891ad992
Rui Chen <rui@chenrui.dev>
2023-07-21 08:48:44 -0400

    go-feature-flag-relay-proxy 1.12.1

--- Formula/go-feature-flag-relay-proxy.rb
+++ Formula/go-feature-flag-relay-proxy.rb
@@ -2,8 +2,8 @@ class GoFeatureFlagRelayProxy < Formula
   desc "Stand alone server to run GO Feature Flag"
   homepage "https://gofeatureflag.org"
   url "https://github.com/thomaspoignant/go-feature-flag.git",
-      tag:      "v1.12.0",
-      revision: "f568822455302b4dd61bfa7da63303cbc41080b1"
+      tag:      "v1.12.1",
+      revision: "8c17cff4bb7ecc598eb0f8a5e4f159f3980db637"
   license "MIT"
   head "https://github.com/thomaspoignant/go-feature-flag.git", branch: "main"
 
