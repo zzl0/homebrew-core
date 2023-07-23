46c2621e99d84176170c172134fbaa89d3b9a22f
Rui Chen <rui@chenrui.dev>
2023-07-23 19:21:05 -0400

    flintrock: build with pyyaml6
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/flintrock.rb
+++ Formula/flintrock.rb
@@ -24,6 +24,7 @@ class Flintrock < Formula
   depends_on "cffi"
   depends_on "openssl@3"
   depends_on "python@3.11"
+  depends_on "pyyaml"
   depends_on "six"
 
   resource "bcrypt" do
@@ -69,11 +70,6 @@ class Flintrock < Formula
   resource "python-dateutil" do
     url "https://files.pythonhosted.org/packages/4c/c4/13b4776ea2d76c115c1d1b84579f3764ee6d57204f6be27119f13a61d0a9/python-dateutil-2.8.2.tar.gz"
     sha256 "0123cacc1627ae19ddf3c27a5de5bd67ee4586fbdd6440d9748f8abb483d3e86"
-  end
-
-  resource "pyyaml" do
-    url "https://files.pythonhosted.org/packages/a0/a4/d63f2d7597e1a4b55aa3b4d6c5b029991d3b824b5bd331af8d4ab1ed687d/PyYAML-5.4.1.tar.gz"
-    sha256 "607774cbba28732bfa802b54baa7484215f530991055bb562efbed5b2f20a45e"
   end
 
   resource "s3transfer" do
