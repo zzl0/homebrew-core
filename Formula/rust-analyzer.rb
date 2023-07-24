fc87ab61a6f314aac25a35e5c2d1c8e29d2c0ca9
Rui Chen <rui@chenrui.dev>
2023-07-24 07:50:15 -0400

    rust-analyzer 2023-07-24

--- Formula/rust-analyzer.rb
+++ Formula/rust-analyzer.rb
@@ -2,9 +2,9 @@ class RustAnalyzer < Formula
   desc "Experimental Rust compiler front-end for IDEs"
   homepage "https://rust-analyzer.github.io/"
   url "https://github.com/rust-lang/rust-analyzer.git",
-       tag:      "2023-07-17",
-       revision: "d82451103962b1482cb137850c81a1acb34db0e7"
-  version "2023-07-17"
+       tag:      "2023-07-24",
+       revision: "99718d0c8bc5aadd993acdcabc1778fc7b5cc572"
+  version "2023-07-24"
   license any_of: ["Apache-2.0", "MIT"]
 
   bottle do
