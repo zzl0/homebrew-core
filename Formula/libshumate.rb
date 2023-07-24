e526ec45bdb2a728d97fd386868ba4d0144aa458
Tao Zuhong <taozuhong@users.noreply.github.com>
2023-07-24 16:47:55 +0800

    libshumate 1.0.5 (new formula)
    Remote test paramters
    
    Fixed style issue
    
    Fixed style issue
    
    Rename sqlite3 to sqlite
    
    Update Formula/libshumate.rb
    
    Co-authored-by: Caleb Xu <calebcenter@live.com>
    
    Update Formula/libshumate.rb
    
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    
    Update Formula/libshumate.rb
    
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    
    Fixed build issue
    
    Fixed build issue
    
    Fixed build issue
    
    add missing tool: cmake
    
    Fixed build issue
    
    Fixed build issue
    
    Fixed build issue
    
    Fixed build issue
    
    Fixed build issue
    
    Fixed build issue
    
    Fixed build issue
    
    Fixed build issue
    
    Fixed build issue
    
    Fixed build issue
    
    Fixed build issue
    
    Fixed build issue
    
    libshumate: update test
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    Update test code

---
+++ Formula/libshumate.rb
@@ -0,0 +1,45 @@
+class Libshumate < Formula
+  desc "Shumate is a GTK toolkit providing widgets for embedded maps"
+  homepage "https://gitlab.gnome.org/GNOME/libshumate"
+  url "https://download.gnome.org/sources/libshumate/1.0/libshumate-1.0.5.tar.xz"
+  sha256 "a8cfd8df9177ee44b04733b017af13c9e93f98765cdc89b750a48edaf701956c"
+  license "LGPL-2.1-or-later"
+
+  depends_on "gettext" => :build
+  depends_on "gobject-introspection" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkg-config" => [:build, :test]
+  depends_on "vala" => :build
+  depends_on "cmake"
+  depends_on "gi-docgen"
+  depends_on "gtk4"
+  depends_on "libsoup"
+  uses_from_macos "icu4c"
+  uses_from_macos "sqlite"
+
+  def install
+    system "meson", "setup", "build", "-Dgtk_doc=false", *std_meson_args
+    system "meson", "compile", "-C", "build", "--verbose"
+    system "meson", "install", "-C", "build"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <shumate/shumate.h>
+
+      int main(int argc, char *argv[]) {
+        char version[32];
+        snprintf(version, 32, "%d.%d.%d", SHUMATE_MAJOR_VERSION, SHUMATE_MINOR_VERSION, SHUMATE_MICRO_VERSION);
+        return 0;
+      }
+    EOS
+    ENV.prepend_path "PKG_CONFIG_PATH", Formula["icu4c"].opt_lib/"pkgconfig" if OS.mac?
+    flags = shell_output("#{Formula["pkg-config"].opt_bin}/pkg-config --cflags --libs shumate-1.0").strip.split
+    system ENV.cc, "test.c", "-o", "test", *flags
+    system "./test"
+
+    # include a version check for the pkg-config files
+    assert_match version.to_s, (lib/"pkgconfig/shumate-1.0.pc").read
+  end
+end
