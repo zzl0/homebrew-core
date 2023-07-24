e9be9052e563ddd3cf5457956bd606ed12b368f6
Rui Chen <rui@chenrui.dev>
2023-07-24 07:58:18 -0400

    jet 2.10.1
    jet: update test
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/jet.rb
+++ Formula/jet.rb
@@ -1,8 +1,8 @@
 class Jet < Formula
   desc "Type safe SQL builder with code generation and auto query result data mapping"
   homepage "https://github.com/go-jet/jet"
-  url "https://github.com/go-jet/jet/archive/refs/tags/v2.10.0.tar.gz"
-  sha256 "6345ed0ea92159ff2648cc1b4154478da8a2b16a61d47c74e394169897ba8130"
+  url "https://github.com/go-jet/jet/archive/refs/tags/v2.10.1.tar.gz"
+  sha256 "94f964b2eba69e2df1f922cf83b193a276f6b64ac742bd65670fd07a307aee22"
   license "Apache-2.0"
   head "https://github.com/go-jet/jet.git", branch: "master"
 
@@ -24,6 +24,6 @@ class Jet < Formula
 
   test do
     cmd = "#{bin}/jet -source=mysql -host=localhost -port=3306 -user=jet -password=jet -dbname=jetdb -path=./gen 2>&1"
-    assert_match "connection refused", shell_output(cmd, 251)
+    assert_match "connection refused", shell_output(cmd, 2)
   end
 end
