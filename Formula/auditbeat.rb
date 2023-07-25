9da796a4ff5b8669e3759bff6f49219f0165cff5
Rui Chen <rui@chenrui.dev>
2023-07-25 19:06:27 -0400

    auditbeat 8.9.0

--- Formula/auditbeat.rb
+++ Formula/auditbeat.rb
@@ -2,8 +2,8 @@ class Auditbeat < Formula
   desc "Lightweight Shipper for Audit Data"
   homepage "https://www.elastic.co/products/beats/auditbeat"
   url "https://github.com/elastic/beats.git",
-      tag:      "v8.8.2",
-      revision: "92c6b2370e46e549acda91b396f665a7e51e249c"
+      tag:      "v8.9.0",
+      revision: "dd50d49baeb99e0d21a31adb621908a7f0091046"
   license "Apache-2.0"
   head "https://github.com/elastic/beats.git", branch: "main"
 
