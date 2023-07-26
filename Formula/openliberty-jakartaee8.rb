918bc093cf318591e371e6d0d839a2d940d8ab98
Rui Chen <rui@chenrui.dev>
2023-07-26 16:40:33 -0400

    openliberty-jakartaee8 23.0.0.7

--- Formula/openliberty-jakartaee8.rb
+++ Formula/openliberty-jakartaee8.rb
@@ -1,8 +1,8 @@
 class OpenlibertyJakartaee8 < Formula
   desc "Lightweight open framework for Java (Jakarta EE 8)"
   homepage "https://openliberty.io"
-  url "https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/release/23.0.0.6/openliberty-javaee8-23.0.0.6.zip"
-  sha256 "a9b3336d8c41940f9727ccf9a3bb76bc362d7acc5ae20d47d6309d45a7506605"
+  url "https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/release/23.0.0.7/openliberty-javaee8-23.0.0.7.zip"
+  sha256 "5449e0367b24e018571b20610233cac9a79761c25bee89f0b9a3cbd1fa74c230"
   license "EPL-1.0"
 
   livecheck do
