95cd2643363d755f05b71899b0e03144445d4702
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-07-26 08:10:22 +0000

    cmake-docs: update 3.27.1 bottle.

--- Formula/cmake-docs.rb
+++ Formula/cmake-docs.rb
@@ -13,13 +13,13 @@ class CmakeDocs < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3c7de62cceaf0e4399c94285841c05a0f0f08ba5c7e43362ff184c215fbf5895"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3c7de62cceaf0e4399c94285841c05a0f0f08ba5c7e43362ff184c215fbf5895"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3c7de62cceaf0e4399c94285841c05a0f0f08ba5c7e43362ff184c215fbf5895"
-    sha256 cellar: :any_skip_relocation, ventura:        "3b63e737b09da6f7c7a3aaeb51ce60c7198fa1bb66f2b14126961f2d8f95ffa7"
-    sha256 cellar: :any_skip_relocation, monterey:       "3b63e737b09da6f7c7a3aaeb51ce60c7198fa1bb66f2b14126961f2d8f95ffa7"
-    sha256 cellar: :any_skip_relocation, big_sur:        "3b63e737b09da6f7c7a3aaeb51ce60c7198fa1bb66f2b14126961f2d8f95ffa7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "918f8b2bc087667165578341e2cd64b360743a77b5b5b07781c6cf480eedbf74"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "c4ee7ee1ac692a489de148d18f030b5db2589bd6e09d3a479a268b20f5f0d1af"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c4ee7ee1ac692a489de148d18f030b5db2589bd6e09d3a479a268b20f5f0d1af"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c4ee7ee1ac692a489de148d18f030b5db2589bd6e09d3a479a268b20f5f0d1af"
+    sha256 cellar: :any_skip_relocation, ventura:        "f0e6f1af63c3c884a45292864c37e31898e6d731a921e6225fae6760e87a7545"
+    sha256 cellar: :any_skip_relocation, monterey:       "f0e6f1af63c3c884a45292864c37e31898e6d731a921e6225fae6760e87a7545"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f0e6f1af63c3c884a45292864c37e31898e6d731a921e6225fae6760e87a7545"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "512dd02ad1f9397ed031f71acb1517cb3384acaeaf42fad62ccbfb7995c1ee2b"
   end
 
   depends_on "cmake" => :build
