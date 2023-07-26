c38a7288c23ecd21ad413ba0a79f14d79c0e92b5
Rui Chen <rui@chenrui.dev>
2023-07-25 23:24:23 -0400

    cfitsio 4.3.0

--- Formula/cfitsio.rb
+++ Formula/cfitsio.rb
@@ -1,8 +1,8 @@
 class Cfitsio < Formula
   desc "C access to FITS data files with optional Fortran wrappers"
   homepage "https://heasarc.gsfc.nasa.gov/docs/software/fitsio/fitsio.html"
-  url "https://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio-4.2.0.tar.gz"
-  sha256 "eba53d1b3f6e345632bb09a7b752ec7ced3d63ec5153a848380f3880c5d61889"
+  url "https://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio-4.3.0.tar.gz"
+  sha256 "fdadc01d09cf9f54253802c5ec87eb10de51ce4130411415ae88c30940621b8b"
   license "CFITSIO"
 
   livecheck do
