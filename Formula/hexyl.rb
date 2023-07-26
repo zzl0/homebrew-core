a7b34992b97a669db630545562070190edc70c7e
Rui Chen <rui@chenrui.dev>
2023-07-26 07:23:39 -0400

    hexyl 0.13.1

--- Formula/hexyl.rb
+++ Formula/hexyl.rb
@@ -1,8 +1,8 @@
 class Hexyl < Formula
   desc "Command-line hex viewer"
   homepage "https://github.com/sharkdp/hexyl"
-  url "https://github.com/sharkdp/hexyl/archive/v0.13.0.tar.gz"
-  sha256 "1b5d2fc49724a7ce76253f328190b9b5e09ba39af7ca1be9aa610c5c7e8c91b9"
+  url "https://github.com/sharkdp/hexyl/archive/v0.13.1.tar.gz"
+  sha256 "a4b3009aa7210f7d80f91d6b01543d1544586c4509247f01e028f1a56781056d"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/sharkdp/hexyl.git", branch: "master"
 
