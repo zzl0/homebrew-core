e3c4938eac4c02ef5b54f581d80300153fab7b7c
Rui Chen <rui@chenrui.dev>
2023-07-26 16:41:35 -0400

    sonar-scanner 5.0.0.2966
    sonar-scanner: update to use openjdk
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/sonar-scanner.rb
+++ Formula/sonar-scanner.rb
@@ -1,8 +1,8 @@
 class SonarScanner < Formula
   desc "Launcher to analyze a project with SonarQube"
   homepage "https://docs.sonarqube.org/latest/analysis/scan/sonarscanner/"
-  url "https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.8.0.2856.zip"
-  sha256 "642d3e189bcca51055bc17d349fc575bf6259df1b54f4077a9a6c586afd65bff"
+  url "https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-5.0.0.2966.zip"
+  sha256 "1552987ca036d85b83ba68b815d641e26df04afff02570c6b72bec0fe4adea37"
   license "LGPL-3.0-or-later"
   head "https://github.com/SonarSource/sonar-scanner-cli.git", branch: "master"
 
@@ -10,7 +10,7 @@ class SonarScanner < Formula
     sha256 cellar: :any_skip_relocation, all: "2afdbf58216d9c6e1dc8138fbaae51ac5945bc9e86f8520d70acf710df97e536"
   end
 
-  depends_on "openjdk@11"
+  depends_on "openjdk"
 
   def install
     rm_rf Dir["bin/*.bat"]
@@ -18,7 +18,9 @@ class SonarScanner < Formula
     bin.install libexec/"bin/sonar-scanner"
     etc.install libexec/"conf/sonar-scanner.properties"
     ln_s etc/"sonar-scanner.properties", libexec/"conf/sonar-scanner.properties"
-    bin.env_script_all_files libexec/"bin/", SONAR_SCANNER_HOME: libexec, JAVA_HOME: Formula["openjdk@11"].opt_prefix
+    bin.env_script_all_files libexec/"bin/",
+                              SONAR_SCANNER_HOME: libexec,
+                              JAVA_HOME:          Formula["openjdk"].opt_prefix
   end
 
   test do
