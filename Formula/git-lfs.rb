00083543ff7ab75db6611e07e8f92c7c8d0c164f
Chris Darroch <chrisd8088@github.com>
2023-07-26 14:12:36 -0700

    git-lfs 3.4.0

--- Formula/git-lfs.rb
+++ Formula/git-lfs.rb
@@ -1,8 +1,8 @@
 class GitLfs < Formula
   desc "Git extension for versioning large files"
   homepage "https://git-lfs.github.com/"
-  url "https://github.com/git-lfs/git-lfs/releases/download/v3.3.0/git-lfs-v3.3.0.tar.gz"
-  sha256 "964c200bb7dcd6da44cbf0cfa88575f7e48d26925f8ec86d634d3f83306a0920"
+  url "https://github.com/git-lfs/git-lfs/releases/download/v3.4.0/git-lfs-v3.4.0.tar.gz"
+  sha256 "d65795242550a9ed823979282cc3572a7b221f9be3440b9bf3a1d6d81c51a416"
   license "MIT"
 
   bottle do
