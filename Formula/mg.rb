bbbf6280bdacdeb8139836b7181ff4c87031ff01
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-07-26 04:47:45 +0000

    mg: update 7.3 bottle.

--- Formula/mg.rb
+++ Formula/mg.rb
@@ -7,14 +7,13 @@ class Mg < Formula
   version_scheme 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "478be534a09e6185a1c0047aa893d4016401ae4a81dc0177651a3c7fd3789b78"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3052fc6f184a489918dcb2f59feae7c25e11d911e4b507e2d1a1f74dfa981a20"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "22ed7427d1bbe11394d61c776f7bd2d8aefa2669a4159e81196c8d4bb706f723"
-    sha256 cellar: :any_skip_relocation, ventura:        "34106b99ddac7dd6bbd65ca44a0469613daeb25c8a2a119497acba2468a7f2cc"
-    sha256 cellar: :any_skip_relocation, monterey:       "39a073c05e204261444939e0e1fbd5aed12c2bbab7a787f713a463a9f4ce6855"
-    sha256 cellar: :any_skip_relocation, big_sur:        "e467c5834007598019dbe5e2fb8ebddf2c06dc153ad3af0f659ef758983125cc"
-    sha256 cellar: :any_skip_relocation, catalina:       "40b95e37d6c760e37102fffb1d037b14ae12bb72b5e9313a6eaa76da4ed933cd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "67149965b00f9cd7a4e8c632354c958ebe2d044ee216ac74e68f57f9cdc4d391"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "22b26617c6ce69d7c1e5e69a0628aac1db8f60e164c788bb7784841fd02818a5"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f367179c081b6bd5f234d68d8134466d1d7a7e457b3258053da668e454b087bb"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a1ae7a5e2cc9fcca2bb497a9dc76bd7473b204a2f806edc1db524dd5aba9e5f2"
+    sha256 cellar: :any_skip_relocation, ventura:        "908aaa04c673ffa9db16ac98987b01b6d822a1285f3ab62a0e4d4f0c2d38b919"
+    sha256 cellar: :any_skip_relocation, monterey:       "e3190e17138e2c21d7429ff591be1c3d574a7e13a0e1a10457f2e479cc5bf9e7"
+    sha256 cellar: :any_skip_relocation, big_sur:        "12e3599c5fe68404690bae22a653cc00915cdf797041be2d67845e4760d41df7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "df7547e37e627c0504896e045d8c8df6adc3ea3dbdade674b1b964fcf333397f"
   end
 
   uses_from_macos "expect" => :test
