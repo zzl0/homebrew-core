0d2e95f1b37dd517f9621e3f09a51d0973442b69
Porkepix <porkepix@gmail.com>
2023-07-27 14:58:06 +0200

    xbyak 6.71

--- Formula/xbyak.rb
+++ Formula/xbyak.rb
@@ -1,8 +1,8 @@
 class Xbyak < Formula
   desc "C++ JIT assembler for x86 (IA32), x64 (AMD64, x86-64)"
   homepage "https://github.com/herumi/xbyak"
-  url "https://github.com/herumi/xbyak/archive/refs/tags/v6.70.tar.gz"
-  sha256 "0d2b8c89a22df9c8ac800de7b60ce21362f86bf41ec0145522b23b464ad25ca3"
+  url "https://github.com/herumi/xbyak/archive/refs/tags/v6.71.tar.gz"
+  sha256 "caa98213d7f5c86720d06bae22ddb71a6b8bbeeac3d7dbfacc01136bc2e5a9ec"
   license "BSD-3-Clause"
   head "https://github.com/herumi/xbyak.git", branch: "master"
 
