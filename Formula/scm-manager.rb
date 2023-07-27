d39fdadb2204f8b7ccdc28a9fe449f3a99175aec
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-07-27 20:37:19 +0000

    scm-manager: update 2.45.2 bottle.

--- Formula/scm-manager.rb
+++ Formula/scm-manager.rb
@@ -11,7 +11,13 @@ class ScmManager < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "5d38ce2a820d6f82b6b618eb2da8c6cb4b42e65571cb6f30074e8849571df333"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "4baa76a1fb3b76f72fdc49b54c8ecbcb36907b94e77df03c63c0fbe4ec786c3a"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "4baa76a1fb3b76f72fdc49b54c8ecbcb36907b94e77df03c63c0fbe4ec786c3a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4baa76a1fb3b76f72fdc49b54c8ecbcb36907b94e77df03c63c0fbe4ec786c3a"
+    sha256 cellar: :any_skip_relocation, ventura:        "4baa76a1fb3b76f72fdc49b54c8ecbcb36907b94e77df03c63c0fbe4ec786c3a"
+    sha256 cellar: :any_skip_relocation, monterey:       "4baa76a1fb3b76f72fdc49b54c8ecbcb36907b94e77df03c63c0fbe4ec786c3a"
+    sha256 cellar: :any_skip_relocation, big_sur:        "4baa76a1fb3b76f72fdc49b54c8ecbcb36907b94e77df03c63c0fbe4ec786c3a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "aca9714e2bb9d8ffe8d5cf70aabb6c9fb5875e6c9276b781e92bbb301078fe83"
   end
 
   depends_on "jsvc"
