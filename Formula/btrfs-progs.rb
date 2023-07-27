b9a9f91bab91c840fc9e60de9c4595351c74a653
Rui Chen <rui@chenrui.dev>
2023-07-27 10:31:49 -0400

    btrfs-progs 6.3.3

--- Formula/btrfs-progs.rb
+++ Formula/btrfs-progs.rb
@@ -1,8 +1,8 @@
 class BtrfsProgs < Formula
   desc "Userspace utilities to manage btrfs filesystems"
   homepage "https://btrfs.wiki.kernel.org/index.php/Main_Page"
-  url "https://mirrors.edge.kernel.org/pub/linux/kernel/people/kdave/btrfs-progs/btrfs-progs-v6.3.2.tar.xz"
-  sha256 "a9f26199d5817816f5b0a8f0f63763f3fb0143a20388a929b742dcad5bf27c24"
+  url "https://mirrors.edge.kernel.org/pub/linux/kernel/people/kdave/btrfs-progs/btrfs-progs-v6.3.3.tar.xz"
+  sha256 "4be30015760270d081642cc0023a8bfee7cb657a2aa055644e6a56d68695b96e"
   license all_of: [
     "GPL-2.0-only",
     "LGPL-2.1-or-later", # libbtrfsutil
