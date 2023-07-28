665d8dc1d23575af4735a33dd81b8f7aadaf56d1
Rui Chen <rui@chenrui.dev>
2023-07-28 11:34:29 -0400

    zbctl 8.2.10

--- Formula/zbctl.rb
+++ Formula/zbctl.rb
@@ -2,8 +2,8 @@ class Zbctl < Formula
   desc "Zeebe CLI client"
   homepage "https://docs.camunda.io/docs/apis-clients/cli-client/index/"
   url "https://github.com/camunda/zeebe.git",
-      tag:      "8.2.9",
-      revision: "8066d8065a9446c75980b4bb003677f823e0e180"
+      tag:      "8.2.10",
+      revision: "123639f566c1e52b9e5e7b659a6945584d775b3e"
   license "Apache-2.0"
   head "https://github.com/camunda/zeebe.git", branch: "develop"
 
