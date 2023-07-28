c5d99964065cfec3d6f4603e90e565306a4de715
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-07-28 14:49:53 +0000

    zsh-history-substring-search: update 1.1.0 bottle.

--- Formula/zsh-history-substring-search.rb
+++ Formula/zsh-history-substring-search.rb
@@ -6,7 +6,13 @@ class ZshHistorySubstringSearch < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "556fffc8eceb018b5079b6e4ff3b152e7223b4f5f9416d1d210409aceabf4203"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "389dd56ef87f77caff258d05baa26c20adbc586e6afa6bf2520c1c47c7095485"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "389dd56ef87f77caff258d05baa26c20adbc586e6afa6bf2520c1c47c7095485"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "389dd56ef87f77caff258d05baa26c20adbc586e6afa6bf2520c1c47c7095485"
+    sha256 cellar: :any_skip_relocation, ventura:        "389dd56ef87f77caff258d05baa26c20adbc586e6afa6bf2520c1c47c7095485"
+    sha256 cellar: :any_skip_relocation, monterey:       "389dd56ef87f77caff258d05baa26c20adbc586e6afa6bf2520c1c47c7095485"
+    sha256 cellar: :any_skip_relocation, big_sur:        "389dd56ef87f77caff258d05baa26c20adbc586e6afa6bf2520c1c47c7095485"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0a6975d72e0b087069583d9e515248371780264481a106a2ec491cbc11945d6e"
   end
 
   uses_from_macos "zsh"
