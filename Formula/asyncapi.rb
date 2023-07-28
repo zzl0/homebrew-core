7b0b03b203223686f354615e5ec0f89c065bc59c
Rui Chen <rui@chenrui.dev>
2023-07-28 18:03:26 -0400

    asyncapi 0.51.4

--- Formula/asyncapi.rb
+++ Formula/asyncapi.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Asyncapi < Formula
   desc "All in one CLI for all AsyncAPI tools"
   homepage "https://github.com/asyncapi/cli"
-  url "https://registry.npmjs.org/@asyncapi/cli/-/cli-0.51.3.tgz"
-  sha256 "a0b2a42a3ba82fbdf843ff9a7f1e18adbfb4802b6f8bf61971fd8a95cbc503d1"
+  url "https://registry.npmjs.org/@asyncapi/cli/-/cli-0.51.4.tgz"
+  sha256 "54a451e7d8e560adc4bdf2c635f30403d284620fa53bc73b5037323170d0cf04"
   license "Apache-2.0"
 
   bottle do
