d65442e1d4cd0c91634df04dec1c60e70feb0339
Rui Chen <rui@chenrui.dev>
2023-07-29 10:24:23 -0400

    simple-tiles: revision bump (gdal 3.7.1)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/simple-tiles.rb
+++ Formula/simple-tiles.rb
@@ -4,7 +4,7 @@ class SimpleTiles < Formula
   url "https://github.com/propublica/simple-tiles/archive/v0.6.2.tar.gz"
   sha256 "343ae52a0b20ee091b14bc145b7c78fed13b7272acd827626283b70f178dfa34"
   license "MIT"
-  revision 1
+  revision 2
   head "https://github.com/propublica/simple-tiles.git", branch: "master"
 
   bottle do
