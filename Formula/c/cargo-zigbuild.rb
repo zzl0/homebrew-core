4b47bb4b08943595fb413110bbafc7bbf502e977
messense <messense@icloud.com>
2023-07-29 13:23:21 +0800

    cargo-zigbuild 0.17.0

--- Formula/c/cargo-zigbuild.rb
+++ Formula/c/cargo-zigbuild.rb
@@ -1,8 +1,8 @@
 class CargoZigbuild < Formula
   desc "Compile Cargo project with zig as linker"
   homepage "https://github.com/rust-cross/cargo-zigbuild"
-  url "https://github.com/rust-cross/cargo-zigbuild/archive/refs/tags/v0.16.12.tar.gz"
-  sha256 "1cb8253043d5c9acd0b12fa98fc30dc7970fa07240a9014dc8f9dc87742e19c8"
+  url "https://github.com/rust-cross/cargo-zigbuild/archive/refs/tags/v0.17.0.tar.gz"
+  sha256 "5ed6d4a01a7fe14e8a9408dd2d3835b0ead3cf435963c1724ef2f79cff0873a0"
   license "MIT"
   head "https://github.com/rust-cross/cargo-zigbuild.git", branch: "main"
 
