f0ffe02251be44e8e4b3d04c6a93a0fc04bef1e5
Rui Chen <rui@chenrui.dev>
2023-07-29 10:24:22 -0400

    postgis: revision bump (gdal 3.7.1)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/postgis.rb
+++ Formula/postgis.rb
@@ -4,6 +4,7 @@ class Postgis < Formula
   url "https://download.osgeo.org/postgis/source/postgis-3.3.4.tar.gz"
   sha256 "9d41eaef70e811a4fe2f4a431d144c0c57ce17c2c1a3c938ddaf4e5a3813b0d8"
   license "GPL-2.0-or-later"
+  revision 1
 
   livecheck do
     url "https://download.osgeo.org/postgis/source/"
