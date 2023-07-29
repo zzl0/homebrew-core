2f53246fcf92f344990d38d68619af374d443b3e
Dustin Rodrigues <dust.rod@gmail.com>
2023-07-28 21:48:40 -0400

    nifi-registry: 1.23.0

--- Formula/nifi-registry.rb
+++ Formula/nifi-registry.rb
@@ -1,9 +1,9 @@
 class NifiRegistry < Formula
   desc "Centralized storage & management of NiFi/MiNiFi shared resources"
   homepage "https://nifi.apache.org/registry"
-  url "https://www.apache.org/dyn/closer.lua?path=/nifi/1.22.0/nifi-registry-1.22.0-bin.zip"
-  mirror "https://archive.apache.org/dist/nifi/1.22.0/nifi-registry-1.22.0-bin.zip"
-  sha256 "c7fc6a4aaf8c73cda6ce7ac3a0a6af1b7bd9a0baa75d51ee1e580729f62f0ed9"
+  url "https://www.apache.org/dyn/closer.lua?path=/nifi/1.23.0/nifi-registry-1.23.0-bin.zip"
+  mirror "https://archive.apache.org/dist/nifi/1.23.0/nifi-registry-1.23.0-bin.zip"
+  sha256 "978b7fe75c6207a8272b1d43b6ce1b44091158bc5169bf64d864df2d94db120b"
   license "Apache-2.0"
 
   bottle do
