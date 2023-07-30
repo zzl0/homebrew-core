93a24ed30d51fee7ad529dc5581d25ae316c5e8d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-07-30 04:43:54 +0000

    sbom-tool: add 1.1.8 bottle.

--- Formula/sbom-tool.rb
+++ Formula/sbom-tool.rb
@@ -6,6 +6,13 @@ class SbomTool < Formula
   license "MIT"
   head "https://github.com/microsoft/sbom-tool.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, ventura:      "926bf0ba5b762ab9158c32056d9111d19794caee7374eb9abe855e83e0bee756"
+    sha256 cellar: :any_skip_relocation, monterey:     "926bf0ba5b762ab9158c32056d9111d19794caee7374eb9abe855e83e0bee756"
+    sha256 cellar: :any_skip_relocation, big_sur:      "926bf0ba5b762ab9158c32056d9111d19794caee7374eb9abe855e83e0bee756"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "c9c1d0406b918f46d24fbb407207b5a342a16a12a61e576b04d5c1f205196a8a"
+  end
+
   depends_on "dotnet" => :build
   # currently does not support arm build
   # upstream issue, https://github.com/microsoft/sbom-tool/issues/223
