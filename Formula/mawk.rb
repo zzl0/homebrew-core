702ffb842d67da7ff9921f53edd63f8cdc2bb03a
Porkepix <porkepix@gmail.com>
2023-07-31 14:23:29 +0200

    mawk 1.3.4-20230730

--- Formula/mawk.rb
+++ Formula/mawk.rb
@@ -1,8 +1,8 @@
 class Mawk < Formula
   desc "Interpreter for the AWK Programming Language"
   homepage "https://invisible-island.net/mawk/"
-  url "https://invisible-mirror.net/archives/mawk/mawk-1.3.4-20230525.tgz"
-  sha256 "5639d14bb9124373b3d7f957d2b925ad8ad9656d46212c3f23dbca810cc9269f"
+  url "https://invisible-mirror.net/archives/mawk/mawk-1.3.4-20230730.tgz"
+  sha256 "810b7cc0aa2bff5ff215f237e275b327b21ba49a0d7b36930e3ddc80f4ce5618"
   license "GPL-2.0-only"
 
   livecheck do
