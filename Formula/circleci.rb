df571a9a23a57616287d7c75a6fc19f3e68a780c
Rui Chen <rui@chenrui.dev>
2023-07-31 10:11:24 -0400

    circleci 0.1.28196
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/circleci.rb
+++ Formula/circleci.rb
@@ -3,8 +3,8 @@ class Circleci < Formula
   homepage "https://circleci.com/docs/2.0/local-cli/"
   # Updates should be pushed no more frequently than once per week.
   url "https://github.com/CircleCI-Public/circleci-cli.git",
-      tag:      "v0.1.28084",
-      revision: "458f94e02dce1ea0eb4fe608cd2943a32de9578e"
+      tag:      "v0.1.28196",
+      revision: "0fd0133721c20a21e8a062ae6fe3ff8bca18fc69"
   license "MIT"
   head "https://github.com/CircleCI-Public/circleci-cli.git", branch: "master"
 
