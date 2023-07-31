ab5c2e3eea587d12a00e552980eeb68a1371ca1e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-07-31 16:48:01 +0000

    arm-linux-gnueabihf-binutils: update 2.41 bottle.

--- Formula/arm-linux-gnueabihf-binutils.rb
+++ Formula/arm-linux-gnueabihf-binutils.rb
@@ -11,13 +11,13 @@ class ArmLinuxGnueabihfBinutils < Formula
   end
 
   bottle do
-    sha256 arm64_ventura:  "c64ae1f287f091d817bfb6652f1d344d479f828d913f95150cd24dc0b55b58b7"
-    sha256 arm64_monterey: "7d8bd2f8bc29a789978bdb0f18aca064b5602a4021ebe3bde182275d96747ef1"
-    sha256 arm64_big_sur:  "98b27eb2768454706840805d1bfcc35aad5721c96e296150c26e45820fc2039c"
-    sha256 ventura:        "35c178b570359bdf49e609783af569e304ff8242f7035b5aa8ea02ed2530d0c4"
-    sha256 monterey:       "f2fb94a14bc2a0a75e26297455332bc32a327cd382cdf5a6164cc94f1b1905be"
-    sha256 big_sur:        "e70d7a7c9db95884954f18a7652017c83002465f8466cfbb95ae91edec873f74"
-    sha256 x86_64_linux:   "fa4359b2a440212ebf5c7739e46d14ab28f2787bcd462f25914ff37c0c7a97e4"
+    sha256 arm64_ventura:  "d306fde79e15534a42e29d74002e8ffd1a3a07e6d52bbf9a68b71cd31f736004"
+    sha256 arm64_monterey: "89c93b9fc1c407f7ffbd430e4499173a2e29a3fd91ed3a422616e9271c8c7e0f"
+    sha256 arm64_big_sur:  "ec759af2b65961d39e1ba7f1ea3e02e6ad36a9e8c315ce017620f37833b5178c"
+    sha256 ventura:        "3a0933c3b448c31d2e4415289da547641bd36a941fe594884e3a181efd910aa6"
+    sha256 monterey:       "a885c09f577446d22596c7f6d87e1c4e4612e77f6e790ca77f5be8bc7e0923d1"
+    sha256 big_sur:        "9ff9eb1dc764fcfa54a4ecb773b405094cfa60fd9391d8f0428a6a1707a8a87a"
+    sha256 x86_64_linux:   "ec5d45bba9e8d1287136d03b6ca3ac4dbae2fe4d88617eafce0604123e82755d"
   end
 
   uses_from_macos "zlib"
