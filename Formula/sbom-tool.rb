4dfee99060d8ff88631ed40b15ed75ee95c07fd3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-08-02 17:48:29 +0000

    sbom-tool: update 1.3.2 bottle.

--- Formula/sbom-tool.rb
+++ Formula/sbom-tool.rb
@@ -7,10 +7,10 @@ class SbomTool < Formula
   head "https://github.com/microsoft/sbom-tool.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, ventura:      "926bf0ba5b762ab9158c32056d9111d19794caee7374eb9abe855e83e0bee756"
-    sha256 cellar: :any_skip_relocation, monterey:     "926bf0ba5b762ab9158c32056d9111d19794caee7374eb9abe855e83e0bee756"
-    sha256 cellar: :any_skip_relocation, big_sur:      "926bf0ba5b762ab9158c32056d9111d19794caee7374eb9abe855e83e0bee756"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "c9c1d0406b918f46d24fbb407207b5a342a16a12a61e576b04d5c1f205196a8a"
+    sha256 cellar: :any_skip_relocation, ventura:      "7c27aeb0cc343d7c38aaffa1b04488b385b2236d2da9b957a5ea9e7a4346fb55"
+    sha256 cellar: :any_skip_relocation, monterey:     "7c27aeb0cc343d7c38aaffa1b04488b385b2236d2da9b957a5ea9e7a4346fb55"
+    sha256 cellar: :any_skip_relocation, big_sur:      "7c27aeb0cc343d7c38aaffa1b04488b385b2236d2da9b957a5ea9e7a4346fb55"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "d3bd12ae1efa11135123a95197f06e864a1671f2cf7927ac6e947ae6cdd3f55b"
   end
 
   depends_on "dotnet" => :build
