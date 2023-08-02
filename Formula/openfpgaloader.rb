ee56ceb464283bdaa9b736c7da68b394a50db41f
Porkepix <porkepix@gmail.com>
2023-08-02 12:13:51 +0200

    openfpgaloader 0.11.0

--- Formula/openfpgaloader.rb
+++ Formula/openfpgaloader.rb
@@ -1,8 +1,8 @@
 class Openfpgaloader < Formula
   desc "Universal utility for programming FPGA"
   homepage "https://github.com/trabucayre/openFPGALoader"
-  url "https://github.com/trabucayre/openFPGALoader/archive/v0.10.0.tar.gz"
-  sha256 "966b4629df86b1d520ddd8a4e0a3fc00060b26b5ab4e172b596bd9d4659a196e"
+  url "https://github.com/trabucayre/openFPGALoader/archive/v0.11.0.tar.gz"
+  sha256 "a463690358d2510919472c2f460039a304d016a08a45970821e667eea1c48cc8"
   license "Apache-2.0"
   head "https://github.com/trabucayre/openFPGALoader.git", branch: "master"
 
