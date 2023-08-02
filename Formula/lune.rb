76cff8b4e16aed4c18d42f546bdac66e5abca5e9
Jack Programs <52725440+jackprogramsjp@users.noreply.github.com>
2023-08-01 22:45:18 -0700

    lune 0.7.5 (new formula)

---
+++ Formula/lune.rb
@@ -0,0 +1,18 @@
+class Lune < Formula
+  desc "Standalone Luau script runtime"
+  homepage "https://lune-org.github.io/docs"
+  url "https://github.com/filiptibell/lune/archive/refs/tags/v0.7.5.tar.gz"
+  sha256 "e8191df5d6844026772cc7afab1083235a265c506474c4c4dee0a7724b04f775"
+  license "MPL-2.0"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", "--all-features", *std_cargo_args
+  end
+
+  test do
+    (testpath/"test.lua").write("print(2 + 2)")
+    assert_equal "4", shell_output("#{bin}/lune test.lua").chomp
+  end
+end
