0c9714d162aea04fc52ca98b1781323cc39257ab
Rui Chen <rui@chenrui.dev>
2023-08-03 15:59:20 -0400

    astro 1.18.1

--- Formula/astro.rb
+++ Formula/astro.rb
@@ -1,8 +1,8 @@
 class Astro < Formula
   desc "To build and run Airflow DAGs locally and interact with the Astronomer API"
   homepage "https://www.astronomer.io/"
-  url "https://github.com/astronomer/astro-cli/archive/refs/tags/v1.17.1.tar.gz"
-  sha256 "c3fc356bc4100934aa75db58e4cf5e7c226ebc54c828f146f5a52390831dd2d9"
+  url "https://github.com/astronomer/astro-cli/archive/refs/tags/v1.18.1.tar.gz"
+  sha256 "42462eb51cb4bca898ea0706a911b32390dbf1251176a191373505b5c891cd66"
   license "Apache-2.0"
 
   livecheck do
