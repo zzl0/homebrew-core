c3cfd66f43fb4637a6333992b0347832fedafc79
Gregory Ling <0b0100011101101100@gmail.com>
2023-08-02 19:27:13 -0500

    arm-none-eabi-gdb 13.2 (new formula)
    Added GDB for the 32-bit ARM baremetal toolchain (arm-none-eabi).

---
+++ Formula/arm-none-eabi-gdb.rb
@@ -0,0 +1,58 @@
+class ArmNoneEabiGdb < Formula
+  desc "GNU debugger for arm-none-eabi cross development"
+  homepage "https://www.gnu.org/software/gdb/"
+  url "https://ftp.gnu.org/gnu/gdb/gdb-13.2.tar.xz"
+  mirror "https://ftpmirror.gnu.org/gdb/gdb-13.2.tar.xz"
+  sha256 "fd5bebb7be1833abdb6e023c2f498a354498281df9d05523d8915babeb893f0a"
+  license "GPL-3.0-or-later"
+  head "https://sourceware.org/git/binutils-gdb.git", branch: "master"
+
+  livecheck do
+    formula "gdb"
+  end
+
+  depends_on "arm-none-eabi-gcc" => :test
+  depends_on "gmp"
+  depends_on "python@3.11"
+  depends_on "xz" # required for lzma support
+
+  uses_from_macos "zlib"
+
+  on_system :linux, macos: :ventura_or_newer do
+    depends_on "texinfo" => :build
+  end
+
+  def install
+    target = "arm-none-eabi"
+    args = %W[
+      --target=#{target}
+      --prefix=#{prefix}
+      --datarootdir=#{share}/#{target}
+      --includedir=#{include}/#{target}
+      --infodir=#{info}/#{target}
+      --mandir=#{man}
+      --disable-debug
+      --disable-dependency-tracking
+      --with-lzma
+      --with-python=#{Formula["python@3.11"].opt_bin}/python3.11
+      --with-system-zlib
+      --disable-binutils
+    ]
+
+    mkdir "build" do
+      system "../configure", *args
+      ENV.deparallelize # Error: common/version.c-stamp.tmp: No such file or directory
+      system "make"
+
+      # Don't install bfd or opcodes, as they are provided by binutils
+      system "make", "install-gdb"
+    end
+  end
+
+  test do
+    (testpath/"test.c").write "void _start(void) {}"
+    system "#{Formula["arm-none-eabi-gcc"].bin}/arm-none-eabi-gcc", "-g", "-nostdlib", "test.c"
+    assert_match "Symbol \"_start\" is a function at address 0x",
+          shell_output("#{bin}/arm-none-eabi-gdb -batch -ex 'info address _start' a.out")
+  end
+end
