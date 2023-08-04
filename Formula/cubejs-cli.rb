c365dd29c5f961768ecca4d64354f884e337964c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-08-04 16:44:29 +0000

    cubejs-cli: update 0.33.43 bottle.

--- Formula/cubejs-cli.rb
+++ Formula/cubejs-cli.rb
@@ -8,12 +8,12 @@ class CubejsCli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any, arm64_ventura:  "be230c75032c1cbfff6cbd84987a9c6c8cabf49d5d6dcb6943ef359210674516"
-    sha256 cellar: :any, arm64_monterey: "be230c75032c1cbfff6cbd84987a9c6c8cabf49d5d6dcb6943ef359210674516"
-    sha256 cellar: :any, arm64_big_sur:  "be230c75032c1cbfff6cbd84987a9c6c8cabf49d5d6dcb6943ef359210674516"
-    sha256 cellar: :any, ventura:        "1b7fd9b986b9af496133d15eb1e92e61269f19a2baa86337ea1ffb7e564de855"
-    sha256 cellar: :any, monterey:       "1b7fd9b986b9af496133d15eb1e92e61269f19a2baa86337ea1ffb7e564de855"
-    sha256 cellar: :any, big_sur:        "1b7fd9b986b9af496133d15eb1e92e61269f19a2baa86337ea1ffb7e564de855"
+    sha256 cellar: :any, arm64_ventura:  "a4b8f06ea7404c600d22359eaab3f7463459609fe33090aba372dcb5d60f9c3a"
+    sha256 cellar: :any, arm64_monterey: "a4b8f06ea7404c600d22359eaab3f7463459609fe33090aba372dcb5d60f9c3a"
+    sha256 cellar: :any, arm64_big_sur:  "a4b8f06ea7404c600d22359eaab3f7463459609fe33090aba372dcb5d60f9c3a"
+    sha256 cellar: :any, ventura:        "6e412713688d3ea35443e3ad95399560568130fe0d9c2e2b8505d535af734f26"
+    sha256 cellar: :any, monterey:       "6e412713688d3ea35443e3ad95399560568130fe0d9c2e2b8505d535af734f26"
+    sha256 cellar: :any, big_sur:        "6e412713688d3ea35443e3ad95399560568130fe0d9c2e2b8505d535af734f26"
   end
 
   depends_on "node"
