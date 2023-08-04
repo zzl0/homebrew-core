ea1c3850cde92fb2a5f69885f7fba87ebae6ca4b
Rui Chen <rui@chenrui.dev>
2023-08-04 16:06:22 -0400

    re-flex 3.3.8

--- Formula/re-flex.rb
+++ Formula/re-flex.rb
@@ -1,8 +1,8 @@
 class ReFlex < Formula
   desc "Regex-centric, fast and flexible scanner generator for C++"
   homepage "https://www.genivia.com/doc/reflex/html"
-  url "https://github.com/Genivia/RE-flex/archive/v3.3.7.tar.gz"
-  sha256 "84e1335e805b9a0248c277910043988bd53ecf00a5d49d235bde71b11317601d"
+  url "https://github.com/Genivia/RE-flex/archive/v3.3.8.tar.gz"
+  sha256 "c50f8ee84437af4a89f91721441d830098f0d064630237f195e3f17941c8b957"
   license "BSD-3-Clause"
 
   bottle do
