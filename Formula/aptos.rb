843e708e0cf5c04330ba7df62fb2e627dcef07ea
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-08-04 09:55:08 +0000

    aptos: update 2.0.3 bottle.

--- Formula/aptos.rb
+++ Formula/aptos.rb
@@ -12,14 +12,13 @@ class Aptos < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "44e12bd609000d65a93460dec06901b72d8e5b98d461bd6d35e34e9596749479"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b6b4514db447eb5b06e0554582e9382e6e929ff702558cb5a78749e7a76b9178"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c898caeef5d011e6cfd73c602ede693d545941b4ad567e32209fa8fad167f6b0"
-    sha256 cellar: :any_skip_relocation, ventura:        "3e5e9906aedd3945383b713f417c8f29a7af8875341992d318d43982ac67543f"
-    sha256 cellar: :any_skip_relocation, monterey:       "be04f1cbb9ba5db62102674c17bfb3a7c203da1a7c72bfd53b46b56df45b6667"
-    sha256 cellar: :any_skip_relocation, big_sur:        "1ef6743624200bc379d43177787911fdc07b5500a171cdb9d97cb845b852aab3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8bcf677fa1ece3d649e79d6aac6b6cea4b764544adb2bc28196f81596fa2f48f"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f3f59bcad940a142226d617f2d4542b20937bbccff76401e14fe4cd89aeb4079"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "faa8ff58a53507bbe504eddc07599bcccaf4dd46227aa8f24e90063e598e155b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "65101ccdf8175ac0f15246313820dc4b7b8f1b182637a438a0d4aad1b63d3f3b"
+    sha256 cellar: :any_skip_relocation, ventura:        "8d1bb781238baf3f12e79df3da65228de2dee1a6203b720a8e473d9f9afd624d"
+    sha256 cellar: :any_skip_relocation, monterey:       "8c1fcf9a4c1723cd45381f7d060e78cf3d440ac4463c4eb7ce199a9e670b2ff9"
+    sha256 cellar: :any_skip_relocation, big_sur:        "6616de72382fada4ed37d6c444c1c70a71d776b08c81a9c77432368d582f008e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8ca17dbbb150e9362ff9c623af3904150ddf0b9b79c4d39e156be287b4758139"
   end
 
   depends_on "cmake" => :build
