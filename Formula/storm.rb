d8d068fb7fb117f487f433224f712029e5af457f
Rui Chen <rui@chenrui.dev>
2023-08-04 16:12:15 -0400

    storm 2.5.0

--- Formula/storm.rb
+++ Formula/storm.rb
@@ -3,8 +3,8 @@ class Storm < Formula
 
   desc "Distributed realtime computation system to process data streams"
   homepage "https://storm.apache.org"
-  url "https://dlcdn.apache.org/storm/apache-storm-2.4.0/apache-storm-2.4.0.tar.gz"
-  sha256 "54535c68848f05130647997f3fafe91a72bb9470a50d6d80997274d20cf1c0c6"
+  url "https://dlcdn.apache.org/storm/apache-storm-2.5.0/apache-storm-2.5.0.tar.gz"
+  sha256 "fa7dc1b010a84a1ed929d8ed55bfc8a821e06763c6bdcdcf027de5bfac40a8cf"
   license "Apache-2.0"
 
   bottle do
