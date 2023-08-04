841d92095af8185ac3c2a0c4a71faedd14c7f63d
Rui Chen <rui@chenrui.dev>
2023-08-04 14:29:09 -0400

    spatialite-tools 5.1.0

--- Formula/spatialite-tools.rb
+++ Formula/spatialite-tools.rb
@@ -1,10 +1,9 @@
 class SpatialiteTools < Formula
   desc "CLI tools supporting SpatiaLite"
   homepage "https://www.gaia-gis.it/fossil/spatialite-tools/index"
-  url "https://www.gaia-gis.it/gaia-sins/spatialite-tools-sources/spatialite-tools-5.0.1.tar.gz"
-  sha256 "9604c205e87f037789bc52302c66ccd1371c3e98c74e8ec4e29b0752de35171c"
+  url "https://www.gaia-gis.it/gaia-sins/spatialite-tools-sources/spatialite-tools-5.1.0.tar.gz"
+  sha256 "df3030367c089ca90fa6630897f3f1a280784da29e1ba634f340dba4b08583b5"
   license "GPL-3.0-or-later"
-  revision 4
 
   livecheck do
     url "https://www.gaia-gis.it/gaia-sins/spatialite-tools-sources/"
