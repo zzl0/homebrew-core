a3a49561c822e0db99dfa261d7f01e995eb53ccb
Rui Chen <rui@chenrui.dev>
2023-08-05 18:10:53 -0400

    rure 1.9.2

--- Formula/rure.rb
+++ Formula/rure.rb
@@ -1,8 +1,8 @@
 class Rure < Formula
   desc "C API for RUst's REgex engine"
   homepage "https://github.com/rust-lang/regex/tree/HEAD/regex-capi"
-  url "https://github.com/rust-lang/regex/archive/1.9.1.tar.gz"
-  sha256 "2db1b79384e8d4d9d00dbd4f71fd940e956746c96b29a4ea08c73bd3aa4834bf"
+  url "https://github.com/rust-lang/regex/archive/1.9.2.tar.gz"
+  sha256 "df6765f027d6b602ced9f6847c97bcb53600661b9bb5eb5216db6b556d1d248a"
   license all_of: [
     "Unicode-TOU",
     any_of: ["Apache-2.0", "MIT"],
