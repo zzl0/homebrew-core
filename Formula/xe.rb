8af6eb1d98418f827b6a7f30db63aff8648843f6
Rui Chen <rui@chenrui.dev>
2023-08-05 16:34:51 -0400

    xe 1.0

--- Formula/xe.rb
+++ Formula/xe.rb
@@ -1,8 +1,8 @@
 class Xe < Formula
   desc "Simple xargs and apply replacement"
   homepage "https://github.com/leahneukirchen/xe"
-  url "https://github.com/leahneukirchen/xe/archive/refs/tags/v0.11.tar.gz"
-  sha256 "4087d40be2db3df81a836f797e1fed17d6ac1c788dcf0fd6a904f2d2178a6f1a"
+  url "https://github.com/leahneukirchen/xe/archive/refs/tags/v1.0.tar.gz"
+  sha256 "1e2484c6295f4eb1c1b789d8edab4b728cf9ea7e4c40ef52a56073f9a273ce30"
   license :public_domain
   head "https://github.com/leahneukirchen/xe.git", branch: "master"
 
