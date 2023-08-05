690380b0d70f080f384d8cad752d65bbc605e3ae
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2023-08-04 21:39:33 -0400

    kakoune 2023.08.05

--- Formula/kakoune.rb
+++ Formula/kakoune.rb
@@ -1,8 +1,8 @@
 class Kakoune < Formula
   desc "Selection-based modal text editor"
   homepage "https://github.com/mawww/kakoune"
-  url "https://github.com/mawww/kakoune/releases/download/v2022.10.31/kakoune-2022.10.31.tar.bz2"
-  sha256 "fb317b62c9048ddc7567fe83dfc409c252ef85778b24bd2863be2762d4e4e58b"
+  url "https://github.com/mawww/kakoune/releases/download/v2023.08.05/kakoune-2023.08.05.tar.bz2"
+  sha256 "3e45151e0addd3500de2d6a29b5aacf2267c42bb256d44a782e73defb29cda5c"
   license "Unlicense"
   head "https://github.com/mawww/kakoune.git", branch: "master"
 
