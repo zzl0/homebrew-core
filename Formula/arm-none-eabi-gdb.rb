59d6745364adf38028b81e384f506069e227da86
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-08-05 06:34:01 +0000

    arm-none-eabi-gdb: add 13.2 bottle.

--- Formula/arm-none-eabi-gdb.rb
+++ Formula/arm-none-eabi-gdb.rb
@@ -11,6 +11,16 @@ class ArmNoneEabiGdb < Formula
     formula "gdb"
   end
 
+  bottle do
+    sha256 arm64_ventura:  "5b21e8a01a8dde31521c1b5311c178bb9f3dc13ea066eefd0b943c8fa805db6a"
+    sha256 arm64_monterey: "444ada6aaf9b1c640f575ce7ec3dfdba04b965541976f8ccf5e1893631eb0a00"
+    sha256 arm64_big_sur:  "10cbde3b5ec2f515f8d3b36ea9b710667874e4f171c75f0b64897fe4ee72ba65"
+    sha256 ventura:        "99a09869b43a6c8406654c1ba2af303a53f612a441ce8a1d9d239cf1f137d294"
+    sha256 monterey:       "e79801911bd93047cb34c6b01e9744fa1e73bec13279a543b38fa02f61e4d48c"
+    sha256 big_sur:        "b363441c21d92cd8b01ae051634b9fe408957ba3ab28db71ba3ad820c689fdd6"
+    sha256 x86_64_linux:   "c2c18bea4ea2de652d83b30658ae445dde42b513d46e7f903aa6730bf3aa2a5e"
+  end
+
   depends_on "arm-none-eabi-gcc" => :test
   depends_on "gmp"
   depends_on "python@3.11"
