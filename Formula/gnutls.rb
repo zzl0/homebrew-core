aeed9e761fc994996fac878165bf5d01e5aaf7a3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-08-05 22:32:53 +0000

    gnutls: update 3.8.1 bottle.

--- Formula/gnutls.rb
+++ Formula/gnutls.rb
@@ -12,13 +12,13 @@ class Gnutls < Formula
   end
 
   bottle do
-    sha256 arm64_ventura:  "a10227b5f3b46064fb325eb21d5103b6fad145dbbb87abd4f8ff8d76270ea32a"
-    sha256 arm64_monterey: "d375f9982faad9b6664508624629c9018ecf807d34c38bb91f875557cc9aa0cf"
-    sha256 arm64_big_sur:  "d21c45d81baaf4ea81a6ff134bad1df8575e5e7e50186e24d74d42402220a2d2"
-    sha256 ventura:        "a41072e29a3fe9bdf8408946b7ad308b1d504e87c79d5bb39dd57172354d4e73"
-    sha256 monterey:       "75da330e4d73ade890aa0c998443319d07d4d63089bfd6c16bf7d87ba756bff5"
-    sha256 big_sur:        "d1cddbab50cd28e1e84b57bb3c08e76c204626894bebf044c80fdb3c44d8a577"
-    sha256 x86_64_linux:   "7a131f11110a752a7326a0b1c57dff58c7fc4eea5f2a1e4ae7de781d71532883"
+    sha256 arm64_ventura:  "e3c3e0156bee79b8af745e9d4587ccca48afe3a50a25071bfb185b3cfe41a215"
+    sha256 arm64_monterey: "cdc74eb7c4741a37f941c079e8b9ffccf5eba5bec0bef0a8ccd4015ffa47a6d6"
+    sha256 arm64_big_sur:  "3c9025adfc14b84487257f7947e0dbe9566713ed885076f21d5d5215c23add87"
+    sha256 ventura:        "ca7d2a952ba2da27cc1a8b2441036ec3da0ec2957c97a97ab309390f147e335e"
+    sha256 monterey:       "dfa28ed4633b865f74e66bc1ceb43c3ab6f372b93e3325323de7a3d00d334c6c"
+    sha256 big_sur:        "18aeb28de21fa30a45feb1de8398f02a87010ee9399c58d15e8b75fe786e81ed"
+    sha256 x86_64_linux:   "e31730b3d965f48969ce3bcc1a8710aa2324ae8d29487f03e75bef68adbe8ab3"
   end
 
   depends_on "pkg-config" => :build
