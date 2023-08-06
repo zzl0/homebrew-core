e27d07698c1d78dd841954090119f9b537e81ebf
Rui Chen <rui@chenrui.dev>
2023-08-05 22:18:14 -0400

    mathlibtools: use python-certifi
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/mathlibtools.rb
+++ Formula/mathlibtools.rb
@@ -24,6 +24,7 @@ class Mathlibtools < Formula
   depends_on "lean" => :test
   depends_on "cffi"
   depends_on "openssl@3"
+  depends_on "python-certifi"
   depends_on "python@3.11"
   depends_on "pyyaml"
   depends_on "six"
@@ -31,11 +32,6 @@ class Mathlibtools < Formula
   resource "atomicwrites" do
     url "https://files.pythonhosted.org/packages/87/c6/53da25344e3e3a9c01095a89f16dbcda021c609ddb42dd6d7c0528236fb2/atomicwrites-1.4.1.tar.gz"
     sha256 "81b2c9071a49367a7f770170e5eec8cb66567cfbbc8c73d20ce5ca4a8d71cf11"
-  end
-
-  resource "certifi" do
-    url "https://files.pythonhosted.org/packages/98/98/c2ff18671db109c9f10ed27f5ef610ae05b73bd876664139cf95bd1429aa/certifi-2023.7.22.tar.gz"
-    sha256 "539cc1d13202e33ca466e88b2807e29f4c13049d6d87031a3c110744495cb082"
   end
 
   resource "charset-normalizer" do
