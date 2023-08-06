4eb912f3f44e69345ba4a31766e79eea09a531d0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-08-06 18:43:32 +0000

    pygments: update 2.16.0 bottle.

--- Formula/pygments.rb
+++ Formula/pygments.rb
@@ -7,13 +7,13 @@ class Pygments < Formula
   head "https://github.com/pygments/pygments.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "22dd62a13b5fa28d0b13af36ac5c065d54671946c8a04e25c9da8c3f5dab0c1e"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "8cf29db56c6db4b94612f46c8c6901faeda2d6638e620eda8a54c42f83088701"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "35f8b8852d8ddde37570525f201edad241dbb0f35a323ef4761de0722a47942f"
-    sha256 cellar: :any_skip_relocation, ventura:        "f5f47a58e80f27048a3ad496eeace812d50b73fbe570059efab7e8b68feb81e9"
-    sha256 cellar: :any_skip_relocation, monterey:       "c60a1549d0f8ac1af0130f98982c987d13ba3ca499d90243f03cbe5011da8f6f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "1eadfd8db2188556b8c8afcc580a8dd767bba6e44e6908c940e42f72798c514c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1a0407703d117068c5fde3fb7bc7633e3cbb3b023fd662f5742c2939db97eaa7"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "99fa2c86e532f8347f5485dbf66a801c7237bc4f7153a69dbd8176cc20d1c409"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "38b194f6f6fa8d06bf8d24fd2851afc47334f541c429e9d4c37e79e366799b9e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ddc5f7c765dd58f4b5678f4a22bbb28e5bb560f281b62403017a1884c60725f8"
+    sha256 cellar: :any_skip_relocation, ventura:        "151ce86f8286c1ec94fee0d97126546d24416c0c86c8a828de682bda2d15169e"
+    sha256 cellar: :any_skip_relocation, monterey:       "8c7b5a1e7ca890a9897f7dd9309f6bd435e19fac64e17339ed023919086125ad"
+    sha256 cellar: :any_skip_relocation, big_sur:        "54e04d811cb5706eb90baaad6cc7f33b4ece4cc9288141e7b6cf418d6c78dfda"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7e9299d59924f27837184c02814fb963d112112dbfd29d59475a2c9f4870f40f"
   end
 
   depends_on "python@3.10" => [:build, :test]
