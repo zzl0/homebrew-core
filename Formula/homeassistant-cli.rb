0acbd4760da5e92d7ec7336262a292a8c95e9f81
Rui Chen <rui@chenrui.dev>
2023-08-05 22:40:34 -0400

    homeassistant-cli: use python-certifi
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/homeassistant-cli.rb
+++ Formula/homeassistant-cli.rb
@@ -25,6 +25,7 @@ class HomeassistantCli < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "f3502eb0ab5127896525ca86271ce7834c0075d42357c1168527fafb9fc12744"
   end
 
+  depends_on "python-certifi"
   depends_on "python-tabulate"
   depends_on "python@3.11"
   depends_on "six"
@@ -47,11 +48,6 @@ class HomeassistantCli < Formula
   resource "attrs" do
     url "https://files.pythonhosted.org/packages/97/90/81f95d5f705be17872843536b1868f351805acf6971251ff07c1b8334dbb/attrs-23.1.0.tar.gz"
     sha256 "6279836d581513a26f1bf235f9acd333bc9115683f14f7e8fae46c98fc50e015"
-  end
-
-  resource "certifi" do
-    url "https://files.pythonhosted.org/packages/98/98/c2ff18671db109c9f10ed27f5ef610ae05b73bd876664139cf95bd1429aa/certifi-2023.7.22.tar.gz"
-    sha256 "539cc1d13202e33ca466e88b2807e29f4c13049d6d87031a3c110744495cb082"
   end
 
   resource "charset-normalizer" do
