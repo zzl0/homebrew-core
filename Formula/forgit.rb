5d13e8b4e3b9be8655969cdd9d52b73627584477
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-08-06 17:35:35 +0000

    forgit: update 23.08.1 bottle.

--- Formula/forgit.rb
+++ Formula/forgit.rb
@@ -6,13 +6,7 @@ class Forgit < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "17459e6266f66664381b6e3f1c9adf8fd02ba280c3282b87b3ebf80f915efa8e"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "17459e6266f66664381b6e3f1c9adf8fd02ba280c3282b87b3ebf80f915efa8e"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "17459e6266f66664381b6e3f1c9adf8fd02ba280c3282b87b3ebf80f915efa8e"
-    sha256 cellar: :any_skip_relocation, ventura:        "17459e6266f66664381b6e3f1c9adf8fd02ba280c3282b87b3ebf80f915efa8e"
-    sha256 cellar: :any_skip_relocation, monterey:       "17459e6266f66664381b6e3f1c9adf8fd02ba280c3282b87b3ebf80f915efa8e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "17459e6266f66664381b6e3f1c9adf8fd02ba280c3282b87b3ebf80f915efa8e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d5cf7c49e779c6e787548f2bdc17dbddfea10c0b0b417a01ecd70cf67f6d1b70"
+    sha256 cellar: :any_skip_relocation, all: "8ead326312187f1d6ca8b54103673c56e3b164299838ff66868a446061ca1eec"
   end
 
   depends_on "fzf"
