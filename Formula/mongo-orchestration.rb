06ce73efdadc600934ff55bbdd7f8621a2c01aed
Rui Chen <rui@chenrui.dev>
2023-08-05 22:13:05 -0400

    mongo-orchestration: use python-certifi
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/mongo-orchestration.rb
+++ Formula/mongo-orchestration.rb
@@ -19,17 +19,13 @@ class MongoOrchestration < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "0010e31ffc78f978e04b684b021504691e2900d453866e583cc7fa8b664fa5e8"
   end
 
+  depends_on "python-certifi"
   depends_on "python@3.11"
   depends_on "six"
 
   resource "bottle" do
     url "https://files.pythonhosted.org/packages/fd/04/1c09ab851a52fe6bc063fd0df758504edede5cc741bd2e807bf434a09215/bottle-0.12.25.tar.gz"
     sha256 "e1a9c94970ae6d710b3fb4526294dfeb86f2cb4a81eff3a4b98dc40fb0e5e021"
-  end
-
-  resource "certifi" do
-    url "https://files.pythonhosted.org/packages/98/98/c2ff18671db109c9f10ed27f5ef610ae05b73bd876664139cf95bd1429aa/certifi-2023.7.22.tar.gz"
-    sha256 "539cc1d13202e33ca466e88b2807e29f4c13049d6d87031a3c110744495cb082"
   end
 
   resource "charset-normalizer" do
