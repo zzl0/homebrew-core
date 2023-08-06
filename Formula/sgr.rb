8f67be6ac9100dd7ee1742decd24c5979039d5ed
Rui Chen <rui@chenrui.dev>
2023-08-05 21:29:59 -0400

    sgr: use python-certifi
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/sgr.rb
+++ Formula/sgr.rb
@@ -22,6 +22,7 @@ class Sgr < Formula
   depends_on "rust" => :build # for cryptography
   depends_on "libpq" # for psycopg2-binary
   depends_on "openssl@3"
+  depends_on "python-certifi"
   depends_on "python-tabulate"
   depends_on "python-typing-extensions"
   depends_on "python@3.11"
@@ -43,11 +44,6 @@ class Sgr < Formula
   resource "attrs" do
     url "https://files.pythonhosted.org/packages/97/90/81f95d5f705be17872843536b1868f351805acf6971251ff07c1b8334dbb/attrs-23.1.0.tar.gz"
     sha256 "6279836d581513a26f1bf235f9acd333bc9115683f14f7e8fae46c98fc50e015"
-  end
-
-  resource "certifi" do
-    url "https://files.pythonhosted.org/packages/98/98/c2ff18671db109c9f10ed27f5ef610ae05b73bd876664139cf95bd1429aa/certifi-2023.7.22.tar.gz"
-    sha256 "539cc1d13202e33ca466e88b2807e29f4c13049d6d87031a3c110744495cb082"
   end
 
   resource "cffi" do
