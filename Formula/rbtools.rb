e11bea6fe1288f89c31d48d3baaa1ac050bbc6a4
Rui Chen <rui@chenrui.dev>
2023-08-05 21:45:18 -0400

    rbtools: use python-certifi
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/rbtools.rb
+++ Formula/rbtools.rb
@@ -19,14 +19,10 @@ class Rbtools < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "59ce185800f1436bc42e8272ceec9aadc5ecc758e791e93a5363fe300a1bcbc1"
   end
 
+  depends_on "python-certifi"
   depends_on "python-typing-extensions"
   depends_on "python@3.11"
   depends_on "six"
-
-  resource "certifi" do
-    url "https://files.pythonhosted.org/packages/98/98/c2ff18671db109c9f10ed27f5ef610ae05b73bd876664139cf95bd1429aa/certifi-2023.7.22.tar.gz"
-    sha256 "539cc1d13202e33ca466e88b2807e29f4c13049d6d87031a3c110744495cb082"
-  end
 
   resource "colorama" do
     url "https://files.pythonhosted.org/packages/d8/53/6f443c9a4a8358a93a6792e2acffb9d9d5cb0a5cfd8802644b7b1c9a02e4/colorama-0.4.6.tar.gz"
