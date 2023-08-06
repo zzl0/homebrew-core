6edaeb71f84ba5a1873aa8c5be8798b4861c858d
Branch Vincent <branchevincent@gmail.com>
2023-08-06 16:05:36 -0700

    ykman: use python-cryptography

--- Formula/ykman.rb
+++ Formula/ykman.rb
@@ -19,14 +19,11 @@ class Ykman < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "984df9910381982a650209c790e8b9d54e01c6d9b7c051cbefa95bcc995062b0"
   end
 
-  # `pkg-config` and `rust` are for cryptography.
-  depends_on "pkg-config" => :build
-  depends_on "rust" => :build
   depends_on "swig" => :build
   depends_on "cffi"
   depends_on "keyring"
-  depends_on "openssl@3"
   depends_on "pycparser"
+  depends_on "python-cryptography"
   depends_on "python@3.11"
 
   uses_from_macos "libffi"
@@ -35,11 +32,6 @@ class Ykman < Formula
   resource "click" do
     url "https://files.pythonhosted.org/packages/7e/ad/7a6a96fab480fb2fbf52f782b2deb3abe1d2c81eca3ef68a575b5a6a4f2e/click-8.1.5.tar.gz"
     sha256 "4be4b1af8d665c6d942909916d31a213a106800c47d0eeba73d34da3cbc11367"
-  end
-
-  resource "cryptography" do
-    url "https://files.pythonhosted.org/packages/93/b7/b6b3420a2f027c1067f712eb3aea8653f8ca7490f183f9917879c447139b/cryptography-41.0.2.tar.gz"
-    sha256 "7d230bf856164de164ecb615ccc14c7fc6de6906ddd5b491f3af90d3514c925c"
   end
 
   resource "fido2" do
@@ -73,10 +65,6 @@ class Ykman < Formula
   end
 
   def install
-    # Ensure that the `openssl` crate picks up the intended library.
-    ENV["OPENSSL_DIR"] = Formula["openssl@3"].opt_prefix
-    ENV["OPENSSL_NO_VENDOR"] = "1"
-
     # Fixes: smartcard/scard/helpers.c:28:22: fatal error: winscard.h: No such file or directory
     ENV.append "CFLAGS", "-I#{Formula["pcsc-lite"].opt_include}/PCSC" if OS.linux?
 
