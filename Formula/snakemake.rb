f6f083f8bad9ff7e4c5870c424b1e5f93fd276bc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-08-07 14:37:08 +0000

    snakemake: update 7.32.2 bottle.

--- Formula/snakemake.rb
+++ Formula/snakemake.rb
@@ -9,14 +9,13 @@ class Snakemake < Formula
   head "https://github.com/snakemake/snakemake.git", branch: "main"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_ventura:  "3ac883c8f6e54397d863021c35f2c5251d5513caaa28147fb89d5283602993b8"
-    sha256 cellar: :any,                 arm64_monterey: "6f889c45b55cb6326315c3436256278d85a0c4dfb4bf514378201803aefe33ba"
-    sha256 cellar: :any,                 arm64_big_sur:  "e12ee52b2a637bdb90cc085d91f392671067a1259d7024e28f502344108451cb"
-    sha256 cellar: :any,                 ventura:        "ae5a3798861828dd371d98a65d6daabca257ee6453de2bbd24b4d052119adb11"
-    sha256 cellar: :any,                 monterey:       "6f9ecf9a25d543ee2918e8fb4b94f91bcf3c9584c15d84ab9d82c3f1cba37a46"
-    sha256 cellar: :any,                 big_sur:        "c8734d9e45d92008f9254eff460a544e8289a2ff3f094ed0901a8fe0558ef5b3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f3d92add2440ed006bc3c2e70395128ac3e61054eb7d492fd2060fa064fa47c9"
+    sha256 cellar: :any,                 arm64_ventura:  "44364b2dcf9b8d2984aba1fe212c7cdf3d8399790455019d5fdbfddf89b7b23d"
+    sha256 cellar: :any,                 arm64_monterey: "bb1dbab9d72c9e6c4a2f02cd683db179e5304011a634fe72da1311d487d04696"
+    sha256 cellar: :any,                 arm64_big_sur:  "27946ea4bde8fb3caa31f29c92c0d6ae294d21ab0a4ee1acda1b96d4c0a82870"
+    sha256 cellar: :any,                 ventura:        "186bc6b3186377867a4e7ba79a6253ad52528ba5660adcdc31910527df495789"
+    sha256 cellar: :any,                 monterey:       "002384c285dbb5d0c3f49f504108c47797df8b1bb4029277c4a59844d40f5378"
+    sha256 cellar: :any,                 big_sur:        "d115c17a0937853a2a464451964933b0249d6dc76f9b01dc28c67cc10efb3d4e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "61f21799b74d652958156e8da8714c26351b684f1e792c3655eb9cba72d41809"
   end
 
   depends_on "rust" => :build # for rpds-py
