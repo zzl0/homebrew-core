cf0bceba97c497f8e3568c821c79f125eb0d34b0
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-08-08 18:10:51 +0800

    jackett 0.21.595

--- Formula/jackett.rb
+++ Formula/jackett.rb
@@ -1,8 +1,8 @@
 class Jackett < Formula
   desc "API Support for your favorite torrent trackers"
   homepage "https://github.com/Jackett/Jackett"
-  url "https://github.com/Jackett/Jackett/archive/refs/tags/v0.21.589.tar.gz"
-  sha256 "36021b881f92a779ebf1f518ea15bd0b41251d950858d7f7d88137396b6411e7"
+  url "https://github.com/Jackett/Jackett/archive/refs/tags/v0.21.595.tar.gz"
+  sha256 "4c5c4b1a79f83c6d74f9541176f1d696527d330ba35c0fd08e9dada7f2c926df"
   license "GPL-2.0-only"
   head "https://github.com/Jackett/Jackett.git", branch: "master"
 
