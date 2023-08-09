181ad5b3d3342acf0c6287dc3c002a3673891b78
maxence-wz <maxence.wangermez@ens-paris-saclay.fr>
2023-08-09 19:37:08 +0200

    mfem 4.5.2 (new formula)

---
+++ Formula/m/mfem.rb
@@ -0,0 +1,45 @@
+class Mfem < Formula
+  desc "Free, lightweight, scalable C++ library for FEM"
+  homepage "http://www.mfem.org/"
+  url "https://github.com/mfem/mfem/archive/refs/tags/v4.5.2.tar.gz"
+  sha256 "9431d72a2834078f25c58430767bf2fd62bf43a0feb003189a86847c68b8af4a"
+  license "BSD-3-Clause"
+  head "https://github.com/mfem/mfem.git", branch: "master"
+
+  depends_on "cmake" => :build
+  depends_on "hypre"        # optional "mpi"
+  depends_on "metis"        # optional "metis"
+  depends_on "openblas"
+  depends_on "suite-sparse"
+
+  def install
+    # fix `lib/cmake/mfem/MFEMConfig.cmake` file audit failure
+    inreplace "config/cmake/MFEMConfig.cmake.in", "@CMAKE_CXX_COMPILER@", ENV.cxx
+
+    # fix `share/mfem/config.mk` file audit failure
+    inreplace "config/config.mk.in", "@MFEM_CXX@", ENV.cxx
+    inreplace "config/config.mk.in", "@MFEM_HOST_CXX@", ENV.cxx
+
+    args = [
+      "-DMFEM_USE_MPI=YES",
+      "-DMFEM_USE_METIS_5=YES",
+      "-DMFEM_USE_SUITESPARSE=YES",
+      "-DMFEM_USE_NETCDF=NO",
+      "-DMFEM_USE_SUPERLU=NO",
+    ]
+    args << "-DMFEM_USE_LAPACK=YES" if OS.linux?
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+    pkgshare.install "examples", "data"
+  end
+
+  test do
+    cp_r pkgshare/"examples", testpath
+    cp pkgshare/"data/star.mesh", testpath/"examples"
+    system "make", "-C", testpath/"examples", "all", "MFEM_INSTALL_DIR=#{prefix}", "CONFIG_MK=#{pkgshare}/config.mk"
+    args = ["-m", testpath/"examples/star.mesh", "--no-visualization"]
+    system testpath/"examples/ex1", *args
+    system "mpirun", "-np", "1", testpath/"examples/ex1p", *args
+  end
+end
