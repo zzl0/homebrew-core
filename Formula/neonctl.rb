43cec3cea02aa7a5d25203670185390f9c85aa68
Alexander Bayandin <alexander@neon.tech>
2023-08-10 00:28:46 +0100

    neonctl 1.20.0

--- Formula/neonctl.rb
+++ Formula/neonctl.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Neonctl < Formula
   desc "Neon CLI tool"
   homepage "https://neon.tech/docs/reference/neon-cli"
-  url "https://registry.npmjs.org/neonctl/-/neonctl-1.19.1.tgz"
-  sha256 "13cc4c9483194d8c16346d4e445adc3b9359914157aa18432b7531e1d90744e2"
+  url "https://registry.npmjs.org/neonctl/-/neonctl-1.20.0.tgz"
+  sha256 "5eff816e04a3e82fe80d10234bfc8a3399ad8be400812dea076c339da91cf88a"
   license "Apache-2.0"
 
   bottle do
@@ -22,10 +22,14 @@ class Neonctl < Formula
   def install
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
     bin.install_symlink Dir["#{libexec}/bin/*"]
+
+    %w[neonctl neon].each do |cmd|
+      generate_completions_from_executable(bin/cmd, "completion")
+    end
   end
 
   test do
     output = shell_output("#{bin}/neonctl --api-key DOES-NOT-EXIST projects create 2>&1", 1)
-    assert_match("Authentication failed,", output)
+    assert_match("Authentication failed", output)
   end
 end
