ed35422b0e84989d263b249ad5d6bfc2f8f60aa1
Anton Medvedev <anton@medv.io>
2023-08-09 18:50:34 +0200

    walk: rename from llama

---
+++ Formula/w/walk.rb
@@ -0,0 +1,28 @@
+class Walk < Formula
+  desc "Terminal navigator"
+  homepage "https://github.com/antonmedv/walk"
+  url "https://github.com/antonmedv/walk/archive/refs/tags/v1.4.0.tar.gz"
+  sha256 "606568e098a91c7b0e694534bf0d268d92cc9ac19bce9e4671775837176eb9f5"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    require "pty"
+
+    PTY.spawn(bin/"walk") do |r, w, _pid|
+      r.winsize = [80, 60]
+      sleep 1
+      w.write "\e"
+      begin
+        r.read
+      rescue Errno::EIO
+        # GNU/Linux raises EIO when read is done on closed pty
+      end
+    end
+  end
+end
