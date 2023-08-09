870ae4a074b6af002983a587ab939b7aaaca9ee4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-08-09 16:04:46 +0000

    medusa: add 0.1.1 bottle.

--- Formula/medusa.rb
+++ Formula/medusa.rb
@@ -5,6 +5,16 @@ class Medusa < Formula
   sha256 "5ba7ff3654b229a44684f473725dd7256e0514926fa553fbccfa556e399b3c3e"
   license "AGPL-3.0-only"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8d5fce00a1fa24bf839644fe2f009a2c2834f4d5f0a4588ec88fd5e23272c5ef"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "348314d4db83f68035fc2427850317fa141e63d68fa9cabaa152b4b02bfaff6b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5ef3d1be8d586c561fcad6e6e77e97955210621c0d0c1cb92c7321586de1a4cd"
+    sha256 cellar: :any_skip_relocation, ventura:        "bf308ef20d5d8c84a42dc3e71ed71b6de7000e527d632ea3402e9c16225a5014"
+    sha256 cellar: :any_skip_relocation, monterey:       "609f93aafa5b92687ad2838a4f6856150e270c6507de81e73adde621fda293dd"
+    sha256 cellar: :any_skip_relocation, big_sur:        "cb8a690fc77ba0067206fd631474515c97ea74719c18e30bfa441b9463849bc7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "449e97d3c58d616d1c9eb1b632e7972b68cd802a1180a0c8b00e48a80f18a155"
+  end
+
   depends_on "go" => :build
   depends_on "truffle" => :test
   depends_on "crytic-compile"
