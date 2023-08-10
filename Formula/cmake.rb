b1edf78dd4a8bace73fa8d1fa22842f92fed3995
Patrick Linnane <patrick@linnane.io>
2023-08-10 13:39:26 -0700

    cmake 3.27.2

--- Formula/cmake.rb
+++ Formula/cmake.rb
@@ -1,10 +1,10 @@
 class Cmake < Formula
   desc "Cross-platform make"
   homepage "https://www.cmake.org/"
-  url "https://github.com/Kitware/CMake/releases/download/v3.27.1/cmake-3.27.1.tar.gz"
-  mirror "http://fresh-center.net/linux/misc/cmake-3.27.1.tar.gz"
-  mirror "http://fresh-center.net/linux/misc/legacy/cmake-3.27.1.tar.gz"
-  sha256 "b1a6b0135fa11b94476e90f5b32c4c8fad480bf91cf22d0ded98ce22c5132004"
+  url "https://github.com/Kitware/CMake/releases/download/v3.27.2/cmake-3.27.2.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/cmake-3.27.2.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/legacy/cmake-3.27.2.tar.gz"
+  sha256 "798e50085d423816fe96c9ef8bee5e50002c9eca09fed13e300de8a91d35c211"
   license "BSD-3-Clause"
   head "https://gitlab.kitware.com/cmake/cmake.git", branch: "master"
 
