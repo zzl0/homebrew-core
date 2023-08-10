833a4ae9798605031a0a18352fec74ed37414e40
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-08-10 20:34:14 +0000

    git-credential-oauth: update 0.10.0 bottle.

--- Formula/git-credential-oauth.rb
+++ Formula/git-credential-oauth.rb
@@ -7,13 +7,13 @@ class GitCredentialOauth < Formula
   head "https://github.com/hickford/git-credential-oauth.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "9cc80fc915b93c9eb0eb504ae9857052507748729795390c41c1de313bed2e9d"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "9cc80fc915b93c9eb0eb504ae9857052507748729795390c41c1de313bed2e9d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9cc80fc915b93c9eb0eb504ae9857052507748729795390c41c1de313bed2e9d"
-    sha256 cellar: :any_skip_relocation, ventura:        "50b4811dbbc6c5db0c96980e5ee659a078e4e3f07594c8ef893a3dea83605c0e"
-    sha256 cellar: :any_skip_relocation, monterey:       "50b4811dbbc6c5db0c96980e5ee659a078e4e3f07594c8ef893a3dea83605c0e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "50b4811dbbc6c5db0c96980e5ee659a078e4e3f07594c8ef893a3dea83605c0e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d0d4f7ba52516a4e95e0db03541eb4d48ea98c921a5559582e0ca62b4f63942b"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "30408de11b6182bf563d036ae36594bfef1dbcf2b1449d657e0d5c16e1df6843"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "30408de11b6182bf563d036ae36594bfef1dbcf2b1449d657e0d5c16e1df6843"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "30408de11b6182bf563d036ae36594bfef1dbcf2b1449d657e0d5c16e1df6843"
+    sha256 cellar: :any_skip_relocation, ventura:        "61f0b170ae5899580395fa70002ff315a54ecbcf4375053573f483793c5de55a"
+    sha256 cellar: :any_skip_relocation, monterey:       "61f0b170ae5899580395fa70002ff315a54ecbcf4375053573f483793c5de55a"
+    sha256 cellar: :any_skip_relocation, big_sur:        "61f0b170ae5899580395fa70002ff315a54ecbcf4375053573f483793c5de55a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "631cea37fd2ee0b72296b15f22193e41be5bea7b7bfeed931e7eb6573da825c0"
   end
 
   depends_on "go" => :build
