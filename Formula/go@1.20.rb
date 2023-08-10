527648c66b7a568e736a0198fd0c45a2ca49c7f2
Rui Chen <rui@chenrui.dev>
2023-08-10 08:11:01 -0400

    go@1.20 1.20.7 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    go@1.20: update build and test
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/go@1.20.rb
@@ -0,0 +1,80 @@
+class GoAT120 < Formula
+  desc "Open source programming language to build simple/reliable/efficient software"
+  homepage "https://go.dev/"
+  url "https://go.dev/dl/go1.20.7.src.tar.gz"
+  mirror "https://fossies.org/linux/misc/go1.20.7.src.tar.gz"
+  sha256 "2c5ee9c9ec1e733b0dbbc2bdfed3f62306e51d8172bf38f4f4e542b27520f597"
+  license "BSD-3-Clause"
+
+  livecheck do
+    url "https://go.dev/dl/"
+    regex(/href=.*?go[._-]?v?(1\.20(?:\.\d+)*)[._-]src\.t/i)
+  end
+
+  keg_only :versioned_formula
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOROOT_BOOTSTRAP"] = Formula["go"].opt_libexec
+
+    cd "src" do
+      ENV["GOROOT_FINAL"] = libexec
+      # Set portable defaults for CC/CXX to be used by cgo
+      with_env(CC: "cc", CXX: "c++") { system "./make.bash" }
+    end
+
+    libexec.install Dir["*"]
+    bin.install_symlink Dir[libexec/"bin/go*"]
+
+    system bin/"go", "install", "std", "cmd"
+
+    # Remove useless files.
+    # Breaks patchelf because folder contains weird debug/test files
+    (libexec/"src/debug/elf/testdata").rmtree
+    # Binaries built for an incompatible architecture
+    (libexec/"src/runtime/pprof/testdata").rmtree
+  end
+
+  test do
+    (testpath/"hello.go").write <<~EOS
+      package main
+
+      import "fmt"
+
+      func main() {
+          fmt.Println("Hello World")
+      }
+    EOS
+
+    # Run go fmt check for no errors then run the program.
+    # This is a a bare minimum of go working as it uses fmt, build, and run.
+    system bin/"go", "fmt", "hello.go"
+    assert_equal "Hello World\n", shell_output("#{bin}/go run hello.go")
+
+    with_env(GOOS: "freebsd", GOARCH: "amd64") do
+      system bin/"go", "build", "hello.go"
+    end
+
+    (testpath/"hello_cgo.go").write <<~EOS
+      package main
+
+      /*
+      #include <stdlib.h>
+      #include <stdio.h>
+      void hello() { printf("%s\\n", "Hello from cgo!"); fflush(stdout); }
+      */
+      import "C"
+
+      func main() {
+          C.hello()
+      }
+    EOS
+
+    # Try running a sample using cgo without CC or CXX set to ensure that the
+    # toolchain's default choice of compilers work
+    with_env(CC: nil, CXX: nil) do
+      assert_equal "Hello from cgo!\n", shell_output("#{bin}/go run hello_cgo.go")
+    end
+  end
+end
