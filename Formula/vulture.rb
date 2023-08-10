b3ceb845dc709e71d76258f958c73a9cb86fba3b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-08-10 20:36:32 +0000

    vulture: update 2.8 bottle.

--- Formula/vulture.rb
+++ Formula/vulture.rb
@@ -9,13 +9,13 @@ class Vulture < Formula
   head "https://github.com/jendrikseipp/vulture.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "7d5dfb1c1062bb14adee775412b7abbef11b38c630a3d80487c4f1ae6c78c152"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "db8de365af3d73df7f7de4ac62175d676949f4ba895d7e4762fd897997a0f3bc"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "76dc4f359ec6f15a3aafc86bbb17dcc1bf8847ae9b0d24fcd9ca4078d99004d6"
-    sha256 cellar: :any_skip_relocation, ventura:        "abf242b7c8b219f3c022173298de96be0cdbc9a7cab581623e0e676a1d4b4286"
-    sha256 cellar: :any_skip_relocation, monterey:       "69059507d049b8b05f9e84690d4b2f448946b299ffb62746662daf98628e53b0"
-    sha256 cellar: :any_skip_relocation, big_sur:        "5efd3717512cb9dcf74601f148400fd427a8e27cd308677b8b4c864011d3e442"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "832b6778a2859b8f5dc40a1edcca62fe0b0605c7018f8d7925e64d3d459cbc5b"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "28808ad1470a0e3cadfcb63cf68c82921e6fda0c04789454890428b314cfcd89"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "cabacd95b9c2e0bac4a37b4459bf310363a0b169dab7553aa3abf2368b5a8785"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e8eb7af91c80458509ff157b57f50d0297d638b14979878664257d53c5135e57"
+    sha256 cellar: :any_skip_relocation, ventura:        "225a367c4a920558f7b33772c5ef35aab23bedb44dffcd42870746794d97100d"
+    sha256 cellar: :any_skip_relocation, monterey:       "3e20bcd278282381534e85376fb858417953066f78d1f05e9e9c7357075c53d6"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e6e8de8bb8050d860550b864a0ff6795ca2b34d25ae5e244dc47c4da768055ba"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ef1619b88935c28ab2eb778554513f85b83da6df870fe52a43aaf08be202d5fc"
   end
 
   depends_on "python-toml"
