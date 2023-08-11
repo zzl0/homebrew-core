ea7a5b2fe9fb1e45b81a89e72c33bdc1aa2be8ea
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-08-11 15:02:08 +0000

    proxify: add 0.0.11 bottle.

--- Formula/proxify.rb
+++ Formula/proxify.rb
@@ -6,6 +6,16 @@ class Proxify < Formula
   license "MIT"
   head "https://github.com/projectdiscovery/proxify.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "91c0b93902207b7027a48b69ccf3de62f3cb8586c3d5b005c75cba874b20c598"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "91c0b93902207b7027a48b69ccf3de62f3cb8586c3d5b005c75cba874b20c598"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "91c0b93902207b7027a48b69ccf3de62f3cb8586c3d5b005c75cba874b20c598"
+    sha256 cellar: :any_skip_relocation, ventura:        "630e0642c1a3f8fb0bb23c0ba5536ea794eb5aca7356a86529950f305f86c92e"
+    sha256 cellar: :any_skip_relocation, monterey:       "630e0642c1a3f8fb0bb23c0ba5536ea794eb5aca7356a86529950f305f86c92e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "630e0642c1a3f8fb0bb23c0ba5536ea794eb5aca7356a86529950f305f86c92e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "32bdc9d7b5844b596eb441cf8795933b7320beb3130f1776f6d1583942a5ed88"
+  end
+
   depends_on "go" => :build
 
   def install
