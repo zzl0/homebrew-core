1f02a9dfe80666060dae5f90e5a81f84ee68664c
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-08-11 08:56:02 +0800

    clair 4.7.1

--- Formula/clair.rb
+++ Formula/clair.rb
@@ -1,8 +1,8 @@
 class Clair < Formula
   desc "Vulnerability Static Analysis for Containers"
   homepage "https://github.com/quay/clair"
-  url "https://github.com/quay/clair/archive/v4.7.0.tar.gz"
-  sha256 "593b2bae3396f4593a6f8186d89a857a97536d57007ca87a1a6d30bbeed6e470"
+  url "https://github.com/quay/clair/archive/v4.7.1.tar.gz"
+  sha256 "476cabaf9c0afe276648e33bcabeb4fb753c8dbfdf9f8e0d6d3cfe2126d06cb1"
   license "Apache-2.0"
 
   livecheck do
