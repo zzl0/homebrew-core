a7afdcb6ab028b72a9df506bc62417937e32c97d
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-08-12 17:45:39 +0800

    oha 0.6.2

--- Formula/oha.rb
+++ Formula/oha.rb
@@ -1,8 +1,8 @@
 class Oha < Formula
   desc "HTTP load generator, inspired by rakyll/hey with tui animation"
   homepage "https://github.com/hatoo/oha/"
-  url "https://github.com/hatoo/oha/archive/refs/tags/v0.6.1.tar.gz"
-  sha256 "53c1f56b0541e8d2c56be60972543da0141a2a1dda09128410bdb8e010adab6a"
+  url "https://github.com/hatoo/oha/archive/refs/tags/v0.6.2.tar.gz"
+  sha256 "c7ab7cd84dacbdd42f6917c23aa2cba28eb016a38d29a62782b0fb0956b1f201"
   license "MIT"
   head "https://github.com/hatoo/oha.git", branch: "master"
 
