b71ba8d69549a1098f878cc882f8da41c445bd4a
Twan Wolthof <twan@xeago.nl>
2023-08-13 20:56:24 +0000

    riff 2.25.0 (new formula)
    riff: update test
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/riff.rb
@@ -0,0 +1,18 @@
+class Riff < Formula
+  desc "Diff filter highlighting which line parts have changed"
+  homepage "https://github.com/walles/riff"
+  url "https://github.com/walles/riff/archive/2.25.0.tar.gz"
+  sha256 "c0a2d0c6beaab92431f6846a25544b3d63b3ea433e33c9db9aa9f5a5d65f1d97"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_empty shell_output("#{bin}/riff /etc/passwd /etc/passwd")
+    assert_match version.to_s, shell_output("#{bin}/riff --version")
+  end
+end
