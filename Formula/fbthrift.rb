1706f738ef2450079e61846c05e5eda7b0680083
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2023-08-14 16:08:14 +0800

    fbthrift 2023.08.14.00

--- Formula/fbthrift.rb
+++ Formula/fbthrift.rb
@@ -1,8 +1,8 @@
 class Fbthrift < Formula
   desc "Facebook's branch of Apache Thrift, including a new C++ server"
   homepage "https://github.com/facebook/fbthrift"
-  url "https://github.com/facebook/fbthrift/archive/refs/tags/v2023.08.07.00.tar.gz"
-  sha256 "3c4a2536c847b137478c6c41b3b8585515a00c0f67fa7ab6a745ff3114a719b0"
+  url "https://github.com/facebook/fbthrift/archive/refs/tags/v2023.08.14.00.tar.gz"
+  sha256 "852fc394d79661fdd42f9630a37bd3a99ee76542c0526539d38b77edf359a323"
   license "Apache-2.0"
   head "https://github.com/facebook/fbthrift.git", branch: "main"
 
