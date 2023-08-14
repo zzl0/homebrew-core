3f84e17a61ada746ad97b77d1d76499dc4209d41
Rui Chen <rui@chenrui.dev>
2023-08-14 01:08:39 -0400

    vineyard 0.16.3

--- Formula/vineyard.rb
+++ Formula/vineyard.rb
@@ -3,8 +3,8 @@ class Vineyard < Formula
 
   desc "In-memory immutable data manager. (Project under CNCF)"
   homepage "https://v6d.io"
-  url "https://github.com/v6d-io/v6d/releases/download/v0.16.2/v6d-0.16.2.tar.gz"
-  sha256 "979e33f2efb46a0549ad7e065fc886a9e8dda03d054b3aae6c6e9bbc2e9a4d65"
+  url "https://github.com/v6d-io/v6d/releases/download/v0.16.3/v6d-0.16.3.tar.gz"
+  sha256 "a7279912c2a66e89153d903ff4da77d328a9f466d9d396785dba8dd1eaaddadc"
   license "Apache-2.0"
 
   bottle do
