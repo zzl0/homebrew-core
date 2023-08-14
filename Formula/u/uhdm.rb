115af3efa58cc4949eeb5111ebfd8d09986a99d2
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2023-08-13 21:56:49 -0400

    uhdm 1.74 (new formula)

---
+++ Formula/u/uhdm.rb
@@ -0,0 +1,74 @@
+class Uhdm < Formula
+  include Language::Python::Virtualenv
+
+  desc "Universal Hardware Data Model, modeling of the SystemVerilog Object Model"
+  homepage "https://github.com/chipsalliance/UHDM"
+  url "https://github.com/chipsalliance/UHDM/archive/refs/tags/v1.74.tar.gz"
+  sha256 "92cefa641610457772c91d45864e3f4d65bfa0bf300886dc3067a561358a8eed"
+  license "Apache-2.0"
+  head "https://github.com/chipsalliance/UHDM.git", branch: "master"
+
+  depends_on "cmake" => :build
+  depends_on "python@3.11" => :build
+  depends_on "six" => :build
+  depends_on "pkg-config" => :test
+  depends_on "capnp"
+
+  resource "orderedmultidict" do
+    url "https://files.pythonhosted.org/packages/53/4e/3823a27d764bb8388711f4cb6f24e58453e92d6928f4163fdb01e3a3789f/orderedmultidict-1.0.1.tar.gz"
+    sha256 "04070bbb5e87291cc9bfa51df413677faf2141c73c61d2a5f7b26bea3cd882ad"
+  end
+
+  def install
+    venv = virtualenv_create(buildpath/"venv", "python3.11")
+    resources.each do |r|
+      venv.pip_install r
+    end
+    # Build shared library
+    system "cmake", "-S", ".", "-B", "build_shared",
+      "-DBUILD_SHARED_LIBS=ON",
+      "-DUHDM_BUILD_TESTS=OFF",
+      "-DUHDM_USE_HOST_GTEST=ON",
+      "-DUHDM_USE_HOST_CAPNP=ON",
+      "-DCMAKE_INSTALL_RPATH=#{rpath}",
+      "-DPython3_EXECUTABLE=#{buildpath}/venv/bin/python", *std_cmake_args
+    system "cmake", "--build", "build_shared"
+    system "cmake", "--install", "build_shared"
+
+    # moving to share
+    # https://github.com/chipsalliance/UHDM/pull/1013
+    rm lib/"UHDM.capnp"
+  end
+
+  test do
+    # Create a minimal .uhdm file and ensure executables work
+    (testpath/"test.cpp").write <<~EOS
+      #include <cassert>
+      #include <stdlib.h>
+      #include "uhdm/constant.h"
+      #include "uhdm/uhdm.h"
+      #include "uhdm/uhdm_types.h"  // for uhdmconstant
+      #include "uhdm/vhpi_user.h"   // vpi_user functions.
+      #include "uhdm/vpi_uhdm.h"    // struct uhdm_handle
+      int main() {
+        UHDM::Serializer serializer;
+        UHDM::constant *value = serializer.MakeConstant();
+        value->VpiFile("hello.v");
+        value->VpiLineNo(42);
+        value->VpiSize(12345);
+        value->VpiDecompile("decompile");
+        uhdm_handle uhdm_handle(UHDM::uhdmconstant, value);
+        vpiHandle vpi_handle = (vpiHandle)&uhdm_handle;
+        assert(vpi_get_str(vpiFile, vpi_handle) == std::string("hello.v"));
+        assert(vpi_get(vpiLineNo, vpi_handle) == 42);
+        assert(vpi_get(vpiSize, vpi_handle) == 12345);
+        assert(vpi_get_str(vpiDecompile, vpi_handle) == std::string("decompile"));
+      }
+    EOS
+
+    flags = shell_output("pkg-config --cflags --libs UHDM").chomp.split
+    system ENV.cxx, testpath/"test.cpp", "-o", "test",
+      "-fPIC", "-std=c++17", *flags
+    system testpath/"test"
+  end
+end
