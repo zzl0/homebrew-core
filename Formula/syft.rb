1339cd88b5b380cb1e84646c66892d07061f30c0
Rui Chen <rui@chenrui.dev>
2023-08-14 14:16:07 -0400

    syft 0.87.0

--- Formula/syft.rb
+++ Formula/syft.rb
@@ -2,8 +2,8 @@ class Syft < Formula
   desc "CLI for generating a Software Bill of Materials from container images"
   homepage "https://github.com/anchore/syft"
   url "https://github.com/anchore/syft.git",
-      tag:      "v0.86.1",
-      revision: "e2f7befbfbf88053dfb2007c6499a4bb2d232c3c"
+      tag:      "v0.87.0",
+      revision: "b3d7ba569b64376cf33df717e3bc40d8375e033b"
   license "Apache-2.0"
   head "https://github.com/anchore/syft.git", branch: "main"
 
