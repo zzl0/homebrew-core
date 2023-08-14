657cdee5d0094701250690e60f977d8b703d6a17
Rui Chen <rui@chenrui.dev>
2023-08-14 18:26:13 -0400

    grsync 1.3.1
    grsync: remove build patch
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/grsync.rb
+++ Formula/grsync.rb
@@ -1,8 +1,8 @@
 class Grsync < Formula
   desc "GUI for rsync"
   homepage "https://www.opbyte.it/grsync/"
-  url "https://downloads.sourceforge.net/project/grsync/grsync-1.3.0.tar.gz"
-  sha256 "b7c7c6a62e05302d8317c38741e7d71ef9ab4639ee5bff2622a383b2043a35fc"
+  url "https://downloads.sourceforge.net/project/grsync/grsync-1.3.1.tar.gz"
+  sha256 "33cc0e25daa62e5ba7091caea3c83a8dc74dc5d7721c4501d349f210c4b3c6d3"
   license "GPL-2.0"
 
   bottle do
@@ -23,11 +23,6 @@ class Grsync < Formula
 
   uses_from_macos "perl" => :build
 
-  # Fix build with Clang.
-  # https://sourceforge.net/p/grsync/code/174/
-  # Remove with the next release.
-  patch :DATA
-
   def install
     ENV.prepend_path "PERL5LIB", Formula["intltool"].libexec/"lib/perl5" unless OS.mac?
 
@@ -44,26 +39,3 @@ class Grsync < Formula
     assert_predicate bin/"grsync", :exist?
   end
 end
-
-__END__
---- a/src/callbacks.c
-+++ b/src/callbacks.c
-@@ -40,12 +40,13 @@
- gboolean more = FALSE, first = TRUE;
- 
- 
-+void _set_label_selectable(gpointer data, gpointer user_data) {
-+	GtkWidget *widget = GTK_WIDGET(data);
-+	if (GTK_IS_LABEL(widget)) gtk_label_set_selectable(GTK_LABEL(widget), TRUE);
-+}
-+
-+
- void dialog_set_labels_selectable(GtkWidget *dialog) {
--	void _set_label_selectable(gpointer data, gpointer user_data) {
--		GtkWidget *widget = GTK_WIDGET(data);
--		if (GTK_IS_LABEL(widget)) gtk_label_set_selectable(GTK_LABEL(widget), TRUE);
--	}
--
- 	GtkWidget *area = gtk_message_dialog_get_message_area(GTK_MESSAGE_DIALOG(dialog));
- 	GtkContainer *box = (GtkContainer *) area;
- 	GList *children = gtk_container_get_children(box);
