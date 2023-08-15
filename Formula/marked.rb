71b1bfb60b4316683298c27bfe059f7d124a7632
Rui Chen <rui@chenrui.dev>
2023-08-14 21:22:42 -0400

    marked 7.0.3

--- Formula/marked.rb
+++ Formula/marked.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Marked < Formula
   desc "Markdown parser and compiler built for speed"
   homepage "https://marked.js.org/"
-  url "https://registry.npmjs.org/marked/-/marked-7.0.2.tgz"
-  sha256 "a64ebaf7130da9aae00b98aa57554f444eb9103924f2ff8ba1cb685d322b3ca3"
+  url "https://registry.npmjs.org/marked/-/marked-7.0.3.tgz"
+  sha256 "04076e539a4374433dcbb9907486700d9da1f288a12dc4aae67daae5d2f4661d"
   license "MIT"
 
   bottle do
