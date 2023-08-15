dac9c28d289cfcc300c1a22574af9c40519cacdc
Rui Chen <rui@chenrui.dev>
2023-08-15 18:58:35 -0400

    qdmr 0.11.3

--- Formula/q/qdmr.rb
+++ Formula/q/qdmr.rb
@@ -1,8 +1,8 @@
 class Qdmr < Formula
   desc "Codeplug programming tool for DMR radios"
   homepage "https://dm3mat.darc.de/qdmr/"
-  url "https://github.com/hmatuschek/qdmr/archive/refs/tags/v0.11.2.tar.gz"
-  sha256 "812e51ac9e2c4fe430673d8f7c9a2f351feb5f70275755c6da88e26bbab2b272"
+  url "https://github.com/hmatuschek/qdmr/archive/refs/tags/v0.11.3.tar.gz"
+  sha256 "bad499faec7533c460e72c121716141f8cca0ea613ed55143ba1780b06a49b9a"
   license "GPL-3.0-or-later"
 
   bottle do
