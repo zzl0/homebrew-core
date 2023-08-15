a73df6ff244a5ba055cbd5ee879595be685512a5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-08-15 21:46:23 +0000

    wakatime-cli: update 1.74.3 bottle.

--- Formula/w/wakatime-cli.rb
+++ Formula/w/wakatime-cli.rb
@@ -8,13 +8,13 @@ class WakatimeCli < Formula
   version_scheme 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3393679058ab51c8687a0e8aed400cd091ac57a667e51287311c07f9b55ece36"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ee5b75db0310b18eb3329fd7099be5fd3b8d12ed30f956933e4cc2a45c510868"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2c3c6bc8c314b171725b67209b0618750920b371be74a4900b35d59b22e8482a"
-    sha256 cellar: :any_skip_relocation, ventura:        "c59d9d80ec98b5faeef4276c7ebc9864fc1a8c1c06249d4848bab24f5cab53fb"
-    sha256 cellar: :any_skip_relocation, monterey:       "2f547f7f2dccdb95119b5e1c88b05a420edba590663895463b4c232d32404c24"
-    sha256 cellar: :any_skip_relocation, big_sur:        "040a2cb7c998dc2b3c2b216d85ce650c3aa162f355f903e578b06f8b1a34bf2b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "25696785f07dbf40dd9c5d9b3f11900a9e0e4f84495b57ce2484042f0e29db9c"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f5d9bf047c789d565640aa6403bf956ca01f1b23f28d0071941cb5e58da88abd"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b7434d02c08d43303281c770a19b184011b50436a2dd56821e275e2bb292526c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "883ec6214519efb3a73a40b043a85223b805702cc0b69a109b8ceeca7760016b"
+    sha256 cellar: :any_skip_relocation, ventura:        "f7294fafeacfc071f2ab8089591ad5e5eb16f928687be038399fd4f0062bf590"
+    sha256 cellar: :any_skip_relocation, monterey:       "37a9ee0f0d80adaa12a363e8a8f1c04680fae4f789532e728595ccbae6ea3914"
+    sha256 cellar: :any_skip_relocation, big_sur:        "94df38a7147c22a5bef6e1b9dbb5cfc41d71c4f01f3c3200d9a523d1fb836d57"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9df540234a09417c36484576db0147ca4764dfa305afd674ad9e1e46dfc438ac"
   end
 
   depends_on "go" => :build
