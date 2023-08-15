5489d06c184778f7c239c6675719a0692c27acd8
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2023-08-15 10:09:10 +0200

    riscv64-elf-binutils 2.41 (new formula)
    Derived from aarch64-elf-binutils.
    
    Signed-off-by: Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>

---
+++ Formula/r/riscv64-elf-binutils.rb
@@ -0,0 +1,42 @@
+class Riscv64ElfBinutils < Formula
+  desc "GNU Binutils for riscv64-elf cross development"
+  homepage "https://www.gnu.org/software/binutils/"
+  url "https://ftp.gnu.org/gnu/binutils/binutils-2.41.tar.bz2"
+  mirror "https://ftpmirror.gnu.org/binutils/binutils-2.41.tar.bz2"
+  sha256 "a4c4bec052f7b8370024e60389e194377f3f48b56618418ea51067f67aaab30b"
+  license "GPL-3.0-or-later"
+
+  livecheck do
+    formula "binutils"
+  end
+
+  on_system :linux, macos: :ventura_or_newer do
+    depends_on "texinfo" => :build
+  end
+
+  def install
+    target = "riscv64-elf"
+    system "./configure", "--target=#{target}",
+           "--prefix=#{prefix}",
+           "--libdir=#{lib}/#{target}",
+           "--infodir=#{info}/#{target}",
+           "--disable-nls"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test-s.s").write <<~EOS
+      .section .text
+      .globl _start
+      _start:
+          li a7, 93
+          li a0, 0
+          ecall
+    EOS
+    system "#{bin}/riscv64-elf-as", "-o", "test-s.o", "test-s.s"
+    assert_match "file format elf64-littleriscv",
+                 shell_output("#{bin}/riscv64-elf-objdump -a test-s.o")
+    assert_match "f()", shell_output("#{bin}/riscv64-elf-c++filt _Z1fv")
+  end
+end
