a27b316eabe8de0730fd7c9e09bc2a7cb17cfbd5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-08-15 01:26:53 +0000

    coder: add 2.0.2 bottle.

--- Formula/coder.rb
+++ Formula/coder.rb
@@ -5,6 +5,16 @@ class Coder < Formula
   sha256 "a9c2a996cb916ef0d74c1cdd2ccf30f95b9953618ce2d504a7477eb2b8b655c2"
   license "AGPL-3.0-only"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "b186e397d831c685fd02e1fc14164873628aab73ae4bff23f95516c058c26fef"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "86a3163174a7ece89c2377803f22bc4cf6cd0223ce5e7170c2d17f098793f762"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2b171b0eb5e9dada9123942f078e600fcd35b7599ab584f8bec95020bb4ac929"
+    sha256 cellar: :any_skip_relocation, ventura:        "b935c29a44f0c97c9d4849029d3d8fd1a489c2fabcd938b21fe7ddf1aba88592"
+    sha256 cellar: :any_skip_relocation, monterey:       "521fd048b8d1fe9a7f1a7755f0c10bf6a668ca6970540a0a01193dc1e7e93cf0"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f56154a06f23fdfa286d4300e27e946614d197c92cf58e16372866d2df4470ed"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4d7082c8c547ea6bf5c0bde81b809cd4f667ae6f1ca4ea843ce62b00bb89e792"
+  end
+
   depends_on "go" => :build
 
   def install
