b477309d0e74c37b0b325bf37e4c6897d64b11a1
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-08-16 23:49:13 +0800

    ntopng: revision bump (mysql-client 8.1.0)

--- Formula/n/ntopng.rb
+++ Formula/n/ntopng.rb
@@ -2,7 +2,7 @@ class Ntopng < Formula
   desc "Next generation version of the original ntop"
   homepage "https://www.ntop.org/products/traffic-analysis/ntop/"
   license "GPL-3.0-only"
-  revision 2
+  revision 3
 
   stable do
     url "https://github.com/ntop/ntopng/archive/5.2.1.tar.gz"
