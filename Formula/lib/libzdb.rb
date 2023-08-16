43431e3699e71fd51f42ccff4e7523f4d3c4354a
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-08-16 23:49:12 +0800

    libzdb: revision bump (mysql-client 8.1.0)

--- Formula/lib/libzdb.rb
+++ Formula/lib/libzdb.rb
@@ -4,7 +4,7 @@ class Libzdb < Formula
   url "https://tildeslash.com/libzdb/dist/libzdb-3.2.3.tar.gz"
   sha256 "a1957826fab7725484fc5b74780a6a7d0d8b7f5e2e54d26e106b399e0a86beb0"
   license "GPL-3.0-only"
-  revision 1
+  revision 2
 
   livecheck do
     url :homepage
