09fc18216445bc43c06987900374380cbafe26d2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-08-17 02:18:40 +0000

    inxi: update 3.3.29-1 bottle.

--- Formula/i/inxi.rb
+++ Formula/i/inxi.rb
@@ -7,13 +7,13 @@ class Inxi < Formula
   head "https://github.com/smxi/inxi.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "968968e7e5de9529c12c8a0e121bebd9d704d2c6485844788cb31ac19ef8d4e2"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "968968e7e5de9529c12c8a0e121bebd9d704d2c6485844788cb31ac19ef8d4e2"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "968968e7e5de9529c12c8a0e121bebd9d704d2c6485844788cb31ac19ef8d4e2"
-    sha256 cellar: :any_skip_relocation, ventura:        "93d02247fed96e63f349038450e6cf5bb9fa7d977195e2e996a7cc8737ecf8f4"
-    sha256 cellar: :any_skip_relocation, monterey:       "93d02247fed96e63f349038450e6cf5bb9fa7d977195e2e996a7cc8737ecf8f4"
-    sha256 cellar: :any_skip_relocation, big_sur:        "93d02247fed96e63f349038450e6cf5bb9fa7d977195e2e996a7cc8737ecf8f4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "968968e7e5de9529c12c8a0e121bebd9d704d2c6485844788cb31ac19ef8d4e2"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "95614d0c99300f386142dc66811192efb1134449de5f0a20963784f686de3851"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "95614d0c99300f386142dc66811192efb1134449de5f0a20963784f686de3851"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "95614d0c99300f386142dc66811192efb1134449de5f0a20963784f686de3851"
+    sha256 cellar: :any_skip_relocation, ventura:        "8a9bd17e375977f50d9d8eed11884a44d0547d9134fc319c19e12e3236907140"
+    sha256 cellar: :any_skip_relocation, monterey:       "8a9bd17e375977f50d9d8eed11884a44d0547d9134fc319c19e12e3236907140"
+    sha256 cellar: :any_skip_relocation, big_sur:        "8a9bd17e375977f50d9d8eed11884a44d0547d9134fc319c19e12e3236907140"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "95614d0c99300f386142dc66811192efb1134449de5f0a20963784f686de3851"
   end
 
   uses_from_macos "perl"
