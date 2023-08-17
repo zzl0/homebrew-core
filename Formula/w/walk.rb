702754b930bea6e80e7a2d082a44fc6873243a70
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-08-17 15:25:56 +0000

    walk: add 1.4.0 bottle.

--- Formula/w/walk.rb
+++ Formula/w/walk.rb
@@ -5,6 +5,16 @@ class Walk < Formula
   sha256 "606568e098a91c7b0e694534bf0d268d92cc9ac19bce9e4671775837176eb9f5"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "504e4846c09e6db3de651514150a0b2940d93d69f013a8c50b95844e2ee67c33"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "504e4846c09e6db3de651514150a0b2940d93d69f013a8c50b95844e2ee67c33"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "504e4846c09e6db3de651514150a0b2940d93d69f013a8c50b95844e2ee67c33"
+    sha256 cellar: :any_skip_relocation, ventura:        "e46b2b053dd1a65b7bbcb96b9735c17a4e916e19e9db1b3bd49c87a5225f2a1c"
+    sha256 cellar: :any_skip_relocation, monterey:       "e46b2b053dd1a65b7bbcb96b9735c17a4e916e19e9db1b3bd49c87a5225f2a1c"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e46b2b053dd1a65b7bbcb96b9735c17a4e916e19e9db1b3bd49c87a5225f2a1c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8a428549fbba7dbe9cd59fa25cc9ceae79554c6a3b776b7e09e7348428118924"
+  end
+
   depends_on "go" => :build
 
   def install
