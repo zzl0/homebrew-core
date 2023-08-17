c102d1bd5265fb73d14cea3a1af56acd7db59e2f
József Bartók <jbartok@gradle.com>
2023-08-17 12:56:21 +0300

    gradle 8.3

--- Formula/g/gradle.rb
+++ Formula/g/gradle.rb
@@ -1,8 +1,8 @@
 class Gradle < Formula
   desc "Open-source build automation tool based on the Groovy and Kotlin DSL"
   homepage "https://www.gradle.org/"
-  url "https://services.gradle.org/distributions/gradle-8.2.1-all.zip"
-  sha256 "7c3ad722e9b0ce8205b91560fd6ce8296ac3eadf065672242fd73c06b8eeb6ee"
+  url "https://services.gradle.org/distributions/gradle-8.3-all.zip"
+  sha256 "bb09982fdf52718e4c7b25023d10df6d35a5fff969860bdf5a5bd27a3ab27a9e"
   license "Apache-2.0"
 
   livecheck do
