579a4805ed43e1cc4e024f4b1a598cc29fefecbe
Rui Chen <rui@chenrui.dev>
2023-08-17 15:35:04 -0400

    buildpulse-test-reporter 0.27.2

--- Formula/b/buildpulse-test-reporter.rb
+++ Formula/b/buildpulse-test-reporter.rb
@@ -1,8 +1,8 @@
 class BuildpulseTestReporter < Formula
   desc "Connect your CI to BuildPulse to detect, track, and rank flaky tests"
   homepage "https://buildpulse.io"
-  url "https://github.com/buildpulse/test-reporter/archive/refs/tags/v0.27.1.tar.gz"
-  sha256 "209618eef253c15bf2d625cb1539fbab5b9b10c342798a6159d9e515f27ac46d"
+  url "https://github.com/buildpulse/test-reporter/archive/refs/tags/v0.27.2.tar.gz"
+  sha256 "fc9a01cb2666bf4c23e7883d870e00304d1090261752c1010294f8a5a5a42915"
   license "MIT"
   head "https://github.com/buildpulse/test-reporter.git", branch: "main"
 
