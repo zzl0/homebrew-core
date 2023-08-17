7a2f758057ff94689c59f902e93640d643bf1b6e
Porkepix <porkepix@gmail.com>
2023-08-17 13:07:15 +0200

    xcb-proto 1.16.0

--- Formula/x/xcb-proto.rb
+++ Formula/x/xcb-proto.rb
@@ -1,8 +1,8 @@
 class XcbProto < Formula
   desc "X.Org: XML-XCB protocol descriptions for libxcb code generation"
   homepage "https://www.x.org/"
-  url "https://xorg.freedesktop.org/archive/individual/proto/xcb-proto-1.15.2.tar.xz"
-  sha256 "7072beb1f680a2fe3f9e535b797c146d22528990c72f63ddb49d2f350a3653ed"
+  url "https://xorg.freedesktop.org/archive/individual/proto/xcb-proto-1.16.0.tar.xz"
+  sha256 "a75a1848ad2a89a82d841a51be56ce988ff3c63a8d6bf4383ae3219d8d915119"
   license "MIT"
 
   bottle do
