9e8533256ff2dcc41ee2d039ff40c9a9dcbab27a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-08-17 13:27:18 +0000

    gradle: update 8.3 bottle.

--- Formula/g/gradle.rb
+++ Formula/g/gradle.rb
@@ -11,13 +11,13 @@ class Gradle < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "cb845ba56cfb528ebd2c35f4b7e93ac5a7017a48c5e17df6deceed93fc30f028"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "cb845ba56cfb528ebd2c35f4b7e93ac5a7017a48c5e17df6deceed93fc30f028"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "cb845ba56cfb528ebd2c35f4b7e93ac5a7017a48c5e17df6deceed93fc30f028"
-    sha256 cellar: :any_skip_relocation, ventura:        "0827979f8a88ab5d214ccf22f4627f4d173140ec16f3c09f7a0ad7689bb45f5e"
-    sha256 cellar: :any_skip_relocation, monterey:       "0827979f8a88ab5d214ccf22f4627f4d173140ec16f3c09f7a0ad7689bb45f5e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0827979f8a88ab5d214ccf22f4627f4d173140ec16f3c09f7a0ad7689bb45f5e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cb845ba56cfb528ebd2c35f4b7e93ac5a7017a48c5e17df6deceed93fc30f028"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a05fd6ee0167769945296ee3e66eb0d72394db5216df3a53d3d2f596dcbc4a80"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a05fd6ee0167769945296ee3e66eb0d72394db5216df3a53d3d2f596dcbc4a80"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a05fd6ee0167769945296ee3e66eb0d72394db5216df3a53d3d2f596dcbc4a80"
+    sha256 cellar: :any_skip_relocation, ventura:        "743d679a05ca1ee12372d1220dea581173ddb31939b03476df19359ab82d24ba"
+    sha256 cellar: :any_skip_relocation, monterey:       "743d679a05ca1ee12372d1220dea581173ddb31939b03476df19359ab82d24ba"
+    sha256 cellar: :any_skip_relocation, big_sur:        "743d679a05ca1ee12372d1220dea581173ddb31939b03476df19359ab82d24ba"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a05fd6ee0167769945296ee3e66eb0d72394db5216df3a53d3d2f596dcbc4a80"
   end
 
   depends_on "openjdk"
