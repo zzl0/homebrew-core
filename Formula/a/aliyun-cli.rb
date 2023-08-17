d1f706c7e2bc4f85f49c2205fbc83e86055b5cee
Rui Chen <rui@chenrui.dev>
2023-08-17 09:45:29 -0400

    aliyun-cli 3.0.176

--- Formula/a/aliyun-cli.rb
+++ Formula/a/aliyun-cli.rb
@@ -2,8 +2,8 @@ class AliyunCli < Formula
   desc "Universal Command-Line Interface for Alibaba Cloud"
   homepage "https://github.com/aliyun/aliyun-cli"
   url "https://github.com/aliyun/aliyun-cli.git",
-      tag:      "v3.0.174",
-      revision: "e39fa1d8b6744dd655c6c34cae8b6a0c2de12178"
+      tag:      "v3.0.176",
+      revision: "2272e828eb9660917c8639a0b93881359f2e7d43"
   license "Apache-2.0"
 
   bottle do
