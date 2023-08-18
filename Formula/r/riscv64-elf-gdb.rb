d7a9324e345d01e87546dcf2d343bd022f7f5d43
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-08-18 03:22:10 +0000

    riscv64-elf-gdb: add 13.2 bottle.

--- Formula/r/riscv64-elf-gdb.rb
+++ Formula/r/riscv64-elf-gdb.rb
@@ -11,6 +11,16 @@ class Riscv64ElfGdb < Formula
     formula "gdb"
   end
 
+  bottle do
+    sha256 arm64_ventura:  "903d1db714bcffec0b16ad4d64dc5f6081a28ece9d466b06effda5368e4204c1"
+    sha256 arm64_monterey: "30a2b9905b314d1c6f97d8885be9994844691b3e5c39d2aa6560609149dc0954"
+    sha256 arm64_big_sur:  "cce6901705f4677a8c12a351ad0418c6c07fb7c897cd4a4962529a1ebf70f582"
+    sha256 ventura:        "be286dba7cbc64fc3f0e6421aa6332c51e134e01d2698ab58fc3ce8952533a08"
+    sha256 monterey:       "9a7ab73441b8f898e6702bb5800be9562c67711d1e4497d5db486e9e67ef4fd4"
+    sha256 big_sur:        "07daa45aad65a85168a581173b57399aa372c0eacb8370ae8c14d1ae59dfbc50"
+    sha256 x86_64_linux:   "fc83ac581b46ef0844e492c96d6d8a27e37dcca381921103cf241ef5445a8066"
+  end
+
   depends_on "riscv64-elf-gcc" => :test
   depends_on "gmp"
   depends_on "python@3.11"
