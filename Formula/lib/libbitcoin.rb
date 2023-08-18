741e2ccf2aab74ff038fc37ae61e0ab1309f5470
Rui Chen <rui@chenrui.dev>
2023-08-18 11:39:42 -0400

    libbitcoin 3.8.0
    libbitcoin: secp256k1 0.1.0.20
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    libbitcoin: update test
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/lib/libbitcoin.rb
+++ Formula/lib/libbitcoin.rb
@@ -1,10 +1,9 @@
 class Libbitcoin < Formula
   desc "Bitcoin Cross-Platform C++ Development Toolkit"
   homepage "https://github.com/libbitcoin/libbitcoin-system"
-  url "https://github.com/libbitcoin/libbitcoin-system/archive/v3.6.0.tar.gz"
-  sha256 "5bcc4c31b53acbc9c0d151ace95d684909db4bf946f8d724f76c711934c6775c"
+  url "https://github.com/libbitcoin/libbitcoin-system/archive/v3.8.0.tar.gz"
+  sha256 "b5dd2a97289370fbb93672dd3114383f30d877061de1d1683fa8bdda5309bfa2"
   license "AGPL-3.0-or-later"
-  revision 8
 
   livecheck do
     url :stable
@@ -32,8 +31,8 @@ class Libbitcoin < Formula
   depends_on "qrencode"
 
   resource "secp256k1" do
-    url "https://github.com/libbitcoin/secp256k1/archive/v0.1.0.13.tar.gz"
-    sha256 "9e48dbc88d0fb5646d40ea12df9375c577f0e77525e49833fb744d3c2a69e727"
+    url "https://github.com/libbitcoin/secp256k1/archive/v0.1.0.20.tar.gz"
+    sha256 "61583939f1f25b92e6401e5b819e399da02562de663873df3056993b40148701"
   end
 
   def install
@@ -63,7 +62,7 @@ class Libbitcoin < Formula
   test do
     boost = Formula["boost@1.76"]
     (testpath/"test.cpp").write <<~EOS
-      #include <bitcoin/bitcoin.hpp>
+      #include <bitcoin/system.hpp>
       int main() {
         const auto block = bc::chain::block::genesis_mainnet();
         const auto& tx = block.transactions().front();
@@ -76,7 +75,7 @@ class Libbitcoin < Formula
     EOS
     system ENV.cxx, "-std=c++11", "test.cpp",
                     "-I#{boost.include}",
-                    "-L#{lib}", "-lbitcoin",
+                    "-L#{lib}", "-lbitcoin-system",
                     "-L#{boost.lib}", "-lboost_system",
                     "-o", "test"
     system "./test"
