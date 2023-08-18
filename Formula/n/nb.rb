d44bd86d7c6931b2f82ee9890a63571017f3251d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-08-18 03:11:27 +0000

    nb: update 7.5.7 bottle.

--- Formula/n/nb.rb
+++ Formula/n/nb.rb
@@ -7,13 +7,13 @@ class Nb < Formula
   head "https://github.com/xwmx/nb.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "fd8aefa75149caca71e775c64fb2a45322e1c6fd57ce09a108114fa38fbb5916"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "fd8aefa75149caca71e775c64fb2a45322e1c6fd57ce09a108114fa38fbb5916"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fd8aefa75149caca71e775c64fb2a45322e1c6fd57ce09a108114fa38fbb5916"
-    sha256 cellar: :any_skip_relocation, ventura:        "61497f1c097543bc55804c5f54fad80eaafd5c77290f10aedf0bec444b84c66c"
-    sha256 cellar: :any_skip_relocation, monterey:       "61497f1c097543bc55804c5f54fad80eaafd5c77290f10aedf0bec444b84c66c"
-    sha256 cellar: :any_skip_relocation, big_sur:        "61497f1c097543bc55804c5f54fad80eaafd5c77290f10aedf0bec444b84c66c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fd8aefa75149caca71e775c64fb2a45322e1c6fd57ce09a108114fa38fbb5916"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "baf08dda9c9eb5b3b7fa98d84c9661f1555c15305f5979c5877292190387f419"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "baf08dda9c9eb5b3b7fa98d84c9661f1555c15305f5979c5877292190387f419"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "baf08dda9c9eb5b3b7fa98d84c9661f1555c15305f5979c5877292190387f419"
+    sha256 cellar: :any_skip_relocation, ventura:        "187681e6da26249753631619e8be43713ae97e814e799d273d5746f44edf3011"
+    sha256 cellar: :any_skip_relocation, monterey:       "187681e6da26249753631619e8be43713ae97e814e799d273d5746f44edf3011"
+    sha256 cellar: :any_skip_relocation, big_sur:        "187681e6da26249753631619e8be43713ae97e814e799d273d5746f44edf3011"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "baf08dda9c9eb5b3b7fa98d84c9661f1555c15305f5979c5877292190387f419"
   end
 
   depends_on "bat"
