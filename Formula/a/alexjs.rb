98f3684e290de9c392a9056318ada5ceb98d30b5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-08-18 22:18:23 +0000

    alexjs: update 11.0.1 bottle.

--- Formula/a/alexjs.rb
+++ Formula/a/alexjs.rb
@@ -8,14 +8,13 @@ class Alexjs < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3d7a81d455214f06a5760072bb542743abe908b4a334231f48465002cf732f7d"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "4a23b144193badd2a84c6d596051f17bd31c1174af959d78ad4e043e29a1f53e"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4a23b144193badd2a84c6d596051f17bd31c1174af959d78ad4e043e29a1f53e"
-    sha256 cellar: :any_skip_relocation, ventura:        "85cb9eb7f7ec9659c9a1242c6e2033854bde87e3cc147b3a0a0f4a9fd709dd03"
-    sha256 cellar: :any_skip_relocation, monterey:       "cacb216ea94fd6e7b20fa41cbabeb44e26a965429f06f8f1ce33e9428175b35e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "cacb216ea94fd6e7b20fa41cbabeb44e26a965429f06f8f1ce33e9428175b35e"
-    sha256 cellar: :any_skip_relocation, catalina:       "cacb216ea94fd6e7b20fa41cbabeb44e26a965429f06f8f1ce33e9428175b35e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4a23b144193badd2a84c6d596051f17bd31c1174af959d78ad4e043e29a1f53e"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "63048df11763713024140ff157290f205111355b18f685c935bf74172e7ff8b6"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "63048df11763713024140ff157290f205111355b18f685c935bf74172e7ff8b6"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "63048df11763713024140ff157290f205111355b18f685c935bf74172e7ff8b6"
+    sha256 cellar: :any_skip_relocation, ventura:        "5932fb08167ee8a713de4e5f523083a622a24c2f68e75d74ace3c07cfa79ca28"
+    sha256 cellar: :any_skip_relocation, monterey:       "5932fb08167ee8a713de4e5f523083a622a24c2f68e75d74ace3c07cfa79ca28"
+    sha256 cellar: :any_skip_relocation, big_sur:        "5932fb08167ee8a713de4e5f523083a622a24c2f68e75d74ace3c07cfa79ca28"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "63048df11763713024140ff157290f205111355b18f685c935bf74172e7ff8b6"
   end
 
   depends_on "node"
