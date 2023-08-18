e2aa881de07ab016875d48c0f22f5f8bcbff767d
Patrick Linnane <patrick@linnane.io>
2023-08-18 15:39:30 -0700

    awscli 2.13.11

--- Formula/a/awscli.rb
+++ Formula/a/awscli.rb
@@ -3,8 +3,8 @@ class Awscli < Formula
 
   desc "Official Amazon AWS command-line interface"
   homepage "https://aws.amazon.com/cli/"
-  url "https://github.com/aws/aws-cli/archive/2.13.10.tar.gz"
-  sha256 "523646f7b86b4beef74c0f0e9a2a178ea1d40a21b7da0e6852db17d76044b07c"
+  url "https://github.com/aws/aws-cli/archive/2.13.11.tar.gz"
+  sha256 "930aa72a5bba77ae75a384cbfede9e204304af182677439d315d1b3da0809e17"
   license "Apache-2.0"
   head "https://github.com/aws/aws-cli.git", branch: "v2"
 
