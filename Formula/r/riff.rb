d194fb24887c53cdeae6a5828a1f0dbbbf9913a8
Johan Walles <johan.walles@gmail.com>
2023-08-20 21:11:36 +0000

    riff 2.25.2

--- Formula/r/riff.rb
+++ Formula/r/riff.rb
@@ -1,8 +1,8 @@
 class Riff < Formula
   desc "Diff filter highlighting which line parts have changed"
   homepage "https://github.com/walles/riff"
-  url "https://github.com/walles/riff/archive/2.25.1.tar.gz"
-  sha256 "4df3c1c55a391961e5f33c4775296f420c0e348e9f74b285ce739d9da791e5f4"
+  url "https://github.com/walles/riff/archive/2.25.2.tar.gz"
+  sha256 "ea108289c0516ecabd35974208d360b189be1ad07f3d67883ff8784c8776b21c"
   license "MIT"
 
   bottle do
