3b5d287fe5cae7c09ace52487773679354419c94
Rui Chen <rui@chenrui.dev>
2023-08-20 10:00:16 -0400

    plog 1.1.10

--- Formula/p/plog.rb
+++ Formula/p/plog.rb
@@ -1,8 +1,8 @@
 class Plog < Formula
   desc "Portable, simple and extensible C++ logging library"
   homepage "https://github.com/SergiusTheBest/plog"
-  url "https://github.com/SergiusTheBest/plog/archive/refs/tags/1.1.9.tar.gz"
-  sha256 "058315b9ec9611b659337d4333519ab4783fad3f2f23b1cc7bb84d977ea38055"
+  url "https://github.com/SergiusTheBest/plog/archive/refs/tags/1.1.10.tar.gz"
+  sha256 "55a090fc2b46ab44d0dde562a91fe5fc15445a3caedfaedda89fe3925da4705a"
   license "MIT"
 
   bottle do
