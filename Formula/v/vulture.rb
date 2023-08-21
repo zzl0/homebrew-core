0ac5c0c1e78f0ec6f3202e23df9c0592df1a9a52
Patrick Linnane <patrick@linnane.io>
2023-08-21 09:47:10 -0700

    vulture 2.9.1

--- Formula/v/vulture.rb
+++ Formula/v/vulture.rb
@@ -3,8 +3,8 @@ class Vulture < Formula
 
   desc "Find dead Python code"
   homepage "https://github.com/jendrikseipp/vulture"
-  url "https://files.pythonhosted.org/packages/a1/e4/456ff34fd6bbdd7695a8b5b06f5b2370ab000ee3f07c33ff8d13e1e0f659/vulture-2.9.tar.gz"
-  sha256 "0f4d86ba515e67db2860539894edb4e387b25696f831234826dd72c636f0331f"
+  url "https://files.pythonhosted.org/packages/4c/5d/4734f4808f63e8b7897cef965a5413e94845eafbc256f70126c0c462a84d/vulture-2.9.1.tar.gz"
+  sha256 "b6a2aa632b6fd51488a8eeac650ab4a509bb1a032e81943817a8a2e6a63a30b3"
   license "MIT"
   head "https://github.com/jendrikseipp/vulture.git", branch: "main"
 
@@ -21,18 +21,12 @@ class Vulture < Formula
   depends_on "python-toml"
   depends_on "python@3.11"
 
-  resource "toml" do
-    url "https://files.pythonhosted.org/packages/be/ba/1f744cdc819428fc6b5084ec34d9b30660f6f9daaf70eead706e3203ec3c/toml-0.10.2.tar.gz"
-    sha256 "b3bda1d108d5dd99f4a20d24d9c348e91c4db7ab1b749200bded2f839ccbe68f"
-  end
-
   def install
     virtualenv_install_with_resources
   end
 
   test do
-    # upstream bug, https://github.com/jendrikseipp/vulture/issues/321
-    assert_equal "vulture #{version}\n", shell_output("#{bin}/vulture --version", 2)
+    assert_equal "vulture #{version}\n", shell_output("#{bin}/vulture --version")
 
     (testpath/"unused.py").write "class Unused: pass"
     assert_match "unused.py:1: unused class 'Unused'", shell_output("#{bin}/vulture #{testpath}/unused.py", 3)
