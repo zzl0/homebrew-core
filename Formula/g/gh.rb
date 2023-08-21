ff2939d7bd225cadbba7389a2e8ddd8ac3ceabbe
Patrick Linnane <patrick@linnane.io>
2023-08-21 11:13:27 -0700

    gh 2.33.0

--- Formula/g/gh.rb
+++ Formula/g/gh.rb
@@ -1,8 +1,8 @@
 class Gh < Formula
   desc "GitHub command-line tool"
   homepage "https://github.com/cli/cli"
-  url "https://github.com/cli/cli/archive/v2.32.1.tar.gz"
-  sha256 "1d569dc82eb6520e6a8959568c2db84fea3bbaab2604c8dd5901849d320e1eae"
+  url "https://github.com/cli/cli/archive/v2.33.0.tar.gz"
+  sha256 "27e22a8e637501ee8e9d45c702dbc5c5c559c2a4fe59cd3d807e075cdbefddd2"
   license "MIT"
 
   head "https://github.com/cli/cli.git", branch: "trunk"
