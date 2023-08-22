e6fed49d135906dcc805c8119b559690c2a1bb8c
Rui Chen <rui@chenrui.dev>
2023-08-22 08:02:49 -0400

    openliberty-webprofile8 23.0.0.8

--- Formula/o/openliberty-webprofile8.rb
+++ Formula/o/openliberty-webprofile8.rb
@@ -1,8 +1,8 @@
 class OpenlibertyWebprofile8 < Formula
   desc "Lightweight open framework for Java (Jakarta EE Web Profile 8)"
   homepage "https://openliberty.io"
-  url "https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/release/23.0.0.7/openliberty-webProfile8-23.0.0.7.zip"
-  sha256 "77688128decc9acdc5effa3f35e39b71c3aa56c87b395597071c80ba50e30435"
+  url "https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/release/23.0.0.8/openliberty-webProfile8-23.0.0.8.zip"
+  sha256 "48b88498989842a94446ab31d99b0fed60e713ea829431af6bf8350259c52f2c"
   license "EPL-1.0"
 
   livecheck do
