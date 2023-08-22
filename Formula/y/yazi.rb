55613127e7370c0806866f86c241f2d8d7d0b725
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-08-22 09:33:23 +0000

    yazi: add 0.1.3 bottle.

--- Formula/y/yazi.rb
+++ Formula/y/yazi.rb
@@ -6,6 +6,16 @@ class Yazi < Formula
   license "MIT"
   head "https://github.com/sxyazi/yazi.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "61fafd26a35bc485ea6271f3e0809b58e80a9898aecc7b731347332455cf30b5"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b8b00b4b6c5f195a72a5de52d03586c067ec6b0da3e3c9ae87cb23489d86d1f0"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "919bf6a02646308126eec570ca27fc5a16135531566ccef3dc6760eac4264787"
+    sha256 cellar: :any_skip_relocation, ventura:        "ecaed183982899737cfe8e4bc2f76e579c519f55cc47eba5848138c5785634d7"
+    sha256 cellar: :any_skip_relocation, monterey:       "bce42f056e28dc664d532b71094286ad4f39c6d1463abe53024b58d0f0df1782"
+    sha256 cellar: :any_skip_relocation, big_sur:        "5fac6a20be20ac3c03a7824bf9c1bf1347c00d1b8d0e21a248185dab743ff406"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "67f55b93826b19f6157b82de039bd7b4a6521dffb6785f519c3815bf2eb6baac"
+  end
+
   depends_on "rust" => :build
 
   def install
