a102eec24d9d54162164d63b667aa6f725567908
Porkepix <porkepix@gmail.com>
2023-08-22 11:15:45 +0200

    algol68g 3.3.3

--- Formula/a/algol68g.rb
+++ Formula/a/algol68g.rb
@@ -1,8 +1,8 @@
 class Algol68g < Formula
   desc "Algol 68 compiler-interpreter"
   homepage "https://jmvdveer.home.xs4all.nl/algol.html"
-  url "https://jmvdveer.home.xs4all.nl/algol68g-3.3.2.tar.gz"
-  sha256 "6ffb646e13eb3b22a427eae0a59e2ea8bca9fff44de8b53467efe6cdd81ddedd"
+  url "https://jmvdveer.home.xs4all.nl/algol68g-3.3.3.tar.gz"
+  sha256 "23190972e6bbea96fa6ef93de377d3a81ae98a90ca5606dc0bd0e934ea9fd305"
   license "GPL-3.0-or-later"
 
   livecheck do
