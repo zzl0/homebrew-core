5a136723f3fabab1bce6efa67119b6cababc8864
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-08-22 21:39:43 +0000

    tpm: add 3.1.0 bottle.

--- Formula/t/tpm.rb
+++ Formula/t/tpm.rb
@@ -5,6 +5,10 @@ class Tpm < Formula
   sha256 "2411fc416c4475d297f61078d0a03afb3a1f5322fff26a13fdb4f20d7e975570"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "bc7fbd2e63cf09eb0dc299caa4447a2508b76341ada7a8f2e5ad31f948aaf629"
+  end
+
   depends_on "tmux"
 
   def install
