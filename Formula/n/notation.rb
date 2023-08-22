2c6c54167e52f2a4a812c7879291c6d2fafb150c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-08-22 15:51:40 +0000

    notation: add 1.0.0 bottle.

--- Formula/n/notation.rb
+++ Formula/n/notation.rb
@@ -6,6 +6,16 @@ class Notation < Formula
   license "Apache-2.0"
   head "https://github.com/notaryproject/notation.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "dace95af3497540a4f288c26dad180ffcac605e39559bf7724543edefa7585be"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "bcf87f3ec5d3065299216fc05f675cd0c41706264209b83cf2d7f6dbc9bbc0e4"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b873594a2ef7d95385666669470fa850f25344354ab0b373b3d6455845f216ec"
+    sha256 cellar: :any_skip_relocation, ventura:        "257bab825349099484695a235b9a380dd32d181bd270657c7e156a478ec0ab81"
+    sha256 cellar: :any_skip_relocation, monterey:       "376ede052a69f5b98552106845090f262c263a7a9d2661bf166971fab32520ba"
+    sha256 cellar: :any_skip_relocation, big_sur:        "4178e3f434a7ae62b7bd6f5af5849a05e2f49680332e1678dc5fe2943cfbe4d3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0e76d825c398991d20ba9e8243bf5cb6f1fbf4f9f1cefaccf70dfe183b5bf3a4"
+  end
+
   depends_on "go" => :build
 
   def install
