ca5f8ded6cc79373c1a8590cb8c36bae3f9dd820
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-08-22 23:41:44 +0000

    legitify: add 1.0.1 bottle.

--- Formula/l/legitify.rb
+++ Formula/l/legitify.rb
@@ -6,6 +6,16 @@ class Legitify < Formula
   license "Apache-2.0"
   head "https://github.com/Legit-Labs/legitify.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0a92a1ddb8ae86d3b8a4338bc7cc6c9d732735871d6c2a6acee75522f63e5df8"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "45d29538e4286db5b2183f681a57bc47680695e0d7efecd002f36fdf59d1670e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0aaf0efd24e7dbbd86993576ebc448d178782452d9bdcfc324f47f0a03e64d14"
+    sha256 cellar: :any_skip_relocation, ventura:        "938fe1279aec5442123042c6b240e598c51384de118cec48b51e0dedb570e1ee"
+    sha256 cellar: :any_skip_relocation, monterey:       "cf8dab3a355f44013d52194a47f0260ebe3e1f26f5712a6db07cf2b7674e50e5"
+    sha256 cellar: :any_skip_relocation, big_sur:        "601b607bd73fc4e9b41f7f2f04d0e66c9e4997178eaa13c39065c69dfc5094aa"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6cbdc057d77b24f734d4e415945023bef229af9d34af2297aa0297039f62bb14"
+  end
+
   depends_on "go" => :build
 
   def install
