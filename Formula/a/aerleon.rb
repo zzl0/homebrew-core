69b9a430467570dcb4fd35b4bcc7fc8d3c8476c8
Patrick Linnane <patrick@linnane.io>
2023-08-22 16:19:48 -0700

    aerleon 1.7.0 (new formula)

---
+++ Formula/a/aerleon.rb
@@ -0,0 +1,91 @@
+class Aerleon < Formula
+  include Language::Python::Virtualenv
+
+  desc "Generate firewall configs for multiple firewall platforms"
+  homepage "https://aerleon.readthedocs.io/en/latest/"
+  url "https://files.pythonhosted.org/packages/b8/e5/f4386abc5d0e7f18bba22650514c1c14dbd235c93e11ac020f6c724614da/aerleon-1.7.0.tar.gz"
+  sha256 "f3145c2a04f37c0463fbd80ee650f039bda9bc445e9c7fd4f18d8eeeda1b6ead"
+  license "Apache-2.0"
+  head "https://github.com/aerleon/aerleon.git", branch: "main"
+
+  depends_on "python-typing-extensions"
+  depends_on "python@3.11"
+  depends_on "pyyaml"
+
+  resource "absl-py" do
+    url "https://files.pythonhosted.org/packages/79/c9/45ecff8055b0ce2ad2bfbf1f438b5b8605873704d50610eda05771b865a0/absl-py-1.4.0.tar.gz"
+    sha256 "d2c244d01048ba476e7c080bd2c6df5e141d211de80223460d5b3b8a2a58433d"
+  end
+
+  resource "ply" do
+    url "https://files.pythonhosted.org/packages/e5/69/882ee5c9d017149285cab114ebeab373308ef0f874fcdac9beb90e0ac4da/ply-3.11.tar.gz"
+    sha256 "00c7c1aaa88358b9c765b6d3000c6eec0ba42abca5351b095321aef446081da3"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    (testpath/"def/definitions.yaml").write <<~EOS
+      networks:
+        RFC1918:
+          values:
+            - address: 10.0.0.0/8
+            - address: 172.16.0.0/12
+            - address: 192.168.0.0/16
+        WEB_SERVERS:
+          values:
+            - address: 10.0.0.1/32
+              comment: Web Server 1
+            - address: 10.0.0.2/32
+              comment: Web Server 2
+        MAIL_SERVERS:
+          values:
+            - address: 10.0.0.3/32
+              comment: Mail Server 1
+            - address: 10.0.0.4/32
+              comment: Mail Server 2
+        ALL_SERVERS:
+          values:
+            - WEB_SERVERS
+            - MAIL_SERVERS
+      services:
+        HTTP:
+          - protocol: tcp
+            port: 80
+        HTTPS:
+          - protocol: tcp
+            port: 443
+        WEB:
+          - HTTP
+          - HTTPS
+        HIGH_PORTS:
+          - port: 1024-65535
+            protocol: tcp
+          - port: 1024-65535
+            protocol: udp
+    EOS
+
+    (testpath/"policies/pol/example.pol.yaml").write <<~EOS
+      filters:
+      - header:
+          comment: Example inbound
+          targets:
+            cisco: inbound extended
+        terms:
+          - name: accept-web-servers
+            comment: Accept connections to our web servers.
+            destination-address: WEB_SERVERS
+            destination-port: WEB
+            protocol: tcp
+            action: accept
+          - name: default-deny
+            comment: Deny anything else.
+            action: deny#{"  "}
+    EOS
+
+    assert_match "writing file: example.pol.acl", shell_output("#{bin}/aclgen 2>&1")
+    assert_path_exists "example.pol.acl"
+  end
+end
