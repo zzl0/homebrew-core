32630c0bd8040877ab59de03c535978ff4263b80
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-08-23 14:08:04 +0000

    livekit-cli: update 1.2.9 bottle.

--- Formula/l/livekit-cli.rb
+++ Formula/l/livekit-cli.rb
@@ -16,13 +16,13 @@ class LivekitCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "e58f8807cfdc2c2bcae5d7101baccdfd4b7bbc81485eb5be54e46766fa34fba0"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "9c919eb115df7facd86bf014a3a429615e12c5081d42b6af7d7e74d2643b80cb"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e8e54d009b3e51ae24511219359d0b697f62977a6379b73351a21b2d798d2814"
-    sha256 cellar: :any_skip_relocation, ventura:        "2d1e788550a13cbc11a047e03bff3915b20f0bec58c4e1f9068b07f8950df03a"
-    sha256 cellar: :any_skip_relocation, monterey:       "18a583a15b3f585852a5a046c4a05242b8cceea50677dbba63f19c02bce6f3c5"
-    sha256 cellar: :any_skip_relocation, big_sur:        "3a78c16ac871e202b88376c576d08878204ca2cb1ff55ee4aa6cff17ef63e97a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "359f6d070fc58de12c32a844d78206fe5a26957e6399cf76b505d468ba13575c"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "150c05c77a8faa323ba661e05e11209dce9b61bc48704c72ba9bd7877af6ab9b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "d9913b8b1a6a63af7aa178bbc8ab3d028c00e9e238d84952417c7b721c79937b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bb3d54fe284f97e5d66c63d5b09c5b8b1f2765091f248885821fb2ae706c234b"
+    sha256 cellar: :any_skip_relocation, ventura:        "588cc2f06d4e407c42a4f41eeb7d00acd743749e24dd97393008b36fdc9d9a24"
+    sha256 cellar: :any_skip_relocation, monterey:       "583d365aa80e21d74c350b037082b74c9b8bd03add9c2a057145d987cda2bc5a"
+    sha256 cellar: :any_skip_relocation, big_sur:        "da575131ea4d035b43e70d74219d5cfd5a64c68521a3af308f94b19eec98176d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a1689673ffafa56522c56aa892915be38b4b587443757049ac4942b7bc2e72d9"
   end
 
   depends_on "go" => :build
