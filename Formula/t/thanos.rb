1a4cec6a90d78987c8889185e34d36b12d337379
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-08-23 15:28:43 +0000

    thanos: update 0.32.0 bottle.

--- Formula/t/thanos.rb
+++ Formula/t/thanos.rb
@@ -6,13 +6,13 @@ class Thanos < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "7c130323995f309008f98181798fa86a6acd4d7b85eb8d583c62b7ae4453fa22"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "4e4c7354961ff645bac66e6daf3b013c6291bc33504302935e671abb8675a846"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f0b52384186c70cf891befd01688238a42ea2bd522cbb2dcc713d74e4adc6dad"
-    sha256 cellar: :any_skip_relocation, ventura:        "b7469949b0d187c5e4c5f6e5853047505f5ac52a63acd65f5515772c35bfb7a9"
-    sha256 cellar: :any_skip_relocation, monterey:       "377eb6732a743f7ea66895d7a5da27aa4ddd725f444b829ae7ab259403a3a4b9"
-    sha256 cellar: :any_skip_relocation, big_sur:        "18623ad1fd1296db0c51f7d3005c76ace156128e2d1166f2cf9c84f26079fcdb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d2abc9c8e872fffa219e8808fd730e0034077b15d8717b448ac347afbb65f4b7"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "bfd61eee84a6d806cd87c12edfa4dec30fbe144bba9ddca0b223753292285056"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3bcfc6b6a025caa162308f4f9d45bf0626cea530f1f193a82a6be50741542170"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ae61d4e81c5da710795b456c0608567f0d7a90a1596cfebc29f1397acee392f0"
+    sha256 cellar: :any_skip_relocation, ventura:        "875fad7cc2e6774a38972e0e5a642ad2af449d41f740d52a62961b008907a134"
+    sha256 cellar: :any_skip_relocation, monterey:       "44562d3686135538897f50aa5cb91540d228cc8b03d6c960f32b010d75c9a67e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f44c387e04e98b96d7a1fd3541cbcd27e9320f8889dc9bc9a3a9c9c3ea9ebc77"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "343e1f9aa6d3c9e8741f9c95f5a44a1c8eecacfa179b2157b6cafa336ca6f62e"
   end
 
   depends_on "go" => :build
