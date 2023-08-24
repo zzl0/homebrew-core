b4b1fd5544013bedfe919e05e73a123f0ee0ce10
Porkepix <porkepix@gmail.com>
2023-08-24 14:57:54 +0200

    wazero 1.5.0

--- Formula/w/wazero.rb
+++ Formula/w/wazero.rb
@@ -1,8 +1,8 @@
 class Wazero < Formula
   desc "Zero dependency WebAssembly runtime"
   homepage "https://wazero.io"
-  url "https://github.com/tetratelabs/wazero/archive/v1.4.0.tar.gz"
-  sha256 "e3035f3578bbd9b74bb82666972c4900c6eb5aa3ad441aa07f3ee86257809d5b"
+  url "https://github.com/tetratelabs/wazero/archive/v1.5.0.tar.gz"
+  sha256 "d9695533ba0c1e297439feeafd701c0e4e24c916f82c1b933d323fc8392f1960"
   license "Apache-2.0"
 
   bottle do
