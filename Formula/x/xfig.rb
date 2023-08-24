ac5f685bb29a213211d60a96ec16c100b02639c3
Rui Chen <rui@chenrui.dev>
2023-08-23 20:59:38 -0400

    xfig 3.2.9
    xfig: fix build
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    xfig: depends_on libxft
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/x/xfig.rb
+++ Formula/x/xfig.rb
@@ -1,10 +1,9 @@
 class Xfig < Formula
   desc "Facility for interactive generation of figures"
   homepage "https://mcj.sourceforge.net/"
-  url "https://downloads.sourceforge.net/mcj/xfig-3.2.8b.tar.xz"
-  sha256 "b2cc8181cfb356f6b75cc28771970447f69aba1d728a2dac0e0bcf1aea7acd3a"
+  url "https://downloads.sourceforge.net/mcj/xfig-3.2.9.tar.xz"
+  sha256 "13ed9d04d1bbc2dec09da7ef49ceec278382d290f6cd926474c2f2d016fec2f7"
   license "MIT"
-  revision 6
 
   livecheck do
     url :stable
@@ -22,17 +21,31 @@ class Xfig < Formula
   end
 
   depends_on "fig2dev"
+  depends_on "freetype"
   depends_on "ghostscript"
   depends_on "jpeg-turbo"
   depends_on "libpng"
   depends_on "libtiff"
   depends_on "libx11"
   depends_on "libxaw3d"
+  depends_on "libxft"
   depends_on "libxi"
   depends_on "libxpm"
   depends_on "libxt"
 
+  on_macos do
+    depends_on "gnu-sed" => :build
+  end
+
   def install
+    # Use GNU sed on macOS to avoid this build failure:
+    # `sed: 1: " /^[ \t]*\(!\|$\)/ d; s ...": bad flag in substitute command: '}'`
+    ENV.prepend_path "PATH", Formula["gnu-sed"].libexec/"gnubin" if OS.mac?
+
+    # Xft #includes <ft2build.h>, not <freetype2/ft2build.h>, hence freetype2
+    # must be put into the search path.
+    ENV.append "CFLAGS", "-I#{Formula["freetype"].opt_include}/freetype2"
+
     system "./configure", "--with-appdefaultdir=#{etc}/X11/app-defaults",
                           "--disable-silent-rules",
                           *std_configure_args
