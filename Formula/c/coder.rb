0d4ab1065a5771d2e224f7de06591b952b129cd3
Patrick Linnane <patrick@linnane.io>
2023-08-24 13:45:27 -0700

    coder 2.1.3

--- Formula/c/coder.rb
+++ Formula/c/coder.rb
@@ -1,8 +1,8 @@
 class Coder < Formula
   desc "Tool for provisioning self-hosted development environments with Terraform"
   homepage "https://coder.com"
-  url "https://github.com/coder/coder/archive/refs/tags/v2.1.2.tar.gz"
-  sha256 "401d999664094819e7cb1e5d7f699e23350e0e8756a21b673c9f9ad606d5f72e"
+  url "https://github.com/coder/coder/archive/refs/tags/v2.1.3.tar.gz"
+  sha256 "a3b37eab6465cd36c416656596e3e13e3e37d5d6ab447c09914b47e70483d029"
   license "AGPL-3.0-only"
 
   bottle do
