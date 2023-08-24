627f640c9f6e56de63ab52b8d83e8253859ccec6
Porkepix <porkepix@gmail.com>
2023-08-24 14:45:56 +0200

    pylyzer 0.0.41

--- Formula/p/pylyzer.rb
+++ Formula/p/pylyzer.rb
@@ -1,8 +1,8 @@
 class Pylyzer < Formula
   desc "Fast static code analyzer & language server for Python"
   homepage "https://github.com/mtshiba/pylyzer"
-  url "https://github.com/mtshiba/pylyzer/archive/refs/tags/v0.0.40.tar.gz"
-  sha256 "647702bf2e3f3cde18d4279dbe6f26287c4f39ee4e5b5365f44400b18b374c11"
+  url "https://github.com/mtshiba/pylyzer/archive/refs/tags/v0.0.41.tar.gz"
+  sha256 "0af70ae8d9fb13c4db85e428dbd881224d01768a010b94bb15a6cd82cddf77b0"
   license "MIT"
 
   bottle do
