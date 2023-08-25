481b83bd1f3837cfb5bebebbed818424bc5e31e6
Simon Sobisch <simonsobisch@web.de>
2023-08-25 14:45:07 +0000

    GnuCOBOL update formula
    * official URLs for homepage and download, plus mirror
    * switch to use of pkg-config,
      also reducing unneeded link and compile flags used in cobc which
      originated from global environment vars
    * add dependencies json-c, libxml2 and ncurses
    * explicit set the optional features matching the dependencies
    * adding head with its additional features (dependencies)

--- Formula/g/gnu-cobol.rb
+++ Formula/g/gnu-cobol.rb
@@ -1,13 +1,14 @@
 class GnuCobol < Formula
-  desc "Implements much of the COBOL 85 and COBOL 2002 standards"
-  homepage "https://sourceforge.net/projects/gnucobol/"
-  url "https://downloads.sourceforge.net/project/gnucobol/gnucobol/3.2/gnucobol-3.2.tar.xz"
+  desc "COBOL85-202x compiler supporting lots of dialect specific extensions"
+  homepage "https://www.gnu.org/software/gnucobol/"
+  url "https://ftp.gnu.org/gnu/gnucobol/gnucobol-3.2.tar.xz"
+  mirror "https://ftpmirror.gnu.org/gnucobol/gnucobol-3.2.tar.xz"
   sha256 "3bb48af46ced4779facf41fdc2ee60e4ccb86eaa99d010b36685315df39c2ee2"
   license "GPL-3.0-or-later"
 
   livecheck do
     url :stable
-    regex(%r{url=.*?/gnucobol[._-]v?(\d+(?:\.\d+)+)\.t}i)
+    regex(/href=.*?gnucobol[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
   bottle do
@@ -20,40 +21,78 @@ class GnuCobol < Formula
     sha256 x86_64_linux:   "4bf957f586d155f1034ac90ac24edff1253e9de935336680689d85f0bbdcf45c"
   end
 
+  head do
+    url "https://svn.code.sf.net/p/gnucobol/code/trunk"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "bison" => :build
+    depends_on "flex" => :build
+    depends_on "gettext" => :build
+    depends_on "help2man" => :build
+    depends_on "libtool" => :build
+    depends_on "texinfo" => :build
+
+    # GnuCOBOL 4.x+ (= currently only on head) features more backends
+    # and multiple indexed handlers, so we add dependencies for those here
+    depends_on "lmdb"
+    depends_on "unixodbc"
+    # TODO: add "visam" and --with-visam, once formula is added
+  end
+
+  depends_on "pkg-config" => :build
   depends_on "berkeley-db"
   depends_on "gmp"
+  depends_on "json-c"
+
+  uses_from_macos "libxml2"
+  uses_from_macos "ncurses"
 
   def install
-    # both environment variables are needed to be set
-    # the cobol compiler takes these variables for calling cc during its run
-    # if the paths to gmp and bdb are not provided, the run of cobc fails
-    gmp = Formula["gmp"]
-    bdb = Formula["berkeley-db"]
-    ENV.append "CPPFLAGS", "-I#{gmp.opt_include} -I#{bdb.opt_include}"
-    ENV.append "LDFLAGS", "-L#{gmp.opt_lib} -L#{bdb.opt_lib}"
-
     # Avoid shim references in binaries on Linux.
     ENV["LD"] = "ld" unless OS.mac?
 
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}",
-                          "--with-libiconv-prefix=/usr",
-                          "--with-libintl-prefix=/usr"
+    args = %w[
+      --disable-silent-rules
+      --with-libiconv-prefix=/usr
+      --with-libintl-prefix=/usr
+      --with-json=json-c
+      --with-xml2
+      --with-curses=ncurses
+      --with-db
+    ]
+
+    if build.head?
+      # GnuCOBOL 4.x+ features (= currently only on head)
+      args += %w[
+        --with-lmdb
+        --with-odbc
+        --with-indexed=db
+      ]
+
+      # bootstrap, go with whatever gettext we have
+      inreplace "configure.ac", "AM_GNU_GETTEXT_VERSION", "AM_GNU_GETTEXT_REQUIRE_VERSION"
+      system "build_aux/bootstrap", "install"
+    end
+    system "./configure", *std_configure_args,
+                          *args
+
     system "make", "install"
   end
 
   test do
     (testpath/"hello.cob").write <<~EOS
-            * COBOL must be indented
+            * COBOL fixed-form must be indented
       000001 IDENTIFICATION DIVISION.
       000002 PROGRAM-ID. hello.
       000003 PROCEDURE DIVISION.
       000004 DISPLAY "Hello World!".
       000005 STOP RUN.
     EOS
-    system "#{bin}/cobc", "-x", "hello.cob"
-    system "./hello"
+
+    # create test executable and run it, with verbose output
+    system "#{bin}/cobc", "-x", "-j", "-v", "hello.cob"
+    # now again as shared object (will also run cobcrun)
+    system "#{bin}/cobc", "-m", "-j", "-v", "hello.cob"
   end
 end
