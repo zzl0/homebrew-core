38cfe084f48db548b81bfb6a3705ff5214d6eab2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-08-25 14:57:10 +0000

    buildozer: update 6.3.3 bottle.

--- Formula/b/buildozer.rb
+++ Formula/b/buildozer.rb
@@ -7,13 +7,13 @@ class Buildozer < Formula
   head "https://github.com/bazelbuild/buildtools.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "62b787d905293f6110f9a0a2f8f145ba6621821bece2dac639b0c1c0077fdb47"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "62b787d905293f6110f9a0a2f8f145ba6621821bece2dac639b0c1c0077fdb47"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "62b787d905293f6110f9a0a2f8f145ba6621821bece2dac639b0c1c0077fdb47"
-    sha256 cellar: :any_skip_relocation, ventura:        "74337e7225878806e6f0dc96105e5066a701a31cf3afa31f79c3957b59336ec3"
-    sha256 cellar: :any_skip_relocation, monterey:       "74337e7225878806e6f0dc96105e5066a701a31cf3afa31f79c3957b59336ec3"
-    sha256 cellar: :any_skip_relocation, big_sur:        "74337e7225878806e6f0dc96105e5066a701a31cf3afa31f79c3957b59336ec3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "aa1319f411070c8dbae8ef757e3618361dc41130ffa3c43089271161bc0a145e"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "44dafbb1c84789d797d058866923ff1a69bbece2dc9b8b53b09892a3cd4c5660"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "44dafbb1c84789d797d058866923ff1a69bbece2dc9b8b53b09892a3cd4c5660"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "44dafbb1c84789d797d058866923ff1a69bbece2dc9b8b53b09892a3cd4c5660"
+    sha256 cellar: :any_skip_relocation, ventura:        "2db7613080f4212fb9152e23e47417543df9b3a571f4badb2c3a22d18c6345ad"
+    sha256 cellar: :any_skip_relocation, monterey:       "2db7613080f4212fb9152e23e47417543df9b3a571f4badb2c3a22d18c6345ad"
+    sha256 cellar: :any_skip_relocation, big_sur:        "2db7613080f4212fb9152e23e47417543df9b3a571f4badb2c3a22d18c6345ad"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7c1416f91727b61af753560528a90866bd6c5caf62e6b858e7ac3eec6ca5efa7"
   end
 
   depends_on "go" => :build
