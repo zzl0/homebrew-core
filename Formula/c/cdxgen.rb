0bca270fd9838b06d4f5bdf94e895bb09efe9505
Patrick Linnane <patrick@linnane.io>
2023-08-25 13:20:00 -0700

    cdxgen 9.6.0 (new formula)
    cdxgen: fix audit
    
    cdxgen: add version check
    
    cdxgen: keep `cargo-auditable-cdxgen` and `goversion`
    
    cdxgen: fix code style
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/c/cdxgen.rb
@@ -0,0 +1,46 @@
+require "language/node"
+
+class Cdxgen < Formula
+  desc "Creates CycloneDX Software Bill-of-Materials (SBOM) for projects"
+  homepage "https://github.com/CycloneDX/cdxgen"
+  url "https://registry.npmjs.org/@cyclonedx/cdxgen/-/cdxgen-9.6.0.tgz"
+  sha256 "7c84b3b63b38dd74fa7d757aaa70c6d7f4878f17c4667f511761cdc5744a0272"
+  license "Apache-2.0"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+
+    # Remove incompatible pre-built binaries
+    os = OS.kernel_name.downcase
+    arch = Hardware::CPU.intel? ? "amd64" : Hardware::CPU.arch.to_s
+    node_modules = libexec/"lib/node_modules/@cyclonedx/cdxgen/node_modules"
+    cdxgen_plugins = node_modules/"@cyclonedx/cdxgen-plugins-bin/plugins"
+    cdxgen_plugins.glob("*/*").each do |f|
+      next if f.basename.to_s.end_with?("-#{os}-#{arch}")
+
+      rm f
+    end
+  end
+
+  test do
+    (testpath/"Gemfile.lock").write <<~EOS
+      GEM
+        remote: https://rubygems.org/
+        specs:
+          hello (0.0.1)
+      PLATFORMS
+        arm64-darwin-22
+      DEPENDENCIES
+        hello
+      BUNDLED WITH
+        2.4.12
+    EOS
+
+    assert_match "BOM includes 1 components and 0 dependencies", shell_output("#{bin}/cdxgen")
+
+    assert_match version.to_s, shell_output("#{bin}/cdxgen --version")
+  end
+end
