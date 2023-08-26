3f9331b1b0f169ce91066224eaa4b5e68a1e4fc9
Curtis Myzie <curtis.myzie@gmail.com>
2023-08-25 22:59:10 -0400

    risor 0.15.0
    Update risor formula to build in cmd/risor subdirectory

--- Formula/r/risor.rb
+++ Formula/r/risor.rb
@@ -1,8 +1,8 @@
 class Risor < Formula
   desc "Fast and flexible scripting for Go developers and DevOps"
   homepage "https://risor.io/"
-  url "https://github.com/risor-io/risor/archive/refs/tags/v0.14.0.tar.gz"
-  sha256 "9ae044bbb63a7a4d51fc561d946f6622505d43c219118ecaf3e275d97fb8ab97"
+  url "https://github.com/risor-io/risor/archive/refs/tags/v0.15.0.tar.gz"
+  sha256 "594fb818f4d994d9e6d898143eef5d9c606d4cac196d66e764e616d3dde200c3"
   license "Apache-2.0"
   head "https://github.com/risor-io/risor.git", branch: "main"
 
@@ -19,16 +19,17 @@ class Risor < Formula
   depends_on "go" => :build
 
   def install
-    ldflags = "-s -w -X main.version=#{version}"
-    system "go", "build", "-tags", "aws", *std_go_args(ldflags: ldflags), "./cmd/risor"
-
-    generate_completions_from_executable(bin/"risor", "completion")
+    chdir "cmd/risor" do
+      ldflags = "-s -w -X 'main.version=#{version}' -X 'main.date=#{time.iso8601}'"
+      system "go", "build", "-tags", "aws", *std_go_args(ldflags: ldflags), "."
+      generate_completions_from_executable(bin/"risor", "completion")
+    end
   end
 
   test do
     output = shell_output("#{bin}/risor -c \"time.now()\"")
     assert_match(/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}/, output)
-
     assert_match version.to_s, shell_output("#{bin}/risor version")
+    assert_match "module(aws)", shell_output("#{bin}/risor -c aws")
   end
 end
