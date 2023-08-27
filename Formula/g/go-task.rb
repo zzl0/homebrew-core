203a62300e950f33bfda73aec13aea273b0d3d57
Rui Chen <rui@chenrui.dev>
2023-08-26 20:35:39 -0400

    go-task 3.29.1

--- Formula/g/go-task.rb
+++ Formula/g/go-task.rb
@@ -1,8 +1,8 @@
 class GoTask < Formula
   desc "Task is a task runner/build tool that aims to be simpler and easier to use"
   homepage "https://taskfile.dev/"
-  url "https://github.com/go-task/task/archive/refs/tags/v3.28.0.tar.gz"
-  sha256 "3627c72b9a8f21568793e6d9b74646c61fb23a55fe2abed285194f37f21c39ff"
+  url "https://github.com/go-task/task/archive/refs/tags/v3.29.1.tar.gz"
+  sha256 "689cd24f2e6af6459bed95f634ab8b50a2344a4f097e69ff1df7027bbc493cfa"
   license "MIT"
   head "https://github.com/go-task/task.git", branch: "main"
 
