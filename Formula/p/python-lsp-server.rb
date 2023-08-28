1dea36eb56927c35ce1c6219742dcfd70d5e6b23
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-08-28 19:34:15 +0000

    python-lsp-server: update 1.7.4 bottle.

--- Formula/p/python-lsp-server.rb
+++ Formula/p/python-lsp-server.rb
@@ -9,14 +9,14 @@ class PythonLspServer < Formula
   head "https://github.com/python-lsp/python-lsp-server.git", branch: "develop"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "eb8b37a15eb903e65accb806f4e65dc032cd6ef7e42f8bbd6c7f0e7053c49513"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5d37a5d42959c0188598f2fe3b01477ecd46cbbc18b70dfde7cfa9ccf42c8007"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "44126dac9bf0d9f1eaba24cd6cc536dc2288ab9b5a8a909ab5295070bcc4231e"
-    sha256 cellar: :any_skip_relocation, ventura:        "d20862919fe83b44cebba195404d65de0b0ba64747f66486d58e9f19e3216737"
-    sha256 cellar: :any_skip_relocation, monterey:       "96a67a2fe928a61e20bc40cf5f57c1098d40a6216d3d9fef7cd2cbe8c82f25ff"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f09d550a792b0a8a1f8c041f90c4211a82e910ee05fe591a4c65752ad4364265"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6dc3f895a332b33e5d6d6c5badecc2571dd80f411a431584a6e6794746fbcc6e"
+    rebuild 2
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a7fb96234f1dc5f4b88e6b032895defc2665e465b9e9cbd5858626a02a96e04d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "47ed95806d4e60f9d700bc9bcff6d48397644537d9d5e1a2eaa3e1fc8e327cfd"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "28658ff59eee7650d9254a647d67cc35ee488793c86d75035d84502c54539c00"
+    sha256 cellar: :any_skip_relocation, ventura:        "48cb325d02a72f9273b741c86659f8926a35bc9ec06b94b9fededd0e6ca29954"
+    sha256 cellar: :any_skip_relocation, monterey:       "81f75b30fafbeb3bbcb786ed262395b260e05fcca6482c69fbc66d15f085ab02"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f0cae81c122af9e5eda5cce18fb14bd5645a3d4260edb630609a7ea6b9bbbb59"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "310457c42ce3d8d0154782108dfc73b7fdae789d0317f4532a87a45c162a4840"
   end
 
   depends_on "black"
