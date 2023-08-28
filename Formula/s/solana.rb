f1d3aa8ca132a6c888c7238e2b72914164ca91a4
Patrick Linnane <patrick@linnane.io>
2023-08-28 11:54:14 -0700

    solana 1.14.25

--- Formula/s/solana.rb
+++ Formula/s/solana.rb
@@ -1,8 +1,8 @@
 class Solana < Formula
   desc "Web-Scale Blockchain for decentralized apps and marketplaces"
   homepage "https://solana.com"
-  url "https://github.com/solana-labs/solana/archive/v1.14.24.tar.gz"
-  sha256 "1ca45518f62e4884c9b30fa5682f47d9874f8d0bb2234ca2d67ef0a9370a724f"
+  url "https://github.com/solana-labs/solana/archive/v1.14.25.tar.gz"
+  sha256 "c181d080e95154be4a8a163dde8b34f00be577e33294f9dc4b8c93f03ca3ecd9"
   license "Apache-2.0"
   version_scheme 1
 
