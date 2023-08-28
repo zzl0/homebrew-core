5760ca20be5805dd53c136f48912a3a9171b3c53
Porkepix <porkepix@gmail.com>
2023-08-28 22:37:05 +0200

    ada-url 2.6.4

--- Formula/a/ada-url.rb
+++ Formula/a/ada-url.rb
@@ -1,8 +1,8 @@
 class AdaUrl < Formula
   desc "WHATWG-compliant and fast URL parser written in modern C++"
   homepage "https://github.com/ada-url/ada"
-  url "https://github.com/ada-url/ada/archive/refs/tags/v2.6.3.tar.gz"
-  sha256 "5599388dd6f380bb308f84539b1a5f01b23799c0ca0056253a5d97a03ba99b33"
+  url "https://github.com/ada-url/ada/archive/refs/tags/v2.6.4.tar.gz"
+  sha256 "5b488e9a7a700de5d40a749c96c4339bcc9c425e5f5406a0887b13e70bd90907"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/ada-url/ada.git", branch: "main"
 
