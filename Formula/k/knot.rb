73b51f80381b6d218b021857ffabd6742393b6f9
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-08-28 15:35:08 +0800

    knot 3.3.0

--- Formula/k/knot.rb
+++ Formula/k/knot.rb
@@ -1,8 +1,8 @@
 class Knot < Formula
   desc "High-performance authoritative-only DNS server"
   homepage "https://www.knot-dns.cz/"
-  url "https://secure.nic.cz/files/knot-dns/knot-3.2.9.tar.xz"
-  sha256 "bc1f9eb8c9f67f52805f3acfa2d0153190245fa145b007fafa9068d2da292506"
+  url "https://secure.nic.cz/files/knot-dns/knot-3.3.0.tar.xz"
+  sha256 "cf12ab736c512eb719a221cd3f65bb94f93ff2b477803d9474d1334af73c1533"
   license all_of: ["GPL-3.0-or-later", "0BSD", "BSD-3-Clause", "LGPL-2.0-or-later", "MIT"]
 
   livecheck do
