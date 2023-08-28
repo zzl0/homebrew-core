65c1738cf26ab754ca9da4ca15b48d1c67b34178
Rui Chen <rui@chenrui.dev>
2023-08-28 11:35:45 -0400

    tccutil: depends_on python-packaging
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/t/tccutil.rb
+++ Formula/t/tccutil.rb
@@ -18,34 +18,16 @@ class Tccutil < Formula
   end
 
   depends_on :macos
+  depends_on "python-packaging"
   depends_on "python@3.11"
-
-  resource "flit-core" do
-    url "https://files.pythonhosted.org/packages/c4/e6/c1ac50fe3eebb38a155155711e6e864e254ce4b6e17fe2429b4c4d5b9e80/flit_core-3.9.0.tar.gz"
-    sha256 "72ad266176c4a3fcfab5f2930d76896059851240570ce9a98733b658cb786eba"
-  end
-
-  resource "packaging" do
-    url "https://files.pythonhosted.org/packages/b9/6c/7c6658d258d7971c5eb0d9b69fa9265879ec9a9158031206d47800ae2213/packaging-23.1.tar.gz"
-    sha256 "a392980d2b6cffa644431898be54b0045151319d1e7ec34f0cfed48767dd334f"
-  end
 
   def python3
     which("python3.11")
   end
 
   def install
-    ENV.prepend_create_path "PYTHONPATH", libexec/"vendor"/Language::Python.site_packages(python3)
-
-    resources.each do |r|
-      r.stage do
-        system python3, "-m", "pip", "install", *std_pip_args(prefix: libexec/"vendor"), "."
-      end
-    end
-
     rewrite_shebang detected_python_shebang, "tccutil.py"
     bin.install "tccutil.py" => "tccutil"
-    bin.env_script_all_files(libexec/"bin", PYTHONPATH: ENV["PYTHONPATH"])
   end
 
   test do
