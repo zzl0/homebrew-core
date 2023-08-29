3c4d933917d79bdd53a0d8276803cdb1d6bdb094
Rui Chen <rui@chenrui.dev>
2023-08-29 12:35:13 -0400

    kumactl 2.4.0

--- Formula/k/kumactl.rb
+++ Formula/k/kumactl.rb
@@ -1,8 +1,8 @@
 class Kumactl < Formula
   desc "Kuma control plane command-line utility"
   homepage "https://kuma.io/"
-  url "https://github.com/kumahq/kuma/archive/2.3.2.tar.gz"
-  sha256 "b1004314201ab58432c65546d605d24c7b391f4f9c15e7ab6d64019a0a766a32"
+  url "https://github.com/kumahq/kuma/archive/2.4.0.tar.gz"
+  sha256 "dcc6c225667f4535cde7516de11bc4908300c746bd4ea23a44c10a7807df380a"
   license "Apache-2.0"
 
   livecheck do
