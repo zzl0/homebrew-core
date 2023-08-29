6f41f24f95075dd9afc46f2fa7981d279cfec77a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-08-29 08:31:57 +0000

    taskwarrior-tui: update 0.25.4 bottle.

--- Formula/t/taskwarrior-tui.rb
+++ Formula/t/taskwarrior-tui.rb
@@ -12,13 +12,13 @@ class TaskwarriorTui < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "fe752cc0b823b16572dca11fa5d8e153b964faf02f4beb8360762717546e08af"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ba66301851aa3e0c314d77611cc013e4add8045a1c5d7ae7ce0f8437e74be888"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "aa90e65b4317226a431703102587905a85cb49391e979d541cb839f0ec5c43d4"
-    sha256 cellar: :any_skip_relocation, ventura:        "b8d8ab0e12d990f8820cbadda47bd975a208924731818bfc6a9a6fb996b7982c"
-    sha256 cellar: :any_skip_relocation, monterey:       "243ba9a2ebf270ca0325844859a03b3bbbf794906b6c1f4dc6b85e3a0e04f4cd"
-    sha256 cellar: :any_skip_relocation, big_sur:        "c5ac29a968ca11333590b1623f41d8034e7032fe01c80451235389713f904cdf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3a56378236be078ad3c1bd98c7abe8d39f06be08261818fa4c8e3de08214e0da"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "457aab331a09b05ca38e086cf6ac6f94cc3a74639def5facae37a8aae6879d9b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "016062bd3a4e1e2dd49c758c188f3b98d645133a6b94f14ae4b891c0f4784af4"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c143f36d7a7837f01396e6c7e65daa9e889ca2b7afa9dbad49c2e150d095ce99"
+    sha256 cellar: :any_skip_relocation, ventura:        "a0db6f8e4d3c0b392b71769892e24d901b771ff4b3442fc8bccde6cc6ea44ad0"
+    sha256 cellar: :any_skip_relocation, monterey:       "6563f8095c8f722f091ad25f1f15d92be1bee9435360ff6a5595d3855de1debf"
+    sha256 cellar: :any_skip_relocation, big_sur:        "1fd7513cc297c3f57abea26dfb63d9a02daaebc4019fef42345f8a49d72f378e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bd1a31692755b7c9ea1047f274c3de4b403ac38041db3cd8ca3bedd83aa69dfa"
   end
 
   depends_on "rust" => :build
