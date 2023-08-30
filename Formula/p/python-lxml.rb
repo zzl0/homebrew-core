58f7a94c074e99ff9adb2fc998d5305759d1c782
Rui Chen <rui@chenrui.dev>
2023-08-30 19:46:10 -0400

    python-lxml 4.9.3 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    python-lxml: needs libxml2 and libxslt
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/p/python-lxml.rb
@@ -0,0 +1,32 @@
+class PythonLxml < Formula
+  desc "Pythonic binding for the libxml2 and libxslt libraries"
+  homepage "https://github.com/lxml/lxml"
+  url "https://files.pythonhosted.org/packages/30/39/7305428d1c4f28282a4f5bdbef24e0f905d351f34cf351ceb131f5cddf78/lxml-4.9.3.tar.gz"
+  sha256 "48628bd53a426c9eb9bc066a923acaa0878d1e86129fd5359aee99285f4eed9c"
+  license "BSD-3-Clause"
+
+  depends_on "python@3.10" => [:build, :test]
+  depends_on "python@3.11" => [:build, :test]
+  depends_on "python@3.9" => [:build, :test]
+
+  uses_from_macos "libxml2"
+  uses_from_macos "libxslt"
+
+  def pythons
+    deps.map(&:to_formula).sort_by(&:version).filter { |f| f.name.start_with?("python@") }
+  end
+
+  def install
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-m", "pip", "install", *std_pip_args, "."
+    end
+  end
+
+  test do
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-c", "import lxml"
+    end
+  end
+end
