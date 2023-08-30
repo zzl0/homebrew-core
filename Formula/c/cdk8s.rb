2874a6bbc18f839f6f9e70bc27db80d92596147f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-08-30 02:25:56 +0000

    cdk8s: update 2.66.0 bottle.

--- Formula/c/cdk8s.rb
+++ Formula/c/cdk8s.rb
@@ -7,13 +7,13 @@ class Cdk8s < Formula
   sha256 "5dca994058524e70816233f7dc86667ee721511bf56ab1357d3d4492b81f99b6"
   license "Apache-2.0"
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "2368d864c73414b60d356f07262f7c550a851e42ccc05ebf5d3aec25809fc21a"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "2368d864c73414b60d356f07262f7c550a851e42ccc05ebf5d3aec25809fc21a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2368d864c73414b60d356f07262f7c550a851e42ccc05ebf5d3aec25809fc21a"
-    sha256 cellar: :any_skip_relocation, ventura:        "7f6b919a091aa7865756c4122a3118fab7718046a958689669908d5fda70740f"
-    sha256 cellar: :any_skip_relocation, monterey:       "7f6b919a091aa7865756c4122a3118fab7718046a958689669908d5fda70740f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "7f6b919a091aa7865756c4122a3118fab7718046a958689669908d5fda70740f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2368d864c73414b60d356f07262f7c550a851e42ccc05ebf5d3aec25809fc21a"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "bf13fad26ba9c8095c15df715f94a123cf36999107f8fe8408b7cf44fa7c76f9"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "bf13fad26ba9c8095c15df715f94a123cf36999107f8fe8408b7cf44fa7c76f9"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bf13fad26ba9c8095c15df715f94a123cf36999107f8fe8408b7cf44fa7c76f9"
+    sha256 cellar: :any_skip_relocation, ventura:        "de1966a22d01a4ad81cc41a2319b4f37f17e34091a6bcc957518093607ed5050"
+    sha256 cellar: :any_skip_relocation, monterey:       "de1966a22d01a4ad81cc41a2319b4f37f17e34091a6bcc957518093607ed5050"
+    sha256 cellar: :any_skip_relocation, big_sur:        "de1966a22d01a4ad81cc41a2319b4f37f17e34091a6bcc957518093607ed5050"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bf13fad26ba9c8095c15df715f94a123cf36999107f8fe8408b7cf44fa7c76f9"
   end
 
   depends_on "node"
