acee82abe7ab67973b2f2b267570914ed641f214
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-08-30 01:43:14 +0000

    kubectx: update 0.9.5 bottle.

--- Formula/k/kubectx.rb
+++ Formula/k/kubectx.rb
@@ -7,7 +7,8 @@ class Kubectx < Formula
   head "https://github.com/ahmetb/kubectx.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "41069e2e7ae9d4d36a5a4ec659641239d02d60ad8102c140126a2bae0c46c29e"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, all: "605e0eab94d6af055cc25cc91c6bda597d360e55fb7b8cd0d6d2aa678a991782"
   end
 
   depends_on "kubernetes-cli"
