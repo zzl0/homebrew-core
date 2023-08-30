0d8120c1799b9987248bcf683d6686c40ea8632b
Rui Chen <rui@chenrui.dev>
2023-08-30 19:54:12 -0400

    python-markupsafe 2.1.3 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/p/python-markupsafe.rb
@@ -0,0 +1,29 @@
+class PythonMarkupsafe < Formula
+  desc "Implements a XML/HTML/XHTML Markup safe string for Python"
+  homepage "https://palletsprojects.com/p/markupsafe/"
+  url "https://files.pythonhosted.org/packages/6d/7c/59a3248f411813f8ccba92a55feaac4bf360d29e2ff05ee7d8e1ef2d7dbf/MarkupSafe-2.1.3.tar.gz"
+  sha256 "af598ed32d6ae86f1b747b82783958b1a4ab8f617b06fe68795c7f026abbdcad"
+  license "BSD-3-Clause"
+
+  depends_on "python@3.10" => [:build, :test]
+  depends_on "python@3.11" => [:build, :test]
+  depends_on "python@3.9" => [:build, :test]
+
+  def pythons
+    deps.map(&:to_formula).sort_by(&:version).filter { |f| f.name.start_with?("python@") }
+  end
+
+  def install
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-m", "pip", "install", *std_pip_args, "."
+    end
+  end
+
+  test do
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-c", "from markupsafe import escape"
+    end
+  end
+end
