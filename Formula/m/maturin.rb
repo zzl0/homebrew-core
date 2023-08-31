56f062edd384ba77c59a1c234f7eefb0c4262539
Rui Chen <rui@chenrui.dev>
2023-08-31 16:50:15 -0400

    maturin: install python bindings for pep517 builds
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/m/maturin.rb
+++ Formula/m/maturin.rb
@@ -16,20 +16,57 @@ class Maturin < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "75672cf4f41c9fa3814a499065b6aed41a2a7001fbd9a194586063c229f25bcd"
   end
 
-  depends_on "python@3.11" => :test
+  depends_on "python-flit-core" => :build
+  depends_on "python-typing-extensions" => :build
+  depends_on "python@3.10" => [:build, :test]
+  depends_on "python@3.11" => [:build, :test]
   depends_on "rust"
 
-  def install
-    system "cargo", "install", *std_cargo_args
+  resource "semantic-version" do
+    url "https://files.pythonhosted.org/packages/7d/31/f2289ce78b9b473d582568c234e104d2a342fd658cc288a7553d83bb8595/semantic_version-2.10.0.tar.gz"
+    sha256 "bdabb6d336998cbb378d4b9db3a4b56a1e3235701dc05ea2690d9a997ed5041c"
+  end
 
+  resource "setuptools-rust" do
+    url "https://files.pythonhosted.org/packages/90/f1/70b31cacce03bf21fa645d359d6303fb5590c1a02c41c7e2df1c480826b4/setuptools-rust-1.7.0.tar.gz"
+    sha256 "c7100999948235a38ae7e555fe199aa66c253dc384b125f5d85473bf81eae3a3"
+  end
+
+  resource "tomli" do
+    url "https://files.pythonhosted.org/packages/c0/3f/d7af728f075fb08564c5949a9c95e44352e23dee646869fa104a3b2060a3/tomli-2.0.1.tar.gz"
+    sha256 "de526c12914f0c550d15924c62d72abc48d6fe7364aa87328337a31007fe8a4f"
+  end
+
+  def pythons
+    deps.map(&:to_formula)
+        .filter { |f| f.name.start_with?("python@") }
+        .sort_by(&:version)
+        .map { |f| f.opt_libexec/"bin/python" }
+  end
+
+  def install
+    pythons.each do |python|
+      ENV.append_path "PYTHONPATH", buildpath/Language::Python.site_packages(python)
+      resources.each do |r|
+        r.stage do
+          system python, "-m", "pip", "install", *std_pip_args(prefix: buildpath), "."
+        end
+      end
+
+      system python, "-m", "pip", "install", *std_pip_args, "."
+    end
+
+    # overwrite the minimal binary that pip installed
+    system "cargo", "install", *std_cargo_args, "--force"
     generate_completions_from_executable(bin/"maturin", "completions")
   end
 
   test do
-    python = Formula["python@3.11"].opt_bin/"python3.11"
     system "cargo", "new", "hello_world", "--bin"
     system bin/"maturin", "build", "-m", "hello_world/Cargo.toml", "-b", "bin", "-o", "dist", "--compatibility", "off"
-    system python, "-m", "pip", "install", "hello_world", "--no-index", "--find-links", testpath/"dist"
-    system python, "-m", "pip", "uninstall", "-y", "hello_world"
+    pythons.each do |python|
+      system python, "-m", "pip", "install", "hello_world", "--no-index", "--find-links", testpath/"dist"
+      system python, "-m", "pip", "uninstall", "-y", "hello_world"
+    end
   end
 end
