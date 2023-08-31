b2f255abd3de825892e0bca995a06d53946cc0d0
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-08-31 13:35:36 +0800

    ctl: revision bump (openexr 3.2.0)

--- Formula/c/ctl.rb
+++ Formula/c/ctl.rb
@@ -2,7 +2,7 @@ class Ctl < Formula
   desc "Programming language for digital color management"
   homepage "https://github.com/ampas/CTL"
   license "AMPAS"
-  revision 11
+  revision 12
   head "https://github.com/ampas/CTL.git", branch: "master"
 
   stable do
