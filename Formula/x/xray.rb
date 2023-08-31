36ee59e497e87c7fe2a2ff244f8f6f48f2538e13
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-08-31 16:49:44 +0000

    xray: update 1.8.4 bottle.

--- Formula/x/xray.rb
+++ Formula/x/xray.rb
@@ -12,14 +12,13 @@ class Xray < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "02a0def74264e8b6308697d83cd59b2091994d212744c956a54eecad23d67673"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "02a0def74264e8b6308697d83cd59b2091994d212744c956a54eecad23d67673"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "02a0def74264e8b6308697d83cd59b2091994d212744c956a54eecad23d67673"
-    sha256 cellar: :any_skip_relocation, ventura:        "173fd8e1c080f4863376842cfb8fb978dc18883242c8434627380fbc1a6f6801"
-    sha256 cellar: :any_skip_relocation, monterey:       "173fd8e1c080f4863376842cfb8fb978dc18883242c8434627380fbc1a6f6801"
-    sha256 cellar: :any_skip_relocation, big_sur:        "173fd8e1c080f4863376842cfb8fb978dc18883242c8434627380fbc1a6f6801"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9de81685475b5e01cdb209d2f0a22fbb50a57cdd2082f993c50d28a32dc0e685"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "88c6074eeafa8f89e58c51bbd7325faaab9bce2003e6856958bb33f0ed570fc3"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "4b6a3e15ea99c4ac4701d3ea08c3681325c11572f3dd7f8039ea8c0b9ddf7d27"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4d7d5700cd773731f28f11ed293ee53e1cfa694f72290ae1dad86508f01af94d"
+    sha256 cellar: :any_skip_relocation, ventura:        "ff889d4a2cdc1b94111ce29437236ee1b3621897ee963922b34d5fb19c5bfe95"
+    sha256 cellar: :any_skip_relocation, monterey:       "188d740801ee78460ddc0f6fe17f067b8c9c5b15bd03866a757bb160907ea6ed"
+    sha256 cellar: :any_skip_relocation, big_sur:        "0237fb0565cb82e311288d24f149731ffb9e8e2fd9fbdb238ea05e7c967eccb9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "48e5ec5166a75795baa3ae51bcf6d94eec65f3585cd2214b2c0b7c79b28e477c"
   end
 
   depends_on "go" => :build
