12692b1a27c14e0f381f5e2b48f865d81e366692
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-08-31 02:36:15 +0000

    python-lxml: add 4.9.3 bottle.

--- Formula/p/python-lxml.rb
+++ Formula/p/python-lxml.rb
@@ -5,6 +5,16 @@ class PythonLxml < Formula
   sha256 "48628bd53a426c9eb9bc066a923acaa0878d1e86129fd5359aee99285f4eed9c"
   license "BSD-3-Clause"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "5521a0623aeb08d39b0ea5554f9678a8afba93023ae0db4fcdfd11cc0bdc28ba"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "1b42a0e7770a74372326f20a1053de97998b58d3f41468cf61ebe71117a29a02"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fd48b0b9528659465d5e38eb1f4b4b1632f285ec8861240059c49489ef938e30"
+    sha256 cellar: :any_skip_relocation, ventura:        "eb23c25272811f76bbafae511cf6719d5e965328c9f93f76ea01245b1ae71b89"
+    sha256 cellar: :any_skip_relocation, monterey:       "c3558c84308ef907e2e000b974e35afbef07e4eb272f10ed27aea186cd030a8e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "7994ef91def9ff96c3146d270ab6224adb45959afd8c6e33152dceff8ebab1f5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9ea4c4dabb7b024ff08a7ff531de53948e73d06b79b0da31ea3281c052106574"
+  end
+
   depends_on "python@3.10" => [:build, :test]
   depends_on "python@3.11" => [:build, :test]
   depends_on "python@3.9" => [:build, :test]
