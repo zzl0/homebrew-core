2ff68b4125a2dde48be271158a1923c5f5d5279d
Rui Chen <rui@chenrui.dev>
2023-08-31 10:59:02 -0400

    python-pytz 2023.3 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/p/python-pytz.rb
@@ -0,0 +1,29 @@
+class PythonPytz < Formula
+  desc "Python library for cross platform timezone"
+  homepage "https://pythonhosted.org/pytz/"
+  url "https://files.pythonhosted.org/packages/5e/32/12032aa8c673ee16707a9b6cdda2b09c0089131f35af55d443b6a9c69c1d/pytz-2023.3.tar.gz"
+  sha256 "1d8ce29db189191fb55338ee6d0387d82ab59f3d00eac103412d64e0ebd0c588"
+  license "MIT"
+
+  depends_on "python@3.10" => [:build, :test]
+  depends_on "python@3.11" => [:build, :test]
+  depends_on "python@3.9" => [:build, :test]
+
+  def pythons
+    deps.map(&:to_formula).sort_by(&:version).filter { |f| f.name.start_with?("python@") }
+  end
+
+  def install
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-m", "pip", "install", *std_pip_args, "."
+    end
+  end
+
+  test do
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-c", "import pytz; print(pytz.timezone('UTC'))"
+    end
+  end
+end
