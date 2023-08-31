0163efcf2d9b1f77d0cb30d6220701afd69be152
Rui Chen <rui@chenrui.dev>
2023-08-31 10:47:39 -0400

    python-pyparsing 3.1.1 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    python-pyparsing: needs python-flit-core for build
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/p/python-pyparsing.rb
@@ -0,0 +1,30 @@
+class PythonPyparsing < Formula
+  desc "Python library for creating PEG parsers"
+  homepage "https://click.palletsprojects.com/"
+  url "https://files.pythonhosted.org/packages/37/fe/65c989f70bd630b589adfbbcd6ed238af22319e90f059946c26b4835e44b/pyparsing-3.1.1.tar.gz"
+  sha256 "ede28a1a32462f5a9705e07aea48001a08f7cf81a021585011deba701581a0db"
+  license "MIT"
+
+  depends_on "python-flit-core" => :build
+  depends_on "python@3.10" => [:build, :test]
+  depends_on "python@3.11" => [:build, :test]
+  depends_on "python@3.9" => [:build, :test]
+
+  def pythons
+    deps.map(&:to_formula).sort_by(&:version).filter { |f| f.name.start_with?("python@") }
+  end
+
+  def install
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-m", "pip", "install", *std_pip_args, "."
+    end
+  end
+
+  test do
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-c", "import pyparsing"
+    end
+  end
+end
