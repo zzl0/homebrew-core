cb5ad96c823dcf73ace5c85cd0f064aab58f2fb9
Rui Chen <rui@chenrui.dev>
2023-08-31 02:13:45 -0400

    python-click 8.1.7 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/p/python-click.rb
@@ -0,0 +1,29 @@
+class PythonClick < Formula
+  desc "Python composable command-line interface toolkit"
+  homepage "https://click.palletsprojects.com/"
+  url "https://files.pythonhosted.org/packages/96/d3/f04c7bfcf5c1862a2a5b845c6b2b360488cf47af55dfa79c98f6a6bf98b5/click-8.1.7.tar.gz"
+  sha256 "ca9853ad459e787e2192211578cc907e7594e294c7ccc834310722b41b9ca6de"
+  license "BSD-3-Clause"
+
+  depends_on "python@3.10" => [:build, :test]
+  depends_on "python@3.11" => [:build, :test]
+  depends_on "python@3.9" => [:build, :test]
+
+  def pythons
+    deps.map(&:to_formula).sort_by(&:version).filter { |f| f.name.start_with?("python@") }
+  end
+
+  def install
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-m", "pip", "install", *std_pip_args, "."
+    end
+  end
+
+  test do
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-c", "import click, click.shell_completion"
+    end
+  end
+end
