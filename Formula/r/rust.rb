e2e973472a02c5a3b6a5a78bdeea4c2c9d4e9625
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-08-31 13:45:33 +0800

    rust: remove conflicts with `rust-analyzer` and `rustfmt`
    `rust` no longer ships `rust-analyzer` and `rustfmt` since #140367.

--- Formula/r/rust.rb
+++ Formula/r/rust.rb
@@ -55,9 +55,6 @@ class Rust < Formula
   uses_from_macos "curl"
   uses_from_macos "zlib"
 
-  conflicts_with "rust-analyzer", because: "both install `rust-analyzer` binary"
-  conflicts_with "rustfmt", because: "both install `cargo-fmt` and `rustfmt` binaries"
-
   # From https://github.com/rust-lang/rust/blob/#{version}/src/stage0.json
   resource "cargobootstrap" do
     on_macos do
