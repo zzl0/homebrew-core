b26d45738c4c9467b77e4fa045ea40e0a90a0500
Rui Chen <rui@chenrui.dev>
2023-08-31 18:03:58 -0400

    python-pyproject-hooks 1.0.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    python-pyproject-hooks: needs python-flit-core for build
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/p/python-pyproject-hooks.rb
@@ -0,0 +1,30 @@
+class PythonPyprojectHooks < Formula
+  desc "Low-level library for calling build-backends in pyproject.toml-based project"
+  homepage "https://pyproject-hooks.readthedocs.io/en/latest/"
+  url "https://files.pythonhosted.org/packages/25/c1/374304b8407d3818f7025457b7366c8e07768377ce12edfe2aa58aa0f64c/pyproject_hooks-1.0.0.tar.gz"
+  sha256 "f271b298b97f5955d53fb12b72c1fb1948c22c1a6b70b315c54cedaca0264ef5"
+  license "MIT"
+
+  depends_on "python-flit-core" => :build
+  depends_on "python@3.10" => [:build, :test]
+  depends_on "python@3.11" => [:build, :test]
+  depends_on "python@3.9" => [:build, :test]
+
+  def pythons
+    deps.map(&:to_formula).sort_by(&:version).filter { |f| f.name.start_with?("python@") }
+  end
+
+  def install
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-m", "pip", "install", *std_pip_args, "."
+    end
+  end
+
+  test do
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-c", "import pyproject_hooks"
+    end
+  end
+end
