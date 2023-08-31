573513436d2f9f9dc877e9769da63f1e060f99e2
Rui Chen <rui@chenrui.dev>
2023-08-31 19:27:30 -0400

    mat2 0.13.4
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/m/mat2.rb
+++ Formula/m/mat2.rb
@@ -1,8 +1,8 @@
 class Mat2 < Formula
   desc "Metadata anonymization toolkit"
   homepage "https://0xacab.org/jvoisin/mat2"
-  url "https://0xacab.org/jvoisin/mat2/-/archive/0.13.3/mat2-0.13.3.tar.gz"
-  sha256 "e1bb0161fa1c2f2adb1b933761f9569534309e90209568f96aca5fc8a74f511a"
+  url "https://files.pythonhosted.org/packages/d5/e4/f02d057fe6cf32b68e402c5f86276244105da40161e84ef785b2ae0bf809/mat2-0.13.4.tar.gz"
+  sha256 "744aeee924c9898a397fe930593b803c540389bf39cd24553b99a89acc2f5901"
   license "LGPL-3.0-or-later"
 
   bottle do
@@ -16,35 +16,22 @@ class Mat2 < Formula
   depends_on "poppler"
   depends_on "py3cairo"
   depends_on "pygobject3"
+  depends_on "python-mutagen"
   depends_on "python@3.11"
 
-  resource "mutagen" do
-    url "https://files.pythonhosted.org/packages/b1/54/d1760a363d0fe345528e37782f6c18123b0e99e8ea755022fd51f1ecd0f9/mutagen-1.46.0.tar.gz"
-    sha256 "6e5f8ba84836b99fe60be5fb27f84be4ad919bbb6b49caa6ae81e70584b55e58"
+  # patch man1 file layout, remove in next release
+  # https://0xacab.org/jvoisin/mat2/-/merge_requests/111
+  patch do
+    url "https://0xacab.org/jvoisin/mat2/-/commit/406924bb6164384fe0a8a8f3dc8dfe7d15577cfc.diff"
+    sha256 "4c1c57ca8fe1eabea41d66f3ef9bd4eb2bac8ac181fceeefece4b92b5be9658d"
   end
 
-  # Fix pyproject.toml config, remove in next release
-  # https://0xacab.org/jvoisin/mat2/-/merge_requests/104
-  patch do
-    url "https://0xacab.org/jvoisin/mat2/-/commit/ed0ffa5693b6d710b19a9101d6cb0625f8b6f1fc.diff"
-    sha256 "b067ede2688af82b77438bf60891356f1aa5b33e78208e1f3ea485570626cbe2"
+  def python3
+    which("python3.11")
   end
 
   def install
-    python = "python3.11"
-
-    ENV.append_path "PYTHONPATH", prefix/Language::Python.site_packages(python)
-    ENV.append_path "PYTHONPATH", Formula["pygobject3"].opt_prefix/Language::Python.site_packages(python)
-    ENV.prepend_create_path "PYTHONPATH", libexec/"vendor"/Language::Python.site_packages(python)
-
-    resources.each do |r|
-      r.stage do
-        system python, *Language::Python.setup_install_args(libexec/"vendor", python), "--install-data=#{prefix}"
-      end
-    end
-
-    system python, *Language::Python.setup_install_args(prefix, python)
-    bin.env_script_all_files(libexec/"bin", PYTHONPATH: ENV["PYTHONPATH"])
+    system python3, "-m", "pip", "install", *std_pip_args, "."
   end
 
   test do
