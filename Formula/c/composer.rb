ece21cac4ec337941e4495e2f055735ab615a134
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-01 10:26:00 +0000

    composer: update 2.6.0 bottle.

--- Formula/c/composer.rb
+++ Formula/c/composer.rb
@@ -11,13 +11,13 @@ class Composer < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "e07159116be2cb513cd08201b92c084cff093b78c115c1a00518a454aab9f309"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e07159116be2cb513cd08201b92c084cff093b78c115c1a00518a454aab9f309"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e07159116be2cb513cd08201b92c084cff093b78c115c1a00518a454aab9f309"
-    sha256 cellar: :any_skip_relocation, ventura:        "9deba5a92bfacce1788651462f34514001c81daee7d8bef18c88b6d33de6a01c"
-    sha256 cellar: :any_skip_relocation, monterey:       "9deba5a92bfacce1788651462f34514001c81daee7d8bef18c88b6d33de6a01c"
-    sha256 cellar: :any_skip_relocation, big_sur:        "9deba5a92bfacce1788651462f34514001c81daee7d8bef18c88b6d33de6a01c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e07159116be2cb513cd08201b92c084cff093b78c115c1a00518a454aab9f309"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "26334a4d2c3eecdd33758a3c710594ad17a3840b8c03bbf7f287b0054f333729"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "26334a4d2c3eecdd33758a3c710594ad17a3840b8c03bbf7f287b0054f333729"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "26334a4d2c3eecdd33758a3c710594ad17a3840b8c03bbf7f287b0054f333729"
+    sha256 cellar: :any_skip_relocation, ventura:        "37006058236651ec38e9942b633ff9ff656d3a0effe04e75e656bafc36b672f1"
+    sha256 cellar: :any_skip_relocation, monterey:       "37006058236651ec38e9942b633ff9ff656d3a0effe04e75e656bafc36b672f1"
+    sha256 cellar: :any_skip_relocation, big_sur:        "37006058236651ec38e9942b633ff9ff656d3a0effe04e75e656bafc36b672f1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "26334a4d2c3eecdd33758a3c710594ad17a3840b8c03bbf7f287b0054f333729"
   end
 
   depends_on "php"
