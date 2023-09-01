2bd1b5dc714e79cb5ad455b12300e7f19e0bf2ad
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-01 11:41:07 +0000

    hz: update 0.6.7 bottle.

--- Formula/h/hz.rb
+++ Formula/h/hz.rb
@@ -12,13 +12,13 @@ class Hz < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "5f01d3302ac00d72d69c4fb420049fec7ab1f3e97acbab082c49a6f058f04fb6"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5f01d3302ac00d72d69c4fb420049fec7ab1f3e97acbab082c49a6f058f04fb6"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5f01d3302ac00d72d69c4fb420049fec7ab1f3e97acbab082c49a6f058f04fb6"
-    sha256 cellar: :any_skip_relocation, ventura:        "28dc54f8a5ff7edaf95223b92a7326d46961181291fe57d57f8c5aa4cd8b4da2"
-    sha256 cellar: :any_skip_relocation, monterey:       "28dc54f8a5ff7edaf95223b92a7326d46961181291fe57d57f8c5aa4cd8b4da2"
-    sha256 cellar: :any_skip_relocation, big_sur:        "28dc54f8a5ff7edaf95223b92a7326d46961181291fe57d57f8c5aa4cd8b4da2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a91802feee185edbd89ff99a66d8bc74e513735a23a92024b1653b57e24d52af"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "bc1f60475c7ead9bd19e98cf41c4f3ca14ae792bdee8515b5bc81502121a297c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "cfa6e9f1b244afab8759fc6e97c7f227370a52a838b4f357bb3dc6cd0521c14f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "48fd18259e20df689455caf940379d3517da5a379d1a4b07e9de47ca245f7d46"
+    sha256 cellar: :any_skip_relocation, ventura:        "3e19dda489577034e239472212721d6261c441b6734e98b0e29282bf641e4a95"
+    sha256 cellar: :any_skip_relocation, monterey:       "ede14230e97850a10ac4770abd1df71c180d4050cd7a46d3b687d8ea4c698ef2"
+    sha256 cellar: :any_skip_relocation, big_sur:        "79a41ca946eed6ced6d2a77953b6586707ce2851e43e9d7177dd44b47cd198bc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c9c3bab54808b9cc6bbc508dd182678d20594def9b99b771d80528952d43452b"
   end
 
   depends_on "go" => :build
