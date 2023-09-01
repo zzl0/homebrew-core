248c1d4df47090f0d0aa5503732b64d681614adc
Rui Chen <rui@chenrui.dev>
2023-09-01 00:16:50 -0400

    cdk8s 2.70.0

--- Formula/c/cdk8s.rb
+++ Formula/c/cdk8s.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Cdk8s < Formula
   desc "Define k8s native apps and abstractions using object-oriented programming"
   homepage "https://cdk8s.io/"
-  url "https://registry.npmjs.org/cdk8s-cli/-/cdk8s-cli-2.68.1.tgz"
-  sha256 "e2963e8dbab539cbb130ebcd34db736c8c3ea7b436bb6443fd1670f44e06845f"
+  url "https://registry.npmjs.org/cdk8s-cli/-/cdk8s-cli-2.70.0.tgz"
+  sha256 "2973338934750905ee8d51e30c5178905b71e490813e1d6ff8f0dc74e2062ed9"
   license "Apache-2.0"
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "f437ea3f23f748faee7138958bf2760efb4b465f275948952990c7fd2df7da5a"
