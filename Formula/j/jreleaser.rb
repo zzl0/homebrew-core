e91f4f6d5650c36eb55d701bb57a8fc2da647ab9
Rui Chen <rui@chenrui.dev>
2023-09-01 00:38:38 -0400

    jreleaser 1.8.0

--- Formula/j/jreleaser.rb
+++ Formula/j/jreleaser.rb
@@ -1,8 +1,8 @@
 class Jreleaser < Formula
   desc "Release projects quickly and easily with JReleaser"
   homepage "https://jreleaser.org/"
-  url "https://github.com/jreleaser/jreleaser/releases/download/v1.7.0/jreleaser-1.7.0.zip"
-  sha256 "e1c387d9a6c553eb92a7f0dcd3022f803e246fc2ac3fa31b09b5b1fb5b733627"
+  url "https://github.com/jreleaser/jreleaser/releases/download/v1.8.0/jreleaser-1.8.0.zip"
+  sha256 "9b9c0c7b4e07cd058593114111a1ddaaa752937308d85216666f64ce1d588d1e"
   license "Apache-2.0"
 
   bottle do
