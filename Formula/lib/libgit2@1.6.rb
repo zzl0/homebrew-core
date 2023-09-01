625d61fa0df9d872617730d4f65c161e5a464877
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-09-01 21:34:14 +0800

    libgit2@1.6 1.6.4 (new formula)

---
+++ Formula/lib/libgit2@1.6.rb
@@ -0,0 +1,54 @@
+class Libgit2AT16 < Formula
+  desc "C library of Git core methods that is re-entrant and linkable"
+  homepage "https://libgit2.github.com/"
+  url "https://github.com/libgit2/libgit2/archive/refs/tags/v1.6.4.tar.gz"
+  sha256 "d25866a4ee275a64f65be2d9a663680a5cf1ed87b7ee4c534997562c828e500d"
+  license "GPL-2.0-only" => { with: "GCC-exception-2.0" }
+  head "https://github.com/libgit2/libgit2.git", branch: "maint/v1.6"
+
+  livecheck do
+    url :stable
+    regex(/v?(1\.6(?:\.\d+)+)/i)
+  end
+
+  keg_only :versioned_formula
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "libssh2"
+  depends_on "openssl@3"
+
+  def install
+    args = %w[-DBUILD_EXAMPLES=OFF -DBUILD_TESTS=OFF -DUSE_SSH=ON]
+
+    system "cmake", "-S", ".", "-B", "build", "-DBUILD_SHARED_LIBS=ON", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+
+    system "cmake", "-S", ".", "-B", "build-static", "-DBUILD_SHARED_LIBS=OFF", *args, *std_cmake_args
+    system "cmake", "--build", "build-static"
+    lib.install "build-static/libgit2.a"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <git2.h>
+      #include <assert.h>
+
+      int main(int argc, char *argv[]) {
+        int options = git_libgit2_features();
+        assert(options & GIT_FEATURE_SSH);
+        return 0;
+      }
+    EOS
+    libssh2 = Formula["libssh2"]
+    flags = %W[
+      -I#{include}
+      -I#{libssh2.opt_include}
+      -L#{lib}
+      -lgit2
+    ]
+    system ENV.cc, "test.c", "-o", "test", *flags
+    system "./test"
+  end
+end
