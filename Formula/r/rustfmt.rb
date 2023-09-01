76795e8bdf11564586561257792e69f6ab2b0673
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-01 03:49:37 +0000

    rustfmt: update 1.6.0 bottle.

--- Formula/r/rustfmt.rb
+++ Formula/r/rustfmt.rb
@@ -7,15 +7,13 @@ class Rustfmt < Formula
   head "https://github.com/rust-lang/rustfmt.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "ae00013ef65a9ed015c968b8ee9daff7e6147cc5a99c8cdac657ee92f4a8461d"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f3812711969be43a9a8a2636b5aa7a76fdf8f1a4b59301cf3224cf36e1ab3422"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0b44fb4f1512e6932dae71f77992458c47ef45d26efcfe2915c734b692385d19"
-    sha256 cellar: :any_skip_relocation, ventura:        "f35c099a16fd131a423d5a181730d569332963687e9b9d052b1ccda54daa5569"
-    sha256 cellar: :any_skip_relocation, monterey:       "95e6c58fe6e57aab6b4824e79eca7f1985cdd57c30e4d8dbb8bdc6c54b4c57c6"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f1a7a2e82215d39777dd959159ae96d130265f8186e64b15254912b1553e4181"
-    sha256 cellar: :any_skip_relocation, catalina:       "1a2d1361705ac673b214c228679a84ede04c843448eb756ae71f5b1ab3e7db01"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6b770894376d5273adc2e8731db63e00a32ea93b05cad6ab94a401e7aa1a9ead"
+    sha256 cellar: :any,                 arm64_ventura:  "903b03af5ff31da6946b920a87c360779bd7350371f9cf0fc8f1ac01797a22ba"
+    sha256 cellar: :any,                 arm64_monterey: "2d4384367fb918037862e0f4167f31e79a45ab0d4464689782e5e1ef430ea415"
+    sha256 cellar: :any,                 arm64_big_sur:  "2dfac00e73d1798016738b0f3ad4b1479e5e3e0801ef0986718e010d9fb231f3"
+    sha256 cellar: :any,                 ventura:        "2d54800a6dff9d2f158b9e00ec8c59c99e88a17427b0d56887bdfc5067854b56"
+    sha256 cellar: :any,                 monterey:       "72863634fc6b628f2a563194678796b16e2b8e1a46e87297a6b5684026840c40"
+    sha256 cellar: :any,                 big_sur:        "66caede3491722db90a8a53dd758d95a9a52b38df13efa0bcd4ec5a713a784ad"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7177c8e937dd964b87c145479e2bf9c2ef7de15545489faaf2c988357ef4e190"
   end
 
   depends_on "rustup-init" => :build
