d82dd917eb1ac494d20d1e9ede489c8a9a5f9dde
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-01 03:46:47 +0000

    wasmtime: update 12.0.1_1 bottle.

--- Formula/w/wasmtime.rb
+++ Formula/w/wasmtime.rb
@@ -14,13 +14,13 @@ class Wasmtime < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f8277c83802794cd5fa15c04a854666db3d58e275fece2da261c190e26e9efb8"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "02ccf3c08014e704932eab04731374494dbc722682bfb6e6705acdba1b29d8c5"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3698dd0bedcfd4e3684d6a3b961961f7716e1518937e53d237c27b01df9c88e2"
-    sha256 cellar: :any_skip_relocation, ventura:        "bdd22d670e2e686ca28874f4c1bd82d814a41c1e69d1f45719fa174f1160f8a4"
-    sha256 cellar: :any_skip_relocation, monterey:       "1a34c29b3ecd2982611a3e76dde852032ed5d9adae7f878c860c802b96b83f20"
-    sha256 cellar: :any_skip_relocation, big_sur:        "c6166197527167a7b9f63be61caef79518185d070cdb5fce8231e89197bfa5d7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "dc9e13d6742f7bc9a44da193f5cc96d1b65af339ad6463e2f684141d8557d38f"
+    sha256 cellar: :any,                 arm64_ventura:  "55c8800ed2ce5c40d1517b03b5e3d7ca015e4d8fa1b4936b3af9e683faa196ce"
+    sha256 cellar: :any,                 arm64_monterey: "fbc5632b650f437dcec5a08d6099a5dabb9afe27250d206bbbab1e0a70ea359b"
+    sha256 cellar: :any,                 arm64_big_sur:  "ab5ab7b1bf0794c4ecf39481623c4ef65f7495b5bf96ef6f2d000e9ca2282444"
+    sha256 cellar: :any,                 ventura:        "926d231e49af9f8fbae8e978918daee6d2580cc4f101631a4a488992207c5da4"
+    sha256 cellar: :any,                 monterey:       "76fa37ee16b94df2dc707d96a9cefcdfd6846847dd24a01b196e09c67a287be4"
+    sha256 cellar: :any,                 big_sur:        "e265898384167a7ff053109141e766ae7e17b0f07cb4f600d10f3da1485930c4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "24c58b3177a943594139ba100ce0bd96da340fb94e7ec97563513f760c928d99"
   end
 
   depends_on "rust" => :build
