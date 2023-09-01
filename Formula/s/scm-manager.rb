a143f172fe7a805ffacbd0112e3bb2b6747b7bfa
Rui Chen <rui@chenrui.dev>
2023-09-01 10:01:44 -0400

    scm-manager 2.46.1

--- Formula/s/scm-manager.rb
+++ Formula/s/scm-manager.rb
@@ -1,8 +1,8 @@
 class ScmManager < Formula
   desc "Manage Git, Mercurial, and Subversion repos over HTTP"
   homepage "https://www.scm-manager.org"
-  url "https://packages.scm-manager.org/repository/releases/sonia/scm/packaging/unix/2.46.0/unix-2.46.0.tar.gz"
-  sha256 "be3b48b3c76ce42881399d97e512a3002ea3fcb917cf321ccc1f29081057516d"
+  url "https://packages.scm-manager.org/repository/releases/sonia/scm/packaging/unix/2.46.1/unix-2.46.1.tar.gz"
+  sha256 "707240c32a660fb4c7c77ac08c2dd0881922248c27602a38c85380bc42d9f4b1"
   license all_of: ["Apache-2.0", "MIT"]
 
   livecheck do
