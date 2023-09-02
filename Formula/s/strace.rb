b5d719fdbc2a906b796d2c130be286fdf3427a19
Porkepix <porkepix@gmail.com>
2023-09-02 07:15:58 +0200

    strace 6.5

--- Formula/s/strace.rb
+++ Formula/s/strace.rb
@@ -1,8 +1,8 @@
 class Strace < Formula
   desc "Diagnostic, instructional, and debugging tool for the Linux kernel"
   homepage "https://strace.io/"
-  url "https://github.com/strace/strace/releases/download/v6.4/strace-6.4.tar.xz"
-  sha256 "27987dbac57fdfd260c6db4dc8328df35c95c6867c8a3d4371d59cdcf4eb9238"
+  url "https://github.com/strace/strace/releases/download/v6.5/strace-6.5.tar.xz"
+  sha256 "dfb051702389e1979a151892b5901afc9e93bbc1c70d84c906ade3224ca91980"
   license "LGPL-2.1-or-later"
 
   bottle do
