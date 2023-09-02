3661945b22efaac7a3ab0b79ce4db9a9051f83ee
Michka Popoff <michkapopoff@gmail.com>
2023-09-02 22:28:18 +0200

    qscintilla2: use qt instead of qt@5
    Note: the spec comment/todo was related to an arm64 flag (see 9cf16762b2bb):
    the flag was removed but not the comment

--- Formula/q/qscintilla2.rb
+++ Formula/q/qscintilla2.rb
@@ -4,6 +4,7 @@ class Qscintilla2 < Formula
   url "https://www.riverbankcomputing.com/static/Downloads/QScintilla/2.14.1/QScintilla_src-2.14.1.tar.gz"
   sha256 "dfe13c6acc9d85dfcba76ccc8061e71a223957a6c02f3c343b30a9d43a4cdd4d"
   license "GPL-3.0-only"
+  revision 1
 
   # The downloads page also lists pre-release versions, which use the same file
   # name format as stable versions. The only difference is that files for
@@ -28,10 +29,9 @@ class Qscintilla2 < Formula
   depends_on "pyqt-builder" => :build
   depends_on "sip"          => :build
 
-  # TODO: use qt when octave can migrate to qt6
-  depends_on "pyqt@5"
+  depends_on "pyqt"
   depends_on "python@3.11"
-  depends_on "qt@5"
+  depends_on "qt"
 
   fails_with gcc: "5"
 
@@ -43,13 +43,12 @@ class Qscintilla2 < Formula
     args = []
 
     if OS.mac?
-      # TODO: when using qt 6, modify the spec
       spec = (ENV.compiler == :clang) ? "macx-clang" : "macx-g++"
       args = %W[-config release -spec #{spec}]
     end
 
-    pyqt = Formula["pyqt@5"]
-    qt = Formula["qt@5"]
+    pyqt = Formula["pyqt"]
+    qt = Formula["qt"]
     site_packages = Language::Python.site_packages(python3)
 
     cd "src" do
@@ -58,13 +57,9 @@ class Qscintilla2 < Formula
           "QMAKE_POST_LINK += install_name_tool -id #{lib}/$(TARGET1) $(TARGET)"
         s.gsub! "$$[QT_INSTALL_LIBS]", lib
         s.gsub! "$$[QT_INSTALL_HEADERS]", include
-        # TODO: use qt6 directory layout when octave can migrate to qt6
-        s.gsub! "$$[QT_INSTALL_TRANSLATIONS]", prefix/"trans"
-        s.gsub! "$$[QT_INSTALL_DATA]", prefix/"data"
-        s.gsub! "$$[QT_HOST_DATA]", prefix/"data"
-        # s.gsub! "$$[QT_INSTALL_TRANSLATIONS]", share/"qt/translations"
-        # s.gsub! "$$[QT_INSTALL_DATA]", share/"qt"
-        # s.gsub! "$$[QT_HOST_DATA]", share/"qt"
+        s.gsub! "$$[QT_INSTALL_TRANSLATIONS]", share/"qt/translations"
+        s.gsub! "$$[QT_INSTALL_DATA]", share/"qt"
+        s.gsub! "$$[QT_HOST_DATA]", share/"qt"
       end
 
       inreplace "features/qscintilla2.prf" do |s|
@@ -84,23 +79,20 @@ class Qscintilla2 < Formula
         sip-include-dirs = ["#{pyqt.opt_prefix/site_packages}/PyQt#{pyqt.version.major}/bindings"]
       EOS
 
-      # TODO: qt6 options
-      # --qsci-features-dir #{share}/qt/mkspecs/features
-      # --api-dir #{share}/qt/qsci/api/python
       args = %W[
         --target-dir #{prefix/site_packages}
 
-        --qsci-features-dir #{prefix}/data/mkspecs/features
+        --qsci-features-dir #{share}/qt/mkspecs/features
         --qsci-include-dir #{include}
         --qsci-library-dir #{lib}
-        --api-dir #{prefix}/data/qsci/api/python
+        --api-dir #{share}/qt/qsci/api/python
       ]
       system "sip-install", *args
     end
   end
 
   test do
-    pyqt = Formula["pyqt@5"]
+    pyqt = Formula["pyqt"]
     (testpath/"test.py").write <<~EOS
       import PyQt#{pyqt.version.major}.Qsci
       assert("QsciLexer" in dir(PyQt#{pyqt.version.major}.Qsci))
