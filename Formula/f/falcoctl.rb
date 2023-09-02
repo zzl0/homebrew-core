f6f0d7c15997248e96a1c3c145ba866a96d94125
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-02 17:32:25 +0000

    falcoctl: add 0.5.1 bottle.

--- Formula/f/falcoctl.rb
+++ Formula/f/falcoctl.rb
@@ -6,6 +6,16 @@ class Falcoctl < Formula
   license "Apache-2.0"
   head "https://github.com/falcosecurity/falcoctl.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "e670b84f4d10a4f96794e794e96eafa551abfce3baaa6ef4f8ab4b9af887afbc"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "8c46b30e3fbc7999843fcb40dd450bfd1c21aa258108a9e35d1ff03b4ad68864"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c68f9d43a7e3bcf7b1854a6fda45a1a781311fe57ccac93c0c10e5802e009af2"
+    sha256 cellar: :any_skip_relocation, ventura:        "a775529f16dc5aad08865c5930e4a28fcc404da6258fa98276f009353db98ccd"
+    sha256 cellar: :any_skip_relocation, monterey:       "a2f6fa72d5c7e7ef9f266aeeca973e090c1571842e8074fdd831094cc3c47987"
+    sha256 cellar: :any_skip_relocation, big_sur:        "8a0eb09e6c83a0a14c88085fdbe35c4fd96902a6cdf5d299ba7112360768ed6d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3c20c38820e3a871e8e73791570453c19ec321c7e5e6b8edd2158f95a1c69e3b"
+  end
+
   depends_on "go" => :build
 
   def install
