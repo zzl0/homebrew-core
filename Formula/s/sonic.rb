4cef46064f65aad632d6b2a7aff719a19927509a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-02 17:52:30 +0000

    sonic: update 1.4.1 bottle.

--- Formula/s/sonic.rb
+++ Formula/s/sonic.rb
@@ -6,14 +6,13 @@ class Sonic < Formula
   license "MPL-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "ba4c2939b999ae73c46cceecb21e25f67f2c0107d8fc53b4ffbe5337299a66c3"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "88a2d41df229febf3a5fc32b1af2c78657810e12ef94ca9522a318b590365bf6"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "42ec66f78a25bd41bfb235b6684aa4f17c7360a5cdc2fdff6206b9be0c0b62e0"
-    sha256 cellar: :any_skip_relocation, ventura:        "cdc64f315ef84bee7373ce6d79bb3d556ab9f834c2a1dd69a096bcb5bef9cdbb"
-    sha256 cellar: :any_skip_relocation, monterey:       "00d1c7357fcb1261433e54856267d7c653701b8d154e4951cd16ba70fe66c3c6"
-    sha256 cellar: :any_skip_relocation, big_sur:        "322caf905b32c77a887ac05a5c9d78d1116e3af9701ecc27652c813d86c9f1a6"
-    sha256 cellar: :any_skip_relocation, catalina:       "9396cb130cd9db11186a103e19371ff06d1e6e72ef784f5b9f94415ef29dfef3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "12a26ea1c3ca5619ac1f72d37526b47862512c136bc0e56b24c5b023b72c1d42"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "11a23492e2ad193b664c3552fe6ba219552805787fb0eb2fbbf174aae5933cd5"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a78f9e55686eaf8951e9eb4055a119872c5de7088a5f1a44c28edfa196d14bc1"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7f8c034a9459392dc521e60a46672f9d6c70e91c30cc5ca5b51c21913872f248"
+    sha256 cellar: :any_skip_relocation, ventura:        "87956eae0267936d007753252e2297cbbb96ef129e57355a131bc5ff687a0e74"
+    sha256 cellar: :any_skip_relocation, monterey:       "d123ac2daa1ef6cdebf2f4e8ab00d6fdcaca19c5367f8f66bfda3ad568bcb431"
+    sha256 cellar: :any_skip_relocation, big_sur:        "532f64da105577f6ba6f90f7953370ef4d04826e7919bade075d694b6741cc31"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ab07e6adf7d8858270f08554aa271b84564c016e23762a45ea8d670abf926342"
   end
 
   # Use `llvm@15` to work around build failure with Clang 16 described in
