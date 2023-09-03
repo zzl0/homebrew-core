1345e2ca61ecf6674bd0905ce23e504a7bf024ee
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-03 08:28:03 +0000

    hledger 1.31

--- Formula/h/hledger.rb
+++ Formula/h/hledger.rb
@@ -1,8 +1,8 @@
 class Hledger < Formula
   desc "Easy plain text accounting with command-line, terminal and web UIs"
   homepage "https://hledger.org/"
-  url "https://github.com/simonmichael/hledger/archive/refs/tags/1.30.tar.gz"
-  sha256 "d2a946c47ed74b835fe90cdb3979fc26a266b9e4b7f6a01f7cf1e05910d752a5"
+  url "https://github.com/simonmichael/hledger/archive/refs/tags/1.31.tar.gz"
+  sha256 "d846d51a0144c10f78a692ce2bb665247600514214393b43498d37701f197264"
   license "GPL-3.0-or-later"
   head "https://github.com/simonmichael/hledger.git", branch: "master"
 
