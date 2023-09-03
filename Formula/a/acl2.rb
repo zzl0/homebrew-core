8cc16fc06856f4ecc557c8c3b4ddef33d036ae1b
Rui Chen <rui@chenrui.dev>
2023-09-02 23:34:58 -0400

    acl2: revision bump (sbcl 2.3.8)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/a/acl2.rb
+++ Formula/a/acl2.rb
@@ -4,7 +4,7 @@ class Acl2 < Formula
   url "https://github.com/acl2/acl2/archive/8.5.tar.gz"
   sha256 "dcc18ab0220027b90f30cd9e5a67d8f603ff0e5b26528f3aab75dc8d3d4ebc0f"
   license "BSD-3-Clause"
-  revision 9
+  revision 10
 
   bottle do
     sha256 arm64_ventura:  "17e3a8dd8c2c19f92cd7dc7e2baca906a2548ebebcba8c70ae91a2b021dbd1fa"
