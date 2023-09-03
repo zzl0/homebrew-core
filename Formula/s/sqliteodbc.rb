48567b52ffb155dbf579ed59ebd03fc9e8a06715
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-03 06:09:00 +0000

    sqliteodbc: update 0.9999 bottle.

--- Formula/s/sqliteodbc.rb
+++ Formula/s/sqliteodbc.rb
@@ -11,13 +11,14 @@ class Sqliteodbc < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "b0a8f9a64312af200f4055137047fbefb91d16ff0ae725c68860201553c67eb0"
-    sha256 cellar: :any,                 arm64_monterey: "66da89464648d171f7c7db9d2e4eb0a284e73a70a74f59a5fcb19c9f92e3870f"
-    sha256 cellar: :any,                 arm64_big_sur:  "7d345b9c5b48cf74eab1f16657cab8bf9352dfca7b2b14fc9da9e714dc38b14c"
-    sha256 cellar: :any,                 ventura:        "d2befd3618c126838e53517eb552f5243735d6b790ac36f92f8299f33f51f266"
-    sha256 cellar: :any,                 monterey:       "51b3ad012528a10185ae3d288bbdac21c232ca31a15bc2c864f6c361ea247091"
-    sha256 cellar: :any,                 big_sur:        "2e557cba3fef6bebc851450e809252afc8fb98158d53ea3e1a56c306f9a12546"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "304e529c638e45f26761ca6838072f0c8015540aefab6173401ee9a0c162de1f"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_ventura:  "55af98413216d502d0cfbd87681c8961eef96515a8df22819234b4b2f600c906"
+    sha256 cellar: :any,                 arm64_monterey: "ba182ccbe7cbd3de486f7ac7647605076f63567b7f9565bf0c8bf019413d758c"
+    sha256 cellar: :any,                 arm64_big_sur:  "ea5c59632a50b4fbc760444c295c9a39e79de33457a6c719f8c86395b6bebcdd"
+    sha256 cellar: :any,                 ventura:        "e5561ee6daba6e6aabfe05104fc6059133e274e94d06cd01c4721819ef376418"
+    sha256 cellar: :any,                 monterey:       "449760d73e087195df2f145e10b0876651a1a5029dc854364e1b199d9b2608fe"
+    sha256 cellar: :any,                 big_sur:        "09adb1cab236efb140cb5e61d53f3fa0c6377e35192a5536de67dfb4d3315085"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4e889ae35df729577817bade6dd9c445e549a017f9154dcc0cf2b48fdbff7659"
   end
 
   depends_on "sqlite"
