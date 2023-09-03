5b80eb3a46842b55088cd19b7d7ebb70b43e6906
Rui Chen <rui@chenrui.dev>
2023-09-03 13:55:35 -0400

    wiremock-standalone 3.0.1
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/w/wiremock-standalone.rb
+++ Formula/w/wiremock-standalone.rb
@@ -1,10 +1,14 @@
 class WiremockStandalone < Formula
   desc "Simulator for HTTP-based APIs"
   homepage "https://wiremock.org/docs/running-standalone/"
-  url "https://search.maven.org/remotecontent?filepath=com/github/tomakehurst/wiremock-jre8-standalone/2.35.0/wiremock-jre8-standalone-2.35.0.jar"
-  sha256 "ae156ae2812e3cfa470c47ed073100ef4ec77927372a4e203f0e3bd531f3eb57"
+  url "https://search.maven.org/remotecontent?filepath=org/wiremock/wiremock-standalone/3.0.1/wiremock-standalone-3.0.1.jar"
+  sha256 "1602d2335f6556838d3a3341266cb3ef81850548ff4cd941750eaeb712a06525"
   license "Apache-2.0"
-  head "https://github.com/tomakehurst/wiremock.git", branch: "master"
+
+  livecheck do
+    url "https://search.maven.org/remotecontent?filepath=org/wiremock/wiremock-standalone/maven-metadata.xml"
+    regex(%r{<version>v?(\d+(?:\.\d+)+)</version>}i)
+  end
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "cf5d7cfa1ff79a55030816f8569480bbedb6221bb0f7900525d29b3793c4c4d9"
@@ -13,8 +17,8 @@ class WiremockStandalone < Formula
   depends_on "openjdk"
 
   def install
-    libexec.install "wiremock-jre8-standalone-#{version}.jar"
-    bin.write_jar_script libexec/"wiremock-jre8-standalone-#{version}.jar", "wiremock"
+    libexec.install "wiremock-standalone-#{version}.jar"
+    bin.write_jar_script libexec/"wiremock-standalone-#{version}.jar", "wiremock"
   end
 
   test do
