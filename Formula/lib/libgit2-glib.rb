fd6d1bdf803c85b7cc8e616ad5652d6ee830d7e3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-03 03:37:31 +0000

    libgit2-glib: update 1.1.0_2 bottle.

--- Formula/lib/libgit2-glib.rb
+++ Formula/lib/libgit2-glib.rb
@@ -18,13 +18,13 @@ class Libgit2Glib < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_ventura:  "dc78ea81c37b8e8977ef2d6469166e37937c06238cee7b5e46c3856b459aed8a"
-    sha256 cellar: :any, arm64_monterey: "e3ff1e622801cc996caff77b2ca138e69764fceab2f4dfccb7a35f4dd622643f"
-    sha256 cellar: :any, arm64_big_sur:  "3a250cd16c5583e345b2eba049765b4b27c67207a2a0373c741710e7a139105a"
-    sha256 cellar: :any, ventura:        "d416576872b58f6b67fe70d94f2af9a0174da2f44c4cb95ddddc8f628de78fc1"
-    sha256 cellar: :any, monterey:       "731ae37b595af558aa4fe4a0d18cc43916405d73b6eecaa0045c42b414e6e735"
-    sha256 cellar: :any, big_sur:        "227b09aa6272fc3e4aba191c7d3144c09d0888a229291a717ba293251ca193c0"
-    sha256               x86_64_linux:   "ef2aaf12a607c505944c2d752f31b8c1120993a9c499bfc93b0f71353975e55a"
+    sha256 cellar: :any, arm64_ventura:  "3e00a33f2d6fcaa935590ae471c1b6b0887b96a5bf04a142207dc4b861db5bcd"
+    sha256 cellar: :any, arm64_monterey: "df185c6c3ef129cfd1862cdeccd0cbb712d96cd8c4533ca5216174ffc6213981"
+    sha256 cellar: :any, arm64_big_sur:  "d1fad3cf7352dc4e9da32662c94ddf1c44b5c5fd69531929113aaac5a5555045"
+    sha256 cellar: :any, ventura:        "91b431c40146fa729d6847aea26ee93ab0c2d90230daafc5014fbfdd1fda5435"
+    sha256 cellar: :any, monterey:       "70757d1a95a3eafad006d22bc47814212e53d074523f1717567c1c18f45d8b98"
+    sha256 cellar: :any, big_sur:        "c37882210af3f421dabdb2244c50b8a113ed10cde71313a8c4098fe24029258d"
+    sha256               x86_64_linux:   "47ca5a8b0430ec37b60fb19d54d87bc9c65b778e0e234151b2d4873b7789e0cb"
   end
 
   depends_on "gobject-introspection" => :build
