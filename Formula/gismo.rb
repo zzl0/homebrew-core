9505928417cf0b0c24ebfe71a3cec22b5013d781
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-03 15:47:15 +0000

    gismo: add 21.12.0 bottle.

--- Formula/gismo.rb
+++ Formula/gismo.rb
@@ -5,6 +5,16 @@ class Gismo < Formula
   sha256 "4001b4c49661ca8b71baf915e773341e115d154077bef218433a3c1d72ee4f0c"
   license "MPL-2.0"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "2c1e2f3e321ee74a7d53fab75dfa03d1228cb328b788eb65b5237d0c5c15c5b9"
+    sha256 cellar: :any,                 arm64_monterey: "67fb8c504ab96451c1be3c590d81e424cb513f5bee72415c101c2dcf28b5d88a"
+    sha256 cellar: :any,                 arm64_big_sur:  "25a1d59c5e94aad0861a99f63ad2adac55890eb35ba9ff997e4de69e334a15e6"
+    sha256 cellar: :any,                 ventura:        "81ca83442c82f9e2b186bddc4c41afafbcd48543e1f4b824a82753a735616639"
+    sha256 cellar: :any,                 monterey:       "1e62c2393cddcbea345d697fc2edc4f89b5e84ea0937d9833ee0b0995c855402"
+    sha256 cellar: :any,                 big_sur:        "b866e490fa3a9f742caab85f3bb290c7c678f6a976be51f70855797066ab8cf4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "dc809b43a86065f48c301531493a3553f042c3990c6e2edabac31ed30bb54db8"
+  end
+
   head do
     url "https://github.com/gismo/gismo.git", branch: "stable"
   end
