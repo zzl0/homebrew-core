468009689bf7744b8d15db0390e4531aa4939742
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-03 03:37:26 +0000

    gfold: update 4.4.0_1 bottle.

--- Formula/g/gfold.rb
+++ Formula/g/gfold.rb
@@ -10,13 +10,13 @@ class Gfold < Formula
   head "https://github.com/nickgerace/gfold.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "387c3b56addb62d66d5b4a90fd4f9c5a810b0410ff4968db25ce6dc918d16dac"
-    sha256 cellar: :any,                 arm64_monterey: "b10b2cc728ebaba127538a8c6c32cf6735cd1c464305c8bfb68bc12554188683"
-    sha256 cellar: :any,                 arm64_big_sur:  "f458032d5fbfc31ade19cc9a5c633268ba5bdbb1376d11ccd816d8b99685da96"
-    sha256 cellar: :any,                 ventura:        "808d959ef6219ac17f0005f8264fb1c0ec49401d63a74cefe200772937549fde"
-    sha256 cellar: :any,                 monterey:       "f0dfc994e3c2148775dcf20e02b93a0d23cb9e636e726fb56241d536f388e8e6"
-    sha256 cellar: :any,                 big_sur:        "68d48c0c4cd08ecd3c0fc79614664e682234d5dea6a7920e1fe8f116b0d9c823"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f12c02b19246e2a783c112ec1f95da175df5c226f31c874a3a3bdf4321fac984"
+    sha256 cellar: :any,                 arm64_ventura:  "2c3202011e9ed676192d573292428cc2f96b6b93d8584b32f5c9902afc91835c"
+    sha256 cellar: :any,                 arm64_monterey: "6bf31918b5130dd9b20c50e1aa0d1ce30eb4e7360d24246568edf1d1ce327d0f"
+    sha256 cellar: :any,                 arm64_big_sur:  "b8575070e071d18fb85d33996f652dd41332efe4de74c39bff9eb31d613cd3a6"
+    sha256 cellar: :any,                 ventura:        "e331f58eb2250adeefc2a2c725963e27c7fb4cd18089141c02f5863bcb27d767"
+    sha256 cellar: :any,                 monterey:       "7c85c66db643ee79188c3ba810f0995750f366bd6fb2252a2df2297dab666bc4"
+    sha256 cellar: :any,                 big_sur:        "a187356fa10c4af5116a9ceebae7f1bc09bef032b7b5c1c2eaebfab3bccfe8be"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f9607d24633b3b73369142d66ca0dba4c10c41f41e2f30cf5153e5e329c1e3ba"
   end
 
   depends_on "pkg-config" => :build
