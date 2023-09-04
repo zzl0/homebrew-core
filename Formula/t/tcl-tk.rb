702f4a3cc8adc90210e00cef866a215bf231ce75
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-09-04 17:26:08 -0500

    tcl-tk: try workaround for itk4 distfile
    `Error: bad URI(is not URI?)` encountered for some SourceForge mirrors

--- Formula/t/tcl-tk.rb
+++ Formula/t/tcl-tk.rb
@@ -72,6 +72,7 @@ class TclTk < Formula
 
   resource "itk4" do
     url "https://downloads.sourceforge.net/project/incrtcl/%5Bincr%20Tcl_Tk%5D-4-source/itk%204.1.0/itk4.1.0.tar.gz"
+    mirror "https://src.fedoraproject.org/lookaside/extras/itk/itk4.1.0.tar.gz/sha512/1deed09daf66ae1d0cc88550be13814edff650f3ef2ecb5ae8d28daf92e37550b0e46921eb161da8ccc3886aaf62a4a3087df0f13610839b7c2d6f4b39c9f07e/itk4.1.0.tar.gz"
     sha256 "da646199222efdc4d8c99593863c8d287442ea5a8687f95460d6e9e72431c9c7"
   end
 
