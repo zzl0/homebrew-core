a90fce3f02eb0a0a35b3bee8368147d2a931af62
Porkepix <porkepix@gmail.com>
2023-09-04 14:19:35 +0200

    oauth2_proxy 7.5.0
    oauth2_prox: update url
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/o/oauth2_proxy.rb
+++ Formula/o/oauth2_proxy.rb
@@ -1,8 +1,8 @@
 class Oauth2Proxy < Formula
   desc "Reverse proxy for authenticating users via OAuth 2 providers"
   homepage "https://oauth2-proxy.github.io/oauth2-proxy/"
-  url "https://github.com/oauth2-proxy/oauth2_proxy/archive/v7.4.0.tar.gz"
-  sha256 "f9c5791537b1286b5985b0123f19ad84d390a7fffc1edc1b7c50c5d66c67ebdd"
+  url "https://github.com/oauth2-proxy/oauth2-proxy/archive/refs/tags/v7.5.0.tar.gz"
+  sha256 "d31206ca1eff121d4560e84a652a55efd0a8a01bf7fbcd30d524f0e4676cd75c"
   license "MIT"
   head "https://github.com/oauth2-proxy/oauth2-proxy.git", branch: "master"
 
