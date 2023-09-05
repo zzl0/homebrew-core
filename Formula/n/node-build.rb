f081ab9d362717278720ae085436c05085669ead
Alexander Frisch <crunchtime-ali@users.noreply.github.com>
2023-09-05 12:18:47 +0000

    node-build 4.9.121

--- Formula/n/node-build.rb
+++ Formula/n/node-build.rb
@@ -1,8 +1,8 @@
 class NodeBuild < Formula
   desc "Install NodeJS versions"
   homepage "https://github.com/nodenv/node-build"
-  url "https://github.com/nodenv/node-build/archive/v4.9.120.tar.gz"
-  sha256 "130ce2d13155180e81ce83d503b27c74712808df68997e375b9760ffc3f695bb"
+  url "https://github.com/nodenv/node-build/archive/v4.9.121.tar.gz"
+  sha256 "d17e3a6193eee144229230cccb1796fbd64c6a063dbed0c6476f9bbd68727107"
   license "MIT"
   head "https://github.com/nodenv/node-build.git", branch: "master"
 
