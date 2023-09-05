0dad587c31815a437db89730973f396fdb1a8bb3
Patrick Linnane <patrick@linnane.io>
2023-09-05 16:08:08 -0700

    kosli-cli 2.6.5 (new formula)

---
+++ Formula/k/kosli-cli.rb
@@ -0,0 +1,29 @@
+class KosliCli < Formula
+  desc "CLI for managing Kosli"
+  homepage "https://docs.kosli.com/client_reference/"
+  url "https://github.com/kosli-dev/cli.git",
+      tag:      "v2.6.5",
+      revision: "8f4dc320cd81002b2e2f83516e679f3b0e685168"
+  license "MIT"
+  head "https://github.com/kosli-dev/cli.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/kosli-dev/cli/internal/version.version=#{version}
+      -X github.com/kosli-dev/cli/internal/version.gitCommit=#{Utils.git_head}
+      -X github.com/kosli-dev/cli/internal/version.gitTreeState=clean
+    ]
+    system "go", "build", *std_go_args(output: bin/"kosli", ldflags: ldflags), "./cmd/kosli"
+
+    generate_completions_from_executable(bin/"kosli", "completion", base_name: "kosli")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/kosli version")
+
+    assert_match "OK", shell_output("#{bin}/kosli status")
+  end
+end
