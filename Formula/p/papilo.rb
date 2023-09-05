59d06ac06466c8d5b627c047ee1b26cb15872d41
Rui Chen <rui@chenrui.dev>
2023-09-05 09:52:18 -0400

    papilo 2.1.3

--- Formula/p/papilo.rb
+++ Formula/p/papilo.rb
@@ -1,10 +1,9 @@
 class Papilo < Formula
   desc "Parallel Presolve for Integer and Linear Optimization"
   homepage "https://www.scipopt.org"
-  url "https://github.com/scipopt/papilo/archive/refs/tags/v2.1.2.tar.gz"
-  sha256 "7e3d829c957767028db50b5c5085601449b00671e7efc2d5eb0701a6903d102f"
+  url "https://github.com/scipopt/papilo/archive/refs/tags/v2.1.3.tar.gz"
+  sha256 "d22e8e2e91e1967afda5cf27eaeaa4ab3d40694c7716b4d328d69a50e05d5115"
   license all_of: ["LGPL-3.0-only", "GPL-3.0-only"]
-  revision 1
   version_scheme 1
 
   bottle do
