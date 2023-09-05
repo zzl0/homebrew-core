adff3c7077bf6676b9ae367aca53f873f1927f35
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-05 19:25:38 +0000

    pyoxidizer: update 0.24.0 bottle.

--- Formula/p/pyoxidizer.rb
+++ Formula/p/pyoxidizer.rb
@@ -12,14 +12,13 @@ class Pyoxidizer < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a438770a4ce6bb1ea06f3d23d1c0cf505f93383de2ff27d99b22d5d5b34c9f51"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "cb62cb641b9b69e0995c19c288a051a5f59e11de3a174aceb4c67daf48332b4c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2dbd06e01ecb46b93bb3250bd0d5e349295ba6c56b77622d29c64676ed17e3e8"
-    sha256 cellar: :any_skip_relocation, ventura:        "4b03858e038e6d7a4a40abf538618704f9b891815766a45492ae41af608eba72"
-    sha256 cellar: :any_skip_relocation, monterey:       "80d2014e88d9937d1a88b99da5a8d035e8ac42ccc4df7480512e020e6ee30fb6"
-    sha256 cellar: :any_skip_relocation, big_sur:        "e9aa0d494c359e0f8b16c8c3ae31159d8f540f3f9d7946ca4bdc20bc6a83fe10"
-    sha256 cellar: :any_skip_relocation, catalina:       "3d1bc85db1bcbb5f69c8dba9c294d65ef9b674d217f6eab670ccc6eb4dedf812"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8de042e72b4759060bdfd5fac56d50594614f7c0cab901b8ccf70955a4191191"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "fa37a4ac504621090ea1f67abee2694e8c6798f5f8b35dba0f67411d70c9e133"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "32a2e334bd1345638385aa4d56b635b8faecb5095607ed4f177660965979478d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6286b57cb42b275d4f6325e17842cd49bc6bedb9804616349a95250e96eb8294"
+    sha256 cellar: :any_skip_relocation, ventura:        "46aa367ab70a1488edd31411f85942a7179c090feecd1cdfa46735668d80b457"
+    sha256 cellar: :any_skip_relocation, monterey:       "ea59d68a7bcdf1237d3eb72348901ea846546598194a44758e4db2521cc38880"
+    sha256 cellar: :any_skip_relocation, big_sur:        "5b9f21cdbd215dbaf5c0a41e70674244b4e192e6a64e6a4e1a3e03c933e4670a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0648492b46bc163e396b0e6e2ca99350a937476050c158dd5d8a32b9b0fc102d"
   end
 
   depends_on "rust" => [:build, :test]
