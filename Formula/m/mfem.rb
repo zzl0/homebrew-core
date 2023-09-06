08840f69e1256c734ff7e63fa3625e776c6321b6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-06 13:58:03 +0000

    mfem: add 4.5.2 bottle.

--- Formula/m/mfem.rb
+++ Formula/m/mfem.rb
@@ -6,6 +6,16 @@ class Mfem < Formula
   license "BSD-3-Clause"
   head "https://github.com/mfem/mfem.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8b7a3ed63a7e5f44598a10355e58efb02967d6d0a7d91021a7f70ee785a3d769"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "4757c720028fd0d2c596d94eddc0d2e57f23bd11a6b7d50750ecf76dccde2fd7"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "584ef622c88ff2ea7173634385776fb292f838d752be5830c018368644615da7"
+    sha256 cellar: :any_skip_relocation, ventura:        "c10c51c3dfab840d9d6a2ccb6872886404b6cf847935302a79ddec69023ae303"
+    sha256 cellar: :any_skip_relocation, monterey:       "a44c2c7cbc4f59908f99fd9b1db39f7f8985e67c712ef5bfc2911ee68c0c5075"
+    sha256 cellar: :any_skip_relocation, big_sur:        "54ee50777555e5566c3331113060be394e1c20c0b39400ee9e3508e57a191b0d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "145f352362f8d1fe19ec60c98a55d92dd35bccc726dc4a8b8c3c7cf911f50444"
+  end
+
   depends_on "cmake" => :build
   depends_on "hypre"        # optional "mpi"
   depends_on "metis"        # optional "metis"
