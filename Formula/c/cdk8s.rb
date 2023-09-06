7f603762cb8586a235063e433b0678e53fc1529b
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-09-06 10:29:13 +0800

    cdk8s 2.76.0

--- Formula/c/cdk8s.rb
+++ Formula/c/cdk8s.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Cdk8s < Formula
   desc "Define k8s native apps and abstractions using object-oriented programming"
   homepage "https://cdk8s.io/"
-  url "https://registry.npmjs.org/cdk8s-cli/-/cdk8s-cli-2.74.0.tgz"
-  sha256 "efb98f641431a632604643cfbdf6735897252b0dbaa8313f6e74f43514288e8b"
+  url "https://registry.npmjs.org/cdk8s-cli/-/cdk8s-cli-2.76.0.tgz"
+  sha256 "154cefd5fa2b6594303d27f20a4a252f550b89e434ce4b252cd1967f450d3865"
   license "Apache-2.0"
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "00c8f33c885a595c7c80c560cdd5738a2eeeac9e4a738c97ae2a02ea2162dcdc"
