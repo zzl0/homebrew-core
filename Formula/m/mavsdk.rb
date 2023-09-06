4a5408de92af65ac9fea5e9e3cda23ab8fb6cade
Rui Chen <rui@chenrui.dev>
2023-09-06 19:11:28 -0400

    mavsdk: revision bump (grpc 1.58.0)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/m/mavsdk.rb
+++ Formula/m/mavsdk.rb
@@ -7,7 +7,7 @@ class Mavsdk < Formula
       tag:      "v1.4.16",
       revision: "a14d604c2baa950dab1510448bce7c0b490a4f1a"
   license "BSD-3-Clause"
-  revision 9
+  revision 10
 
   livecheck do
     url :stable
