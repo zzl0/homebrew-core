264ce205f1df4edf3a921c770b196f972dc3e763
Rui Chen <rui@chenrui.dev>
2023-09-05 22:47:36 -0400

    gdal: revision bump (poppler 23.09.0)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/g/gdal.rb
+++ Formula/g/gdal.rb
@@ -4,7 +4,7 @@ class Gdal < Formula
   url "https://github.com/OSGeo/gdal/releases/download/v3.7.1/gdal-3.7.1.tar.gz"
   sha256 "c131a39d0f7c2a76f02dd648f2906179ddb38958673461eee1da4f9c9a166e76"
   license "MIT"
-  revision 3
+  revision 4
 
   livecheck do
     url "https://download.osgeo.org/gdal/CURRENT/"
