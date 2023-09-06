8eda57eb4255ac2b87b9f501e7b0afe6101a8499
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-06 07:54:36 +0000

    rpmspectool: update 1.99.7 bottle.

--- Formula/r/rpmspectool.rb
+++ Formula/r/rpmspectool.rb
@@ -8,9 +8,10 @@ class Rpmspectool < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "e6a3984ff1a7e27b7434158d82a9c424d0413aa1b2d5e31c7418d591aec57e40"
-    sha256 cellar: :any_skip_relocation, ventura:       "0bb3455e3b2f5f36ff74f4fbfd74ab0ba3e06905d97ad4add24b0d8cf6130f58"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2806fc85d5ea031c34610e073d70f601c15433d5c3e4f6beb6abc3c9aeb1fd97"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "ae788048a8d4a77be470b7fc6fefc1d70c2cb4073e2a3634cbf962c84430d60c"
+    sha256 cellar: :any_skip_relocation, ventura:       "964ebc4c8fad13b63affbbc5f800aba1551b4bcbbc7ad94556d87d791e156b67"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3b27e1de9b1da031214a7cfd84e39491faca0f84fa616f1f358b8602c7961bca"
   end
 
   depends_on "python-pycurl"
