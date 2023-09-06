9e39191c824cd060493f3250ca908cdd035d4ecf
Rui Chen <rui@chenrui.dev>
2023-09-06 14:12:45 -0400

    grpc 1.58.0

--- Formula/g/grpc.rb
+++ Formula/g/grpc.rb
@@ -2,10 +2,9 @@ class Grpc < Formula
   desc "Next generation open source RPC library and framework"
   homepage "https://grpc.io/"
   url "https://github.com/grpc/grpc.git",
-      tag:      "v1.57.0",
-      revision: "a61640173d00b63e0b55ad61915a9b1708e12d27"
+      tag:      "v1.58.0",
+      revision: "0417b882aa7abc1512f534e56f9cd97c557bb68f"
   license "Apache-2.0"
-  revision 1
   head "https://github.com/grpc/grpc.git", branch: "master"
 
   # There can be a notable gap between when a version is tagged and a
