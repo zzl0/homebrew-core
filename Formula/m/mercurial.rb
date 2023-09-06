614410efb0e8a11c9cc86379f581da12d05b3162
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-06 17:26:48 +0000

    mercurial: update 6.5.2 bottle.

--- Formula/m/mercurial.rb
+++ Formula/m/mercurial.rb
@@ -13,13 +13,13 @@ class Mercurial < Formula
   end
 
   bottle do
-    sha256 arm64_ventura:  "dce0b4b198aa2ca4c72f11241e13f9fc4af2e1cfd5c2c05e6fc71cac4f4b9aa9"
-    sha256 arm64_monterey: "79c5c1e2f62a404dab949c918a7e27cb65ede38c891a9e94c96ea98c403d245a"
-    sha256 arm64_big_sur:  "e7715a6d5ae02fd1470e639f3acbf665ddf01bd7ea1dbc0e2e653fee04af53bb"
-    sha256 ventura:        "275751b65ce8f1be29d5238178b466227ccedccc1234b9b06bb0e22d41b1ee5f"
-    sha256 monterey:       "b4871fb5add36b8e31b85be868d1389f09de16a3ead6caf9c9026e45e9e6a4c0"
-    sha256 big_sur:        "4eaf78c19a85722f0cf92f7cbbff98207302f3a8e82ed0c59ecb37ac909d9c56"
-    sha256 x86_64_linux:   "a793c0ed8f469ae60cae89045c336dfd5764f29952b13c4646d0a7941ac54777"
+    sha256 arm64_ventura:  "b55165a71f173f4584fa125924b104c774a817369fcb03bec2eab093c7676ec8"
+    sha256 arm64_monterey: "ff85084729bd075861ede0d68f570d88824dc6c1c7b04b8171307cb804b25fdb"
+    sha256 arm64_big_sur:  "a9dc023f3096d960872bb70a04811fdfefbefede2c36f2f65cf83fca88fd2d0a"
+    sha256 ventura:        "9a06ef4ce2ce14852ec5a40eb9ffed06cb649b89b304654229d1e98cbba4dff4"
+    sha256 monterey:       "5a91a1efb3b25ee6dd70dfb3b74ffd283e45c6eb42b666c061ef76106f38e460"
+    sha256 big_sur:        "0bad9b520371939fee2ea16c8b3cd5a93915f18baf2c209d98540f33ea6af9c1"
+    sha256 x86_64_linux:   "ab687c12c29f0af2fa836ec4750830217e5b4337d3d6ef8d52720973fba31af4"
   end
 
   depends_on "python@3.11"
