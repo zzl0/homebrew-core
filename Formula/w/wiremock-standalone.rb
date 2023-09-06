32aed18ee3d55c0dcc5b28b8193560432d64c385
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-06 19:09:00 +0000

    wiremock-standalone: update 3.0.3 bottle.

--- Formula/w/wiremock-standalone.rb
+++ Formula/w/wiremock-standalone.rb
@@ -11,13 +11,13 @@ class WiremockStandalone < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "007ee2e6fc63507150cf8f661015ce23466362ee069e77f0e1b87d485ec3069d"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "007ee2e6fc63507150cf8f661015ce23466362ee069e77f0e1b87d485ec3069d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "007ee2e6fc63507150cf8f661015ce23466362ee069e77f0e1b87d485ec3069d"
-    sha256 cellar: :any_skip_relocation, ventura:        "007ee2e6fc63507150cf8f661015ce23466362ee069e77f0e1b87d485ec3069d"
-    sha256 cellar: :any_skip_relocation, monterey:       "007ee2e6fc63507150cf8f661015ce23466362ee069e77f0e1b87d485ec3069d"
-    sha256 cellar: :any_skip_relocation, big_sur:        "007ee2e6fc63507150cf8f661015ce23466362ee069e77f0e1b87d485ec3069d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cbfeee6863d6fcbcdee6aa0dc857fee97fd9958dd6b41a83f582d79a2df96e2b"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f98c022ebeea015ad9119e9c23c9f4e36df3add8741b7c13cdde3d0aa1ad52ac"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f98c022ebeea015ad9119e9c23c9f4e36df3add8741b7c13cdde3d0aa1ad52ac"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f98c022ebeea015ad9119e9c23c9f4e36df3add8741b7c13cdde3d0aa1ad52ac"
+    sha256 cellar: :any_skip_relocation, ventura:        "f98c022ebeea015ad9119e9c23c9f4e36df3add8741b7c13cdde3d0aa1ad52ac"
+    sha256 cellar: :any_skip_relocation, monterey:       "f98c022ebeea015ad9119e9c23c9f4e36df3add8741b7c13cdde3d0aa1ad52ac"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f98c022ebeea015ad9119e9c23c9f4e36df3add8741b7c13cdde3d0aa1ad52ac"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "329b7dc029ebfdcce8f39f2472a3b5a87f1d51d43c91cd456fd84a9f2d003850"
   end
 
   depends_on "openjdk"
