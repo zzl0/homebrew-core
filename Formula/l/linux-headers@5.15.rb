69aa460a8b09a782adf6a67e5be88ccde07eb12d
Patrick Linnane <patrick@linnane.io>
2023-09-06 15:17:55 -0700

    linux-headers@5.15 5.15.131

--- Formula/l/linux-headers@5.15.rb
+++ Formula/l/linux-headers@5.15.rb
@@ -1,8 +1,8 @@
 class LinuxHeadersAT515 < Formula
   desc "Header files of the Linux kernel"
   homepage "https://kernel.org/"
-  url "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.15.130.tar.gz"
-  sha256 "b006a21164c20a50599127cefec85a4af95ead19c4d654af4ec6fe48d0250d2e"
+  url "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.15.131.tar.gz"
+  sha256 "690e6eea90de6757a7273cd2e9508faeb9439e0e6d74955ad387f2637c1fb189"
   license "GPL-2.0-only"
 
   livecheck do
