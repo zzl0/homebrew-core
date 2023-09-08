b5f482aedf2ccb6b389a90e8fc2b4103ae736178
Patrick Linnane <patrick@linnane.io>
2023-09-07 19:08:57 -0700

    mosh: revision bump (protobuf 24.3)

--- Formula/m/mosh.rb
+++ Formula/m/mosh.rb
@@ -4,7 +4,7 @@ class Mosh < Formula
   url "https://github.com/mobile-shell/mosh/releases/download/mosh-1.4.0/mosh-1.4.0.tar.gz"
   sha256 "872e4b134e5df29c8933dff12350785054d2fd2839b5ae6b5587b14db1465ddd"
   license "GPL-3.0-or-later"
-  revision 6
+  revision 7
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "c92a411f516871d71fd86987de194516ed90fb2bdb66fd199295903b0a449f35"
