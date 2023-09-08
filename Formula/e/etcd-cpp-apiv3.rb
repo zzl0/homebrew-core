a1a1f6c930e323f01cefca9bcde787a63f4066b4
Patrick Linnane <patrick@linnane.io>
2023-09-07 19:07:30 -0700

    etcd-cpp-apiv3: revision bump (protobuf 24.3)

--- Formula/e/etcd-cpp-apiv3.rb
+++ Formula/e/etcd-cpp-apiv3.rb
@@ -4,7 +4,7 @@ class EtcdCppApiv3 < Formula
   url "https://github.com/etcd-cpp-apiv3/etcd-cpp-apiv3/archive/refs/tags/v0.15.3.tar.gz"
   sha256 "1a6f4ed634f3c75cbfaa2768917e280c6df77cac2af5d945ad67ef20d53d8b60"
   license "BSD-3-Clause"
-  revision 4
+  revision 5
 
   bottle do
     sha256 cellar: :any,                 arm64_ventura:  "ca772d1ecb19ceef1a48f49ac1938043bb450d22ce0d798620d32da687d9c42d"
