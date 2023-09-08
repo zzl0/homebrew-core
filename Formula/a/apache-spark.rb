5749cbbd5d19148a275ea720cd72e65f0890ccca
David Penn <david.penn@me.com>
2023-09-08 21:56:43 +0200

    apache-spark: rebuild
    * apache-spark: use overridable java home env
    * apache-spark: use java 17 if JAVA_HOME is not set
    
    Closes #141814.
    
    Co-authored-by: Alexander Bayandin <alexander@bayandin.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/a/apache-spark.rb
+++ Formula/a/apache-spark.rb
@@ -21,7 +21,7 @@ class ApacheSpark < Formula
     rm_f Dir["bin/*.cmd"]
     libexec.install Dir["*"]
     bin.install Dir[libexec/"bin/*"]
-    bin.env_script_all_files(libexec/"bin", JAVA_HOME: Formula["openjdk@17"].opt_prefix)
+    bin.env_script_all_files(libexec/"bin", JAVA_HOME: Language::Java.overridable_java_home_env("17")[:JAVA_HOME])
   end
 
   test do
