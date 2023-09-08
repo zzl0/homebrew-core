52f7d53c8010d6f8617f0a69f871e7fb3da255b2
Patrick Linnane <patrick@linnane.io>
2023-09-07 19:13:32 -0700

    protobuf-c: revision bump (protobuf 24.3)

--- Formula/p/protobuf-c.rb
+++ Formula/p/protobuf-c.rb
@@ -4,7 +4,7 @@ class ProtobufC < Formula
   url "https://github.com/protobuf-c/protobuf-c/releases/download/v1.4.1/protobuf-c-1.4.1.tar.gz"
   sha256 "4cc4facd508172f3e0a4d3a8736225d472418aee35b4ad053384b137b220339f"
   license "BSD-2-Clause"
-  revision 6
+  revision 7
 
   bottle do
     sha256 cellar: :any,                 arm64_ventura:  "38a3719df8ab598eace8db030bce1ab583b1d2b18c013adc5d9964a97f971a95"
