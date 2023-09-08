21d50fada6443ff1624487bc6e5b394f82b51fcd
Patrick Linnane <patrick@linnane.io>
2023-09-07 19:09:20 -0700

    opencv: revision bump (protobuf 24.3)

--- Formula/o/opencv.rb
+++ Formula/o/opencv.rb
@@ -4,7 +4,7 @@ class Opencv < Formula
   url "https://github.com/opencv/opencv/archive/refs/tags/4.8.0.tar.gz"
   sha256 "cbf47ecc336d2bff36b0dcd7d6c179a9bb59e805136af6b9670ca944aef889bd"
   license "Apache-2.0"
-  revision 5
+  revision 6
 
   livecheck do
     url :stable
