00395dca246b97f8290709f62d203c287b818566
David Baumgold <david@davidbaumgold.com>
2023-09-09 08:46:22 +0200

    s4cmd: depend on python-pytz

--- Formula/s/s4cmd.rb
+++ Formula/s/s4cmd.rb
@@ -21,6 +21,7 @@ class S4cmd < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "82f269866cfadecf803bbf8901c120760a63efc5bd21bfe9d86073ea11370f43"
   end
 
+  depends_on "python-pytz"
   depends_on "python@3.11"
   depends_on "six"
 
@@ -42,11 +43,6 @@ class S4cmd < Formula
   resource "python-dateutil" do
     url "https://files.pythonhosted.org/packages/4c/c4/13b4776ea2d76c115c1d1b84579f3764ee6d57204f6be27119f13a61d0a9/python-dateutil-2.8.2.tar.gz"
     sha256 "0123cacc1627ae19ddf3c27a5de5bd67ee4586fbdd6440d9748f8abb483d3e86"
-  end
-
-  resource "pytz" do
-    url "https://files.pythonhosted.org/packages/76/63/1be349ff0a44e4795d9712cc0b2d806f5e063d4d34631b71b832fac715a8/pytz-2022.6.tar.gz"
-    sha256 "e89512406b793ca39f5971bc999cc538ce125c0e51c27941bef4568b460095e2"
   end
 
   resource "s3transfer" do
