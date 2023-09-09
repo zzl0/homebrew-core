ab8e0a182b7ddb08cfbb0e4e8c6781f45c94830b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-09 16:22:07 +0000

    s4cmd: update 2.1.0_1 bottle.

--- Formula/s/s4cmd.rb
+++ Formula/s/s4cmd.rb
@@ -10,15 +10,14 @@ class S4cmd < Formula
   head "https://github.com/bloomreach/s4cmd.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "56fbd838e588d65381bbf8aed3502dc32f8ebfb72cc4853ee2ed31af2cc8a9ea"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "579c89b3b9fa40c0ac1a426e55bd5b9a88f9f8e0560e08f89f5faccafa1d797c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bddd00a80e1ebe79b113c0f7f5d42158f438f5c4382dfe7c7a25eb24a453c65a"
-    sha256 cellar: :any_skip_relocation, ventura:        "b5f8276bab65617391c6ec39c73481cd318c2b914beb729b29eea4343e89face"
-    sha256 cellar: :any_skip_relocation, monterey:       "4fb07106e61b0d9317a091193d4a221053f24a5f02dc81e106c34710f749457e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "7bb2576ce8c56705a123814b30d90e20f0361fb763aadf6c141fef33b3b64b06"
-    sha256 cellar: :any_skip_relocation, catalina:       "ff4ceeea473f6e83f1a9c59c16a056d6d6c1829bf3aad1770aeb82248f883ce6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "82f269866cfadecf803bbf8901c120760a63efc5bd21bfe9d86073ea11370f43"
+    rebuild 2
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "4330aa16ed535e58baec539539457273ca137b7fd940381a5ece53b5ed264377"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "61b5e9c004791b55d9b448c58823c8e64766b8aae3f98b41d85c9225ea8d3a69"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "dc21a20e8b4e7fc2df9e5f8f4be01ef866cb9c5ea2b59ffbc9912baf8b1a9f0e"
+    sha256 cellar: :any_skip_relocation, ventura:        "89921083aec7aecee35c209b8f9507a4ca2d7a1b2ea1d8a025c63221f0751700"
+    sha256 cellar: :any_skip_relocation, monterey:       "02ba6c7b38706ba52e55eb92017eaab34ae59f9c5ae70742f5aeb6b015a8fb68"
+    sha256 cellar: :any_skip_relocation, big_sur:        "0e8c0d2faf3f76e97c7821d4c99b01d2751bc4d55bc9a2fb368b6dd15c2da4e4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "91ea903e1a7f0904623d53ed6f1b16dd33affcdd1c8c9a8db55f4b955b050f91"
   end
 
   depends_on "python-pytz"
