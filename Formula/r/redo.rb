6ae7ff7c964933368e9138539a49521c406e2bac
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-10 21:46:41 +0000

    redo: update 0.42d_2 bottle.

--- Formula/r/redo.rb
+++ Formula/r/redo.rb
@@ -9,15 +9,14 @@ class Redo < Formula
   revision 2
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "1232319e15044a5dbd0e2b0f2608a4bbbad086f07beb2abc33fb7492c4f66204"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "1232319e15044a5dbd0e2b0f2608a4bbbad086f07beb2abc33fb7492c4f66204"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1232319e15044a5dbd0e2b0f2608a4bbbad086f07beb2abc33fb7492c4f66204"
-    sha256 cellar: :any_skip_relocation, ventura:        "1232319e15044a5dbd0e2b0f2608a4bbbad086f07beb2abc33fb7492c4f66204"
-    sha256 cellar: :any_skip_relocation, monterey:       "1232319e15044a5dbd0e2b0f2608a4bbbad086f07beb2abc33fb7492c4f66204"
-    sha256 cellar: :any_skip_relocation, big_sur:        "1232319e15044a5dbd0e2b0f2608a4bbbad086f07beb2abc33fb7492c4f66204"
-    sha256 cellar: :any_skip_relocation, catalina:       "1232319e15044a5dbd0e2b0f2608a4bbbad086f07beb2abc33fb7492c4f66204"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3ff5f989f702472df0da1a1a6fa57b1648226cbc302f434f3f89678f39375764"
+    rebuild 2
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f7e392b19dabdac8d71937132f56dc068b462b36cb90c1bc345f8f3362935bd8"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f7e392b19dabdac8d71937132f56dc068b462b36cb90c1bc345f8f3362935bd8"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f7e392b19dabdac8d71937132f56dc068b462b36cb90c1bc345f8f3362935bd8"
+    sha256 cellar: :any_skip_relocation, ventura:        "f7e392b19dabdac8d71937132f56dc068b462b36cb90c1bc345f8f3362935bd8"
+    sha256 cellar: :any_skip_relocation, monterey:       "f7e392b19dabdac8d71937132f56dc068b462b36cb90c1bc345f8f3362935bd8"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f7e392b19dabdac8d71937132f56dc068b462b36cb90c1bc345f8f3362935bd8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8311708347ac6e5dd70472cbae269bb3a647e49068ff2cc4d6c87571523364ce"
   end
 
   depends_on "python-markdown"
