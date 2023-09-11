89da44365ba9638ba9835ce6b7e6e18944e82321
Patrick Linnane <patrick@linnane.io>
2023-09-11 07:54:01 -0700

    asdf 0.13.0
    asdf: update test

--- Formula/a/asdf.rb
+++ Formula/a/asdf.rb
@@ -1,8 +1,8 @@
 class Asdf < Formula
   desc "Extendable version manager with support for Ruby, Node.js, Erlang & more"
   homepage "https://asdf-vm.com/"
-  url "https://github.com/asdf-vm/asdf/archive/refs/tags/v0.12.0.tar.gz"
-  sha256 "8416d3556f093b3f2f9931b30165efd8800c520c129bed22a537f889aa6dff90"
+  url "https://github.com/asdf-vm/asdf/archive/refs/tags/v0.13.0.tar.gz"
+  sha256 "b161fc696b020e5b9c61040397f57bf388a3deedcd6639729645b7518401b002"
   license "MIT"
   head "https://github.com/asdf-vm/asdf.git", branch: "master"
 
@@ -42,7 +42,7 @@ class Asdf < Formula
 
   test do
     assert_match version.to_s, shell_output("#{bin}/asdf version")
-    output = shell_output("#{bin}/asdf plugin-list 2>&1", 1)
+    output = shell_output("#{bin}/asdf plugin-list 2>&1")
     assert_match "No plugins installed", output
     assert_match "Update command disabled.", shell_output("#{bin}/asdf update", 42)
   end
