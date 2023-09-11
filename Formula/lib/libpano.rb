eafbcf406e6779dd3e1558075a4ad6bd1b7799fe
Rui Chen <rui@chenrui.dev>
2023-09-11 18:21:11 -0400

    libpano 13-2.9.22

--- Formula/lib/libpano.rb
+++ Formula/lib/libpano.rb
@@ -1,11 +1,10 @@
 class Libpano < Formula
   desc "Build panoramic images from a set of overlapping images"
   homepage "https://panotools.sourceforge.net/"
-  url "https://downloads.sourceforge.net/project/panotools/libpano13/libpano13-2.9.21/libpano13-2.9.21.tar.gz"
-  version "13-2.9.21"
-  sha256 "79e5a1452199305e2961462720ef5941152779c127c5b96fc340d2492e633590"
+  url "https://downloads.sourceforge.net/project/panotools/libpano13/libpano13-2.9.22/libpano13-2.9.22.tar.gz"
+  version "13-2.9.22"
+  sha256 "affc6830cdbe71c28d2731dcbf8dea2acda6d9ffd4609c6dbf3ba0c68440a8e3"
   license "GPL-2.0-or-later"
-  revision 2
 
   livecheck do
     url :stable
