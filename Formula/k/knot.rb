11676fcd415e0b4c6e511019bcabc3b7c6e959f3
Porkepix <porkepix@gmail.com>
2023-09-11 14:47:48 +0200

    knot 3.3.1

--- Formula/k/knot.rb
+++ Formula/k/knot.rb
@@ -1,8 +1,8 @@
 class Knot < Formula
   desc "High-performance authoritative-only DNS server"
   homepage "https://www.knot-dns.cz/"
-  url "https://secure.nic.cz/files/knot-dns/knot-3.3.0.tar.xz"
-  sha256 "cf12ab736c512eb719a221cd3f65bb94f93ff2b477803d9474d1334af73c1533"
+  url "https://secure.nic.cz/files/knot-dns/knot-3.3.1.tar.xz"
+  sha256 "f3f4b1d49ec9b81113b14a38354b823bd4a470356ed7e8e555595b6fd1ac80c9"
   license all_of: ["GPL-3.0-or-later", "0BSD", "BSD-3-Clause", "LGPL-2.0-or-later", "MIT"]
 
   livecheck do
