2597ad52c7f6d4070641e3d157a7800882236aab
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-12 11:28:22 +0000

    libcoap: update 4.3.3 bottle.

--- Formula/lib/libcoap.rb
+++ Formula/lib/libcoap.rb
@@ -6,13 +6,13 @@ class Libcoap < Formula
   license "BSD-2-Clause"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "56bc8c3f7f6c8efb9cf34940134a115b0388cb558467a375d792765069421ccf"
-    sha256 cellar: :any,                 arm64_monterey: "efa59e578a537930bcd40d74a7ddda2c738b16c9bf99fad8bf35b23495021a23"
-    sha256 cellar: :any,                 arm64_big_sur:  "b4c2a7cd7ea91dc4cbe1aae159e070912ba3ca6b95a4304c101542f7d8f00c0a"
-    sha256 cellar: :any,                 ventura:        "c1e1cac76bb7a2aad36d84934ef2a8f16dc6b783c54f58f80f62954bcf950920"
-    sha256 cellar: :any,                 monterey:       "d20f28a35e47756c5c4c6794dcfbc10230036dd41434fd3f2fc27e040434668a"
-    sha256 cellar: :any,                 big_sur:        "ba7ddf2bd7e6ab994bca39331dc4ff7498000dfe33dd515a3813be8f54ea9697"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "81d01d515270b8c7e07800d92b4168240c4d10a52da69f8bd3895d2a09d690a6"
+    sha256 cellar: :any,                 arm64_ventura:  "f0a4b5bfb7167e42c73ab56b1420ffe70106a46582eba3ab639cb71c7514bdcc"
+    sha256 cellar: :any,                 arm64_monterey: "b5681b016f932be8c9adf1a1b6b6232c00185bd3ee22348e19a914e087c88a14"
+    sha256 cellar: :any,                 arm64_big_sur:  "ae20f5a9b46bfdde22bf1bd01b41539815ca2ab96d93416c77b78f6ad4d5c079"
+    sha256 cellar: :any,                 ventura:        "6dd0cec93ce11f4e41c1681b09bf0c526ace9b56a4fb3dc48651b688e3b8895d"
+    sha256 cellar: :any,                 monterey:       "cee4b37b76662376290f97e5cd8385393c75633953e82ac493c9ab538b5c8db8"
+    sha256 cellar: :any,                 big_sur:        "3d2110aeb7c6961cee20fe8cd66184c6b63a08e8ddba34cef37720ac49aff192"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d4573838ac7365af81891b8258bc9651de621d2ce3b23ea95f05ba39ce894261"
   end
 
   depends_on "autoconf" => :build
