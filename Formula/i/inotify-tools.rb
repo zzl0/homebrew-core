8ebfa8f535e0d616e6b2ee8a69c9af6d0cfb826c
Patrick Linnane <patrick@linnane.io>
2023-09-12 15:12:36 -0700

    inotify-tools 4.23.9.0

--- Formula/i/inotify-tools.rb
+++ Formula/i/inotify-tools.rb
@@ -1,8 +1,8 @@
 class InotifyTools < Formula
   desc "C library and command-line programs providing a simple interface to inotify"
   homepage "https://github.com/inotify-tools/inotify-tools"
-  url "https://github.com/inotify-tools/inotify-tools/archive/refs/tags/4.23.8.0.tar.gz"
-  sha256 "8ad8b72a146af57688f3289b33b92a026915fc677997147071887b65b603d20a"
+  url "https://github.com/inotify-tools/inotify-tools/archive/refs/tags/4.23.9.0.tar.gz"
+  sha256 "1dfa33f80b6797ce2f6c01f454fd486d30be4dca1b0c5c2ea9ba3c30a5c39855"
   license "GPL-2.0-or-later"
 
   bottle do
