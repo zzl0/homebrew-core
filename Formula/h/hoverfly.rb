a2127301a762c5846e5020448b7d51cb3c7ee2f2
Rui Chen <rui@chenrui.dev>
2023-09-12 19:40:10 -0400

    hoverfly 1.6.0

--- Formula/h/hoverfly.rb
+++ Formula/h/hoverfly.rb
@@ -1,8 +1,8 @@
 class Hoverfly < Formula
   desc "API simulations for development and testing"
   homepage "https://hoverfly.io/"
-  url "https://github.com/SpectoLabs/hoverfly/archive/refs/tags/v1.5.3.tar.gz"
-  sha256 "534ca7b594b0a5e1313c479487233cbf8f5c9a3aae06991e2af84088495b92c5"
+  url "https://github.com/SpectoLabs/hoverfly/archive/refs/tags/v1.6.0.tar.gz"
+  sha256 "1c02e727deef9a0f67da6517fe3b91ef965b2a66824b1a2e9fda7dac27c5a855"
   license "Apache-2.0"
   head "https://github.com/SpectoLabs/hoverfly.git", branch: "master"
 
