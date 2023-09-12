0332778c66f85e5b4195458626a0ec511002f3f7
Porkepix <porkepix@gmail.com>
2023-09-12 12:32:29 +0200

    hysteria 2.0.1

--- Formula/h/hysteria.rb
+++ Formula/h/hysteria.rb
@@ -1,8 +1,8 @@
 class Hysteria < Formula
   desc "Feature-packed proxy & relay tool optimized for lossy, unstable connections"
   homepage "https://hysteria.network/"
-  url "https://github.com/apernet/hysteria/archive/refs/tags/app/v2.0.0.tar.gz"
-  sha256 "06f86cf466cbe08e7aaea68914263780ed4474cd73df9a591676779535d330d5"
+  url "https://github.com/apernet/hysteria/archive/refs/tags/app/v2.0.1.tar.gz"
+  sha256 "5a181f84c1dc1b14680478e03efe11608e743b501b528c5cfa0771462ba1a350"
   license "MIT"
   head "https://github.com/apernet/hysteria.git", branch: "master"
 
