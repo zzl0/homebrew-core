b105337567e4e7a0d0e59de876cccd63a512d3df
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-12 11:25:28 +0000

    pocketbase: update 0.18.4 bottle.

--- Formula/p/pocketbase.rb
+++ Formula/p/pocketbase.rb
@@ -6,13 +6,13 @@ class Pocketbase < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "39f04daad38bb6d47264a7f97b8ddc0e4412f9d4d2fe14edbc97cd2b52aa0447"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "39f04daad38bb6d47264a7f97b8ddc0e4412f9d4d2fe14edbc97cd2b52aa0447"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "39f04daad38bb6d47264a7f97b8ddc0e4412f9d4d2fe14edbc97cd2b52aa0447"
-    sha256 cellar: :any_skip_relocation, ventura:        "4103e0530e97b108feee94944b35c3bdf0c382d55832b677e5fc4227e527b3ac"
-    sha256 cellar: :any_skip_relocation, monterey:       "4103e0530e97b108feee94944b35c3bdf0c382d55832b677e5fc4227e527b3ac"
-    sha256 cellar: :any_skip_relocation, big_sur:        "4103e0530e97b108feee94944b35c3bdf0c382d55832b677e5fc4227e527b3ac"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d6a522e1b3c5d0068e87ee51271deb5db936911d7d762ef9deff68f98541b253"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "42b022ec3c106324c5beb2fc9ec7a72b99f297fd97924b0162e364eeeba569be"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "42b022ec3c106324c5beb2fc9ec7a72b99f297fd97924b0162e364eeeba569be"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "42b022ec3c106324c5beb2fc9ec7a72b99f297fd97924b0162e364eeeba569be"
+    sha256 cellar: :any_skip_relocation, ventura:        "4ad46e83642504c087b06ff9fbf9cd4e95f21e96b424d8ed8f4edfb38a3e8d99"
+    sha256 cellar: :any_skip_relocation, monterey:       "4ad46e83642504c087b06ff9fbf9cd4e95f21e96b424d8ed8f4edfb38a3e8d99"
+    sha256 cellar: :any_skip_relocation, big_sur:        "4ad46e83642504c087b06ff9fbf9cd4e95f21e96b424d8ed8f4edfb38a3e8d99"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7097a0fffb3e154e152b015bb20b0022d3ae46c7efef4ea61151c366ec1c1fe6"
   end
 
   depends_on "go" => :build
