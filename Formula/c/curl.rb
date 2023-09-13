c1578520e20468c259c935a96b861c89c3c92100
Alexander M. Turek <me@derrabus.de>
2023-09-13 08:36:08 +0200

    curl 8.3.0
    * curl 8.3.0
    * Update curl.rb
    
    Closes #142159.
    
    Co-authored-by: Ruoyu Zhong <zhongruoyu@outlook.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/c/curl.rb
+++ Formula/c/curl.rb
@@ -2,11 +2,11 @@ class Curl < Formula
   desc "Get a file from an HTTP, HTTPS or FTP server"
   homepage "https://curl.se"
   # Don't forget to update both instances of the version in the GitHub mirror URL.
-  url "https://curl.se/download/curl-8.2.1.tar.bz2"
-  mirror "https://github.com/curl/curl/releases/download/curl-8_2_1/curl-8.2.1.tar.bz2"
-  mirror "http://fresh-center.net/linux/www/curl-8.2.1.tar.bz2"
-  mirror "http://fresh-center.net/linux/www/legacy/curl-8.2.1.tar.bz2"
-  sha256 "0f1e31ebe336c09ec66381f1532f8350e466e1d02ffe10c4ac44a867f1b9d343"
+  url "https://curl.se/download/curl-8.3.0.tar.bz2"
+  mirror "https://github.com/curl/curl/releases/download/curl-8_3_0/curl-8.3.0.tar.bz2"
+  mirror "http://fresh-center.net/linux/www/curl-8.3.0.tar.bz2"
+  mirror "http://fresh-center.net/linux/www/legacy/curl-8.3.0.tar.bz2"
+  sha256 "051a217095671e925a129ba9e2ff2e223b44b08399003ba50738060955d010ff"
   license "curl"
 
   livecheck do
