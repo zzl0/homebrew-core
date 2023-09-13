3d36c09857d847851f3a181fc5f21b6e5323e7e8
Kreeblah <kreeblah@gmail.com>
2023-09-13 08:51:41 -0700

    dovi_tool 2.0.3 (new formula)
    rename dovitool to dovi_tool
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    dovi_tool: update build and test
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    dovi_tool: update pkg-config for linux build
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/d/dovi_tool.rb
@@ -0,0 +1,33 @@
+class DoviTool < Formula
+  desc "CLI tool for Dolby Vision metadata on video streams"
+  homepage "https://github.com/quietvoid/dovi_tool/"
+  url "https://github.com/quietvoid/dovi_tool/archive/refs/tags/2.0.3.tar.gz"
+  sha256 "6a4afe2733ab1a4eca6ff98c6038f60bbbed5c649b99c239759ccf0b17a3f818"
+  license "MIT"
+  head "https://github.com/quietvoid/dovi_tool.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  on_linux do
+    depends_on "pkg-config" => :build
+    depends_on "fontconfig"
+  end
+
+  def install
+    system "cargo", "install", *std_cargo_args
+    pkgshare.install "assets"
+  end
+
+  test do
+    output = shell_output("#{bin}/dovi_tool info #{pkgshare}/assets/hevc_tests/regular_rpu.bin --frame 0")
+    assert_match <<~EOS, output
+      Parsing RPU file...
+      {
+        "dovi_profile": 8,
+        "header": {
+          "rpu_nal_prefix": 25,
+    EOS
+
+    assert_match "dovi_tool #{version}", shell_output("#{bin}/dovi_tool --version")
+  end
+end
