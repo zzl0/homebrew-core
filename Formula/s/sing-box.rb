428c5855e675121e37974c052b59c1a9c7b203ff
Rui Chen <rui@chenrui.dev>
2023-09-13 09:40:22 -0400

    sing-box 1.4.3
    sing-box: update sha
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/s/sing-box.rb
+++ Formula/s/sing-box.rb
@@ -2,8 +2,8 @@ class SingBox < Formula
   desc "Universal proxy platform"
   homepage "https://sing-box.sagernet.org"
   # using `:homebrew_curl` to work around audit failure from TLS 1.3-only homepage
-  url "https://github.com/SagerNet/sing-box/archive/refs/tags/v1.4.2.tar.gz", using: :homebrew_curl
-  sha256 "afed0fcfcf5ff92a8e0496af0813366cdd14279cfb08ab45a4d1309dd62191ae"
+  url "https://github.com/SagerNet/sing-box/archive/refs/tags/v1.4.3.tar.gz", using: :homebrew_curl
+  sha256 "baf7c87f2e5005bf268975b1a2511f30927210b1607f20451fec2de0044edfa8"
   license "GPL-3.0-or-later"
 
   bottle do
