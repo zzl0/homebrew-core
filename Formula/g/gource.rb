55e2c25b965125b48d11e292eb51e0c8df884739
Sean Molenaar <smillerdev@me.com>
2023-09-14 11:22:32 +0200

    gource: revision bump for boost 1.83

--- Formula/g/gource.rb
+++ Formula/g/gource.rb
@@ -4,7 +4,7 @@ class Gource < Formula
   url "https://github.com/acaudwell/Gource/releases/download/gource-0.54/gource-0.54.tar.gz"
   sha256 "1dcbcedf65d2cf4d69fe0b633e54c202926c08b829bcad0b73eaf9e29cd6fae5"
   license "GPL-3.0-or-later"
-  revision 1
+  revision 2
 
   bottle do
     sha256 arm64_sonoma:   "12890c4d132262201c394013213294907bbdd62b923533dff002358e90b801f6"
