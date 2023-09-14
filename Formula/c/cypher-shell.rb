b636dbe06141e38f9127d8eb3d0af811402d6e68
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-14 20:13:58 +0000

    cypher-shell: update 5.12.0 bottle.

--- Formula/c/cypher-shell.rb
+++ Formula/c/cypher-shell.rb
@@ -12,7 +12,7 @@ class CypherShell < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "3bc8944ae76dcfc11acd9f96fd86cdbca239d401427fec279bd01fa0d5eeb062"
+    sha256 cellar: :any_skip_relocation, all: "7c1b00cdfc86eb433203b0133c9568968c5fd8d11702cd061fbce8a29ee14d0e"
   end
 
   depends_on "openjdk"
