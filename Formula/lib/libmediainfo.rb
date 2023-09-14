49a7bb5208a4a0bab5ee43dfd5c302e19498b7de
Rui Chen <rui@chenrui.dev>
2023-09-14 10:27:02 -0400

    libmediainfo 23.09
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/lib/libmediainfo.rb
+++ Formula/lib/libmediainfo.rb
@@ -1,8 +1,8 @@
 class Libmediainfo < Formula
   desc "Shared library for mediainfo"
   homepage "https://mediaarea.net/en/MediaInfo"
-  url "https://mediaarea.net/download/source/libmediainfo/23.07/libmediainfo_23.07.tar.xz"
-  sha256 "60456c8b2ab8769a6081d96fd7be86db4fe32520e4a022397cb22cacf47ce820"
+  url "https://mediaarea.net/download/source/libmediainfo/23.09/libmediainfo_23.09.tar.xz"
+  sha256 "1c326f166572ac6c2c1c88a6d53af7e451388840c7eebeec71d5abbfd1bb2ed3"
   license "BSD-2-Clause"
   head "https://github.com/MediaArea/MediaInfoLib.git", branch: "master"
 
