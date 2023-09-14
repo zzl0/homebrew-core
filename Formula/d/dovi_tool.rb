f01d75ae3a606615db1a671bc8880f6ca00a7f05
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-14 17:25:59 +0000

    dovi_tool: add 2.0.3 bottle.

--- Formula/d/dovi_tool.rb
+++ Formula/d/dovi_tool.rb
@@ -6,6 +6,16 @@ class DoviTool < Formula
   license "MIT"
   head "https://github.com/quietvoid/dovi_tool.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "be9203db492cbbc19a2be63bc6e3c38afbf400cc7a248bddc1bbc154082230f7"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2969966d41ac5f184920327758aef057d6c494efc5bf30c689825d2bd2f288b7"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "18b6fa8b33d62fceee6e7d3f93476ccf8aa434d47a92908b6a7abe6d3994ca07"
+    sha256 cellar: :any_skip_relocation, ventura:        "20d201b2018edece99efd7d2697dde13fa178d45427ec67e403293a403aa265b"
+    sha256 cellar: :any_skip_relocation, monterey:       "571131171d3f95269479c29bfbfd0caf2ea4ad8fdbeee37ae17fea39ae845e15"
+    sha256 cellar: :any_skip_relocation, big_sur:        "5c1b7416c7abb7b987e03f50929c8cabbc8c8b8907e4fa3d834c4173b630f52e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "68f211eeb42e98aba295b3fbdfaa708e2542c7dbf17e382e51b5fb4ef620efff"
+  end
+
   depends_on "rust" => :build
 
   on_linux do
