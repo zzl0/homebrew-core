a86db854c6f23112b73ea9e6255122c4e9960605
Sean Molenaar <smillerdev@me.com>
2023-09-14 11:20:38 +0200

    bastet: revision bump for boost 1.83

--- Formula/b/bastet.rb
+++ Formula/b/bastet.rb
@@ -4,7 +4,7 @@ class Bastet < Formula
   url "https://github.com/fph/bastet/archive/0.43.2.tar.gz"
   sha256 "f219510afc1d83e4651fbffd5921b1e0b926d5311da4f8fa7df103dc7f2c403f"
   license "GPL-3.0-or-later"
-  revision 5
+  revision 6
 
   bottle do
     sha256 arm64_sonoma:   "441eef57cde0386b5da6340b9d2358a8311666477153cc2e06d8c0b43d00330f"
