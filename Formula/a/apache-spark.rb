1482d5fe50d2abcec3a30b885210570ecca357a0
Rui Chen <rui@chenrui.dev>
2023-09-14 11:02:30 -0400

    apache-spark 3.5.0

--- Formula/a/apache-spark.rb
+++ Formula/a/apache-spark.rb
@@ -1,10 +1,10 @@
 class ApacheSpark < Formula
   desc "Engine for large-scale data processing"
   homepage "https://spark.apache.org/"
-  url "https://dlcdn.apache.org/spark/spark-3.4.1/spark-3.4.1-bin-hadoop3.tgz"
-  mirror "https://archive.apache.org/dist/spark/spark-3.4.1/spark-3.4.1-bin-hadoop3.tgz"
-  version "3.4.1"
-  sha256 "de24e511aebd95e7408c636fde12d19391f57a33730fe30735d6742180e338d4"
+  url "https://dlcdn.apache.org/spark/spark-3.5.0/spark-3.5.0-bin-hadoop3.tgz"
+  mirror "https://archive.apache.org/dist/spark/spark-3.5.0/spark-3.5.0-bin-hadoop3.tgz"
+  version "3.5.0"
+  sha256 "ac064ec620129b4b9630e180e25ac2b71a8ccd4c6912bf5b5631943b742f0379"
   license "Apache-2.0"
   head "https://github.com/apache/spark.git", branch: "master"
 
