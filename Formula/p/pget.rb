c980f527fe5a01276aa03033d1ec7060ba7d8050
Rui Chen <rui@chenrui.dev>
2023-09-15 00:07:04 -0400

    pget 0.2.1

--- Formula/p/pget.rb
+++ Formula/p/pget.rb
@@ -1,8 +1,8 @@
 class Pget < Formula
   desc "File download client"
   homepage "https://github.com/Code-Hex/pget"
-  url "https://github.com/Code-Hex/pget/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "3619c32ed021f22262d19edadc678620e2cf2d69acecf31c044e5dc60899d485"
+  url "https://github.com/Code-Hex/pget/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "fa7646bec975dd3995fb45d6b1f190565d6c4fae03c46c4eda34716c83ede03e"
   license "MIT"
   head "https://github.com/Code-Hex/pget.git", branch: "master"
 
