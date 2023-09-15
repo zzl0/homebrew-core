ee566f299b366bc76935d24dc873e20e19079e35
Anatoli Babenia <anatoli@rainforce.org>
2023-09-15 20:06:36 +0300

    web-ext 7.7.0 (new formula)
    web-ext: fix tap_syntax failures
    
    web-ext: assert command response in test
    
    `web-ext lint` output is made by `addons-linter` and it contains
    trailing whitespace from `columnify` library
    
    https://github.com/mozilla/addons-linter/blob/025e033696b42b27753ae31cbf8d5dde8138d301/src/linter.js#L174-L180
    
    Related `columnify` issue
    
    https://github.com/timoxley/columnify/issues/46
    
    web-ext: tap_syntax fixes
    
    web-ext: copy terminal-notifier fix from vue-cli
    
    web-ext: update test
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/w/web-ext.rb
@@ -0,0 +1,53 @@
+require "language/node"
+
+class WebExt < Formula
+  desc "Command-line tool to help build, run, and test web extensions"
+  homepage "https://github.com/mozilla/web-ext"
+  url "https://registry.npmjs.org/web-ext/-/web-ext-7.7.0.tgz"
+  sha256 "482e9eab19c3df4cd353c20bc099c483bf0587a6120482ea811216cf7a02136a"
+  license "MPL-2.0"
+
+  depends_on "node"
+
+  on_macos do
+    depends_on "terminal-notifier"
+  end
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+
+    # Remove vendored pre-built binary `terminal-notifier`
+    node_notifier_vendor_dir = libexec/"lib/node_modules/web-ext/node_modules/node-notifier/vendor"
+    node_notifier_vendor_dir.rmtree # remove vendored pre-built binaries
+
+    if OS.mac?
+      terminal_notifier_dir = node_notifier_vendor_dir/"mac.noindex"
+      terminal_notifier_dir.mkpath
+
+      # replace vendored `terminal-notifier` with our own
+      terminal_notifier_app = Formula["terminal-notifier"].opt_prefix/"terminal-notifier.app"
+      ln_sf terminal_notifier_app.relative_path_from(terminal_notifier_dir), terminal_notifier_dir
+    end
+  end
+
+  test do
+    (testpath/"manifest.json").write <<~EOF
+      {
+        "manifest_version": 2,
+        "name": "minimal web extension",
+        "version": "0.0.1"
+      }
+    EOF
+    assert_equal <<~EOF, shell_output("#{bin}/web-ext lint").gsub(/ +$/, "")
+      Validation Summary:
+
+      errors          0
+      notices         0
+      warnings        0
+
+    EOF
+
+    assert_match version.to_s, shell_output("#{bin}/web-ext --version")
+  end
+end
