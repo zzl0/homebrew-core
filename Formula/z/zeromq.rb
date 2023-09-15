dac506dbd8363f080a423e5a95c7589915f73afe
Rui Chen <rui@chenrui.dev>
2023-09-15 00:05:36 -0400

    zeromq: revision bump (libsodium 1.0.19)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/z/zeromq.rb
+++ Formula/z/zeromq.rb
@@ -4,6 +4,7 @@ class Zeromq < Formula
   url "https://github.com/zeromq/libzmq/releases/download/v4.3.5/zeromq-4.3.5.tar.gz"
   sha256 "6653ef5910f17954861fe72332e68b03ca6e4d9c7160eb3a8de5a5a913bfab43"
   license "MPL-2.0"
+  revision 1
 
   livecheck do
     url :stable
