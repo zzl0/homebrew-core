9a2a9d7057c93ceaf9895fb3d317c7c01c740474
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-16 03:04:17 +0000

    web-ext: add 7.7.0 bottle.

--- Formula/w/web-ext.rb
+++ Formula/w/web-ext.rb
@@ -7,6 +7,16 @@ class WebExt < Formula
   sha256 "482e9eab19c3df4cd353c20bc099c483bf0587a6120482ea811216cf7a02136a"
   license "MPL-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "94f3ea601503d9e184d3406cc83e63ff6d6d2cd2a88db7d150b27fbf695cecb4"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3f3ea7d994c7ad54e723410298c1054efbce799ae51ada353473212a12eba88d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "dcee44d567bf4cf3f2ed0b72366918286377650f5679242eb3fd8828e5c10660"
+    sha256 cellar: :any_skip_relocation, ventura:        "0fa28ef9228b232735d19454481e029c3b806b2c7e8111fd61c0b8751689114e"
+    sha256 cellar: :any_skip_relocation, monterey:       "eec58db2156b115a89699df713d469a80085c7cb3b50492c87df59cc1b09e40e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "8781576a093945a0c131a0c5580e929615a5196e4754cffd6e1a3bfc80fe94f1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2393064ee493ef241ed67ae6a0a1fa7fa95c6b38569839720b0b7a059afaa5cf"
+  end
+
   depends_on "node"
 
   on_macos do
