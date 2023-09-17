63e51aca8628ea017a5b6e688f770b79e89709cc
Rui Chen <rui@chenrui.dev>
2023-09-17 09:43:45 -0400

    lf 31

--- Formula/l/lf.rb
+++ Formula/l/lf.rb
@@ -1,8 +1,8 @@
 class Lf < Formula
   desc "Terminal file manager"
   homepage "https://godoc.org/github.com/gokcehan/lf"
-  url "https://github.com/gokcehan/lf/archive/r30.tar.gz"
-  sha256 "6f8bc88797710926867a74f3ef68e408e05758bb399266d2841bf0d341d4c146"
+  url "https://github.com/gokcehan/lf/archive/r31.tar.gz"
+  sha256 "ed47fc22c58cf4f4e4116a58c500bdb9f9ccc0b89f87be09f321e8d1431226ab"
   license "MIT"
 
   bottle do
