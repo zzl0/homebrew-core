90d763c35e9151167e2504ed4496a6fee91630da
Rui Chen <rui@chenrui.dev>
2023-09-17 23:29:10 -0400

    htmldoc 1.9.17
    htmldoc: needs cups for linux build
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/h/htmldoc.rb
+++ Formula/h/htmldoc.rb
@@ -1,10 +1,9 @@
 class Htmldoc < Formula
   desc "Convert HTML to PDF or PostScript"
   homepage "https://www.msweet.org/htmldoc/"
-  url "https://github.com/michaelrsweet/htmldoc/archive/v1.9.16.tar.gz"
-  sha256 "f0d19d8be0fd961d07556f85dbea1d95f0d38728a45dc0f2cf92c715e4140542"
+  url "https://github.com/michaelrsweet/htmldoc/archive/v1.9.17.tar.gz"
+  sha256 "438cbd4c6673a9e156ef9a4793540762e8438298063cac019a45f55ac577872e"
   license "GPL-2.0-only"
-  revision 1
   head "https://github.com/michaelrsweet/htmldoc.git", branch: "master"
 
   bottle do
@@ -22,6 +21,7 @@ class Htmldoc < Formula
   depends_on "jpeg-turbo"
   depends_on "libpng"
 
+  uses_from_macos "cups"
   uses_from_macos "zlib"
 
   on_linux do
