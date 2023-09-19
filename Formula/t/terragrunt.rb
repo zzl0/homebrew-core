8f911f1d82f981ef2576136a23a734a2f9bf1445
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-09-19 19:16:34 -0400

    terragrunt 0.51.3

--- Formula/t/terragrunt.rb
+++ Formula/t/terragrunt.rb
@@ -1,8 +1,8 @@
 class Terragrunt < Formula
   desc "Thin wrapper for Terraform e.g. for locking state"
   homepage "https://terragrunt.gruntwork.io/"
-  url "https://github.com/gruntwork-io/terragrunt/archive/refs/tags/v0.51.1.tar.gz"
-  sha256 "2273fa8dffba8488c5e58f7eea720f8978ad5b1ee31cc92daae7add9e7c31c80"
+  url "https://github.com/gruntwork-io/terragrunt/archive/refs/tags/v0.51.3.tar.gz"
+  sha256 "f6c002d323108f4dcd8789b956481ba8ccf5c0c66c0b0e8535cf64c89c5046a2"
   license "MIT"
 
   bottle do
