050a9e8945bfc1e9b75f980eef0879044bfe8537
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-09-19 19:14:16 -0400

    stellar-core 19.14.0

--- Formula/s/stellar-core.rb
+++ Formula/s/stellar-core.rb
@@ -2,8 +2,8 @@ class StellarCore < Formula
   desc "Backbone of the Stellar (XLM) network"
   homepage "https://www.stellar.org/"
   url "https://github.com/stellar/stellar-core.git",
-      tag:      "v19.13.0",
-      revision: "c2599d62274931c11ea7f36c5ee9255a202f5739"
+      tag:      "v19.14.0",
+      revision: "5664eff4e76ca6a277883d4085711dc3fa7c318a"
   license "Apache-2.0"
   head "https://github.com/stellar/stellar-core.git", branch: "master"
 
