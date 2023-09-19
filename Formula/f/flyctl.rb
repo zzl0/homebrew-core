d842c63ee2e0e5ab595bbd290c063b6a1c871708
Nhan Doan <2561973+dnhn@users.noreply.github.com>
2023-09-19 22:22:23 +0700

    flyctl 0.1.93

--- Formula/f/flyctl.rb
+++ Formula/f/flyctl.rb
@@ -2,8 +2,8 @@ class Flyctl < Formula
   desc "Command-line tools for fly.io services"
   homepage "https://fly.io"
   url "https://github.com/superfly/flyctl.git",
-      tag:      "v0.1.92",
-      revision: "f8e5cccff1f16869d16ad2f8b95f4a396ec771b7"
+      tag:      "v0.1.93",
+      revision: "0a198d277fa3de01bc2334b493ae6bb674e11d55"
   license "Apache-2.0"
   head "https://github.com/superfly/flyctl.git", branch: "master"
 
