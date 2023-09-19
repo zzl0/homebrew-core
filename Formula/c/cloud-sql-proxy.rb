6d5579a12b34dd4138097358eadcee8ea1d1c118
Rui Chen <rui@chenrui.dev>
2023-09-19 19:01:54 -0400

    cloud-sql-proxy 2.7.0

--- Formula/c/cloud-sql-proxy.rb
+++ Formula/c/cloud-sql-proxy.rb
@@ -1,8 +1,8 @@
 class CloudSqlProxy < Formula
   desc "Utility for connecting securely to your Cloud SQL instances"
   homepage "https://github.com/GoogleCloudPlatform/cloud-sql-proxy"
-  url "https://github.com/GoogleCloudPlatform/cloud-sql-proxy/archive/v2.6.1.tar.gz"
-  sha256 "b5c4fd1decb2d4091f0ae118e2df8387cbee7ea166047be12c4183fbeecf0c46"
+  url "https://github.com/GoogleCloudPlatform/cloud-sql-proxy/archive/v2.7.0.tar.gz"
+  sha256 "75911bbfe61a46d70ae0f6e1370d765164dab23fe2138f57bab938ebaed6618f"
   license "Apache-2.0"
   head "https://github.com/GoogleCloudPlatform/cloud-sql-proxy.git", branch: "main"
 
