079ae6b745a8efcdf903ec68b3332e6a16787a1c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-19 12:10:51 +0000

    metabase: update 0.47.2 bottle.

--- Formula/m/metabase.rb
+++ Formula/m/metabase.rb
@@ -11,13 +11,13 @@ class Metabase < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "011f19438629fb777f5c3885cdf1a3dcb8a395a999d23c0fe5cee56b1d299f9b"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "011f19438629fb777f5c3885cdf1a3dcb8a395a999d23c0fe5cee56b1d299f9b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "011f19438629fb777f5c3885cdf1a3dcb8a395a999d23c0fe5cee56b1d299f9b"
-    sha256 cellar: :any_skip_relocation, ventura:        "011f19438629fb777f5c3885cdf1a3dcb8a395a999d23c0fe5cee56b1d299f9b"
-    sha256 cellar: :any_skip_relocation, monterey:       "011f19438629fb777f5c3885cdf1a3dcb8a395a999d23c0fe5cee56b1d299f9b"
-    sha256 cellar: :any_skip_relocation, big_sur:        "011f19438629fb777f5c3885cdf1a3dcb8a395a999d23c0fe5cee56b1d299f9b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "84a973aecbc17ada9da22a4a881b171a2802a281c5e60d21860885266d0528d8"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "9ce04ebc6493e6be13ad9e2a7440d3e7329bf96d7093ae26e546f3cb27c2c198"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "9ce04ebc6493e6be13ad9e2a7440d3e7329bf96d7093ae26e546f3cb27c2c198"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9ce04ebc6493e6be13ad9e2a7440d3e7329bf96d7093ae26e546f3cb27c2c198"
+    sha256 cellar: :any_skip_relocation, ventura:        "9ce04ebc6493e6be13ad9e2a7440d3e7329bf96d7093ae26e546f3cb27c2c198"
+    sha256 cellar: :any_skip_relocation, monterey:       "9ce04ebc6493e6be13ad9e2a7440d3e7329bf96d7093ae26e546f3cb27c2c198"
+    sha256 cellar: :any_skip_relocation, big_sur:        "9ce04ebc6493e6be13ad9e2a7440d3e7329bf96d7093ae26e546f3cb27c2c198"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8baf06f41e7d0971c3ab469630f59507dfa8dcdafa116d94ad1f09ee71a2606f"
   end
 
   head do
