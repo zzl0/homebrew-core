4c02a2fbb9071d3f83e84c7c3d34ae57bae9ec83
Mislav Marohnić <hi@mislav.net>
2023-09-19 15:24:02 +0200

    ruby-build 20230919
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/r/ruby-build.rb
+++ Formula/r/ruby-build.rb
@@ -1,8 +1,8 @@
 class RubyBuild < Formula
   desc "Install various Ruby versions and implementations"
   homepage "https://github.com/rbenv/ruby-build"
-  url "https://github.com/rbenv/ruby-build/archive/v20230914.1.tar.gz"
-  sha256 "69fd968c2cb28a374deaa705d457b3b101f7ea3685e2eb3ee99b3737ba437bc9"
+  url "https://github.com/rbenv/ruby-build/archive/v20230919.tar.gz"
+  sha256 "367956ebb1c5bdda3802f27de87367cd1aaf319ed7249ce8133481fb894bef4d"
   license "MIT"
   head "https://github.com/rbenv/ruby-build.git", branch: "master"
 
