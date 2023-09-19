c38d7fa7663922fe4de0c0a5230f06b138bba476
Rui Chen <rui@chenrui.dev>
2023-09-19 12:36:24 -0400

    bandwhich 0.21.0
    bandwhich: remove build patch
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/b/bandwhich.rb
+++ Formula/b/bandwhich.rb
@@ -1,8 +1,8 @@
 class Bandwhich < Formula
   desc "Terminal bandwidth utilization tool"
   homepage "https://github.com/imsnif/bandwhich"
-  url "https://github.com/imsnif/bandwhich/archive/0.20.0.tar.gz"
-  sha256 "4bbf05be32439049edd50bd1e4d5a2a95b0be8d36782e4100732f0cc9f19ba12"
+  url "https://github.com/imsnif/bandwhich/archive/refs/tags/v0.21.0.tar.gz"
+  sha256 "f9c50c340372593bf4c54fcf2608ef37c2c56a37367b2f430c27cce3ea947828"
   license "MIT"
 
   bottle do
@@ -20,14 +20,6 @@ class Bandwhich < Formula
   end
 
   depends_on "rust" => :build
-
-  # patch build
-  # upstream issue, https://github.com/imsnif/bandwhich/issues/258
-  # upstream PR, https://github.com/imsnif/bandwhich/pull/259
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/96bfb409db46dfe53b76da7682ddcf650af45921/bandwhich/0.20.0.patch"
-    sha256 "ea446f63c9e766ab9c987c83f1ca5f6759175df4e2b3e377604fd87a2b0b26de"
-  end
 
   def install
     system "cargo", "install", *std_cargo_args
