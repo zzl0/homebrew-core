9348700579e4ffe2da8e5ba8208aec30ad67e3c7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-19 13:28:46 +0000

    sourcery: update 2.1.1 bottle.

--- Formula/s/sourcery.rb
+++ Formula/s/sourcery.rb
@@ -8,11 +8,9 @@ class Sourcery < Formula
   head "https://github.com/krzysztofzablocki/Sourcery.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3df6faef9028d8647dc275cad471f255d84a1befeeb28ca601bc517de1e705db"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "b18f07023f563c3152ea345e09cbd3fcbd2cb7cf6aa6938c228717dabbd26f81"
-    sha256 cellar: :any_skip_relocation, sonoma:        "e310f2b9ff6249a7fbdddebe234ba8426a253406d5e22932f0df4180b28f7ce5"
-    sha256 cellar: :any_skip_relocation, ventura:       "60aae10574ccb3e26d2e9c0e03ec5e05cd626e89e91f414732f2177a5a1e6c6e"
-    sha256                               x86_64_linux:  "d04a75e6f9432d5f45aeb7b71c5607b74dd395b71f978c394db3782e9b78532e"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "4b830387533d8a4356e35ddb896b8be2bd80da75dcddf5ccb213a1f3604dd44d"
+    sha256 cellar: :any_skip_relocation, ventura:       "fd06b30db30585ce954a51b22c13c7512e3024f859f159dd313156d2a5ea6c36"
+    sha256                               x86_64_linux:  "f2903a70b16fb835bcd91c90b28b8dbae75418f9881959266d2459aa8212a966"
   end
 
   depends_on xcode: "14.3"
