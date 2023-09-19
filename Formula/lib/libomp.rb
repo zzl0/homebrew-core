304d2854dba670c323512981fddd062b6df5c26a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-19 23:18:56 +0000

    libomp: update 17.0.1 bottle.

--- Formula/lib/libomp.rb
+++ Formula/lib/libomp.rb
@@ -11,9 +11,11 @@ class Libomp < Formula
   end
 
   bottle do
+    sha256 cellar: :any,                 arm64_sonoma:   "0b49f04d30dafe89e3e986986dbd5c8c2939e50eeee76b38516ff7ca03fc6f14"
     sha256 cellar: :any,                 arm64_ventura:  "3837963331024d9dac0d767143319e0fd8969da62e034337c252df18204b990d"
     sha256 cellar: :any,                 arm64_monterey: "5ae86654170bbc1f799bf25e72fd55fb00c99ebbde1d6676df832d3886b0f1fa"
     sha256 cellar: :any,                 arm64_big_sur:  "06ce8da414325ee89fce6de57622da8c150b58d4b392edf711184123053962d9"
+    sha256 cellar: :any,                 sonoma:         "aa4303595c1a81cf7008ec7930b915a95140710a808ca4cb08bba69c8c7ff158"
     sha256 cellar: :any,                 ventura:        "5e794b1f3d68a9e2b721c6a1875892968cb0da74e7097dde07a89d6908200397"
     sha256 cellar: :any,                 monterey:       "5e02c68a28a98c1c34b3436061484458ae49c4f21ec7fb6ffa0b9d9d43e6e454"
     sha256 cellar: :any,                 big_sur:        "b969a154c0c132501a82a9dcd64952a179c5a9a424cf3a1f93419920e8a83be9"
