b29239de3ee3e655a1bc1d4dc788797869164c20
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-09-19 19:37:51 -0400

    sslscan 2.1.1

--- Formula/s/sslscan.rb
+++ Formula/s/sslscan.rb
@@ -1,8 +1,8 @@
 class Sslscan < Formula
   desc "Test SSL/TLS enabled services to discover supported cipher suites"
   homepage "https://github.com/rbsec/sslscan"
-  url "https://github.com/rbsec/sslscan/archive/2.1.0.tar.gz"
-  sha256 "3140af055f067411a726f1061d8d7fd79e08fd4286e228dc3a230067b5704a72"
+  url "https://github.com/rbsec/sslscan/archive/2.1.1.tar.gz"
+  sha256 "ccb1ffcc97cbde5c184542debe2ac3529e2c64b3690a402b592ed4ee374955e1"
   license "GPL-3.0-or-later" => { with: "openvpn-openssl-exception" }
   head "https://github.com/rbsec/sslscan.git", branch: "master"
 
