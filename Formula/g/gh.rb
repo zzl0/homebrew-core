e2aac63a2c215c104d913d623d1175c3eaed5fe9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-19 19:41:54 +0000

    gh: update 2.35.0 bottle.

--- Formula/g/gh.rb
+++ Formula/g/gh.rb
@@ -13,9 +13,11 @@ class Gh < Formula
   end
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "c7a6da5d934a5ad0660ec124a3995b4dd18a27a6da6532e1881f36e65bbcddb1"
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "a96c8a08558fc48df45de2149ff6d0fe11e4628da5451ba5c91a116221761f04"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "d28d628b393e8edeedb5b06c599ead82f071a0e3844837d58884f095a2777e55"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ed1021da6ff37ff5b35e73e55c1733d949d126af8a9ac344c327b841bd6b7d4c"
+    sha256 cellar: :any_skip_relocation, sonoma:         "ec86bd47e4b645a95f9088a0b1a3c7d3fbed3158b9f361a89afb5d8a49ff5546"
     sha256 cellar: :any_skip_relocation, ventura:        "e6e48fcda77833de8b236e1ac8a0de2c7604206cd96c7988dd9c9fc69602bc3d"
     sha256 cellar: :any_skip_relocation, monterey:       "a3f0705282b55d3c78e66bd80ef38e2b6ef59dfee5185ec5626a8ca7c38afefe"
     sha256 cellar: :any_skip_relocation, big_sur:        "df3e928799bea2d9ba43905300d8cf992967828057089026369fc6e2e7487d8b"
