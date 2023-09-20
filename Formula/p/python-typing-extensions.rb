8306aebdf318e9b6e7501e0170303a6e262d2f1d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-20 05:37:18 +0000

    python-typing-extensions: update 4.8.0 bottle.

--- Formula/p/python-typing-extensions.rb
+++ Formula/p/python-typing-extensions.rb
@@ -6,9 +6,11 @@ class PythonTypingExtensions < Formula
   license "Python-2.0"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "929bc38a9d739276be2baa68ff25d7adcbd469ed6f4c35d0f3e596d91b5b686f"
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "156ebcbfa1417d709fbcb9652b7fe4d8b7c80851a130cd1487a41032b65e88bc"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "df903f019ad19a03847d49e1c9c314b1487ebf3165237dc96affaff4732365bf"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b31cd7c96ea59569d16a1d7a6fb2b54e94e9f39bb1ad9e763a40fa92a84aa260"
+    sha256 cellar: :any_skip_relocation, sonoma:         "aea58a751d8b1b9fd8e15d9d139721433e6c3f4522e254c8e19fba0c03d3773a"
     sha256 cellar: :any_skip_relocation, ventura:        "51a7c46b5d2c640e8e417f85f71de2a93628635c5a47a718b072459e532c2920"
     sha256 cellar: :any_skip_relocation, monterey:       "5278ebee95997fc4b3a71a36f826a0fc5c7281c01fbf1f68b687e3d897dbb321"
     sha256 cellar: :any_skip_relocation, big_sur:        "c26c7e839a1d8552feb80b944dbcb780b8e7b0efbf37341f1d5b034e32efb852"
