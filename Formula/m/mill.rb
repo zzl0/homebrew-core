10bd2accf928990c4787580a484d6abfeac85574
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-09-20 22:06:34 +0800

    mill: switch to `openjdk@17`
    Regression seen in #144045, likely due to Scala not fully supporting
    Java 21 (yet).

--- Formula/m/mill.rb
+++ Formula/m/mill.rb
@@ -1,6 +1,7 @@
 class Mill < Formula
   desc "Scala build tool"
   homepage "https://com-lihaoyi.github.io/mill/mill/Intro_to_Mill.html"
+  # TODO: Check if we can use unversioned `openjdk` (or `openjdk@21`) at version bump.
   url "https://github.com/com-lihaoyi/mill/releases/download/0.11.4/0.11.4-assembly"
   sha256 "e17fae7d31c408e98cefded2374545004f1bf669c791d3aa489f21dbd3ee8cb5"
   license "MIT"
@@ -14,12 +15,12 @@ class Mill < Formula
     sha256 cellar: :any_skip_relocation, all: "b220c94709073b446cf6a428484aa3589915d072e12d3573a1d382311209acb1"
   end
 
-  depends_on "openjdk"
+  depends_on "openjdk@17"
 
   def install
     libexec.install Dir["*"].shift => "mill"
     chmod 0555, libexec/"mill"
-    (bin/"mill").write_env_script libexec/"mill", Language::Java.overridable_java_home_env
+    (bin/"mill").write_env_script libexec/"mill", Language::Java.overridable_java_home_env("17")
   end
 
   test do
