d8b8cb63be39c0fa8c372913c141ad0357f0f032
git <badonyx@proton.me>
2023-09-20 18:36:24 +0000

    gossip 0.8.1 (new formula)

---
+++ Formula/g/gossip.rb
@@ -0,0 +1,38 @@
+class Gossip < Formula
+  desc "Desktop client for Nostr written in Rust"
+  homepage "https://github.com/mikedilger/gossip"
+  url "https://github.com/mikedilger/gossip.git",
+      tag:      "v0.8.1",
+      revision: "404f5a405c8796e90bb5cc078378149d3c341f26"
+  license "MIT"
+  head "https://github.com/mikedilger/gossip.git", branch: "master"
+
+  depends_on "rust" => :build
+
+  on_linux do
+    depends_on "libxkbcommon"
+    depends_on "mesa"
+  end
+
+  def install
+    ENV["RUSTFLAGS"] = "--cfg tokio_unstable"
+    system "cargo", "install", *std_cargo_args, "--features", "lang-cjk"
+  end
+
+  test do
+    mkdir_p testpath/"Library/Application Support" # for macos
+    mkdir_p testpath/".local/share" # for linux
+    json = <<~JSON
+      {
+        "id": "b9fead6eef87d8400cbc1a5621600b360438affb9760a6a043cc0bddea21dab6",
+        "kind": 1,
+        "pubkey": "82341f882b6eabcd2ba7f1ef90aad961cf074af15b9ef44a09f9d2a8fbfbe6a2",
+        "created_at": 1676161639,
+        "content": "this is going to work",
+        "tags": [],
+        "sig": "76d19889a803236165a290fa8f3cf5365af8977ee1e002afcfd37063d1355fc755d0293d27ba0ec1c2468acfaf95b7e950e57df275bb32d7a4a3136f8862d2b7"
+      }
+    JSON
+    assert_match "Valid event", shell_output("#{bin}/gossip verify_json '#{json}'")
+  end
+end
