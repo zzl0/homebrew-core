dace6bbc845aa0bf8310951a350a62f8126128d3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-20 22:46:38 +0000

    mapcidr: update 1.1.6 bottle.

--- Formula/m/mapcidr.rb
+++ Formula/m/mapcidr.rb
@@ -7,9 +7,11 @@ class Mapcidr < Formula
   head "https://github.com/projectdiscovery/mapcidr.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "c7e221d78957e894d0d6373ac007a743ce396f475db449019d352b54bad285ad"
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "cda0ba04c82b831aab9ccaffabd4f99503f9bbc29d0d42daf27c8180f64732ca"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "3dddd067aeaf96470900d2235fdbbde77456b86540f01b51e1580343f73a35cb"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d2e4d5016e2925c5caa59a212efac8a967db2284a770a21a1574ca1f2eb35b4b"
+    sha256 cellar: :any_skip_relocation, sonoma:         "ee77712903516cf2b34a2d7136aa63464283931825aab1007facfbaa6d3cbe10"
     sha256 cellar: :any_skip_relocation, ventura:        "099a60f5081a83c16dfb0b496fe5a37e6dc9fb9dd7a7c12a36bcaee9ba8a3d8d"
     sha256 cellar: :any_skip_relocation, monterey:       "77a4c5eed6ddee795bbee7aaee8a2492ae661fa1450bfacf1e821709ad90a870"
     sha256 cellar: :any_skip_relocation, big_sur:        "60c26e628732a5240ecfb5591405da9777311778e251e53ee81347e3c46c583c"
