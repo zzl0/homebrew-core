a0e87996291840a2c6971109ce48c4557bbae49a
Rui Chen <rui@chenrui.dev>
2023-09-21 14:31:45 -0400

    mcpp: add build patch
    ```
      clang -DHAVE_CONFIG_H -I.     -g -O2 -c -o mcpp-main_libmcpp.o `test -f 'main_libmcpp.c' || echo './'`main_libmcpp.c
      system.c:2510:20: error: call to undeclared function 'readlink'; ISO C99 and later do not support implicit function declarations [-Wimplicit-function-declaration]
              if ((len = readlink( slbuf1, slbuf2, PATHMAX)) > 0) {
                         ^
      system.c:2685:20: error: call to undeclared function 'readlink'; ISO C99 and later do not support implicit function declarations [-Wimplicit-function-declaration]
              if ((len = readlink( slbuf1, slbuf2, PATHMAX)) > 0) {
                         ^
      2 errors generated.
    ```
    
    https://github.com/Homebrew/homebrew-core/actions/runs/6260707830/job/16999312330
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/m/mcpp.rb
+++ Formula/m/mcpp.rb
@@ -34,8 +34,10 @@ class Mcpp < Formula
   end
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}",
+    # Workaround for Xcode 14.3
+    ENV.append_to_cflags "-Wno-implicit-function-declaration" if DevelopmentTools.clang_build_version >= 1403
+
+    system "./configure", *std_configure_args,
                           "--enable-mcpplib"
     system "make", "install"
   end
