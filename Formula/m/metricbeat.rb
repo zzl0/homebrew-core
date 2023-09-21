458df15042dee825bb070fcfd56e1730d0004907
Alexander Frisch <crunchtime-ali@users.noreply.github.com>
2023-09-21 12:18:45 +0000

    metricbeat 8.10.2

--- Formula/m/metricbeat.rb
+++ Formula/m/metricbeat.rb
@@ -2,8 +2,8 @@ class Metricbeat < Formula
   desc "Collect metrics from your systems and services"
   homepage "https://www.elastic.co/beats/metricbeat"
   url "https://github.com/elastic/beats.git",
-      tag:      "v8.10.1",
-      revision: "ba6a0bcef9ec28790a10888070eab35b95277e38"
+      tag:      "v8.10.2",
+      revision: "480bccf4f0423099bb2c0e672a44c54ecd7a805e"
   license "Apache-2.0"
   head "https://github.com/elastic/beats.git", branch: "master"
 
