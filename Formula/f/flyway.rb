5fed870ee32885d9daadd2aee5d6c9e0c65cf7c1
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-09-21 18:55:01 +0800

    flyway 9.22.2

--- Formula/f/flyway.rb
+++ Formula/f/flyway.rb
@@ -1,8 +1,8 @@
 class Flyway < Formula
   desc "Database version control to control migrations"
   homepage "https://flywaydb.org/"
-  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/9.22.1/flyway-commandline-9.22.1.tar.gz"
-  sha256 "4c4da2b65f1e2d0f7e44ab5b2710f8155c9a77d0a1b4a3bb8fe6cf7c56f78b96"
+  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/9.22.2/flyway-commandline-9.22.2.tar.gz"
+  sha256 "21a511ad13f543f35ebbf1e63df083b0beab679f46965e40ce09e242144adc65"
   license "Apache-2.0"
 
   livecheck do
