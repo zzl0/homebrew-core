2841f4ea1b4c61d7ba583723bd4191c3256a7d22
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-22 20:28:37 +0000

    cryptominisat: update 5.11.14 bottle.

--- Formula/c/cryptominisat.rb
+++ Formula/c/cryptominisat.rb
@@ -13,15 +13,13 @@ class Cryptominisat < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "f2e635d8038c1efd59aaf38133eb09eff39b79000ff14e58a46ac52fc898bfad"
-    sha256 cellar: :any,                 arm64_ventura:  "f4de656aa865667cb8f6b7f18bd15fd9a0f3cf5525aec53507100268b7c083f7"
-    sha256 cellar: :any,                 arm64_monterey: "b0528b2edba4932609be44ab1a026c217b3ac390082068ed1b4d935f47c2d98c"
-    sha256 cellar: :any,                 arm64_big_sur:  "6b515e6e9a00c7f8dfbc8e37c95363f7d581eed113ed5451b49da2142309153b"
-    sha256 cellar: :any,                 sonoma:         "7d882af64f627e08926a7e58f1a9998dc4ff14bd6c8b3a357460f68238df9664"
-    sha256 cellar: :any,                 ventura:        "a31ba29eb5e0c24b4bd86a23e32aedd39b3853487e004e2f0fb0b8e7d647d063"
-    sha256 cellar: :any,                 monterey:       "3bf4fd1ef6dea8e670adc3fa486a72d06f05e8f64e48f005d4dd6b5ba4597454"
-    sha256 cellar: :any,                 big_sur:        "d3fcf58b537413d0c675ed90fa79f13419faacd7d306c9b7b6c654ea372ef272"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f52e7b7a4b7faa91ea27b84da64a6a66a1e1d8eb0d16ff1a277228f445e676de"
+    sha256 cellar: :any,                 arm64_ventura:  "a5a95f3d7cea906853202955c4c4c9fcb84893f0be6fb15bf5e7859468245c7b"
+    sha256 cellar: :any,                 arm64_monterey: "4fb9e91aa63923ffc2006e3a0e85c86bc71e78b6595404c300d3e1b4fdf68b31"
+    sha256 cellar: :any,                 arm64_big_sur:  "4f859db8b42ca7439e1f3cad28bc55f67c4b12b4e309d54f66f1fd70e088a337"
+    sha256 cellar: :any,                 ventura:        "e3107d15e9c75b3e53361ce56343d8facb1b82714e18c638a133dc156150fc70"
+    sha256 cellar: :any,                 monterey:       "17e2fc35d04c4e9091f0034224f9a53a3c13e8499739f862afb5091dddca3302"
+    sha256 cellar: :any,                 big_sur:        "43629f3cee7a5035aeacfed05c0991dd3e5844ef8d6198c7c4527457138e9c0e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "021e712dd189684887b40b2e2f1f9cca3127ec02f1310ca1d5131f8ffd5f5b3e"
   end
 
   depends_on "cmake" => :build
