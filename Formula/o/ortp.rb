18638d1d22d9ae2c037fcf38267301af7704aa3e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-22 12:46:23 +0000

    ortp: update 5.2.107 bottle.

--- Formula/o/ortp.rb
+++ Formula/o/ortp.rb
@@ -7,13 +7,13 @@ class Ortp < Formula
   head "https://gitlab.linphone.org/BC/public/ortp.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "5515c72ca908120d557b1b1fb8ea2045b57ba36dc4e6317214d5eb96ad84931c"
-    sha256 cellar: :any,                 arm64_monterey: "a2936dea7d7bf9658d1d11a65c4a52be2f34f41dcf736cd5e4f6ad2fbee259b8"
-    sha256 cellar: :any,                 arm64_big_sur:  "f5c462fbcb2c731d09610fd23b1519f8084431ca0005f6b92ebb9e4cb40b29f3"
-    sha256 cellar: :any,                 ventura:        "1ed2c4388323b16dd4a0172119dd25c3b1964708899cf6830d0c328cdca3cd12"
-    sha256 cellar: :any,                 monterey:       "f81860050a02e3dc02800f5d5a844e4cfedabf94ae8c3d887030fcfedcbf9908"
-    sha256 cellar: :any,                 big_sur:        "d1dc891e143fbf1b06b03c701dfd24fc6569c347eb96e52e2ddffed382109836"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d71dc7c466d370b3bcb773ed939e9ff63be9454e8b078c5ace40b7374a9be1b5"
+    sha256 cellar: :any,                 arm64_ventura:  "3e4b16808b11033f1ef87db0a0932bdef167b5315525eefa1e3dc2a0df436655"
+    sha256 cellar: :any,                 arm64_monterey: "923dc467162d1df411ac272828a9036bf633c346bce20e0ab663db875135f6ae"
+    sha256 cellar: :any,                 arm64_big_sur:  "b1efa9f7e6fad53153307d22eb6a27e06be686a95a2199ae7a7a123c5121803b"
+    sha256 cellar: :any,                 ventura:        "30fddd4fb6ab4c1b25475273abe6604250b72c3639c5b1c48d9e8e0d91b072be"
+    sha256 cellar: :any,                 monterey:       "5201de7213f3ef471c2e1173804b5df3813a03bfa94ea7ddabbcb89199f6789d"
+    sha256 cellar: :any,                 big_sur:        "8239118c3f16bb4405c04ab50374409d1efae0bd6e4eb15c2e175897eea39627"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e05b1cdee27efe4522e3ca63b7a554f47e33f13121083b09484ee8cbc1dee21a"
   end
 
   depends_on "cmake" => :build
