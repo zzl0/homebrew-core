bd02c447c48b2bef75391bc457ba543f56112196
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-22 23:10:54 +0000

    dunamai: update 1.18.1 bottle.

--- Formula/d/dunamai.rb
+++ Formula/d/dunamai.rb
@@ -8,14 +8,13 @@ class Dunamai < Formula
   license "MIT"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "ecb0f63fa62c43e87d625e058a727773adb88d966bb1fa73c0f925cc541d5c4f"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3a6c139106ef9979e2d052b9ef7f9adf41a25936b3ed9d197e70bbebefb285c4"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "23f613b8e47081d6b5c79302719c9761dcfb1a670a9db650a99d62e6d4e47252"
-    sha256 cellar: :any_skip_relocation, ventura:        "fd9cc9d120a0648ec86c98b3e3593c047e51f8363b2b809face37c6522023cf1"
-    sha256 cellar: :any_skip_relocation, monterey:       "1f6c6d848c3aacce1c3e55331d768d7d0d2d0f70cbcbb8041f19934d8a8d5a67"
-    sha256 cellar: :any_skip_relocation, big_sur:        "1985cf0e853bcdd2a28a5464640b4f50cfd10e85888b4da8f3279374d9c6378c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3c403a7a27376bea29bb5bf6d126d735c5a568f01e6ba5190dc2f59df680df9a"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "caa2225fdf48444298424b60887eb0ed28d486faba484873e907a9bf54c5f6db"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2448421f350edb4f4ac76704827d6c30bf33876da7d861f80b4b0aea71b6aed9"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "db76ded7233363f3f8538f9581fef119a6ab3237e5789a794c6a9800abf1afe1"
+    sha256 cellar: :any_skip_relocation, ventura:        "ae43ea2e87c37456d770b6cf85ff38ab8be0565b31d1f3cfbc27032b9280a399"
+    sha256 cellar: :any_skip_relocation, monterey:       "532ad466acd49661945263389a234aa81327e31734b5cd8468e42b27aae6c9bc"
+    sha256 cellar: :any_skip_relocation, big_sur:        "9ad7005fa07caea1d5c47544192d20786bebf239f9422b14208ca9c605038a8c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2d5b62b8f371fb341cc936f76c788111f8eccda2012de87b079ea2af66ee056c"
   end
 
   depends_on "python-packaging"
