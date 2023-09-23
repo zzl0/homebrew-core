526bb9077ccd98d3542f1f5ca13982ab802034ed
Patrick Linnane <patrick@linnane.io>
2023-09-23 05:36:46 +0000

    dolt: update 1.16.3 bottle.

--- Formula/d/dolt.rb
+++ Formula/d/dolt.rb
@@ -11,9 +11,11 @@ class Dolt < Formula
   end
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "722a7a6004cac5990d6e5dc6e5f60349a61d7c7aee79cd7fe59ce881559fd1c7"
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "546827254ce76c0359e7edcece0c4e55534d90e4eb5c686936dfbff46786207e"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "9b5fd452d8e6af991dff0b6c494af87508d1097a9ee83e68b0bc5baa70902b58"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "96096d7a23b977ba096e722053786c17c9ed1049c12895eaa3f87dff3d3dc2f6"
+    sha256 cellar: :any_skip_relocation, sonoma:         "4191e712da2ea850e3c59b3f13526cb340fdf8a851b94e46a755d0051c606e53"
     sha256 cellar: :any_skip_relocation, ventura:        "f0c6a4557bae99bfc380f246bece90c183d66cef4f2a5ddfe59f070d06f54fc4"
     sha256 cellar: :any_skip_relocation, monterey:       "35fa7611b65aa3db859322dbf343f77a424dccbf407d78a79da829a9d924670a"
     sha256 cellar: :any_skip_relocation, big_sur:        "02156a69dd8320406f612c436c5e02146d26e9a1b3aa8ef6b7ef1340b7b57ea9"
