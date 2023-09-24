0e8c24855e5c91299ee39d4f5568a77b927c2da2
Patrick Linnane <patrick@linnane.io>
2023-09-24 04:02:14 +0000

    oh-my-posh: update 18.10.0 bottle.

--- Formula/o/oh-my-posh.rb
+++ Formula/o/oh-my-posh.rb
@@ -7,9 +7,11 @@ class OhMyPosh < Formula
   head "https://github.com/JanDeDobbeleer/oh-my-posh.git", branch: "main"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "42719671e246ed06af3a7f73cd4a372982d4b29ac0be53450fb6234d5c4cddc3"
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "785b7e59e197dd28d0f81cf63a434ef4ff833d7ce05c933dbf13010b71801c83"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "76c85824ee9bcee910ccb8d5010b6d5dfdcf2ffee090dfc34fed1d4b94fca921"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4ca2b4f76e743168280af77274c91dd11caa665aad333e0397fba80d8ee821f1"
+    sha256 cellar: :any_skip_relocation, sonoma:         "962a1cea56698706ac96a31bf127fe403ceb0e80469a51cbb21fbc67cea2cb4f"
     sha256 cellar: :any_skip_relocation, ventura:        "37fa859cac694966bc18dd4722c29a78b2d3971c4d54375410b9558da0f72e78"
     sha256 cellar: :any_skip_relocation, monterey:       "fcdc24ea356c7518480ee86da86c249def6007de414c8c3d4ce751f7d43f0122"
     sha256 cellar: :any_skip_relocation, big_sur:        "459f4c02d5f08a43baf22d6fcb95559ea8899602a5bd608502370f29912c0ee2"
