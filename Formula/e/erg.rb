0aa81e943352226ea845fdacbd362b5faa88c6fc
FX Coudert <fxcoudert@gmail.com>
2023-09-24 18:00:20 +0000

    erg: update 0.6.21 bottle.

--- Formula/e/erg.rb
+++ Formula/e/erg.rb
@@ -6,9 +6,11 @@ class Erg < Formula
   license any_of: ["Apache-2.0", "MIT"]
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "a0c32fd9524d770253dce5642cfd02f3acdff48fe9e851f0b26b6bca7d1dcc3c"
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "eea1d47d916cac87e7676dbc10071527264653a62cbf01da9bae74ff60a2b50b"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "8bebc2ca4882754accf6d65de38096082ebe3bed614da1a14e8a9f469d7b98a1"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "abdbb377a812028c2ec4eaf5a47eb4b405b72964066f3d08b56a890a6d79a9d5"
+    sha256 cellar: :any_skip_relocation, sonoma:         "919bec61ad929f3c6f788d18b375567bc8b2647c11a00229c6329b2bfb45214e"
     sha256 cellar: :any_skip_relocation, ventura:        "f6e8b3768e9881e1bf6e5b16c017ddf477890da7ccbadbb4e98a364166816649"
     sha256 cellar: :any_skip_relocation, monterey:       "0f8b0e2bb3430006c0b8c9e394d63bae46559a756799a485de369896e370262a"
     sha256 cellar: :any_skip_relocation, big_sur:        "54ebea9ecd2d112ab631825c362f4617c81d4a6885136c4ffeafae3da67ba75e"
