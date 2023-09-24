c5e1199aca80aa427ff40f4faf221b22fa08b88d
Patrick Linnane <patrick@linnane.io>
2023-09-24 03:57:41 +0000

    libcouchbase: update 3.3.9 bottle.

--- Formula/lib/libcouchbase.rb
+++ Formula/lib/libcouchbase.rb
@@ -7,9 +7,11 @@ class Libcouchbase < Formula
   head "https://github.com/couchbase/libcouchbase.git", branch: "master"
 
   bottle do
+    sha256 arm64_sonoma:   "cec5e46ca83e2ccb2ca36365b511b8974d919d5f31168eadea4b6156ddcfe1f4"
     sha256 arm64_ventura:  "737c903a05fb271b7813a88b9e729d2411d71b8bc5a0855034f8b6d2fc5c015b"
     sha256 arm64_monterey: "9cae5d8cc8ce3e080a81b2d17f2d9649188d9745fa9725e318ce2a34f94fa283"
     sha256 arm64_big_sur:  "7bceef0deb0e6cc5c08934e32a1604c62e1d2518b41a6e964671bcfcc00fe64d"
+    sha256 sonoma:         "c353961c6b85e6878e88657f7bb946aa8803c0493c01e9187757f961e3d716b8"
     sha256 ventura:        "a57abe1a67642168ce296ebe2b60e737084b8a7c475d1f2a7586b0cbf5f17890"
     sha256 monterey:       "1b342afd260c0c7f33ac6f8acfc8c177fab2c2f15a27d54d564029b5e7b0c892"
     sha256 big_sur:        "976a5e8c7cbc5f202575ed91a23c3e7c4021144a81cb3781c83995d9a83327c2"
