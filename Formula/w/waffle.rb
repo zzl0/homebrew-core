cac05fe551e40c026941d245ab8fb6c92bdd4855
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-25 23:11:34 +0000

    waffle: update 1.8.0 bottle.

--- Formula/w/waffle.rb
+++ Formula/w/waffle.rb
@@ -7,15 +7,13 @@ class Waffle < Formula
   head "https://gitlab.freedesktop.org/mesa/waffle.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any, arm64_sonoma:   "f885c9317964ed3b841645b75b5c929d98c5457ed0911c41a586d4e436913669"
-    sha256 cellar: :any, arm64_ventura:  "721f578546fef947d5988c423d62edbf5ca4df91bf23132edd0a3dba96e415f3"
-    sha256 cellar: :any, arm64_monterey: "bd5edd13379bac6277403de70afc6a513ab780f0aaf19ee40c0e56487a1fb7b4"
-    sha256 cellar: :any, arm64_big_sur:  "3fdbf3b04577f26ecc7b15bb75ccb973157a3aea16e76b604627c215e4b79fa4"
-    sha256 cellar: :any, sonoma:         "909e9fe5b65a4978db9744ca93f00a2656a0391071e08431479a37c53e57ea26"
-    sha256 cellar: :any, ventura:        "88d13319655ba24a51c7de5342d3e01a90ec5e74abd415f2d5a6ac5f5d601ff6"
-    sha256 cellar: :any, monterey:       "9675280d92c77e188cf93c766040a4f713a9e387817183c3cc646dd581a876a4"
-    sha256 cellar: :any, big_sur:        "cfda02466d84027d572e6c524bda6041d9f3e401f5ff8a7994cdffdaca0be1d8"
-    sha256               x86_64_linux:   "ef964770f31d2fdae40b23efc0b3b1c1fffdfa8430eb125adf9a9d8c805f0bb1"
+    sha256 cellar: :any, arm64_sonoma:   "9262c7229fc67f91e8c58e7dccf11ebc7652d63925f6488704f087975ed67647"
+    sha256 cellar: :any, arm64_ventura:  "6af22d78936c2b6e67001861b7a4a449649914a5cba4adf2d0bef1fd7c6bf029"
+    sha256 cellar: :any, arm64_monterey: "26ae9441eede32d245fb40bfeda38953e73b361ed1a504f7429d857107b48c41"
+    sha256 cellar: :any, sonoma:         "0aef39cc92a84c10da9114377a516c59ff4c11ed644ade74b22349bcfedbb9c1"
+    sha256 cellar: :any, ventura:        "147a48d3cde58b8e210e82353c420c22e0bc357d50546da5d45315b1f2178342"
+    sha256 cellar: :any, monterey:       "8ad7eec2aa3521e34bf908c5795e3a3ae5d3bc84a48d14ee509f6e5fac04a5c5"
+    sha256               x86_64_linux:   "8e9c5d2b594568c7f49e447824a3e7d1f6c0da7de6fad365ecfc83822d0a30ea"
   end
 
   depends_on "docbook-xsl" => :build
