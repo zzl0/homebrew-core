d965cb04bfc002006b7b023e8bd9716777be9d80
Patrick Linnane <patrick@linnane.io>
2023-09-25 08:17:53 -0700

    lacework-cli 1.33.0

--- Formula/l/lacework-cli.rb
+++ Formula/l/lacework-cli.rb
@@ -2,8 +2,8 @@ class LaceworkCli < Formula
   desc "CLI for managing Lacework"
   homepage "https://docs.lacework.com/cli"
   url "https://github.com/lacework/go-sdk.git",
-      tag:      "v1.32.2",
-      revision: "be854786d8b6263864f1678611b2794b7a64c92b"
+      tag:      "v1.33.0",
+      revision: "f09ee5afe451285555619b72b62cc6828a5fdbf7"
   license "Apache-2.0"
   head "https://github.com/lacework/go-sdk.git", branch: "main"
 
