5d218a2314cbfeb7789d4bebf51c323362df4aec
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-25 04:46:46 +0000

    utf8cpp: update 3.2.5 bottle.

--- Formula/u/utf8cpp.rb
+++ Formula/u/utf8cpp.rb
@@ -6,7 +6,7 @@ class Utf8cpp < Formula
   license "BSL-1.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "9cb863de843378bed416d75ace5a55d4cc5547bbaf86ecb7ad721ace8c92d2f9"
+    sha256 cellar: :any_skip_relocation, all: "ef0a0128a797444a7941f62dbb8e6780837cbcbc77c863b8a2dea51344917b94"
   end
 
   depends_on "cmake" => [:build, :test]
