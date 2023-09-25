ea58a1bda7cdbc237877ed99176a29b8dfc179dc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-25 15:22:59 +0000

    pdm: update 2.9.3 bottle.

--- Formula/p/pdm.rb
+++ Formula/p/pdm.rb
@@ -9,13 +9,13 @@ class Pdm < Formula
   head "https://github.com/pdm-project/pdm.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "2356bc5417849ce5e7b38d730110444d0538437b76468779d8c0c3e8e0c3001d"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "99352d89b5ed46028e7bd5f51d0c810638000b6948b8169a036bc1e6f2b71064"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0f2a1ebcb5ce94b3e4aff90454df715ab32200ceb8bd5bb6b30758199804e9f5"
-    sha256 cellar: :any_skip_relocation, ventura:        "f985f924952dcfc80c97c07aa603803c8fec9165b710080de51942931135d704"
-    sha256 cellar: :any_skip_relocation, monterey:       "15d6d7ffed19a7ff4c8c037819a105cb8f1e9c9d569df8b6f6798b9f7717f746"
-    sha256 cellar: :any_skip_relocation, big_sur:        "c5fdac7a3c0da12740b05638964ecb7df33e0ec33712a9e671ee8b95dd6b7d75"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8375998a9a3359b27a48a7e4d5805e6aad03fcd882f1e42dfc010bc91cb45106"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f9575e9fb086318f52d7bb1c20536bcafa0423c11a55e5928ffcbe0d2d8c8e15"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "6b7fbbbd3353911164de1ee580e2bca76be3ef8ead2c50e5da15207296a6b894"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2f8e2c40419d0d1695869f545eea29cbcbd6b2ee297065f4761118c4878abbc9"
+    sha256 cellar: :any_skip_relocation, ventura:        "d7e320e2753a87ccb2a0253760926e2a9ba596d1d3dffd15be710345b0c5338b"
+    sha256 cellar: :any_skip_relocation, monterey:       "2dcbe138bdefd307453987887e6d6e31407f1c08c748c09b67b4a6c87bbd3466"
+    sha256 cellar: :any_skip_relocation, big_sur:        "9af45482fb38904ea8fbd0b34ed5033d5678467a3ccea7f7fefa40b373cd74f9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "592291128ce1441a12bcac8b2cf3c38aa54b58e924c89674c9316fbe7c6fcadc"
   end
 
   depends_on "pygments"
