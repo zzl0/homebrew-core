97fc743bf44256ec826115a0bba06741f618b0c6
Rui Chen <rui@chenrui.dev>
2023-09-25 05:59:24 +0000

    numbat: add 1.6.2 bottle.

--- Formula/n/numbat.rb
+++ Formula/n/numbat.rb
@@ -6,6 +6,16 @@ class Numbat < Formula
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/sharkdp/numbat.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "bb2eba59e3cfdcff0a62de2782c9f972a963eaf196dc2b4009dbca9d2d9f4513"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "fe2146028ae6ee9292d721a565bda5ae01bae688e5a1d24e9215e789d977e7a0"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c430a90f41d1446b7d9f09120057ccbbf813d20156e19414e2c624b448be5afe"
+    sha256 cellar: :any_skip_relocation, ventura:        "e45dbd22b43f999cfc78663efb8517b34e5a552d3f8bfa47d01d0259cc76ab40"
+    sha256 cellar: :any_skip_relocation, monterey:       "488d06bd0450f60bf64e0a8242a158f7e748651d5f75675694b40f978ad5701f"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ffd34826b5aa205dd9a91d8250223c284381db52e0f638b55bca2fc34c2ef079"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5dec542e5b5b473b3c9e59cf4901e416c07ce836172b97826118246331872065"
+  end
+
   depends_on "rust" => :build
 
   def install
