81644f06d26cd090a8493c94dacff1f21a53d502
Pierre Wendling <pierre.wendling.4@gmail.com>
2023-09-25 13:16:38 +0200

    fluid-synth 2.3.4

--- Formula/f/fluid-synth.rb
+++ Formula/f/fluid-synth.rb
@@ -1,8 +1,8 @@
 class FluidSynth < Formula
   desc "Real-time software synthesizer based on the SoundFont 2 specs"
   homepage "https://www.fluidsynth.org"
-  url "https://github.com/FluidSynth/fluidsynth/archive/v2.3.3.tar.gz"
-  sha256 "321f7d3f72206b2522f30a1cb8ad1936fd4533ffc4d29dd335b1953c9fb371e6"
+  url "https://github.com/FluidSynth/fluidsynth/archive/v2.3.4.tar.gz"
+  sha256 "1529ef5bc3b9ef3adc2a7964505912f7305103e269e50cc0316f500b22053ac9"
   license "LGPL-2.1-or-later"
   head "https://github.com/FluidSynth/fluidsynth.git", branch: "master"
 
