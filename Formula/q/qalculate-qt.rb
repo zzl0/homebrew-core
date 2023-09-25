4b6af311a884d1d36c04d4b82876eaa004ebee75
Rui Chen <rui@chenrui.dev>
2023-09-25 05:59:46 +0000

    qalculate-qt: add 4.8.1 bottle.

--- Formula/q/qalculate-qt.rb
+++ Formula/q/qalculate-qt.rb
@@ -6,6 +6,16 @@ class QalculateQt < Formula
   license "GPL-2.0-or-later"
   head "https://github.com/Qalculate/qalculate-qt.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "7e0ab81a6d899a73123d89c84ed9ff3caf8a2975ee4fd5724e9ae3245afca824"
+    sha256 cellar: :any,                 arm64_monterey: "19268f4d6b7b1ade02cb1c76be39a81e0a9963b2388dd9d1215ae715c8aa093e"
+    sha256 cellar: :any,                 arm64_big_sur:  "5c11dd083c9fef85eb3e1f976260f3483c3b43ea509b203009f01d56a320da85"
+    sha256 cellar: :any,                 ventura:        "8603ac4f1fd33b4fd812072592cca84d5f655603e3c4f4b778c808ef149d25e2"
+    sha256 cellar: :any,                 monterey:       "23e0e3b8c9414885342d6c21d7d297cab1e7ca311cddd9eceb28cadccbd63c52"
+    sha256 cellar: :any,                 big_sur:        "d1b1411ef55a8bb1ad7dee7a8629214ceb56ee0283ad4936a77cb9b3801939f3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8c7d8ddd88aea32ff54f071731601012a8abf26ae883eea47a36058073eaa943"
+  end
+
   depends_on "pkg-config" => :build
   depends_on "libqalculate"
   depends_on "qt"
