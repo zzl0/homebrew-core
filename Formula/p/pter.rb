330a6660ed40db678a7195c6714cce7b622e66bc
Ulysse Buonomo <buonomo.ulysse@gmail.com>
2023-09-26 17:30:31 -0500

    pter 3.8.0 (new formula)

---
+++ Formula/p/pter.rb
@@ -0,0 +1,38 @@
+class Pter < Formula
+  include Language::Python::Virtualenv
+
+  desc "Your console and graphical UI to manage your todo.txt file(s)"
+  homepage "https://vonshednob.cc/pter/"
+  url "https://files.pythonhosted.org/packages/cc/3d/6fc7f754cc7f76b4392a781dabc3029bff9cc43efbe59e7a1cd534f51fbb/pter-3.8.0.tar.gz"
+  sha256 "29672889f0a2fe77a327621a436f0a7a08735fea923966d905f9d1e615815334"
+  license "MIT"
+
+  depends_on "python@3.11"
+
+  resource "cursedspace" do
+    url "https://files.pythonhosted.org/packages/cd/3b/72657c9e867dd5034814dcea21b1128a70a1b8427e48c7de8b3b9ea3dd93/cursedspace-1.5.2.tar.gz"
+    sha256 "21043f80498db9a79d5ee1bb52229fd28ad8871a360601c8f9120ff9dadc2aec"
+  end
+
+  resource "pytodotxt" do
+    url "https://files.pythonhosted.org/packages/51/18/a8f4d15eb31bcde441b0ec090c5d97c435beabc9620199e7f90d2f5ad1af/pytodotxt-1.5.0.tar.gz"
+    sha256 "99be359438c52e0c4fc007e11a89f5a03af00fc6851a6ba7070dfe0e00189009"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    PTY.spawn(bin/"pter", "todo.txt") do |r, w, _pid|
+      w.write "n"         # Create new task
+      w.write "some task" # Task description
+      w.write "\r"        # Confirm
+      w.write "q"         # Quit
+      r.read
+    rescue Errno::EIO
+      # GNU/Linux raises EIO when read is done on closed pty
+    end
+    assert_match "some task", (testpath/"todo.txt").read
+  end
+end
