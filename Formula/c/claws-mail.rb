083a3f1238f4c4e91c143470caa2dcc22ab2b66f
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-09-25 21:14:36 -0400

    claws-mail 3.19.1

--- Formula/c/claws-mail.rb
+++ Formula/c/claws-mail.rb
@@ -1,8 +1,8 @@
 class ClawsMail < Formula
   desc "User-friendly, lightweight, and fast email client"
   homepage "https://www.claws-mail.org/"
-  url "https://www.claws-mail.org/releases/claws-mail-3.19.0.tar.gz"
-  sha256 "3feef9ff72b15fb9f1ecc5102d7dfbb5b1c2c53172d331a3fb453645a6b53a6b"
+  url "https://www.claws-mail.org/releases/claws-mail-3.19.1.tar.gz"
+  sha256 "4fcc2b0b39a6d40e4dc3e49fac2f1cf063575d6570e93408fa4a76ab67531ae1"
   license "GPL-3.0-or-later"
 
   livecheck do
