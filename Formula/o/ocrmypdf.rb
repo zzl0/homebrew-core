a871574f9cae039e561994bd4bd9fe3eb56e7fb4
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-09-27 11:26:58 +0800

    ocrmypdf 15.0.0.post1

--- Formula/o/ocrmypdf.rb
+++ Formula/o/ocrmypdf.rb
@@ -3,8 +3,8 @@ class Ocrmypdf < Formula
 
   desc "Adds an OCR text layer to scanned PDF files"
   homepage "https://ocrmypdf.readthedocs.io/en/latest/"
-  url "https://files.pythonhosted.org/packages/13/fb/a97021b662979417074fa4d54ed8103dbbc2b18fb9cfe98348fd9140847c/ocrmypdf-14.4.0.tar.gz"
-  sha256 "bd6fcc813cc9766768058687cf94efe3403993244b19c51771617403858c692d"
+  url "https://files.pythonhosted.org/packages/f3/8b/caa73b1ec60915ec02e5d26843a2d71c235679ef996511bc9dd139b6763b/ocrmypdf-15.0.0.post1.tar.gz"
+  sha256 "1faf028caf3162991b7d79f80e2fba4d0164bc2b314bda2c73d3ce80d2359022"
   license "MPL-2.0"
 
   bottle do
@@ -76,13 +76,13 @@ class Ocrmypdf < Formula
   end
 
   resource "pikepdf" do
-    url "https://files.pythonhosted.org/packages/29/26/482a1dc27164672a2f34328a12ad6fb7fe23bf73b009e3696e1ddf139b9b/pikepdf-8.3.1.tar.gz"
-    sha256 "dc555099be135b8144bf74b24bff4096f521150be173e299f80f68749d974068"
+    url "https://files.pythonhosted.org/packages/0a/81/c29a09060c763b2a5479b64223f8c9f3565d73a5a35118c709295cf7b88b/pikepdf-8.4.1.tar.gz"
+    sha256 "3f8b46875426a307585f64b97c1937c9d488982f832793387ee2836a594b8ddf"
   end
 
   resource "pluggy" do
-    url "https://files.pythonhosted.org/packages/8a/42/8f2833655a29c4e9cb52ee8a2be04ceac61bcff4a680fb338cbd3d1e322d/pluggy-1.2.0.tar.gz"
-    sha256 "d12f0c4b579b15f5e054301bb226ee85eeeba08ffec228092f8defbaa3a4c4b3"
+    url "https://files.pythonhosted.org/packages/36/51/04defc761583568cae5fd533abda3d40164cbdcf22dee5b7126ffef68a40/pluggy-1.3.0.tar.gz"
+    sha256 "cf61ae8f126ac6f7c451172cf30e3e43d3ca77615509771b3a984a0730651e12"
   end
 
   resource "pygments" do
@@ -91,18 +91,13 @@ class Ocrmypdf < Formula
   end
 
   resource "reportlab" do
-    url "https://files.pythonhosted.org/packages/67/5f/096c281d19b10b68f6bbf3f1b773c8f83aa94c4aa2e0c8f07e9921fb2cdb/reportlab-4.0.4.tar.gz"
-    sha256 "7f70b3b56aff5f11cb4136c51a0f5a56fe6e4c8fbbac7b903076db99a8ef31c1"
+    url "https://files.pythonhosted.org/packages/10/ec/4a423a97e53399c05889cd12f789ab9175f2498c77b47cc006b6482b71c1/reportlab-4.0.5.tar.gz"
+    sha256 "9c68f277736f585c5c9938755b826dd57c877fcaeb203e21cefea12b3b1db4f5"
   end
 
   resource "rich" do
-    url "https://files.pythonhosted.org/packages/ad/1a/94fe086875350afbd61795c3805e38ef085af466a695db605bcdd34b4c9c/rich-13.5.2.tar.gz"
-    sha256 "fb9d6c0a0f643c99eed3875b5377a184132ba9be4d61516a55273d3554d75a39"
-  end
-
-  resource "tqdm" do
-    url "https://files.pythonhosted.org/packages/62/06/d5604a70d160f6a6ca5fd2ba25597c24abd5c5ca5f437263d177ac242308/tqdm-4.66.1.tar.gz"
-    sha256 "d88e651f9db8d8551a62556d3cff9e3034274ca5d66e93197cf2490e2dcb69c7"
+    url "https://files.pythonhosted.org/packages/1d/d6/9773d48804d085962c4f522db96f6a9ea9bd2e0480b3959a929176d92f01/rich-13.5.3.tar.gz"
+    sha256 "87b43e0543149efa1253f485cd845bb7ee54df16c9617b8a893650ab84b4acb6"
   end
 
   def install
