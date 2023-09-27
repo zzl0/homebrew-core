4bc5a0947d01c396aaed182b1088cf2c78deef25
Porkepix <porkepix@gmail.com>
2023-09-27 10:54:30 +0200

    d2 0.6.1

--- Formula/d/d2.rb
+++ Formula/d/d2.rb
@@ -1,8 +1,8 @@
 class D2 < Formula
   desc "Modern diagram scripting language that turns text to diagrams"
   homepage "https://d2lang.com/"
-  url "https://github.com/terrastruct/d2/archive/refs/tags/v0.6.0.tar.gz"
-  sha256 "8409de84c1d47fd325bc6daf7d1f012ff483f6e873b14550881803e6f6a07f35"
+  url "https://github.com/terrastruct/d2/archive/refs/tags/v0.6.1.tar.gz"
+  sha256 "c76e822755a64e2a6902d1f75a17bcda779c46cb36edc751b3eaa8d6f168b243"
   license "MPL-2.0"
   head "https://github.com/terrastruct/d2.git", branch: "master"
 
