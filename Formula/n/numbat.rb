372e575f50d076e3d5307e447074d19dd4dde680
Aaron Rumpler <80610725+Aaron-Rumpler@users.noreply.github.com>
2023-09-27 15:38:29 +1300

    numbat 1.6.3

--- Formula/n/numbat.rb
+++ Formula/n/numbat.rb
@@ -1,8 +1,8 @@
 class Numbat < Formula
   desc "Statically typed programming language for scientific computations"
   homepage "https://github.com/sharkdp/numbat"
-  url "https://github.com/sharkdp/numbat/archive/refs/tags/v1.6.2.tar.gz"
-  sha256 "62400366887ad2f52a0de3d9f47cb17606566a3ca238aa496402c1538d507566"
+  url "https://github.com/sharkdp/numbat/archive/refs/tags/v1.6.3.tar.gz"
+  sha256 "0f70e14d408f6129f2a5621053ebc4c47cdda9088ceb367151fb73436620523c"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/sharkdp/numbat.git", branch: "master"
 
@@ -21,7 +21,10 @@ class Numbat < Formula
   depends_on "rust" => :build
 
   def install
+    ENV["NUMBAT_SYSTEM_MODULE_PATH"] = "#{pkgshare}/modules"
     system "cargo", "install", *std_cargo_args(path: "numbat-cli")
+
+    pkgshare.install "numbat/modules"
   end
 
   test do
