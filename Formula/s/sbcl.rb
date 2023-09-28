cf83f6903d7580695805f0d58d9da7c321c7197d
Porkepix <porkepix@gmail.com>
2023-09-28 13:05:26 +0200

    sbcl 2.3.9

--- Formula/s/sbcl.rb
+++ Formula/s/sbcl.rb
@@ -1,8 +1,8 @@
 class Sbcl < Formula
   desc "Steel Bank Common Lisp system"
   homepage "http://www.sbcl.org/"
-  url "https://downloads.sourceforge.net/project/sbcl/sbcl/2.3.8/sbcl-2.3.8-source.tar.bz2"
-  sha256 "421571b2ac916e58be8ebcea5ef4abf8d7902863db6a1d0a5efa9500adca0d29"
+  url "https://downloads.sourceforge.net/project/sbcl/sbcl/2.3.9/sbcl-2.3.9-source.tar.bz2"
+  sha256 "7d289a91232022028bf0128900c32bf00e4c5430c32f28af0594c8a592a98654"
   license all_of: [:public_domain, "MIT", "Xerox", "BSD-3-Clause"]
   head "https://git.code.sf.net/p/sbcl/sbcl.git", branch: "master"
 
