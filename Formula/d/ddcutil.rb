f91193422152ace5efd3e2dfd21bcd84a4dd86e2
Porkepix <porkepix@gmail.com>
2023-09-28 12:15:18 +0200

    ddcutil 2.0.0
    ddcutil: add `jansson` dependency

--- Formula/d/ddcutil.rb
+++ Formula/d/ddcutil.rb
@@ -1,8 +1,8 @@
 class Ddcutil < Formula
   desc "Control monitor settings using DDC/CI and USB"
   homepage "https://www.ddcutil.com"
-  url "https://www.ddcutil.com/tarballs/ddcutil-1.4.2.tar.gz"
-  sha256 "c06c136617911e2e6919fe9607d84677c9bfa4b3a69e5618ad2e5d77e408b404"
+  url "https://www.ddcutil.com/tarballs/ddcutil-2.0.0.tar.gz"
+  sha256 "0ae9c54501ca937abc094eb44d2467468096efef57d7e2692479bbef71e1999b"
   license "GPL-2.0-or-later"
 
   livecheck do
@@ -17,6 +17,7 @@ class Ddcutil < Formula
   depends_on "pkg-config" => :build
   depends_on "glib"
   depends_on "i2c-tools"
+  depends_on "jansson"
   depends_on "kmod"
   depends_on "libdrm"
   depends_on "libusb"
