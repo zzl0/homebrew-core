09d8e8fa40b5706f4d6798f7e3689b8d9b80a0f6
Roger Coll <rogercoll@protonmail.com>
2023-09-29 10:47:18 +0200

    newrelic-infra-agent 1.47.2

--- Formula/n/newrelic-infra-agent.rb
+++ Formula/n/newrelic-infra-agent.rb
@@ -2,8 +2,8 @@ class NewrelicInfraAgent < Formula
   desc "New Relic infrastructure agent"
   homepage "https://github.com/newrelic/infrastructure-agent"
   url "https://github.com/newrelic/infrastructure-agent.git",
-      tag:      "1.47.1",
-      revision: "a42687da1980cee7d153a86e415871d38e330c68"
+      tag:      "1.47.2",
+      revision: "9b48d5394edc7b657c6c29817e0d095cc2547ec6"
   license "Apache-2.0"
   head "https://github.com/newrelic/infrastructure-agent.git", branch: "master"
 
