05b7db1e4862b1c37bf174b3ce14427bbc139be7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-29 23:18:24 +0000

    glassfish: update 7.0.9 bottle.

--- Formula/g/glassfish.rb
+++ Formula/g/glassfish.rb
@@ -12,7 +12,7 @@ class Glassfish < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "acb208104a2319421d51c84f585d357a4bfb9b03535b8a5f8dbe169a9c3aa99b"
+    sha256 cellar: :any_skip_relocation, all: "1d9ac39b379e1ca4b4a7020914de59751e2c15fb51ab19ac8f220bac99c15e13"
   end
 
   depends_on "openjdk@17"
