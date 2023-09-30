fca5d93cfc3622515d8e0d90d56f3a89c625b1e0
Patrick Linnane <patrick@linnane.io>
2023-09-30 09:19:47 -0700

    docfx 2.71.0

--- Formula/d/docfx.rb
+++ Formula/d/docfx.rb
@@ -1,8 +1,8 @@
 class Docfx < Formula
   desc "Tools for building and publishing API documentation for .NET projects"
   homepage "https://dotnet.github.io/docfx/"
-  url "https://github.com/dotnet/docfx/archive/refs/tags/v2.70.4.tar.gz"
-  sha256 "0e986e5c46504db53da73711e7922c3ea8fe6fa80aa818fce2eb3370e79ab7c5"
+  url "https://github.com/dotnet/docfx/archive/refs/tags/v2.71.0.tar.gz"
+  sha256 "3423dc8b24d9271b6c33da93374e4682a311cc416b4017cfc98cfe553d6b1997"
   license "MIT"
 
   bottle do
