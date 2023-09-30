5b23149eb42c345bf9591b80ef2dd87b154224e3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-30 21:06:24 +0000

    docfx: update 2.71.0 bottle.

--- Formula/d/docfx.rb
+++ Formula/d/docfx.rb
@@ -6,13 +6,11 @@ class Docfx < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "23319a6fd07db42c07d30f49d9d19b4fc428232a6f1caf75baf0a080a20d3a77"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "9b0b3aef94ff0fa317a328bd5b391d2d61028a3373c3d5f67ae29ec7fd3e7b19"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "23ce420b8fb600f004c68758e59e073ef340f3153bbb6c6b6ad379f80c5ac5b7"
-    sha256 cellar: :any_skip_relocation, ventura:        "00b513fd37e1a146064c0de1a2ccbe027a1a129679e325598a75c634ba674b74"
-    sha256 cellar: :any_skip_relocation, monterey:       "8ef537046d2e33ce35775d32db2d17e3892b5624e8ae13f9d529ee31ce937bdc"
-    sha256 cellar: :any_skip_relocation, big_sur:        "9d757b3af056df7c9a9201eeb83062187979c048d374ca743e7d335661d3fb3f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "900fd346df10821bdcc586ce724ee128aaf4a328084a99e802dba9d9e6487428"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "694be96a5ae7ba8d41bdfe23b9c7043ad58b885d16aab251e450854b6c2fd31c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ba03d7df58f592690e13d19d447819c0e9b65523dba260524c3f7b4551ede165"
+    sha256 cellar: :any_skip_relocation, ventura:        "6824afed33c3abe89d55e8e24eebd95bce7a490c8b5a80e06778cd8d987eaf9e"
+    sha256 cellar: :any_skip_relocation, monterey:       "b5e3c7a83dee8978e981eb98947b94b4f7653355fe8c7f8424ba79faddd4e8be"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "395aa82bdb6b92ee578cc7af66fab0098fe6891d1ccaf21aa7ddfe788f58898b"
   end
 
   depends_on "dotnet"
