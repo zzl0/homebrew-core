381f8280133079a04d6abfb5c87fa8624a69fa01
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-10-01 03:11:36 +0000

    signal-cli: update 0.12.2 bottle.

--- Formula/s/signal-cli.rb
+++ Formula/s/signal-cli.rb
@@ -6,15 +6,13 @@ class SignalCli < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "2088ee49b8c3c06776f813b24e5386e87fa5531128e7763b3e38e0d9f572c95c"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f4ca9f9ec346ff80ebb0daa8c1b422509ff9da0622a5b508a9695ee8ddb80199"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "449957852a354b082d015268bbe6341fe19f39546c95549bc72bf25660e42a26"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9997470bd853d93394f1f6b86216b34b341a7a7ea68d4c9b3c620db66cd2c67f"
-    sha256 cellar: :any_skip_relocation, sonoma:         "197b6db4c4d6542b67e221c360ba2c33fcc38ea5309faf5d165803b35cbc5316"
-    sha256 cellar: :any_skip_relocation, ventura:        "c7c9eabd50e2fd1564123fe62ba8503ab99d2a48f0d050ea0f3db9f8bb65cf99"
-    sha256 cellar: :any_skip_relocation, monterey:       "18480f79264022f64d85b86c7088106b5fe7b8655f39058618be68af3ef38a2a"
-    sha256 cellar: :any_skip_relocation, big_sur:        "82a4aefedc6a74981e8c7e1ca70ed2efaace75f8ce7429537f009a135d1651e5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "85ec386fc856c3efc20b60f023644d065dd2441ac19900b2d7cf2c839bf5960b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "8045d9d482db399f2cc8d12761f63e27c351a725a2036d3fa827cbd858fa0228"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3d7b3c047b88e0dee6b68c32335ba1b0bce34a9bd7f486da5d920dd0d82098b1"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "22e8bc62b967ff760d28da2145a23c4d6ad6de79ac5bfbf27ed8bf87f04cc3c4"
+    sha256 cellar: :any_skip_relocation, sonoma:         "a1af8f910d07002e2a1efa549999e0f77c67baf95cf407c5b86ad45acfd36f8f"
+    sha256 cellar: :any_skip_relocation, ventura:        "f424abaa2a42cf80e6081e3e812675c27ff4d80efd0be558cf0972fb04ccf76b"
+    sha256 cellar: :any_skip_relocation, monterey:       "87957f5c9a9a6ac2a6efb9e45fd2319c0d5ec2f7c417b6e4e5de6210cb7d9dea"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0e953816120b6bb49e8562a9bde4d50f2907ae3621b27306078e8f01f857f56b"
   end
 
   depends_on "cmake" => :build # For `boring-sys` crate in `libsignal-client`
