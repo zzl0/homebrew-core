27fed6371a22a01c059a8a05f18649aa3ed47633
James J Balamuta <james.balamuta@gmail.com>
2023-10-02 11:47:01 -0700

    bandicoot 1.11.0 (new formula)
    Adds the new Bandicoot C++ Library for GPU computations that complements the Armadillo C++ matrix library.
    
    Co-Authored-By: Patrick Linnane <patrick@linnane.io>

---
+++ Formula/b/bandicoot.rb
@@ -0,0 +1,51 @@
+class Bandicoot < Formula
+  desc "C++ library for GPU accelerated linear algebra"
+  homepage "https://coot.sourceforge.io/"
+  url "https://gitlab.com/conradsnicta/bandicoot-code/-/archive/1.11.0/bandicoot-code-1.11.0.tar.bz2"
+  sha256 "e53fa0db2cd890475ad41452b0a70b036315e32dcbe45687f149789e341a85f6"
+  license "Apache-2.0"
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "clblas"
+  depends_on "openblas"
+
+  # Ensure CL components are present on Linux
+  on_linux do
+    depends_on "opencl-headers" => [:build, :test]
+    depends_on "opencl-icd-loader"
+    depends_on "pocl"
+  end
+
+  def install
+    if OS.mac?
+      # Enable the detection of OpenBLAS on macOS
+      system "cmake", "-S", ".", "-B", "build",
+                      "-DALLOW_OPENBLAS_MACOS=ON",
+                      "-DALLOW_BLAS_LAPACK_MACOS=ON",
+                      *std_cmake_args
+    else
+      # Avoid specifying detection for linux
+      system "cmake", "-S", ".", "-B", "build",
+                      *std_cmake_args
+    end
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    # Create a test script that compiles a program
+    (testpath/"test.cpp").write <<~EOS
+      #include <iostream>
+      #include <bandicoot>
+
+      int main(int argc, char** argv) {
+        std::cout << coot::coot_version::as_string() << std::endl;
+      }
+    EOS
+    system ENV.cxx, "-std=c++11", "test.cpp", "-I#{include}", "-L#{lib}", "-lbandicoot", "-o", "test"
+
+    # Check that the coot version matches with the formula version
+    assert_equal shell_output("./test").to_i, version.to_s.to_i
+  end
+end
