c8b4c94c2712e009c4ec63c7d26f535ce065a51d
Joshua J. Berry <des@condordes.net>
2023-10-02 15:30:25 -0700

    temporal 0.10.6

--- Formula/t/temporal.rb
+++ Formula/t/temporal.rb
@@ -1,8 +1,8 @@
 class Temporal < Formula
   desc "Command-line interface for running and interacting with Temporal Server and UI"
   homepage "https://temporal.io/"
-  url "https://github.com/temporalio/cli/archive/refs/tags/v0.10.5.tar.gz"
-  sha256 "41ddfad62861eaa8414cda2807bad7a51ebb158c841bd3bfcc1db2fcd3774c17"
+  url "https://github.com/temporalio/cli/archive/refs/tags/v0.10.6.tar.gz"
+  sha256 "1a8f1fef8e58e3bb431901f328ca492790fc5c716f2af19b0d00754a85283604"
   license "MIT"
   head "https://github.com/temporalio/cli.git", branch: "main"
 
