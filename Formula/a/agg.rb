5990c8c361420bdff406f151f635e7efe5c1734a
Rui Chen <rui@chenrui.dev>
2023-10-03 00:50:42 +0800

    agg 1.4.3

--- Formula/a/agg.rb
+++ Formula/a/agg.rb
@@ -1,8 +1,8 @@
 class Agg < Formula
   desc "Asciicast to GIF converter"
   homepage "https://github.com/asciinema/agg"
-  url "https://github.com/asciinema/agg/archive/refs/tags/v1.4.2.tar.gz"
-  sha256 "8a4b81733085d81de1076e2964dedf729a8603941de11659d775478293d9efd0"
+  url "https://github.com/asciinema/agg/archive/refs/tags/v1.4.3.tar.gz"
+  sha256 "1089e47a8e6ca7f147f74b2347e6b29d94311530a8b817c2f30f19744e4549c1"
   license "Apache-2.0"
   head "https://github.com/asciinema/agg.git", branch: "main"
 
