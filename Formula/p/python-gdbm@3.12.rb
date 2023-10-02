e990f84ff943cf91bb016f0b8e304b515d519c88
Patrick Linnane <patrick@linnane.io>
2023-10-02 09:36:12 -0700

    python-gdbm@3.12 3.12.0 (new formula)
    Co-authored-by: Branch Vincent <branchevincent@gmail.com>

---
+++ Formula/p/python-gdbm@3.12.rb
@@ -0,0 +1,63 @@
+class PythonGdbmAT312 < Formula
+  desc "Python interface to gdbm"
+  homepage "https://www.python.org/"
+  url "https://www.python.org/ftp/python/3.12.0/Python-3.12.0.tgz"
+  sha256 "51412956d24a1ef7c97f1cb5f70e185c13e3de1f50d131c0aac6338080687afb"
+  license "Python-2.0"
+
+  livecheck do
+    formula "python@3.12"
+  end
+
+  depends_on "gdbm"
+  depends_on "python@3.12"
+
+  resource "setuptools" do
+    url "https://files.pythonhosted.org/packages/ef/cc/93f7213b2ab5ed383f98ce8020e632ef256b406b8569606c3f160ed8e1c9/setuptools-68.2.2.tar.gz"
+    sha256 "4ac1475276d2f1c48684874089fefcd83bd7162ddaafb81fac866ba0db282a87"
+  end
+
+  def python3
+    "python3.12"
+  end
+
+  def install
+    ENV.append_path "PYTHONPATH", buildpath/Language::Python.site_packages(python3)
+    resource("setuptools").stage do
+      system python3, "-m", "pip", "install", *std_pip_args(prefix: buildpath), "."
+    end
+
+    cd "Modules" do
+      (Pathname.pwd/"setup.py").write <<~EOS
+        from setuptools import setup, Extension
+
+        setup(name="gdbm",
+              description="#{desc}",
+              version="#{version}",
+              ext_modules = [
+                Extension("_gdbm", ["_gdbmmodule.c"],
+                          include_dirs=["#{Formula["gdbm"].opt_include}"],
+                          libraries=["gdbm"],
+                          library_dirs=["#{Formula["gdbm"].opt_lib}"])
+              ]
+        )
+      EOS
+      system python3, *Language::Python.setup_install_args(libexec, python3),
+                      "--install-lib=#{libexec}"
+      rm_r libexec.glob("*.egg-info")
+    end
+  end
+
+  test do
+    testdb = testpath/"test.db"
+    system python3, "-c", <<~EOS
+      import dbm.gnu
+
+      with dbm.gnu.open("#{testdb}", "n") as db:
+        db["testkey"] = "testvalue"
+
+      with dbm.gnu.open("#{testdb}", "r") as db:
+        assert db["testkey"] == b"testvalue"
+    EOS
+  end
+end
