141f3081d64700585db46f8e8b6d2586f572ac2c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-10-03 12:12:28 +0000

    athenacli: update 1.6.8_3 bottle.

--- Formula/a/athenacli.rb
+++ Formula/a/athenacli.rb
@@ -9,16 +9,13 @@ class Athenacli < Formula
   revision 3
 
   bottle do
-    rebuild 4
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "289ba4f7ca50b43de6d977b7a866019a733d4cb3a7763613df06f9c5461b57f3"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "19abdd458c36309d2ad2100dae56ce5bc4faa927b6e9b56080141deb08ecadc7"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3301f4e7df42efbe11e59d6c9f23c627954675b5d306ae6dfce0a1d03dbe167e"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f7352cc00a19e95a24c91b399d31127d90f68136b29966db6d1d8520284400d1"
-    sha256 cellar: :any_skip_relocation, sonoma:         "b3194353e0aff8d56c865ef3bab1887387c188e928c9dac90427409060c4b596"
-    sha256 cellar: :any_skip_relocation, ventura:        "1d0ddb8308bd53ea087a9977352b942785fdd33a192c51f727b810b14dc84568"
-    sha256 cellar: :any_skip_relocation, monterey:       "d913515ae10f7d4cc012624f2f2b20e87cfbe91572b15538623b7a534704bfb8"
-    sha256 cellar: :any_skip_relocation, big_sur:        "12907a64698643c40fb0fdb3a7a5fd791dbd617177dced1dc993e0dcd3ecfc15"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ecfb036ceb566cc93d605e2feadf27d0fffbb0c82719eb9ac11b40e6c08ceac9"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "9c2092e8e48ec0eeae9aae29e678e784f5a917fbc9495fded63d8d04211bcce9"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "2152d1aca8a2873ed1557a9480803ea146c5bbb2bb49c9b1779432f2f00d710d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "860ca233b7c1a1c4e433ae552bd7d798db0d4e14f82edbf25ea7fe5b75ee08b2"
+    sha256 cellar: :any_skip_relocation, sonoma:         "264b94c58f133f6996f3cc662c7a78bfad75d0dd414764ee585ff28fa6e182e6"
+    sha256 cellar: :any_skip_relocation, ventura:        "13a510ce645b642715870831973ae12d78b60a8c863eb8ab64c7edfb9211857c"
+    sha256 cellar: :any_skip_relocation, monterey:       "eccd083610a38d72ae54d0a5e1285e0b36c3fe65abc694925496e337ad9b30f7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f85747d35edb9304c6ac01b8dc291d2e600401d204539b848f6ff13902b58b26"
   end
 
   depends_on "python-tabulate"
