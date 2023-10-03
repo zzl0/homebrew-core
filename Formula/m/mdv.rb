3c5e15f20b0714f8988cd2a2c48837de9e692ebe
Rui Chen <rui@chenrui.dev>
2023-10-03 09:35:06 +0800

    mdv 1.7.5
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/m/mdv.rb
+++ Formula/m/mdv.rb
@@ -3,10 +3,8 @@ class Mdv < Formula
 
   desc "Styled terminal markdown viewer"
   homepage "https://github.com/axiros/terminal_markdown_viewer"
-  # TODO: Remove `depends_on "python-tabulate"` on the next release
-  # Ref: https://github.com/axiros/terminal_markdown_viewer/commit/d2e8d26f39590fdc9c0b9ec0b80b578c7e260c6c
-  url "https://files.pythonhosted.org/packages/70/6d/831e188f8079c9793eac4f62ae55d04a93d90979fd2d8271113687605380/mdv-1.7.4.tar.gz"
-  sha256 "1534f477c85d580352c82141436f6fdba79d329af8a5ee7e329fea14424a660d"
+  url "https://files.pythonhosted.org/packages/d0/32/f5e1b8c70dc40b02604fbd0be3ff0bd5e01ee99c9fddf8f423b10d07cd31/mdv-1.7.5.tar.gz"
+  sha256 "eb84ed52a2b68d2e083e007cb485d14fac1deb755fd8f35011eff8f2889df6e9"
   license "BSD-3-Clause"
 
   bottle do
@@ -23,31 +21,9 @@ class Mdv < Formula
   end
 
   depends_on "pygments"
-  depends_on "python-tabulate"
+  depends_on "python-markdown"
   depends_on "python@3.11"
   depends_on "pyyaml"
-
-  resource "Markdown" do
-    url "https://files.pythonhosted.org/packages/d6/58/79df20de6e67a83f0d0bbfe6c19bb82adf68cdf362885257eb01099f930a/Markdown-3.3.7.tar.gz"
-    sha256 "cbb516f16218e643d8e0a95b309f77eb118cb138d39a4f27851e6a63581db874"
-  end
-
-  # Upstream fix for code blocks not being indexed like expected.
-  # Patching this in allows directly applying Python 3.9 fix in subsequent patch.
-  # Issue ref: https://github.com/axiros/terminal_markdown_viewer/issues/66
-  # TODO: Remove in the next release
-  patch do
-    url "https://github.com/axiros/terminal_markdown_viewer/commit/80f333ba51dc2f1dfa854e203d3374a112aecbd3.patch?full_index=1"
-    sha256 "81e487a8e6ab5c72298186a8097da3a962549b6b8112241cccbeb1666ce54cf2"
-  end
-
-  # Upstream fix for Python 3.9 support.
-  # Issue ref: https://github.com/axiros/terminal_markdown_viewer/issues/86
-  # TODO: Remove in the next release
-  patch do
-    url "https://github.com/axiros/terminal_markdown_viewer/commit/aa6f377d568a0d3188b624bf5218af002373ad87.patch?full_index=1"
-    sha256 "500ddecd02c093cff32edc704628612c8f82e1298452a278cd00767daa073fbe"
-  end
 
   def install
     virtualenv_install_with_resources
