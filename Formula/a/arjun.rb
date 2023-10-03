8ecbef01c3c676ae02cd54f2ad36677080c0885c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-10-03 09:55:37 +0000

    arjun: update 2.2.1_2 bottle.

--- Formula/a/arjun.rb
+++ Formula/a/arjun.rb
@@ -9,16 +9,13 @@ class Arjun < Formula
   revision 2
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "c222a6a78644bffa4f4210001ea1370280275068ce4bac69b85c4d1130e5db61"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "58fb3067a738ec816d57d2749bc304703ad30ce39cc73d97d049e142c4b13d86"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a73485dc5b8e940f5a8653b15eb5d3dc02b841199551de73019d4154d4030789"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "31f7be78b336f904e935f7e2a5f192b5e5cb6aec331193cb8a6600e6b2525f87"
-    sha256 cellar: :any_skip_relocation, sonoma:         "50fa3dfe6d192e985e80f445d380ee17f15580be4573aa1356acd7b392fc8d40"
-    sha256 cellar: :any_skip_relocation, ventura:        "92eba3573ef8e3c438186c0045836817fb6ed89ec8eb0a0eaaabec34c5057077"
-    sha256 cellar: :any_skip_relocation, monterey:       "7fabbdb2a7a3164bd01ab139ba7ccd4d254efa7e40c2ade599c412de20aee68d"
-    sha256 cellar: :any_skip_relocation, big_sur:        "19df3cf79b5734b715ae8a3c0996fdcd748e60f163ac8727956c8bf05a06ac5f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f662d9a2aeaa8b36c6ff54a091858051823bb7cbd09ba65ca102c5576a99d0d6"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "050ae0d3052775038b3b47ac74f78586012fefc9d714850ae1a8a083e0d031a5"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "ed879f6f20e4475ac95baa631a33163bb317bf1dfd8210e9c00fc621e2ef1ccc"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "aa3171a50acc3429288d64518c15504ed3cac6e2667197d828b24baac5ca4efc"
+    sha256 cellar: :any_skip_relocation, sonoma:         "354f61abd8263b24596799285a5d5bacc565ffb0e5b56ed74ebaa833d26438c2"
+    sha256 cellar: :any_skip_relocation, ventura:        "ac4d13ca93b5c21db850c9496eaf2614644170f47434406d684f47f18c42f191"
+    sha256 cellar: :any_skip_relocation, monterey:       "ae5ca6c9a4229c33173c550dc9770b8f4963980424eb38e0766b2311fd0c1dd7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3ea71efb190ad11babe2115273a57f29ba3a89c3fb8729765cbe662d7dd28ba8"
   end
 
   depends_on "python-certifi"
