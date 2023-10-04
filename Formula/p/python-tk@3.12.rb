645f0b0a94676c6763878b27c846d3be84ca9ad7
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-10-04 02:51:56 +0000

    python-tk@3.12: add 3.12.0 bottle.

--- Formula/p/python-tk@3.12.rb
+++ Formula/p/python-tk@3.12.rb
@@ -9,6 +9,16 @@ class PythonTkAT312 < Formula
     formula "python@3.12"
   end
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sonoma:   "c1e3f95ef0bbb7f7b985300063497c6daa4abf5131f93b711663565c4d240af9"
+    sha256 cellar: :any,                 arm64_ventura:  "d73fd18d8772759c5db612d25de99c6a9c5bb8d876b967267c72e493cff1c06d"
+    sha256 cellar: :any,                 arm64_monterey: "adb4224b81a9e32c9c66f1b055dae2325e8416768d4e96f36a883ba2a3ab16f9"
+    sha256 cellar: :any,                 sonoma:         "b3163730c16bf5ede90c961cdb3800a05180f37149c6d9a5d1ce011356bb94be"
+    sha256 cellar: :any,                 ventura:        "7da79232776f6e13ec59e23ac61ed74d1115697af7dcd7f6f71e7c2de8b44afb"
+    sha256 cellar: :any,                 monterey:       "b6ec1890f7aba762eeb685b1e7cde18ac054b60d96e58aee9e130883238c3b5b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "20baa46e683be2d9dda96a3b38550eee7290a0514e1bcf97ce9d7b529ee1c70a"
+  end
+
   depends_on "python@3.12"
   depends_on "tcl-tk"
 
