697ac0ebadaed00ae5acb56690af4a3f8c5b5aef
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-10-04 02:51:57 +0000

    python-gdbm@3.12: add 3.12.0 bottle.

--- Formula/p/python-gdbm@3.12.rb
+++ Formula/p/python-gdbm@3.12.rb
@@ -9,6 +9,16 @@ class PythonGdbmAT312 < Formula
     formula "python@3.12"
   end
 
+  bottle do
+    sha256 cellar: :any, arm64_sonoma:   "9a1f0b964794486c299c9d64f69e76e08d826009d451e1651e2a74a68f46ed78"
+    sha256 cellar: :any, arm64_ventura:  "eeb870ac691c7bed08fd192fb232d2394b5530d112c90a03be77986c20d04339"
+    sha256 cellar: :any, arm64_monterey: "534e52055ee5abf1359d3bd3466558913ca5d72c8e4739396f1d897464168113"
+    sha256 cellar: :any, sonoma:         "58460f812979ac5b3dabf25c85ad4b5df00b95e7e9b6871b03143e5732ba473c"
+    sha256 cellar: :any, ventura:        "044939b1213c668bd3e50e641b03366d57923b36cb00b351190c447f5ba1ea71"
+    sha256 cellar: :any, monterey:       "eee819c21f41327961f46398895835691fb7c3cf4c156518ae935029cbaca079"
+    sha256               x86_64_linux:   "5faea4deef4ca2df7e4514d5c6b5b4104b29ca7989681a1cbde5062d42324ab6"
+  end
+
   depends_on "gdbm"
   depends_on "python@3.12"
 
