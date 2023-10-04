232b1115694b69b2914bf73d0d85962d3bc3d951
Branch Vincent <branchevincent@gmail.com>
2023-10-04 08:04:55 -0700

    cxxtest: migrate to `python@3.12`

--- Formula/c/cxxtest.rb
+++ Formula/c/cxxtest.rb
@@ -2,7 +2,7 @@ class Cxxtest < Formula
   include Language::Python::Virtualenv
 
   desc "C++ unit testing framework similar to JUnit, CppUnit and xUnit"
-  homepage "https://cxxtest.com/"
+  homepage "https://github.com/CxxTest/cxxtest"
   url "https://github.com/CxxTest/cxxtest/releases/download/4.4/cxxtest-4.4.tar.gz"
   mirror "https://deb.debian.org/debian/pool/main/c/cxxtest/cxxtest_4.4.orig.tar.gz"
   sha256 "1c154fef91c65dbf1cd4519af7ade70a61d85a923b6e0c0b007dc7f4895cf7d8"
@@ -23,10 +23,10 @@ class Cxxtest < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "9cf39bf39e516f6a2649f9693b6c45e2cd73dd6eaf63ccae2cc6ea34f116e93e"
   end
 
-  depends_on "python@3.11"
+  depends_on "python@3.12"
 
   def install
-    venv = virtualenv_create(libexec, "python3.11")
+    venv = virtualenv_create(libexec, "python3.12")
     venv.pip_install_and_link buildpath/"python"
 
     include.install "cxxtest"
