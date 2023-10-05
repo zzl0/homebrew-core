dc977fb771d6d46fc153f1df4fe7165511648c6b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-10-05 05:58:35 +0000

    gradle: update 8.4 bottle.

--- Formula/g/gradle.rb
+++ Formula/g/gradle.rb
@@ -11,15 +11,13 @@ class Gradle < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "a05fd6ee0167769945296ee3e66eb0d72394db5216df3a53d3d2f596dcbc4a80"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a05fd6ee0167769945296ee3e66eb0d72394db5216df3a53d3d2f596dcbc4a80"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a05fd6ee0167769945296ee3e66eb0d72394db5216df3a53d3d2f596dcbc4a80"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a05fd6ee0167769945296ee3e66eb0d72394db5216df3a53d3d2f596dcbc4a80"
-    sha256 cellar: :any_skip_relocation, sonoma:         "743d679a05ca1ee12372d1220dea581173ddb31939b03476df19359ab82d24ba"
-    sha256 cellar: :any_skip_relocation, ventura:        "743d679a05ca1ee12372d1220dea581173ddb31939b03476df19359ab82d24ba"
-    sha256 cellar: :any_skip_relocation, monterey:       "743d679a05ca1ee12372d1220dea581173ddb31939b03476df19359ab82d24ba"
-    sha256 cellar: :any_skip_relocation, big_sur:        "743d679a05ca1ee12372d1220dea581173ddb31939b03476df19359ab82d24ba"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a05fd6ee0167769945296ee3e66eb0d72394db5216df3a53d3d2f596dcbc4a80"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "37fd452bf27f2996b459d9d24b1631a26e0625b4960f4219d0ff1db61e27faa4"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "37fd452bf27f2996b459d9d24b1631a26e0625b4960f4219d0ff1db61e27faa4"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "37fd452bf27f2996b459d9d24b1631a26e0625b4960f4219d0ff1db61e27faa4"
+    sha256 cellar: :any_skip_relocation, sonoma:         "e13e584cd2c8432c9c66f19d4a78b86cc75dceab0895f831ffc39e10806bd323"
+    sha256 cellar: :any_skip_relocation, ventura:        "e13e584cd2c8432c9c66f19d4a78b86cc75dceab0895f831ffc39e10806bd323"
+    sha256 cellar: :any_skip_relocation, monterey:       "e13e584cd2c8432c9c66f19d4a78b86cc75dceab0895f831ffc39e10806bd323"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "37fd452bf27f2996b459d9d24b1631a26e0625b4960f4219d0ff1db61e27faa4"
   end
 
   depends_on "openjdk"
