9924ad002c32ab1b06a879989632b25f004de680
David Baumgold <david@davidbaumgold.com>
2023-10-05 23:32:47 +0200

    datasette: set up shell completions

--- Formula/d/datasette.rb
+++ Formula/d/datasette.rb
@@ -137,6 +137,8 @@ class Datasette < Formula
 
   def install
     virtualenv_install_with_resources
+
+    generate_completions_from_executable(bin/"datasette", shells: [:fish, :zsh], shell_parameter_format: :click)
   end
 
   test do
