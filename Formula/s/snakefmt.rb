54270e1daff26865925f5bb6601a557b3f681824
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-10-05 18:43:28 +0000

    snakefmt: update 0.8.5 bottle.

--- Formula/s/snakefmt.rb
+++ Formula/s/snakefmt.rb
@@ -9,16 +9,13 @@ class Snakefmt < Formula
   head "https://github.com/snakemake/snakefmt.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "8b81c797f17508984661dda6e0e1440593b90a0570169ef6231c1809b1a0f5bc"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "78fa80a9a0dd8bed819be21b14eb7f5ba5256e26a3ba79da185ed73dbbc162b6"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e3df170687d2d0d028e335724a8d8c0ba1294671fdace69565b54ad0ec901573"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a6e7660112c57b1db419e5374a4402c2d6b39cc69f7a1bdbd0a3557f459539b3"
-    sha256 cellar: :any_skip_relocation, sonoma:         "443cd227468ef9490ec0da3d8c44ddce73f0cfc8e549cb611a5999cf24aaff13"
-    sha256 cellar: :any_skip_relocation, ventura:        "a1f0e554d2aa849e04e1b8c5dd003468f5a80865339e1965c2678c52b3449678"
-    sha256 cellar: :any_skip_relocation, monterey:       "e70923304c46034abb3af0cf3993ca7b24673a0e38edf9142862d11425b0e6de"
-    sha256 cellar: :any_skip_relocation, big_sur:        "9057a5052ecc45abc5df3ba28b0daaaf5a8ba017a99887bda5f676af7597e0c4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "890638778841809d83b6fc803cd9ea611c0b1aa2d80cb3f8c35f49373a61eef7"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "4e8d9b840fd280e35b5ab8d21fd0f5c3fa20d30c0955d8e6cc011f1baf39f0a3"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "fbd5ee3e29200c87c548b25c24ae241b65ae82e0e558f43b2f643c04d0d11e02"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b5906a25bb2dff36ec64b2586720fbca3910fef3878d0bb8fd2b4a8b912ce40f"
+    sha256 cellar: :any_skip_relocation, sonoma:         "1705fa2e6de9b030120c35efd5bdc70ed458a34cfb5b034a59913c7109317ea3"
+    sha256 cellar: :any_skip_relocation, ventura:        "df96b25cc3bb72b5f3fc8bd209cf66a169728d991726621238037ef7a67d99f1"
+    sha256 cellar: :any_skip_relocation, monterey:       "1b25b83483830c3ac009992898d1762bb4721210259df86a2e88b4efd2e10a9a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0ebbafbdd6b2d1418f3c4e8ce1ce8cf905206c263be4ddcc8ce845650b0028b3"
   end
 
   depends_on "black"
