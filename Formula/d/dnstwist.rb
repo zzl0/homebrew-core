0503aa83ff91afe475760c13361a35bc1141dbd2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-10-06 10:47:32 +0000

    dnstwist: update 20230918_1 bottle.

--- Formula/d/dnstwist.rb
+++ Formula/d/dnstwist.rb
@@ -9,15 +9,13 @@ class Dnstwist < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "7aa32ef2171b35086922a2d7b91f24481b149ac059637df5bf6fa7cf9bb9c34c"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "04c2d50783edfc1c0e1585a8296217744f95707045cd36dbbc8be5dc37fc4cc0"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "2699210156ca537194b2a58175d666a9a6fac3d3214ae887b25b3bde9db72c39"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "27c9fb74c3021080f2c90dcd9c82c86b897aa132a3820108e691c9481d45a829"
-    sha256 cellar: :any_skip_relocation, sonoma:         "25215ce1386177f7990b17cf10dc3309cf8ffa8b19a65967c6af9e8b3dcdb595"
-    sha256 cellar: :any_skip_relocation, ventura:        "70c708523c38375c83caa789c319a41d22d1d9ad106b20a0ae7d56f3213542d7"
-    sha256 cellar: :any_skip_relocation, monterey:       "7a450b0837ede4843aa51b1b983d57dd9383d5bb4467777f04de147fe9142256"
-    sha256 cellar: :any_skip_relocation, big_sur:        "2d4373a1283ef87fe0ca77126a3e25a816f7330204cd63a64a6270eebf01ca84"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bdd172ee845368103d986124c1d4ca4e159dc42edabf165b1dd43e292bcba27a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "1016306caafeec2b6746fbf02de07f32039cf2022e018ea23c7296745cd38881"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "5782805101b4c0a538d308ac281fdc13101763427f5b20f2ce0a796bfc5b4a3f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "74fd2eeefa9fb978d1aec98d19e7138f4091fa0db0183145c5ea97003dd03768"
+    sha256 cellar: :any_skip_relocation, sonoma:         "7cb1c6e4c25d6dc87c3232da71051de30ccc3fcfbca9c97f71e88c4d7cd0b1a6"
+    sha256 cellar: :any_skip_relocation, ventura:        "5b03c7a2ef159bc35ab0e0e6e8b6ca7f73519f19a69cf0603cf952ea6b89230a"
+    sha256 cellar: :any_skip_relocation, monterey:       "75dba5b4217c7a578a41891307748f70fa037a0c66719027f73280ec0be248b6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6f0402e6c2ac7ee81f9af35df1cdae9031e9e1a092619bd2b74b77b72814e58a"
   end
 
   depends_on "geoip"
