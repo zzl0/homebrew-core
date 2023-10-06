9eb85e3af889feafa9da880f7d39fa801e86ab25
Branch Vincent <branchevincent@gmail.com>
2023-10-05 22:08:28 -0700

    meson-python: add python@3.12

--- Formula/m/meson-python.rb
+++ Formula/m/meson-python.rb
@@ -18,9 +18,10 @@ class MesonPython < Formula
   end
 
   depends_on "python-flit-core" => :build
+  depends_on "python-setuptools" => :build
   depends_on "python@3.10" => [:build, :test]
   depends_on "python@3.11" => [:build, :test]
-  depends_on "python@3.9" => [:build, :test]
+  depends_on "python@3.12" => [:build, :test]
   depends_on "meson"
   depends_on "python-packaging"
 
