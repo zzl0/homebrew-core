7767f9e967b2f52ebaa29933ff46bbec8d740d98
Patrick Linnane <patrick@linnane.io>
2023-10-06 18:20:50 -0700

    spack 0.20.2

--- Formula/s/spack.rb
+++ Formula/s/spack.rb
@@ -1,8 +1,8 @@
 class Spack < Formula
   desc "Package manager that builds multiple versions and configurations of software"
   homepage "https://spack.io"
-  url "https://github.com/spack/spack/archive/v0.20.1.tar.gz"
-  sha256 "141be037b56e4b095840a95ac51c428c29dad078f7f88140ae6355b2a1b32dc3"
+  url "https://github.com/spack/spack/archive/v0.20.2.tar.gz"
+  sha256 "62f87ab6ca332118f2812a255edcf4be4977623d067b9396251ce8c44b158e49"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/spack/spack.git", branch: "develop"
 
