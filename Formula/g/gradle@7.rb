04dd0455436e7bad17a17ac5d33d8a16255c8519
Patrick Linnane <patrick@linnane.io>
2023-10-06 18:07:47 -0700

    gradle@7 7.6.3

--- Formula/g/gradle@7.rb
+++ Formula/g/gradle@7.rb
@@ -1,8 +1,8 @@
 class GradleAT7 < Formula
   desc "Open-source build automation tool based on the Groovy and Kotlin DSL"
   homepage "https://www.gradle.org/"
-  url "https://services.gradle.org/distributions/gradle-7.6.2-all.zip"
-  sha256 "5861dcc093556fef71cdde8cbae9887be70a8bf40e0f5856b61181ae92dccf33"
+  url "https://services.gradle.org/distributions/gradle-7.6.3-all.zip"
+  sha256 "6001aba9b2204d26fa25a5800bb9382cf3ee01ccb78fe77317b2872336eb2f80"
   license "Apache-2.0"
 
   livecheck do
