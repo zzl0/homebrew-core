b6a2c98dc99169defce04b747732e85cb518c01e
Patrick Linnane <patrick@linnane.io>
2023-10-06 18:19:00 -0700

    pyside@2 5.15.11
    pyside@2: remove build patch

--- Formula/p/pyside@2.rb
+++ Formula/p/pyside@2.rb
@@ -2,8 +2,8 @@ class PysideAT2 < Formula
   desc "Official Python bindings for Qt"
   homepage "https://wiki.qt.io/Qt_for_Python"
   # TODO: Check if we can use unversioned `llvm` at version bump.
-  url "https://download.qt.io/official_releases/QtForPython/pyside2/PySide2-5.15.10-src/pyside-setup-opensource-src-5.15.10.tar.xz"
-  sha256 "2af691d3613a41f83a60439b46568fc2c696dbfae42f7cd7b07152d115ead33a"
+  url "https://download.qt.io/official_releases/QtForPython/pyside2/PySide2-5.15.11-src/pyside-setup-opensource-src-5.15.11.tar.xz"
+  sha256 "da567cd3b7854d27a0b4afe3e89de8b2f98b7a6d57393be56f1fc13f770faf29"
   license all_of: ["GFDL-1.3-only", "GPL-2.0-only", "GPL-3.0-only", "LGPL-3.0-only"]
 
   livecheck do
@@ -43,16 +43,6 @@ class PysideAT2 < Formula
   patch do
     url "https://src.fedoraproject.org/rpms/python-pyside2/raw/009100c67a63972e4c5252576af1894fec2e8855/f/pyside2-tools-obsolete.patch"
     sha256 "ede69549176b7b083f2825f328ca68bd99ebf8f42d245908abd320093bac60c9"
-  end
-
-  # Fix error: use of undeclared identifier 'NPY_ARRAY_UPDATEIFCOPY'.
-  # Remove when the patch available in the next release.
-  # Ref: https://codereview.qt-project.org/c/pyside/pyside-setup/+/418321
-  # Ref: https://bugreports.qt.io/browse/PYSIDE-2035
-  patch :p3 do
-    url "https://code.qt.io/cgit/pyside/pyside-setup.git/patch/sources/shiboken6/libshiboken/sbknumpyarrayconverter.cpp?id=1422cf4a7f277fb13fd209f24a90d6c02641497d"
-    sha256 "2148af37249932be15ca417076baa9539d487b38a434ec901a67fa9ede724f52"
-    directory "sources/shiboken2"
   end
 
   def python3
