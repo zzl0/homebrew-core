9acadcc17dd3a612367e8559ee4789e9a82176cd
Patrick Linnane <patrick@linnane.io>
2023-10-06 18:14:04 -0700

    leapp-cli 0.1.45

--- Formula/l/leapp-cli.rb
+++ Formula/l/leapp-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class LeappCli < Formula
   desc "Cloud credentials manager cli"
   homepage "https://github.com/noovolari/leapp"
-  url "https://registry.npmjs.org/@noovolari/leapp-cli/-/leapp-cli-0.1.44.tgz"
-  sha256 "9e1315f9a8204073555bead50443be0c3d0c8b3c4cbcde1ab9c81dd54bde34f3"
+  url "https://registry.npmjs.org/@noovolari/leapp-cli/-/leapp-cli-0.1.45.tgz"
+  sha256 "8f9430f08a7a4969d4ec7caf0b3b6b4140788684b6d141651a3f04913f45def1"
   license "MPL-2.0"
 
   bottle do
