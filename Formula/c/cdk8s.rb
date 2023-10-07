548ce855b3030e026d36ce66951ade81bb1abe68
Patrick Linnane <patrick@linnane.io>
2023-10-06 18:00:52 -0700

    cdk8s 2.129.0

--- Formula/c/cdk8s.rb
+++ Formula/c/cdk8s.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Cdk8s < Formula
   desc "Define k8s native apps and abstractions using object-oriented programming"
   homepage "https://cdk8s.io/"
-  url "https://registry.npmjs.org/cdk8s-cli/-/cdk8s-cli-2.127.0.tgz"
-  sha256 "32e028d2c50ac2dfc7aaec32092dd35796c17c252d25d9962c078a614591a404"
+  url "https://registry.npmjs.org/cdk8s-cli/-/cdk8s-cli-2.129.0.tgz"
+  sha256 "a4a990e396fa3bc1b7d9721a06a07d65eb3017b21eb1f599f576fd42f1551be6"
   license "Apache-2.0"
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_sonoma:   "cbb721c6c27b6628fb5fe811ee1bd037264c69d614cdf067e91cb39f8fc2e169"
