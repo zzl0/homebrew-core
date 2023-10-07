dde24d504e93f08a049695f6a4e7959db0ec71a5
David Baumgold <david@davidbaumgold.com>
2023-10-07 12:14:07 +0200

    pip-audit: depend on python-packaging

--- Formula/p/pip-audit.rb
+++ Formula/p/pip-audit.rb
@@ -21,6 +21,7 @@ class PipAudit < Formula
 
   depends_on "pygments"
   depends_on "python-certifi"
+  depends_on "python-packaging"
   depends_on "python@3.11"
   depends_on "six"
 
@@ -87,11 +88,6 @@ class PipAudit < Formula
   resource "packageurl-python" do
     url "https://files.pythonhosted.org/packages/33/34/a7843f732e1e0b01e961f6ae835b3fd6bd4e361c1a3a72debd31244cb718/packageurl-python-0.11.2.tar.gz"
     sha256 "01fbf74a41ef85cf413f1ede529a1411f658bda66ed22d45d27280ad9ceba471"
-  end
-
-  resource "packaging" do
-    url "https://files.pythonhosted.org/packages/fb/2b/9b9c33ffed44ee921d0967086d653047286054117d584f1b1a7c22ceaf7b/packaging-23.2.tar.gz"
-    sha256 "048fb0e9405036518eaaf48a55953c750c11e1a1b68e0dd1a9d62ed0c092cfc5"
   end
 
   resource "pip-api" do
