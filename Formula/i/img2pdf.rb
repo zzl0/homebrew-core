2baf260b37329ce9ff9400f4a557da327cf4a8ea
David Baumgold <david@davidbaumgold.com>
2023-10-07 12:19:47 +0200

    img2pdf: depend on python-packaging, python-lxml

--- Formula/i/img2pdf.rb
+++ Formula/i/img2pdf.rb
@@ -20,25 +20,14 @@ class Img2pdf < Formula
   end
 
   depends_on "pillow"
+  depends_on "python-lxml"
+  depends_on "python-packaging"
   depends_on "python@3.11"
   depends_on "qpdf"
-
-  uses_from_macos "libxml2"
-  uses_from_macos "libxslt"
 
   resource "deprecation" do
     url "https://files.pythonhosted.org/packages/5a/d3/8ae2869247df154b64c1884d7346d412fed0c49df84db635aab2d1c40e62/deprecation-2.1.0.tar.gz"
     sha256 "72b3bde64e5d778694b0cf68178aed03d15e15477116add3fb773e581f9518ff"
-  end
-
-  resource "lxml" do
-    url "https://files.pythonhosted.org/packages/30/39/7305428d1c4f28282a4f5bdbef24e0f905d351f34cf351ceb131f5cddf78/lxml-4.9.3.tar.gz"
-    sha256 "48628bd53a426c9eb9bc066a923acaa0878d1e86129fd5359aee99285f4eed9c"
-  end
-
-  resource "packaging" do
-    url "https://files.pythonhosted.org/packages/b9/6c/7c6658d258d7971c5eb0d9b69fa9265879ec9a9158031206d47800ae2213/packaging-23.1.tar.gz"
-    sha256 "a392980d2b6cffa644431898be54b0045151319d1e7ec34f0cfed48767dd334f"
   end
 
   resource "pikepdf" do
