159c2b105c7c0ddd71f1085f1250acdde9632f62
Branch Vincent <branchevincent@gmail.com>
2023-10-06 21:01:57 -0700

    python-pyproject-hooks: migrate to `python@3.12`

--- Formula/p/python-pyproject-hooks.rb
+++ Formula/p/python-pyproject-hooks.rb
@@ -20,7 +20,7 @@ class PythonPyprojectHooks < Formula
   depends_on "python-flit-core" => :build
   depends_on "python@3.10" => [:build, :test]
   depends_on "python@3.11" => [:build, :test]
-  depends_on "python@3.9" => [:build, :test]
+  depends_on "python@3.12" => [:build, :test]
 
   def pythons
     deps.map(&:to_formula).sort_by(&:version).filter { |f| f.name.start_with?("python@") }
