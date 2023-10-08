f7d9e19c3aadcefe1c748a91db60b4cc3217abaa
Kostas Pagiamtzis <kostas@pagiamtzis.com>
2023-10-08 18:16:46 -0400

    netlistsvg 1.0.2 (new formula)

---
+++ Formula/n/netlistsvg.rb
@@ -0,0 +1,33 @@
+require "language/node"
+
+class Netlistsvg < Formula
+  desc "Draws an SVG schematic from a yosys JSON netlist"
+  homepage "https://github.com/nturley/netlistsvg"
+  url "https://github.com/nturley/netlistsvg/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "b7f218f9d8f2b826d28bff6ebe1f5b1a4fb5b7c9465a034ae0a8fcd2b9d53a67"
+  license "MIT"
+
+  depends_on "yosys" => :test
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    (testpath/"dff.v").write <<~EOS
+      module DFF (output reg Q, input C, D, R);
+      always @(posedge C)
+        if (~R) begin
+            Q <= 1'b0;
+        end else begin
+            Q <= D;
+        end
+      endmodule
+    EOS
+    system "yosys -q -p \"prep -top DFF; write_json dff.json\" dff.v"
+    system bin/"netlistsvg", "dff.json", "-o", "dff.svg"
+    assert_predicate testpath/"dff.svg", :exist?
+  end
+end
