941adaa497e8c58cbc6779e504f07674d99d8509
Rui Chen <rui@chenrui.dev>
2023-10-09 19:19:09 -0400

    murex 5.1.2210

--- Formula/m/murex.rb
+++ Formula/m/murex.rb
@@ -1,8 +1,8 @@
 class Murex < Formula
   desc "Bash-like shell designed for greater command-line productivity and safer scripts"
   homepage "https://murex.rocks"
-  url "https://github.com/lmorg/murex/archive/refs/tags/v5.0.9310.tar.gz"
-  sha256 "4548275096c72171f726bd973952a8e11001b49b7d56470404afeaea46539caf"
+  url "https://github.com/lmorg/murex/archive/refs/tags/v5.1.2210.tar.gz"
+  sha256 "df32c34138b4fc03ca03339ac3061488e87507cf96fe1aad625f876d27091284"
   license "GPL-2.0-only"
   head "https://github.com/lmorg/murex.git", branch: "master"
 
