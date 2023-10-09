65e7b5b6c0cf55ee7ccecebbdf2cb921bfa412d7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-10-09 01:57:21 +0000

    cmake-docs: update 3.27.7 bottle.

--- Formula/c/cmake-docs.rb
+++ Formula/c/cmake-docs.rb
@@ -13,15 +13,13 @@ class CmakeDocs < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "d07c7d3fa9bac89acfefe95a634c2073f1e2f89f9f477f4259b2913cb74064f6"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "d07c7d3fa9bac89acfefe95a634c2073f1e2f89f9f477f4259b2913cb74064f6"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "d07c7d3fa9bac89acfefe95a634c2073f1e2f89f9f477f4259b2913cb74064f6"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d07c7d3fa9bac89acfefe95a634c2073f1e2f89f9f477f4259b2913cb74064f6"
-    sha256 cellar: :any_skip_relocation, sonoma:         "1bef147810c6d0d50fc325044226f3d4beff600a453346505eab3a28d3a5c583"
-    sha256 cellar: :any_skip_relocation, ventura:        "1bef147810c6d0d50fc325044226f3d4beff600a453346505eab3a28d3a5c583"
-    sha256 cellar: :any_skip_relocation, monterey:       "1bef147810c6d0d50fc325044226f3d4beff600a453346505eab3a28d3a5c583"
-    sha256 cellar: :any_skip_relocation, big_sur:        "1bef147810c6d0d50fc325044226f3d4beff600a453346505eab3a28d3a5c583"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d07c7d3fa9bac89acfefe95a634c2073f1e2f89f9f477f4259b2913cb74064f6"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "1f84b3fcf6c137754156590d3028d4ba103a714e6ddb2998133b73d6de172737"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "1f84b3fcf6c137754156590d3028d4ba103a714e6ddb2998133b73d6de172737"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "1f84b3fcf6c137754156590d3028d4ba103a714e6ddb2998133b73d6de172737"
+    sha256 cellar: :any_skip_relocation, sonoma:         "ed2256a403f0b0440e1e1aaa5f0a0dc3c3aa70358b75df3c4ddfa51c7f90b6f3"
+    sha256 cellar: :any_skip_relocation, ventura:        "ed2256a403f0b0440e1e1aaa5f0a0dc3c3aa70358b75df3c4ddfa51c7f90b6f3"
+    sha256 cellar: :any_skip_relocation, monterey:       "ed2256a403f0b0440e1e1aaa5f0a0dc3c3aa70358b75df3c4ddfa51c7f90b6f3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1f84b3fcf6c137754156590d3028d4ba103a714e6ddb2998133b73d6de172737"
   end
 
   depends_on "cmake" => :build
