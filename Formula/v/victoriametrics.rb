94febc3a91a4e8d3443adec4192dc3153659aafb
Rui Chen <rui@chenrui.dev>
2023-10-09 18:33:04 +0000

    victoriametrics: update 1.94.0 bottle.

--- Formula/v/victoriametrics.rb
+++ Formula/v/victoriametrics.rb
@@ -14,15 +14,11 @@ class Victoriametrics < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "4970f349dd10ccde08a70ec686c9f3266f659b0c6d852d35c58ba48aa31541cc"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "deebffac8c59c3de989df2517101d7efcc558c40ea78f4380d9f0d403aff867f"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "1e21bbfde166208a8035a4f8a2f3bc0026a6c92e13de16998b0bb5ec560458a2"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "091a6041401cc9f0fc75c5048abc168117574a1a2a992146746131da31672d6d"
-    sha256 cellar: :any_skip_relocation, sonoma:         "2ed4df6dbe2c69223cb14236cba16fad03f653d8fe95e5baeb8e0154b9a55799"
-    sha256 cellar: :any_skip_relocation, ventura:        "d32113fa04da5ef3df57a1427b338c8404a09bbac990a7c958f7864920f0de62"
-    sha256 cellar: :any_skip_relocation, monterey:       "6591f16048d6ed49cbb77116c104ccd986cefe6504d512b27589928543912d99"
-    sha256 cellar: :any_skip_relocation, big_sur:        "dc5b39a919fd3e729e74078230afc2bb87955cd10f871db74354f19e9725565c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "375613bc2815cc8e5e9e3a3bf2a1b4ceb2dbbe395380d4b96d4869419bbbc324"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "a4f336aab95ca048c28eb1a327c06aab2c2ab6ac566660b8df860e6db50725ae"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "2ad7b6de09dc4e8e816a825553dafa53e58a39385d8aecbbebac2daad7c85fe1"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "0d74ca69a1f551112d8156f961be8c371599cbf10d3e5076be615a057b3db185"
+    sha256 cellar: :any_skip_relocation, monterey:       "4eda7eccdadac58d78acc1804caee35a42fba6c1e828f5aff529e85b1c48082a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "19e837c8a9bf4fc0756f3ba1bb9dc9d06c322e727795c4ef7b762581b22b8eac"
   end
 
   depends_on "go" => :build
