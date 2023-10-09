bf58bdd91f833626723d08af9b0dae9eb2b297b6
Branch Vincent <branchevincent@gmail.com>
2023-10-08 22:11:41 -0700

    cppcheck: migrate to `python@3.12`

--- Formula/c/cppcheck.rb
+++ Formula/c/cppcheck.rb
@@ -19,14 +19,14 @@ class Cppcheck < Formula
   end
 
   depends_on "cmake" => :build
-  depends_on "python@3.11" => [:build, :test]
+  depends_on "python@3.12" => [:build, :test]
   depends_on "pcre"
   depends_on "tinyxml2"
 
   uses_from_macos "libxml2"
 
   def python3
-    which("python3.11")
+    which("python3.12")
   end
 
   def install
