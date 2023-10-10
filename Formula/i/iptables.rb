a26e76af3345d515fb5468a4c866b01ee40798a5
Porkepix <porkepix@gmail.com>
2023-10-10 18:18:53 +0200

    iptables 1.8.10

--- Formula/i/iptables.rb
+++ Formula/i/iptables.rb
@@ -1,8 +1,8 @@
 class Iptables < Formula
   desc "Linux kernel packet control tool"
   homepage "https://www.netfilter.org/projects/iptables/index.html"
-  url "https://www.netfilter.org/pub/iptables/iptables-1.8.9.tar.xz"
-  sha256 "ef6639a43be8325a4f8ea68123ffac236cb696e8c78501b64e8106afb008c87f"
+  url "https://www.netfilter.org/pub/iptables/iptables-1.8.10.tar.xz"
+  sha256 "5cc255c189356e317d070755ce9371eb63a1b783c34498fb8c30264f3cc59c9c"
   license "GPL-2.0-or-later"
 
   livecheck do
