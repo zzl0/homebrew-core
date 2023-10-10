3d028d89ffab280a928d8662e6d13030f699e145
Patrick Linnane <patrick@linnane.io>
2023-10-09 20:15:21 -0700

    scamper 20230614d

--- Formula/s/scamper.rb
+++ Formula/s/scamper.rb
@@ -1,8 +1,8 @@
 class Scamper < Formula
   desc "Advanced traceroute and network measurement utility"
   homepage "https://www.caida.org/catalog/software/scamper/"
-  url "https://www.caida.org/catalog/software/scamper/code/scamper-cvs-20230614c.tar.gz"
-  sha256 "50a58ed36c7e7a4045717c5acc82b510bd2ad329f3b336b3504118e0280ad9ca"
+  url "https://www.caida.org/catalog/software/scamper/code/scamper-cvs-20230614d.tar.gz"
+  sha256 "54423b64a0b68aafa903d9260c2bff5c16f3cca44997e0e830d17296f6f03a59"
   license "GPL-2.0-only"
 
   livecheck do
