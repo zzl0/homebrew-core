7c84dc34e30c2af49aad88744c4de09e01a08d05
Jeffrey H. Johnson <trnsz@pobox.com>
2023-10-10 16:20:23 -0400

    openvi 7.4.24

--- Formula/o/openvi.rb
+++ Formula/o/openvi.rb
@@ -1,8 +1,8 @@
 class Openvi < Formula
   desc "Portable OpenBSD vi for UNIX systems"
   homepage "https://github.com/johnsonjh/OpenVi#readme"
-  url "https://github.com/johnsonjh/OpenVi/archive/refs/tags/7.4.23.tar.gz"
-  sha256 "e353af0ea205663615218a02ac00520187dbb318205c209099027ce3b031fb0d"
+  url "https://github.com/johnsonjh/OpenVi/archive/refs/tags/7.4.24.tar.gz"
+  sha256 "c79c87021c059fbd234578741f623f28aead5b3355edf0e677995d76b10b741b"
   license "BSD-3-Clause"
   head "https://github.com/johnsonjh/OpenVi.git", branch: "master"
 
