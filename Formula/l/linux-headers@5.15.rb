5aa78367b18742d3f698ac2aa8022c69dc1c4baa
Porkepix <porkepix@gmail.com>
2023-10-11 08:06:54 +0200

    linux-headers@5.15 5.15.135

--- Formula/l/linux-headers@5.15.rb
+++ Formula/l/linux-headers@5.15.rb
@@ -1,8 +1,8 @@
 class LinuxHeadersAT515 < Formula
   desc "Header files of the Linux kernel"
   homepage "https://kernel.org/"
-  url "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.15.134.tar.gz"
-  sha256 "5398e612ecda1ea39d14242767fd74625a236699f1ae9e0b41a16672237993e9"
+  url "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.15.135.tar.gz"
+  sha256 "8e1fd63c74b3eaf98b769fb26996923bfd9f9e6829c946dd74223fa20cf5d3d8"
   license "GPL-2.0-only"
 
   livecheck do
