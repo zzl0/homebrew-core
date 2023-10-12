e01349984e08e4165a77bc7db39761618c087da3
Rui Chen <rui@chenrui.dev>
2023-10-12 08:07:10 -0400

    mx 6.51.4

--- Formula/m/mx.rb
+++ Formula/m/mx.rb
@@ -1,8 +1,8 @@
 class Mx < Formula
   desc "Command-line tool used for the development of Graal projects"
   homepage "https://github.com/graalvm/mx"
-  url "https://github.com/graalvm/mx/archive/refs/tags/6.51.3.tar.gz"
-  sha256 "d6a393bc7d81843c936522ede369fafc90c0295c841df14b2ef75ef704ed20cf"
+  url "https://github.com/graalvm/mx/archive/refs/tags/6.51.4.tar.gz"
+  sha256 "1f4b28a4233e3483c785c8c34d71c9e21f3a48ab106c58bf7d028671be3061bb"
   license "GPL-2.0-only"
 
   livecheck do
