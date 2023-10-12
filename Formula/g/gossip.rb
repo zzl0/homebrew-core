ed48c2794311f8c1127a8029c50fb323adc2aab6
Patrick Linnane <patrick@linnane.io>
2023-10-12 18:17:19 +0000

    gossip: add 0.8.1 bottle.

--- Formula/g/gossip.rb
+++ Formula/g/gossip.rb
@@ -7,6 +7,16 @@ class Gossip < Formula
   license "MIT"
   head "https://github.com/mikedilger/gossip.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "2574d71aa492b51c619097ef875c91ddb9b435c5d20a88e19bbbc5f1f4d26ef5"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "4cf9ff27bc96868f2b49668d7b40c66dfb6555bc019c9b3c8b9c6fd97893e625"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "fab2f3352410bbe227be6fb894a17cb1b76668bedc93b0ae953987ac09cc19f3"
+    sha256 cellar: :any_skip_relocation, sonoma:         "594738f0fe3a8da03db3532862bd56524ec6d7141c71af08b441b58d870dbb7e"
+    sha256 cellar: :any_skip_relocation, ventura:        "eb75e908cefff63f2e58a4cfaeba4c753fc645ab7a8cc97ebc70653a26ab3714"
+    sha256 cellar: :any_skip_relocation, monterey:       "62bb0748a3058dd47c8f14f49726e957571e60a790f8007bbf13dc54e83807dd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8c497311838d29ea3feab5c4f00d88227494d5b33ac2129dc6e3e8237a885800"
+  end
+
   depends_on "rust" => :build
 
   on_linux do
