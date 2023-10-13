ebf3efd5910482162568f14ad4747af057654eb9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-10-13 03:04:05 +0000

    contentful-cli: update 3.0.5 bottle.

--- Formula/c/contentful-cli.rb
+++ Formula/c/contentful-cli.rb
@@ -9,13 +9,13 @@ class ContentfulCli < Formula
   head "https://github.com/contentful/contentful-cli.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "0789494df05e34147160b36b720e0ecd3b4ea5dc10bd382428464c4dd1694a0d"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0789494df05e34147160b36b720e0ecd3b4ea5dc10bd382428464c4dd1694a0d"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0789494df05e34147160b36b720e0ecd3b4ea5dc10bd382428464c4dd1694a0d"
-    sha256 cellar: :any_skip_relocation, sonoma:         "d41251911929c54ed11ccae6c1c893bbde05271107505fa53e92788a999ead11"
-    sha256 cellar: :any_skip_relocation, ventura:        "d41251911929c54ed11ccae6c1c893bbde05271107505fa53e92788a999ead11"
-    sha256 cellar: :any_skip_relocation, monterey:       "d41251911929c54ed11ccae6c1c893bbde05271107505fa53e92788a999ead11"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0789494df05e34147160b36b720e0ecd3b4ea5dc10bd382428464c4dd1694a0d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "c33f9334443481855bd976083cd55669b429a303f9616f1f7de8116e64ed061c"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "c33f9334443481855bd976083cd55669b429a303f9616f1f7de8116e64ed061c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c33f9334443481855bd976083cd55669b429a303f9616f1f7de8116e64ed061c"
+    sha256 cellar: :any_skip_relocation, sonoma:         "a2a8f8907ac47422dc8b7feb3a880e3ac98797f1a06261c9aabe01c2d779c5b2"
+    sha256 cellar: :any_skip_relocation, ventura:        "a2a8f8907ac47422dc8b7feb3a880e3ac98797f1a06261c9aabe01c2d779c5b2"
+    sha256 cellar: :any_skip_relocation, monterey:       "a2a8f8907ac47422dc8b7feb3a880e3ac98797f1a06261c9aabe01c2d779c5b2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c33f9334443481855bd976083cd55669b429a303f9616f1f7de8116e64ed061c"
   end
 
   depends_on "node"
