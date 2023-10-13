d8f7a1e411873df0878ef9e820b319cf910bf898
Patrick Linnane <patrick@linnane.io>
2023-10-12 17:51:43 -0700

    saf-cli 1.2.32 (new formula)

---
+++ Formula/s/saf-cli.rb
@@ -0,0 +1,26 @@
+require "language/node"
+
+class SafCli < Formula
+  desc "CLI for the MITRE Security Automation Framework (SAF)"
+  homepage "https://saf-cli.mitre.org"
+  url "https://registry.npmjs.org/@mitre/saf/-/saf-1.2.32.tgz"
+  sha256 "71c2a1c8eead8dde1aed2b682684503d2b66508a2dfe3dbf2027cbab21d82e28"
+  license "Apache-2.0"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+
+    # Replace universal binaries with their native slices
+    deuniversalize_machos
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/saf --version")
+
+    output = shell_output("#{bin}/saf scan")
+    assert_match "Visit https://saf.mitre.org/#/validate to explore and run inspec profiles", output
+  end
+end
