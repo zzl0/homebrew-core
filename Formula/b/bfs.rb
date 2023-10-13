60a41bbd7f102331036a82f8d773f8827e273420
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-10-13 10:35:46 +0000

    bfs: update 3.0.4 bottle.

--- Formula/b/bfs.rb
+++ Formula/b/bfs.rb
@@ -6,13 +6,13 @@ class Bfs < Formula
   license "0BSD"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "3712919efb8b9412915adb24b05b2d09440b1d89c66ca31d67738c35b191cb76"
-    sha256 cellar: :any,                 arm64_ventura:  "bbce0db13af693dbafb1a6446bf061e40a725420ed09386e49e42969d33d7239"
-    sha256 cellar: :any,                 arm64_monterey: "5f66ea744bd1907f7b5826a7b71b84a2c3b812ca13572dbb509bd54f97ad417f"
-    sha256 cellar: :any,                 sonoma:         "a493a9a7edd398c67d4d94b9235ada1294c117a70d2087565fc1cf3c3dd4d505"
-    sha256 cellar: :any,                 ventura:        "9d8574ae854b5db01ecc000ee7e6995a3fce4ffb1fe7db651ac14de762b93632"
-    sha256 cellar: :any,                 monterey:       "a7979cbe57addcd23bf5837d1135c3c1b621c22e35992842c1030fafef9e0ad3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2b8872d15af24b198ec2ea6984d097e27e88500e0c485b63aca2c9c5858265fb"
+    sha256 cellar: :any,                 arm64_sonoma:   "9213655cd67161b40fa5033414040718ca6fe3ac38cd83972e4718e98ddd121a"
+    sha256 cellar: :any,                 arm64_ventura:  "8605ec59b64d396ee9ea203a0ee91cc306158e8a4ccbcd63bf2306ec464e9f09"
+    sha256 cellar: :any,                 arm64_monterey: "26c5e478c43bfafd4c960d75e202682fd70af4d1ce0dd9be933bcfa30257edc2"
+    sha256 cellar: :any,                 sonoma:         "e7a6c10c7673a9a6a54758f0733654295ecb25882ed70b14a4fe72af2bdde5a3"
+    sha256 cellar: :any,                 ventura:        "f8f6a500978011019142ce9dc5d354074553359f6f69673fe0bd90c36458501c"
+    sha256 cellar: :any,                 monterey:       "003fe8e4e3b16d1a98d3e030b0d91588dc60b8dc1f63a5909cbb3877805ac65d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "343104f1672be06cc2e32cf15aad3924aad7becb52dfd40a817d14515b22c1b5"
   end
 
   depends_on "oniguruma"
