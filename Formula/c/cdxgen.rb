a6255a9d93eaf0b6267294014c4d69bb5d92989a
Rui Chen <rui@chenrui.dev>
2023-10-13 21:15:58 -0400

    cdxgen 9.8.10

--- Formula/c/cdxgen.rb
+++ Formula/c/cdxgen.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Cdxgen < Formula
   desc "Creates CycloneDX Software Bill-of-Materials (SBOM) for projects"
   homepage "https://github.com/CycloneDX/cdxgen"
-  url "https://registry.npmjs.org/@cyclonedx/cdxgen/-/cdxgen-9.8.9.tgz"
-  sha256 "64b220a26a377973b30142e840994462c90057d4cef7ccde2a3404f3ec1cd40a"
+  url "https://registry.npmjs.org/@cyclonedx/cdxgen/-/cdxgen-9.8.10.tgz"
+  sha256 "49311abfe04fda69028404cdfde9198b3d4364e48280ed51746dc46ac2de377a"
   license "Apache-2.0"
 
   bottle do
