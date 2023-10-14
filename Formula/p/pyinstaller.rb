be9c6b5f2d3be107349141090524031e28cf014e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-10-14 14:27:53 +0000

    pyinstaller: update 6.1.0 bottle.

--- Formula/p/pyinstaller.rb
+++ Formula/p/pyinstaller.rb
@@ -9,15 +9,13 @@ class Pyinstaller < Formula
   head "https://github.com/pyinstaller/pyinstaller.git", branch: "develop"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "25e9ad835214fc000b8b0a07febfa0017a371eee3a8ff356ca5de0d85f175007"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "2b683665732bb7b3c7d7bcb6999c393aa5cbf05090584c08f7d7df9990a80ff4"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "7979d6856269eab532f78d97b758b1897e0c4522be1739bde3efc14dec1793e3"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5b71f8400b2360e73c16b3f91dd3f386c5e6e1157b3973a19ac87d9a19e9a022"
-    sha256 cellar: :any_skip_relocation, sonoma:         "ff3560a83d9d6792d28ddeef0e460feaff601ba9d56ff7b6acdede3b44f8e4dc"
-    sha256 cellar: :any_skip_relocation, ventura:        "05f2077e99c517b32a02eaa463ee28b108eeeef2561df82db72bfffa6f37ba47"
-    sha256 cellar: :any_skip_relocation, monterey:       "bdd0ebb8d555277b470a50b8f28108b27f6eab38c77890c4598e647864293d17"
-    sha256 cellar: :any_skip_relocation, big_sur:        "193b08fed00df9afae277ff2de5676cfa540bf673460ea33e3460d87572cf79c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7ac0a72a10e361d2c0cfe3cc85601e4ec2a407b8d81c4d18c5ee95beafc020ac"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "31417be402844ac286287843ee86fc57d5f3dca8198979c351d321de8532d9a3"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f1aab0ea79ec25ffd84b4afc441a3b76277b12a4faf8337271b8775ae0ca3a9f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "7f651d1e854168258dbd53db82902dbada392ae7da344c0b5c1ff992fc98c48b"
+    sha256 cellar: :any_skip_relocation, sonoma:         "cb2195b95843e68ffc4caf008a49ff3d929800d5e50d6335965b5e53eb5bf221"
+    sha256 cellar: :any_skip_relocation, ventura:        "10f15ebbdca2394d557000ca2fc2e6054ee67b26daa7c974265d006fe10e887a"
+    sha256 cellar: :any_skip_relocation, monterey:       "71eb1fa554eec785e356d28cc09c40ecbf21246709bb459dcf83c0878241dfdf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8aa832e07e69348612bbaeeb0c366c7ebc0039dee6794bceb7368c4504992b6b"
   end
 
   depends_on "python-packaging"
