17dc5584cb63f88f75370cde282022bff4d15565
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-10-14 14:03:35 +0000

    geph4 4.9.6

--- Formula/g/geph4.rb
+++ Formula/g/geph4.rb
@@ -1,8 +1,8 @@
 class Geph4 < Formula
   desc "Modular Internet censorship circumvention system to deal with national filtering"
   homepage "https://geph.io/"
-  url "https://github.com/geph-official/geph4-client/archive/refs/tags/v4.9.4.tar.gz"
-  sha256 "22b027e78342b7d3f4da0702b966331f6d29d6f9c3146e509977e3c8b2b79de1"
+  url "https://github.com/geph-official/geph4-client/archive/refs/tags/v4.9.6.tar.gz"
+  sha256 "8a5258fbf2423af2ece951b140e91bc7b74acd0139e626ad19b3e9cd3fb9380f"
   license "GPL-3.0-only"
   head "https://github.com/geph-official/geph4-client.git", branch: "master"
 
