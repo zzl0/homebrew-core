51e6e3ab378ac6c08783850f4c59e8cd2e1259e6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-10-15 03:11:35 +0000

    xcb-proto: update 1.16.0 bottle.

--- Formula/x/xcb-proto.rb
+++ Formula/x/xcb-proto.rb
@@ -6,7 +6,8 @@ class XcbProto < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "1b6168c2541cdc925dcb4de03e071365e60ed74d39173c8bc136424b01d2ecb2"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, all: "7866e405dd4eaace1342222fe48949d5cb2d9f750b78d8f2b710b0f0125ae0c6"
   end
 
   depends_on "pkg-config" => [:build, :test]
