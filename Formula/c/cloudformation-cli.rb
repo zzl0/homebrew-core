676320cc210987b348b2cdd4ba9546c761ad1de5
Rui Chen <rui@chenrui.dev>
2023-10-15 10:32:55 -0400

    cloudformation-cli: migrate to `python@3.12`
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    cloudformation-cli: depends_on python-setuptools
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/c/cloudformation-cli.rb
+++ Formula/c/cloudformation-cli.rb
@@ -24,8 +24,9 @@ class CloudformationCli < Formula
   depends_on "python-click"
   depends_on "python-markupsafe"
   depends_on "python-packaging"
+  depends_on "python-setuptools"
   depends_on "python-typing-extensions"
-  depends_on "python@3.11"
+  depends_on "python@3.12"
   depends_on "pyyaml"
   depends_on "six"
 
