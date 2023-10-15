57434a59446516744f243f595d3329d4cbb2afd6
Peter Wu <peter@lekensteyn.nl>
2023-10-15 21:47:17 +0200

    libnghttp3 1.0.0

--- Formula/lib/libnghttp3.rb
+++ Formula/lib/libnghttp3.rb
@@ -1,8 +1,8 @@
 class Libnghttp3 < Formula
   desc "HTTP/3 library written in C"
   homepage "https://nghttp2.org/nghttp3/"
-  url "https://github.com/ngtcp2/nghttp3/archive/refs/tags/v0.15.0.tar.gz"
-  sha256 "6a75f5563e58d99e6b98d442111ac677984011c66b8b4f923764712741399027"
+  url "https://github.com/ngtcp2/nghttp3/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "838def499e368b24d8a4656ad9a1f38bb7ca8b2857a44c5de1c006420cc0bbee"
   license "MIT"
   head "https://github.com/ngtcp2/nghttp3.git", branch: "main"
 
