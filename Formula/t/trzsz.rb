3ce6d82dcaeff5f82cbeb021c634e73b6dc81c0f
Lonny Wong <lonnywong@qq.com>
2023-10-15 11:17:46 +0800

    trzsz 1.1.4

--- Formula/t/trzsz.rb
+++ Formula/t/trzsz.rb
@@ -3,8 +3,8 @@ class Trzsz < Formula
 
   desc "Simple file transfer tools, similar to lrzsz (rz/sz), and compatible with tmux"
   homepage "https://trzsz.github.io"
-  url "https://files.pythonhosted.org/packages/33/14/5b54b20f4027f23c4dab039a3ac2c73df2b79039c449ebbe2903c98c20e4/trzsz-1.1.3.tar.gz"
-  sha256 "4d38e35c128ef86a350bead31cc754c7bbf33ea323d8050ea7ef7d81caf9595e"
+  url "https://files.pythonhosted.org/packages/02/bf/1ee2d66a0ae97eb7fa893239a512a8a19058e325323828a591232f9dd8aa/trzsz-1.1.4.tar.gz"
+  sha256 "942ad73d1c307880ab23fde569ab41191fce469d861b534826a8a1e07c4edf96"
   license "MIT"
 
   bottle do
@@ -28,18 +28,18 @@ class Trzsz < Formula
   end
 
   resource "trzsz-iterm2" do
-    url "https://files.pythonhosted.org/packages/2d/e2/ef11908cb52c1a66b751c7615c7a62aeaa2ca1c13594fcb8bac4403ce4e5/trzsz-iterm2-1.1.3.tar.gz"
-    sha256 "6910e413e65efc14a938a04df7fde5d8c5090f343e572c71e00ff32335fc326f"
+    url "https://files.pythonhosted.org/packages/84/a7/13d3f511f73dd0de5f7f3c2abda51a3fe5acd9cd0afcff5281570e3df1dc/trzsz-iterm2-1.1.4.tar.gz"
+    sha256 "27898b478283e42011066ab52e388c8f4de662ebb61b0ebab5efd92223d8ce63"
   end
 
   resource "trzsz-libs" do
-    url "https://files.pythonhosted.org/packages/9d/7e/59ccff82d9fc525d29721e3e0acd4d3bb0adc9c56e6e56a506009800cba1/trzsz-libs-1.1.3.tar.gz"
-    sha256 "dc64e9cfcfeb23ca406932c89755e1141d443c54795eed0d15cec5581de2fc9f"
+    url "https://files.pythonhosted.org/packages/ae/81/c1e3cc75c7bfa69bf5f190a8399be84fb59b871e182b0b90b2f8448b733d/trzsz-libs-1.1.4.tar.gz"
+    sha256 "2c48a7268294ef535479823a3eaee801d9e4fb8ba2ac4f152f8396849c3f851f"
   end
 
   resource "trzsz-svr" do
-    url "https://files.pythonhosted.org/packages/65/ca/febf965d861a7766fd93349d91f73892abd78b28b30d8ad97b540d1ba07c/trzsz-svr-1.1.3.tar.gz"
-    sha256 "e994511688a62b16ce8c1b28f7abf782bccfd8a41728456ba58b5e7fef970c79"
+    url "https://files.pythonhosted.org/packages/22/a0/e9a530592ecc36d0ccc2dae6cabb45540d3b6da5bf5fb2d6dc79ae999544/trzsz-svr-1.1.4.tar.gz"
+    sha256 "0cf4abda5a6832eebfeeeab4598fdd9fb5512fb6143442da17c54398af1a4e1e"
   end
 
   resource "websockets" do
