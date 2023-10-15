d5e9c6ff738bf29b31f3533b2672d74a58306fe6
Rui Chen <rui@chenrui.dev>
2023-10-15 12:56:55 -0400

    dotdrop: migrate to `python@3.12`
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/d/dotdrop.rb
+++ Formula/d/dotdrop.rb
@@ -21,8 +21,9 @@ class Dotdrop < Formula
 
   depends_on "libmagic"
   depends_on "python-certifi"
+  depends_on "python-markupsafe"
   depends_on "python-packaging"
-  depends_on "python@3.11"
+  depends_on "python@3.12"
 
   resource "charset-normalizer" do
     url "https://files.pythonhosted.org/packages/cf/ac/e89b2f2f75f51e9859979b56d2ec162f7f893221975d244d8d5277aa9489/charset-normalizer-3.3.0.tar.gz"
@@ -47,11 +48,6 @@ class Dotdrop < Formula
   resource "jinja2" do
     url "https://files.pythonhosted.org/packages/7a/ff/75c28576a1d900e87eb6335b063fab47a8ef3c8b4d88524c4bf78f670cce/Jinja2-3.1.2.tar.gz"
     sha256 "31351a702a408a9e7595a8fc6150fc3f43bb6bf7e319770cbc0db9df9437e852"
-  end
-
-  resource "markupsafe" do
-    url "https://files.pythonhosted.org/packages/6d/7c/59a3248f411813f8ccba92a55feaac4bf360d29e2ff05ee7d8e1ef2d7dbf/MarkupSafe-2.1.3.tar.gz"
-    sha256 "af598ed32d6ae86f1b747b82783958b1a4ab8f617b06fe68795c7f026abbdcad"
   end
 
   resource "python-magic" do
