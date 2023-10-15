b925f37c1d8992f2d0a46cc5c007bfbd8f7e8d6d
Rui Chen <rui@chenrui.dev>
2023-10-15 14:43:40 -0400

    http-prompt: migrate to `python@3.12`
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/h/http-prompt.rb
+++ Formula/h/http-prompt.rb
@@ -21,18 +21,14 @@ class HttpPrompt < Formula
 
   depends_on "pygments"
   depends_on "python-certifi"
-  depends_on "python@3.11"
+  depends_on "python-click"
+  depends_on "python@3.12"
   depends_on "pyyaml"
   depends_on "six"
 
   resource "charset-normalizer" do
     url "https://files.pythonhosted.org/packages/cf/ac/e89b2f2f75f51e9859979b56d2ec162f7f893221975d244d8d5277aa9489/charset-normalizer-3.3.0.tar.gz"
     sha256 "63563193aec44bce707e0c5ca64ff69fa72ed7cf34ce6e11d5127555756fd2f6"
-  end
-
-  resource "click" do
-    url "https://files.pythonhosted.org/packages/96/d3/f04c7bfcf5c1862a2a5b845c6b2b360488cf47af55dfa79c98f6a6bf98b5/click-8.1.7.tar.gz"
-    sha256 "ca9853ad459e787e2192211578cc907e7594e294c7ccc834310722b41b9ca6de"
   end
 
   resource "defusedxml" do
