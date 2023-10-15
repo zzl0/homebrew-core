292c5ab2d039eae974351f5c6a66cddfe6285f12
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-10-15 15:30:57 +0000

    coconut: update 3.0.3 bottle.

--- Formula/c/coconut.rb
+++ Formula/c/coconut.rb
@@ -8,15 +8,14 @@ class Coconut < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "a6ccd87d8ecef727d80f87619ac5cb211e0405b920cfed8204b586df85d34dda"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "632c51ca99a56bd2df9897c209407667ceb9329738358d11175b0db309eb6b31"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5cf3176715fcf41cd445ea3e6ee5282a837da9d1d6f0ccfac173e95b3930d856"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "09ca958748b13f389c0eba7f3680a0665a8e8677f41f441fb227cba9a3b253d2"
-    sha256 cellar: :any_skip_relocation, sonoma:         "5c90b549b437231155ee0f3673178c8cfb225b0b3ca4df0bff47ed4cf982394f"
-    sha256 cellar: :any_skip_relocation, ventura:        "e0eb0a04ae4a8023ed2e04b5a81b5a607850842f4712fbdc14fe4733ba5b864f"
-    sha256 cellar: :any_skip_relocation, monterey:       "690c3217fe38be2d19779b08a03ca00bb1ac205b3f263de9269a6cfec884d011"
-    sha256 cellar: :any_skip_relocation, big_sur:        "17b29d5cc866cc727d03ea376bdfe5480f589eb39b25cc08558a880c16bf71ba"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ef4859b5baadfb7f45687ec69b7a0465d449273b995ed223151f52967c55da65"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "db52e0d6a5e2524009b7ce23bf62f453dc0dbbc2d1e833f7b91fdc1ec1c7ed87"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "4d150da03dbbdc798128a5e49905990401805e793f74dbc44674f6d8b1250824"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3978aab75be877dc206a76c73f2254f587101cccf00c82e36b2c8b74d21aed2d"
+    sha256 cellar: :any_skip_relocation, sonoma:         "259eabc26d6adc8d1078c997ac22a326ec7c490c28c650894e4ddf16024c4d9e"
+    sha256 cellar: :any_skip_relocation, ventura:        "1fe79f37a88688b5e04da41e2f9cb06dc281eccb85b7eac10cf1b551e0336a27"
+    sha256 cellar: :any_skip_relocation, monterey:       "14238493fd40f764bb5a3bb1f87aac37b06cd439e78c727f62291edf1fca28c4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e343e2c13525b1fe786db1f33d24098cc80145fb523d7ef7ed2de47b3c661abe"
   end
 
   depends_on "pygments"
