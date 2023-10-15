89678888adb146120424cc67a76a0e2d0f5f5e9c
Rui Chen <rui@chenrui.dev>
2023-10-15 10:54:28 -0400

    conan: migrate to `python@3.12`
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/c/conan.rb
+++ Formula/c/conan.rb
@@ -23,7 +23,8 @@ class Conan < Formula
   depends_on "cmake" => :test
   depends_on "pygments"
   depends_on "python-certifi"
-  depends_on "python@3.11"
+  depends_on "python-markupsafe"
+  depends_on "python@3.12"
   depends_on "pyyaml"
   depends_on "six"
 
@@ -55,11 +56,6 @@ class Conan < Formula
   resource "jinja2" do
     url "https://files.pythonhosted.org/packages/7a/ff/75c28576a1d900e87eb6335b063fab47a8ef3c8b4d88524c4bf78f670cce/Jinja2-3.1.2.tar.gz"
     sha256 "31351a702a408a9e7595a8fc6150fc3f43bb6bf7e319770cbc0db9df9437e852"
-  end
-
-  resource "markupsafe" do
-    url "https://files.pythonhosted.org/packages/6d/7c/59a3248f411813f8ccba92a55feaac4bf360d29e2ff05ee7d8e1ef2d7dbf/MarkupSafe-2.1.3.tar.gz"
-    sha256 "af598ed32d6ae86f1b747b82783958b1a4ab8f617b06fe68795c7f026abbdcad"
   end
 
   resource "patch-ng" do
