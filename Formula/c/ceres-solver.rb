a256d36609011fa7455f94c249ebfc88bb22a0a3
Sean Molenaar <smillerdev@me.com>
2023-10-15 12:27:00 +0200

    ceres-solver 2.2.0

--- Formula/c/ceres-solver.rb
+++ Formula/c/ceres-solver.rb
@@ -1,10 +1,9 @@
 class CeresSolver < Formula
   desc "C++ library for large-scale optimization"
   homepage "http://ceres-solver.org/"
-  url "http://ceres-solver.org/ceres-solver-2.1.0.tar.gz"
-  sha256 "f7d74eecde0aed75bfc51ec48c91d01fe16a6bf16bce1987a7073286701e2fc6"
+  url "http://ceres-solver.org/ceres-solver-2.2.0.tar.gz"
+  sha256 "48b2302a7986ece172898477c3bcd6deb8fb5cf19b3327bc49969aad4cede82d"
   license "BSD-3-Clause"
-  revision 3
   head "https://ceres-solver.googlesource.com/ceres-solver.git", branch: "master"
 
   livecheck do
@@ -34,12 +33,6 @@ class CeresSolver < Formula
   depends_on "tbb"
 
   fails_with gcc: "5" # C++17
-
-  # Fix version detection for suite-sparse >= 6.0. Remove in next release.
-  patch do
-    url "https://github.com/ceres-solver/ceres-solver/commit/352b320ab1b5438a0838aea09cbbf07fa4ff5d71.patch?full_index=1"
-    sha256 "0289adbea4cb66ccff57eeb844dd6d6736c37649b6ff329fed73cf0e9461fb53"
-  end
 
   def install
     system "cmake", "-S", ".", "-B", "homebrew-build",
