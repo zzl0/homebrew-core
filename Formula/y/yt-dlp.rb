478c809bcc1c01e0916330d8bb8ba254061e4c19
Rui Chen <rui@chenrui.dev>
2023-10-16 10:12:42 -0400

    yt-dlp: migrate to `python@3.12`
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/y/yt-dlp.rb
+++ Formula/y/yt-dlp.rb
@@ -24,7 +24,7 @@ class YtDlp < Formula
 
   depends_on "python-certifi"
   depends_on "python-mutagen"
-  depends_on "python@3.11"
+  depends_on "python@3.12"
 
   resource "brotli" do
     url "https://files.pythonhosted.org/packages/2f/c2/f9e977608bdf958650638c3f1e28f85a1b075f075ebbe77db8555463787b/Brotli-1.1.0.tar.gz"
