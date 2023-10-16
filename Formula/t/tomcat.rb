80b7d8501221c4ce90fff721a13fda04a197e1df
Rui Chen <rui@chenrui.dev>
2023-10-16 10:21:03 -0400

    tomcat 10.1.15

--- Formula/t/tomcat.rb
+++ Formula/t/tomcat.rb
@@ -1,9 +1,9 @@
 class Tomcat < Formula
   desc "Implementation of Java Servlet and JavaServer Pages"
   homepage "https://tomcat.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=tomcat/tomcat-10/v10.1.14/bin/apache-tomcat-10.1.14.tar.gz"
-  mirror "https://archive.apache.org/dist/tomcat/tomcat-10/v10.1.14/bin/apache-tomcat-10.1.14.tar.gz"
-  sha256 "3ec0e07d91a77f17de2ae31c2d586c3069bb348a4b94002fca1b9ac15a022816"
+  url "https://www.apache.org/dyn/closer.lua?path=tomcat/tomcat-10/v10.1.15/bin/apache-tomcat-10.1.15.tar.gz"
+  mirror "https://archive.apache.org/dist/tomcat/tomcat-10/v10.1.15/bin/apache-tomcat-10.1.15.tar.gz"
+  sha256 "72a416dc3737b02ff5ce889d30818304dc381b96e7c58be1987cfb53b2ba0e38"
   license "Apache-2.0"
 
   bottle do
