a63292753fa441e5a0d31e4713ee39eb2187734b
Patrick Linnane <patrick@linnane.io>
2023-10-16 08:51:39 -0700

    falco 1.1.0

--- Formula/f/falco.rb
+++ Formula/f/falco.rb
@@ -1,8 +1,8 @@
 class Falco < Formula
   desc "VCL parser and linter optimized for Fastly"
   homepage "https://github.com/ysugimoto/falco"
-  url "https://github.com/ysugimoto/falco/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "a118b1038221fb999f654572c992f7be447805644a5f9b3144db27ce4693b78a"
+  url "https://github.com/ysugimoto/falco/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "7fd4507f1be53e6af84492d205eb976bc1ff6ef66a1fa27ff856a7fd4449edaf"
   license "MIT"
   head "https://github.com/ysugimoto/falco.git", branch: "main"
 
