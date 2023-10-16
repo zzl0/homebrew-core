7e43cb93d885911c3890effb7d02e8c0cc95af0c
Rui Chen <rui@chenrui.dev>
2023-10-16 07:43:59 -0400

    protobuf@21: support python 3.12
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    protobuf@21: use `python-setuptools` for build
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/p/protobuf@21.rb
+++ Formula/p/protobuf@21.rb
@@ -20,8 +20,10 @@ class ProtobufAT21 < Formula
   keg_only :versioned_formula
 
   depends_on "cmake" => :build
+  depends_on "python-setuptools" => :build
   depends_on "python@3.10" => [:build, :test]
   depends_on "python@3.11" => [:build, :test]
+  depends_on "python@3.12" => [:build, :test]
   uses_from_macos "zlib"
 
   def pythons
