973a775cd07235d66b797583f258ad6f6cbd349e
Porkepix <porkepix@gmail.com>
2023-10-16 12:28:41 +0200

    pandocomatic 1.1.1

--- Formula/p/pandocomatic.rb
+++ Formula/p/pandocomatic.rb
@@ -1,8 +1,8 @@
 class Pandocomatic < Formula
   desc "Automate the use of pandoc"
   homepage "https://heerdebeer.org/Software/markdown/pandocomatic/"
-  url "https://github.com/htdebeer/pandocomatic/archive/1.1.0.tar.gz"
-  sha256 "33a2f2ac628c54851be1b5b10114b3e086f08efd8c28826d00608fca1f3616b7"
+  url "https://github.com/htdebeer/pandocomatic/archive/1.1.1.tar.gz"
+  sha256 "fb3b77f01cb52927163965fd911f9b59a04f133896b3a15d9aa1b56704a0d6e8"
   license "GPL-3.0-or-later"
 
   bottle do
