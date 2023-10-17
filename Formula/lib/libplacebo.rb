dff6533ef356613af15ec49725a67f4e6f2156d9
Patrick Linnane <patrick@linnane.io>
2023-10-17 08:36:04 -0700

    libplacebo 6.338.1
    libplacebo: use `python-markupsafe`
    
    libplacebo: migrate to `python@3.12`

--- Formula/lib/libplacebo.rb
+++ Formula/lib/libplacebo.rb
@@ -7,8 +7,8 @@ class Libplacebo < Formula
   head "https://code.videolan.org/videolan/libplacebo.git", branch: "master"
 
   stable do
-    url "https://code.videolan.org/videolan/libplacebo/-/archive/v6.338.0/libplacebo-v6.338.0.tar.bz2"
-    sha256 "256c5cb01cafddc341bb7cd4df9b17f7e92b943e6cf5696ec1bd5cd1d5a381c8"
+    url "https://code.videolan.org/videolan/libplacebo/-/archive/v6.338.1/libplacebo-v6.338.1.tar.bz2"
+    sha256 "66f173e511884ad96c23073e6c3a846215db804f098e11698132abe5a63d6f72"
 
     resource "fast_float" do
       url "https://github.com/fastfloat/fast_float/archive/refs/tags/v5.2.0.tar.gz"
@@ -24,11 +24,6 @@ class Libplacebo < Formula
       url "https://files.pythonhosted.org/packages/7a/ff/75c28576a1d900e87eb6335b063fab47a8ef3c8b4d88524c4bf78f670cce/Jinja2-3.1.2.tar.gz"
       sha256 "31351a702a408a9e7595a8fc6150fc3f43bb6bf7e319770cbc0db9df9437e852"
     end
-
-    resource "markupsafe" do
-      url "https://files.pythonhosted.org/packages/6d/7c/59a3248f411813f8ccba92a55feaac4bf360d29e2ff05ee7d8e1ef2d7dbf/MarkupSafe-2.1.3.tar.gz"
-      sha256 "af598ed32d6ae86f1b747b82783958b1a4ab8f617b06fe68795c7f026abbdcad"
-    end
   end
 
   bottle do
@@ -43,11 +38,13 @@ class Libplacebo < Formula
 
   depends_on "meson" => :build
   depends_on "ninja" => :build
-  depends_on "python@3.11" => :build
+  depends_on "python-setuptools" => :build
+  depends_on "python@3.12" => :build
   depends_on "vulkan-headers" => :build
 
   depends_on "glslang"
   depends_on "little-cms2"
+  depends_on "python-markupsafe"
   depends_on "sdl2"
   depends_on "vulkan-loader"
 
