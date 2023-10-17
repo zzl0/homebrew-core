ab3f194c50dc0a403a831f04ccb77bfbe73ebaee
Patrick Linnane <patrick@linnane.io>
2023-10-17 08:40:18 -0700

    openliberty-webprofile8 23.0.0.10

--- Formula/o/openliberty-webprofile8.rb
+++ Formula/o/openliberty-webprofile8.rb
@@ -1,8 +1,8 @@
 class OpenlibertyWebprofile8 < Formula
   desc "Lightweight open framework for Java (Jakarta EE Web Profile 8)"
   homepage "https://openliberty.io"
-  url "https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/release/23.0.0.9/openliberty-webProfile8-23.0.0.9.zip"
-  sha256 "5b8a18598d606ea2b2d4144985a989690fec0e9fea9b97dc896c047fbb8a4389"
+  url "https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/release/23.0.0.10/openliberty-webProfile8-23.0.0.10.zip"
+  sha256 "7ee32560455bd786ba75ce9b7379e6a3de236d5cd5f12c50d0c8832c31c3162e"
   license "EPL-1.0"
 
   livecheck do
