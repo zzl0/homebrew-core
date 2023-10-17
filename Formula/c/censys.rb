d1a20400333e4702820e3cb63266dab55a58d0ec
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-10-17 22:16:22 +0000

    censys: bump python resources

--- Formula/c/censys.rb
+++ Formula/c/censys.rb
@@ -6,7 +6,7 @@ class Censys < Formula
   url "https://files.pythonhosted.org/packages/31/3f/2094e4c07b5f8142242705b8f7af42550f1b70c9ce457684b834850d40ea/censys-2.2.7.tar.gz"
   sha256 "abec858e5be89c45a60477bd7e51412fee7f55917325b11c3cd970298de3e2bd"
   license "Apache-2.0"
-  revision 1
+  revision 2
 
   bottle do
     rebuild 2
@@ -60,8 +60,8 @@ class Censys < Formula
   end
 
   resource "urllib3" do
-    url "https://files.pythonhosted.org/packages/8b/00/db794bb94bf09cadb4ecd031c4295dd4e3536db4da958e20331d95f1edb7/urllib3-2.0.6.tar.gz"
-    sha256 "b19e1a85d206b56d7df1d5e683df4a7725252a964e3993648dd0fb5a1c157564"
+    url "https://files.pythonhosted.org/packages/af/47/b215df9f71b4fdba1025fc05a77db2ad243fa0926755a52c5e71659f4e3c/urllib3-2.0.7.tar.gz"
+    sha256 "c97dfde1f7bd43a71c8d2a58e369e9b2bf692d1334ea9f9cae55add7d0dd0f84"
   end
 
   def install
