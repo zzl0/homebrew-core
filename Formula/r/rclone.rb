af43f125961913213cc4cb79bc6f0095c93912ee
djerfy <djerfy@gmail.com>
2023-10-17 20:32:18 +0200

    rclone 1.64.1

--- Formula/r/rclone.rb
+++ Formula/r/rclone.rb
@@ -1,8 +1,8 @@
 class Rclone < Formula
   desc "Rsync for cloud storage"
   homepage "https://rclone.org/"
-  url "https://github.com/rclone/rclone/archive/v1.64.0.tar.gz"
-  sha256 "3297838fdcf611a5ad605835f41c0e51031ce9f220c77a4ad0af6283b7805329"
+  url "https://github.com/rclone/rclone/archive/v1.64.1.tar.gz"
+  sha256 "c94ad2c2fa79485667aae4a239ed04e786bd6e26bced05f9d95dda6d10f7a014"
   license "MIT"
   head "https://github.com/rclone/rclone.git", branch: "master"
 
