18fc5ee78ffbe9350dfca14b470dc808af29a566
Patrick Linnane <patrick@linnane.io>
2023-10-17 08:29:44 -0700

    faas-cli 0.16.17

--- Formula/f/faas-cli.rb
+++ Formula/f/faas-cli.rb
@@ -2,8 +2,8 @@ class FaasCli < Formula
   desc "CLI for templating and/or deploying FaaS functions"
   homepage "https://www.openfaas.com/"
   url "https://github.com/openfaas/faas-cli.git",
-      tag:      "0.16.16",
-      revision: "7934de92b73aa169b163e3281d8bdc89e22ada51"
+      tag:      "0.16.17",
+      revision: "f72db8de657001a2967582c535fe8351c259c5f6"
   license "MIT"
   head "https://github.com/openfaas/faas-cli.git", branch: "master"
 
