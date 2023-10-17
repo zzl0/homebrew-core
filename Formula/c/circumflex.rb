2b1c33225569e08c50ab050cb7e186e03b3e9dca
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-10-17 13:50:43 +0000

    circumflex: update 3.5 bottle.

--- Formula/c/circumflex.rb
+++ Formula/c/circumflex.rb
@@ -6,13 +6,13 @@ class Circumflex < Formula
   license "AGPL-3.0-only"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "671eb0f32aea8923e55b3f8d910f614dcecc0e89c7f0a220dcc6837485ba16b2"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a773f40874f88b907e23bbfe372beefdf5e127461e7f61e4dee3802460cd7c37"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "07af3e61ff32b873de8c2861ee38de9deace3d5a5f849b57fd6804a5260103f5"
-    sha256 cellar: :any_skip_relocation, sonoma:         "d24db7a043efe062338a32ae56c0947a9f0286d508c91a9be7d7aacdcd36c5bf"
-    sha256 cellar: :any_skip_relocation, ventura:        "2b93078164ecfd9eeb4a50f8b439241a763aab6b67a00eb676d7d6db7eb31d98"
-    sha256 cellar: :any_skip_relocation, monterey:       "8a9af69756bc6623341a66d98c3a571ed556a716e141413acc84c0ce21b9aff1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3c6ab73bcb9db986943c946bcbbf32c0f7fd2f048854c857a307fa7e31073f8d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "cbc2af822ea36db0ccd4d3558917aa8181f7fa50bde955525e0feaf945dbb956"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a191af7749b700fd6e6c005410a52d6aac38012acc3c8a12c8414ae0f0980a1e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c67741ab4de1c00bcb83bc1531b4c5898e9b5aa35cfbc568643032719c181b72"
+    sha256 cellar: :any_skip_relocation, sonoma:         "a2b73df956b323eaad6c8a4a4403570608f9a31fb4f394011369834ca53af15f"
+    sha256 cellar: :any_skip_relocation, ventura:        "acf2d7cfa8ec8f3e9ad6b199496857db33983644519511a496a7a7f238f5eaf0"
+    sha256 cellar: :any_skip_relocation, monterey:       "367e9b6107f7d1f2857a3607d6dcfe582669fb9dfb219f7197fc1f5023406393"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0a81b1d1469b7a6de175d3514248b1da2d4aa1e0f7f8ab201f33c68c5a7908f2"
   end
 
   depends_on "go" => :build
