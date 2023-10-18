c7a5665994b3cc87a088b3ffbb364b8c4d8c8f44
Michael Cho <michael@michaelcho.dev>
2023-10-18 11:29:14 -0700

    python-packaging: revision bump for `python@3.12` support

--- Formula/p/python-packaging.rb
+++ Formula/p/python-packaging.rb
@@ -4,6 +4,7 @@ class PythonPackaging < Formula
   url "https://files.pythonhosted.org/packages/fb/2b/9b9c33ffed44ee921d0967086d653047286054117d584f1b1a7c22ceaf7b/packaging-23.2.tar.gz"
   sha256 "048fb0e9405036518eaaf48a55953c750c11e1a1b68e0dd1a9d62ed0c092cfc5"
   license any_of: ["Apache-2.0", "BSD-2-Clause"]
+  revision 1
 
   bottle do
     rebuild 1
