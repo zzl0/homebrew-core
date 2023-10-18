e6881f7c2b2a02f351d11ed4dfc5e0bcedea349e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-10-18 05:23:07 +0000

    numcpp 2.12.1

--- Formula/n/numcpp.rb
+++ Formula/n/numcpp.rb
@@ -1,8 +1,8 @@
 class Numcpp < Formula
   desc "C++ implementation of the Python Numpy library"
   homepage "https://dpilger26.github.io/NumCpp"
-  url "https://github.com/dpilger26/NumCpp/archive/Version_2.11.0.tar.gz"
-  sha256 "ea675775c3535589f268224efa31ae6003a68e95a465e92c99496a0c9366bdc2"
+  url "https://github.com/dpilger26/NumCpp/archive/Version_2.12.1.tar.gz"
+  sha256 "f462ecd27126e6057b31fa38f1f72cef2c4223c9d848515412970714a5bb6d16"
   license "MIT"
   head "https://github.com/dpilger26/NumCpp.git", branch: "master"
 
