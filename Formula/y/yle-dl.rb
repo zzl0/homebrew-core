cc90570f613fc29ca55268abefd73398537ee446
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-10-18 20:52:14 +0000

    yle-dl: bump python resources

--- Formula/y/yle-dl.rb
+++ Formula/y/yle-dl.rb
@@ -6,7 +6,7 @@ class YleDl < Formula
   url "https://files.pythonhosted.org/packages/34/40/2f3a90f891a32a16929fcca7099e874ebb0af4dfc5c06309e309d5198f65/yle_dl-20230611.tar.gz"
   sha256 "b6e26d95f5a748bb82126058b548b6515798815967fde24ffe933585a4411df6"
   license "GPL-3.0-or-later"
-  revision 2
+  revision 3
   head "https://github.com/aajanki/yle-dl.git", branch: "master"
 
   bottle do
@@ -54,8 +54,8 @@ class YleDl < Formula
   end
 
   resource "urllib3" do
-    url "https://files.pythonhosted.org/packages/8b/00/db794bb94bf09cadb4ecd031c4295dd4e3536db4da958e20331d95f1edb7/urllib3-2.0.6.tar.gz"
-    sha256 "b19e1a85d206b56d7df1d5e683df4a7725252a964e3993648dd0fb5a1c157564"
+    url "https://files.pythonhosted.org/packages/af/47/b215df9f71b4fdba1025fc05a77db2ad243fa0926755a52c5e71659f4e3c/urllib3-2.0.7.tar.gz"
+    sha256 "c97dfde1f7bd43a71c8d2a58e369e9b2bf692d1334ea9f9cae55add7d0dd0f84"
   end
 
   resource "xattr" do
