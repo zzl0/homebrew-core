e133835612827799d1426b2f69c618a02d5a07f3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-10-18 03:19:27 +0000

    chatblade: bump python resources
    chatblade: revert aiohttp dep change
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/c/chatblade.rb
+++ Formula/c/chatblade.rb
@@ -6,7 +6,7 @@ class Chatblade < Formula
   url "https://files.pythonhosted.org/packages/02/bc/c3dd9e46eaeae6218909b2a13b79a71b8b6ad30fe7ede8f9dc2dba530d5b/chatblade-0.3.1.tar.gz"
   sha256 "401511bcf7c305b845f80850d12b07627673af944ddfa0aa6b60eb010656d205"
   license "GPL-3.0-only"
-  revision 3
+  revision 4
 
   bottle do
     rebuild 1
@@ -111,8 +111,8 @@ class Chatblade < Formula
   end
 
   resource "urllib3" do
-    url "https://files.pythonhosted.org/packages/8b/00/db794bb94bf09cadb4ecd031c4295dd4e3536db4da958e20331d95f1edb7/urllib3-2.0.6.tar.gz"
-    sha256 "b19e1a85d206b56d7df1d5e683df4a7725252a964e3993648dd0fb5a1c157564"
+    url "https://files.pythonhosted.org/packages/af/47/b215df9f71b4fdba1025fc05a77db2ad243fa0926755a52c5e71659f4e3c/urllib3-2.0.7.tar.gz"
+    sha256 "c97dfde1f7bd43a71c8d2a58e369e9b2bf692d1334ea9f9cae55add7d0dd0f84"
   end
 
   resource "yarl" do
