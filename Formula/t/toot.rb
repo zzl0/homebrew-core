c44c8a4cb4c927b4255f5d36cc3a3c98ffc2e4a5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-10-18 16:38:04 +0000

    toot: bump python resources

--- Formula/t/toot.rb
+++ Formula/t/toot.rb
@@ -6,7 +6,7 @@ class Toot < Formula
   url "https://files.pythonhosted.org/packages/4c/55/f7c6578f031e1c5d6605c1977ea36a9d7b89a72aef79b93fbd9719a15a74/toot-0.38.1.tar.gz"
   sha256 "be9e5479a21ea8fb13cf7ba98d542daae07fd87fb56b20b8923b69ffa521c6b2"
   license "GPL-3.0-only"
-  revision 1
+  revision 2
   head "https://github.com/ihabunek/toot.git", branch: "master"
 
   bottle do
@@ -53,8 +53,8 @@ class Toot < Formula
   end
 
   resource "urllib3" do
-    url "https://files.pythonhosted.org/packages/8b/00/db794bb94bf09cadb4ecd031c4295dd4e3536db4da958e20331d95f1edb7/urllib3-2.0.6.tar.gz"
-    sha256 "b19e1a85d206b56d7df1d5e683df4a7725252a964e3993648dd0fb5a1c157564"
+    url "https://files.pythonhosted.org/packages/af/47/b215df9f71b4fdba1025fc05a77db2ad243fa0926755a52c5e71659f4e3c/urllib3-2.0.7.tar.gz"
+    sha256 "c97dfde1f7bd43a71c8d2a58e369e9b2bf692d1334ea9f9cae55add7d0dd0f84"
   end
 
   resource "urwid" do
