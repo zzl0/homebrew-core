1cb9b8f790da7ab593e4e9523fc6d1cf8913cf3c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-10-18 20:48:46 +0000

    twine-pypi: bump python resources

--- Formula/t/twine-pypi.rb
+++ Formula/t/twine-pypi.rb
@@ -6,7 +6,7 @@ class TwinePypi < Formula
   url "https://files.pythonhosted.org/packages/b7/1a/a7884359429d801cd63c2c5512ad0a337a509994b0e42d9696d4778d71f6/twine-4.0.2.tar.gz"
   sha256 "9e102ef5fdd5a20661eb88fad46338806c3bd32cf1db729603fe3697b1bc83c8"
   license "Apache-2.0"
-  revision 2
+  revision 3
   head "https://github.com/pypa/twine.git", branch: "main"
 
   bottle do
@@ -102,8 +102,8 @@ class TwinePypi < Formula
   end
 
   resource "urllib3" do
-    url "https://files.pythonhosted.org/packages/8b/00/db794bb94bf09cadb4ecd031c4295dd4e3536db4da958e20331d95f1edb7/urllib3-2.0.6.tar.gz"
-    sha256 "b19e1a85d206b56d7df1d5e683df4a7725252a964e3993648dd0fb5a1c157564"
+    url "https://files.pythonhosted.org/packages/af/47/b215df9f71b4fdba1025fc05a77db2ad243fa0926755a52c5e71659f4e3c/urllib3-2.0.7.tar.gz"
+    sha256 "c97dfde1f7bd43a71c8d2a58e369e9b2bf692d1334ea9f9cae55add7d0dd0f84"
   end
 
   resource "zipp" do
