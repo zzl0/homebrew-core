1e95bcda2afc2b6cc5c8f95ce041f3792b5cc56a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-10-18 11:59:33 +0000

    grokmirror: bump python resources

--- Formula/g/grokmirror.rb
+++ Formula/g/grokmirror.rb
@@ -6,7 +6,7 @@ class Grokmirror < Formula
   url "https://files.pythonhosted.org/packages/b0/ef/ffad6177d84dafb7403ccaca2fef735745d5d43200167896a2068422ae89/grokmirror-2.0.11.tar.gz"
   sha256 "6bc1310dc9a0e97836201e6bb14ecbbee332b0f812b9ff345a8386cb267c908c"
   license "GPL-3.0-or-later"
-  revision 3
+  revision 4
   head "https://github.com/mricon/grokmirror.git", branch: "master"
 
   bottle do
@@ -39,8 +39,8 @@ class Grokmirror < Formula
   end
 
   resource "urllib3" do
-    url "https://files.pythonhosted.org/packages/8b/00/db794bb94bf09cadb4ecd031c4295dd4e3536db4da958e20331d95f1edb7/urllib3-2.0.6.tar.gz"
-    sha256 "b19e1a85d206b56d7df1d5e683df4a7725252a964e3993648dd0fb5a1c157564"
+    url "https://files.pythonhosted.org/packages/af/47/b215df9f71b4fdba1025fc05a77db2ad243fa0926755a52c5e71659f4e3c/urllib3-2.0.7.tar.gz"
+    sha256 "c97dfde1f7bd43a71c8d2a58e369e9b2bf692d1334ea9f9cae55add7d0dd0f84"
   end
 
   def install
