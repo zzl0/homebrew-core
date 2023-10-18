fd1a00054e31cfc905da0659ca81e3e47c8016e1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-10-18 12:01:57 +0000

    internetarchive: bump python resources

--- Formula/i/internetarchive.rb
+++ Formula/i/internetarchive.rb
@@ -6,7 +6,7 @@ class Internetarchive < Formula
   url "https://files.pythonhosted.org/packages/7a/6a/ce5deecc9a65709e48367bf949874551fee5f4f595141b7fd0d4426b0d95/internetarchive-3.5.0.tar.gz"
   sha256 "2a9625e1dcbe431e5b898402273a4345bf5cb46012599cbe92e664ffdc36e881"
   license "AGPL-3.0-or-later"
-  revision 2
+  revision 3
 
   bottle do
     rebuild 1
@@ -69,8 +69,8 @@ class Internetarchive < Formula
   end
 
   resource "urllib3" do
-    url "https://files.pythonhosted.org/packages/8b/00/db794bb94bf09cadb4ecd031c4295dd4e3536db4da958e20331d95f1edb7/urllib3-2.0.6.tar.gz"
-    sha256 "b19e1a85d206b56d7df1d5e683df4a7725252a964e3993648dd0fb5a1c157564"
+    url "https://files.pythonhosted.org/packages/af/47/b215df9f71b4fdba1025fc05a77db2ad243fa0926755a52c5e71659f4e3c/urllib3-2.0.7.tar.gz"
+    sha256 "c97dfde1f7bd43a71c8d2a58e369e9b2bf692d1334ea9f9cae55add7d0dd0f84"
   end
 
   def install
