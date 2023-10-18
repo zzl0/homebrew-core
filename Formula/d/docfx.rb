9b18723b4e0efffbf49e67733157aed3bc5f3f14
Rui Chen <rui@chenrui.dev>
2023-10-18 15:51:54 -0400

    docfx 2.71.1

--- Formula/d/docfx.rb
+++ Formula/d/docfx.rb
@@ -1,8 +1,8 @@
 class Docfx < Formula
   desc "Tools for building and publishing API documentation for .NET projects"
   homepage "https://dotnet.github.io/docfx/"
-  url "https://github.com/dotnet/docfx/archive/refs/tags/v2.71.0.tar.gz"
-  sha256 "3423dc8b24d9271b6c33da93374e4682a311cc416b4017cfc98cfe553d6b1997"
+  url "https://github.com/dotnet/docfx/archive/refs/tags/v2.71.1.tar.gz"
+  sha256 "e5817a279674d9e3a1d85bd5efb109830ab5753a8c3e615fd0628a37db88f147"
   license "MIT"
 
   bottle do
