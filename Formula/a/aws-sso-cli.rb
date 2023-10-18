06ac6d9857c222eb9899f8ef2b17a65d2faaf7c7
Aaron Turner <synfinatic@gmail.com>
2023-10-17 19:30:48 -0700

    aws-sso-cli 1.14.1

--- Formula/a/aws-sso-cli.rb
+++ Formula/a/aws-sso-cli.rb
@@ -2,8 +2,8 @@ class AwsSsoCli < Formula
   desc "Securely manage AWS API credentials using AWS SSO"
   homepage "https://github.com/synfinatic/aws-sso-cli"
   url "https://github.com/synfinatic/aws-sso-cli.git",
-      tag:      "v1.14.0",
-      revision: "4415620e18eef221579f334f2d0687a605b54b72"
+      tag:      "v1.14.1",
+      revision: "b3f6be7babc4e05956d0b7988bf7b3465bd25ad3"
   license "GPL-3.0-only"
   head "https://github.com/synfinatic/aws-sso-cli.git", branch: "main"
 
