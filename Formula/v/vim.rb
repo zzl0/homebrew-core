b6623df4ec33570cc9a210f85491038181ec578d
Rui Chen <rui@chenrui.dev>
2023-10-18 18:08:44 -0400

    vim: migrate to `python@3.12`
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/v/vim.rb
+++ Formula/v/vim.rb
@@ -31,7 +31,7 @@ class Vim < Formula
   depends_on "lua"
   depends_on "ncurses"
   depends_on "perl"
-  depends_on "python@3.11"
+  depends_on "python@3.12"
   depends_on "ruby"
 
   conflicts_with "ex-vi",
@@ -41,7 +41,7 @@ class Vim < Formula
     because: "vim and macvim both install vi* binaries"
 
   def install
-    ENV.prepend_path "PATH", Formula["python@3.11"].opt_libexec/"bin"
+    ENV.prepend_path "PATH", Formula["python@3.12"].opt_libexec/"bin"
 
     # https://github.com/Homebrew/homebrew-core/pull/1046
     ENV.delete("SDKROOT")
