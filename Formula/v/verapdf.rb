c82156443963fb7215d721af2bf30ba11f8fe418
Porkepix <porkepix@gmail.com>
2023-10-19 17:39:05 +0200

    verapdf 1.25.76

--- Formula/v/verapdf.rb
+++ Formula/v/verapdf.rb
@@ -1,8 +1,8 @@
 class Verapdf < Formula
   desc "Open-source industry-supported PDF/A validation"
   homepage "https://verapdf.org/home/"
-  url "https://github.com/veraPDF/veraPDF-apps/archive/refs/tags/v1.25.73.tar.gz"
-  sha256 "61de8c7b82fd279e2af429ec1ce0d13198af51474624682f8c3129f797e094fb"
+  url "https://github.com/veraPDF/veraPDF-apps/archive/refs/tags/v1.25.76.tar.gz"
+  sha256 "132ca218196090ccbd3afff85b7da4650142f42fea71b1c1abc480a65ceb4839"
   license any_of: ["GPL-3.0-or-later", "MPL-2.0"]
   head "https://github.com/veraPDF/veraPDF-apps.git", branch: "integration"
 
