fd8f46d2554388e6ce8abc0b4e732bfe3d810820
Patrick Linnane <patrick@linnane.io>
2023-10-19 15:46:51 -0700

    repo 2.38

--- Formula/r/repo.rb
+++ Formula/r/repo.rb
@@ -4,8 +4,8 @@ class Repo < Formula
   desc "Repository tool for Android development"
   homepage "https://source.android.com/source/developing.html"
   url "https://gerrit.googlesource.com/git-repo.git",
-      tag:      "v2.37",
-      revision: "83c66ec661e39e47795086a5d26d0f3782ac1d49"
+      tag:      "v2.38",
+      revision: "024df06ec15d7304fbb5f9a2b1aa44f2af9daf4c"
   license "Apache-2.0"
   version_scheme 1
 
