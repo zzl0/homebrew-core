21aa8119dffc7de306344a0b553e0baa696dcf75
Patrick Linnane <patrick@linnane.io>
2023-10-19 14:12:29 -0700

    hapi-fhir-cli 6.8.4

--- Formula/h/hapi-fhir-cli.rb
+++ Formula/h/hapi-fhir-cli.rb
@@ -1,8 +1,8 @@
 class HapiFhirCli < Formula
   desc "Command-line interface for the HAPI FHIR library"
   homepage "https://hapifhir.io/"
-  url "https://github.com/hapifhir/hapi-fhir/releases/download/v6.8.3/hapi-fhir-6.8.3-cli.zip"
-  sha256 "afa59f36eb610a8251438b5e98f76a24313bca775d42568793a9b5b1bda0674b"
+  url "https://github.com/hapifhir/hapi-fhir/releases/download/v6.8.4/hapi-fhir-6.8.4-cli.zip"
+  sha256 "836eb2c6e6dffadddefa50b2d22f51ec057bf7e75de21a882c83dd0757114d49"
   license "Apache-2.0"
 
   # The "latest" release on GitHub is sometimes for an older major/minor, so we
