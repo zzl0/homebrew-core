e7e03a75e0d57ab826e628aa52e0555db4b394e9
Rui Chen <rui@chenrui.dev>
2023-10-19 16:02:46 -0400

    zlib-ng 2.1.4

--- Formula/z/zlib-ng.rb
+++ Formula/z/zlib-ng.rb
@@ -1,8 +1,8 @@
 class ZlibNg < Formula
   desc "Zlib replacement with optimizations for next generation systems"
   homepage "https://github.com/zlib-ng/zlib-ng"
-  url "https://github.com/zlib-ng/zlib-ng/archive/2.1.3.tar.gz"
-  sha256 "d20e55f89d71991c59f1c5ad1ef944815e5850526c0d9cd8e504eaed5b24491a"
+  url "https://github.com/zlib-ng/zlib-ng/archive/2.1.4.tar.gz"
+  sha256 "a0293475e6a44a3f6c045229fe50f69dc0eebc62a42405a51f19d46a5541e77a"
   license "Zlib"
 
   bottle do
