b8fc4a9ef4c4e70e0e8642b4a483ff7685437a07
Patrick Linnane <patrick@linnane.io>
2023-10-19 10:19:37 -0700

    nixpacks 1.18.0

--- Formula/n/nixpacks.rb
+++ Formula/n/nixpacks.rb
@@ -1,8 +1,8 @@
 class Nixpacks < Formula
   desc "App source + Nix packages + Docker = Image"
   homepage "https://nixpacks.com/"
-  url "https://github.com/railwayapp/nixpacks/archive/refs/tags/v1.17.0.tar.gz"
-  sha256 "2bbd6ef1502fb41cd9cf91b0d3ee217de27886b13c958e04e849c3f661772d30"
+  url "https://github.com/railwayapp/nixpacks/archive/refs/tags/v1.18.0.tar.gz"
+  sha256 "25b8b61db06bfadb42a84492baed213dd05341f06b153602d8a26f097ae51a69"
   license "MIT"
   head "https://github.com/railwayapp/nixpacks.git", branch: "main"
 
