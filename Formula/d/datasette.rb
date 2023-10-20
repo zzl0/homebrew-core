95c8bad00ecae4d7aa62eb63c11161e7205017be
David Baumgold <david@davidbaumgold.com>
2023-10-20 20:38:04 +0200

    datasette: depend on python-click, python-markupsafe

--- Formula/d/datasette.rb
+++ Formula/d/datasette.rb
@@ -19,6 +19,8 @@ class Datasette < Formula
   end
 
   depends_on "python-certifi"
+  depends_on "python-click"
+  depends_on "python-markupsafe"
   depends_on "python-setuptools"
   depends_on "python-typing-extensions"
   depends_on "python@3.12"
@@ -43,11 +45,6 @@ class Datasette < Formula
   resource "asgiref" do
     url "https://files.pythonhosted.org/packages/12/19/64e38c1c2cbf0da9635b7082bbdf0e89052e93329279f59759c24a10cc96/asgiref-3.7.2.tar.gz"
     sha256 "9e0ce3aa93a819ba5b45120216b23878cf6e8525eb3848653452b4192b92afed"
-  end
-
-  resource "click" do
-    url "https://files.pythonhosted.org/packages/96/d3/f04c7bfcf5c1862a2a5b845c6b2b360488cf47af55dfa79c98f6a6bf98b5/click-8.1.7.tar.gz"
-    sha256 "ca9853ad459e787e2192211578cc907e7594e294c7ccc834310722b41b9ca6de"
   end
 
   resource "click-default-group" do
@@ -93,11 +90,6 @@ class Datasette < Formula
   resource "jinja2" do
     url "https://files.pythonhosted.org/packages/7a/ff/75c28576a1d900e87eb6335b063fab47a8ef3c8b4d88524c4bf78f670cce/Jinja2-3.1.2.tar.gz"
     sha256 "31351a702a408a9e7595a8fc6150fc3f43bb6bf7e319770cbc0db9df9437e852"
-  end
-
-  resource "markupsafe" do
-    url "https://files.pythonhosted.org/packages/6d/7c/59a3248f411813f8ccba92a55feaac4bf360d29e2ff05ee7d8e1ef2d7dbf/MarkupSafe-2.1.3.tar.gz"
-    sha256 "af598ed32d6ae86f1b747b82783958b1a4ab8f617b06fe68795c7f026abbdcad"
   end
 
   resource "mergedeep" do
