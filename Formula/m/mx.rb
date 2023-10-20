63e804ee35b86c70adf9afbcc1f65a26fa7328dc
Porkepix <porkepix@gmail.com>
2023-10-20 10:47:30 +0200

    mx 6.52.0

--- Formula/m/mx.rb
+++ Formula/m/mx.rb
@@ -1,8 +1,8 @@
 class Mx < Formula
   desc "Command-line tool used for the development of Graal projects"
   homepage "https://github.com/graalvm/mx"
-  url "https://github.com/graalvm/mx/archive/refs/tags/6.51.6.tar.gz"
-  sha256 "46cb5d6d287feee42b93fd86dc3aa6b8fc2c65891cb882f7c14483c998aefe33"
+  url "https://github.com/graalvm/mx/archive/refs/tags/6.52.0.tar.gz"
+  sha256 "939b7e59bd31d68176b54e249da3f10f8cc2cc172468f4c6d8ec47cc6e087ac0"
   license "GPL-2.0-only"
 
   livecheck do
