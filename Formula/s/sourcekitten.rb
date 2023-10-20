13cd99282586ea897166c8da24f74411413885b7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-10-20 21:49:29 +0000

    sourcekitten: update 0.34.1 bottle.

--- Formula/s/sourcekitten.rb
+++ Formula/s/sourcekitten.rb
@@ -8,10 +8,13 @@ class Sourcekitten < Formula
   head "https://github.com/jpsim/SourceKitten.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "bf824c9e874b8f19c74a0553ba0d7977cd151295f9920059a740768cb1b99913"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "2aba055153236af33d75a900140217f5bee6f939a291de46720ae73b5cc0583f"
-    sha256 cellar: :any_skip_relocation, ventura:        "64087bd481a91558143401b526a698b33594a8b831898d7f15b23c8d0d0fb50c"
-    sha256 cellar: :any_skip_relocation, monterey:       "760aeb628d7253077edca91193dc39cb18b54af8668ab520b28b0ee357ac2a01"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "b8e8c03abf8411f909a910b72e91eec75c96435be7011806158d10b7d5991ae0"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "98ca219d2062f4501b0dc5821274ee57e93b357889709ed1b68e09c417c70658"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2396d7736386e9b389da34e9667a20c84d42db627e0cd1435fb4a5ed552637d1"
+    sha256 cellar: :any_skip_relocation, sonoma:         "672d24b4160cff44cf0e7f0b90c3daeb9f19819eef33cfec6c703c9c4df81a85"
+    sha256 cellar: :any_skip_relocation, ventura:        "ff7817850aeea5f48f6ad8767975527f07f0987688a2bbe25922efb100898b15"
+    sha256 cellar: :any_skip_relocation, monterey:       "d99b27e850722229a9da7fd8f55a655dde4f5b920e03d3d660ecb4088b363613"
   end
 
   depends_on xcode: ["14.0", :build]
