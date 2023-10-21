22d73d77958722240d4b2dd5bcde79d68e74457a
Porkepix <porkepix@gmail.com>
2023-10-21 21:19:40 +0200

    astyle 3.4.10

--- Formula/a/astyle.rb
+++ Formula/a/astyle.rb
@@ -1,8 +1,8 @@
 class Astyle < Formula
   desc "Source code beautifier for C, C++, C#, and Java"
   homepage "https://astyle.sourceforge.net/"
-  url "https://downloads.sourceforge.net/project/astyle/astyle/astyle%203.4/astyle-3.4.9.tar.bz2"
-  sha256 "44239275c70eda6591471129962cc03d19eba41b15cb194590971c3b4a2de18d"
+  url "https://downloads.sourceforge.net/project/astyle/astyle/astyle%203.4/astyle-3.4.10.tar.bz2"
+  sha256 "6f67ec872b437bd3c51e0f75e112e4dbfdb26d5fb7bd9cf8a480f10af5557063"
   license "MIT"
   head "https://svn.code.sf.net/p/astyle/code/trunk/AStyle"
 
