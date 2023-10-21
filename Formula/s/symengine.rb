3d2bd4aa54f5d11fc51affff5e8cea1a72f7819c
Rui Chen <rui@chenrui.dev>
2023-10-21 11:23:48 -0400

    symengine: revision bump (flint 3.0.0)
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    symengine: add upstream patch for flint v3
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/s/symengine.rb
+++ Formula/s/symengine.rb
@@ -5,6 +5,7 @@ class Symengine < Formula
   url "https://github.com/symengine/symengine/releases/download/v0.11.1/symengine-0.11.1.tar.gz"
   sha256 "217b39955dc19f920c6f54c057fdc89e8e155ddee8f0e3c3cacc67b3e3850b64"
   license "MIT"
+  revision 1
 
   bottle do
     sha256 cellar: :any,                 arm64_sonoma:   "452da25ab2ac00b95aa885ae5e3f47169dc309f76afa0b662c2469032615b4af"
@@ -33,6 +34,13 @@ class Symengine < Formula
   patch do
     url "https://gitweb.gentoo.org/repo/gentoo.git/plain/sci-libs/symengine/files/symengine-0.8.1-fix_llvm.patch?id=83ab9587be9f89e667506b861208d613a2f016e5"
     sha256 "c654ea7c4ee44c689433e87f71c7ae78e6c04968e7dfe89be5e4ba4c8c53713b"
+  end
+
+  # fix flint 3 compatibility
+  # https://github.com/symengine/symengine/pull/1985, remove in next release
+  patch do
+    url "https://github.com/symengine/symengine/commit/9b2526ffee85ebb2b3011513ae544aec1b54d623.patch?full_index=1"
+    sha256 "8232c3cd757e8f9eecf296bc8e8353830c604d89e368b12de0c45715a7fa660d"
   end
 
   def install
