adb1407572b539136090ce9bcdbe2568bb34196a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-10-22 11:38:05 +0000

    regipy: update 3.1.6 bottle.

--- Formula/r/regipy.rb
+++ Formula/r/regipy.rb
@@ -9,16 +9,14 @@ class Regipy < Formula
   head "https://github.com/mkorman90/regipy.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "e507dfeeea70f242daad7191a617d78d4966480867ea096f8fb501d8c8219b45"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "52230b24a98fee373f2ac1c31a2201305805fd9ea42753c0662066d4f2e8d950"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "52ef445e8ede15b5290cb3b666375b45bf460042569799491732359c9fc88f01"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "742f3e46a6323c2e26f01195d72a17f15a9f4b1461c8c710e9f2d8155cc85c3d"
-    sha256 cellar: :any_skip_relocation, sonoma:         "2f95a14e10accdff6918158d9bfdcb6744585c1ec4d86939713cb015954056fc"
-    sha256 cellar: :any_skip_relocation, ventura:        "36c82ae06f653cc2bca956a7b658f864ce1055cc7130b10ac3f80cdb3a7d8999"
-    sha256 cellar: :any_skip_relocation, monterey:       "f7386b0e830d654eec78f9d921ee05b53684d2f45411d32b63da5fd75bf2f91b"
-    sha256 cellar: :any_skip_relocation, big_sur:        "3e09699a9518bf88789d87cef68c49b035eccea88411124623aa245db79a8bdc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "80b37ae8e56b2fac41fa904ef956035fcd881fdea7386604561ce57824b2dfce"
+    rebuild 2
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "3c228285dae25a3651e6baba502a522837c8d2cc1076d6adaca7f8fc806a731c"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "41d0c06d9a4f35b4d6c2a90fe484940c92f564316cc5672a5d14dff0486f349b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b89c5207e8037065d74c6df55dd63633063ea4a039190901643bb44ab8858bba"
+    sha256 cellar: :any_skip_relocation, sonoma:         "efa3c9158dedf6675c04ef54dd3126d57a35b4ad89c518d8be70cfb2df6bbda2"
+    sha256 cellar: :any_skip_relocation, ventura:        "e94c00ab84ebed8cd117f98da53b6cace219a3e422af0ea7b8433d9d3102d85e"
+    sha256 cellar: :any_skip_relocation, monterey:       "022f206299aebaa2f8319f3e040d91ce64094051939b61a2cc5acf02fcb3c4d4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "03872c4afcca0055fe5457ccdc5f87cc3eec37a3691af6f7e7b73b1e46e26017"
   end
 
   depends_on "python-click"
