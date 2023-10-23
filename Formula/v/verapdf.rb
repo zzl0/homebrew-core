f251bdfdb99b6e6c2f21cc05246bc6ea0de68fd9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-10-23 19:11:47 +0000

    verapdf: update 1.25.82 bottle.

--- Formula/v/verapdf.rb
+++ Formula/v/verapdf.rb
@@ -12,13 +12,13 @@ class Verapdf < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "26b72c47b9795c385b3d1afd90b0dafdb7590b78c9af4c8a5239bd76e161ef19"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0bb850a6c5e2f4b7d13c2d9149a7496f20acc495446c82875e6f8bea3e45fd49"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ccd8db652170ec57a35095747a94c1d5b262f6674e4cea8940a7e13500e9ea7f"
-    sha256 cellar: :any_skip_relocation, sonoma:         "4a2dcc748a753c5e2e27ef82963add00f9c8b1d5deaefae39c64c761db44043e"
-    sha256 cellar: :any_skip_relocation, ventura:        "a0c4d70adb5bfc33a739d76d3a2b93b9acbb6867b8383f8a7ec6758c51cb217c"
-    sha256 cellar: :any_skip_relocation, monterey:       "90dc4c5940cdc39f340f834a5ab3b82704d0d38f0ba37ed0063b5e7baf8280d2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b3a076bc3076e252755fbd5ac91abb9b4e44758c9bd281f9c5cf4a457e47451a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "9fa8fa9daaccc8f51e4115f08af3190a7237681d9402d1587392b1673fdf7f43"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "c89f859d9a691864f8fd7347c9b7e407e4b7c61fe675f7e95ac0bfcf3658539c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "fc88aeb8be5ad378b5b1fd315db3b046f1002d5997506b12ff047a06fb144ca2"
+    sha256 cellar: :any_skip_relocation, sonoma:         "59760cf1490492a02c8b7fd6ce867f2144ee0bf2acb424adecf05a6735d912f6"
+    sha256 cellar: :any_skip_relocation, ventura:        "4d1c3a499fd9697da6d70a97ddbb127d1e6bdabcc76b45ad0959c6de786f3dd7"
+    sha256 cellar: :any_skip_relocation, monterey:       "f579e1b2f6ad2844c5c394a079e34676d8f8157bba876cb921a9045b281d5c51"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a44928c40c6bb7728024d75d518174b043faa0e3ed54ddbbe8b6ca78db550ab5"
   end
 
   depends_on "maven" => :build
