2ce123cd94e66c6fd5f2c98389549012f95ae5d9
Patrick Linnane <patrick@linnane.io>
2023-10-23 15:32:30 -0700

    xeol 0.9.5 (new formula)

---
+++ Formula/x/xeol.rb
@@ -0,0 +1,29 @@
+class Xeol < Formula
+  desc "Xcanner for end-of-life software in container images, filesystems, and SBOMs"
+  homepage "https://github.com/xeol-io/xeol"
+  url "https://github.com/xeol-io/xeol/archive/refs/tags/v0.9.5.tar.gz"
+  sha256 "462db8fc69be684b6ff9d91af400739ecd75883c0f42dc3b36e97b5fad869daf"
+  license "Apache-2.0"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X main.version=#{version}
+      -X main.gitCommit=#{tap.user}
+      -X main.buildDate=#{time.iso8601}
+      -X main.gitDescription=#{tap.user}
+    ]
+    system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/xeol"
+
+    generate_completions_from_executable(bin/"xeol", "completion")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/xeol version")
+
+    output = shell_output("#{bin}/xeol alpine:latest")
+    assert_match "no EOL software has been found", output
+  end
+end
