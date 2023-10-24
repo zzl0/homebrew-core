af6ee6f266ea559d5c3b475a919d6cb6d2f2fe35
Patrick Linnane <patrick@linnane.io>
2023-10-24 14:08:07 -0700

    rom-tools 0.260

--- Formula/r/rom-tools.rb
+++ Formula/r/rom-tools.rb
@@ -1,11 +1,10 @@
 class RomTools < Formula
   desc "Tools for Multiple Arcade Machine Emulator"
   homepage "https://mamedev.org/"
-  url "https://github.com/mamedev/mame/archive/refs/tags/mame0259.tar.gz"
-  version "0.259"
-  sha256 "46baf431079a3373ffe8d984b3ba5d62ad5b1d5e356d1f60cf60f6ad03d4cec6"
+  url "https://github.com/mamedev/mame/archive/refs/tags/mame0260.tar.gz"
+  version "0.260"
+  sha256 "104ca8daab3ce7bb9637e19f1dc60a08ac6856db730ab544275567addb9541cd"
   license "GPL-2.0-or-later"
-  revision 1
   head "https://github.com/mamedev/mame.git", branch: "master"
 
   livecheck do
