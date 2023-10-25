600d020f68e59e50a04b72aa7f91d9993a6eb58f
Patrick Linnane <patrick@linnane.io>
2023-10-24 17:27:29 -0700

    snyk-cli 1.1237.0 (new formula)

---
+++ Formula/s/snyk-cli.rb
@@ -0,0 +1,23 @@
+require "language/node"
+
+class SnykCli < Formula
+  desc "Scans and monitors projects for security vulnerabilities"
+  homepage "https://snyk.io"
+  url "https://registry.npmjs.org/snyk/-/snyk-1.1237.0.tgz"
+  sha256 "8eea3a603e5a220d89d37e87c020cebb456ee7848cecc62a8bdeba4ebb2256f9"
+  license "Apache-2.0"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/snyk version")
+
+    output = shell_output("#{bin}/snyk auth homebrew", 2)
+    assert_match "Authentication failed. Please check the API token on https://snyk.io", output
+  end
+end
