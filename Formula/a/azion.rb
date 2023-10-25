31ee2bc4511bd244f6cd11fe6790282b9dcb5fa5
Patrick Linnane <patrick@linnane.io>
2023-10-25 11:02:30 -0700

    azion: rename from azion-cli

---
+++ Formula/a/azion.rb
@@ -0,0 +1,27 @@
+class Azion < Formula
+  desc "CLI for the Azion service"
+  homepage "https://github.com/aziontech/azion"
+  url "https://github.com/aziontech/azion/archive/refs/tags/1.5.1.tar.gz"
+  sha256 "26c362b63075afac06a1c0a70f8299c0a53ae9ad38f3eff7f6b1af04e7b83e18"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/aziontech/azion-cli/pkg/cmd/version.BinVersion=#{version}
+      -X github.com/aziontech/azion-cli/pkg/constants.StorageApiURL=https://storage-api.azion.com
+      -X github.com/aziontech/azion-cli/pkg/constants.AuthURL=https://sso.azion.com/api/user/me
+      -X github.com/aziontech/azion-cli/pkg/constants.ApiURL=https://api.azionapi.net
+    ]
+    system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/azion"
+
+    generate_completions_from_executable(bin/"azion", "completion")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/azion --version")
+    assert_match "Failed to build your resource", shell_output("#{bin}/azion dev 2>&1", 1)
+  end
+end
