d66ad9a3fed5432404c108090a6b3dfbe1b58b36
Patrick Linnane <patrick@linnane.io>
2023-10-25 16:52:16 +0000

    bob: add 2.6.0 bottle.

--- Formula/b/bob.rb
+++ Formula/b/bob.rb
@@ -5,6 +5,16 @@ class Bob < Formula
   sha256 "05ac4956812d5cde18cf4059f0e5ce92a0556bf7a78bd487eec8670d235a4617"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "2b7f1cfc7f99a1d89fb25447da1188a86dea65a05588f678433f40c340100752"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f36b343d2ebcb6e64e592856143ac2c9f6ddcc30a6fb2bc22ad5ee3334031e7e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "afc74a19ebcb5791af1ae1e565559f0599c0f78c99e30cae7f7eef2c12bced08"
+    sha256 cellar: :any_skip_relocation, sonoma:         "b28d36e21f34cf56160787d5cb337454436f614aea763e428c0e9499b76d19e9"
+    sha256 cellar: :any_skip_relocation, ventura:        "60607219fc1d337f4a9753b2757f093daf99f347402b75575fc569f2e48d7691"
+    sha256 cellar: :any_skip_relocation, monterey:       "cb8f0703a4384ca99d58c87dff01717fc6c9e403534b236feb52bb5f308aa79b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3c905142359be287df601b380bb79984064fb84737ad1928bd18b93ce9c1eb66"
+  end
+
   depends_on "rust" => :build
 
   def install
