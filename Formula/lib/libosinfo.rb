a06b958724013b5138afc628eba6ba4762566a9d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-10-27 22:31:58 +0000

    libosinfo: update 1.11.0 bottle.

--- Formula/lib/libosinfo.rb
+++ Formula/lib/libosinfo.rb
@@ -11,14 +11,11 @@ class Libosinfo < Formula
   end
 
   bottle do
-    sha256 arm64_ventura:  "b849e568f1debeed02519d9d5bf34072403397c1cc33998a238eb8daaf3eb09b"
-    sha256 arm64_monterey: "2659892b2d277e688c8edc0f03875c1e518f5ab515a200d58698f717e9ed7dac"
-    sha256 arm64_big_sur:  "e8f42ab6e678acb61213692e472cc233112067f87b9d10744c4db4a10e14729c"
-    sha256 ventura:        "8ce2df8bcd25e66c828f766dc7de3055e03b0c28878b4c54153acc89fa9fd2e2"
-    sha256 monterey:       "6fa3411b44dcdd8d33e73fa6d7b4e4d21828d0e00c52430c68684e9a0a2a45c6"
-    sha256 big_sur:        "494af85f0b66b208db81e2114e492c683028ebe8c1748c61bfaaaa5b8fc7892f"
-    sha256 catalina:       "ae3b5681f80d8a2fb8413b4d0e86ea64a4c6e48d809b9f96d2064ed1d7ecb570"
-    sha256 x86_64_linux:   "eb8f140219fb3208a0ee34a0e412a6a4d24581e622e2d81cd269c2b10a8312e3"
+    sha256 arm64_ventura:  "ecbf5376391dd7932c61d6112f86cbdb2f7bf3f7f457694157abea71cdf289f9"
+    sha256 arm64_monterey: "a7d126750cd6bc8fc8abb8433c3835da6ebfba0dbc0a8a6b19273648334f69c6"
+    sha256 ventura:        "6ddc2d382c032be8472373fa603c3b9b0d5238cc5e2219e736377c19d61df384"
+    sha256 monterey:       "556d1cddbdc4c90e958754ee315c8f2e4f3b63ee59b5d00015debbf8bfc783ed"
+    sha256 x86_64_linux:   "c17b5a93160795024c5be3de4e763140cf9aac0e14df7b396b5c480fbc83faf6"
   end
 
   depends_on "gobject-introspection" => :build
