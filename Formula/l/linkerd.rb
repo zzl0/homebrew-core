e8ee4699eea24e7ccb61ffa296d01c121af3cb0b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-10-27 02:24:46 +0000

    linkerd 2.14.2

--- Formula/l/linkerd.rb
+++ Formula/l/linkerd.rb
@@ -2,8 +2,8 @@ class Linkerd < Formula
   desc "Command-line utility to interact with linkerd"
   homepage "https://linkerd.io"
   url "https://github.com/linkerd/linkerd2.git",
-      tag:      "stable-2.14.1",
-      revision: "f496587bc5e9d9061dfcf82ad9e913ae3cd4372c"
+      tag:      "stable-2.14.2",
+      revision: "2f25cdeeb5a433d10bbce81d58f7f652ae96d44e"
   license "Apache-2.0"
 
   livecheck do
