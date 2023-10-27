3440d41360d920738150c5d80a2942744bbdf9ee
Patrick Linnane <patrick@linnane.io>
2023-10-27 21:35:55 +0000

    changie: add 1.15.0 bottle.

--- Formula/c/changie.rb
+++ Formula/c/changie.rb
@@ -6,6 +6,16 @@ class Changie < Formula
   license "MIT"
   head "https://github.com/miniscruff/changie.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "3b097d14d1c774348cc297f74a213d7192a826318f09710b796d4d0bb90067d5"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "956a64af9d02687c1b3721d0422c282513c59521fb3bb741b99ba934693035ac"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2dafad13a03afaa26e79f4be089181275e559a51348383011970454956297960"
+    sha256 cellar: :any_skip_relocation, sonoma:         "af31d0a73042cc2d3c6f3d0dbd1765dc7ec45ff45b6cef9abba0e9e7bd06dded"
+    sha256 cellar: :any_skip_relocation, ventura:        "13d633f3c026cc440cb04827b91c7dceaa0ff38641e8f73ac878df1bf71ce551"
+    sha256 cellar: :any_skip_relocation, monterey:       "bc4c22f2129b566dab2b8fbc58578b1558498f415037bd9c116f87749a7a31e4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "edbc5fa134657a0904fc2f34b70e4de4e1f1d0d9ad6aabb0528e1e86fa73e110"
+  end
+
   depends_on "go" => :build
 
   def install
