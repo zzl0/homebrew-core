f1f6809badc9ec5e604e95cdd16560b717bb81b1
Porkepix <porkepix@gmail.com>
2023-10-27 11:42:13 +0200

    ncnn 20231027

--- Formula/n/ncnn.rb
+++ Formula/n/ncnn.rb
@@ -1,10 +1,9 @@
 class Ncnn < Formula
   desc "High-performance neural network inference framework"
   homepage "https://github.com/Tencent/ncnn"
-  url "https://github.com/Tencent/ncnn/archive/refs/tags/20230816.tar.gz"
-  sha256 "6b14105b6aba1e5fc87321b161c1d996c507f9b671a961831c8cd9987e807aa1"
+  url "https://github.com/Tencent/ncnn/archive/refs/tags/20231027.tar.gz"
+  sha256 "8d85896ed095d09f05fff32fc85d75eea0b971796ce0f48a9874d93d3d347674"
   license "BSD-3-Clause"
-  revision 4
   head "https://github.com/Tencent/ncnn.git", branch: "master"
 
   bottle do
