bf3d1b066fcc399df8c34e2a800bc444eff91d63
Rui Chen <rui@chenrui.dev>
2023-10-27 22:12:57 +0000

    scilla: add 1.2.7 bottle.

--- Formula/s/scilla.rb
+++ Formula/s/scilla.rb
@@ -6,6 +6,16 @@ class Scilla < Formula
   license "GPL-3.0-or-later"
   head "https://github.com/edoardottt/scilla.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "69baec3497812a02e6696b0461bb9440ff58b94e12d5bcf31e99c622ef808527"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "039527494f3f17c66014aec9d96864c4b4a8b0b872e17ac7c0fac2adf178f4ce"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "eb5c3d6084a64a174792e865212d082d9b401a58468b49211558620458052dfc"
+    sha256 cellar: :any_skip_relocation, sonoma:         "ae371c2742878f593a9ae762bb8a71aa209992cbcd11fe6c5c622cfbab85bbcd"
+    sha256 cellar: :any_skip_relocation, ventura:        "37eb1ecba2eac939a47805c4ddc17fe0453e51b88d06a7904f1d3e09e0a4a473"
+    sha256 cellar: :any_skip_relocation, monterey:       "6c7c2ea4227c1c39157fccb3472c11a84b79946b77da99714134ca36ddb4d732"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "03a8f2960436b22aebd0642db667c9d60e11fd70cb8f294f3407de6ed5d98e2b"
+  end
+
   depends_on "go" => :build
 
   def install
