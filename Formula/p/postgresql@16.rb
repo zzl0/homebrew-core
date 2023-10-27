c8d15f01022c6b13fced217606c29e8b780e685e
Rui Chen <rui@chenrui.dev>
2023-10-26 20:27:10 -0400

    postgresql@16: revision bump (perl 5.38.0)
    ```
    brew linkage --test postgresql@15
      Missing libraries:
        unexpected (libperl.so)
    ```
    
    found in:
    - #152259
    
    relates to:
    - #145939
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/p/postgresql@16.rb
+++ Formula/p/postgresql@16.rb
@@ -4,6 +4,7 @@ class PostgresqlAT16 < Formula
   url "https://ftp.postgresql.org/pub/source/v16.0/postgresql-16.0.tar.bz2"
   sha256 "df9e823eb22330444e1d48e52cc65135a652a6fdb3ce325e3f08549339f51b99"
   license "PostgreSQL"
+  revision 1
 
   livecheck do
     url "https://ftp.postgresql.org/pub/source/"
