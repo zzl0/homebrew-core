10b180c2abb7a98bfd8b38b140fa4e976dd13cb0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-10-28 00:17:04 +0000

    solhint: update 4.0.0 bottle.

--- Formula/s/solhint.rb
+++ Formula/s/solhint.rb
@@ -8,7 +8,13 @@ class Solhint < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "6324c453a6c07d09909ce7a0ea302de752e6847de3cd25aafc62289f433df87c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "741495caac816a6784c3732b4308147015ebd39918a64f89a3d85b3348f3605a"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "741495caac816a6784c3732b4308147015ebd39918a64f89a3d85b3348f3605a"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "741495caac816a6784c3732b4308147015ebd39918a64f89a3d85b3348f3605a"
+    sha256 cellar: :any_skip_relocation, sonoma:         "965b360eb9ce92b902f79297809f05343d889d4c1601371645761e7491f1c88c"
+    sha256 cellar: :any_skip_relocation, ventura:        "965b360eb9ce92b902f79297809f05343d889d4c1601371645761e7491f1c88c"
+    sha256 cellar: :any_skip_relocation, monterey:       "965b360eb9ce92b902f79297809f05343d889d4c1601371645761e7491f1c88c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "741495caac816a6784c3732b4308147015ebd39918a64f89a3d85b3348f3605a"
   end
 
   depends_on "node"
