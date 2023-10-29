aca5d14c44e5f07fdeac14e6ceb2af218dfc3472
Patrick Linnane <patrick@linnane.io>
2023-10-29 19:23:40 +0000

    patch-package: add 8.0.0 bottle.

--- Formula/p/patch-package.rb
+++ Formula/p/patch-package.rb
@@ -7,6 +7,16 @@ class PatchPackage < Formula
   sha256 "4d2bd29c0d73a6eb8c43270125998bb7586d4f4128a2f1f7002e69edc5fed8e2"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "3755be156502d4299ffa798a6f4600e8c4397c3ae262a9bdd44eeb30f16732d9"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3755be156502d4299ffa798a6f4600e8c4397c3ae262a9bdd44eeb30f16732d9"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3755be156502d4299ffa798a6f4600e8c4397c3ae262a9bdd44eeb30f16732d9"
+    sha256 cellar: :any_skip_relocation, sonoma:         "aad33f8dbdd1315003bb356498affe9a92a3a75d83d6aa80ce8fdea95b88134d"
+    sha256 cellar: :any_skip_relocation, ventura:        "aad33f8dbdd1315003bb356498affe9a92a3a75d83d6aa80ce8fdea95b88134d"
+    sha256 cellar: :any_skip_relocation, monterey:       "aad33f8dbdd1315003bb356498affe9a92a3a75d83d6aa80ce8fdea95b88134d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3755be156502d4299ffa798a6f4600e8c4397c3ae262a9bdd44eeb30f16732d9"
+  end
+
   depends_on "node"
 
   def install
