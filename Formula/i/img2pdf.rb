815908c195634b4f437ff945454145c7b8bef89e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-10-30 15:58:02 +0000

    img2pdf: update 0.5.0 bottle.

--- Formula/i/img2pdf.rb
+++ Formula/i/img2pdf.rb
@@ -8,13 +8,13 @@ class Img2pdf < Formula
   license "LGPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "a93aa3091566600d462cdada6230f1f4e719936965b789dba7754606aabeb47a"
-    sha256 cellar: :any,                 arm64_ventura:  "01650a4d1aa5234b48eeeee4209e9e71aa6a0ed0debaa3cd706a294a95c30954"
-    sha256 cellar: :any,                 arm64_monterey: "ff3374f676856e052de7d1db31574dd21bb097d92dffc98e329f65fce265bee6"
-    sha256 cellar: :any,                 sonoma:         "c348745f00f56765220dd0eaa23997c1bf83845d6e6a8260b19dc14f1b3b4ced"
-    sha256 cellar: :any,                 ventura:        "008a2f535e03de53b70b65ec6b8c4d0575f3dc250bb72a422c2cdae2d61adbb6"
-    sha256 cellar: :any,                 monterey:       "81c4510ca8e11eb6c72f29dbc87a8f9d5b5702f33ca913cd6557c28aa4d2d928"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ed561971b932a1b94a74d0f96dae9ea7dd37567633568a9f85de9fc0ffac077e"
+    sha256 cellar: :any,                 arm64_sonoma:   "492b0d07a574f776f02af896bdaff257492d086ee6dfaddf9d7f07ff735b5716"
+    sha256 cellar: :any,                 arm64_ventura:  "309b4c9fd9da6b1bb858d01124e3961c59605580bd902278d5f4f0e6e6cd0c53"
+    sha256 cellar: :any,                 arm64_monterey: "f632abe8306b30d27742dcdc201985ef0a389b96bb70896aac697506a45d39ae"
+    sha256 cellar: :any,                 sonoma:         "36f941f12619c369374271d943d922ff46050f60d1307c5215e68e32b114401a"
+    sha256 cellar: :any,                 ventura:        "b42e9e40a5c2dd209bab9bf759b0eb007225e73f9abd8716448f4e863497155a"
+    sha256 cellar: :any,                 monterey:       "74c4047679ebcb7b918cb02a6938d19dc49b1cbda519294487173fe83acf6a83"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4e6e6fcc37339427d439ec1be62ca53f436cf8e391833e8597b131deb3257f47"
   end
 
   depends_on "pillow"
