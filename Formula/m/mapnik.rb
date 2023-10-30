63e3800c50066f5188f2cb9016f4c4d24c2700fd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-10-30 16:54:11 +0000

    mapnik: update 3.1.0_23 bottle.

--- Formula/m/mapnik.rb
+++ Formula/m/mapnik.rb
@@ -32,13 +32,13 @@ class Mapnik < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "f7d27be65cbe32c04839c5fdbad0e274a7b2fcbdb2fb779f57f31af28ed89889"
-    sha256 cellar: :any,                 arm64_ventura:  "95e60fb0d3e171ab84c4737acabb381192a9fca28d4ca9bc859e24b70f45c3c3"
-    sha256 cellar: :any,                 arm64_monterey: "1f59ebc32d6e8f5bec88b38bf4b66521f9f46fe1f5bc94dbf3a9e7d0189ee0de"
-    sha256 cellar: :any,                 sonoma:         "05a26cefa57e3c1677477da4b35320395491fa9b8f1347ade60196c267095a62"
-    sha256 cellar: :any,                 ventura:        "deaf0723bb99a1dfc70e7cb532d8c401143e01c73d62b1ff31dfdfde9ce739e2"
-    sha256 cellar: :any,                 monterey:       "45fc08f3c0d566bad96df494a31bacdbdb0d29fc6cd20a36fc7c4ebfe632bd35"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "874c70dc093dbfdcd2a6ab6cb0b02bba79b735d9ee4f2a09431873cff99e5d14"
+    sha256 cellar: :any,                 arm64_sonoma:   "a0a1b6360c0cece81b347399035adf72c37aaa8a0571429e187e3164a96a6666"
+    sha256 cellar: :any,                 arm64_ventura:  "b2019d903eff1a8bde945fcb9b088b095b2c5a5b4f091a96a85d7a0f9fa008c0"
+    sha256 cellar: :any,                 arm64_monterey: "cc89afb86d3575a249a597befceecdc09b2362b7b8e2151b2175fc7b7d17d6de"
+    sha256 cellar: :any,                 sonoma:         "68235c5ce9297306713dbb7c71066956de420a0229019b9764a73814cfd59805"
+    sha256 cellar: :any,                 ventura:        "6bf46176062aeba1a4ed2acedc146a4722710a9973730fd90bde2b37bf5aa59c"
+    sha256 cellar: :any,                 monterey:       "6f045a71e924bca05c295e7f3116005a1dba0ee5b62664bc4dc093ca125f4a6b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d1cc2990f23e96b662d3c342992368c295f464914c77af26668f1c13e461f428"
   end
 
   depends_on "pkg-config" => :build
