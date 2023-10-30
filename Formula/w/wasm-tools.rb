7bae59162fb2a68f59fe23c3845751a751503879
Rui Chen <rui@chenrui.dev>
2023-10-30 13:38:07 -0400

    wasm-tools 1.0.50

--- Formula/w/wasm-tools.rb
+++ Formula/w/wasm-tools.rb
@@ -1,8 +1,8 @@
 class WasmTools < Formula
   desc "Low level tooling for WebAssembly in Rust"
   homepage "https://github.com/bytecodealliance/wasm-tools"
-  url "https://github.com/bytecodealliance/wasm-tools/archive/refs/tags/wasm-tools-1.0.48.tar.gz"
-  sha256 "c842e9ab62866b8de1d9b513cd205854c261b885051650b35f26f9c8b8644599"
+  url "https://github.com/bytecodealliance/wasm-tools/archive/refs/tags/wasm-tools-1.0.50.tar.gz"
+  sha256 "4b840e3860c7e1bb80e9fe838ce752bb1d25dc9a5468b8e9125b2b042fc4b36d"
   license "Apache-2.0" => { with: "LLVM-exception" }
   head "https://github.com/bytecodealliance/wasm-tools.git", branch: "main"
 
