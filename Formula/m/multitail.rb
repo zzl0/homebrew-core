7a3b9d9feec1bc7b68fe62d03f86942f648dc1a5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-10-30 18:35:31 +0000

    multitail: update 7.1.1 bottle.

--- Formula/m/multitail.rb
+++ Formula/m/multitail.rb
@@ -7,16 +7,13 @@ class Multitail < Formula
   head "https://github.com/folkertvanheusden/multitail.git"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "500bb76266f0a1c130e6ae3c8021a67e8132d8c26f4812ae0dcbc601fb9e2fd6"
-    sha256 cellar: :any,                 arm64_ventura:  "a19b4eb52cc23c3a9f3190dbdca3db1d34032bd8790a357f022f1b408dbd0cff"
-    sha256 cellar: :any,                 arm64_monterey: "2d1abd3e7e31719e362d7a1f7e22375b60eb75403bab05562975dc8758b424d2"
-    sha256 cellar: :any,                 arm64_big_sur:  "746bcb020d1cac7511697bb8c0a0933a3e2948544aa3069c44c79bfe7179f031"
-    sha256 cellar: :any,                 sonoma:         "daa674ab915ff077d6729ff75e971d752576b6f8d14ab2d01cce1223e0ca9e61"
-    sha256 cellar: :any,                 ventura:        "0bd2424867668b48d47985d2679c6aa705a6d1ce8801ebad42d737828a3d11c7"
-    sha256 cellar: :any,                 monterey:       "a59560fd92bc0e68010cbe215edbcb6d31cc7f57a9acd43715f7adf93bd754f2"
-    sha256 cellar: :any,                 big_sur:        "4361b3a0326daff64e701bfdfcdd105d2a36003a87a9e4196ceff935beba9807"
-    sha256 cellar: :any,                 catalina:       "1506d3e77bb07b8c8e6726982ce235497ba2914f872b87587a5e21b0fa3bf627"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f9ea2a3e55053e2784688f277a659e1a977165e3616205f6706b3ec0ff8e4492"
+    sha256 cellar: :any,                 arm64_sonoma:   "a7abf3e9432f663dadd24eb0358949ed3a6fc9563a3fb75affa7fb4ad23d7225"
+    sha256 cellar: :any,                 arm64_ventura:  "36a558274f68601894f69bad2c76067c84dcd93c0bb0f8a8d7707bcc64184127"
+    sha256 cellar: :any,                 arm64_monterey: "764942343f5b7d5b85dad59fbdce1f205903e1a2e7e5cea56886bf37eae3f918"
+    sha256 cellar: :any,                 sonoma:         "91ac1e160fbb6a6e4ee590fdc1111eb2ec08e814aac509614191d6672f0a785a"
+    sha256 cellar: :any,                 ventura:        "2e4a9df2577a707edd7c68d4a69eb2405777d223481c1a440a775533014362c6"
+    sha256 cellar: :any,                 monterey:       "f5d069f18899048c02379a45f8abb47db111f77a70c6bc9b18dad171e8f38b4f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "029b1f39b69a347c51f0309e62304d69b5548f3e2983d6618f1b20a1fc61e1e5"
   end
 
   depends_on "pkg-config" => :build
