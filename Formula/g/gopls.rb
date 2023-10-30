af6672666991be65d3d08f36695e107179f71170
Patrick Linnane <patrick@linnane.io>
2023-10-30 10:15:35 -0700

    gopls 0.14.1

--- Formula/g/gopls.rb
+++ Formula/g/gopls.rb
@@ -1,8 +1,8 @@
 class Gopls < Formula
   desc "Language server for the Go language"
   homepage "https://github.com/golang/tools/tree/master/gopls"
-  url "https://github.com/golang/tools/archive/refs/tags/gopls/v0.14.0.tar.gz"
-  sha256 "a0705600556f09f52a4dc6ddca44a84b9ff626690e5ce8d49dc9c06ebc8425be"
+  url "https://github.com/golang/tools/archive/refs/tags/gopls/v0.14.1.tar.gz"
+  sha256 "33f886f65268c6b1e3e3a0177681c2aeaeba3993b2a0829d18efb49869c1ca0e"
   license "BSD-3-Clause"
 
   livecheck do
