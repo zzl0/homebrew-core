830e60a0ca192b07d2256f895c00def093d3ebbf
Patrick Linnane <patrick@linnane.io>
2023-10-30 20:31:38 -0700

    zug 0.1.1 (new formula)

---
+++ Formula/z/zug.rb
@@ -0,0 +1,35 @@
+class Zug < Formula
+  desc "C++ library providing transducers"
+  homepage "https://sinusoid.es/zug/"
+  url "https://github.com/arximboldi/zug/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "1b9c8f962e40baa6f0c6af35f957444850063d550078a3ebd0227727b8ef193c"
+  license "BSL-1.0"
+
+  depends_on "cmake" => :build
+
+  def install
+    args = %w[
+      -Dzug_BUILD_EXAMPLES=OFF
+      -Dzug_BUILD_TESTS=OFF
+    ]
+    system "cmake", "-S", ".", "-B", "_build", *args, *std_cmake_args.reject { |s| s["-DBUILD_TESTING=OFF"] }
+    system "cmake", "--build", "_build"
+    system "cmake", "--install", "_build"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <string>
+      #include <zug/transducer/filter.hpp>
+      #include <zug/transducer/map.hpp>
+      int main()
+      {
+      auto xf = zug::filter([](int x) { return x > 0; })
+          | zug::map([](int x) { return std::to_string(x); });
+      }
+    EOS
+
+    system ENV.cxx, "-std=c++17", "-I#{include}", "test.cpp", "-o", "test"
+    system "./test"
+  end
+end
