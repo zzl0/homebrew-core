c62672a78647c3c1b376fcf1b3a1111a0b215a7e
Rui Chen <rui@chenrui.dev>
2023-10-30 23:38:22 -0400

    dagger 0.9.2

--- Formula/d/dagger.rb
+++ Formula/d/dagger.rb
@@ -2,8 +2,8 @@ class Dagger < Formula
   desc "Portable devkit for CI/CD pipelines"
   homepage "https://dagger.io"
   url "https://github.com/dagger/dagger.git",
-      tag:      "v0.9.1",
-      revision: "ed98d775ad923c56f130bbf5237fda0399fde1b4"
+      tag:      "v0.9.2",
+      revision: "a7b02a33007a886ec93ebaa5ac7a4500911febab"
   license "Apache-2.0"
   head "https://github.com/dagger/dagger.git", branch: "main"
 
