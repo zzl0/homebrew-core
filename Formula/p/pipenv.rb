836e83af2561947f9cf9df46242f291e156e941d
David Baumgold <david@davidbaumgold.com>
2023-10-31 20:29:18 +0100

    pipenv: depend on virtualenv

--- Formula/p/pipenv.rb
+++ Formula/p/pipenv.rb
@@ -19,44 +19,18 @@ class Pipenv < Formula
 
   depends_on "python-certifi"
   depends_on "python@3.12"
-
-  resource "distlib" do
-    url "https://files.pythonhosted.org/packages/29/34/63be59bdf57b3a8a8dcc252ef45c40f3c018777dc8843d45dd9b869868f0/distlib-0.3.7.tar.gz"
-    sha256 "9dafe54b34a028eafd95039d5e5d4851a13734540f1331060d31c9916e7147a8"
-  end
-
-  resource "filelock" do
-    url "https://files.pythonhosted.org/packages/d5/71/bb1326535231229dd69a9dd2e338f6f54b2d57bd88fc4a52285c0ab8a5f6/filelock-3.12.4.tar.gz"
-    sha256 "2e6f249f1f3654291606e046b09f1fd5eac39b360664c27f5aad072012f8bcbd"
-  end
-
-  resource "platformdirs" do
-    url "https://files.pythonhosted.org/packages/d3/e3/aa14d6b2c379fbb005993514988d956f1b9fdccd9cbe78ec0dbe5fb79bf5/platformdirs-3.11.0.tar.gz"
-    sha256 "cf8ee52a3afdb965072dcc652433e0c7e3e40cf5ea1477cd4b3b1d2eb75495b3"
-  end
-
-  resource "virtualenv" do
-    url "https://files.pythonhosted.org/packages/8d/e9/f4550b3af1b5c71d42913430d325ca270ace65896bfd8ba04472566709cc/virtualenv-20.24.6.tar.gz"
-    sha256 "02ece4f56fbf939dbbc33c0715159951d6bf14aaf5457b092e4548e1382455af"
-  end
+  depends_on "virtualenv"
 
   def python3
     "python3.12"
   end
 
   def install
-    # Using the virtualenv DSL here because the alternative of using
-    # write_env_script to set a PYTHONPATH breaks things.
-    # https://github.com/Homebrew/homebrew-core/pull/19060#issuecomment-338397417
-    venv = virtualenv_create(libexec, python3)
-    venv.pip_install resources
-    venv.pip_install buildpath
+    virtualenv_install_with_resources
 
-    # `pipenv` needs to be able to find `virtualenv` on PATH. So we
-    # install symlinks for those scripts in `#{libexec}/tools` and create a
-    # wrapper script for `pipenv` which adds `#{libexec}/tools` to PATH.
-    (libexec/"tools").install_symlink libexec/"bin/pip", libexec/"bin/virtualenv"
-    (bin/"pipenv").write_env_script libexec/"bin/pipenv", PATH: "#{libexec}/tools:${PATH}"
+    site_packages = Language::Python.site_packages("python3.12")
+    paths = %w[virtualenv].map { |p| Formula[p].opt_libexec/site_packages }
+    (libexec/site_packages/"homebrew-deps.pth").write paths.join("\n")
 
     generate_completions_from_executable(libexec/"bin/pipenv", shells:                 [:fish, :zsh],
                                                                shell_parameter_format: :click)
