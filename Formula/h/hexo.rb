072dbd2fdf6d90dc88e3027491c22d0243bc22b7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-10-31 17:06:54 +0000

    hexo: update 7.0.0 bottle.

--- Formula/h/hexo.rb
+++ Formula/h/hexo.rb
@@ -9,16 +9,13 @@ class Hexo < Formula
   head "https://github.com/hexojs/hexo.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "146e6a8483c1e76f0dc6dbbb2f1a7c94c5c0e02c0d0645f807013496da464482"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "6910c3128a0ffdcb7239a1fe6aeb9344e6b6ea8d9a8500291fa5423179ae45f7"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e8c8eb3ef1a60934230d70b1195c6bf1d708be8ae1fb7f1922f31d3e8796cf35"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e8c8eb3ef1a60934230d70b1195c6bf1d708be8ae1fb7f1922f31d3e8796cf35"
-    sha256 cellar: :any_skip_relocation, sonoma:         "e50d1a2d5cba61d74d7c7b15712a57fdbeddcebb0abe54d86ecd7e35be677fa8"
-    sha256 cellar: :any_skip_relocation, ventura:        "775cdefaaf9057243806ddd222912d35f964340d7f436d3f6437af5dfabf06b5"
-    sha256 cellar: :any_skip_relocation, monterey:       "a5daf9b48cf25528279debdd2b1ad37baaa6d08cfd6e18e212a60a68dc3f35ef"
-    sha256 cellar: :any_skip_relocation, big_sur:        "a5daf9b48cf25528279debdd2b1ad37baaa6d08cfd6e18e212a60a68dc3f35ef"
-    sha256 cellar: :any_skip_relocation, catalina:       "a5daf9b48cf25528279debdd2b1ad37baaa6d08cfd6e18e212a60a68dc3f35ef"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "95e726055fa242838fa081dce26d2ff0441d1aeef2ddd6b5065b04961441d625"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "e3632ba768033d12bb7b368335872e39bed5e640f6857b0d992bdcb2597316d0"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "e3632ba768033d12bb7b368335872e39bed5e640f6857b0d992bdcb2597316d0"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e3632ba768033d12bb7b368335872e39bed5e640f6857b0d992bdcb2597316d0"
+    sha256 cellar: :any_skip_relocation, sonoma:         "78d7610fd35dc53ff1bfe7aaf14a6ebe7b013b6974a5c2d0774f8b8d15bfb5de"
+    sha256 cellar: :any_skip_relocation, ventura:        "78d7610fd35dc53ff1bfe7aaf14a6ebe7b013b6974a5c2d0774f8b8d15bfb5de"
+    sha256 cellar: :any_skip_relocation, monterey:       "78d7610fd35dc53ff1bfe7aaf14a6ebe7b013b6974a5c2d0774f8b8d15bfb5de"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5672c9ea5b39b77a6830c741f791983eab68b74b797958d5cc977b0b2977b72a"
   end
 
   depends_on "node"
