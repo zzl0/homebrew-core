862006dc1d8d2b6078533dead3727891c4f1a5ca
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-10-31 13:48:34 +0000

    mx: update 6.53.2 bottle.

--- Formula/m/mx.rb
+++ Formula/m/mx.rb
@@ -11,7 +11,7 @@ class Mx < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "0141066d6639e01746b47ab837fb1b5292935b33c6c0741db595cb1ceb4baa55"
+    sha256 cellar: :any_skip_relocation, all: "3e847c2a30d4aff727952cd8a5acd3f01e425f4f2399a21839a1bd52c6f1632b"
   end
 
   depends_on "openjdk" => :test
