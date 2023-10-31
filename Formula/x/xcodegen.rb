0429f682d8f0e312b1436ca7e249f8c7033870e9
Porkepix <porkepix@gmail.com>
2023-10-31 13:15:07 +0100

    xcodegen 2.38.0

--- Formula/x/xcodegen.rb
+++ Formula/x/xcodegen.rb
@@ -1,8 +1,8 @@
 class Xcodegen < Formula
   desc "Generate your Xcode project from a spec file and your folder structure"
   homepage "https://github.com/yonaskolb/XcodeGen"
-  url "https://github.com/yonaskolb/XcodeGen/archive/refs/tags/2.37.0.tar.gz"
-  sha256 "3eb78c0bbaa3342f2bcd3781571c70937e8c51b962c5aebeea1390f56fabd3c8"
+  url "https://github.com/yonaskolb/XcodeGen/archive/refs/tags/2.38.0.tar.gz"
+  sha256 "8dc9757feb532c7b2ce71184048776a8ebab8ef07677779f2a5c537a80abf30e"
   license "MIT"
   head "https://github.com/yonaskolb/XcodeGen.git", branch: "master"
 
