aca3dcb734fbd07ea95bbceaa37ab1423ae44db9
Rui Chen <rui@chenrui.dev>
2023-10-30 21:04:26 -0400

    pmix 4.2.7 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    pmix: update head
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/p/pmix.rb
@@ -0,0 +1,65 @@
+class Pmix < Formula
+  desc "Process Management Interface for HPC environments"
+  homepage "https://openpmix.github.io/"
+  url "https://github.com/openpmix/openpmix/releases/download/v4.2.7/pmix-4.2.7.tar.bz2"
+  sha256 "ac9cf58a0bf01bfacd51d342100234f04c740ec14257e4492d1dd0207ff2a917"
+  license "BSD-3-Clause"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  head do
+    url "https://github.com/openpmix/openpmix.git", branch: "master"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "hwloc"
+  depends_on "libevent"
+
+  uses_from_macos "python" => :build
+  uses_from_macos "zlib"
+
+  def install
+    # Avoid references to the Homebrew shims directory
+    cc = OS.linux? ? "gcc" : ENV.cc
+    inreplace "src/tools/pmix_info/support.c", "PMIX_CC_ABSOLUTE", "\"#{cc}\""
+
+    args = %W[
+      --disable-silent-rules
+      --enable-ipv6
+      --sysconfdir=#{etc}
+      --with-hwloc=#{Formula["hwloc"].opt_prefix}
+      --with-libevent=#{Formula["libevent"].opt_prefix}
+      --with-sge
+    ]
+
+    system "./autogen.pl", "--force" if build.head?
+    system "./configure", *args, *std_configure_args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <stdio.h>
+      #include <pmix.h>
+
+      int main(int argc, char **argv) {
+        pmix_value_t *val;
+        pmix_proc_t myproc;
+        pmix_status_t rc;
+
+        return 0;
+      }
+    EOS
+
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-lpmix", "-o", "test"
+    system "./test"
+
+    assert_match "PMIX: #{version}", shell_output("#{bin}/pmix_info --pretty-print")
+  end
+end
