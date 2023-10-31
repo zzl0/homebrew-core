86a7b768251b3af0a7a2b0575e5c8f874ab5e919
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-10-31 19:58:07 +0800

    strace 6.6

--- Formula/s/strace.rb
+++ Formula/s/strace.rb
@@ -1,8 +1,8 @@
 class Strace < Formula
   desc "Diagnostic, instructional, and debugging tool for the Linux kernel"
   homepage "https://strace.io/"
-  url "https://github.com/strace/strace/releases/download/v6.5/strace-6.5.tar.xz"
-  sha256 "dfb051702389e1979a151892b5901afc9e93bbc1c70d84c906ade3224ca91980"
+  url "https://github.com/strace/strace/releases/download/v6.6/strace-6.6.tar.xz"
+  sha256 "421b4186c06b705163e64dc85f271ebdcf67660af8667283147d5e859fc8a96c"
   license "LGPL-2.1-or-later"
 
   bottle do
