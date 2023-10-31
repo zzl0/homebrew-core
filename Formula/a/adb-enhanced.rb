67bd807139c1d9548583ca1d6d8274a719383443
David Baumgold <david@davidbaumgold.com>
2023-10-31 16:08:47 +0100

    adb-enhanced: depend on python-psutil

--- Formula/a/adb-enhanced.rb
+++ Formula/a/adb-enhanced.rb
@@ -18,16 +18,12 @@ class AdbEnhanced < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "941de410df686f9a6621583202e012e5ce0f28de0fa4999192887acc67ab6ef1"
   end
 
+  depends_on "python-psutil"
   depends_on "python@3.12"
 
   resource "docopt" do
     url "https://files.pythonhosted.org/packages/a2/55/8f8cab2afd404cf578136ef2cc5dfb50baa1761b68c9da1fb1e4eed343c9/docopt-0.6.2.tar.gz"
     sha256 "49b3a825280bd66b3aa83585ef59c4a8c82f2c8a522dbe754a8bc8d08c85c491"
-  end
-
-  resource "psutil" do
-    url "https://files.pythonhosted.org/packages/d6/0f/96b7309212a926c1448366e9ce69b081ea79d63265bde33f11cc9cfc2c07/psutil-5.9.5.tar.gz"
-    sha256 "5410638e4df39c54d957fc51ce03048acd8e6d60abc0f5107af51e5fb566eb3c"
   end
 
   def install
