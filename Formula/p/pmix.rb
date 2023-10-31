d61b906cba768edc21afe82da953709ed557ae4d
Rui Chen <rui@chenrui.dev>
2023-10-31 14:04:59 +0000

    pmix: add 4.2.7 bottle.

--- Formula/p/pmix.rb
+++ Formula/p/pmix.rb
@@ -10,6 +10,16 @@ class Pmix < Formula
     strategy :github_latest
   end
 
+  bottle do
+    sha256 arm64_sonoma:   "d4df14c911de4cef20c67dc2603a0c7b73363fb90ed79a0b166c6c79040385a9"
+    sha256 arm64_ventura:  "311200ed5cb03bae0bb24e81ee674539c2540d676a006f6480b179c1ee400fb6"
+    sha256 arm64_monterey: "cebacf4ff0a50b22567ab80f364cd34ec06ebc07790d87cd80c15e0fdddb990c"
+    sha256 sonoma:         "1ffa3e4a7c16159e0129abe31df105664c202e4c8e5a6599a2dd05cfc71075c2"
+    sha256 ventura:        "a051514a49e0c020960693679387ae228595998cf9c46cd06752c469506fc0b3"
+    sha256 monterey:       "dfeda5fd2b6f96917073e347c91804eac41fad3d1182ed1b7257480ca3253e87"
+    sha256 x86_64_linux:   "903e382c3cbee1841c503428513841deaed7cdc2bc22d149487b063121f22f4b"
+  end
+
   head do
     url "https://github.com/openpmix/openpmix.git", branch: "master"
 
