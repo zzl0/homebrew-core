7e41465868e8a09352f383ba25c01934ceef0550
Rui Chen <rui@chenrui.dev>
2023-11-01 10:55:26 -0400

    spack 0.20.3

--- Formula/s/spack.rb
+++ Formula/s/spack.rb
@@ -1,8 +1,8 @@
 class Spack < Formula
   desc "Package manager that builds multiple versions and configurations of software"
   homepage "https://spack.io"
-  url "https://github.com/spack/spack/archive/refs/tags/v0.20.2.tar.gz"
-  sha256 "62f87ab6ca332118f2812a255edcf4be4977623d067b9396251ce8c44b158e49"
+  url "https://github.com/spack/spack/archive/refs/tags/v0.20.3.tar.gz"
+  sha256 "c7deaa2f51502ff6f84f79845d8bd23202b0524f7669d3f779bd2049bc43e177"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/spack/spack.git", branch: "develop"
 
