af598fa73588afa2fb98465ac84f0a6babe01a20
Patrick Linnane <patrick@linnane.io>
2023-10-31 21:15:29 -0700

    verilator 5.018

--- Formula/v/verilator.rb
+++ Formula/v/verilator.rb
@@ -1,8 +1,8 @@
 class Verilator < Formula
   desc "Verilog simulator"
   homepage "https://www.veripool.org/wiki/verilator"
-  url "https://github.com/verilator/verilator/archive/refs/tags/v5.016.tar.gz"
-  sha256 "66fc36f65033e5ec904481dd3d0df56500e90c0bfca23b2ae21b4a8d39e05ef1"
+  url "https://github.com/verilator/verilator/archive/refs/tags/v5.018.tar.gz"
+  sha256 "8b544273eedee379e3c1a3bb849e14c754c9b5035d61ad03acdf3963092ba6c0"
   license any_of: ["LGPL-3.0-only", "Artistic-2.0"]
   head "https://github.com/verilator/verilator.git", branch: "master"
 
