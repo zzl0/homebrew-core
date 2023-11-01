181b8897093022347902b3b8e4b56ea8d501ee37
Patrick Linnane <patrick@linnane.io>
2023-11-01 14:00:14 -0700

    typos-cli 1.16.22

--- Formula/t/typos-cli.rb
+++ Formula/t/typos-cli.rb
@@ -1,8 +1,8 @@
 class TyposCli < Formula
   desc "Source code spell checker"
   homepage "https://github.com/crate-ci/typos"
-  url "https://github.com/crate-ci/typos/archive/refs/tags/v1.16.21.tar.gz"
-  sha256 "3cb31aef6660c1ff58b0f8a37b437894fd505f65ed13562d1d956351e7ea6ed3"
+  url "https://github.com/crate-ci/typos/archive/refs/tags/v1.16.22.tar.gz"
+  sha256 "683e711ab7fdf3c888fe8c812bd22923c63d13033e009a2432ec5763baa65ac3"
   license any_of: ["Apache-2.0", "MIT"]
 
   livecheck do
