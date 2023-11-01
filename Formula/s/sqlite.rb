7eedeb4b7105df69dca115c46d5ca018a2c376d9
ik-fib <ik11235@gmail.com>
2023-11-01 20:59:36 +0900

    sqlite 3.44.0

--- Formula/s/sqlite.rb
+++ Formula/s/sqlite.rb
@@ -1,9 +1,9 @@
 class Sqlite < Formula
   desc "Command-line interface for SQLite"
   homepage "https://sqlite.org/index.html"
-  url "https://www.sqlite.org/2023/sqlite-autoconf-3430200.tar.gz"
-  version "3.43.2"
-  sha256 "6d422b6f62c4de2ca80d61860e3a3fb693554d2f75bb1aaca743ccc4d6f609f0"
+  url "https://www.sqlite.org/2023/sqlite-autoconf-3440000.tar.gz"
+  version "3.44.0"
+  sha256 "b9cd386e7cd22af6e0d2a0f06d0404951e1bef109e42ea06cc0450e10cd15550"
   license "blessing"
 
   livecheck do
