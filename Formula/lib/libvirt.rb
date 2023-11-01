5b898ea0d775206e0e4b0eab596383592d1838bb
Rui Chen <rui@chenrui.dev>
2023-11-01 10:02:56 -0400

    libvirt 9.9.0

--- Formula/lib/libvirt.rb
+++ Formula/lib/libvirt.rb
@@ -1,8 +1,8 @@
 class Libvirt < Formula
   desc "C virtualization API"
   homepage "https://libvirt.org/"
-  url "https://download.libvirt.org/libvirt-9.8.0.tar.xz"
-  sha256 "7aa90d133bb301e94663a45c36176f46f4a9fc1b34d77d2e22b7a2517106f506"
+  url "https://download.libvirt.org/libvirt-9.9.0.tar.xz"
+  sha256 "cb83be795548161af6718a6db75ee40719a87bcd6f9207a3565db2fc0e53a52e"
   license all_of: ["LGPL-2.1-or-later", "GPL-2.0-or-later"]
   head "https://gitlab.com/libvirt/libvirt.git", branch: "master"
 
