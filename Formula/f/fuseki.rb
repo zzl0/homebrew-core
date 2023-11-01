1aea75632682d94190070b4871393fc0d8239477
Porkepix <porkepix@gmail.com>
2023-11-01 22:11:22 +0100

    fuseki 4.10.0

--- Formula/f/fuseki.rb
+++ Formula/f/fuseki.rb
@@ -1,9 +1,9 @@
 class Fuseki < Formula
   desc "SPARQL server"
   homepage "https://jena.apache.org/documentation/fuseki2/"
-  url "https://www.apache.org/dyn/closer.lua?path=jena/binaries/apache-jena-fuseki-4.9.0.tar.gz"
-  mirror "https://archive.apache.org/dist/jena/binaries/apache-jena-fuseki-4.9.0.tar.gz"
-  sha256 "b76e50d256fe79c475d9c0c3d69e9e667ccbc56d246693ce146be8e582bb0252"
+  url "https://www.apache.org/dyn/closer.lua?path=jena/binaries/apache-jena-fuseki-4.10.0.tar.gz"
+  mirror "https://archive.apache.org/dist/jena/binaries/apache-jena-fuseki-4.10.0.tar.gz"
+  sha256 "55c859df4f1b0224c325b8144a68161566a3494ff267d9f6a35a2882b7c51132"
   license "Apache-2.0"
 
   bottle do
