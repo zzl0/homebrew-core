f8eb5fd66aaf04ad2493a27f390ddebb57f09aad
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-11-01 10:26:23 +0000

    angular-cli: update 16.2.9 bottle.

--- Formula/a/angular-cli.rb
+++ Formula/a/angular-cli.rb
@@ -8,13 +8,13 @@ class AngularCli < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "a7998e28369e75bda55edbf9e9f3a07764a1f0672ed0818d3e881d6110be697e"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a7998e28369e75bda55edbf9e9f3a07764a1f0672ed0818d3e881d6110be697e"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a7998e28369e75bda55edbf9e9f3a07764a1f0672ed0818d3e881d6110be697e"
-    sha256 cellar: :any_skip_relocation, sonoma:         "fdb1be51738d949189193af50f83f940343dcb6825d7975c463c73cc2f7d0803"
-    sha256 cellar: :any_skip_relocation, ventura:        "fdb1be51738d949189193af50f83f940343dcb6825d7975c463c73cc2f7d0803"
-    sha256 cellar: :any_skip_relocation, monterey:       "fdb1be51738d949189193af50f83f940343dcb6825d7975c463c73cc2f7d0803"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a7998e28369e75bda55edbf9e9f3a07764a1f0672ed0818d3e881d6110be697e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "0e76eec5b224d4ddac2bb9660d8cf9ea86c7755486ea1665188a258bd924e4a3"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0e76eec5b224d4ddac2bb9660d8cf9ea86c7755486ea1665188a258bd924e4a3"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "0e76eec5b224d4ddac2bb9660d8cf9ea86c7755486ea1665188a258bd924e4a3"
+    sha256 cellar: :any_skip_relocation, sonoma:         "4b0c21a6a99480a9fd889ae00fe8784003647f98fbafdcda62dd346b505fcdc0"
+    sha256 cellar: :any_skip_relocation, ventura:        "4b0c21a6a99480a9fd889ae00fe8784003647f98fbafdcda62dd346b505fcdc0"
+    sha256 cellar: :any_skip_relocation, monterey:       "4b0c21a6a99480a9fd889ae00fe8784003647f98fbafdcda62dd346b505fcdc0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0e76eec5b224d4ddac2bb9660d8cf9ea86c7755486ea1665188a258bd924e4a3"
   end
 
   depends_on "node"
