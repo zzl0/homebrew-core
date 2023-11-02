4a4e657a2b06c408236cc6463d7edb11fd0cde1c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-11-02 12:36:11 +0000

    kube-linter 0.6.5

--- Formula/k/kube-linter.rb
+++ Formula/k/kube-linter.rb
@@ -1,8 +1,8 @@
 class KubeLinter < Formula
   desc "Static analysis tool for Kubernetes YAML files and Helm charts"
   homepage "https://github.com/stackrox/kube-linter"
-  url "https://github.com/stackrox/kube-linter/archive/refs/tags/v0.6.4.tar.gz"
-  sha256 "d83bb858531afce887416f6f0da0132a38b06c28b5da4b989363061a9ad65ca8"
+  url "https://github.com/stackrox/kube-linter/archive/refs/tags/v0.6.5.tar.gz"
+  sha256 "494aa84a57cfab3decdb093a78a78daa62497d49047f8f97be584313aea10c44"
   license "Apache-2.0"
   head "https://github.com/stackrox/kube-linter.git", branch: "master"
 
