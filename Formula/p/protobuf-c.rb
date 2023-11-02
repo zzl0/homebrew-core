5f281aaec88fefaca830a2cf8f3ec9a823ce667d
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-11-02 11:08:41 +0800

    protobuf-c: revision bump (protobuf 25.0)

--- Formula/p/protobuf-c.rb
+++ Formula/p/protobuf-c.rb
@@ -4,7 +4,7 @@ class ProtobufC < Formula
   url "https://github.com/protobuf-c/protobuf-c/releases/download/v1.4.1/protobuf-c-1.4.1.tar.gz"
   sha256 "4cc4facd508172f3e0a4d3a8736225d472418aee35b4ad053384b137b220339f"
   license "BSD-2-Clause"
-  revision 8
+  revision 9
 
   bottle do
     sha256 cellar: :any,                 arm64_sonoma:   "d4e2f1dc4429a893df2cfc2da5a658d13376c6b83f96ae2c20207a9438d2a4d0"
