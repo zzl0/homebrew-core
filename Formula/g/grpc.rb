65d5bc5d4e55f52935080eb36ccc33183295a168
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-11-02 11:08:40 +0800

    grpc: revision bump (protobuf 25.0)

--- Formula/g/grpc.rb
+++ Formula/g/grpc.rb
@@ -5,6 +5,7 @@ class Grpc < Formula
       tag:      "v1.59.2",
       revision: "883e5f76976b86afee87415dc67bde58d9b295a4"
   license "Apache-2.0"
+  revision 1
   head "https://github.com/grpc/grpc.git", branch: "master"
 
   # There can be a notable gap between when a version is tagged and a
