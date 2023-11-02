a3df66d18fbf8e472cd01a99097d3159aa99431b
Patrick Linnane <patrick@linnane.io>
2023-11-02 21:30:03 +0000

    gittuf: add 0.1.0 bottle.

--- Formula/g/gittuf.rb
+++ Formula/g/gittuf.rb
@@ -6,6 +6,16 @@ class Gittuf < Formula
   license "Apache-2.0"
   head "https://github.com/gittuf/gittuf.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "e14f5d3738fe2dbbafa5f15f3ac25296a176186913b429e23d8b2d3dfe070cd7"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "239404d9047a810c5a9fc66840bc23e362b7a0c3c0afcf4a423b447b51a7a8f7"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b6beeb52d2f1b53566384ab7718cf33c979dc934eed7741f18a9bf0a97604d8b"
+    sha256 cellar: :any_skip_relocation, sonoma:         "519ad6c10cd8ca65ce0fe3b90f77ff6b2780a52c4f0f9295dba7e1d3e47209c9"
+    sha256 cellar: :any_skip_relocation, ventura:        "a32c7d1aff08c0a7da5ac21cbe6d9c57cc8102f2b6b44417f102e844bc57aae9"
+    sha256 cellar: :any_skip_relocation, monterey:       "a3962c0eee0ba15235fe03d3feeb7208d54f49030372c543da384a95ca740c70"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bb7aa1476b2b87678eb409b8a44f563a8a3b3ccbfa6fc13ef775e1956fe28027"
+  end
+
   depends_on "go" => :build
 
   def install
