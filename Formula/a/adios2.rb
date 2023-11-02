2c78e6aa9dec2820aa44d1af70decde429b885a5
Rui Chen <rui@chenrui.dev>
2023-11-01 22:20:05 -0400

    adios2 2.9.2

--- Formula/a/adios2.rb
+++ Formula/a/adios2.rb
@@ -1,10 +1,9 @@
 class Adios2 < Formula
   desc "Next generation of ADIOS developed in the Exascale Computing Program"
   homepage "https://adios2.readthedocs.io"
-  url "https://github.com/ornladios/ADIOS2/archive/refs/tags/v2.9.1.tar.gz"
-  sha256 "ddfa32c14494250ee8a48ef1c97a1bf6442c15484bbbd4669228a0f90242f4f9"
+  url "https://github.com/ornladios/ADIOS2/archive/refs/tags/v2.9.2.tar.gz"
+  sha256 "78309297c82a95ee38ed3224c98b93d330128c753a43893f63bbe969320e4979"
   license "Apache-2.0"
-  revision 1
   head "https://github.com/ornladios/ADIOS2.git", branch: "master"
 
   livecheck do
