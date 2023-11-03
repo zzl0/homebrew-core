1385ef2fcb2d9b5b4d6a7117575207d61475ed1c
Rui Chen <rui@chenrui.dev>
2023-11-03 09:57:48 -0400

    bk 0.6.0

--- Formula/b/bk.rb
+++ Formula/b/bk.rb
@@ -1,8 +1,8 @@
 class Bk < Formula
   desc "Terminal EPUB Reader"
   homepage "https://github.com/aeosynth/bk"
-  url "https://github.com/aeosynth/bk/archive/refs/tags/v0.5.3.tar.gz"
-  sha256 "c9c54fa2cd60f3ca0576cab8bdd95b74da4d80c109eb91b5426e7ee0575b54f1"
+  url "https://github.com/aeosynth/bk/archive/refs/tags/v0.6.0.tar.gz"
+  sha256 "c720c8e81e86709f8543ca1a97a3c30b3bb33d55692a536cefed0ad2e3dfabcd"
   license "MIT"
   head "https://github.com/aeosynth/bk.git", branch: "master"
 
