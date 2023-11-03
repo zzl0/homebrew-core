c571b05145fc8609a650b4dbe083047a08260de8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-11-03 18:27:09 +0000

    devspace 6.3.4

--- Formula/d/devspace.rb
+++ Formula/d/devspace.rb
@@ -2,8 +2,8 @@ class Devspace < Formula
   desc "CLI helps develop/deploy/debug apps with Docker and k8s"
   homepage "https://devspace.sh/"
   url "https://github.com/loft-sh/devspace.git",
-      tag:      "v6.3.3",
-      revision: "41c8128c61e4a8432d802638c5480927091eaf25"
+      tag:      "v6.3.4",
+      revision: "419d97eaa442b843f7be4260ad6a0db188759c6d"
   license "Apache-2.0"
   head "https://github.com/loft-sh/devspace.git", branch: "master"
 
