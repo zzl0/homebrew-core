6c5509c3fd365aeaf2fc605d32c0dcafdc10b030
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-11-03 03:50:31 +0000

    wolfmqtt: update 1.17.0 bottle.

--- Formula/w/wolfmqtt.rb
+++ Formula/w/wolfmqtt.rb
@@ -7,13 +7,13 @@ class Wolfmqtt < Formula
   head "https://github.com/wolfSSL/wolfMQTT.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "cc547f6e172dce904661b25d848f111fbc7099894c2afa02d8291f283feafbd9"
-    sha256 cellar: :any,                 arm64_ventura:  "5c734f483db1b13af183e10d76996796a215efe9f2b32d990724fa4bb724e871"
-    sha256 cellar: :any,                 arm64_monterey: "507d8378c3cda9ea98d9bf8e524ffec7afcb8286e309849f978d1198420acf89"
-    sha256 cellar: :any,                 sonoma:         "23ab5c422b1ec659fd2fd2bdf630f6db5ad375b955820836e8759d64cde2c1f3"
-    sha256 cellar: :any,                 ventura:        "cf4321672ede78c5499b33d6ff6bff3a29c80aa45db13cac3a2ac2a77d736bfc"
-    sha256 cellar: :any,                 monterey:       "eeff914da5569d7bd4d9ee992b2ee325db6e75accfaebe5241edbdf89b120a34"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4967be8f8baf0e5ccf3c67f5717e685c78bb6955a7a9d6346bb5741e9f5e0849"
+    sha256 cellar: :any,                 arm64_sonoma:   "11880982fdb3c6a93b180402af8bafb598b9420ff10fc436d411d0a89d51665e"
+    sha256 cellar: :any,                 arm64_ventura:  "fc4eb03177a6ec79935663be31677d1ed7bbd2bf7029a1123263e3b86ed5f4ef"
+    sha256 cellar: :any,                 arm64_monterey: "2000bdb0b8bb05b7ee96daa25470a256dede98bc0194f29decf4e0da703fd7fa"
+    sha256 cellar: :any,                 sonoma:         "39a87d96ad70c58942289dc83f7174ac6f0dcc3d8fb8bd8cbf1a712f2d0fb716"
+    sha256 cellar: :any,                 ventura:        "5e7fb58798a4544cbf5fb59ad3bd3282d4d3a4286b6fbce0cd9cba98872403d0"
+    sha256 cellar: :any,                 monterey:       "a2d259fc4653a1e85d904d5025b1a864f9be6f7913142fe891f47e662d119ac3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "68f5643e873c54d9b3c8e47049c931e074466f75a5f83e298340b4549b8e4e8d"
   end
 
   depends_on "autoconf" => :build
