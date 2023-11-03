e85e13a1a5290cadd50087516fa494e5e2954d79
Rui Chen <rui@chenrui.dev>
2023-11-03 04:38:45 +0000

    tlrc: add 1.7.1 bottle.

--- Formula/t/tlrc.rb
+++ Formula/t/tlrc.rb
@@ -6,6 +6,16 @@ class Tlrc < Formula
   license "MIT"
   head "https://github.com/tldr-pages/tlrc.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "d361cc0f22b467a76ec29c888110c866b0895aa073924c9f459700be421fc80b"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "b1b925d808e9183d3283f5249c8e9218959b0b0a5bcad7e98499a9784862f056"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "10794ed1130581b4e00dfc7dcfcc4c613eb8201873f7f22e55b945ed7b60005e"
+    sha256 cellar: :any_skip_relocation, sonoma:         "8fe4bf3f0f965ba59619fdc364e7d588569a34a001f75cbdca7a14c550c0913d"
+    sha256 cellar: :any_skip_relocation, ventura:        "69cef06e13edcf0aa8c5a085367471974e0381b99bd085f8628628cbde9ebc0e"
+    sha256 cellar: :any_skip_relocation, monterey:       "fdac5ff55164f9f9c9d5389ca08ce046031d015c1361ce6395091eace27ba3c2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d72f78bf8c9d6a6fbcf22a37e2621d3b60e3d25fe548b49213458f00006bbdb7"
+  end
+
   depends_on "rust" => :build
 
   def install
