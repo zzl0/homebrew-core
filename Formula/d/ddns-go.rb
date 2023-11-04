6f66e08a66fcf097974bb71ea1b4a7664612c5cf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-11-04 02:52:45 +0000

    ddns-go: update 5.6.5 bottle.

--- Formula/d/ddns-go.rb
+++ Formula/d/ddns-go.rb
@@ -7,13 +7,13 @@ class DdnsGo < Formula
   head "https://github.com/jeessy2/ddns-go.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "9d4f546507073a56efe6b25c40d785f44bedf51d52ccc3bfe652eff65f8870a0"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3c0dcac2d84ab4a5ef7d41938b340d8668e4aa4c52cfd74fdd6a2323247f6dcc"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "4a1eae91ed1265b5ce7fa9756d646dfd89d3b673d985085abfdd3bc78682f761"
-    sha256 cellar: :any_skip_relocation, sonoma:         "ab6a8f80f3a066a27302c41ce2654fd402f85fc4d52525d7af3f4e3b978150cb"
-    sha256 cellar: :any_skip_relocation, ventura:        "f37e370366a0f4366538069e71b2c1477d5b548063b7cf8107af5a068adae72c"
-    sha256 cellar: :any_skip_relocation, monterey:       "024bfaaa84d51f8bf22601c6a4bd1b6b5dc0be18d7018e9147bfa7c264f661f5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e0ea962775097fff56f34c8593895d9fb9ba424d5ec998719fa3f3a09c0958b4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "95c240d070a0f92d8dd52a2e6ecd22e11da5c3444d3d52d432f08aedeb018b8c"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "d197e9a43cdf9b165710e33aab3c928943fbcb2a30048e0c96a543f54ae1de4c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "6d0000aa8484410abd6c126077ec82f7b543c910f34a727cad62788801f01f53"
+    sha256 cellar: :any_skip_relocation, sonoma:         "f4259024ef2c89987fbefea352471f78f23227d9990411c128a4cf9ebb311b8d"
+    sha256 cellar: :any_skip_relocation, ventura:        "eeb7df8a260fc8155366691257683720f96e272334ecb9813e6deb31b3ae79f5"
+    sha256 cellar: :any_skip_relocation, monterey:       "00415cadf3daf62a77727d2d3b2ebcbc355b50bace2d8e70bfdc67e84ad8ab45"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "daaabc5deb57abf3a50466e064f905f593d264af7503f97821206f9621862c42"
   end
 
   depends_on "go" => :build
