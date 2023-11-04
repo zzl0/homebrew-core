19bab4be644ac62f1d0c80db3aebb00c63f10867
Rui Chen <rui@chenrui.dev>
2023-11-04 10:23:32 -0400

    pylyzer 0.0.49

--- Formula/p/pylyzer.rb
+++ Formula/p/pylyzer.rb
@@ -1,8 +1,8 @@
 class Pylyzer < Formula
   desc "Fast static code analyzer & language server for Python"
   homepage "https://github.com/mtshiba/pylyzer"
-  url "https://github.com/mtshiba/pylyzer/archive/refs/tags/v0.0.48.tar.gz"
-  sha256 "82630523fac721ea5d2a428201dd95c518937ba67471db2638adf4c3f1b3d557"
+  url "https://github.com/mtshiba/pylyzer/archive/refs/tags/v0.0.49.tar.gz"
+  sha256 "c07e9adba4a475cfce76fafbf34fdd7153e2ccecdad3bbb01eb99b53d7b66484"
   license "MIT"
 
   bottle do
