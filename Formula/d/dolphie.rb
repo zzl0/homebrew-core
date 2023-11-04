907c1aedf86f8a2e50ccf99789af303077edd678
Rui Chen <rui@chenrui.dev>
2023-11-04 16:52:29 +0000

    dolphie: update 3.1.4 bottle.

--- Formula/d/dolphie.rb
+++ Formula/d/dolphie.rb
@@ -8,13 +8,13 @@ class Dolphie < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "01eb71c04c0b7d8c7b857e1234da956ec4079d85fd0d383bde41e0a9a4d693eb"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "1de946dc43a3b7eb1f34dbb2fce621672f30cc76044b39b8102fcb155c865a2a"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3e81619f3df88b6b80b5f400be20bdab1eecf9de5a0b74193653c0300e80323c"
-    sha256 cellar: :any_skip_relocation, sonoma:         "3815c4fa864a9851629afc9c637291fa37a6d7d7edf230e450146bc496dede91"
-    sha256 cellar: :any_skip_relocation, ventura:        "6fd85ac036448f71794c43f274db8071692dfaff1f1c449da7187585ddd51d0e"
-    sha256 cellar: :any_skip_relocation, monterey:       "d2a5f5b464b49307ba8ac8b3c4fdd664439b9fdeea38c49eab439ac07f97c902"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "221efbf561cc9f8f0c8cd7cc16e93588a546f7e0ec56244742fdd19ae9647ba0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "e3e69148f8db27fbc55c79c5429d3cccb66083fc75fbc5dccda1175b1d4b90ab"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0c953dce2e263a1731c23b99e7967f551cedd2f32877a17fa10b6b4d6bd3f526"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e72c0d4b8d6aa510e49089681f46de2b3652cef3327d2d9103e3e5b70aa79253"
+    sha256 cellar: :any_skip_relocation, sonoma:         "77187ae8a702207661f4d49476a7b365dbb89c0b8407ca17613c4917fb542659"
+    sha256 cellar: :any_skip_relocation, ventura:        "c1fbb009d8d94c144bc892dbb744d58faff8a9189a30eab277b046f8f122e6e6"
+    sha256 cellar: :any_skip_relocation, monterey:       "ea93ac80d1f79b484d5f1eaad034a09f6bcfcbe34417de63d8c837bf877503fc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "24511e91c5d061acd0232f677858a9cf7b7299d0449e8f1179a2063e11d3b9d3"
   end
 
   depends_on "cffi"
