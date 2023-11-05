87e624e3f1f12e9da6c2f6166f789c1888400c17
David Baumgold <david@davidbaumgold.com>
2023-11-05 16:14:33 +0100

    python-mako 1.2.4 (new formula)
    python-mako: depends on `python-setuptools`

---
+++ Formula/p/python-mako.rb
@@ -0,0 +1,36 @@
+class PythonMako < Formula
+  desc "Fast templating language for Python"
+  homepage "https://www.makotemplates.org/"
+  url "https://files.pythonhosted.org/packages/05/5f/2ba6e026d33a0e6ddc1dddf9958677f76f5f80c236bd65309d280b166d3e/Mako-1.2.4.tar.gz"
+  sha256 "d60a3903dc3bb01a18ad6a89cdbe2e4eadc69c0bc8ef1e3773ba53d44c3f7a34"
+  license "MIT"
+
+  depends_on "python-setuptools" => :build
+  depends_on "python@3.11" => [:build, :test]
+  depends_on "python@3.12" => [:build, :test]
+  depends_on "python-markupsafe"
+
+  def pythons
+    deps.map(&:to_formula).sort_by(&:version).filter { |f| f.name.start_with?("python@") }
+  end
+
+  def install
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-m", "pip", "install", *std_pip_args, "."
+    end
+  end
+
+  test do
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-c", "from mako.template import Template"
+    end
+
+    (testpath/"test.mako").write <<~EOS
+      Hello, ${name}!
+    EOS
+    output = shell_output("#{bin}/mako-render --var name=Homebrew test.mako")
+    assert_equal "Hello, Homebrew!\n", output
+  end
+end
