cc8aa04df277f0901816a61ff3d1eb18a2645659
David Baumgold <david@davidbaumgold.com>
2023-11-06 20:22:47 +0000

    orbuculum: add 2.1.0 bottle.

--- Formula/o/orbuculum.rb
+++ Formula/o/orbuculum.rb
@@ -5,6 +5,16 @@ class Orbuculum < Formula
   sha256 "ccdd86130094001a0ab61e5501a6636e12c82b0b44690795a2911c65c5618c46"
   license "BSD-3-Clause"
 
+  bottle do
+    sha256 cellar: :any, arm64_sonoma:   "556cd8cbd595a45ac1c8990a2614b48b894374e96f2795bfea176297e8151d18"
+    sha256 cellar: :any, arm64_ventura:  "8c6a7c6c15b2d618e7df9fe26d6962d6e09251fb7f421c69e5ea97de8aa6e9db"
+    sha256 cellar: :any, arm64_monterey: "f947c45c58dc788c8b01e4bd6f4fa9dc6e717d7df77c08c181f0ce57dcc9f607"
+    sha256 cellar: :any, sonoma:         "eb447bef31cb0ce5f7b26e2f2bbc35e2d6019caf5d5bb9b8c8d669e11e599ea7"
+    sha256 cellar: :any, ventura:        "43d6988953f1798a89e73143f91e68d0738ee21028271c74d0200b67fdd7854f"
+    sha256 cellar: :any, monterey:       "706ebe4db79b819f884e13db2d54b32b408f4ac971ea754b5f61c7987c4f910a"
+    sha256               x86_64_linux:   "ee7af224831a8aa48cdf5980457d49e4ec80dd4664810d98442bc2039a02804e"
+  end
+
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
