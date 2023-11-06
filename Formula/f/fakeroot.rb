9d0da0ce14ad1d7b6c6d880bdb8c4a16f85c9521
Rui Chen <rui@chenrui.dev>
2023-11-06 10:17:01 -0500

    fakeroot 1.32.2

--- Formula/f/fakeroot.rb
+++ Formula/f/fakeroot.rb
@@ -1,8 +1,8 @@
 class Fakeroot < Formula
   desc "Provide a fake root environment"
   homepage "https://tracker.debian.org/pkg/fakeroot"
-  url "https://deb.debian.org/debian/pool/main/f/fakeroot/fakeroot_1.32.1.orig.tar.gz"
-  sha256 "c072b0f65bafc4cc5b6112f7c61185f5170ce4cb0c410d1681c1af4a183e94e6"
+  url "https://deb.debian.org/debian/pool/main/f/fakeroot/fakeroot_1.32.2.orig.tar.gz"
+  sha256 "f0f72b504f288eea5b043cd5fe37585bc163f5acaacd386e1976b1055686116d"
   license "GPL-3.0-or-later"
 
   livecheck do
