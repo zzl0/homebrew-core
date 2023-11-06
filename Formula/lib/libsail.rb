9a650ea2765540470a19101c8b2c51c7106f4c82
Rui Chen <rui@chenrui.dev>
2023-11-06 18:39:49 +0000

    libsail: add 0.9.0 bottle.

--- Formula/lib/libsail.rb
+++ Formula/lib/libsail.rb
@@ -5,6 +5,16 @@ class Libsail < Formula
   sha256 "892738e0f56fed8c6387e1045bba2bfbf1b095024a495845d4879edb310cd1a7"
   license "MIT"
 
+  bottle do
+    sha256 arm64_sonoma:   "65920d269a4808976c62a633c44842392a14782f40f7c8d2ae13cf3690aca4d6"
+    sha256 arm64_ventura:  "72e3d594e8f0969372ea6185a49e067cd3d7413d7e57f8c56bf1ee56111f93df"
+    sha256 arm64_monterey: "2460273c0459e5c506ae0fe2549b3773e524cda54e52ea0dda76c69726b9b329"
+    sha256 sonoma:         "61db1c9ae86851397c187f5eff9c2220c70202c82d1192b7a969b8fb2c2fdd9a"
+    sha256 ventura:        "07b58b25676f108676432feb75dfb10b8d1be3a518452058dab8f3bbf630ec43"
+    sha256 monterey:       "f4431c1af8d6e5cd39340766bf48f94558bc65b8c2fb9a8fd1af9e62af261b24"
+    sha256 x86_64_linux:   "5078ea7828d61766f094b081970e4127c9fed853f13bfed8b34895921977da36"
+  end
+
   depends_on "cmake"      => :build
   depends_on "pkg-config" => [:build, :test]
 
