71c140590135a119fba7ace12b2adb582e9ab19f
Porkepix <porkepix@gmail.com>
2023-11-06 13:35:36 +0100

    argparse 3.0

--- Formula/a/argparse.rb
+++ Formula/a/argparse.rb
@@ -1,8 +1,8 @@
 class Argparse < Formula
   desc "Argument Parser for Modern C++"
   homepage "https://github.com/p-ranav/argparse"
-  url "https://github.com/p-ranav/argparse/archive/refs/tags/v2.9.tar.gz"
-  sha256 "cd563293580b9dc592254df35b49cf8a19b4870ff5f611c7584cf967d9e6031e"
+  url "https://github.com/p-ranav/argparse/archive/refs/tags/v3.0.tar.gz"
+  sha256 "ba7b465759bb01069d57302855eaf4d1f7d677f21ad7b0b00b92939645c30f47"
   license "MIT"
   head "https://github.com/p-ranav/argparse.git", branch: "master"
 
