93ffb28ed409b84fbf52e3ac548f24b2ef19be9d
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2023-11-05 22:59:09 -0500

    ispc 1.21.1

--- Formula/i/ispc.rb
+++ Formula/i/ispc.rb
@@ -2,10 +2,9 @@ class Ispc < Formula
   desc "Compiler for SIMD programming on the CPU"
   homepage "https://ispc.github.io"
   # TODO: Check if we can use unversioned `llvm` at version bump.
-  url "https://github.com/ispc/ispc/archive/refs/tags/v1.21.0.tar.gz"
-  sha256 "ac0941ce4a0aae76901133c0d65975a17632734534668ce2871aacb0d99a036c"
+  url "https://github.com/ispc/ispc/archive/refs/tags/v1.21.1.tar.gz"
+  sha256 "9f4a11d68c1a3f64be7bad912ef00df50fa8fb3c3e98cebb502ee41d8f9e9fd7"
   license "BSD-3-Clause"
-  revision 1
 
   bottle do
     sha256 cellar: :any,                 arm64_sonoma:   "af5ef48ee6f33c9ac86c183eca004fbf4f254187c715f5d2c3c0f42b8f5243c0"
