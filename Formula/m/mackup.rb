78a504274490ffe18ab9e25dd59f2d32bbc04253
David Baumgold <david@davidbaumgold.com>
2023-11-07 13:24:58 +0100

    mackup: depend on python-docopt

--- Formula/m/mackup.rb
+++ Formula/m/mackup.rb
@@ -18,13 +18,9 @@ class Mackup < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "c818c7735d3347dbc2419b604aeaebcae7a93e7acd036cbb4222b051668ab70d"
   end
 
+  depends_on "python-docopt"
   depends_on "python@3.12"
   depends_on "six"
-
-  resource "docopt" do
-    url "https://files.pythonhosted.org/packages/a2/55/8f8cab2afd404cf578136ef2cc5dfb50baa1761b68c9da1fb1e4eed343c9/docopt-0.6.2.tar.gz"
-    sha256 "49b3a825280bd66b3aa83585ef59c4a8c82f2c8a522dbe754a8bc8d08c85c491"
-  end
 
   def install
     virtualenv_install_with_resources
