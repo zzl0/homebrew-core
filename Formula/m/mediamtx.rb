d9af9faba1e1bcbabedcbf4001f0fee1d338d97c
Dan Arnfield <dan@arnfield.com>
2023-11-07 12:19:19 -0600

    mediamtx 1.2.1 (new formula)

---
+++ Formula/m/mediamtx.rb
@@ -0,0 +1,46 @@
+class Mediamtx < Formula
+  desc "Zero-dependency real-time media server and media proxy"
+  homepage "https://github.com/bluenviron/mediamtx"
+  url "https://github.com/bluenviron/mediamtx/archive/refs/tags/v1.2.1.tar.gz"
+  sha256 "222553dada1947877867ffeb1f93aaa1e29c13dc2d64a1ba924693ca52f0a404"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X github.com/bluenviron/mediamtx/internal/core.version=#{version}"
+    system "go", "build", *std_go_args(ldflags: ldflags)
+
+    # Install default config
+    (etc/"mediamtx").install "mediamtx.yml"
+  end
+
+  def post_install
+    (var/"log/mediamtx").mkpath
+  end
+
+  service do
+    run [opt_bin/"mediamtx", etc/"mediamtx/mediamtx.yml"]
+    keep_alive true
+    working_dir HOMEBREW_PREFIX
+    log_path var/"log/mediamtx/output.log"
+    error_log_path var/"log/mediamtx/error.log"
+  end
+
+  test do
+    assert_equal version, shell_output(bin/"mediamtx --version")
+
+    mediamtx_api = "127.0.0.1:#{free_port}"
+    mediamtx = fork do
+      exec({ "MTX_API" => "yes", "MTX_APIADDRESS" => mediamtx_api }, bin/"mediamtx", etc/"mediamtx/mediamtx.yml")
+    end
+    sleep 3
+
+    # Check API output matches configuration
+    curl_output = shell_output("curl --silent http://#{mediamtx_api}/v3/config/global/get")
+    assert_match "\"apiAddress\":\"#{mediamtx_api}\"", curl_output
+  ensure
+    Process.kill("TERM", mediamtx)
+    Process.wait mediamtx
+  end
+end
