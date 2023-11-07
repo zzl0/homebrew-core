25896f4855f8b23b4341e7fe12beced170332db4
Porkepix <porkepix@gmail.com>
2023-11-07 11:44:59 +0100

    sing-box 1.6.2

--- Formula/s/sing-box.rb
+++ Formula/s/sing-box.rb
@@ -2,8 +2,8 @@ class SingBox < Formula
   desc "Universal proxy platform"
   homepage "https://sing-box.sagernet.org"
   # using `:homebrew_curl` to work around audit failure from TLS 1.3-only homepage
-  url "https://github.com/SagerNet/sing-box/archive/refs/tags/v1.6.1.tar.gz", using: :homebrew_curl
-  sha256 "0b19797d806c5739837e53d9c1207ae7bf674488cabd3f125ecf3e7f9cedeaa7"
+  url "https://github.com/SagerNet/sing-box/archive/refs/tags/v1.6.2.tar.gz", using: :homebrew_curl
+  sha256 "bb4ca130f4deaabdb5addcc873a5976f14e2573077985492e88ae37ec9fbcc45"
   license "GPL-3.0-or-later"
 
   bottle do
