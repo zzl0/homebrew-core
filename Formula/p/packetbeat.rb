bdea745eac8e05831a88d3cb7c2c5738ef761727
Nhan Doan <2561973+dnhn@users.noreply.github.com>
2023-11-08 02:39:01 +0900

    packetbeat 8.11.0

--- Formula/p/packetbeat.rb
+++ Formula/p/packetbeat.rb
@@ -2,8 +2,8 @@ class Packetbeat < Formula
   desc "Lightweight Shipper for Network Data"
   homepage "https://www.elastic.co/products/beats/packetbeat"
   url "https://github.com/elastic/beats.git",
-      tag:      "v8.10.4",
-      revision: "10b198c985eb95c16405b979c63847881a199aba"
+      tag:      "v8.11.0",
+      revision: "d3facc808d2ba293a42b2ad3fc8e21b66c5f2a7f"
   license "Apache-2.0"
   head "https://github.com/elastic/beats.git", branch: "master"
 
