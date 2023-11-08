cc86462a07814aea87e61cd740246b5c7c069ee3
Patrick Linnane <patrick@linnane.io>
2023-11-08 01:04:31 +0000

    dicebear: add 7.0.1 bottle.

--- Formula/d/dicebear.rb
+++ Formula/d/dicebear.rb
@@ -7,6 +7,16 @@ class Dicebear < Formula
   sha256 "3b787a849f720244648d01e6f25120a92dd88c900c799b92bb4207ff311ab2ee"
   license "MIT"
 
+  bottle do
+    sha256                               arm64_sonoma:   "9685d5dcfd18fe7ecee18f565fafcae60b1d835db95ede5e44fe659a69c40700"
+    sha256                               arm64_ventura:  "8185cb5be9847d02137f6512718f21524fe426817050c9f33e126cbadf9a0131"
+    sha256                               arm64_monterey: "725678da423ad2b8cc9984b9fb0ed47113042691a9f7652bd581d107f8e9df2d"
+    sha256                               sonoma:         "a991610d84c07d1a20606f32d675e7e2aea09257a32be65657f3e12bb120c6a1"
+    sha256                               ventura:        "e32f498f9d2978fb388d0ffcbd30cf5d9b7ff076fdf4f65ae874c72dc8e0cc3d"
+    sha256                               monterey:       "aaff211699ed5c4450441e689ca4fdb708f680b6ffc612de8e8dd5d911815426"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "59471671bdf88f35c866ec29649cd8660ccb7e80d661ef103e59ea8882ad459d"
+  end
+
   depends_on "node"
 
   on_linux do
