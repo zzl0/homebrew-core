9647b70a75505388b6e74ae18acc5222154cf292
Patrick Linnane <patrick@linnane.io>
2023-11-08 01:05:02 +0000

    ain: add 1.3.0 bottle.

--- Formula/a/ain.rb
+++ Formula/a/ain.rb
@@ -6,6 +6,16 @@ class Ain < Formula
   license "MIT"
   head "https://github.com/jonaslu/ain.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "ea25b30846e0726917a3657c40e6ea27f1ff562de9fea12c521df1e3350c5918"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "ea25b30846e0726917a3657c40e6ea27f1ff562de9fea12c521df1e3350c5918"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ea25b30846e0726917a3657c40e6ea27f1ff562de9fea12c521df1e3350c5918"
+    sha256 cellar: :any_skip_relocation, sonoma:         "5e06f44649af98a5b34a120f52dc20175932fe35b634566d7387e302ab9808dd"
+    sha256 cellar: :any_skip_relocation, ventura:        "5e06f44649af98a5b34a120f52dc20175932fe35b634566d7387e302ab9808dd"
+    sha256 cellar: :any_skip_relocation, monterey:       "5e06f44649af98a5b34a120f52dc20175932fe35b634566d7387e302ab9808dd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "939940932807210dfe45f6bb26c9a43dac10e77003670841a1f92a94b8635246"
+  end
+
   depends_on "go" => :build
 
   def install
