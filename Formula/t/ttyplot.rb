5fb7b43e7a79fa2d5f289a8a1fcca0b1ea7993cd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-11-09 15:55:17 +0000

    ttyplot: update 1.5.1 bottle.

--- Formula/t/ttyplot.rb
+++ Formula/t/ttyplot.rb
@@ -6,15 +6,13 @@ class Ttyplot < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "c24df0685dc076f14dcd00fa961d3c6c6a446a221271a069631c6daddc3d1aa2"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "767a44e685f2079d82454f7a4e7fc6d56d6b402f3a82df88ef00989d9b31098e"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "863baa45ba7f5ab1aad586332f76c91fc6210bf6dc6ceb1af7d40a5cdddd7f6a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1a39bab7eb01b6f56fbfbb48f519a1bcc15ec1eedbf72197572aa55913fb11cf"
-    sha256 cellar: :any_skip_relocation, sonoma:         "3103a2439ec90049b6945544b76e094809b3e6cf82ab42c1510775c7f5322c9e"
-    sha256 cellar: :any_skip_relocation, ventura:        "39258c0803f33905e7ab7aa5a76f3192fcba5b15f955a7b7e7a27ce9e5e45bd5"
-    sha256 cellar: :any_skip_relocation, monterey:       "bcde373e4c5603d06be8fae27214d7869705e2bd419961e3d056160b2833b246"
-    sha256 cellar: :any_skip_relocation, big_sur:        "987e9fa6aa9238163f994e245c1c2004faba72ec110a09cc237b5257cc5dbd1a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4b2c7594e230dce7c24a66d6745d0be3bb6d137b2ace6da15447f97939460ccd"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "dbfefcffe08bc4996aed76350eb01a4eb06d628b751b3c24ef43da3ee9cbd172"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "9881ee688e9ce54c7af9f29c9450980b2655903edd95499acd10f1511edd48b1"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "22aec3b740a3021769ff7856d3551b78eb123aaf576c90cc1c34ec752adcfdbd"
+    sha256 cellar: :any_skip_relocation, sonoma:         "62adcf8960be6f740ef587e1449825f2e7a1e84709e396a19b04fc5d6c84c5cb"
+    sha256 cellar: :any_skip_relocation, ventura:        "e4a058d713918e42d6167872bf5d2a9402b627dfcb7986c4f87ab4973b291ca4"
+    sha256 cellar: :any_skip_relocation, monterey:       "3cab19df5f1ecf3d6230da30b53a997776a7703459b72c92714007c211ea0078"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9c63dfcbac92d4bed8702e494a7eb28850fa708f209b6cd2ea952394192a83e5"
   end
 
   depends_on "pkg-config" => :build
