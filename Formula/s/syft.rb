43cf055950ccfcd8f5a7dd5ceede31767b6cda5f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-11-09 16:13:52 +0000

    syft: update 0.96.0 bottle.

--- Formula/s/syft.rb
+++ Formula/s/syft.rb
@@ -7,13 +7,13 @@ class Syft < Formula
   head "https://github.com/anchore/syft.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "04e8ed24c3bfd89e72c686958851831ca97ddd168a7e1555bc7fd4369c44033a"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "23b970dc68af24130c4163fed61a3ad336b2dad4fb710a6b43c4fdef5ce969d1"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "4e1d90c6cba241788164b67d6ab015a1c66aafd255649c48cfaa989e23e43b78"
-    sha256 cellar: :any_skip_relocation, sonoma:         "f94abea9804e260157c5c5f3ee0ae4587116fd34f7dc6602935fad9a8067ba53"
-    sha256 cellar: :any_skip_relocation, ventura:        "b10d8ff6de5bfb2d46a52fdbd72dc359a2d61382a6ff7dd8dda8ada430779a18"
-    sha256 cellar: :any_skip_relocation, monterey:       "ffe44abd5b3dd8b18aba40854f8071de564d99b5ffbe3454766d47bee84e7859"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d83c116040742ef6f077c9598af8e22515ef41b743e9914600ef370371cd4524"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "ef7e1056c3fb52af6298418e368aaca4e0eae9855e2f7d16824335f7b60411d3"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "29246295557056fda897ca57bb89c013faaa29ac32558cde9206e70dff47355f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b2aa135027640e49907109fe6b2216bb429a08c4a06f9be5a9fa03d05b05c8c4"
+    sha256 cellar: :any_skip_relocation, sonoma:         "79d6a5d8170cd6d8e5a9b9964fc11c859613d6ef0a453b41a8f3900b516307f7"
+    sha256 cellar: :any_skip_relocation, ventura:        "9c5aff5e1c7fa7cfef8d4261ee9c499fa87ba401206656474cfcc44d678c2c7d"
+    sha256 cellar: :any_skip_relocation, monterey:       "8bbdd87c0ccf18d185586a4e3e2ee6963705a0f37fb5f277324a9bffe114756e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b0e7febf2e4c8244a885515c51340549e2ee265b22d82c087bf77c024ee61913"
   end
 
   depends_on "go" => :build
