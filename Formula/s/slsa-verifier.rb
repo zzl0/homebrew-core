daf1b2a343d85a4312545d4bf9622e22692116d8
Rui Chen <rui@chenrui.dev>
2023-11-09 11:58:00 -0500

    slsa-verifier 2.4.1

--- Formula/s/slsa-verifier.rb
+++ Formula/s/slsa-verifier.rb
@@ -1,8 +1,8 @@
 class SlsaVerifier < Formula
   desc "Verify provenance from SLSA compliant builders"
   homepage "https://github.com/slsa-framework/slsa-verifier"
-  url "https://github.com/slsa-framework/slsa-verifier/archive/refs/tags/v2.4.0.tar.gz"
-  sha256 "8588428d22f931b751dcdc7d9b0489e11840efe91c5074ec3fb84b9d4206a52f"
+  url "https://github.com/slsa-framework/slsa-verifier/archive/refs/tags/v2.4.1.tar.gz"
+  sha256 "f601e768a3a1b073a0e89d5933fcece349e4c729f75f149319696eb5a88b8df2"
   license "Apache-2.0"
   head "https://github.com/slsa-framework/slsa-verifier.git", branch: "main"
 
