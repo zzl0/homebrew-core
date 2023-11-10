89a1053c579219ee17bbc9c31b5eef47ee084c75
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-11-10 22:23:08 +0000

    renovate: update 37.56.0 bottle.

--- Formula/r/renovate.rb
+++ Formula/r/renovate.rb
@@ -19,13 +19,13 @@ class Renovate < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "a141aed780340cccdb4dbd95e442aeb3e5c73e34b1e10eb3958b46f082073a17"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3705df0583a9fe73080d547582e6ec1fecc8af4c217919a6267bd7e38faf8a21"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "92ac3b1bd614b2d25cea28e8120bfd8ed7c3048a93e675e3dc87ca9b3e6f87c2"
-    sha256 cellar: :any_skip_relocation, sonoma:         "a44b6c5066f60ba1ff41218750cd921842c9b6bbc4a037a1c0a047c21ff014a9"
-    sha256 cellar: :any_skip_relocation, ventura:        "30ed390d4fed11b12b189e45c0144afbce8176b95d6cccc3f783b2d18b5bc2cc"
-    sha256 cellar: :any_skip_relocation, monterey:       "25464807c1b83482728622be8c56826fa417d52e0b0dcb1bd8c8df518d55de79"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4c37bed03e1daf9ff4d843e4f27a245b50acecd5324ff825f80e467612f15aa3"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "ac22d1aa369ce7af5c536a8dbb3c09428982961f32addc4c2a3e05cf25b9eca6"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "547490ca85b01630bf39a484d7060d94b40f9437b80eadf6eaa54d97e151f304"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "23fab03c0752e740b5f27df62dd0a8ba31879d95407132a69c5854a4f95e6657"
+    sha256 cellar: :any_skip_relocation, sonoma:         "55da1cb604f98732caa6895c86bf409ce3b985e2549d5320492ba5aab1a132e9"
+    sha256 cellar: :any_skip_relocation, ventura:        "6234f250ce08d72e6e8e7bd991b666e8ae3e60131bfc38bb6122d7bcf71d1a7f"
+    sha256 cellar: :any_skip_relocation, monterey:       "0b6aa7ba0db834c1be476ed187cdafdacfcd99715ec971ccd8d9fc3981e6961f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "39a6037406f6981a62dd328d633716f9a889472db39132a766e04420390f60d5"
   end
 
   depends_on "node"
