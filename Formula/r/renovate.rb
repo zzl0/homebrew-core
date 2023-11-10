be5e10a417bf3d643f80728a41faec52d0e0babd
Rui Chen <rui@chenrui.dev>
2023-11-10 11:16:04 -0500

    renovate 37.54.0

--- Formula/r/renovate.rb
+++ Formula/r/renovate.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Renovate < Formula
   desc "Automated dependency updates. Flexible so you don't need to be"
   homepage "https://github.com/renovatebot/renovate"
-  url "https://registry.npmjs.org/renovate/-/renovate-37.53.0.tgz"
-  sha256 "4b69f43ed6c373f911b929d5a4a87b474793ec7bf7376aaac08bad02e91ae2fc"
+  url "https://registry.npmjs.org/renovate/-/renovate-37.54.0.tgz"
+  sha256 "588d301adc107c435c0aa862001d4e141810e252a60893a01c92e86d40728a4e"
   license "AGPL-3.0-only"
 
   # There are thousands of renovate releases on npm and page the `Npm` strategy
