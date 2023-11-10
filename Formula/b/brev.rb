3a6094ad5417ebd65eac2fe6df98b313c432dc4d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-11-10 03:49:17 +0000

    brev: update 0.6.265 bottle.

--- Formula/b/brev.rb
+++ Formula/b/brev.rb
@@ -14,13 +14,13 @@ class Brev < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "a00694520b95284be39c1b6116ba3261fefce193ab18ecd8a424e88066e765ca"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8eae6fa65cc156f7f081c46d3bffe8f701a67c5051c576c57f76de272509cb3b"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "37e233949577cc3412793b001ec0a8c5860f4c9fadf70c84a2a084b8176fe2cb"
-    sha256 cellar: :any_skip_relocation, sonoma:         "54a8b36c4788efaf23d62cf652b9e482960d16962ed2e5178c4dad652947c586"
-    sha256 cellar: :any_skip_relocation, ventura:        "47d827ebf9571d13b8dade47a46f186a256eff77989e5a3506cccc3782b4f9aa"
-    sha256 cellar: :any_skip_relocation, monterey:       "c0f14de5d027b4eca95da92efb2c12e91ce08c38eb84c2d389523587fd3a4426"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c9738deee460894ded671e6d36569d74421909ca5263c19e3de40830077f868e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "b84f428f921df419582c2b95a81b1eb4f487ec728af7d20cbc648020411e6416"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3012f8b7032001f5f3fa72b441ec4b671ed955a1b3be7f14822e09fe0cad1ca9"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "d256ad779b8122d18266f6c23cd155f2ea0f4e6b445412271d0f6edda3a77bb1"
+    sha256 cellar: :any_skip_relocation, sonoma:         "d56dc99894dc09ec4c4b9af048164b27937d99e9e4d5c2ddcb2b46648c4cf851"
+    sha256 cellar: :any_skip_relocation, ventura:        "2661a58f343856a28b50124199b819c44e5fe65c49304143c04fe716e1ed0be4"
+    sha256 cellar: :any_skip_relocation, monterey:       "4545e5ed8857ac85010b52899215b1e67151c64d0f693a5f8985ad0bd10f434f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "760ed0f03f15946861a8c8de6adce92c1d96c30b522727fd5db845f08490810e"
   end
 
   depends_on "go" => :build
