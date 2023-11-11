95a50d28f29ca3fb6c7d60c149d48b0f05fb02b8
Rui Chen <rui@chenrui.dev>
2023-11-11 10:28:54 -0500

    joern 2.0.150

--- Formula/j/joern.rb
+++ Formula/j/joern.rb
@@ -2,8 +2,8 @@ class Joern < Formula
   desc "Open-source code analysis platform based on code property graphs"
   homepage "https://joern.io/"
   # joern should only be updated every 10 releases on multiples of 10
-  url "https://github.com/joernio/joern/archive/refs/tags/v2.0.140.tar.gz"
-  sha256 "ca3b2fec0a9249e90ccb6daadfb0c9ca929ab55e359b9818343a5776c2082459"
+  url "https://github.com/joernio/joern/archive/refs/tags/v2.0.150.tar.gz"
+  sha256 "c1e7d612886664c77fb30c56e6c5412e35dd4d2032d30f364775d65a00e4e55c"
   license "Apache-2.0"
 
   livecheck do
