73f55fa951772426c51102e7ffa82e9866c7e280
Patrick Linnane <patrick@linnane.io>
2023-11-10 19:27:53 -0800

    bomber 0.4.7
    bomber: update test

--- Formula/b/bomber.rb
+++ Formula/b/bomber.rb
@@ -1,8 +1,8 @@
 class Bomber < Formula
   desc "Scans Software Bill of Materials for security vulnerabilities"
   homepage "https://github.com/devops-kung-fu/bomber"
-  url "https://github.com/devops-kung-fu/bomber/archive/refs/tags/v0.4.6.tar.gz"
-  sha256 "bc5e372d6f336c54f676fa9c7f39f20983987590b826962eca1e4a16109cc8f6"
+  url "https://github.com/devops-kung-fu/bomber/archive/refs/tags/v0.4.7.tar.gz"
+  sha256 "4f3c66204b776c8a87a8ad48f9ed87d756164c890e03ce2aa0e217d809c3ff0e"
   license "MPL-2.0"
 
   bottle do
@@ -29,7 +29,7 @@ class Bomber < Formula
     assert_match version.to_s, shell_output("#{bin}/bomber --version")
 
     cp pkgshare/"_TESTDATA_/sbom/bomber.spdx.json", testpath
-    output = shell_output("#{bin}/bomber scan bomber.spdx.json", 10)
+    output = shell_output("#{bin}/bomber scan bomber.spdx.json")
     assert_match "Total vulnerabilities found:", output
   end
 end
