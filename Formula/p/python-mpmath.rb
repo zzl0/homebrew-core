745fce6d3e2f08305a117cf203eb78bfdc72e09b
David Baumgold <david@davidbaumgold.com>
2023-11-12 10:13:16 +0100

    python-mpmath 1.3.0 (new formula)

---
+++ Formula/p/python-mpmath.rb
@@ -0,0 +1,29 @@
+class PythonMpmath < Formula
+  desc "Python library for floating-point arithmetic with arbitrary precision"
+  homepage "https://mpmath.org"
+  url "https://files.pythonhosted.org/packages/e0/47/dd32fa426cc72114383ac549964eecb20ecfd886d1e5ccf5340b55b02f57/mpmath-1.3.0.tar.gz"
+  sha256 "7a28eb2a9774d00c7bc92411c19a89209d5da7c4c9a9e227be8330a23a25b91f"
+  license "BSD-3-Clause"
+
+  depends_on "python-setuptools" => :build
+  depends_on "python@3.11" => [:build, :test]
+  depends_on "python@3.12" => [:build, :test]
+
+  def pythons
+    deps.map(&:to_formula).sort_by(&:version).filter { |f| f.name.start_with?("python@") }
+  end
+
+  def install
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-m", "pip", "install", *std_pip_args, "."
+    end
+  end
+
+  test do
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-c", "import mpmath"
+    end
+  end
+end
