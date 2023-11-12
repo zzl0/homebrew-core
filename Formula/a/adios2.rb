b3fb691a181285bf752fe83bd2e2d5c7f55bac78
Michael Cho <michael@michaelcho.dev>
2023-11-11 22:07:44 -0500

    adios2: migrate to `python@3.12`

--- Formula/a/adios2.rb
+++ Formula/a/adios2.rb
@@ -4,7 +4,7 @@ class Adios2 < Formula
   url "https://github.com/ornladios/ADIOS2/archive/refs/tags/v2.9.2.tar.gz"
   sha256 "78309297c82a95ee38ed3224c98b93d330128c753a43893f63bbe969320e4979"
   license "Apache-2.0"
-  revision 1
+  revision 2
   head "https://github.com/ornladios/ADIOS2.git", branch: "master"
 
   livecheck do
@@ -32,7 +32,7 @@ class Adios2 < Formula
   depends_on "open-mpi"
   depends_on "pugixml"
   depends_on "pybind11"
-  depends_on "python@3.11"
+  depends_on "python@3.12"
   depends_on "yaml-cpp"
   depends_on "zeromq"
 
@@ -48,7 +48,7 @@ class Adios2 < Formula
   fails_with :clang if DevelopmentTools.clang_build_version == 1400
 
   def python3
-    "python3.11"
+    "python3.12"
   end
 
   def install
