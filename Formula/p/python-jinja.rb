ce040f140e684cdcad50148b3c73011d33ec7f2f
David Baumgold <david@davidbaumgold.com>
2023-11-12 10:09:34 +0100

    python-jinja 3.1.2 (new formula)

---
+++ Formula/p/python-jinja.rb
@@ -0,0 +1,30 @@
+class PythonJinja < Formula
+  desc "Fast, expressive, extensible templating engine for Python"
+  homepage "https://palletsprojects.com/p/jinja/"
+  url "https://files.pythonhosted.org/packages/7a/ff/75c28576a1d900e87eb6335b063fab47a8ef3c8b4d88524c4bf78f670cce/Jinja2-3.1.2.tar.gz"
+  sha256 "31351a702a408a9e7595a8fc6150fc3f43bb6bf7e319770cbc0db9df9437e852"
+  license "BSD-3-Clause"
+
+  depends_on "python-setuptools" => :build
+  depends_on "python@3.11" => [:build, :test]
+  depends_on "python@3.12" => [:build, :test]
+  depends_on "python-markupsafe"
+
+  def pythons
+    deps.map(&:to_formula).sort_by(&:version).filter { |f| f.name.start_with?("python@") }
+  end
+
+  def install
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-m", "pip", "install", *std_pip_args, "."
+    end
+  end
+
+  test do
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-c", "from jinja2 import Environment"
+    end
+  end
+end
