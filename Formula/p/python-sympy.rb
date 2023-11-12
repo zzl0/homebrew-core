8541c08b8d666ef869edbe331bd812e76f705042
David Baumgold <david@davidbaumgold.com>
2023-11-12 17:19:55 +0100

    python-sympy 1.12 (new formula)

---
+++ Formula/p/python-sympy.rb
@@ -0,0 +1,30 @@
+class PythonSympy < Formula
+  desc "Python library for symbolic mathematics"
+  homepage "https://www.sympy.org"
+  url "https://files.pythonhosted.org/packages/e5/57/3485a1a3dff51bfd691962768b14310dae452431754bfc091250be50dd29/sympy-1.12.tar.gz"
+  sha256 "ebf595c8dac3e0fdc4152c51878b498396ec7f30e7a914d6071e674d49420fb8"
+  license "BSD-3-Clause"
+
+  depends_on "python-setuptools" => :build
+  depends_on "python@3.11" => [:build, :test]
+  depends_on "python@3.12" => [:build, :test]
+  depends_on "python-mpmath"
+
+  def pythons
+    deps.map(&:to_formula).sort_by(&:version).filter { |f| f.name.start_with?("python@") }
+  end
+
+  def install
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-m", "pip", "install", *std_pip_args, "."
+    end
+  end
+
+  test do
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-c", "import sympy"
+    end
+  end
+end
