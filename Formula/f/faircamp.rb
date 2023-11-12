67e4cdac79f571904cf02b7de84998b867d0d479
Rui Chen <rui@chenrui.dev>
2023-11-12 22:10:49 +0000

    faircamp: add 0.9.2 bottle.

--- Formula/f/faircamp.rb
+++ Formula/f/faircamp.rb
@@ -5,6 +5,15 @@ class Faircamp < Formula
   sha256 "15c826e22d8297223ae6c591d14ff79226682ef4db3911a1f349d639406295b7"
   license "AGPL-3.0-or-later"
 
+  bottle do
+    sha256 cellar: :any, arm64_sonoma:   "c1e5bea71211ec1023a6642cb6e724c4fede822d3e05dfc34b18457c1c1cf34f"
+    sha256 cellar: :any, arm64_ventura:  "4700c89421188730e7f10141ad304bc77fed9ca14b3274a5af604365a8bb960e"
+    sha256 cellar: :any, arm64_monterey: "087e98915ae8676581e7e6f32c5230279af684885cbce817ce85f88d5dd63ed7"
+    sha256 cellar: :any, sonoma:         "c9f9e347b03d917bda3dd5ce772a0108a15ec29af17c4d25dcea2c4cdb2d5326"
+    sha256 cellar: :any, ventura:        "22b6b0f5cf4809afab09e9aec1d7f73ab0dab8d9c5f0fa9b1d0cc2d5ffb11715"
+    sha256 cellar: :any, monterey:       "28dbad8e63dec63f10e2d3ba96b97a5e561e1c82a6f6518bb0453192ea67ebc6"
+  end
+
   depends_on "opus" => :build
   depends_on "pkg-config" => :build
   depends_on "rust" => :build
