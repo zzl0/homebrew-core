f2309e03a9316504a9a589184c9e0f16ac4c1909
Michael Cho <michael@michaelcho.dev>
2023-11-13 09:44:35 -0500

    python-markupsafe: revision bump for `python@3.12` support
    Missing revision bump in #149596

--- Formula/p/python-markupsafe.rb
+++ Formula/p/python-markupsafe.rb
@@ -4,6 +4,7 @@ class PythonMarkupsafe < Formula
   url "https://files.pythonhosted.org/packages/6d/7c/59a3248f411813f8ccba92a55feaac4bf360d29e2ff05ee7d8e1ef2d7dbf/MarkupSafe-2.1.3.tar.gz"
   sha256 "af598ed32d6ae86f1b747b82783958b1a4ab8f617b06fe68795c7f026abbdcad"
   license "BSD-3-Clause"
+  revision 1
 
   bottle do
     rebuild 1
