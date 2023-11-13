dbfa693914756c14d2cbb16841ddc00a9081ccdb
Rui Chen <rui@chenrui.dev>
2023-11-13 05:39:43 +0000

    intercept: add 1.5.2 bottle.

--- Formula/i/intercept.rb
+++ Formula/i/intercept.rb
@@ -6,6 +6,16 @@ class Intercept < Formula
   license "AGPL-3.0-only"
   head "https://github.com/xfhg/intercept.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "00b60f3cb72b046e0ec2ab4de14621671ae9b047e33f46e064ed07c0cefd95c6"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "061be5b71333292fa7e261ef6d61241ad34b766f0c6a3575b80e11a62eb5285c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c16f931334c50458d2e9412ace7a7bde8979414a01f3c03074f97a8d7980a2b5"
+    sha256 cellar: :any_skip_relocation, sonoma:         "5ced7ddba62f63c4960c5be00bbb7fa06558d2a359fa806adc57a5c71e220421"
+    sha256 cellar: :any_skip_relocation, ventura:        "4bbac1b25675a94edb1386b3b9fa600d0ee9436aeb9a9a4cc53c19dd6d39620a"
+    sha256 cellar: :any_skip_relocation, monterey:       "d04dec14b2a7d162b619d9a01ee3d374e8c3cda881bb96c4eb1fe8cf3c8ea2b5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a42873e58969e38c58ba4b030c23f09beb7a2aa2b1e2d39a39e2f940168d5a91"
+  end
+
   depends_on "go" => :build
 
   def install
