6422b95309085951efb88d9de054e7890214cea2
Michael Cho <michael@michaelcho.dev>
2023-11-13 09:48:55 -0500

    python-click: revision bump for `python@3.12` support
    Missing revision bump in #150006

--- Formula/p/python-click.rb
+++ Formula/p/python-click.rb
@@ -4,6 +4,7 @@ class PythonClick < Formula
   url "https://files.pythonhosted.org/packages/96/d3/f04c7bfcf5c1862a2a5b845c6b2b360488cf47af55dfa79c98f6a6bf98b5/click-8.1.7.tar.gz"
   sha256 "ca9853ad459e787e2192211578cc907e7594e294c7ccc834310722b41b9ca6de"
   license "BSD-3-Clause"
+  revision 1
 
   bottle do
     rebuild 1
