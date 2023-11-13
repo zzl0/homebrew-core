251d6b3ed465068b4b44ac66850e222a225bad1e
Patrick Linnane <patrick@linnane.io>
2023-11-13 08:19:21 -0800

    rust-analyzer 2023-11-13

--- Formula/r/rust-analyzer.rb
+++ Formula/r/rust-analyzer.rb
@@ -2,8 +2,8 @@ class RustAnalyzer < Formula
   desc "Experimental Rust compiler front-end for IDEs"
   homepage "https://rust-analyzer.github.io/"
   url "https://github.com/rust-lang/rust-analyzer.git",
-       tag:      "2023-11-06",
-       revision: "c1c9e10f72ffd2e829d20ff1439ff49c2e121731"
+       tag:      "2023-11-13",
+       revision: "416e9c856a792ce2214c449677ca0a1f38965248"
   license any_of: ["Apache-2.0", "MIT"]
 
   bottle do
