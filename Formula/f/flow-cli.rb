82173439e231cc124807ae0a7e247e9dfe0d863b
IT-Flow-Service-Account <117774934+IT-Flow-Service-Account@users.noreply.github.com>
2023-11-12 22:35:28 -0700

    flow-cli 1.7.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/f/flow-cli.rb
+++ Formula/f/flow-cli.rb
@@ -1,8 +1,8 @@
 class FlowCli < Formula
   desc "Command-line interface that provides utilities for building Flow applications"
   homepage "https://onflow.org"
-  url "https://github.com/onflow/flow-cli/archive/refs/tags/v1.6.0.tar.gz"
-  sha256 "0bbabdc310e9327b0896f5ec54f72f1baba228e2d7583c83658b616bd644c944"
+  url "https://github.com/onflow/flow-cli/archive/refs/tags/v1.7.0.tar.gz"
+  sha256 "fd215b3c136d59641d19d033c7ab99896045c7accdf8fc4bf88725dcddf16358"
   license "Apache-2.0"
   head "https://github.com/onflow/flow-cli.git", branch: "master"
 
