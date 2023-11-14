b64a6de620080b6c5ad0623c78bca6aeb1efc476
Porkepix <porkepix@gmail.com>
2023-11-14 10:31:49 +0100

    pspg 5.8.1

--- Formula/p/pspg.rb
+++ Formula/p/pspg.rb
@@ -1,8 +1,8 @@
 class Pspg < Formula
   desc "Unix pager optimized for psql"
   homepage "https://github.com/okbob/pspg"
-  url "https://github.com/okbob/pspg/archive/refs/tags/5.8.0.tar.gz"
-  sha256 "e1043e61aa18916f0c02627a32059e0f0d68f5050a38fc36ea578d517ebb12ce"
+  url "https://github.com/okbob/pspg/archive/refs/tags/5.8.1.tar.gz"
+  sha256 "57f086f91927e0c1c2cfe1660049d7bed03b075c742a40c16bea5702a22169d0"
   license "BSD-2-Clause"
   head "https://github.com/okbob/pspg.git", branch: "master"
 
