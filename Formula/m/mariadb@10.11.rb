eff23d4f5503dc33f82896ba7784cb5982b39b0a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-11-14 10:23:19 +0000

    mariadb@10.11: update 10.11.6 bottle.

--- Formula/m/mariadb@10.11.rb
+++ Formula/m/mariadb@10.11.rb
@@ -18,15 +18,13 @@ class MariadbAT1011 < Formula
   end
 
   bottle do
-    sha256 arm64_sonoma:   "f3bafa553a67644ab1d8e6eb76c9d1db3bf7a638fcb43681d54bc729705ac5ee"
-    sha256 arm64_ventura:  "d117a91532231a2f91b39716e4257a3e2257a3d84006a34d582b59c9d98b07c0"
-    sha256 arm64_monterey: "fe27faf769b10b55e700ab93c5f7c265bdee0c6b2c3f92a6cfe9a4cfed4cdb5d"
-    sha256 arm64_big_sur:  "ae967cfea2127d4c1212e3d54b919f733ee66a5b409c3ae3f37e12c8a436fcad"
-    sha256 sonoma:         "278f9fd53a9219870bac2f4e06a9883605bd29adcffb921337fba5f749de53b5"
-    sha256 ventura:        "22e8c05cb2c76b8cf87c3ccf71a8c29bfba346d19f8171f804730e6f57a946a8"
-    sha256 monterey:       "3d8d364cf09544470425f97a2b8f4c9b8c55acc8d9307e9c20f2719aeb1f59c3"
-    sha256 big_sur:        "fc430504bccc930ee57e892204d3aa0e682b65fd65762ce9e46f1e319d11be88"
-    sha256 x86_64_linux:   "e1c75f860a355fbe418086acefee7d1c4f64e16247b701251d63beeea3ab6295"
+    sha256 arm64_sonoma:   "e93bbca206efd46e8a6c036beefd81df28a55dd85158881b2860e708fbd2200f"
+    sha256 arm64_ventura:  "a8a78ae028d20d99191915e6f9bc19d5d265c9e183995f4a4bc0f0a36cc49458"
+    sha256 arm64_monterey: "68f48dc066ab1f3ecfdc3597c69484402c7762dd12ef852b5458b87a6dced90b"
+    sha256 sonoma:         "5a0e888a8621c3080d5a1d652c6f7c642eed85b96bc223fc3c3753698e8e81e1"
+    sha256 ventura:        "d7b2751956bfbaa3d42d5c3e56c1f54094fc072dca6d044cf6f7b243180f179c"
+    sha256 monterey:       "e11be45ed8fdeae9a9c272c88814fefa446c73b500cb15d3ef91fa8ed978e1fb"
+    sha256 x86_64_linux:   "39a75e57130a03db495c0bba3acc84e49020520b95333a580c2caeead5fe5750"
   end
 
   keg_only :versioned_formula
