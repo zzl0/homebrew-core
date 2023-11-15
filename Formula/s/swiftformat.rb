0093210f02f20674fee128c6829668146c09b935
Rui Chen <rui@chenrui.dev>
2023-11-15 14:06:56 -0500

    swiftformat 0.52.10

--- Formula/s/swiftformat.rb
+++ Formula/s/swiftformat.rb
@@ -1,8 +1,8 @@
 class Swiftformat < Formula
   desc "Formatting tool for reformatting Swift code"
   homepage "https://github.com/nicklockwood/SwiftFormat"
-  url "https://github.com/nicklockwood/SwiftFormat/archive/refs/tags/0.52.9.tar.gz"
-  sha256 "f831e8be2524de2b47cb5ddf059573d9813625bb172de123e5a106d9f4d2f7ea"
+  url "https://github.com/nicklockwood/SwiftFormat/archive/refs/tags/0.52.10.tar.gz"
+  sha256 "5e6df043dbe44b99ccf4929b675224983e24f5f2900f4aa5e1d87796e6adf6db"
   license "MIT"
   head "https://github.com/nicklockwood/SwiftFormat.git", branch: "master"
 
