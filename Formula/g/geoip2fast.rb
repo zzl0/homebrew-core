a52889932d23c6c1ed620b92f9237044167ac64d
Patrick Linnane <patrick@linnane.io>
2023-11-15 21:38:09 +0000

    geoip2fast: add 1.1.8 bottle.

--- Formula/g/geoip2fast.rb
+++ Formula/g/geoip2fast.rb
@@ -6,6 +6,16 @@ class Geoip2fast < Formula
   license "MIT"
   head "https://github.com/rabuchaim/geoip2fast.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "0f999bb4afaa29f86f033fdbc6316b289e71a1c2b0158d0985fb4e7d63e4598d"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "36d87744e7fd44aaca31dc8538c6dbe0250068da43e344b6e7c6c23976cee444"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2036fd43b13e7311d81b775b1185746b20cb409b9ffaaee3938374a9916bb037"
+    sha256 cellar: :any_skip_relocation, sonoma:         "864e66a3cd21d3727e7ad611a22c3aae0188794c9cedc8802a66945ff4e381a8"
+    sha256 cellar: :any_skip_relocation, ventura:        "7d1a34faf1dfc058f3a3545b43ec35cca56b78025e1f11d164de62cacbcfd58a"
+    sha256 cellar: :any_skip_relocation, monterey:       "4af043208658bd0ce2391bf72d5e99cdba9a516d4fc7d521b4ff5a0c31666247"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1321f2a461559db9a056caaee33e308e05e62134c4d9d1bdf2f2c4f0b7e0c92f"
+  end
+
   depends_on "python-setuptools" => :build
   depends_on "python@3.12"
 
