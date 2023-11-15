584fef711ec254cf431ccaa8c3ae476ef9f60f97
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-11-15 18:41:01 +0000

    fortio 1.61.0

--- Formula/f/fortio.rb
+++ Formula/f/fortio.rb
@@ -2,8 +2,8 @@ class Fortio < Formula
   desc "HTTP and gRPC load testing and visualization tool and server"
   homepage "https://fortio.org/"
   url "https://github.com/fortio/fortio.git",
-      tag:      "v1.60.3",
-      revision: "197424c51b1d811b458415d55141f92c5f3464ef"
+      tag:      "v1.61.0",
+      revision: "f35afd8d70fe75b6ed8a2c6b56b54d6ee4e2eb36"
   license "Apache-2.0"
 
   # There can be a notable gap between when a version is tagged and a
