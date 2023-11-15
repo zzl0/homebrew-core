010a82b2f7350607dcaa8bc461319197ee328956
Rui Chen <rui@chenrui.dev>
2023-11-15 14:42:16 -0500

    emqx 5.3.1

--- Formula/e/emqx.rb
+++ Formula/e/emqx.rb
@@ -3,8 +3,8 @@ class Emqx < Formula
   homepage "https://www.emqx.io/"
   # TODO: Check if we can use unversioned `erlang` at version bump:
   #   https://github.com/emqx/emqx/blob/v#{version}/scripts/ensure-rebar3.sh#L9
-  url "https://github.com/emqx/emqx/archive/refs/tags/v5.3.0.tar.gz"
-  sha256 "086b1df81a7e5c93c7838a79076dacd2c2ce0371031991f239b6b5a628ecd08f"
+  url "https://github.com/emqx/emqx/archive/refs/tags/v5.3.1.tar.gz"
+  sha256 "11c3fead5af8359da7c55b74a0b4e9fb6a980f9e7b0752684867e65eda897fe9"
   license "Apache-2.0"
   head "https://github.com/emqx/emqx.git", branch: "master"
 
