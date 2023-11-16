5484ae5f74c9f01090cb4ae91db5c7d872e21de6
Patrick Linnane <patrick@linnane.io>
2023-11-16 09:43:01 -0800

    whisper-cpp 1.5.0
    whisper-cpp: update test

--- Formula/w/whisper-cpp.rb
+++ Formula/w/whisper-cpp.rb
@@ -1,8 +1,8 @@
 class WhisperCpp < Formula
   desc "Port of OpenAI's Whisper model in C/C++"
   homepage "https://github.com/ggerganov/whisper.cpp"
-  url "https://github.com/ggerganov/whisper.cpp/archive/refs/tags/v1.4.0.tar.gz"
-  sha256 "b2e34e65777033584fa6769a366cdb0228bc5c7da81e58a5e8dc0ce94d0fb54e"
+  url "https://github.com/ggerganov/whisper.cpp/archive/refs/tags/v1.5.0.tar.gz"
+  sha256 "088e6fcb5a38179308c4120c6ac4296918d2ea974321f6f2a062aec067c36880"
   license "MIT"
   head "https://github.com/ggerganov/whisper.cpp.git", branch: "master"
 
@@ -25,11 +25,11 @@ class WhisperCpp < Formula
     system "make"
     bin.install "main" => "whisper-cpp"
 
-    pkgshare.install ["samples/jfk.wav", "models/for-tests-ggml-tiny.bin"]
+    pkgshare.install ["samples/jfk.wav", "models/for-tests-ggml-tiny.bin", "ggml-metal.metal"]
   end
 
   test do
-    cp [pkgshare/"jfk.wav", pkgshare/"for-tests-ggml-tiny.bin"], testpath
+    cp [pkgshare/"jfk.wav", pkgshare/"for-tests-ggml-tiny.bin", pkgshare/"ggml-metal.metal"], testpath
 
     system "#{bin}/whisper-cpp", "samples", "-m", "for-tests-ggml-tiny.bin"
     assert_equal 0, $CHILD_STATUS.exitstatus, "whisper-cpp failed with exit code #{$CHILD_STATUS.exitstatus}"
