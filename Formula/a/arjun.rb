da5e20119e6bc7f983fb43bf96a499858d252e9f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-11-16 17:55:45 +0000

    arjun: update 2.2.2 bottle.

--- Formula/a/arjun.rb
+++ Formula/a/arjun.rb
@@ -8,13 +8,13 @@ class Arjun < Formula
   license "AGPL-3.0-only"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "bb788dc5db6eeffe1017b76e545ab7496eb864ea494d6cee5a340e88ddb54347"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "d16e4bd910501c8cdea6ab3c627d9bda3818ec321153608d248ad93e6788edf8"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "237ff6e8dec46ba50de22927fc469fbb1dc53a0b446c3b964ca57f56b74f6eae"
-    sha256 cellar: :any_skip_relocation, sonoma:         "972ae08a334985cfe0dc5e63e2ad185bc638d2b1bc989ac73d97e79a73e921b8"
-    sha256 cellar: :any_skip_relocation, ventura:        "2bf2d8dc1da010b4c39ad698a5706f91e48b2847eb825c25100ce483edccacee"
-    sha256 cellar: :any_skip_relocation, monterey:       "24c39bdd2fe7e1e9c39f8fbb07437b25f37e7bc188c210cd1be32133ee504979"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "06fdfa646ed1c5c10047db402bca1c89cc281a08c7a28b96514438688db55fd1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "192f7ae94916acd47d28413c5abeb492a1642c1b059c1093c1e95cd62e87a073"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "47cd188c43e6f7b0e2b301c00d68da0ee61719941e239e75a176853800701322"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f112f1ad2d0a9d64ce41d87d1e664c3d601de97cde3de6146e13ef8202adae6d"
+    sha256 cellar: :any_skip_relocation, sonoma:         "bee8fadcac1d7c77cce0e80c8cd0a9fc11f74180a1fd7522c789c0e14020d55c"
+    sha256 cellar: :any_skip_relocation, ventura:        "a194a2368caa2bbf92f53635712e27c427b853f3f7242e8f7f0f1231b53c5d55"
+    sha256 cellar: :any_skip_relocation, monterey:       "d925de5da66003db36f3e7b2414cea411c34bc28189cab5ccb4f87db076a295c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "eb9e5a170f2cda7983d63b6f598c7dd2106f81091360dd905aa7a064ea3b97b7"
   end
 
   depends_on "python-certifi"
