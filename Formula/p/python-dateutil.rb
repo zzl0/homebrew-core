61018ad14cfaef0aa042441fc43ee75cd0d59068
David Baumgold <david@davidbaumgold.com>
2023-11-17 22:12:42 +0100

    python-dateutil 2.8.2 (new formula)

---
+++ Formula/p/python-dateutil.rb
@@ -0,0 +1,30 @@
+class PythonDateutil < Formula
+  desc "Useful extensions to the standard Python datetime features"
+  homepage "https://github.com/dateutil/dateutil"
+  url "https://files.pythonhosted.org/packages/4c/c4/13b4776ea2d76c115c1d1b84579f3764ee6d57204f6be27119f13a61d0a9/python-dateutil-2.8.2.tar.gz"
+  sha256 "0123cacc1627ae19ddf3c27a5de5bd67ee4586fbdd6440d9748f8abb483d3e86"
+  license "Apache-2.0"
+
+  depends_on "python-setuptools" => :build
+  depends_on "python@3.11" => [:build, :test]
+  depends_on "python@3.12" => [:build, :test]
+  depends_on "six"
+
+  def pythons
+    deps.map(&:to_formula).sort_by(&:version).filter { |f| f.name.start_with?("python@") }
+  end
+
+  def install
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-m", "pip", "install", *std_pip_args, "."
+    end
+  end
+
+  test do
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-c", "import dateutil"
+    end
+  end
+end
