50ef8a6d44a5ea0326727e598d6cc9db602be451
Patrick Linnane <patrick@linnane.io>
2023-11-17 00:05:22 +0000

    oslo: add 0.11.0 bottle.

--- Formula/o/oslo.rb
+++ Formula/o/oslo.rb
@@ -6,6 +6,16 @@ class Oslo < Formula
   license "Apache-2.0"
   head "https://github.com/openslo/oslo.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "8683f78264b1e09294fa3d84fa0f27d0519c2212ad408c06c2910e26bf1483b4"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "30acddc46ade3dfafde77909568ff5dbc365365eaf8a04fa8684d52790c233c8"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5cc2e5bf21728181c866c176de73143cc8b868696f69c439dce034969d9e3dc2"
+    sha256 cellar: :any_skip_relocation, sonoma:         "0f0ad1e5922520937f86068721d1161851047c0bbd16e77a47e2fb7efffc6ca3"
+    sha256 cellar: :any_skip_relocation, ventura:        "70304446419104b0ca04b37f542371cab148570b5d18bc09662a4bf1e3a6c6bc"
+    sha256 cellar: :any_skip_relocation, monterey:       "54295eaf3564e40f085a1b1f8adb4cd12b4b0513da419006b6525665334accec"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6913e40ef59f049df1c0c95480a1a5b0aa9330c42e71b1501ad98242400d0645"
+  end
+
   depends_on "go" => :build
 
   def install
