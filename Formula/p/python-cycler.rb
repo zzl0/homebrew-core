c2e2717b6a352567fa125342214e1910f569ffb9
David Baumgold <david@davidbaumgold.com>
2023-11-17 21:57:03 +0100

    python-cycler 0.12.1 (new formula)

---
+++ Formula/p/python-cycler.rb
@@ -0,0 +1,29 @@
+class PythonCycler < Formula
+  desc "Composable style cycles"
+  homepage "https://github.com/matplotlib/cycler"
+  url "https://files.pythonhosted.org/packages/a9/95/a3dbbb5028f35eafb79008e7522a75244477d2838f38cbb722248dabc2a8/cycler-0.12.1.tar.gz"
+  sha256 "88bb128f02ba341da8ef447245a9e138fae777f6a23943da4540077d3601eb1c"
+  license "BSD-3-Clause"
+
+  depends_on "python-setuptools" => :build
+  depends_on "python@3.11" => [:build, :test]
+  depends_on "python@3.12" => [:build, :test]
+
+  def pythons
+    deps.map(&:to_formula).sort_by(&:version).filter { |f| f.name.start_with?("python@") }
+  end
+
+  def install
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-m", "pip", "install", *std_pip_args, "."
+    end
+  end
+
+  test do
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-c", "import cycler"
+    end
+  end
+end
