f76ff15676d365646c7896ab4697388df7650721
Michael Cho <michael@michaelcho.dev>
2023-11-18 12:21:00 -0500

    gtkglext: revision bump (at-spi2-core 2.50.0)

--- Formula/g/gtkglext.rb
+++ Formula/g/gtkglext.rb
@@ -4,7 +4,7 @@ class Gtkglext < Formula
   url "https://download.gnome.org/sources/gtkglext/1.2/gtkglext-1.2.0.tar.gz"
   sha256 "e5073f3c6b816e7fa67d359d9745a5bb5de94a628ac85f624c992925a46844f9"
   license "LGPL-2.1-or-later"
-  revision 3
+  revision 4
 
   bottle do
     rebuild 2
