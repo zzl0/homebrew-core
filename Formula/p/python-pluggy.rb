e4cfedb0b65440bbcbd74ca8b7c9484c9befdcba
David Baumgold <david@davidbaumgold.com>
2023-11-19 19:35:50 +0100

    python-pluggy 1.3.0 (new formula)

---
+++ Formula/p/python-pluggy.rb
@@ -0,0 +1,29 @@
+class PythonPluggy < Formula
+  desc "Minimalist production ready plugin system"
+  homepage "https://github.com/pytest-dev/pluggy"
+  url "https://files.pythonhosted.org/packages/36/51/04defc761583568cae5fd533abda3d40164cbdcf22dee5b7126ffef68a40/pluggy-1.3.0.tar.gz"
+  sha256 "cf61ae8f126ac6f7c451172cf30e3e43d3ca77615509771b3a984a0730651e12"
+  license "MIT"
+
+  depends_on "python-setuptools-scm" => :build
+  depends_on "python@3.11" => [:build, :test]
+  depends_on "python@3.12" => [:build, :test]
+
+  def pythons
+    deps.map(&:to_formula).sort_by(&:version).filter { |f| f.name.start_with?("python@") }
+  end
+
+  def install
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-m", "pip", "install", *std_pip_args, "."
+    end
+  end
+
+  test do
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-c", "import pluggy"
+    end
+  end
+end
