7820517ebda32b154f33e195553c1dc45dacdeee
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-11-19 18:53:30 -0500

    yq 4.40.2

--- Formula/y/yq.rb
+++ Formula/y/yq.rb
@@ -1,8 +1,8 @@
 class Yq < Formula
   desc "Process YAML, JSON, XML, CSV and properties documents from the CLI"
   homepage "https://github.com/mikefarah/yq"
-  url "https://github.com/mikefarah/yq/archive/refs/tags/v4.35.2.tar.gz"
-  sha256 "8b17d710c56f764e9beff06d7a7b1c77d87c4ba4219ce4ce67e7ee29670f4f13"
+  url "https://github.com/mikefarah/yq/archive/refs/tags/v4.40.2.tar.gz"
+  sha256 "96c2fdd4881ab21065c5ee324139609870a9b2d84b95abbdf0f282ce71e59cf4"
   license "MIT"
   head "https://github.com/mikefarah/yq.git", branch: "master"
 
