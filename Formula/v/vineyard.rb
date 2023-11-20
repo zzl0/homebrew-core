23ea65de1207cf9e5f80400bdce946c9e79473ab
Eric Knibbe <enk3@outlook.com>
2023-11-20 08:36:58 -0500

    vineyard: revision bump (open-mpi 5.0.0)

--- Formula/v/vineyard.rb
+++ Formula/v/vineyard.rb
@@ -6,7 +6,7 @@ class Vineyard < Formula
   url "https://github.com/v6d-io/v6d/releases/download/v0.18.2/v6d-0.18.2.tar.gz"
   sha256 "1255ca4ebeb5da34e33cb82a8d5530a5fc6b5fcebfb52d1b085058b2eab4a53c"
   license "Apache-2.0"
-  revision 1
+  revision 2
 
   bottle do
     sha256                               arm64_sonoma:   "059930e4ca93f5c26c522738eb75ecc55a90f0c6284cffa412aac54cb1e3c1a6"
