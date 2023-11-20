109fc913aeb8107473e8a32599bdc64a8110e16e
David Anthoff <anthoff@berkeley.edu>
2023-11-20 23:19:17 +0000

    juliaup 1.12.0

--- Formula/j/juliaup.rb
+++ Formula/j/juliaup.rb
@@ -1,8 +1,8 @@
 class Juliaup < Formula
   desc "Julia installer and version multiplexer"
   homepage "https://github.com/JuliaLang/juliaup"
-  url "https://github.com/JuliaLang/juliaup/archive/refs/tags/v1.11.23.tar.gz"
-  sha256 "8c4e20a332e2966671cc14bdd4e628aa7363192c036b53963e75ef8f27846ce7"
+  url "https://github.com/JuliaLang/juliaup/archive/refs/tags/v1.12.0.tar.gz"
+  sha256 "57cfc5d7fc57acba6e90f5708f2420a2601f9a9a7bf3f1ca2f4c8e26d6e30df9"
   license "MIT"
   head "https://github.com/JuliaLang/juliaup.git", branch: "master"
 
