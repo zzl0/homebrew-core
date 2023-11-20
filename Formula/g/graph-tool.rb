62d39985104144c823063a5a3c273ea5b034702b
David Baumgold <david@davidbaumgold.com>
2023-11-20 09:57:36 +0100

    graph-tool: depend on python-matplotlib

--- Formula/g/graph-tool.rb
+++ Formula/g/graph-tool.rb
@@ -31,7 +31,6 @@ class GraphTool < Formula
   depends_on "boost-python3"
   depends_on "cairomm@1.14"
   depends_on "cgal"
-  depends_on "fonttools"
   depends_on "google-sparsehash"
   depends_on "gtk+3"
   depends_on "librsvg"
@@ -40,6 +39,7 @@ class GraphTool < Formula
   depends_on "pillow"
   depends_on "py3cairo"
   depends_on "pygobject3"
+  depends_on "python-matplotlib"
   depends_on "python-packaging"
   depends_on "python-pyparsing"
   depends_on "python@3.12"
@@ -47,39 +47,6 @@ class GraphTool < Formula
   depends_on "six"
 
   uses_from_macos "expat" => :build
-
-  fails_with :gcc do
-    version "6"
-    cause "Requires C++17 compiler"
-  end
-
-  resource "contourpy" do
-    url "https://files.pythonhosted.org/packages/b4/9b/6edb9d3e334a70a212f66a844188fcb57ddbd528cbc3b1fe7abfc317ddd7/contourpy-1.0.7.tar.gz"
-    sha256 "d8165a088d31798b59e91117d1f5fc3df8168d8b48c4acc10fc0df0d0bdbcc5e"
-  end
-
-  resource "cycler" do
-    url "https://files.pythonhosted.org/packages/34/45/a7caaacbfc2fa60bee42effc4bcc7d7c6dbe9c349500e04f65a861c15eb9/cycler-0.11.0.tar.gz"
-    sha256 "9c87405839a19696e837b3b818fed3f5f69f16f1eec1a1ad77e043dcea9c772f"
-  end
-
-  resource "kiwisolver" do
-    url "https://files.pythonhosted.org/packages/5f/5c/272a7dd49a1914f35cd8d6d9f386defa8b047f6fbd06badd6b77b3ba24e7/kiwisolver-1.4.4.tar.gz"
-    sha256 "d41997519fcba4a1e46eb4a2fe31bc12f0ff957b2b81bac28db24744f333e955"
-  end
-
-  resource "matplotlib" do
-    url "https://files.pythonhosted.org/packages/b7/65/d6e00376dbdb6c227d79a2d6ec32f66cfb163f0cd924090e3133a4f85a11/matplotlib-3.7.1.tar.gz"
-    sha256 "7b73305f25eab4541bd7ee0b96d87e53ae9c9f1823be5659b806cd85786fe882"
-
-    # fix numpy build issue, https://github.com/matplotlib/matplotlib/issues/26246
-    patch :DATA
-  end
-
-  resource "python-dateutil" do
-    url "https://files.pythonhosted.org/packages/4c/c4/13b4776ea2d76c115c1d1b84579f3764ee6d57204f6be27119f13a61d0a9/python-dateutil-2.8.2.tar.gz"
-    sha256 "0123cacc1627ae19ddf3c27a5de5bd67ee4586fbdd6440d9748f8abb483d3e86"
-  end
 
   resource "zstandard" do
     url "https://files.pythonhosted.org/packages/4d/70/1f883646641d7ad3944181549949d146fa19e286e892bc013f7ce1579e8f/zstandard-0.21.0.tar.gz"
@@ -94,8 +61,6 @@ class GraphTool < Formula
 
   # https://git.skewed.de/count0/graph-tool/-/wikis/Installation-instructions#manual-compilation
 
-  # Resources are for Python `matplotlib` and `zstandard` packages
-
   def python3
     "python3.12"
   end
@@ -109,11 +74,6 @@ class GraphTool < Formula
     xy = Language::Python.major_minor_version(python3)
     venv = virtualenv_create(libexec, python3)
     venv.pip_install resources
-
-    %w[fonttools].each do |package_name|
-      package = Formula[package_name].opt_libexec
-      (libexec/site_packages/"homebrew-#{package_name}.pth").write package/site_packages
-    end
 
     args = %W[
       PYTHON=#{python3}
@@ -145,18 +105,3 @@ class GraphTool < Formula
     system python3, "test.py"
   end
 end
-
-__END__
-diff --git a/pyproject.toml b/pyproject.toml
-index 907b05a..81e3d80 100644
---- a/pyproject.toml
-+++ b/pyproject.toml
-@@ -2,7 +2,7 @@
- build-backend = "setuptools.build_meta"
- requires = [
-     "certifi>=2020.06.20",
--    "oldest-supported-numpy",
-+    "numpy",
-     "pybind11>=2.6",
-     "setuptools_scm>=7",
- ]
