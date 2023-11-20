86506e36dcd022bbfb1f6f3e4ad9b4bfeaba6d29
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-11-20 18:25:41 +0000

    balena-cli 17.4.4

--- Formula/b/balena-cli.rb
+++ Formula/b/balena-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class BalenaCli < Formula
   desc "Command-line tool for interacting with the balenaCloud and balena API"
   homepage "https://www.balena.io/docs/reference/cli/"
-  url "https://registry.npmjs.org/balena-cli/-/balena-cli-17.4.3.tgz"
-  sha256 "0be2c0e133c78f0ba46c1b75a51b52a4aa365405a4ced7f8f457b6d5ac4c2c39"
+  url "https://registry.npmjs.org/balena-cli/-/balena-cli-17.4.4.tgz"
+  sha256 "37f52d447955830bca272e939d05af8cbe168a12f5db219448f559ab5831013c"
   license "Apache-2.0"
 
   livecheck do
