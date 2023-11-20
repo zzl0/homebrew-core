705634daf49ceda72abeace7bd5d036dd171aa99
Rui Chen <rui@chenrui.dev>
2023-11-20 21:15:26 +0000

    spicetify-cli: add 2.27.1 bottle.

--- Formula/s/spicetify-cli.rb
+++ Formula/s/spicetify-cli.rb
@@ -6,6 +6,16 @@ class SpicetifyCli < Formula
   license "LGPL-2.1-only"
   head "https://github.com/spicetify/spicetify-cli.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "6f50e1bb26289e70c0fc179110b3d93de53d8b60583785c26882ba2117b266e2"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "1f06d26f99b502efed9fd568b5caf33f603e5b101257cce2de0a8c81491d46e8"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "711556dde2b73d017b6120f6d3d1a5e5b83991ab6d9925cd8a0e5b271deb8136"
+    sha256 cellar: :any_skip_relocation, sonoma:         "3686ef6e0e4b020fbd28a24afe524570e57b3c3c40de427e9dd7b5ce7a149550"
+    sha256 cellar: :any_skip_relocation, ventura:        "a04b1d05b70de1c31e7b9131417a7430114b5343828464748df330bbd6622a7f"
+    sha256 cellar: :any_skip_relocation, monterey:       "c0b4c99ccc588b2d39b28ab770c91633e4568ef90a1e3283566111b915b7a41a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "478bb40fb690b5debf9501292acd60585cb0a3555e49973c77f5ec5381480fc7"
+  end
+
   depends_on "go" => :build
 
   def install
