ac7bcfa698aa7b7aa576ce38720c25e479d3d92a
Patrick Linnane <patrick@linnane.io>
2023-11-20 03:43:14 +0000

    python-hatchling: add 1.18.0 bottle.

--- Formula/p/python-hatchling.rb
+++ Formula/p/python-hatchling.rb
@@ -5,6 +5,16 @@ class PythonHatchling < Formula
   sha256 "50e99c3110ce0afc3f7bdbadff1c71c17758e476731c27607940cfa6686489ca"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "40585512588943a1d1b2f172853591258d9e04cc56548984d9bbf840a0e5a602"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "385d0f6148e4c68c2f4f8d73db4f02bbc5f2b27f4c1dd63f7458cbccf1783fac"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c0226db0190cf76954950f7cc77bc1450ef13e9392fa15522f0325d5edb923e6"
+    sha256 cellar: :any_skip_relocation, sonoma:         "98bcb3bd4d38a864a8f0379231f93207eb66635410a88b52ea0d49d5846c1fd2"
+    sha256 cellar: :any_skip_relocation, ventura:        "d85d12902558e8376011b300c07aa46c5c2546e0134e2cdc8c87f1289180b34e"
+    sha256 cellar: :any_skip_relocation, monterey:       "0dbda1cf944f4a446882f5cbe6881739c2ebfb20b592fae11ab0220535c89cae"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "622d5f5c2ca82cbb86fd94a962ab12e85048b63fd4fd59ffb2562c6b0f3920da"
+  end
+
   depends_on "python-setuptools" => :build
   depends_on "python@3.11" => [:build, :test]
   depends_on "python@3.12" => [:build, :test]
