fe4106002f18ac2d508eb191c7ddb58fb84e8e73
Patrick Linnane <patrick@linnane.io>
2023-11-20 02:33:34 +0000

    python-pathspec: add 0.11.2 bottle.

--- Formula/p/python-pathspec.rb
+++ Formula/p/python-pathspec.rb
@@ -5,6 +5,16 @@ class PythonPathspec < Formula
   sha256 "e0d8d0ac2f12da61956eb2306b69f9469b42f4deb0f3cb6ed47b9cce9996ced3"
   license "MPL-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "6efed506957d0ee13629b91611da115fcc5dfc88e7a302c865d916f66872cfa9"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "179241c63484fc67d4ec3384079f31b2ac5c3b82f906886e0a4e6f3a2dbbd1c4"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "fce55aeeae1c5cc865a0ba0ab79050fdc1cafe38a9445e618f51d1b262d0b9bd"
+    sha256 cellar: :any_skip_relocation, sonoma:         "c4ba34a88e66d5fbfb509a5de3b49d4d91efcedd385782208844231f489bc4a0"
+    sha256 cellar: :any_skip_relocation, ventura:        "463c426fa0b4f7839bef49afc8d1d5b434601aa37d486190c8e32d30d1531070"
+    sha256 cellar: :any_skip_relocation, monterey:       "f8df2a98bd0b316f7e9a7f597e0c64be1db187549f37030e517b814968c6770b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3826917cffa95f99d056869b663b31fbf1b3e14eeb65c4d40da3b771caf06a25"
+  end
+
   depends_on "python-flit-core" => :build
   depends_on "python@3.11" => [:build, :test]
   depends_on "python@3.12" => [:build, :test]
