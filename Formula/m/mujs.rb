16861afe7930fd8d3e45b54b4315a0172de4cc92
miccal <miccal.matthews@gmail.com>
2023-11-22 04:39:33 +0800

    mujs 1.3.4

--- Formula/m/mujs.rb
+++ Formula/m/mujs.rb
@@ -1,8 +1,8 @@
 class Mujs < Formula
   desc "Embeddable Javascript interpreter"
   homepage "https://www.mujs.com/"
-  url "https://mujs.com/downloads/mujs-1.3.3.tar.gz"
-  sha256 "e2c5ee5416dfda2230c7a0cb7895df9a9b2d5b2065bb18e7e64dec2a796abe1b"
+  url "https://mujs.com/downloads/mujs-1.3.4.tar.gz"
+  sha256 "c015475880f6a382e706169c94371a7dd6cc22078832f6e0865af8289c2ef42b"
   license "ISC"
   head "https://github.com/ccxvii/mujs.git", branch: "master"
 
