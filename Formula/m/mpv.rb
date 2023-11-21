2fda5eda5989eae6230b3cb1a4c51a7a90ac29bd
Rui Chen <rui@chenrui.dev>
2023-11-21 16:21:46 -0500

    mpv: revision bump (mujs 1.3.4)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/m/mpv.rb
+++ Formula/m/mpv.rb
@@ -4,6 +4,7 @@ class Mpv < Formula
   url "https://github.com/mpv-player/mpv/archive/refs/tags/v0.37.0.tar.gz"
   sha256 "1d2d4adbaf048a2fa6ee134575032c4b2dad9a7efafd5b3e69b88db935afaddf"
   license :cannot_represent
+  revision 1
   head "https://github.com/mpv-player/mpv.git", branch: "master"
 
   bottle do
