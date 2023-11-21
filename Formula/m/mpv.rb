9b803f7a8981f46e86bfd9fc803df8959114e5e3
Dan <dan@wadoku.de>
2023-11-21 21:06:36 +0100

    mpv 0.37.0

--- Formula/m/mpv.rb
+++ Formula/m/mpv.rb
@@ -1,8 +1,8 @@
 class Mpv < Formula
   desc "Media player based on MPlayer and mplayer2"
   homepage "https://mpv.io"
-  url "https://github.com/mpv-player/mpv/archive/refs/tags/v0.36.0.tar.gz"
-  sha256 "29abc44f8ebee013bb2f9fe14d80b30db19b534c679056e4851ceadf5a5e8bf6"
+  url "https://github.com/mpv-player/mpv/archive/refs/tags/v0.37.0.tar.gz"
+  sha256 "1d2d4adbaf048a2fa6ee134575032c4b2dad9a7efafd5b3e69b88db935afaddf"
   license :cannot_represent
   head "https://github.com/mpv-player/mpv.git", branch: "master"
 
