f51ee9b3b522a15fe2d95f627316b614385afeca
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-11-21 02:12:31 +0000

    httm: update 0.31.1 bottle.

--- Formula/h/httm.rb
+++ Formula/h/httm.rb
@@ -7,13 +7,13 @@ class Httm < Formula
   head "https://github.com/kimono-koans/httm.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "d4485a5c5f61e9e159fb4ab015596890db6dfdc6e208bf6a768b16472e6afaa4"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "d582d7f15ec8a73515b5b7111c9a84d0803d455820806cc998d2e26136ac740c"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "eb51ad5959c9f4913249657748cec2ec49ed22f6a0941535bd21640693717e5a"
-    sha256 cellar: :any_skip_relocation, sonoma:         "639e0a4faa3814397942b033160764b4ee3cdc5c213f5858f937e8a25d9441cc"
-    sha256 cellar: :any_skip_relocation, ventura:        "f5b8eb8bd560fba8c4fc0f77b97f5ebd2f4bb684789a6c78babfc4678d48c69f"
-    sha256 cellar: :any_skip_relocation, monterey:       "4c5bb8b1b2f2ea1fbac01f409494ecc2cce6426d4d0886eac011c186b9e7104f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "af71c002241e9982e1accfb7d312b59cee9198f00f80b8e2bbc236f79bbd111f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "a62bd8caa5d309e1708488b6b3ea3e143043ec9b8dea79a44d253d955e995fb0"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "ea5c5b89b89d16ae3d41776b7938072d5fde3b573ec06b872d1f276f44fe001e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "eefe4c0a3a9bacd38b5fc2da71a1a2cdf13e897dc88cdda740e4626d6b323aac"
+    sha256 cellar: :any_skip_relocation, sonoma:         "7633d129cf9efb061268ac4efaee619faa717c5092ec638ef46900f13a86cf06"
+    sha256 cellar: :any_skip_relocation, ventura:        "dcad669e8ee416288fc86014847f5d2adf1585ca5ea7c6952f856799bcb2c019"
+    sha256 cellar: :any_skip_relocation, monterey:       "11a8d31fa1223995670add0f57fa0df51e7907ba80c406aec862b82982d2ce7c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "37143ebcb561b95dcca805255378baef537e03516760175fb14577afa260a180"
   end
 
   depends_on "rust" => :build
