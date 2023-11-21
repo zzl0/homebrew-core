2a846d0e410ab9a1eaea96394ab85b754a462e3c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-11-21 18:25:12 +0000

    s4cmd: update 2.1.0_2 bottle.

--- Formula/s/s4cmd.rb
+++ Formula/s/s4cmd.rb
@@ -10,14 +10,14 @@ class S4cmd < Formula
   head "https://github.com/bloomreach/s4cmd.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "2422fd3cfc7d26bf2d6a928c8200e5a492254d343b544ba18b5d1c81bef7c795"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "75185449973afa555ca68d183ac8f52ab55b6f071f017314b8b9199c9f83b2f2"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "6c50276fc8504006255a52c3850c386ef944bd96fa6f6dcc13faefa7788e268f"
-    sha256 cellar: :any_skip_relocation, sonoma:         "f9c671e266a8c7c20f255a10e2a339ec0ebbc24dac6e26e93695b37ec628c68e"
-    sha256 cellar: :any_skip_relocation, ventura:        "28cb5879f4d53c3f739f6e797fab6643f069ce539c44524eaea6c318e3b65c4c"
-    sha256 cellar: :any_skip_relocation, monterey:       "4039bac60900e575c917adabb32202ea14d9bca482992894796c2d46852a7bce"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3577d2d77fe3ba4f379ecabb34aef8ea74b1f509c83072d24dbe7b6c06c4e101"
+    rebuild 2
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "dfa74e751836c043bb6d908e92f59818408e7eed6bb701d4cfe02feff676f587"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "11adfe8f1c87eb35d70d950465d4c9ff92482cf0da8ad84a1d02d09c1cd76820"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "d7e56759aef5c24f86c1152e52c565de712f9706bb4111595e80c8ae895de180"
+    sha256 cellar: :any_skip_relocation, sonoma:         "854f04b313ddbc68dfb859749c49f343cdbcd384c278ae6e2d11b5b913076366"
+    sha256 cellar: :any_skip_relocation, ventura:        "c8f40364d09dcb7c9a72ebcc7b0aaf1f1d438ce7ccef44b6054becd37b0fd601"
+    sha256 cellar: :any_skip_relocation, monterey:       "681ddc2aba08ba18ec2d3bb4db42f3ba3a45cdcfce973ca24b3536fbf2dbf70f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d8aa586e1f2f5f8c89f67f21ca0be9a3fe654263947ba34fd0634e3809dd94fa"
   end
 
   depends_on "python-dateutil"
