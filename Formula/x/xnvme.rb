128c65591751bcdc29a65f655128989834e2c782
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-11-22 11:35:50 +0000

    xnvme: update 0.7.3 bottle.

--- Formula/x/xnvme.rb
+++ Formula/x/xnvme.rb
@@ -6,13 +6,13 @@ class Xnvme < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "830f567b154ea9df0fdd892ecfa8f003a81d26f42fe887ec2227ab6de18aadae"
-    sha256 cellar: :any,                 arm64_ventura:  "a12fe1d121afb52c9f4f5410f5583e74c9219159ccd0e3d46eb9227d342ddfbd"
-    sha256 cellar: :any,                 arm64_monterey: "88adc7d6f8b4fa311963849e19eea159f2bf6c3a0c8cede32747bd3228002f22"
-    sha256 cellar: :any,                 sonoma:         "2491c41820dd64410b86badc26f8beec6c26d935e82d048210ae13b2e405704c"
-    sha256 cellar: :any,                 ventura:        "864a99143acff3569e21ce28c9df2a496dc87619668fd81678e93c50c04cd5b5"
-    sha256 cellar: :any,                 monterey:       "1efcfd3804a055bd1c5336564ca37c237acc112c6228a3b8765097d80d104f76"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "46b46af58ae5a58229d6ddcb84e1d0cf4a4bc6816609dabf476c3f824231a36d"
+    sha256 cellar: :any,                 arm64_sonoma:   "78f18d003e4b8bde3d43a5fae95b3b8c04d065bab323aac7286beda3fc93c261"
+    sha256 cellar: :any,                 arm64_ventura:  "5130ed24dfd455e2c511414f9a0adbbd9ddf91567a3b5ce59923bf8206e19b4c"
+    sha256 cellar: :any,                 arm64_monterey: "6bfc95c6573e3568693e2d5a58f9f624c08b68ca136dc66d87c1a25277e9f831"
+    sha256 cellar: :any,                 sonoma:         "60c283760ad38a376b2ff0ba98775826fb40a76973842dd52236482df2c3ee35"
+    sha256 cellar: :any,                 ventura:        "8464dd0c68aa28aa3b4673d7467eaa6aa4acd582cbff2ae9a8b78c20fee09322"
+    sha256 cellar: :any,                 monterey:       "16b421921b495a64b1e783bd686e0de2027c82b5144bd1b5c4c058b39cade9e4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f672e44c00cf323535356d2fcad60bc080dc6b4affcf6c7b3ca7218139dbdea7"
   end
 
   depends_on "meson" => :build
