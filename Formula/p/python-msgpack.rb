955240d8bff47588a658abca3dc7caf24196c46d
Rui Chen <rui@chenrui.dev>
2023-11-22 13:36:54 -0500

    python-msgpack 1.0.7 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/p/python-msgpack.rb
@@ -0,0 +1,29 @@
+class PythonMsgpack < Formula
+  desc "MessagePack serializer implementation for Python"
+  homepage "https://msgpack.org/"
+  url "https://files.pythonhosted.org/packages/c2/d5/5662032db1571110b5b51647aed4b56dfbd01bfae789fa566a2be1f385d1/msgpack-1.0.7.tar.gz"
+  sha256 "572efc93db7a4d27e404501975ca6d2d9775705c2d922390d878fcf768d92c87"
+  license "Apache-2.0"
+
+  depends_on "python-setuptools" => :build
+  depends_on "python@3.11" => [:build, :test]
+  depends_on "python@3.12" => [:build, :test]
+
+  def pythons
+    deps.map(&:to_formula)
+        .select { |f| f.name.start_with?("python@") }
+        .map { |f| f.opt_libexec/"bin/python" }
+  end
+
+  def install
+    pythons.each do |python|
+      system python, "-m", "pip", "install", *std_pip_args, "."
+    end
+  end
+
+  test do
+    pythons.each do |python|
+      system python, "-c", "from msgpack import Packer,Unpacker"
+    end
+  end
+end
