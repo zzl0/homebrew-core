129fcd3d9172d10ae3eeced1687513bc50febee6
Rui Chen <rui@chenrui.dev>
2023-11-22 13:57:40 -0500

    cpplint: use `std_pip_args`
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/c/cpplint.rb
+++ Formula/c/cpplint.rb
@@ -1,6 +1,4 @@
 class Cpplint < Formula
-  include Language::Python::Virtualenv
-
   desc "Static code checker for C++"
   homepage "https://pypi.org/project/cpplint/"
   url "https://files.pythonhosted.org/packages/18/72/ea0f4035bcf35d8f8df053657d7f3370d56ff4d4e6617021b6544b9958d4/cpplint-1.6.1.tar.gz"
@@ -18,10 +16,15 @@ class Cpplint < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "85c26fd3ea019e2ba8c946b0d631fd913e780ae556744add5a7c7cf5edc6a7e8"
   end
 
+  depends_on "python-setuptools" => :build
   depends_on "python@3.12"
 
+  def python3
+    "python3.12"
+  end
+
   def install
-    virtualenv_install_with_resources
+    system python3, "-m", "pip", "install", *std_pip_args, "."
 
     # install test data
     pkgshare.install "samples"
@@ -31,7 +34,8 @@ class Cpplint < Formula
     output = shell_output("#{bin}/cpplint --version")
     assert_match "cpplint #{version}", output.strip
 
-    output = shell_output("#{bin}/cpplint #{pkgshare}/samples/v8-sample/src/interface-descriptors.h", 1)
+    test_file = pkgshare/"samples/v8-sample/src/interface-descriptors.h"
+    output = shell_output("#{bin}/cpplint #{test_file} 2>&1", 1)
     assert_match "Total errors found: 2", output
   end
 end
