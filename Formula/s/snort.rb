e395a765d7a4cd6483a6ac545814f71b51473c76
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-11-22 18:09:15 +0000

    snort: update 3.1.75.0 bottle.

--- Formula/s/snort.rb
+++ Formula/s/snort.rb
@@ -16,11 +16,11 @@ class Snort < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "caaeb592c46c762b2f1c5f5e80404f8b58fbbf3131ee2829a166ea6ef835f63b"
-    sha256 cellar: :any,                 arm64_monterey: "630fc011870aaa0e2bf9a543447890a2c2793879694a59b587ec682e9b57ba06"
-    sha256 cellar: :any,                 ventura:        "5ebf79f0aa6bc1ce36637a35746a8a208186e7c937649b8145ca5b513e278866"
-    sha256 cellar: :any,                 monterey:       "8d163fc73ed2e655a3d88c5a7ddea548c41914c905fe72b7efbac4029692f833"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d346c8dca54a3e9a58ecd223336bb3322d3de1c5a9af281c53a77cf8e25fe898"
+    sha256 cellar: :any,                 arm64_ventura:  "d14a03115761cf29dc57fb07bade8548badd6991c72fb7a505774221eb1055b5"
+    sha256 cellar: :any,                 arm64_monterey: "f8d44f17299c1b8ddae35f19412ebf80878d82b1580fcaf7f75e7d553baaabea"
+    sha256 cellar: :any,                 ventura:        "ab77e83843f9d2e83eda854e68d39ced5784c71a87ccc9eeb775d0fd088616d2"
+    sha256 cellar: :any,                 monterey:       "eca2357accb2e8abc49f18dd6faffac7074f73eb77ce3977cb48ef6fa5e0c4fc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f476aba56729c901acc81dd281e0513e893e7f1fa5b27dcdf2f813580d057fe3"
   end
 
   depends_on "cmake" => :build
