bfcd08ec4c839d39208126da8c0cfaefa3f6e7fa
Rui Chen <rui@chenrui.dev>
2023-11-22 18:12:04 -0500

    moarvm 2023.11
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/m/moarvm.rb
+++ Formula/m/moarvm.rb
@@ -1,8 +1,8 @@
 class Moarvm < Formula
   desc "VM with adaptive optimization and JIT compilation, built for Rakudo"
   homepage "https://moarvm.org"
-  url "https://github.com/MoarVM/MoarVM/releases/download/2023.10/MoarVM-2023.10.tar.gz"
-  sha256 "aa79d77896c312ba23e01074a53ef3d060becae2ed70b066f902f332da65a499"
+  url "https://github.com/MoarVM/MoarVM/releases/download/2023.11/MoarVM-2023.11.tar.gz"
+  sha256 "32f4b100dcdb7f4bbe8575fca0eea4b7b271f177c701138a8f58418352bdd103"
   license "Artistic-2.0"
 
   livecheck do
@@ -31,8 +31,8 @@ class Moarvm < Formula
   conflicts_with "rakudo-star", because: "rakudo-star currently ships with moarvm included"
 
   resource "nqp" do
-    url "https://github.com/Raku/nqp/releases/download/2023.10/nqp-2023.10.tar.gz"
-    sha256 "41051246635db1d4de08d373e2515ab76280f5e0c57d4a4fa2426545ac40956d"
+    url "https://github.com/Raku/nqp/releases/download/2023.11/nqp-2023.11.tar.gz"
+    sha256 "e7176b1a6fbaa98c132e385f325c6211ff9f93c0a3f0a23ceb6ffe823747b297"
   end
 
   def install
