0a50988e998375333d6afdd7e49bbf9973ae7dbc
Rui Chen <rui@chenrui.dev>
2023-11-22 11:06:34 -0500

    snort 3.1.75.0

--- Formula/s/snort.rb
+++ Formula/s/snort.rb
@@ -1,9 +1,9 @@
 class Snort < Formula
   desc "Flexible Network Intrusion Detection System"
   homepage "https://www.snort.org"
-  url "https://github.com/snort3/snort3/archive/refs/tags/3.1.74.0.tar.gz"
-  mirror "https://fossies.org/linux/misc/snort3-3.1.74.0.tar.gz"
-  sha256 "4a4529e74bc202303c0330ae8b2317f0bef3ac92ae7216df8cfedfce24ddd129"
+  url "https://github.com/snort3/snort3/archive/refs/tags/3.1.75.0.tar.gz"
+  mirror "https://fossies.org/linux/misc/snort3-3.1.75.0.tar.gz"
+  sha256 "c1a1b7d00df5ab45df968f0fb0125eba95bad27c181018b8d68a41e1bb6fc111"
   license "GPL-2.0-only"
   head "https://github.com/snort3/snort3.git", branch: "master"
 
