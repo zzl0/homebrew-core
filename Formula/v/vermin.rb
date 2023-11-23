1583d94103e8a7bd5d99df0b54a3b4283ef57cf8
Rui Chen <rui@chenrui.dev>
2023-11-23 00:19:52 -0500

    vermin: use `std_pip_args`
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/v/vermin.rb
+++ Formula/v/vermin.rb
@@ -1,8 +1,7 @@
 class Vermin < Formula
-  include Language::Python::Virtualenv
-
   desc "Concurrently detect the minimum Python versions needed to run code"
   homepage "https://github.com/netromdk/vermin"
+  # pypi sdist issue, https://github.com/netromdk/vermin/issues/243
   url "https://github.com/netromdk/vermin/archive/refs/tags/v1.5.2.tar.gz"
   sha256 "e4b6ca6f3e71b0d83a179dc4a4ba50682f60474cf8c948ba9f82e330f219ff4a"
   license "MIT"
@@ -19,14 +18,23 @@ class Vermin < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "9eee3a7b11728ea390cccf0afc1df7e34a47bdad40c8640a6d6c091b67f05b97"
   end
 
+  depends_on "python-setuptools" => :build
   depends_on "python@3.12"
 
+  def python3
+    "python3.12"
+  end
+
   def install
-    virtualenv_install_with_resources
+    system python3, "-m", "pip", "install", *std_pip_args, "."
   end
 
   test do
-    path = libexec/Language::Python.site_packages("python3.12")/"vermin"
-    assert_match "Minimum required versions: 2.7, 3.0", shell_output("#{bin}/vermin #{path}")
+    assert_equal <<~EOS, shell_output("#{bin}/vermin --no-parse-comments #{bin}/vermin")
+      Minimum required versions: ~2, ~3
+      Note: Not enough evidence to conclude it won't work with Python 2 or 3.
+    EOS
+
+    assert_match version.to_s, shell_output("#{bin}/vermin --version")
   end
 end
