a5a1c442d46a854e4ee40aab5a0dc03bf132f1fb
Porkepix <porkepix@gmail.com>
2023-11-23 16:32:00 +0100

    hut 0.4.0

--- Formula/h/hut.rb
+++ Formula/h/hut.rb
@@ -1,8 +1,8 @@
 class Hut < Formula
   desc "CLI tool for sr.ht"
   homepage "https://sr.ht/~emersion/hut"
-  url "https://git.sr.ht/~emersion/hut/archive/v0.3.0.tar.gz"
-  sha256 "ca191d663be81000c8ac0e952cd1b95fbded8c1d918d6d89ff08adbcd3d75289"
+  url "https://git.sr.ht/~emersion/hut/archive/v0.4.0.tar.gz"
+  sha256 "f25ab4452e4622f404a6fa5713e8505302bfcee4dd3a8dfe76f1fc4c05688c09"
   license "AGPL-3.0-or-later"
   head "https://git.sr.ht/~emersion/hut", branch: "master"
 
