c72e53bf8b9b12e7b98035668ea32c8d3c94013c
Nhan Doan <2561973+dnhn@users.noreply.github.com>
2023-11-23 13:57:43 +0700

    libphonenumber 8.13.26

--- Formula/lib/libphonenumber.rb
+++ Formula/lib/libphonenumber.rb
@@ -1,10 +1,9 @@
 class Libphonenumber < Formula
   desc "C++ Phone Number library by Google"
   homepage "https://github.com/google/libphonenumber"
-  url "https://github.com/google/libphonenumber/archive/refs/tags/v8.13.25.tar.gz"
-  sha256 "229e16404dec98a9b09a368fb7b09e0916b2802e6b9f6428cb0278472245d537"
+  url "https://github.com/google/libphonenumber/archive/refs/tags/v8.13.26.tar.gz"
+  sha256 "a7df5f274dafa0b590524560fba72f561284a224344272fa8b2101677a7aa317"
   license "Apache-2.0"
-  revision 1
 
   livecheck do
     url :stable
