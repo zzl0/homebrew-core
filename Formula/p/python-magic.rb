f3d23117f232311c34fa42dd087ba8e9e7f26f5e
Rui Chen <rui@chenrui.dev>
2023-11-22 19:26:35 -0500

    python-magic 0.4.27 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    python-magic: fix test
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/p/python-magic.rb
@@ -0,0 +1,31 @@
+class PythonMagic < Formula
+  desc "Python wrapper for libmagic"
+  homepage "https://github.com/ahupp/python-magic"
+  url "https://files.pythonhosted.org/packages/da/db/0b3e28ac047452d079d375ec6798bf76a036a08182dbb39ed38116a49130/python-magic-0.4.27.tar.gz"
+  sha256 "c1ba14b08e4a5f5c31a302b7721239695b2f0f058d125bd5ce1ee36b9d9d3c3b"
+  license all_of: ["BSD-2-Clause", "MIT"]
+
+  depends_on "python-setuptools" => :build
+  depends_on "python@3.11" => [:build, :test]
+  depends_on "python@3.12" => [:build, :test]
+  depends_on "libmagic"
+
+  def pythons
+    deps.map(&:to_formula)
+        .select { |f| f.name.match?(/^python@\d\.\d+$/) }
+        .sort_by(&:version)
+        .map { |f| f.opt_libexec/"bin/python" }
+  end
+
+  def install
+    pythons.each do |python|
+      system python, "-m", "pip", "install", *std_pip_args, "."
+    end
+  end
+
+  test do
+    pythons.each do |python|
+      system python, "-c", "from magic import Magic"
+    end
+  end
+end
