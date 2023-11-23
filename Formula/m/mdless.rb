eb2db46eb48275a7352d114e0a1c79528661fdd3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-11-23 04:59:53 +0000

    mdless: update 2.0.4 bottle.

--- Formula/m/mdless.rb
+++ Formula/m/mdless.rb
@@ -6,15 +6,13 @@ class Mdless < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "a5e20dc35f8c5fac99066e982b12842e50e66859c539c020a2c05b49e13dff5a"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a5e20dc35f8c5fac99066e982b12842e50e66859c539c020a2c05b49e13dff5a"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a5e20dc35f8c5fac99066e982b12842e50e66859c539c020a2c05b49e13dff5a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a5e20dc35f8c5fac99066e982b12842e50e66859c539c020a2c05b49e13dff5a"
-    sha256 cellar: :any_skip_relocation, sonoma:         "a5e20dc35f8c5fac99066e982b12842e50e66859c539c020a2c05b49e13dff5a"
-    sha256 cellar: :any_skip_relocation, ventura:        "a5e20dc35f8c5fac99066e982b12842e50e66859c539c020a2c05b49e13dff5a"
-    sha256 cellar: :any_skip_relocation, monterey:       "a5e20dc35f8c5fac99066e982b12842e50e66859c539c020a2c05b49e13dff5a"
-    sha256 cellar: :any_skip_relocation, big_sur:        "a5e20dc35f8c5fac99066e982b12842e50e66859c539c020a2c05b49e13dff5a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "74689bfcf5f62937fffa69da30455f66ac2147ce80118f5aa17acdba93d10d0e"
+    sha256                               arm64_sonoma:   "a60558ca7cc9c049c63fef8800a4230e6bf19f1a8d3889054f9e4116a82818e0"
+    sha256                               arm64_ventura:  "82d9d5db1594b4f95525e45ffb1eb00e4aa04f46687584e81ec3a236e42c7503"
+    sha256                               arm64_monterey: "dbba37dea09438bee1877bf45ec04e544afef8a69dbde17c0f45e3e30b14d026"
+    sha256                               sonoma:         "156155f484b307161da955c9bda3404f14143175b915925686196d498c059efd"
+    sha256                               ventura:        "72f64d616cfbfe003d0a09fc66dccfa79db508b875a6b937c1941e323e2b0725"
+    sha256                               monterey:       "b18a56dfcf4cd3454a6383921ae4ac927fd7beb41df70d2ccafae4cb7b0d7308"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "79bacc5aae547894c5a2c6ed853cbff8468185988441c341a8bfc8d20d8c612b"
   end
 
   uses_from_macos "ruby"
