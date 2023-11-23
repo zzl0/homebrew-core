706e2c5024f0a898178ab3e4b0e14f1625ba015b
Michka Popoff <michkapopoff@gmail.com>
2023-11-23 23:05:45 +0100

    mp3fs: bump revision
    Missed in #152027

--- Formula/m/mp3fs.rb
+++ Formula/m/mp3fs.rb
@@ -4,7 +4,7 @@ class Mp3fs < Formula
   url "https://github.com/khenriks/mp3fs/releases/download/v1.1.1/mp3fs-1.1.1.tar.gz"
   sha256 "942b588fb623ea58ce8cac8844e6ff2829ad4bc9b4c163bba58e3fa9ebc15608"
   license "GPL-3.0-or-later"
-  revision 3
+  revision 4
 
   bottle do
     sha256 cellar: :any_skip_relocation, x86_64_linux: "3539466612aa0f46175ab58a559978aa14b92ba973b1d3819649aa8474537885"
