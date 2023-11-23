d709c47e646991dcdbe3a5f1ef025409d86f3c45
Raymond Tau <raymond.tau@RaymonddeMBP.lan>
2023-11-23 23:47:53 +0800

    memray 1.10.0 (new formula)
    memray: update build and test
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    memray: needs lz4
    
    ```
        src/memray/_memray/lz4_stream.h:35:10: fatal error: 'lz4frame.h' file not found
        #include <lz4frame.h>
                 ^~~~~~~~~~~~
        1 error generated.
    ```
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    memray: needs `libunwind` for linux build
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: Raymond Tau <raymond.tau@RaymonddeMBP.lan>

---
+++ Formula/m/memray.rb
@@ -0,0 +1,45 @@
+class Memray < Formula
+  include Language::Python::Virtualenv
+
+  desc "Memory profiler for Python applications"
+  homepage "https://bloomberg.github.io/memray/"
+  url "https://files.pythonhosted.org/packages/d4/28/41748b83b7db03d9e3d25c19e48851af62505ba88100d1e25fc3b7d36027/memray-1.10.0.tar.gz"
+  sha256 "38322e052b882790993412f1840517a51818aa55c47037f69915b2007f2c4cee"
+  license "Apache-2.0"
+
+  depends_on "lz4"
+  depends_on "pygments"
+  depends_on "python-jinja"
+  depends_on "python-markupsafe"
+  depends_on "python@3.12"
+
+  on_linux do
+    depends_on "libunwind"
+  end
+
+  resource "markdown-it-py" do
+    url "https://files.pythonhosted.org/packages/38/71/3b932df36c1a044d397a1f92d1cf91ee0a503d91e470cbd670aa66b07ed0/markdown-it-py-3.0.0.tar.gz"
+    sha256 "e3f60a94fa066dc52ec76661e37c851cb232d92f9886b15cb560aaada2df8feb"
+  end
+
+  resource "mdurl" do
+    url "https://files.pythonhosted.org/packages/d6/54/cfe61301667036ec958cb99bd3efefba235e65cdeb9c84d24a8293ba1d90/mdurl-0.1.2.tar.gz"
+    sha256 "bb413d29f5eea38f31dd4754dd7377d4465116fb207585f97bf925588687c1ba"
+  end
+
+  resource "rich" do
+    url "https://files.pythonhosted.org/packages/a7/ec/4a7d80728bd429f7c0d4d51245287158a1516315cadbb146012439403a9d/rich-13.7.0.tar.gz"
+    sha256 "5cb5123b5cf9ee70584244246816e9114227e0b98ad9176eede6ad54bf5403fa"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    system "#{bin}/memray", "run", "--output", "output.bin", "-c", "print()"
+    assert_predicate testpath/"output.bin", :exist?
+
+    assert_match version.to_s, shell_output("#{bin}/memray --version")
+  end
+end
