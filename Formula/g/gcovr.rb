a7549bfea0ee07ab63afdb4be4327963e0f61990
Rui Chen <rui@chenrui.dev>
2023-11-23 17:31:39 -0500

    gcovr: use `std_pip_args`
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/g/gcovr.rb
+++ Formula/g/gcovr.rb
@@ -1,6 +1,4 @@
 class Gcovr < Formula
-  include Language::Python::Virtualenv
-
   desc "Reports from gcov test coverage program"
   homepage "https://gcovr.com/"
   url "https://files.pythonhosted.org/packages/19/6d/2942ab8c693f2b9f97052d6a6de4c27323a3bd85af7d062dc5bd3a2a9604/gcovr-6.0.tar.gz"
@@ -19,18 +17,19 @@ class Gcovr < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "541a7704b5fd30a5d311846d71c39a188278bba4be4fcb5455dc2bd953af0dd4"
   end
 
+  depends_on "python-setuptools" => :build
   depends_on "pygments"
+  depends_on "python-jinja"
   depends_on "python-lxml"
   depends_on "python-markupsafe"
   depends_on "python@3.12"
 
-  resource "Jinja2" do
-    url "https://files.pythonhosted.org/packages/7a/ff/75c28576a1d900e87eb6335b063fab47a8ef3c8b4d88524c4bf78f670cce/Jinja2-3.1.2.tar.gz"
-    sha256 "31351a702a408a9e7595a8fc6150fc3f43bb6bf7e319770cbc0db9df9437e852"
+  def python3
+    "python3.12"
   end
 
   def install
-    virtualenv_install_with_resources
+    system python3, "-m", "pip", "install", *std_pip_args, "."
   end
 
   test do
