5f57685b821367f7fa8c0d28ddfcb9f3213d4066
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-11-23 14:32:31 +0000

    pipgrip: update 0.10.10 bottle.

--- Formula/p/pipgrip.rb
+++ Formula/p/pipgrip.rb
@@ -8,13 +8,13 @@ class Pipgrip < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "55addb6f5484988dd0a7ff77b96dd0919db102382e831faf8960d464d202338b"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "2fdf73145e562ddb95ecf2de9a29a23ea96cb10fead10656d57aa4e6aad1aa2c"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "dd0dccc7d8836030c0d054c2e7cd7958c8797ee2c2496dd2bd17caba7363d4d7"
-    sha256 cellar: :any_skip_relocation, sonoma:         "5c272e0bd604b2fa9c1e09bf2318ecba8996882919cd70a6e59ab866737bf47e"
-    sha256 cellar: :any_skip_relocation, ventura:        "93ed28420b733ea27f307879f5468d9a4612200bf011b6169c5f28c26220bc12"
-    sha256 cellar: :any_skip_relocation, monterey:       "13dd37fa4da8b95b0b2f374b91a647b5afd44aee8c8ef2b059bc6c3ad3dc7017"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a5ed675110380eff721572de63fa7be2a4e87e5e26fe9b95d07d5b8fd98e1c01"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "a90cca68643dd574d05ac12ad8bab32fe35e44ea2ee7f03b6501980b5b4d948f"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0805cdeab867487cb687c41111b47e15bdbe06e22d1165e22fb1095e8658e133"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "53f76d2b8261aeaaaedef651933ec837caca270ebe99ec9d8cff6b6f19b0adde"
+    sha256 cellar: :any_skip_relocation, sonoma:         "8a68902f3dd106a133650d517656d8e9baee6ba6eb42920b3dccdbbf866b43d5"
+    sha256 cellar: :any_skip_relocation, ventura:        "6f3c075ef1cc11c51d009aea52705e242798b5b8bf051b01dcfce3882bdcc5c0"
+    sha256 cellar: :any_skip_relocation, monterey:       "6cb489486187db1dfd3b8d2300922abbc26dec786d01a0b5c6eff1a0327a9ac6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a4d440684f8a867e112f6f164397cb217b637b822946b4f616091604b63bc9a8"
   end
 
   depends_on "python-click"
