d82165fb02c14a88db0e34ddb60fe2d35fee388c
Patrick Linnane <patrick@linnane.io>
2023-11-23 14:56:22 -0800

    jackett 0.21.1250

--- Formula/j/jackett.rb
+++ Formula/j/jackett.rb
@@ -1,8 +1,8 @@
 class Jackett < Formula
   desc "API Support for your favorite torrent trackers"
   homepage "https://github.com/Jackett/Jackett"
-  url "https://github.com/Jackett/Jackett/archive/refs/tags/v0.21.1243.tar.gz"
-  sha256 "71e23cf9e1d5a188a125ccb67c6e57cd62d4523ffb938852b7023d9acca0cb8b"
+  url "https://github.com/Jackett/Jackett/archive/refs/tags/v0.21.1250.tar.gz"
+  sha256 "1f8869627788d95b4f3464f30c2992231ea998289ee918892473fc80e303aef6"
   license "GPL-2.0-only"
   head "https://github.com/Jackett/Jackett.git", branch: "master"
 
