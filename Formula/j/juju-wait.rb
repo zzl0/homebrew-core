4e7dec887191ccf00fae2a7634018a3c1fd50ddb
Rui Chen <rui@chenrui.dev>
2023-11-23 15:15:12 -0500

    juju-wait: use `std_pip_args`
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/j/juju-wait.rb
+++ Formula/j/juju-wait.rb
@@ -1,6 +1,4 @@
 class JujuWait < Formula
-  include Language::Python::Virtualenv
-
   desc "Juju plugin for waiting for deployments to settle"
   homepage "https://launchpad.net/juju-wait"
   url "https://files.pythonhosted.org/packages/0c/2b/f4bd0138f941e4ba321298663de3f1c8d9368b75671b17aa1b8d41a154dc/juju-wait-2.8.4.tar.gz"
@@ -24,8 +22,12 @@ class JujuWait < Formula
   depends_on "python@3.12"
   depends_on "pyyaml"
 
+  def python3
+    "python3.12"
+  end
+
   def install
-    virtualenv_install_with_resources
+    system python3, "-m", "pip", "install", *std_pip_args, "."
   end
 
   test do
