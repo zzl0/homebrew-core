b36ab53d6164a1f86c2fb32b3e8b00a292de5566
Rui Chen <rui@chenrui.dev>
2023-11-24 15:39:03 -0500

    aiven-client: use `std_pip_args`
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/a/aiven-client.rb
+++ Formula/a/aiven-client.rb
@@ -1,6 +1,4 @@
 class AivenClient < Formula
-  include Language::Python::Virtualenv
-
   desc "Official command-line client for Aiven"
   homepage "https://docs.aiven.io/docs/tools/cli"
   url "https://files.pythonhosted.org/packages/b3/dc/869bcceb3e6f33ebd8e7518fb70e522af975e7f3d78eda23642f640c393c/aiven_client-4.0.0.tar.gz"
@@ -18,31 +16,19 @@ class AivenClient < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "d57dcba0965c44a2bb7687c686234ff1d91c10d2817e8d5e7fe747fcd4d438c2"
   end
 
-  depends_on "python-certifi"
+  depends_on "python-hatch-vcs" => :build
+  depends_on "python-hatchling" => :build
+  depends_on "python-setuptools" => :build
+  depends_on "python-setuptools-scm" => :build
+  depends_on "python-requests"
   depends_on "python@3.12"
 
-  resource "charset-normalizer" do
-    url "https://files.pythonhosted.org/packages/63/09/c1bc53dab74b1816a00d8d030de5bf98f724c52c1635e07681d312f20be8/charset-normalizer-3.3.2.tar.gz"
-    sha256 "f30c3cb33b24454a82faecaf01b19c18562b1e89558fb6c56de4d9118a032fd5"
-  end
-
-  resource "idna" do
-    url "https://files.pythonhosted.org/packages/8b/e1/43beb3d38dba6cb420cefa297822eac205a277ab43e5ba5d5c46faf96438/idna-3.4.tar.gz"
-    sha256 "814f528e8dead7d329833b91c5faa87d60bf71824cd12a7530b5526063d02cb4"
-  end
-
-  resource "requests" do
-    url "https://files.pythonhosted.org/packages/9d/be/10918a2eac4ae9f02f6cfe6414b7a155ccd8f7f9d4380d62fd5b955065c3/requests-2.31.0.tar.gz"
-    sha256 "942c5a758f98d790eaed1a29cb6eefc7ffb0d1cf7af05c3d2791656dbd6ad1e1"
-  end
-
-  resource "urllib3" do
-    url "https://files.pythonhosted.org/packages/af/47/b215df9f71b4fdba1025fc05a77db2ad243fa0926755a52c5e71659f4e3c/urllib3-2.0.7.tar.gz"
-    sha256 "c97dfde1f7bd43a71c8d2a58e369e9b2bf692d1334ea9f9cae55add7d0dd0f84"
+  def python3
+    "python3.12"
   end
 
   def install
-    virtualenv_install_with_resources
+    system python3, "-m", "pip", "install", *std_pip_args, "."
   end
 
   test do
