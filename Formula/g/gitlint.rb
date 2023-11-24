b69adada3deb3ad50951d3a4d3cf8f5ccdb4f18d
David Baumgold <david@davidbaumgold.com>
2023-11-24 14:56:31 +0100

    gitlint: depend on python-click, python-dateutil

--- Formula/g/gitlint.rb
+++ Formula/g/gitlint.rb
@@ -18,22 +18,14 @@ class Gitlint < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "7337c2a46eec782bcda2cb48ae2d1419bf14da25e6c5b32af324ad22a38f75b6"
   end
 
+  depends_on "python-click"
+  depends_on "python-dateutil"
   depends_on "python@3.12"
   depends_on "six"
 
   resource "arrow" do
     url "https://files.pythonhosted.org/packages/2e/00/0f6e8fcdb23ea632c866620cc872729ff43ed91d284c866b515c6342b173/arrow-1.3.0.tar.gz"
     sha256 "d4540617648cb5f895730f1ad8c82a65f2dad0166f57b75f3ca54759c4d67a85"
-  end
-
-  resource "click" do
-    url "https://files.pythonhosted.org/packages/96/d3/f04c7bfcf5c1862a2a5b845c6b2b360488cf47af55dfa79c98f6a6bf98b5/click-8.1.7.tar.gz"
-    sha256 "ca9853ad459e787e2192211578cc907e7594e294c7ccc834310722b41b9ca6de"
-  end
-
-  resource "python-dateutil" do
-    url "https://files.pythonhosted.org/packages/4c/c4/13b4776ea2d76c115c1d1b84579f3764ee6d57204f6be27119f13a61d0a9/python-dateutil-2.8.2.tar.gz"
-    sha256 "0123cacc1627ae19ddf3c27a5de5bd67ee4586fbdd6440d9748f8abb483d3e86"
   end
 
   resource "sh" do
