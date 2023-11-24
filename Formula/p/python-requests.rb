3ca5c8c2815079e8a69d0890b56136abfb61ce92
Rui Chen <rui@chenrui.dev>
2023-11-24 00:38:30 -0500

    python-requests 2.31.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    pypi_formula_mappings: updates for python-requests
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/p/python-requests.rb
@@ -0,0 +1,33 @@
+class PythonRequests < Formula
+  desc "Python HTTP for Humans"
+  homepage "https://requests.readthedocs.io/en/latest/"
+  url "https://files.pythonhosted.org/packages/9d/be/10918a2eac4ae9f02f6cfe6414b7a155ccd8f7f9d4380d62fd5b955065c3/requests-2.31.0.tar.gz"
+  sha256 "942c5a758f98d790eaed1a29cb6eefc7ffb0d1cf7af05c3d2791656dbd6ad1e1"
+  license "Apache-2.0"
+
+  depends_on "python-setuptools" => :build
+  depends_on "python@3.11" => [:build, :test]
+  depends_on "python@3.12" => [:build, :test]
+  depends_on "python-certifi"
+  depends_on "python-charset-normalizer"
+  depends_on "python-idna"
+  depends_on "python-urllib3"
+
+  def pythons
+    deps.map(&:to_formula).sort_by(&:version).filter { |f| f.name.start_with?("python@") }
+  end
+
+  def install
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-m", "pip", "install", *std_pip_args, "."
+    end
+  end
+
+  test do
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-c", "import requests"
+    end
+  end
+end
