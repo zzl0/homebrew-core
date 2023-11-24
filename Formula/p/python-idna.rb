1a9a372a987911b27acb9ae09a70b77f81edf3d6
Patrick Linnane <patrick@linnane.io>
2023-11-24 04:16:00 +0000

    python-idna: add 3.4 bottle.

--- Formula/p/python-idna.rb
+++ Formula/p/python-idna.rb
@@ -5,6 +5,16 @@ class PythonIdna < Formula
   sha256 "814f528e8dead7d329833b91c5faa87d60bf71824cd12a7530b5526063d02cb4"
   license "BSD-3-Clause"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "a8539307f91bfcb96c0bd44501a6b1479b3572b5f6c8d9b660a4efe5a129b6ab"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8112da98633ad2b2a49e1760aea71633c480807a86cca32c8d5d75e22af8fd38"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a048755f0b098b2a5fbc98ac9d9126813b8d2711c3ba4ff981006d8659b84611"
+    sha256 cellar: :any_skip_relocation, sonoma:         "b30d80debc84a704620694f927d87cbb8372c2a1ee57a2fb6a8c2b3c49de3d64"
+    sha256 cellar: :any_skip_relocation, ventura:        "b12e35431360154d03a87b63096191f1e3c85db1dcdec9535b19945eb8c4f87a"
+    sha256 cellar: :any_skip_relocation, monterey:       "aaa2e7b70adee0e3f66ecc37d1008329dc3c337bf6276099c59d185f1bef43ee"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "36e286da3fcdd019a670c45881b41541013f769e71a811874ffbf3eb302a2121"
+  end
+
   depends_on "python-flit-core" => :build
   depends_on "python@3.11" => [:build, :test]
   depends_on "python@3.12" => [:build, :test]
