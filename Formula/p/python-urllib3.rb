96eda40e3e4e0ed66e93ce284cd130f49fed675d
Rui Chen <rui@chenrui.dev>
2023-11-23 20:47:32 -0500

    python-urllib3 2.1.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/p/python-urllib3.rb
@@ -0,0 +1,29 @@
+class PythonUrllib3 < Formula
+  desc "HTTP library with thread-safe connection pooling, file post, and more"
+  homepage "https://urllib3.readthedocs.io/en/stable/"
+  url "https://files.pythonhosted.org/packages/36/dd/a6b232f449e1bc71802a5b7950dc3675d32c6dbc2a1bd6d71f065551adb6/urllib3-2.1.0.tar.gz"
+  sha256 "df7aa8afb0148fa78488e7899b2c59b5f4ffcfa82e6c54ccb9dd37c1d7b52d54"
+  license "MIT"
+
+  depends_on "python-hatchling" => :build
+  depends_on "python@3.11" => [:build, :test]
+  depends_on "python@3.12" => [:build, :test]
+
+  def pythons
+    deps.map(&:to_formula).sort_by(&:version).filter { |f| f.name.start_with?("python@") }
+  end
+
+  def install
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-m", "pip", "install", *std_pip_args, "."
+    end
+  end
+
+  test do
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-c", "import urllib3"
+    end
+  end
+end
