22a2deb27d5c1c3088854498384d60b6682fa090
Patrick Linnane <patrick@linnane.io>
2023-11-23 21:03:34 -0800

    rapidfuzz-cpp 2.2.3 (new formula)

---
+++ Formula/r/rapidfuzz-cpp.rb
@@ -0,0 +1,33 @@
+class RapidfuzzCpp < Formula
+  desc "Rapid fuzzy string matching in C++ using the Levenshtein Distance"
+  homepage "https://github.com/maxbachmann/rapidfuzz-cpp"
+  url "https://github.com/maxbachmann/rapidfuzz-cpp/archive/refs/tags/v2.2.3.tar.gz"
+  sha256 "df4412e9593945782de2212095bd4b70a8f8e63ae8f313976c616809be124d2c"
+  license "MIT"
+  head "https://github.com/maxbachmann/rapidfuzz-cpp.git", branch: "main"
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <rapidfuzz/fuzz.hpp>
+      #include <string>
+      #include <iostream>
+
+      int main()
+      {
+          std::string a = "aaaa";
+          std::string b = "abab";
+          std::cout << rapidfuzz::fuzz::ratio(a, b) << std::endl;
+      }
+    EOS
+    system ENV.cxx, "test.cpp", "-std=c++17", "-I#{include}", "-o", "test"
+    assert_equal "50", shell_output("./test").strip
+  end
+end
