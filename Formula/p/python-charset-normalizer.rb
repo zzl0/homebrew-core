3c38ac74b83752ae0503a5ec879d08f33a56fd06
Rui Chen <rui@chenrui.dev>
2023-11-23 20:50:12 -0500

    python-charset-normalizer 3.3.2 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/p/python-charset-normalizer.rb
@@ -0,0 +1,29 @@
+class PythonCharsetNormalizer < Formula
+  desc "Real First Universal Charset Detector, maintained alternative to Chardet"
+  homepage "https://charset-normalizer.readthedocs.io/en/latest/"
+  url "https://files.pythonhosted.org/packages/63/09/c1bc53dab74b1816a00d8d030de5bf98f724c52c1635e07681d312f20be8/charset-normalizer-3.3.2.tar.gz"
+  sha256 "f30c3cb33b24454a82faecaf01b19c18562b1e89558fb6c56de4d9118a032fd5"
+  license "MIT"
+
+  depends_on "python-setuptools" => :build
+  depends_on "python@3.11" => [:build, :test]
+  depends_on "python@3.12" => [:build, :test]
+
+  def pythons
+    deps.map(&:to_formula).sort_by(&:version).filter { |f| f.name.start_with?("python@") }
+  end
+
+  def install
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-m", "pip", "install", *std_pip_args, "."
+    end
+  end
+
+  test do
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-c", "import charset_normalizer"
+    end
+  end
+end
