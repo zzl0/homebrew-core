459d114ef0df95e11cf332d9731d0e6cd538fe8a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-11-25 16:53:07 +0000

    gitlab-ci-local: update 4.45.2 bottle.

--- Formula/g/gitlab-ci-local.rb
+++ Formula/g/gitlab-ci-local.rb
@@ -9,13 +9,13 @@ class GitlabCiLocal < Formula
   head "https://github.com/firecow/gitlab-ci-local.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "fadfdfff789ce19b36b75980da6c22b51f2bda421137d0b86aabaf8473e0fd4f"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "fadfdfff789ce19b36b75980da6c22b51f2bda421137d0b86aabaf8473e0fd4f"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "fadfdfff789ce19b36b75980da6c22b51f2bda421137d0b86aabaf8473e0fd4f"
-    sha256 cellar: :any_skip_relocation, sonoma:         "8e38a04f5baa6bbbbedd186045586403a2bf73ceebd84a123097ecf789561410"
-    sha256 cellar: :any_skip_relocation, ventura:        "8e38a04f5baa6bbbbedd186045586403a2bf73ceebd84a123097ecf789561410"
-    sha256 cellar: :any_skip_relocation, monterey:       "8e38a04f5baa6bbbbedd186045586403a2bf73ceebd84a123097ecf789561410"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fadfdfff789ce19b36b75980da6c22b51f2bda421137d0b86aabaf8473e0fd4f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "6db9ba335c4a64245ee21cf9251c21ac54fc4b57b643d58026c33fb31609481d"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "6db9ba335c4a64245ee21cf9251c21ac54fc4b57b643d58026c33fb31609481d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "6db9ba335c4a64245ee21cf9251c21ac54fc4b57b643d58026c33fb31609481d"
+    sha256 cellar: :any_skip_relocation, sonoma:         "320061ab166f172b5adce3b7e119b6eb45da4578daccca967a2a2db52b47405f"
+    sha256 cellar: :any_skip_relocation, ventura:        "320061ab166f172b5adce3b7e119b6eb45da4578daccca967a2a2db52b47405f"
+    sha256 cellar: :any_skip_relocation, monterey:       "320061ab166f172b5adce3b7e119b6eb45da4578daccca967a2a2db52b47405f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6db9ba335c4a64245ee21cf9251c21ac54fc4b57b643d58026c33fb31609481d"
   end
 
   depends_on "node"
