0448e11a7baa43cfa2d5f8794b3f4dd5c52fd8ce
Rui Chen <rui@chenrui.dev>
2023-11-25 17:58:22 -0500

    virtualenv: use `std_pip_args`
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    virtualenv: add python-hatch-vcs/python-hatchling/python-setuptools-scm build deps
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/v/virtualenv.rb
+++ Formula/v/virtualenv.rb
@@ -1,6 +1,4 @@
 class Virtualenv < Formula
-  include Language::Python::Virtualenv
-
   desc "Tool for creating isolated virtual python environments"
   homepage "https://virtualenv.pypa.io/"
   url "https://files.pythonhosted.org/packages/ac/fe/028d5b32d0a54fe3ecac1c170966757f185f84b81e06af98c820a546c691/virtualenv-20.24.7.tar.gz"
@@ -18,25 +16,21 @@ class Virtualenv < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "6c1d652f899cfc1b543365994c0c7c884e86870796e480784ead3deaaee57dc8"
   end
 
+  depends_on "python-hatch-vcs" => :build
+  depends_on "python-hatchling" => :build
+  depends_on "python-setuptools" => :build
+  depends_on "python-setuptools-scm" => :build
+  depends_on "python-distlib"
+  depends_on "python-filelock"
+  depends_on "python-platformdirs"
   depends_on "python@3.12"
 
-  resource "distlib" do
-    url "https://files.pythonhosted.org/packages/29/34/63be59bdf57b3a8a8dcc252ef45c40f3c018777dc8843d45dd9b869868f0/distlib-0.3.7.tar.gz"
-    sha256 "9dafe54b34a028eafd95039d5e5d4851a13734540f1331060d31c9916e7147a8"
-  end
-
-  resource "filelock" do
-    url "https://files.pythonhosted.org/packages/70/70/41905c80dcfe71b22fb06827b8eae65781783d4a14194bce79d16a013263/filelock-3.13.1.tar.gz"
-    sha256 "521f5f56c50f8426f5e03ad3b281b490a87ef15bc6c526f168290f0c7148d44e"
-  end
-
-  resource "platformdirs" do
-    url "https://files.pythonhosted.org/packages/31/28/e40d24d2e2eb23135f8533ad33d582359c7825623b1e022f9d460def7c05/platformdirs-4.0.0.tar.gz"
-    sha256 "cb633b2bcf10c51af60beb0ab06d2f1d69064b43abf4c185ca6b28865f3f9731"
+  def python3
+    "python3.12"
   end
 
   def install
-    virtualenv_install_with_resources
+    system python3, "-m", "pip", "install", *std_pip_args, "."
   end
 
   test do
