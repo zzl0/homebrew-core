0361932539c6731cbf6c5d3052b0899ef390539b
Patrick Linnane <patrick@linnane.io>
2023-11-25 01:22:23 +0000

    python-hatch-vcs: add 0.4.0 bottle.

--- Formula/p/python-hatch-vcs.rb
+++ Formula/p/python-hatch-vcs.rb
@@ -5,6 +5,16 @@ class PythonHatchVcs < Formula
   sha256 "093810748fe01db0d451fabcf2c1ac2688caefd232d4ede967090b1c1b07d9f7"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "6d7c17572aa90c961a51d32f01c9ca96229a29a219e2d124220728ae8950f4c5"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "cb2225aeb7d0652ea78b055eebcee949a40fbd532cc58e9424a91b9cfe09eb0a"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c3e773ace2c22ce7ef503fab58602ca8da4a5ea1c6d391b93718c12f5d874836"
+    sha256 cellar: :any_skip_relocation, sonoma:         "4c2f475632067ec487a15c7a003678c51e971e2ed1741e0825ae71b0a1873653"
+    sha256 cellar: :any_skip_relocation, ventura:        "8d38d1c2c46ebba8f8a6f7c5fc4076c55725ca4078406fd6fab28f12953e46e0"
+    sha256 cellar: :any_skip_relocation, monterey:       "c809a6ef08cfedb1c74fb80db6d1374a5977ac4c448d1697d2ac1efb9f6784ab"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8ad459eac02f22df06d581c4ace6f4748379cd4d2ac0cdfa2beeb0cfeee5be3f"
+  end
+
   depends_on "python-hatchling" => :build
   depends_on "python@3.11" => [:build, :test]
   depends_on "python@3.12" => [:build, :test]
