736b8ec0a0be3cf779fe8dbf996a40bb93ea572a
Rui Chen <rui@chenrui.dev>
2023-11-25 18:47:07 -0500

    python-configobj 5.0.8 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    python-configobj: depends_on six
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/p/python-configobj.rb
@@ -0,0 +1,30 @@
+class PythonConfigobj < Formula
+  desc "Config file reading, writing and validation"
+  homepage "https://configobj.readthedocs.io/en/latest/"
+  url "https://files.pythonhosted.org/packages/cb/87/17d4c6d634c044ab08b11c0cd2a8a136d103713d438f8792d7be2c5148fb/configobj-5.0.8.tar.gz"
+  sha256 "6f704434a07dc4f4dc7c9a745172c1cad449feb548febd9f7fe362629c627a97"
+  license "BSD-3-Clause"
+
+  depends_on "python-setuptools" => :build
+  depends_on "python@3.11" => [:build, :test]
+  depends_on "python@3.12" => [:build, :test]
+  depends_on "six"
+
+  def pythons
+    deps.map(&:to_formula).sort_by(&:version).filter { |f| f.name.start_with?("python@") }
+  end
+
+  def install
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-m", "pip", "install", *std_pip_args, "."
+    end
+  end
+
+  test do
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-c", "from configobj import ConfigObj"
+    end
+  end
+end
