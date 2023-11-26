97e9ea4d4c95e23aee2234763d579bd787a48943
Rui Chen <rui@chenrui.dev>
2023-11-26 15:32:38 -0500

    python-chardet 5.2.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/p/python-chardet.rb
@@ -0,0 +1,39 @@
+class PythonChardet < Formula
+  desc "Python character encoding detector"
+  homepage "https://chardet.readthedocs.io/en/latest/"
+  url "https://files.pythonhosted.org/packages/f3/0d/f7b6ab21ec75897ed80c17d79b15951a719226b9fababf1e40ea74d69079/chardet-5.2.0.tar.gz"
+  sha256 "1b3b6ff479a8c414bc3fa2c0852995695c4a026dcd6d0633b2dd092ca39c1cf7"
+  license "LGPL-2.1-or-later"
+
+  depends_on "python-setuptools" => :build
+  depends_on "python@3.11" => [:build, :test]
+  depends_on "python@3.12" => [:build, :test]
+
+  def pythons
+    deps.map(&:to_formula).sort_by(&:version).filter { |f| f.name.start_with?("python@") }
+  end
+
+  def install
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-m", "pip", "install", *std_pip_args, "."
+    end
+  end
+
+  def caveats
+    <<~EOS
+      To run `chardetect`, you may need to `brew install #{pythons.last}`
+    EOS
+  end
+
+  test do
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-c", "import chardet"
+    end
+
+    (testpath/"test.txt").write "你好"
+    output = shell_output("#{bin}/chardetect #{testpath}/test.txt")
+    assert_match "test.txt: utf-8 with confidence 0.7525", output
+  end
+end
