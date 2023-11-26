c17b82b54fa2661bb8af9030811fd50b42e03b3c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-11-26 00:09:09 +0000

    git-review: update 2.3.1_3 bottle.

--- Formula/g/git-review.rb
+++ Formula/g/git-review.rb
@@ -8,13 +8,14 @@ class GitReview < Formula
   head "https://opendev.org/opendev/git-review.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "bfc345d706adf37a43b6dfbbd09e97c17c8d48c6d76dc2291606ee41844185b6"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "cba292566603f89c722a430308c5bcfa43e867085836e613c886cd1fefbfe23a"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "07b1ec48d55c4a5eaa7fac678c2479b22e377ac47afd55339856d6e04d0e8e69"
-    sha256 cellar: :any_skip_relocation, sonoma:         "193a1eff4f5810c6a28fc7bd058a1b834a080056f966831c48e8329f24524e29"
-    sha256 cellar: :any_skip_relocation, ventura:        "bd5557008d90abc65d6f868e0fd32e1ae538f91b47e0e0f362c6ee226288d59b"
-    sha256 cellar: :any_skip_relocation, monterey:       "db5407dff0d547028e733d438142f98b7d4555ba500e7f593b5537b8bd7234c8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "21f1110e2bc0bdfd47c9ca25458063cb94abbc7df6352a6eddc79e17191a7b27"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "7e3e07cd3081c724acabed1b87c5de98195cdcfed701cd8e2d5a9b123a4005ca"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "119c392a0f881ef5fa797fa5c068fa7d1668281cb129e137a2140fa61e0ec9f3"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "9a4819494bbd02ec9588319c9233531d0bcd81c455464d4449b881094f58ac7b"
+    sha256 cellar: :any_skip_relocation, sonoma:         "02183437c33b30c51c5471091830a09958f590670009bb1940adf9ae09a72a23"
+    sha256 cellar: :any_skip_relocation, ventura:        "151d000730015c13a9922a4467687b146febf6ae1412a0ae478ac85e53bb72e7"
+    sha256 cellar: :any_skip_relocation, monterey:       "79912fd48bd1c9ebcb6e080a2118bf8841bea906d6b8167be6f8e29a7d987a9d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bc4c21a96afbd8a772fe47cb80f9c1b60e623fab18a152bd0e2a0aef9d5fbf55"
   end
 
   depends_on "python-requests"
