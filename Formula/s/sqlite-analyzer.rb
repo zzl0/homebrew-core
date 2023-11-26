5c52f245a9fea77ba7bbbe6d7a768d504189d92e
Rui Chen <rui@chenrui.dev>
2023-11-25 19:21:15 -0500

    sqlite-analyzer 3.44.1
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/s/sqlite-analyzer.rb
+++ Formula/s/sqlite-analyzer.rb
@@ -2,7 +2,7 @@ class SqliteAnalyzer < Formula
   desc "Analyze how space is allocated inside an SQLite file"
   homepage "https://www.sqlite.org/"
   url "https://www.sqlite.org/2023/sqlite-src-3440100.zip"
-  version "3.44.0"
+  version "3.44.1"
   sha256 "52aa53c59ec9be4c28e2d2524676cb2938558f11c2b4c00d8b1a546000453919"
   license "blessing"
 
