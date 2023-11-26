74f1f89a63e4723dc4e147d4efa9990c7b4ec5db
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-11-26 00:09:11 +0000

    goolabs: update 0.4.0_8 bottle.

--- Formula/g/goolabs.rb
+++ Formula/g/goolabs.rb
@@ -7,13 +7,14 @@ class Goolabs < Formula
   revision 8
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "ec2d48ef64de82848734cde06416e318e0e71d226a5c9517d683ef68f3b67820"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0d8bf5afd13ac12fe10795c9a04cb75f4586d1661ff92b61cf3591a1e14bf368"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "45434838d1d08ab79ec52a6c30be5cc611b4e6bc5f342b5c10ed7ebde214c07c"
-    sha256 cellar: :any_skip_relocation, sonoma:         "969fa451d5fec752c6d4bcf063f34fe4e68d65ec1f2de01681f19170bf3c2761"
-    sha256 cellar: :any_skip_relocation, ventura:        "89730e6267251c28fe7b536803d865b3f4c773c50810310e95c69246c79f8835"
-    sha256 cellar: :any_skip_relocation, monterey:       "fcd0aa599bc30bec9971ab63fd8d62c2c72aa66d63c0e84b232e98c92e8322af"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "958887708d216fead50d597c2763e779e38bbb1e66b68c417837a41c1ba1975d"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "dc22ccfb139d1b5d9182e00ec095b5f53a67d2570c572102a73387cfe3234686"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "4f0bd55b28ccc5b9ebbec680d617a787887b91719cf273abc66f0236295c8f20"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "d2f0cb3e6d48d4bfa753a7d047707a053636874b60364406de13b3e4ce576825"
+    sha256 cellar: :any_skip_relocation, sonoma:         "5f775dc815f2f41287e19db174a5d7e99254c80dced2811e2ec6b3bad909f844"
+    sha256 cellar: :any_skip_relocation, ventura:        "b04b263c9d5f2e802c330013141a30ff8a1f6084a574f8024fdea9bcbdcd458c"
+    sha256 cellar: :any_skip_relocation, monterey:       "2fc7dcfaf17d51aa952104e2ca09f2b143860d3144bc1b1552f8aa9187799eb8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "359ad52e29bc76fefc5b2e26ccf3f91876244c578f48b8768c1f598532c4d116"
   end
 
   depends_on "python-setuptools" => :build
