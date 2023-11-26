4d949e81ed92cf0908cf859f07d577506195408a
Rui Chen <rui@chenrui.dev>
2023-11-26 15:15:41 -0500

    python-s3transfer 0.7.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/p/python-s3transfer.rb
@@ -0,0 +1,30 @@
+class PythonS3transfer < Formula
+  desc "Amazon S3 Transfer Manager for Python"
+  homepage "https://github.com/boto/s3transfer"
+  url "https://files.pythonhosted.org/packages/3f/ff/5fd9375f3fe467263cff9cad9746fd4c4e1399440ea9563091c958ff90b5/s3transfer-0.7.0.tar.gz"
+  sha256 "fd3889a66f5fe17299fe75b82eae6cf722554edca744ca5d5fe308b104883d2e"
+  license "Apache-2.0"
+
+  depends_on "python-setuptools" => :build
+  depends_on "python@3.11" => [:build, :test]
+  depends_on "python@3.12" => [:build, :test]
+  depends_on "python-botocore"
+
+  def pythons
+    deps.map(&:to_formula).sort_by(&:version).filter { |f| f.name.start_with?("python@") }
+  end
+
+  def install
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-m", "pip", "install", *std_pip_args, "."
+    end
+  end
+
+  test do
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-c", "from s3transfer.manager import TransferManager"
+    end
+  end
+end
