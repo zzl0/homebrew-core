e9a82552463dd9fdfa9bb2bc2175b87f770f2f1b
Rui Chen <rui@chenrui.dev>
2023-11-26 00:42:01 -0500

    biosig: revision bump (suite-sparse 7.3.1)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/b/biosig.rb
+++ Formula/b/biosig.rb
@@ -4,6 +4,7 @@ class Biosig < Formula
   url "https://downloads.sourceforge.net/project/biosig/BioSig%20for%20C_C%2B%2B/src/biosig-2.5.2.src.tar.xz"
   sha256 "3c87fa4ae6d69e1a75477f85451b6f16480418a0018d59e1586a2e3e8954ec47"
   license "GPL-3.0-or-later"
+  revision 1
 
   livecheck do
     url :stable
