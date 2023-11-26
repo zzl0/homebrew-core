8b64d483df07a54e775a36fad27e26a44ca0cb94
Patrick Linnane <patrick@linnane.io>
2023-11-26 00:05:07 +0000

    python-hatch-fancy-pypi-readme: add 23.1.0 bottle.

--- Formula/p/python-hatch-fancy-pypi-readme.rb
+++ Formula/p/python-hatch-fancy-pypi-readme.rb
@@ -5,6 +5,16 @@ class PythonHatchFancyPypiReadme < Formula
   sha256 "b1df44063094af1e8248ceacd47a92c9cf313d6b9823bf66af8a927c3960287d"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "4697468bfb7171cea5e7a6047fa6852882b704cc82a2ad70c4a0dd79ff893a55"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3a34810e1207bf8c38eda110f66c4d5b9b07717673e1916bba6253ed13eb5d97"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c987096a84770d8456abb423c3ff62292621b1d3e705fa30f48401216a27c8d8"
+    sha256 cellar: :any_skip_relocation, sonoma:         "1e8c68f39db2b2c5b95efb0b8113f1ea20c03627b6dbdc707b1d2248b3a08178"
+    sha256 cellar: :any_skip_relocation, ventura:        "8bebaa85dbd55e254e697f7365a9d962536a421e3e98a73288e496b1ea2f89bb"
+    sha256 cellar: :any_skip_relocation, monterey:       "7d68c0e41ffee4cc1daaf20d492d981a0dae7fa698a47525d5ab1bff5d01696d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "10b807619868dbceef3076769c5ff39fc42ad37e325710f89be83e1f59029741"
+  end
+
   depends_on "python-hatchling" => [:build, :test]
   depends_on "python@3.11" => [:build, :test]
   depends_on "python@3.12" => [:build, :test]
