a4f9392017f1f5e8bd2b424c8d3d725cfcd2b0a7
Rui Chen <rui@chenrui.dev>
2023-11-27 11:54:16 -0500

    python-asn1crypto 1.5.1 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/p/python-asn1crypto.rb
@@ -0,0 +1,29 @@
+class PythonAsn1crypto < Formula
+  desc "Python ASN.1 library with a focus on performance"
+  homepage "https://github.com/wbond/asn1crypto"
+  url "https://files.pythonhosted.org/packages/de/cf/d547feed25b5244fcb9392e288ff9fdc3280b10260362fc45d37a798a6ee/asn1crypto-1.5.1.tar.gz"
+  sha256 "13ae38502be632115abf8a24cbe5f4da52e3b5231990aff31123c805306ccb9c"
+  license "MIT"
+
+  depends_on "python-setuptools" => :build
+  depends_on "python@3.11" => [:build, :test]
+  depends_on "python@3.12" => [:build, :test]
+
+  def pythons
+    deps.map(&:to_formula).sort_by(&:version).filter { |f| f.name.start_with?("python@") }
+  end
+
+  def install
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-m", "pip", "install", *std_pip_args, "."
+    end
+  end
+
+  test do
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-c", "from asn1crypto import pem, x509"
+    end
+  end
+end
