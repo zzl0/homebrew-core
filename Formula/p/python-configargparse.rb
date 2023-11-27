3aa2f923e494cbe61840be3421fa97e3d5205b91
Rui Chen <rui@chenrui.dev>
2023-11-27 11:15:43 -0500

    python-configargparse 1.7 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/p/python-configargparse.rb
@@ -0,0 +1,29 @@
+class PythonConfigargparse < Formula
+  desc "Drop-in replacement for argparse"
+  homepage "https://github.com/bw2/ConfigArgParse"
+  url "https://files.pythonhosted.org/packages/70/8a/73f1008adfad01cb923255b924b1528727b8270e67cb4ef41eabdc7d783e/ConfigArgParse-1.7.tar.gz"
+  sha256 "e7067471884de5478c58a511e529f0f9bd1c66bfef1dea90935438d6c23306d1"
+  license "MIT"
+
+  depends_on "python-setuptools" => :build
+  depends_on "python@3.11" => [:build, :test]
+  depends_on "python@3.12" => [:build, :test]
+
+  def pythons
+    deps.map(&:to_formula).sort_by(&:version).filter { |f| f.name.start_with?("python@") }
+  end
+
+  def install
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-m", "pip", "install", *std_pip_args, "."
+    end
+  end
+
+  test do
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-c", "import configargparse"
+    end
+  end
+end
