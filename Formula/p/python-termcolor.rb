fe2375d9a2847d45544b74330cbbb8d3d03bb20a
Patrick Linnane <patrick@linnane.io>
2023-11-27 16:30:15 +0000

    python-termcolor: add 2.3.0 bottle.

--- Formula/p/python-termcolor.rb
+++ Formula/p/python-termcolor.rb
@@ -5,6 +5,16 @@ class PythonTermcolor < Formula
   sha256 "b5b08f68937f138fe92f6c089b99f1e2da0ae56c52b78bf7075fd95420fd9a5a"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "4f182f2018a5533bdb094af2ab939b0943f079d287d318e1694f4b83aa7e29fd"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "455291ea47180f3f9e62ace232df0a010cf2dbe38e3a7469e3570158b81060b3"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "30ba2fe70833e494e0cdeca79fa0d9d8a2b5074e4630f484413b38209de83cb7"
+    sha256 cellar: :any_skip_relocation, sonoma:         "74dd75dab12cb34068b723dcce9eb49bfa920a4d3cdb772df7b71acd1c2db14e"
+    sha256 cellar: :any_skip_relocation, ventura:        "e7364c03c54c4b442b3ea545c8149ca0a0c1ade696e4bd63095fbb1c713258e0"
+    sha256 cellar: :any_skip_relocation, monterey:       "425d7252471829fa2a13fff76589ae0eda3a213e42f6b15210499dc2f8cd1ff5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f77462ea68e2d9e2a2fa2890baeef9af1359dfef8c8cb34797a1ac88f60b15ad"
+  end
+
   depends_on "python-hatch-vcs" => :build
   depends_on "python-hatchling" => :build
   depends_on "python-setuptools" => :build
