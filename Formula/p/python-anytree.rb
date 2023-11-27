c7a0c54ff742539f89864a486f8644b4ef1631c4
Patrick Linnane <patrick@linnane.io>
2023-11-27 05:14:23 +0000

    python-anytree: add 2.12.1 bottle.

--- Formula/p/python-anytree.rb
+++ Formula/p/python-anytree.rb
@@ -5,6 +5,16 @@ class PythonAnytree < Formula
   sha256 "244def434ccf31b668ed282954e5d315b4e066c4940b94aff4a7962d85947830"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "353d20e8180d437225ccd0d94f35bee8cd2d800bcd65e4b44ea9d9129350c546"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "1f6221a6daf5d84abd26b653cd3eee01183760caa1beb673ee740aa30fcf9c22"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "7aacebee3c63fbcfe99102c99b4ed2b5685b26f71c988e24fe6e90070f0eeb02"
+    sha256 cellar: :any_skip_relocation, sonoma:         "fc0d36b010c65d5825f2156d5e4f5c8d43c962af9d7d180f076cf94ebd91bd87"
+    sha256 cellar: :any_skip_relocation, ventura:        "20ec71e2c9143173f717eed39928428c12a60fc3efd682a07df11885f5926083"
+    sha256 cellar: :any_skip_relocation, monterey:       "fc4d56643f771a64f69666eac8b56a19c92bdeff3afce3ec3e61cef0e723cd03"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "645ba2f4f017d7319e7a74e97ae7c0706ecca18607eeec52730d439a2fe72ebf"
+  end
+
   depends_on "poetry" => :build
   depends_on "python@3.11" => [:build, :test]
   depends_on "python@3.12" => [:build, :test]
