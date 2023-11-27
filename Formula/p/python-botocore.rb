52b0a310c502eafb06f2bfe772c0d0ad8a5a7117
Patrick Linnane <patrick@linnane.io>
2023-11-27 04:01:40 +0000

    python-botocore: add 1.32.5 bottle.

--- Formula/p/python-botocore.rb
+++ Formula/p/python-botocore.rb
@@ -5,6 +5,16 @@ class PythonBotocore < Formula
   sha256 "75a68f942cd87baff83b3a20dfda11b3aeda48aad32e4dcd6fe8992c0cb0e7db"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "fc12682a86adf13deddf6547f35c7f390e9ecce00053655cb5d887307eca6b2f"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a7dfc4dc521cb5183b5f19ffe66e220d3345fff7d1dbba78a3e9ede9aea6d697"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b34cd7eb6d729f0dd095637cd1a8c554b760f345ce99eee76140582a5a71150d"
+    sha256 cellar: :any_skip_relocation, sonoma:         "9ff69a93eb1117bd3b97cd115fe22c4d48624117822ef61e97971877c34f9b29"
+    sha256 cellar: :any_skip_relocation, ventura:        "453e26e7090f689ec16f7b2269ca54eb312e8aeb02f2ca5b17bb322d9d1207b3"
+    sha256 cellar: :any_skip_relocation, monterey:       "88614949168dadf9ffa3007acd70b8bdb5ddd537818a7c817139bf415d8b6ef4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d9eb6a09ad38ef044f9a15cfbee72af4ea8668b0011ed129abaffcf58815e57c"
+  end
+
   depends_on "python-setuptools" => :build
   depends_on "python@3.11" => [:build, :test]
   depends_on "python@3.12" => [:build, :test]
