829d969b7ed714de79c1b92a8d25404aa445dd3c
Rui Chen <rui@chenrui.dev>
2023-11-27 11:41:26 -0500

    python-openapi3 1.8.2 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    python-openapi3: depends_on python-requests and pyyaml
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/p/python-openapi3.rb
@@ -0,0 +1,31 @@
+class PythonOpenapi3 < Formula
+  desc "Python3 OpenAPI 3 Spec Parser"
+  homepage "https://github.com/dorthu/openapi3"
+  url "https://files.pythonhosted.org/packages/94/0a/e7862c7870926ecb86d887923e36b7853480a2a97430162df1b972bd9d5b/openapi3-1.8.2.tar.gz"
+  sha256 "a21a490573d89ca69ada7cbe585adb2fca4964257f6f3a1df531f12815455d2c"
+  license "BSD-3-Clause"
+
+  depends_on "python-setuptools" => :build
+  depends_on "python@3.11" => [:build, :test]
+  depends_on "python@3.12" => [:build, :test]
+  depends_on "python-requests"
+  depends_on "pyyaml"
+
+  def pythons
+    deps.map(&:to_formula).sort_by(&:version).filter { |f| f.name.start_with?("python@") }
+  end
+
+  def install
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-m", "pip", "install", *std_pip_args, "."
+    end
+  end
+
+  test do
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-c", "from openapi3 import OpenAPI"
+    end
+  end
+end
