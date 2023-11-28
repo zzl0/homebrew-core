c5c96d65b9b226810d81877f2e7c8a3e8011a39a
Patrick Linnane <patrick@linnane.io>
2023-11-28 09:55:18 -0800

    wangle 2023.11.27.00

--- Formula/w/wangle.rb
+++ Formula/w/wangle.rb
@@ -1,8 +1,8 @@
 class Wangle < Formula
   desc "Modular, composable client/server abstractions framework"
   homepage "https://github.com/facebook/wangle"
-  url "https://github.com/facebook/wangle/releases/download/v2023.11.20.00/wangle-v2023.11.20.00.tar.gz"
-  sha256 "3017f08a81d11dec2eb4e4bb7a67cb960ed0aafd6f175bc26a80de55073aef1d"
+  url "https://github.com/facebook/wangle/releases/download/v2023.11.27.00/wangle-v2023.11.27.00.tar.gz"
+  sha256 "639c0dd6200b51e36ee1e4d60fab4721aff6bfe7e1971240c69a7aec323c831d"
   license "Apache-2.0"
   head "https://github.com/facebook/wangle.git", branch: "main"
 
