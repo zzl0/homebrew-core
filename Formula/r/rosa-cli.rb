3a6b8d2bd3efbaca88fff538509651a19b46fb4e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-11-28 15:49:43 +0000

    rosa-cli 1.2.31

--- Formula/r/rosa-cli.rb
+++ Formula/r/rosa-cli.rb
@@ -1,8 +1,8 @@
 class RosaCli < Formula
   desc "RedHat OpenShift Service on AWS (ROSA) command-line interface"
   homepage "https://www.openshift.com/products/amazon-openshift"
-  url "https://github.com/openshift/rosa/archive/refs/tags/v1.2.30.tar.gz"
-  sha256 "38eb5665d5cee28921f1db3a4e59304a3eb98f2eb34bd30b454bfc9493c1da8b"
+  url "https://github.com/openshift/rosa/archive/refs/tags/v1.2.31.tar.gz"
+  sha256 "8cf22a34c00371ec4384c03af0ee32269e6b43272cee332dbf7e936aa0140af0"
   license "Apache-2.0"
   head "https://github.com/openshift/rosa.git", branch: "master"
 
