0f81b0dcebc42d08959b98eed27c67bab1bb555e
Rui Chen <rui@chenrui.dev>
2023-11-28 00:05:05 -0500

    python-distro 1.8.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/p/python-distro.rb
@@ -0,0 +1,38 @@
+class PythonDistro < Formula
+  desc "Linux OS platform information API"
+  homepage "https://distro.readthedocs.io/en/latest/"
+  url "https://files.pythonhosted.org/packages/4b/89/eaa3a3587ebf8bed93e45aa79be8c2af77d50790d15b53f6dfc85b57f398/distro-1.8.0.tar.gz"
+  sha256 "02e111d1dc6a50abb8eed6bf31c3e48ed8b0830d1ea2a1b78c61765c2513fdd8"
+  license "Apache-2.0"
+
+  depends_on "python-setuptools" => :build
+  depends_on "python@3.11" => [:build, :test]
+  depends_on "python@3.12" => [:build, :test]
+
+  def pythons
+    deps.map(&:to_formula).sort_by(&:version).filter { |f| f.name.start_with?("python@") }
+  end
+
+  def install
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-m", "pip", "install", *std_pip_args, "."
+    end
+  end
+
+  def caveats
+    <<~EOS
+      To run `distro`, you may need to `brew install #{pythons.last}`
+    EOS
+  end
+
+  test do
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-c", "import distro"
+    end
+
+    version_json = JSON.parse(shell_output("#{bin}/distro --json"))["version_parts"]
+    assert_match "build_number", version_json.to_s
+  end
+end
