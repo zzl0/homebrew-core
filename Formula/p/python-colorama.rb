c6b596548ff2f6d5141ca453efc8215d90a088a2
Rui Chen <rui@chenrui.dev>
2023-11-27 23:50:49 -0500

    python-colorama 0.4.6 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/p/python-colorama.rb
@@ -0,0 +1,32 @@
+class PythonColorama < Formula
+  desc "Cross-platform colored terminal text"
+  homepage "https://github.com/tartley/colorama"
+  url "https://files.pythonhosted.org/packages/d8/53/6f443c9a4a8358a93a6792e2acffb9d9d5cb0a5cfd8802644b7b1c9a02e4/colorama-0.4.6.tar.gz"
+  sha256 "08695f5cb7ed6e0531a20572697297273c47b8cae5a63ffc6d6ed5c201be6e44"
+  license "BSD-3-Clause"
+
+  depends_on "python-hatchling" => :build
+  depends_on "python@3.11" => [:build, :test]
+  depends_on "python@3.12" => [:build, :test]
+
+  def pythons
+    deps.map(&:to_formula).sort_by(&:version).filter { |f| f.name.start_with?("python@") }
+  end
+
+  def install
+    # no need to install tests files
+    rm_rf "colorama/tests"
+
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-m", "pip", "install", *std_pip_args, "."
+    end
+  end
+
+  test do
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-c", "import colorama"
+    end
+  end
+end
