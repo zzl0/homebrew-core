a5860886c0256d89cebeef59da69af4d9aafd57f
Rui Chen <rui@chenrui.dev>
2023-11-28 20:27:06 +0000

    goresym: add 2.6.3 bottle.

--- Formula/g/goresym.rb
+++ Formula/g/goresym.rb
@@ -6,6 +6,16 @@ class Goresym < Formula
   license "MIT"
   head "https://github.com/mandiant/GoReSym.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "be759944a0ef2237921fa66b17f8a6ece362450471f5420b69d78eed3ae6bc95"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "be759944a0ef2237921fa66b17f8a6ece362450471f5420b69d78eed3ae6bc95"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "be759944a0ef2237921fa66b17f8a6ece362450471f5420b69d78eed3ae6bc95"
+    sha256 cellar: :any_skip_relocation, sonoma:         "6d42c461ff9c96d7232e5a638b57606a1982b6f2e50a1f55b8f985a7134342c6"
+    sha256 cellar: :any_skip_relocation, ventura:        "6d42c461ff9c96d7232e5a638b57606a1982b6f2e50a1f55b8f985a7134342c6"
+    sha256 cellar: :any_skip_relocation, monterey:       "6d42c461ff9c96d7232e5a638b57606a1982b6f2e50a1f55b8f985a7134342c6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8031bd4313fa2fc094a6d9d5a51a81318f9b07793f243c398fa1b199d28d217f"
+  end
+
   depends_on "go" => :build
 
   def install
