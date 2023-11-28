5bdc490092fa5c9aae261e84d546b1e544f1287a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-11-28 02:31:27 +0000

    bashate: update 2.1.1 bottle.

--- Formula/b/bashate.rb
+++ Formula/b/bashate.rb
@@ -6,14 +6,14 @@ class Bashate < Formula
   license "Apache-2.0"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "d22a13965b8badc19e991a9a271e9f1975f85b96d68dbc22d25cf196f42b4ae3"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0137ed279d4f0f319d8c50eb4d86e20b25f7bfbe4c07d45fb455fde83516acd6"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e886d384c41e915700b72256b7cf60e0d33743958eb8bd960544d15918ebede8"
-    sha256 cellar: :any_skip_relocation, sonoma:         "58523165172e73d1b4658e6beb217ad0ce1aecba52185f6b6cd7f7fd3736d75a"
-    sha256 cellar: :any_skip_relocation, ventura:        "f4ff5795ad844e2bd2235e0528b04551fad9264616cd072dba0f806de9c15c2d"
-    sha256 cellar: :any_skip_relocation, monterey:       "314b0d6247591b2812a710cdc133e2fbdce772fb723e44e7dd893a7d08bf0394"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "104403b5c06e8bdb94a82195bbba24462d62cee9717b2a3c29a3da75707f968d"
+    rebuild 2
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "ef03c3fa96db7cfbf69ebeba21301c3760bb7f14dc46d9b74588bfac756ec01b"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "ff66343cf32860271f667598d87696fe72d6e65900dfdcf6115abea14d613eed"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b353b15aba538b3c8d4590f7b801d89c4ad5db691a485a841f0ea436221c1fe2"
+    sha256 cellar: :any_skip_relocation, sonoma:         "0fc245bac330eab2cefc2dd3212ed58a061f6a5500d578abec4f8a3db4a2721d"
+    sha256 cellar: :any_skip_relocation, ventura:        "5a3d887b439b7c2e487a4dd8b75fe15424c30d65963c1629343ba745d1ccdbcf"
+    sha256 cellar: :any_skip_relocation, monterey:       "5dde169aec8a9ed21dff77b70a83f282620805110eebb0a7f56c4a9bf4cfb208"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d760da6d91318d5f9e1ee0a0bdb716145bf0c9bf50531350796df64a583d1721"
   end
 
   depends_on "python-setuptools" => :build
