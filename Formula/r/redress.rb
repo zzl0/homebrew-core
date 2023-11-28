26b0338db9bdff0f8843471944ad1055084d024a
Glenn 'devalias' Grant <glenn@devalias.net>
2023-11-28 14:56:45 +1100

    redress 1.2.0 (new formula)
    Co-Authored-By: Sean Molenaar <SMillerDev@users.noreply.github.com>

---
+++ Formula/r/redress.rb
@@ -0,0 +1,41 @@
+class Redress < Formula
+  desc "Tool for analyzing stripped Go binaries compiled with the Go compiler"
+  homepage "https://go-re.tk/redress/"
+  url "https://github.com/goretk/redress/archive/refs/tags/v1.2.0.tar.gz"
+  sha256 "60b8c6fcdeb4516c32ce5bab40bf345db995f3c2a80561662a190c37eb3284a9"
+  license "AGPL-3.0-only"
+  head "https://github.com/goretk/redress.git", branch: "develop"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  depends_on "go" => :build
+
+  def install
+    # https://github.com/goretk/redress/blob/develop/Makefile#L11-L14
+    gore_version = File.read(buildpath/"go.mod").scan(%r{goretk/gore v(\S+)}).flatten.first
+
+    ldflags = %W[
+      -s -w
+      -X main.redressVersion=#{version}
+      -X main.goreVersion=#{gore_version}
+      -X main.compilerVersion=#{Formula["go"].version}
+    ]
+
+    system "go", "build", *std_go_args(ldflags: ldflags)
+
+    generate_completions_from_executable(bin/"redress", "completion")
+  end
+
+  test do
+    assert_match "Version:  #{version}", shell_output("#{bin}/redress version")
+
+    test_module_root = "github.com/goretk/redress"
+    test_bin_path = bin/"redress"
+
+    output = shell_output("#{bin}/redress info '#{test_bin_path}'")
+    assert_match(/Main root\s+#{Regexp.escape(test_module_root)}/, output)
+  end
+end
