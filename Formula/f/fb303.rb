aa339a693c87b7165ac27f21b16d01d275987f8d
Patrick Linnane <patrick@linnane.io>
2023-11-28 09:54:40 -0800

    fb303 2023.11.27.00

--- Formula/f/fb303.rb
+++ Formula/f/fb303.rb
@@ -1,8 +1,8 @@
 class Fb303 < Formula
   desc "Thrift functions for querying information from a service"
   homepage "https://github.com/facebook/fb303"
-  url "https://github.com/facebook/fb303/archive/refs/tags/v2023.11.20.00.tar.gz"
-  sha256 "7653ebc37a9a954c9cc9cfbcb918a694e6740d206673463892b3defaa35384d9"
+  url "https://github.com/facebook/fb303/archive/refs/tags/v2023.11.27.00.tar.gz"
+  sha256 "285407f581b696840fe785b618b77b51e65f09f40fb9944664e5505aa0caff23"
   license "Apache-2.0"
   head "https://github.com/facebook/fb303.git", branch: "main"
 
