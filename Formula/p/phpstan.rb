9cb502478d4062b16bfcfea3317480265fbb400b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-11-28 15:21:54 +0000

    phpstan: update 1.10.46 bottle.

--- Formula/p/phpstan.rb
+++ Formula/p/phpstan.rb
@@ -6,13 +6,13 @@ class Phpstan < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "0f70da6888b2643ee520ef8224d1af56727de90ff9acf1ed226a3f49043679c4"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0f70da6888b2643ee520ef8224d1af56727de90ff9acf1ed226a3f49043679c4"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0f70da6888b2643ee520ef8224d1af56727de90ff9acf1ed226a3f49043679c4"
-    sha256 cellar: :any_skip_relocation, sonoma:         "82962fd6ba482a60819efd2af878b911b6b75eeba89262ee59f07e657d40248c"
-    sha256 cellar: :any_skip_relocation, ventura:        "82962fd6ba482a60819efd2af878b911b6b75eeba89262ee59f07e657d40248c"
-    sha256 cellar: :any_skip_relocation, monterey:       "82962fd6ba482a60819efd2af878b911b6b75eeba89262ee59f07e657d40248c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0f70da6888b2643ee520ef8224d1af56727de90ff9acf1ed226a3f49043679c4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "f888fbae81ca99d33644d178903c30ce1ae6f2665b5abc39f576d360cb50b0aa"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f888fbae81ca99d33644d178903c30ce1ae6f2665b5abc39f576d360cb50b0aa"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f888fbae81ca99d33644d178903c30ce1ae6f2665b5abc39f576d360cb50b0aa"
+    sha256 cellar: :any_skip_relocation, sonoma:         "2b61d3e7f0a13600cda17f4d5ba6eaf65996e8460968635c16e48f56e19216f4"
+    sha256 cellar: :any_skip_relocation, ventura:        "2b61d3e7f0a13600cda17f4d5ba6eaf65996e8460968635c16e48f56e19216f4"
+    sha256 cellar: :any_skip_relocation, monterey:       "2b61d3e7f0a13600cda17f4d5ba6eaf65996e8460968635c16e48f56e19216f4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f888fbae81ca99d33644d178903c30ce1ae6f2665b5abc39f576d360cb50b0aa"
   end
 
   depends_on "php" => :test
