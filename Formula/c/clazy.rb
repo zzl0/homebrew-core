0d6ae76af40da9bed788cbb68f6ce04fb6ca6ead
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-11-28 02:21:41 +0000

    clazy: update 1.11_2 bottle.

--- Formula/c/clazy.rb
+++ Formula/c/clazy.rb
@@ -35,16 +35,14 @@ class Clazy < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "08c7381e2a2fe8faed207b42c5d86f2d8ec714d3c5eadb03bc3891eddd709507"
-    sha256 cellar: :any,                 arm64_ventura:  "a716670d3c6a16f67ac39d82813c7fd054761f4d026255f904b10457f67791fb"
-    sha256 cellar: :any,                 arm64_monterey: "e5401775aff4f1e6fad0fd459ae01f492c0398101457ffc1d6d0381d4ceb8828"
-    sha256 cellar: :any,                 arm64_big_sur:  "d022ef65ffa388d2b6db61b16077dc08a5d5b29185a2723bb631914a4da1fa75"
-    sha256 cellar: :any,                 sonoma:         "3feb9c52f4410597b34b499b447c50ed99d54d44ead264982bcf65e13ece35a3"
-    sha256 cellar: :any,                 ventura:        "5938ca0a911bb8031870f91b12915eb359eceb0f92f07bc9d5d97996be9bc952"
-    sha256 cellar: :any,                 monterey:       "90ef44a60aff61eeb80c8a14ce55954b8d952da25a632c72f255d365c03cfc11"
-    sha256 cellar: :any,                 big_sur:        "5237f20d2a267e396777e6afabd0bf7eab77ff174ce05d771aef96d5e5ce62ac"
-    sha256 cellar: :any,                 catalina:       "ca761dbfa90f36a5880bf962e68b22d44f8449c5e74083d35dd79ae2d379b4fa"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f358bdb38ce2bebd90cb2fe9a59dfb8e4a65531a72488f37e0f2473b0ba6bb6c"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_sonoma:   "7930c7e75590e2e873007960e2c568efc2213a7805e1d95b06fe905e2890975e"
+    sha256 cellar: :any,                 arm64_ventura:  "295d6651040ee1c9bdcd90eb7a5bbe3e5f7e1b9c5482f506b2323c9d1f51b34b"
+    sha256 cellar: :any,                 arm64_monterey: "a12ee1e0f5861abdaa08009fccc7614baf1f5082927c18fb240d89230b828d70"
+    sha256 cellar: :any,                 sonoma:         "4085b060be10127cb9ecfe9f3164d033c814b9831d3c2ba0d5176cddd0c81085"
+    sha256 cellar: :any,                 ventura:        "a2a34ceedf8e3bdf28f4fa83d2e97d2efb8e32478b347541d950e4e87cbb12d1"
+    sha256 cellar: :any,                 monterey:       "f45df63085bf00011a5453cb2f22b782a2df2ffa79aa14d75a3d8cda996178c1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "541e5a5facd4406bd0bf5b29a854416fb99117cabf87a553ab6a470d7d128314"
   end
 
   depends_on "cmake"   => [:build, :test]
