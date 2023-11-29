e5446762fc7447b39f8eb8c84c47ec77ae2e894b
Patrick Linnane <patrick@linnane.io>
2023-11-29 01:33:15 +0000

    geeqie: update 2.1_2 bottle.

--- Formula/g/geeqie.rb
+++ Formula/g/geeqie.rb
@@ -12,13 +12,13 @@ class Geeqie < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_sonoma:   "82aab3a3043bfb4cb20b3a9dfd4712962c9a9ef29008489b7eed1621f9f97c93"
-    sha256 cellar: :any, arm64_ventura:  "13811bfe1db32a6382981a9e96a4cad44d841c27cd14041d8d68679e49ce1309"
-    sha256 cellar: :any, arm64_monterey: "7b67b06b1f1ac205a892aad310dc953671736f631fcc580ab4d6981a34ac0fb7"
-    sha256 cellar: :any, sonoma:         "0d7c4d29e6da9097537841dec76c651e116707e02fb0ea2c6ce6d6a65fb07495"
-    sha256 cellar: :any, ventura:        "8d30e3c9192385c314cab258cdf844ed778c4a64ddb92aff58d4811c4f1bad7b"
-    sha256 cellar: :any, monterey:       "36f6fbe9f4ecbf8c5585e64a4dc656d6033143b7efba9a8ec2ceeb77b566ec58"
-    sha256               x86_64_linux:   "6870d6da92fc7dadef99578d19216e5df2f32279b60f5296a602af62001c9c4e"
+    sha256 cellar: :any, arm64_sonoma:   "3d7ae1d94cef077ebe54e7fa3d6f167a90f6e6b535e7f9ac6242c0ad32e16de4"
+    sha256 cellar: :any, arm64_ventura:  "f93422f6e30ee5841aa7c1b2d1a812e2a8d62196c2cbaaf8ad8c27f7ad5ec53c"
+    sha256 cellar: :any, arm64_monterey: "f942a659c5c97508bb87f3e5dc6ec3ede4412799063314e0b8ca76d18c667bf2"
+    sha256 cellar: :any, sonoma:         "b6ca6e29b80b895f0dcf3b9a2e7dd737190ca0a9bf506c43818391be06a03e4f"
+    sha256 cellar: :any, ventura:        "69d4a5a09166f1fcc40aa8a6b7b73166723614a5c5d5d7a82aaeb6f30efdf2fb"
+    sha256 cellar: :any, monterey:       "e2334655057bbd7bdd512cc3f391ae7263b109b3d1d10b7b304f3fc89b0530ba"
+    sha256               x86_64_linux:   "ff872a4a1ede2ca7d15b0e09d521255a48ded1b536ab71e90de2c96df93f01b2"
   end
 
   depends_on "meson" => :build
