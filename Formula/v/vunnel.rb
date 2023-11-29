16d04f71bd63ce756fe071702f6fae1a1b57adcd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-11-29 22:03:49 +0000

    vunnel: update 0.17.10 bottle.

--- Formula/v/vunnel.rb
+++ Formula/v/vunnel.rb
@@ -9,14 +9,13 @@ class Vunnel < Formula
   head "https://github.com/anchore/vunnel.git", branch: "main"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_sonoma:   "84cef8fb6b2cd582eb08d547f065cf16abf3cfae0928bc0012e0f1a034e44ef3"
-    sha256 cellar: :any,                 arm64_ventura:  "150677f57ce63c938c89bf944415892b2727fb06ae943decabcc5dc9c44a67b0"
-    sha256 cellar: :any,                 arm64_monterey: "c62fa0b68923d8153de718db8600d8bf3898f929ca66b71c4f01003dbd3bbe56"
-    sha256 cellar: :any,                 sonoma:         "0c554561b9045b9fa0331e4b4c6dc8483214900ccb3b3d0fbe408e77202f8e0c"
-    sha256 cellar: :any,                 ventura:        "43cac6a77d857860a1ed868e3b81ff47996f34736a5437f69959b1e3e540a9f1"
-    sha256 cellar: :any,                 monterey:       "440753390c1b8848199e47f1aa4db34b2d5932313bd9bd2b52c811281d8b844c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "41ff42c805de13420af3296e7d6c84ea3e3349f789a65bf1656d4b5757ee4ff3"
+    sha256 cellar: :any,                 arm64_sonoma:   "582dccb93d7e759eb6db077cf58df162007a5dd1d74bab3f3b01f221b3bd5ad3"
+    sha256 cellar: :any,                 arm64_ventura:  "98e65dd089ac65e12b245ef85fdbdb88e0e1dacd5522cc6e95f7262899010f72"
+    sha256 cellar: :any,                 arm64_monterey: "d2de74382f12febe18b9322d649c99c1b7d8c2dbcb9ecd443c195984745c900f"
+    sha256 cellar: :any,                 sonoma:         "dabc263bbf841245beec9093bb125409a59c1948640d4b82aca5971b0815364a"
+    sha256 cellar: :any,                 ventura:        "b8e99ac481da1be3a85889fc64424c2e9a3dde4dd6d8ce6dc54366cafacefcd6"
+    sha256 cellar: :any,                 monterey:       "bd21f1d92835d8d59e633506e5c9e309871bc6f46c739732753132eac18c4d3a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0ae1082d64fd76151e67f911c6c952fa42062582050d81f34a35a15823339c0c"
   end
 
   depends_on "rust" => :build
