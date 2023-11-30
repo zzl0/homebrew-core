93a1a805bf75d7b461d64a78b0a85402dfc8cb3d
Patrick Linnane <patrick@linnane.io>
2023-11-29 20:47:54 -0800

    go-task 3.32.0

--- Formula/g/go-task.rb
+++ Formula/g/go-task.rb
@@ -1,8 +1,8 @@
 class GoTask < Formula
   desc "Task is a task runner/build tool that aims to be simpler and easier to use"
   homepage "https://taskfile.dev/"
-  url "https://github.com/go-task/task/archive/refs/tags/v3.31.0.tar.gz"
-  sha256 "189f3b98ed6b15290d4fd4dbab687c87de268b5c9f2f1c93a7c02710da062785"
+  url "https://github.com/go-task/task/archive/refs/tags/v3.32.0.tar.gz"
+  sha256 "04666e5f763af99bcf98c6897d726f483165a7ed04801a35ffe1e55f78bbfe2f"
   license "MIT"
   head "https://github.com/go-task/task.git", branch: "main"
 
