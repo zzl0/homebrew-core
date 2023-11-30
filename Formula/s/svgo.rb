924d85c2ec364ce0eab4bf9f5aa5a0b88c00faf4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-11-30 19:57:35 +0000

    svgo: update 3.0.5 bottle.

--- Formula/s/svgo.rb
+++ Formula/s/svgo.rb
@@ -8,7 +8,7 @@ class Svgo < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "081eba9d32f4ba2e455d6c62a339f6cf1c6679863e3b417d7c79201ecdfc93fb"
+    sha256 cellar: :any_skip_relocation, all: "599b0651756119d2af116bf1887df50b136afc765309a276b0edcf3ca26dce59"
   end
 
   depends_on "node"
