7d9c3dc908aafe6ad99f3247de220f029f7873d1
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-12-01 04:52:32 +0000

    solo2-cli: add 0.2.2 bottle.

--- Formula/s/solo2-cli.rb
+++ Formula/s/solo2-cli.rb
@@ -6,6 +6,16 @@ class Solo2Cli < Formula
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/solokeys/solo2-cli.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "ac06ba094da706b68c527fa6904187aa9c2d21fd26246bf9a343a4a14a4d8dd9"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8c876fcb91927f6c382994c92cbd8fcf3041d12ee2e43f6239c325bd852d4d3b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "30c6fd9a7c4eaa8345fc68740a10232ffe26755d9c19b3bd4f7f86e650471d3e"
+    sha256 cellar: :any_skip_relocation, sonoma:         "4b0ce4ae4cc661cb11b6746b536e736256c6a3b978af3508ea36997bd667d0c9"
+    sha256 cellar: :any_skip_relocation, ventura:        "9529d0379455886612d51ade076b55ff1bffe3395b09fe80edba6ebb571d119c"
+    sha256 cellar: :any_skip_relocation, monterey:       "ffb93967fd1171798157e502212337d93d644f6e31f8e1457f17c0c62f088a01"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "00c7bd01f05ac244c1b50fbfc559a9dcc641c23d71a2f9664e8d88d22a522316"
+  end
+
   depends_on "rust" => :build
 
   on_linux do
