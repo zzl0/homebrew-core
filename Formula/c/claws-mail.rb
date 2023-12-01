c0bfc388b2c05da2051799adfd573f6559b360dd
Michael Cho <michael@michaelcho.dev>
2023-11-30 23:12:11 -0500

    claws-mail 4.2.0

--- Formula/c/claws-mail.rb
+++ Formula/c/claws-mail.rb
@@ -1,10 +1,9 @@
 class ClawsMail < Formula
   desc "User-friendly, lightweight, and fast email client"
   homepage "https://www.claws-mail.org/"
-  url "https://www.claws-mail.org/releases/claws-mail-3.19.1.tar.gz"
-  sha256 "4fcc2b0b39a6d40e4dc3e49fac2f1cf063575d6570e93408fa4a76ab67531ae1"
+  url "https://www.claws-mail.org/releases/claws-mail-4.2.0.tar.gz"
+  sha256 "446c89f27c2205277f08e776b53d9d151be013211d91e7d9da006bc95c051c60"
   license "GPL-3.0-or-later"
-  revision 1
 
   livecheck do
     url "https://www.claws-mail.org/releases.php"
@@ -24,13 +23,20 @@ class ClawsMail < Formula
   depends_on "cairo"
   depends_on "glib"
   depends_on "gnutls"
-  depends_on "gtk+"
+  depends_on "gtk+3"
   depends_on "libetpan"
   depends_on "nettle"
 
+  # Backport fix for building on non-X11 systems.
+  patch do
+    url "https://git.claws-mail.org/?p=claws.git;a=patch;h=dd4c4e5152235f9f4f319cc9fdad9227ebf688c9"
+    sha256 "883c30d0aa0a6450051c9452475ecc0e106297d0765facc07cacf96cde4a4556"
+  end
+
   def install
-    system "./configure", "--prefix=#{prefix}",
-                          "LDFLAGS=-Wl,-framework -Wl,Security",
+    ENV.append "LDFLAGS", "-Wl,-framework -Wl,Security" if OS.mac?
+    system "./configure", *std_configure_args,
+                          "--disable-silent-rules",
                           "--disable-archive-plugin",
                           "--disable-dillo-plugin",
                           "--disable-notification-plugin"
