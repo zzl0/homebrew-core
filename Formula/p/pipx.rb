c2667572dd11c08a012d4c4cc680b71ab275d64b
Branch Vincent <branchevincent@gmail.com>
2023-12-02 09:43:39 -0800

    pipx 1.3.0

--- Formula/p/pipx.rb
+++ Formula/p/pipx.rb
@@ -3,8 +3,8 @@ class Pipx < Formula
 
   desc "Execute binaries from Python packages in isolated environments"
   homepage "https://pypa.github.io/pipx"
-  url "https://files.pythonhosted.org/packages/f7/fc/a7d0af355ad074872c9fe282cb2877acb3395cfa3ec044d7c032031f6ae5/pipx-1.2.1.tar.gz"
-  sha256 "698777c05a97cca81df4dc6a71d9ca4ece2184c6f91dc7a0e4802ac51d86d32a"
+  url "https://files.pythonhosted.org/packages/da/1b/5ebbf68e321684ffca2ee6757c4f6962a136e75a4925554f774f511666ad/pipx-1.3.0.tar.gz"
+  sha256 "db07ec2a3a15d772a71b2608c134f065d7fbc51d4654abe6adfc412800ed6ba4"
   license "MIT"
   head "https://github.com/pypa/pipx.git", branch: "main"
 
@@ -26,6 +26,11 @@ class Pipx < Formula
   resource "click" do
     url "https://files.pythonhosted.org/packages/96/d3/f04c7bfcf5c1862a2a5b845c6b2b360488cf47af55dfa79c98f6a6bf98b5/click-8.1.7.tar.gz"
     sha256 "ca9853ad459e787e2192211578cc907e7594e294c7ccc834310722b41b9ca6de"
+  end
+
+  resource "platformdirs" do
+    url "https://files.pythonhosted.org/packages/31/28/e40d24d2e2eb23135f8533ad33d582359c7825623b1e022f9d460def7c05/platformdirs-4.0.0.tar.gz"
+    sha256 "cb633b2bcf10c51af60beb0ab06d2f1d69064b43abf4c185ca6b28865f3f9731"
   end
 
   resource "userpath" do
