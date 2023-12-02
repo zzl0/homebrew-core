c4be5ac5ef4e0fe22e89074b8e4ca6606f438237
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-12-02 01:14:35 +0000

    fnt: update 1.5 bottle.

--- Formula/f/fnt.rb
+++ Formula/f/fnt.rb
@@ -6,15 +6,11 @@ class Fnt < Formula
   license "MIT"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3354ea71f3c637ff74666a5561a77b6c717bfc319c96682cb6affc7593fe5cee"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3354ea71f3c637ff74666a5561a77b6c717bfc319c96682cb6affc7593fe5cee"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3354ea71f3c637ff74666a5561a77b6c717bfc319c96682cb6affc7593fe5cee"
-    sha256 cellar: :any_skip_relocation, ventura:        "fd63e7162fbaa82f98a00c390f07d2860c5732a22194abd0f4b3b2550c36fd96"
-    sha256 cellar: :any_skip_relocation, monterey:       "fd63e7162fbaa82f98a00c390f07d2860c5732a22194abd0f4b3b2550c36fd96"
-    sha256 cellar: :any_skip_relocation, big_sur:        "fd63e7162fbaa82f98a00c390f07d2860c5732a22194abd0f4b3b2550c36fd96"
-    sha256 cellar: :any_skip_relocation, catalina:       "fd63e7162fbaa82f98a00c390f07d2860c5732a22194abd0f4b3b2550c36fd96"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3354ea71f3c637ff74666a5561a77b6c717bfc319c96682cb6affc7593fe5cee"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "b229a7fc67dd933e6651538c17a9b103877d1d2c396cc59b2657c147b1ed5884"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b229a7fc67dd933e6651538c17a9b103877d1d2c396cc59b2657c147b1ed5884"
+    sha256 cellar: :any_skip_relocation, ventura:        "95687ee5860d1bdecaba8b81a981d3f6a57ee41fedb6f6a0266f6757a6e0cb07"
+    sha256 cellar: :any_skip_relocation, monterey:       "95687ee5860d1bdecaba8b81a981d3f6a57ee41fedb6f6a0266f6757a6e0cb07"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b229a7fc67dd933e6651538c17a9b103877d1d2c396cc59b2657c147b1ed5884"
   end
 
   depends_on "chafa"
