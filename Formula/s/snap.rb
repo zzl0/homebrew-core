d5950e9cb3d30e79430701831fbc3e6e5ab6ce5b
Porkepix <porkepix@gmail.com>
2023-12-02 02:43:32 +0100

    snap 2.61.1

--- Formula/s/snap.rb
+++ Formula/s/snap.rb
@@ -1,9 +1,9 @@
 class Snap < Formula
   desc "Tool to work with .snap files"
   homepage "https://snapcraft.io/"
-  url "https://github.com/snapcore/snapd/releases/download/2.61/snapd_2.61.vendor.tar.xz"
-  version "2.61"
-  sha256 "af30dc20c09872bf42faedb8f5306abc85faf9e3b010cb2388086cbc42a0909a"
+  url "https://github.com/snapcore/snapd/releases/download/2.61.1/snapd_2.61.1.vendor.tar.xz"
+  version "2.61.1"
+  sha256 "775b7a250f5241b3bfcebcb3df5055b9c9304c4520502b7abf12438a1cdd771d"
   license "GPL-3.0-only"
 
   livecheck do
