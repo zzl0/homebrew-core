866b7add5bc929b574e7f567de55fec82c885d26
Porkepix <porkepix@gmail.com>
2023-12-03 15:02:21 +0100

    asn 0.75.2

--- Formula/a/asn.rb
+++ Formula/a/asn.rb
@@ -1,8 +1,8 @@
 class Asn < Formula
   desc "Organization lookup and server tool (ASN / IPv4 / IPv6 / Prefix / AS Path)"
   homepage "https://github.com/nitefood/asn"
-  url "https://github.com/nitefood/asn/archive/refs/tags/v0.75.tar.gz"
-  sha256 "98c52bfb02b36342ebdea0b9d7c487128576027c76bf39519d2ddc8f15997add"
+  url "https://github.com/nitefood/asn/archive/refs/tags/v0.75.2.tar.gz"
+  sha256 "b3d2f768f296bd8a8e6ab9f8e28287915390feca661d20110518c79580871b00"
   license "MIT"
   head "https://github.com/nitefood/asn.git", branch: "master"
 
