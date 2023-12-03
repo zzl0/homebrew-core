b612fd872660afb94d880b65de614f360e4bce6f
Porkepix <porkepix@gmail.com>
2023-12-03 14:46:52 +0100

    borgbackup 1.2.7

--- Formula/b/borgbackup.rb
+++ Formula/b/borgbackup.rb
@@ -1,8 +1,8 @@
 class Borgbackup < Formula
   desc "Deduplicating archiver with compression and authenticated encryption"
   homepage "https://borgbackup.org/"
-  url "https://files.pythonhosted.org/packages/cd/a2/d4375923a8b858312e6f4593ac7f613d338394f3feb669f67d2a6269d2f9/borgbackup-1.2.6.tar.gz"
-  sha256 "b7a6f8f086039eeec79070b914f3c651ed7f3612c965374af910d277c7a2139d"
+  url "https://files.pythonhosted.org/packages/a6/19/f94be9fda92ea73cbf22b643a03a0b64559027ef5467765142d8242e712a/borgbackup-1.2.7.tar.gz"
+  sha256 "f63f28a3383c041971cec87b061ca39a815b5fd445db24aa8172cac417d9411a"
   license "BSD-3-Clause"
 
   bottle do
@@ -36,9 +36,15 @@ class Borgbackup < Formula
     "python3.12"
   end
 
-  # master build already support 1.0.7 without much change
-  # new release requst issue, https://github.com/borgbackup/borg/issues/7948
-  patch :DATA
+  resource "msgpack" do
+    url "https://files.pythonhosted.org/packages/c2/d5/5662032db1571110b5b51647aed4b56dfbd01bfae789fa566a2be1f385d1/msgpack-1.0.7.tar.gz"
+    sha256 "572efc93db7a4d27e404501975ca6d2d9775705c2d922390d878fcf768d92c87"
+  end
+
+  resource "packaging" do
+    url "https://files.pythonhosted.org/packages/fb/2b/9b9c33ffed44ee921d0967086d653047286054117d584f1b1a7c22ceaf7b/packaging-23.2.tar.gz"
+    sha256 "048fb0e9405036518eaaf48a55953c750c11e1a1b68e0dd1a9d62ed0c092cfc5"
+  end
 
   def install
     ENV["BORG_LIBB2_PREFIX"] = Formula["libb2"].prefix
@@ -69,18 +75,3 @@ class Borgbackup < Formula
     assert_equal File.size(testpath/"restore/test.pdf"), File.size(testpath/"test.pdf")
   end
 end
-
-__END__
-diff --git a/src/borg/helpers/msgpack.py b/src/borg/helpers/msgpack.py
-index 309c988..197d2de 100644
---- a/src/borg/helpers/msgpack.py
-+++ b/src/borg/helpers/msgpack.py
-@@ -182,7 +182,7 @@ def is_slow_msgpack():
- def is_supported_msgpack():
-     # DO NOT CHANGE OR REMOVE! See also requirements and comments in setup.py.
-     import msgpack
--    return (0, 5, 6) <= msgpack.version <= (1, 0, 5) and \
-+    return (0, 5, 6) <= msgpack.version <= (1, 0, 7) and \
-            msgpack.version not in [(1, 0, 1), ]  # < add bad releases here to deny list
- 
- 
