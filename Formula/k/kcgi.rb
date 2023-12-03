88e9f5fbd4b63d189f490c3b7007be103fd74228
Porkepix <porkepix@gmail.com>
2023-12-03 23:31:53 +0100

    kcgi 0.13.3
    kcgi: needs zlib for linux build
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/k/kcgi.rb
+++ Formula/k/kcgi.rb
@@ -1,8 +1,8 @@
 class Kcgi < Formula
   desc "Minimal CGI and FastCGI library for C/C++"
   homepage "https://kristaps.bsd.lv/kcgi/"
-  url "https://kristaps.bsd.lv/kcgi/snapshots/kcgi-0.13.1.tgz"
-  sha256 "3246ee78a091b496f7040c5f29fb9e45a7aa2873f4d8d77a30b6437f07db4d49"
+  url "https://kristaps.bsd.lv/kcgi/snapshots/kcgi-0.13.3.tgz"
+  sha256 "1c13538e21511086a6ba1a87f40543de257fc3d8871840b17626c16d714d2f5a"
   license "ISC"
 
   livecheck do
@@ -21,6 +21,8 @@ class Kcgi < Formula
   end
 
   depends_on "bmake" => :build
+
+  uses_from_macos "zlib"
 
   on_linux do
     depends_on "libseccomp"
