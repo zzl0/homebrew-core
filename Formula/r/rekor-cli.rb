27d2a7faa204dbba250c5a1fd9ef1949a971df36
Patrick Linnane <patrick@linnane.io>
2023-12-05 11:07:42 -0800

    rekor-cli 1.3.4

--- Formula/r/rekor-cli.rb
+++ Formula/r/rekor-cli.rb
@@ -1,8 +1,8 @@
 class RekorCli < Formula
   desc "CLI for interacting with Rekor"
   homepage "https://docs.sigstore.dev/logging/overview/"
-  url "https://github.com/sigstore/rekor/archive/refs/tags/v1.3.3.tar.gz"
-  sha256 "2e4e75fcae81ef85e6e3c20e00a9b590bfa86c0706a6c902024222cd61b64c47"
+  url "https://github.com/sigstore/rekor/archive/refs/tags/v1.3.4.tar.gz"
+  sha256 "08e220b6fbc473ecd3561e88c4fde2ca259f9daa895a17bed1f458c33c33a2b9"
   license "Apache-2.0"
 
   bottle do
