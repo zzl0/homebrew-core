478e6ffe09ab091829bde994ad4b86ef650de3fb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-12-05 09:51:57 +0000

    trivy 0.48.0

--- Formula/t/trivy.rb
+++ Formula/t/trivy.rb
@@ -1,8 +1,8 @@
 class Trivy < Formula
   desc "Vulnerability scanner for container images, file systems, and Git repos"
   homepage "https://aquasecurity.github.io/trivy/"
-  url "https://github.com/aquasecurity/trivy/archive/refs/tags/v0.47.0.tar.gz"
-  sha256 "ed4c7d62cf651d5fc2fdef6cd208c0f1dfcf6b177971dcc9478415a9394510b8"
+  url "https://github.com/aquasecurity/trivy/archive/refs/tags/v0.48.0.tar.gz"
+  sha256 "3ae1fa2989b25f9b993b60169275abbb8c7eaba30f88792d0d18307db8fd85e6"
   license "Apache-2.0"
   head "https://github.com/aquasecurity/trivy.git", branch: "main"
 
