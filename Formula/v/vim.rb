d252654975717880a9d82d3ab0cc66a2836dfda9
Patrick Linnane <patrick@linnane.io>
2023-12-05 11:17:46 -0800

    vim 9.0.2150

--- Formula/v/vim.rb
+++ Formula/v/vim.rb
@@ -2,8 +2,8 @@ class Vim < Formula
   desc "Vi 'workalike' with many additional features"
   homepage "https://www.vim.org/"
   # vim should only be updated every 50 releases on multiples of 50
-  url "https://github.com/vim/vim/archive/refs/tags/v9.0.2100.tar.gz"
-  sha256 "01e08b3234a357f6b17e80a695390ed537b50d10b9c95bf5b1ec9182e2c0725c"
+  url "https://github.com/vim/vim/archive/refs/tags/v9.0.2150.tar.gz"
+  sha256 "3d4c1b2bddaf9f5e29ebdab1447d030de77853f504063e69979c8e41580c6d8d"
   license "Vim"
   head "https://github.com/vim/vim.git", branch: "master"
 
