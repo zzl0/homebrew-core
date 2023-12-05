166ff2793b5d1fd2e4d2738ce481c194f019d07d
Patrick Linnane <patrick@linnane.io>
2023-12-05 11:09:30 -0800

    snort 3.1.76.0

--- Formula/s/snort.rb
+++ Formula/s/snort.rb
@@ -1,9 +1,9 @@
 class Snort < Formula
   desc "Flexible Network Intrusion Detection System"
   homepage "https://www.snort.org"
-  url "https://github.com/snort3/snort3/archive/refs/tags/3.1.75.0.tar.gz"
-  mirror "https://fossies.org/linux/misc/snort3-3.1.75.0.tar.gz"
-  sha256 "c1a1b7d00df5ab45df968f0fb0125eba95bad27c181018b8d68a41e1bb6fc111"
+  url "https://github.com/snort3/snort3/archive/refs/tags/3.1.76.0.tar.gz"
+  mirror "https://fossies.org/linux/misc/snort3-3.1.76.0.tar.gz"
+  sha256 "5586199be8b7a7c6a1b73e0af2e2e004db8417b8282668b10583071e35c9c7a9"
   license "GPL-2.0-only"
   head "https://github.com/snort3/snort3.git", branch: "master"
 
