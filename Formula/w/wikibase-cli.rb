47a7b93f2dcb877e1e25596afd82f5a697527d0b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-12-05 22:28:54 +0000

    wikibase-cli: update 17.0.6 bottle.

--- Formula/w/wikibase-cli.rb
+++ Formula/w/wikibase-cli.rb
@@ -8,13 +8,13 @@ class WikibaseCli < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "a32ddeb87aaf17d3280110c611f784b99ae9d2a3aa2c6f8687f834b515913136"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a32ddeb87aaf17d3280110c611f784b99ae9d2a3aa2c6f8687f834b515913136"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a32ddeb87aaf17d3280110c611f784b99ae9d2a3aa2c6f8687f834b515913136"
-    sha256 cellar: :any_skip_relocation, sonoma:         "76d97acd1640e0b510927943921ef7cd390b8bfd136750e0d56ea7832cbd20ce"
-    sha256 cellar: :any_skip_relocation, ventura:        "76d97acd1640e0b510927943921ef7cd390b8bfd136750e0d56ea7832cbd20ce"
-    sha256 cellar: :any_skip_relocation, monterey:       "76d97acd1640e0b510927943921ef7cd390b8bfd136750e0d56ea7832cbd20ce"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a32ddeb87aaf17d3280110c611f784b99ae9d2a3aa2c6f8687f834b515913136"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "d3496537ce219910c28a5961f0ac8ff94a70eed8406e2a92e8b289f886894e38"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "d3496537ce219910c28a5961f0ac8ff94a70eed8406e2a92e8b289f886894e38"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "d3496537ce219910c28a5961f0ac8ff94a70eed8406e2a92e8b289f886894e38"
+    sha256 cellar: :any_skip_relocation, sonoma:         "352d7a80a111b2ab792bd22111b2f11ac9c70a289a2ee26edbb357cbe641314d"
+    sha256 cellar: :any_skip_relocation, ventura:        "352d7a80a111b2ab792bd22111b2f11ac9c70a289a2ee26edbb357cbe641314d"
+    sha256 cellar: :any_skip_relocation, monterey:       "352d7a80a111b2ab792bd22111b2f11ac9c70a289a2ee26edbb357cbe641314d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d3496537ce219910c28a5961f0ac8ff94a70eed8406e2a92e8b289f886894e38"
   end
 
   depends_on "node"
