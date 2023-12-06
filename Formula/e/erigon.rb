8f59f68484e58cc4badddc5fecb5c3b74fc63dd1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-12-06 18:37:12 +0000

    erigon 2.55.1

--- Formula/e/erigon.rb
+++ Formula/e/erigon.rb
@@ -1,8 +1,8 @@
 class Erigon < Formula
   desc "Implementation of Ethereum (execution client), on the efficiency frontier"
   homepage "https://github.com/ledgerwatch/erigon"
-  url "https://github.com/ledgerwatch/erigon/archive/refs/tags/v2.55.0.tar.gz"
-  sha256 "ab6b76f87469d7ee803e1d4090b7f283d8494bfa6a6be7e45cd9de44eaa80e0c"
+  url "https://github.com/ledgerwatch/erigon/archive/refs/tags/v2.55.1.tar.gz"
+  sha256 "2e340bb5504f565bb9fe8c4d246dea50bd54bfcf6a91e9196aeb4fbda722ae9e"
   license all_of: ["GPL-3.0-or-later", "LGPL-3.0-or-later"]
   head "https://github.com/ledgerwatch/erigon.git", branch: "devel"
 
