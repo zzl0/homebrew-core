46e6237454ce1627c7952d8aa83f1f0d9e2bc703
Alistair Scott <aliscott@users.noreply.github.com>
2023-12-06 09:57:19 +0000

    infracost 0.10.31

--- Formula/i/infracost.rb
+++ Formula/i/infracost.rb
@@ -1,8 +1,8 @@
 class Infracost < Formula
   desc "Cost estimates for Terraform"
   homepage "https://www.infracost.io/docs/"
-  url "https://github.com/infracost/infracost/archive/refs/tags/v0.10.30.tar.gz"
-  sha256 "fb08b9b7c81e3874efaf27eb9660545875f4aa75e8efb682205333a3929145c4"
+  url "https://github.com/infracost/infracost/archive/refs/tags/v0.10.31.tar.gz"
+  sha256 "53905d6e63e05634ecc643f6166fd0ec977bcbb29409557dc290f5f7eb72c1f3"
   license "Apache-2.0"
   head "https://github.com/infracost/infracost.git", branch: "master"
 
