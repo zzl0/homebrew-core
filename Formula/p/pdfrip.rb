d57532684f26e8bdacc7acc967de7730409173dc
Patrick Linnane <patrick@linnane.io>
2023-12-06 00:36:34 +0000

    pdfrip: add 1.0.0 bottle.

--- Formula/p/pdfrip.rb
+++ Formula/p/pdfrip.rb
@@ -6,6 +6,16 @@ class Pdfrip < Formula
   license "MIT"
   head "https://github.com/mufeedvh/pdfrip.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "b91c5633810d0154329c65530b762949d40594dae0c8e5eb6f4ede599653e338"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "bfdbac5a1186ef16379f695e6b65112fa3a93549d8df59cd1563deaf7b5e201f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "36a3127e437066e814ca4d4458350793ef7da38d059c0c738e184710057407bd"
+    sha256 cellar: :any_skip_relocation, sonoma:         "3b3faefa6e674ecf90e770a4aae8720eda69fd1d1b2dc532d0524762dc07ae48"
+    sha256 cellar: :any_skip_relocation, ventura:        "c232fbe1a2bc4edcad7a20b5df89608bab5144018adfb92ddf2cf5108f75c9db"
+    sha256 cellar: :any_skip_relocation, monterey:       "8057bca1c775488e5bd12ea90514465f8977d2851f796cbee92aa2ff16ba6dbf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "58d857cf8ae9061d5516419d0dfcbde4bbed6cedf44861137cf0f9e7afff30b8"
+  end
+
   depends_on "cmake" => :build
   depends_on "rust" => :build
 
