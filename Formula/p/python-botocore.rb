d60ae21ef8dabf143b062cb3591d60368cddf588
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-12-06 00:00:06 +0000

    python-botocore: update 1.33.7 bottle.

--- Formula/p/python-botocore.rb
+++ Formula/p/python-botocore.rb
@@ -6,13 +6,13 @@ class PythonBotocore < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "d4c9f1936443173a75023560edd71b8f567bb9fb83b544a13b9fb38347f27b90"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "709270e8cf9f56670d6c1ec4d6c152bc8adca63e4cda449284f4cbe60fb96f06"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "26a8ae3cfeef0862490639b2ee24e068e3fe79fbc4bf1c763260bfbce857c824"
-    sha256 cellar: :any_skip_relocation, sonoma:         "e666952e88937437c997b5498abf498120674d0ee6b7299d578f275aaebd9d15"
-    sha256 cellar: :any_skip_relocation, ventura:        "6419284c779c9e41819205811947067ca1ead15ce61ad85eab48f059992da2a6"
-    sha256 cellar: :any_skip_relocation, monterey:       "cdf811e7381dcf166d5a1a29c8cb14bc1e244ddc3c0aceba64348eb0f62c85ce"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0638d2c50d8f4248ff70a516081b73c8377eef0980cdcb321b9fb5d24e8ed1ef"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "e2ebaa40ef05ce8dfc658b32c154df3daf2a0556d837457ecf7e5393cd9c43fd"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0e2e097925124b2f90af0c5f914e360e3fd4efca27137971217f0f012306355a"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "0691911663dcf7194918946bec53ba9d10ae9418eeaef4076b9f7f7c6a3e16ab"
+    sha256 cellar: :any_skip_relocation, sonoma:         "4277054b575791717070ce23ca255f3dbca7256c70ea60b4b3a6e471a3b5e8b4"
+    sha256 cellar: :any_skip_relocation, ventura:        "a29cd3ac60857bbeb67a136289dcda273674ec5780f8d44d18750de17a3bc87a"
+    sha256 cellar: :any_skip_relocation, monterey:       "4e7a555a87764d8e6c63ad68f6e8cbeaa252c482d96f269d7c356249d0b3457a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3557819e83b780861c28c73727158f50779f08587a939ec0108e553cdc587bc7"
   end
 
   depends_on "python-setuptools" => :build
