babfd4256dc729ff97bc028c50a8f475a6aab25c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-12-06 00:33:54 +0000

    cfengine 3.23.0
    cfengine: update resource
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Michael Cho <michael@michaelcho.dev>

--- Formula/c/cfengine.rb
+++ Formula/c/cfengine.rb
@@ -1,8 +1,8 @@
 class Cfengine < Formula
   desc "Help manage and understand IT infrastructure"
   homepage "https://cfengine.com/"
-  url "https://cfengine-package-repos.s3.amazonaws.com/tarballs/cfengine-community-3.22.0.tar.gz"
-  sha256 "c2e7ac88d2371fda2809e0bbd3a3908cab50592b6c1479353bd18ba809b93528"
+  url "https://cfengine-package-repos.s3.amazonaws.com/tarballs/cfengine-community-3.23.0.tar.gz"
+  sha256 "fcf4b6ddb325ffe99c949cdee8d0f5a40b5f5c9a482df1a4eba0b38892d354f9"
   license all_of: ["BSD-3-Clause", "GPL-2.0-or-later", "GPL-3.0-only", "LGPL-2.0-or-later"]
 
   livecheck do
@@ -26,6 +26,7 @@ class Cfengine < Formula
   depends_on "openssl@3"
   depends_on "pcre"
 
+  uses_from_macos "curl", since: :ventura # uses CURLOPT_PROTOCOLS_STR, available since curl 7.85.0
   uses_from_macos "libxml2"
 
   on_linux do
@@ -33,8 +34,8 @@ class Cfengine < Formula
   end
 
   resource "masterfiles" do
-    url "https://cfengine-package-repos.s3.amazonaws.com/tarballs/cfengine-masterfiles-3.22.0.tar.gz"
-    sha256 "5eae6ba6881de1aef4f8115cd6b6ecb0c0809c7ea956b7097a99d62c014506ea"
+    url "https://cfengine-package-repos.s3.amazonaws.com/tarballs/cfengine-masterfiles-3.23.0.tar.gz"
+    sha256 "47a47afa141d980793b1b4f832095bc3e680bcec2a4534458c86cbf1649368e2"
   end
 
   def install
