47b96a87392d0471703bfcbb50dae73da668ce5d
Alexander F. Rødseth <52813+xyproto@users.noreply.github.com>
2023-12-06 10:19:53 +0100

    algernon 1.15.5
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/a/algernon.rb
+++ Formula/a/algernon.rb
@@ -1,8 +1,8 @@
 class Algernon < Formula
   desc "Pure Go web server with Lua, Markdown, HTTP/2 and template support"
   homepage "https://github.com/xyproto/algernon"
-  url "https://github.com/xyproto/algernon/archive/refs/tags/v1.15.4.tar.gz"
-  sha256 "8db84c35fcc87e0a376a44e7406d6168a7c90a588c7a4045c6ede272a5d9a3c2"
+  url "https://github.com/xyproto/algernon/archive/refs/tags/v1.15.5.tar.gz"
+  sha256 "00440c18ebe08c1498c0ddfbebe10e5d70936251295d9ab0d8673a431e38cd48"
   license "BSD-3-Clause"
   version_scheme 1
   head "https://github.com/xyproto/algernon.git", branch: "main"
