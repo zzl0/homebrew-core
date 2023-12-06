17a005ae7091f377df52594c43c3c179fdd638b9
Patrick Linnane <patrick@linnane.io>
2023-12-06 11:57:13 -0800

    retire 4.3.4 (new formula)

---
+++ Formula/r/retire.rb
@@ -0,0 +1,25 @@
+require "language/node"
+
+class Retire < Formula
+  desc "Scanner detecting the use of JavaScript libraries with known vulnerabilities"
+  homepage "https://retirejs.github.io/retire.js/"
+  url "https://registry.npmjs.org/retire/-/retire-4.3.4.tgz"
+  sha256 "3c4477797280550ebcd6559cf9ba1d619239c04dd4002128372315e2fa59b20c"
+  license "Apache-2.0"
+  head "https://github.com/RetireJS/retire.js.git", branch: "master"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/retire --version")
+
+    system "git", "clone", "https://github.com/appsecco/dvna.git"
+    output = shell_output("#{bin}/retire --path dvna 2>&1", 13)
+    assert_match(/jquery (\d+(?:\.\d+)+) has known vulnerabilities/, output)
+  end
+end
