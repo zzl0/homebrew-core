f65f7766104461559d8af702611da7e6e659100e
MoonFruit <dkmoonfruit@gmail.com>
2023-12-07 12:25:15 +0800

    ugrep 4.3.5

--- Formula/u/ugrep.rb
+++ Formula/u/ugrep.rb
@@ -1,8 +1,8 @@
 class Ugrep < Formula
   desc "Ultra fast grep with query UI, fuzzy search, archive search, and more"
   homepage "https://github.com/Genivia/ugrep"
-  url "https://github.com/Genivia/ugrep/archive/refs/tags/v4.3.4.tar.gz"
-  sha256 "d206a76b3bc103a0c5187f973943cbd8339995a289f62a237a0e658ecb2d0743"
+  url "https://github.com/Genivia/ugrep/archive/refs/tags/v4.3.5.tar.gz"
+  sha256 "e7ba12da8c1ac4b9a1752e11aecc90067289b149e47d214f5befb0f63a1a7728"
   license "BSD-3-Clause"
 
   bottle do
