bc612d77b5da5297a2b5d490933e48619ee62cab
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-12-08 18:40:27 +0000

    doctl 1.101.0

--- Formula/d/doctl.rb
+++ Formula/d/doctl.rb
@@ -1,8 +1,8 @@
 class Doctl < Formula
   desc "Command-line tool for DigitalOcean"
   homepage "https://github.com/digitalocean/doctl"
-  url "https://github.com/digitalocean/doctl/archive/refs/tags/v1.100.0.tar.gz"
-  sha256 "ecfbcdb576271e79834ae28fe0cd7bcb29307f68f179de247af1a68015a755cd"
+  url "https://github.com/digitalocean/doctl/archive/refs/tags/v1.101.0.tar.gz"
+  sha256 "093ace8f58e2ef3b07a2cae8b21a7a6ff0a8dbff9dc8040954d8d12691634364"
   license "Apache-2.0"
   head "https://github.com/digitalocean/doctl.git", branch: "main"
 
