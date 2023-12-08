2b6095352a038d7f811daa18b3c3515ca6214153
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-12-08 13:00:52 +0000

    linux-headers@5.15: update 5.15.142 bottle.

--- Formula/l/linux-headers@5.15.rb
+++ Formula/l/linux-headers@5.15.rb
@@ -11,7 +11,7 @@ class LinuxHeadersAT515 < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "c3a59fb3c080a8f620e3b90571e0c1215bc1023d5a6ff7fad63efcd5635b576f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "76f1dc31141815dd54b84035d095176693ab9a9e94a78e560e6a588674f8a47e"
   end
 
   depends_on :linux
