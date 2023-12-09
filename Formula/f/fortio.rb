910f11d101659734a65647e13e342b85b9930c14
Porkepix <porkepix@gmail.com>
2023-12-09 03:24:00 +0100

    fortio 1.63.0

--- Formula/f/fortio.rb
+++ Formula/f/fortio.rb
@@ -2,8 +2,8 @@ class Fortio < Formula
   desc "HTTP and gRPC load testing and visualization tool and server"
   homepage "https://fortio.org/"
   url "https://github.com/fortio/fortio.git",
-      tag:      "v1.62.1",
-      revision: "a95f803d5082c3745ae02315bbc24f1608182009"
+      tag:      "v1.63.0",
+      revision: "6b492e20fffc070f3ab624cf960793a01651cd19"
   license "Apache-2.0"
 
   # There can be a notable gap between when a version is tagged and a
