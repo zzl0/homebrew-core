4b1f1e60db77070d45b138d21be75d5dac0d4166
Michael Cho <michael@michaelcho.dev>
2023-12-10 17:41:24 -0500

    widelands 1.1

--- Formula/w/widelands.rb
+++ Formula/w/widelands.rb
@@ -1,10 +1,9 @@
 class Widelands < Formula
   desc "Free real-time strategy game like Settlers II"
   homepage "https://www.widelands.org/"
-  url "https://github.com/widelands/widelands/archive/refs/tags/v1.0.tar.gz"
-  sha256 "1dab0c4062873cc72c5e0558f9e9620b0ef185f1a78923a77c4ce5b9ed76031a"
+  url "https://github.com/widelands/widelands/archive/refs/tags/v1.1.tar.gz"
+  sha256 "6853fcf3daec9b66005691e5bcb00326634baf0985ad89a7e6511502612f6412"
   license "GPL-2.0-or-later"
-  revision 4
   version_scheme 1
 
   livecheck do
@@ -24,41 +23,40 @@ class Widelands < Formula
     sha256 x86_64_linux:   "510fcbb44beed3d73da985066e4b025bba843087da917716f1a4d71112de39a2"
   end
 
+  depends_on "asio" => :build
   depends_on "cmake" => :build
-  depends_on "boost"
-  depends_on "doxygen"
-  depends_on "gettext"
+  depends_on "doxygen" => :build
+  depends_on "gettext" => :build
+  depends_on "pkg-config" => :build
   depends_on "glew"
   depends_on "icu4c"
   depends_on "libpng"
   depends_on "lua"
   depends_on "minizip"
+  depends_on "sdl2"
   depends_on "sdl2_image"
   depends_on "sdl2_mixer"
   depends_on "sdl2_ttf"
 
   uses_from_macos "python" => :build
-  uses_from_macos "curl"
+  uses_from_macos "zlib"
 
-  # Fix build with Boost 1.77+.
-  # Remove with the next release (1.1).
-  patch do
-    url "https://github.com/widelands/widelands/commit/316eaea209754368a57f445ea4dd016ecf8eded6.patch?full_index=1"
-    sha256 "358cae53bbc854e7e9248bdea0ca5af8bce51e188626a7f366bc6a87abd33dc9"
+  on_macos do
+    depends_on "gettext"
   end
 
   def install
-    ENV.cxx11
     system "cmake", "-S", ".", "-B", "build",
-                    # Without the following option, Cmake intend to use the library of MONO framework.
-                    "-DPNG_PNG_INCLUDE_DIR:PATH=#{Formula["libpng"].opt_include}",
+                    "-DWL_INSTALL_BASEDIR=#{pkgshare}",
+                    "-DWL_INSTALL_BINDIR=#{bin}",
                     "-DWL_INSTALL_DATADIR=#{pkgshare}/data",
-                    # older versions of macOS may not have `python3`
+                    "-DOPTION_BUILD_CODECHECK=OFF",
+                    "-DOPTION_BUILD_TESTS=OFF",
+                    "-DOPTION_BUILD_WEBSITE_TOOLS=OFF",
                     "-DPYTHON_EXECUTABLE=#{which("python3") || which("python")}",
                     *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
-    bin.write_exec_script prefix/"widelands"
   end
 
   test do
