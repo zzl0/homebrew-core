f4f2094e0317f3fb058a77e6a7c21c336e20808a
Michael Cho <michael@michaelcho.dev>
2023-12-10 12:57:18 -0500

    python-markupsafe: remove `python@3.10` support

--- Formula/p/python-markupsafe.rb
+++ Formula/p/python-markupsafe.rb
@@ -17,7 +17,6 @@ class PythonMarkupsafe < Formula
   end
 
   depends_on "python-setuptools" => :build
-  depends_on "python@3.10" => [:build, :test]
   depends_on "python@3.11" => [:build, :test]
   depends_on "python@3.12" => [:build, :test]
 
