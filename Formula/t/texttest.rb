b62e4819500da8e5f31a3c65c2ee907d412a87cb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-12-11 17:15:23 +0000

    texttest: update 4.3.1 bottle.

--- Formula/t/texttest.rb
+++ Formula/t/texttest.rb
@@ -8,13 +8,13 @@ class Texttest < Formula
   license "LGPL-2.1-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "9ac65b78ffbda96051f7ef01afc307eaafced2bd290fcb70d84e89ba07397406"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "e10a6420ea6e6fc53c1bb499d0f94550c7acc77407ab67d3b74eac7f4961861a"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b8fb5a98453edae0db4b2a175b5a7dd47f7a429a88b725835c0a0145158b3945"
-    sha256 cellar: :any_skip_relocation, sonoma:         "ab54ed9f3764c2733c4bfebcd3575b0f627568c1c772407a39cb98a64f1773b8"
-    sha256 cellar: :any_skip_relocation, ventura:        "13a2039dff6eba8bb0eb8c8be0ead4949cc7c976df99fb71b6ed53a30fe11388"
-    sha256 cellar: :any_skip_relocation, monterey:       "ac74fc81df8021f736223d96f464f9a7959fa0825f7fe8cf0abe5003f91e1fad"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "468a696b7a676bbafbd479e23dbc14ce6a9e829ec21c746a751999b1425002f9"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "356098cb25f79a7e9af7bf4b6a2b8480f6affa1ff13e8c863c24313f2060fe0e"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0a6a3c22805677216e4abdd709461ef81ff8ef2d2ee792630ee9b150f32de780"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "0f77d2351320d6bb33e59ef67b772b37d1bd589cfcfb928e7f38944af2671690"
+    sha256 cellar: :any_skip_relocation, sonoma:         "7f6bd92d9241fadc56fbd829e37927c7b5a12b1e15b2c276366425c300e3d2bc"
+    sha256 cellar: :any_skip_relocation, ventura:        "836a0421a967bf3464adce4d8be9faee8d175a9e62cca8a6f1336d07070b3173"
+    sha256 cellar: :any_skip_relocation, monterey:       "2eae1663be9f971f0d729ff0ee91ed9e0c5e6d7b4b85900f90192de5f7216e07"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "32fd474b7f1c1090b9d06a302ac520c82c0f78f5431606ac4eca3f9920956e5c"
   end
 
   depends_on "adwaita-icon-theme"
