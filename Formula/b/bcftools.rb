3938db8a45b2d30a2e3d5fef5def1564aee96a7a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-12-12 18:27:17 +0000

    bcftools 1.19

--- Formula/b/bcftools.rb
+++ Formula/b/bcftools.rb
@@ -1,8 +1,8 @@
 class Bcftools < Formula
   desc "Tools for BCF/VCF files and variant calling from samtools"
   homepage "https://www.htslib.org/"
-  url "https://github.com/samtools/bcftools/releases/download/1.18/bcftools-1.18.tar.bz2"
-  sha256 "d9b9d36293e4cc62ab7473aa2539389d4e1de79b1a927d483f6e91f3c3ceac7e"
+  url "https://github.com/samtools/bcftools/releases/download/1.19/bcftools-1.19.tar.bz2"
+  sha256 "782b5f1bc690415192231e82213b3493b047f45e630dc8ef6f154d6126ab3e68"
   # The bcftools source code is MIT/Expat-licensed, but when it is configured
   # with --enable-libgsl the resulting executable is GPL-licensed.
   license "GPL-3.0-or-later"
