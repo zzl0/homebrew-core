418755f66cddda90fead94f8bde0ddcd6b97d928
ik-fib <ik11235@gmail.com>
2023-12-13 01:05:47 +0900

    libxml2 2.12.3

--- Formula/lib/libxml2.rb
+++ Formula/lib/libxml2.rb
@@ -1,8 +1,8 @@
 class Libxml2 < Formula
   desc "GNOME XML library"
   homepage "http://xmlsoft.org/"
-  url "https://download.gnome.org/sources/libxml2/2.12/libxml2-2.12.2.tar.xz"
-  sha256 "3f2e6464fa15073eb8f3d18602d54fafc489b7715171064615a40490c6be9f4f"
+  url "https://download.gnome.org/sources/libxml2/2.12/libxml2-2.12.3.tar.xz"
+  sha256 "8c8f1092340a89ff32bc44ad5c9693aff9bc8a7a3e161bb239666e5d15ac9aaa"
   license "MIT"
 
   # We use a common regex because libxml2 doesn't use GNOME's "even-numbered
