32e41df32a518271cc3993404125d35a494274ed
Richie <a83912a@gmail.com>
2023-12-12 16:57:56 +0800

    libvatek 3.10

--- Formula/lib/libvatek.rb
+++ Formula/lib/libvatek.rb
@@ -1,8 +1,8 @@
 class Libvatek < Formula
   desc "User library to control VATek chips"
   homepage "https://github.com/VisionAdvanceTechnologyInc/vatek_sdk_2"
-  url "https://github.com/VisionAdvanceTechnologyInc/vatek_sdk_2/archive/refs/tags/v3.08.tar.gz"
-  sha256 "b2ccf79d2575a9a63faac394e739ed483ed6267039e55cebcb7c4b35bf76122e"
+  url "https://github.com/VisionAdvanceTechnologyInc/vatek_sdk_2/archive/refs/tags/v3.10.tar.gz"
+  sha256 "cf445ba21c78ddc013b8f123719543156774e0878183dcef058611871d34ec45"
   license "BSD-2-Clause"
   head "https://github.com/VisionAdvanceTechnologyInc/vatek_sdk_2.git", branch: "main"
 
