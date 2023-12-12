1356464f0bc31b58b952f3942fb1665d8b28008d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-12-12 16:10:26 +0000

    tomcat 10.1.17

--- Formula/t/tomcat.rb
+++ Formula/t/tomcat.rb
@@ -1,9 +1,9 @@
 class Tomcat < Formula
   desc "Implementation of Java Servlet and JavaServer Pages"
   homepage "https://tomcat.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=tomcat/tomcat-10/v10.1.16/bin/apache-tomcat-10.1.16.tar.gz"
-  mirror "https://archive.apache.org/dist/tomcat/tomcat-10/v10.1.16/bin/apache-tomcat-10.1.16.tar.gz"
-  sha256 "432b0b98a377450f13b9a47b82ea7df78ed0bcf2ee4b759182b4c7fdc1fe5841"
+  url "https://www.apache.org/dyn/closer.lua?path=tomcat/tomcat-10/v10.1.17/bin/apache-tomcat-10.1.17.tar.gz"
+  mirror "https://archive.apache.org/dist/tomcat/tomcat-10/v10.1.17/bin/apache-tomcat-10.1.17.tar.gz"
+  sha256 "ba89ba45c13c54511bb4201758e93fe902970abd163b42f5714668828f13d991"
   license "Apache-2.0"
 
   bottle do
