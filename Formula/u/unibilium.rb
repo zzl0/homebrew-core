6afc92ff70ad5ccc580ab2c547f69c4f71ea8c4b
Francois-Xavier Coudert <fxcoudert@gmail.com>
2023-12-13 20:58:41 +0100

    unibilium: remove Sonoma workaround

--- Formula/u/unibilium.rb
+++ Formula/u/unibilium.rb
@@ -23,13 +23,7 @@ class Unibilium < Formula
     terminfo_dirs = [Formula["ncurses"].opt_share/"terminfo"]
 
     terminfo_dirs += if OS.mac?
-      if MacOS.version == :sonoma
-        # There is a bug in ncurses5.4-config in the 14.0 SDK (FB13204756)
-        # We know what it should output, so enforce it.
-        ["/usr/share/terminfo"]
-      else
-        [Utils.safe_popen_read("ncurses5.4-config", "--terminfo-dirs").strip]
-      end
+      [Utils.safe_popen_read("ncurses5.4-config", "--terminfo-dirs").strip]
     else
       # Unibilium's default terminfo path
       %w[
