10830db9455f49c1fb2da5a6c11f198c78abd89c
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-12-13 16:11:16 -0500

    python-hatchling 1.20.0
    python-hatchling: needs `python-flit-core` build dep
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/p/python-hatchling.rb
+++ Formula/p/python-hatchling.rb
@@ -1,8 +1,8 @@
 class PythonHatchling < Formula
   desc "Modern, extensible Python build backend"
   homepage "https://github.com/pypa/hatch/tree/master/backend"
-  url "https://files.pythonhosted.org/packages/e3/57/87da2c5adc173950ebe9f1acce4d5f2cd0a960783992fd4879a899a0b637/hatchling-1.18.0.tar.gz"
-  sha256 "50e99c3110ce0afc3f7bdbadff1c71c17758e476731c27607940cfa6686489ca"
+  url "https://files.pythonhosted.org/packages/0d/ee/2afbbb1e8e17a792e83e894e35e4a151dff121aa841ebdbe92f4b51779fe/hatchling-1.20.0.tar.gz"
+  sha256 "0e0893cbe3d5f9275fc0e5b629087fc23b17abd7065e4db0a310e0a0237bc945"
   license "MIT"
 
   bottle do
@@ -15,6 +15,7 @@ class PythonHatchling < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "622d5f5c2ca82cbb86fd94a962ab12e85048b63fd4fd59ffb2562c6b0f3920da"
   end
 
+  depends_on "python-flit-core" => :build
   depends_on "python-setuptools" => :build
   depends_on "python@3.11" => [:build, :test]
   depends_on "python@3.12" => [:build, :test]
@@ -24,8 +25,28 @@ class PythonHatchling < Formula
   depends_on "python-trove-classifiers"
 
   resource "editables" do
-    url "https://files.pythonhosted.org/packages/01/b0/a2a87db4b6cb8e7d57004b6836faa634e0747e3e39ded126cdbe5a33ba36/editables-0.3.tar.gz"
-    sha256 "167524e377358ed1f1374e61c268f0d7a4bf7dbd046c656f7b410cde16161b1a"
+    url "https://files.pythonhosted.org/packages/37/4a/986d35164e2033ddfb44515168a281a7986e260d344cf369c3f52d4c3275/editables-0.5.tar.gz"
+    sha256 "309627d9b5c4adc0e668d8c6fa7bac1ba7c8c5d415c2d27f60f081f8e80d1de2"
+  end
+
+  resource "packaging" do
+    url "https://files.pythonhosted.org/packages/fb/2b/9b9c33ffed44ee921d0967086d653047286054117d584f1b1a7c22ceaf7b/packaging-23.2.tar.gz"
+    sha256 "048fb0e9405036518eaaf48a55953c750c11e1a1b68e0dd1a9d62ed0c092cfc5"
+  end
+
+  resource "pathspec" do
+    url "https://files.pythonhosted.org/packages/ca/bc/f35b8446f4531a7cb215605d100cd88b7ac6f44ab3fc94870c120ab3adbf/pathspec-0.12.1.tar.gz"
+    sha256 "a482d51503a1ab33b1c67a6c3813a26953dbdc71c31dacaef9a838c4e29f5712"
+  end
+
+  resource "pluggy" do
+    url "https://files.pythonhosted.org/packages/36/51/04defc761583568cae5fd533abda3d40164cbdcf22dee5b7126ffef68a40/pluggy-1.3.0.tar.gz"
+    sha256 "cf61ae8f126ac6f7c451172cf30e3e43d3ca77615509771b3a984a0730651e12"
+  end
+
+  resource "trove-classifiers" do
+    url "https://files.pythonhosted.org/packages/3d/14/fe9a127564317f1670d1dd2e2e74b9e09fc157563aa2ffbe7d113d004c7a/trove-classifiers-2023.11.29.tar.gz"
+    sha256 "ff8f7fd82c7932113b46e7ef6742c70091cc63640c8c65db00d91f2e940b9514"
   end
 
   def pythons
