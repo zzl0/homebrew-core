e9ea5e432c1020ce9d25ebe83dff5983178fa0db
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-12-13 12:13:41 -0500

    hebcal 5.8.3

--- Formula/h/hebcal.rb
+++ Formula/h/hebcal.rb
@@ -1,8 +1,8 @@
 class Hebcal < Formula
   desc "Perpetual Jewish calendar for the command-line"
   homepage "https://github.com/hebcal/hebcal"
-  url "https://github.com/hebcal/hebcal/archive/refs/tags/v5.8.2.tar.gz"
-  sha256 "9d881cac33e98cd55056bd38df863b3bcc4476ffda360871e35080ac57e9dc91"
+  url "https://github.com/hebcal/hebcal/archive/refs/tags/v5.8.3.tar.gz"
+  sha256 "9d5bc2837d961e822261af0f28fbed379c7d4ab22c68b4ca9c156c56d731fad4"
   license "GPL-2.0-or-later"
   head "https://github.com/hebcal/hebcal.git", branch: "main"
 
