da4fbbc746590c1f1dfe7a654611616463e63427
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-12-13 13:25:46 -0500

    python-prompt-toolkit 3.0.43

--- Formula/p/python-prompt-toolkit.rb
+++ Formula/p/python-prompt-toolkit.rb
@@ -1,8 +1,8 @@
 class PythonPromptToolkit < Formula
   desc "Library for building powerful interactive CLI in Python"
   homepage "https://python-prompt-toolkit.readthedocs.io/en/master/"
-  url "https://files.pythonhosted.org/packages/d9/7b/7d88d94427e1e179e0a62818e68335cf969af5ca38033c0ca02237ab6ee7/prompt_toolkit-3.0.41.tar.gz"
-  sha256 "941367d97fc815548822aa26c2a269fdc4eb21e9ec05fc5d447cf09bad5d75f0"
+  url "https://files.pythonhosted.org/packages/cc/c6/25b6a3d5cd295304de1e32c9edbcf319a52e965b339629d37d42bb7126ca/prompt_toolkit-3.0.43.tar.gz"
+  sha256 "3527b7af26106cbc65a040bcc84839a3566ec1b051bb0bfe953631e704b0ff7d"
   license "BSD-3-Clause"
 
   bottle do
