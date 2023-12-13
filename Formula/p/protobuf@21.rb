ebb180fa2797420f61a329f472763afcff693493
Michael Cho <michael@michaelcho.dev>
2023-12-13 11:52:37 -0500

    protobuf@21: remove `python@3.10` support

--- Formula/p/protobuf@21.rb
+++ Formula/p/protobuf@21.rb
@@ -20,7 +20,6 @@ class ProtobufAT21 < Formula
 
   depends_on "cmake" => :build
   depends_on "python-setuptools" => :build
-  depends_on "python@3.10" => [:build, :test]
   depends_on "python@3.11" => [:build, :test]
   depends_on "python@3.12" => [:build, :test]
   uses_from_macos "zlib"
