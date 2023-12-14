4209fded60c68e7d27420b080f9417acf9126b55
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-12-13 20:22:45 -0500

    glfw 3.3.9

--- Formula/g/glfw.rb
+++ Formula/g/glfw.rb
@@ -1,8 +1,8 @@
 class Glfw < Formula
   desc "Multi-platform library for OpenGL applications"
   homepage "https://www.glfw.org/"
-  url "https://github.com/glfw/glfw/archive/refs/tags/3.3.8.tar.gz"
-  sha256 "f30f42e05f11e5fc62483e513b0488d5bceeab7d9c5da0ffe2252ad81816c713"
+  url "https://github.com/glfw/glfw/archive/refs/tags/3.3.9.tar.gz"
+  sha256 "a7e7faef424fcb5f83d8faecf9d697a338da7f7a906fc1afbc0e1879ef31bd53"
   license "Zlib"
   head "https://github.com/glfw/glfw.git", branch: "master"
 
