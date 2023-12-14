003a0bad892040a9b7932e020a6f5047aadbd82b
Rui Chen <rui@chenrui.dev>
2023-12-14 08:57:23 -0500

    ruby-completion 1.0.0
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/r/ruby-completion.rb
+++ Formula/r/ruby-completion.rb
@@ -1,15 +1,11 @@
 class RubyCompletion < Formula
   desc "Bash completion for Ruby"
   homepage "https://github.com/mernen/completion-ruby"
-  url "https://github.com/mernen/completion-ruby.git",
-      revision: "f3e4345042b0cc48317e45b673dfd3d23904b9a7"
-  version "2"
+  url "https://github.com/mernen/completion-ruby/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "ee8b84d7ac7444a7388e58a406af56dc0b690a57faa7bcfa4c10671deb788991"
   license "MIT"
+  version_scheme 1
   head "https://github.com/mernen/completion-ruby.git", branch: "main"
-
-  livecheck do
-    skip "No version information available to check"
-  end
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "534da18dd29bafad68930a70b41d94de09a99d0c4cf01e62bdb1a3c3f49fd3f3"
