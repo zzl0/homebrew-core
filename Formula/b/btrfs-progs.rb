a4a410fe0a91e05325df8df06bf6865cdf262157
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-12-14 15:25:14 +0000

    btrfs-progs 6.6.3
    btrfs-progs: remove sphinx-rtd-theme extension, which used for html page generation
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/b/btrfs-progs.rb
+++ Formula/b/btrfs-progs.rb
@@ -1,8 +1,8 @@
 class BtrfsProgs < Formula
   desc "Userspace utilities to manage btrfs filesystems"
   homepage "https://btrfs.wiki.kernel.org/index.php/Main_Page"
-  url "https://mirrors.edge.kernel.org/pub/linux/kernel/people/kdave/btrfs-progs/btrfs-progs-v6.6.2.tar.xz"
-  sha256 "b426736dc94e2b04b6983e998c4dc427dc166d66ddea246c890ed9c2a450044f"
+  url "https://mirrors.edge.kernel.org/pub/linux/kernel/people/kdave/btrfs-progs/btrfs-progs-v6.6.3.tar.xz"
+  sha256 "f41ce53f6673ff551ee4a3fe7dc9601e5a0dde6b6d09177d1fab62718abc6d9a"
   license all_of: [
     "GPL-2.0-only",
     "LGPL-2.1-or-later", # libbtrfsutil
@@ -33,6 +33,9 @@ class BtrfsProgs < Formula
     which("python3.12")
   end
 
+  # remove sphinx-rtd-theme extension for html docs
+  patch :DATA
+
   def install
     system "./configure", "--disable-python", *std_configure_args
     # Override `udevdir` since Homebrew's `pkg-config udev --variable=udevdir` output
@@ -56,3 +59,26 @@ class BtrfsProgs < Formula
     system python3, "-c", "import btrfsutil"
   end
 end
+
+__END__
+diff --git a/Documentation/conf.py b/Documentation/conf.py
+index 0bff1bd..3c08545 100644
+--- a/Documentation/conf.py
++++ b/Documentation/conf.py
+@@ -29,10 +29,6 @@ templates_path = ['_templates']
+ # This pattern also affects html_static_path and html_extra_path.
+ exclude_patterns = ['_build']
+
+-# The theme to use for HTML and HTML Help pages.  See the documentation for
+-# a list of builtin themes.
+-html_theme = 'sphinx_rtd_theme'
+-
+ # Add any paths that contain custom static files (such as style sheets) here,
+ # relative to this directory. They are copied after the builtin static files,
+ # so a file named "default.css" will overwrite the builtin "default.css".
+@@ -71,5 +67,3 @@ man_pages = [
+     ('mkfs.btrfs', 'mkfs.btrfs', 'create a btrfs filesystem', '', 8),
+     ('btrfs-man5', 'btrfs', 'topics about the BTRFS filesystem (mount options, supported file attributes and other)', '', 5),
+ ]
+-
+-extensions = [ 'sphinx_rtd_theme' ]
