66913c0c4670264881da93372e7a907046441422
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-12-14 13:45:05 +0000

    terrascan: update 1.18.8 bottle.

--- Formula/t/terrascan.rb
+++ Formula/t/terrascan.rb
@@ -7,13 +7,13 @@ class Terrascan < Formula
   head "https://github.com/tenable/terrascan.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "2081394f6411ac17582e07c68ed6a09c1b14950170f45703c3dd7237e51c8ad4"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "6439a8fefc6cd6ded85f07db0c71e2ed7b202b9e86f0e6506c7a01fab3de5dbc"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "8f6aa7f231acc38b0e9d7c5767fde9eea12cff748064cbb4770692680991cbe5"
-    sha256 cellar: :any_skip_relocation, sonoma:         "b5270f2d296153be317da572d564277c40e0bf67e7ceaad7adf30ba9b99747f7"
-    sha256 cellar: :any_skip_relocation, ventura:        "9cfabcbc84e8ec6fca25315bc45c7ea65b2da802657b3987832e6ab6ab83c0dc"
-    sha256 cellar: :any_skip_relocation, monterey:       "60b38f85ffa59e33d2a35cc38f5a0ddfa90f390d7af66de0169f90c8fa3145cd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b1067dab3055fd81b7260648707e8ed7b59f79f29bf925d87c06cb6ba1a6cd6e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "c58944f526b2adc0f98a0ac50066ac72e4ef1c1a3b16523210bd1ca1e6fc889c"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "97afaece7fcd8d85d9d3f2d70e9ad4ec1922ca7344daa997abe76d3eaa475de5"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "9806c8b85ec0a4d4c73c5ddeb06436f064cca629b29c215bec1d3f907a890da1"
+    sha256 cellar: :any_skip_relocation, sonoma:         "88aea10774712628d299a41dea80c95a96662f8f8eb37c372e9f2765940c6869"
+    sha256 cellar: :any_skip_relocation, ventura:        "8456ae4bfcf5cd8df3a774f49dba645119ca325d5ec848f6bb03321510e8d9a0"
+    sha256 cellar: :any_skip_relocation, monterey:       "d90ed1bcf938c8bb88bdcb2a9e52edde84d8d63ae40551551d33c0eef8b57ea7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b273ea752d0ac8f4f12504c20f7d6e23a5142f66dfb0eaba57820ef9e3779de0"
   end
 
   depends_on "go" => :build
