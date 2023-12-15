eea5c2a1fb5b0811cd4fcaa8a962253073e60499
Francois-Xavier Coudert <fxcoudert@gmail.com>
2023-12-15 18:52:21 +0100

    opensca-cli: add 3.0.0 bottle.

--- Formula/o/opensca-cli.rb
+++ Formula/o/opensca-cli.rb
@@ -6,6 +6,16 @@ class OpenscaCli < Formula
   license "Apache-2.0"
   head "https://github.com/XmirrorSecurity/OpenSCA-cli.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "7c8f1c4758080385aace1b5f3c29ce37fe1e550c3b293304eb06ac86546a55da"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "7fa23298b50f435b6be3f15e38c745288d536b3a5234f435d4dc2fb14ef9e54e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3f6694ba7a4ee2a521f73cf05256a949fefb9186d3b521badd44d44ef5ce2769"
+    sha256 cellar: :any_skip_relocation, sonoma:         "cb1f45f4d31c71ae6daa4956f843c4c86bd13420d4e628faaf862168cc878b86"
+    sha256 cellar: :any_skip_relocation, ventura:        "eea39c4662ea4d4185fcbf42ca435c232e4fa5eefe8b6c91f89175adf7cd5137"
+    sha256 cellar: :any_skip_relocation, monterey:       "0d224a5a69310cde08ae498425426fe14137b9bf8b9e5af1e20492606a61002a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0a8fee3ea602858e1fbdf921d6cf7230af4f7921b5a27b1b671073119dc64b49"
+  end
+
   depends_on "go" => :build
 
   def install
