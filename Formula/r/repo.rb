0b8e8837e238cb789a38ea212ff55be48e200b73
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-12-15 16:42:27 +0000

    repo: update 2.40 bottle.

--- Formula/r/repo.rb
+++ Formula/r/repo.rb
@@ -10,13 +10,7 @@ class Repo < Formula
   version_scheme 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "6c9acf142568e72443c7a8ce2ae7eaea4d73f5b36b657376e032828351908b92"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "6c9acf142568e72443c7a8ce2ae7eaea4d73f5b36b657376e032828351908b92"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "6c9acf142568e72443c7a8ce2ae7eaea4d73f5b36b657376e032828351908b92"
-    sha256 cellar: :any_skip_relocation, sonoma:         "6c9acf142568e72443c7a8ce2ae7eaea4d73f5b36b657376e032828351908b92"
-    sha256 cellar: :any_skip_relocation, ventura:        "6c9acf142568e72443c7a8ce2ae7eaea4d73f5b36b657376e032828351908b92"
-    sha256 cellar: :any_skip_relocation, monterey:       "6c9acf142568e72443c7a8ce2ae7eaea4d73f5b36b657376e032828351908b92"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f29e5f5e11ad48e94c62ac693439a99cecb1ffdff6e19999c7f932be2f39bdbd"
+    sha256 cellar: :any_skip_relocation, all: "158c053b443694bdfbc4e3cd94681cd579fe9ba5702f048de48c7f1495375016"
   end
 
   uses_from_macos "python"
