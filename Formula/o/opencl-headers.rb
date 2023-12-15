842111035ffbcfb3b42689fe8427d5ccbca11461
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-12-15 11:37:36 -0500

    opencl-headers 2023.12.14

--- Formula/o/opencl-headers.rb
+++ Formula/o/opencl-headers.rb
@@ -1,8 +1,8 @@
 class OpenclHeaders < Formula
   desc "C language header files for the OpenCL API"
   homepage "https://www.khronos.org/registry/OpenCL/"
-  url "https://github.com/KhronosGroup/OpenCL-Headers/archive/refs/tags/v2023.04.17.tar.gz"
-  sha256 "0ce992f4167f958f68a37918dec6325be18f848dee29a4521c633aae3304915d"
+  url "https://github.com/KhronosGroup/OpenCL-Headers/archive/refs/tags/v2023.12.14.tar.gz"
+  sha256 "407d5e109a70ec1b6cd3380ce357c21e3d3651a91caae6d0d8e1719c69a1791d"
   license "Apache-2.0"
   head "https://github.com/KhronosGroup/OpenCL-Headers.git", branch: "main"
 
