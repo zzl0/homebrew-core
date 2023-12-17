664f3024e3bd4a3ce9e683e32602d757f7d3ef29
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-12-17 12:28:31 +0000

    c-ares 1.24.0

--- Formula/c/c-ares.rb
+++ Formula/c/c-ares.rb
@@ -1,11 +1,11 @@
 class CAres < Formula
   desc "Asynchronous DNS library"
   homepage "https://c-ares.org/"
-  url "https://c-ares.org/download/c-ares-1.23.0.tar.gz"
-  mirror "https://github.com/c-ares/c-ares/releases/download/cares-1_17_2/c-ares-1.23.0.tar.gz"
-  mirror "http://fresh-center.net/linux/misc/dns/c-ares-1.23.0.tar.gz"
-  mirror "http://fresh-center.net/linux/misc/dns/legacy/c-ares-1.23.0.tar.gz"
-  sha256 "cb614ecf78b477d35963ebffcf486fc9d55cc3d3216f00700e71b7d4868f79f5"
+  url "https://c-ares.org/download/c-ares-1.24.0.tar.gz"
+  mirror "https://github.com/c-ares/c-ares/releases/download/cares-1_17_2/c-ares-1.24.0.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/dns/c-ares-1.24.0.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/dns/legacy/c-ares-1.24.0.tar.gz"
+  sha256 "c517de6d5ac9cd55a9b72c1541c3e25b84588421817b5f092850ac09a8df5103"
   license "MIT"
   head "https://github.com/c-ares/c-ares.git", branch: "main"
 
