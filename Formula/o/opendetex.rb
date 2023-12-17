dc57b6687ba4c3a2dfa8ddf2dea04083609c4f53
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-12-17 04:15:57 +0000

    opendetex: update 2.8.10 bottle.

--- Formula/o/opendetex.rb
+++ Formula/o/opendetex.rb
@@ -6,17 +6,13 @@ class Opendetex < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "afd6b3ab9f3bacb9a887b246c9fe68f352c0336008664eab394ca91cae160cb7"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "ed55ca17def1bdc6fec261be8ddb46618fb1a310170796d1ad90bfadbeeee68d"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "7b7c22303e3de7519dc20cc7862c6aa3ac49fa6bae8d3bd043c46c3930dea810"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9416484618318a11e895667857e7d8b39598bc31c2c1d8fbdbb7914176345e5e"
-    sha256 cellar: :any_skip_relocation, sonoma:         "d29efbda370eb0441fa0366e446869c408d8eaf583fe88e7660f6eab91678d95"
-    sha256 cellar: :any_skip_relocation, ventura:        "7562459cf4cb848fbc8ea3d3d1843355e6c200bb112890bf511ce9d67b6c61bd"
-    sha256 cellar: :any_skip_relocation, monterey:       "18e2156648aac0c7429e14b30ebe1444cc6b95fd0ca99366fa39f81148bc5e17"
-    sha256 cellar: :any_skip_relocation, big_sur:        "ce26ea02e5c47385374aba395951434319d5e48e6dbda94f7ffa25e4632b54a6"
-    sha256 cellar: :any_skip_relocation, catalina:       "46db3f033cb646e360fcabc83eb6fabba87b858eb1cc3e32d4bad78e73816bc6"
-    sha256 cellar: :any_skip_relocation, mojave:         "92d55157d568aa004dd09342308f8e4be8dfb6a95f9719646c5d9792b677f7a2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e88e075265c2480dd11436e5d6027a090fb8e199c51fb6e5ffb431db5bf1662c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "1a046f41d5f06e58bd4424b1c44fe9aad39a5e716dd39f4205be6cb5419bf3b8"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "7e8d3d426c7d1f17c8939d089385d2bfe34ffc413f9972460db1d718b36ee49c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "492e3ac4f09d8ec46842f3c9a76ebe7e1c750c8872a7349513b5b6274fb5b726"
+    sha256 cellar: :any_skip_relocation, sonoma:         "60a8f762f9fb9cbd7fe0036daec56fab0ed9713efb1c251e805ca2cf692e7029"
+    sha256 cellar: :any_skip_relocation, ventura:        "d63d41beb9c3f2e2b16e815593b12e6bd23f2b495df93423a33af28c98b6e0b0"
+    sha256 cellar: :any_skip_relocation, monterey:       "8ae1595f01817366f9abd99559ae95d6e70b90ba88ec5ef0f6973b9f3dc8c3b8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4ee8f83764d07fdb0ff7cc7795251aeb694ffce8ebeabadbd52514ce980f6566"
   end
 
   uses_from_macos "flex" => :build
