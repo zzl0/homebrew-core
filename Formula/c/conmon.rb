0e8e28489d2b0895268be5e6b115c418127d77e7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-12-18 23:22:37 +0000

    conmon: update 2.1.10 bottle.

--- Formula/c/conmon.rb
+++ Formula/c/conmon.rb
@@ -6,7 +6,7 @@ class Conmon < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "7fcc3f69caa7f49d19e6b7a1ded32faa1ed8124baa0ffb360feb0d7b82c98071"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "2d9a69320d9e55e3989311435e8bf7d999a54af949ce5d1d1726003642ac0536"
   end
 
   depends_on "go" => :build
