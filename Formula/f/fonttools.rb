a2ce282af2aae833c4fdf82c49e2263e86513913
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-12-18 21:41:17 +0000

    fonttools 4.47.0

--- Formula/f/fonttools.rb
+++ Formula/f/fonttools.rb
@@ -1,8 +1,8 @@
 class Fonttools < Formula
   desc "Library for manipulating fonts"
   homepage "https://github.com/fonttools/fonttools"
-  url "https://files.pythonhosted.org/packages/55/69/f2bc415e10feb4ae6067628c983b54999a6e6d7c1dc7513ea9c0dd35a0c9/fonttools-4.46.0.tar.gz"
-  sha256 "2ae45716c27a41807d58a9f3f59983bdc8c0a46cb259e4450ab7e196253a9853"
+  url "https://files.pythonhosted.org/packages/dd/e5/9adc30ebca9009d5ad36c7e74462ee5fc51985ca9a845fd26f9f5c99b3df/fonttools-4.47.0.tar.gz"
+  sha256 "ec13a10715eef0e031858c1c23bfaee6cba02b97558e4a7bfa089dba4a8c2ebf"
   license "MIT"
   head "https://github.com/fonttools/fonttools.git", branch: "main"
 
