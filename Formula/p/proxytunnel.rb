7f4042cf62b43d21a75167e7b4dfbd0cf269c0cf
Porkepix <porkepix@gmail.com>
2023-12-18 15:36:51 +0100

    proxytunnel 1.12.0

--- Formula/p/proxytunnel.rb
+++ Formula/p/proxytunnel.rb
@@ -1,8 +1,8 @@
 class Proxytunnel < Formula
   desc "Create TCP tunnels through HTTPS proxies"
   homepage "https://github.com/proxytunnel/proxytunnel"
-  url "https://github.com/proxytunnel/proxytunnel/archive/refs/tags/v1.11.1.tar.gz"
-  sha256 "d95e4974d37cc96ea5d9906ea7808db4560981902a40217368877409145ada96"
+  url "https://github.com/proxytunnel/proxytunnel/archive/refs/tags/v1.12.0.tar.gz"
+  sha256 "76091c8d6161c73ad02f25bee26396bb6aa45422156ee7b63f46128fa8b39f18"
   license "GPL-2.0-or-later" => { with: "openvpn-openssl-exception" }
 
   bottle do
