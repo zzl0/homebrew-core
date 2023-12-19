4aa03096b8a55f0b055fc584a7e95e04e61175ca
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-12-19 17:15:43 +0000

    ugrep: update 4.4.1 bottle.

--- Formula/u/ugrep.rb
+++ Formula/u/ugrep.rb
@@ -6,13 +6,13 @@ class Ugrep < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256                               arm64_sonoma:   "eec34db8c8aa55fe686e047587741777586d8be5c375142ef69b0da0f9899893"
-    sha256                               arm64_ventura:  "df7a43742dd1763f4ba0993aebd01d700ec6bb8ed0d0dc03892d652a4147b720"
-    sha256                               arm64_monterey: "f4f5ca66e406cf3df1581a43159ac595f6f0c57d0a5c9ac4d453e7c5d14bba50"
-    sha256                               sonoma:         "06c3f71c9d25d018d33e8289e103311b732e8c1eaaed750ad715aee20ef6f25a"
-    sha256                               ventura:        "f372513af217cd09ed24046d128821bd10dc54b86116399c27fcb759dcb6aae3"
-    sha256                               monterey:       "161850de9bc7306758d7b9b47f684d2ff1d769e036eb3c37b36ded1254030815"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1a7e80a2d87b30b9542cf6fdaec9f95ec6a904e9a52512c57cdd83607acc4f52"
+    sha256                               arm64_sonoma:   "8bd56e07f24f27dfc9d3ab88a36d9c448cd9a4c248b91502faded1f500b1dfd2"
+    sha256                               arm64_ventura:  "bfeea2840f8196be3b57a5ae962ed1212a2443416981df526221827d07187c4f"
+    sha256                               arm64_monterey: "5b91cb20a1d16200ba48f8f6146fb5396669d9e997d46f8ea98172cc94319f01"
+    sha256                               sonoma:         "4f809594a33f006ed92ef1d92ff0ae4259248fde8574b3171410dff018edd029"
+    sha256                               ventura:        "7da551885a5f1401a677eae688b717f2a7a89734ad2d14a875f95ca9caa0cdab"
+    sha256                               monterey:       "43baf0cc8dd4c135bfcc60d76618de203980bd0a246527b1332ae4d18ae39e54"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8818df971b8dfdd7a7e312a039de074667fff93ed00a09ad3669854beaab1578"
   end
 
   depends_on "pcre2"
