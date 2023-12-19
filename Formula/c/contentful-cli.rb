40c22330544ebb08b91a44471fe1ce45ac23ca60
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-12-19 01:20:03 +0000

    contentful-cli: update 3.1.30 bottle.

--- Formula/c/contentful-cli.rb
+++ Formula/c/contentful-cli.rb
@@ -9,13 +9,13 @@ class ContentfulCli < Formula
   head "https://github.com/contentful/contentful-cli.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "87d6e274419851d8b2250f9a603ff0c50d6c52cd41862d11bb071f349241493c"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "87d6e274419851d8b2250f9a603ff0c50d6c52cd41862d11bb071f349241493c"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "87d6e274419851d8b2250f9a603ff0c50d6c52cd41862d11bb071f349241493c"
-    sha256 cellar: :any_skip_relocation, sonoma:         "c968df323fd49a9c46921ca8808eb83280bed63a1065b98dac3152f45f04ce97"
-    sha256 cellar: :any_skip_relocation, ventura:        "c968df323fd49a9c46921ca8808eb83280bed63a1065b98dac3152f45f04ce97"
-    sha256 cellar: :any_skip_relocation, monterey:       "c968df323fd49a9c46921ca8808eb83280bed63a1065b98dac3152f45f04ce97"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "87d6e274419851d8b2250f9a603ff0c50d6c52cd41862d11bb071f349241493c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "456173344c95717ca8cd9025dd4adbd3070cd1dbaa9ceccd90d35bb7fb23cece"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "456173344c95717ca8cd9025dd4adbd3070cd1dbaa9ceccd90d35bb7fb23cece"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "456173344c95717ca8cd9025dd4adbd3070cd1dbaa9ceccd90d35bb7fb23cece"
+    sha256 cellar: :any_skip_relocation, sonoma:         "e14901f471c47d37cfe3304123105122567c328804f082cf991172014facde5d"
+    sha256 cellar: :any_skip_relocation, ventura:        "e14901f471c47d37cfe3304123105122567c328804f082cf991172014facde5d"
+    sha256 cellar: :any_skip_relocation, monterey:       "e14901f471c47d37cfe3304123105122567c328804f082cf991172014facde5d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "456173344c95717ca8cd9025dd4adbd3070cd1dbaa9ceccd90d35bb7fb23cece"
   end
 
   depends_on "node"
