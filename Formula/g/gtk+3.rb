28b63f1e7134d82ada53820f9315c80c7c745d02
Sean Molenaar <smillerdev@me.com>
2023-12-20 10:35:42 +0100

    gtk+3 3.24.39

--- Formula/g/gtk+3.rb
+++ Formula/g/gtk+3.rb
@@ -1,10 +1,9 @@
 class Gtkx3 < Formula
   desc "Toolkit for creating graphical user interfaces"
   homepage "https://gtk.org/"
-  url "https://download.gnome.org/sources/gtk+/3.24/gtk+-3.24.38.tar.xz"
-  sha256 "ce11decf018b25bdd8505544a4f87242854ec88be054d9ade5f3a20444dd8ee7"
+  url "https://download.gnome.org/sources/gtk+/3.24/gtk+-3.24.39.tar.xz"
+  sha256 "1cac3e566b9b2f3653a458c08c2dcdfdca9f908037ac03c9d8564b4295778d79"
   license "LGPL-2.0-or-later"
-  revision 1
 
   livecheck do
     url :stable
