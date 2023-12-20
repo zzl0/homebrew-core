43599d2b45048438e4fb46b8adf97ee12a2469b7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-12-20 18:54:47 +0000

    psysh: update 0.12.0 bottle.

--- Formula/p/psysh.rb
+++ Formula/p/psysh.rb
@@ -6,13 +6,13 @@ class Psysh < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "9395f9f2debae156a41d6913eae3c23fc5f9ce2b200a9d787cfd5088d2c217b4"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "9395f9f2debae156a41d6913eae3c23fc5f9ce2b200a9d787cfd5088d2c217b4"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "9395f9f2debae156a41d6913eae3c23fc5f9ce2b200a9d787cfd5088d2c217b4"
-    sha256 cellar: :any_skip_relocation, sonoma:         "75b40ec5e86d782a9342c7a96c4c53742dc0966a65c193ea4ab53c3349f089ad"
-    sha256 cellar: :any_skip_relocation, ventura:        "75b40ec5e86d782a9342c7a96c4c53742dc0966a65c193ea4ab53c3349f089ad"
-    sha256 cellar: :any_skip_relocation, monterey:       "75b40ec5e86d782a9342c7a96c4c53742dc0966a65c193ea4ab53c3349f089ad"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9395f9f2debae156a41d6913eae3c23fc5f9ce2b200a9d787cfd5088d2c217b4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "2956acd5f6fa0deceaf2ac02c1d43ecca75f613c67354ee48564de10500b8624"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "2956acd5f6fa0deceaf2ac02c1d43ecca75f613c67354ee48564de10500b8624"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2956acd5f6fa0deceaf2ac02c1d43ecca75f613c67354ee48564de10500b8624"
+    sha256 cellar: :any_skip_relocation, sonoma:         "38acfd2c24e8acabdaabc4dc32ffbcee9456762e833851cc250bb592c981ed70"
+    sha256 cellar: :any_skip_relocation, ventura:        "38acfd2c24e8acabdaabc4dc32ffbcee9456762e833851cc250bb592c981ed70"
+    sha256 cellar: :any_skip_relocation, monterey:       "38acfd2c24e8acabdaabc4dc32ffbcee9456762e833851cc250bb592c981ed70"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2956acd5f6fa0deceaf2ac02c1d43ecca75f613c67354ee48564de10500b8624"
   end
 
   depends_on "php"
