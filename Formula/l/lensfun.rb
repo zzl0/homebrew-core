c7443e4bd068e38fc0e288b47577663c28f80c5c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-12-20 19:58:23 +0000

    lensfun: update 0.3.4 bottle.

--- Formula/l/lensfun.rb
+++ Formula/l/lensfun.rb
@@ -22,15 +22,14 @@ class Lensfun < Formula
   end
 
   bottle do
-    sha256 arm64_sonoma:   "83687062a7ded4ce3a1c499a341191641a3b42e6ed69134b40219ebd1c63c112"
-    sha256 arm64_ventura:  "4a99ed8713c56bd81fec4c7f35881a5c5cc83ce73e23b08f8f82e7fcd922b978"
-    sha256 arm64_monterey: "4a9857407b226accdc5d8790c3c3dbfbad4ff8483d55e9f78cf534e81cad8bba"
-    sha256 arm64_big_sur:  "d546dd5e1c72fd2a067b0525665925cc357a80c15c1b1f368370e7f3b8405940"
-    sha256 sonoma:         "13fc156fdc63d24ab4d5d01b45236f077f8ba78d4c7063e22266f5e80a19dbda"
-    sha256 ventura:        "6e16b67dc7484b899c0aaba0abd336dbdadcae77c8cbc8b952b6751c25b0acd3"
-    sha256 monterey:       "eb05f7aa7df729187805b00bfdcc2493d8dfb08edc6602f70efcb41d84931b83"
-    sha256 big_sur:        "bdb3fecd6744dbb7f16910351e3f950082bc5ee5a40c27bf0de833a5fa3986a6"
-    sha256 x86_64_linux:   "2211b5b79be1624137ceb4a197bcc8180151e13c26930016428102101a461beb"
+    rebuild 1
+    sha256 arm64_sonoma:   "4509de164f26e03f7dde33e45c7c82994ade1cd087118ea44d096966e820aa21"
+    sha256 arm64_ventura:  "4d66d4326ff847e40cbdea3efad6b141ca4a60e3af369dfd38b0bbac05377693"
+    sha256 arm64_monterey: "d3b2be29200d9d2fed399acfa6c3688630f67a8d92d934fa201ccb926ba9d3ee"
+    sha256 sonoma:         "893fc14bc16e6841f22f8211f980ead61bf157f1a2936289dda5de3a46684d67"
+    sha256 ventura:        "81b1d46a4bbbf4dbfbf634713a997fa2bf7b060e65ad7b981a470ab266495f73"
+    sha256 monterey:       "130ff44bb69bfeae078ddf16c2a4dcae0750e16db4e54e81738c56dfd8fc875f"
+    sha256 x86_64_linux:   "2e4d736b2e405d14c4b18b5ccd316455927ef40b74076131d4b1bf37c5001b8e"
   end
 
   depends_on "cmake" => :build
