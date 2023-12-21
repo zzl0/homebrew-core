6a6e6a85d954d0129d6456d31cbe73116e38eb42
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-12-21 16:57:26 +0000

    mdless: update 2.1.42 bottle.

--- Formula/m/mdless.rb
+++ Formula/m/mdless.rb
@@ -6,13 +6,13 @@ class Mdless < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "d808938e92139c3641c74220ce5301241c9e9286b278b7a1e0d4c1d78a7dca33"
-    sha256 cellar: :any,                 arm64_ventura:  "aba4b17d841be49041c8ad31abf783c1c3ef9fe56309693211f49ceea02dd8ec"
-    sha256 cellar: :any,                 arm64_monterey: "90e885feea23c65759126627f58fa22fecbc6eb7512fd2a22b2170a7bdb3bc1d"
-    sha256 cellar: :any,                 sonoma:         "6c7efd2ae3c9e90d934986854444792691ba43d4ec25b8517a0fe6db73ec3cc7"
-    sha256 cellar: :any,                 ventura:        "46598edde27ed6ee11632d618996764f1041587c752862ae9f51f03aac6c106b"
-    sha256 cellar: :any,                 monterey:       "3af4c8c2f1c190f4d9e4b4cd231c1a3cbf002b5c6c195fbfa627bf04379b4fd2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "74f628b89d25a03d2ebaebea01e84ee7f1f7e615ec8c02591417761c14a19178"
+    sha256 cellar: :any,                 arm64_sonoma:   "5cfc807befb1543428e53061bcee8ca8b20226905439f7874e80b68c760bc307"
+    sha256 cellar: :any,                 arm64_ventura:  "9689e76d484216e991611d7d7ae2a892fea2de0550c46db9cd5d15380f7b0489"
+    sha256 cellar: :any,                 arm64_monterey: "f0c93622ae62db99093f1d6c5801816e05988cdfee9e8ac6acea556795dab350"
+    sha256 cellar: :any,                 sonoma:         "bdb322312b3996d1e3d20e4a9d9c061a63558b7c57b491c788ee5142bfcf8b02"
+    sha256 cellar: :any,                 ventura:        "328146f19871b949f6ab90395985386b3ebff358f294745e2b271de1dec709c6"
+    sha256 cellar: :any,                 monterey:       "aa19789c23893cb3422da1967404417eaf324bbaa9e6c6c024291e46572d7892"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "30a918fe7c00e25e7ec0e9341b39d0777fb587360039ee1767b52a6064ae64cf"
   end
 
   # Requires Ruby >= 2.7
