623a84276dc7d92226b95b1fa60916b508ed4a2e
Porkepix <porkepix@gmail.com>
2023-12-23 03:13:55 +0100

    git-quick-stats 2.5.3

--- Formula/g/git-quick-stats.rb
+++ Formula/g/git-quick-stats.rb
@@ -1,8 +1,8 @@
 class GitQuickStats < Formula
   desc "Simple and efficient way to access statistics in git"
   homepage "https://github.com/arzzen/git-quick-stats"
-  url "https://github.com/arzzen/git-quick-stats/archive/refs/tags/2.5.2.tar.gz"
-  sha256 "6487bc197422d5e37841f304f245707d2640c402b0cc457536942d96713fa2b8"
+  url "https://github.com/arzzen/git-quick-stats/archive/refs/tags/2.5.3.tar.gz"
+  sha256 "39130314ac006cf6887316179af66462378eeb3cbf28ff568a480f3f42d0b61d"
   license "MIT"
 
   bottle do
