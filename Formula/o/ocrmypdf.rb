ef8baa4c952fde1107390681d152e5a7e5b0ef20
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-12-24 01:01:50 +0000

    ocrmypdf 16.0.2

--- Formula/o/ocrmypdf.rb
+++ Formula/o/ocrmypdf.rb
@@ -3,8 +3,8 @@ class Ocrmypdf < Formula
 
   desc "Adds an OCR text layer to scanned PDF files"
   homepage "https://ocrmypdf.readthedocs.io/en/latest/"
-  url "https://files.pythonhosted.org/packages/03/65/fea55ebfdad6f94e65415a32fd1c836510ce1da8de036a3b5875292d9038/ocrmypdf-16.0.1.post1.tar.gz"
-  sha256 "c05a364c2bda4d11df1e0e0c11e7b554e1199caa51ed0a61268c3074b2ef50e4"
+  url "https://files.pythonhosted.org/packages/a9/e4/7c86016d069b6f2d458abc5f4432cc9aa77abd2dba31e9e12dd23405018b/ocrmypdf-16.0.2.tar.gz"
+  sha256 "22aff9a734430aef1259fcad3697324b02a0147c2566b3df03d45a1d4401a0cc"
   license "MPL-2.0"
 
   bottle do
