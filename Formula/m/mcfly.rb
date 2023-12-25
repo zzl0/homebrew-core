99dd0f79fd7668bba497cfad4d0785e19ca36551
Rui Chen <rui@chenrui.dev>
2023-12-25 18:31:51 -0500

    mcfly 0.8.4

--- Formula/m/mcfly.rb
+++ Formula/m/mcfly.rb
@@ -1,8 +1,8 @@
 class Mcfly < Formula
   desc "Fly through your shell history"
   homepage "https://github.com/cantino/mcfly"
-  url "https://github.com/cantino/mcfly/archive/refs/tags/v0.8.3.tar.gz"
-  sha256 "1d9f4e3b55fdac8b9c3007c19875bd308eb79dd473d371647e82250a52446d53"
+  url "https://github.com/cantino/mcfly/archive/refs/tags/v0.8.4.tar.gz"
+  sha256 "637f50756366604d4d19a6f623cfd490c38e1b971708ec8ccdb39887a0e9c1f1"
   license "MIT"
   head "https://github.com/cantino/mcfly.git", branch: "master"
 
