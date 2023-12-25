a4fd6c2106b93b49845e3ed7219d95dbca49f590
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-12-25 16:10:58 +0000

    esphome: update 2023.12.5 bottle.

--- Formula/e/esphome.rb
+++ Formula/e/esphome.rb
@@ -8,13 +8,13 @@ class Esphome < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "86fad46793544e7d096e2edd0f9c0e43346510998401b9179a26fae0d0b4c04a"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "691fad445ecbf96c4bc6f49835559434fd754239e1b34f56f776870a6bd9f0e5"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "4d6639ae58fdce6225a40c39501b840a990892f7f0104bcc3dbcceaa2a4b7051"
-    sha256 cellar: :any_skip_relocation, sonoma:         "afdc15c665e8c5a89cb0646ffbec4c1e9ed4ebdbf18bc6c5e3a8f33f84b88484"
-    sha256 cellar: :any_skip_relocation, ventura:        "1180e635e7eac797fcbd933ccc4cc4ccbd90b454756c175cefaea475f12a1291"
-    sha256 cellar: :any_skip_relocation, monterey:       "2ef8e8ebcbcf37cc70ddfe4cd6a3b97574d5355e0a238f81a4f24c8ff0547621"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3a3fa24d5bb58881af16cb714853dfe2ccdadef9ab8d7026dcccbd4b34927d8d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "411d5d7cc82d5061e964add673d2cb9b569f5ff84befb9682435147484bf9f98"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "c884f5bf141e95fcd8ceec19f9726fa9c1e411a50417788b9355a23759c67f32"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "db6a2a761d5b615a8856969262b71ccfda7f85c6e10b15b5aeb31c953c107a2d"
+    sha256 cellar: :any_skip_relocation, sonoma:         "4827de7bdb31cb9b7d8369cf4d008e86578b28fd7cd81972797d1faf8f50cd6f"
+    sha256 cellar: :any_skip_relocation, ventura:        "a2ccfe7f9fa1f4558f26ee6d2ac2148a918afdfeb344e458baec7a01f07ff7a9"
+    sha256 cellar: :any_skip_relocation, monterey:       "bc934fa1382d01523ea5a5fd5ba253cbcd5ac25c278dd62603539ae70b973d30"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c43df8b97c31f49b4f78c8dfff79c9b8063e888ddc3aacd1ac2c436e580d8b88"
   end
 
   depends_on "cffi"
