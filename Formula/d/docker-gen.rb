789cd663a03f9caace40719055db2d28ee1870ab
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-12-25 21:30:35 +0000

    docker-gen 0.11.1

--- Formula/d/docker-gen.rb
+++ Formula/d/docker-gen.rb
@@ -1,8 +1,8 @@
 class DockerGen < Formula
   desc "Generate files from docker container metadata"
   homepage "https://github.com/nginx-proxy/docker-gen"
-  url "https://github.com/nginx-proxy/docker-gen/archive/refs/tags/0.11.0.tar.gz"
-  sha256 "15029c70d6b062440f37a04be6b978892b6397ce2cf8dbcdd95d3ffe64fef2ed"
+  url "https://github.com/nginx-proxy/docker-gen/archive/refs/tags/0.11.1.tar.gz"
+  sha256 "b957fef090a085c0ed30a748af1825acf5d4ac044af4079d198f69633b6375ac"
   license "MIT"
   head "https://github.com/nginx-proxy/docker-gen.git", branch: "main"
 
