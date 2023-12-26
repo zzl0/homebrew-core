d5f5c355137ebd011b9f36569148fee3b8b80d55
Alexander Frisch <crunchtime-ali@users.noreply.github.com>
2023-12-26 18:12:47 +0000

    k9s 0.30.4

--- Formula/k/k9s.rb
+++ Formula/k/k9s.rb
@@ -2,8 +2,8 @@ class K9s < Formula
   desc "Kubernetes CLI To Manage Your Clusters In Style!"
   homepage "https://k9scli.io/"
   url "https://github.com/derailed/k9s.git",
-      tag:      "v0.30.3",
-      revision: "26d1585699fb1faed828161c13265200f83486d8"
+      tag:      "v0.30.4",
+      revision: "34da44b441598c68fb6de1571f2a38f74d66bb01"
   license "Apache-2.0"
   head "https://github.com/derailed/k9s.git", branch: "master"
 
