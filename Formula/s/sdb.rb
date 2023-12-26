c79f097a0492f3b6ec2638fd20666aa14a697538
Mikl Wolfe <wolfemikl@gmail.com>
2023-12-26 15:19:06 -0700

    sdb 1.9.8

--- Formula/s/sdb.rb
+++ Formula/s/sdb.rb
@@ -1,8 +1,8 @@
 class Sdb < Formula
   desc "Ondisk/memory hashtable based on CDB"
   homepage "https://github.com/radareorg/sdb"
-  url "https://github.com/radareorg/sdb/archive/refs/tags/1.9.6.tar.gz"
-  sha256 "da7ee00ed239f68dbb6a8fad165911ccbe332c6c664a5896cbd867fc9209c934"
+  url "https://github.com/radareorg/sdb/archive/refs/tags/1.9.8.tar.gz"
+  sha256 "c519f10a56a0ab6a151b4e0f2f097b6c2af4709c5259463de50a94e8b7eea6a1"
   license "MIT"
   head "https://github.com/radareorg/sdb.git", branch: "master"
 
@@ -23,13 +23,6 @@ class Sdb < Formula
   depends_on "pkg-config" => :build
   depends_on "vala" => :build
   depends_on "glib"
-
-  # patch build to fix version.h not found
-  # remove in next release
-  patch do
-    url "https://github.com/radareorg/sdb/commit/3bc55289a73bddbd63a11d993c949f57e8a7f7cc.patch?full_index=1"
-    sha256 "d272212a0308a4e8f45f1413c67fb027409d885f3e97166e1a896c7d6b772c4b"
-  end
 
   def install
     system "meson", *std_meson_args, "build"
