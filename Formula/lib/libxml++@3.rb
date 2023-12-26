b5f5168bacdd7538d487202b443de0ef2eef2269
Rui Chen <rui@chenrui.dev>
2023-12-26 17:01:00 +0000

    libxml++@3: add 3.2.4 bottle.

--- Formula/lib/libxml++@3.rb
+++ Formula/lib/libxml++@3.rb
@@ -10,6 +10,16 @@ class LibxmlxxAT3 < Formula
     regex(/libxml\+\+[._-]v?(3\.([0-8]\d*?)?[02468](?:\.\d+)*?)\.t/i)
   end
 
+  bottle do
+    sha256 cellar: :any, arm64_sonoma:   "e7c499c2375d9bfa937a174a21f9d8fba1b4743fcac3a2f8186afa77c519720f"
+    sha256 cellar: :any, arm64_ventura:  "27afebe6ebb966654ea6ca39e5feaa916bbdc2435554e32f4fb7ddf53404c75e"
+    sha256 cellar: :any, arm64_monterey: "b84c0eaae133a13db2e06a2ff5902a0982ae26a3e4b5c543003b6df39cb94e3c"
+    sha256 cellar: :any, sonoma:         "9d1bdc8411b1a55fc16a3676dd732f6f372f2f69e333d73ede91cdb7b51caabe"
+    sha256 cellar: :any, ventura:        "8b8802982bd457546e8a9607c5001148eb356b09d2569ae3ff23593f2378411f"
+    sha256 cellar: :any, monterey:       "4e8d9e3fc7d3d3b9e72cf7950ee1b5a15788334ac74f69f9eba554f2fca7fb8c"
+    sha256               x86_64_linux:   "05572d1751f44129df78f110d24a2385c10f16d1a218d73be53cbbd3841581b1"
+  end
+
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => [:build, :test]
