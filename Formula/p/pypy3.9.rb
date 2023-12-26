1fbb50344074df859e1ef5a379090dd84da74145
Porkepix <porkepix@gmail.com>
2023-12-26 15:26:48 +0100

    pypy3.9 7.3.14
    pypy3.9: pip 23.3.2
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    pypy3.9: add comment about pip
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/p/pypy3.9.rb
+++ Formula/p/pypy3.9.rb
@@ -1,8 +1,8 @@
 class Pypy39 < Formula
   desc "Implementation of Python 3 in Python"
   homepage "https://pypy.org/"
-  url "https://downloads.python.org/pypy/pypy3.9-v7.3.13-src.tar.bz2"
-  sha256 "bc6147268105e7cb3bd57b401e6d97f66aa4ede269104b2712a7cdd9f02f68cd"
+  url "https://downloads.python.org/pypy/pypy3.9-v7.3.14-src.tar.bz2"
+  sha256 "560fe6161e159557e1fe612aaadf9b293eefded1da372e70b8e3b23bba598366"
   license "MIT"
   head "https://foss.heptapod.net/pypy/pypy", using: :hg, branch: "py3.9"
 
@@ -43,9 +43,10 @@ class Pypy39 < Formula
     sha256 "09980778aa734c3037a47997f28d6db5ab18bdf2af0e49f719bfc53967fd2e82"
   end
 
+  # always pull the latest pip, https://pypi.org/project/pip/#files
   resource "pip" do
-    url "https://files.pythonhosted.org/packages/ba/19/e63fb4e0d20e48bd2167bb7e857abc0e21679e24805ba921a224df8977c0/pip-23.2.1.tar.gz"
-    sha256 "fb0bd5435b3200c602b5bf61d2d43c2f13c02e29c1707567ae7fbc514eb9faf2"
+    url "https://files.pythonhosted.org/packages/b7/06/6b1ad0ae8f97d7a0d6f6ad640db10780578999e647a9593512ceb6f06469/pip-23.3.2.tar.gz"
+    sha256 "7fd9972f96db22c8077a1ee2691b172c8089b17a5652a44494a9ecb0d78f9149"
   end
 
   # Build fixes:
