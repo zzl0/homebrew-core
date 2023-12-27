4ec92d7c46d4758d3094be8dd2d1665c8f70580d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-12-27 19:01:02 +0000

    asyncapi: update 1.2.29 bottle.

--- Formula/a/asyncapi.rb
+++ Formula/a/asyncapi.rb
@@ -8,13 +8,13 @@ class Asyncapi < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "81df0691a2b513742f79457eebdaaeb9be8370f103396e1eca23ac8c0eae05dd"
-    sha256 cellar: :any,                 arm64_ventura:  "81df0691a2b513742f79457eebdaaeb9be8370f103396e1eca23ac8c0eae05dd"
-    sha256 cellar: :any,                 arm64_monterey: "81df0691a2b513742f79457eebdaaeb9be8370f103396e1eca23ac8c0eae05dd"
-    sha256 cellar: :any,                 sonoma:         "2530c45b5ccb1796cfb1063477786377c0301865c45e8f70ba3f0d4c72793034"
-    sha256 cellar: :any,                 ventura:        "2530c45b5ccb1796cfb1063477786377c0301865c45e8f70ba3f0d4c72793034"
-    sha256 cellar: :any,                 monterey:       "2530c45b5ccb1796cfb1063477786377c0301865c45e8f70ba3f0d4c72793034"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f580289e452888b0ee6c443b6dbb88140b9868177073f279ef0e884b766197ed"
+    sha256 cellar: :any,                 arm64_sonoma:   "9c590d47fea4e6182f6520a4aa4d037d9267468d946197fa2092161aeeba7762"
+    sha256 cellar: :any,                 arm64_ventura:  "9c590d47fea4e6182f6520a4aa4d037d9267468d946197fa2092161aeeba7762"
+    sha256 cellar: :any,                 arm64_monterey: "9c590d47fea4e6182f6520a4aa4d037d9267468d946197fa2092161aeeba7762"
+    sha256 cellar: :any,                 sonoma:         "19fca66f0cf5bfdee33b31c11136899dadf0c6639baeec34eda87d198175e7b9"
+    sha256 cellar: :any,                 ventura:        "19fca66f0cf5bfdee33b31c11136899dadf0c6639baeec34eda87d198175e7b9"
+    sha256 cellar: :any,                 monterey:       "19fca66f0cf5bfdee33b31c11136899dadf0c6639baeec34eda87d198175e7b9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8172eab91c929d884d7040544a4fa2f74dcd997dcb7fc14473b657df529a6749"
   end
 
   depends_on "node"
