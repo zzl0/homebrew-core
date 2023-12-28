c2191378d873608b0bf3895bce719ee973c057fa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-12-28 20:49:32 +0000

    php-cs-fixer: update 3.43.0 bottle.

--- Formula/p/php-cs-fixer.rb
+++ Formula/p/php-cs-fixer.rb
@@ -6,7 +6,7 @@ class PhpCsFixer < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "1b555404d6fd84c56b35d74bec6d6553894fd0fab294bc361a634b3cd44ba498"
+    sha256 cellar: :any_skip_relocation, all: "dcc9490080d014c07f65b9a064af1314e7a4725764351b5a0a775de279a25644"
   end
 
   depends_on "php"
