3e9e74ea20e13d0231f8af07218c885513c1edca
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-12-28 01:26:28 +0000

    k9s: update 0.30.5 bottle.

--- Formula/k/k9s.rb
+++ Formula/k/k9s.rb
@@ -13,13 +13,13 @@ class K9s < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "76a1abbdd5a9fd003126ed0f82e935c3fb7f557728658128bbe8c51efee23902"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a796da08d3deb1833b093763e7d2d5209420f5c0c13fd6279d33b05b554b775f"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c3b6991a500e557044947823736d583e1e5e03c36c05ee8213186317700effa9"
-    sha256 cellar: :any_skip_relocation, sonoma:         "83092716021af18ba53b8d25abbb433581cf04d9ab3c0b351a9d6f2f4037dfdd"
-    sha256 cellar: :any_skip_relocation, ventura:        "5984c9617e0fa5674265b93e1905f8bb6b80f66c900913b9c7bc2f4f6b9dd1a0"
-    sha256 cellar: :any_skip_relocation, monterey:       "f3d9856495f11191141641068621bd1ff344c7a6c4b8ddea9090b94fbd56b622"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3aadcfec9f52f3c507f57f9af47d0412e24a5a180dbc310c1c718a9087b05d1c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "9ad7a9475708313286882b247ee8f79644406fa08c7a9727442cb95217a26923"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "cdbbbc7ff6a7d88bf6bdd3ce6c4b1395f8db1a4fdccd9bdc9d001cd10b0edfc6"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "60c0550b0f7bcb88e9deef05a5c20135eadaa2b31b594b725ebea3893f4c556c"
+    sha256 cellar: :any_skip_relocation, sonoma:         "631ff6262d156389c7d945a455a16454e92b0d021d7d3eda1f226e081d6f9b05"
+    sha256 cellar: :any_skip_relocation, ventura:        "1d71e706e1a348db12b1bb5cf324f6d5b41d20b60b0863eac13ad27424e00120"
+    sha256 cellar: :any_skip_relocation, monterey:       "c927b25a7600952bd439fd3bc792699dd7ae1473506a2e81afb076bcd8cccbac"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b5f7a622d4e0d4b76fc89439338dd84e6586dd1826effb01759c3edcf3be6630"
   end
 
   depends_on "go" => :build
