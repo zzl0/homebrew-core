805f1c7711469a369d95c30d712f216b65b2ec80
Bevan Kay <email@bevankay.me>
2023-12-29 23:01:01 +1100

    geeqie: revision bump for jpeg-xl 0.9.0

--- Formula/g/geeqie.rb
+++ Formula/g/geeqie.rb
@@ -4,7 +4,7 @@ class Geeqie < Formula
   url "https://github.com/BestImageViewer/geeqie/releases/download/v2.1/geeqie-2.1.tar.xz"
   sha256 "d0511b7840169d37e457880d1ab2a787c52b609a0ab8fa1a8a391e841fdd2dde"
   license "GPL-2.0-or-later"
-  revision 2
+  revision 3
 
   livecheck do
     url :stable
