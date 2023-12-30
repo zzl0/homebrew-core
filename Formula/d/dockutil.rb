e6e990b8e4ddfaa35ae2aff32ca0e46139719772
Branch Vincent <branchevincent@gmail.com>
2023-12-30 12:11:41 -0800

    dockutil 3.1.0

--- Formula/d/dockutil.rb
+++ Formula/d/dockutil.rb
@@ -1,35 +1,22 @@
 class Dockutil < Formula
   desc "Tool for managing dock items"
   homepage "https://github.com/kcrawford/dockutil"
-  url "https://github.com/kcrawford/dockutil/archive/refs/tags/2.0.5.tar.gz"
-  sha256 "6dbbc1467caaab977bf4c9f2d106ceadfedd954b6a4848c54c925aff81159a65"
+  url "https://github.com/kcrawford/dockutil/archive/refs/tags/3.1.0.tar.gz"
+  sha256 "5f45a9079da6b3cb7e832ae0dd8c10cddf96fb8ab9096a6c5cf74bb9f09950e7"
   license "Apache-2.0"
+  head "https://github.com/kcrawford/dockutil.git", branch: "main"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "f5f87d9e286c2b294bb157ac9f87baa2720fff044c7a92c0b80b9cb82db8a87e"
   end
 
-  head do
-    url "https://github.com/kcrawford/dockutil.git", branch: "main"
-
-    depends_on xcode: ["13.0", :build]
-
-    uses_from_macos "swift"
-  end
-
-  # https://github.com/kcrawford/dockutil/pull/131
-  # https://github.com/Homebrew/homebrew-core/pull/97394
-  deprecate! date: "2023-09-03", because: :does_not_build
-
+  depends_on xcode: ["13.0", :build]
   depends_on :macos
+  uses_from_macos "swift"
 
   def install
-    if build.head?
-      system "swift", "build", "--disable-sandbox", "--configuration", "release"
-      bin.install ".build/release/dockutil"
-    else
-      bin.install "scripts/dockutil"
-    end
+    system "swift", "build", "--disable-sandbox", "--configuration", "release"
+    bin.install ".build/release/dockutil"
   end
 
   test do
