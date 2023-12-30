b7789dc6c3ab4e1ae846dc64426fb60fe8dd15c3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-12-30 21:53:29 +0000

    rav1e 0.7.0
    ravie: bump resources

--- Formula/r/rav1e.rb
+++ Formula/r/rav1e.rb
@@ -5,13 +5,13 @@ class Rav1e < Formula
   head "https://github.com/xiph/rav1e.git", branch: "master"
 
   stable do
-    url "https://github.com/xiph/rav1e/archive/refs/tags/v0.6.6.tar.gz"
-    sha256 "723696e93acbe03666213fbc559044f3cae5b8b888b2ddae667402403cff51e5"
+    url "https://github.com/xiph/rav1e/archive/refs/tags/v0.7.0.tar.gz"
+    sha256 "dd6c4b771d985f547787383f5d77bc124ac406d574a308a897da9642410c1855"
 
     # keep the version in sync
     resource "Cargo.lock" do
-      url "https://github.com/xiph/rav1e/releases/download/v0.6.6/Cargo.lock"
-      sha256 "2014f7d76e7d0d7eaa63158ef5a1a1cea15a095fd5fb79b20b1052015a7fcd0c"
+      url "https://github.com/xiph/rav1e/releases/download/v0.7.0/Cargo.lock"
+      sha256 "2c5b50b978cc1e8cddd898c226276100419953ff9e0bafc5b02fbdb67a9dd346"
     end
   end
 
