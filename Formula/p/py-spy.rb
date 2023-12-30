cbab7b426da05e016105a3b712ff6a32b0129a15
Michael Cho <20700669+cho-m@users.noreply.github.com>
2023-12-30 14:14:35 +0000

    py-spy: update 0.3.14 bottle.

--- Formula/p/py-spy.rb
+++ Formula/p/py-spy.rb
@@ -15,9 +15,11 @@ class PySpy < Formula
   end
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "4e312bcb385abab9511b9e96b4575180ee43734ba88293102b7a26f5a1a102f5"
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "6640d4124619c0e3d007cb0284f2fb33d393c279baee741432d586c28e48f612"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "4c9528218834611e0d11368cd892d576887fa0c614cda521203b9c665b000785"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0cbeb6a465786ff60f02816f708b1184612fbb27a3142cdf9731cc70f6b5ec59"
+    sha256 cellar: :any_skip_relocation, sonoma:         "c9fa48e910c4b7df321fa6096f1af1f24bd8894b54a643af861c917be940a928"
     sha256 cellar: :any_skip_relocation, ventura:        "633076498c9549f079573bf14bd52590fa001929e36136047f99369e2cf86f84"
     sha256 cellar: :any_skip_relocation, monterey:       "e4e51038926d8e3e375f02c0e1511c7eb8274a40dfb4509f8d0d36e5a4ee1ff0"
     sha256 cellar: :any_skip_relocation, big_sur:        "411fd9ea3515958e15d197598cfe7e39cc9087cb86c2fd13db6e5af8dbb78864"
