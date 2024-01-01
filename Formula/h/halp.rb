dd3ef00d3149fb40e37d22acfb7c90ac1ddb054a
Bevan Kay <email@bevankay.me>
2024-01-01 02:01:14 +0000

    halp: add 0.1.7 bottle.

--- Formula/h/halp.rb
+++ Formula/h/halp.rb
@@ -6,6 +6,16 @@ class Halp < Formula
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/orhun/halp.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "845b1a13a6e9af898ff014d4ed91c6519fbac150ef7f1dfa7d4aafca90202205"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "d7ce882891a41e4dc4ca293ead3b7d01eb4b2fad57cb2b2513b2e8935cbe4e9b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c0ab6b981379efece5204560139c5a7c91bdd4ca49c244c2ac88b8f4868eecee"
+    sha256 cellar: :any_skip_relocation, sonoma:         "cf695d832c299486b4fc0609d66d8050f164415fb86d966797bcb0e74f23b6fe"
+    sha256 cellar: :any_skip_relocation, ventura:        "ad8e26ebdc55f290d8c9b734e89cfb11c3856a1b07b58f25c9703e624807fb57"
+    sha256 cellar: :any_skip_relocation, monterey:       "eda52507afd62e7ed2d9007a725aca4a3ec69c271d2feba1d876a1f56adb5682"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "be7cb64c843bcee429b3fd7e7dadee064a3803a2dc500f7eff674787e4ccd695"
+  end
+
   depends_on "rust" => :build
 
   def install
