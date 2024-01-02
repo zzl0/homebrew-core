613b9ad38149d2c185be0b0bd8ec7b3712c8b273
git <badonyx@proton.me>
2024-01-02 20:13:04 +0000

    gossip 0.9.0

--- Formula/g/gossip.rb
+++ Formula/g/gossip.rb
@@ -2,8 +2,8 @@ class Gossip < Formula
   desc "Desktop client for Nostr written in Rust"
   homepage "https://github.com/mikedilger/gossip"
   url "https://github.com/mikedilger/gossip.git",
-      tag:      "v0.8.2",
-      revision: "32eecc4c465660be149148f8940ae4ec4a5f94da"
+      tag:      "v0.9.0",
+      revision: "f1442ae2311bcf72fb8d4e716bb6ca471a78b7b2"
   license "MIT"
   head "https://github.com/mikedilger/gossip.git", branch: "master"
 
@@ -26,7 +26,7 @@ class Gossip < Formula
 
   def install
     ENV["RUSTFLAGS"] = "--cfg tokio_unstable"
-    system "cargo", "install", *std_cargo_args, "--features", "lang-cjk"
+    system "cargo", "install", *std_cargo_args(path: "gossip-bin"), "--features", "lang-cjk"
   end
 
   test do
