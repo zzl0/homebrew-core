320c39a461aa47ae906d7c736d1a0a85aef5c247
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-01-02 12:13:42 +0000

    sdns: update 1.3.6 bottle.

--- Formula/s/sdns.rb
+++ Formula/s/sdns.rb
@@ -7,15 +7,13 @@ class Sdns < Formula
   head "https://github.com/semihalev/sdns.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "1a89801291167e410a70478bb281dc5cc27f284fc6ccb53a9909dcb8f7fbefc6"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "2db72f4bc242268228c14aff700bded9c258979acf776a83bcef6faf99d9efdc"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "7ff40495f1e536d0ca5406a3c24583f9fa2902cc8510b859dff2ebb606c1ab7a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "63f5d2d4a95e9366b6ba56e1b5665c73cd10fc98944a02d35d9f031457d2e799"
-    sha256 cellar: :any_skip_relocation, sonoma:         "301fc244c40e268663d622ea2768ce19929e28dbceab662a168354290568c1ce"
-    sha256 cellar: :any_skip_relocation, ventura:        "bcf312133b2e6bc720fe54245ca761720f4e1ab93a58a99c714a8bbf3c432004"
-    sha256 cellar: :any_skip_relocation, monterey:       "562d73d3c62088f24a3a307a6c575e5ce92a7dfd052fdf3732d19e6c567a592c"
-    sha256 cellar: :any_skip_relocation, big_sur:        "e35adcdc17988c5b27fd3d928cf3c962eca1ebf821e4bc984f1478ce0ca09bc2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0b15d61513cba8faa07b85ed6185022a97f9c31af8446ac968e1ca2a5fc72efb"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "ccfce9784c9cd606a6bde0b291a5a5e8624cb510b57330a68b714dd707e2ea2a"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "66d5d60257c3cecca9439f494f3f1b13d51209dd67d53325910674d581fe9efd"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3ebccb0e1e6337f15598400d33f1f2c0878e8771da935f8ccbc8115d773be34b"
+    sha256 cellar: :any_skip_relocation, sonoma:         "9c0561c5c503d21697071d0fb690b79dc6e629778c3f3e2978bfdad008413805"
+    sha256 cellar: :any_skip_relocation, ventura:        "5d8ead7596c7019b08343de9252bd66c59bf46340b80f5cb600833584014448a"
+    sha256 cellar: :any_skip_relocation, monterey:       "6b4863b33896de4b439513dfbc4c35c0fdee8a9a1e631f9a40a456711517e89d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e5188b90cff22adcd0d0f751c95b71ed4bf3416a9f98c4a36b8da3c03c3de07d"
   end
 
   depends_on "go" => :build
