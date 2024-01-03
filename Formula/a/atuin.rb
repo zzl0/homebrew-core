6429ab72f69ea0013cf9ba40521c37eee5e050c1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-01-03 15:48:43 +0000

    atuin: update 17.2.0 bottle.

--- Formula/a/atuin.rb
+++ Formula/a/atuin.rb
@@ -6,13 +6,13 @@ class Atuin < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "d62f71e30e13567c5ed6a3db149487d5edd92ab99e82d5dafe62a45f44274c84"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3732fe398acb59319f3bb7be159224c3f7a6b1e3eebc8fbc6a3c135c9de94849"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "aaf9728ddb5f734142d49206bcaf9ae147f3e7a0f98a4f8d70e141d08020295e"
-    sha256 cellar: :any_skip_relocation, sonoma:         "a75c96a139205ea77ab2ad14c87087b74bf825f765e7fb8d85c40b80ea6a2e07"
-    sha256 cellar: :any_skip_relocation, ventura:        "6d99deb98741a7858d186f3caa307b7752933d2dbbad75c615e1c8cd8cbe088a"
-    sha256 cellar: :any_skip_relocation, monterey:       "e1d61666845f1667e17426f42f057275c357a96f4a68d04a0c2af921fdc3ef86"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "73899566dd32eb72256264080ccb06a5939fa9b9ed787d9e573006f404eb6ad5"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "13c7a4f5ca50dbbdbb0b79bc642fa6564c454da2ee6d6b329cdd54672115dbb0"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "782ffdcd5f2c855ca9bbd8e3736686b3ce14945e13f7216ca87581befeb0f5b9"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "941676190e8782c45a91da33fe8438d34b9fd4a04b8e66a3832c67ec8344b940"
+    sha256 cellar: :any_skip_relocation, sonoma:         "9985e5774685df95d98a0a0eb65f3108e84d098d58a4fe80e378281c819a814d"
+    sha256 cellar: :any_skip_relocation, ventura:        "cd1cda6b970e2d9b950a7289d4172f32f0775a086adcca3410b486110b442ee9"
+    sha256 cellar: :any_skip_relocation, monterey:       "3c6ad816612d0a351daab5ed08bedd191b00d095d744b20b88d37412e922850e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f451633460c3bde9f6d93b86840a15dde73878e382d27ee70130b1f7abd718d2"
   end
 
   depends_on "rust" => :build
