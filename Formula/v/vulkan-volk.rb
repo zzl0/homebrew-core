192ac8ea55c534ac90f26f42f70b4d687d0a367d
Patrick Linnane <patrick@linnane.io>
2024-01-04 07:53:07 +0000

    vulkan-volk: add 1.3.268.0 bottle.

--- Formula/v/vulkan-volk.rb
+++ Formula/v/vulkan-volk.rb
@@ -11,6 +11,16 @@ class VulkanVolk < Formula
     regex(/^(?:vulkan[._-])?sdk[._-]v?(\d+(?:\.\d+)+)$/i)
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "c4fdcef8fed7247cdfb8ad61b3c04675436222954c1f81a92f0ea03ff44dcb4e"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "c87c61104f73c14a0e31c035fbfe90eeb2e240ebb5cf320898c3468521e914ad"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "08cf920caae07d2556e693a8f50215643ea7ed078dbe7ff47021fa698cc19834"
+    sha256 cellar: :any_skip_relocation, sonoma:         "ddd12208adc49f14e8f1169159ad7dc23e72ffd6730a855a1619931d4b32f60a"
+    sha256 cellar: :any_skip_relocation, ventura:        "193c7794eb88c2b178e3d7e0920b50d383ec1111b55481a1419b804b33c29756"
+    sha256 cellar: :any_skip_relocation, monterey:       "b12d48405694e5e1784737af790f9e06e6914d8165065ab825bbd07c67e61065"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7664ad5471681baf6713b26beb11a49efc4b3a54ea4b8b2b74c3e36b541f3023"
+  end
+
   depends_on "cmake" => :build
   depends_on "vulkan-headers" => [:build, :test]
   depends_on "vulkan-loader"
