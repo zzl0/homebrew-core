0f58fd66b7d15b4afe6121841eb576aa2ac1be8c
Faisal N Jawdat <faisal@faisal.com>
2024-01-03 20:38:47 -0500

    licensefinder: patch and update for Ruby 3.3

--- Formula/l/licensefinder.rb
+++ Formula/l/licensefinder.rb
@@ -2,10 +2,12 @@ class Licensefinder < Formula
   desc "Find licenses for your project's dependencies"
   homepage "https://github.com/pivotal/LicenseFinder"
   # pull from git tag as gemspec uses `git ls-files`
+  # For versions following v7.1.0, may be able to remove 4cac18e5 patch.
   url "https://github.com/pivotal/LicenseFinder.git",
       tag:      "v7.1.0",
       revision: "81092404aeaf1cb39dbf2551f50f007ed049c26c"
   license "MIT"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_sonoma:   "9ef656bf28c2086f747658798a989432e14c90e58c01c15bf792b1a5b5b711e7"
@@ -20,6 +22,14 @@ class Licensefinder < Formula
   end
 
   depends_on "ruby"
+
+  # Ruby 3.3 introduced changes that mean we now need to manually require
+  # racc. See https://bugs.ruby-lang.org/issues/19702 for details.
+  # LicenseFinder versions after v7.1.0 may address this requirement.
+  patch do
+    url "https://github.com/pivotal/LicenseFinder/commit/4cac18e5c7a48f72700b8de4db97d3150637a20d.patch?full_index=1"
+    sha256 "7a7a9b201cd34a5f868901841ba5f144f0e75580664c8ec024792449348f5875"
+  end
 
   def install
     ENV["GEM_HOME"] = libexec
@@ -36,7 +46,9 @@ class Licensefinder < Formula
     (testpath/"Gemfile").write <<~EOS
       source 'https://rubygems.org'
       gem 'license_finder', '#{version}'
+      gem 'racc'
     EOS
+
     system "bundle", "install"
     assert_match "license_finder, #{version}, #{license}",
                   shell_output(bin/"license_finder", 1)
