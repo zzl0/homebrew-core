b607ee329ae66119231691924d36945e8728b30d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-01-04 00:12:49 +0000

    vercel-cli: update 33.0.2 bottle.

--- Formula/v/vercel-cli.rb
+++ Formula/v/vercel-cli.rb
@@ -8,13 +8,13 @@ class VercelCli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "412923197e175cfd658821b008c562b30fadec0e978f172196c5440ac6df9d1a"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "412923197e175cfd658821b008c562b30fadec0e978f172196c5440ac6df9d1a"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "412923197e175cfd658821b008c562b30fadec0e978f172196c5440ac6df9d1a"
-    sha256 cellar: :any_skip_relocation, sonoma:         "f310e74f3031807b123daf0135f1ac0981234a4141d7b972965318c61586755e"
-    sha256 cellar: :any_skip_relocation, ventura:        "f310e74f3031807b123daf0135f1ac0981234a4141d7b972965318c61586755e"
-    sha256 cellar: :any_skip_relocation, monterey:       "f310e74f3031807b123daf0135f1ac0981234a4141d7b972965318c61586755e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "43a91517219c427f1e1c7c1b5739654f8215e510be8863794b5adba0e2c707d3"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "64da531621d1a67d3a2e6b2dd51c1ccf0f87a38726341decbdff5c60cd0b1dbb"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "64da531621d1a67d3a2e6b2dd51c1ccf0f87a38726341decbdff5c60cd0b1dbb"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "64da531621d1a67d3a2e6b2dd51c1ccf0f87a38726341decbdff5c60cd0b1dbb"
+    sha256 cellar: :any_skip_relocation, sonoma:         "00675756b0fd3b1bea5c35e0dea68113eaa547257a5ed6e9fcf883ebe0ea452c"
+    sha256 cellar: :any_skip_relocation, ventura:        "00675756b0fd3b1bea5c35e0dea68113eaa547257a5ed6e9fcf883ebe0ea452c"
+    sha256 cellar: :any_skip_relocation, monterey:       "00675756b0fd3b1bea5c35e0dea68113eaa547257a5ed6e9fcf883ebe0ea452c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "eac8a84f5bee71e922729d74689693441587bdccfbe045d5a00860d531eec565"
   end
 
   depends_on "node"
