bc087ac150df7c0ece5e987df465fdabdd4e56f0
Rui Chen <rui@chenrui.dev>
2024-01-05 14:04:38 -0500

    k8sgpt 0.3.25 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/k/k8sgpt.rb
@@ -0,0 +1,27 @@
+class K8sgpt < Formula
+  desc "Scanning your k8s clusters, diagnosing, and triaging issues in simple English"
+  homepage "https://k8sgpt.ai/"
+  url "https://github.com/k8sgpt-ai/k8sgpt/archive/refs/tags/v0.3.25.tar.gz"
+  sha256 "db8304df35089fb69eb16b8720f902dee508f1cbbec2ed4015a8d343d8701868"
+  license "Apache-2.0"
+  head "https://github.com/k8sgpt-ai/k8sgpt.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X main.version=#{version}
+      -X main.commit=#{tap.user}
+      -X main.date=#{time.iso8601}
+    ]
+    system "go", "build", *std_go_args(ldflags: ldflags)
+  end
+
+  test do
+    output = shell_output("#{bin}/k8sgpt analyze --explain --filter=Service", 1)
+    assert_match "try setting KUBERNETES_MASTER environment variable", output
+
+    assert_match version.to_s, shell_output("#{bin}/k8sgpt version")
+  end
+end
