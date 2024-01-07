226449596b77445fc885fb7e133bd3a3e08292be
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-01-07 08:06:20 +0000

    mise: update 2024.1.9 bottle.

--- Formula/m/mise.rb
+++ Formula/m/mise.rb
@@ -12,13 +12,13 @@ class Mise < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "1e654f78f67c6dc9bff31162009ba43309c755fa798a259874077c71ce983ffd"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "29a11b7407b34ce1082f49cf9f1c16fb11d52cba1fe8f94dea3502e03f0ae3d8"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "d662a2a53a76bdddef7f7932cbf31844cf1558186b3b745447cc0f18fca1dd7d"
-    sha256 cellar: :any_skip_relocation, sonoma:         "71d2997a1483659be330d43a23838906c62657f9fe1aed15a10a744bbd70d1cc"
-    sha256 cellar: :any_skip_relocation, ventura:        "945a65b9c809f0b570f428a0bf4ef1811ee87270ad14e9786d54cedae295152c"
-    sha256 cellar: :any_skip_relocation, monterey:       "4c3110541e1faba655e6ca96e510dbd0e7298a7ac6a74a569b6ce19adc27099d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c30019c41dfde42d8de7d1f93b218c4591aa81e5b992349eaf448366623a7324"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "4b23e1dd88d425e4796c31ee302360c1736cce87fe6c3290ee33014f67bbde54"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "cecd0a5766d30e2066473011f1272d35a0e0387bf54eceb749bad9fe2b9c932a"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3af031158909e4d5505312dab2d6c719700b369c1641c368dade0288dc824702"
+    sha256 cellar: :any_skip_relocation, sonoma:         "ac46a777471f43a7e4ff4429b91911b855c7759a9a47e479a7e8f465c86394d2"
+    sha256 cellar: :any_skip_relocation, ventura:        "d2e3d7413144982f096650feec7d247574508e5f03e3263f5196c4d239b5233c"
+    sha256 cellar: :any_skip_relocation, monterey:       "5be7aec279921bec9d821bb86379bf111ad0df8079d2512793243506e5b10ba2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8390f6a52de069f14ad0745f7f5cc37c64864a1a1a403d4f919a984f2aab377d"
   end
 
   depends_on "rust" => :build
