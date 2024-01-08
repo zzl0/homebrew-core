6000f7f864cbb270bfc1c4cd3816a6746d7d5ad6
Porkepix <porkepix@gmail.com>
2024-01-08 15:15:32 +0100

    uftrace 0.15

--- Formula/u/uftrace.rb
+++ Formula/u/uftrace.rb
@@ -1,10 +1,9 @@
 class Uftrace < Formula
   desc "Function graph tracer for C/C++/Rust"
   homepage "https://uftrace.github.io/slide/"
-  url "https://github.com/namhyung/uftrace/archive/refs/tags/v0.14.tar.gz"
-  sha256 "b81255bc288e79e96a5b158d4875fa6425ad51c85f4c4c8523c1defb9366ad12"
+  url "https://github.com/namhyung/uftrace/archive/refs/tags/v0.15.tar.gz"
+  sha256 "c4f2a45687fd39dbde509635ebf094d7ed301793920f37bcaabb8161ff69f2fd"
   license "GPL-2.0-only"
-  revision 1
   head "https://github.com/namhyung/uftrace.git", branch: "master"
 
   bottle do
