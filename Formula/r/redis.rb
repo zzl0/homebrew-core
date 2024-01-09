23bd40824fc5868a8ecfada3e75ab226247a44a4
Nhan Doan <2561973+dnhn@users.noreply.github.com>
2024-01-09 18:58:18 +0700

    redis 7.2.4

--- Formula/r/redis.rb
+++ Formula/r/redis.rb
@@ -1,8 +1,8 @@
 class Redis < Formula
   desc "Persistent key-value database, with built-in net interface"
   homepage "https://redis.io/"
-  url "https://download.redis.io/releases/redis-7.2.3.tar.gz"
-  sha256 "3e2b196d6eb4ddb9e743088bfc2915ccbb42d40f5a8a3edd8cb69c716ec34be7"
+  url "https://download.redis.io/releases/redis-7.2.4.tar.gz"
+  sha256 "8d104c26a154b29fd67d6568b4f375212212ad41e0c2caa3d66480e78dbd3b59"
   license "BSD-3-Clause"
   head "https://github.com/redis/redis.git", branch: "unstable"
 
