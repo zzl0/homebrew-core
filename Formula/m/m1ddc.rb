791e6b417cf90126697fb93e4818ca1809e91079
Porkepix <porkepix@gmail.com>
2024-01-10 19:04:59 +0100

    m1ddc 1.2.0

--- Formula/m/m1ddc.rb
+++ Formula/m/m1ddc.rb
@@ -1,8 +1,8 @@
 class M1ddc < Formula
   desc "Control external displays (USB-C/DisplayPort Alt Mode) using DDC/CI on M1 Macs"
   homepage "https://github.com/waydabber/m1ddc"
-  url "https://github.com/waydabber/m1ddc/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "d12bf9e59f9e9a09a0b6fd54bcf752cdc01dd3a8dae3df0bcaa0abf8dcf6d388"
+  url "https://github.com/waydabber/m1ddc/archive/refs/tags/v1.2.0.tar.gz"
+  sha256 "d633c06502e650108bb2f581b5db25d2592955fa9a57de7feeae3ed7710c59ca"
   license "MIT"
   head "https://github.com/waydabber/m1ddc.git", branch: "main"
 
