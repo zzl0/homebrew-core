5c95187fb6ecc5a74a0067d7c16170de2772b488
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-01-10 07:11:30 +0000

    opensca-cli: update 3.0.4 bottle.

--- Formula/o/opensca-cli.rb
+++ Formula/o/opensca-cli.rb
@@ -7,13 +7,13 @@ class OpenscaCli < Formula
   head "https://github.com/XmirrorSecurity/OpenSCA-cli.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "dd32437e2225a18a073420921c98c59a29f7be2f2a96653a9870f0a54569ef16"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "5207e068935f7f325b81e21782a205e758ae50d0c1c556149c53f7b092575eae"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e0aacbb66b6c8761399c78c0db6284334b88bd65b00a6791536326056cf98300"
-    sha256 cellar: :any_skip_relocation, sonoma:         "39c34a7be7b68b98a08f8742a8dacbf9ec63feab72d319b5f90a5dacefa66996"
-    sha256 cellar: :any_skip_relocation, ventura:        "5698ec87aa459e1726b8ae49a65d426019201cbd34297adb21396cac6214291a"
-    sha256 cellar: :any_skip_relocation, monterey:       "751674df11cb77a98a845586f8ac41ebf47a2108df4d776ade2d8c771ac32339"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "185234379a727461872ee8d31d43c5bace0176717f103d039a70e2d4b6505ab1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "dfa13b0c029685e7949510e4cfd769307d19834b5f6e053e8360bf215d35da80"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "6e6f010516788c6a9cbc96a8d87e8f21a60f57aea373401a0e365ba0bc805030"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "fd471e12aae7c8d2b7c38163eaa259e929e2f38bfb715b71a19876c0333a4597"
+    sha256 cellar: :any_skip_relocation, sonoma:         "14954df31ab1be2deaa9c2d6acbba430b62a1dee31b2ffc38748d396ea7f8188"
+    sha256 cellar: :any_skip_relocation, ventura:        "e76a15549fe154384354931b5b7090190109cf57fa4ed7dfee3f3eda378b6931"
+    sha256 cellar: :any_skip_relocation, monterey:       "4392e05c10880ce9bdd88a79d0fc8f56cdc8c6d117dd1641da11cd31c58d8715"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "06216fcc38bcc4eafe2afefbbaed174e45fbeb07c9631b469cbaa805662ba135"
   end
 
   depends_on "go" => :build
