25a9ff49514b1aa73ba40d445211cc67c53f77de
Porkepix <porkepix@gmail.com>
2024-01-10 02:35:40 +0100

    gwt 2.11.0

--- Formula/g/gwt.rb
+++ Formula/g/gwt.rb
@@ -1,8 +1,8 @@
 class Gwt < Formula
   desc "Google web toolkit"
   homepage "https://www.gwtproject.org/"
-  url "https://github.com/gwtproject/gwt/releases/download/2.10.0/gwt-2.10.0.zip"
-  sha256 "3be5fe11c27e8fd5a513eff8b14c2f26999faf4b991a8ad428f1916a36884427"
+  url "https://github.com/gwtproject/gwt/releases/download/2.11.0/gwt-2.11.0.zip"
+  sha256 "44d83cd0eb32d857f197e9e76f0fdf79f1ed240b47173bafeeaf8db2b5ad205c"
   license "Apache-2.0"
 
   livecheck do
