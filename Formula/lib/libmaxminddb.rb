8f57398c31bd24e4d5212c7d57c61bb6d3353640
Porkepix <porkepix@gmail.com>
2024-01-10 02:34:05 +0100

    libmaxminddb 1.9.1

--- Formula/lib/libmaxminddb.rb
+++ Formula/lib/libmaxminddb.rb
@@ -1,8 +1,8 @@
 class Libmaxminddb < Formula
   desc "C library for the MaxMind DB file format"
   homepage "https://github.com/maxmind/libmaxminddb"
-  url "https://github.com/maxmind/libmaxminddb/releases/download/1.8.0/libmaxminddb-1.8.0.tar.gz"
-  sha256 "1107799f77be6aa3b9796ad0eed8ffcc334bf45f8bd18e6a984d8adf3e719c6d"
+  url "https://github.com/maxmind/libmaxminddb/releases/download/1.9.1/libmaxminddb-1.9.1.tar.gz"
+  sha256 "a80682a89d915fdf60b35d316232fb04ebf36fff27fda9bd39fe8a38d3cd3f12"
   license "Apache-2.0"
 
   bottle do
