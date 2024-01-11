203f5e5541b6d85b6a41b20e0a3023cc566bd8a1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-01-11 10:38:39 +0000

    sjk: update 0.22 bottle.

--- Formula/s/sjk.rb
+++ Formula/s/sjk.rb
@@ -6,15 +6,7 @@ class Sjk < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "8624ea6fdff5006d1eab75c07239ea1196d4c69f92185545d713e01d60bd9a66"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "ad0ceb71f9c1ed966e05b7ac8816579f0a7b72cc1cae547609533d1ab1a64f2e"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ad0ceb71f9c1ed966e05b7ac8816579f0a7b72cc1cae547609533d1ab1a64f2e"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ad0ceb71f9c1ed966e05b7ac8816579f0a7b72cc1cae547609533d1ab1a64f2e"
-    sha256 cellar: :any_skip_relocation, sonoma:         "8624ea6fdff5006d1eab75c07239ea1196d4c69f92185545d713e01d60bd9a66"
-    sha256 cellar: :any_skip_relocation, ventura:        "ad0ceb71f9c1ed966e05b7ac8816579f0a7b72cc1cae547609533d1ab1a64f2e"
-    sha256 cellar: :any_skip_relocation, monterey:       "ad0ceb71f9c1ed966e05b7ac8816579f0a7b72cc1cae547609533d1ab1a64f2e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "ad0ceb71f9c1ed966e05b7ac8816579f0a7b72cc1cae547609533d1ab1a64f2e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ae7d27a086b25de39f9cc6cdeb402f7fc1fab711a23828cb7617c72b427de088"
+    sha256 cellar: :any_skip_relocation, all: "1ac4202db26376047a911b521591a4b3ce22460148c06fce568113ac6ba8f30e"
   end
 
   depends_on "openjdk"
