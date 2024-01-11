9aecf24ae72d59b4458b4b773520e0e4db6df10e
Patrick Linnane <patrick@linnane.io>
2024-01-11 03:54:16 +0000

    rathole: add 0.5.0 bottle.

--- Formula/r/rathole.rb
+++ Formula/r/rathole.rb
@@ -5,6 +5,16 @@ class Rathole < Formula
   sha256 "c8698dc507c4c2f7e0032be24cac42dd6656ac1c52269875d17957001aa2de41"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "c50a599d6af659f38e664edc68a64e310d4644e393cd6bbb3305a30742d9a359"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "17c4060ee9ee8843616218c57079814ffab9e8d6985ccc433da9b5d4796d1693"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "70de936393eb4685c9b4e5cb525ede6aa057622555fb17bbcf8eb9e2141bc175"
+    sha256 cellar: :any_skip_relocation, sonoma:         "3c511f72f60fc490db1e891416a0b279e7d5ade7984bd70d505e86b4f8859613"
+    sha256 cellar: :any_skip_relocation, ventura:        "acbf4897dd9560e0f0e9597f020b9b434e349edb5b9ec70251fa08a237bcf923"
+    sha256 cellar: :any_skip_relocation, monterey:       "87be8e479e7d36ca01aa450fd7b6f5e820054ade4a06b43ca967f0828eddee17"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9258fe59a3e086fa53c499d8a086c31fe7a779b8cd0426f85d53b6124916ce02"
+  end
+
   depends_on "rust" => :build
 
   on_linux do
