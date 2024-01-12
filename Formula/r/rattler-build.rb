6d6776975c85c34da70fc9801f2c6c965d38995f
Rui Chen <rui@chenrui.dev>
2024-01-12 02:29:06 +0000

    rattler-build: add 0.7.0 bottle.

--- Formula/r/rattler-build.rb
+++ Formula/r/rattler-build.rb
@@ -6,6 +6,16 @@ class RattlerBuild < Formula
   license "BSD-3-Clause"
   head "https://github.com/prefix-dev/rattler-build.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sonoma:   "4cdb456092ea7929b8c306a27df08eb7eca73ffc434fd911397a5aac60d50041"
+    sha256 cellar: :any,                 arm64_ventura:  "78320a7eca4dde90a40e1259318bd3d8e61ebcb0b8476abf599c084b907a3ad3"
+    sha256 cellar: :any,                 arm64_monterey: "87b51a2f44c7984f9f8d5b4898e8467850cfb0bdb08b2fbf50da194d4b51399f"
+    sha256 cellar: :any,                 sonoma:         "e68c94a6beca2e209bb7a59d9c0af8bec6c8ed9bf9b619f5570d8d4bbec7d3fd"
+    sha256 cellar: :any,                 ventura:        "7f465034a1abbd0f338a8bb72854a0d524c557186f94b6ba45b8b3d344aa7628"
+    sha256 cellar: :any,                 monterey:       "39b09f42feaf66169cbb44175df86617e11f4ab7c2f84db3d87fc3724a580e29"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "611ccf5d5a8a19a18513c5915a00baa609280b3e91dd8b6a8880aa2eee04f7d3"
+  end
+
   depends_on "pkg-config" => :build
   depends_on "rust" => :build
   depends_on "openssl@3"
