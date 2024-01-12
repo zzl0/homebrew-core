bb2e659de8fbc03a60d832d21685bb3a485155df
Patrick Linnane <patrick@linnane.io>
2024-01-12 13:36:07 -0800

    fastnetmon: revision bump (capnp 1.0.2)

--- Formula/f/fastnetmon.rb
+++ Formula/f/fastnetmon.rb
@@ -4,7 +4,7 @@ class Fastnetmon < Formula
   url "https://github.com/pavel-odintsov/fastnetmon/archive/refs/tags/v1.2.6.tar.gz"
   sha256 "b6a7d1e9ba98c1c042d774bff82ea3e8bbf03085e0be43a2676e41d590f668cf"
   license "GPL-2.0-only"
-  revision 2
+  revision 3
 
   bottle do
     rebuild 1
