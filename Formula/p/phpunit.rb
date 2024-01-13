294f30817b8cfa6b17762205cfb11cbe9a2aad9a
Porkepix <porkepix@gmail.com>
2024-01-13 18:10:01 +0100

    phpunit 10.5.6

--- Formula/p/phpunit.rb
+++ Formula/p/phpunit.rb
@@ -1,8 +1,8 @@
 class Phpunit < Formula
   desc "Programmer-oriented testing framework for PHP"
   homepage "https://phpunit.de"
-  url "https://phar.phpunit.de/phpunit-10.5.5.phar"
-  sha256 "521aef8ca6419169ed10d03f1298d1731028d6342adc7f645d51c5a6977a3afa"
+  url "https://phar.phpunit.de/phpunit-10.5.6.phar"
+  sha256 "9130ef14d18f6a77ab9b832e2c2daca34a349b0d5319a70f18604d474351c73b"
   license "BSD-3-Clause"
 
   livecheck do
