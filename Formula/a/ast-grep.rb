bf30b36953d2de7a269268092af8b4146feb9aab
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-01-13 00:29:07 +0000

    ast-grep 0.17.1

--- Formula/a/ast-grep.rb
+++ Formula/a/ast-grep.rb
@@ -1,8 +1,8 @@
 class AstGrep < Formula
   desc "Code searching, linting, rewriting"
   homepage "https://github.com/ast-grep/ast-grep"
-  url "https://github.com/ast-grep/ast-grep/archive/refs/tags/0.17.0.tar.gz"
-  sha256 "ffc432546e1e8c3bb7d0143c35e765f6562e0c2979ec723f7abf50748d0a4559"
+  url "https://github.com/ast-grep/ast-grep/archive/refs/tags/0.17.1.tar.gz"
+  sha256 "76b0a681c313b821b4d4ca8ee8ca8604c1e61fa4dcc1a94a50b24c2fb8dbc5b1"
   license "MIT"
   head "https://github.com/ast-grep/ast-grep.git", branch: "main"
 
