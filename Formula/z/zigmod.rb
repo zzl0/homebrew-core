6ba4c7506cbdbc19c2c96eeb0a904819b3880e72
Patrick Linnane <patrick@linnane.io>
2024-01-14 19:32:48 +0000

    zigmod: add 89 bottle.

--- Formula/z/zigmod.rb
+++ Formula/z/zigmod.rb
@@ -10,6 +10,16 @@ class Zigmod < Formula
     regex(/^r(\d+)$/i)
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "2018735989a352dc810333e9fe56e988f8ec6cc04e2f2ef110c423c2e8bc78d7"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "5075953d63bf642db089d7186541b5196dd02a6f874c8df558e2ee6a877dd362"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f9db743ebce1eb091fc482a50428f0c81e2750792c7239ab609fde5858a69ae9"
+    sha256 cellar: :any_skip_relocation, sonoma:         "5abcc719b5feba7cece3905da953fc78f29168ece9f782d5ef4e8ca2db014984"
+    sha256 cellar: :any_skip_relocation, ventura:        "5e9be7c44ff6c9fccd95fe91f6a6d73f1aea774021d3421f68b53dc15330b21b"
+    sha256 cellar: :any_skip_relocation, monterey:       "a3e79885e16d0f981998efe8e77528b9f2554178692094753ff82b610ac6fbcf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "494a4e835b2f3c9a9fd096b5aee9cdad64c236d1b364f2c0970d832c54b91308"
+  end
+
   depends_on "pkg-config" => :build
   depends_on "zig"
 
