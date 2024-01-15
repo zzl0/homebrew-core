ad5c99c885405aa4c04b5b144429a2b65cfa5ca5
zuisong <com.me@foxmail.com>
2024-01-15 09:28:08 +0800

    genact: add shell completion

--- Formula/g/genact.rb
+++ Formula/g/genact.rb
@@ -19,6 +19,7 @@ class Genact < Formula
 
   def install
     system "cargo", "install", *std_cargo_args
+    generate_completions_from_executable(bin/"genact", "--print-completions")
   end
 
   test do
