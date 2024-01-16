dde4562cfd264cf0d6fc02a1061cfc6ceea1d832
Timothy Sutton <tim@synthist.net>
2024-01-16 08:43:14 -0500

    fbthrift 2024.01.15.00

--- Formula/f/fbthrift.rb
+++ Formula/f/fbthrift.rb
@@ -1,8 +1,8 @@
 class Fbthrift < Formula
   desc "Facebook's branch of Apache Thrift, including a new C++ server"
   homepage "https://github.com/facebook/fbthrift"
-  url "https://github.com/facebook/fbthrift/archive/refs/tags/v2023.12.04.00.tar.gz"
-  sha256 "c3d6a85ca734d04892bffa42309f79454f8d079a3f0da8c9bc5a9494c7d3b0b3"
+  url "https://github.com/facebook/fbthrift/archive/refs/tags/v2024.01.15.00.tar.gz"
+  sha256 "e86f2e0b4ae117dc0bf3016df5e1d66d40f3c9bbd2bb602151524b3df5f4860f"
   license "Apache-2.0"
   head "https://github.com/facebook/fbthrift.git", branch: "main"
 
