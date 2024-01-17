fed082b277769f6568b4ddcabc362571197ee4dc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-01-17 17:46:59 +0000

    bareos-client: update 23.0.1 bottle.

--- Formula/b/bareos-client.rb
+++ Formula/b/bareos-client.rb
@@ -11,14 +11,13 @@ class BareosClient < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 arm64_sonoma:   "ef5f2a3d25b571296c1e138756c42662f97d447e3e1d60dcd12681b5b6b78a0c"
-    sha256 arm64_ventura:  "b8aa57ddef35e0e9c2edea1652014b71b2d6da07f4468418ae3967e5933d3341"
-    sha256 arm64_monterey: "85bb03c17350869e586b8ce6515e8ce7569a53c91667837dee04b34213af13fa"
-    sha256 sonoma:         "bb84187fcca7fb6830456d6c088a019f16ea2108c02bb0e78e1229e08ccc5f4f"
-    sha256 ventura:        "3b89a7f79abd81d62cedf03ca2385291f6d60cb21d8683914d18fd6d94c267aa"
-    sha256 monterey:       "a736bf49d969b6ef1d6db2222a21033b0513c76c267721ef3ca23e50c55d19d7"
-    sha256 x86_64_linux:   "09c26125753007bd80a9e2957416392a8914d79bf912986ed2b47048a484b232"
+    sha256 arm64_sonoma:   "a7a814cf9eac8df80afe10f4eb21fdf72c0362448b732657a43bf6f46b0cf382"
+    sha256 arm64_ventura:  "d18015280063235aed7ced1733fba06156083c05403a9f5747bc90b1d34cf089"
+    sha256 arm64_monterey: "1d56374c995c5cefea8d1432f239a556735c1bda2e0be86c6896a5b98b5948e9"
+    sha256 sonoma:         "dd5ebc261bc7413dfede0f7b009f5b4e13e5f870f90a5fe3f7a7cb31656c4b9a"
+    sha256 ventura:        "2e58648a883b85420f4a33a27a7ab1c74a6b6727dd3a0c96e5815ac6066b338a"
+    sha256 monterey:       "349f60e621070721b5b124a0996f1f5e8f7c28fcd6b1ab91c76256a5e921629a"
+    sha256 x86_64_linux:   "aaf1dc789cedd97278166c29b789c463e10b10298627ae0750a75af52abdcec9"
   end
 
   depends_on "cmake" => :build
