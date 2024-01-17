e63b03e884a1dad4c04eded115dc9ac2436a76fd
Rui Chen <rui@chenrui.dev>
2024-01-17 07:42:03 -0500

    terminalimageviewer 1.2.1

--- Formula/t/terminalimageviewer.rb
+++ Formula/t/terminalimageviewer.rb
@@ -1,8 +1,8 @@
 class Terminalimageviewer < Formula
   desc "Display images in a terminal using block graphic characters"
   homepage "https://github.com/stefanhaustein/TerminalImageViewer"
-  url "https://github.com/stefanhaustein/TerminalImageViewer/archive/refs/tags/v.1.2.tar.gz"
-  sha256 "6807ab4986b3893e97e8ee0eb59f02d93ff74a3994bf524d7747ac80b827184b"
+  url "https://github.com/stefanhaustein/TerminalImageViewer/archive/refs/tags/v1.2.1.tar.gz"
+  sha256 "08d0c30e3ffa47b69d1bce07bea56f04b7deb4a8a79307ce435a4f0852fbcd5f"
   license "Apache-2.0"
   head "https://github.com/stefanhaustein/TerminalImageViewer.git", branch: "master"
 
