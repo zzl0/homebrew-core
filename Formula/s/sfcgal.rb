c4edba5f441df7ebf794de8a2a7cce9a82907390
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-01-17 17:04:17 +0000

    sfcgal: update 1.5.1 bottle.

--- Formula/s/sfcgal.rb
+++ Formula/s/sfcgal.rb
@@ -6,13 +6,13 @@ class Sfcgal < Formula
   license "LGPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "7a9b7c29901a9e1b267511456b3ce11039cd88c865ea32e056e8cf79deb92a5c"
-    sha256 cellar: :any,                 arm64_ventura:  "002d649da42e0828a60ebb9244dd0bf76988c0c64972079191db234fc80b1cf6"
-    sha256 cellar: :any,                 arm64_monterey: "eac1d2e955ad287d4e4666431f479655916e964ad77a1ccc033aeb06696845ca"
-    sha256 cellar: :any,                 sonoma:         "393570b456f2c887e1401fb04492782cbf723f1cc3e976cad994a97ab94aac02"
-    sha256 cellar: :any,                 ventura:        "ce162156abadb2e65e7842a55a5a4876d69a0aeebafab8592c34b4c20ef9206e"
-    sha256 cellar: :any,                 monterey:       "71b2a4230207167d51f1f7d8fcbfc7615ec3db27550cd5e24ded36f0b5c1cfe9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4a34a0545d5096fc817d0e5e61474154931512f2aa4d2b4ed02e42f5563954fa"
+    sha256 cellar: :any,                 arm64_sonoma:   "4f1c614cd999a7295cb88640c695d8eaf83517f79dce924629a85fd47c827f76"
+    sha256 cellar: :any,                 arm64_ventura:  "3d2d521b724bec59ffed0036514c72554d828717863ff97fbd084a911d711188"
+    sha256 cellar: :any,                 arm64_monterey: "58fd30ef73d9392a9c607d3fde7e3b4ecc6df2549ccbba65231865e86b3e8d3b"
+    sha256 cellar: :any,                 sonoma:         "3ec852763d9340d72cb12bede2d437a15f0ceff4cdbbf81f25a6f6c461e3b141"
+    sha256 cellar: :any,                 ventura:        "b37a971fe87d1c73c7bbd8dfff0da8dadd070c00caa5774bc0cc61dd873122e0"
+    sha256 cellar: :any,                 monterey:       "c2d59ba70d6043c99589b9d91c40b8bea011bea5b8013fc0102fa331fc9c693a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "43538286e5e61e1ce3d55b7e15dca68fe8b011cfbb4ca377f7a6091514487d8a"
   end
 
   depends_on "cmake" => :build
