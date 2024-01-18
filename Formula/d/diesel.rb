08f68d6d54a33732d668aef33abb3c43998c7e8f
Bevan Kay <email@bevankay.me>
2024-01-19 09:18:10 +1100

    diesel: revision bump (mysql-client 8.3.0)

--- Formula/d/diesel.rb
+++ Formula/d/diesel.rb
@@ -4,6 +4,7 @@ class Diesel < Formula
   url "https://github.com/diesel-rs/diesel/archive/refs/tags/v2.1.4.tar.gz"
   sha256 "5aac923078a5b431902d75cfca36f2990b3f11dbb2bbbc44f4538305af939657"
   license any_of: ["Apache-2.0", "MIT"]
+  revision 1
   head "https://github.com/diesel-rs/diesel.git", branch: "master"
 
   bottle do
