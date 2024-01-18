af6d0758ebffde1c52e078b27934d4c0387ca36c
Porkepix <porkepix@gmail.com>
2024-01-19 00:20:34 +0100

    skopeo 1.14.1

--- Formula/s/skopeo.rb
+++ Formula/s/skopeo.rb
@@ -1,8 +1,8 @@
 class Skopeo < Formula
   desc "Work with remote images registries"
   homepage "https://github.com/containers/skopeo"
-  url "https://github.com/containers/skopeo/archive/refs/tags/v1.14.0.tar.gz"
-  sha256 "062ca24dcc106c3758e90a4af207b67166437ab71128bd33749b0414e0a42f79"
+  url "https://github.com/containers/skopeo/archive/refs/tags/v1.14.1.tar.gz"
+  sha256 "b174ada87751ecd7f8e0e292d163c9b0c4a2172a6ba32e1725ae272c24f7f841"
   license "Apache-2.0"
 
   bottle do
