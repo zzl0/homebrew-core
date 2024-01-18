f6e2d466c5696f7592ffa0abbc45fe0c5b4f07fa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-01-18 06:40:07 +0000

    ipopt 3.14.14

--- Formula/i/ipopt.rb
+++ Formula/i/ipopt.rb
@@ -1,8 +1,8 @@
 class Ipopt < Formula
   desc "Interior point optimizer"
   homepage "https://coin-or.github.io/Ipopt/"
-  url "https://github.com/coin-or/Ipopt/archive/refs/tags/releases/3.14.13.tar.gz"
-  sha256 "2afcb057e7cf8ed7e07f50ee0a4a06d2e4d39e0f964777e9dd55fe56199a5e0a"
+  url "https://github.com/coin-or/Ipopt/archive/refs/tags/releases/3.14.14.tar.gz"
+  sha256 "264d2d3291cd1cd2d0fa0ad583e0a18199e3b1378c3cb015b6c5600083f1e036"
   license "EPL-2.0"
   head "https://github.com/coin-or/Ipopt.git", branch: "stable/3.14"
 
@@ -43,8 +43,8 @@ class Ipopt < Formula
   end
 
   resource "test" do
-    url "https://github.com/coin-or/Ipopt/archive/refs/tags/releases/3.14.13.tar.gz"
-    sha256 "2afcb057e7cf8ed7e07f50ee0a4a06d2e4d39e0f964777e9dd55fe56199a5e0a"
+    url "https://github.com/coin-or/Ipopt/archive/refs/tags/releases/3.14.14.tar.gz"
+    sha256 "264d2d3291cd1cd2d0fa0ad583e0a18199e3b1378c3cb015b6c5600083f1e036"
   end
 
   def install
