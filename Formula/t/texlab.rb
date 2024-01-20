e0fcc411c6334714e181b9f76f23cdfdb1715615
Christian Clason <c.clason@uni-graz.at>
2024-01-20 12:39:12 +0100

    texlab 5.12.2

--- Formula/t/texlab.rb
+++ Formula/t/texlab.rb
@@ -1,8 +1,8 @@
 class Texlab < Formula
   desc "Implementation of the Language Server Protocol for LaTeX"
   homepage "https://texlab.netlify.com/"
-  url "https://github.com/latex-lsp/texlab/archive/refs/tags/v5.12.1.tar.gz"
-  sha256 "008db560759f207c31001dab94beb2229f0d263f86f4703f07d8eb8fc93e12c9"
+  url "https://github.com/latex-lsp/texlab/archive/refs/tags/v5.12.2.tar.gz"
+  sha256 "679d72a73269addcd870d6742b418aed2fb7e256137f360e2cc80271f20f0704"
   license "GPL-3.0-only"
   head "https://github.com/latex-lsp/texlab.git", branch: "master"
 
