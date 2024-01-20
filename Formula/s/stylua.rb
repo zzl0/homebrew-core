80d58fb66a1f53c987a8e893ceccec2bdcc69d8c
Christian Clason <c.clason@uni-graz.at>
2024-01-20 13:56:03 +0100

    stylua 0.20.0

--- Formula/s/stylua.rb
+++ Formula/s/stylua.rb
@@ -1,8 +1,8 @@
 class Stylua < Formula
   desc "Opinionated Lua code formatter"
   homepage "https://github.com/JohnnyMorganz/StyLua"
-  url "https://github.com/JohnnyMorganz/StyLua/archive/refs/tags/v0.19.1.tar.gz"
-  sha256 "c232227bf6085e3039b47a2ee24c76dad6ba1e786df65d9933ee000a3ee2c36e"
+  url "https://github.com/JohnnyMorganz/StyLua/archive/refs/tags/v0.20.0.tar.gz"
+  sha256 "f4a27b12669953d2edf55b89cc80381f97a2dfa735f53f95c6ae6015c8c35ffb"
   license "MPL-2.0"
 
   bottle do
