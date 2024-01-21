dd790ba01b18fb46ddd5ad0066a5959bcc219dca
Pierre Wendling <pierre.wendling.4@gmail.com>
2024-01-21 21:25:02 +0100

    aarch64-elf-gdb: enable extra features
    - Enable XML and zstd support
    - Prefer system libraries over vendored
    - Use std_configure_args

--- Formula/a/aarch64-elf-gdb.rb
+++ Formula/a/aarch64-elf-gdb.rb
@@ -5,6 +5,7 @@ class Aarch64ElfGdb < Formula
   mirror "https://ftpmirror.gnu.org/gdb/gdb-14.1.tar.xz"
   sha256 "d66df51276143451fcbff464cc8723d68f1e9df45a6a2d5635a54e71643edb80"
   license "GPL-3.0-or-later"
+  revision 1
   head "https://sourceware.org/git/binutils-gdb.git", branch: "master"
 
   livecheck do
@@ -21,12 +22,17 @@ class Aarch64ElfGdb < Formula
     sha256 x86_64_linux:   "cce543b130cb30ba013fda6eae561373ecd5afd79945d4fb09e43f2b90e80578"
   end
 
+  depends_on "pkg-config" => :build
   depends_on "aarch64-elf-gcc" => :test
   depends_on "gmp"
   depends_on "mpfr"
   depends_on "python@3.12"
+  depends_on "readline"
   depends_on "xz" # required for lzma support
+  depends_on "zstd"
 
+  uses_from_macos "expat"
+  uses_from_macos "ncurses"
   uses_from_macos "zlib"
 
   on_system :linux, macos: :ventura_or_newer do
@@ -37,21 +43,24 @@ class Aarch64ElfGdb < Formula
     target = "aarch64-elf"
     args = %W[
       --target=#{target}
-      --prefix=#{prefix}
       --datarootdir=#{share}/#{target}
       --includedir=#{include}/#{target}
       --infodir=#{info}/#{target}
       --mandir=#{man}
-      --disable-debug
-      --disable-dependency-tracking
+      --enable-tui
+      --with-curses
+      --with-expat
       --with-lzma
       --with-python=#{which("python3.12")}
+      --with-system-readline
       --with-system-zlib
+      --with-zstd
       --disable-binutils
+      --disable-nls
     ]
 
     mkdir "build" do
-      system "../configure", *args
+      system "../configure", *args, *std_configure_args
       ENV.deparallelize # Error: common/version.c-stamp.tmp: No such file or directory
       system "make"
 
