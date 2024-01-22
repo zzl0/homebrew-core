05e2bbff538094dd58f32bb01774ae788231317b
Porkepix <porkepix@gmail.com>
2024-01-22 01:24:47 +0100

    sslscan 2.1.3

--- Formula/s/sslscan.rb
+++ Formula/s/sslscan.rb
@@ -1,8 +1,8 @@
 class Sslscan < Formula
   desc "Test SSL/TLS enabled services to discover supported cipher suites"
   homepage "https://github.com/rbsec/sslscan"
-  url "https://github.com/rbsec/sslscan/archive/refs/tags/2.1.2.tar.gz"
-  sha256 "c218d21fdf0b57dfc7c7b110ff32d9c7e77eb8c631b670a2c0fc92a80ded294d"
+  url "https://github.com/rbsec/sslscan/archive/refs/tags/2.1.3.tar.gz"
+  sha256 "6beec9345635b41fa2c1bbc5f0854f10014e4b2b4179e9e9a3bda6bdb9e1aa41"
   license "GPL-3.0-or-later" => { with: "openvpn-openssl-exception" }
   head "https://github.com/rbsec/sslscan.git", branch: "master"
 
