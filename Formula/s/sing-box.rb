dd63673ce9dceafdef5817ead85727d3856a78c6
MoonFruit <dkmoonfruit@gmail.com>
2024-01-23 17:58:05 +0800

    sing-box 1.8.4

--- Formula/s/sing-box.rb
+++ Formula/s/sing-box.rb
@@ -2,8 +2,8 @@ class SingBox < Formula
   desc "Universal proxy platform"
   homepage "https://sing-box.sagernet.org"
   # using `:homebrew_curl` to work around audit failure from TLS 1.3-only homepage
-  url "https://github.com/SagerNet/sing-box/archive/refs/tags/v1.8.2.tar.gz", using: :homebrew_curl
-  sha256 "0d9b947817e33d8a965c1367f623cd3b8dc415282f9e4d658efd2573d9fd7cc1"
+  url "https://github.com/SagerNet/sing-box/archive/refs/tags/v1.8.4.tar.gz", using: :homebrew_curl
+  sha256 "949feec1da2bc9d43b6c766c1dfb6f71f737a221e5ce4220616a3900dfb40c82"
   license "GPL-3.0-or-later"
 
   bottle do
