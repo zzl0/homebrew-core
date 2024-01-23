299328d065785e3735ed4360aaca5e8322ece8fa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-01-23 19:16:08 +0000

    azion: update 1.10.4 bottle.

--- Formula/a/azion.rb
+++ Formula/a/azion.rb
@@ -6,13 +6,14 @@ class Azion < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "fda1b887935a08bae3adf62ad3d60d72702d1da377589e26486f7811d4765048"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "295507083c3290ea72a839075e88818f6fcd6d84bf7ee7d51462983f104f72e0"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "48a91c75d542709d260ce3a2239bbce60d11c0327841cb05cc22f702284a4c86"
-    sha256 cellar: :any_skip_relocation, sonoma:         "c016bff122e977d6c88243acd84ad130858d6a79a00231d2b4ebf670e0fd8c9e"
-    sha256 cellar: :any_skip_relocation, ventura:        "2e7346a090bf51f6dcd9ecef2dc759033df66b4aeb5c91e71a8b9e1a3134a491"
-    sha256 cellar: :any_skip_relocation, monterey:       "dea54445ebca6fcba8b707f611db3b7dd39ffd232fa3c13ab6f23e8078ac9773"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e0f929b1931ac3e7b779d146cda1bb3512ca8a151fa91341ce6d44acfd8fdccb"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "b3b3d6eda2a5822cb78ca5fffff5c37b8fe0d2c28c6356e2ed88c9f087500804"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "4487ac378015057c224470baf75ca08fc1f75e18ac046fc0e1644e327a1a59ac"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "587d1e6b1dc43179ccbce436dc0b352e71c1eff9e9a247eb134f0abb3d7be925"
+    sha256 cellar: :any_skip_relocation, sonoma:         "c5621cf2d316c5d4de415b3a35866dd731721148b090488224f5cdb72c86055e"
+    sha256 cellar: :any_skip_relocation, ventura:        "13678b99c6008cb5ba517b14f9ae7e7c59acc5a03f2fb35a9e907dc22c0a327a"
+    sha256 cellar: :any_skip_relocation, monterey:       "e3c979b4eb004d9ced6b7730bfaba705e9b09df68cdd49435920995b9f64b76c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "aee2860493dc890bcac98e55abc7866ff7ac2b4c7e728020ce896e69aae4d347"
   end
 
   depends_on "go" => :build
