889c944f3fc2e1c71b944fa895079918d414ec15
Porkepix <porkepix@gmail.com>
2024-01-23 12:01:58 +0100

    cargo-about 0.6.1

--- Formula/c/cargo-about.rb
+++ Formula/c/cargo-about.rb
@@ -1,8 +1,8 @@
 class CargoAbout < Formula
   desc "Cargo plugin to generate list of all licenses for a crate"
   homepage "https://github.com/EmbarkStudios/cargo-about"
-  url "https://github.com/EmbarkStudios/cargo-about/archive/refs/tags/0.6.0.tar.gz"
-  sha256 "b2967f406d68cb09dff8ffea4f60c398ad873a41ac19e6a841e30e22c730791d"
+  url "https://github.com/EmbarkStudios/cargo-about/archive/refs/tags/0.6.1.tar.gz"
+  sha256 "8ef8f0e2048b10fd2048db27c5400dd1d18be9e4a3a4735b4b7472debffecf38"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/EmbarkStudios/cargo-about.git", branch: "main"
 
