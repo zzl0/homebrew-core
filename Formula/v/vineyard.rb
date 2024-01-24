491283742ac8d078032e1a25b7b8bb5555745b12
Michka Popoff <michkapopoff@gmail.com>
2024-01-24 22:43:46 +0100

    vineyard: revision bump for boost 1.84

--- Formula/v/vineyard.rb
+++ Formula/v/vineyard.rb
@@ -6,7 +6,7 @@ class Vineyard < Formula
   url "https://github.com/v6d-io/v6d/releases/download/v0.20.3/v6d-0.20.3.tar.gz"
   sha256 "99b5165c19e0e70809cb8b8d1172f1f2dcccf28667bc8b4df46754794b7e8c16"
   license "Apache-2.0"
-  revision 1
+  revision 2
 
   bottle do
     sha256                               arm64_sonoma:   "39853b200729bfc7d43c2cb1863212a5b2d2387d3560e2a568d2b3c647571269"
