36b8b9794a79916288d6bae6b12434b0c8dd14d8
Hao Nguyen <hao@haonguyen.tech>
2024-01-24 14:44:47 +0700

    benerator 3.2.0
    bump benerator version to 3.2.0

--- Formula/b/benerator.rb
+++ Formula/b/benerator.rb
@@ -1,8 +1,8 @@
 class Benerator < Formula
   desc "Tool for realistic test data generation"
   homepage "https://rapiddweller.github.io/homebrew-benerator/"
-  url "https://github.com/rapiddweller/rapiddweller-benerator-ce/releases/download/3.1.0/rapiddweller-benerator-ce-3.1.0-jdk-11-dist.tar.gz"
-  sha256 "194feb051ae18cfcd407b8e1668ce9c60561394bc454f9fc9747c274166843bc"
+  url "https://github.com/rapiddweller/rapiddweller-benerator-ce/releases/download/3.2.0/rapiddweller-benerator-ce-3.2.0-jdk-11-dist.tar.gz"
+  sha256 "dadc11c8f05efc30c51fd12d87d6d67a960f1ce1f03a1453c65873e1642ccae4"
   license "Apache-2.0"
 
   bottle do
