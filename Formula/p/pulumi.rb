8a331b7f325ecaa85a3a315f3a5cb0fdaf527094
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-01-25 09:48:42 +0000

    pulumi: update 3.103.1 bottle.

--- Formula/p/pulumi.rb
+++ Formula/p/pulumi.rb
@@ -8,13 +8,13 @@ class Pulumi < Formula
   head "https://github.com/pulumi/pulumi.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "9d55f79b7cdd618916491eae7fe24ca18b3bca70e342a2dd03c3f7ad193d8c87"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "9fe1bbe91d188b5309fb872ef742a34afa9665ddc08e408efbd860da9694029b"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "28d096ed888033ce7006cab96d71f124b836e4fe0393193ae6a2293ac0b8873f"
-    sha256 cellar: :any_skip_relocation, sonoma:         "06a9b3361f05bf3ae39c9b3fa4b93d30d936404d6cf9843ad5d5a04c61f458c5"
-    sha256 cellar: :any_skip_relocation, ventura:        "058b2b4d96db0ae64ed71c6f9b416c934b0c37b7ff884910dbd43f026a70d13b"
-    sha256 cellar: :any_skip_relocation, monterey:       "facc85f1af58a215f142cfe0a305b7f44981c721e03c5e47fbfab1985c552562"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e7bbf8377245b34f1cc5b0c351b5878bf025e5d37ca7a26e4d36b234dc681971"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "714aee25ff42433f3b7173c5a3a0d9d8715bfb2086eec9ae2bf09db5ee593710"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3b5d4f66fdb183017f565cf375d3f3c2ae3a74696c95d8f40ab8c1f8cb378fcb"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "d5c8b767f81b62efc8651614d174ca3cde7f8f7f0c2887e604a86a2e3ac2e61b"
+    sha256 cellar: :any_skip_relocation, sonoma:         "1f0b71b37d4dc789052f1e912437f61ae73a0a0d0c07cf8403be454c2c288cf8"
+    sha256 cellar: :any_skip_relocation, ventura:        "880eee2061081e43c7458dd2012007fad7c67b8db3a10a2129b196ff7446e13f"
+    sha256 cellar: :any_skip_relocation, monterey:       "0b68e2b19d53e3074007f1349a80818af7fddd0c7248a3ca56fa80b6240f9759"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "849534184ddb1c248e3d727be9ef756ea6a621fd9abdb7926aafb9f8e83bce2e"
   end
 
   depends_on "go" => :build
