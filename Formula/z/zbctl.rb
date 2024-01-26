a82b2a862537bae0d2fcd6ffa061750da9a97e43
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-01-26 18:57:23 +0000

    zbctl 8.4.1

--- Formula/z/zbctl.rb
+++ Formula/z/zbctl.rb
@@ -2,8 +2,8 @@ class Zbctl < Formula
   desc "Zeebe CLI client"
   homepage "https://docs.camunda.io/docs/apis-clients/cli-client/index/"
   url "https://github.com/camunda/zeebe.git",
-      tag:      "8.4.0",
-      revision: "c59d24240d3c44a083f35ab315583f7f9724a337"
+      tag:      "8.4.1",
+      revision: "9b2059f6e3439f4642d49a1195c5a4c267861c98"
   license "Apache-2.0"
   head "https://github.com/camunda/zeebe.git", branch: "develop"
 
