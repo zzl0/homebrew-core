840599401ed8018a6a9c65b272896713c119ebf0
Rui Chen <rui@chenrui.dev>
2024-01-27 19:44:49 -0500

    mbedtls@2 2.28.7

--- Formula/m/mbedtls@2.rb
+++ Formula/m/mbedtls@2.rb
@@ -1,8 +1,8 @@
 class MbedtlsAT2 < Formula
   desc "Cryptographic & SSL/TLS library"
   homepage "https://tls.mbed.org/"
-  url "https://github.com/Mbed-TLS/mbedtls/archive/refs/tags/mbedtls-2.28.6.tar.gz"
-  sha256 "18cac49f4efef7269d233972bb09c57ace40d992229fa49804e7b10cf0debe43"
+  url "https://github.com/Mbed-TLS/mbedtls/archive/refs/tags/mbedtls-2.28.7.tar.gz"
+  sha256 "4390bc4ab1ea9a1ddf3725f540d0f80838c656d1d7987a1cee8b4da43e4571de"
   license "Apache-2.0"
   head "https://github.com/Mbed-TLS/mbedtls.git", branch: "mbedtls-2.28"
 
