c007686e99bfc577ad3738c52f55748c2c9aa6b9
kohashimoto <kohashimoto@bbtower.co.jp>
2024-01-28 16:50:44 +0900

    kubetui 1.5.0 (new formula)

---
+++ Formula/k/kubetui.rb
@@ -0,0 +1,34 @@
+class Kubetui < Formula
+  desc "TUI tool for monitoring and exploration of Kubernetes resources"
+  homepage "https://github.com/sarub0b0/kubetui"
+  url "https://github.com/sarub0b0/kubetui/archive/refs/tags/v1.5.0.tar.gz"
+  sha256 "137ef45cb539a56b16990c72ae60c7247d4ff90be3c16eec029dbd5e0480e677"
+  license "MIT"
+  head "https://github.com/sarub0b0/kubetui.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/kubetui --version")
+
+    # Use pty because it fails with this error in Linux CI:
+    #   failed to enable raw mode: Os { code: 6, kind: Uncategorized, message: "No such device or address" }
+    r, _w, pid = PTY.spawn("#{bin}/kubetui --kubeconfig not_exist")
+
+    output = ""
+    begin
+      r.each_line { |line| output += line }
+    rescue Errno::EIO
+      # GNU/Linux raises EIO when read is done on closed pty
+    ensure
+      Process.wait(pid)
+    end
+
+    assert_match "failed to read kubeconfig", output
+    assert_equal 1, $CHILD_STATUS.exitstatus
+  end
+end
