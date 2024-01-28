c11945fb5e1b27b93d45ca3cde0f805b4f44a159
Patrick Linnane <patrick@linnane.io>
2024-01-28 02:34:06 +0000

    tfautomv: add 0.5.4 bottle.

--- Formula/t/tfautomv.rb
+++ Formula/t/tfautomv.rb
@@ -6,6 +6,16 @@ class Tfautomv < Formula
   license "Apache-2.0"
   head "https://github.com/busser/tfautomv.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "81e7a98f47adfbdedaf6464a05d618657ae00565cd9da4afe84a30fdb61ce0b3"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "81e7a98f47adfbdedaf6464a05d618657ae00565cd9da4afe84a30fdb61ce0b3"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "81e7a98f47adfbdedaf6464a05d618657ae00565cd9da4afe84a30fdb61ce0b3"
+    sha256 cellar: :any_skip_relocation, sonoma:         "2dd052ac8354764aefb5be8f33dcfdfc1349a79626e0de9e80793174e66e9b97"
+    sha256 cellar: :any_skip_relocation, ventura:        "2dd052ac8354764aefb5be8f33dcfdfc1349a79626e0de9e80793174e66e9b97"
+    sha256 cellar: :any_skip_relocation, monterey:       "2dd052ac8354764aefb5be8f33dcfdfc1349a79626e0de9e80793174e66e9b97"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "740603825102cc347d4b2cf42090bc45f2d41a3f9cc28268b63042ad8ff89c4a"
+  end
+
   depends_on "go" => :build
   depends_on "opentofu" => :test
 
