3111064f60407bc4b837d696dfdf5f9200be9f31
miccal <miccal.matthews@gmail.com>
2024-01-28 11:46:54 +0800

    libidn2 2.3.7

--- Formula/lib/libidn2.rb
+++ Formula/lib/libidn2.rb
@@ -1,10 +1,10 @@
 class Libidn2 < Formula
   desc "International domain name library (IDNA2008, Punycode and TR46)"
   homepage "https://www.gnu.org/software/libidn/#libidn2"
-  url "https://ftp.gnu.org/gnu/libidn/libidn2-2.3.5.tar.gz"
-  mirror "https://ftpmirror.gnu.org/libidn/libidn2-2.3.5.tar.gz"
-  mirror "http://ftp.gnu.org/gnu/libidn/libidn2-2.3.5.tar.gz"
-  sha256 "0c19609b3ba7450a9c8b0536accca8d091cdd9fe6a24307af38e23b3bddec803"
+  url "https://ftp.gnu.org/gnu/libidn/libidn2-2.3.7.tar.gz"
+  mirror "https://ftpmirror.gnu.org/libidn/libidn2-2.3.7.tar.gz"
+  mirror "http://ftp.gnu.org/gnu/libidn/libidn2-2.3.7.tar.gz"
+  sha256 "4c21a791b610b9519b9d0e12b8097bf2f359b12f8dd92647611a929e6bfd7d64"
   license any_of: ["GPL-2.0-or-later", "LGPL-3.0-or-later"]
 
   livecheck do
