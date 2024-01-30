a88e9bf3a74c03df56ea4a86e9b791dd85c1437f
Porkepix <porkepix@gmail.com>
2024-01-30 01:21:50 +0100

    neon 0.33.0

--- Formula/n/neon.rb
+++ Formula/n/neon.rb
@@ -1,11 +1,10 @@
 class Neon < Formula
   desc "HTTP and WebDAV client library with a C interface"
   homepage "https://notroj.github.io/neon/"
-  url "https://notroj.github.io/neon/neon-0.32.5.tar.gz"
-  mirror "https://fossies.org/linux/www/neon-0.32.5.tar.gz"
-  sha256 "4872e12f802572dedd4b02f870065814b2d5141f7dbdaf708eedab826b51a58a"
+  url "https://notroj.github.io/neon/neon-0.33.0.tar.gz"
+  mirror "https://fossies.org/linux/www/neon-0.33.0.tar.gz"
+  sha256 "659a5cc9cea05e6e7864094f1e13a77abbbdbab452f04d751a8c16a9447cf4b8"
   license "LGPL-2.0-or-later"
-  revision 1
 
   livecheck do
     url :homepage
