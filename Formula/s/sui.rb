28f951ce43fe1471340a1e262f45470be58dbbe4
John Martin <johnm@mystenlabs.com>
2024-01-31 08:45:35 -0800

    sui 1.17.3 (new formula)

---
+++ Formula/s/sui.rb
@@ -0,0 +1,36 @@
+class Sui < Formula
+  desc "Next-generation smart contract platform powered by the Move programming language"
+  homepage "https://sui.io"
+  url "https://github.com/MystenLabs/sui/archive/refs/tags/testnet-v1.17.3.tar.gz"
+  sha256 "815e6acf5756e7eeab7fb1494642e3d26a69a66da260deac8efac82c5b280771"
+  license "Apache-2.0"
+
+  livecheck do
+    url :stable
+    regex(/^testnet[._-]v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  depends_on "cmake" => :build
+  depends_on "libpq" => :build
+  depends_on "rust" => :build
+
+  on_linux do
+    depends_on "llvm" => :build
+  end
+
+  def install
+    system "cargo", "install", *std_cargo_args(path: "crates/sui")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/sui --version")
+
+    (testpath/"testing.keystore").write <<~EOS
+      [
+        "AOLe60VN7M+X7H3ZVEdfNt8Zzsj1mDJ7FlAhPFWSen41"
+      ]
+    EOS
+    keystore_output = shell_output("#{bin}/sui keytool --keystore-path testing.keystore list")
+    assert_match "0xd52f9cae5db1f8ab2cb0ac437cbcdda47900e92ee0a0c06906ffc84e26f999ce", keystore_output
+  end
+end
