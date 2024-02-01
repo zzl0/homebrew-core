e514c1f92a794759bb04decbb1cf9bcae7befb19
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-02-01 16:09:36 +0000

    allure: update 2.27.0 bottle.

--- Formula/a/allure.rb
+++ Formula/a/allure.rb
@@ -11,7 +11,7 @@ class Allure < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "d359bf94383022ba443bf511229c721388b40a89d9a5448b96a9947131546287"
+    sha256 cellar: :any_skip_relocation, all: "a0279241d2318ba52abfc282ff2e5daece60a269b337b43107095bd093582ba1"
   end
 
   depends_on "openjdk"
