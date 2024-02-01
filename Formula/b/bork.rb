6648073c38c04e25ea4368516be78c37e049fef3
Skylar MacDonald <biology-loose0g@icloud.com>
2024-02-01 18:36:11 +0000

    bork 0.14.0

--- Formula/b/bork.rb
+++ Formula/b/bork.rb
@@ -1,8 +1,8 @@
 class Bork < Formula
   desc "Bash-Operated Reconciling Kludge"
   homepage "https://bork.sh/"
-  url "https://github.com/borksh/bork/archive/refs/tags/v0.13.0.tar.gz"
-  sha256 "5eaca1ebd984121df008b93c43ac259a455db7ccf13da1b1465d704e1faab563"
+  url "https://github.com/borksh/bork/archive/refs/tags/v0.14.0.tar.gz"
+  sha256 "718331c54c94bf7eddeff089227c0f57093361f7e6e24066cb544cc9ebd2f6c5"
   license "Apache-2.0"
   head "https://github.com/borksh/bork.git", branch: "main"
 
@@ -21,9 +21,6 @@ class Bork < Formula
   end
 
   def install
-    files = %w[types/shells.sh types/pipsi.sh types/cask.sh test/type-pipsi.bats test/type-cask.bats]
-    inreplace files, "/usr/local/", HOMEBREW_PREFIX
-
     man1.install "docs/bork.1"
     prefix.install %w[bin lib test types]
   end
@@ -31,7 +28,7 @@ class Bork < Formula
   test do
     expected_output = "checking: directory #{testpath}/foo\r" \
                       "missing: directory #{testpath}/foo           \n" \
-                      "verifying : directory #{testpath}/foo\n" \
+                      "verifying install: directory #{testpath}/foo\n" \
                       "* success\n"
     assert_match expected_output, shell_output("#{bin}/bork do ok directory #{testpath}/foo", 1)
   end
