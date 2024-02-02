28cf5ba8effa0edb49555983b12fe298b38b9c50
zuisong <com.me@foxmail.com>
2024-02-02 13:00:50 +0800

    pixi 0.13.0

--- Formula/p/pixi.rb
+++ Formula/p/pixi.rb
@@ -1,8 +1,8 @@
 class Pixi < Formula
   desc "Package management made easy"
   homepage "https://pixi.sh"
-  url "https://github.com/prefix-dev/pixi/archive/refs/tags/v0.12.0.tar.gz"
-  sha256 "99919d95508d3d561910d6f4724336128c56a7ac10b35f2c7f7c256bbac64490"
+  url "https://github.com/prefix-dev/pixi/archive/refs/tags/v0.13.0.tar.gz"
+  sha256 "7b4416899be4a308cdb55db6665c20870d7a3c614c4f99aa79fb91af282fcd32"
   license "BSD-3-Clause"
   head "https://github.com/prefix-dev/pixi.git", branch: "main"
 
