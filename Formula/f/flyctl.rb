21be8e5356f4aca399ea121091c9a0353ea42d8b
Bevan Kay <email@bevankay.me>
2024-02-02 08:58:04 +0100

    flyctl 0.1.147

--- Formula/f/flyctl.rb
+++ Formula/f/flyctl.rb
@@ -2,8 +2,8 @@ class Flyctl < Formula
   desc "Command-line tools for fly.io services"
   homepage "https://fly.io"
   url "https://github.com/superfly/flyctl.git",
-      tag:      "v0.1.146",
-      revision: "81fceeabde42511cf2a585e068fe8e415322657c"
+      tag:      "v0.1.147",
+      revision: "4c5c97cd4cf9fba252937825fba858177dff24ea"
   license "Apache-2.0"
   head "https://github.com/superfly/flyctl.git", branch: "master"
 
