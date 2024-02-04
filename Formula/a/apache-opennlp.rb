c6068b140cc6875c43d0a87cc22c490e00761f09
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-02-04 18:24:39 +0000

    apache-opennlp 2.3.2

--- Formula/a/apache-opennlp.rb
+++ Formula/a/apache-opennlp.rb
@@ -1,9 +1,9 @@
 class ApacheOpennlp < Formula
   desc "Machine learning toolkit for processing natural language text"
   homepage "https://opennlp.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=opennlp/opennlp-2.3.1/apache-opennlp-2.3.1-bin.tar.gz"
-  mirror "https://archive.apache.org/dist/opennlp/opennlp-2.3.1/apache-opennlp-2.3.1-bin.tar.gz"
-  sha256 "4637782175fef7992f3a125a967064bf502de99532988dda305a0b0159d6f970"
+  url "https://www.apache.org/dyn/closer.lua?path=opennlp/opennlp-2.3.2/apache-opennlp-2.3.2-bin.tar.gz"
+  mirror "https://archive.apache.org/dist/opennlp/opennlp-2.3.2/apache-opennlp-2.3.2-bin.tar.gz"
+  sha256 "8d053305c3f1321392323cd5bd17c2c5e2bb5c6daba218be06d545c614e58b84"
   license "Apache-2.0"
 
   bottle do
