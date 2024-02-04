4b17018419150de68d7319dc87575152e7ea5f6d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-02-04 15:25:06 +0000

    bindgen 0.69.4

--- Formula/b/bindgen.rb
+++ Formula/b/bindgen.rb
@@ -1,8 +1,8 @@
 class Bindgen < Formula
   desc "Automatically generates Rust FFI bindings to C (and some C++) libraries"
   homepage "https://rust-lang.github.io/rust-bindgen/"
-  url "https://github.com/rust-lang/rust-bindgen/archive/refs/tags/v0.69.3.tar.gz"
-  sha256 "5cdaa156403841e7b286ccbb7b31398c8b49b99f89ebf329457101819aa5eaf0"
+  url "https://github.com/rust-lang/rust-bindgen/archive/refs/tags/v0.69.4.tar.gz"
+  sha256 "c02ce18b95c4e5021b95b8b461e5dbe6178edffc52a5f555cbca35b910559b5e"
   license "BSD-3-Clause"
   head "https://github.com/rust-lang/rust-bindgen.git", branch: "main"
 
