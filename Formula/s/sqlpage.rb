662f0891ced271923aafe0599c6b0b8cbea21341
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-02-04 03:05:23 +0000

    sqlpage: update 0.18.3 bottle.

--- Formula/s/sqlpage.rb
+++ Formula/s/sqlpage.rb
@@ -7,13 +7,13 @@ class Sqlpage < Formula
   head "https://github.com/lovasoa/SQLpage.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "ad868d6abf33b4b65a3f037b8f14f98cc1df099e37064e780ee3d731b9701174"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "df0a5bd7b5250484e807de7f989f275404b99cf3bba8d196e3d298b752a2fc14"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e4295538918d0042ebb77d772c975ce68ee830e4a4b3e854a871b94b9090c1e9"
-    sha256 cellar: :any_skip_relocation, sonoma:         "0cb78b972358d823e9d856fdaf876a68daa6afdc358e5647a47eec3c355f07db"
-    sha256 cellar: :any_skip_relocation, ventura:        "f20ba4780bb383a490990ba8106cda3924400f6a77d42e0f6dddfb75b2d15897"
-    sha256 cellar: :any_skip_relocation, monterey:       "a9f52f1fffba9bc926c650c70398ebedac6ddb51f0f5365881da922469033145"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "994f761b33320434825575aa53511a0cabda32462c1012702cf66394e44261b7"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "0bd74380774fc8475b4adb36e527fd2648647e59ccc99d58cb35ae01f197c38f"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "07883ba3303393b594558e80ca2bd7ec2745d8b18fc6d7f63dcfaf1af3375e5d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "7c6ffe3e3fc3a4ae32b06e51860c72368f197348f50dd9d7dc146983a5286a40"
+    sha256 cellar: :any_skip_relocation, sonoma:         "c5092d36c4095d6a7e7cae271b0853cb863bdd754377857b554fc91e65d36572"
+    sha256 cellar: :any_skip_relocation, ventura:        "f6f9a8c8c84a471b809f19f59b2527f7b290c608527362c7cec92c3f7b35d6da"
+    sha256 cellar: :any_skip_relocation, monterey:       "7bfa8dc911a7381dcb9dd5c93713ebcfb3b4d93d0f31b67aed98f27d4edfa1ab"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fa977065c6cc227bf20cd6eb0e55a1dbaa100524cdcccaee947f413f6575c773"
   end
 
   depends_on "rust" => :build
