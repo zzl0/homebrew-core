9fee520521a700736c9767b9a9ba4b20b51110f1
Porkepix <porkepix@gmail.com>
2024-02-04 20:56:07 +0100

    git-codereview 1.9.0

--- Formula/g/git-codereview.rb
+++ Formula/g/git-codereview.rb
@@ -1,8 +1,8 @@
 class GitCodereview < Formula
   desc "Tool for working with Gerrit code reviews"
   homepage "https://pkg.go.dev/golang.org/x/review/git-codereview"
-  url "https://github.com/golang/review/archive/refs/tags/v1.8.0.tar.gz"
-  sha256 "ec69d2778dd11e374fe3403c824f08284560c174ffc73aa596d8ba5f1d6ebf40"
+  url "https://github.com/golang/review/archive/refs/tags/v1.9.0.tar.gz"
+  sha256 "a71de773e760727a7365ba532cbf3e037a7150165eee56dca7fd7e00e89a364f"
   license "BSD-3-Clause"
 
   bottle do
