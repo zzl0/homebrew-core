7295d7a35c530f2e682f072615cbd7dedab7d7da
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-02-04 09:24:28 +0000

    cdk8s 2.198.39

--- Formula/c/cdk8s.rb
+++ Formula/c/cdk8s.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Cdk8s < Formula
   desc "Define k8s native apps and abstractions using object-oriented programming"
   homepage "https://cdk8s.io/"
-  url "https://registry.npmjs.org/cdk8s-cli/-/cdk8s-cli-2.198.38.tgz"
-  sha256 "8fbe7cb75a031a39b3e1b661f1bc825cf0d6bb593da92b690e2c98d017b0bdf9"
+  url "https://registry.npmjs.org/cdk8s-cli/-/cdk8s-cli-2.198.39.tgz"
+  sha256 "2543e52d469188e5ddcb14c836aca23b61cb5589153ec5e163c3766d126465d3"
   license "Apache-2.0"
 
   bottle do
