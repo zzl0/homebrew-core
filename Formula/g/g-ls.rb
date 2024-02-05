533e1386c910038b9d84734c3d8090dc15988065
Porkepix <porkepix@gmail.com>
2024-02-05 13:51:00 +0100

    g-ls 0.25.4

--- Formula/g/g-ls.rb
+++ Formula/g/g-ls.rb
@@ -1,8 +1,8 @@
 class GLs < Formula
   desc "Powerful and cross-platform ls"
   homepage "https://g.equationzhao.space"
-  url "https://github.com/Equationzhao/g/archive/refs/tags/v0.25.3.tar.gz"
-  sha256 "4c4fb3a1790cf4635795925d93f69db624a56f6d9abd8eb9a634c18a7be73f7a"
+  url "https://github.com/Equationzhao/g/archive/refs/tags/v0.25.4.tar.gz"
+  sha256 "e65b805911aea06a5de65cddef6ee3c43ae755b4b95e930fa9461a336c819647"
   license "MIT"
   head "https://github.com/Equationzhao/g.git", branch: "master"
 
