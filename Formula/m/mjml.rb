fa397e60f1647d386bf11d2321ed3d7661f772b0
Dustin Rodrigues <dust.rod@gmail.com>
2024-02-05 20:21:18 -0500

    mjml 4.15.3

--- Formula/m/mjml.rb
+++ Formula/m/mjml.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Mjml < Formula
   desc "JavaScript framework that makes responsive-email easy"
   homepage "https://mjml.io"
-  url "https://registry.npmjs.org/mjml/-/mjml-4.15.2.tgz"
-  sha256 "47ced5d72f001745b0edadd7075e7c8b1bac3fe4f0c479a0898ee9eb5158f0d2"
+  url "https://registry.npmjs.org/mjml/-/mjml-4.15.3.tgz"
+  sha256 "1918171314f07c1283a84f24bbf6e9cd1325906d48722b26a4b901f0eea00176"
   license "MIT"
 
   bottle do
