cc06babe20167231b4151e82c1b777bbd778bb59
Nhan Doan <2561973+dnhn@users.noreply.github.com>
2024-02-06 19:50:21 +0700

    auditbeat 8.12.1

--- Formula/a/auditbeat.rb
+++ Formula/a/auditbeat.rb
@@ -2,8 +2,8 @@ class Auditbeat < Formula
   desc "Lightweight Shipper for Audit Data"
   homepage "https://www.elastic.co/products/beats/auditbeat"
   url "https://github.com/elastic/beats.git",
-      tag:      "v8.12.0",
-      revision: "27c592782c25906c968a41f0a6d8b1955790c8c5"
+      tag:      "v8.12.1",
+      revision: "c7ec8f634ed6052674762b32fa640087d32f165f"
   license "Apache-2.0"
   head "https://github.com/elastic/beats.git", branch: "main"
 
