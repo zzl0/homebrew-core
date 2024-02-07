b35498918de3786e85f59d7512ecc278ddd5227c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-02-07 02:14:17 +0000

    prometheus-cpp: update 1.2.2 bottle.

--- Formula/p/prometheus-cpp.rb
+++ Formula/p/prometheus-cpp.rb
@@ -8,13 +8,13 @@ class PrometheusCpp < Formula
   head "https://github.com/jupp0r/prometheus-cpp.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "d15cfda2ab2021a48349562cf80c38791cf0f00bbe3307c5f9fb0376777e6743"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f30fd0dfcdce66b3e16a96aca355a2cfccdf163e2d5bd86a8163092a340c8e74"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "6f41eab01b174de60066a69737a62eeccf0032e069e6bfa73f0d7a3ecd171f49"
-    sha256 cellar: :any_skip_relocation, sonoma:         "4ba47b1dd934c0733998c98243674a9f181e8b71f78e6a84436e85faadc926bc"
-    sha256 cellar: :any_skip_relocation, ventura:        "8472d971fe145ab80badbaa82d3f779583893c4ecc956efaa224e96eb5eaf719"
-    sha256 cellar: :any_skip_relocation, monterey:       "07c4a71fbd219a5b0f2a4b8b977936a1ed0a3189591ddd8476ef1daf6f456549"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f4f90b8cf08ccc68ae1786d31cc815dcac867f15fa87b24c4cd9f865413cf4f7"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "af49d3c22a8cb1c8d872f6e46956e50eb68733ce23097ff872f69c5886f0721f"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "089f8b1956603b44a20db21c0b912306f0c18312e64ad08a42222b0bea4605e4"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "0975b622460f237086b287799f9c233db85142e260c781336df31c02485d4e23"
+    sha256 cellar: :any_skip_relocation, sonoma:         "79e7620de736994b87dc70adf81ad1f4fc89f6b503162e4a7046fc2e9f97b1e5"
+    sha256 cellar: :any_skip_relocation, ventura:        "f58738f31bebfb2df67565c3029fcf626aa456727d0660d8773722ae730fffde"
+    sha256 cellar: :any_skip_relocation, monterey:       "85286c3cd9484df1f9372b81b125e483a50cc10a12b0980784e2fb0963c8dcab"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e8c8c8a637e750c68972f7faa1e058d75677e8e9142bc70bd057482c1a08dffc"
   end
 
   depends_on "cmake" => :build
