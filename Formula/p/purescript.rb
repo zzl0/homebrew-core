a6c61ad3c5b0aaec18f60fdfa29daf3881a29a1c
Porkepix <porkepix@gmail.com>
2024-02-07 21:48:26 +0100

    purescript 0.15.15

--- Formula/p/purescript.rb
+++ Formula/p/purescript.rb
@@ -2,8 +2,8 @@ class Purescript < Formula
   desc "Strongly typed programming language that compiles to JavaScript"
   homepage "https://www.purescript.org/"
   # TODO: Try to switch `ghc@9.2` to `ghc` when purescript.cabal allows base>=4.17
-  url "https://hackage.haskell.org/package/purescript-0.15.14/purescript-0.15.14.tar.gz"
-  sha256 "450d8bce772ee0a3b7ded51466543649a105f4aedf2469fe1f7687233b638dd6"
+  url "https://hackage.haskell.org/package/purescript-0.15.15/purescript-0.15.15.tar.gz"
+  sha256 "9c4a23ea47ff09adc34e260610beabd940ec5c15088234cf120e8660dd220e67"
   license "BSD-3-Clause"
   head "https://github.com/purescript/purescript.git", branch: "master"
 
