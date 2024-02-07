ce23f90672a7b80535f102ad6fa80d14e1dbbac6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-02-07 09:29:42 +0000

    mysqltuner: update 2.5.2 bottle.

--- Formula/m/mysqltuner.rb
+++ Formula/m/mysqltuner.rb
@@ -7,15 +7,13 @@ class Mysqltuner < Formula
   head "https://github.com/major/MySQLTuner-perl.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "98d7ffd6b70d13f6dc196a3123a6714ce3df78e6f0ee7d69f330c5378b76b99a"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "98d7ffd6b70d13f6dc196a3123a6714ce3df78e6f0ee7d69f330c5378b76b99a"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "98d7ffd6b70d13f6dc196a3123a6714ce3df78e6f0ee7d69f330c5378b76b99a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "98d7ffd6b70d13f6dc196a3123a6714ce3df78e6f0ee7d69f330c5378b76b99a"
-    sha256 cellar: :any_skip_relocation, sonoma:         "7e9380ae45cc0494ebd7d96a4a8f140406c78f014756258a5e5e5839a130adbe"
-    sha256 cellar: :any_skip_relocation, ventura:        "7e9380ae45cc0494ebd7d96a4a8f140406c78f014756258a5e5e5839a130adbe"
-    sha256 cellar: :any_skip_relocation, monterey:       "7e9380ae45cc0494ebd7d96a4a8f140406c78f014756258a5e5e5839a130adbe"
-    sha256 cellar: :any_skip_relocation, big_sur:        "7e9380ae45cc0494ebd7d96a4a8f140406c78f014756258a5e5e5839a130adbe"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "98d7ffd6b70d13f6dc196a3123a6714ce3df78e6f0ee7d69f330c5378b76b99a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "98751a0d2ef92094e6b43cbc168cdb91747981b92f56705a0c0a47bb1e68b9b2"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "98751a0d2ef92094e6b43cbc168cdb91747981b92f56705a0c0a47bb1e68b9b2"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "98751a0d2ef92094e6b43cbc168cdb91747981b92f56705a0c0a47bb1e68b9b2"
+    sha256 cellar: :any_skip_relocation, sonoma:         "030ec7296fb4cd9f95fd85e2c1e33fdfedff8b7c80b3fce4596a0f322097438e"
+    sha256 cellar: :any_skip_relocation, ventura:        "030ec7296fb4cd9f95fd85e2c1e33fdfedff8b7c80b3fce4596a0f322097438e"
+    sha256 cellar: :any_skip_relocation, monterey:       "030ec7296fb4cd9f95fd85e2c1e33fdfedff8b7c80b3fce4596a0f322097438e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "98751a0d2ef92094e6b43cbc168cdb91747981b92f56705a0c0a47bb1e68b9b2"
   end
 
   # upstream PR ref, https://github.com/major/MySQLTuner-perl/pull/757
