88b6e2a8f5b5c1e51bc303c0c5d6bac050ea3ea5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-02-08 08:08:46 +0000

    chezscheme: update 10.0.0 bottle.

--- Formula/c/chezscheme.rb
+++ Formula/c/chezscheme.rb
@@ -6,10 +6,13 @@ class Chezscheme < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256                               sonoma:       "7bde84d1ef5d0878b0e544c02fd09d8721c8838940e039d140c99a3d7c2603c7"
-    sha256                               ventura:      "31392d3de2181da6ced8af77927f35d72a47ea68720ee1325f0019fb48e4d449"
-    sha256                               monterey:     "96ff356056109f07be45832cdca2c696c60a124ad9ef6d61ce94a5e6b36b26ab"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "9ed36679ce44c6093e5aa47bf9f1ffdcd5943e88ced7f27c572cecd56a9a9fb4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "041cfbb591db74e11611099248e00c428eb50fe5230140de1196de476389d89c"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "bb57159e3717a654c41103593f45476aa45638442e54f350d6463bdd26ba500a"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "708326531c59c33f0806ecd23fdb5fa1a0915da6523485d8299302424cc9cd07"
+    sha256 cellar: :any_skip_relocation, sonoma:         "554001e15c93bb99e0d5caabf9143f72c073b480e2d1c6abba7a5b57d6a52fb1"
+    sha256 cellar: :any_skip_relocation, ventura:        "2db40142397688c10ecb57bd74ae0249cd004668e6c50d09daa1f8acad54fe6a"
+    sha256 cellar: :any_skip_relocation, monterey:       "b46dcb58270a1e656fb18d4ff344c1078cd337af0b05efde2ac96e48fbd636f1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "55203016d10b96b4b99d96cea642c9a987e71182651f4509ccb3e5b2ce74d4bf"
   end
 
   depends_on "libx11" => :build
