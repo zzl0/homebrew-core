60396dc04d382f5ebcfa3680ae461da5abad6b8c
Timothy Sutton <tim@synthist.net>
2024-02-08 19:40:44 -0500

    cloudflared 2024.2.0

--- Formula/c/cloudflared.rb
+++ Formula/c/cloudflared.rb
@@ -1,8 +1,8 @@
 class Cloudflared < Formula
   desc "Cloudflare Tunnel client (formerly Argo Tunnel)"
   homepage "https://developers.cloudflare.com/cloudflare-one/connections/connect-apps/install-and-setup/tunnel-guide"
-  url "https://github.com/cloudflare/cloudflared/archive/refs/tags/2024.1.5.tar.gz"
-  sha256 "0a0da188e162680927ebafcef32c3366aed26661273dc63c540bbebee435bd4e"
+  url "https://github.com/cloudflare/cloudflared/archive/refs/tags/2024.2.0.tar.gz"
+  sha256 "62d8b3c32fcddee24e22707a22b4a33a014cd9314471e069f991fd359a102d75"
   license "Apache-2.0"
   head "https://github.com/cloudflare/cloudflared.git", branch: "master"
 
